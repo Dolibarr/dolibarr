c2209fc59c2a377719ab293704676ec20e8e7389
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2002-08-25 19:58:49 +0000

    *** empty log message ***

--- mysql/tables/llx_bank_account.sql
+++ mysql/tables/llx_bank_account.sql
@@ -25,5 +25,6 @@ create table llx_bank_account
   rowid          integer AUTO_INCREMENT PRIMARY KEY,
   label          varchar(30),
   bank           varchar(255),
-  number         varchar(255)
+  number         varchar(255),
+  courant        smallint default 0 not null
 );
