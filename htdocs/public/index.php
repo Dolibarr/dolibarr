19085af2355865ca14cd20f8ea391e8474ba02a5
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2002-12-21 17:39:28 +0000

    *** empty log message ***

---
+++ htdocs/public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>Dolibarr</title>
+  </head>
+
+  <body>
+    <h1>Section Public</h1>
+
+
+
+  </body>
+</html>
