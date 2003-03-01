4d9416a20eb63cf446703e46f3113522102c93c9
jlb <jlb>
2003-03-01 16:30:47 +0000

    reajout suite a fausse manip du Rodo :-))

---
+++ mysql/tables/drop.sql
@@ -0,0 +1,139 @@
+--
+-- Copyright (C) 2001-2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+
+drop table if exists actioncomm;
+
+drop table if exists bots;
+
+drop table if exists c_anexpe;
+
+drop table if exists c_actioncomm;
+
+drop table if exists c_chargesociales;
+
+drop table if exists c_effectif;
+
+drop table if exists c_paiement ;
+
+drop table if exists c_pays ;
+
+drop table if exists c_prestatype ;
+
+drop table if exists c_propalst ;
+
+drop table if exists c_stcomm;
+
+drop table if exists c_typent ;
+
+drop table if exists llx_adherent;
+
+drop table if exists llx_adherent_type;
+
+drop table if exists llx_adherent_options;
+
+drop table if exists llx_adherent_options_label;
+
+drop table if exists llx_bank;
+
+drop table if exists llx_bank_account;
+
+drop table if exists llx_bank_categ;
+
+drop table if exists llx_bank_class;
+
+drop table if exists llx_bookmark;
+
+drop table if exists llx_boxes;
+
+drop table if exists llx_boxes_def;
+
+drop table if exists llx_chargesociales;
+
+drop table if exists llx_compta;
+
+drop table if exists llx_compta_account;
+
+drop table if exists llx_const;
+
+drop table if exists llx_cotisation;
+
+drop table if exists llx_don;
+
+drop table if exists llx_don_projet;
+
+drop table if exists llx_fa_pr;
+
+drop table if exists llx_facture;
+
+drop table if exists llx_facturedet;
+
+drop table if exists llx_facture_fourn;
+
+drop table if exists llx_fichinter;
+
+drop table if exists llx_paiement;
+
+drop table if exists llx_pointmort;
+
+drop table if exists llx_product;
+
+drop table if exists llx_projet;
+
+drop table if exists llx_propal;
+
+drop table if exists llx_propaldet;
+
+drop table if exists llx_service;
+
+drop table if exists llx_soc_recontact;
+
+drop table if exists llx_tva;
+
+drop table if exists llx_user;
+
+drop table if exists llx_ventes;
+
+drop table if exists llx_voyage;
+
+drop table if exists llx_voyage_reduc;
+
+drop table if exists login ;
+
+drop table if exists socappoint ;
+
+drop table if exists soccontact;
+
+drop table if exists soccontactinfo;
+
+drop table if exists socfollowresume;
+
+drop table if exists socstatutlog ;
+
+drop table if exists region_resume ;
+
+drop table if exists societe;
+
+drop table if exists soc_events;
+
+drop table if exists soc_recontact;
+
+drop table if exists socpeople;
+
