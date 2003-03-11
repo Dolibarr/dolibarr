9f58dd3882e5aa13e7704d6c76696ff4efceee9d
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-03-11 15:59:08 +0000

    Update copyright notice

--- mysql/tables/llx_adherent_type.sql
+++ mysql/tables/llx_adherent_type.sql
@@ -1,7 +1,6 @@
 -- ===================================================================
--- Copyright (C) 2003 Jean-Louis Bergamo <jlb@j1b.org>
--- $Id$
--- $Source$
+-- Copyright (C) 2002-2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+-- Copyright (C) 2002-2003 Jean-Louis Bergamo <jlb@j1b.org>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -16,6 +15,9 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
 --
 -- ===================================================================
 
