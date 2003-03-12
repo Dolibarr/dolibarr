6872acde9fb717eac6d8e4810c1ae6e830b3cf05
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-03-12 00:53:17 +0000

    Nouveau fichier

---
+++ htdocs/boutique/livre/pre.inc.php
@@ -0,0 +1,55 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+require("../../main.inc.php3");
+require("./livre.class.php");
+require("../editeur/editeur.class.php");
+require("../auteur/auteur.class.php");
+require("../../product/categorie/categorie.class.php");
+
+function llxHeader($head = "", $urlp = "") {
+  global $user, $conf;
+
+  /*
+   *
+   *
+   */
+  top_menu($head);
+
+  $menu = new Menu();
+
+  $menu->add("/boutique/livre/", "Livres");
+
+  $menu->add_submenu("/boutique/livre/fiche.php?&action=create","Nouvel ouvrage");
+
+  $menu->add("/boutique/auteur/", "Auteurs");
+
+  $menu->add_submenu("/boutique/auteur/fiche.php?&action=create","Nouvel auteur");
+
+  $menu->add("/product/osc-reviews.php", "Critiques");
+
+  $menu->add_submenu("/product/osc-productsbyreviews.php", "Meilleurs produits");
+
+  $menu->add("/product/categorie/", "Catégories");
+
+  left_menu($menu->liste);
+}
+?>
