b733147634202ab4d5d5b1f322516f033c705251
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-03-13 19:13:27 +0000

    Nouveaux fichiers

---
+++ mysql/tables/llx_album_to_groupart.sql
@@ -0,0 +1,29 @@
+-- ============================================================================
+-- Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- ============================================================================
+
+create table llx_album_to_groupart
+(
+  fk_album        integer NOT NULL,
+  fk_groupart     integer NOT NULL
+);
+
+alter table  llx_album_to_groupart add unique key (fk_album, fk_groupart);
