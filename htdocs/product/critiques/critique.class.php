7b7396d47dd7758f6480a20a2bee3a963605f6f6
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-03-24 18:01:32 +0000

    Nouveau fichier

---
+++ htdocs/product/critiques/critique.class.php
@@ -0,0 +1,72 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+class Critique {
+  var $db ;
+
+  var $id ;
+  var $nom;
+
+  Function Critique($DB, $id=0) {
+    $this->db = $DB;
+    $this->id = $id ;
+  }  
+  /*
+   *
+   *
+   *
+   */
+  Function fetch ($id) {
+    
+    
+    $sql = "SELECT r.reviews_id, r.reviews_rating, d.reviews_text, p.products_name";
+
+    $sql .= " FROM ".DB_NAME_OSC.".reviews as r, ".DB_NAME_OSC.".reviews_description as d";
+    $sql .= " ,".DB_NAME_OSC.".products_description as p";
+
+    $sql .= " WHERE r.reviews_id = d.reviews_id AND r.products_id=p.products_id";
+    $sql .= " AND p.language_id = ".OSC_LANGUAGE_ID. " AND d.languages_id=".OSC_LANGUAGE_ID;
+    $sql .= " AND r.reviews_id=$id";
+
+    $result = $this->db->query($sql) ;
+
+    if ( $result )
+      {
+	$result = $this->db->fetch_array();
+
+	$this->id           = $result["reviews_id"];
+	$this->product_name = stripslashes($result["products_name"]);
+	$this->text         = stripslashes($result["reviews_text"]);
+	
+	$this->db->free();
+      }
+    else
+      {
+	print $this->db->error();
+	print "<p>$sql";
+      }
+    
+    return $result;
+  }
+
+}
+?>
---
+++ htdocs/product/critiques/fiche.php
@@ -0,0 +1,76 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+require("./pre.inc.php");
+
+llxHeader();
+
+$db = new Db();
+
+if ($id)
+{
+
+  $critique = new Critique($db);
+  $result = $critique->fetch($id);
+  
+  if ( $result )
+    { 
+
+      print '<div class="titre">Fiche Critique</div><br>';
+      
+      print '<table border="1" width="100%" cellspacing="0" cellpadding="4">';
+      print "<tr>";
+      print '<td width="20%">Produit</td><td width="80%">'.$critique->product_name.'</td></tr>';
+
+      print '<tr><td width="20%">Texte</td><td width="80%">'.nl2br($critique->text).'</td></tr>';
+      print "</table>";
+      
+
+
+    }
+  else
+    {
+      print "Fetch failed";
+    }
+  
+}
+
+/* ************************************************************************** */
+/*                                                                            */ 
+/* Barre d'action                                                             */ 
+/*                                                                            */ 
+/* ************************************************************************** */
+
+print '<br><table width="100%" border="1" cellspacing="0" cellpadding="3">';
+print '<td width="20%" align="center">-</td>';
+print '<td width="20%" align="center">-</td>';
+print '<td width="20%" align="center">-</td>';
+print '<td width="20%" align="center">-</td>';
+print '<td width="20%" align="center">-</td>';    
+print '</table><br>';
+
+
+
+$db->close();
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
