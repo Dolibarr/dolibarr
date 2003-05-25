62b6801beaf071973e53b093532592a77a13eb40
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-05-25 16:10:02 +0000

    Nouveau fichier

---
+++ mysql/rename-tables.sql
@@ -0,0 +1,7 @@
+alter table societe rename llx_societe ;
+
+alter table socpeople rename llx_socpeople ;
+
+alter table socstatutlog rename llx_socstatutlog ;
+
+alter table actioncomm rename llx_actioncomm ;
