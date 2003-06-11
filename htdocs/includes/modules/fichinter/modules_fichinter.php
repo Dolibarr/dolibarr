790baf0b2861c168cf242953eeccd532f80956c5
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-06-11 13:32:01 +0000

    nouveau fichier

---
+++ htdocs/includes/modules/fichinter/modules_fichinter.php
@@ -0,0 +1,47 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * or see http://www.gnu.org/
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+function fichinter_pdf_create($db, $facid)
+{
+  
+  $dir = $GLOBALS["DOCUMENT_ROOT"]."/includes/modules/fichinter/";
+
+  if (defined("FICHEINTER_ADDON_PDF"))
+    {
+
+      $file = "pdf_".FICHEINTER_ADDON_PDF.".modules.php";
+
+      $classname = "pdf_".FICHEINTER_ADDON_PDF;
+      require_once($dir.$file);
+
+      $obj = new $classname($db);
+
+      return $obj->write_pdf_file($facid);
+    }
+  else
+    {
+      return 0;
+    }
+}
+
+?>
