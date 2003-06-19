8b76046d563c6ae491d6092e2825327fdfe2e593
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-06-19 12:38:34 +0000

    Nouveau fichier

---
+++ htdocs/admin/propale.php
@@ -0,0 +1,117 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ */
+require("./pre.inc.php");
+
+llxHeader();
+
+if (!$user->admin)
+{
+  print "Forbidden";
+  llxfooter();
+  exit;
+}
+
+$db = new Db();
+
+// positionne la variable pour le test d'affichage de l'icone
+
+$propale_addon_var_pdf = PROPALE_ADDON_PDF;
+
+if ($action == 'setpdf')
+{
+  $sql = "REPLACE INTO llx_const SET name = 'PROPALE_ADDON_PDF', value='".$value."', visible=0";
+
+  if ($db->query($sql))
+    {
+      // la constante qui a été lue en avant du nouveau set
+      // on passe donc par une variable pour avoir un affichage cohérent
+      $propale_addon_var_pdf = $value;
+    }
+}
+
+$dir = "../includes/modules/propale/";
+
+/*
+ * PDF
+ */
+
+print_titre("Modèles de propale pdf");
+
+print '<table border="1" cellpadding="3" cellspacing="0">';
+print '<TR class="liste_titre">';
+print '<td>Nom</td>';
+print '<td>Info</td>';
+print '<td align="center">Activé</td>';
+print '<td>&nbsp;</td>';
+print "</TR>\n";
+
+clearstatcache();
+
+$handle=opendir($dir);
+
+while (($file = readdir($handle))!==false)
+{
+  if (substr($file, strlen($file) -12) == '.modules.php' && substr($file,0,4) == 'pdf_')
+    {
+      $name = substr($file, 4, strlen($file) -16);
+      $classname = substr($file, 0, strlen($file) -12);
+
+      print '<tr><td>';
+      echo "$name";
+      print "</td><td>\n";
+      require_once($dir.$file);
+      $obj = new $classname();
+      
+      print $obj->description;
+
+      print '</td><td align="center">';
+
+      if ($propale_addon_var_pdf == "$name")
+	{
+	  print '<img src="/theme/'.$conf->theme.'/img/tick.png" border="0"></a>';
+	}
+      else
+	{
+	  print "&nbsp;";
+	}
+
+      print "</td><td>\n";
+
+      print '<a href="propale.php?action=setpdf&value='.$name.'">activer</a>';
+
+      print '</td></tr>';
+    }
+}
+closedir($handle);
+
+print '</table>';
+
+
+/*
+ *
+ *
+ */
+
+
+$db->close();
+
+llxFooter();
+?>
