8b44ba9a3a5c6385f9719c8904f5b82a63c4e6b8
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-06-21 15:21:18 +0000

    Modif table llx_propaldet

--- mysql/migration/0.1.1-0.1.2.sql
+++ mysql/migration/0.1.1-0.1.2.sql
@@ -8,3 +8,7 @@ alter table llx_product modify price real ;
 alter table llx_product add tva_tx real ;
 
 update llx_product set tva_tx = 19.6 ;
+
+alter table llx_propaldet add tva_tx real ;
+
+alter table llx_propaldet add description text ;
