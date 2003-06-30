ab1d0aefe66fbdbe5009bb730b56fb9d4f5b92e7
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-06-30 14:50:02 +0000

    nouveau fichier

---
+++ htdocs/societe/pre.inc.php
@@ -0,0 +1,55 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org> 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+require ("../main.inc.php3");
+
+function llxHeader($head = "")
+{
+  global $user, $conf;
+
+  /*
+   *
+   *
+   */
+  top_menu($head);
+
+  $menu = new Menu();
+
+  if ($conf->societe && $conf->commercial->enabled) 
+    {
+      $menu->add(DOL_URL_ROOT."/societe.php", "Sociétés","company");
+
+      $menu->add_submenu(DOL_URL_ROOT."/soc.php3?&action=create", "Nouvelle société");
+
+      $menu->add_submenu(DOL_URL_ROOT."/contact.php", "Contacts");
+
+      $menu->add_submenu("notify/index.php", "Notifications");
+    }
+
+  /*
+   *
+   */
+
+  left_menu($menu->liste);
+
+}
+?>
