0dff95b8de4c3795ccd2376457f455901ec822c0
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-07-07 16:38:19 +0000

    Ajout DOL_URL_ROOT

--- htdocs/admin/fichinter.php
+++ htdocs/admin/fichinter.php
@@ -87,7 +87,7 @@ while (($file = readdir($handle))!==false)
 
       if ($ficheinter_addon_var_pdf == "$name")
 	{
-	  print '<img src="/theme/'.$conf->theme.'/img/tick.png" border="0"></a>';
+	  print '<img src="'.DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/tick.png" border="0"></a>';
 	}
       else
 	{
