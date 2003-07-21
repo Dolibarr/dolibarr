d5a95209bc6c7241c1fc1978db571bcd46c24ab6
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-07-21 09:45:00 +0000

    Nouvelle version

--- mysql/migration/0.2.3-0.2.4.sql
+++ mysql/migration/0.2.3-0.2.4.sql
@@ -12,7 +12,7 @@ alter table llx_product modify tva_tx double default 19.6 ;
 
 create table llx_cond_reglement
 (
-  rowid           integer AUTO_INCREMENT PRIMARY KEY,
+  rowid           integer PRIMARY KEY,
   sortorder       smallint,
   actif           tinyint default 1,
   libelle         varchar(255),
@@ -20,3 +20,16 @@ create table llx_cond_reglement
   fdm             tinyint,    -- reglement fin de mois
   nbjour          smallint
 );
+
+alter table llx_facture add fk_cond_reglement integer; 
+
+alter table llx_facture add date_lim_reglement date ;
+
+insert into llx_cond_reglement values (1,1,1, "A réception","Réception de facture",0,0);
+insert into llx_cond_reglement values (2,2,1, "30 jours","Réglement à 30 jours",0,30);
+insert into llx_cond_reglement values (3,3,1, "30 jours fin de mois","Réglement à 30 jours fin de mois",1,30);
+insert into llx_cond_reglement values (4,4,1, "60 jours","Réglement à 60 jours",0,60);
+insert into llx_cond_reglement values (5,5,1, "60 jours fin de mois","Réglement à 60 jours fin de mois",1,60);
+
+update llx_facture set fk_cond_reglement = 1 where fk_cond_reglement IS NULL ;
+update llx_facture set date_lim_reglement = datef where date_lim_reglement IS NULL ;
