ed501bc1081d632ba4e045123dba0b367f8a08d6
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-07-23 14:04:32 +0000

    Nouveau fichier

---
+++ htdocs/includes/modules/modExternalRss.class.php
@@ -0,0 +1,72 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+class modExternalRss
+{
+
+  /*
+   * Initialisation
+   *
+   */
+
+  Function modExternalRss($DB)
+  {
+    $this->db = $DB ;
+  }
+  /*
+   *
+   *
+   *
+   */
+
+  Function init()
+  {
+    /*
+     *  Activation du module
+     */
+    
+    $sql = "INSERT INTO llx_boxes_def (name,file) VALUES('Syndication', 'box_external_rss.php');";
+
+    if ( $this->db->query($sql) )
+      {
+	
+      }
+  }
+  /*
+   *
+   *
+   */
+  Function remove()
+  {
+    /*
+     *  Activation du module
+     */
+    
+    $sql = "DELETE FROM llx_boxes_def WHERE file = 'box_external_rss.php';";
+
+    if ( $this->db->query($sql) )
+      {
+	
+      }
+  }
+}
+?>
