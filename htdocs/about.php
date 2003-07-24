37d32f5c7a45067aefc30b5efde568925ef775cb
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-07-24 11:50:45 +0000

    nouveau fichier

---
+++ htdocs/about.php
@@ -0,0 +1,80 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+require("./pre.inc.php3");
+
+llxHeader();
+
+print_fiche_titre( "Dolibarr ".DOL_VERSION,'');
+
+?>
+
+<p>
+Dolibarr est publié sous licence GNU/GPL
+</p>
+
+<p>
+Dolibarr est développé par :
+<ul>
+<li><a target="blank" href="http://rodolphe.quiedeville.org">Rodolphe Quiédeville</a>
+</ul>
+D'autres développeurs y contribuent activement :
+<ul>
+<li>Jean-Louis Bergamo</li>
+<li>Eric Seigne</li>
+</ul>
+
+<p>
+Informations :
+<ul>
+<li>
+<a target="blank" href="http://rodolphe.quiedeville.org/projets/dolibarr/">page officielle</a> du projet
+<li>
+<a target="blank" href="http://freshmeat.net/projects/dolibarr/">Page sur Freshmeat</a>
+
+
+<li>
+Les tâches en cours de réalisation sur Dolibarr sont consultable dans le <a target="blank" href="http://savannah.nongnu.org/pm/task.php?group_id=1915&group_project_id=0&func=browse&advsrch=0&set=open">gestionnaire de projet</a> sur Savannah.
+</li>
+
+<li>
+Si vous trouvez un bug sur Dolibarr vous pouvez en informer les développeurs sur le <a target="blank" href="http://savannah.gnu.org/bugs/?group_id=1915">système de gestion des bug</a> de Savannah.
+
+</li>
+
+<li>
+Le code source de Dolibarr est consultable par l'<a target="blank" href="http://savannah.nongnu.org/cgi-bin/viewcvs/dolibarr/dolibarr/">interface web du cvs</a>.
+</li>
+</ul>
+<?php
+
+llxFooter();
+?>
+
+
+
+
+
+
+
+
+
+
