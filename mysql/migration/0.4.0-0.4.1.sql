c840be744274200dac664d70a641a88fa7ee0625
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-08-04 19:45:05 +0000

    ajout llx_rights

--- mysql/migration/0.4.0-0.4.1.sql
+++ mysql/migration/0.4.0-0.4.1.sql
@@ -5,3 +5,28 @@
 
 alter table llx_user add fk_socpeople integer default 0;
 alter table llx_socpeople add fk_user integer default 0;
+
+
+
+create table llx_rights_def
+(
+  id            integer PRIMARY KEY,
+  libelle       varchar(255),
+  module        varchar(12),
+  type          enum('r','w','m','d','a')
+);
+
+
+
+insert into llx_rights_def values (1,'Tous les droits','all','a');
+insert into llx_rights_def values (10,'Tous les droits sur les factures','facture','a');
+insert into llx_rights_def values (11,'Lire les factures','facture','r');
+insert into llx_rights_def values (12,'Créer modifier les factures','facture','w');
+insert into llx_rights_def values (13,'Modifier les factures d\'autrui','facture','m');
+insert into llx_rights_def values (14,'Supprimer les factures','facture','d');
+
+insert into llx_rights_def values (20,'Tous les droits sur les propositions commerciales','propale','a');
+insert into llx_rights_def values (21,'Lire les propositions commerciales','propale','r');
+insert into llx_rights_def values (22,'Créer modifier les propositions commerciales','propale','w');
+insert into llx_rights_def values (23,'Modifier les propositions commerciales d\'autrui','propale','m');
+insert into llx_rights_def values (24,'Supprimer les propositions commerciales','propale','d');
