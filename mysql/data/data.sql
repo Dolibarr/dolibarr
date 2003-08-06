75f8e681b2c3518633dd02f9f011e54e78c34000
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-08-06 09:08:54 +0000

    ajout des droits sur les utilisateurs

--- mysql/data/data.sql
+++ mysql/data/data.sql
@@ -21,21 +21,35 @@
 -- Valeurs pour les bases de langues francaises
 --
 
+insert into llx_rights_def values (1,'Tous les droits','all','a',0);
+insert into llx_rights_def values (10,'Tous les droits sur les factures','facture','a',0);
+insert into llx_rights_def values (11,'Lire les factures','facture','r',1);
+insert into llx_rights_def values (12,'Créer modifier les factures','facture','w',0);
+insert into llx_rights_def values (13,'Modifier les factures d\'autrui','facture','m',0);
+insert into llx_rights_def values (14,'Supprimer les factures','facture','d',0);
 
+insert into llx_rights_def values (20,'Tous les droits sur les propositions commerciales','propale','a',0);
+insert into llx_rights_def values (21,'Lire les propositions commerciales','propale','r',1);
+insert into llx_rights_def values (22,'Créer modifier les propositions commerciales','propale','w',0);
+insert into llx_rights_def values (23,'Modifier les propositions commerciales d\'autrui','propale','m',0);
+insert into llx_rights_def values (24,'Supprimer les propositions commerciales','propale','d',0);
 
-insert into llx_rights_def values (1,'Tous les droits','all','a');
-insert into llx_rights_def values (10,'Tous les droits sur les factures','facture','a');
-insert into llx_rights_def values (11,'Lire les factures','facture','r');
-insert into llx_rights_def values (12,'Créer modifier les factures','facture','w');
-insert into llx_rights_def values (13,'Modifier les factures d\'autrui','facture','m');
-insert into llx_rights_def values (14,'Supprimer les factures','facture','d');
+insert into llx_rights_def values (30,'Tous les droits sur les produits','produit','a',0);
+insert into llx_rights_def values (31,'Lire les produits','produit','r',1);
+insert into llx_rights_def values (32,'Créer modifier les produits','produit','w',0);
+insert into llx_rights_def values (33,'Modifier les produits d\'autrui','produit','m',0);
+insert into llx_rights_def values (34,'Supprimer les produits','produit','d',0);
 
-insert into llx_rights_def values (20,'Tous les droits sur les propositions commerciales','propale','a');
-insert into llx_rights_def values (21,'Lire les propositions commerciales','propale','r');
-insert into llx_rights_def values (22,'Créer modifier les propositions commerciales','propale','w');
-insert into llx_rights_def values (23,'Modifier les propositions commerciales d\'autrui','propale','m');
-insert into llx_rights_def values (24,'Supprimer les propositions commerciales','propale','d');
+insert into llx_rights_def values (40,'Tous les droits sur les projets','projet','a',0);
+insert into llx_rights_def values (41,'Lire les projets','projet','r',1);
+insert into llx_rights_def values (42,'Créer modifier les projets','projet','w',0);
+insert into llx_rights_def values (43,'Modifier les projets d\'autrui','projet','m',0);
+insert into llx_rights_def values (44,'Supprimer les projets','projet','d',0);
 
+insert into llx_rights_def values (50,'Tous les droits sur les utilisateurs','utilisateur','a',0);
+insert into llx_rights_def values (51,'Lire les utilisateurs','utilisateur','r',1);
+insert into llx_rights_def values (52,'Créer modifier les utilisateurs','utilisateur','w',0);
+insert into llx_rights_def values (54,'Supprimer les utilisateurs','utilisateur','d',0);
 
 
 insert into llx_cond_reglement values (1,1,1, "A réception","Réception de facture",0,0);
