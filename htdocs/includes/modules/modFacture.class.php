e55e9a38331d6a984f24192975665dbdc93c187f
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-08-12 13:27:27 +0000

    Ajout permissions

--- htdocs/includes/modules/modFacture.class.php
+++ htdocs/includes/modules/modFacture.class.php
@@ -95,12 +95,16 @@ class modFacture
     /*
      * Permissions
      */
-    $sql = array();
-    $sql[0] = "insert into llx_rights_def values (10,'Tous les droits sur les factures','facture','a',0);";
-    $sql[1] = "insert into llx_rights_def values (11,'Lire les factures','facture','r',1);";
-    $sql[2] = "insert into llx_rights_def values (12,'Créer modifier les factures','facture','w',0);";
-    //    $sql[3] = "insert into llx_rights_def values (13,'Modifier les factures d\'autrui','facture','m',0);";
-    $sql[3] = "insert into llx_rights_def values (14,'Supprimer les factures','facture','d',0);";
+    $sql = array(
+		 "insert into llx_rights_def values (10,'Tous les droits sur les factures','facture','a',0);",
+		 "insert into llx_rights_def values (11,'Lire les factures','facture','r',1);",
+		 "insert into llx_rights_def values (12,'Créer modifier les factures','facture','w',0);",
+		 //"insert into llx_rights_def values (13,'Modifier les factures d\'autrui','facture','m',0);",
+		 "insert into llx_rights_def values (14,'Valider les factures','facture','d',0);",
+		 "insert into llx_rights_def values (15,'Envoyer les factures aux clients','facture','d',0);",
+		 "insert into llx_rights_def values (16,'Emettre des paiements sur les factures','facture','d',0);",
+		 "insert into llx_rights_def values (19,'Supprimer les factures','facture','d',0);"
+		 );
     
     for ($i = 0 ; $i < sizeof($sql) ; $i++)
       {
