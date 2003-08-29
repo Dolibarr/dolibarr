b808c9762984192dab23d6298f812af79912a2e7
jlb <jlb>
2003-08-29 10:12:51 +0000

    creation du script de migration de la prochaine version de dolibarr - ajout d'un variable pour la partie adherent

---
+++ mysql/migration/0.5.1-0.5.2.sql
@@ -0,0 +1,6 @@
+
+--
+-- Mise à jour de la version 0.5.1 à 0.5.2
+--
+
+insert into llx_const(name, value, type, note, visible) values ('ADH_TEXT_NEW_ADH','','texte','Texte d\'entete du formaulaire d\'adhesion en ligne',0);
