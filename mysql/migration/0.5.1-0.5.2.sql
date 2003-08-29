6428162486f91a9b0ddf0bea0d094dd022c894eb
jlb <jlb>
2003-08-29 13:00:50 +0000

    Rajout des constantes suivantes : ADH_CARD_HEADER_TEXT : texte d'entente de la carte d'adherent ADH_CARD_TEXT : texte de la carte d'adherent ADH_CARD_FOOTER_TEXT : texte de base de carte d'adherent

--- mysql/migration/0.5.1-0.5.2.sql
+++ mysql/migration/0.5.1-0.5.2.sql
@@ -4,3 +4,6 @@
 --
 
 insert into llx_const(name, value, type, note, visible) values ('ADH_TEXT_NEW_ADH','','texte','Texte d\'entete du formaulaire d\'adhesion en ligne',0);
+insert into llx_const(name, value, type, note, visible) values ('ADH_CARD_HEADER_TEXT','%ANNEE%','string','Texte imprime sur le haut de la carte adherent',0);
+insert into llx_const(name, value, type, note, visible) values ('ADH_CARD_FOOTER_TEXT','Association FreeLUG http://www.freelug.org/','string','Texte imprime sur le bas de la carte adherent',0);
+insert into llx_const(name, value, type, note, visible) values ('ADH_CARD_TEXT','%TYPE% n° %ID%\r\n%PRENOM% %NOM%\r\n<%EMAIL%>\r\n%ADRESSE%\r\n%CP% %VILLE%\r\n%PAYS%','texte','Texte imprime sur la carte adherent',0);
