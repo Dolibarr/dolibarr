c2f63a3b8e360b076f89539d6f2df8c0389b4ad8
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-08-29 20:44:42 +0000

    Nouveau fichier

---
+++ htdocs/includes/boxes/box_services_vendus.php
@@ -0,0 +1,58 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+$info_box_head = array();
+$info_box_head[] = array('text' => "Les 5 derniers services vendus");
+
+$info_box_contents = array();
+
+$sql = "SELECT s.nom,s.idp";
+$sql .= " FROM llx_societe as s, llx_contrat as c WHERE s.idp = c.fk_soc";  
+$sql .= " ORDER BY c.tms DESC ";
+/*
+ * Il faudrait ajouter un datec à cette table
+ *
+ */
+$sql .= $db->plimit(5, 0);
+
+$result = $db->query($sql);
+
+if ($result) 
+{
+  $num = $db->num_rows();
+    
+  $i = 0;
+    
+  while ($i < $num)
+    {
+      $objp = $db->fetch_object( $i);
+      
+      $info_box_contents[$i][0] = array('align' => 'left',
+					'text' => $objp->nom,
+					'url' => DOL_URL_ROOT."/comm/fiche.php3?socid=".$objp->idp);
+
+      $i++;
+    }
+}
+
+new infoBox($info_box_head, $info_box_contents);
+?>
