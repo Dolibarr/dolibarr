8810f6817dcacaaaf812d39cf09152111b8c6746
jlb <jlb>
2003-08-31 22:01:26 +0000

    ajout d'un pre.inc specifique

---
+++ htdocs/adherents/cartes/pre.inc.php
@@ -0,0 +1,65 @@
+<?PHP
+/* Copyright (C) 2001-2002 Rodolphe Quiedeville <rodolphe@quiedeville.org> 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+require("../../main.inc.php3");
+
+function llxHeader($head = "") {
+  global $user, $conf;
+
+
+  /*
+   *
+   *
+   */
+  top_menu($head);
+
+  $menu = new Menu();
+
+  $menu->add("index.php","Adherents");
+  $menu->add_submenu("liste.php?statut=1","Adhérents à ce jour");
+  $menu->add_submenu("liste.php?statut=-1","Adhésions à valider");
+
+  $menu->add_submenu("liste.php?statut=0","Adhésions résiliées");
+  $menu->add_submenu("cartes/carte.php","Cartes d'adhérents");
+  if ($user->admin)
+    {
+      $menu->add("fiche.php?action=create","Nouvel adhérent");
+    }
+
+  $menu->add("/public/adherents/","Espace adherents public");
+
+  $menu->add("index.php","Comptabilite");
+  $menu->add_submenu("cotisations.php","Cotisations");
+  $menu->add_submenu("/compta/bank/","Bank");
+
+  if ($user->admin)
+    {
+      $menu->add("index.php","Configuration");
+      $menu->add_submenu("type.php","Type d'adhérent");
+      $menu->add_submenu("options.php","Champs optionnels");
+      $menu->add_submenu("/admin/adherent.php","Constantes");
+    }
+
+  left_menu($menu->liste);
+
+}
+
+?>
