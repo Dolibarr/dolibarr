471ceb5875bf61eb4316ec69f3847362757a4ebb
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-08-31 21:52:50 +0000

    Nouveau fichier

---
+++ htdocs/includes/modules/propale/mod_propale_ivoire.php
@@ -0,0 +1,43 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * or see http://www.gnu.org/
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+function propale_get_num($objsoc=0)
+{ 
+  global $db;
+
+  $sql = "SELECT count(*) FROM llx_propale";
+
+  if ( $db->query($sql) ) 
+    {
+      $row = $db->fetch_row(0);
+      
+      $num = $row[0];
+    }
+
+  $y = strftime("%y",time());
+
+  return  "PR" . "$y" . substr("000".$num, strlen("000".$num)-4,4);
+
+}
+
+?>
