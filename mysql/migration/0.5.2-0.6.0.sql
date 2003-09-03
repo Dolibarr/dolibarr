30f88ced58d38df3ed7722e7ba46214e7488378c
jlb <jlb>
2003-09-03 14:03:02 +0000

    -rajout d'un champ dans la table llx_cotisation -rajout de constantes pour faire le lien avec la gestion banquaire

--- mysql/migration/0.5.2-0.6.0.sql
+++ mysql/migration/0.5.2-0.6.0.sql
@@ -16,3 +16,10 @@ create table llx_deplacement
   fk_soc          integer,
   note            text
 );
+
+insert into llx_const(name, value, type, note, visible) values ('ADHERENT_BANK_USE','0','yesno','Utilisation de la gestion banquaire',0);
+insert into llx_const(name, value, type, note, visible) values ('ADHERENT_BANK_USE_AUTO','0','yesno','Insertion automatique des cotisation dans le compte banquaire',0);
+insert into llx_const(name, value, type, note, visible) values ('ADHERENT_BANK_ACCOUNT','','string','ID du Compte banquaire utilise',0);
+insert into llx_const(name, value, type, note, visible) values ('ADHERENT_BANK_CATEGORIE','','string','ID de la categorie banquaire des cotisations',0);
+
+alter table llx_cotisation ADD fk_bank int(11);
