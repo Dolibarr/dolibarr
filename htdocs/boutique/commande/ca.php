393550305584d8b2f0ceb7c034e0e62db296a846
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-09-04 11:59:46 +0000

    Nouveau fichier

---
+++ htdocs/boutique/commande/ca.php
@@ -0,0 +1,106 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+require("./pre.inc.php");
+
+llxHeader();
+
+if ($sortfield == "")
+{
+  $sortfield="date_purchased";
+}
+if ($sortorder == "")
+{
+  $sortorder="DESC";
+}
+
+if ($page == -1) { $page = 0 ; }
+$limit = $conf->liste_limit;
+$offset = $limit * $page ;
+
+print_barre_liste("Liste des commandes", $page, $PHP_SELF);
+
+print '<p><TABLE border="0" cellspacing="0" cellpadding="4">';
+print '<tr class="liste_titre"><td>Description</td>';
+print '<td align="right">Total</td></tr>';
+
+$sql = "SELECT sum(t.value) as value";
+$sql .= " FROM ".DB_NAME_OSC.".orders_total as t";
+$sql .= " WHERE t.class = 'ot_subtotal'";
+ 
+if ( $db->query($sql) )
+{
+  $num = $db->num_rows();
+
+  $var=True;
+  if ($num > 0)
+    {
+      $objp = $db->fetch_object(0);
+      $var=!$var;
+      print "<TR $bc[$var]>";
+      print '<td>Somme des commandes</td>';
+      print '<td align="right">'.price($objp->value).'</td>';
+
+      print "</TR>\n";
+      $i++;
+    }
+
+  $db->free();
+}
+else
+{
+  print $db->error();
+}
+
+$sql = "SELECT sum(t.value) as value";
+$sql .= " FROM ".DB_NAME_OSC.".orders_total as t";
+$sql .= " WHERE t.class = 'ot_shipping'";
+ 
+if ( $db->query($sql) )
+{
+  $num = $db->num_rows();
+
+  $var=True;
+  if ($num > 0)
+    {
+      $objp = $db->fetch_object(0);
+      $var=!$var;
+      print "<TR $bc[$var]>";
+      print '<td>Somme des frais de port</td>';
+      print '<td align="right">'.price($objp->value).'</td></tr>';
+      $i++;
+    }
+
+  $db->free();
+}
+else
+{
+  print $db->error();
+}
+
+
+print "</table>";
+
+$db->close();
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
