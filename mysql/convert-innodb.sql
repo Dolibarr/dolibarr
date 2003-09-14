51050626cf0d7d011d5f342390ebf2d26e58925e
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-09-14 15:54:58 +0000

    Nouveau fichier

---
+++ mysql/convert-innodb.sql
@@ -0,0 +1,174 @@
+--
+-- Copyright (C) 2001-2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+
+alter table c_actioncomm type=INNODB;
+
+alter table c_chargesociales type=INNODB;
+
+alter table c_effectif type=INNODB;
+
+alter table c_paiement  type=INNODB;
+
+alter table c_pays  type=INNODB;
+
+alter table c_propalst  type=INNODB;
+
+alter table c_stcomm type=INNODB;
+
+alter table c_typent  type=INNODB;
+
+alter table llx_action_def type=INNODB;
+
+alter table llx_actioncomm type=INNODB;
+
+alter table llx_adherent type=INNODB;
+
+alter table llx_adherent_type type=INNODB;
+
+alter table llx_adherent_options type=INNODB;
+
+alter table llx_adherent_options_label type=INNODB;
+
+alter table llx_album type=INNODB;
+
+alter table llx_album_to_groupart  type=INNODB;
+
+alter table llx_appro  type=INNODB;
+
+alter table llx_auteur type=INNODB;
+
+alter table llx_bank type=INNODB;
+
+alter table llx_bank_account type=INNODB;
+
+alter table llx_bank_categ type=INNODB;
+
+alter table llx_bank_class type=INNODB;
+
+alter table llx_bookmark type=INNODB;
+
+alter table llx_boxes type=INNODB;
+
+alter table llx_boxes_def type=INNODB;
+
+alter table llx_chargesociales type=INNODB;
+
+alter table llx_contrat type=INNODB;
+
+alter table llx_compta type=INNODB;
+
+alter table llx_compta_account type=INNODB;
+
+alter table llx_concert  type=INNODB;
+
+alter table llx_cond_reglement  type=INNODB;
+
+alter table llx_const type=INNODB;
+
+alter table llx_cotisation type=INNODB;
+
+alter table llx_deplacement type=INNODB;
+
+alter table llx_domain  type=INNODB;
+
+alter table llx_don type=INNODB;
+
+alter table llx_don_projet type=INNODB;
+
+alter table llx_editeur type=INNODB;
+
+alter table llx_fa_pr type=INNODB;
+
+alter table llx_facture type=INNODB;
+
+alter table llx_facturedet type=INNODB;
+
+alter table llx_facture_fourn type=INNODB;
+
+alter table llx_facture_fourn_det type=INNODB;
+
+alter table llx_fichinter type=INNODB;
+
+alter table llx_groupart type=INNODB;
+
+alter table llx_lieu_concert  type=INNODB;
+
+alter table llx_livre type=INNODB;
+
+alter table llx_livre_to_auteur  type=INNODB;
+
+alter table llx_newsletter type=INNODB;
+
+alter table llx_notify  type=INNODB;
+
+alter table llx_notify_def type=INNODB;
+
+alter table llx_paiement type=INNODB;
+
+alter table llx_paiementfourn type=INNODB;
+
+alter table llx_pointmort type=INNODB;
+
+alter table llx_product type=INNODB;
+
+alter table llx_product_price type=INNODB;
+
+alter table llx_projet type=INNODB;
+
+alter table llx_propal type=INNODB;
+
+alter table llx_propal_model_pdf type=INNODB;
+
+alter table llx_propaldet type=INNODB;
+
+alter table llx_rights_def type=INNODB;
+
+alter table llx_service type=INNODB;
+
+alter table llx_societe type=INNODB;
+
+alter table llx_socpeople type=INNODB;
+
+alter table llx_soc_events type=INNODB;
+
+alter table llx_soc_recontact type=INNODB;
+
+alter table llx_socstatutlog  type=INNODB;
+
+alter table llx_sqltables type=INNODB;
+
+alter table llx_todocomm  type=INNODB;
+
+alter table llx_transaction_bplc  type=INNODB;
+
+alter table llx_tva type=INNODB;
+
+alter table llx_user type=INNODB;
+
+alter table llx_user_rights type=INNODB;
+
+alter table llx_ventes type=INNODB;
+
+alter table llx_voyage type=INNODB;
+
+alter table llx_voyage_reduc type=INNODB;
+
+
