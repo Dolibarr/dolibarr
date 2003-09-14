ab1ab849f204d769bd6b93efe838711fb71b611b
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-09-14 12:08:14 +0000

    Nouveau fichier

---
+++ htdocs/stats.php
@@ -0,0 +1,69 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+require("./pre.inc.php");
+
+llxHeader();
+
+$mesg = '';
+
+/*
+ *
+ *
+ */
+$sql = array(
+	     array("Société","SELECT count(*) FROM llx_societe"),
+	     array("Contacts","SELECT count(*) FROM llx_socpeople"),
+	     array("Facture","SELECT count(*) FROM llx_facture"),
+	     array("Proposition commerciales","SELECT count(*) FROM llx_propal")
+);
+
+
+print_fiche_titre('Statistiques produits et services', $mesg);
+      
+print '<table class="liste" border="1" width="100%" cellspacing="0" cellpadding="4">';
+
+foreach ($sql as $key => $value)
+{
+  $titre = $sql[$key][0];
+
+  if ($db->query($sql[$key][1]))
+    {
+      $row = $db->fetch_row(0);
+      $nbhv = $row[0];
+      $db->free();
+
+      print "<tr $bc[1]>";
+      print '<td width="40%">'.$titre.'</td>';
+      print '<td>'.$nbhv.'</td></tr>';
+
+    }
+
+
+}
+
+print '</table>';
+
+$db->close();
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
