61b167f4d485da7b077bf6e736528b5d003858df
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-09-15 18:15:36 +0000

    modif table llx_facture_tva_sum

--- mysql/migration/1.0.0_RC1-1.0.0_RC2.sql
+++ mysql/migration/1.0.0_RC1-1.0.0_RC2.sql
@@ -5,7 +5,6 @@
 
 create table llx_facture_tva_sum
 (
-  rowid         integer AUTO_INCREMENT PRIMARY KEY,
   fk_facture    integer NOT NULL,
   amount        real,
   tva_tx        real default 19.6,
