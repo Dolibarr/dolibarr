396af63f6d7e12a95ab76a31b333f9ba1c46f46e
jlb <jlb>
2003-09-15 15:21:27 +0000

    mise a jour des valeurs de constantes et des droits

--- htdocs/includes/modules/modAdherent.class.php
+++ htdocs/includes/modules/modAdherent.class.php
@@ -39,81 +39,75 @@ class modAdherent extends modDolibarrModules
     $this->const = array();
     $this->boxes = array();
 
-    $this->const[0][0] = "ADHERENT_MAIL_RESIL";
-    $this->const[0][1] = "texte";
-    $this->const[0][2] = "Mail de résiliation";
+    $this->const[0]= array("ADHERENT_MAIL_RESIL","texte","Votre adhesion sur %SERVEUR% vient d'etre resilie.\r\nNous esperons vous revoir tres bientot","Mail de résiliation");
 
-    $this->const[1][0] = "ADHERENT_MAIL_VALID";
-    $this->const[1][1] = "texte";
-    $this->const[1][2] = "Mail de validation";
+    $this->const[1]=array("ADHERENT_MAIL_VALID","texte","Votre adhesion vient d'etre validee. \r\nVoici le rappel de vos coordonnees (toute information erronee entrainera la non validation de votre inscription) :\r\n\r\n%INFO%\r\n\r\nVous pouvez a tout moment, grace a votre login et mot de passe, modifier vos coordonnees a l'adresse suivante : \r\n%SERVEUR%public/adherents/","Mail de validation");
 
-    $this->const[2][0] = "ADHERENT_MAIL_EDIT";
-    $this->const[2][1] = "texte";
-    $this->const[2][2] = "Mail d'edition";
+    $this->const[2]= array("ADHERENT_MAIL_EDIT","texte","Voici le rappel des coordonnees que vous avez modifiees (toute information erronee entrainera la non validation de votre inscription) :\r\n\r\n%INFO%\r\n\r\nVous pouvez a tout moment, grace a votre login et mot de passe, modifier vos coordonnees a l'adresse suivante :\r\n%SERVEUR%public/adherents/","Mail d'edition");
 
-    $this->const[3] = array("ADHERENT_MAIL_RESIL","texte","Mail de résiliation");
+    $this->const[3] = array("ADHERENT_MAIL_RESIL","texte","Votre adhesion sur %SERVEUR% vient d'etre resilie.\r\nNous esperons vous revoir tres bientot","Mail de résiliation");
 
-    $this->const[4] = array("ADHERENT_MAIL_NEW","texte","Mail de nouvel inscription");
+    $this->const[4] = array("ADHERENT_MAIL_NEW","texte","Merci de votre inscription. Votre adhesion devrait etre rapidement validee.^M\nVoici le rappel des coordonnees que vous avez rentrees (toute information erronee entrainera la non validation de votre inscription) :\r\n\r\n%INFO%\r\n\r\nVous pouvez a tout moment, grace a votre login et mot de passe, modifier vos coordonnees a l'adresse suivante :\r\n%SERVEUR%public/adherents/","Mail de nouvel inscription");
 
-    $this->const[5] = array("ADHERENT_MAIL_VALID_SUBJECT","chaine","sujet du mail de validation");
+    $this->const[5] = array("ADHERENT_MAIL_VALID_SUBJECT","chaine"," Votre adh<E9>sion a ete valid<E9>e sur %SERVEUR%","sujet du mail de validation");
 
-    $this->const[6] = array("ADHERENT_MAIL_RESIL_SUBJECT","chaine","sujet du mail de resiliation");
+    $this->const[6] = array("ADHERENT_MAIL_RESIL_SUBJECT","chaine","Resiliation de votre adhesion sur %SERVEUR% ","sujet du mail de resiliation");
 
-    $this->const[7] = array("ADHERENT_MAIL_NEW_SUBJECT","chaine","Sujet du mail de nouvelle adhesion");
+    $this->const[7] = array("ADHERENT_MAIL_NEW_SUBJECT","chaine","Bienvenue sur %SERVEUR%","Sujet du mail de nouvelle adhesion");
 
-    $this->const[8] = array("ADHERENT_MAIL_EDIT_SUBJECT","chaine","Sujet du mail d'edition");
+    $this->const[8] = array("ADHERENT_MAIL_EDIT_SUBJECT","chaine","Votre fiche a ete editee sur %SERVEUR%","Sujet du mail d'edition");
 
-    $this->const[9] = array("ADHERENT_GLASNOST_SERVEUR","chaine","serveur glasnost");
+    $this->const[9] = array("ADHERENT_GLASNOST_SERVEUR","chaine","","serveur glasnost");
 
-    $this->const[10] = array("ADHERENT_MAILMAN_UNSUB_URL","chaine","Url de desinscription aux listes mailman");
+    $this->const[10] = array("ADHERENT_MAILMAN_UNSUB_URL","chaine","http://%SERVER%/cgi-bin/mailman/admin/%LISTE%/members?adminpw=%ADMINPW%&user=%EMAIL%","Url de desinscription aux listes mailman");
 
-    $this->const[11] = array("ADHERENT_MAILMAN_URL","chaine","url pour les inscriptions mailman");
+    $this->const[11] = array("ADHERENT_MAILMAN_URL","chaine","http://%SERVER%/cgi-bin/mailman/admin/%LISTE%/members?adminpw=%ADMINPW%&send_welcome_msg_to_this_batch=1&subscribees=%EMAIL%","url pour les inscriptions mailman");
 
-    $this->const[12] = array("ADHERENT_MAILMAN_LISTS","chaine","Listes auxquelles les nouveaux adhérents sont inscris");
+    $this->const[12] = array("ADHERENT_MAILMAN_LISTS","chaine","","Listes auxquelles les nouveaux adhérents sont inscris");
 
-    $this->const[13] = array("ADHERENT_GLASNOST_USER","chaine","Administrateur glasnost");
+    $this->const[13] = array("ADHERENT_GLASNOST_USER","chaine","","Administrateur glasnost");
 
-    $this->const[14] = array("ADHERENT_GLASNOST_PASS","chaine","password de l'administrateur");
+    $this->const[14] = array("ADHERENT_GLASNOST_PASS","chaine","","password de l'administrateur");
 
-    $this->const[15] = array("ADHERENT_USE_GLASNOST_AUTO","yesno","inscription automatique a glasnost ?");
+    $this->const[15] = array("ADHERENT_USE_GLASNOST_AUTO","yesno","","inscription automatique a glasnost ?");
 
-    $this->const[16] = array("ADHERENT_USE_SPIP_AUTO","yesno","Utilisation de SPIP automatiquement");
+    $this->const[16] = array("ADHERENT_USE_SPIP_AUTO","yesno","","Utilisation de SPIP automatiquement");
 
-    $this->const[17] = array("ADHERENT_SPIP_USER","chaine","Utilisateur de connection a la base spip");
+    $this->const[17] = array("ADHERENT_SPIP_USER","chaine","","Utilisateur de connection a la base spip");
 
-    $this->const[18] = array("ADHERENT_SPIP_PASS","chaine","Mot de passe de connection a la base spip");
+    $this->const[18] = array("ADHERENT_SPIP_PASS","chaine","","Mot de passe de connection a la base spip");
 
-    $this->const[19] = array("ADHERENT_SPIP_SERVEUR","chaine","serveur spip");
+    $this->const[19] = array("ADHERENT_SPIP_SERVEUR","chaine","","serveur spip");
 
-    $this->const[20] = array("ADHERENT_SPIP_DB","chaine","db spip");
+    $this->const[20] = array("ADHERENT_SPIP_DB","chaine","","db spip");
 
-    $this->const[21] = array("ADHERENT_MAIL_FROM","chaine","From des mails");
+    $this->const[21] = array("ADHERENT_MAIL_FROM","chaine","","From des mails");
 
-    $this->const[22] = array("ADHERENT_MAIL_COTIS","texte","Mail de validation de cotisation");
+    $this->const[22] = array("ADHERENT_MAIL_COTIS","texte","Bonjour %PRENOM%,^M\n^M\nCet email confirme que votre cotisation a ete recue\r\net enregistree","Mail de validation de cotisation");
 
-    $this->const[23] = array("ADHERENT_MAIL_COTIS_SUBJECT","chaine","sujet du mail de validation de cotisation");
+    $this->const[23] = array("ADHERENT_MAIL_COTIS_SUBJECT","chaine"," Recu de votre cotisation","sujet du mail de validation de cotisation");
 
-    $this->const[24] = array("ADHERENT_TEXT_NEW_ADH","texte","Texte d'entete du formaulaire d'adhesion en ligne");
+    $this->const[24] = array("ADHERENT_TEXT_NEW_ADH","texte","","Texte d'entete du formaulaire d'adhesion en ligne");
 
-    $this->const[25] = array("ADHERENT_CARD_HEADER_TEXT","chaine","Texte imprime sur le haut de la carte adherent");
+    $this->const[25] = array("ADHERENT_CARD_HEADER_TEXT","chaine","%ANNEE%","Texte imprime sur le haut de la carte adherent");
 
-    $this->const[26] = array("ADHERENT_CARD_FOOTER_TEXT","chaine","Texte imprime sur le bas de la carte adherent");
+    $this->const[26] = array("ADHERENT_CARD_FOOTER_TEXT","chaine","Association %SERVER%","Texte imprime sur le bas de la carte adherent");
 
-    $this->const[27] = array("ADHERENT_CARD_TEXT","texte","Texte imprime sur la carte adherent");
+    $this->const[27] = array("ADHERENT_CARD_TEXT","texte","%PRENOM% %NOM%\r\nMembre n° %ID%\r\n%EMAIL%\r\n%ADRESSE%\r\n%CP% %VILLE%\r\n%PAYS%","Texte imprime sur la carte adherent");
 
-    $this->const[28] = array("ADHERENT_MAILMAN_ADMINPW","chaine","Mot de passe Admin des liste mailman");
+    $this->const[28] = array("ADHERENT_MAILMAN_ADMINPW","chaine","","Mot de passe Admin des liste mailman");
 
-    $this->const[29] = array("ADHERENT_MAILMAN_SERVER","chaine","Serveur hebergeant les interfaces d'Admin des listes mailman");
+    $this->const[29] = array("ADHERENT_MAILMAN_SERVER","chaine","","Serveur hebergeant les interfaces d'Admin des listes mailman");
 
-    $this->const[30] = array("ADHERENT_MAILMAN_LISTS_COTISANT","chaine","Liste(s) auxquelles les nouveaux cotisants sont inscris automatiquement");
+    $this->const[30] = array("ADHERENT_MAILMAN_LISTS_COTISANT","chaine","","Liste(s) auxquelles les nouveaux cotisants sont inscris automatiquement");
 
-    $this->const[31] = array("ADHERENT_BANK_USE_AUTO","yesno","Insertion automatique des cotisation dans le compte banquaire");
+    $this->const[31] = array("ADHERENT_BANK_USE_AUTO","yesno","","Insertion automatique des cotisation dans le compte banquaire");
 
-    $this->const[32] = array("ADHERENT_BANK_ACCOUNT","chaine","ID du Compte banquaire utilise");
+    $this->const[32] = array("ADHERENT_BANK_ACCOUNT","chaine","","ID du Compte banquaire utilise");
 
-    $this->const[33] = array("ADHERENT_BANK_CATEGORIE","chaine","ID de la categorie banquaire des cotisations");
+    $this->const[33] = array("ADHERENT_BANK_CATEGORIE","chaine","","ID de la categorie banquaire des cotisations");
 
-    $this->const[34] = array("ADHERENT_ETIQUETTE_TYPE","chaine","Type d etiquette (pour impression de planche d etiquette)");
+    $this->const[34] = array("ADHERENT_ETIQUETTE_TYPE","chaine","L7163","Type d etiquette (pour impression de planche d etiquette)");
   }
   /*
    *
@@ -127,7 +121,11 @@ class modAdherent extends modDolibarrModules
      * Permissions
      */
     $sql = array(
-		 "insert into llx_rights_def values (10,'Tous les droits sur les adherents','adherent','a',0);"
+		 "insert into llx_rights_def values (70,'Tous les droits sur les adherents','adherent','a',0);",
+		 "insert into llx_rights_def values (71,'Lire les fiche adherents','adherent','r',1);",
+		 "insert into llx_rights_def values (72,'Créer modifier des adherents','adherent','w',0);",
+		 "insert into llx_rights_def values (73,'Modifier les adherents','adherent','m',0);",
+		 "insert into llx_rights_def values (74,'Supprimer les adherents','adherent','d',0);"
 		 );
     
     return $this->_init($sql);
