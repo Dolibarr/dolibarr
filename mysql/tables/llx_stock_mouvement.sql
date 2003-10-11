9c54da4d67321cb89dee5087145a41d073a065a5
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-10-11 18:53:13 +0000

    Nouvelles tables

---
+++ mysql/tables/llx_stock_mouvement.sql
@@ -0,0 +1,37 @@
+-- ============================================================================
+-- Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- ============================================================================
+
+create table llx_stock_mouvement
+(
+  rowid           integer AUTO_INCREMENT PRIMARY KEY,
+  tms             timestamp,
+  datem           datetime,
+  fk_product      integer NOT NULL,
+  fk_stock        integer NOT NULL,
+  value           integer,
+  type_mouvement  smallint,
+  fk_user_author  integer,
+
+  key(fk_product),
+  key(fk_stock)
+)
+
