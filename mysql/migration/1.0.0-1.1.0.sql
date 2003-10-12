98c53eb2472ff1fa5c6a6d3a85233bd647e9932b
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-10-12 18:25:08 +0000

    Ajout des tables pour les factures recurrentes

--- mysql/migration/1.0.0-1.1.0.sql
+++ mysql/migration/1.0.0-1.1.0.sql
@@ -14,7 +14,7 @@ alter table llx_facturedet add remise_percent real default 0;
 alter table llx_facturedet add remise real default 0;
 alter table llx_facturedet add subprice real default 0;
 
-alter table llx_facturedet modify fk_product NOT NULL default 0;
+alter table llx_facturedet modify fk_product integer NOT NULL default 0;
 
 create table llx_product_fournisseur
 (
@@ -30,3 +30,40 @@ create table llx_product_fournisseur
   key(fk_soc)
 );
 
+create table llx_facture_rec
+(
+  rowid              integer AUTO_INCREMENT PRIMARY KEY,
+  titre              varchar(50) NOT NULL,
+  fk_soc             integer NOT NULL,
+  datec              datetime,  -- date de creation
+
+  amount             real     default 0 NOT NULL,
+  remise             real     default 0,
+  remise_percent     real     default 0,
+  tva                real     default 0,
+  total              real     default 0,
+  total_ttc          real     default 0,
+
+  fk_user_author     integer,   -- createur
+  fk_projet          integer,   -- projet auquel est associé la facture
+  fk_cond_reglement  integer,   -- condition de reglement
+
+  note               text,
+
+  INDEX fksoc (fk_soc)
+);
+
+
+create table llx_facturedet_rec
+(
+  rowid           integer AUTO_INCREMENT PRIMARY KEY,
+  fk_facture      integer NOT NULL,
+  fk_product      integer,
+  description     text,
+  tva_taux       real default 19.6, -- taux tva
+  qty		 real,              -- quantité
+  remise_percent real default 0,    -- pourcentage de remise
+  remise         real default 0,    -- montant de la remise
+  subprice       real,              -- prix avant remise
+  price          real               -- prix final
+);
