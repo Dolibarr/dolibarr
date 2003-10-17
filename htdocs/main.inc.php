d6c35b1e68e3cb3d90414790eb3c8450e51f2004
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-10-17 18:00:13 +0000

    Ajout link rel

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -346,6 +346,12 @@ function top_menu($head, $title="")
   //  print '<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">';
   //  print '<BASE href="'.DOL_URL_ROOT.'/">';
 
+  //print '<link rel="home" title="Accueil" href="/" />'; TODO RODO
+  //  print '<link rel="help" title="HowToUseWiki" href="HowToUseWiki" />';
+
+  print '<link rel="copyright" title="GNU General Public License" href="http://www.gnu.org/copyleft/gpl.html#SEC1" />';
+  print '<link rel="author" title="Equipe de développement" href="http://www.dolibarr.com/dev.fr.html" />';
+
   print '<LINK REL="stylesheet" TYPE="text/css" HREF="'.DOL_URL_ROOT.'/'.$conf->css.'">';
   print '<LINK REL="stylesheet" TYPE="text/css" media="print" HREF="'.DOL_URL_ROOT.'/theme/print.css">';
   print "\n";
