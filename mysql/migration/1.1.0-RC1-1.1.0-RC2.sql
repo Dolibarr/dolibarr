50d946a77c814cfa91e43bdb4e185b4831c58dc7
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-10-21 16:31:30 +0000

    Ajout table llx_bank_url

--- mysql/migration/1.1.0-RC1-1.1.0-RC2.sql
+++ mysql/migration/1.1.0-RC1-1.1.0-RC2.sql
@@ -9,8 +9,16 @@ delete from llx_boxes;
 
 update llx_facturedet set subprice = price where remise_percent = 0 ;
 
-
 drop table if exists llx_entrepot;
 drop table if exists llx_product_stock;
 drop table if exists llx_stock;
 drop table if exists llx_stock_mouvement;
+
+create table llx_bank_url
+(
+  rowid           integer AUTO_INCREMENT PRIMARY KEY,
+  fk_bank         integer,
+  url_id          integer,
+  url             varchar(255),
+  label           varchar(255)
+);
