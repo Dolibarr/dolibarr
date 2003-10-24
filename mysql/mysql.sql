64c80100601f6ec5e129d7e220a9a09efe0f3480
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-10-24 08:15:59 +0000

    Nouvelle version

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -1,4 +1,13 @@
 
+create table llx_bank_url
+(
+  rowid           integer AUTO_INCREMENT PRIMARY KEY,
+  fk_bank         integer,
+  url_id          integer,
+  url             varchar(255),
+  label           varchar(255)
+);
+
 create table llx_actioncomm
 (
   id             integer AUTO_INCREMENT PRIMARY KEY,
