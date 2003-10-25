799bb4f22f1043ad7e3e5b69db52f80e46e10cb9
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-10-25 10:10:10 +0000

    Mise aux normes

--- htdocs/product/stats/facture.php
+++ htdocs/product/stats/facture.php
@@ -40,11 +40,10 @@ if ($id)
     { 
       print_fiche_titre('Fiche produit : '.$product->ref, $mesg);
       
-      print '<table border="1" width="100%" cellspacing="0" cellpadding="4">';
-      print "<tr>";
+      print '<table class="border" width="100%" cellspacing="0" cellpadding="4"><tr>';
       print '<td width="20%">Référence</td><td width="40%"><a href="../fiche.php?id='.$product->id.'">'.$product->ref.'</a></td>';
       print '<td><a href="fiche.php?id='.$id.'">Statistiques</a></td></tr>';
-      print "<td>Libellé</td><td>$product->libelle</td>";
+      print "<tr><td>Libellé</td><td>$product->libelle</td>";
       print '<td valign="top" rowspan="2">';
       print "Propositions commerciales : ".$product->count_propale();
       print "<br>Proposé à <b>".$product->count_propale_client()."</b> clients";
@@ -69,7 +68,7 @@ if ($id)
 	  $sortfield="f.datef";
 	}
       
-      print_barre_liste("Factures",$page,$PHP_SELF,"&id=$id",$sortfield,$sortorder);
+      print_barre_liste("Factures",$page,$PHP_SELF,"&amp;id=$id",$sortfield,$sortorder);
       
       $sql = "SELECT distinct(f.rowid), s.nom,s.idp,f.facnumber,f.amount,".$db->pdate("f.datef")." as df,f.paye,f.rowid as facid";
       $sql .= " FROM llx_societe as s,llx_facture as f, llx_facturedet as d WHERE f.fk_soc = s.idp";
@@ -86,9 +85,9 @@ if ($id)
 	print '<TR class="liste_titre">';
 	print '<TD>Num&eacute;ro</TD>';
 	print '<td>';
-	print_liste_field_titre("Société",$PHP_SELF,"s.nom","","&socidp=$socidp");
+	print_liste_field_titre("Société",$PHP_SELF,"s.nom","","&amp;socidp=$socidp");
 	print '</td><TD align="right">';
-	print_liste_field_titre("Date",$PHP_SELF,"f.datef","","&socidp=$socidp");
+	print_liste_field_titre("Date",$PHP_SELF,"f.datef","","&amp;socidp=$socidp");
 	print '</td><TD align="right">Montant</TD>';
 	print '<td>&nbsp;</td>';
 	print "</TR>\n";
