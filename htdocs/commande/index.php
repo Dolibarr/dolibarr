4a3b75636dae58d86b6db21f9bcbae749c002d15
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-11-09 18:00:27 +0000

    Correction bug url fiche societe

--- htdocs/commande/index.php
+++ htdocs/commande/index.php
@@ -65,7 +65,7 @@ if ( $db->query($sql) )
 	  $var=!$var;
 	  $obj = $db->fetch_object($i);
 	  print "<tr $bc[$var]><td width=\"20%\"><a href=\"fiche.php?id=$obj->rowid\">$obj->ref</a></td>";
-	  print '<td><a href="fiche.php?socid='.$obj->idp.'">'.$obj->nom.'</a></td></tr>';
+	  print '<td><a href="'.DOL_URL_ROOT.'/comm/fiche.php?socid='.$obj->idp.'">'.$obj->nom.'</a></td></tr>';
 	  $i++;
 	}
       print "</table><br>";
