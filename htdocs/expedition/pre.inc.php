f40d4cd94844e7e1d8f428d0d17c93ad832f40fe
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-11-13 17:31:43 +0000

    Nouveau fichier

---
+++ htdocs/expedition/pre.inc.php
@@ -0,0 +1,50 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+require("../main.inc.php");
+require("../commande/commande.class.php");
+require("./expedition.class.php");
+
+$form_search = '<form method="post" action="liste.php">
+Rechercher une expédition<br>
+<input class="flat" type="text" name="sf_ref" size="10">&nbsp;<input type="submit" value="go" class="flat">
+</form>';
+
+function llxHeader($head = "", $title="", $help_url='', $form_search='')
+{
+  global $user, $conf, $form_search;
+
+  /*
+   *
+   *
+   */
+  top_menu($head, $title);
+
+  $menu = new Menu();
+
+  $menu->add(DOL_URL_ROOT."/expedition/", "Expeditions");
+  $menu->add_submenu(DOL_URL_ROOT."/expedition/liste.php", "Liste");
+
+  $menu->add(DOL_URL_ROOT."/commande/", "Commandes");
+
+  left_menu($menu->liste, $help_url, $form_search);
+}
+?>
