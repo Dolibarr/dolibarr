92524592cca861a94d67b17aa91ff2766f330580
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-11-17 09:53:28 +0000

    Nouveau fichier

---
+++ htdocs/expedition/stats/expeditionstats.class.php
@@ -0,0 +1,113 @@
+<?PHP
+/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+class ExpeditionStats 
+{
+  var $db ;
+
+  Function ExpeditionStats($DB)
+    {
+      $this->db = $DB;
+    }
+  /**
+   * Renvoie le nombre de expedition par année
+   *
+   */
+  Function getNbExpeditionByYear()
+  {
+    $result = array();
+    $sql = "SELECT date_format(date_expedition,'%Y') as dm, count(*)  FROM llx_expedition GROUP BY dm DESC WHERE fk_statut > 0";
+    if ($this->db->query($sql))
+      {
+	$num = $this->db->num_rows();
+	$i = 0;
+	while ($i < $num)
+	  {
+	    $row = $this->db->fetch_row($i);
+	    $result[$i] = $row;
+
+	    $i++;
+	  }
+	$this->db->free();
+      }
+    return $result;
+  }
+  /**
+   * Renvoie le nombre de expedition par mois pour une année donnée
+   *
+   */
+  Function getNbExpeditionByMonth($year)
+  {
+    $result = array();
+    $sql = "SELECT date_format(date_expedition,'%m') as dm, count(*)  FROM llx_expedition";
+    $sql .= " WHERE date_format(date_expedition,'%Y') = $year AND fk_statut > 0";
+    $sql .= " GROUP BY dm DESC";
+
+    if ($this->db->query($sql))
+      {
+	$num = $this->db->num_rows();
+	$i = 0;
+	while ($i < $num)
+	  {
+	    $row = $this->db->fetch_row($i);
+	    $j = $row[0] * 1;
+	    $result[$j] = $row[1];
+	    $i++;
+	  }
+	$this->db->free();
+      }
+    
+    for ($i = 1 ; $i < 13 ; $i++)
+      {
+	$res[$i] = $result[$i] + 0;
+      }
+
+    $data = array();
+    
+    for ($i = 1 ; $i < 13 ; $i++)
+      {
+	$data[$i-1] = array(strftime("%b",mktime(12,12,12,$i,1,$year)), $res[$i]);
+      }
+
+    return $data;
+  }
+
+
+  Function getNbExpeditionByMonthWithPrevYear($year)
+  {
+    $data1 = $this->getNbExpeditionByMonth($year);
+    $data2 = $this->getNbExpeditionByMonth($year - 1);
+
+    $data = array();
+
+    for ($i = 1 ; $i < 13 ; $i++)
+      {
+	$data[$i-1] = array(strftime("%b",mktime(12,12,12,$i,1,$year)), 
+			    $data1[$i][1],
+			    $data2[$i][1]);
+      }
+    return $data;
+  }
+
+}
+
+?>
---
+++ htdocs/expedition/stats/index.php
@@ -0,0 +1,59 @@
+<?PHP
+/* Copyright (C) 2001-2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+require("./pre.inc.php");
+require("../expedition.class.php");
+require("../../graph.class.php");
+
+llxHeader();
+/*
+ *
+ *
+ */
+
+print_fiche_titre('Statistiques expéditions', $mesg);
+      
+print '<table class="border" width="100%" cellspacing="0" cellpadding="4">';
+print '<tr><td align="center">Année</td><td width="40%" align="center">Nb d\'expédition</td></tr>';
+
+$sql = "SELECT count(*), date_format(date_expedition,'%Y') as dm FROM llx_expedition WHERE fk_statut > 0 GROUP BY dm DESC ";
+if ($db->query($sql))
+{
+  $num = $db->num_rows();
+  $i = 0;
+  while ($i < $num)
+    {
+      $row = $db->fetch_row($i);
+      $nbproduct = $row[0];
+      $year = $row[1];
+      print "<tr>";
+      print '<td align="center"><a href="month.php?year='.$year.'">'.$year.'</a></td><td align="center">'.$nbproduct.'</td></tr>';
+      $i++;
+    }
+}
+$db->free();
+
+print '</table><br><i>Statistiques effectuées sur les expéditions validées uniquement</i>';
+
+$db->close();
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
