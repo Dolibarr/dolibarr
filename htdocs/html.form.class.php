b7b9af03af3ed528668329de319ea45fd4771877
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-11-19 14:36:57 +0000

    Modif presentation html dans form_confirm

--- htdocs/html.form.class.php
+++ htdocs/html.form.class.php
@@ -40,7 +40,7 @@ class Form {
   {
     print '<form method="post" action="'.$page.'">';
     print '<input type="hidden" name="action" value="'.$action.'">';
-    print '<table cellspacing="0" border="1" width="100%" cellpadding="3">';
+    print '<table cellspacing="0" class="border=" width="100%" cellpadding="3">';
     print '<tr><td colspan="3">'.$title.'</td></tr>';
     
     print '<tr><td class="valid">'.$question.'</td><td class="valid">';
