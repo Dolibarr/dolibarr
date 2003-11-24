9ffa926bc852d775646fbdb418e78719afab2933
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2003-11-24 15:52:41 +0000

    Ajout du champs description

--- mysql/tables/llx_expedition_methode.sql
+++ mysql/tables/llx_expedition_methode.sql
@@ -26,5 +26,6 @@ create table llx_expedition_methode
   tms              timestamp,
   code             varchar(30) NOT NULL,
   libelle          varchar(50) NOT NULL,
+  description      text,
   statut           tinyint default 0
 );
