241668581adb79f753cc2b53f4aa67a1a89caf85
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-01-23 12:53:56 +0000

    Nouveau fichier

---
+++ htdocs/includes/modules/modLdap.class.php
@@ -0,0 +1,71 @@
+<?PHP
+/* Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+include_once "DolibarrModules.class.php";
+
+class modLdap extends DolibarrModules
+{
+  /*
+   * Initialisation
+   *
+   */
+  Function modLdap($DB)
+  {
+    $this->db = $DB ;
+
+    $this->name = "Ldap";
+    $this->description = "Synchronisation Ldap";
+    $this->const_name = "MAIN_MODULE_LDAP";
+    $this->const_config = MAIN_MODULE_LDAP;
+    $this->config_page_url = "ldap.php";
+    $this->depends = array();
+
+    $this->const = array();
+    $this->boxes = array();
+  }
+  /*
+   *
+   *
+   *
+   */
+  Function init()
+  {
+    /*
+     *  Activation du module
+     */
+
+    $sql = array(
+		 );
+    
+    return $this->_init($sql);
+  }
+  /*
+   *
+   *
+   */
+  Function remove()
+  {
+    $sql = array();
+
+    return $this->_remove($sql);   
+  }
+}
+?>
