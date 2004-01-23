cae7aa443462ec7aff751986d098799b4086531a
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-01-23 12:42:17 +0000

    Nouveau fichier

---
+++ htdocs/admin/ldap.php
@@ -0,0 +1,146 @@
+<?PHP
+/* Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ */
+require("./pre.inc.php");
+
+if ($_GET["action"] == 'setvalue' && $user->admin)
+{
+  $sql = "REPLACE INTO llx_const SET name = 'LDAP_SERVER_HOST', value='".$HTTP_POST_VARS["host"]."', visible=0";
+
+  $db->query($sql);
+
+  $sql = "REPLACE INTO llx_const SET name = 'LDAP_SERVER_DN', value='".$HTTP_POST_VARS["dn"]."', visible=0";
+
+  $db->query($sql);
+
+  $sql = "REPLACE INTO llx_const SET name = 'LDAP_SERVER_LOGIN', value='".$HTTP_POST_VARS["login"]."', visible=0";
+
+  $db->query($sql);
+
+  $sql = "REPLACE INTO llx_const SET name = 'LDAP_SERVER_PASS', value='".$HTTP_POST_VARS["pass"]."', visible=0";
+
+  $db->query($sql);
+
+
+  if ($db->query($sql))
+    {
+      Header("Location: ldap.php");
+    }
+}
+
+llxHeader();
+
+if (!$user->admin)
+{
+  print "Forbidden";
+  llxfooter();
+  exit;
+}
+/*
+ *
+ */
+print_titre("Configuration de ldap");
+/*
+ *
+ */
+print '<table class="noborder" width="100%" cellpadding="3" cellspacing="0">';
+print '<tr>';
+print '<td width="50%" valign="top">';
+
+print '<table class="border" cellpadding="3" cellspacing="0">';
+print '<tr class="liste_titre">';
+print '<td>Nom</td>';
+print '<td>Valeur</td><td colspan="2">&nbsp;</td>';
+print "</tr>\n";
+print '<tr><td>Serveur LDAP</td><td>'.LDAP_SERVER_HOST.'</td></tr>';
+
+print '<tr><td>DN</td><td>'.LDAP_SERVER_DN.'</td></tr>';
+print '<tr><td>DN</td><td>'.LDAP_SERVER_LOGIN.'</td></tr>';
+print '<tr><td>DN</td><td>'.LDAP_SERVER_PASS.'</td></tr>';
+
+print '</table>';
+
+print '</td><td width="50%">';
+
+print '<form method="post" action="ldap.php?action=setvalue">';
+
+print '<table class="border" cellpadding="3" cellspacing="0">';
+print '<TR class="liste_titre">';
+print '<td>Nom</td>';
+print '<td>Valeur</td><td colspan="2">&nbsp;</td>';
+print "</tr>\n";
+print '<tr><td>';
+print 'Serveur LDAP</td><td>';
+print '<input size="15" type="text" name="host" value="'.LDAP_SERVER_HOST.'">';
+print '</td></tr>';
+print '<tr><td>DN</td><td>';
+print '<input size="25" type="text" name="dn" value="'.LDAP_SERVER_DN.'">';
+print '</td></tr>';
+print '<tr><td>Login</td><td>';
+print '<input size="25" type="text" name="login" value="'.LDAP_SERVER_LOGIN.'">';
+print '</td></tr>';
+print '<tr><td>Pass</td><td>';
+print '<input size="25" type="text" name="pass" value="'.LDAP_SERVER_PASS.'">';
+print '</td></tr>';
+
+print '<tr><td><input type="submit" value="changer"></td></tr>';
+print '</table></form>';
+
+print '</td></tr></table>';
+
+
+if (defined("LDAP_SERVER_HOST") && LDAP_SERVER_HOST && $test)
+{
+  $ds = ldap_connect(LDAP_SERVER_HOST);
+
+  if ($ds)
+    {
+      $connect = "ok";
+
+      //ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+      if ($pass)
+	{
+	  $ldapbind=ldap_bind($ds, $dn, $pass);
+	}
+      else
+	{
+	  $ldapbind=ldap_bind($ds, $dn);
+	  if ($ldapbind)
+	    {
+	      $bind = "ok";
+	    }
+	  else
+	    {
+	      $bind = "erreur";
+	    }
+	}
+    }
+  else
+    {
+      $connect = "erreur";
+    }
+}
+
+
+
+$db->close();
+
+llxFooter();
+?>
