d35053d2e29a804d4c94b35c4e47f3cf8cd441c0
Laurent Destailleur <eldy@users.sourceforge.net>
2004-01-27 22:57:18 +0000

    Correction copyright.

--- htdocs/includes/boxes/box_fournisseurs.php
+++ htdocs/includes/boxes/box_fournisseurs.php
@@ -1,5 +1,5 @@
 <?PHP
-/* Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+/* Copyright (C) 2004 Destailleur Laurent <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
