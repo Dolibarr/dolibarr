809ceb8629e99366ba58043595198f8535c85a80
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-01-27 17:46:05 +0000

    Modif index sur table facture founisseurs

--- mysql/migration/1.1.0-1.2.0-RC1.sql
+++ mysql/migration/1.1.0-1.2.0-RC1.sql
@@ -8,10 +8,30 @@ alter table  llx_product add stock_commande integer default 0;
 
 alter table  llx_product add seuil_stock_alerte integer default 0;
 
+
+alter table `llx_groupart` add `description` text after `groupart` ;
+
+alter table llx_socpeople add phone_perso varchar(30) after phone ;
+alter table llx_socpeople add phone_mobile varchar(30) after phone_perso ;
+
+alter table llx_socpeople add jabberid varchar(255) after email ;
+
+alter table llx_socpeople add birthday date after address ;
+
+
+create table llx_birthday_alert
+(
+  rowid        integer AUTO_INCREMENT PRIMARY KEY,
+  fk_contact   integer, -- pointe sur llx_socpeople
+  fk_user      integer
+);
+
+
+alter table llx_facture_fourn drop index facnumber ;
+alter table llx_facture_fourn add unique index (facnumber, fk_soc) ;=======
 ALTER TABLE `llx_groupart` ADD `description` TEXT AFTER `groupart` ;
 
 
--- Nouvelle table
 create table llx_co_fa
 (
   rowid       integer AUTO_INCREMENT PRIMARY KEY,
@@ -21,3 +41,4 @@ create table llx_co_fa
   key(fk_commande),
   key(fk_facture)
 );
+
