9da7c9fd5a808fce3fcef0a3c1fb3bfe2bd7ec31
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-01-30 15:22:44 +0000

    Modif des noms des tables de constantes

--- mysql/data/data.sql
+++ mysql/data/data.sql
@@ -134,96 +134,96 @@ INSERT INTO llx_const (name, value, type, note) VALUES ('MAIN_MENU_BARRETOP','de
 -- Constantes 
 --
 
-delete from c_chargesociales;
-insert into c_chargesociales (id,libelle,deductible) values ( 1, 'Allocations familiales',1);
-insert into c_chargesociales (id,libelle,deductible) values ( 2, 'GSG Deductible',1);
-insert into c_chargesociales (id,libelle,deductible) values ( 3, 'GSG/CRDS NON Deductible',0);
+delete from llx_c_chargesociales;
+insert into llx_c_chargesociales (id,libelle,deductible) values ( 1, 'Allocations familiales',1);
+insert into llx_c_chargesociales (id,libelle,deductible) values ( 2, 'GSG Deductible',1);
+insert into llx_c_chargesociales (id,libelle,deductible) values ( 3, 'GSG/CRDS NON Deductible',0);
 
 
-delete from c_actioncomm;
-insert into c_actioncomm (id,libelle) values ( 0, '-');
-insert into c_actioncomm (id,libelle) values ( 1, 'Appel Téléphonique');
-insert into c_actioncomm (id,libelle) values ( 2, 'Envoi Fax');
-insert into c_actioncomm (id,libelle) values ( 3, 'Envoi propal par mail');
-insert into c_actioncomm (id,libelle) values ( 4, 'Envoi d\'un email'); 
-insert into c_actioncomm (id,libelle) values ( 5, 'Rendez-vous'); 
-insert into c_actioncomm (id,libelle) values ( 9, 'Envoi Facture');
-insert into c_actioncomm (id,libelle) values (10, 'Relance effectuée');
-insert into c_actioncomm (id,libelle) values (11, 'Clôture');
+delete from llx_c_actioncomm;
+insert into llx_c_actioncomm (id,libelle) values ( 0, '-');
+insert into llx_c_actioncomm (id,libelle) values ( 1, 'Appel Téléphonique');
+insert into llx_c_actioncomm (id,libelle) values ( 2, 'Envoi Fax');
+insert into llx_c_actioncomm (id,libelle) values ( 3, 'Envoi propal par mail');
+insert into llx_c_actioncomm (id,libelle) values ( 4, 'Envoi d\'un email'); 
+insert into llx_c_actioncomm (id,libelle) values ( 5, 'Rendez-vous'); 
+insert into llx_c_actioncomm (id,libelle) values ( 9, 'Envoi Facture');
+insert into llx_c_actioncomm (id,libelle) values (10, 'Relance effectuée');
+insert into llx_c_actioncomm (id,libelle) values (11, 'Clôture');
 
-delete from c_stcomm;
-insert into c_stcomm (id,libelle) values (-1, 'NE PAS CONTACTER');
-insert into c_stcomm (id,libelle) values ( 0, 'Jamais contacté');
-insert into c_stcomm (id,libelle) values ( 1, 'A contacter');
-insert into c_stcomm (id,libelle) values ( 2, 'Contact en cours');
-insert into c_stcomm (id,libelle) values ( 3, 'Contactée');
+delete from llx_c_stcomm;
+insert into llx_c_stcomm (id,libelle) values (-1, 'NE PAS CONTACTER');
+insert into llx_c_stcomm (id,libelle) values ( 0, 'Jamais contacté');
+insert into llx_c_stcomm (id,libelle) values ( 1, 'A contacter');
+insert into llx_c_stcomm (id,libelle) values ( 2, 'Contact en cours');
+insert into llx_c_stcomm (id,libelle) values ( 3, 'Contactée');
 
 --
 -- Types d'entreprise
 --
-delete from c_typent;
-insert into c_typent (id,libelle) values (  0, 'Indifférent');
-insert into c_typent (id,libelle) values (  1, 'Start-up');
-insert into c_typent (id,libelle) values (  2, 'Grand groupe');
-insert into c_typent (id,libelle) values (  3, 'PME/PMI');
-insert into c_typent (id,libelle) values (  4, 'Administration');
-insert into c_typent (id,libelle) values (100, 'Autres');
+delete from llx_c_typent;
+insert into llx_c_typent (id,libelle) values (  0, 'Indifférent');
+insert into llx_c_typent (id,libelle) values (  1, 'Start-up');
+insert into llx_c_typent (id,libelle) values (  2, 'Grand groupe');
+insert into llx_c_typent (id,libelle) values (  3, 'PME/PMI');
+insert into llx_c_typent (id,libelle) values (  4, 'Administration');
+insert into llx_c_typent (id,libelle) values (100, 'Autres');
 
 --
 -- Pays
 --
-delete from c_pays;
-insert into c_pays (id,libelle,code) values (0, 'France',          'FR');
-insert into c_pays (id,libelle,code) values (2, 'Belgique',        'BE');
-insert into c_pays (id,libelle,code) values (3, 'Italie',          'IT');
-insert into c_pays (id,libelle,code) values (4, 'Espagne',         'ES');
-insert into c_pays (id,libelle,code) values (5, 'Allemagne',       'DE');
-insert into c_pays (id,libelle,code) values (6, 'Suisse',          'CH');
-insert into c_pays (id,libelle,code) values (7, 'Royaume uni',     'GB');
-insert into c_pays (id,libelle,code) values (8, 'Irlande',         'IE');
-insert into c_pays (id,libelle,code) values (9, 'Chine',           'CN');
-insert into c_pays (id,libelle,code) values (10, 'Tunisie',        'TN');
-insert into c_pays (id,libelle,code) values (11, 'Etats Unis',     'US');
-insert into c_pays (id,libelle,code) values (12, 'Maroc',          'MA');
-insert into c_pays (id,libelle,code) values (13, 'Algérie',        'DZ');
-insert into c_pays (id,libelle,code) values (14, 'Canada',         'CA');
-insert into c_pays (id,libelle,code) values (15, 'Togo',           'TG');
-insert into c_pays (id,libelle,code) values (16, 'Gabon',          'GA');
-insert into c_pays (id,libelle,code) values (17, 'Pays Bas',       'NL');
-insert into c_pays (id,libelle,code) values (18, 'Hongrie',        'HU');
-insert into c_pays (id,libelle,code) values (19, 'Russie',         'RU');
-insert into c_pays (id,libelle,code) values (20, 'Suède',          'SE');
-insert into c_pays (id,libelle,code) values (21, 'Côte d\'Ivoire', 'CI');
-insert into c_pays (id,libelle,code) values (23, 'Sénégal',        'SN');
-insert into c_pays (id,libelle,code) values (24, 'Argentine',      'AR');
-insert into c_pays (id,libelle,code) values (25, 'Cameroun',       'CM');
+delete from llx_c_pays;
+insert into llx_c_pays (id,libelle,code) values (0, 'France',          'FR');
+insert into llx_c_pays (id,libelle,code) values (2, 'Belgique',        'BE');
+insert into llx_c_pays (id,libelle,code) values (3, 'Italie',          'IT');
+insert into llx_c_pays (id,libelle,code) values (4, 'Espagne',         'ES');
+insert into llx_c_pays (id,libelle,code) values (5, 'Allemagne',       'DE');
+insert into llx_c_pays (id,libelle,code) values (6, 'Suisse',          'CH');
+insert into llx_c_pays (id,libelle,code) values (7, 'Royaume uni',     'GB');
+insert into llx_c_pays (id,libelle,code) values (8, 'Irlande',         'IE');
+insert into llx_c_pays (id,libelle,code) values (9, 'Chine',           'CN');
+insert into llx_c_pays (id,libelle,code) values (10, 'Tunisie',        'TN');
+insert into llx_c_pays (id,libelle,code) values (11, 'Etats Unis',     'US');
+insert into llx_c_pays (id,libelle,code) values (12, 'Maroc',          'MA');
+insert into llx_c_pays (id,libelle,code) values (13, 'Algérie',        'DZ');
+insert into llx_c_pays (id,libelle,code) values (14, 'Canada',         'CA');
+insert into llx_c_pays (id,libelle,code) values (15, 'Togo',           'TG');
+insert into llx_c_pays (id,libelle,code) values (16, 'Gabon',          'GA');
+insert into llx_c_pays (id,libelle,code) values (17, 'Pays Bas',       'NL');
+insert into llx_c_pays (id,libelle,code) values (18, 'Hongrie',        'HU');
+insert into llx_c_pays (id,libelle,code) values (19, 'Russie',         'RU');
+insert into llx_c_pays (id,libelle,code) values (20, 'Suède',          'SE');
+insert into llx_c_pays (id,libelle,code) values (21, 'Côte d\'Ivoire', 'CI');
+insert into llx_c_pays (id,libelle,code) values (23, 'Sénégal',        'SN');
+insert into llx_c_pays (id,libelle,code) values (24, 'Argentine',      'AR');
+insert into llx_c_pays (id,libelle,code) values (25, 'Cameroun',       'CM');
 
 --
 -- Effectif des sociétés
 --
-delete from c_effectif;
-insert into c_effectif (id,libelle) values (0,  'Non spécifié');
-insert into c_effectif (id,libelle) values (1,  '1 - 5');
-insert into c_effectif (id,libelle) values (2,  '6 - 10');
-insert into c_effectif (id,libelle) values (3,  '11 - 50');
-insert into c_effectif (id,libelle) values (4,  '51 - 100');
-insert into c_effectif (id,libelle) values (5,  '100 - 500');
-insert into c_effectif (id,libelle) values (6,  '> 500');
+delete from llx_c_effectif;
+insert into llx_c_effectif (id,libelle) values (0,  'Non spécifié');
+insert into llx_c_effectif (id,libelle) values (1,  '1 - 5');
+insert into llx_c_effectif (id,libelle) values (2,  '6 - 10');
+insert into llx_c_effectif (id,libelle) values (3,  '11 - 50');
+insert into llx_c_effectif (id,libelle) values (4,  '51 - 100');
+insert into llx_c_effectif (id,libelle) values (5,  '100 - 500');
+insert into llx_c_effectif (id,libelle) values (6,  '> 500');
 
-delete from c_paiement;
-insert into c_paiement (id,libelle,type) values (0, '-', 3);
-insert into c_paiement (id,libelle,type) values (1, 'TIP', 1);
-insert into c_paiement (id,libelle,type) values (2, 'Virement', 2);
-insert into c_paiement (id,libelle,type) values (3, 'Prélèvement', 1);
-insert into c_paiement (id,libelle,type) values (4, 'Liquide', 0);
-insert into c_paiement (id,libelle,type) values (5, 'Paiement en ligne', 0);
-insert into c_paiement (id,libelle,type) values (6, 'CB', 1);
-insert into c_paiement (id,libelle,type) values (7, 'Chèque', 2);
+delete from llx_c_paiement;
+insert into llx_c_paiement (id,libelle,type) values (0, '-', 3);
+insert into llx_c_paiement (id,libelle,type) values (1, 'TIP', 1);
+insert into llx_c_paiement (id,libelle,type) values (2, 'Virement', 2);
+insert into llx_c_paiement (id,libelle,type) values (3, 'Prélèvement', 1);
+insert into llx_c_paiement (id,libelle,type) values (4, 'Liquide', 0);
+insert into llx_c_paiement (id,libelle,type) values (5, 'Paiement en ligne', 0);
+insert into llx_c_paiement (id,libelle,type) values (6, 'CB', 1);
+insert into llx_c_paiement (id,libelle,type) values (7, 'Chèque', 2);
 
-delete from c_propalst;
-insert into c_propalst (id,label) values (0, 'Brouillon');
-insert into c_propalst (id,label) values (1, 'Ouverte');
-insert into c_propalst (id,label) values (2, 'Signée');
-insert into c_propalst (id,label) values (3, 'Non Signée');
-insert into c_propalst (id,label) values (4, 'Facturée');
+delete from llx_c_propalst;
+insert into llx_c_propalst (id,label) values (0, 'Brouillon');
+insert into llx_c_propalst (id,label) values (1, 'Ouverte');
+insert into llx_c_propalst (id,label) values (2, 'Signée');
+insert into llx_c_propalst (id,label) values (3, 'Non Signée');
+insert into llx_c_propalst (id,label) values (4, 'Facturée');
 
