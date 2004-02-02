cd4c826edd3d0c327c342137a55cad23d8f9cfcd
Laurent Destailleur <eldy@users.sourceforge.net>
2004-02-02 01:41:33 +0000

    Correction copyright.

--- htdocs/install/index.php
+++ htdocs/install/index.php
@@ -1,4 +1,26 @@
 <?PHP
+/* Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org> 
+ * Copyright (C) 2004 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
 //
 // Essaye de créer le fichier de conf
 //
@@ -36,7 +58,7 @@ $docurl = '<a href="doc/dolibarr-install.html">documentation</a>';
 if (!file_exists("../conf/conf.php"))
 {
   print '<div class="error">';
-  print "Le fichier <b>conf.php</b> n'existe pas reportez-vous à la ".$docurl." pour créer ce fichier<br>";
+  print "Le fichier <b>conf.php</b> n'existe pas, reportez-vous à la ".$docurl." pour créer ce fichier<br>";
 
   print '</div>';
 }
