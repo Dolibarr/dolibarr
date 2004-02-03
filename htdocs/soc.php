d35b59ea534c1a026071ef4a83dc71c4c7ca6d94
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-02-03 19:08:38 +0000

    Correction bad url

--- htdocs/soc.php
+++ htdocs/soc.php
@@ -204,7 +204,7 @@ else
   print '<td width="20%" align="center"><a href="soc.php?socid='.$socid.'&action=edit">Editer</a></td>';
   print '<td width="20%" align="center">-</td>';
   print '<td width="20%" align="center">';
-  print '<a href="../contact/fiche.php?socid='.$socid.'&amp;action=create">Ajouter un contact</a></td>';
+  print '<a href="./contact/fiche.php?socid='.$socid.'&amp;action=create">Ajouter un contact</a></td>';
   
   print '<td width="20%" align="center">-</td>';
   print '<td width="20%" align="center"><a href="societe/notify/fiche.php?socid='.$socid.'">Notifications</a></td>';
