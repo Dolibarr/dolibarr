d1e2d8e8d28fe5eb3cfd6532d81963a39eb78195
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-02-07 17:25:30 +0000

    Ajout champ fk_user_creat et champ fk_user_modif

--- mysql/tables/llx_societe.sql
+++ mysql/tables/llx_societe.sql
@@ -22,37 +22,39 @@
 
 create table llx_societe
 (
-  idp            integer AUTO_INCREMENT PRIMARY KEY,
-  id             varchar(32),                         -- private id
-  active         smallint       default 0,            --
-  parent         integer        default 0,            --
-  tms            timestamp,
-  datec	         datetime,                            -- creation date
-  datea	         datetime,                            -- activation date
-  nom            varchar(60),                         -- company name
-  address        varchar(255),                        -- company adresse
-  cp             varchar(10),                         -- zipcode
-  ville          varchar(50),                         -- town
-  fk_pays        integer        default 0,            --
-  tel            varchar(20),                         -- phone number
-  fax            varchar(20),                         -- fax number
-  url            varchar(255),                        --
-  fk_secteur     integer        default 0,            --
-  fk_effectif    integer        default 0,            --
-  fk_typent      integer        default 0,            --
-  siren	         varchar(9),                          -- siren ou RCS
-  siret          varchar(14),                         -- numero de siret
-  ape            varchar(4),                          -- code ape
-  tva_intra      varchar(20),                         -- tva intracommunautaire
-  capital        real,                                -- capital de la société
-  description    text,                                --
-  fk_stcomm      smallint       default 0,            -- commercial statut
-  note           text,                                --
-  services       integer        default 0,            --
-  prefix_comm    varchar(5),                          -- prefix commercial
-  client         int            default 0,            -- client oui/non
-  fournisseur    smallint       default 0,            -- fournisseur oui/non
-  rubrique       varchar(255),                        -- champ rubrique libre
+  idp             integer AUTO_INCREMENT PRIMARY KEY,
+  id              varchar(32),                         -- private id
+  active          smallint       default 0,            --
+  parent          integer        default 0,            --
+  tms             timestamp,
+  datec	          datetime,                            -- creation date
+  datea	          datetime,                            -- activation date
+  nom             varchar(60),                         -- company name
+  address         varchar(255),                        -- company adresse
+  cp              varchar(10),                         -- zipcode
+  ville           varchar(50),                         -- town
+  fk_pays         integer        default 0,            --
+  tel             varchar(20),                         -- phone number
+  fax             varchar(20),                         -- fax number
+  url             varchar(255),                        --
+  fk_secteur      integer        default 0,            --
+  fk_effectif     integer        default 0,            --
+  fk_typent       integer        default 0,            --
+  siren	          varchar(9),                          -- siren ou RCS
+  siret           varchar(14),                         -- numero de siret
+  ape             varchar(4),                          -- code ape
+  tva_intra       varchar(20),                         -- tva intracommunautaire
+  capital         real,                                -- capital de la société
+  description     text,                                --
+  fk_stcomm       smallint       default 0,            -- commercial statut
+  note            text,                                --
+  services        integer        default 0,            --
+  prefix_comm     varchar(5),                          -- prefix commercial
+  client          int            default 0,            -- client oui/non
+  fournisseur     smallint       default 0,            -- fournisseur oui/non
+  rubrique        varchar(255),                        -- champ rubrique libre
+  fk_user_creat   integer,                       -- utilisateur qui a créé l'info
+  fk_user_modif   integer,                       -- utilisateur qui a modifié l'info
 
   UNIQUE INDEX(prefix_comm)
 )type=innodb;
