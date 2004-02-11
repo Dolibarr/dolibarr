e3f78992ff93fde25648a94695f74fef4f58f17b
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-02-11 18:08:35 +0000

    Ajout fonctions

--- htdocs/install/inc.php
+++ htdocs/install/inc.php
@@ -1,3 +1,61 @@
 <?PHP
+/* Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org> 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
 $etapes=5;
+
+Function pHeader($soutitre,$next)
+{
+
+print '
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso8859-1">
+<link rel="stylesheet" type="text/css" href="./default.css">
+<title>Dolibarr Installation</title>
+</head>
+<body>
+<div class="titre">
+<span class="titre"><a class="titre" href="index.php">Dolibarr Installation</a></span>
+</div>
+';
+ print '<form action="'.$next.'.php" method="POST">';
+
+print '<div class="main"><div class="soustitre">'.$soutitre.'</div>
+<div class="main-inside">
+';
+}
+
+
+Function pFooter($nonext=0)
+{
+print '</div></div>';
+if ($nonext == 0)
+  {
+    print '<div class="barrebottom"><input type="submit" value="Etape suivante ->"></div>';
+  }
+print '
+</form>
+</body>
+</html>';
+    }
+
 ?>
