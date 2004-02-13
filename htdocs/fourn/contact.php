219ec20b75cc71cb26e41d893ff4f2db8f46da39
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-02-13 15:05:16 +0000

    Nouveau look

--- htdocs/fourn/contact.php
+++ htdocs/fourn/contact.php
@@ -103,7 +103,7 @@ if ($result) {
   } else {
     $sortorder="DESC";
   }
-  print "<p><TABLE border=\"0\" width=\"100%\" cellspacing=\"0\" cellpadding=\"4\">";
+  print '<p><table class="noborder" width="100%" cellspacing="0" cellpadding="4">';
   print "<TR class=\"liste_titre\">";
   print "<TD>";
   print_liste_field_titre("Nom",$PHP_SELF,"lower(p.name)", $begin);
@@ -111,34 +111,34 @@ if ($result) {
   print_liste_field_titre("Prénom",$PHP_SELF,"lower(p.firstname)", $begin);
   print "</td><td>";
   print_liste_field_titre("Société",$PHP_SELF,"lower(s.nom)", $begin);
-  print "</td><TD>email</TD>";
-  print '<TD>Téléphone</TD>';
-  print "</TR>\n";
+  print '</td><td>email</td><td>Téléphone</td>';
+  print "</tr>\n";
   $var=True;
   $i = 0;
-  while ($i < min($num,$limit)) {
-    $obj = $db->fetch_object( $i);
+  while ($i < min($num,$limit))
+    {
+      $obj = $db->fetch_object( $i);
     
-    $var=!$var;
+      $var=!$var;
 
-    print "<TR $bc[$var]>";
+      print "<TR $bc[$var]>";
 
-    print "<TD>$obj->name</TD>";
-    print "<TD>$obj->firstname</TD>";
-
-    print '<TD><a href="contact.php?socid='.$obj->idp.'"><img src="'.DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/filter.png" border="0"></a>&nbsp;';
-    print "<a href=\"fiche.php?socid=$obj->idp\">$obj->nom</A></td>\n";
-    print "<TD>$obj->email&nbsp;</TD>\n";
-    print '<td>'.$obj->phone.'</td>';
-
-    print "</TR>\n";
-    $i++;
-  }
+      print '<td width="13%">'.$obj->name.'</td>';
+      print '<td width="13%">'.$obj->firstname.'</td>';
+      
+      print '<TD><a href="contact.php?socid='.$obj->idp.'"><img src="'.DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/filter.png" border="0"></a>&nbsp;';
+      print "<a href=\"fiche.php?socid=$obj->idp\">$obj->nom</A></td>\n";
+      print "<TD>$obj->email&nbsp;</TD>\n";
+      print '<td>'.$obj->phone.'</td>';
+      
+      print "</TR>\n";
+      $i++;
+    }
   print "</TABLE>";
   $db->free();
 } else {
   print_barre_liste("Liste des contacts $label",$page, $PHP_SELF);
-
+  
   print $db->error();
 }
 
