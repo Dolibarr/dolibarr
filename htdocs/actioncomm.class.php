86355f8fe233faafd7c450a538d3a7192ac5feec
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-02-13 14:05:00 +0000

    Synchro

--- htdocs/actioncomm.class.php
+++ htdocs/actioncomm.class.php
@@ -1,5 +1,5 @@
 <?PHP
-/* Copyright (C) 2002-2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+/* Copyright (C) 2002-2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  *
  * $Id$
  * $Source$
