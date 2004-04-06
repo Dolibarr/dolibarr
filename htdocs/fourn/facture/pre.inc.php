6c69e818cf90be9c672c800d18f6bc4332fc32ee
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-04-06 10:17:05 +0000

    Utilisation de la classe account pour la saisie dans les comptes bancaires

--- htdocs/fourn/facture/pre.inc.php
+++ htdocs/fourn/facture/pre.inc.php
@@ -1,5 +1,5 @@
 <?PHP
-/* Copyright (C) 2001-2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+/* Copyright (C) 2001-2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,6 +21,8 @@
  */
 require("../../main.inc.php");
 require("../../facturefourn.class.php");
+
+include_once DOL_DOCUMENT_ROOT."/compta/bank/account.class.php";
 
 function llxHeader($head = "", $urlp = "") {
   global $user, $conf;
