56cf1c5c74c560dd849bceb18bf2ba8c2f8b5b1f
opensides <opensides>
2004-06-05 23:50:11 +0000

    nouvelle version pour dolibarr 1.1.0

---
+++ pgsql/tables/llx_groupart.sql
@@ -0,0 +1,33 @@
+-- ============================================================================
+-- Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+-- Copyright (C) 2004 Benoit Mortier <benoit.mortier@opensides.be>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- ============================================================================
+
+create table llx_groupart
+(
+  rowid           SERIAL PRIMARY KEY,
+  osc_id          integer NOT NULL,
+  tms             timestamp,
+  nom		  varchar(64),
+  groupart	  CHAR(8) CHECK (groupart IN ('artiste','groupe')) NOT NULL,
+  fk_user_author  integer
+);
+
---
+++ pgsql/tables/llx_livre.sql
@@ -0,0 +1,45 @@
+-- ============================================================================
+-- Copyright (C) 2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+-- Copyright (C) 2004 Benoit Mortier <benoit.mortier@opensides.be>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- ============================================================================
+
+create table llx_livre
+(
+  rowid           SERIAL PRIMARY KEY,
+  oscid           integer NOT NULL,
+  tms             timestamp,
+  status          smallint,
+  date_ajout      timestamp,
+  ref		  varchar(12),
+  title		  varchar(64),
+  annee		  smallint,
+  description     text,
+  prix            decimal(15,4),
+  fk_editeur      integer,
+  fk_user_author  integer,
+  frais_de_port   smallint default 1,
+
+UNIQUE(ref)
+
+);
+
+
+
