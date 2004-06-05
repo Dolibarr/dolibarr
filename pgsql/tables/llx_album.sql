4d9e137d0f30f456538a666f0dbb998456e244a4
opensides <opensides>
2004-06-05 23:33:32 +0000

    nouvelle version pour dolibarr 1.1.0

---
+++ pgsql/tables/llx_album.sql
@@ -0,0 +1,32 @@
+-- ============================================================================
+-- Copyright (C) 2004 Benoit mortier <benoit;mortier@opensides.be>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- ============================================================================
+
+create table llx_album
+(
+  rowid           serial PRIMARY KEY,
+  osc_id          integer NOT NULL,
+  tms             timestamp,
+  ref		  varchar(12),
+  title		  varchar(64),
+  annee		  smallint, -- pourquoi smallint(64)
+  description     text,
+  collectif       smallint,
+  fk_user_author  integer
+);
+
