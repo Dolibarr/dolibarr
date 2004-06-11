ce4e39e715f4717672d7320a1158e15ce7b2ced5
opensides <opensides>
2004-06-11 15:59:43 +0000

    le champ code doit etre unique dans llx_c_forme_juridique j'ai commence les formes juridiques belges a 100

--- mysql/data/data.sql
+++ mysql/data/data.sql
@@ -463,14 +463,14 @@ insert into llx_c_forme_juridique (fk_pays, code, libelle) values (1,93,'Fondati
 insert into llx_c_forme_juridique (fk_pays, code, libelle) values (1,99,'Autre personne morale de droit privé');
 
 -- Pour la Belgique
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 1,'Indépendant');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 2,'SC - Coopérative');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 3,'SCRL - Coopérative à responsabilité limitée');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 4,'SPRL - Société à responsabilité limitée');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 5,'SA - Société Anonyme');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,52,'Société en nom collectif');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,53,'Société en commandite');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,71,'Administration publique');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,91,'Syndicat de propriétaires');
-insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,93,'Fondations');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 100,'Indépendant');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 101,'SC - Coopérative');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 102,'SCRL - Coopérative à responsabilité limitée');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 103,'SPRL - Société à responsabilité limitée');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2, 104,'SA - Société Anonyme');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,105,'Société en nom collectif');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,106,'Société en commandite');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,107,'Administration publique');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,108,'Syndicat de propriétaires');
+insert into llx_c_forme_juridique (fk_pays, code, libelle) values (2,109,'Fondations');
 
