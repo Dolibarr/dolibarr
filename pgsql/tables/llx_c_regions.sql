b6abd0e294b85fa14577ac8fc937281c8a3c4c9b
opensides <opensides>
2004-06-12 17:55:11 +0000

    adaptation pour dolibarr head

---
+++ pgsql/tables/llx_c_regions.sql
@@ -0,0 +1,34 @@
+-- ========================================================================
+-- Copyright (C) 2001-2002,2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+-- Copyright (C) 2004 Benoit Mortier <benoit.mortier@opensides.be>
+--
+-- $Id$
+-- $Source$
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- ========================================================================
+
+create table llx_c_regions
+(
+  rowid       serial PRIMARY KEY,
+  code_region integer UNIQUE,
+  fk_pays     integer default 1,
+  cheflieu    varchar(7),
+  tncc        integer,
+  nom         varchar(50),
+  active      smallint default 1
+);
+
---
+++ pgsql/tables/llx_user_param.sql
@@ -0,0 +1,33 @@
+-- ============================================================================
+-- Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+-- Copyright (C) 2004 Laurent Destailleur  <eldy@users.sourceforge.net>
+-- Copyright (C) 2004 Benoit Mortier <benoit.mortier@opensides.be>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- ===========================================================================
+
+create table llx_user_param
+(
+  fk_user       integer,
+  page          varchar(255),
+  param         varchar(64),
+  value         varchar(255),
+
+  UNIQUE (fk_user,page,param)
+);
