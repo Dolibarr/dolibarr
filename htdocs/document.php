fa3365f22cd71478897300c7eb0370657110e0b3
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-07-12 09:46:13 +0000

    Nouveau fichier

---
+++ htdocs/document.php
@@ -0,0 +1,35 @@
+<?PHP
+/* Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * or see http://www.gnu.org/
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+$original_file = urldecode($_GET["file"]);
+
+$filename = basename ($original_file);
+
+header('Content-type: application/pdf');
+
+header('Content-Disposition: attachment; filename="'.$filename.'"');
+
+// The PDF source is in original.pdf
+readfile($original_file);
+
+?>
