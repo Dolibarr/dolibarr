dd95d82afbd432312bd241a4abd9f8e53f883ae3
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-07-13 16:10:32 +0000

    Ajout des permissions sur les societes

--- htdocs/user.class.php
+++ htdocs/user.class.php
@@ -357,8 +357,20 @@ class User
 
 		  if ($obj->fk_id == 113)
 		    $this->rights->banque->configurer = 1;
-
 		}
+
+	      if ($module == 'societe' or $module == '')
+		{
+		  if ($obj->fk_id == 121)
+		    $this->rights->societe->lire = 1;
+
+		  if ($obj->fk_id == 122)
+		    $this->rights->societe->creer = 1;
+
+		  if ($obj->fk_id == 129)
+		    $this->rights->societe->supprimer = 1;
+		}
+
 	      $i++;
 	  }
 	    $this->db->free();	    
