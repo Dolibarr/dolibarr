c822c7e0f8160e195f877dcee35a3a79de9e134b
opensides <opensides>
2004-07-24 11:51:39 +0000

    rajout de la table dans pgsql

---
+++ pgsql/tables/llx_c_accountingsystem.sql
@@ -0,0 +1,32 @@
+-- ============================================================================
+-- Copyright (C) 2004 Laurent Destailleur <eldy@users.sourceforge.net>
+-- Copyright (C) 2004 Benoit Mortier <benoit.mortier@opensides.be>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- ============================================================================
+
+create table llx_c_accountingsystem
+(
+  rowid           SERIAL PRIMARY KEY,
+  fk_pays         integer      NOT NULL,
+  pcg_version     varchar(12)  NOT NULL,
+  pcg_type        varchar(20)  NOT NULL,
+  label           varchar(128) NOT NULL,
+  account_number  varchar(20)  NOT NULL
+);
