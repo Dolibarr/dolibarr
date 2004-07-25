6a1e6f84abfcc1bf8c4bbd74ab2d9cd691419573
Laurent Destailleur <eldy@users.sourceforge.net>
2004-07-25 16:25:29 +0000

    Fix: Correction bug laissant un retour chariot dans les chaines traduites.

--- htdocs/translate.class.php
+++ htdocs/translate.class.php
@@ -55,7 +55,7 @@ Class Translate {
                     if ($ligne[0] != "\n" && $ligne[0] != " " && $ligne[0] != "#") {
                         $tab=split('=',$ligne,2);
                         //print "Ligne=$ligne<br>Found a string for $tab[0] with value $tab[1]<br>";
-                        $this->tab_translate[$tab[0]]=$tab[1];
+                        $this->tab_translate[$tab[0]]=trim($tab[1]);
                     }
                 }
                 fclose($fp);
