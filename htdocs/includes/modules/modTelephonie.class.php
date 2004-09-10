35daf87a267377dd3771334fac0633ac89be98a8
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-09-10 14:22:35 +0000

    Nouveau fichier

---
+++ htdocs/includes/modules/modTelephonie.class.php
@@ -0,0 +1,95 @@
+<?PHP
+/* Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+/*!     \defgroup   telephonie     Module telephonie
+        \brief      Module pour gérer la telephonie
+*/
+
+/*!
+        \file       htdocs/includes/modules/modTelephonie.class.php
+        \ingroup    telephonie
+        \brief      Fichier de description et activation du module Telephonie
+*/
+
+include_once "DolibarrModules.class.php";
+
+/*! \class modTelephonie
+        \brief      Classe de description et activation du module Telephonie
+*/
+
+class modTelephonie extends DolibarrModules
+{
+
+  /** Initialisation de l'objet
+   *
+   *
+   */
+
+  function modTelephonie($DB)
+  {
+    $this->db = $DB ;
+    $this->numero = 56 ;
+
+    $this->family = "technic";
+    $this->name = "Telephonie";
+    $this->description = "Gestion de la Telephonie (experimental)";
+    $this->const_name = "MAIN_MODULE_TELEPHONIE";
+    $this->const_config = MAIN_MODULE_TELEPHONIE;
+
+    // Dépendances
+    $this->depends = array();
+    $this->requiredby = array();
+
+    $this->const = array();
+    $this->boxes = array();
+  }
+  /** initialisation du module
+   *
+   *
+   *
+   */
+
+  function init()
+  {
+    /*
+     * Permissions
+     */    
+    $this->remove();
+    $sql = array(
+		 "insert into ".MAIN_DB_PREFIX."rights_def values (140,'Tous les droits sur la telephonie','telephonie','a',0);",
+		 "insert into ".MAIN_DB_PREFIX."rights_def values (141,'Configurer la telephonie','telephonie','w',0);",
+		 );
+    
+    return $this->_init($sql);
+  }
+  /** suppression du module
+   *
+   *
+   */
+  function remove()
+  {
+    $sql = array("DELETE FROM ".MAIN_DB_PREFIX."rights_def WHERE module = 'telephonie';");
+
+    return $this->_remove($sql);
+  }
+}
+?>
