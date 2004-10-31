7fe57e3b12518fe443286029d9b5da431ca7d2b7
Laurent Destailleur <eldy@users.sourceforge.net>
2004-10-31 13:33:37 +0000

    Trad: Traductions sur le module adherents

--- htdocs/public/adherents/priv_liste.php
+++ htdocs/public/adherents/priv_liste.php
@@ -1,6 +1,7 @@
 <?php
 /* Copyright (C) 2001-2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
- * Copyright (C) 2002-2003 Jean-Louis Bergamo <jlb@j1b.org>
+ * Copyright (C) 2002-2003 Jean-Louis Bergamo   <jlb@j1b.org>
+ * Copyright (C) 2004      Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -47,47 +48,32 @@ if ($result)
   $i = 0;
   
   print_barre_liste("Liste des adhérents", $page, "priv_liste.php", "&statut=$statut&sortorder=$sortorder&sortfield=$sortfield");
-  print "<TABLE border=\"0\" width=\"100%\" cellspacing=\"0\" cellpadding=\"4\">";
+  print "<table class=\"noborder\" width=\"100%\">";
 
-  print '<TR class="liste_titre">';
-
-
+  print '<tr class="liste_titre">';
   print "<td><a href=\"".$_SERVER['SCRIPT_NAME'] . "?page=$page&sortorder=ASC&sortfield=d.prenom\">Prenom</a> <a href=\"".$_SERVER['SCRIPT_NAME'] . "?page=$page&sortorder=ASC&sortfield=d.nom\">Nom</a> / <a href=\"".$_SERVER['SCRIPT_NAME'] . "?page=$page&sortorder=ASC&sortfield=d.societe\">Société</a></td>\n";
-
-  print "<td>";
   print_liste_field_titre("Date naissance","priv_liste.php","naiss","&page=$page");
-  print "</td>\n";
-
-  print "<td>";
   print_liste_field_titre("Email","priv_liste.php","email","&page=$page");
-  print "</td>\n";
-
-  print "<td>";
   print_liste_field_titre("CP","priv_liste.php","cp","&page=$page");
-  print "</td>\n";
-
-  print "<td>";
   print_liste_field_titre("Vile","priv_liste.php","ville","&page=$page");
-  print "</td>\n";
-
   print "<td>Photo</td>\n";
-  print "</TR>\n";
+  print "</tr>\n";
     
   $var=True;
   while ($i < $num)
     {
       $objp = $db->fetch_object( $i);
       $var=!$var;
-      print "<TR $bc[$var]>";
-      print "<TD><a href=\"priv_fiche.php?rowid=$objp->rowid\">".stripslashes($objp->prenom)." ".stripslashes($objp->nom)." / ".stripslashes($objp->societe)."</a></TD>\n";
-      print "<TD>$objp->naiss</TD>\n";
-      print "<TD>$objp->email</TD>\n";
-      print "<TD>$objp->cp</TD>\n";
-      print "<TD>$objp->ville</TD>\n";
+      print "<tr $bc[$var]>";
+      print "<td><a href=\"priv_fiche.php?rowid=$objp->rowid\">".stripslashes($objp->prenom)." ".stripslashes($objp->nom)." / ".stripslashes($objp->societe)."</a></TD>\n";
+      print "<td>$objp->naiss</td>\n";
+      print "<td>$objp->email</td>\n";
+      print "<td>$objp->cp</td>\n";
+      print "<td>$objp->ville</td>\n";
       if (isset($objp->photo) && $objp->photo!= ''){
-	print "<TD><A HREF=\"$objp->photo\"><IMG SRC=\"$objp->photo\" HEIGHT=64 WIDTH=64></A></TD>\n";
+	print "<td><A HREF=\"$objp->photo\"><IMG SRC=\"$objp->photo\" HEIGHT=64 WIDTH=64></A></TD>\n";
       }else{
-	print "<TD>&nbsp;</TD>\n";
+	print "<td>&nbsp;</td>\n";
       }
       print "</tr>";
       $i++;
@@ -96,8 +82,7 @@ if ($result)
 }
 else
 {
-  print $sql;
-  print $db->error();
+  dolibarr_print_error($db);
 }
 
 
