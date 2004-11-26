3cd152a85469866c589e084f24ee2ea0707ba8e4
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-11-26 14:27:09 +0000

    Ajout champs Date

--- mysql/tables/llx_contratdet_log.sql
+++ mysql/tables/llx_contratdet_log.sql
@@ -25,6 +25,7 @@ create table llx_contratdet_log
   rowid                 integer AUTO_INCREMENT PRIMARY KEY,
   tms                   timestamp,
   fk_contratdet         integer NOT NULL,
+  date                  datetime NOT NULL,
   statut                smallint NOT NULL,
   fk_user_author        integer NOT NULL,
   commentaire           text
