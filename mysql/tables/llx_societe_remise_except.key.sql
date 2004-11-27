b77dee7bfcee06331d635fa401cc29b1dcc5986a
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-11-27 16:10:25 +0000

    Nouveau fichier

---
+++ mysql/tables/llx_societe_remise_except.key.sql
@@ -0,0 +1,37 @@
+-- ============================================================================
+-- Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- Remises exceptionnelles
+--
+--
+-- ============================================================================
+
+
+
+ALTER TABLE llx_societe_remise_except ADD INDEX (fk_user);
+ALTER TABLE llx_societe_remise_except ADD INDEX (fk_soc);
+ALTER TABLE llx_societe_remise_except ADD INDEX (fk_facture);
+
+ALTER TABLE llx_societe_remise_except ADD FOREIGN KEY (fk_user)    REFERENCES llx_user (rowid);
+ALTER TABLE llx_societe_remise_except ADD FOREIGN KEY (fk_soc)     REFERENCES llx_societe (idp);
+ALTER TABLE llx_societe_remise_except ADD FOREIGN KEY (fk_facture) REFERENCES llx_facture (rowid);
+
+
+
