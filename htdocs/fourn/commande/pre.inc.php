4272dc9daed82a5ff2cb92afb11308cba831b6e0
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-11-29 09:53:33 +0000

    Nouveau fichier

---
+++ htdocs/fourn/commande/pre.inc.php
@@ -0,0 +1,44 @@
+<?PHP
+/* Copyright (C) 2001-2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+require("../../main.inc.php");
+
+function llxHeader($head = "", $urlp = "")
+{
+  global $user, $langs;
+  $langs->load("orders");
+  /*
+   *
+   *
+   */
+  top_menu($head);
+
+  $menu = new Menu();
+
+  $menu->add(DOL_URL_ROOT."/fourn/index.php", "Fournisseurs");
+
+  $menu->add(DOL_URL_ROOT."/fourn/commande/", $langs->trans("Orders"));
+  $menu->add_submenu(DOL_URL_ROOT."/fourn/commande/liste.php", $langs->trans("Liste"));
+
+  left_menu($menu->liste);
+}
+
+?>
