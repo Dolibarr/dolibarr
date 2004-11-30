9863191ff36a01ffb3f3f86f96bdff6dc9bc05df
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2004-11-30 16:32:19 +0000

    Ajout fk_methode_commande

--- mysql/tables/llx_commande_fournisseur.sql
+++ mysql/tables/llx_commande_fournisseur.sql
@@ -22,29 +22,30 @@
 
 create table llx_commande_fournisseur
 (
-  rowid            integer AUTO_INCREMENT PRIMARY KEY,
-  tms              timestamp,
-  fk_soc           integer,
-  fk_soc_contact   integer,
-  fk_projet        integer DEFAULT 0,     -- projet auquel est rattache la commande
-  ref              varchar(30) NOT NULL,  -- propal number
-  date_creation    datetime,              -- date de creation 
-  date_valid       datetime,              -- date de validation
-  date_cloture     datetime,              -- date de cloture
-  date_commande    date,                  -- date de la commande
-  fk_user_author   integer,               -- createur de la commande
-  fk_user_valid    integer,               -- valideur de la commande
-  fk_user_cloture  integer,               -- cloture de la propale signee ou non signee
-  source           smallint NOT NULL,
-  fk_statut        smallint  default 0,
-  amount_ht        real      default 0,
-  remise_percent   real      default 0,
-  remise           real      default 0,
-  tva              real      default 0,
-  total_ht         real      default 0,
-  total_ttc        real      default 0,
-  note             text,
-  model_pdf        varchar(50),
-  facture          tinyint default 0,   
+  rowid               integer AUTO_INCREMENT PRIMARY KEY,
+  tms                 timestamp,
+  fk_soc              integer,
+  fk_soc_contact      integer,
+  fk_projet           integer DEFAULT 0,     -- projet auquel est rattache la commande
+  ref                 varchar(30) NOT NULL,  -- propal number
+  date_creation       datetime,              -- date de creation 
+  date_valid          datetime,              -- date de validation
+  date_cloture        datetime,              -- date de cloture
+  date_commande       date,                  -- date de la commande
+  fk_methode_commande integer default 0,
+  fk_user_author      integer,               -- createur de la commande
+  fk_user_valid       integer,               -- valideur de la commande
+  fk_user_cloture     integer,               -- cloture de la propale signee ou non signee
+  source              smallint NOT NULL,
+  fk_statut           smallint  default 0,
+  amount_ht           real      default 0,
+  remise_percent      real      default 0,
+  remise              real      default 0,
+  tva                 real      default 0,
+  total_ht            real      default 0,
+  total_ttc           real      default 0,
+  note                text,
+  model_pdf           varchar(50),
+
   UNIQUE INDEX (ref)
 )type=innodb;
