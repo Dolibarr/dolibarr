fc9cd4841409742232c381d70e34383eb58fbee0
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-01-03 14:25:43 +0000

    Suppression fk_export_compta

--- mysql/tables/llx_facture.sql
+++ mysql/tables/llx_facture.sql
@@ -44,7 +44,6 @@ create table llx_facture
   fk_mode_reglement   integer,   -- mode de reglement (Virement, Prélèvement)
   date_lim_reglement  date,      -- date limite de reglement
   note                text,
-  fk_export_compta    integer DEFAULT 0 NOT NULL,
 
   UNIQUE INDEX (facnumber),
   INDEX fksoc (fk_soc)
