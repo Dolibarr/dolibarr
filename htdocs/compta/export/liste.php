f7abd56c7a3f7a1e087fb248cd5b6589b6b1057f
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-01-05 09:04:12 +0000

    Nouveau fichier

---
+++ htdocs/compta/export/liste.php
@@ -0,0 +1,113 @@
+<?PHP
+/* Copyright (C) 2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+require("./pre.inc.php");
+
+$page = $_GET["page"];
+$sortorder = $_GET["sortorder"];
+$sortfield = $_GET["sortfield"];
+
+llxHeader('','Compta - Export');
+/*
+ * Sécurité accés client
+ */
+if ($user->societe_id > 0) 
+{
+  $action = '';
+  $socidp = $user->societe_id;
+}
+
+if ($sortorder == "") {
+  $sortorder="DESC";
+}
+if ($sortfield == "") {
+  $sortfield="ec.date_export";
+}
+
+/*
+ * Recherche
+ *
+ *
+ */
+
+if ($page == -1) { $page = 0 ; }
+
+$offset = $conf->liste_limit * $page ;
+$pageprev = $page - 1;
+$pagenext = $page + 1;
+
+/*
+ * Mode Liste
+ *
+ *
+ *
+ */
+
+$sql = "SELECT ec.date_export, ec.ref";
+$sql .= " FROM ".MAIN_DB_PREFIX."export_compta as ec";
+
+$sql .= " ORDER BY $sortfield $sortorder " . $db->plimit($conf->liste_limit+1, $offset);
+
+$result = $db->query($sql);
+if ($result)
+{
+  $num = $db->num_rows();
+  $i = 0;
+  
+  print_barre_liste("Exports", $page, "liste.php", $urladd, $sortfield, $sortorder, '', $num);
+  print"\n<!-- debut table -->\n";
+  print '<table class="noborder" width="100%" cellspacing="0" cellpadding="4">';
+  print '<tr class="liste_titre">';
+
+  print_liste_field_titre("Réf","liste.php","ec.ref");
+
+  print '<td>Client facturé</td>';
+
+  print "</tr>\n";
+
+  $var=True;
+
+  $dir = DOL_DATA_ROOT."/compta/export/";
+
+  while ($i < min($num,$conf->liste_limit))
+    {
+      $obj = $db->fetch_object($i);	
+      $var=!$var;
+
+      print "<tr $bc[$var]>";
+
+      print '<td>'.stripslashes($obj->ref).'</td>';
+
+      print "</tr>\n";
+      $i++;
+    }
+  print "</table>";
+  $db->free();
+}
+else 
+{
+  print $db->error() . ' ' . $sql;
+}
+
+$db->close();
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
