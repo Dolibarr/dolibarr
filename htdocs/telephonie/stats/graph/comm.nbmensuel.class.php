7c9b703ccf8bdbd72b39c2d201a6a6b4d3cd2e4b
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-01-12 15:21:22 +0000

    Nouveau fichier

---
+++ htdocs/telephonie/stats/graph/comm.nbmensuel.class.php
@@ -0,0 +1,77 @@
+<?PHP
+/* Copyright (C) 2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+require_once (DOL_DOCUMENT_ROOT."/telephonie/stats/bar.class.php");
+
+class GraphCommNbMensuel extends GraphBar{
+
+
+  Function GraphCommNbMensuel($DB, $file)
+  {
+    $this->name = "comm.nbmensuel";
+    $this->db = $DB;
+    $this->file = $file;
+    $this->showframe = true;
+    $this->client = 0;
+    $this->titre = "Nombre de communication par mois";
+
+    $this->barcolor = "bisque3";
+
+    $this->datas = array();
+    $this->labels = array();
+  }
+
+  Function Graph($datas='', $labels='')
+  {    
+    $this->GetDatas();
+    $this->GraphDraw($this->file, $this->datas, $this->labels);
+  }
+
+  Function GetDatas()
+  {
+    $sql = "SELECT date_format(date, '%Y%m'), count(*)";
+    $sql .= " FROM ".MAIN_DB_PREFIX."telephonie_communications_details";
+    $sql .= " GROUP BY date_format(date, '%Y%m') ASC";
+    
+    if ($this->db->query($sql))
+      {
+	$num = $this->db->num_rows();
+	$i = 0;
+		
+	while ($i < $num)
+	  {
+	    $row = $this->db->fetch_row();	
+	    
+	    $this->labels[$i] = substr($row[0],4,2) . '/'.substr($row[0],2,2);
+	    $this->datas[$i] = $row[1];
+	    
+	    $i++;
+	  }	
+	$this->db->free();
+      }
+    else 
+      {
+	
+      }
+  }
+}  
+?>
