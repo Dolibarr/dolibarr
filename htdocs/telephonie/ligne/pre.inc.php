ec25a5226db2488ddd8c9bd274741ba4f50b5851
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-01-26 14:34:47 +0000

    Ajout test sur le menu de simulation

--- htdocs/telephonie/ligne/pre.inc.php
+++ htdocs/telephonie/ligne/pre.inc.php
@@ -37,12 +37,17 @@ function llxHeader($head = "", $title="") {
 
   $menu->add(DOL_URL_ROOT."/telephonie/index.php", "Telephonie");
 
-  $menu->add(DOL_URL_ROOT."/telephonie/simulation/fiche.php", "Simulation");
-  $menu->add_submenu(DOL_URL_ROOT."/telephonie/simulation/fiche.php?action=create", "Nouvelle");
+  if (TELEPHONIE_MODULE_SIMULATION == 1)
+    {
+      $menu->add(DOL_URL_ROOT."/telephonie/simulation/fiche.php", "Simulation");
+      $menu->add_submenu(DOL_URL_ROOT."/telephonie/simulation/fiche.php?action=create", "Nouvelle");
+    }
 
   $menu->add(DOL_URL_ROOT."/telephonie/tarifs/", "Tarifs");
 
   $menu->add(DOL_URL_ROOT."/telephonie/client/index.php", "Clients");
+
+  $menu->add(DOL_URL_ROOT."/telephonie/contrat/", "Contrats");
 
   $menu->add(DOL_URL_ROOT."/telephonie/ligne/index.php", "Lignes");
   $menu->add_submenu(DOL_URL_ROOT."/telephonie/ligne/liste.php", "Liste");
