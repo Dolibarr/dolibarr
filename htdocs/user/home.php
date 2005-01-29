82494a3598360dc7b05a8b4bc1b79ff1731a2f21
Laurent Destailleur <eldy@users.sourceforge.net>
2005-01-29 15:27:01 +0000

    New: Ajout page accueil de l'espace utilisateurs et groupes pour le gestionnaire de menu eldy.

---
+++ htdocs/user/home.php
@@ -0,0 +1,162 @@
+<?php
+/* Copyright (C) 2005 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+/**
+	    \file       htdocs/user/home.php
+		\brief      Page acceuil de la zone utilisateurs et groupes
+		\version    $Revision$
+*/
+ 
+require("./pre.inc.php");
+	  
+$langs->load("users");
+
+$user->getrights('users');
+
+
+
+llxHeader();
+
+
+print_titre($langs->trans("MenuUsersAndGroups"));
+
+print '<table border="0" width="100%">';
+
+print '<tr><td valign="top" width="30%">';
+
+
+/*
+ * Recherche User
+ */
+    $var=false;
+	print '<form method="post" action="'.DOL_URL_ROOT.'/user/index.php">';
+	print '<table class="noborder" width="100%">';
+	print '<tr class="liste_titre"><td colspan="2">Rechercher un utilisateur</td></tr>';
+	print '<tr '.$bc[$var].'><td>';
+	print $langs->trans("Ref").' : <input type="text" name="sf_ref">&nbsp;<input type="submit" value="'.$langs->trans("Search").'" class="flat"></td></tr>';
+	print "</table></form><br>\n";
+
+/*
+ * Recherche Group
+ */
+    $var=false;
+	print '<form method="post" action="'.DOL_URL_ROOT.'/user/group/index.php">';
+	print '<table class="noborder" width="100%">';
+	print '<tr class="liste_titre"><td colspan="2">Rechercher un groupe</td></tr>';
+	print '<tr '.$bc[$var].'><td>';
+	print $langs->trans("Ref").' : <input type="text" name="search_contract">&nbsp;<input type="submit" value="'.$langs->trans("Search").'" class="flat"></td></tr>';
+	print "</table></form><br>\n";
+
+
+print '</td><td valign="top" width="70%">';
+
+
+/*
+ * Derniers groupes créés
+ *
+ */
+$max=0;
+
+$sql = "SELECT g.rowid, g.nom, g.note, ".$db->pdate("g.datec")." as datec";
+$sql .= " FROM ".MAIN_DB_PREFIX."usergroup as g";
+$sql .= " ORDER BY g.datec DESC";
+if ($max) $sql .= " LIMIT $max";
+
+if ( $db->query($sql) ) 
+{
+  $num = $db->num_rows();
+  print '<table class="noborder" width="100%">';
+  print '<tr class="liste_titre"><td colspan="4">'.$langs->trans("LastGroupsCreated").'</td></tr>';
+  $var = true;
+  $i = 0;
+
+  while ($i < $num && (! $max || $i < $max)) 
+	{
+	  $obj = $db->fetch_object();
+	  $var=!$var;
+	  
+	  print "<tr $bc[$var]>";
+	  print "<td><a href=\"".DOL_URL_ROOT."/user/group/fiche.php?id=$obj->rowid\">".img_file()."</a>&nbsp;";
+	  print "<a href=\"".DOL_URL_ROOT."/user/group/fiche.php?id=$obj->rowid\">$obj->nom</a></td>";
+	  print "<td width=\"100\" align=\"center\">".dolibarr_print_date($obj->datec,"%d %b %Y")."</td>";
+      print '</tr>';
+	  $i++;
+	}
+  print "</table><br>";
+
+  $db->free();
+} 
+else
+{
+  dolibarr_print_error($db);
+}
+
+
+/*
+ * Derniers utilisateurs créés
+ *
+ */
+$max=5;
+
+$sql = "SELECT u.rowid, u.login, u.name, u.firstname, ".$db->pdate("u.datec")." as datec";
+$sql .= " FROM ".MAIN_DB_PREFIX."user as u";
+$sql .= " ORDER BY u.datec DESC limit $max";
+
+if ( $db->query($sql) ) 
+{
+  $num = $db->num_rows();
+  print '<table class="noborder" width="100%">';
+  print '<tr class="liste_titre"><td colspan="4">'.$langs->trans("LastUsersCreated",min($num,$max)).'</td></tr>';
+  $var = true;
+  $i = 0;
+
+  while ($i < $num && $i < $max) 
+	{
+	  $obj = $db->fetch_object();
+	  $var=!$var;
+	  
+	  print "<tr $bc[$var]>";
+	  print "<td><a href=\"".DOL_URL_ROOT."/user/fiche.php?id=$obj->rowid\">".img_file()."</a>&nbsp;";
+	  print "<a href=\"".DOL_URL_ROOT."/user/fiche.php?id=$obj->rowid\">$obj->firstname $obj->name</a></td>";
+	  print "<td width=\"100\" align=\"center\">".strftime("%d %b %Y",$obj->datec)."</td>";
+      print '</tr>';
+	  $i++;
+	}
+  print "</table><br>";
+
+  $db->free();
+} 
+else
+{
+  dolibarr_print_error($db);
+}
+
+
+
+print '</td></tr>';
+print '</table>';
+
+$db->close();
+ 
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
