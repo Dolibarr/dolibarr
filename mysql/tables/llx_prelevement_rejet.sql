0fcaa6f3dae56f3d14dd45d2124b4fadf94ddba7
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-01-31 09:58:33 +0000

    Nouveau fichier

---
+++ mysql/tables/llx_prelevement_rejet.sql
@@ -0,0 +1,35 @@
+-- ===================================================================
+-- Copyright (C) 2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+-- ===================================================================
+--
+--
+
+create table llx_prelevement_rejet
+(
+  rowid            integer AUTO_INCREMENT PRIMARY KEY,
+  fk_soc           integer NOT NULL,
+  fk_prelevement   integer,
+  amount           real DEFAULT 0,     -- montant total du prelevement
+  motif            integer,
+  note             text,
+  date_creation    datetime,
+  fk_user_creation integer
+
+)type=innodb;
