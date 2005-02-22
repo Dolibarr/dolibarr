0d79b4f4d1fffcec5357641f857b7138d40696d2
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-02-22 16:53:09 +0000

    Nouveau fichiers

---
+++ htdocs/telephonie/stats/fournisseurs/index.php
@@ -0,0 +1,68 @@
+<?PHP
+/* Copyright (C) 2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+require("./pre.inc.php");
+
+$page = $_GET["page"];
+$sortorder = $_GET["sortorder"];
+
+if (!$user->rights->telephonie->lire)
+  accessforbidden();
+
+llxHeader('','Telephonie - Statistiques');
+/*
+ * Sécurité accés client
+ */
+if ($user->societe_id > 0) 
+{
+  $action = '';
+  $socidp = $user->societe_id;
+}
+
+
+$h = 0;
+
+$head[$h][0] = DOL_URL_ROOT.'/telephonie/stats/communications/index.php';
+$head[$h][1] = "Global";
+$hselected = $h;
+$h++;
+
+
+dolibarr_fiche_head($head, $hselected, "Fournisseurs");
+
+print '<table class="noborder" width="100%" cellspacing="0" cellpadding="4">'."\n";
+
+print '<tr><td width="50%" valign="top">'."\n";
+
+print '<img src="'.DOL_URL_ROOT.'/viewimage.php?modulepart=telephoniegraph&file=communications/duree.png" alt="Duree Moyenne"><br /><br />'."\n";
+
+print '</td><td valign="top" width="50%">'."\n";
+
+
+
+print '</td></tr>';
+
+print '</table>';
+
+$db->close();
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
