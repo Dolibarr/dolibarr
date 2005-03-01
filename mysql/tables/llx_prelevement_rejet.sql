00e28a8a2fa95ea12236574ac3264db8de8090d1
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-03-01 15:02:03 +0000

    Modif structure again

--- mysql/tables/llx_prelevement_rejet.sql
+++ mysql/tables/llx_prelevement_rejet.sql
@@ -25,7 +25,6 @@ create table llx_prelevement_rejet
 (
   rowid                 integer AUTO_INCREMENT PRIMARY KEY,
   fk_prelevement_lignes integer,
-  amount                real DEFAULT 0,     -- montant total du prelevement
   motif                 integer,
   note                  text,
   date_creation         datetime,
