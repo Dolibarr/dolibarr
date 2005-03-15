ded45e1082e862174640c7291e131b0d45bba379
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-03-15 19:20:57 +0000

    Ajout tables pour les prelev

--- mysql/migration/1.1.0-1.2.0-RC1.sql
+++ mysql/migration/1.1.0-1.2.0-RC1.sql
@@ -202,12 +202,96 @@ alter table c_propalst       rename llx_c_propalst ;
 alter table c_stcomm         rename llx_c_stcomm ;
 alter table c_typent         rename llx_c_typent ;
 
-
-
 alter table llx_c_actioncomm add type varchar(10) not null default 'system' after id;
 alter table llx_c_actioncomm add active tinyint default 1 NOT NULL after libelle;
 
 alter table llx_c_paiement add code varchar(6) after id;
+
+
+create table llx_prelevement_facture
+(
+  rowid                  integer AUTO_INCREMENT PRIMARY KEY,
+  fk_facture             integer NOT NULL,
+  fk_prelevement_lignes  integer NOT NULL
+
+)type=innodb;
+
+
+create table llx_prelevement_facture_demande
+(
+  rowid               integer AUTO_INCREMENT PRIMARY KEY,
+  fk_facture          integer NOT NULL,
+  amount              real NOT NULL,
+  date_demande        datetime NOT NULL,
+  traite              smallint DEFAULT 0,
+  date_traite         datetime,
+  fk_prelevement_bons integer,
+  fk_user_demande     integer NOT NULL,
+
+  code_banque         varchar(7),
+  code_guichet        varchar(6),
+  number              varchar(255),
+  cle_rib             varchar(5)
+
+)type=innodb;
+
+
+create table llx_prelevement_bons
+(
+  rowid          integer AUTO_INCREMENT PRIMARY KEY,
+  ref            varchar(12),        -- reference
+  datec          datetime,           -- date de creation
+  amount         real DEFAULT 0,     -- montant total du prélèvement
+  statut         smallint DEFAULT 0, -- statut
+  credite        smallint DEFAULT 0, -- indique si le prelevement a été credité
+  note           text,
+  date_trans     datetime,           -- date de transmission à la banque
+  method_trans   smallint,           -- méthode de transmission
+  fk_user_trans  integer,            -- user qui a effectué la transmission
+  date_credit    datetime,           -- date de crédit sur le compte
+  fk_user_credit integer,            -- user qui a remonté l'info de crédit
+  
+  UNIQUE(ref)
+)type=innodb;
+
+
+create table llx_prelevement_lignes
+(
+  rowid               integer AUTO_INCREMENT PRIMARY KEY,
+  fk_prelevement_bons integer,
+  fk_soc              integer NOT NULL,
+  statut              smallint DEFAULT 0,
+
+  client_nom          varchar(255),
+  amount              real DEFAULT 0,
+  code_banque         varchar(7),
+  code_guichet        varchar(6),
+  number              varchar(255),
+  cle_rib             varchar(5),
+
+  note                text
+
+)type=innodb;
+
+
+create table llx_prelevement_rejet
+(
+  rowid                 integer AUTO_INCREMENT PRIMARY KEY,
+  fk_prelevement_lignes integer,
+  date_rejet            datetime,
+  motif                 integer,
+  date_creation         datetime,
+  fk_user_creation      integer,
+  note                  text
+
+)type=innodb;
+
+
+ALTER TABLE llx_prelevement_facture ADD INDEX (fk_prelevement_lignes);
+ALTER TABLE llx_prelevement_facture ADD FOREIGN KEY (fk_prelevement_lignes) REFERENCES llx_prelevement_lignes (rowid);
+ALTER TABLE llx_prelevement_lignes ADD INDEX (fk_prelevement_bons);
+ALTER TABLE llx_prelevement_lignes ADD FOREIGN KEY (fk_prelevement_bons) REFERENCES llx_prelevement_bons (rowid);
+
 
 
 create table llx_mailing
