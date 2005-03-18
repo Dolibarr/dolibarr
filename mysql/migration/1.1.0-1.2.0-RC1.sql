3501cce5609385134389410f05c41c1494de944e
Laurent Destailleur <eldy@users.sourceforge.net>
2005-03-18 01:20:58 +0000

    Fix: Correction oublis dans le scripts de migration

--- mysql/migration/1.1.0-1.2.0-RC1.sql
+++ mysql/migration/1.1.0-1.2.0-RC1.sql
@@ -6,6 +6,15 @@
 -- ;
 
 
+create table llx_usergroup
+(
+  rowid         integer AUTO_INCREMENT PRIMARY KEY,
+  datec         datetime,
+  tms           timestamp,
+  nom           varchar(255) NOT NULL UNIQUE,
+  note          text
+
+)type=innodb;
 
 create table llx_usergroup_rights
 (
@@ -15,6 +24,19 @@ create table llx_usergroup_rights
 
   UNIQUE(fk_usergroup,fk_id)
 )type=innodb;
+
+
+create table llx_usergroup_user
+(
+  rowid         integer AUTO_INCREMENT PRIMARY KEY,
+  fk_user       integer NOT NULL,
+  fk_usergroup  integer NOT NULL,
+
+  UNIQUE(fk_user,fk_usergroup)
+)type=innodb;
+
+DELETE llx_usergroup_rights FROM llx_usergroup_rights LEFT JOIN llx_usergroup ON llx_usergroup_rights.fk_usergroup = llx_usergroup.rowid WHERE llx_usergroup.rowid IS NULL;
+ALTER TABLE llx_usergroup_rights ADD FOREIGN KEY (fk_usergroup)    REFERENCES llx_usergroup (rowid);
 
 
 alter table llx_facture add  increment           varchar(10);
@@ -428,12 +450,14 @@ create table llx_paiementcharge
 update llx_const set visible=0 where name like 'ADHERENT%';
 update llx_const set visible=0 where name like 'PROPALE_ADDON%';
 
+
 create table llx_user_param
 (
   fk_user       integer,
   page          varchar(255),
-  param         varchar(255),
+  param         varchar(64),
   value         varchar(255),
+
   UNIQUE (fk_user,page,param)
 )type=innodb;
 
@@ -1236,7 +1260,6 @@ create table llx_c_currencies
 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'BT', 'THB', 1, 'Bath thailandais'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'CD', 'DKK', 1, 'Couronnes dannoises'); 
-insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'CL', 'CLP', 1, 'Peso chilien'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'CN', 'NOK', 1, 'Couronnes norvegiennes'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'CS', 'SEK', 1, 'Couronnes suedoises'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'CZ', 'CZK', 1, 'Couronnes tcheques'); 
@@ -1244,7 +1267,6 @@ insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'DA', 'A
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'DC', 'CAD', 1, 'Dollars canadiens'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'DH', 'HKD', 1, 'Dollars hong kong'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'DS', 'SGD', 1, 'Dollars singapour'); 
-insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'DT', 'TWD', 1, 'Dollars taiwanais'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'DU', 'USD', 1, 'Dollars us'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'EC', 'XEU', 1, 'Ecus'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'ES', 'PTE', 1, 'Escudos'); 
@@ -1259,11 +1281,11 @@ insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'LS', 'G
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'MA', 'DEM', 1, 'Deutsch mark'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'MF', 'FIM', 1, 'Mark finlandais'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'PA', 'ARP', 1, 'Pesos argentins'); 
-insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'PC', 'CLP', 1, 'Nouveaux pesos chilien'); 
+insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'PC', 'CLP', 1, 'Pesos chilien'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'PE', 'ESP', 1, 'Pesete'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'PL', 'PLN', 1, 'Zlotys polonais'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'SA', 'ATS', 1, 'Shiliing autrichiens'); 
-insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'TW', 'TWD', 1, 'Nvx dollar de taiwan'); 
+insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'TW', 'TWD', 1, 'Dollar taiwanais'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'YE', 'JPY', 1, 'Yens'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'ZA', 'ZAR', 1, 'Rand africa'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'DR', 'GRD', 1, 'Drachme (grece)'); 
@@ -1277,7 +1299,6 @@ insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'EG', 'E
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'KR', 'KRW', 1, 'Won coree du sud'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'NZ', 'NZD', 1, 'Dollar neo-zelandais'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'TR', 'TRL', 1, 'Livre turque'); 
-insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'CY', 'CNY', 1, 'Yvanrenminbi chinois'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'ID', 'IDR', 1, 'Rupiahs d''indonesie'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'IN', 'INR', 1, 'Roupie indienne'); 
 insert into llx_c_currencies ( code, code_iso, active, label ) values ( 'LT', 'LTL', 1, 'Litas'); 
