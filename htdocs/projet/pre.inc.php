082a80e05dac83f59be5b8dd9bdd339073688831
Laurent Destailleur <eldy@users.sourceforge.net>
2005-04-02 10:18:01 +0000

    Doc: Documentation doxygen

--- htdocs/projet/pre.inc.php
+++ htdocs/projet/pre.inc.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2001-2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2005      Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +21,8 @@
  *
  */
 
-/*! \file htdocs/projet/pre.inc.php
+/**
+        \file       htdocs/projet/pre.inc.php
         \ingroup    projet
 		\brief      Fichier de gestion du menu gauche du module projet
 		\version    $Revision$
