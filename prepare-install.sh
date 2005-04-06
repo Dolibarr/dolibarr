3f113d3526738d3b68969c19c481f0d1b8d59903
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-04-06 12:01:19 +0000

    Correction Bug #12579

--- prepare-install.sh
+++ prepare-install.sh
@@ -3,9 +3,9 @@
 #
 # Prepare l'installation web de Dolibarr
 #
-mkdir document
-chown www-data document
-mkdir htdocs/document
-chown www-data htdocs/document
+mkdir documents
+chown www-data documents
+mkdir htdocs/documents
+chown www-data htdocs/documents
 touch htdocs/conf/conf.php
 chown www-data htdocs/conf/conf.php
