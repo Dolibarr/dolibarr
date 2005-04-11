cd13ce901ea7836f492b94d2a120289a7eaab036
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-04-11 15:54:39 +0000

    Nouveau fichier

---
+++ htdocs/fourn/product/pre.inc.php
@@ -0,0 +1,77 @@
+<?php
+/* Copyright (C) 2001-2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004      Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+/*!
+  \file       htdocs/product/pre.inc.php
+  \ingroup    product,service
+  \brief      Fichier gestionnaire du menu gauche des produits et services
+  \version    $Revision$
+*/
+
+require("../../main.inc.php");
+require_once DOL_DOCUMENT_ROOT."/fourn/fournisseur.class.php";
+$langs->load("suppliers");
+$langs->load("products");
+
+$user->getrights("produit");
+
+function llxHeader($head = "", $title="", $help_url='',$addons='')
+{
+  global $user, $conf, $langs;
+
+  
+  top_menu($head, $title);
+
+  $menu = new Menu();
+
+  if (is_array($addons))
+    {
+      //$menu->add($url, $libelle);
+      $menu->add($addons[0][0], $addons[0][1]);
+    }
+
+  if ($conf->fournisseur->enabled) 
+    {
+      $menu->add(DOL_URL_ROOT."/fourn/index.php", $langs->trans("Suppliers"));      
+    }
+  
+  if ($conf->societe->enabled)
+    {
+      $menu->add_submenu(DOL_URL_ROOT."/fourn/contact.php",$langs->trans("Contacts"));
+    }
+  
+  $langs->load("bills");
+  $menu->add(DOL_URL_ROOT."/fourn/facture/index.php", $langs->trans("Bills"));   
+     
+  $langs->load("orders");
+  $menu->add(DOL_URL_ROOT."/fourn/commande/",$langs->trans("Orders"));
+  
+  if ($conf->produit->enabled)
+  {
+    $menu->add(DOL_URL_ROOT."/fourn/product/", $langs->trans("Products"));
+    $menu->add_submenu(DOL_URL_ROOT."/fourn/product/liste.php?type=0", $langs->trans("List"));
+  }
+
+  left_menu($menu->liste, $help_url);
+}
+?>
