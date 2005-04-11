be966bef44f63b63e0f956527f8b63a5ba58fe41
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-04-11 16:00:06 +0000

    Nouveau fichier

---
+++ htdocs/fourn/product/index.php
@@ -0,0 +1,154 @@
+<?php
+/* Copyright (C) 2001-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2005 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+/*!
+  \file       htdocs/product/index.php
+  \ingroup    product
+  \brief      Page accueil des produits et services
+  \version    $Revision$
+*/
+
+require("./pre.inc.php");
+
+if (!$user->rights->produit->lire) accessforbidden();
+
+/*
+ * Affichage page accueil
+ *
+ */
+
+llxHeader("","",$langs->trans("ProductsAndServices"));
+
+print_titre($langs->trans("ProductsAndServicesArea"));
+
+print '<table border="0" width="100%">';
+
+print '<tr><td valign="top" width="30%">';
+
+/*
+ * Zone recherche produit/service
+ */
+print '<form method="post" action="liste.php">';
+print '<table class="noborder" width="100%">';
+print "<tr class=\"liste_titre\">\n";
+print '<td colspan="3">'.$langs->trans("Search").'</td></tr>';
+print "<tr $bc[0]><td>";
+print $langs->trans("Ref").' :</td><td><input class="flat" type="text" size="20" name="sf_ref"></td><td><input type="submit" value="'.$langs->trans("Search").'" class="button"></td></tr>';
+print "<tr $bc[0]><td>";
+print $langs->trans("Label").' :</td><td><input class="flat" type="text" size="20" name="snom"></td><td><input class="button" type="submit" value="'.$langs->trans("Search").'"></td></tr>';
+print "</table></form><br>\n";
+
+/*
+ * Nombre de produits et/ou services
+ */
+$prodser = array();
+$sql = "SELECT count(*), fk_product_type  FROM ".MAIN_DB_PREFIX."product as p GROUP BY fk_product_type";
+if ($db->query($sql))
+{
+  $num = $db->num_rows();
+  $i = 0;
+  while ($i < $num)
+    {
+      $row = $db->fetch_row($i);
+      $prodser[$row[1]] = $row[0];
+      $i++;
+    }
+  $db->free();
+}
+
+print '<table class="noborder" width="100%">';
+print '<tr class="liste_titre"><td colspan="2">'.$langs->trans("Statistics").'</td></tr>';
+if ($conf->produit->enabled)
+{
+    print "<tr $bc[0]>";
+    print '<td><a href="liste.php?type=0&amp;envente=0">'.$langs->trans("Products").'</a></td><td>'.round($prodser[0]).'</td>';
+    print "</tr>";
+}
+if ($conf->service->enabled)
+{
+    print "<tr $bc[1]>";
+    print '<td><a href="liste.php?type=1&amp;envente=1">'.$langs->trans("Services").'</a></td><td>'.round($prodser[1]).'</td>';
+    print "</tr>";
+}
+print '</table>';
+
+print '</td><td valign="top" width="70%">';
+
+
+/*
+ * Derniers produits/services en vente
+ */
+$sql = "SELECT p.rowid, p.label, p.price, p.ref, p.fk_product_type";
+$sql .= " FROM ".MAIN_DB_PREFIX."product as p ";
+$sql .= " WHERE p.fk_product_type=0";
+$sql .= " ORDER BY p.datec DESC ";
+$sql .= $db->plimit(15 ,0);
+
+$resql = $db->query($sql) ;
+
+if ($resql)
+{
+  $num = $db->num_rows($resql);
+
+  $i = 0;
+
+  $typeprodser[0]=$langs->trans("Product");
+  $typeprodser[1]=$langs->trans("Service");
+    
+  if ($num > 0)
+    {
+      print '<table class="noborder" width="100%">';
+
+      print '<tr class="liste_titre"><td colspan="3">'.$langs->trans("LastProducts").'</td></tr>';
+    
+      $var=True;
+      while ($i < $num)
+	{
+	  $objp = $db->fetch_object($resql);
+	  $var=!$var;
+	  print "<tr $bc[$var]>";
+	  print "<td><a href=\"fiche.php?id=$objp->rowid\">";
+	  if ($objp->fk_product_type) print img_object($langs->trans("ShowService"),"service");
+	  else print img_object($langs->trans("ShowProduct"),"product");
+	  print "</a> <a href=\"fiche.php?id=$objp->rowid\">$objp->ref</a></td>\n";
+	  print "<td>$objp->label</td>";
+	  print "<td>".$typeprodser[$objp->fk_product_type]."</td>";
+	  print "</tr>\n";
+	  $i++;
+	}
+      $db->free($resql);
+
+      print "</table>";
+    }
+}
+else
+{
+  dolibarr_print_error();
+}
+
+print '</td></tr></table>';
+
+$db->close();
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
---
+++ htdocs/fourn/product/liste.php
@@ -0,0 +1,225 @@
+<?php
+/* Copyright (C) 2001-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2005 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ */
+
+/**
+   \file       htdocs/fourn/product/liste.php
+   \ingroup    produit
+   \brief      Page liste des produits ou services
+   \version    $Revision$
+*/
+
+require("./pre.inc.php");
+
+$langs->load("products");
+
+$user->getrights('produit');
+
+if (!$user->rights->produit->lire) accessforbidden();
+
+$sref=isset($_GET["sref"])?$_GET["sref"]:$_POST["sref"];
+$snom=isset($_GET["snom"])?$_GET["snom"]:$_POST["snom"];
+
+$type=isset($_GET["type"])?$_GET["type"]:$_POST["type"];
+
+$sortfield = isset($_GET["sortfield"])?$_GET["sortfield"]:$_POST["sortfield"];
+$sortorder = isset($_GET["sortorder"])?$_GET["sortorder"]:$_POST["sortorder"];
+$page = $_GET["page"];
+if ($page < 0) { 
+  $page = 0 ; }
+
+$limit = $conf->liste_limit;
+$offset = $limit * $page ;
+  
+if (! $sortfield) $sortfield="p.ref";
+if (! $sortorder) $sortorder="DESC";
+
+if ($_POST["button_removefilter"] == $langs->trans("RemoveFilter")) {
+    $sref="";
+    $snom="";
+}
+
+/*
+ * Mode Liste
+ *
+ */
+
+$title=$langs->trans("ProductsAndServices");
+
+$sql = "SELECT p.rowid, p.label, p.price, p.ref, p.fk_product_type";
+$sql .= " FROM ".MAIN_DB_PREFIX."product as p";
+
+if ($_GET["fourn_id"] > 0)
+{
+  $fourn_id = $_GET["fourn_id"];
+  $sql .= ", ".MAIN_DB_PREFIX."product_fournisseur as pf";
+}
+
+if ($_POST["mode"] == 'search')
+{
+  $sql .= " WHERE p.ref like '%".$_POST["sall"]."%'";
+  $sql .= " OR p.label like '%".$_POST["sall"]."%'";
+}
+else
+{
+  $sql .= " WHERE 1=1";
+  if (isset($_GET["type"]) || isset($_POST["type"]))
+    {
+      $sql .= " AND p.fk_product_type = ".(isset($_GET["type"])?$_GET["type"]:$_POST["type"]);
+    }
+  if ($sref)
+    {
+      $sql .= " AND p.ref like '%".$sref."%'";
+    }
+  if ($snom)
+    {
+      $sql .= " AND p.label like '%".$snom."%'";
+    }
+  if (isset($_GET["envente"]) && strlen($_GET["envente"]) > 0)
+    {
+      $sql .= " AND p.envente = ".$_GET["envente"];
+    }
+  else
+    {
+      $sql .= " AND p.envente = 1";
+    }
+}
+
+if ($fourn_id > 0)
+{
+  $sql .= " AND p.rowid = pf.fk_product AND pf.fk_soc = $fourn_id";
+}
+
+$sql .= " ORDER BY $sortfield $sortorder ";
+$sql .= $db->plimit($limit + 1 ,$offset);
+$result = $db->query($sql) ;
+
+if ($result)
+{
+    $num = $db->num_rows();
+    
+    $i = 0;
+  
+    if ($num == 1 && (isset($_POST["sall"]) or $snom or $sref))
+    {
+        $objp = $db->fetch_object($i);
+        Header("Location: fiche.php?id=$objp->rowid");
+    }
+    
+    if (isset($_GET["envente"]) || isset($_POST["envente"])) {
+        $envente = (isset($_GET["envente"])?$_GET["envente"]:$_POST["envente"]);
+    }
+    else {
+        $envente=1;
+    }
+    
+    if (! $envente)
+    {
+        if (isset($_GET["type"]) || isset($_POST["type"])) {
+            $type=isset($_GET["type"])?$_GET["type"]:$_POST["type"];
+            if ($type) { $texte = $langs->trans("ServicesNotOnSell"); }
+            else { $texte = $langs->trans("ProductsNotOnSell"); }
+        } else {
+            $texte = $langs->trans("ProductsAndServicesNotOnSell");
+        }
+    }
+    else
+    {
+        if (isset($_POST["type"]) || isset($_GET["type"])) {
+            if ($type) { $texte = $langs->trans("ServicesOnSell"); }
+            else { $texte = $langs->trans("ProductsOnSell"); }
+        } else {
+            $texte = $langs->trans("ProductsAndServicesOnSell");
+        }
+    }
+
+  llxHeader("","",$texte);
+
+  if ($sref || $snom || $_POST["sall"] || $_POST["search"])
+    {
+      print_barre_liste($texte, $page, "liste.php", "&sref=".$sref."&snom=".$snom."&amp;envente=".$_POST["envente"], $sortfield, $sortorder,'',$num);
+    }
+  else
+    {
+      print_barre_liste($texte, $page, "liste.php", "&sref=$sref&snom=$snom&fourn_id=$fourn_id".(isset($type)?"&amp;type=$type":""), $sortfield, $sortorder,'',$num);
+    }
+
+  print '<table class="noborder" width="100%">';
+
+  // Lignes des titres
+  print "<tr class=\"liste_titre\">";
+  print_liste_field_titre($langs->trans("Ref"),"liste.php", "p.ref","&amp;envente=$envente".(isset($type)?"&amp;type=$type":"")."&fourn_id=$fourn_id&amp;snom=$snom&amp;sref=$sref","","",$sortfield);
+  print_liste_field_titre($langs->trans("Label"),"liste.php", "p.label","&envente=$envente&".(isset($type)?"&amp;type=$type":"")."&fourn_id=$fourn_id&amp;snom=$snom&amp;sref=$sref","","",$sortfield);
+  print_liste_field_titre($langs->trans("SellingPrice"),"liste.php", "p.price","&envente=$envente&".(isset($type)?"&amp;type=$type":"")."&fourn_id=$fourn_id&amp;snom=$snom&amp;sref=$sref","",'align="right"',$sortfield);
+  print "</tr>\n";
+  
+  // Lignes des champs de filtre
+  print '<form action="liste.php" method="post">';
+  print '<input type="hidden" name="sortfield" value="'.$sortfield.'">';
+  print '<input type="hidden" name="sortorder" value="'.$sortorder.'">';
+  print '<input type="hidden" name="type" value="'.$type.'">';
+  print '<tr class="liste_titre">';
+  print '<td>';
+  print '<input class="fat" type="text" name="sref" value="'.$sref.'">';
+  print '</td>';
+  print '<td valign="right">';
+  print '<input class="fat" type="text" name="snom" value="'.$snom.'">';
+  print '</td>';
+  print '<td align="center">';
+  print '<input type="submit" class="button" name="button_search" value="'.$langs->trans("Search").'">';
+  print '&nbsp; <input type="submit" class="button" name="button_removefilter" value="'.$langs->trans("RemoveFilter").'">';
+  print '</td>';
+  print '</tr>';
+  print '</form>';
+  
+  
+  $var=True;
+  while ($i < min($num,$limit))
+    {
+      $objp = $db->fetch_object( $i);
+      $var=!$var;
+      print "<tr $bc[$var]><td>";
+      print "<a href=\"../../product/fiche.php?id=$objp->rowid\">";
+      if ($objp->fk_product_type) print img_object($langs->trans("ShowService"),"service");
+      else print img_object($langs->trans("ShowProduct"),"product");
+      print "</a> ";
+      print "<a href=\"fiche.php?id=$objp->rowid\">$objp->ref</a></td>\n";
+      print "<td>$objp->label</td>\n";
+      print '<td align="right">'.price($objp->price).'</td>';
+      print "</tr>\n";
+      $i++;
+    }
+  $db->free();
+
+  print "</table>";
+
+}
+else
+{
+  dolibarr_print_error($db);
+}
+
+
+$db->close();
+
+llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+?>
