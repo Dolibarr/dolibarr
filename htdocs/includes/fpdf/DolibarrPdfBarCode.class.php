da7352b606e543fc57beeab648cafa19c946fe95
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-04-16 17:54:15 +0000

    Ajout i25

--- htdocs/includes/fpdf/DolibarrPdfBarCode.class.php
+++ htdocs/includes/fpdf/DolibarrPdfBarCode.class.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2001-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org> 
+/* Copyright (C) 2005 Rodolphe Quiedeville <rodolphe@quiedeville.org> 
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
