8970a4bae8085d5a16c3d0807c0f0ed091c23a09
Laurent Destailleur <eldy@users.sourceforge.net>
2005-04-23 18:28:21 +0000

    Fix: Traduction

--- htdocs/contrat/ligne.php
+++ htdocs/contrat/ligne.php
@@ -132,7 +132,7 @@ $html = new Form($db);
 
 	  $h = 0;
 	  $head[$h][0] = DOL_URL_ROOT.'/contrat/fiche.php?id='.$contrat->id;
-	  $head[$h][1] = $langs->trans("Contract");
+	  $head[$h][1] = $langs->trans("ContractCard");
 	  $h++;
 
 	  $head[$h][0] = DOL_URL_ROOT.'/contrat/ligne.php?id='.$contrat->id."&ligne=".$_GET["ligne"];
