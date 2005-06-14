79d55aaed60dcbd2a21f8688c60a5b677e173a20
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-06-14 14:39:59 +0000

    Gestion des comissions

--- htdocs/telephonie/telephonie.contrat.class.php
+++ htdocs/telephonie/telephonie.contrat.class.php
@@ -477,19 +477,62 @@ class TelephonieContrat {
 	    $this->db->free($resql);
 	  }
 
-	$sql = "INSERT INTO ".MAIN_DB_PREFIX."telephonie_contrat_priseordre";
-	$sql .= " (fk_contrat, datepo, montant, fk_commercial, fk_distributeur, fk_user)";
-	$sql .= " VALUES (".$this->id.",now(), ".$montant;
-	$sql .= ",".$this->commercial_sign_id.",".$did.",".$user->id.")";
-	
-	$resql = $this->db->query($sql);
-	if ($resql)
+
+	if ($did > 0)
 	  {
 	    
+	    $sql = "SELECT avance_pourcent, avance_duree, rem_pour_prev, rem_pour_autr";
+	    $sql .= " FROM ".MAIN_DB_PREFIX."telephonie_distributeur ";
+	    $sql .= " WHERE rowid = ".$did;
+	    
+	    $resql = $this->db->query($sql);
+	    
+	    if ($resql)
+	      {
+		if ($this->db->num_rows($resql))
+		  {
+		    $pour = $this->db->fetch_row($resql);		
+		  }
+		$this->db->free($resql);
+	      }
+	    
+	    
+	    
+	    $sql = "INSERT INTO ".MAIN_DB_PREFIX."telephonie_contrat_priseordre";
+	    $sql .= " (fk_contrat, datepo, montant, fk_commercial, fk_distributeur, fk_user";
+	    $sql .= " , avance_pourcent, avance_duree, rem_pour_prev, rem_pour_autr)";
+	    $sql .= " VALUES (".$this->id.",now(), ".$montant;
+	    $sql .= ",".$this->commercial_sign_id.",".$did.",".$user->id;
+	    $sql .= ",".$pour[0].",".$pour[1].",".$pour[2].",".$pour[3].")";
+	    
+	    $resql = $this->db->query($sql);
+	    if ($resql)
+	      {
+		
+	      }
+	    else
+	      {
+		dolibarr_syslog($sql);
+	      }
 	  }
 	else
 	  {
-	    dolibarr_syslog($sql);
+	
+	    $sql = "INSERT INTO ".MAIN_DB_PREFIX."telephonie_contrat_priseordre";
+	    $sql .= " (fk_contrat, datepo, montant, fk_commercial, fk_distributeur, fk_user)";
+	    $sql .= " VALUES (".$this->id.",now(), ".$montant;
+	    $sql .= ",".$this->commercial_sign_id.",".$did.",".$user->id.")";
+	    
+	    
+	    $resql = $this->db->query($sql);
+	    if ($resql)
+	      {
+		
+	      }
+	    else
+	      {
+		dolibarr_syslog($sql);
+	      }	    	    
 	  }
       }
   }
