7889182e57374b605935819451d356404684e73f
Laurent Destailleur <eldy@users.sourceforge.net>
2005-06-26 02:22:14 +0000

    New: Ajout index sur dates contrats

--- mysql/tables/llx_contratdet.key.sql
+++ mysql/tables/llx_contratdet.key.sql
@@ -23,6 +23,9 @@
 --
 ALTER TABLE llx_contratdet ADD INDEX idx_contratdet_fk_contrat (fk_contrat);
 ALTER TABLE llx_contratdet ADD INDEX idx_contratdet_fk_product (fk_product);
+ALTER TABLE llx_contratdet ADD INDEX idx_contratdet_date_ouverture_prevue (date_ouverture_prevue);
+ALTER TABLE llx_contratdet ADD INDEX idx_contratdet_date_ouverture (date_ouverture);
+ALTER TABLE llx_contratdet ADD INDEX idx_contratdet_date_fin_validite (date_fin_validite);
 --
 --
 ALTER TABLE llx_contratdet ADD FOREIGN KEY (fk_contrat) REFERENCES llx_contrat (rowid);
