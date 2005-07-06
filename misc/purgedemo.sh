37906e9649330f7803bb0be2cd6e33f1b2e27e37
Regis Houssin <regis@dolibarr.fr>
2005-07-06 15:58:22 +0000

    Suppression de l'ancien purgedemo.sh

--- misc/purgedemo.sh
+++
@@ -1,9 +0,0 @@
-
-mysql dolidemo -e 'delete from llx_facture';
-mysql dolidemo -e 'delete from llx_facturedet';
-mysql dolidemo -e 'delete from llx_paiement';
-mysql dolidemo -e 'delete from llx_propal';
-mysql dolidemo -e 'delete from llx_propaldet';
-
-rm -fr ../htdocs/document/facture/*
-rm -fr ../htdocs/document/propale/*
