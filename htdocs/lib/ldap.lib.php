5c4d64aecec7c9e299283d4c3cd4fa9a3700f6b4
Regis Houssin <regis@dolibarr.fr>
2005-07-10 12:58:27 +0000

    Modification pour la gestion des utilisateurs via LDAP

--- htdocs/lib/ldap.lib.php
+++ htdocs/lib/ldap.lib.php
@@ -54,9 +54,9 @@ function dolibarr_ldap_connect()
 
 function dolibarr_ldap_bind($ds)
 {
-  if (defined("LDAP_ADMIN_PASS") && LDAP_ADMIN_DN && LDAP_ADMIN_PASS)
+  if (defined("LDAP_ADMIN_PASS") && LDAP_SUFFIX_DN && LDAP_ADMIN_DN && LDAP_ADMIN_PASS)
     {
-      $ldapbind = ldap_bind($ds, LDAP_ADMIN_DN, LDAP_ADMIN_PASS);
+      $ldapbind = ldap_bind($ds, LDAP_ADMIN_DN.LDAP_SUFFIX_DN, LDAP_ADMIN_PASS);
     }
 
   return $ldapbind;
