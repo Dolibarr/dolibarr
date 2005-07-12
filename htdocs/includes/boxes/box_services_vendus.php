9084fc91639de1ff592d61e6a66b56c29bf911ea
Laurent Destailleur <eldy@users.sourceforge.net>
2005-07-12 21:09:00 +0000

    Fix: La box des derniers services utilise la table contratdet

--- htdocs/includes/boxes/box_services_vendus.php
+++ htdocs/includes/boxes/box_services_vendus.php
@@ -64,9 +64,9 @@ class box_services_vendus extends ModeleBoxes {
 
         if ($user->rights->produit->lire)
         {
-            $sql  = "SELECT s.nom, s.idp, p.label, p.fk_product_type, c.rowid";
-            $sql .= " FROM ".MAIN_DB_PREFIX."societe as s, ".MAIN_DB_PREFIX."contrat as c, ".MAIN_DB_PREFIX."product as p";
-            $sql .= " WHERE s.idp = c.fk_soc AND c.fk_product = p.rowid";
+            $sql  = "SELECT s.nom, s.idp, c.rowid, cd.rowid as cdid, p.label, p.fk_product_type";
+            $sql .= " FROM ".MAIN_DB_PREFIX."societe as s, ".MAIN_DB_PREFIX."contrat as c, ".MAIN_DB_PREFIX."contratdet as cd, ".MAIN_DB_PREFIX."product as p";
+            $sql .= " WHERE s.idp = c.fk_soc AND c.rowid = cd.fk_contrat AND cd.fk_product = p.rowid";
             if($user->societe_id)
             {
                 $sql .= " AND s.idp = $user->societe_id";
