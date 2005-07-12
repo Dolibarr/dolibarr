15f09bd2b3558e12dc0322884de750898bf05b01
erics <erics>
2005-07-12 13:33:28 +0000

    auto increment manquant

--- mysql/tables/llx_c_chargesociales.sql
+++ mysql/tables/llx_c_chargesociales.sql
@@ -23,7 +23,7 @@
 
 create table llx_c_chargesociales
 (
-  id          integer PRIMARY KEY,
+  id          integer AUTO_INCREMENT PRIMARY KEY,
   libelle     varchar(80),
   deductible  smallint DEFAULT 0 NOT NULL,
   active      tinyint DEFAULT 1  NOT NULL
