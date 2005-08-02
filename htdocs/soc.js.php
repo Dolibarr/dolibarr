b1a181dd880677f829dbb49b3a3ff93739118968
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-08-02 08:57:07 +0000

    Nouveau fichier

---
+++ htdocs/soc.js.php
@@ -0,0 +1,13 @@
+<?php
+
+$js_OpenPopupWindow = "function PopupPostalCode(postalcode,objectville)
+{
+  var url = 'searchpostalcode.php?cp=' + postalcode + '&targetobject=window.opener.document.formsoc.' + objectville.name;
+  //  alert(url);
+  var hWnd = window.open(url, \"SearchPostalCodeWindow\", \"width=\" + 300 + \",height=\" + 150 + \",resizable=yes,scrollbars=yes\");
+  if((document.window != null) && (!hWnd.opener))
+     hWnd.opener = document.window;
+}
+";
+
+?>
