d897acbf0d1c39b4451e1306e717bb9653f2a627
Laurent Destailleur <eldy@users.sourceforge.net>
2005-08-11 20:06:04 +0000

    Qual: Uniformisation code

--- htdocs/bookmark4u.class.php
+++ htdocs/bookmark4u.class.php
@@ -19,140 +19,143 @@
  * $Source$
  */
 
-/*!	\file htdocs/bookmark4u.class.php
-  \brief  Fichier de la classe bookmark4u
-  \author Rodolphe Quiedeville
-  \version $Revision$
+/**
+        \file       htdocs/bookmark4u.class.php
+        \brief      Fichier de la classe bookmark4u
+        \author     Rodolphe Quiedeville
+        \version    $Revision$
 */
 
 class Bookmark4u
 {
-  var $db;
-	
-  var $id;
-
-  /**
-   *    \brief Constructeur de la classe
-   *    \param  $DB         handler accès base de données
-   *    \param  $id         id de l'utilisateur (0 par défaut)
-   */
-  function Bookmark4u($DB, $id=0)
+    var $db;
+    
+    var $id;
+    
+    
+    /**
+     *    \brief Constructeur de la classe
+     *    \param  $DB         handler accès base de données
+     *    \param  $id         id de l'utilisateur (0 par défaut)
+     */
+    function Bookmark4u(&$DB, $id=0)
     {
-
-      $this->db = $DB;
+      $this->db = &$DB;
       $this->id = $id;
-
       return 1;
     }
-
-  /**
-   *
-   *
-   */
-  function get_bk4u_uid($user)
+    
+    /**
+     *
+     *
+     */
+    function get_bk4u_uid($user)
     {
-
-      $sql = "SELECT bk4u_uid FROM ".MAIN_DB_PREFIX."bookmark4u_login";
-      $sql .= " WHERE fk_user =".$user->id;
-
-      if ($this->db->query($sql)) 
-	{
-	  $num = $this->db->num_rows();
-
-	  if ($num == 0)
-	    {
-	      $this->uid = 0;
-	      return 0;
-	    }
-	  else
-	    {
-	      $row = $this->db->fetch_row(0);
-
-	      $this->uid = $row[0];
-	      return 0;
-	    }
-	
-	  $this->db->free();
-	}
-      else
-	{
-	  return 1;
-	}
+        $sql = "SELECT bk4u_uid FROM ".MAIN_DB_PREFIX."bookmark4u_login";
+        $sql .= " WHERE fk_user =".$user->id;
+    
+        if ($this->db->query($sql))
+        {
+            $num = $this->db->num_rows();
+    
+            if ($num == 0)
+            {
+                $this->uid = 0;
+                return 0;
+            }
+            else
+            {
+                $row = $this->db->fetch_row();
+    
+                $this->uid = $row[0];
+                return 0;
+            }
+    
+            $this->db->free();
+        }
+        else
+        {
+            return 1;
+        }
     }
-  /**
-   *
-   *
-   *
-   */
-
-  function get_bk4u_login()
+    
+    /**
+     *
+     *
+     *
+     */
+    function get_bk4u_login()
     {
-
-      $sql = "SELECT user FROM bookmark4u.bk4u_passwd";
-      $sql .= " WHERE uid =".$this->uid;
-
-      if ($this->db->query($sql)) 
-	{
-	  $num = $this->db->num_rows();
-
-	  if ($num == 0)
-	    {
-	      return 0;
-	    }
-	  else
-	    {
-	      $row = $this->db->fetch_row(0);
-
-	      $this->login = $row[0];
-	      return 0;
-	    }
-	
-	  $this->db->free();
-	}
-      else
-	{
-	  return 1;
-	}
+        $sql = "SELECT user FROM bookmark4u.bk4u_passwd";
+        $sql .= " WHERE uid =".$this->uid;
+    
+        if ($this->db->query($sql))
+        {
+            $num = $this->db->num_rows();
+    
+            if ($num == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                $row = $this->db->fetch_row();
+    
+                $this->login = $row[0];
+                return 0;
+            }
+    
+            $this->db->free();
+        }
+        else
+        {
+            return 1;
+        }
     }
-
-
-
-  /**
-   * \brief     Créé un compte
-   * \param     user Objet du user
-   *
-   */
-  function create_account_from_user($user)
+    
+    
+    
+    /**
+     *    \brief      Créé un compte bookmark4u
+     *    \param      user    Objet du user
+     *    \return     int     <0 si ko, >0 si ok
+     */
+    function create_account_from_user($user)
     {
-      // TODO rendre la base et la table générique
-
-      $sql = "INSERT INTO bookmark4u.bk4u_passwd (user, passwd, name, email, logincnt,rdate)";
-      $sql .= " VALUES ('$user->login',password('$user->pass'),'$user->fullname','$user->email',0,now());";
-      if ($this->db->query($sql))
-	{
-	  if ($this->db->affected_rows()) 
-	    {
-	      $this->uid = $this->db->last_insert_id("bookmark4u.bk4u_passwd");
-	      
-	      $sql = "INSERT INTO ".MAIN_DB_PREFIX."bookmark4u_login";
-	      $sql .= " (fk_user, bk4u_uid)";
-	      $sql .= " VALUES ($user->id, $this->uid)";
-
-	      $this->db->query($sql);
-	      
-	      return 0;
-	    }
-	  else
-	    {
-	      dolibarr_syslog("Bookmark4u::Create_account_from_user INSERT 2");
-	    }
-	}
-      else
-	{
-	  dolibarr_syslog("Bookmark4u::Create_account_from_user INSERT 1");
-	  print $sql;
-	}
-      
+        global $langs;
+        // \todo rendre la base et la table générique
+    
+        $sql = "INSERT INTO bookmark4u.bk4u_passwd (user, passwd, name, email, logincnt, rdate)";
+        $sql .= " VALUES ('$user->login',password('$user->pass'),'$user->fullname','$user->email',0,now());";
+        $resql=$this->db->query($sql);
+        if ($resql)
+        {
+            $this->uid = $this->db->last_insert_id("bookmark4u.bk4u_passwd");
+    
+            $sql = "INSERT INTO ".MAIN_DB_PREFIX."bookmark4u_login";
+            $sql .= " (fk_user, bk4u_uid)";
+            $sql .= " VALUES ($user->id, $this->uid)";
+    
+            $resql2=$this->db->query($sql);
+            if ($resql2)
+            {
+                return 1;
+            }
+            else
+            {
+                dolibarr_syslog("Bookmark4u::Create_account_from_user INSERT 2");
+                $this->error=$langs->trans("UnknownError");
+                return -2;
+            }
+        }
+        else
+        {
+            dolibarr_syslog("Bookmark4u::Create_account_from_user INSERT 1");
+            $this->error=$langs->trans("UnknownError");
+            return -1;
+        }
+    
     }
+
 }
 ?>
