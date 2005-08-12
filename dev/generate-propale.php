8698ad7d39b7ecee1423670060c90ca2d50f566f
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-08-12 21:24:47 +0000

    nouveau fichier

---
+++ dev/generate-propale.php
@@ -0,0 +1,145 @@
+<?PHP
+/* Copyright (C) 2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ *
+ *
+ * ATTENTION DE PAS EXECUTER CE SCRIPT SUR UNE INSTALLATION DE PRODUCTION
+ *
+ * Genere un nombre aleatoire de propale
+ *
+ */
+
+require ("../htdocs/master.inc.php");
+require_once ("../htdocs/facture.class.php");
+require_once ("../htdocs/propal.class.php");
+require_once ("../htdocs/commande/commande.class.php");
+require_once ("../htdocs/societe.class.php");
+
+/*
+ * Parametre
+ */
+
+define (GEN_NUMBER_PROPAL, 2);
+
+/*
+ *
+ *
+ */
+
+$sql = "SELECT min(rowid) FROM ".MAIN_DB_PREFIX."user";
+$resql = $db->query($sql);
+if ($resql) 
+{
+  $row = $db->fetch_row($resql);
+  $user = new User($db, $row[0]);
+}
+
+$socids = array();
+$sql = "SELECT idp FROM ".MAIN_DB_PREFIX."societe WHERE client=1";
+$resql = $db->query($sql);
+if ($resql) 
+{
+  $num_socs = $db->num_rows($resql);
+  $i = 0;
+  while ($i < $num_socs)
+    {
+      $i++;
+      
+      $row = $db->fetch_row($resql);
+      $socids[$i] = $row[0];
+    }
+}
+
+$contids = array();
+$sql = "SELECT idp, fk_soc FROM ".MAIN_DB_PREFIX."socpeople";
+$resql = $db->query($sql);
+if ($resql) 
+{
+  $num_conts = $db->num_rows($resql);
+  $i = 0;
+  while ($i < $num_conts)
+    {
+      $i++;
+      
+      $row = $db->fetch_row($resql);
+      $contids[$row[1]][0] = $row[0]; // A ameliorer
+    }
+}
+
+$prodids = array();
+$sql = "SELECT rowid FROM ".MAIN_DB_PREFIX."product WHERE envente=1";
+$resql = $db->query($sql);
+if ($resql) 
+{
+  $num_prods = $db->num_rows($resql);
+  $i = 0;
+  while ($i < $num_prods)
+    {
+      $i++;
+      
+      $row = $db->fetch_row($resql);
+      $prodids[$i] = $row[0];
+    }
+}
+
+$user->rights->propale->valider=1;
+
+
+if (defined("PROPALE_ADDON") && is_readable(DOL_DOCUMENT_ROOT ."/includes/modules/propale/".PROPALE_ADDON.".php"))
+{
+  require_once(DOL_DOCUMENT_ROOT ."/includes/modules/propale/".PROPALE_ADDON.".php");
+}
+
+$i=0;
+while ($i < GEN_NUMBER_PROPAL)
+{
+  $i++;
+  $socid = rand(1, $num_socs);
+
+  $soc = new Societe($db);
+
+
+
+  $obj = PROPALE_ADDON;
+  $modPropale = new $obj;
+  $numpr = $modPropale->propale_get_num($soc);
+
+  $propal = new Propal($db, $socids[$socid]);
+
+  $propal->ref = $numpr;
+  $propal->contactid = $contids[$socids[$socid]][0];
+  $propal->datep = time();
+  $propal->author = $user->id;
+
+  $nbp = rand(1, 9);
+  $xnbp = 0;
+
+  while ($xnbp < $nbp)
+    {
+      $prodid = rand(1, $num_prods);
+      $propal->insert_product($prodids[$prodid], rand(1,5));
+      $xnbp++;
+    }
+
+  $propal->create($user);
+
+}
+
+
+?>
