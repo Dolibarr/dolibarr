98100f59fbd8228f9c99863911b9bf007ed8e202
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-08-23 19:22:55 +0000

    Ajout Nouveau client

--- htdocs/telephonie/pre.inc.php
+++ htdocs/telephonie/pre.inc.php
@@ -48,6 +48,8 @@ function llxHeader($head = "", $title="") {
 
 
   $menu->add(DOL_URL_ROOT."/telephonie/client/index.php", "Clients");
+  if ($user->rights->telephonie->ligne->creer)
+    $menu->add_submenu(DOL_URL_ROOT."/telephonie/client/new.php", "Nouveau client");
 
   $menu->add(DOL_URL_ROOT."/telephonie/contrat/", "Contrats");
 
