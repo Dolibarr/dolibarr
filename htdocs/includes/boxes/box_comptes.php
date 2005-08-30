3fed5b791de20d07fdf29f1c2a2ab746a81af98a
Laurent Destailleur <eldy@users.sourceforge.net>
2005-08-30 21:19:05 +0000

    Trad: Traduction boites

--- htdocs/includes/boxes/box_comptes.php
+++ htdocs/includes/boxes/box_comptes.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2005 Christophe
+ * Copyright (C) 2005 Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +18,6 @@
  *
  * $Id$
  * $Source$
- *
  */
 
 /**
