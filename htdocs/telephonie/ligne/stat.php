90bf6b819a7abd7bf9b17c12287d1edac8de67a9
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-09-06 15:26:49 +0000

    Modif permissions

--- htdocs/telephonie/ligne/stat.php
+++ htdocs/telephonie/ligne/stat.php
@@ -136,7 +136,7 @@ if ($_GET["id"] or $_GET["numero"])
 	  print '</td><td width="50%" valign="top" align="center">';
 
 	  $file = $img_root.$ligne->id."/graphgain.png";
-	  if (file_exists($file)) 
+	  if (file_exists($file) && $user->rights->telephonie->ligne->gain)
 	    {
 	      print '<img src="'.DOL_URL_ROOT.'/telephonie/showgraph.php?graph='.$file.'" alt="CA Mensuel">';
 	    }
