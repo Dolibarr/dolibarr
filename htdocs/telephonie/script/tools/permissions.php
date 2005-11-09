760f7f92722de7a55dbec73fd937443309760416
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2005-11-09 09:09:03 +0000

    Ajout utilisateur

--- htdocs/telephonie/script/tools/permissions.php
+++ htdocs/telephonie/script/tools/permissions.php
@@ -70,6 +70,12 @@ if ( $resql )
       $sqlu .= " (fk_soc, fk_user, pread, pwrite, pperms) ";
       $sqlu .= " VALUES (".$row[0].",33,1,0,0)";
       $resqlu = $db->query($sqlu);
+
+      $sqlu = "REPLACE INTO llx_societe_perms";
+      $sqlu .= " (fk_soc, fk_user, pread, pwrite, pperms) ";
+      $sqlu .= " VALUES (".$row[0].",34,1,0,0)";
+      $resqlu = $db->query($sqlu);
+
     }
   $db->free($resql);
 }
