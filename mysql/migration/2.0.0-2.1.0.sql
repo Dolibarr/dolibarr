84bc54cfcad278bee2dd51720ca8c3dba28a36a8
Regis Houssin <regis@dolibarr.fr>
2006-02-08 18:08:26 +0000

    Ajout table pour utiliser les modeles commande pdf

--- mysql/migration/2.0.0-2.1.0.sql
+++ mysql/migration/2.0.0-2.1.0.sql
@@ -4,3 +4,11 @@
 -- ce fichier doit être chargé sur une version 2.0.0 
 -- sans AUCUNE erreur ni warning
 -- 
+
+
+create table llx_commande_model_pdf
+(
+  nom         varchar(50) PRIMARY KEY,
+  libelle     varchar(255),
+  description text
+)type=innodb;
