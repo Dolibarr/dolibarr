f76f3558740b104124175a9a113699a1145e2c03
Regis Houssin <regis@dolibarr.fr>
2006-02-12 14:37:46 +0000

    modification de la description produit

--- htdocs/propal.class.php
+++ htdocs/propal.class.php
@@ -173,16 +173,16 @@ class Propal
                         $price = $prod->price - $remise;
                     }
                     
-                    if ($conf->global->CHANGE_PROD_DESC)
+                  /*  if ($conf->global->CHANGE_PROD_DESC)
 			              {
 			              	$sql = "INSERT INTO ".MAIN_DB_PREFIX."propaldet (fk_propal, fk_product, qty, price, tva_tx, description, remise_percent, subprice) VALUES ";
                       $sql .= " (".$this->id.",". $idproduct.",'". $qty."','". $price."','".$txtva."','".addslashes($p_product_desc)."','".price2num($remise_percent)."','".price2num($subprice)."')";
                     }
                     else
-                    {
+                    {*/
                     	$sql = "INSERT INTO ".MAIN_DB_PREFIX."propaldet (fk_propal, fk_product, qty, price, tva_tx, description, remise_percent, subprice) VALUES ";
                       $sql .= " (".$this->id.",". $idproduct.",'". $qty."','". $price."','".$txtva."','".addslashes($p_desc?$p_desc:$prod->label)."','".price2num($remise_percent)."','".price2num($subprice)."')";
-                    }
+                   // }
         
                     if ($this->db->query($sql) )
                     {
