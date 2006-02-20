067dc95fae7851d26a51ae31b19b974282becdca
Regis Houssin <regis@dolibarr.fr>
2006-02-20 19:32:17 +0000

    bugfix

--- htdocs/categories/categorie.class.php
+++ htdocs/categories/categorie.class.php
@@ -462,7 +462,7 @@ class Categorie
     $sql = "SELECT count(c.rowid)";
     $sql.= " FROM ".MAIN_DB_PREFIX."categorie as c, ".MAIN_DB_PREFIX."categorie_association as ca";
     $sql.= " WHERE c.label = '".$label."'";
-    if ($catmere)
+    if ($catmere!='' && $catmere > 0)
     {
     $sql.= " AND c.rowid = ca.fk_categorie_fille";
     $sql.= " AND ca.fk_categorie_mere = '".$catmere."'";
