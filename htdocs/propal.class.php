9b019be58c1971095e8c302248a3b476c5777c73
Regis Houssin <regis@dolibarr.fr>
2006-02-28 17:52:21 +0000

    bugfix

--- htdocs/propal.class.php
+++ htdocs/propal.class.php
@@ -557,7 +557,7 @@ class Propal
                 {
                    $sql = "SELECT cr.rowid, cr.libelle as cond_libelle, cr.code as cond_code, cp.rowid, cp.libelle as mode_libelle, cp.code as mode_code";
                    $sql.= " FROM ".MAIN_DB_PREFIX."cond_reglement as cr, ".MAIN_DB_PREFIX."c_paiement as cp";
-                   $sql.= " WHERE cr.rowid = ".this->cond_reglement_id." AND cp.rowid = ".$this->mode_reglement_id;
+                   $sql.= " WHERE cr.rowid = ".$this->cond_reglement_id." AND cp.rowid = ".$this->mode_reglement_id;
                    
                    $resqlcond = $this->db->query($sql);
                    
