820ee12861700a9ea230447e5a1f7ed2a7d43ca9
Regis Houssin <regis@dolibarr.fr>
2006-04-24 12:33:54 +0000

    ajout lien sur le label de l'adresse de livraison

--- htdocs/html.form.class.php
+++ htdocs/html.form.class.php
@@ -1614,7 +1614,7 @@ class Form
             	require_once(DOL_DOCUMENT_ROOT ."/comm/adresse_livraison.class.php");
             	$livraison=new Livraison($this->db);
 				      $livraison->fetch_adresse($selected);
-				      print $livraison->label;
+				      print '<a href=/comm/adresse_livraison.php?socid='.$socid.'>'.$livraison->label.'</a>';
             }
             else
             {
