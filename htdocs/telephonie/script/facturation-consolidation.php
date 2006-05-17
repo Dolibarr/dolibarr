0cfb05cea381ce993c47d20285b48150813694d2
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2006-05-17 12:42:01 +0000

    Bugfix sur le champ distributeur

--- htdocs/telephonie/script/facturation-consolidation.php
+++ htdocs/telephonie/script/facturation-consolidation.php
@@ -109,7 +109,7 @@ if ( $resql )
 	  while ($rows = $db->fetch_row($resqls))
 	    {
 	      $sqlu = "UPDATE ".MAIN_DB_PREFIX."telephonie_facture_consol";
-	      $sqlu .= " SET distri='".$paye[$rows[0]]."'";
+	      $sqlu .= " SET distri='".$rows[0]."'";
 	      $sqlu .= " WHERE ligne = '$row[2]'";
 	      if (!  $resqlu = $db->query($sqlu))
 		{
