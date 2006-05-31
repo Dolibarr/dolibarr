d4c2146c535e93d703191440143f5464269531eb
Regis Houssin <regis@dolibarr.fr>
2006-05-31 17:34:52 +0000

    ajout lien vers bon de livraison et image alerte sur le stock

--- htdocs/expedition/commande.php
+++ htdocs/expedition/commande.php
@@ -456,7 +456,7 @@ if ($_GET["id"] > 0)
             {
                 if ($value > 0)
                 {
-                    $sql = "SELECT e.label as entrepot, ps.reel, p.label, p.ref";
+                    $sql = "SELECT e.label as entrepot, ps.reel, p.label, p.ref, p.rowid as product_id";
                     $sql .= " FROM ".MAIN_DB_PREFIX."entrepot as e, ".MAIN_DB_PREFIX."product_stock as ps, ".MAIN_DB_PREFIX."product as p";
                     $sql .= " WHERE e.rowid = ps.fk_entrepot AND ps.fk_product = p.rowid AND ps.fk_product = $key";
                     $sql .= " AND e.statut = 1 AND reel < $value";
@@ -472,10 +472,10 @@ if ($_GET["id"] > 0)
                         {
                             $obja = $db->fetch_object($resql);
                             print "<tr $bc[$var]>";
-                            print '<td width="54%">';
+                            print '<td width="30%">';
                             print img_warning($langs->trans("Alert"));
-                            print $obja->ref.'</td>';
-                            print '<td>'.$obja->label.'</td><td>'.$obja->entrepot.'</td><td><b>Stock : '.$obja->reel.'</b></td>';
+                            print '<a href="'.DOL_URL_ROOT.'/product/fiche.php?id='.$objp->product_id.'">'.img_object($langs->trans("ShowProduct"),"product").' '.$obja->ref.'</a> - '.$obja->label;
+                            print '<td>'.$obja->entrepot.'</td><td><b>Stock : '.$obja->reel.'</b>'.img_warning($langs->trans("Alert")).'</td>';
                             print "</tr>\n";
                             $i++;
                         }
