5bb0e0ffbe573257b4596fbe17e770a9a56e524a
Laurent Destailleur <eldy@users.sourceforge.net>
2006-06-03 22:15:02 +0000

    Qual: Factorisation du code de gestion des onglets pour les propales

---
+++ htdocs/lib/propal.lib.php
@@ -0,0 +1,76 @@
+<?php
+/* Copyright (C) 2006 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * or see http://www.gnu.org/
+ *
+ * $Id$
+ * $Source$
+ */
+
+/**
+	    \file       htdocs/lib/propal.lib.php
+		\brief      Ensemble de fonctions de base pour le module propal
+        \ingroup    propal
+        \version    $Revision$
+
+		Ensemble de fonctions de base de dolibarr sous forme d'include
+*/
+
+function propal_prepare_head($propal)
+{
+	global $langs, $conf, $user;
+	$langs->load("propal");
+
+	$h = 0;
+	$head = array();
+
+	$head[$h][0] = DOL_URL_ROOT.'/comm/propal.php?propalid='.$propal->id;
+	$head[$h][1] = $langs->trans('CommercialCard');
+	$head[$h][2] = 'comm';
+	$h++;
+
+	$head[$h][0] = DOL_URL_ROOT.'/compta/propal.php?propalid='.$propal->id;
+	$head[$h][1] = $langs->trans('AccountancyCard');
+	$head[$h][2] = 'compta';
+	$h++;
+
+	if ($conf->use_preview_tabs)
+	{
+		$head[$h][0] = DOL_URL_ROOT.'/comm/propal/apercu.php?propalid='.$propal->id;
+		$head[$h][1] = $langs->trans("Preview");
+		$head[$h][2] = 'preview';
+		$h++;
+	}
+
+	$head[$h][0] = DOL_URL_ROOT.'/comm/propal/note.php?propalid='.$propal->id;
+	$head[$h][1] = $langs->trans('Note');
+	$head[$h][2] = 'note';
+	$h++;
+
+	$head[$h][0] = DOL_URL_ROOT.'/comm/propal/document.php?propalid='.$propal->id;
+	$head[$h][1] = $langs->trans('Documents');
+	$head[$h][2] = 'document';
+	$h++;
+
+	$head[$h][0] = DOL_URL_ROOT.'/comm/propal/info.php?propalid='.$propal->id;
+	$head[$h][1] = $langs->trans('Info');
+	$head[$h][2] = 'info';
+	$h++;
+
+	return $head;
+}
+
+?>
