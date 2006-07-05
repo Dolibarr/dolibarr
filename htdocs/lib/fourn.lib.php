eae939e6102551dcf65d685a59237627eb550f4b
marc_ocebo <marc_ocebo>
2006-07-05 10:52:49 +0000

    enh: factorisation de code

---
+++ htdocs/lib/fourn.lib.php
@@ -0,0 +1,56 @@
+<?php
+/* Copyright (C) 2005 Laurent Destailleur   <eldy@users.sourceforge.net>
+ * Copyright (C) 2006 Marc Barilley / Ocebo <marc@ocebo.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * or see http://www.gnu.org/
+ *
+ * $Id$
+ * $Source$
+ */
+
+/**
+	    \file       htdocs/lib/fourn.lib.php
+		\brief      Ensemble de fonctions de base pour le module fournisseur
+		\version    $Revision$
+
+		Ensemble de fonctions de base de dolibarr sous forme d'include
+*/
+
+function facturefourn_prepare_head($fac)
+{
+	global $langs, $conf;
+	$h = 0;
+	$head = array();
+	
+	$head[$h][0] = DOL_URL_ROOT.'/fourn/facture/fiche.php?facid='.$fac->id;
+	$head[$h][1] = $langs->trans('Card');
+	$head[$h][2] = 'card';
+	$h++;
+
+	$head[$h][0] = DOL_URL_ROOT.'/fourn/facture/document.php?facid='.$fac->id;
+	$head[$h][1] = $langs->trans('Documents');
+	$head[$h][2] = 'documents';
+	$h++;
+
+	$head[$h][0] = DOL_URL_ROOT.'/fourn/facture/info.php?facid='.$fac->id;
+	$head[$h][1] = $langs->trans('Info');
+	$head[$h][2] = 'info';
+	$h++;
+
+	return $head;
+}
+
+?>
