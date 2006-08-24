d4cf07810909b9e9e26d3813a3de78078eecccd2
Laurent Destailleur <eldy@users.sourceforge.net>
2006-08-24 18:29:35 +0000

    Correction type non raprochable sur compte caisse pour corriger vieux bug au cas ou

--- mysql/migration/2.0.0-2.1.0.sql
+++ mysql/migration/2.0.0-2.1.0.sql
@@ -306,6 +306,7 @@ alter table llx_bank_account add column min_allowed integer DEFAULT 0;
 alter table llx_bank_account add column min_desired integer DEFAULT 0;
 alter table llx_bank_account add column comment varchar(254);
 update llx_bank_account set currency_code='EU';
+update llx_bank_account set rappro=0 where courant=2;
 
 alter table llx_accountingsystem_det rename to llx_accountingaccount;
 
