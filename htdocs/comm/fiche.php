29a0f71859a088181a1408caa4f3937c9656af73
Laurent Destailleur <eldy@users.sourceforge.net>
2006-09-16 01:25:41 +0000

    Fix: Warning sur retard propal alors que tou est ok

--- htdocs/comm/fiche.php
+++ htdocs/comm/fiche.php
@@ -392,7 +392,8 @@ if ($socid > 0)
 
         print '<table class="noborder" width="100%">';
 
-        $sql = "SELECT s.nom, s.idp, p.rowid as propalid, p.fk_statut, p.price, p.ref, p.remise, ".$db->pdate("p.datep")." as dp, c.label as statut, c.id as statutid";
+	    $sql = "SELECT s.nom, s.idp, p.rowid as propalid, p.fk_statut, p.price, p.ref, p.remise, ";
+	    $sql.= " ".$db->pdate("p.datep")." as dp, ".$db->pdate("p.fin_validite")." as datelimite,";
         $sql .= " FROM ".MAIN_DB_PREFIX."societe as s, ".MAIN_DB_PREFIX."propal as p, ".MAIN_DB_PREFIX."c_propalst as c";
         $sql .= " WHERE p.fk_soc = s.idp AND p.fk_statut = c.id";
         $sql .= " AND s.idp = ".$objsoc->id;
