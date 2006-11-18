09d36f5d79e06e1a20d61b89bf668b9920e73d2a
Laurent Destailleur <eldy@users.sourceforge.net>
2006-11-18 00:41:00 +0000

    Ajout index sur date echeance

--- mysql/tables/llx_facture_fourn.key.sql
+++ mysql/tables/llx_facture_fourn.key.sql
@@ -23,6 +23,8 @@
 
 ALTER TABLE llx_facture_fourn ADD UNIQUE INDEX uk_facture_fourn_ref (facnumber, fk_soc);
 
+ALTER TABLE llx_facture_fourn ADD INDEX idx_facture_fourn_date_lim_reglement (date_lim_reglement);
+
 ALTER TABLE llx_facture_fourn ADD INDEX idx_facture_fourn_fk_soc (fk_soc);
 ALTER TABLE llx_facture_fourn ADD INDEX idx_facture_fourn_fk_user_author (fk_user_author);
 ALTER TABLE llx_facture_fourn ADD INDEX idx_facture_fourn_fk_user_valid (fk_user_valid);
