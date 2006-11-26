00bf1e33118ff34838747ef4b15143e8e45c6e97
Laurent Destailleur <eldy@users.sourceforge.net>
2006-11-26 19:30:16 +0000

    New: Ajout onglet Suivi sur fiche adherent

---
+++ htdocs/adherents/info.php
@@ -0,0 +1,78 @@
+<?php
+/* Copyright (C) 2005-2006 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2006 Regis Houssin        <regis.houssin@cap-networks.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ */
+
+/**
+        \file       htdocs/adherents/info.php
+        \ingroup    member
+		\brief      Page des informations d'un adherent
+		\version    $Revision$
+*/
+
+require("./pre.inc.php");
+require_once(DOL_DOCUMENT_ROOT."/adherents/adherent.class.php");
+require_once(DOL_DOCUMENT_ROOT."/lib/member.lib.php");
+
+$langs->load("companies");
+$langs->load("bills");
+$langs->load("members");
+$langs->load("users");
+
+$user->getrights('adherent');
+if (!$user->rights->adherent->lire)
+	accessforbidden();
+
+
+/*
+ * Visualisation de la fiche
+ *
+ */
+
+llxHeader();
+
+$adh = new Adherent($db);
+$adh->id=$_GET["id"];
+$adh->fetch($_GET["id"]);
+$adh->info($_GET["id"]);
+
+/*
+ * Affichage onglets
+ */
+$head = member_prepare_head($adh);
+
+dolibarr_fiche_head($head, 'info', $langs->trans("Member").": ".$adh->fullname);
+
+
+print '<table width="100%"><tr><td>';
+dolibarr_print_object_info($adh);
+print '</td></tr></table>';
+
+print '</div>';
+
+// Juste pour éviter bug IE qui réorganise mal div précédents si celui-ci absent
+print '<div class="tabsAction">';
+print '</div>';
+
+
+$db->close();
+
+llxFooter('$Date$ - $Revision$');
+?>
