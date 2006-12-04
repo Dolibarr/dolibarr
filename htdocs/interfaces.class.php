cf8adb4a4364281f7cafca2321ec84ec55bbe06e
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2006-12-04 11:35:55 +0000

    Moficiation de la lecture du repertoire pour exclure les fichiers archives *~

--- htdocs/interfaces.class.php
+++ htdocs/interfaces.class.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2005 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2006 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,75 +22,75 @@
  */
 
 /**
-        \file       htdocs/interfaces.class.php
-        \ingroup    core
-        \brief      Fichier de la classe de gestion des triggers
+   \file       htdocs/interfaces.class.php
+   \ingroup    core
+   \brief      Fichier de la classe de gestion des triggers
 */
 
 
 /**
-        \class      Interfaces
-        \brief      Classe de la gestion des triggers
+   \class      Interfaces
+   \brief      Classe de la gestion des triggers
 */
 
 class Interfaces
 {
-    var $dir;
+  //! Repertoire contenant les definitions des triggers
+  var $dir;
 
-    /**
-     *   \brief      Constructeur.
-     *   \param      DB      handler d'accès base
-     */
-    function Interfaces($DB)
-    {
-        $this->db = $DB ;
-        $this->dir = DOL_DOCUMENT_ROOT . "/includes/triggers";
-    }
+  /**
+   *   \brief      Constructeur.
+   *   \param      DB      handler d'accès base
+   */
+  function Interfaces($DB)
+  {
+    $this->db = $DB ;
+    $this->dir = DOL_DOCUMENT_ROOT . "/includes/triggers";
+  }
+  
+  /**
+   *   \brief      Fonction appelée lors du déclenchement d'un évènement Dolibarr.
+   *               Cette fonction déclenche tous les triggers trouvés
+   *   \param      action      Code de l'evenement
+   *   \param      object      Objet concern
+   *   \param      user        Objet user
+   *   \param      lang        Objet lang
+   *   \param      conf        Objet conf
+   *   \return     int         Nbre de triggers déclenchés si pas d'erreurs. Nb en erreur sinon.
+   */
+  function run_triggers($action,$object,$user,$lang,$conf)
+  {
     
-    /**
-     *   \brief      Fonction appelée lors du déclenchement d'un évènement Dolibarr.
-     *               Cette fonction déclenche tous les triggers trouvés
-     *   \param      action      Code de l'evenement
-     *   \param      object      Objet concern
-     *   \param      user        Objet user
-     *   \param      lang        Objet lang
-     *   \param      conf        Objet conf
-     *   \return     int         Nbre de triggers déclenchés si pas d'erreurs. Nb en erreur sinon.
-     */
-    function run_triggers($action,$object,$user,$lang,$conf)
-    {
+    $handle=opendir($this->dir);
+    $modules = array();
+    $nbok = $nbko = 0;
     
-        $handle=opendir($this->dir);
-        $modules = array();
-        $nbok = $nbko = 0;
-    
-        while (($file = readdir($handle))!==false)
-        {
-            if (is_readable($this->dir."/".$file) && eregi('interface_(.*).class.php',$file,$reg))
-            {
-                $modName = "Interface".ucfirst($reg[1]);
-                //print "file=$file"; print "modName=$modName"; exit;
-                if ($modName)
-                {
-                    include_once($this->dir."/".$file);
-                    $objMod = new $modName($this->db);
-                    if ($objMod)
-                    {
-                        if ($objMod->run_trigger($action,$object,$user,$lang,$conf) > 0)
-                        {
-                            $nbok++;
-                        }
-                        else
-                        {
-                            $nbko++;
-                        }
-                    }
-                }
-            }
-        }
-        if ($nbko) return $nbko;
-        return $nbok;
-    }
-
+    while (($file = readdir($handle))!==false)
+      {
+	if (is_readable($this->dir."/".$file) && eregi('interface_(.*).class.php$',$file,$reg))
+	  {
+	    $modName = "Interface".ucfirst($reg[1]);
+	    //print "file=$file"; print "modName=$modName"; exit;
+	    if ($modName)
+	      {
+		include_once($this->dir."/".$file);
+		$objMod = new $modName($this->db);
+		if ($objMod)
+		  {
+		    if ($objMod->run_trigger($action,$object,$user,$lang,$conf) > 0)
+		      {
+			$nbok++;
+		      }
+		    else
+		      {
+			$nbko++;
+		      }
+		  }
+	      }
+	  }
+      }
+    if ($nbko) return $nbko;
+    return $nbok;
+  }  
 }
 ?>
