3d9f62bd77e71a691fb7e9624551b1ccb99183c6
Laurent Destailleur <eldy@users.sourceforge.net>
2006-12-05 00:48:05 +0000

    New: Ajout module Mantis

---
+++ htdocs/includes/modules/modMantis.class.php
@@ -0,0 +1,127 @@
+<?php
+/* Copyright (C) 2003      Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2006 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ */
+
+/**     \defgroup   mantis     Module Mantis
+        \brief      Module to include Mantis into Dolibarr
+*/
+
+/**
+        \file       htdocs/includes/modules/modMantis.class.php
+        \ingroup    mantis
+        \brief      Description and activation file for module Mantis
+*/
+
+include_once "DolibarrModules.class.php";
+
+/**     \class      modMantis
+        \brief      Description and activation class for module Mantis
+*/
+
+class modMantis extends DolibarrModules
+{
+
+   /**
+    *   \brief      Constructor. Define names, constants, directories, boxes, permissions
+    *   \param      DB      Database handler
+    */
+	function modMantis($DB)
+	{
+		$this->db = $DB;
+		
+		// Id of module (must be unique for all modules)
+		// Use same value here than in file modXxx.class.php
+		$this->id = 'mantis';   	
+		// Another id for module (must be unique).
+		// Use here a free id.
+		$this->numero = 1200;
+		
+		// Family can be 'crm','financial','hr','projects','product','technic','other'
+		// It is used to sort modules in module setup page 
+		$this->family = "projects";		
+		// Module title used if translation string 'ModuleXXXName' not found (XXX is id value)
+		$this->name = "Mantis";	
+		// Module descriptoin used translation string 'ModuleXXXDesc' not found (XXX is id value)
+		$this->description = "Interfaçage avec le bug tracking Mantis";
+		// Possible values for version are: 'experimental' or 'dolibarr' or version
+		$this->version = 'dolibarr';    
+		// Id used in llx_const table to manage module status (enabled/disabled)	
+		$this->const_name = 'MAIN_MODULE_MANTIS';
+		// Where to store the module in setup page (0=common,1=interface,2=other)
+		$this->special = 1;
+		// Name of png file (without png) used for this module
+		$this->picto='calendar';
+		
+		// Data directories to create when module is enabled
+		$this->dirs = array();
+		
+		// Config pages
+		$this->config_page_url = array("mantis.php");
+		
+		// Dependencies
+		$this->depends = array();		// List of modules id that must be enabled
+		$this->requiredby = array();	// List of modules id to disable if this one is disabled
+		
+		// Constants
+		$this->const = array();			// List of parameters
+		
+		// Boxes
+		$this->boxes = array();			// List of boxes 
+		
+		// Permissions
+		$this->rights_class = 'mantis';	// Permission key
+		$this->rights = array();		// Permission array used by this module
+		// Example
+		// $r++;
+		// $this->rights[$r][0] = 1200; 				// Permission id (must not be already used)
+		// $this->rights[$r][1] = 'Permision label';	// Permission label
+		// $this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
+		// $this->rights[$r][4] = 'level1';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
+		// $this->rights[$r][5] = 'level2';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
+	}
+
+	/**
+     *		\brief      Function called when module is enabled.
+     *					Add constants, boxes and permissions into Dolibarr database.
+     *					It also creates data directories.
+     */
+	function init()
+  	{
+    	$sql = array();
+    
+    	return $this->_init($sql);
+  	}
+
+	/**
+	 *		\brief		Function called when module is disabled.
+ 	 *              	Remove from database constants, boxes and permissions from Dolibarr database.
+ 	 *					Data directories are not deleted.
+ 	 */
+	function remove()
+	{
+    	$sql = array();
+
+    	return $this->_remove($sql);
+  	}
+
+}
+
+?>
---
+++ htdocs/lib/mantis.class.php
@@ -0,0 +1,73 @@
+<?php
+/* Copyright (C) 2002-2003 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2005 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Id$
+ * $Source$
+ */
+
+/**
+        \file       htdocs/lib/mantis.class.php
+        \ingroup    mantis
+		\brief      Ensemble des fonctions permettant d'acceder a la database mantis.
+		\author     Laurent Destailleur.
+		\version    $Revision$
+*/
+
+
+/**
+        \class      Mantis
+		\brief      Classe permettant d'acceder a la database mantis
+*/
+
+class Mantis {
+    
+    var $localdb;
+
+    var $date;
+    var $duree = 0;     // Secondes
+    var $texte;
+    var $desc;
+    
+    var $error;
+
+  
+    /**
+    		\brief      Constructeur de la classe d'interface à mantisendar
+    */
+    function Mantis()
+    {
+        global $conf;
+        global $dolibarr_main_db_type,$dolibarr_main_db_host,$dolibarr_main_db_user;
+        global $dolibarr_main_db_pass,$dolibarr_main_db_name;
+
+        // Défini parametres mantis (avec substitution eventuelle)
+        $mantistype=eregi_replace('__dolibarr_main_db_type__',$dolibarr_main_db_type,$conf->mantis->db->type);
+        $mantishost=eregi_replace('__dolibarr_main_db_host__',$dolibarr_main_db_host,$conf->mantis->db->host);
+        $mantisuser=eregi_replace('__dolibarr_main_db_user__',$dolibarr_main_db_user,$conf->mantis->db->user);
+        $mantispass=eregi_replace('__dolibarr_main_db_pass__',$dolibarr_main_db_pass,$conf->mantis->db->pass);
+        $mantisname=eregi_replace('__dolibarr_main_db_name__',$dolibarr_main_db_name,$conf->mantis->db->name);
+
+        // On initie la connexion à la base mantisendar
+        require_once (DOL_DOCUMENT_ROOT ."/lib/databases/".$mantistype.".lib.php");
+        $this->localdb = new DoliDb($mantistype,$mantishost,$mantisuser,$mantispass,$mantisname);
+    }
+
+
+   
+}
+?>
