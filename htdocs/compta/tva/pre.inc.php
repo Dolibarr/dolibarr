1d5622d98e249b8b05eb21c0a363330812d5a7ae
ywarnier <ywarnier>
2006-12-05 11:40:45 +0000

    Ajout de lien vers la nouvelle page quadri

--- htdocs/compta/tva/pre.inc.php
+++ htdocs/compta/tva/pre.inc.php
@@ -39,6 +39,7 @@ function llxHeader($head = "") {
   $menu->add_submenu("reglement.php",$langs->trans("Payments"));
   $menu->add_submenu("fiche.php?action=create",$langs->trans("NewPayment"));
   $menu->add_submenu("clients.php",$langs->trans("Clients"));
+  $menu->add_submenu("quadri.php",$langs->trans("Quadri"));
 
   left_menu($menu->liste);
 }
