7b0bf13c5cb10cd12763d0a63d7ae4ff7193a93f
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2006-12-12 17:53:49 +0000

    Ajoute COLUMN

--- mysql/migration/2.0.0-2.1.0.sql
+++ mysql/migration/2.0.0-2.1.0.sql
@@ -59,11 +59,11 @@ ALTER TABLE llx_societe drop column id;
 ALTER TABLE llx_societe modify parent             integer;
 UPDATE llx_societe set parent = null where parent = 0;
 
-alter table llx_product add stock_loc varchar(10) DEFAULT NULL;
-alter table llx_product add gencode varchar(255) DEFAULT NULL;
-alter table llx_product add weight float DEFAULT NULL;
-alter table llx_product add weight_units tinyint DEFAULT NULL;
-alter table llx_product add canvas varchar(15) DEFAULT '';
+ALTER TABLE llx_product ADD COLUMN stock_loc VARCHAR(10) DEFAULT NULL;
+ALTER TABLE llx_product ADD COLUMN gencode VARCHAR(255) DEFAULT NULL;
+ALTER TABLE llx_product ADD COLUMN weight float DEFAULT NULL;
+ALTER TABLE llx_product ADD COLUMN weight_units tinyint DEFAULT NULL;
+ALTER TABLE llx_product ADD COLUMN canvas varchar(15) DEFAULT '';
 
 ALTER TABLE llx_stock_mouvement ADD COLUMN price FLOAT(13,4) DEFAULT 0;
 
