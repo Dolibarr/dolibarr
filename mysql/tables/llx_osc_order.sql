22aacbef4e29977f02e0922da6c1a5e802861736
Laurent Destailleur <eldy@users.sourceforge.net>
2006-12-12 20:10:45 +0000

    Ajout patch oscommerce WS

---
+++ mysql/tables/llx_osc_order.sql
@@ -0,0 +1,26 @@
+-- phpMyAdmin SQL Dump
+-- version 2.6.2-Debian-3sarge1
+-- http://www.phpmyadmin.net
+-- 
+-- Serveur: localhost
+-- Généré le : Samedi 05 Août 2006 à 17:25
+-- Version du serveur: 4.0.24
+-- Version de PHP: 4.3.10-16
+-- 
+-- Base de données: `dolidev`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Structure de la table `llx_osc_order`
+-- 
+
+DROP TABLE IF EXISTS `llx_osc_order`;
+CREATE TABLE IF NOT EXISTS `llx_osc_order` (
+  `osc_orderid` int(11) NOT NULL default '0',
+  `osc_lastmodif` datetime default NULL,
+  `doli_orderidp` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`osc_orderid`),
+  UNIQUE KEY `doli_orderidp` (`doli_orderidp`)
+) TYPE=InnoDB COMMENT='Table transition commande OSC - commande Dolibarr';
---
+++ mysql/tables/llx_osc_product.sql
@@ -0,0 +1,28 @@
+-- phpMyAdmin SQL Dump
+-- version 2.6.2-Debian-3sarge1
+-- http://www.phpmyadmin.net
+-- 
+-- Serveur: localhost
+-- Généré le : Samedi 05 Août 2006 à 17:25
+-- Version du serveur: 4.0.24
+-- Version de PHP: 4.3.10-16
+-- 
+-- Base de données: `dolidev`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Structure de la table `llx_osc_product`
+-- 
+
+DROP TABLE IF EXISTS `llx_osc_product`;
+CREATE TABLE IF NOT EXISTS `llx_osc_product` (
+  `osc_prodid` int(11) NOT NULL default '0',
+  `osc_lastmodif` datetime default NULL,
+  `doli_prodidp` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`osc_prodid`),
+  UNIQUE KEY `doli_prodidp` (`doli_prodidp`)
+) TYPE=InnoDB COMMENT='Table transition produit OSC - produit Dolibarr';
+
+
