7f9f64902570c1b5101797b972a4614f33d5e415
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2006-12-13 14:18:49 +0000

    Modif structure

--- mysql/tables/llx_entrepot_valorisation.sql
+++ mysql/tables/llx_entrepot_valorisation.sql
@@ -22,10 +22,11 @@
 
 create table llx_entrepot_valorisation
 (
-  date_calcul     date,          -- date auquel a ete calcule la valeur
+  rowid           integer AUTO_INCREMENT PRIMARY KEY,
+  tms             timestamp,      -- date technique mise à jour automatiquement
+  date_calcul     date,           -- date auquel a ete calcule la valeur
   fk_entrepot     integer UNSIGNED NOT NULL ,
-  value           integer,
-
-  UNIQUE (date_calcul, fk_entrepot)
+  valo_pmp        float(12,4),    -- valoristaion du stock en PMP
+  key(fk_entrepot)
 )type=innodb;
 
