77b337331f88c13dd5acbc843b1535c19de16552
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2006-12-18 14:11:30 +0000

    Ajout menu remise de cheque

--- htdocs/compta/paiement/pre.inc.php
+++ htdocs/compta/paiement/pre.inc.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2003-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org> 
+/* Copyright (C) 2003-2006 Rodolphe Quiedeville <rodolphe@quiedeville.org> 
  * Copyright (C) 2005 Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -35,16 +35,18 @@ function llxHeader($head = "", $title="") {
 
   $menu = new Menu();
 
-  $menu->add("liste.php",$langs->trans("List"));
+  $menu->add("liste.php",$langs->trans("Payments"));
+  $menu->add_submenu("liste.php",$langs->trans("List"));
+
+  $menu->add(DOL_URL_ROOT."/compta/paiement/cheque/index.php",$langs->trans("CheckReceipt"));
 
   $menu->add("rapport.php",$langs->trans("Reportings"));
 
   $menu->add(DOL_URL_ROOT."/compta/facture.php",$langs->trans("Bills"));
   $menu->add_submenu(DOL_URL_ROOT."/compta/facture/impayees.php",$langs->trans("Unpayed"));
-  $menu->add_submenu("liste.php",$langs->trans("Payments"));
+
   $menu->add_submenu("avalider.php",$langs->trans("MenuToValid"));
 
   left_menu($menu->liste);
 }
-
 ?>
