7ca14661d8d46024bba29535be248818706b759a
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2006-12-20 20:55:24 +0000

    Nouveau fichier

---
+++ mysql/tables/llx_bordereau_cheque.sql
@@ -0,0 +1,36 @@
+-- ===================================================================
+-- Copyright (C) 2006 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+--
+-- $Id$
+-- $Source$
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- ===================================================================
+--
+-- Bordereaux de remise de cheque
+--
+create table llx_bordereau_cheque
+(
+  rowid             integer AUTO_INCREMENT PRIMARY KEY,
+  datec             datetime,
+  date_bordereau    date,
+  number            varchar(12),
+  amount            float(12,4),
+  fk_bank_account   integer,
+  fk_user_author    integer,
+  note              text,
+  statut            tinyint(1) UNSIGNED DEFAULT 0
+)type=innodb;
