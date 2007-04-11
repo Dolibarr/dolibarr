e2b314384d18a84c62cc127b3154046ff4fe8b4a
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2007-04-11 11:53:11 +0000

    Nouveau fichier

---
+++ htdocs/telephonie/sql/data/data.sql
@@ -0,0 +1,4 @@
+insert into llx_telephonie_concurrents (nom,active) values ('Inconnu',0);
+insert into llx_telephonie_concurrents (nom) values ('Cegetel');
+insert into llx_telephonie_concurrents (nom) values ('France Telecom');
+insert into llx_telephonie_concurrents (nom) values ('Neuf Telecom');
