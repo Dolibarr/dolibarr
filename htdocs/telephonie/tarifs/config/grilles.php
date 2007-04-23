e56cd7d070a3c450de5378a1ed6c549fb8090bb5
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2007-04-23 12:36:31 +0000

    Gestion de la suppression d'une grille

--- htdocs/telephonie/tarifs/config/grilles.php
+++ htdocs/telephonie/tarifs/config/grilles.php
@@ -30,9 +30,18 @@ if ($_POST["action"] == 'add' && $user->rights->telephonie->tarif->permission)
   $obgrille->CreateGrille($user, $_POST["nom"], $_POST["type"], $_POST["copy"]);
 
   Header("Location: grilles.php");
-
 }
 
+if ($_POST["action"] == 'remove' && $user->rights->telephonie->tarif->permission)
+{
+  require_once DOL_DOCUMENT_ROOT."/telephonie/telephonie.tarif.grille.class.php";
+
+  $obgrille = new TelephonieTarifGrille($db);
+      
+  $obgrille->RemoveGrille($user, $_POST["id"], $_POST["replace"]);
+
+  Header("Location: grilles.php");
+}
 
 llxHeader("","Configuration des grilles tarifs");
 
@@ -72,10 +81,9 @@ if ($result)
   while ($i < $num)
     {
       $obj = $db->fetch_object($i);	
-      $grilles[$i][0] = $obj->rowid;
-      $grilles[$i][1] = stripslashes($obj->tarif_desc);
-      $grilles[$i][2] = $obj->type_tarif;
-
+      $grilles[$obj->rowid][0] = $obj->rowid;
+      $grilles[$obj->rowid][1] = stripslashes($obj->tarif_desc);
+      $grilles[$obj->rowid][2] = $obj->type_tarif;
       $i++;
     }
 
@@ -89,7 +97,7 @@ else
 
 print '<table class="noborder" width="100%" cellspacing="0" cellpadding="4">';
 print '<tr class="liste_titre">';
-print "<td>Grille</td><td>Type</td>";
+print "<td>Grille</td><td>Type</td><td>&nbsp;</td>";
 print "</tr>\n";
 
 $var=True;
@@ -100,46 +108,61 @@ foreach($grilles as $grille)
   
   print "<tr $bc[$var]>";
   print '<td><a href="../grille.php?id='.$grille[0].'">'.$grille[1]."</a></td>\n";
-  print '<td>'.$grille[2]."</a></td>\n";
+  print '<td>'.$grille[2]."</td>\n";
+  print '<td align="right"><a href="grilles.php?id='.$grille[0].'&amp;action=delete">'.$langs->trans("Delete")."</a></td>\n";
   print "</tr>\n";
 }
 print "</table>";
 
 
-
-
-print '</td></tr></table>';
-
-
-
-print '<br><br><form action="grilles.php" method="post">';
-print '<input type="hidden" name="action" value="add">';
-
-print '<table class="border" width="100%">';
-
-// Nom
-print "<tr>".'<td valign="top">'.$langs->trans("Lastname").'*</td>';
-print '<td>';
-
-print '<input size="30" type="text" name="nom" value="">';
-
-print '</td></tr>';
-print "<tr><td>Type de grille</td>".'<td><select name="type"><option value="vente">vente<option value="achat">achat</select></td></tr>';
-
-print "<tr><td>Copier la grille</td>".'<td><select name="copy">';
-print '<option value="0">Grille vide</option>';
-foreach($grilles as $grille)
+if ($_GET["action"] == 'delete')
 {
-  print '<option value="'.$grille[0].'">'.$grille[1]."</option>\n";
+  print '<br><br><form action="grilles.php" method="post">';
+  print '<input type="hidden" name="action" value="remove">';
+  print '<input type="hidden" name="id" value="'.$_GET['id'].'">';
+  print '<table class="border" width="100%">';
+  print '<tr><td colspan="2">Supprimer la grille : '.$grilles[$_GET['id']][1].' ?</td></tr>';
+  // Nom
+  print "<tr><td>Utiliser la grille</td>".'<td><select name="replace">';
+  foreach($grilles as $grille)
+    {
+      if ($grille[0] <> $_GET['id'])
+	print '<option value="'.$grille[0].'">'.$grille[1]."</option>\n";
+    }
+  print '</select> en remplacement de la grille supprimee.</td></tr>';
+  
+  print "<tr>".'<td align="center" colspan="2"><input class="button" value="'.$langs->trans("Delete").'" type="submit"></td></tr>';
+  
+  print '</table></form>';
 }
-print '</select></td></tr>';
-
-print "<tr>".'<td align="center" colspan="2"><input class="button" value="'.$langs->trans("Create").'" type="submit"></td></tr>';
-
-print '</table></form>';
-
-
-//print '<br>Tableur des <a href="grille-export-achat.php">tarifs d\'achats comparés</a>';
+else
+{
+  print '<br><br><form action="grilles.php" method="post">';
+  print '<input type="hidden" name="action" value="add">';
+  
+  print '<table class="border" width="100%">';
+  
+  // Nom
+  print "<tr>".'<td valign="top">'.$langs->trans("Lastname").'*</td>';
+  print '<td>';
+  
+  print '<input size="30" type="text" name="nom" value="">';
+  
+  print '</td></tr>';
+  print "<tr><td>Type de grille</td>".'<td><select name="type"><option value="vente">vente<option value="achat">achat</select></td></tr>';
+  
+  print "<tr><td>Copier la grille</td>".'<td><select name="copy">';
+  print '<option value="0">Grille vide</option>';
+  foreach($grilles as $grille)
+    {
+      print '<option value="'.$grille[0].'">'.$grille[1]."</option>\n";
+    }
+  print '</select></td></tr>';
+  
+  print "<tr>".'<td align="center" colspan="2"><input class="button" value="'.$langs->trans("Create").'" type="submit"></td></tr>';
+  
+  print '</table></form>';
+}
 
 $db->close();
 
