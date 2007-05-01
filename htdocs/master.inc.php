f83ba0633f097a7734bd581f8b78744c692fff9d
Laurent Destailleur <eldy@users.sourceforge.net>
2007-05-01 20:07:27 +0000

    On passe a la 2.2

--- htdocs/master.inc.php
+++ htdocs/master.inc.php
@@ -32,7 +32,7 @@
    \version    $Revision$
 */
 
-define('DOL_VERSION','2.1-beta');
+define('DOL_VERSION','2.2-beta');
 define('EURO',chr(128)); 
 
 // La fonction clearstatcache ne doit pas etre appelé de manière globale car ralenti.
