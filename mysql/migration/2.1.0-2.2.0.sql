dff9ed682fad269d3de3393ccc76a88e468ba557
Laurent Destailleur <eldy@users.sourceforge.net>
2007-05-11 01:37:14 +0000

    Fix: Creation action ko sur relance facture.

--- mysql/migration/2.1.0-2.2.0.sql
+++ mysql/migration/2.1.0-2.2.0.sql
@@ -8,6 +8,8 @@
 -- sans AUCUNE erreur ni warning
 --
 
+update llx_actioncomm set fk_action = 9 where fk_action = 10;
+
 ALTER TABLE llx_cotisation ADD UNIQUE INDEX uk_cotisation (fk_adherent,dateadh);
 
 update llx_const set name='MAIN_ENABLE_DEVELOPMENT' where name='MAIN_SHOW_DEVELOPMENT_MODULES';
