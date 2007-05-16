f0c47eaa09b50b84f4019729e681528260d7fd6f
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2007-05-16 14:19:01 +0000

    Ajout champs manquants

--- htdocs/telephonie/sql/llx_telephonie_communications_details.sql
+++ htdocs/telephonie/sql/llx_telephonie_communications_details.sql
@@ -23,28 +23,32 @@
 -- Communications téléphoniques traitées
 
 create table llx_telephonie_communications_details (
-  fk_ligne         integer,
-  ligne            varchar(255) NOT NULL,
-  date             datetime,
-  numero           varchar(255),
-  dest             varchar(255),
-  dureetext        varchar(255),
-  fourn_cout       varchar(255),
-  fourn_montant    real,
-  duree            integer,        -- duree en secondes
-  tarif_achat_temp real,  
-  tarif_achat_fixe real,  
-  tarif_vente_temp real,  
-  tarif_vente_fixe real,  
-  cout_achat       real,
-  cout_vente       real,
-  remise           real,
-  fichier_cdr      varchar(255),
-  fk_fournisseur   integer,
-  fk_telephonie_facture       integer,
+  fk_ligne              integer,
+  ligne                 varchar(255) NOT NULL,
+  date                  datetime,
+  numero                varchar(255),
+  dest                  varchar(255),
+  dureetext             varchar(255),
+  fourn_cout            varchar(255),
+  fourn_montant         real,
+  duree                 integer,        -- duree en secondes
+  tarif_achat_temp      real,  
+  tarif_achat_fixe      real,  
+  tarif_vente_temp      real,  
+  tarif_vente_fixe      real,  
+  cout_achat            real,
+  cout_vente            real,
+  remise                real,
+  fichier_cdr           varchar(255),
+  fk_fournisseur        integer,
+  fk_telephonie_facture integer,
+  ym                    char(4),
+  num_prefix            char(2),
 
   key (fk_fournisseur),
   key (fk_ligne),
   key (fk_telephonie_facture)
+  key (ym),
+  key (num_prefix)
 
 )type=innodb;
