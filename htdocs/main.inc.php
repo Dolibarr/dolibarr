85d8a4939c42f81f3c914336fda769f1ee2ec5be
Regis Houssin <regis@dolibarr.fr>
2007-05-29 16:39:53 +0000

    Fix: bug sur authentification ldap

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -277,8 +277,8 @@ if (! session_id() || ! isset($_SESSION["dol_login"]))
 		$ldapport=$dolibarr_main_auth_ldap_port;
 		$ldapversion=(int) $dolibarr_main_auth_ldap_version;	// Si pas de int, PEAR LDAP plante.
 		$ldapdn=$dolibarr_main_auth_ldap_dn;
-		$ldaplogin=$dolibarr_main_auth_ldap_admin_login;
-		$ldappass=$dolibarr_main_auth_ldap_admin_pass;
+		$ldapadminlogin=$dolibarr_main_auth_ldap_admin_login;
+		$ldapadminpass=$dolibarr_main_auth_ldap_admin_pass;
 		$ldapdebug=((! $dolibarr_main_auth_ldap_debug || $dolibarr_main_auth_ldap_debug=="false")?false:true);
 		
 	    if ($ldapdebug) print "DEBUG: Logging LDAP steps<br>\n";
