4aef2412ba08bc5e911fe812520f02a12a992098
ywarnier <ywarnier>
2007-08-27 14:39:22 +0000

    Added llx_facturedet.special_code (for shipping and ecotax)

--- mysql/tables/llx_facturedet.sql
+++ mysql/tables/llx_facturedet.sql
@@ -41,5 +41,6 @@ create table llx_facturedet
   info_bits		  integer DEFAULT 0, 	-- TVA NPR ou non
   fk_code_ventilation integer DEFAULT 0 NOT NULL,
   fk_export_compta    integer DEFAULT 0 NOT NULL,
-  rang                integer DEFAULT 0
+  rang                integer DEFAULT 0,         -- ordre d'affichage
+  special_code    tinyint UNSIGNED DEFAULT 0, -- code pour les lignes speciales (livraison=1, ecotaxe=2)
 )type=innodb;
