207fb63fe6adf425ba0c1b9c883143299577fbfc
Laurent Destailleur <eldy@users.sourceforge.net>
2007-10-23 22:22:09 +0000

    Fix: Notice sur styles

--- htdocs/theme/auguria/auguria.css.php
+++ htdocs/theme/auguria/auguria.css.php
@@ -31,7 +31,7 @@ require("../../conf/conf.php");
 header('Content-type: text/css');
 // Important: Avoid page request by browser and dynamic build at
 // each Dolibarr page access.
-if (! $conf->global->MAIN_FEATURES_LEVEL)
+if (! isset($conf->global->MAIN_FEATURES_LEVEL) || $conf->global->MAIN_FEATURES_LEVEL < 2)
 {
 	//header('Cache-Control: max-age=3600, public, must-revalidate');
 }
