e5b43f28e298d3f8678399ddffa83ff175f8e7da
Laurent Destailleur <eldy@users.sourceforge.net>
2007-11-08 17:57:21 +0000

    MPD pour module sondage

---
+++ mysql/tables/llx_surveys_answers_summary.sql
@@ -0,0 +1,33 @@
+-- ============================================================================
+-- Copyright (C) 2007 Laurent Destailleur  <eldy@users.sourceforge.net>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- ===========================================================================
+
+CREATE TABLE llx_surveys_answers_summary (
+  fk_question       integer NOT NULL,
+  nb_rep1           decimal(10,0) NOT NULL default '0',
+  nb_rep2           decimal(10,0) default NULL,
+  nb_rep3           decimal(10,0) default NULL,
+  nb_rep4           decimal(10,0) default NULL,
+  tot_rep1          decimal(10,0) NOT NULL default '0',
+  tot_rep2          decimal(10,0) default NULL,
+  tot_rep3          decimal(10,0) default NULL,
+  tot_rep4          decimal(10,0) default NULL
+)
---
+++ mysql/tables/llx_surveys_questions.sql
@@ -0,0 +1,33 @@
+-- ============================================================================
+-- Copyright (C) 2007 Laurent Destailleur  <eldy@users.sourceforge.net>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- $Source$
+--
+-- ===========================================================================
+
+CREATE TABLE llx_surveys_questions (
+  rowid             integer AUTO_INCREMENT PRIMARY KEY,
+  type_question     decimal(1,0) NOT NULL default '0',
+  group_question    varchar(16)  NOT NULL default 'NONE',
+  status            decimal(1,0) NOT NULL default '0',
+  lib               varchar(255) NOT NULL default '',
+  lib_rep1          varchar(100) NOT NULL default '',
+  lib_rep2          varchar(100) default NULL,
+  lib_rep3          varchar(100) default NULL,
+  lib_rep4          varchar(100) default NULL
+)type=innodb;
