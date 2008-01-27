ba48b59b859df29552a107a9e0b45c72c1994739
Laurent Destailleur <eldy@users.sourceforge.net>
2008-01-27 12:12:15 +0000

    Doc: Doxygen

--- htdocs/admin/menus/index.php
+++ htdocs/admin/menus/index.php
@@ -15,14 +15,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * $Id$
  */
 
 /**
 		\file       htdocs/admin/menus/index.php
         \ingroup    core
-		\brief      Gestion des menus
+		\brief      Page to show personalized menus
+		\version	$Id$
 */
 
 require("./pre.inc.php");
