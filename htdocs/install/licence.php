2c0f0760374847b9bf34ffd395c848e9386ef156
Laurent Destailleur <eldy@users.sourceforge.net>
2008-02-01 01:22:40 +0000

    Fix: Regression pg install

--- htdocs/install/licence.php
+++ htdocs/install/licence.php
@@ -15,16 +15,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * $Id$
- * $Source$
  */
 
 /**
 	    \file       htdocs/install/licence.php
         \ingroup    install
 		\brief      Page affichage license
-		\version    $Revision$
+		\version    $Source$
 */
 
 include_once("./inc.php");
