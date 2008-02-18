cffe74f4b88d19f3134373c8532d31ff53d0d2de
Laurent Destailleur <eldy@users.sourceforge.net>
2008-02-18 23:19:55 +0000

    Doc: Doxygen

--- htdocs/product/index.php
+++ htdocs/product/index.php
@@ -16,15 +16,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * $Id$
  */
 
 /**
    \file       htdocs/product/index.php
    \ingroup    product
    \brief      Page accueil des produits et services
-   \version    $Revision$
+   \version    $Id$
 */
 
 require("./pre.inc.php");
