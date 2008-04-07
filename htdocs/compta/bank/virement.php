5129918adb69e363352211d80c1431265c318c8c
Laurent Destailleur <eldy@users.sourceforge.net>
2008-04-07 01:16:52 +0000

    Minor change

--- htdocs/compta/bank/virement.php
+++ htdocs/compta/bank/virement.php
@@ -15,15 +15,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * $Id$
  */
 
 /**
 		\file       htdocs/compta/bank/virement.php
 		\ingroup    banque
 		\brief      Page de saisie d'un virement
-		\version    $Revision$
+		\version    $Id$
 */
 
 require("./pre.inc.php");
