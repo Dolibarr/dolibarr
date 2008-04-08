40b2b3550f9523dcf2c2c3583c9339424e51d9a0
Laurent Destailleur <eldy@users.sourceforge.net>
2008-04-08 18:24:56 +0000

    New: Module agenda go on calendar view by default

---
+++ htdocs/admin/agenda_xcal.php
@@ -0,0 +1,126 @@
+<?php
+/* Copyright (C) 2008 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/**
+	    \file       htdocs/admin/agenda.php
+        \ingroup    agenda
+        \brief      Page de configuration du module agenda
+		\version    $Id$
+*/
+
+require("./pre.inc.php");
+require_once(DOL_DOCUMENT_ROOT."/lib/admin.lib.php");
+require_once(DOL_DOCUMENT_ROOT.'/lib/agenda.lib.php');
+
+
+if (!$user->admin)
+    accessforbidden();
+
+$langs->load("admin");
+$langs->load("other");
+$langs->load("agenda");
+
+$def = array();
+$actionsave=$_POST["save"];
+
+// Sauvegardes parametres
+if ($actionsave)
+{
+    $i=0;
+
+    $db->begin();
+    
+    $i+=dolibarr_set_const($db,'MAIN_PASSWORD_VCALEXPORT',trim($_POST["MAIN_PASSWORD_VCALEXPORT"]),'chaine',0);
+
+    if ($i > 0)
+    {
+        $db->commit();
+        $mesg = "<font class=\"ok\">".$langs->trans("SetupSaved")."</font>";
+    }
+    else
+    {
+        $db->rollback();
+        $mesg = "<font class=\"error\">".$langs->trans("SaveFailed")."</font>";
+    }
+}
+
+
+
+/**
+ * Vies
+ */
+
+llxHeader();
+
+$linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToModuleList").'</a>';
+print_fiche_titre($langs->trans("AgendaSetup"),$linkback,'setup');
+print '<br>';
+
+print $langs->trans("AgendaSetupOtherDesc")."<br>\n";
+print "<br>\n";
+
+$head=agenda_prepare_head();
+
+dolibarr_fiche_head($head, 'xcal', $langs->trans("Agenda"));
+
+
+print '<form name="agendasetupform" action="'.$_SERVER["PHP_SELF"].'" method="post">';
+print "<table class=\"noborder\" width=\"100%\">";
+
+print "<tr class=\"liste_titre\">";
+print "<td width=\"30%\">".$langs->trans("Parameter")."</td>";
+print "<td>".$langs->trans("Value")."</td>";
+//print "<td>".$langs->trans("Examples")."</td>";
+print "<td>&nbsp;</td>";
+print "</tr>";
+
+print "<tr class=\"impair\">";
+print "<td>".$langs->trans("PasswordTogetVCalExport")."</td>";
+print "<td><input type=\"text\" class=\"flat\" name=\"MAIN_PASSWORD_VCALEXPORT\" value=\"". ($_POST["MAIN_PASSWORD_VCALEXPORT"]?$_POST["MAIN_PASSWORD_VCALEXPORT"]:$conf->global->MAIN_PASSWORD_VCALEXPORT) . "\" size=\"40\"></td>";
+print "<td>&nbsp;</td>";
+print "</tr>";
+
+print '</table>';
+
+print '<br><center>';
+print "<input type=\"submit\" name=\"save\" class=\"button\" value=\"".$langs->trans("Save")."\">";
+print "</center>";
+
+print "</form>\n";
+
+print '</div>';
+
+clearstatcache();
+
+if ($mesg) print "<br>$mesg<br>";
+print "<br>";
+
+// Show message
+$message='';
+$urlwithouturlroot=eregi_replace(DOL_URL_ROOT.'$','',$dolibarr_main_url_root);
+$urlvcal='<a href="'.DOL_URL_ROOT.'/comm/action/agendaexport.php?format=vcal&exportkey='.($conf->global->MAIN_PASSWORD_VCALEXPORT?urlencode($conf->global->MAIN_PASSWORD_VCALEXPORT):'...').'" target="_blank">'.$urlwithouturlroot.DOL_URL_ROOT.'/comm/action/agendaexport.php?format=vcal&exportkey='.($conf->global->MAIN_PASSWORD_VCALEXPORT?urlencode($conf->global->MAIN_PASSWORD_VCALEXPORT):'...').'</a>';
+$message.=$langs->trans("WebCalUrlForVCalExport",'vcal',$urlvcal);
+$message.='<br>';
+$urlical='<a href="'.DOL_URL_ROOT.'/comm/action/agendaexport.php?format=ical&type=event&exportkey='.($conf->global->MAIN_PASSWORD_VCALEXPORT?urlencode($conf->global->MAIN_PASSWORD_VCALEXPORT):'...').'" target="_blank">'.$urlwithouturlroot.DOL_URL_ROOT.'/comm/action/agendaexport.php?format=ical&type=event&exportkey='.($conf->global->MAIN_PASSWORD_VCALEXPORT?urlencode($conf->global->MAIN_PASSWORD_VCALEXPORT):'...').'</a>';
+$message.=$langs->trans("WebCalUrlForVCalExport",'ical',$urlical);
+print info_admin($message);
+
+$db->close();
+
+llxFooter('$Date$ - $Revision$');
+?>
