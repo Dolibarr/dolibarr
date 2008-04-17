f2a4c90b873aa17fa48df9be62bcf4691175d107
Laurent Destailleur <eldy@users.sourceforge.net>
2008-04-17 10:26:38 +0000

    Do not show php info on doliwamp

--- htdocs/install/check.php
+++ htdocs/install/check.php
@@ -61,7 +61,7 @@ else
 {
     print '<img src="../theme/eldy/img/tick.png" alt="Ok"> '.$langs->trans("PHPVersion")." ".versiontostring(versionphparray());
 }
-print ' (<a href="phpinfo.php" target="_info">'.$langs->trans("MoreInformation").'</a>)';
+if (empty($force_install_nophpinfo)) print ' (<a href="phpinfo.php" target="_info">'.$langs->trans("MoreInformation").'</a>)';
 print "<br>\n";
 
 
