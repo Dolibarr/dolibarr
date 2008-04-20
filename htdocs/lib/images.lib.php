abe025686bfe1bcec496b0fc0042dd60ebb13157
Laurent Destailleur <eldy@users.sourceforge.net>
2008-04-20 04:31:31 +0000

    Qual: Move graphic thermometer function to images.lib.php library.

--- htdocs/lib/images.lib.php
+++ htdocs/lib/images.lib.php
@@ -241,4 +241,163 @@ function vignette($file, $maxWidth = 160, $maxHeight = 120, $extName='_small', $
 	return $imgThumbName;
 }
 
+
+/**
+		\brief permet d'afficher un thermometre monetaire.
+		\param actualValue
+		\param pendingValue
+		\param intentValue
+		\return thermometer htmlLegenda
+	    This function returns the html for the moneymeter.
+	    cachedValue: amount of actual money
+	    pendingValue: amount of money of pending memberships
+	    intentValue: amount of intended money (that's without the amount of actual money)
+*/
+function moneyMeter($actualValue=0, $pendingValue=0, $intentValue=0)
+{
+
+    // variables
+  $height="200";
+  $maximumValue=125000;
+        
+  $imageDir = "http://eucd.info/images/";
+                
+  $imageTop = $imageDir . "therm_top.png";    
+  $imageMiddleActual = $imageDir . "therm_actual.png";
+  $imageMiddlePending = $imageDir . "therm_pending.png";
+  $imageMiddleIntent = $imageDir . "therm_intent.png";
+  $imageMiddleGoal = $imageDir . "therm_goal.png";
+  $imageIndex = $imageDir . "therm_index.png";
+  $imageBottom =  $imageDir . "therm_bottom.png";
+  $imageColorActual = $imageDir . "therm_color_actual.png";
+  $imageColorPending = $imageDir . "therm_color_pending.png";
+  $imageColorIntent = $imageDir . "therm_color_intent.png";
+       
+  $htmlThermTop = '
+        <!-- Thermometer Begin -->
+        <table cellpadding="0" cellspacing="4" border="0">
+        <tr><td>
+        <table cellpadding="0" cellspacing="0" border="0">
+          <tr>
+            <td colspan="2"><img src="' . $imageTop . '" width="58" height="6" border="0"></td>
+          </tr>
+          <tr>
+            <td>
+              <table cellpadding="0" cellspacing="0" border="0">';
+                  
+  $htmlSection = '
+          <tr><td><img src="{image}" width="26" height="{height}" border="0"></td></tr>';
+         
+  $htmlThermbottom = '        
+              </table>
+            </td>
+            <td><img src="' . $imageIndex . '" width="32" height="200" border="0"></td>
+          </tr>
+          <tr>
+            <td colspan="2"><img src="' . $imageBottom . '" width="58" height="32" border="0"></td>
+          </tr>
+        </table>
+        </td>
+      </tr></table>';        
+
+  // legenda
+    
+  $legendaActual = "&euro; " . round($actualValue);
+  $legendaPending = "&euro; " . round($pendingValue);
+  $legendaIntent = "&euro; " . round($intentValue);
+  $legendaTotal = "&euro; " . round($actualValue + $pendingValue + $intentValue);
+  $htmlLegenda = '
+
+        <table cellpadding="0" cellspacing="0" border="0">
+          <tr><td><img src="' . $imageColorActual . '" width="9" height="9">&nbsp;</td><td><font size="1" face="Verdana, Arial, Helvetica, sans-serif"><b>Payé:<br />' . $legendaActual . '</b></font></td></tr>
+          <tr><td><img src="' . $imageColorPending . '" width="9" height="9">&nbsp;</td><td><font size="1" face="Verdana, Arial, Helvetica, sans-serif">En attente:<br />' . $legendaPending . '</font></td></tr>
+          <tr><td><img src="' . $imageColorIntent . '" width="9" height="9">&nbsp;</td><td><font size="1" face="Verdana, Arial, Helvetica, sans-serif">Promesses:<br />' . $legendaIntent . '</font></td></tr>
+          <tr><td>&nbsp;</td><td><font size="1" face="Verdana, Arial, Helvetica, sans-serif">Total:<br />' . $legendaTotal . '</font></td></tr>
+        </table>
+
+        <!-- Thermometer End -->';
+      
+  // check and edit some values
+      
+  $error = 0;    
+  if ( $maximumValue <= 0 || $height <= 0 || $actualValue < 0 || $pendingValue < 0 || $intentValue < 0)
+    {
+      return "The money meter could not be processed<br>\n";        
+    }
+  if ( $actualValue > $maximumValue ) 
+    {
+      $actualValue = $maximumValue;
+      $pendingValue = 0;
+      $intentValue = 0;
+    }
+  else
+    {
+      if ( ($actualValue + $pendingValue) > $maximumValue )
+	{
+	  $pendingValue = $maximumValue - $actualValue;
+	  $intentValue = 0;
+	}
+      else
+	{
+	  if ( ($actualValue + $pendingValue + $intentValue) > $maximumValue )
+	    {
+	      $intentValue = $maximumValue - $actualValue - $pendingValue;
+	    }            
+	}
+    }
+    
+  // start writing the html (from bottom to top)
+        
+  // bottom    
+  $thermometer = $htmlThermbottom;
+    
+  // actual
+  $sectionHeight = round(($actualValue / $maximumValue) * $height);
+  $totalHeight = $totalHeight + $sectionHeight;
+  if ( $sectionHeight > 0 )
+    {
+      $section = $htmlSection;
+      $section = str_replace("{image}", $imageMiddleActual, $section);
+      $section = str_replace("{height}", $sectionHeight, $section);
+      $thermometer = $section . $thermometer;
+    }
+  
+  // pending
+  $sectionHeight = round(($pendingValue / $maximumValue) * $height);
+  $totalHeight = $totalHeight + $sectionHeight;
+  if ( $sectionHeight > 0 )
+    {
+      $section = $htmlSection;
+      $section = str_replace("{image}", $imageMiddlePending, $section);
+      $section = str_replace("{height}", $sectionHeight, $section);
+      $thermometer = $section . $thermometer;
+    }
+  
+  // intent
+  $sectionHeight = round(($intentValue / $maximumValue) * $height);
+  $totalHeight = $totalHeight + $sectionHeight;
+  if ( $sectionHeight > 0 )
+    {
+      $section = $htmlSection;
+      $section = str_replace("{image}", $imageMiddleIntent, $section);
+      $section = str_replace("{height}", $sectionHeight, $section);
+      $thermometer = $section . $thermometer;
+    }
+  
+  // goal        
+  $sectionHeight = $height- $totalHeight;
+  if ( $sectionHeight > 0 )
+    {
+      $section = $htmlSection;
+      $section = str_replace("{image}", $imageMiddleGoal, $section);
+      $section = str_replace("{height}", $sectionHeight, $section);
+      $thermometer = $section . $thermometer;
+    }      
+  
+  // top
+  $thermometer = $htmlThermTop . $thermometer;
+  
+  return $thermometer . $htmlLegenda;
+}
+
 ?>
--- htdocs/public/dons/therm.php
+++ htdocs/public/dons/therm.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2002 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2008 Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,78 +15,34 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * $Id$
  */
 
-$thermlib = "../../lib/thermometer.php";
+/**
+	    \file       htdocs/public/dons/therm.php
+        \ingroup    donation
+		\brief      Screen with thermometer
+		\version    $Id$
+*/
 
-if (file_exists ($thermlib))
-{
-  include($thermlib);
-
-  if (1)
-    {
-
-
-      $dons_file = "/var/run/dolibarr.don.eucd";
-
-      /*
-       * Read Values
-       */
-      
-      if (file_exists ($dons_file))
-	{
-	  
-	  $fp = fopen($dons_file, 'r' );
-	  
-	  
-	  if ($fp)
-	    {
-	      $intentValue = fgets( $fp, 10 );
-	      $pendingValue = fgets( $fp, 10 );
-	      $actualValue = fgets( $fp, 10 );
-	      fclose( $fp ); 
-	    }
-	  
-	}
-      
-    }
-  else
-    {
+require("../../master.inc.php");
+require_once(DOL_DOCUMENT_ROOT."/lib/images.lib.php");
 
 
+/*
+* Read Values
+*/
+$dontherm = new Don($db);
+ 
+$intentValue  = $dontherm->sum_donations(1);
+$pendingValue = $dontherm->sum_donations(2);
+$actualValue  = $dontherm->sum_donations(3);
 
-      /*
-       * Read Values
-       */
-      
-      $conf = new Conf();
-    $conf->db->type = $dolibarr_main_db_type;
-    $conf->db->port = $dolibarr_main_db_port;
-    $conf->db->host = $dolibarr_main_db_host;
-    $conf->db->name = $dolibarr_main_db_name;
-    $conf->db->user = $dolibarr_main_db_user;
-    $conf->db->pass = $dolibarr_main_db_pass;
-
-      $dbt = new DoliDb($conf->db->type,$conf->db->host,$conf->db->user,$conf->db->pass,$conf->db->name,$conf->db->port);
-
-      $dontherm = new Don($dbt);
-      
-      $intentValue  = $dontherm->sum_donations(1);
-      $pendingValue = $dontherm->sum_donations(2);
-      $actualValue  = $dontherm->sum_donations(3);
-
-      $dbt->close();
-    }
+$dbt->close();
 
 
-  /* 
-   * Graph thermometer
-   */
-  
-  print moneyMeter($actualValue, $pendingValue, $intentValue);
+/* 
+* Graph thermometer
+*/
+print moneyMeter($actualValue, $pendingValue, $intentValue);
 
-
-}
 ?>
