a9d4386bc4bf9187f60f893af34f2187f7a0a621
Laurent Destailleur <eldy@users.sourceforge.net>
2008-05-13 06:48:48 +0000

    Update bad link

--- htdocs/about.php
+++ htdocs/about.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (C) 2003-2004 Rodolphe Quiedeville  <rodolphe@quiedeville.org>
  * Copyright (C) 2003      Jean-Louis Bergamo    <jlb@j1b.org>
- * Copyright (C) 2004-2006 Laurent Destailleur   <eldy@users.sourceforge.net>
+ * Copyright (C) 2004-2008 Laurent Destailleur   <eldy@users.sourceforge.net>
  * Copyright (C) 2007      Franky Van Liedekerke <franky.van.liedekerke@telenet.be>
  * Copyright (C) 2005-2007 Regis Houssin         <regis@dolibarr.fr>
  *
@@ -18,14 +18,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * $Id$
  */
 
 /**
  *        \file       htdocs/about.php
  *       \brief      Fichier page a propos
- *       \version    $Revision$
+ *       \version    $Id$
  */
 
 require("./pre.inc.php");
@@ -115,14 +113,13 @@ if (eregi('^fr_',$langs->getDefaultLang()))
     print 'Vente / Support';
     print '<ul>';
     print '<li>';
-    print 'Contactez Rodolphe Quiédeville sur <a target="blank" href="http://rodolphe.quiedeville.org">www.dolibarr.com</a>';
+    print 'Contactez Rodolphe Quiédeville sur <a target="blank" href="http://rodolphe.quiedeville.org">rodolphe.quiedeville.org</a>';
     print '</li>';
     print '</ul>';
 }
 
 
 llxFooter('$Date$ - $Revision$');
-
 ?>
 
 
