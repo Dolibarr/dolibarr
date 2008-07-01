bb2ae0306f92f77e71dab9e38780de112b37af38
Laurent Destailleur <eldy@users.sourceforge.net>
2008-07-01 19:54:00 +0000

    Fix: Bad init value at install

--- mysql/data/data.sql
+++ mysql/data/data.sql
@@ -201,10 +201,10 @@ insert into llx_const (name, value, type, visible) values ('FACTURE_ADDON_PDF', 
 --
 -- Forcer les locales
 --
-insert into llx_const (name, value, type, visible, note) VALUES ('MAIN_FORCE_SETLOCALE_LC_ALL',      'MAIN_FORCE_SETLOCALE_LC_ALL', 'chaine', 1, 'Pour forcer LC_ALL si pb de locale');
-insert into llx_const (name, value, type, visible, note) VALUES ('MAIN_FORCE_SETLOCALE_LC_TIME',     'MAIN_FORCE_SETLOCALE_LC_TIME', 'chaine', 1, 'Pour forcer LC_TIME si pb de locale');
-insert into llx_const (name, value, type, visible, note) VALUES ('MAIN_FORCE_SETLOCALE_LC_MONETARY', 'MAIN_FORCE_SETLOCALE_LC_MONETARY', 'chaine', 1, 'Pour forcer LC_MONETARY si pb de locale');
-insert into llx_const (name, value, type, visible, note) VALUES ('MAIN_FORCE_SETLOCALE_LC_NUMERIC',  'MAIN_FORCE_SETLOCALE_LC_NUMERIC', 'chaine', 1, 'Mettre la valeur C si problème de centimes');
+insert into llx_const (name, value, type, visible, note) VALUES ('MAIN_FORCE_SETLOCALE_LC_ALL',      '', 'chaine', 1, 'Pour forcer LC_ALL si pb de locale');
+insert into llx_const (name, value, type, visible, note) VALUES ('MAIN_FORCE_SETLOCALE_LC_TIME',     '', 'chaine', 1, 'Pour forcer LC_TIME si pb de locale');
+insert into llx_const (name, value, type, visible, note) VALUES ('MAIN_FORCE_SETLOCALE_LC_MONETARY', '', 'chaine', 1, 'Pour forcer LC_MONETARY si pb de locale');
+insert into llx_const (name, value, type, visible, note) VALUES ('MAIN_FORCE_SETLOCALE_LC_NUMERIC',  '', 'chaine', 1, 'Mettre la valeur C si problème de centimes');
 
 --
 -- Duree de validite des propales
