e65cf96e66eae237751ae6ef969afb7bda0d4f67
Laurent Destailleur <eldy@users.sourceforge.net>
2008-07-09 09:24:37 +0000

    Fix: Standatd HTML

--- htdocs/compta/index.php
+++ htdocs/compta/index.php
@@ -116,11 +116,11 @@ if ($conf->facture->enabled)
 
 	print '<form method="post" action="'.DOL_URL_ROOT.'/fourn/facture/index.php">';
 	print '<table class="noborder" width="100%">';
-	print "<tr class=\"liste_titre\">";
-	print '<td colspan="3">'.$langs->trans("SearchASupplierInvoice").'</td></tr>';
-	print "<tr $bc[0]><td>".$langs->trans("Ref").':</td><td><input type="text" name="search_ref" class="flat" size="18"></td>';
-	print '<td><input type="submit" value="'.$langs->trans("Search").'" class="button"></td></tr>';
-	//print "<tr $bc[0]><td>".$langs->trans("Other").':</td><td><input type="text" name="sall" class="flat" size="18"></td>';
+	print '<tr class="liste_titre"><td colspan="3">'.$langs->trans("SearchASupplierInvoice").'</td></tr>';
+	print "<tr ".$bc[0].">";
+	print "<td>".$langs->trans("Ref").':</td><td><input type="text" name="search_ref" class="flat" size="18"></td>';
+	print '<td><input type="submit" value="'.$langs->trans("Search").'" class="button"></td>';
+	//print "<tr ".$bc[0]."><td>".$langs->trans("Other").':</td><td><input type="text" name="sall" class="flat" size="18"></td>';
 	print '</tr>';
 	print "</table></form><br>";
 }
