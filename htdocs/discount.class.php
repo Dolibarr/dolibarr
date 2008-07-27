3f858d010cc33c04bc403b71c678ed9d41cb59b8
Laurent Destailleur <eldy@users.sourceforge.net>
2008-07-27 23:12:00 +0000

    Doxygen

--- htdocs/discount.class.php
+++ htdocs/discount.class.php
@@ -15,15 +15,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * $Id$
  */
 
 /**
 		\file       htdocs/discount.class.php
-		\ingroup    propal,facture,commande
+		\ingroup    propal facture commande
 		\brief      Fichier de la classe de gestion des remises
-		\version    $Revision$
+		\version    $Id$
 */
 
 
