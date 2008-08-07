ebc6542c3b6978cf3edb21de7a271e0811475a5b
Laurent Destailleur <eldy@users.sourceforge.net>
2008-08-07 07:47:38 +0000

    Qual: Remove dead code

--- htdocs/includes/modules/propale/pdf_propale_azur.modules.php
+++ htdocs/includes/modules/propale/pdf_propale_azur.modules.php
@@ -324,17 +324,18 @@ class pdf_propale_azur extends ModelePDFPropales
 
 					$nexY+=2;    // Passe espace entre les lignes
 						
-					//on compte le nombre de ligne afin de vérifier la place disponible
-					$nblineFollowDesc = (num_lines($follow_descproduitservice)*4);
-					// cherche nombre de lignes a venir pour savoir si place suffisante
-					if($i < ($nblignes - 1)){
+					// Cherche nombre de lignes a venir pour savoir si place suffisante
+					if ($i < ($nblignes - 1))	// If it's not last line
+					{
 						//on récupère la description du produit suivant
 						$follow_descproduitservice = $propale->lignes[$i+1]->desc;
 						//on compte le nombre de ligne afin de vérifier la place disponible (largeur de ligne 52 caracteres)
 						$nblineFollowDesc = (num_lines($follow_descproduitservice,52)*4);
 					}
-					else
+					else	// If it's last line
+					{
 						$nblineFollowDesc = 0;
+					}
 					
 					// test si besoin nouvelle page
 					if (($nexY+$nblineFollowDesc) > ($tab_top+$tab_height) && $i < ($nblignes - 1))
