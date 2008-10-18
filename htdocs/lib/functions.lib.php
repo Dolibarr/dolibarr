af7b8fed4e3dd718d01536840149185b4a7d971f
Laurent Destailleur <eldy@users.sourceforge.net>
2008-10-18 15:43:10 +0000

    Add date in error output

--- htdocs/lib/functions.lib.php
+++ htdocs/lib/functions.lib.php
@@ -1553,6 +1553,7 @@ function dolibarr_print_error($db='',$error='')
 		print $langs->trans("InformationToHelpDiagnose").":<br><br>\n";
 
 		print "<b>".$langs->trans("Dolibarr").":</b> ".DOL_VERSION."<br>\n";;
+		print "<b>".$langs->trans("Date").":</b> ".dolibarr_print_date(time(),'dayhourlog')."<br>\n";;
 		if (isset($conf->global->MAIN_FEATURES_LEVEL)) print "<b>".$langs->trans("LevelOfFeature").":</b> ".$conf->global->MAIN_FEATURES_LEVEL."<br>\n";;
 		print "<b>".$langs->trans("Server").":</b> ".$_SERVER["SERVER_SOFTWARE"]."<br>\n";;
 		print "<b>".$langs->trans("RequestedUrl").":</b> ".$_SERVER["REQUEST_URI"]."<br>\n";;
