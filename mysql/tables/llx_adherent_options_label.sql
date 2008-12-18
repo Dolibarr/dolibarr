1efaa445d369e64bab5b990f07a201f29b212a17
Regis Houssin <regis@dolibarr.fr>
2008-12-18 20:32:27 +0000

    Fix: Syntax error

--- mysql/tables/llx_adherent_options_label.sql
+++ mysql/tables/llx_adherent_options_label.sql
@@ -24,7 +24,7 @@ create table llx_adherent_options_label
 	name            varchar(64) PRIMARY KEY, -- nom de l'attribut
 	tms             timestamp,
 	label           varchar(255) NOT NULL, -- label correspondant a l'attribut
-	type 			varchar(8),
-	size			integer DEFAULT 0;
-	pos				integer DEFAULT 0;
+	type            varchar(8),
+	size            integer DEFAULT 0,
+	pos             integer DEFAULT 0
 )type=innodb;
