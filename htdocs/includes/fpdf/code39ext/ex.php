36e400b4ba1d2669b94e3c375de193d561acd3f4
Laurent Destailleur <eldy@users.sourceforge.net>
2009-01-05 00:43:47 +0000

    Removed deprecated files

--- htdocs/includes/fpdf/code39ext/ex.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('FPDF_FONTPATH','font/');
-require('code39.php');
-
-$pdf=new PDF_Code39();
-$pdf->AddPage();
-$pdf->Code39(60, 30, 'Code 39');
-$pdf->Output();
-?>
--- htdocs/includes/fpdf/i25/ex.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('FPDF_FONTPATH','font/');
-require('i25.php');
-
-$pdf=new PDF_i25();
-$pdf->AddPage();
-$pdf->i25(90,40,'12345678');
-$pdf->Output();
-?>
