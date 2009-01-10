ec94209502e456811ba13dea6bad66b88cb5c230
Laurent Destailleur <eldy@users.sourceforge.net>
2009-01-10 03:50:18 +0000

    *** empty log message ***

--- mysql/migration/2.4.0-2.5.0.sql
+++ mysql/migration/2.4.0-2.5.0.sql
@@ -93,4 +93,15 @@ alter table llx_bank_account modify comment        text;
 
 update llx_bank_account set ref = label where ref is null or ref = '';
 
+-- 2.5.1
+update llx_actioncomm set datep = datea where datep is null;
+
+
+insert into llx_c_type_contact(rowid, element, source, code, libelle, active ) values (70, 'facture_fourn', 'internal', 'SALESREPFOLL',  'Responsable suivi du paiement', 1);
+insert into llx_c_type_contact(rowid, element, source, code, libelle, active ) values (71, 'facture_fourn', 'external', 'BILLING',       'Contact fournisseur facturation', 1);
+insert into llx_c_type_contact(rowid, element, source, code, libelle, active ) values (72, 'facture_fourn', 'external', 'SHIPPING',      'Contact fournisseur livraison', 1);
+insert into llx_c_type_contact(rowid, element, source, code, libelle, active ) values (73, 'facture_fourn', 'external', 'SERVICE',       'Contact fournisseur prestation', 1);
+
+alter table llx_product_stock add column   pmp             double(24,8) default 0 NOT NULL;
+
 
