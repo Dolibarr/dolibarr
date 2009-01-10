738a22385906064b07a56865a42d69581cf83be0
Laurent Destailleur <eldy@users.sourceforge.net>
2009-01-10 02:33:29 +0000

    Fix: Syntax error

--- htdocs/compta/propal.php
+++ htdocs/compta/propal.php
@@ -759,7 +759,7 @@ else
             print '</td>';
           
             print '<td width="20" class="nobordernopadding" nowrap="nowrap">';
-            if ($objp->fk_statut == 1 && $objp->din < ($now() - $conf->propal->cloture->warning_delay)) print img_warning($langs->trans("Late"));
+            if ($objp->fk_statut == 1 && $objp->din < ($now - $conf->propal->cloture->warning_delay)) print img_warning($langs->trans("Late"));
             print '</td>';
                     
             print '<td width="16" align="right" class="nobordernopadding">';
