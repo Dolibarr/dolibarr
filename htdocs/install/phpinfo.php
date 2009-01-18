930c211eb9d187ae2adb77366ec0d4cf44d8ba2c
Laurent Destailleur <eldy@users.sourceforge.net>
2009-01-18 17:57:10 +0000

    Fix: Error in ereg_replace

--- htdocs/install/phpinfo.php
+++ htdocs/install/phpinfo.php
@@ -14,16 +14,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * $Id$
- * $Source$
  */
 
 /**
         \file       htdocs/install/phpinfo.php
         \ingroup    install
         \brief      Affichage page phpinfo
-        \version    $Revision$
+        \version    $Id$
 */
 
 phpinfo()
