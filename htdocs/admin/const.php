0bd1156782c4108a902e1b2cbe56cae6ed4f1e60
Regis Houssin <regis@dolibarr.fr>
2009-05-15 10:27:38 +0000

    Todo: protection faille CSRF !!!

--- htdocs/admin/const.php
+++ htdocs/admin/const.php
@@ -30,9 +30,12 @@ require_once(DOL_DOCUMENT_ROOT."/lib/admin.lib.php");
 
 $langs->load("admin");
 
-if (!$user->admin)
+//Todo protection faille CSRF !!!
+if (!eregi(DOL_MAIN_URL_ROOT, $_SERVER['HTTP_REFERER']))
 accessforbidden();
 
+if (!$user->admin)
+accessforbidden();
 
 
 $typeconst=array('yesno','texte','chaine');
