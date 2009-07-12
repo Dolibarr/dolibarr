f6d86611c41334a5551b96f11d68ba2574893040
Regis Houssin <regis@dolibarr.fr>
2009-07-12 06:56:51 +0000

    Add: possibility to add google adsense in demo mode

--- htdocs/lib/security.lib.php
+++ htdocs/lib/security.lib.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2008-2009 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2008-2009 Regis Houssin        <regis@dolibarr.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -269,6 +270,24 @@ function dol_loginfunction($langs,$conf,$mysoc)
 		}
 		print nl2br($conf->global->MAIN_HOME);
 		print '</td></tr></table></center><br>'."\n";
+	}
+	
+	// Google Adsense (ex: demo mode)
+	if (! empty($conf->global->MAIN_GOOGLE_AD_CLIENT) && ! empty($conf->global->MAIN_GOOGLE_AD_SLOT))
+	{
+		print '<div align="center">'."\n";
+		print '<script type="text/javascript"><!--'."\n";
+		print 'google_ad_client = "'.$conf->global->MAIN_GOOGLE_AD_CLIENT.'";'."\n";
+		print '/* '.$conf->global->MAIN_GOOGLE_AD_WIDTH.'x'.$conf->global->MAIN_GOOGLE_AD_HEIGHT.', '.$conf->global->MAIN_GOOGLE_AD_NAME.' */'."\n";
+		print 'google_ad_slot = "'.$conf->global->MAIN_GOOGLE_AD_SLOT.'";'."\n";
+		print 'google_ad_width = '.$conf->global->MAIN_GOOGLE_AD_WIDTH.';'."\n";
+		print 'google_ad_height = '.$conf->global->MAIN_GOOGLE_AD_HEIGHT.';'."\n";
+		print '//-->'."\n";
+		print '</script>'."\n";
+		print '<script type="text/javascript"'."\n";
+		print 'src="http://pagead2.googlesyndication.com/pagead/show_ads.js">'."\n";
+		print '</script>'."\n";
+		print '</div>'."\n";
 	}
 
 	print "\n";
