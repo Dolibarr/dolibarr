66df3817e42222b5ba5c91578be7b104683f64ad
Laurent Destailleur <eldy@users.sourceforge.net>
2009-07-29 22:29:09 +0000

    Doc

--- htdocs/translate.class.php
+++ htdocs/translate.class.php
@@ -143,8 +143,8 @@ class Translate {
 
 
 	/**
-	 *	\brief      	Deprecated function. Do nothing.
-	 *	\deprecated		Kept for backward compatibility with third party modules.
+	 *	Deprecated function. Do nothing.
+	 *	@deprecated		Kept for backward compatibility with third party modules.
 	 */
 	function setPhpLang()
 	{
