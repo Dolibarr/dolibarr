f84afda6d9d3dac3b9700cee14bafcda9078f855
Laurent Destailleur <eldy@users.sourceforge.net>
2009-08-10 13:25:02 +0000

    Fix version of gravatar module

--- htdocs/includes/modules/modGravatar.class.php
+++ htdocs/includes/modules/modGravatar.class.php
@@ -57,7 +57,7 @@ class modGravatar extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Use online Gravatar service (www.gravatar.com) to show photo of users/members (found with their emails).<br>Need an internet access.";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.0';
+		$this->version = 'dolibarr';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
