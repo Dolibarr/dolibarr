7c833eb82d10479ca2ecb4d740a43d2a80a0e016
Laurent Destailleur <eldy@users.sourceforge.net>
2009-09-07 13:33:08 +0000

    Fix: The dolibarr_install.log was not saved with DoliWamp

--- htdocs/install/inc.php
+++ htdocs/install/inc.php
@@ -151,7 +151,9 @@ if (! defined('SYSLOG_FILE'))	// To avoid warning on systems with constant alrea
 	if (@is_writable('/tmp')) define('SYSLOG_FILE','/tmp/dolibarr_install.log');
 	else if (! empty($_ENV["TMP"])  && @is_writable($_ENV["TMP"]))  define('SYSLOG_FILE',$_ENV["TMP"].'/dolibarr_install.log');
 	else if (! empty($_ENV["TEMP"]) && @is_writable($_ENV["TEMP"])) define('SYSLOG_FILE',$_ENV["TEMP"].'/dolibarr_install.log');
-	else if (@is_writable("/")) define('SYSLOG_FILE','/dolibarr_install.log');
+	else if (@is_writable('../../../../')) define('SYSLOG_FILE','../../../../dolibarr_install.log');	// For DoliWamp
+	else if (@is_writable('../../')) define('SYSLOG_FILE','../../dolibarr_install.log');				// For others
+	//print 'SYSLOG_FILE='.SYSLOG_FILE;exit;
 }
 if (! defined('SYSLOG_FILE_NO_ERROR'))
 {
@@ -233,7 +235,9 @@ function conf($dolibarr_main_document_root)
 		if (@is_writable('/tmp')) define('SYSLOG_FILE','/tmp/dolibarr_install.log');
 		else if (! empty($_ENV["TMP"])  && @is_writable($_ENV["TMP"]))  define('SYSLOG_FILE',$_ENV["TMP"].'/dolibarr_install.log');
 		else if (! empty($_ENV["TEMP"]) && @is_writable($_ENV["TEMP"])) define('SYSLOG_FILE',$_ENV["TEMP"].'/dolibarr_install.log');
-		else if (@is_writable("/")) define('SYSLOG_FILE','/dolibarr_install.log');
+		else if (@is_writable('../../../../')) define('SYSLOG_FILE','../../../../dolibarr_install.log');	// For DoliWamp
+		else if (@is_writable('../../')) define('SYSLOG_FILE','../../dolibarr_install.log');				// For others
+		//print 'SYSLOG_FILE='.SYSLOG_FILE;exit;
 	}
 	if (! defined('SYSLOG_FILE_NO_ERROR'))
 	{
