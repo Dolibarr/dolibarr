28705e8d71c9699483dc6f35f15d75bfd32b690c
Laurent Destailleur <eldy@users.sourceforge.net>
2009-10-07 17:57:42 +0000

    Remove deprecated file

--- build/deb/conf.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-$dolibarr_main_document_root="/usr/share/dolibarr";
-$dolibarr_main_url_root="http://localhost/dolibarr";
-$dolibarr_main_db_host="DBHOST";
-$dolibarr_main_db_name="DBNAME";
-$dolibarr_main_db_user="DBUSER";
-$dolibarr_main_db_pass="DBPASS";
-?>
