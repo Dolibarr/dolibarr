c7fe80367af8d71edb12b8197fe7d437a064cd6f
Laurent Destailleur <eldy@users.sourceforge.net>
2009-12-14 00:35:03 +0000

    First add for test tools.

--- dev/test/testutf.php
+++ dev/test/testutf.php
@@ -1,9 +1,13 @@
 <?php
 // This source file must be UTF-8 encoded
 
-$filename='filewithÃ©';
+$filename='filewithé';
 
 print 'Test to create a file on disk'."\n";
+print 'ENV[LANG]='.$_ENV["LANG"]."\n";
+print 'ENV[LANGUAGE]='.$_ENV["LANGUAGE"]."\n";
+
+// Si LANG contient UTF8, system en UTF8, pas de conversion requise pour fopen
 $s=fopen('/tmp/'.$filename,'w');
 fclose($s);
 
