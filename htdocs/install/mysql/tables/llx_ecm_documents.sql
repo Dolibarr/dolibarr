8dd2a8c7169f09aa4b4d6b00231f7683ae079972
Regis Houssin <regis@dolibarr.fr>
2009-12-15 15:15:45 +0000

    Works on files encryption

--- htdocs/install/mysql/tables/llx_ecm_documents.sql
+++ htdocs/install/mysql/tables/llx_ecm_documents.sql
@@ -1,5 +1,6 @@
 -- ===================================================================
 -- Copyright (C) 2008 Laurent Destailleur  <eldy@users.sourceforge.net>
+-- Copyright (C) 2009 Regis Houssin        <regis@dolibarr.fr>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -22,20 +23,24 @@
 
 create table llx_ecm_document
 (
-  rowid           integer AUTO_INCREMENT PRIMARY KEY,
-  ref             varchar(16)  NOT NULL,
-  filename        varchar(255) NOT NULL,
-  filesize        integer      NOT NULL,
-  filemime        varchar(32)  NOT NULL,
-  fullpath_dol    varchar(255) NOT NULL,
-  fullpath_orig   varchar(255) NOT NULL,
-  description     text,
-  manualkeyword   text,
-  fk_create       integer  NOT NULL,
-  fk_update       integer,
-  date_c	      datetime NOT NULL,
-  date_u		  timestamp,
-  fk_directory    integer,
-  fk_status		  smallint DEFAULT 0,
-  private         smallint DEFAULT 0
+  rowid           	integer AUTO_INCREMENT PRIMARY KEY,
+  ref             	varchar(16)  NOT NULL,
+  entity          	integer DEFAULT 1 NOT NULL,				-- multi company id
+  filename        	varchar(255) NOT NULL,
+  filesize        	integer      NOT NULL,
+  filemime        	varchar(32)  NOT NULL,
+  fullpath_dol    	varchar(255) NOT NULL,
+  fullpath_orig   	varchar(255) NOT NULL,
+  description     	text,
+  manualkeyword   	text,
+  fk_create       	integer  NOT NULL,
+  fk_update       	integer,
+  date_c	      	datetime NOT NULL,
+  date_u		  	timestamp,
+  fk_directory    	integer,
+  fk_status		  	smallint DEFAULT 0,
+  private         	smallint DEFAULT 0,
+  crc 				varchar(32) NOT NULL DEFAULT '',  		-- checksum
+  key				varchar(50) NOT NULL DEFAULT '',  		-- crypt key
+  cipher 			varchar(50) NOT NULL DEFAULT 'twofish', -- crypt cipher
 ) type=innodb;
