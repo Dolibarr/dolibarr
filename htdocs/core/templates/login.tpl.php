a0521a26144547102b66034e9c912d6a9aee7862
Laurent Destailleur <eldy@users.sourceforge.net>
2010-01-04 19:46:00 +0000

    Core templates are in core/templates directory

--- htdocs/core/templates/login.tpl.php
+++ htdocs/core/templates/login.tpl.php
@@ -23,6 +23,7 @@ header("Content-type: text/html; charset=".$conf->file->character_set_client);
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!-- BEGIN TEMPLATE -->
 
 <html>
 <head>
@@ -163,7 +164,17 @@ function donnefocus() {
 
 	<?php if ($main_google_ad_client) { ?>
 		<div align="center">
-		<?php include('google_ad.tpl.php'); ?>
+			<script type="text/javascript"><!--
+				google_ad_client = "{$main_google_ad_client}";
+				/* {$main_google_ad_width}x{$main_google_ad_height}, {$main_google_ad_name} */
+				google_ad_slot = "{$main_google_ad_slot}";
+				google_ad_width = {$main_google_ad_width};
+				google_ad_height = {$main_google_ad_height};
+				//-->
+			</script>
+			<script type="text/javascript"
+				src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>
 		</div>
 	<?php } ?>
 
@@ -179,4 +190,4 @@ function donnefocus() {
 </body>
 </html>
 
-<!-- END SMARTY TEMPLATE -->
+<!-- END TEMPLATE -->
