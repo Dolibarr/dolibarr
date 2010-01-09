6404866bffb6193ed2c74820048fac4103a8c85c
Laurent Destailleur <eldy@users.sourceforge.net>
2010-01-09 01:22:16 +0000

    Fix: Error for upgrades from 2.5

--- htdocs/install/upgrade2.php
+++ htdocs/install/upgrade2.php
@@ -29,7 +29,7 @@ if (! file_exists($conffile))
 {
 	print 'Error: Dolibarr config file was not found. This may means that Dolibarr is not installed yet. Please call the page "/install/install.php" instead of "/install/upgrade.php").';
 }
-require_once($conffile);
+require_once($conffile); if (! isset($dolibarr_main_db_type)) $dolibarr_main_db_type='mysql';	// For backward compatibility
 require_once($dolibarr_main_document_root . "/lib/databases/".$dolibarr_main_db_type.".lib.php");
 require_once($dolibarr_main_document_root . '/facture.class.php');
 require_once($dolibarr_main_document_root . '/propal.class.php');
