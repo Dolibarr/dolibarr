36024ed4af6fdfddafa39151a5fef0b973d79225
Laurent Destailleur <eldy@users.sourceforge.net>
2010-01-23 19:44:14 +0000

    Fix: Bad field name

--- htdocs/install/mysql/migration/2.7.0-2.8.0.sql
+++ htdocs/install/mysql/migration/2.7.0-2.8.0.sql
@@ -3,9 +3,10 @@
 --
 -- Be carefull to requests order.
 -- This file must be loaded by calling /install/index.php page
--- when current version is 2.7.0 or higher. 
+-- when current version is 2.8.0 or higher. 
 --
 
+ALTER TABLE llx_actioncomm CHANGE fk_projet fk_project integer;
 
 ALTER TABLE llx_don ADD COLUMN ref varchar(30) DEFAULT NULL AFTER rowid;
 ALTER TABLE llx_don ADD COLUMN entity integer DEFAULT 1 NOT NULL AFTER ref;
