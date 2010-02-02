d6d1612f3ca95a365450e4f2cbdec44664206fd9
Laurent Destailleur <eldy@users.sourceforge.net>
2010-02-02 08:54:47 +0000

    Minor change

--- htdocs/admin/webservices.php
+++ htdocs/admin/webservices.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2004      Rodolphe Quiedeville <rodolphe@quiedeville.org>
- * Copyright (C) 2005-2009 Laurent Destailleur  <eldy@users.sourceforge.org>
+ * Copyright (C) 2005-2010 Laurent Destailleur  <eldy@users.sourceforge.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -61,33 +61,6 @@ print $langs->trans("WebServicesDesc")."<br>\n";
 
 if ($mesg) print '<br>'.$mesg;
 
-/*
-print '<br>';
-print '<form method="post" action="'.$_SERVER["PHP_SELF"].'">';
-print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
-print '<input type="hidden" name="action" value="setvalue">';
-
-$var=true;
-
-print '<table class="noborder" width="100%">';
-print '<tr class="liste_titre">';
-print '<td>'.$langs->trans("Parameter").'</td>';
-print '<td>'.$langs->trans("Value").'</td>';
-print "</tr>\n";
-
-
-$var=!$var;
-print '<tr '.$bc[$var].'><td>';
-print $langs->trans("PAYBOX_IBS_DEVISE").'</td><td>';
-print '<input size="32" type="text" name="PAYBOX_IBS_DEVISE" value="'.$conf->global->PAYBOX_IBS_DEVISE.'">';
-print '<br>'.$langs->trans("Example").': 978 (EUR)';
-print '</td></tr>';
-
-print '</table>';
-print '</form>';
-*/
-
-
 print '<br><br>';
 
 // Should work with DOL_URL_ROOT='' or DOL_URL_ROOT='/dolibarr'
@@ -96,41 +69,13 @@ $firstpart=preg_replace('/'.preg_quote(DOL_URL_ROOT,'/').'$/i','',$firstpart);
 
 print '<u>'.$langs->trans("WSDLCanBeDownloadedHere").':</u><br>';
 $url=$firstpart.DOL_URL_ROOT.'/webservices/server.php?wsdl';
-print img_picto('','puce.png').' '.'<a href="'.$url.'">'.$url."</a><br>\n";
+print img_picto('','puce.png').' '.'<a href="'.$url.'" target="_blank">'.$url."</a><br>\n";
 print '<br>';
 
 print '<u>'.$langs->trans("EndPointIs").':</u><br>';
 $url=$firstpart.DOL_URL_ROOT.'/webservices/server.php';
-print img_picto('','puce.png').' '.'<a href="'.$url.'">'.$url."</a><br>\n";
+print img_picto('','puce.png').' '.'<a href="'.$url.'" target="_blank">'.$url."</a><br>\n";
 print '<br>';
-
-/*
-if ($conf->commande->enabled)
-{
-	print img_picto('','puce.png').' '.$langs->trans("ToOfferALinkForOnlinePaymentOnOrder",$servicename).':<br>';
-	print '<b>'.$firstpart.DOL_URL_ROOT.'/public/paybox/newpayment.php?amount=order&ref=<i>order_ref</i></b>'."<br>\n";
-	print '<br>';
-}
-if ($conf->facture->enabled)
-{
-	print img_picto('','puce.png').' '.$langs->trans("ToOfferALinkForOnlinePaymentOnInvoice",$servicename).':<br>';
-	print '<b>'.$firstpart.DOL_URL_ROOT.'/public/paybox/newpayment.php?amount=invoice&ref=<i>invoice_ref</i></b>'."<br>\n";
-//	print $langs->trans("SetupPayBoxToHavePaymentCreatedAutomatically",$langs->transnoentitiesnoconv("FeatureNotYetAvailable"))."<br>\n";
-	print '<br>';
-}
-if ($conf->contrat->enabled)
-{
-	print img_picto('','puce.png').' '.$langs->trans("ToOfferALinkForOnlinePaymentOnContractLine",$servicename).':<br>';
-	print '<b>'.$firstpart.DOL_URL_ROOT.'/public/paybox/newpayment.php?amount=contractline&ref=<i>contractline_ref</i></b>'."<br>\n";
-	print '<br>';
-}
-if ($conf->adherent->enabled)
-{
-	print img_picto('','puce.png').' '.$langs->trans("ToOfferALinkForOnlinePaymentOnMemberSubscription",$servicename).':<br>';
-	print '<b>'.$firstpart.DOL_URL_ROOT.'/public/paybox/newpayment.php?amount=membersubscription&ref=<i>member_ref</i></b>'."<br>\n";
-	print '<br>';
-}
-*/
 
 $db->close();
 
