68b2188e8d8dc8a727ba77596ba28b33305d798d
Laurent Destailleur <eldy@users.sourceforge.net>
2010-02-13 23:37:43 +0000

    Fix: Compare between dev and beta was wrong

--- htdocs/lib/admin.lib.php
+++ htdocs/lib/admin.lib.php
@@ -55,8 +55,10 @@ function versioncompare($versionarray1,$versionarray2)
 	{
 		$operande1=isset($versionarray1[$level])?$versionarray1[$level]:0;
 		$operande2=isset($versionarray2[$level])?$versionarray2[$level]:0;
-		if (preg_match('/beta|alpha|dev/i',$operande1)) $operande1=-1;
-		if (preg_match('/beta|alpha|dev/i',$operande2)) $operande2=-1;
+		if (preg_match('/alpha|dev/i',$operande1)) $operande1=-2;
+		if (preg_match('/alpha|dev/i',$operande2)) $operande2=-2;
+		if (preg_match('/beta/i',$operande1)) $operande1=-1;
+		if (preg_match('/beta/i',$operande2)) $operande2=-1;
 		$level++;
 		//print 'level '.$level.' '.$operande1.'-'.$operande2.'<br>';
 		if ($operande1 < $operande2) { $ret = -$level; break; }
