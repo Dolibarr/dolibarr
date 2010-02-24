d3a8fb190a0e548550e3775b05098b061afd68df
Laurent Destailleur <eldy@users.sourceforge.net>
2010-02-24 14:55:22 +0000

    Doxygen

--- htdocs/societe.php
+++ htdocs/societe.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2001-2004 Rodolphe Quiedeville <rodolphe@quiedeville.org>
- * Copyright (C) 2004-2009 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2004-2010 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2009 Regis Houssin        <regis@dolibarr.fr>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -21,7 +21,7 @@
 /**
  *	\file       htdocs/societe.php
  *	\ingroup    societe
- *	\brief      Page des societes
+ *	\brief      Page to show a third party
  *	\version    $Id$
  */
 
