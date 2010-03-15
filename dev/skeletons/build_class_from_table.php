c942d1928ea441dbc2c84a1cc3cd10579ab15549
Philippe Grand <philippe.grand@atoo-net.com>
2010-03-15 09:39:07 +0000

    Some spelling changes

--- dev/skeletons/build_class_from_table.php
+++ dev/skeletons/build_class_from_table.php
@@ -30,7 +30,7 @@ $path=preg_replace('@[\\\/]+$@','',$path).'/';
 
 // Test if batch mode
 if (substr($sapi_type, 0, 3) == 'cgi') {
-    echo "Error: You ar usingr PH for CGI. To execute ".$script_file." from command line, you must use PHP for CLI mode.\n";
+    echo "Error: You are using PHP for CGI. To execute ".$script_file." from command line, you must use PHP for CLI mode.\n";
     exit;
 }
 
