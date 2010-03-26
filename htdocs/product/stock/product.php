c97336183263280240a8e5e5ed298e8acebc5f22
Laurent Destailleur <eldy@users.sourceforge.net>
2010-03-26 23:23:38 +0000

    Fix: Load trad

--- htdocs/product/stock/product.php
+++ htdocs/product/stock/product.php
@@ -36,6 +36,7 @@ require_once(DOL_DOCUMENT_ROOT."/html.formproduct.class.php");
 $langs->load("products");
 $langs->load("orders");
 $langs->load("bills");
+$langs->load("stocks");
 
 // Security check
 if (isset($_GET["id"]) || isset($_GET["ref"]))
