cab4356e3d5aec69c66e96c21e5220dfd835ccd9
Philippe Grand <philippe.grand@atoo-net.com>
2010-03-27 17:09:09 +0000

    correct call to class commande.class.php

--- htdocs/commande/liste.php
+++ htdocs/commande/liste.php
@@ -29,6 +29,7 @@
 
 require("../main.inc.php");
 require_once(DOL_DOCUMENT_ROOT."/html.formfile.class.php");
+require_once(DOL_DOCUMENT_ROOT ."/commande/commande.class.php");
 
 $langs->load('orders');
 $langs->load('companies');
