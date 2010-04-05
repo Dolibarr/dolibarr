b1f01a6330721f28d0675f4f395ff36700d25711
Regis Houssin <regis@dolibarr.fr>
2010-04-05 08:43:53 +0000

    Fix: syntax

--- htdocs/viewimage.php
+++ htdocs/viewimage.php
@@ -40,7 +40,7 @@ if (! defined('NOCSRFCHECK'))     define('NOCSRFCHECK','1');
 if (! defined('NOTOKENRENEWAL'))  define('NOTOKENRENEWAL','1');
 if (! defined('NOREQUIREMENU'))  define('NOREQUIREMENU','1');
 if (! defined('NOREQUIREHTML'))  define('NOREQUIREHTML','1');
-if (! defined('NOREQUIREAJAX'))  define('NOREQUIREAJAXL','1');
+if (! defined('NOREQUIREAJAX'))  define('NOREQUIREAJAX','1');
 // Pour autre que companylogo, on charge environnement + info issus de logon comme le user
 if (($modulepart == 'companylogo') && ! defined("NOLOGIN")) define("NOLOGIN",'1');
 
