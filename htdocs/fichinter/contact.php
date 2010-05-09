0e83ec13595ed9bedff10f0753418697920132ab
Laurent Destailleur <eldy@users.sourceforge.net>
2010-05-09 15:04:17 +0000

    Fix: Bad parameters

--- htdocs/fichinter/contact.php
+++ htdocs/fichinter/contact.php
@@ -266,7 +266,7 @@ if ($id > 0)
 
 			print '<td colspan="1">';
 			//$contactAlreadySelected = $fichinter->getListContactId('external');	// On ne doit pas desactiver un contact deja selectionner car on doit pouvoir le seclectionner une deuxieme fois pour un autre type
-			$nbofcontacts=$html->select_contacts($selectedCompany, '', 'contactid', 0, $contactAlreadySelected);
+			$nbofcontacts=$html->select_contacts($selectedCompany, $selected = '', 'contactid', 0, $contactAlreadySelected);
 			if ($nbofcontacts == 0) print $langs->trans("NoContactDefined");
 			print '</td>';
 			print '<td>';
