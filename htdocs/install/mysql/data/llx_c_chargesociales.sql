542b5c7e64df229e9722e406779f32fc9a1fb1d9
Laurent Destailleur <eldy@users.sourceforge.net>
2010-05-18 20:47:07 +0000

    Keep key of tax line starting with "TAX".

--- htdocs/install/mysql/data/llx_c_chargesociales.sql
+++ htdocs/install/mysql/data/llx_c_chargesociales.sql
@@ -35,23 +35,23 @@
 --
 -- France
 --
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 1, 'Allocations familiales', 1,1,'TAXFAM'   ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 2, 'CSG Deductible',         1,1,'TAXCSGD'  ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 3, 'CSG/CRDS NON Deductible',0,1,'TAXCSGND' ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (10, 'Taxe apprentissage',     0,1,'TAXAPP'   ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (11, 'Taxe professionnelle',   0,1,'TAXPRO'   ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (20, 'Impots locaux/fonciers', 0,1,'TAXFON'   ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (25, 'Impots revenus',         0,1,'TAXREV'   ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (30, 'Assurance Sante',        0,1,'TAXSECU'  ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (40, 'Mutuelle',               0,1,'TAXMUT'   ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (50, 'Assurance vieillesse',   0,1,'TAXRET'   ,'1');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (60, 'Assurance Chomage',      0,1,'TAXCHOM'  ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (  1, 'Allocations familiales', 1,1,'TAXFAM'   ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (  2, 'CSG Deductible',         1,1,'TAXCSGD'  ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (  3, 'CSG/CRDS NON Deductible',0,1,'TAXCSGND' ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 10, 'Taxe apprentissage',     0,1,'TAXAPP'   ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 11, 'Taxe professionnelle',   0,1,'TAXPRO'   ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 20, 'Impots locaux/fonciers', 0,1,'TAXFON'   ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 25, 'Impots revenus',         0,1,'TAXREV'   ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 30, 'Assurance Sante',        0,1,'TAXSECU'  ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 40, 'Mutuelle',               0,1,'TAXMUT'   ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 50, 'Assurance vieillesse',   0,1,'TAXRET'   ,'1');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 60, 'Assurance Chomage',      0,1,'TAXCHOM'  ,'1');
 
 --
 -- Belgique
 --
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 201, 'ONSS',						1,1,'BETAXONSS'   ,'2');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 210, 'Precompte professionnel', 	1,1,'BETAXPREPRO' ,'2');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 220, 'Prime d\'existence', 		1,1,'BETAXPRIEXI' ,'2');
-insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values ( 230, 'Precompte immobilier', 	1,1,'BETAXPREIMMO','2');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (201, 'ONSS',						1,1,'TAXBEONSS'   ,'2');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (210, 'Precompte professionnel', 	1,1,'TAXBEPREPRO' ,'2');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (220, 'Prime d\'existence', 		1,1,'TAXBEPRIEXI' ,'2');
+insert into llx_c_chargesociales (id, libelle, deductible, active, code, fk_pays) values (230, 'Precompte immobilier',      1,1,'TAXBEPREIMMO','2');
 
