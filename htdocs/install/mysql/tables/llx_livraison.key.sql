15338f6b3a881023b9799b1c46138e3f015f7be8
Regis Houssin <regis@dolibarr.fr>
2010-05-29 21:00:36 +0000

    Works on delivery and billing address first step: rename llx_societe_adresse_livraison to llx_societe_address

--- htdocs/install/mysql/tables/llx_livraison.key.sql
+++ htdocs/install/mysql/tables/llx_livraison.key.sql
@@ -25,10 +25,9 @@ ALTER TABLE llx_livraison ADD UNIQUE INDEX idx_livraison_uk_ref (ref, entity);
 ALTER TABLE llx_livraison ADD INDEX idx_livraison_fk_soc (fk_soc);
 ALTER TABLE llx_livraison ADD INDEX idx_livraison_fk_user_author (fk_user_author);
 ALTER TABLE llx_livraison ADD INDEX idx_livraison_fk_user_valid (fk_user_valid);
-ALTER TABLE llx_livraison ADD INDEX idx_livraison_fk_address (fk_address);
+-- obsolete ALTER TABLE llx_livraison ADD INDEX idx_livraison_fk_adresse_livraison (fk_adresse_livraison);
 
 ALTER TABLE llx_livraison ADD CONSTRAINT fk_livraison_fk_soc			FOREIGN KEY (fk_soc)			REFERENCES llx_societe (rowid);
 ALTER TABLE llx_livraison ADD CONSTRAINT fk_livraison_fk_user_author	FOREIGN KEY (fk_user_author)	REFERENCES llx_user (rowid);
 ALTER TABLE llx_livraison ADD CONSTRAINT fk_livraison_fk_user_valid		FOREIGN KEY (fk_user_valid)		REFERENCES llx_user (rowid);
--- obsolete
--- ALTER TABLE llx_livraison ADD CONSTRAINT fk_livraison_fk_address		FOREIGN KEY (fk_address)		REFERENCES llx_societe_address (rowid);
+-- obsolete ALTER TABLE llx_livraison ADD CONSTRAINT fk_livraison_fk_adresse_livraison	FOREIGN KEY (fk_adresse_livraison)	REFERENCES llx_societe_address (rowid);
