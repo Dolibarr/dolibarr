491e58a4e74661447a210839c65f9997729575a3
Philippe Grand <philippe.grand@atoo-net.com>
2010-06-02 12:33:44 +0000

    Fix: call to expedition class missing

--- htdocs/lib/sendings.lib.php
+++ htdocs/lib/sendings.lib.php
@@ -23,6 +23,7 @@
  *	\version    $Id$
  */
 require_once(DOL_DOCUMENT_ROOT."/product/class/product.class.php");
+require_once(DOL_DOCUMENT_ROOT."/expedition/class/expedition.class.php");
 
 
 function shipping_prepare_head($object)
