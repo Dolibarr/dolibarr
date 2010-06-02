0fcf0a13a10c4c0f5fcae37f9006bbe767657d89
Laurent Destailleur <eldy@users.sourceforge.net>
2010-06-02 18:47:05 +0000

    Move multicompany to experimental level instead of development

--- htdocs/includes/modules/modMultiCompany.class.php
+++ htdocs/includes/modules/modMultiCompany.class.php
@@ -58,7 +58,7 @@ class modMultiCompany extends DolibarrModules
 		// Can be enabled / disabled only in the main company with superadmin account
 		$this->core_enabled = 1;
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = 'development';
+		$this->version = 'experimental';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
