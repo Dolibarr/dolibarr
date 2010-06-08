6064f5605565060dbd5c465b680972efe67c8ae4
Regis Houssin <regis@dolibarr.fr>
2010-06-08 06:58:03 +0000

    Fix: this choice is not part of the Milestone module

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -479,12 +479,9 @@ class Form
 			if (1 == $selected) print ' selected="true"';
 			print '>'.$langs->trans("Service");
 
-			if ($conf->milestone->enabled)
-			{
-				print '<option value="9"';
-				if (9 == $selected) print ' selected="true"';
-				print '>'.$langs->trans("Notes");
-			}
+			print '<option value="9"';
+			if (9 == $selected) print ' selected="true"';
+			print '>'.$langs->trans("Notes");
 
 			print '</select>';
 			//if ($user->admin) print info_admin($langs->trans("YouCanChangeValuesForThisListFromDictionnarySetup"),1);
