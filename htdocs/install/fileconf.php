99e5f46afba26e7090e2ce24fe1f8e6c5769adc6
Laurent Destailleur <eldy@users.sourceforge.net>
2010-07-20 18:59:53 +0000

    Fix: Remove warning

--- htdocs/install/fileconf.php
+++ htdocs/install/fileconf.php
@@ -201,7 +201,7 @@ print $langs->trans("Examples").":<br>";
 <tr>
 <td valign="top" class="label">
 <?php echo $langs->trans("ForceHttps"); ?>
-<td class="label" valign="top"><input type="checkbox" name="main_force_https"<?php if ($force_install_mainforcehttps) print ' checked="on"'; ?>></td>
+<td class="label" valign="top"><input type="checkbox" name="main_force_https"<?php if (! empty($force_install_mainforcehttps)) print ' checked="on"'; ?>></td>
 <td class="comment">
 <?php echo $langs->trans("CheckToForceHttps"); ?>
 </td>
