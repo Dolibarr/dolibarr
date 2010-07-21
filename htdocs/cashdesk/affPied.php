f8200b00d4ce056f4054f744df2d809911827034
Laurent Destailleur <eldy@users.sourceforge.net>
2010-07-21 12:04:39 +0000

    Doxygen

--- htdocs/cashdesk/affPied.php
+++ htdocs/cashdesk/affPied.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2007-2008 Jeremie Ollivier <jeremie.o@laposte.net>
+/* Copyright (C) 2007-2008 J�r�mie Ollivier <jeremie.o@laposte.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
