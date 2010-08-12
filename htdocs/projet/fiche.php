ef043984b972453301a384455772838970a821f0
Laurent Destailleur <eldy@users.sourceforge.net>
2010-08-12 18:52:34 +0000

    Fix: Usage of numbering project ref

--- htdocs/projet/fiche.php
+++ htdocs/projet/fiche.php
@@ -269,7 +269,7 @@ if ($_GET["action"] == 'create' && $user->rights->projet->creer)
 		$defaultref = $modProject->getNextValue($soc,$project);
 	}
 
-	if ($defaultref <= 0) $defaultref='';
+    if (is_numeric($defaultref) && $defaultref <= 0) $defaultref='';
 
 	// Ref
 	print '<tr><td><span class="fieldrequired">'.$langs->trans("Ref").'</span></td><td><input size="12" type="text" name="ref" value="'.($_POST["ref"]?$_POST["ref"]:$defaultref).'"></td></tr>';
