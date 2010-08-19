9c333459c6b0b82d3bcc2b83cd353d87385cdf40
Regis Houssin <regis@dolibarr.fr>
2010-08-19 14:20:56 +0000

    Fix: delete old PWC files and css

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -1771,10 +1771,6 @@ class Form
 			$pageyes=$page.'&action='.$action.'&confirm=yes';
 			$pageno=($useajax == 2?$page.'&confirm=no':'');
 
-			// Old code using PWC + Prototype
-            //print '<script type="text/javascript">window.onload = function(){ dialogConfirm(\''.$title.'\',\''.$pageyes.'\',\''.$pageno.'\',\''.dol_escape_js('<b>'.$title.'</b><br>'.$more.$question).'\',\''.$langs->trans("Yes").'\',\''.$langs->trans("No").'\',\'validate\'); }</script>';
-			//print '<script id="eee" type="text/javascript">dialogConfirm(\'aaa\',\'bbb\',\'ccc\',\'eee\',\'yyy\',\'zzz\',\'validate\')</script>';
-
 			// New code using jQuery only
 			print '<div id="dialog-confirm" title="'.dol_escape_htmltag($title).'">';
             print img_help('','').' '.$more.$question;
