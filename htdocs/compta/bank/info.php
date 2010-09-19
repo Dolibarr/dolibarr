3dfad634734045e6e52d5d583bc86bc765eab6e8
Laurent Destailleur <eldy@users.sourceforge.net>
2010-09-19 13:35:03 +0000

    Fix: Bad param

--- htdocs/compta/bank/info.php
+++ htdocs/compta/bank/info.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2005-2009 Laurent Destailleur  <eldy@users.sourceforge.net>
+/* Copyright (C) 2005-2010 Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ $langs->load("companies");
 llxHeader();
 
 $line = new AccountLine($db);
-$line->fetch($_GET["rowid"], $user);
+$line->fetch($_GET["rowid"]);
 $line->info($_GET["rowid"]);
 
 
