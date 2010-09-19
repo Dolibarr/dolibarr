11044f07ad89a9041528982f369babf5d3817e9d
Laurent Destailleur <eldy@users.sourceforge.net>
2010-09-19 10:55:35 +0000

    Security: Removed a dangerous default permission

--- htdocs/includes/modules/modUser.class.php
+++ htdocs/includes/modules/modUser.class.php
@@ -89,7 +89,7 @@ class modUser extends DolibarrModules
 		$this->rights[$r][0] = 251;
 		$this->rights[$r][1] = 'Consulter les autres utilisateurs, leurs groupes et permissions';
 		$this->rights[$r][2] = 'r';
-		$this->rights[$r][3] = 1;
+		$this->rights[$r][3] = 0;
 		$this->rights[$r][4] = 'user';
 		$this->rights[$r][5] = 'lire';
 
