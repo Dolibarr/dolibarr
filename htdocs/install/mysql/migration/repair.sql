425b6844e8f270e7e7eb74e3c2a8cf474258ed75
Laurent Destailleur <eldy@users.sourceforge.net>
2010-10-20 18:34:04 +0000

    Fix: Enhance the repair script

--- htdocs/install/mysql/migration/repair.sql
+++ htdocs/install/mysql/migration/repair.sql
@@ -12,5 +12,7 @@ delete from llx_facture where facnumber = '(PROV)';
 delete from llx_commande where ref = '(PROV)';
 delete from llx_propal where ref = '(PROV)';
 delete from llx_facture where facnumber = '';
+-- V4.1 delete from llx_commandedet where fk_commande in (select rowid from llx_commande where ref = '');
 delete from llx_commande where ref = '';
+-- V4.1 delete from llx_propaldet where fk_propal in (select rowid from llx_propal where ref = '');
 delete from llx_propal where ref = '';
