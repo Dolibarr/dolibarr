88f4539ccdabd4c3978b0ce4a8ae9f3acd51bbfe
Regis Houssin <regis@dolibarr.fr>
2010-10-24 19:03:18 +0000

    New: use jQuery mobile for smartphone menu New: add a footer bar menu Fix: more simple and more optimized

--- htdocs/theme/phones/smartphone/tpl/menu.tpl.php
+++ htdocs/theme/phones/smartphone/tpl/menu.tpl.php
@@ -36,7 +36,7 @@ $this->smartheader();
 	
 	</div><!-- /content -->
 	
-	<div data-role="footer" data-id="foo1" data-position="fixed" data-theme="b">
+	<div data-role="footer" data-id="foo1" data-theme="b">
 		<div data-role="navbar">
 			<ul>
 				<li><a href="<?php echo DOL_URL_ROOT.'/user/logout.php'; ?>"><?php echo $langs->trans("Logout"); ?></a></li>
