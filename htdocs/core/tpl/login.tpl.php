84f874c49a82611ef29c9db3f55834c89d167ae7
Laurent Destailleur <eldy@users.sourceforge.net>
2010-11-01 15:37:37 +0000

    Fix: W3C

--- htdocs/core/tpl/login.tpl.php
+++ htdocs/core/tpl/login.tpl.php
@@ -21,9 +21,8 @@
 header('Cache-Control: Public, must-revalidate');
 header("Content-type: text/html; charset=".$conf->file->character_set_client);
 ?>
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<!-- Ce DTD est KO car inhibe document.body.scrollTop ->
+<!-- Ce DTD est KO car inhibe document.body.scrollTop -->
 <!-- print '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">'; -->
 
 <!-- BEGIN PHP TEMPLATE -->
