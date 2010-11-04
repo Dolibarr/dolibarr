6da6637c43a0e3e11cc932c7931c173364384638
Laurent Destailleur <eldy@users.sourceforge.net>
2010-11-04 18:51:33 +0000

    Enhance repair field

--- htdocs/install/mysql/migration/repair.sql
+++ htdocs/install/mysql/migration/repair.sql
@@ -2,15 +2,19 @@
 -- $Id$
 --
 -- Script to repair some fatal errors due to database corruption
--- 
 -- when current version is 2.6.0 or higher. 
 --
 
 
--- Request to clean corrupted database
+-- Requests to clean corrupted database
+
+-- V4.1 delete from llx_facturedet where fk_facture in (select rowid from llx_facture where facnumber = '(PROV)');
 delete from llx_facture where facnumber = '(PROV)';
+-- V4.1 delete from llx_commandedet where fk_commande in (select rowid from llx_commande where ref = '(PROV)');
 delete from llx_commande where ref = '(PROV)';
+-- V4.1 delete from llx_propaldet where fk_propal in (select rowid from llx_propal where ref = '(PROV)');
 delete from llx_propal where ref = '(PROV)';
+-- V4.1 delete from llx_facturedet where fk_facture in (select rowid from llx_facture where facnumber = '');
 delete from llx_facture where facnumber = '';
 -- V4.1 delete from llx_commandedet where fk_commande in (select rowid from llx_commande where ref = '');
 delete from llx_commande where ref = '';
