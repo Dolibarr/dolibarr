3fa11011225ae1b75120eea3489d815d540b8f47
Laurent Destailleur <eldy@users.sourceforge.net>
2010-11-13 19:00:22 +0000

    Trad

--- htdocs/includes/modules/modPaypal.class.php
+++ htdocs/includes/modules/modPaypal.class.php
@@ -55,7 +55,7 @@ class modPaypal extends DolibarrModules
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
-		$this->description = "Module to offer an online payment page by credit card with PayPal";
+		$this->description = "Module to offer an online payment page with PayPal";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 		$this->version = 'dolibarr';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
