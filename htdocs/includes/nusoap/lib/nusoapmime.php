f1f604a0a74da60844461b8309f0575e80a4cf49
tiaris <tiaris>
2010-12-12 17:19:03 +0000

    update nusoap lib with standard nusoap 0.9.5 (http://sourceforge.net/projects/nusoap/)

--- htdocs/includes/nusoap/lib/nusoapmime.php
+++ htdocs/includes/nusoap/lib/nusoapmime.php
@@ -51,7 +51,7 @@ require_once('Mail/mimePart.php');
 * @version  $Id$
 * @access   public
 */
-class nusoap_client_mime extends soapclient_nusoap {
+class nusoap_client_mime extends nusoap_client {
 	/**
 	 * @var array Each array element in the return is an associative array with keys
 	 * data, filename, contenttype, cid
