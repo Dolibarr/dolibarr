3566889b3ad6430e66749853251fa144b4d74061
Regis Houssin <regis@dolibarr.fr>
2010-12-29 12:44:08 +0000

    Fix: path is ok

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -30,7 +30,6 @@
  */
 
 @ini_set('memory_limit', '64M');	// This may be useless if memory is hard limited by your PHP
-//define(DOL_DOCUMENT_ROOT_ALT,'');	// Set extra custom path, ex: /path/to/htdocs/custom (for dev only)
 
 // For optionnal tuning. Enabled if environment variable DOL_TUNING is defined.
 // A call first. Is the equivalent function dol_microtime_float not yet loaded.
