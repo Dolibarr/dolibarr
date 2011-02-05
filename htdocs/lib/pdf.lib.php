55b436c254009b71ad3fe3a1606cce35b9aedeb3
Laurent Destailleur <eldy@users.sourceforge.net>
2011-02-05 14:57:18 +0000

    Fix: Position of bank information

--- htdocs/lib/pdf.lib.php
+++ htdocs/lib/pdf.lib.php
@@ -423,9 +423,9 @@ function pdf_bank(&$pdf,$outputlangs,$curx,$cury,$account,$onlynumber=0)
         $pdf->MultiCell(90, 3, $outputlangs->transnoentities("Residence").': ' . $outputlangs->convToOutputCharset($account->domiciliation), 0, 'L', 0);
     }
 
-    $pdf->SetXY ($curx, $cury+9);
+    $pdf->SetXY ($curx, $cury+11);
     $pdf->MultiCell(90, 3, $outputlangs->transnoentities($ibankey).': ' . $outputlangs->convToOutputCharset($account->iban), 0, 'L', 0);
-    $pdf->SetXY ($curx, $cury+12);
+    $pdf->SetXY ($curx, $cury+14);
     $pdf->MultiCell(90, 3, $outputlangs->transnoentities($bickey).': ' . $outputlangs->convToOutputCharset($account->bic), 0, 'L', 0);
 
     return $pdf->getY();
