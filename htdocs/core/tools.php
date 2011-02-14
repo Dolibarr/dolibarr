dad02f923248607c06c9389af478947a9983a7c3
Laurent Destailleur <eldy@users.sourceforge.net>
2011-02-14 18:01:52 +0000

    Tools area has its own page.

---
+++ htdocs/core/tools.php
@@ -0,0 +1,59 @@
+<?php
+/* Copyright (C) 2001-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2010 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2010 Regis Houssin        <regis@dolibarr.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/**
+ *       \file       htdocs/core/tools.php
+ *       \brief      Home page for top menu tools
+ *       \version    $Id$
+ */
+
+require("../main.inc.php");
+
+$langs->load("companies");
+$langs->load("other");
+
+// Security check
+$socid=0;
+if ($user->societe_id > 0) $socid=$user->societe_id;
+if (!$user->rights->projet->lire) accessforbidden();
+
+
+/*
+ * View
+ */
+
+$socstatic=new Societe($db);
+
+llxHeader("",$langs->trans("Tools"),"");
+
+$text=$langs->trans("Tools");
+
+print_fiche_titre($text);
+
+// Show description of content
+print $langs->trans("ToolsDesc").'<br><br>';
+
+
+
+
+$db->close();
+
+llxFooter('$Date$ - $Revision$');
+?>
