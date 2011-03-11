fd067f3c647b3cd675153cb97a4af2f3c565acdc
Regis Houssin <regis@dolibarr.fr>
2011-03-11 19:47:14 +0000

    New: add table for lock/unlock an element during the modification and logging all

---
+++ htdocs/install/mysql/tables/llx_element_lock.sql
@@ -0,0 +1,31 @@
+-- ============================================================================
+-- Copyright (C) 2011 Regis Houssin <regis@dolibarr.fr>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Id$
+-- ============================================================================
+
+create table llx_element_lock
+(
+	rowid				integer	AUTO_INCREMENT	PRIMARY KEY,
+	fk_element			integer NOT NULL,
+	elementtype			varchar(16) NOT NULL,
+	datel    			datetime,				-- date of lock
+	datem    			datetime,				-- date of unlock/modif
+	fk_user_modif   	integer,
+	lock				tinyint DEFAULT 0,		-- 0 unlock, 1 lock
+	
+)ENGINE=innodb;
