19f173ec9081ff8a2f8b81d23d08a7b6d3cb6743
Regis Houssin <regis@dolibarr.fr>
2011-04-17 18:22:49 +0000

    Fix: bad permission and unused translation

--- htdocs/commande/contact.php
+++ htdocs/commande/contact.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (C) 2005      Patrick Rouillon     <patrick@rouillon.net>
- * Copyright (C) 2005-2009 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2005-2009 Regis Houssin        <regis@dolibarr.fr>
+ * Copyright (C) 2005-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2011 Regis Houssin        <regis@dolibarr.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,7 +31,6 @@ require_once(DOL_DOCUMENT_ROOT."/contact/class/contact.class.php");
 require_once(DOL_DOCUMENT_ROOT."/lib/order.lib.php");
 require_once(DOL_DOCUMENT_ROOT.'/core/class/html.formcompany.class.php');
 
-$langs->load("facture");
 $langs->load("orders");
 $langs->load("sendings");
 $langs->load("companies");
@@ -220,7 +219,7 @@ if ($id > 0 || ! empty($ref))
 		* Ajouter une ligne de contact
 		* Non affiche en mode modification de ligne
 		*/
-		if ($_GET["action"] != 'editline' && $user->rights->facture->creer)
+		if ($_GET["action"] != 'editline' && $user->rights->commande->creer)
 		{
 			print '<tr class="liste_titre">';
 			print '<td>'.$langs->trans("Source").'</td>';
