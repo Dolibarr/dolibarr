7eaa8c7ebf3fffb4b1dddcfcf791a6f125e1cf5b
Laurent Destailleur <eldy@users.sourceforge.net>
2011-05-03 10:18:15 +0000

    GETPOST can work with POST in priority

--- htdocs/lib/functions.lib.php
+++ htdocs/lib/functions.lib.php
@@ -61,14 +61,15 @@ function dol_shutdown()
  *  Return value of a param into GET or POST supervariable
  *  @param          paramname   Name of parameter to found
  *  @param			check		Type of check (''=no check,  'int'=check it's numeric, 'alpha'=check it's alpha only)
- *  @param			method		Type of method (0 = get or post, 1 = only get, 2 = only post)
+ *  @param			method		Type of method (0 = get then post, 1 = only get, 2 = only post, 3 = post then get)
  *  @return         string      Value found or '' if check fails
  */
 function GETPOST($paramname,$check='',$method=0)
 {
-    if ($method==1) $out = isset($_GET[$paramname])?$_GET[$paramname]:'';
-    else if ($method==2) $out = isset($_POST[$paramname])?$_POST[$paramname]:'';
-    else $out = isset($_GET[$paramname])?$_GET[$paramname]:(isset($_POST[$paramname])?$_POST[$paramname]:'');
+    if (empty($method)) $out = isset($_GET[$paramname])?$_GET[$paramname]:(isset($_POST[$paramname])?$_POST[$paramname]:'');
+    elseif ($method==1) $out = isset($_GET[$paramname])?$_GET[$paramname]:'';
+    elseif ($method==2) $out = isset($_POST[$paramname])?$_POST[$paramname]:'';
+    elseif ($method==3) $out = isset($_POST[$paramname])?$_POST[$paramname]:(isset($_GET[$paramname])?$_GET[$paramname]:'');
 
     if (!empty($check))
     {
