ceda1ad55cff57708fd09ff310bbd1b682028200
Regis Houssin <regis@dolibarr.fr>
2011-05-17 14:45:20 +0000

    Fix: missing socid

--- htdocs/fourn/liste.php
+++ htdocs/fourn/liste.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (C) 2001-2006 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2005-2006 Regis Houssin        <regis@dolibarr.fr>
+ * Copyright (C) 2005-2011 Regis Houssin        <regis@dolibarr.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -172,7 +172,7 @@ if ($resql)
 		$obj = $db->fetch_object($resql);
 		$var=!$var;
 
-        $thirdpartystatic->id=$obj->rowid;
+        $thirdpartystatic->id=$obj->socid;
         $thirdpartystatic->nom=$obj->nom;
         $thirdpartystatic->status=$obj->status;
 
