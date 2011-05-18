ff904e662b209c5cd2a44e40a6e164b9cd22a24e
Regis Houssin <regis@dolibarr.fr>
2011-05-18 17:59:47 +0000

    Fix: missing class project

--- htdocs/comm/action/document.php
+++ htdocs/comm/action/document.php
@@ -34,6 +34,7 @@ require_once(DOL_DOCUMENT_ROOT."/comm/action/class/cactioncomm.class.php");
 require_once(DOL_DOCUMENT_ROOT."/comm/action/class/actioncomm.class.php");
 require_once(DOL_DOCUMENT_ROOT."/lib/files.lib.php");
 require_once(DOL_DOCUMENT_ROOT."/core/class/html.formfile.class.php");
+if ($conf->projet->enabled) require_once(DOL_DOCUMENT_ROOT."/projet/class/project.class.php");
 
 $langs->load("companies");
 $langs->load("commercial");
