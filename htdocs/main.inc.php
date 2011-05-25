2115ecce62b334c6eb541a9e5f05615b9283c70a
Laurent Destailleur <eldy@users.sourceforge.net>
2011-05-25 23:47:01 +0000

    Removed warning

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -1145,7 +1145,7 @@ function top_menu($head, $title='', $target='', $disablejs=0, $disablehead=0, $a
     		</script>';
 		}
 
-		if ($conf->global->MAIN_MENU_USE_JQUERY_ACCORDION)
+		if (! empty($conf->global->MAIN_MENU_USE_JQUERY_ACCORDION))
 		{
 			print "\n".'<script type="text/javascript">
 					jQuery(document).ready(function () {
