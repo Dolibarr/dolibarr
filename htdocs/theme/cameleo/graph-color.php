bf09b62f17d742804029f004b8bdb49521867d01
Regis Houssin <regis@dolibarr.fr>
2011-05-26 18:02:46 +0000

    New: add new theme "cameleo" by Herve Prot (Symeos)

---
+++ htdocs/theme/cameleo/graph-color.php
@@ -0,0 +1,34 @@
+<?PHP
+/* Copyright (C) 2004      Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2007 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2010-2011 Herve Prot           <herve.prot@symeos.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/**
+ *		\file       htdocs/theme/cameleo/graph-color.php
+ *		\brief      File to declare colors to use to build graphics with theme cameleo
+ *      \ingroup    core
+ *		\version    $Id$
+ */
+
+global $theme_bordercolor, $theme_datacolor, $theme_bgcolor, $theme_bgcoloronglet;
+$theme_bordercolor = array(235,235,224);
+$theme_datacolor = array(array(120,130,150), array(200,160,180), array(190,190,220));
+$theme_bgcolor = array(hexdec('F4'),hexdec('F4'),hexdec('F4'));
+$theme_bgcoloronglet = array(hexdec('DE'),hexdec('E7'),hexdec('EC'));
+
+?>
