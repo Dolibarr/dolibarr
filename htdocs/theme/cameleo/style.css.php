fa29a9c0d69693ac5a53df8602930795c45f9792
Juanjo Menent <jmenent@2byte.es>
2011-06-21 14:27:37 +0000

    Fix: ECM Module resize bar misaligned

--- htdocs/theme/cameleo/style.css.php
+++ htdocs/theme/cameleo/style.css.php
@@ -3,6 +3,7 @@
  * Copyright (C)      2006 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (C) 2007-2011 Regis Houssin        <regis@dolibarr.fr>
  * Copyright (C) 2010-2011 Herve Prot           <herve.prot@symeos.com>
+ * Copyright (C) 2011      Juanjo Menent        <jmenent@2byte.es>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -794,7 +795,7 @@ td.photo {
 /* ============================================================================== */
 
 .ui-layout-resizer-west-open {
-	left: 200px !important;
+	/*left: 200px !important;*/
 }
 
 .ui-layout-north {
