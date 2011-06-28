26504802987db609f16d11caa5f9945bb89fc950
Regis Houssin <regis@dolibarr.fr>
2011-06-28 15:05:44 +0000

    Commit to dev branch test 2

--- htdocs/boxes.php
+++ htdocs/boxes.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (C) 2003      Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2005-2010 Regis Houssin        <regis@dolibarr.fr>
+ * Copyright (C) 2005-2011 Regis Houssin        <regis@dolibarr.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
  *	\brief      File of class to manage widget boxes
  *	\author     Rodolphe Qiedeville
  *	\author	    Laurent Destailleur
- *	\version    $Id$
+ *	\version    $Id: boxes.php,v 1.52 2011/06/28 15:05:44 hregis Exp $
  */
 
 
