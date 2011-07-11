e9c69fcb9e0822da39e645eb883d1fb36995eb83
Cyrille de Lambert <cyrille.delambert@auguria.net>
2011-07-11 09:32:35 +0000

    Correction paiement Ajax

--- htdocs/compta/ajaxpayment.php
+++ htdocs/compta/ajaxpayment.php
@@ -22,8 +22,8 @@
  *       \version    ajaxpayment.php,v 1.0
  */
 
-if (! defined('NOREQUIREUSER'))  define('NOREQUIREUSER','1');
-if (! defined('NOREQUIREDB'))    define('NOREQUIREDB','1');
+//if (! defined('NOREQUIREUSER'))  define('NOREQUIREUSER','1');
+//if (! defined('NOREQUIREDB'))    define('NOREQUIREDB','1');
 if (! defined('NOREQUIRESOC'))   define('NOREQUIRESOC','1');
 //if (! defined('NOREQUIRETRAN'))  define('NOREQUIRETRAN','1');
 if (! defined('NOCSRFCHECK'))    define('NOCSRFCHECK','1');
