a9e2e709842c8491666f50eac09e67bf69f7e2ff
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-21 22:31:49 +0000

    Enhance debian package

--- htdocs/core/class/interfaces.class.php
+++ htdocs/core/class/interfaces.class.php
@@ -14,15 +14,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *   \file		    htdocs/core/class/interfaces.class.php
  *   \ingroup		core
  *   \brief			Fichier de la classe de gestion des triggers
- *   \version		$Id: interfaces.class.php,v 1.8 2011/07/28 10:36:43 eldy Exp $
+ *   \version		$Id: interfaces.class.php,v 1.9 2011/07/31 23:45:13 eldy Exp $
  */
 
 
