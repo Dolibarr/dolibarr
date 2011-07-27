002b0f6976346fbc7d77ba2af0d5c5351ed131d4
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-27 23:50:52 +0000

    Fix: Message at bottom

--- htdocs/admin/system/index.php
+++ htdocs/admin/system/index.php
@@ -14,13 +14,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *  \file       htdocs/admin/system/index.php
  *  \brief      Home page of system information
- *  \version    $Id: index.php,v 1.49 2011/07/31 22:23:14 eldy Exp $
+ *  \version    $Id: index.php,v 1.48 2011/07/27 23:50:52 eldy Exp $
  */
 
 require("../../main.inc.php");
@@ -126,5 +127,5 @@ print '<br>';
 print info_admin($langs->trans("SystemInfoDesc")).'<br>';
 
 
-llxFooter('$Date: 2011/07/31 22:23:14 $ - $Revision: 1.49 $');
+llxFooter('$Date: 2011/07/27 23:50:52 $ - $Revision: 1.48 $');
 ?>
