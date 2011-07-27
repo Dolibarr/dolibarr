ed522f3050257030cb4c5b8a4b84e160a2ab1b92
Juanjo Menent <jmenent@2byte.es>
2011-07-27 13:19:38 +0000

    Trad: Improves the simplified payment MAIN_JS_ON_PAYMENT must be set

--- htdocs/comm/clients.php
+++ htdocs/comm/clients.php
@@ -14,15 +14,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *	\file       htdocs/comm/clients.php
  *	\ingroup    commercial, societe
  *	\brief      List of customers
- *	\version    $Id$
+ *	\version    $Id: clients.php,v 1.79 2011/08/03 00:46:26 eldy Exp $
  */
 
 require("../main.inc.php");
@@ -250,5 +249,5 @@ else
 
 $db->close();
 
-llxFooter('$Date$ - $Revision$');
+llxFooter('$Date: 2011/08/03 00:46:26 $ - $Revision: 1.79 $');
 ?>
