760c6b7ecd94bb3316b0b5c35573cfaa38b83292
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-27 23:37:01 +0000

    Fix: autoselect was not done

--- htdocs/adherents/class/adherentstats.class.php
+++ htdocs/adherents/class/adherentstats.class.php
@@ -14,14 +14,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *	\file       htdocs/adherents/class/adherentstats.class.php
  *	\ingroup    member
  *	\brief      Fichier de la classe de gestion des stats des adhérents
- *	\version    $Id: adherentstats.class.php,v 1.3 2011/08/03 00:45:44 eldy Exp $
+ *	\version    $Id: adherentstats.class.php,v 1.2 2011/07/04 10:30:01 eldy Exp $
  */
 
 include_once DOL_DOCUMENT_ROOT . "/core/class/stats.class.php";
