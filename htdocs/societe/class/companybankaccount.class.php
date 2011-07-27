49fc75d7a2b45fea04ca07c485e070f66e33ae66
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-27 23:50:52 +0000

    Fix: Message at bottom

--- htdocs/societe/class/companybankaccount.class.php
+++ htdocs/societe/class/companybankaccount.class.php
@@ -13,15 +13,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
  * 		\files		htdocs/societe/class/companybankaccount.class.php
  *		\ingroup    societe
  *		\brief      File of class to manage bank accounts description of third parties
- *   	\version	$Id: companybankaccount.class.php,v 1.10 2011/07/29 20:47:35 eldy Exp $
+ *   	\version	$Id: companybankaccount.class.php,v 1.11 2011/07/31 23:22:58 eldy Exp $
  */
 
 require_once(DOL_DOCUMENT_ROOT ."/compta/bank/class/account.class.php");
