26f030596966262cf6e08a7f358e28dec7c90eda
Juanjo Menent <jmenent@2byte.es>
2011-07-28 15:09:53 +0000

    Add Mexico society types

--- htdocs/compta/sociales/class/chargesociales.class.php
+++ htdocs/compta/sociales/class/chargesociales.class.php
@@ -13,15 +13,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *      \file       htdocs/compta/sociales/class/chargesociales.class.php
  *		\ingroup    facture
  *		\brief      Fichier de la classe des charges sociales
- *		\version    $Id$
+ *		\version    $Id: chargesociales.class.php,v 1.14 2011/08/03 00:46:38 eldy Exp $
  */
 
 require_once(DOL_DOCUMENT_ROOT."/core/class/commonobject.class.php");
