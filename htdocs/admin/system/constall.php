ed8e195569ab0e4035a75ffef95a391c40e97073
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-28 23:05:13 +0000

    Work on aps package

--- htdocs/admin/system/constall.php
+++ htdocs/admin/system/constall.php
@@ -14,14 +14,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *		\file 		htdocs/admin/system/constall.php
  *		\brief      Page to show all Dolibarr setup (config file and database constants)
- *		\version    $Id: constall.php,v 1.23 2011/07/30 10:23:26 eldy Exp $
+ *		\version    $Id: constall.php,v 1.24 2011/07/31 22:23:14 eldy Exp $
  */
 
 require("../../main.inc.php");
