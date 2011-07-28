558babcc799d3272a798e32d95cf5db74c0fec8e
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-28 18:34:45 +0000

    Add barbaros info

--- htdocs/compta/journal/index.php
+++ htdocs/compta/journal/index.php
@@ -13,7 +13,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 require("../../main.inc.php");
@@ -64,5 +65,5 @@ $somethingshown=$myobject->showLinkedObjectBlock();
 
 // End of page
 $db->close();
-llxFooter('$Date: 2011/08/03 00:46:37 $ - $Revision: 1.4 $');
+llxFooter('$Date$ - $Revision$');
 ?>
