4f58d73bedcf7de6823d4e5176f8d361f6a09534
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-29 20:55:19 +0000

    Fix: Trans

--- htdocs/install/mysql/tables/llx_c_type_contact.sql
+++ htdocs/install/mysql/tables/llx_c_type_contact.sql
@@ -13,8 +13,7 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
 --
 -- Defini les types de contact d'un element sert de reference pour
 -- la table llx_element_contact
@@ -24,7 +23,7 @@
 -- Libelle est un texte decrivant le type de contact.
 -- active precise si cette valeur est 'active' ou 'archive'.
 --
--- $Id: llx_c_type_contact.sql,v 1.4 2011/06/28 07:12:41 eldy Exp $
+-- $Id: llx_c_type_contact.sql,v 1.5 2011/08/03 01:25:41 eldy Exp $
 -- ========================================================================
 
 
