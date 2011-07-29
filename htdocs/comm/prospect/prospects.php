19a635d1f11d31859db5af0c6821f311cfc25621
Juanjo Menent <jmenent@2byte.es>
2011-07-29 10:49:58 +0000

    task #10501: Can use point of sale with different bank accounts

--- htdocs/comm/prospect/prospects.php
+++ htdocs/comm/prospect/prospects.php
@@ -15,15 +15,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *	\file       htdocs/comm/prospect/prospects.php
  *	\ingroup    prospect
  *	\brief      Page to list prospects
- *	\version    $Id$
+ *	\version    $Id: prospects.php,v 1.85 2011/08/03 00:46:27 eldy Exp $
  */
 
 require("../../main.inc.php");
@@ -427,5 +426,5 @@ else
 
 $db->close();
 
-llxFooter('$Date$ - $Revision$');
+llxFooter('$Date: 2011/08/03 00:46:27 $ - $Revision: 1.85 $');
 ?>
