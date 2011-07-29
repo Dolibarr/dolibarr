546da0ea4cf2e92081a0b34929ea60e1fe8ec07f
Juanjo Menent <jmenent@2byte.es>
2011-07-29 14:15:29 +0000

    Fix: Missing translation Exit make a logout

--- htdocs/boutique/index.php
+++ htdocs/boutique/index.php
@@ -13,15 +13,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
- \file       htdocs/boutique/index.php
- \ingroup    boutique
- \brief      Page accueil zone boutique
- \version    $Id$
+ *	\file       htdocs/boutique/index.php
+ *	\ingroup    boutique
+ *	\brief      Page accueil zone boutique
+ *	\version    $Id: index.php,v 1.17 2011/08/03 01:17:17 eldy Exp $
  */
 
 require("../main.inc.php");
@@ -239,5 +238,5 @@ print '</tr></table>';
 
 $dbosc->close();
 
-llxFooter('$Date$ - $Revision$');
+llxFooter('$Date: 2011/08/03 01:17:17 $ - $Revision: 1.17 $');
 ?>
