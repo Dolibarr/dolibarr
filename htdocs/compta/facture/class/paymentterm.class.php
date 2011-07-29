c8e09a4349fbca65aa54125cdd96674da6f287c5
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-29 20:47:34 +0000

    Fix: Uniformize error management

--- htdocs/compta/facture/class/paymentterm.class.php
+++ htdocs/compta/facture/class/paymentterm.class.php
@@ -12,14 +12,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *      \file       compta/facture/class/paymentterm.class.php
  *      \ingroup    facture
  *      \brief      This file is an example for a CRUD class file (Create/Read/Update/Delete)
- *		\version    $Id: paymentterm.class.php,v 1.5 2011/08/03 00:46:25 eldy Exp $
+ *		\version    $Id$
  *		\author		Put author name here
  *		\remarks	Initialy built by build_class_from_table on 2010-09-06 00:33
  */
