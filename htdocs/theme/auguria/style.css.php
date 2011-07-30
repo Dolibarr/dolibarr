7fee384a1f3ba3b950a72b235e1223a6610621bd
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-30 10:23:24 +0000

    Fix: Code could not works for debian packages

--- htdocs/theme/auguria/style.css.php
+++ htdocs/theme/auguria/style.css.php
@@ -14,13 +14,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *		\file       htdocs/theme/auguria/style.css.php
  *		\brief      Fichier de style CSS du theme auguria
- *		\version    $Id: style.css.php,v 1.92 2011/07/31 23:22:04 eldy Exp $
+ *		\version    $Id: style.css.php,v 1.91 2011/07/28 20:54:02 eldy Exp $
  */
 
 //if (! defined('NOREQUIREUSER')) define('NOREQUIREUSER','1');	// Not disabled cause need to load personalized language
