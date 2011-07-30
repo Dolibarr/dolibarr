663b9edc9b703399d15f75a14aa26b87bff1ef7b
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-30 10:23:24 +0000

    Fix: Code could not works for debian packages

--- htdocs/paypal/lib/paypalfunctions.lib.php
+++ htdocs/paypal/lib/paypalfunctions.lib.php
@@ -13,13 +13,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**	    \file       htdocs/paypal/lib/paypalfunctions.lib.php
  *		\ingroup    paypal
  *		\brief      Page with Paypal init var.
- *		\version    $Id: paypalfunctions.lib.php,v 1.8 2011/08/03 01:34:59 eldy Exp $
+ *		\version    $Id$
  */
 
 if (session_id() == "") session_start();
