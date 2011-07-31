b04737267fa5a1cea856d7e63c3755aa246274c8
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 21:24:20 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -17,14 +17,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *	\file       htdocs/product/class/product.class.php
  *	\ingroup    produit
  *	\brief      Fichier de la classe des produits predefinis
- *	\version    $Id: product.class.php,v 1.48 2011/07/31 23:24:03 eldy Exp $
+ *	\version    $Id: product.class.php,v 1.47 2011/07/29 20:47:35 eldy Exp $
  */
 require_once(DOL_DOCUMENT_ROOT ."/core/class/commonobject.class.php");
 
