3812d9eccdafe2c9a2b39abbc69530c89be1001d
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 23:19:04 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/admin/tools/export.php
+++ htdocs/admin/tools/export.php
@@ -12,13 +12,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *		\file 		htdocs/admin/tools/export.php
  *		\brief      Page to export a database into a dump file
- *		\version    $Id: export.php,v 1.45 2011/08/03 00:45:43 eldy Exp $
+ *		\version    $Id$
  */
 
 require("../../main.inc.php");
@@ -316,5 +317,5 @@ print '<br>';
 
 $time_end = time();
 
-llxFooter('$Date: 2011/08/03 00:45:43 $ - $Revision: 1.45 $');
+llxFooter('$Date$ - $Revision$');
 ?>
