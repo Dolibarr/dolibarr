a30309704b505ce9d0b986260b9a0100516be62a
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 23:45:11 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/boutique/osc_master.inc.php
+++ htdocs/boutique/osc_master.inc.php
@@ -12,14 +12,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
- *       \file       htdocs/boutique/osc_master.inc.php
- *       \brief      Fichier de preparation de l'environnement Dolibarr pour OSCommerce
- *       \version    $Id: osc_master.inc.php,v 1.6 2011/08/03 01:17:17 eldy Exp $
- */
+        \file       htdocs/boutique/osc_master.inc.php
+        \brief      Fichier de preparation de l'environnement Dolibarr pour OSCommerce
+        \version    $Id$
+*/
 
 require_once(DOL_DOCUMENT_ROOT ."/lib/databases/".$conf->db->type.".lib.php");
 
--- htdocs/comm/remise.php
+++ htdocs/comm/remise.php
@@ -13,14 +13,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *	    \file       htdocs/comm/remise.php
  *      \ingroup    societe
  *		\brief      Onglet remise de la societe
- *		\version    $Id: remise.php,v 1.35 2011/08/03 00:46:27 eldy Exp $
+ *		\version    $Id$
  */
 
 require("../main.inc.php");
@@ -180,5 +181,5 @@ if ($_socid > 0)
 
 $db->close();
 
-llxFooter('$Date: 2011/08/03 00:46:27 $ - $Revision: 1.35 $');
+llxFooter('$Date$ - $Revision$');
 ?>
