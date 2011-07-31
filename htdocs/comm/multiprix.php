f34e254df49963e2c1beca8b716694061273d01b
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 22:21:57 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/comm/multiprix.php
+++ htdocs/comm/multiprix.php
@@ -14,15 +14,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *	\file       htdocs/comm/multiprix.php
  *	\ingroup    societe
  *	\brief      Onglet choix du niveau de prix
- *	\version    $Id$
+ *	\version    $Id: multiprix.php,v 1.21 2011/08/03 00:46:27 eldy Exp $
  */
 
 require("../main.inc.php");
@@ -171,5 +170,5 @@ if ($_socid > 0)
 
 $db->close();
 
-llxFooter('$Date$ - $Revision$');
+llxFooter('$Date: 2011/08/03 00:46:27 $ - $Revision: 1.21 $');
 ?>
