139b6857278edd7344c9a12a931ecaa7a4c9743c
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 23:19:04 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/install/mysql/tables/llx_usergroup_rights.key.sql
+++ htdocs/install/mysql/tables/llx_usergroup_rights.key.sql
@@ -12,9 +12,10 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program. If not, see <http://www.gnu.org/licenses/>.
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 --
--- $Id: llx_usergroup_rights.key.sql,v 1.3 2011/08/03 01:25:36 eldy Exp $
+-- $Id$
 -- ===========================================================================
 
 
