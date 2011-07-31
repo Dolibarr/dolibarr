0cd61531f6f35c56c0bc2b21a9f9a08049217fd2
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 12:31:00 +0000

    Fix: Duplicate entries

--- htdocs/boutique/client/index.php
+++ htdocs/boutique/client/index.php
@@ -13,16 +13,17 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * $Id: index.php,v 1.17 2011/08/03 00:45:42 eldy Exp $
+ * $Id$
  */
 
 /**
 	    \file       htdocs/boutique/client/index.php
 		\ingroup    boutique
 		\brief      Page gestion client OSCommerce
-		\version    $Revision: 1.17 $
+		\version    $Revision$
 */
 
 require("../../main.inc.php");
@@ -90,5 +91,5 @@ else
 
 $dbosc->close();
 
-llxFooter('$Date: 2011/08/03 00:45:42 $ - $Revision: 1.17 $');
+llxFooter('$Date$ - $Revision$');
 ?>
--- htdocs/comm/fiche.php
+++ htdocs/comm/fiche.php
@@ -18,14 +18,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *       \file       htdocs/comm/fiche.php
  *       \ingroup    commercial, compta
  *       \brief      Page to show customer card of a third party
- *       \version    $Id: fiche.php,v 1.282 2011/08/03 00:46:27 eldy Exp $
+ *       \version    $Id: fiche.php,v 1.281 2011/07/18 10:22:22 hregis Exp $
  */
 
 require("../main.inc.php");
@@ -891,5 +892,5 @@ else
 $db->close();
 
 
-llxFooter('$Date: 2011/08/03 00:46:27 $ - $Revision: 1.282 $');
+llxFooter('$Date: 2011/07/18 10:22:22 $ - $Revision: 1.281 $');
 ?>
