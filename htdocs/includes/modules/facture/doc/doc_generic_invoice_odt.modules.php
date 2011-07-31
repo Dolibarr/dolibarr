105f2278bc978728a225da38405f9bc2b9438f6d
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 23:54:11 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/includes/modules/facture/doc/doc_generic_invoice_odt.modules.php
+++ htdocs/includes/modules/facture/doc/doc_generic_invoice_odt.modules.php
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * or see http://www.gnu.org/
  */
 
@@ -22,7 +21,7 @@
  *	\ingroup    societe
  *	\brief      File of class to build ODT documents for third parties
  *	\author	    Laurent Destailleur
- *	\version    $Id: doc_generic_invoice_odt.modules.php,v 1.19 2011/07/21 22:54:36 eldy Exp $
+ *	\version    $Id: doc_generic_invoice_odt.modules.php,v 1.20 2011/07/31 23:28:15 eldy Exp $
  */
 
 require_once(DOL_DOCUMENT_ROOT."/includes/modules/facture/modules_facture.php");
