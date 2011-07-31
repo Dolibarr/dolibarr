4660c9f709bd63ddb619c93f46cac09316a97438
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 23:24:38 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/install/inc.php
+++ htdocs/install/inc.php
@@ -15,14 +15,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**	    \file       htdocs/install/inc.php
  * 		\ingroup	core
  *		\brief      File that define environment for install pages
- *		\version    $Id: inc.php,v 1.136 2011/07/30 10:23:25 eldy Exp $
+ *		\version    $Id: inc.php,v 1.137 2011/07/31 23:26:18 eldy Exp $
  */
 
 define('DOL_VERSION','3.1.0-beta');	// Also defined in htdocs/master.inc.php (Ex: x.y.z-alpha, x.y.z)
@@ -75,10 +74,12 @@ $conffiletoshowshort = "conf.php";
 # Define localization of conf file
 $conffile = "../conf/conf.php";
 $conffiletoshow = "htdocs/conf/conf.php";
-# For debian like systems
+$lockfile = '../../install.lock';
+# For debian/redhat like systems
 #$conffile = "/etc/dolibarr/conf.php";
 #$conffiletoshow = "/etc/dolibarr/conf.php";
-
+#$lockfile = '/etc/dolibarr/install.lock';    # Web has no permission to write here
+$lockfile = '../../install.lock';
 
 if (! defined('DONOTLOADCONF') && file_exists($conffile))
 {
@@ -178,7 +179,7 @@ if (preg_match('/install.lock/i',$_SERVER["SCRIPT_FILENAME"]))
     }
 	exit;
 }
-if (file_exists('../../install.lock'))
+if (file_exists($lockfile))
 {
 	print 'Install pages have been disabled for security reason (by lock file install.lock into dolibarr root directory).<br>';
 	if (! empty($dolibarr_main_url_root))
