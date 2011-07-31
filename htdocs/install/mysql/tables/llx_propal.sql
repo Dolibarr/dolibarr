48c459870120176dd74a5da87c576213dd6d3528
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 23:54:11 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/install/mysql/tables/llx_propal.sql
+++ htdocs/install/mysql/tables/llx_propal.sql
@@ -15,9 +15,10 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program. If not, see <http://www.gnu.org/licenses/>.
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 --
--- $Id: llx_propal.sql,v 1.14 2011/08/03 01:25:38 eldy Exp $
+-- $Id: llx_propal.sql,v 1.13 2011/07/17 21:35:12 hregis Exp $
 -- ===================================================================
 
 create table llx_propal
