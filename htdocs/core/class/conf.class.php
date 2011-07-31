e6c16f6b8bf463bfeefad6a867abad691804cbde
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 12:31:00 +0000

    Fix: Duplicate entries

--- htdocs/core/class/conf.class.php
+++ htdocs/core/class/conf.class.php
@@ -16,7 +16,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
@@ -24,7 +25,7 @@
  *	\ingroup		core
  *  \brief      	File of class to manage storage of current setup
  *  				Config is stored into file conf.php
- *  \version    	$Id: conf.class.php,v 1.63 2011/07/31 23:45:13 eldy Exp $
+ *  \version    	$Id: conf.class.php,v 1.62 2011/07/30 10:23:26 eldy Exp $
  */
 
 
