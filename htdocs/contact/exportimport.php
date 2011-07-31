d164f833dbfffe829bb7d1d7596b80c3b149a344
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 23:54:11 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/contact/exportimport.php
+++ htdocs/contact/exportimport.php
@@ -13,15 +13,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *	\file       htdocs/contact/exportimport.php
  *	\ingroup    societe
  *	\brief      Onglet exports-imports d'un contact
- *	\version    $Id$
+ *	\version    $Id: exportimport.php,v 1.29 2011/07/31 23:54:12 eldy Exp $
  */
 
 require("../main.inc.php");
@@ -107,5 +106,5 @@ print '</a>';
 
 $db->close();
 
-llxFooter('$Date$ - $Revision$');
+llxFooter('$Date: 2011/07/31 23:54:12 $ - $Revision: 1.29 $');
 ?>
--- htdocs/contact/fiche.php
+++ htdocs/contact/fiche.php
@@ -16,15 +16,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *       \file       htdocs/contact/fiche.php
  *       \ingroup    societe
  *       \brief      Card of a contact
- *       \version    $Id: fiche.php,v 1.221 2011/07/04 08:00:52 eldy Exp $
+ *       \version    $Id: fiche.php,v 1.222 2011/07/31 23:54:12 eldy Exp $
  */
 
 require("../main.inc.php");
@@ -974,5 +973,5 @@ else
 
 $db->close();
 
-llxFooter('$Date: 2011/07/04 08:00:52 $ - $Revision: 1.221 $');
+llxFooter('$Date: 2011/07/31 23:54:12 $ - $Revision: 1.222 $');
 ?>
