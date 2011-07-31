56f5030d044c7d418f9fbe8f6b03d437beec827f
Laurent Destailleur <eldy@users.sourceforge.net>
2011-07-31 17:14:02 +0000

    Work on 3.1 packaging

--- build/makepack-dolibarr.pl
+++ build/makepack-dolibarr.pl
@@ -2,7 +2,7 @@
 #----------------------------------------------------------------------------
 # \file         build/makepack-dolibarr.pl
 # \brief        Dolibarr package builder (tgz, zip, rpm, deb, exe, aps)
-# \version      $Id: makepack-dolibarr.pl,v 1.125 2011/07/31 15:31:32 eldy Exp $
+# \version      $Id: makepack-dolibarr.pl,v 1.126 2011/07/31 17:14:03 eldy Exp $
 # \author       (c)2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
 #----------------------------------------------------------------------------
 
@@ -48,7 +48,7 @@ if (-d "/usr/src/RPM") {
 
 
 use vars qw/ $REVISION $VERSION /;
-$REVISION='$Revision: 1.125 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
+$REVISION='$Revision: 1.126 $'; $REVISION =~ /\s(.*)\s/; $REVISION=$1;
 $VERSION="1.0 (build $REVISION)";
 
 
@@ -414,7 +414,8 @@ if ($nboftargetok) {
     	}
     
     	if ($target eq 'RPM') {                 # Linux only
-    		$ARCH='i386';
+    		#$ARCH='i386';
+    		$ARCH='noarch';
 			if ($RPMDIR eq "") { $RPMDIR=$ENV{'HOME'}."/rpmbuild"; }
            	$newbuild = $BUILD;
             $newbuild =~ s/(dev|alpha)/0/gi;				# dev
@@ -439,6 +440,7 @@ if ($nboftargetok) {
 
  			print "Remove other files\n";
             $ret=`rm -f $BUILDROOT/$FILENAMETGZ2/htdocs/includes/barcode/php-barcode/genbarcode/genbarcode`;
+            $ret=`rm -fr $BUILDROOT/$PROJECT.tmp/$PROJECT/README`;
             $ret=`rm -fr $BUILDROOT/$PROJECT.tmp/$PROJECT/build/aps`;
             $ret=`rm -fr $BUILDROOT/$PROJECT.tmp/$PROJECT/build/deb`;
             $ret=`rm -fr $BUILDROOT/$PROJECT.tmp/$PROJECT/build/dmg`;
@@ -476,6 +478,10 @@ if ($nboftargetok) {
    		    print "Move $RPMDIR/RPMS/".$ARCH."/".$FILENAMETGZ2."-".$RPMSUBVERSION.".".$ARCH.".rpm into $DESTI/".$FILENAMETGZ2."-".$RPMSUBVERSION.".".$ARCH.".rpm\n";
    		    $cmd="mv \"$RPMDIR/RPMS/".$ARCH."/".$FILENAMETGZ2."-".$RPMSUBVERSION.".".$ARCH.".rpm\" \"$DESTI/".$FILENAMETGZ2."-".$RPMSUBVERSION.".".$ARCH.".rpm\"";
     		$ret=`$cmd`;
+   		    print "Move $RPMDIR/SRPMS/".$FILENAMETGZ2."-".$RPMSUBVERSION.".src.rpm into $DESTI/".$FILENAMETGZ2."-".$RPMSUBVERSION.".src.rpm\n";
+   		    $cmd="mv \"$RPMDIR/SRPMS/".$FILENAMETGZ2."-".$RPMSUBVERSION.".src.rpm\" \"$DESTI/".$FILENAMETGZ2."-".$RPMSUBVERSION.".src.rpm\"";
+    		$ret=`$cmd`;
+
     		next;
     	}
     	
@@ -679,7 +685,7 @@ if ($nboftargetok) {
             $ret=`cp -f  "$SOURCE/build/deb/compat"         "$BUILDROOT/$PROJECT.tmp/debian/compat"`;
             $ret=`cp -f  "$SOURCE/build/deb/format"         "$BUILDROOT/$PROJECT.tmp/debian/source/format"`;
             $ret=`cp -fr "$SOURCE/build/deb/po"             "$BUILDROOT/$PROJECT.tmp/debian/po"`;
-			$ret=`rm -fr "$SOURCE/build/deb/po/CVS"`;
+			$ret=`rm -fr "$BUILDROOT/$PROJECT.tmp/debian/po/CVS"`;
             # Add files also required to build binary package with dpkg-buildpackages
             $ret=`cp -f  "$SOURCE/build/deb/config"         "$BUILDROOT/$PROJECT.tmp/debian"`;
             $ret=`cp -f  "$SOURCE/build/deb/postinst"       "$BUILDROOT/$PROJECT.tmp/debian"`;
@@ -701,11 +707,14 @@ if ($nboftargetok) {
             {
                 print "Move *_all.deb to $DESTI\n";
                 $ret=`mv $BUILDROOT/*_all.deb "$DESTI/"`;
+                $ret=`mv $BUILDROOT/*.dsc "$DESTI/"`;
+                $ret=`mv $BUILDROOT/*.tar.gz "$DESTI/"`;
             }
             else
             {
                 print "Move *_all.deb to $DESTI\n";
-                $ret=`mv $BUILDROOT/*_all.deb "$DESTI/"`;
+                $ret=`mv $BUILDROOT/*.dsc "$DESTI/"`;
+                $ret=`mv $BUILDROOT/*.tar.gz "$DESTI/"`;
             }
         	next;
         }
