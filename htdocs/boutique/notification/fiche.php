4d5b43d9424d78ec37f8b4522fb1d0c1cc0fe3eb
Regis Houssin <regis@dolibarr.fr>
2011-08-01 13:26:21 +0000

    Fix: rename inc to includes

--- htdocs/boutique/notification/fiche.php
+++ htdocs/boutique/notification/fiche.php
@@ -13,14 +13,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
 	    \file       htdocs/boutique/notification/fiche.php
 		\ingroup    boutique
 		\brief      Page fiche notification OS Commerce
-		\version    $Id: fiche.php,v 1.17 2011/08/03 00:45:42 eldy Exp $
+		\version    $Id$
 */
 
 require("../../main.inc.php");
@@ -149,5 +150,5 @@ print '</div>';
 
 $dbosc->close();
 
-llxFooter('$Date: 2011/08/03 00:45:42 $ - $Revision: 1.17 $');
+llxFooter('$Date$ - $Revision$');
 ?>
