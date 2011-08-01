2d8c2dcf67cbf9fcacf7916bdd3c95356aa669e1
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-01 18:22:25 +0000

    Work on debian package

--- htdocs/comm/propal/class/propal.class.php
+++ htdocs/comm/propal/class/propal.class.php
@@ -20,7 +20,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
@@ -29,7 +30,7 @@
  *	\author     Rodolphe Qiedeville
  *	\author	    Eric Seigne
  *	\author	    Laurent Destailleur
- *	\version    $Id: propal.class.php,v 1.110 2011/08/03 00:46:39 eldy Exp $
+ *	\version    $Id: propal.class.php,v 1.109 2011/06/30 13:27:21 hregis Exp $
  */
 
 require_once(DOL_DOCUMENT_ROOT ."/core/class/commonobject.class.php");
