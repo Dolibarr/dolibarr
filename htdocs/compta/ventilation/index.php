e0b6d5a95427f570abd4253a6181e4f1d7dad9df
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-01 19:10:27 +0000

    Work on debian package

--- htdocs/compta/ventilation/index.php
+++ htdocs/compta/ventilation/index.php
@@ -13,16 +13,17 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * $Id: index.php,v 1.13 2011/08/03 00:46:37 eldy Exp $
+ * $Id$
  */
 
 /**
    \file       htdocs/compta/ventilation/index.php
     \ingroup    compta
      \brief      Page accueil ventilation
-      \version    $Revision: 1.13 $
+      \version    $Revision$
 */
 
 require('../../main.inc.php');
@@ -94,6 +95,6 @@ print "</table>\n";
 
 print '</td></tr></table>';
 
-llxFooter('$Date: 2011/08/03 00:46:37 $ - $Revision: 1.13 $');
+llxFooter('$Date$ - $Revision$');
 
 ?>
