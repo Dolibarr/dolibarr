176bbc676cfdac6e13edee69511e1fec539005bf
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-01 18:58:37 +0000

    Work on debian package

--- htdocs/install/mysql/tables/llx_societe_remise.sql
+++ htdocs/install/mysql/tables/llx_societe_remise.sql
@@ -13,9 +13,10 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program. If not, see <http://www.gnu.org/licenses/>.
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 --
--- $Id: llx_societe_remise.sql,v 1.5 2011/08/03 01:25:27 eldy Exp $
+-- $Id$
 --
 -- Historique evolution de la remise relative des tiers
 -- ========================================================================
