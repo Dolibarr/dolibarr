d1f11f29dcae4da9abeb179080a6d24a0e188f10
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-01 00:38:49 +0000

    Fix: bug #33900 : Third party name reset after the autocompletion of city name

--- htdocs/compta/paiement/avalider.php
+++ htdocs/compta/paiement/avalider.php
@@ -13,14 +13,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
         \file       htdocs/compta/paiement/avalider.php
         \ingroup    compta
         \brief      Page liste des paiements a valider des factures clients
-        \version    $Id: avalider.php,v 1.13 2011/08/03 00:46:34 eldy Exp $
+        \version    $Id$
 */
 
 require("../../main.inc.php");
@@ -120,5 +121,5 @@ if ($resql)
 
 $db->close();
 
-llxFooter('$Date: 2011/08/03 00:46:34 $ - $Revision: 1.13 $');
+llxFooter('$Date$ - $Revision$');
 ?>
