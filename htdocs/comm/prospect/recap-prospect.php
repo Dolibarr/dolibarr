fef34412d59a4371c1c0db9ca0f81a142f82e6ac
Regis Houssin <regis@dolibarr.fr>
2011-08-01 12:25:14 +0000

    New: possibility to use includes/modules in external modules dir

--- htdocs/comm/prospect/recap-prospect.php
+++ htdocs/comm/prospect/recap-prospect.php
@@ -13,14 +13,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *   	\file       htdocs/fourn/recap-fourn.php
  *		\ingroup    fournisseur
  *		\brief      Page de fiche recap fournisseur
- *		\version    $Id: recap-prospect.php,v 1.9 2011/08/03 00:46:28 eldy Exp $
+ *		\version    $Id$
  */
 
 require("../../main.inc.php");
@@ -92,5 +93,5 @@ else
 
 $db->close();
 
-llxFooter('$Date: 2011/08/03 00:46:28 $ - $Revision: 1.9 $');
+llxFooter('$Date$ - $Revision$');
 ?>
--- htdocs/compta/resultat/clientfourn.php
+++ htdocs/compta/resultat/clientfourn.php
@@ -14,13 +14,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *  \file        htdocs/compta/resultat/clientfourn.php
  *	\brief       Page reporting
- *  \version     $Id: clientfourn.php,v 1.63 2011/08/03 00:46:31 eldy Exp $
+ *  \version     $Id: clientfourn.php,v 1.62 2011/07/20 19:03:33 eldy Exp $
  */
 
 require('../../main.inc.php');
@@ -688,5 +689,5 @@ print '<br>';
 
 $db->close();
 
-llxFooter('$Date: 2011/08/03 00:46:31 $ - $Revision: 1.63 $');
+llxFooter('$Date: 2011/07/20 19:03:33 $ - $Revision: 1.62 $');
 ?>
