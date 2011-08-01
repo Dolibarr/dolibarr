3c05f36ebba59e04daea312b924f7a730830f1b8
Regis Houssin <regis@dolibarr.fr>
2011-08-01 12:25:14 +0000

    New: possibility to use includes/modules in external modules dir

--- htdocs/commande/stats/month.php
+++ htdocs/commande/stats/month.php
@@ -14,14 +14,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *	    \file       htdocs/commande/stats/month.php
  *      \ingroup    commande
  *		\brief      Page des stats commandes par mois
- *		\version    $Id: month.php,v 1.34 2011/08/03 00:46:39 eldy Exp $
+ *		\version    $Id$
  */
 require("../../main.inc.php");
 require_once(DOL_DOCUMENT_ROOT."/commande/class/commande.class.php");
@@ -191,5 +192,5 @@ print '</td></tr></table>';
 
 $db->close();
 
-llxFooter('$Date: 2011/08/03 00:46:39 $ - $Revision: 1.34 $');
+llxFooter('$Date$ - $Revision$');
 ?>
