c1b3b718f77a6ffd67c445bdb079fb3c447a452e
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 01:03:45 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/adherents/cartes/carte.php
+++ htdocs/adherents/cartes/carte.php
@@ -14,15 +14,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *	\file 		htdocs/adherents/cartes/carte.php
  *	\ingroup    member
  *	\brief      Page to output members business cards
- *	\version    $Id$
+ *	\version    $Id: carte.php,v 1.42 2011/08/03 00:45:46 eldy Exp $
  */
 require("../../main.inc.php");
 require_once(DOL_DOCUMENT_ROOT."/lib/files.lib.php");
@@ -88,7 +87,7 @@ if ((empty($foruserid) && empty($foruserlogin) && empty($mode)) || $mesg)
     print '</form>';
     print '<br>';
 
-    llxFooter('$Date$ - $Revision$');
+    llxFooter('$Date: 2011/08/03 00:45:46 $ - $Revision: 1.42 $');
 }
 else
 {
@@ -211,7 +210,7 @@ else
     {
     	dol_print_error($db);
 
-    	llxFooter('$Date$ - $Revision$');
+    	llxFooter('$Date: 2011/08/03 00:45:46 $ - $Revision: 1.42 $');
     }
 }
 
--- htdocs/comm/prospect/recap-prospect.php
+++ htdocs/comm/prospect/recap-prospect.php
@@ -13,15 +13,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *   	\file       htdocs/fourn/recap-fourn.php
  *		\ingroup    fournisseur
  *		\brief      Page de fiche recap fournisseur
- *		\version    $Id$
+ *		\version    $Id: recap-prospect.php,v 1.9 2011/08/03 00:46:28 eldy Exp $
  */
 
 require("../../main.inc.php");
@@ -93,5 +92,5 @@ else
 
 $db->close();
 
-llxFooter('$Date$ - $Revision$');
+llxFooter('$Date: 2011/08/03 00:46:28 $ - $Revision: 1.9 $');
 ?>
