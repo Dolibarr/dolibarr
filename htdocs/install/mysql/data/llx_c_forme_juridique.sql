69e251e6d4569f228dcc1c585369de7ee7e927e1
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 01:22:37 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/install/mysql/data/llx_c_forme_juridique.sql
+++ htdocs/install/mysql/data/llx_c_forme_juridique.sql
@@ -18,10 +18,9 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
 --
--- $Id: llx_c_forme_juridique.sql,v 1.5 2011/07/28 15:09:53 simnandez Exp $
+-- $Id: llx_c_forme_juridique.sql,v 1.6 2011/08/03 01:25:45 eldy Exp $
 --
 
 --
