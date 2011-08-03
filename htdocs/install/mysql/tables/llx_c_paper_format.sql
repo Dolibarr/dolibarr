be803af4982d18599a2912b44c3c7950d0c718f1
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 01:22:37 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/install/mysql/tables/llx_c_paper_format.sql
+++ htdocs/install/mysql/tables/llx_c_paper_format.sql
@@ -12,10 +12,9 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
 --
--- $Id: llx_c_paper_format.sql,v 1.3 2011/06/29 11:22:35 eldy Exp $
+-- $Id: llx_c_paper_format.sql,v 1.4 2011/08/03 01:25:37 eldy Exp $
 -- ========================================================================
 
 create table llx_c_paper_format
