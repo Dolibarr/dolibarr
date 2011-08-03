79befeb86fd31b4a96f25bb824620a9d2ea36f05
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 01:13:30 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/install/etape5.php
+++ htdocs/install/etape5.php
@@ -16,15 +16,14 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *       \file      htdocs/install/etape5.php
  *	 	 \ingroup	install
  *       \brief     Last page of upgrade or install process
- *       \version   $Id: etape5.php,v 1.106 2011/07/31 13:28:45 eldy Exp $
+ *       \version   $Id: etape5.php,v 1.107 2011/07/31 23:26:19 eldy Exp $
  */
 
 include_once("./inc.php");
