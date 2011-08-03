ed25f0b3a3efee95a4c78f4153b92a5d0b277128
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 00:45:22 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/comm/propal/class/propal.class.php
+++ htdocs/comm/propal/class/propal.class.php
@@ -20,8 +20,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -30,7 +29,7 @@
  *	\author     Rodolphe Qiedeville
  *	\author	    Eric Seigne
  *	\author	    Laurent Destailleur
- *	\version    $Id: propal.class.php,v 1.109 2011/06/30 13:27:21 hregis Exp $
+ *	\version    $Id: propal.class.php,v 1.110 2011/08/03 00:46:39 eldy Exp $
  */
 
 require_once(DOL_DOCUMENT_ROOT ."/core/class/commonobject.class.php");
