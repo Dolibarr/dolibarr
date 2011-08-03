14e3de02088187bffe1bb6af7355445339805e30
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 01:22:37 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/install/etape1.php
+++ htdocs/install/etape1.php
@@ -16,14 +16,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /**
  *		\file       htdocs/install/etape1.php
  *		\ingroup	install
  *		\brief      Build conf file on disk
- *		\version    $Id: etape1.php,v 1.136 2011/07/31 23:26:18 eldy Exp $
+ *		\version    $Id: etape1.php,v 1.135 2011/07/30 14:56:42 eldy Exp $
  */
 
 define('DONOTLOADCONF',1);	// To avoid loading conf by file inc.php
