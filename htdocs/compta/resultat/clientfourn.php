2114a19ac92995f2a3a7e2f201146216774bdab0
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 01:03:45 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/compta/resultat/clientfourn.php
+++ htdocs/compta/resultat/clientfourn.php
@@ -14,14 +14,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *  \file        htdocs/compta/resultat/clientfourn.php
  *	\brief       Page reporting
- *  \version     $Id: clientfourn.php,v 1.62 2011/07/20 19:03:33 eldy Exp $
+ *  \version     $Id: clientfourn.php,v 1.63 2011/08/03 00:46:31 eldy Exp $
  */
 
 require('../../main.inc.php');
@@ -689,5 +688,5 @@ print '<br>';
 
 $db->close();
 
-llxFooter('$Date: 2011/07/20 19:03:33 $ - $Revision: 1.62 $');
+llxFooter('$Date: 2011/08/03 00:46:31 $ - $Revision: 1.63 $');
 ?>
