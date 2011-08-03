344b688f0065712b386b4f51a66e183432a18647
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 00:45:22 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/compta/resultat/bilan.php
+++ htdocs/compta/resultat/bilan.php
@@ -12,14 +12,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**     \file   	htdocs/compta/resultat/bilan.php
  *      \ingroup    compta
  *      \brief  	Fichier page bilan compta
- *		\version	$Id$
+ *		\version	$Id: bilan.php,v 1.12 2011/08/03 00:46:31 eldy Exp $
  */
 
 require('../../main.inc.php');
@@ -52,5 +51,5 @@ print "Cet �tat n'est pas disponible.";
 
 $db->close();
 
-llxFooter("<em>Derni&egrave;re modification $Date$ r&eacute;vision $Revision$</em>");
+llxFooter("<em>Derni&egrave;re modification $Date: 2011/08/03 00:46:31 $ r&eacute;vision $Revision: 1.12 $</em>");
 ?>
--- htdocs/compta/resultat/index.php
+++ htdocs/compta/resultat/index.php
@@ -14,14 +14,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
  *       \file        htdocs/compta/resultat/index.php
  *       \brief       Page reporting resultat
- *       \version     $Id$
+ *       \version     $Id: index.php,v 1.49 2011/08/03 00:46:31 eldy Exp $
  */
 
 require('../../main.inc.php');
@@ -514,6 +513,6 @@ print "</table>";
 
 $db->close();
 
-llxFooter('$Date$ - $Revision$');
+llxFooter('$Date: 2011/08/03 00:46:31 $ - $Revision: 1.49 $');
 
 ?>
