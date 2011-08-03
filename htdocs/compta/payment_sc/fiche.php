9f1460245add4fb753181cc39e666d78ec566a2d
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 00:47:15 +0000

    Work on debian package

--- htdocs/compta/payment_sc/fiche.php
+++ htdocs/compta/payment_sc/fiche.php
@@ -15,8 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -24,7 +23,7 @@
  *		\ingroup    facture
  *		\brief      Onglet payment of a social contribution
  *		\remarks	Fichier presque identique a fournisseur/paiement/fiche.php
- *		\version    $Id$
+ *		\version    $Id: fiche.php,v 1.14 2011/08/03 00:46:36 eldy Exp $
  */
 
 require('../../main.inc.php');
@@ -327,5 +326,5 @@ print '</div>';
 
 $db->close();
 
-llxFooter('$Date$ - $Revision$');
+llxFooter('$Date: 2011/08/03 00:46:36 $ - $Revision: 1.14 $');
 ?>
