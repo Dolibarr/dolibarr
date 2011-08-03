e6832ae4cdc5bc8c6d17ebc64cadd353e01c5bf8
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 00:47:15 +0000

    Work on debian package

--- htdocs/includes/barcode/php-barcode/php-barcode.php
+++ htdocs/includes/barcode/php-barcode/php-barcode.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * PHP-Barcode 0.3pl1
- *
+
  * PHP-Barcode generates
  *   - Barcode-Images using libgd2 (png, jpg, gif)
  *   - HTML-Images (using 1x1 pixel and html-table)
@@ -14,24 +14,25 @@
  *     genbarcode can encode EAN-13, EAN-8, UPC, ISBN, 39, 128(a,b,c),
  *     I25, 128RAW, CBR, MSI, PLS
  *     genbarcode is available at www.ashberg.de/bar
- *
+
  * (C) 2001,2002,2003,2004 by Folke Ashberg <folke@ashberg.de>
- *
+
  * The newest version can be found at http://www.ashberg.de/bar
- *
+
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
  */
 
 
