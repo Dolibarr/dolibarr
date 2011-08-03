263241d2de1b3343c3f4295b2de85cbfe71d1ca0
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-03 01:22:37 +0000

    Fix: Avoid errors into rpm packages

--- htdocs/lib/ajax.lib.php
+++ htdocs/lib/ajax.lib.php
@@ -13,14 +13,15 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  * or see http://www.gnu.org/
  */
 
 /**
  *  \file		htdocs/lib/ajax.lib.php
  *  \brief		Page called by Ajax request for produts
- *  \version	$Id: ajax.lib.php,v 1.60 2011/07/31 23:25:18 eldy Exp $
+ *  \version	$Id: ajax.lib.php,v 1.59 2011/07/28 18:57:23 eldy Exp $
  */
 
 
