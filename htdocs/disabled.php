0d36d426a873f059caf49d5668c8e6527ee68995
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-08 15:51:13 +0000

    Fix: Missing copyright

--- htdocs/disabled.php
+++ htdocs/disabled.php
@@ -1,3 +1,27 @@
+<?php
+/* Copyright (C) 2011 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * or see http://www.gnu.org/
+ */
+
+/**
+ *	\file       htdocs/disabled.php
+ *  \brief      Page to use to replace index.php to disable all software
+ *  \version    $Id: disabled.php,v 1.4 2011/08/08 15:51:13 eldy Exp $
+ */
+?>
 <html>
 <body>
 This feature has been disabled on this test server.<br>
