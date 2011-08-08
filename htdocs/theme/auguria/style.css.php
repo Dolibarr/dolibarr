c93b825cdf288b8734dcf4291ff7a68201030f07
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-08 15:33:50 +0000

    New: Add focus

--- htdocs/theme/auguria/style.css.php
+++ htdocs/theme/auguria/style.css.php
@@ -20,7 +20,7 @@
 /**
  *		\file       htdocs/theme/auguria/style.css.php
  *		\brief      Fichier de style CSS du theme auguria
- *		\version    $Id: style.css.php,v 1.93 2011/08/08 15:19:28 eldy Exp $
+ *		\version    $Id: style.css.php,v 1.94 2011/08/08 15:33:50 eldy Exp $
  */
 
 //if (! defined('NOREQUIREUSER')) define('NOREQUIREUSER','1');	// Not disabled cause need to load personalized language
@@ -81,6 +81,9 @@ body {
 
 a:link, a:visited, a:hover, a:active { font-family: <?php print $fontlist ?>; font-weight: bold; color: #000000; text-decoration: none; }
 
+input:focus, textarea:focus, button:focus, select:focus {
+    box-shadow: 0 0 4px #8091BF;
+}
 input {
     font-size: <?php print $fontsize ?>px;
     font-family: <?php print $fontlist ?>;
