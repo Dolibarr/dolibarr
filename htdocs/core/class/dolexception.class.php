9d99b61f2d9218199151b025b4b9876ab587a840
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-10 12:46:04 +0000

    Missing constructor

--- htdocs/core/class/dolexception.class.php
+++ htdocs/core/class/dolexception.class.php
@@ -19,15 +19,17 @@
  *   \file			htdocs/core/class/dolexception.class.php
  *   \ingroup		core
  *   \brief			File of class for exceptions
- *   \version		$Id: dolexception.class.php,v 1.3 2011/07/31 23:45:14 eldy Exp $
+ *   \version		$Id: dolexception.class.php,v 1.4 2011/08/10 12:46:04 eldy Exp $
  */
 
 
 /**
  *    \class      DolException
- *    \brief      Classe de la gestion des exceptions
+ *    \brief      Class to manage exceptions
  */
 class DolException extends Exception
 {
-
+    function DolException()
+    {
+    }
 }
