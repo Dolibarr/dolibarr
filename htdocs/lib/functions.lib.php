a49bea1b0329fb4fe0b0a22ead551279376fe69d
Regis Houssin <regis@dolibarr.fr>
2011-08-19 22:15:22 +0000

    multicompany enhancement (in progress)

--- htdocs/lib/functions.lib.php
+++ htdocs/lib/functions.lib.php
@@ -28,7 +28,7 @@
  *	\file			htdocs/lib/functions.lib.php
  *	\brief			A set of functions for Dolibarr
  *					This file contains all frequently used functions.
- *	\version		$Id: functions.lib.php,v 1.558 2011/08/17 16:33:53 eldy Exp $
+ *	\version		$Id: functions.lib.php,v 1.559 2011/08/19 22:15:22 hregis Exp $
  */
 
 // For compatibility during upgrade
@@ -2224,7 +2224,14 @@ function restrictedArea($user, $features='societe', $objectid=0, $dbtablename=''
                 $sql = "SELECT dbt.".$dbt_select;
                 $sql.= " FROM ".MAIN_DB_PREFIX.$dbtablename." as dbt";
                 $sql.= " WHERE dbt.".$dbt_select." = ".$objectid;
-                $sql.= " AND dbt.entity IN (0,".(! empty($conf->entities[$dbtablename]) ? $conf->entities[$dbtablename] : $conf->entity).")";
+                if (($feature == 'user' || $feature == 'usergroup') && $conf->multicompany->enabled && $conf->entity == 1 && $user->admin && ! $user->entity)
+                {
+                	$sql.= " AND dbt.entity IS NOT NULL";
+                }
+                else
+                {
+                	$sql.= " AND dbt.entity IN (0,".(! empty($conf->entities[$dbtablename]) ? $conf->entities[$dbtablename] : $conf->entity).")";
+                }
             }
             else if (in_array($feature,$checksoc))
             {
@@ -2245,7 +2252,7 @@ function restrictedArea($user, $features='societe', $objectid=0, $dbtablename=''
                     $sql.= " AND s.entity IN (0,".(! empty($conf->entities[$dbtablename]) ? $conf->entities[$dbtablename] : $conf->entity).")";
                 }
                 // If multicompany and internal users with all permissions, check user is in correct entity
-                else if ($conf->global->MAIN_MODULE_MULTICOMPANY)
+                else if ($conf->multicompany->enabled)
                 {
                     $sql = "SELECT s.rowid";
                     $sql.= " FROM ".MAIN_DB_PREFIX."societe as s";
@@ -2274,7 +2281,7 @@ function restrictedArea($user, $features='societe', $objectid=0, $dbtablename=''
                     $sql.= " AND dbt.entity IN (0,".(! empty($conf->entities[$dbtablename]) ? $conf->entities[$dbtablename] : $conf->entity).")";
                 }
                 // If multicompany and internal users with all permissions, check user is in correct entity
-                else if ($conf->global->MAIN_MODULE_MULTICOMPANY)
+                else if ($conf->multicompany->enabled)
                 {
                     $sql = "SELECT dbt.rowid";
                     $sql.= " FROM ".MAIN_DB_PREFIX.$dbtablename." as dbt";
@@ -2317,7 +2324,7 @@ function restrictedArea($user, $features='societe', $objectid=0, $dbtablename=''
                     $sql.= " AND sc.fk_user = ".$user->id;
                 }
                 // If multicompany and internal users with all permissions, check user is in correct entity
-                else if ($conf->global->MAIN_MODULE_MULTICOMPANY)
+                else if ($conf->multicompany->enabled)
                 {
                     $sql = "SELECT dbt.".$dbt_select;
                     $sql.= " FROM ".MAIN_DB_PREFIX.$dbtablename." as dbt";
--- htdocs/user/class/user.class.php
+++ htdocs/user/class/user.class.php
@@ -25,7 +25,7 @@
 /**
  *  \file       htdocs/user/class/user.class.php
  *  \brief      Fichier de la classe utilisateur
- *  \version    $Id: user.class.php,v 1.48 2011/08/19 07:22:17 hregis Exp $
+ *  \version    $Id: user.class.php,v 1.49 2011/08/19 22:15:22 hregis Exp $
  */
 
 require_once(DOL_DOCUMENT_ROOT ."/core/class/commonobject.class.php");
@@ -126,7 +126,7 @@ class User extends CommonObject
 	 */
 	function fetch($id='', $login='',$sid='',$loadpersonalconf=1)
 	{
-		global $conf;
+		global $conf, $user;
 
 		// Clean parameters
 		$login=trim($login);
@@ -144,10 +144,15 @@ class User extends CommonObject
 		$sql.= " u.photo as photo,";
 		$sql.= " u.openid as openid";
 		$sql.= " FROM ".MAIN_DB_PREFIX."user as u";
-                if($conf->entity==0)
-                    $sql.= " WHERE u.entity IS NOT NULL";
-                else
-                    $sql.= " WHERE u.entity IN (0,".$conf->entity.")";
+		
+		if($conf->multicompany->enabled && $conf->entity == 1 && ($conf->global->MULTICOMPANY_TRANSVERSE_MODE || ($user->admin && ! $user->entity)))
+		{
+			$sql.= " WHERE u.entity IS NOT NULL";
+		}
+		else
+		{
+			$sql.= " WHERE u.entity IN (0,".$conf->entity.")";
+		}
 
 		if ($sid)
 		{
