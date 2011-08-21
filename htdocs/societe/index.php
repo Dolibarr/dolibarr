c45c0ba3ac36290ed3e6c709e073ddf43411b042
Laurent Destailleur <eldy@users.sourceforge.net>
2011-08-21 13:12:47 +0000

    Qual: Uniformize page name

--- htdocs/societe/index.php
+++ htdocs/societe/index.php
@@ -21,7 +21,7 @@
  *	\file       htdocs/societe/index.php
  *  \ingroup    societe
  *  \brief      Home page for third parties area
- *  \version    $Id: index.php,v 1.18 2011/08/21 13:07:10 eldy Exp $
+ *  \version    $Id: index.php,v 1.19 2011/08/21 13:12:48 eldy Exp $
  */
 
 require("../main.inc.php");
@@ -115,7 +115,7 @@ else
     if ($conf->societe->enabled && empty($conf->global->SOCIETE_DISABLE_PROSPECTS_STATS))
     {
         $statstring = "<tr $bc[0]>";
-        $statstring.= '<td><a href="'.DOL_URL_ROOT.'/comm/prospect/prospects.php">'.$langs->trans("Prospects").'</a></td><td align="right">'.round($third['prospect']).'</td>';
+        $statstring.= '<td><a href="'.DOL_URL_ROOT.'/comm/prospect/list.php">'.$langs->trans("Prospects").'</a></td><td align="right">'.round($third['prospect']).'</td>';
         $statstring.= "</tr>";
     }
     if ($conf->societe->enabled && empty($conf->global->SOCIETE_DISABLE_CUSTOMERS_STATS))
@@ -239,5 +239,5 @@ print '</td></tr></table>';
 
 $db->close();
 
-llxFooter('$Date: 2011/08/21 13:07:10 $ - $Revision: 1.18 $');
+llxFooter('$Date: 2011/08/21 13:12:48 $ - $Revision: 1.19 $');
 ?>
