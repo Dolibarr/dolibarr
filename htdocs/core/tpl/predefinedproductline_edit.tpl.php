692d8beb0af1dd065045c3d3538a8be9bcbe15d1
Regis Houssin <regis@dolibarr.fr>
2011-08-24 21:19:04 +0000

    Fix: syntax error in tpl/predefinedproductline_edit.tpl.php

--- htdocs/core/tpl/predefinedproductline_edit.tpl.php
+++ htdocs/core/tpl/predefinedproductline_edit.tpl.php
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * $Id: predefinedproductline_edit.tpl.php,v 1.20 2011/08/11 09:25:07 hregis Exp $
+ * $Id: predefinedproductline_edit.tpl.php,v 1.21 2011/08/24 21:19:04 hregis Exp $
  *
  * Need to have following variables defined:
  * $conf
@@ -27,23 +27,24 @@
 ?>
 
 <!-- BEGIN PHP TEMPLATE predefinedproductline_edit.tpl.php -->
-<form
-	action="<?php echo $_SERVER["PHP_SELF"].'?id='.$this->id.'#'.$line->id; ?>"
-	method="POST"><input type="hidden" name="token"
-	value="<?php  echo $_SESSION['newtoken']; ?>"> <input type="hidden"
-	name="action" value="updateligne"> <input type="hidden" name="id"
-	value="<?php echo $this->id; ?>"> <input type="hidden" name="lineid"
-	value="<?php echo $line->id; ?>">
+<form action="<?php echo $_SERVER["PHP_SELF"].'?id='.$this->id.'#'.$line->id; ?>" method="POST">
+<input type="hidden" name="token" value="<?php  echo $_SESSION['newtoken']; ?>" />
+<input type="hidden" name="action" value="updateligne" />
+<input type="hidden" name="id" value="<?php echo $this->id; ?>" />
+<input type="hidden" name="lineid" value="<?php echo $line->id; ?>" />
 
 <tr <?php echo $bc[$var]; ?>>
-	<td><a name="<?php echo $line->id; ?>"></a> <input type="hidden"
-		name="productid" value="<?php echo $line->fk_product; ?>"> <a
-		href="<?php echo DOL_URL_ROOT.'/product/fiche.php?id='.$line->fk_product; ?>">
+	<td>
+		<a name="<?php echo $line->id; ?>"></a>
+		<input type="hidden"	name="productid" value="<?php echo $line->fk_product; ?>" />
+		<a href="<?php echo DOL_URL_ROOT.'/product/fiche.php?id='.$line->fk_product; ?>">
 		<?php
 		if ($line->product_type==1) echo img_object($langs->trans('ShowService'),'service');
 		else print img_object($langs->trans('ShowProduct'),'product');
 		echo ' '.$line->ref;
-		?></a><?php
+		?>
+		</a>
+		<?php
 		echo ' - '.nl2br($line->product_label);
 		echo '<br>';
 
@@ -60,28 +61,33 @@
 		require_once(DOL_DOCUMENT_ROOT."/lib/doleditor.class.php");
 		$doleditor=new DolEditor('desc',$line->description,'',164,'dolibarr_details','',false,true,$conf->fckeditor->enabled && $conf->global->FCKEDITOR_ENABLE_DETAILS,$nbrows,70);
 		$doleditor->Create();
-		?></td>
+		?>
+	</td>
 
 	<td align="right"><?php echo $html->load_tva('tva_tx',$line->tva_tx,$seller,$buyer,'',$line->info_bits); ?></td>
 
-	<td align="right"><input size="6" type="text" class="flat"
-		name="subprice" value="<?php echo price($line->subprice,0,'',0); ?>"></td>
-
-	<td align="right"><?php if (($line->info_bits & 2) != 2) { ?> <input
-		size="2" type="text" class="flat" name="qty"
-		value="<?php echo $line->qty; ?>"> <?php } else { ?> &nbsp; <?php } ?>
+	<td align="right">
+		<input size="6" type="text" class="flat" name="subprice" value="<?php echo price($line->subprice,0,'',0); ?>" />
 	</td>
 
-	<td align="right" nowrap><?php if (($line->info_bits & 2) != 2) { ?> <input
-		size="1" type="text" class="flat" name="remise_percent"
-		value="<?php echo $line->remise_percent; ?>">% <?php } else { ?>
-	&nbsp; <?php } ?></td>
+	<td align="right">
+	<?php if (($line->info_bits & 2) != 2) { ?>
+		<input size="2" type="text" class="flat" name="qty"	value="<?php echo $line->qty; ?>" />
+	<?php } else { ?> &nbsp; <?php } ?>
+	</td>
 
-	<td align="center" colspan="5" valign="middle"><input type="submit"
-		class="button" name="save"
-		value="<?php echo $langs->trans("Save"); ?>"> <br>
-	<input type="submit" class="button" name="cancel"
-		value="<?php echo $langs->trans("Cancel"); ?>"></td>
+	<td align="right" nowrap>
+	<?php if (($line->info_bits & 2) != 2) { ?>
+		<input size="1" type="text" class="flat" name="remise_percent" value="<?php echo $line->remise_percent; ?>" />% 
+	<?php } else { ?>
+		&nbsp; 
+	<?php } ?>
+	</td>
+
+	<td align="center" colspan="5" valign="middle">
+		<input type="submit" class="button" name="save"	value="<?php echo $langs->trans("Save"); ?>"><br>
+		<input type="submit" class="button" name="cancel" value="<?php echo $langs->trans("Cancel"); ?>">
+	</td>
 </tr>
 
 <?php if ($conf->service->enabled && $dateSelector && $line->product_type == 1)	{ ?>
@@ -91,7 +97,8 @@
 	echo $html->select_date($line->date_start,'date_start',$conf->global->MAIN_USE_HOURMIN_IN_DATE_RANGE,$conf->global->MAIN_USE_HOURMIN_IN_DATE_RANGE,$line->date_start?0:1,"updateligne");
 	echo ' '.$langs->trans('to').' ';
 	echo $html->select_date($line->date_end,'date_end',$conf->global->MAIN_USE_HOURMIN_IN_DATE_RANGE,$conf->global->MAIN_USE_HOURMIN_IN_DATE_RANGE,$line->date_end?0:1,"updateligne");
-	?></td>
+	?>
+	</td>
 </tr>
 <?php } ?></form>
 <!-- END PHP TEMPLATE predefinedproductline_edit.tpl.php -->
