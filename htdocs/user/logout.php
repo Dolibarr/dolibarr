053ff24db809f6acc6581979a4707d0be62796e2
Regis Houssin <regis@dolibarr.fr>
2011-09-04 19:38:04 +0000

    avoid warning

--- htdocs/user/logout.php
+++ htdocs/user/logout.php
@@ -72,7 +72,7 @@ dol_syslog("End of session ".$sessionname);
 // Define url to go
 $url=DOL_URL_ROOT."/index.php";		// By default go to login page
 if ($urlfrom) $url=DOL_URL_ROOT.$urlfrom;
-if ($conf->global->MAIN_LOGOUT_GOTO_URL) $url=$conf->global->MAIN_LOGOUT_GOTO_URL;
+if (! empty($conf->global->MAIN_LOGOUT_GOTO_URL)) $url=$conf->global->MAIN_LOGOUT_GOTO_URL;
 
 //print 'url='.$url;exit;
 header("Location: ".$url);
