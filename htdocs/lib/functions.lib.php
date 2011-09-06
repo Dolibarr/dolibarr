432d4d31f76c01dc6b342abd1712e97d5a4d6500
Regis Houssin <regis@dolibarr.fr>
2011-09-06 07:02:24 +0000

    Fix: security.lib.php not include here

--- htdocs/lib/functions.lib.php
+++ htdocs/lib/functions.lib.php
@@ -92,7 +92,7 @@ function GETPOST($paramname,$check='',$method=0)
  */
 function dol_getprefix()
 {
-    return dol_hash($_SERVER["SERVER_NAME"].$_SERVER["DOCUMENT_ROOT"].DOL_DOCUMENT_ROOT.DOL_URL_ROOT);
+    return md5($_SERVER["SERVER_NAME"].$_SERVER["DOCUMENT_ROOT"].DOL_DOCUMENT_ROOT.DOL_URL_ROOT);
 }
 
 /**
