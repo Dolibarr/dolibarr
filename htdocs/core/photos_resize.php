27e034c07478769b8e3f28ccc98bc83bc2e612d0
Laurent Destailleur <eldy@users.sourceforge.net>
2011-09-12 19:49:40 +0000

    Doxygen

--- htdocs/core/photos_resize.php
+++ htdocs/core/photos_resize.php
@@ -16,6 +16,12 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ *       \file      htdocs/core/photos_resize.php
+ *       \ingroup	core
+ *       \brief     File of page to resize photos
+ */
+
 //if (! defined('NOREQUIREUSER'))  define('NOREQUIREUSER','1');
 //if (! defined('NOREQUIREDB'))    define('NOREQUIREDB','1');
 //if (! defined('NOREQUIRESOC'))   define('NOREQUIRESOC','1');
