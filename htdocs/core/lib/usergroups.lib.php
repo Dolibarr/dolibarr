e6d27e0731b5fd60fa9984716f37e257591aa2c1
Laurent Destailleur <eldy@destailleur.fr>
2011-11-02 14:15:44 +0100

    Doxygen

--- htdocs/core/lib/usergroups.lib.php
+++ htdocs/core/lib/usergroups.lib.php
@@ -74,8 +74,8 @@ function user_prepare_head($object)
 
     // Show more tabs from modules
     // Entries must be declared in modules descriptor with line
-    // $this->tabs = array('entity:+tabname:Title:@mymodule:/mymodule/mypage.php?id=__ID__');   to add new tab
-    // $this->tabs = array('entity:-tabname:Title:@mymodule:/mymodule/mypage.php?id=__ID__');   to remove a tab
+    // $this->tabs = array('entity:+tabname:Title:@mymodule:conditiontoshow:/mymodule/mypage.php?id=__ID__');   to add new tab
+    // $this->tabs = array('entity:-tabname:Title:@mymodule:conditiontoshow:/mymodule/mypage.php?id=__ID__');   to remove a tab
     complete_head_from_modules($conf,$langs,$object,$head,$h,'user');
 
     if (! $user->societe_id)
