3f495ef3b50d184661ae5347ce9b814dfcfe08f7
Laurent Destailleur <eldy@destailleur.fr>
2011-11-12 00:54:32 +0100

    Qual: Removed not used file Qual: Update licence header

--- htdocs/core/js/editinplace.js
+++ htdocs/core/js/editinplace.js
@@ -1,10 +1,25 @@
 // Copyright (C) 2011 Regis Houssin  <regis@dolibarr.fr>
+// Copyright (C) 2009 Laurent Destailleur  <eldy@users.sourceforge.net>
 //
-// Script javascript that contains functions for edit in place
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// or see http://www.gnu.org/
+//
+
 //
 // \file       htdocs/core/js/editinplace.js
 // \brief      File that include javascript functions for edit in place
-
+//
 
 $(document).ready(function() {
 	var element = $('#jeditable_element').html();
