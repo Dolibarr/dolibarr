b87a29806dd23c7864131310467365c84ce3e4d7
Laurent Destailleur <eldy@destailleur.fr>
2011-12-07 00:16:19 +0100

    Fix: Message

--- htdocs/comm/action/class/actioncomm.class.php
+++ htdocs/comm/action/class/actioncomm.class.php
@@ -3,17 +3,17 @@
  * Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2011 Regis Houssin        <regis@dolibarr.fr>
  * Copyright (C) 2011	   Juanjo Menent        <jmenent@2byte.es>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -133,7 +133,7 @@ class ActionComm extends CommonObject
             }
             else if ($result == 0)
             {
-                $this->error='Failed to get record with code AC_OTH from dictionnary "type of events"';
+                $this->error='Failed to get record with code '.$this->type_code.' from dictionnary "type of events"';
                 return -1;
             }
             else
