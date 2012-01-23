7e5c347bb85666d22f50a19056c8013a6cf85bd2
Regis Houssin <regis@dolibarr.fr>
2012-01-24 05:23:00 +0800

    Fix: correct reorder childrens of lines

--- htdocs/core/tpl/ajaxrow.tpl.php
+++ htdocs/core/tpl/ajaxrow.tpl.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2010-2011 Regis Houssin       <regis@dolibarr.fr>
+/* Copyright (C) 2010-2012 Regis Houssin       <regis@dolibarr.fr>
  * Copyright (C) 2010-2011 Laurent Destailleur <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -21,22 +21,22 @@
 <!-- BEGIN PHP TEMPLATE FOR JQUERY -->
 <?php if (count($object->lines) > 1 && $_GET['action'] != 'editline') { ?>
 <script>
-jQuery(document).ready(function(){
-	jQuery(".imgup").hide();
-	jQuery(".imgdown").hide();
-    jQuery(".lineupdown").removeAttr('href');
-    jQuery(".tdlineupdown").css("background-image",'url(<?php echo DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/grip.png'; ?>)');
-    jQuery(".tdlineupdown").css("background-repeat","no-repeat");
-    jQuery(".tdlineupdown").css("background-position","center center");
+$(document).ready(function(){
+	$(".imgup").hide();
+	$(".imgdown").hide();
+    $(".lineupdown").removeAttr('href');
+    $(".tdlineupdown").css("background-image",'url(<?php echo DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/grip.png'; ?>)');
+    $(".tdlineupdown").css("background-repeat","no-repeat");
+    $(".tdlineupdown").css("background-position","center center");
 
-    jQuery("#tablelines").tableDnD({
+    $("#tablelines").tableDnD({
 		onDrop: function(table, row) {
 			var reloadpage = "<?php echo $conf->global->MAIN_FORCE_RELOAD_PAGE; ?>";
-			var roworder = cleanSerialize(jQuery("#tablelines").tableDnDSerialize());
+			var roworder = cleanSerialize($("#tablelines").tableDnDSerialize());
 			var table_element_line = "<?php echo $object->table_element_line; ?>";
 			var fk_element = "<?php echo $object->fk_element; ?>";
 			var element_id = "<?php echo $object->id; ?>";
-			jQuery.get("<?php echo DOL_URL_ROOT; ?>/core/ajax/row.php",
+			$.get("<?php echo DOL_URL_ROOT; ?>/core/ajax/row.php",
 					{
 						roworder: roworder,
 						table_element_line: table_element_line,
@@ -47,11 +47,11 @@ jQuery(document).ready(function(){
 						if (reloadpage == 1) {
 							location.href = '<?php echo $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']; ?>';
 						} else {
-							jQuery("#tablelines .drag").each(
+							$("#tablelines .drag").each(
 									function( intIndex ) {
-										jQuery(this).removeClass("pair impair");
-										if (intIndex % 2 == 0) jQuery(this).addClass('impair');
-										if (intIndex % 2 == 1) jQuery(this).addClass('pair');
+										$(this).removeClass("pair impair");
+										if (intIndex % 2 == 0) $(this).addClass('impair');
+										if (intIndex % 2 == 1) $(this).addClass('pair');
 									});
 						}
 					});
@@ -59,17 +59,17 @@ jQuery(document).ready(function(){
 		onDragClass: "dragClass",
 		dragHandle: "tdlineupdown"
 	});
-    jQuery(".tdlineupdown").hover(    	function() { jQuery(this).addClass('showDragHandle'); },
-    	function() { jQuery(this).removeClass('showDragHandle'); }
+    $(".tdlineupdown").hover( function() { $(this).addClass('showDragHandle'); },
+    	function() { $(this).removeClass('showDragHandle'); }
     );
 });
 </script>
 <?php } else { ?>
 <script>
-jQuery(document).ready(function(){
-	jQuery(".imgup").hide();
-	jQuery(".imgdown").hide();
-    jQuery(".lineupdown").removeAttr('href');
+$(document).ready(function(){
+	$(".imgup").hide();
+	$(".imgdown").hide();
+    $(".lineupdown").removeAttr('href');
 });
 </script>
 <?php } ?>
