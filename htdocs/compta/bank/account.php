f07b6397fc02e0ac49ca42c3628709df5905c31a
Regis Houssin <regis@dolibarr.fr>
2012-02-13 15:18:23 +0100

    Fix: test if type of paiement is cash for cash account

--- htdocs/compta/bank/account.php
+++ htdocs/compta/bank/account.php
@@ -2,9 +2,9 @@
 /* Copyright (C) 2001-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (C) 2003      Jean-Louis Bergamo   <jlb@j1b.org>
  * Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copytight (C) 2004      Christophe Combelles <ccomb@free.fr>
- * Copytight (C) 2005-2012 Regis Houssin        <regis@dolibarr.fr>
- * Copytight (C) 2010-2011 Juanjo Menent        <jmenent@@2byte.es>
+ * Copyright (C) 2004      Christophe Combelles <ccomb@free.fr>
+ * Copyright (C) 2005-2012 Regis Houssin        <regis@dolibarr.fr>
+ * Copyright (C) 2010-2011 Juanjo Menent        <jmenent@@2byte.es>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
