50d1ff44720cf8e4ffa1a4e6fefc7646dbe0bacb
Regis Houssin <regis@dolibarr.fr>
2012-02-23 08:45:26 +0100

    Fix: indent

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -1,33 +1,33 @@
 <?php
 /* Copyright (C) 2002-2007 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (C) 2003      Xavier Dutoit        <doli@sydesy.com>
-* Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
-* Copyright (C) 2004      Sebastien Di Cintio  <sdicintio@ressource-toi.org>
-* Copyright (C) 2004      Benoit Mortier       <benoit.mortier@opensides.be>
-* Copyright (C) 2005-2012 Regis Houssin        <regis@dolibarr.fr>
-* Copyright (C) 2011      Philippe Grand       <philippe.grand@atoo-net.com>
-* Copyright (C) 2008      Matteli
-* Copyright (C) 2011      Juanjo Menent		<jmenent@2byte.es>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2004-2012 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2004      Sebastien Di Cintio  <sdicintio@ressource-toi.org>
+ * Copyright (C) 2004      Benoit Mortier       <benoit.mortier@opensides.be>
+ * Copyright (C) 2005-2012 Regis Houssin        <regis@dolibarr.fr>
+ * Copyright (C) 2011      Philippe Grand       <philippe.grand@atoo-net.com>
+ * Copyright (C) 2008      Matteli
+ * Copyright (C) 2011      Juanjo Menent		<jmenent@2byte.es>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  *	\file       htdocs/main.inc.php
-*	\ingroup	core
-*	\brief      File that defines environment for Dolibarr pages only (variables not required by scripts)
-*/
+ *	\ingroup	core
+ *	\brief      File that defines environment for Dolibarr pages only (variables not required by scripts)
+ */
 
 //@ini_set('memory_limit', '64M');	// This may be useless if memory is hard limited by your PHP
 
