cc6a2c8d840606a151558dc8f6e10eaf6d66eab3
Laurent Destailleur <eldy@destailleur.fr>
2012-02-24 11:06:05 +0100

    Fix: Compatibility of new module setup with old setup

--- htdocs/core/class/hookmanager.class.php
+++ htdocs/core/class/hookmanager.class.php
@@ -59,10 +59,10 @@ class HookManager
 	 *  class found into file /mymodule/class/actions_mymodule.class.php (if module has declared the context as a managed context).
 	 *  Then when a hook is executeHook('aMethod'...) is called, the method aMethod found into class will be executed.
 	 *
-	 *	@param	array	$arraytype	    Array list of searched hooks tab/features. For example: 'thirdpartycard' (for hook methods into page card thirdparty), 'thirdpartydao' (for hook methods into Societe), ...
-	 *	@return	int						Always 1
+	 *	@param	array	$arraycontext	    Array list of searched hooks tab/features. For example: 'thirdpartycard' (for hook methods into page card thirdparty), 'thirdpartydao' (for hook methods into Societe), ...
+	 *	@return	int							Always 1
 	 */
-	function callHooks($arraytype)
+	function callHooks($arraycontext)
 	{
 		global $conf;
 
@@ -70,24 +70,27 @@ class HookManager
         if (! is_array($conf->hooks_modules) || empty($conf->hooks_modules)) return;
 
         // For backward compatibility
-		if (! is_array($arraytype)) $arraytype=array($arraytype);
+		if (! is_array($arraycontext)) $arraycontext=array($arraycontext);
 
-		$this->contextarray=array_merge($arraytype,$this->contextarray);
+		$this->contextarray=array_merge($arraycontext,$this->contextarray);    // All contexts are concatenated
 
 		$i=0;
 		foreach($conf->hooks_modules as $module => $hooks)
 		{
 			if ($conf->$module->enabled)
 			{
-				foreach($arraytype as $type)
+				foreach($arraycontext as $context)
 				{
-					if (in_array($type,$hooks))    // We instantiate action class only if hook is required
+				    if (is_array($hooks)) $arrayhooks=$hooks;    // New system
+				    else $arrayhooks=explode(':',$hooks);        // Old system (for backward compatibility)
+
+					if (in_array($context,$arrayhooks))    // We instantiate action class only if hook is required
 					{
 						$path 		= '/'.$module.'/class/';
 						$actionfile = 'actions_'.$module.'.class.php';
 						$pathroot	= '';
 
-						$this->hooks[$i]['type']=$type;
+						$this->hooks[$i]['type']=$context;
 
 						// Include actions class overwriting hooks
 						$resaction=dol_include_once($path.$actionfile);
