242fbc16f4fd1660a29478ea76c0723efd8f7b96
Regis Houssin <regis@dolibarr.fr>
2012-03-12 16:02:50 +0100

    Fix: try jeditable autocomplete

--- htdocs/includes/jquery/plugins/jeditable/jquery.jeditable.ui-autocomplete.js
+++ htdocs/includes/jquery/plugins/jeditable/jquery.jeditable.ui-autocomplete.js
@@ -4,6 +4,6 @@
 $.editable.addInputType('autocomplete', {
 	element	: $.editable.types.text.element,
 	plugin	: function(settings, original) {
-		$('input', this).autocomplete(settings.autocomplete.data);
+		$('input', this).autocomplete(settings.autocomplete);
 	}
 });
