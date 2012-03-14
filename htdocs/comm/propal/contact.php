4e98687b13c9fce3a8b2a863772a6d527c97fd91
Regis Houssin <regis@dolibarr.fr>
2012-03-14 16:37:48 +0100

    Fix: remove old constants

--- htdocs/comm/propal/contact.php
+++ htdocs/comm/propal/contact.php
@@ -176,7 +176,7 @@ if ($id > 0 || ! empty($ref))
 		print '<td colspan="3">'.$object->client->getNomUrl(1).'</td></tr>';
 		
 		// Delivery address
-		if ($conf->global->PROPALE_ADD_DELIVERY_ADDRESS)
+		if ($conf->global->SOCIETE_ADDRESSES_MANAGEMENT)
 		{
 			print '<tr><td>';
 			print '<table class="nobordernopadding" width="100%"><tr><td>';
