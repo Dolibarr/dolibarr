e84c77ba311f1cd7220a833791becb769d9a5084
Regis Houssin <regis@dolibarr.fr>
2012-03-16 22:02:14 +0100

    New: add template for infos and contacts tabs and show/hide bloc system

--- htdocs/comm/propal.php
+++ htdocs/comm/propal.php
@@ -1044,6 +1044,67 @@ else if ($action == 'down' && $user->rights->propale->creer)
 	exit;
 }
 
+if (! empty($conf->global->MAIN_DISABLE_CONTACTS_TAB))
+{
+	if ($action == 'addcontact' && $user->rights->propale->creer)
+	{
+		$result = $object->fetch($id);
+	
+		if ($result > 0 && $id > 0)
+		{
+			$result = $object->add_contact($_POST["contactid"], $_POST["type"], $_POST["source"]);
+		}
+	
+		if ($result >= 0)
+		{
+			Header("Location: ".$_SERVER['PHP_SELF']."?id=".$object->id);
+			exit;
+		}
+		else
+		{
+			if ($object->error == 'DB_ERROR_RECORD_ALREADY_EXISTS')
+			{
+				$langs->load("errors");
+				$mesg = '<div class="error">'.$langs->trans("ErrorThisContactIsAlreadyDefinedAsThisType").'</div>';
+			}
+			else
+			{
+				$mesg = '<div class="error">'.$object->error.'</div>';
+			}
+		}
+	}
+	
+	// Bascule du statut d'un contact
+	else if ($action == 'swapstatut' && $user->rights->propale->creer)
+	{
+		if ($object->fetch($id) > 0)
+		{
+			$result=$object->swapContactStatus(GETPOST('ligne'));
+		}
+		else
+		{
+			dol_print_error($db);
+		}
+	}
+	
+	// Efface un contact
+	else if ($action == 'deletecontact' && $user->rights->propale->creer)
+	{
+		$object->fetch($id);
+		$result = $object->delete_contact($lineid);
+	
+		if ($result >= 0)
+		{
+			Header("Location: ".$_SERVER['PHP_SELF']."?id=".$object->id);
+			exit;
+		}
+		else
+		{
+			dol_print_error($db);
+		}
+	}
+}
+
 
 /*
  * View
@@ -1479,9 +1540,21 @@ if ($id > 0 || ! empty($ref))
 	
 	print '</table><br>';
 
+	if (! empty($conf->global->MAIN_DISABLE_CONTACTS_TAB))
+	{
+		require_once(DOL_DOCUMENT_ROOT.'/core/class/html.formcompany.class.php');
+		$formcompany= new FormCompany($db);
+		 
+		$blocname = 'contacts';
+		$title = $langs->trans('ContactsAddresses');
+		include(DOL_DOCUMENT_ROOT.'/core/tpl/bloc_showhide.tpl.php');
+	}
+	
     if (! empty($conf->global->MAIN_DISABLE_NOTES_TAB))
     {
-    	include(DOL_DOCUMENT_ROOT.'/core/tpl/notes.tpl.php');
+    	$blocname = 'notes';
+    	$title = $langs->trans('Notes');
+    	include(DOL_DOCUMENT_ROOT.'/core/tpl/bloc_showhide.tpl.php');
     }
 
 	/*
