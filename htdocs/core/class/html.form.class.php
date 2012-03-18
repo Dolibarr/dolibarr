9526465824ed2116e7fd4382a9975c8a15e5f606
Regis Houssin <regis@dolibarr.fr>
2012-03-18 11:22:28 +0100

    Fix: for compatibility with edit in place

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -177,7 +177,7 @@ class Form
                 {
                     $tmp=explode(':',$typeofdata);
                     require_once(DOL_DOCUMENT_ROOT."/core/class/doleditor.class.php");
-                    $doleditor=new DolEditor($htmlname, ($editvalue?$editvalue:$value), ($tmp[2]?$tmp[2]:''), ($tmp[3]?$tmp[3]:'100'), ($tmp[1]?$tmp[1]:'dolibarr_notes'), 'In', ($tmp[4]?$tmp[4]:0), true, true, ($tmp[5]?$tmp[5]:'20'), ($tmp[6]?$tmp[6]:'100'));
+                    $doleditor=new DolEditor($htmlname, ($editvalue?$editvalue:$value), ($tmp[2]?$tmp[2]:''), ($tmp[3]?$tmp[3]:'100'), ($tmp[1]?$tmp[1]:'dolibarr_notes'), 'In', ($tmp[5]?$tmp[5]:0), true, true, ($tmp[6]?$tmp[6]:'20'), ($tmp[7]?$tmp[7]:'100'));
                     $ret.=$doleditor->Create(1);
                 }
                 $ret.='</td>';
