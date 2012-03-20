b83026f8832757e3183e047c654642e5574458d1
Regis Houssin <regis@dolibarr.fr>
2012-03-20 22:45:15 +0800

    Fix: for multicompany category sharing

--- htdocs/categories/edit.php
+++ htdocs/categories/edit.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (C) 2005      Matthieu Valleton    <mv@seeschloss.org>
  * Copyright (C) 2006-2010 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2005-2011 Regis Houssin        <regis@dolibarr.fr>
+ * Copyright (C) 2005-2012 Regis Houssin        <regis@dolibarr.fr>
  * Copyright (C) 2007      Patrick Raguin	  	<patrick.raguin@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -41,7 +41,7 @@ if ($id == "")
 }
 
 // Security check
-$result = restrictedArea($user, 'categorie', $id);
+$result = restrictedArea($user, 'categorie', $id, '&category');
 
 
 
