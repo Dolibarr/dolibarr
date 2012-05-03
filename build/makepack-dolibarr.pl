7921965fe2f2d23e3b60635cfd3e6b84763cdd69
Laurent Destailleur <eldy@destailleur.fr>
2012-05-03 23:13:21 +0200

    Fix: Use correct dest dir.

--- build/makepack-dolibarr.pl
+++ build/makepack-dolibarr.pl
@@ -102,7 +102,8 @@ for (0..@ARGV-1) {
     	$FILENAMESNAPSHOT.="-".$PREFIX; 
     }
 }
-if ($ENV{"DESTI"}) { $DESTI = $ENV{"DESTI"}; }		# Force output dir if env DESTI is defined
+if ($ENV{"DESTIBETARC"} && $BUILD =~ /[a-z]/i)    { $DESTI = $ENV{"DESTIBETARC"}; }		# Force output dir if env DESTI is defined
+if ($ENV{"DESTISTABLE"}  && $BUILD =~ /^[0-9]+$/) { $DESTI = $ENV{"DESTISTABLE"}; }	# Force output dir if env DESTI is defined
 
 
 print "Makepack version $VERSION\n";
