c24b901dd64516bd4d628e3395fff26461b03c47
juanjo <juanjo@samsung>
2012-05-30 14:48:23 +0200

    [ task #104 ] Possibilité de facturer plusieurs commandes. Init works

--- htdocs/core/menus/standard/eldy.lib.php
+++ htdocs/core/menus/standard/eldy.lib.php
@@ -944,6 +944,8 @@ function print_left_eldy_menu($db,$menu_array_before,$menu_array_after)
                 $langs->load("orders");
                 if ($conf->facture->enabled) $newmenu->add("/commande/liste.php?leftmenu=orders&amp;viewstatut=3", $langs->trans("MenuOrdersToBill"), 0, $user->rights->commande->lire, '', $mainmenu, 'orders');
                 //                  if ($leftmenu=="orders") $newmenu->add("/commande/", $langs->trans("StatusOrderToBill"), 1, $user->rights->commande->lire);
+                if ($conf->global->MAIN_FEATURES_LEVEL > 1)
+     				if ($leftmenu=="orders") $newmenu->add("/commande/customer.php", $langs->trans("GenerateBill"), 1, $user->rights->commande->lire);
             }
 
             // Donations
