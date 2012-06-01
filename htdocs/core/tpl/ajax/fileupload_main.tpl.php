2d818fc886d0c0244f7a1a8a4c3746cbf3503636
Laurent Destailleur <eldy@destailleur.fr>
2012-06-01 02:07:24 +0200

    Revert "Fix: add max file size"
    This reverts commit 8b224e9e2d680d3a2fc77baa157b5ff71e65db70.

--- htdocs/core/tpl/ajax/fileupload_main.tpl.php
+++ htdocs/core/tpl/ajax/fileupload_main.tpl.php
@@ -44,11 +44,6 @@ $(function () {
 	// Initialize the jQuery File Upload widget:
 	$('#fileupload').fileupload();
 
-	// Options
-	$('#fileupload').fileupload('option', {
-		maxFileSize: max_file_size
-	});
-
 });
 </script>
 <!-- END TEMPLATE FILE UPLOAD MAIN -->
