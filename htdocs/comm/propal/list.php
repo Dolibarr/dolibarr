db4008850b1c4b424a250c456af6cce757720076
Laurent Destailleur <eldy@destailleur.fr>
2012-06-23 22:25:40 +0200

    Fix: missing include

--- htdocs/comm/propal/list.php
+++ htdocs/comm/propal/list.php
@@ -33,6 +33,7 @@ require("../../main.inc.php");
 require_once(DOL_DOCUMENT_ROOT."/core/class/html.formother.class.php");
 require_once(DOL_DOCUMENT_ROOT."/core/class/html.formfile.class.php");
 require_once(DOL_DOCUMENT_ROOT."/core/class/html.formpropal.class.php");
+require_once(DOL_DOCUMENT_ROOT."/core/lib/date.lib.php");
 require_once(DOL_DOCUMENT_ROOT.'/comm/propal/class/propal.class.php');
 if ($conf->projet->enabled)   require_once(DOL_DOCUMENT_ROOT.'/projet/class/project.class.php');
 
