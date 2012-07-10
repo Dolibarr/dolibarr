60dc11872e6af09dd5e185993471db0a3aba5ad5
Regis Houssin <regis@dolibarr.fr>
2012-07-10 13:35:22 +0200

    Fix: for multicompany transverse mode

--- htdocs/user/fiche.php
+++ htdocs/user/fiche.php
@@ -1313,15 +1313,15 @@ else
                 $usergroup=new UserGroup($db);
                 $groupslist = $usergroup->listGroupsForUser($fuser->id);
 
-                if (! empty($groupslist))
+                if (! empty($conf->multicompany->enabled) && ! empty($conf->multicompany->transverse_mode))
                 {
-                    if( ! ($conf->multicompany->enabled && $conf->multicompany->transverse_mode))
-                    {
-                        foreach($groupslist as $groupforuser)
-                        {
-                            $exclude[]=$groupforuser->id;
-                        }
-                    }
+                	if (! empty($groupslist))
+                	{
+                		foreach($groupslist as $groupforuser)
+                		{
+                			$exclude[]=$groupforuser->id;
+                		}
+                	}
                 }
 
                 if ($caneditgroup)
@@ -1400,15 +1400,19 @@ else
                         		{
                         			$mc->getInfo($group_entity);
                         			print ($nb > 0 ? ', ' : '').$mc->label;
+                        			print '<a href="'.$_SERVER['PHP_SELF'].'?id='.$fuser->id.'&amp;action=removegroup&amp;group='.$group->id.'&amp;entity='.$group_entity.'">';
+                        			print img_delete($langs->trans("RemoveFromGroup"));
+                        			print '</a>';
                         			$nb++;
                         		}
                         	}
                         }
                         print '<td align="right">';
-                        if ($caneditgroup)
+                        if ($caneditgroup && empty($conf->multicompany->transverse_mode))
                         {
-                            print '<a href="'.$_SERVER['PHP_SELF'].'?id='.$fuser->id.'&amp;action=removegroup&amp;group='.$group->id.'&amp;entity='.$group->usergroup_entity.'">';
+                            print '<a href="'.$_SERVER['PHP_SELF'].'?id='.$fuser->id.'&amp;action=removegroup&amp;group='.$group->id.'">';
                             print img_delete($langs->trans("RemoveFromGroup"));
+                            print '</a>';
                         }
                         else
                         {
