970d946e5b28b7030f497a04f900a3e5ee3ae17c
Laurent Destailleur <eldy@destailleur.fr>
2012-07-22 16:53:32 +0200

    Fix: sql injection for create database

--- htdocs/core/db/mysql.class.php
+++ htdocs/core/db/mysql.class.php
@@ -800,8 +800,8 @@ class DoliDBMysql
 		if (empty($collation)) $collation=$this->forcecollate;
 
 		// ALTER DATABASE dolibarr_db DEFAULT CHARACTER SET latin DEFAULT COLLATE latin1_swedish_ci
-		$sql = 'CREATE DATABASE '.$database;
-		$sql.= ' DEFAULT CHARACTER SET '.$charset.' DEFAULT COLLATE '.$collation;
+        $sql = "CREATE DATABASE '".$this->escape($database)."'";
+        $sql.= " DEFAULT CHARACTER SET '".$this->escape($charset)."' DEFAULT COLLATE '".$this->escape($collation)."'";
 
 		dol_syslog($sql,LOG_DEBUG);
 		$ret=$this->query($sql);
