5772e46c9af8a2a33bbc0a8b8602433e1dae58c8
Laurent Destailleur <eldy@destailleur.fr>
2012-07-30 22:18:54 +0200

    We start new modules with level "experimental".

--- htdocs/core/modules/modCommissions.class.php
+++ htdocs/core/modules/modCommissions.class.php
@@ -54,11 +54,11 @@ class modCommissions extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Commissions management";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = 'dolibarr';
+		$this->version = 'experimental';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=other)
-		$this->special = 0;
+		$this->special = 2;
 		// Name of png file (without png) used for this module.
 		// Png file must be in theme/yourtheme/img directory under name object_pictovalue.png.
 		$this->picto='commissions';
