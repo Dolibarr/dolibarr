89cd23452f4268c739d19f244d30a319aaebc64e
Regis Houssin <regis@dolibarr.fr>
2012-08-05 20:34:51 +0200

    Fix: missing delete shipping and delivery files (minimum for possibility to merge with 3.2)

--- htdocs/core/modules/livraison/modules_livraison.php
+++ htdocs/core/modules/livraison/modules_livraison.php
@@ -156,7 +156,7 @@ abstract class ModeleNumRefDeliveryOrder
  */
 function delivery_order_pdf_create($db, $object, $modele, $outputlangs='')
 {
-	global $conf,$langs;
+	global $conf,$user,$langs;
 
 	$langs->load("deliveries");
 
@@ -224,6 +224,16 @@ function delivery_order_pdf_create($db, $object, $modele, $outputlangs='')
 			// we delete preview files
         	require_once(DOL_DOCUMENT_ROOT."/core/lib/files.lib.php");
 			dol_delete_preview($object);
+
+			// Appel des triggers
+			include_once(DOL_DOCUMENT_ROOT . "/core/class/interfaces.class.php");
+			$interface=new Interfaces($db);
+			$result=$interface->run_triggers('DELIVERY_BUILDDOC',$object,$user,$langs,$conf);
+			if ($result < 0) {
+				$error++; $this->errors=$interface->errors;
+			}
+			// Fin appel triggers
+
 			return 1;
 		}
 		else
