74c1fa2263c7d58752490fe139f1ec9665bf1cf3
Regis Houssin <regis@dolibarr.fr>
2012-08-18 10:51:13 +0200

    Fix: bad rights

--- htdocs/projet/tasks/note.php
+++ htdocs/projet/tasks/note.php
@@ -78,18 +78,20 @@ if (! empty($project_ref) && ! empty($withproject))
 	}
 }
 
+$permission=($user->rights->projet->creer || $user->rights->projet->all->creer);
+
 
 /*
  * Actions
  */
 
-if ($action == 'setnote_public' && $user->rights->ficheinter->creer)
+if ($action == 'setnote_public' && ! empty($permission))
 {
     $result=$object->update_note_public(dol_html_entity_decode(GETPOST('note_public'), ENT_QUOTES));
     if ($result < 0) dol_print_error($db,$object->error);
 }
 
-else if ($action == 'setnote_private' && $user->rights->ficheinter->creer)
+else if ($action == 'setnote_private' && ! empty($permission))
 {
     $result=$object->update_note(dol_html_entity_decode(GETPOST('note_private'), ENT_QUOTES));
     if ($result < 0) dol_print_error($db,$object->error);
@@ -203,7 +205,6 @@ if ($object->id > 0)
 	print '<br>';
 
 	$colwidth=30;
-	$permission=($user->rights->projet->creer || $user->rights->projet->all->creer);
     $moreparam=$param;
 	include(DOL_DOCUMENT_ROOT.'/core/tpl/notes.tpl.php');
 
