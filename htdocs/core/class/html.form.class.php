a249061017f17fdeefde2a997698de242ecb1aa2
Laurent Destailleur <eldy@destailleur.fr>
2012-09-11 15:49:46 +0200

    Fix: If object already loaded, the url was not returned.

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -3765,10 +3765,9 @@ class Form
         if (empty($object->barcode_type_code) || empty($object->barcode_type_coder))
         {
             $result = $object->fetch_barcode();
+	        //Check if fetch_barcode() failed
+        	if ($result < 1) return '<!-- ErrorFetchBarcode -->';
         }
-
-        //Check if fetch_barcode() failed
-        if ($result < 1) return '';
 
         // Barcode image
         $url=DOL_URL_ROOT.'/viewimage.php?modulepart=barcode&generator='.urlencode($object->barcode_type_coder).'&code='.urlencode($object->barcode).'&encoding='.urlencode($object->barcode_type_code);
