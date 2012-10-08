229005b1a94f20976cfaf5f16908628919a0e7f0
Maxime Kohlhaas <mko@atm-consulting.fr>
2012-10-09 00:54:02 +0200

    Task # 186 : Forgot to add the new box in the mod definition...

--- htdocs/core/modules/modProduct.class.php
+++ htdocs/core/modules/modProduct.class.php
@@ -90,6 +90,7 @@ class modProduct extends DolibarrModules
 		// Boxes
 		$this->boxes = array();
 		$this->boxes[0][1] = "box_produits.php";
+		$this->boxes[0][2] = "box_produits_alerte_stock.php";
 
 		// Permissions
 		$this->rights = array();
