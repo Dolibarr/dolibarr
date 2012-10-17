cf814dc7fd9cbc0ca46284d3ef9cc0d4972be174
simnandez <jmenent@2byte.es>
2012-10-17 10:42:16 +0200

    New: More import options. Categories Trad: Add es_ES and ca_ES missing translations

--- htdocs/core/modules/modCategorie.class.php
+++ htdocs/core/modules/modCategorie.class.php
@@ -1,6 +1,7 @@
 <?php
 /* Copyright (C) 2005      Matthieu Valleton    <mv@seeschloss.org>
  * Copyright (C) 2005-2010 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2012	   Juanjo Menent		<jmenent@2byte.es>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
