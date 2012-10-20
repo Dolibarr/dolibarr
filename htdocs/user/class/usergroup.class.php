97245d36aa3c5f40bf45812ca664b0b01c6292d7
Regis Houssin <regis@dolibarr.fr>
2012-10-20 11:10:36 +0200

    Fix: wrong method

--- htdocs/user/class/usergroup.class.php
+++ htdocs/user/class/usergroup.class.php
@@ -245,7 +245,7 @@ class UserGroup extends CommonObject
 			// les caracteristiques (module, perms et subperms) de ce droit.
 			$sql = "SELECT module, perms, subperms";
 			$sql.= " FROM ".MAIN_DB_PREFIX."rights_def";
-			$sql.= " WHERE id = '".$this->escape($rid)."'";
+			$sql.= " WHERE id = '".$this->db->escape($rid)."'";
 			$sql.= " AND entity = ".$conf->entity;
 
 			$result=$this->db->query($sql);
@@ -261,7 +261,7 @@ class UserGroup extends CommonObject
 			}
 
 			// Where pour la liste des droits a ajouter
-			$whereforadd="id=".$this->escape($rid);
+			$whereforadd="id=".$this->db->escape($rid);
 			// Ajout des droits induits
 			if ($subperms)   $whereforadd.=" OR (module='$module' AND perms='$perms' AND (subperms='lire' OR subperms='read'))";
 			else if ($perms) $whereforadd.=" OR (module='$module' AND (perms='lire' OR perms='read') AND subperms IS NULL)";
@@ -272,8 +272,8 @@ class UserGroup extends CommonObject
 		}
 		else {
 			// Where pour la liste des droits a ajouter
-			if (! empty($allmodule)) $whereforadd="module='".$this->escape($allmodule)."'";
-			if (! empty($allperms))  $whereforadd=" AND perms='".$this->escape($allperms)."'";
+			if (! empty($allmodule)) $whereforadd="module='".$this->db->escape($allmodule)."'";
+			if (! empty($allperms))  $whereforadd=" AND perms='".$this->db->escape($allperms)."'";
 		}
 
 		// Ajout des droits de la liste whereforadd
@@ -345,7 +345,7 @@ class UserGroup extends CommonObject
 			// les caracteristiques module, perms et subperms de ce droit.
 			$sql = "SELECT module, perms, subperms";
 			$sql.= " FROM ".MAIN_DB_PREFIX."rights_def";
-			$sql.= " WHERE id = '".$this->escape($rid)."'";
+			$sql.= " WHERE id = '".$this->db->escape($rid)."'";
 			$sql.= " AND entity = ".$conf->entity;
 
 			$result=$this->db->query($sql);
@@ -361,7 +361,7 @@ class UserGroup extends CommonObject
 			}
 
 			// Where pour la liste des droits a supprimer
-			$wherefordel="id=".$this->escape($rid);
+			$wherefordel="id=".$this->db->escape($rid);
 			// Suppression des droits induits
 			if ($subperms=='lire' || $subperms=='read') $wherefordel.=" OR (module='$module' AND perms='$perms' AND subperms IS NOT NULL)";
 			if ($perms=='lire' || $perms=='read')    $wherefordel.=" OR (module='$module')";
@@ -372,8 +372,8 @@ class UserGroup extends CommonObject
 		}
 		else {
 			// Where pour la liste des droits a supprimer
-			if (! empty($allmodule)) $wherefordel="module='".$this->escape($allmodule)."'";
-			if (! empty($allperms))  $wherefordel=" AND perms='".$this->escape($allperms)."'";
+			if (! empty($allmodule)) $wherefordel="module='".$this->db->escape($allmodule)."'";
+			if (! empty($allperms))  $wherefordel=" AND perms='".$this->db->escape($allperms)."'";
 		}
 
 		// Suppression des droits de la liste wherefordel
