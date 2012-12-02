60486417c3514f9ca44b8680b4887482f8628951
Laurent Destailleur <eldy@destailleur.fr>
2012-12-02 19:55:42 +0100

    Test. Try to create a backport branch.

--- htdocs/core/modules/supplier_order/pdf/pdf_muscadet.modules.php
+++ htdocs/core/modules/supplier_order/pdf/pdf_muscadet.modules.php
@@ -353,6 +353,15 @@ class pdf_muscadet extends ModelePDFSuppliersOrders
 					$this->localtax1[$localtax1rate]+=$localtax1ligne;
 					$this->localtax2[$localtax2rate]+=$localtax2ligne;
 
+					// Add line
+					if (! empty($conf->global->MAIN_PDF_DASH_BETWEEN_LINES) && $i < ($nblignes - 1))
+					{
+						$pdf->SetLineStyle(array('dash'=>'1,1','color'=>array(210,210,210)));
+						//$pdf->SetDrawColor(190,190,200);
+						$pdf->line($this->marge_gauche, $nexY+1, $this->page_largeur - $this->marge_droite, $nexY+1);
+						$pdf->SetLineStyle(array('dash'=>0));
+					}
+
 					$nexY+=2;    // Passe espace entre les lignes
 
 					// Detect if some page were added automatically and output _tableau for past pages
