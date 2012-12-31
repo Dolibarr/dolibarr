Dolibarr/dolibarr

commit ca2b3f11b166f395e11af74d825989737920e925

Fix: Several fix in trigger names

htdocs/contrat/class/contrat.class.php
@@ -1112,7 +1112,7 @@ class Contrat extends CommonObject
 			// Appel des triggers
 			include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
 			$interface=new Interfaces($this->db);
-			$result=$interface->run_triggers('CONTRACTLINE_DELETE',$this,$user,$langs,$conf);
+			$result=$interface->run_triggers('CONTRACT_LINE_DELETE',$this,$user,$langs,$conf);
 			if ($result < 0) { $error++; $this->errors=$interface->errors; }
 			// Fin appel triggers
 
@@ -1940,7 +1940,7 @@ class ContratLigne
 			// Appel des triggers
 			include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
 			$interface=new Interfaces($this->db);
-			$result=$interface->run_triggers('MYOBJECT_MODIFY',$this,$user,$langs,$conf);
+			$result=$interface->run_triggers('CONTRACT_LINE_MODIFY',$this,$user,$langs,$conf);
 			if ($result < 0) { $error++; $this->errors=$interface->errors; }
 			// Fin appel triggers
 		}

htdocs/commande/class/commande.class.php
@@ -426,7 +426,7 @@ class Commande extends CommonOrder
             // Appel des triggers
             include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
             $interface=new Interfaces($this->db);
-            $result=$interface->run_triggers('BILL_REOPEN',$this,$user,$langs,$conf);
+            $result=$interface->run_triggers('ORDER_REOPEN',$this,$user,$langs,$conf);
             if ($result < 0) { $error++; $this->errors=$interface->errors; }
             // Fin appel triggers
         }

htdocs/user/class/usergroup.class.php
@@ -532,7 +532,7 @@ class UserGroup extends CommonObject
 			// Appel des triggers
 			include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
 			$interface=new Interfaces($this->db);
-			$result=$interface->run_triggers('USER_DELETE',$this,$user,$langs,$conf);
+			$result=$interface->run_triggers('GROUP_DELETE',$this,$user,$langs,$conf);
 			if ($result < 0) { $error++; $this->errors=$interface->errors; }
 			// Fin appel triggers
 

commit a90db96421b3719d7c4518554fbb47c152eb0340

Fixed typo introduced in commit ca2b3f11b166f395e11af74d825989737920e925

htdocs/contrat/class/contrat.class.php
@@ -1940,7 +1940,7 @@ class ContratLigne
 			// Appel des triggers
 			include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
 			$interface=new Interfaces($this->db);
-			$result=$interface->run_triggers('CONTRACT_LINE_MODIFY',$this,$user,$langs,$conf);
+			$result=$interface->run_triggers('CONTRACT_MODIFY',$this,$user,$langs,$conf);
 			if ($result < 0) { $error++; $this->errors=$interface->errors; }
 			// Fin appel triggers
 		}