e1683f5f2209ff0eb2432ec790d34860e1a6d5a5
simnandez <jmenent@2byte.es>
2013-02-22 08:57:28 +0100

    Fix: Pagination at bottom not work correctly with filters applied

--- htdocs/product/liste.php
+++ htdocs/product/liste.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@capnetworks.com>
  * Copyright (C) 2012      Marcos García        <marcosgdf@gmail.com>
+ * Copyright (C) 2013      Juanjo Menent        <jmenent@2byte.es>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
