9fc18d23e7b00cdb347ccf11386b2a9127a84495
Grand Philippe <contact@atoo-net.com>
2013-02-23 13:59:17 +0100

    fix: change name with lastname

--- htdocs/user/index.php
+++ htdocs/user/index.php
@@ -99,7 +99,7 @@ if ($result)
     print '<table class="noborder" width="100%">';
     print '<tr class="liste_titre">';
     print_liste_field_titre($langs->trans("Login"),"index.php","u.login",$param,"","",$sortfield,$sortorder);
-    print_liste_field_titre($langs->trans("LastName"),"index.php","u.name",$param,"","",$sortfield,$sortorder);
+    print_liste_field_titre($langs->trans("LastName"),"index.php","u.lastname",$param,"","",$sortfield,$sortorder);
     print_liste_field_titre($langs->trans("FirstName"),"index.php","u.firstname",$param,"","",$sortfield,$sortorder);
     print_liste_field_titre($langs->trans("Company"),"index.php","u.fk_societe",$param,"","",$sortfield,$sortorder);
     print_liste_field_titre($langs->trans("DateCreation"),"index.php","u.datec",$param,"",'align="center"',$sortfield,$sortorder);
@@ -123,7 +123,7 @@ if ($result)
         	print img_picto($langs->trans("Administrator"),'star');
         }
         print '</td>';
-        print '<td>'.ucfirst($obj->name).'</td>';
+        print '<td>'.ucfirst($obj->lastname).'</td>';
         print '<td>'.ucfirst($obj->firstname).'</td>';
         print "<td>";
         if ($obj->fk_societe)
