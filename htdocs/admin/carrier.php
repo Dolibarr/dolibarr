c8872d7e982dd9535c794f871be17d44dc2261bd
frederic34 <frederic.france@free.fr>
2013-02-23 10:20:31 +0100

    WIP : Admin for carriers

--- htdocs/admin/carrier.php
+++ htdocs/admin/carrier.php
@@ -41,6 +41,10 @@ $object = new Expedition($db);
 /*
  * Actions
  */
+if ($action==setvalue AND $carrier)
+{
+    //$object->update_delivery_method($carrier);
+}
 
 if ($action==activate_carrier AND $carrier)
 {
@@ -98,51 +102,115 @@ dol_fiche_head($head, $hselected, $langs->trans("ModuleSetup"));
 /*
  * Carrier List
  */
-
-$object->all_delivery_methods();
-$var=true;
-print_titre($langs->trans("CarrierList"));
-
-print '<table class="noborder" width="100%">';
-print '<tr class="liste_titre">';
-print '<td width="80">'.$langs->trans("Code").'</td>';
-print '<td width="150">'.$langs->trans("Name").'</td>';
-print '<td>'.$langs->trans("Description").'</td>';
-print '<td>'.$langs->trans("TrackingUrl").'</td>';
-print '<td align="center" width="60">'.$langs->trans("Status").'</td>';
-print '<td align="center" width="30">'.$langs->trans("Edit").'</td>';
-print "</tr>\n";
-for ($i=0; $i<sizeof($object->listmeths); $i++)
+if ($action=='edit_carrier' || $action=='setvalue')
 {
-    $var=!$var;
-    print "<tr ".$bc[$var].">";
-    print '<td>'.$object->listmeths[$i][code].'</td>';
-    print '<td>'.$object->listmeths[$i][libelle].'</td>';
-    print '<td>'.$object->listmeths[$i][description].'</td>';
-    print '<td>'.$object->listmeths[$i][tracking].'</td>';
-    print '<td align="center">';
-    if($object->listmeths[$i][active] == 0)
-    {
-        print '<a href="carrier.php?action=activate_carrier&amp;carrier='.$object->listmeths[$i][rowid].'">'.img_picto($langs->trans("Disabled"),'switch_off').'</a>';
-    }
-    else
-    {
-        print '<a href="carrier.php?action=disable_carrier&amp;carrier='.$object->listmeths[$i][rowid].'">'.img_picto($langs->trans("Enabled"),'switch_on').'</a>';
-    }
-    print '</td><td align="center">';
-    if($object->listmeths[$i][editable] == 1)
-    {
-        print '<a href="carrier.php?action=edit_carrier&amp;carrier='.$object->listmeths[$i][rowid].'">'.img_picto($langs->trans("Edit"),'edit').'</a>';
-    }
-    else
-    {
-        print '&nbsp;';
-    }
-    print '</td>';
-}
-print "</tr>\n";
+    // Carrier Edit
+    if ($carrier) $object->list_delivery_methods($carrier);
+    print_titre($langs->trans("CarrierEdit"));
 
-print '</table><br>';
+    print '<form method="post" action="'.$_SERVER["PHP_SELF"].'?carrier='.$carrier.'">';
+    print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+    print '<input type="hidden" name="action" value="setvalue">';
+
+
+    print '<table class="nobordernopadding" width="100%">';
+
+    $var=true;
+    print '<tr class="liste_titre">';
+    print '<td width="150">'.$langs->trans("CarrierParameter").'</td>';
+    print '<td>'.$langs->trans("Value").'</td>';
+    print "</tr>\n";
+
+    $var=!$var;
+    print '<tr '.$bc[$var].'><td class="fieldrequired">';
+    print $langs->trans("Code").'</td><td>';
+    print '<input size="32" type="text" name="code" value="'.$object->listmeths[0][code].'">';
+    print ' &nbsp; '.$langs->trans("Example").': CODE';
+    print '</td></tr>';
+
+    $var=!$var;
+    print '<tr '.$bc[$var].'><td class="fieldrequired">';
+    print $langs->trans("Name").'</td><td>';
+    print '<input size="32" type="text" name="name" value="'.$object->listmeths[0][libelle].'">';
+    print '</td></tr>';
+
+    $var=!$var;
+    print '<tr '.$bc[$var].'><td class="fieldrequired">';
+    print $langs->trans("Description").'</td><td>';
+    print '<input size="64" type="text" name="description" value="'.$object->listmeths[0][description].'">';
+    print '</td></tr>';
+
+    $var=!$var;
+    print '<tr '.$bc[$var].'><td class="fieldrequired">';
+    print $langs->trans("Tracking").'</td><td>';
+    print '<input size="128" type="text" name="tracking" value="'.$object->listmeths[0][tracking].'">';
+    print ' &nbsp; '.$langs->trans("Example").': http://www.website.com/dir/{TRACKID}';
+    print '</td></tr>';
+
+    if ($carrier)
+    {
+    print '<tr><td colspan="2" align="center"><br><input type="submit" class="button" value="'.$langs->trans("Modify").'"></td></tr>';
+    }
+    else
+    {
+    print '<tr><td colspan="2" align="center"><br><input type="submit" class="button" value="'.$langs->trans("Add").'"></td></tr>';
+    }
+
+    print '</table>';
+    print '</form>';
+
+}
+else
+{
+    // Display List
+    $object->list_delivery_methods();
+    $var=true;
+    print_titre($langs->trans("CarrierList"));
+
+    print '<table class="noborder" width="100%">';
+    print '<tr class="liste_titre">';
+    print '<td width="80">'.$langs->trans("Code").'</td>';
+    print '<td width="150">'.$langs->trans("Name").'</td>';
+    print '<td>'.$langs->trans("Description").'</td>';
+    print '<td>'.$langs->trans("TrackingUrl").'</td>';
+    print '<td align="center" width="60">'.$langs->trans("Status").'</td>';
+    print '<td align="center" width="30">'.$langs->trans("Edit").'</td>';
+    print "</tr>\n";
+    for ($i=0; $i<sizeof($object->listmeths); $i++)
+    {
+        $var=!$var;
+        print "<tr ".$bc[$var].">";
+        print '<td>'.$object->listmeths[$i][code].'</td>';
+        print '<td>'.$object->listmeths[$i][libelle].'</td>';
+        print '<td>'.$object->listmeths[$i][description].'</td>';
+        print '<td>'.$object->listmeths[$i][tracking].'</td>';
+        print '<td align="center">';
+        if($object->listmeths[$i][active] == 0)
+        {
+            print '<a href="carrier.php?action=activate_carrier&amp;carrier='.$object->listmeths[$i][rowid].'">'.img_picto($langs->trans("Disabled"),'switch_off').'</a>';
+        }
+        else
+        {
+            print '<a href="carrier.php?action=disable_carrier&amp;carrier='.$object->listmeths[$i][rowid].'">'.img_picto($langs->trans("Enabled"),'switch_on').'</a>';
+        }
+        print '</td><td align="center">';
+        if($object->listmeths[$i][editable] == 1)
+        {
+            print '<a href="carrier.php?action=edit_carrier&amp;carrier='.$object->listmeths[$i][rowid].'">'.img_picto($langs->trans("Edit"),'edit').'</a>';
+        }
+        else
+        {
+            print '&nbsp;';
+        }
+        print '</td>';
+        print "</tr>\n";
+    }
+            print '<tr><td align="center"><a href="carrier.php?action=edit_carrier"><br>'.$langs->trans("Add").'</a></td><tr>';
+
+    print '</table><br>';
+
+    print '</div>';
+}
 
 llxFooter();
 
