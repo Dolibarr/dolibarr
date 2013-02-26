bdcb6bdef517ca3266828d02c1d637b5e001028b
Laurent Destailleur <eldy@destailleur.fr>
2013-02-26 20:15:20 +0100

    Try to aplpy enhancements defined into http://www.dolibarr.fr/forum/527-bugs-sur-la-version-stable-courante/38958-resolu-probleme-exports#38968

--- htdocs/core/modules/export/export_excel2007.modules.php
+++ htdocs/core/modules/export/export_excel2007.modules.php
@@ -29,8 +29,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 
 
 /**
- *	\class      ExportExcel2007
- *	\brief      Class to build export files with Excel format
+ *	Class to build export files with Excel format
  */
 class ExportExcel2007 extends ExportExcel
 {
@@ -277,7 +276,8 @@ class ExportExcel2007 extends ExportExcel
 
 		foreach($array_selected_sorted as $code => $value)
 		{
-			$alias=str_replace(array('.','-'),'_',$code);
+			if (strpos($code,' as ') == 0) $alias=str_replace(array('.','-'),'_',$code);
+			else $alias=substr($code, strpos($code, ' as ') + 4);
             if (empty($alias)) dol_print_error('','Bad value for field with code='.$code.'. Try to redefine export.');
             $newvalue=$objp->$alias;
 
