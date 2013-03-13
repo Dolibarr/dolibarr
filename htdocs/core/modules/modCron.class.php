277784af2525b9a35a5540b1b65e62123483639d
Laurent Destailleur <eldy@destailleur.fr>
2013-03-13 19:55:21 +0100

    New: Start to work on a cron module

---
+++ htdocs/core/modules/modCron.class.php
@@ -0,0 +1,124 @@
+<?php
+/* Copyright (C) 2013 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *      \defgroup   webservices     Module webservices
+ *      \brief      Module to enable the Dolibarr server of web services
+ *		\file       htdocs/core/modules/modCron.class.php
+ *      \ingroup    cron
+ *      \brief      File to describe cron module
+ */
+include_once DOL_DOCUMENT_ROOT .'/core/modules/DolibarrModules.class.php';
+
+/**
+ *	Class to describe a Cron module
+ */
+class modCron extends DolibarrModules
+{
+
+    /**
+	 *   Constructor. Define names, constants, directories, boxes, permissions
+	 *
+	 *   @param      DoliDB		$db      Database handler
+     */
+    function __construct($db)
+    {
+        $this->db = $db;
+        $this->numero = 2300;
+
+		// Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
+		// It is used to group modules in module setup page
+        $this->family = "technic";
+        // Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
+        $this->name = preg_replace('/^mod/i','',get_class($this));
+        $this->description = "Enable the Dolibarr cron service";
+        $this->version = 'experimental';                        // 'experimental' or 'dolibarr' or version
+        // Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
+        $this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
+        // Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
+        $this->special = 2;
+        // Name of image file used for this module.
+        $this->picto='technic';
+
+        // Data directories to create when module is enabled
+        $this->dirs = array();
+
+        // Config pages
+        //-------------
+        $this->config_page_url = array("cron.php@cron");
+
+        // Dependancies
+        //-------------
+        $this->depends = array();
+        $this->requiredby = array();
+        $this->langfiles = array("cron");
+
+        // Constantes
+        //-----------
+        $this->const = array();
+
+        // New pages on tabs
+        // -----------------
+        $this->tabs = array();
+
+        // Boxes
+        //------
+        $this->boxes = array();
+
+        // Permissions
+        //------------
+        $this->rights = array();
+        $this->rights_class = 'cron';
+        $r=0;
+    }
+
+
+    /**
+	 *		Function called when module is enabled.
+	 *		The init function add constants, boxes, permissions and menus (defined in constructor) into Dolibarr database.
+	 *		It also creates data directories
+	 *
+     *      @param      string	$options    Options when enabling module ('', 'noboxes')
+	 *      @return     int             	1 if OK, 0 if KO
+     */
+    function init($options='')
+    {
+        // Prevent pb of modules not correctly disabled
+        //$this->remove($options);
+
+        $sql = array();
+
+        return $this->_init($sql,$options);
+    }
+
+    /**
+	 *		Function called when module is disabled.
+	 *      Remove from database constants, boxes and permissions from Dolibarr database.
+	 *		Data directories are not deleted
+	 *
+     *      @param      string	$options    Options when enabling module ('', 'noboxes')
+	 *      @return     int             	1 if OK, 0 if KO
+     */
+    function remove($options='')
+    {
+		$sql = array();
+
+		return $this->_remove($sql,$options);
+    }
+
+}
+?>
---
+++ htdocs/cron/admin/cron.php
@@ -0,0 +1,132 @@
+<?php
+/* Copyright (C) 2004		Rodolphe Quiedeville	<rodolphe@quiedeville.org>
+ * Copyright (C) 2005-2013	Laurent Destailleur		<eldy@users.sourceforge.org>
+ * Copyright (C) 2011		Juanjo Menent			<jmenent@2byte.es>
+ * Copyright (C) 2012		Regis Houssin			<regis.houssin@capnetworks.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *      \file       htdocs/cron/admin/cron.php
+ *		\ingroup    cron
+ *		\brief      Page to setup cron module
+ */
+
+require '../../main.inc.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
+
+$langs->load("admin");
+$langs->load("cron");
+
+if (! $user->admin)
+	accessforbidden();
+
+$actionsave=GETPOST("save");
+
+// Sauvegardes parametres
+if ($actionsave)
+{
+    $i=0;
+
+    $db->begin();
+
+    $i+=dolibarr_set_const($db,'CRON_KEY',trim(GETPOST("CRON_KEY")),'chaine',0,'',$conf->entity);
+
+    if ($i >= 1)
+    {
+        $db->commit();
+        setEventMessage($langs->trans("SetupSaved"));
+    }
+    else
+    {
+        $db->rollback();
+        setEventMessage($langs->trans("Error"), 'errors');
+    }
+}
+
+
+/*
+ *	View
+ */
+
+llxHeader();
+
+$linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToModuleList").'</a>';
+print_fiche_titre($langs->trans("CronSetup"),$linkback,'setup');
+
+print $langs->trans("CronDesc")."<br>\n";
+print "<br>\n";
+
+print '<form name="agendasetupform" action="'.$_SERVER["PHP_SELF"].'" method="post">';
+print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+print '<table class="noborder" width="100%">';
+
+print '<tr class="liste_titre">';
+print "<td>".$langs->trans("Parameter")."</td>";
+print "<td>".$langs->trans("Value")."</td>";
+//print "<td>".$langs->trans("Examples")."</td>";
+print "<td>&nbsp;</td>";
+print "</tr>";
+
+print '<tr class="impair">';
+print '<td class="fieldrequired">'.$langs->trans("KeyForCronAccess").'</td>';
+print '<td><input type="text" class="flat" id="CRON_KEY" name="CRON_KEY" value="'. (GETPOST('CRON_KEY')?GETPOST('CRON_KEY'):(! empty($conf->global->CRON_KEY)?$conf->global->CRON_KEY:'')) . '" size="40">';
+if (! empty($conf->use_javascript_ajax))
+	print '&nbsp;'.img_picto($langs->trans('Generate'), 'refresh', 'id="generate_token" class="linkobject"');
+print '</td>';
+print '<td>&nbsp;</td>';
+print '</tr>';
+
+print '</table>';
+
+print '<br><center>';
+print '<input type="submit" name="save" class="button" value="'.$langs->trans("Save").'">';
+print '</center>';
+
+print '</form>';
+
+print '<br><br>';
+
+
+// Cron launch
+print '<u>'.$langs->trans("URLToLaunchCronJobs").':</u><br>';
+$url=DOL_MAIN_URL_ROOT.'/cron/cron_run_jobs.php'.(empty($conf->global->CRON_KEY)?'':'?securitykey='.$conf->global->CRON_KEY);
+print img_picto('','object_globe.png').' <a href="'.$url.'" target="_blank">'.$url."</a><br>\n";
+print '<br>';
+
+
+print '<br>';
+
+if (! empty($conf->use_javascript_ajax))
+{
+	print "\n".'<script type="text/javascript">';
+	print '$(document).ready(function () {
+            $("#generate_token").click(function() {
+            	$.get( "'.DOL_URL_ROOT.'/core/ajax/security.php", {
+            		action: \'getrandompassword\',
+            		generic: true
+				},
+				function(token) {
+					$("#CRON_KEY").val(token);
+				});
+            });
+    });';
+	print '</script>';
+}
+
+
+llxFooter();
+$db->close();
+?>
