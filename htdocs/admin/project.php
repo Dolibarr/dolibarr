b5751ab6586a497332b5113e07a9b1843f129a3a
fhenry <florian.henry@open-concept.pro>
2013-03-25 19:31:17 +0100

    Add copyright

--- htdocs/admin/project.php
+++ htdocs/admin/project.php
@@ -1,21 +1,22 @@
 <?php
 /* Copyright (C) 2010 		Regis Houssin		<regis.houssin@capnetworks.com>
  * Copyright (C) 2011 		Laurent Destailleur	<eldy@users.sourceforge.net>
-* Copyright (C) 2011-2012 	Juanjo Menent		<jmenent@2byte.es>
-* Copyright (C) 2011-2013  Philippe Grand	    <philippe.grand@atoo-net.com>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2011-2012 	Juanjo Menent		<jmenent@2byte.es>
+ * Copyright (C) 2011-2013  Philippe Grand	    <philippe.grand@atoo-net.com>
+ * Copyright (C) 2013	   Florian Henry        <florian.henry@open-concept.pro>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /**
