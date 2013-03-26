e9a8457d7ed2b75f7801cb4b9699dd2e5a506d13
Regis Houssin <regis.houssin@capnetworks.com>
2013-03-26 17:29:40 +0100

    Fix: broken features due to the removal of the trigger FILE_UPLOAD !

--- htdocs/core/lib/files.lib.php
+++ htdocs/core/lib/files.lib.php
@@ -649,7 +649,7 @@ function dol_move_uploaded_file($src_file, $dest_file, $allowoverwrite, $disable
 
 		$hookmanager->initHooks(array('fileslib'));
 
-		$parameters=array('filename' => $file_name, 'varfiles' => $varfiles, 'allowoverwrite' => $allowoverwrite);
+		$parameters=array('src_file' => $src_file, 'file_name' => $file_name, 'varfiles' => $varfiles, 'allowoverwrite' => $allowoverwrite);
 		$reshook=$hookmanager->executeHooks('moveUploadedFile', $parameters, $object);
 	}
 
