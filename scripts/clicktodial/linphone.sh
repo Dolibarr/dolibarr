a0dc4ae62ebeafb17ce86a177ed701ee7395b1af
Laurent Destailleur <eldy@destailleur.fr>
2013-03-31 04:12:20 +0200

    Avoid terminal

--- scripts/clicktodial/linphone.sh
+++ scripts/clicktodial/linphone.sh
@@ -6,6 +6,13 @@
 # More information on http://wiki.dolibarr.org/index.php/Module_ClickToDial_En
 #---------------------------------------------------------
 
+# Note: Adding handler into gconf-tools seems to do nothing
+# gconftool-2 -t string -s /desktop/gnome/url-handlers/sip/command "linphone-3 -c %s"
+# gconftool-2 -s /desktop/gnome/url-handlers/sip/needs_terminal false -t bool
+# gconftool-2 -t bool -s /desktop/gnome/url-handlers/sip/enabled true
+
 echo Launch Linphone $1 $2
+
 param=`echo $1 | sed -s 's/^sip:[\/]*//' `
-/usr/bin/linphone-3 -c $param
+
+/usr/bin/linphone-3 -c $param &
