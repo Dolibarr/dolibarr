538194f43579574333c2d9adcc9df68e20f0ee61
Laurent Destailleur <eldy@destailleur.fr>
2013-04-09 23:05:50 +0200

    New: Add module opensurvey. Final. Test are welcome.

---
+++ htdocs/opensurvey/class/opensurveysondage.class.php
@@ -0,0 +1,512 @@
+<?php
+/* Copyright (C) 2013 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  \file       dev/skeletons/opensurveysondage.class.php
+ *  \ingroup    mymodule othermodule1 othermodule2
+ *  \brief      This file is an example for a CRUD class file (Create/Read/Update/Delete)
+ *				Initialy built by build_class_from_table on 2013-03-10 00:32
+ */
+
+// Put here all includes required by your class file
+require_once(DOL_DOCUMENT_ROOT."/core/class/commonobject.class.php");
+//require_once(DOL_DOCUMENT_ROOT."/societe/class/societe.class.php");
+//require_once(DOL_DOCUMENT_ROOT."/product/class/product.class.php");
+
+
+/**
+ *	Put here description of your class
+ */
+class Opensurveysondage extends CommonObject
+{
+	var $db;							//!< To store db handler
+	var $error;							//!< To return error code (or message)
+	var $errors=array();				//!< To return several error codes (or messages)
+	var $element='opensurvey_sondage';			//!< Id that identify managed objects
+	var $table_element='opensurvey_sondage';	//!< Name of table without prefix where object is stored
+
+    var $id;
+
+	var $id_sondage;
+	var $commentaires;
+	var $mail_admin;
+	var $nom_admin;
+	var $titre;
+	var $id_sondage_admin;
+	var $date_fin='';
+	var $format;
+	var $mailsonde;
+	var $survey_link_visible;
+	var $canedit;
+
+
+
+
+    /**
+     *  Constructor
+     *
+     *  @param	DoliDb		$db      Database handler
+     */
+    function __construct($db)
+    {
+        $this->db = $db;
+        return 1;
+    }
+
+
+    /**
+     *  Create object into database
+     *
+     *  @param	User	$user        User that creates
+     *  @param  int		$notrigger   0=launch triggers after, 1=disable triggers
+     *  @return int      		   	 <0 if KO, Id of created object if OK
+     */
+    function create($user, $notrigger=0)
+    {
+    	global $conf, $langs;
+		$error=0;
+
+		// Clean parameters
+
+		if (isset($this->id_sondage)) $this->id_sondage=trim($this->id_sondage);
+		if (isset($this->commentaires)) $this->commentaires=trim($this->commentaires);
+		if (isset($this->mail_admin)) $this->mail_admin=trim($this->mail_admin);
+		if (isset($this->nom_admin)) $this->nom_admin=trim($this->nom_admin);
+		if (isset($this->titre)) $this->titre=trim($this->titre);
+		if (isset($this->id_sondage_admin)) $this->id_sondage_admin=trim($this->id_sondage_admin);
+		if (isset($this->format)) $this->format=trim($this->format);
+		if (isset($this->mailsonde)) $this->mailsonde=trim($this->mailsonde);
+		if (isset($this->survey_link_visible)) $this->survey_link_visible=trim($this->survey_link_visible);
+		if (isset($this->canedit)) $this->canedit=trim($this->canedit);
+
+
+
+		// Check parameters
+		// Put here code to add control on parameters values
+
+        // Insert request
+		$sql = "INSERT INTO ".MAIN_DB_PREFIX."opensurvey_sondage(";
+
+		$sql.= "id_sondage,";
+		$sql.= "commentaires,";
+		$sql.= "mail_admin,";
+		$sql.= "nom_admin,";
+		$sql.= "titre,";
+		$sql.= "id_sondage_admin,";
+		$sql.= "date_fin,";
+		$sql.= "format,";
+		$sql.= "mailsonde,";
+		$sql.= "survey_link_visible,";
+		$sql.= "canedit";
+        $sql.= ") VALUES (";
+
+		$sql.= " ".(! isset($this->id_sondage)?'NULL':"'".$this->id_sondage."'").",";
+		$sql.= " ".(! isset($this->commentaires)?'NULL':"'".$this->db->escape($this->commentaires)."'").",";
+		$sql.= " ".(! isset($this->mail_admin)?'NULL':"'".$this->db->escape($this->mail_admin)."'").",";
+		$sql.= " ".(! isset($this->nom_admin)?'NULL':"'".$this->db->escape($this->nom_admin)."'").",";
+		$sql.= " ".(! isset($this->titre)?'NULL':"'".$this->db->escape($this->titre)."'").",";
+		$sql.= " ".(! isset($this->id_sondage_admin)?'NULL':"'".$this->id_sondage_admin."'").",";
+		$sql.= " ".(! isset($this->date_fin) || dol_strlen($this->date_fin)==0?'NULL':$this->db->idate($this->date_fin)).",";
+		$sql.= " ".(! isset($this->format)?'NULL':"'".$this->db->escape($this->format)."'").",";
+		$sql.= " ".(! isset($this->mailsonde)?'NULL':"'".$this->mailsonde."'").",";
+		$sql.= " ".(! isset($this->survey_link_visible)?'NULL':"'".$this->survey_link_visible."'").",";
+		$sql.= " ".(! isset($this->canedit)?'NULL':"'".$this->canedit."'")."";
+
+		$sql.= ")";
+
+		$this->db->begin();
+
+	   	dol_syslog(get_class($this)."::create sql=".$sql, LOG_DEBUG);
+        $resql=$this->db->query($sql);
+    	if (! $resql) { $error++; $this->errors[]="Error ".$this->db->lasterror(); }
+
+		if (! $error)
+        {
+            $this->id = $this->db->last_insert_id(MAIN_DB_PREFIX."opensurvey_sondage");
+
+			if (! $notrigger)
+			{
+	            // Uncomment this and change MYOBJECT to your own tag if you
+	            // want this action calls a trigger.
+
+	            //// Call triggers
+	            //include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
+	            //$interface=new Interfaces($this->db);
+	            //$result=$interface->run_triggers('MYOBJECT_CREATE',$this,$user,$langs,$conf);
+	            //if ($result < 0) { $error++; $this->errors=$interface->errors; }
+	            //// End call triggers
+			}
+        }
+
+        // Commit or rollback
+        if ($error)
+		{
+			foreach($this->errors as $errmsg)
+			{
+	            dol_syslog(get_class($this)."::create ".$errmsg, LOG_ERR);
+	            $this->error.=($this->error?', '.$errmsg:$errmsg);
+			}
+			$this->db->rollback();
+			return -1*$error;
+		}
+		else
+		{
+			$this->db->commit();
+            return $this->id;
+		}
+    }
+
+
+    /**
+     *  Load object in memory from the database
+     *
+     *  @param	int		$id    				Id object
+     *  @param	string	$numsurvey			Ref of survey (admin or not)
+     *  @return int          				<0 if KO, >0 if OK
+     */
+    function fetch($id,$numsurvey='')
+    {
+    	global $langs;
+
+    	$sql = "SELECT";
+		//$sql.= " t.rowid,";
+		$sql.= " t.id_sondage,";
+		$sql.= " t.commentaires,";
+		$sql.= " t.mail_admin,";
+		$sql.= " t.nom_admin,";
+		$sql.= " t.titre,";
+		$sql.= " t.id_sondage_admin,";
+		$sql.= " t.date_fin,";
+		$sql.= " t.format,";
+		$sql.= " t.mailsonde,";
+		$sql.= " t.survey_link_visible,";
+		$sql.= " t.canedit,";
+		$sql.= " t.sujet,";
+		$sql.= " t.tms";
+        $sql.= " FROM ".MAIN_DB_PREFIX."opensurvey_sondage as t";
+        if ($id > 0) $sql.= " WHERE t.rowid = ".$id;
+        else if (strlen($numsurvey) == 16) $sql.= " WHERE t.id_sondage = '".$numsurvey."'";
+        else $sql.= " WHERE t.id_sondage_admin = '".$numsurvey."'";
+
+    	dol_syslog(get_class($this)."::fetch sql=".$sql, LOG_DEBUG);
+        $resql=$this->db->query($sql);
+        if ($resql)
+        {
+            if ($this->db->num_rows($resql))
+            {
+                $obj = $this->db->fetch_object($resql);
+
+                //$this->id  = $obj->rowid;
+				$this->ref = $obj->id_sondage_admin;
+
+				$this->id_sondage = $obj->id_sondage;
+				$this->commentaires = $obj->commentaires;
+				$this->mail_admin = $obj->mail_admin;
+				$this->nom_admin = $obj->nom_admin;
+				$this->titre = $obj->titre;
+				$this->id_sondage_admin = $obj->id_sondage_admin;
+				$this->date_fin = $this->db->jdate($obj->date_fin);
+				$this->format = $obj->format;
+				$this->mailsonde = $obj->mailsonde;
+				$this->survey_link_visible = $obj->survey_link_visible;
+				$this->canedit = $obj->canedit;
+				$this->sujet = $obj->sujet;
+
+				$this->date_m = $this->db->jdate($obj->tls);
+				$ret=1;
+            }
+            else $ret=0;
+
+            $this->db->free($resql);
+        }
+        else
+       {
+      	    $this->error="Error ".$this->db->lasterror();
+            dol_syslog(get_class($this)."::fetch ".$this->error, LOG_ERR);
+            $ret=-1;
+        }
+
+        return $ret;
+    }
+
+
+    /**
+     *  Update object into database
+     *
+     *  @param	User	$user        User that modifies
+     *  @param  int		$notrigger	 0=launch triggers after, 1=disable triggers
+     *  @return int     		   	 <0 if KO, >0 if OK
+     */
+    function update($user=0, $notrigger=0)
+    {
+    	global $conf, $langs;
+		$error=0;
+
+		// Clean parameters
+
+		if (isset($this->id_sondage)) $this->id_sondage=trim($this->id_sondage);
+		if (isset($this->commentaires)) $this->commentaires=trim($this->commentaires);
+		if (isset($this->mail_admin)) $this->mail_admin=trim($this->mail_admin);
+		if (isset($this->nom_admin)) $this->nom_admin=trim($this->nom_admin);
+		if (isset($this->titre)) $this->titre=trim($this->titre);
+		if (isset($this->id_sondage_admin)) $this->id_sondage_admin=trim($this->id_sondage_admin);
+		if (isset($this->format)) $this->format=trim($this->format);
+		if (isset($this->mailsonde)) $this->mailsonde=trim($this->mailsonde);
+		if (isset($this->survey_link_visible)) $this->survey_link_visible=trim($this->survey_link_visible);
+		if (isset($this->canedit)) $this->canedit=trim($this->canedit);
+
+
+		// Check parameters
+		// Put here code to add a control on parameters values
+
+        // Update request
+        $sql = "UPDATE ".MAIN_DB_PREFIX."opensurvey_sondage SET";
+
+		$sql.= " id_sondage='".(isset($this->id_sondage)?$this->id_sondage:"null")."',";
+		$sql.= " commentaires=".(isset($this->commentaires)?"'".$this->db->escape($this->commentaires)."'":"null").",";
+		$sql.= " mail_admin=".(isset($this->mail_admin)?"'".$this->db->escape($this->mail_admin)."'":"null").",";
+		$sql.= " nom_admin=".(isset($this->nom_admin)?"'".$this->db->escape($this->nom_admin)."'":"null").",";
+		$sql.= " titre=".(isset($this->titre)?"'".$this->db->escape($this->titre)."'":"null").",";
+		$sql.= " id_sondage_admin='".(isset($this->id_sondage_admin)?$this->id_sondage_admin:"null")."',";
+		$sql.= " date_fin=".(dol_strlen($this->date_fin)!=0 ? "'".$this->db->idate($this->date_fin)."'" : 'null').",";
+		$sql.= " format=".(isset($this->format)?"'".$this->db->escape($this->format)."'":"null").",";
+		$sql.= " mailsonde=".(isset($this->mailsonde)?$this->mailsonde:"null").",";
+		$sql.= " survey_link_visible=".(isset($this->survey_link_visible)?$this->survey_link_visible:"null").",";
+		$sql.= " canedit=".(isset($this->canedit)?$this->canedit:"null")."";
+
+        //$sql.= " WHERE rowid=".$this->id;
+		$sql.= " WHERE id_sondage_admin='".$this->id_sondage_admin."'";
+
+		$this->db->begin();
+
+		dol_syslog(get_class($this)."::update sql=".$sql, LOG_DEBUG);
+        $resql = $this->db->query($sql);
+    	if (! $resql) { $error++; $this->errors[]="Error ".$this->db->lasterror(); }
+
+		if (! $error)
+		{
+			if (! $notrigger)
+			{
+	            // Uncomment this and change MYOBJECT to your own tag if you
+	            // want this action calls a trigger.
+
+	            //// Call triggers
+	            //include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
+	            //$interface=new Interfaces($this->db);
+	            //$result=$interface->run_triggers('MYOBJECT_MODIFY',$this,$user,$langs,$conf);
+	            //if ($result < 0) { $error++; $this->errors=$interface->errors; }
+	            //// End call triggers
+	    	}
+		}
+
+        // Commit or rollback
+		if ($error)
+		{
+			foreach($this->errors as $errmsg)
+			{
+	            dol_syslog(get_class($this)."::update ".$errmsg, LOG_ERR);
+	            $this->error.=($this->error?', '.$errmsg:$errmsg);
+			}
+			$this->db->rollback();
+			return -1*$error;
+		}
+		else
+		{
+			$this->db->commit();
+			return 1;
+		}
+    }
+
+
+ 	/**
+	 *  Delete object in database
+	 *
+     *	@param  User	$user        		User that deletes
+     *  @param  int		$notrigger	 		0=launch triggers after, 1=disable triggers
+     *  @param	string	$numsondageadmin	Num sondage to delete
+	 *  @return	int					 		<0 if KO, >0 if OK
+	 */
+	function delete($user, $notrigger=0, $numsondageadmin)
+	{
+		global $conf, $langs;
+		$error=0;
+
+		$this->db->begin();
+
+		if (! $error)
+		{
+			if (! $notrigger)
+			{
+				// Uncomment this and change MYOBJECT to your own tag if you
+		        // want this action calls a trigger.
+
+		        //// Call triggers
+		        //include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
+		        //$interface=new Interfaces($this->db);
+		        //$result=$interface->run_triggers('MYOBJECT_DELETE',$this,$user,$langs,$conf);
+		        //if ($result < 0) { $error++; $this->errors=$interface->errors; }
+		        //// End call triggers
+			}
+		}
+
+		if (! $error)
+		{
+
+			$sql='DELETE FROM '.MAIN_DB_PREFIX."opensurvey_comments WHERE id_sondage_admin = '".$numsondageadmin."'";
+			dol_syslog(get_class($this)."::delete sql=".$sql, LOG_DEBUG);
+			$resql=$this->db->query($sql);
+			$sql='DELETE FROM '.MAIN_DB_PREFIX."opensurvey_user_studs WHERE id_sondage_admin = '".$numsondageadmin."'";
+			dol_syslog(get_class($this)."::delete sql=".$sql, LOG_DEBUG);
+			$resql=$this->db->query($sql);
+
+    		$sql = "DELETE FROM ".MAIN_DB_PREFIX."opensurvey_sondage";
+    		$sql.= " WHERE id_sondage_admin = '".$numsondageadmin."'";
+
+    		dol_syslog(get_class($this)."::delete sql=".$sql);
+    		$resql = $this->db->query($sql);
+        	if (! $resql) { $error++; $this->errors[]="Error ".$this->db->lasterror(); }
+		}
+
+        // Commit or rollback
+		if ($error)
+		{
+			foreach($this->errors as $errmsg)
+			{
+	            dol_syslog(get_class($this)."::delete ".$errmsg, LOG_ERR);
+	            $this->error.=($this->error?', '.$errmsg:$errmsg);
+			}
+			$this->db->rollback();
+			return -1*$error;
+		}
+		else
+		{
+			$this->db->commit();
+			return 1;
+		}
+	}
+
+	/**
+	 * Return array of lines
+	 *
+	 * @return 	array	Array of lines
+	 */
+	function fetch_lines()
+	{
+		$ret=array();
+		$sql = "SELECT id_users, nom, reponses FROM ".MAIN_DB_PREFIX."opensurvey_user_studs";
+		$sql.= " WHERE id_sondage = '".$this->id_sondage."'";
+		$resql=$this->db->query($sql);
+
+		if ($resql)
+		{
+			$num=$this->db->num_rows($resql);
+			$i=0;
+			while ($i < $num)
+			{
+				$obj=$this->db->fetch_object($resql);
+				$tmp=array('id_users'=>$obj->id_users, 'nom'=>$obj->nom, 'reponses'=>$obj->reponses);
+
+				$ret[]=$tmp;
+				$i++;
+			}
+		}
+		else dol_print_error($this->db);
+
+		$this->lines=$ret;
+
+		return $this->lines;
+	}
+
+	/**
+	 *	Load an object from its id and create a new one in database
+	 *
+	 *	@param	int		$fromid     Id of object to clone
+	 * 	@return	int					New id of clone
+	 */
+	function createFromClone($fromid)
+	{
+		global $user,$langs;
+
+		$error=0;
+
+		$object=new Opensurveysondage($this->db);
+
+		$this->db->begin();
+
+		// Load source object
+		$object->fetch($fromid);
+		$object->id=0;
+		$object->statut=0;
+
+		// Clear fields
+		// ...
+
+		// Create clone
+		$result=$object->create($user);
+
+		// Other options
+		if ($result < 0)
+		{
+			$this->error=$object->error;
+			$error++;
+		}
+
+		if (! $error)
+		{
+
+
+		}
+
+		// End
+		if (! $error)
+		{
+			$this->db->commit();
+			return $object->id;
+		}
+		else
+		{
+			$this->db->rollback();
+			return -1;
+		}
+	}
+
+
+	/**
+	 *	Initialise object with example values
+	 *	Id must be 0 if object instance is a specimen
+	 *
+	 *	@return	void
+	 */
+	function initAsSpecimen()
+	{
+		$this->id=0;
+
+		$this->id_sondage='';
+		$this->commentaires='';
+		$this->mail_admin='';
+		$this->nom_admin='';
+		$this->titre='';
+		$this->id_sondage_admin='';
+		$this->date_fin='';
+		$this->format='';
+		$this->mailsonde='';
+		$this->survey_link_visible='';
+		$this->canedit=0;
+	}
+
+}
+?>
---
+++ htdocs/opensurvey/fonctions.php
@@ -0,0 +1,292 @@
+<?php
+/* Copyright (C) 2013 Laurent Destailleur  <eldy@users.sourceforge.net>
+ *
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ *	\file       htdocs/opensurvey/fonction.php
+ *	\ingroup    opensurvey
+ *	\brief      Functions for module
+ */
+
+
+
+/**
+ * Show header for new member
+ *
+ * @param 	string		$title				Title
+ * @param 	string		$head				Head array
+ * @param 	int    		$disablejs			More content into html header
+ * @param 	int    		$disablehead		More content into html header
+ * @param 	array  		$arrayofjs			Array of complementary js files
+ * @param 	array  		$arrayofcss			Array of complementary css files
+ * @return	void
+ */
+function llxHeaderSurvey($title, $head="", $disablejs=0, $disablehead=0, $arrayofjs='', $arrayofcss='')
+{
+	global $user, $conf, $langs, $mysoc;
+
+	top_htmlhead($head, $title, $disablejs, $disablehead, $arrayofjs, $arrayofcss); // Show html headers
+	print '<body id="mainbody" class="publicnewmemberform" style="margin-top: 10px;">';
+
+	showlogo();
+
+	print '<div style="margin-left: 50px; margin-right: 50px;">';
+}
+
+/**
+ * Show footer for new member
+ *
+ * @return	void
+ */
+function llxFooterSurvey()
+{
+	print '</div>';
+
+	printCommonFooter('public');
+
+	dol_htmloutput_events();
+
+	print "</body>\n";
+	print "</html>\n";
+}
+
+
+/**
+ * Show logo
+ *
+ * @return	void
+ */
+function showlogo()
+{
+	global $user, $conf, $langs, $mysoc;
+
+	// Print logo
+	$urllogo=DOL_URL_ROOT.'/theme/login_logo.png';
+
+	if (! empty($mysoc->logo_small) && is_readable($conf->mycompany->dir_output.'/logos/thumbs/'.$mysoc->logo_small))
+	{
+		$urllogo=DOL_URL_ROOT.'/viewimage.php?cache=1&amp;modulepart=companylogo&amp;file='.urlencode('thumbs/'.$mysoc->logo_small);
+	}
+	elseif (! empty($mysoc->logo) && is_readable($conf->mycompany->dir_output.'/logos/'.$mysoc->logo))
+	{
+		$urllogo=DOL_URL_ROOT.'/viewimage.php?cache=1&amp;modulepart=companylogo&amp;file='.urlencode($mysoc->logo);
+		$width=128;
+	}
+	elseif (is_readable(DOL_DOCUMENT_ROOT.'/theme/dolibarr_logo.png'))
+	{
+		$urllogo=DOL_URL_ROOT.'/theme/dolibarr_logo.png';
+	}
+	print '<center>';
+	print '<img alt="Logo" id="logosubscribe" title="" src="'.$urllogo.'" style="max-width: 120px" /><br>';
+	print '<strong>'.$langs->trans("OpenSurvey").'</strong>';
+	print '</center><br>';
+}
+
+
+/**
+ * get_server_name
+ *
+ * @return	string		URL to use
+ */
+function get_server_name()
+{
+	$urlwithouturlroot=preg_replace('/'.preg_quote(DOL_URL_ROOT,'/').'$/i','',trim($dolibarr_main_url_root));
+	$urlwithroot=$urlwithouturlroot.DOL_URL_ROOT;		// This is to use external domain name found into config file
+	//$urlwithroot=DOL_MAIN_URL_ROOT;					// This is to use same domain name than current
+
+	$url=$urlwithouturlroot.dol_buildpath('/opensurvey/',1);
+
+	if (!preg_match("|/$|", $url)) {
+		$url = $url."/";
+	}
+
+	return $url;
+}
+
+
+/**
+ * is_error
+ *
+ * @param unknown_type $cerr
+ * @return boolean
+ */
+function is_error($cerr)
+{
+	global $err;
+	if ( $err == 0 ) {
+		return false;
+	}
+
+	return (($err & $cerr) != 0 );
+}
+
+
+
+/**
+ * Vérifie une adresse e-mail selon les normes RFC
+ *
+ * @param  	string  $email  l'adresse e-mail a vérifier
+ * @return  bool    vrai si l'adresse est correcte, faux sinon
+ * @see 	http://fightingforalostcause.net/misc/2006/compare-email-regex.php
+ * @see 	http://svn.php.net/viewvc/php/php-src/trunk/ext/filter/logical_filters.c?view=markup
+ */
+function validateEmail($email)
+{
+	$pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
+
+	return (bool)preg_match($pattern, $email);
+}
+
+
+/**
+ * Fonction vérifiant l'existance et la valeur non vide d'une clé d'un tableau
+ *
+ * @param   string  $name       La clé à tester
+ * @param   array   $tableau    Le tableau où rechercher la clé ($_POST par défaut)
+ * @return  bool                Vrai si la clé existe et renvoie une valeur non vide
+ */
+function issetAndNoEmpty($name, $tableau = null)
+{
+	if ($tableau === null) {
+		$tableau = $_POST;
+	}
+
+	return (isset($tableau[$name]) === true && empty($tableau[$name]) === false);
+}
+
+
+/**
+ * Fonction permettant de générer les URL pour les sondage
+ *
+ * @param   string    $id     L'identifiant du sondage
+ * @param   bool      $admin  True pour générer une URL pour l'administration d'un sondage, False pour un URL publique
+ * @return  string            L'url pour le sondage
+ */
+function getUrlSondage($id, $admin = false)
+{
+	if ($admin === true) {
+		$url = get_server_name().'adminstuds_preview.php?sondage='.$id;
+	} else {
+		$url = get_server_name().'/public/studs.php?sondage='.$id;
+	}
+
+	return $url;
+}
+
+
+/**
+ * Generate a random id
+ *
+ * @return	void
+ */
+function dol_survey_random($car)
+{
+	$string = "";
+	$chaine = "abcdefghijklmnopqrstuvwxyz123456789";
+	srand((double)microtime()*1000000);
+	for($i=0; $i<$car; $i++) {
+		$string .= $chaine[rand()%strlen($chaine)];
+	}
+	return $string;
+}
+
+/**
+ * Add a poll
+ *
+ * @param	string	$origin		Origin of poll creation
+ * @return	void
+ */
+function ajouter_sondage($origin)
+{
+	global $conf, $db;
+
+	$sondage=dol_survey_random(16);
+	$sondage_admin=$sondage.dol_survey_random(8);
+
+	if ($_SESSION["formatsondage"]=="A"||$_SESSION["formatsondage"]=="A+") {
+		//extraction de la date de fin choisie
+		if ($_SESSION["champdatefin"]) {
+			if ($_SESSION["champdatefin"]>time()+250000) {
+				$date_fin=$_SESSION["champdatefin"];
+			}
+		} else {
+			$date_fin=time()+15552000;
+		}
+	}
+
+	if ($_SESSION["formatsondage"]=="D"||$_SESSION["formatsondage"]=="D+") {
+		//Calcul de la date de fin du sondage
+		$taille_tableau=sizeof($_SESSION["totalchoixjour"])-1;
+		$date_fin=$_SESSION["totalchoixjour"][$taille_tableau]+200000;
+	}
+
+	if (is_numeric($date_fin) === false) {
+		$date_fin = time()+15552000;
+	}
+	$canedit=empty($_SESSION['formatcanedit'])?'0':'1';
+
+	// Insert survey
+	$sql = 'INSERT INTO '.MAIN_DB_PREFIX.'opensurvey_sondage';
+	$sql.= '(id_sondage, commentaires, mail_admin, nom_admin, titre, id_sondage_admin, date_fin, format, mailsonde, canedit, origin, sujet)';
+	$sql.= " VALUES ('".$db->escape($sondage)."', '".$db->escape($_SESSION['commentaires'])."', '".$db->escape($_SESSION['adresse'])."', '".$db->escape($_SESSION['nom'])."',";
+	$sql.= " '".$db->escape($_SESSION['titre'])."', '".$sondage_admin."', '".$db->idate($date_fin)."', '".$_SESSION['formatsondage']."', '".$db->escape($_SESSION['mailsonde'])."',";
+	$sql.= " '".$canedit."', '".$db->escape($origin)."',";
+	$sql.= " '".$db->escape($_SESSION['toutchoix'])."'";
+	$sql.= ")";
+	dol_syslog($sql);
+	$resql=$db->query($sql);
+
+	if ($origin == 'dolibarr') $urlback=dol_buildpath('/opensurvey/adminstuds_preview.php',1).'?sondage='.$sondage_admin;
+	else
+	{
+		// Define $urlwithroot
+		$urlwithouturlroot=preg_replace('/'.preg_quote(DOL_URL_ROOT,'/').'$/i','',trim($dolibarr_main_url_root));
+		$urlwithroot=$urlwithouturlroot.DOL_URL_ROOT;		// This is to use external domain name found into config file
+		//$urlwithroot=DOL_MAIN_URL_ROOT;					// This is to use same domain name than current
+
+		$url=$urlwithouturlroot.dol_buildpath('/opensurvey/public/studs.php',1).'?sondage='.$sondage;
+
+		$urlback=$url;
+
+		//var_dump($urlback);exit;
+	}
+
+	unset($_SESSION["titre"]);
+	unset($_SESSION["nom"]);
+	unset($_SESSION["adresse"]);
+	unset($_SESSION["commentaires"]);
+	unset($_SESSION["canedit"]);
+	unset($_SESSION["mailsonde"]);
+
+	header("Location: ".$urlback);
+	exit();
+}
+
+
+
+define('COMMENT_EMPTY',         0x0000000001);
+define('COMMENT_USER_EMPTY',    0x0000000010);
+define('COMMENT_INSERT_FAILED', 0x0000000100);
+define('NAME_EMPTY',            0x0000001000);
+define('NAME_TAKEN',            0x0000010000);
+define('NO_POLL',               0x0000100000);
+define('NO_POLL_ID',            0x0001000000);
+define('INVALID_EMAIL',         0x0010000000);
+define('TITLE_EMPTY',           0x0100000000);
+define('INVALID_DATE',          0x1000000000);
+$err = 0;
+
+?>
