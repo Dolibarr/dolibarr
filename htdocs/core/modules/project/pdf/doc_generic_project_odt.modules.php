629ef78dc82ec6985d848654388b9b495a9996f9
fhenry <florian.henry@open-concept.pro>
2013-04-11 18:38:50 +0200

    Add subtitution inside header and fotter document

--- htdocs/core/modules/project/pdf/doc_generic_project_odt.modules.php
+++ htdocs/core/modules/project/pdf/doc_generic_project_odt.modules.php
@@ -511,6 +511,7 @@ class doc_generic_project_odt extends ModelePDFProjects
 						else	// Text
 						{
 							$odfHandler->setVars($key, $value, true, 'UTF-8');
+							$odfHandler->setVarsHeadFooter($key, $value, true, 'UTF-8');
 						}
 					}
 					catch(OdfException $e)
