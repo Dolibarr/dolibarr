610fed6b3cc9ef0e76977e2b12edf604aea51c52
jfefe <jfefe@aternatik.fr>
2013-04-15 18:42:00 +0200

    Fix on extrafields date type : do not show current date when field not required

--- htdocs/core/class/extrafields.class.php
+++ htdocs/core/class/extrafields.class.php
@@ -627,6 +627,10 @@ class ExtraFields
         	$formstat = new Form($db);
         	
         	$showtime = in_array($type,array('datetime')) ? 1 : 0;
+        	// Do not show current date when field not required (see select_date() method)
+        	if(!$required && $value == '')
+        		$value = '-1';
+        	
         	$out = $formstat->select_date($value, 'options_'.$key, $showtime, $showtime, $required, '', 1, 1, 1, 0, 1);
         	//$out='<input type="text" name="options_'.$key.'" size="'.$showsize.'" maxlength="'.$newsize.'" value="'.$value.'"'.($moreparam?$moreparam:'').'>';
         }
