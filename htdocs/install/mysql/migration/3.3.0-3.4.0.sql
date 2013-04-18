2538a9e20e0888832d3300b71801c9ff5cc537a7
Regis Houssin <regis.houssin@capnetworks.com>
2013-04-18 09:39:43 +0200

    Fix: duplicate sql request

--- htdocs/install/mysql/migration/3.3.0-3.4.0.sql
+++ htdocs/install/mysql/migration/3.3.0-3.4.0.sql
@@ -266,7 +266,6 @@ ALTER TABLE llx_commande_extrafields ADD INDEX idx_commande_extrafields (fk_obje
 ALTER TABLE llx_socpeople ADD COLUMN note_public text after note;
 ALTER TABLE llx_societe ADD COLUMN note_public text after note;
 
-ALTER TABLE llx_facture_fourn_det ADD COLUMN info_bits integer NOT NULL DEFAULT 0 after date_end;
 ALTER TABLE llx_actioncomm ADD COLUMN transparency integer after fk_user_action;
 
 INSERT INTO llx_c_action_trigger (rowid,code,label,description,elementtype,rang) VALUES (29,'FICHINTER_SENTBYMAIL','Intervention sent by mail','Executed when a intervention is sent by mail','ficheinter',29);
