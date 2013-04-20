1e00ccd07dd0cda704028b0f6bf16f0af0aa2f9b
Marcos García <marcosgdf@gmail.com>
2013-04-20 12:59:34 +0200

    Purchases and sellsjournal used tabs in their copyright comment. Restored 'em

--- htdocs/compta/journal/purchasesjournal.php
+++ htdocs/compta/journal/purchasesjournal.php
@@ -3,8 +3,8 @@
  * Copyright (C) 2007-2010	Jean Heimburger		<jean@tiaris.info>
  * Copyright (C) 2011		Juanjo Menent		<jmenent@2byte.es>
  * Copyright (C) 2012		Regis Houssin		<regis.houssin@capnetworks.com>
- * Copyright (C) 2011-2012 Alexandre Spangaro	  <alexandre.spangaro@gmail.com>
- * Copyright (C) 2013       Marcos García       <marcosgdf@gmail.com>
+ * Copyright (C) 2011-2012	Alexandre spangaro	<alexandre.spangaro@gmail.com>
+ * Copyright (C) 2013		Marcos García		<marcosgdf@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
