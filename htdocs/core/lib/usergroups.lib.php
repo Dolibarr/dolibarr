62c9366d69bbd8688c9c8265e8aefcbd34b38735
fhenry <florian.henry@open-concept.pro>
2013-04-20 07:21:26 +0200

    fix debug

--- htdocs/core/lib/usergroups.lib.php
+++ htdocs/core/lib/usergroups.lib.php
@@ -78,7 +78,7 @@ function user_prepare_head($object)
 	    $head[$h][2] = 'clicktodial';
         $h++;
     }
-    print $user->societe_id;
+    
     // Show more tabs from modules
     // Entries must be declared in modules descriptor with line
     // $this->tabs = array('entity:+tabname:Title:@mymodule:/mymodule/mypage.php?id=__ID__');   to add new tab
