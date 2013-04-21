642ba10d84f038aca41bfdfc9b4f26d25292a587
JF FERRY <jfefe@aternatik.fr>
2013-04-21 17:04:54 +0200

    New hidden option to introduce timing value into odt document nam

--- htdocs/core/modules/societe/doc/doc_generic_odt.modules.php
+++ htdocs/core/modules/societe/doc/doc_generic_odt.modules.php
@@ -215,7 +215,14 @@ class doc_generic_odt extends ModeleThirdPartyDoc
 				$newfiletmp=preg_replace('/modele_/i','',$newfiletmp);
 				// Get extension (ods or odt)
 				$newfileformat=substr($newfile, strrpos($newfile, '.')+1);
-				$filename=$newfiletmp.'.'.dol_print_date(dol_now(),'%Y%m%d%H%M%S').'.'.$newfileformat;
+				if ( ! empty($conf->global->MAIN_ODT_USE_TIMING))
+				{
+					$filename=$newfiletmp.'.'.dol_print_date(dol_now(),'%Y%m%d%H%M%S').'.'.$newfileformat;
+				}
+				else
+				{
+					$filename=$newfiletmp.'.'.$newfileformat;
+				}
 				$file=$dir.'/'.$filename;
 				$object->builddoc_filename=$filename; // For triggers
 				//print "newfileformat=".$newfileformat;
