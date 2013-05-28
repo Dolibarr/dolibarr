7f3c78f56bbc6a1b2ed57dece86e69b878a475e7
BENKE Charles <charles.fr@benke.fr>
2013-05-28 16:00:29 +0200

    Update mouvementstock.class.php
    Subproduct movement need only on composed product, allready made on manufactured product

--- htdocs/product/stock/class/mouvementstock.class.php
+++ htdocs/product/stock/class/mouvementstock.class.php
@@ -212,7 +212,9 @@ class MouvementStock
 		// Add movement for sub products (recursive call)
 		if (! $error && ! empty($conf->global->PRODUIT_SOUSPRODUITS))
 		{
-			$error = $this->_createSubProduct($user, $fk_product, $entrepot_id, $qty, $type, 0, $label);	// we use 0 as price, because pmp is not changed for subproduct
+			// Subproduct movement need only on composed product, allready made on manufactured product
+			if ($product->finished == 2)
+				$error = $this->_createSubProduct($user, $fk_product, $entrepot_id, $qty, $type, 0, $label);	// we use 0 as price, because pmp is not changed for subproduct
 		}
 
 		if ($movestock && ! $error)
