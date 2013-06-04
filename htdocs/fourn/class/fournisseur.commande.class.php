14ea02ecac7411519f79630b7b918820b726f599
Cédric Salvador <csalvador@gpcsolutions.fr>
2013-06-04 16:33:23 +0200

    copyright

--- htdocs/fourn/class/fournisseur.commande.class.php
+++ htdocs/fourn/class/fournisseur.commande.class.php
@@ -7,6 +7,7 @@
  * Copyright (C) 2010-2013	Philippe Grand			<philippe.grand@atoo-net.com>
  * Copyright (C) 2012       Marcos García           <marcosgdf@gmail.com>
  * Copyright (C) 2013       Florian Henry		  	<florian.henry@open-concept.pro>
+ * Copyright (C) 2013       Cédric Salvador         <csalvador@gpcsolutions.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
