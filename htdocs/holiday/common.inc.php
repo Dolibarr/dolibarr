050819ce0b6e5a2ef0318c46e0f3fb38a75f1f8f
Laurent Destailleur <eldy@destailleur.fr>
2013-06-09 16:15:12 +0200

    Fix: dead code

--- htdocs/holiday/common.inc.php
+++ htdocs/holiday/common.inc.php
@@ -42,27 +42,4 @@ if (empty($conf->holiday->enabled))
     exit();
 }
 
-
-/*$sql = "SELECT value";
-$sql.= " FROM ".MAIN_DB_PREFIX."holiday_config";
-$sql.= " WHERE name = 'userGroup'";
-
-$result = $db->query($sql);
-$obj = $db->fetch_object($result);
-
-if ($obj->value == null || $obj->value < 0)
-{
-    llxHeader('',$langs->trans('CPTitreMenu'));
-
-    $langs->load("errors");
-	$warnpicto=img_error($langs->trans("WarningMandatorySetupNotComplete"));
-	print '<div class="tabBar">';
-	print $warnpicto.' '.$langs->trans("NotConfigModCP");
-	print '</div>';
-
-    llxFooter();
-    exit();
-}
-*/
-
 ?>
