5f526fd84a232eb766d6cafcd41d306c14861346
Cédric Salvador <csalvador@gpcsolutions.fr>
2013-06-10 17:20:49 +0200

    separated replenishment configuration

--- htdocs/admin/stock.php
+++ htdocs/admin/stock.php
@@ -142,19 +142,6 @@ print '<input type="submit" class="button" value="'.$langs->trans("Modify").'">'
 print '</form>';
 print "</td>\n";
 print "</tr>\n";
-
-print "<tr ".$bc[$var].">";
-print '<td width="60%">'.$langs->trans("UseVirtualStock").'</td>';
-print '<td width="160" align="right">';
-print "<form method=\"post\" action=\"stock.php\">";
-print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
-print "<input type=\"hidden\" name=\"action\" value=\"USE_VIRTUAL_STOCK\">";
-print $form->selectyesno("USE_VIRTUAL_STOCK",$conf->global->USE_VIRTUAL_STOCK,1);
-print '<input type="submit" class="button" value="'.$langs->trans("Modify").'">';
-print '</form>';
-print "</td>\n";
-print "</tr>\n";
-
 print '<br>';
 print '</table>';
 print '<br>';
@@ -260,6 +247,25 @@ if (! empty($conf->fournisseur->enabled))
 	print "</form>\n</td>\n</tr>\n";
 }
 
+print '</table>';
+
+print '<br>';
+print '<table class="noborder" width="100%">';
+print '<tr class="liste_titre">';
+print "  <td>".$langs->trans("RuleForStockReplenishment")."</td>\n";
+print "  <td align=\"right\" width=\"160\">&nbsp;</td>\n";
+print '</tr>'."\n";
+print "<tr ".$bc[$var].">";
+print '<td width="60%">'.$langs->trans("UseVirtualStock").'</td>';
+print '<td width="160" align="right">';
+print "<form method=\"post\" action=\"stock.php\">";
+print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+print "<input type=\"hidden\" name=\"action\" value=\"USE_VIRTUAL_STOCK\">";
+print $form->selectyesno("USE_VIRTUAL_STOCK",$conf->global->USE_VIRTUAL_STOCK,1);
+print '<input type="submit" class="button" value="'.$langs->trans("Modify").'">';
+print '</form>';
+print "</td>\n";
+print "</tr>\n";
 print '</table>';
 
 dol_htmloutput_mesg($mesg);
