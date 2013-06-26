f02fa8a41dc861f5d2f26cea4c21726d5c049718
Laurent Destailleur <eldy@destailleur.fr>
2013-06-26 17:58:34 +0200

    Fix: css when using checkbox into label

--- htdocs/theme/cameleo/style.css.php
+++ htdocs/theme/cameleo/style.css.php
@@ -2394,6 +2394,10 @@ div.ui-radio
 	margin: 4px;
 	position: static;
 }
+div.ui-checkbox label+input, div.ui-radio label+input {
+	position: absolute;
+}
+
 .ui-mobile fieldset
 {
 	padding-bottom: 10px; margin-bottom: 4px; border-bottom: 1px solid #AAAAAA !important;
