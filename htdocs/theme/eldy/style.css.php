606b60b17c0ed458421436787fb0ef8c88849f79
Laurent Destailleur <eldy@destailleur.fr>
2013-06-29 04:01:54 +0200

    Minor fixes into css

--- htdocs/theme/eldy/style.css.php
+++ htdocs/theme/eldy/style.css.php
@@ -1964,6 +1964,9 @@ a.impayee:hover { font-weight: bold; color: #550000; }
 
 .dolgraphtitle { margin-top: 6px; margin-bottom: 4px; }
 .dolgraphtitlecssboxes { margin: 0px; }
+.legendColorBox, .legendLabel { border: none !important; }
+div.dolgraph div.legend, div.dolgraph div.legend div { background-color: rgba(255,255,255,0) !important; }
+div.dolgraph div.legend table tbody tr { height: auto; }
 
 .photo {
 border: 0px;
