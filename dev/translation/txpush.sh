68e13b09e950454bf75e2e868a6d863ef5422987
Laurent Destailleur <eldy@destailleur.fr>
2013-07-31 14:11:58 +0200

    Enhance tx tool.

--- dev/translation/txpush.sh
+++ dev/translation/txpush.sh
@@ -4,13 +4,16 @@
 #
 # Laurent Destailleur - eldy@users.sourceforge.net
 #------------------------------------------------------
-# Usage: txpush.sh [all|xx_XX]
+# Usage: txpush.sh (source|all|xx_XX) [-r dolibarr.file] [-f]
 #------------------------------------------------------
 
 # Syntax
 if [ "x$1" = "x" ]
 then
-	echo "Usage: txpush.sh (source|all|xx_XX) [-r dolibarr.file]"
+	echo "This push local files to transifex."
+	echo "Note:  If you push a langauge file (not source), file will be skipped if transifex file is newer."
+	echo "       Using -f will overwrite translation but not memory."
+	echo "Usage: txpush.sh (source|all|xx_XX) [-r dolibarr.file] [-f]"
 	exit
 fi
 
@@ -28,7 +31,7 @@ then
 	echo "tx push -s $2 $3"
 	tx push -s $2 $3 
 else
-	echo "tx push -t -l $1 $2 $3"
-	tx push -t -l $1 $2 $3
+	echo "tx push -t -l $1 $2 $3 $4"
+	tx push -t -l $1 $2 $3 $4
 fi
 fi
