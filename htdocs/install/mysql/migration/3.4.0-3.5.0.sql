5ea6518a6dc8d906310dbca447aaffbcc81085b9
Florian Henry <florian.henry@open-concept.pro>
2013-08-26 10:47:43 +0200

    Fix PgSQL upgrade process failed

--- htdocs/install/mysql/migration/3.4.0-3.5.0.sql
+++ htdocs/install/mysql/migration/3.4.0-3.5.0.sql
@@ -22,7 +22,7 @@ DELETE FROM llx_menu where module='holiday';
 
 ALTER TABLE llx_projet_task ADD COLUMN planned_workload	real DEFAULT 0 NOT NULL AFTER duration_effective;
 
-ALTER TABLE llx_socpeople ADD COLUMN statut tinyint(4) DEFAULT 1 NOT NULL after import_key;
+ALTER TABLE llx_socpeople ADD COLUMN statut tinyint DEFAULT 1 NOT NULL AFTER import_key;
 
 create table llx_fichinter_extrafields
 (
