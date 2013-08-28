29396bd51a98483962ca71c2358a3662aaad74f2
Florian Henry <florian.henry@open-concept.pro>
2013-08-28 18:05:31 +0200

    Change SQL syntax

--- htdocs/core/modules/project/task/mod_task_simple.php
+++ htdocs/core/modules/project/task/mod_task_simple.php
@@ -73,9 +73,9 @@ class mod_task_simple extends ModeleNumRefTask
 
 		$posindice=8;
 		$sql = "SELECT MAX(SUBSTRING(task.ref FROM " . $posindice . ")) as max";
-		$sql .= " FROM " . MAIN_DB_PREFIX . "projet_task AS task";
-		$sql .= " INNER JOIN " . MAIN_DB_PREFIX . "projet AS project ON task.fk_projet=project.rowid";
-		$sql .= " WHERE task.ref LIKE '" . $this->prefix . "____-%'";
+		$sql .= " FROM " . MAIN_DB_PREFIX . "projet_task AS task, ";
+		$sql .= MAIN_DB_PREFIX . "projet AS project WHERE task.fk_projet=project.rowid";
+		$sql .= " AND task.ref LIKE '" . $this->prefix . "____-%'";
 		$sql .= " AND project.entity = " . $conf->entity;
         $resql=$db->query($sql);
         if ($resql)
