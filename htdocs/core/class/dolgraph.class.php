9314859ead497e3149df6e9c3f09196ff1aea323
Laurent Destailleur <eldy@destailleur.fr>
2013-09-10 15:21:56 +0200

    More log to find bug

--- htdocs/core/class/dolgraph.class.php
+++ htdocs/core/class/dolgraph.class.php
@@ -1,20 +1,20 @@
 <?php
 /* Copyright (c) 2003-2006 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (c) 2004-2013 Laurent Destailleur  <eldy@users.sourceforge.net>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  *	\file       htdocs/core/class/dolgraph.class.php
@@ -437,6 +437,7 @@ class DolGraph
     function SetBgColor($bg_color = array(255,255,255))
     {
         global $theme_bgcolor,$theme_bgcoloronglet;
+        
         if (! is_array($bg_color))
         {
             if ($bg_color == 'onglet')
@@ -464,6 +465,7 @@ class DolGraph
     function SetBgColorGrid($bg_colorgrid = array(255,255,255))
     {
         global $theme_bgcolor,$theme_bgcoloronglet;
+        
         if (! is_array($bg_colorgrid))
         {
             if ($bg_colorgrid == 'onglet')
@@ -1032,6 +1034,7 @@ class DolGraph
     {
         return $this->_stringtoshow;
     }
+
 
     /**
      * getDefaultGraphSizeForStats
