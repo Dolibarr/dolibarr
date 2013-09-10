13e86e9b0dd7b4cd85bd3f78443e5b3873ea8127
Cédric Salvador <csalvador@gpcsolutions.fr>
2013-09-10 11:10:42 +0200

    Added a field to group by to avoid error with mysql strict mode

--- htdocs/compta/facture/list.php
+++ htdocs/compta/facture/list.php
@@ -215,7 +215,7 @@ if (! $sall)
     $sql.= ' GROUP BY f.rowid, f.facnumber, f.type, f.increment, f.total,f.tva, f.total_ttc,';
     $sql.= ' f.datef, f.date_lim_reglement,';
     $sql.= ' f.paye, f.fk_statut,';
-    $sql.= ' s.nom, s.rowid';
+    $sql.= ' s.nom, s.rowid, f.note_private';
 }
 else
 {
