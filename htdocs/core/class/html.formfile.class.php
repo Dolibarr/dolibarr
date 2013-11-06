f11f9b0a69374997d614b1b718e0cce6d213d777
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2013-11-06 10:33:29 +0100

    Added missing copyright

--- htdocs/core/class/html.formfile.class.php
+++ htdocs/core/class/html.formfile.class.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2010-2012 Regis Houssin		<regis.houssin@capnetworks.com>
  * Copyright (c) 2010      Juanjo Menent		<jmenent@2byte.es>
  * Copyright (c) 2013      Charles-Fr BENKE		<charles.fr@benke.fr>
+ * Copyright (C) 2013      Cédric Salvador      <csalvador@gpcsolutions.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
