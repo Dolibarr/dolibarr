23f7de0873c10d4b8a7eae6e455179d84089135d
Laurent Destailleur <eldy@destailleur.fr>
2013-11-21 02:07:13 +0100

    Fix: [ bug #1094 ] Invoice Text Reminder
    Conflicts:
    	htdocs/compta/facture.php

--- htdocs/compta/facture.php
+++ htdocs/compta/facture.php
@@ -3674,16 +3674,16 @@ else if ($id > 0 || ! empty($ref))
 				// By default if $action=='presend'
 				$titreform='SendBillByMail';
 				$topicmail='SendBillRef';
-				$action='send';
 				$modelmail='facture_send';
 
 				if ($action == 'prerelance')	// For backward compatibility
 				{
 					$titrefrom='SendReminderBillByMail';
 					$topicmail='SendReminderBillRef';
-					$action='relance';
 					$modelmail='facture_relance';
+					$action='relance';
 				}
+				else $action='send';
 
 				$ref = dol_sanitizeFileName($object->ref);
 				include_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
