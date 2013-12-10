071dd8e758d75f01608474be925252e057177755
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2013-12-10 11:33:27 +0100

    Make bookmarks module multicompany aware

--- htdocs/bookmarks/liste.php
+++ htdocs/bookmarks/liste.php
@@ -73,6 +73,7 @@ $sql = "SELECT b.fk_soc as rowid, b.dateb, b.rowid as bid, b.fk_user, b.url, b.t
 $sql.= " u.login, u.lastname, u.firstname";
 $sql.= " FROM ".MAIN_DB_PREFIX."bookmark as b LEFT JOIN ".MAIN_DB_PREFIX."user as u ON b.fk_user=u.rowid";
 $sql.= " WHERE 1=1";
+$sql.= " AND b.entity = ".$conf->entity;
 if (! $user->admin) $sql.= " AND (b.fk_user = ".$user->id." OR b.fk_user is NULL OR b.fk_user = 0)";
 $sql.= $db->order($sortfield.", position",$sortorder);
 $sql.= $db->plimit($limit, $offset);
