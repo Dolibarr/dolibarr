8b781e46c64e51ca0eaf0678293e8c2dfca2fedc
Marcos García de La Fuente <marcosgdf@gmail.com>
2014-01-03 16:43:34 +0100

    Added missing copyright info

--- htdocs/opensurvey/public/choix_date.php
+++ htdocs/opensurvey/public/choix_date.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2013      Laurent Destailleur <eldy@users.sourceforge.net>
+ * Copyright (C) 2014 Marcos García				<marcosgdf@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
