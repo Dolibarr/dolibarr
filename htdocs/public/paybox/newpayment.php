6e5f3b8bddf39371f0d72847e4e3f15facc91171
Laurent Destailleur <eldy@destailleur.fr>
2014-01-07 16:29:25 +0100

    Fix: entity into public pages not set at correct place

--- htdocs/public/paybox/newpayment.php
+++ htdocs/public/paybox/newpayment.php
@@ -27,6 +27,12 @@
 define("NOLOGIN",1);		// This means this output page does not require to be logged.
 define("NOCSRFCHECK",1);	// We accept to go on this page from external web site.
 
+// For MultiCompany module. 
+// Do not use GETPOST here, function is not defined and define must be done before including main.inc.php
+// TODO This should be useless. Because entity must be retreive from object ref and not from url.
+$entity=(! empty($_GET['entity']) ? (int) $_GET['entity'] : (! empty($_POST['entity']) ? (int) $_POST['entity'] : 1));
+if (is_int($entity)) define("DOLENTITY", $entity);
+
 require '../../main.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/paybox/lib/paybox.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
