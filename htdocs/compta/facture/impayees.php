37d2d990af8d94135fb744c7becab45b57ebef3f
Laurent Destailleur <eldy@destailleur.fr>
2014-02-24 10:17:56 +0100

    Add comment into doc

--- htdocs/compta/facture/impayees.php
+++ htdocs/compta/facture/impayees.php
@@ -16,6 +16,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Note: Page can be call with param mode=sendremind to bring feature to send
+ * remind by emails.
  */
 
 /**
