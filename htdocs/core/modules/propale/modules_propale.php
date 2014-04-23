e738a294e80491568ccdca84ed6f0c197c880fde
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2014-04-23 17:44:48 +0200

    Qual: Wrong variable name

--- htdocs/core/modules/propale/modules_propale.php
+++ htdocs/core/modules/propale/modules_propale.php
@@ -233,7 +233,7 @@ function propale_pdf_create($db, $object, $modele, $outputlangs, $hidedetails=0,
 			include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
 			$interface=new Interfaces($db);
 			$result=$interface->run_triggers('PROPAL_BUILDDOC',$object,$user,$langs,$conf);
-			if ($result < 0) { $error++; $this->errors=$interface->errors; }
+			if ($result < 0) { $error++; $obj->errors=$interface->errors; }
 			// Fin appel triggers
 
 			return 1;
