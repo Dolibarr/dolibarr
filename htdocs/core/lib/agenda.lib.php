aad43597860ca7574cba86761154c7502c144e21
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2014-04-23 15:33:15 +0200

    Qual: Removed missing variable from function call

--- htdocs/core/lib/agenda.lib.php
+++ htdocs/core/lib/agenda.lib.php
@@ -386,14 +386,14 @@ function agenda_prepare_head()
 	$head[$h][2] = 'other';
 	$h++;
 
-	complete_head_from_modules($conf,$langs,$object,$head,$h,'agenda_admin');
+	complete_head_from_modules($conf,$langs,null,$head,$h,'agenda_admin');
 
 	$head[$h][0] = DOL_URL_ROOT."/admin/agenda_extrafields.php";
 	$head[$h][1] = $langs->trans("ExtraFields");
 	$head[$h][2] = 'attributes';
 	$h++;
 
-	complete_head_from_modules($conf,$langs,$object,$head,$h,'agenda_admin','remove');
+	complete_head_from_modules($conf,$langs,null,$head,$h,'agenda_admin','remove');
 
 
 	return $head;
