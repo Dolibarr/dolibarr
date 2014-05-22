3481884731ea1956b83c747d3a78a238287a6f9b
Laurent Destailleur <eldy@destailleur.fr>
2014-05-22 13:20:03 +0200

    Fix: A project is linked to a thirdparty (may be a customer, a supplier or a prospect).

--- htdocs/projet/element.php
+++ htdocs/projet/element.php
@@ -114,7 +114,7 @@ print '</td></tr>';
 
 print '<tr><td>'.$langs->trans("Label").'</td><td>'.$project->title.'</td></tr>';
 
-print '<tr><td>'.$langs->trans("Company").'</td><td>';
+print '<tr><td>'.$langs->trans("ThirdParty").'</td><td>';
 if (! empty($project->societe->id)) print $project->societe->getNomUrl(1);
 else print '&nbsp;';
 print '</td></tr>';
