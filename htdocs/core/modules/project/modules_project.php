fa75ef3b57d23a7709f22dcf55c142d81e9df412
BENKE Charles <charles.fr@benke.fr>
2014-06-21 00:35:11 +0200

    made change on all the modules_element definition

--- htdocs/core/modules/project/modules_project.php
+++ htdocs/core/modules/project/modules_project.php
@@ -134,6 +134,7 @@ abstract class ModeleNumRefProjects
 		if ($this->version == 'development') return $langs->trans("VersionDevelopment");
 		if ($this->version == 'experimental') return $langs->trans("VersionExperimental");
 		if ($this->version == 'dolibarr') return DOL_VERSION;
+		if ($this->version) return $this->version;
 		return $langs->trans("NotAvailable");
 	}
 }
