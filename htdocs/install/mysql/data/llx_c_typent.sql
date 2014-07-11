2766b1d8f153f20425ac22a12e65301577641ce1
Laurent Destailleur <eldy@destailleur.fr>
2014-07-11 17:43:57 +0200

    Add record for argentina but disabled by default.

--- htdocs/install/mysql/data/llx_c_typent.sql
+++ htdocs/install/mysql/data/llx_c_typent.sql
@@ -41,6 +41,6 @@ insert into llx_c_typent (id,code,libelle,fk_country,active) values (  6, 'TE_WH
 insert into llx_c_typent (id,code,libelle,fk_country,active) values (  7, 'TE_RETAIL',  'Revendeur',     NULL, 0);
 insert into llx_c_typent (id,code,libelle,fk_country,active) values (  8, 'TE_PRIVATE', 'Particulier',   NULL, 1);
 insert into llx_c_typent (id,code,libelle,fk_country,active) values (100, 'TE_OTHER',   'Autres',        NULL, 1);
---insert into llx_c_typent (id,code,libelle,fk_country,active) values (231, 'TE_A_RI',    'Responsable Inscripto',    23, 1);
---insert into llx_c_typent (id,code,libelle,fk_country,active) values (232, 'TE_B_RNI',   'Responsable No Inscripto', 23, 1);
---insert into llx_c_typent (id,code,libelle,fk_country,active) values (233, 'TE_C_FE',    'Consumidor Final/Exento',  23, 1);
+insert into llx_c_typent (id,code,libelle,fk_country,active) values (231, 'TE_A_RI',    'Responsable Inscripto',    23, 0);
+insert into llx_c_typent (id,code,libelle,fk_country,active) values (232, 'TE_B_RNI',   'Responsable No Inscripto', 23, 0);
+insert into llx_c_typent (id,code,libelle,fk_country,active) values (233, 'TE_C_FE',    'Consumidor Final/Exento',  23, 0);
