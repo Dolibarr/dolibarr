Dolibarr/dolibarr

commit 0916d2cf3b1307db8d8e7612ae96f2e5ce71f67b

Fix link element from project "associates object" pages http://www.dolibarr.fr/forum/527-bugs-sur-la-version-stable-courante/50687-objet-associes-au-projet

htdocs/projet/element.php
@@ -209,13 +209,18 @@ foreach ($listofreferent as $key => $value)
 	$classname=$value['class'];
 	$tablename=$value['table'];
 	$qualified=$value['test'];
+	
 	if ($qualified)
 	{
 		print '<br>';
 
 		print_titre($langs->trans($title));
-
+		
 		$selectList=$formproject->select_element($tablename);
+		if ($selectList<0) {
+			setEventMessage($formproject->error,'errors');
+		}
+		
 		if ($selectList)
 		{
 			print '<form action="'.$_SERVER["PHP_SELF"].'?id='.$projectid.'" method="post">';

htdocs/core/class/html.formprojet.class.php
@@ -168,6 +168,7 @@ class FormProjets
 	 */
 	function select_element($table_element)
 	{
+		global $conf;
 
 		$projectkey="fk_projet";
 		switch ($table_element)
@@ -195,7 +196,7 @@ class FormProjets
 		if (!empty($this->societe->id)) {
 			$sql.= " AND fk_soc=".$this->societe->id;
 		}
-		$sql.= ' AND entity='.$conf->entity;
+		$sql.= ' AND entity='.getEntity('project');
 		$sql.= " ORDER BY ref DESC";
 
 		dol_syslog(get_class($this).'::select_element sql='.$sql,LOG_DEBUG);
@@ -219,6 +220,10 @@ class FormProjets
 			return $sellist ;
 
 			$this->db->free($resql);
+		}else {
+			$this->error=$this->db->lasterror();
+			dol_syslog(get_class($this) . "::select_element " . $errmsg, LOG_ERR);
+			return -1;
 		}
 	}
 

commit b0ae2fe2aaf069a4a9893d09ae3d588742862318

Fixed a bug introduced in commit 0916d2cf3b

htdocs/projet/element.php
@@ -217,12 +217,10 @@ foreach ($listofreferent as $key => $value)
 		print_titre($langs->trans($title));
 		
 		$selectList=$formproject->select_element($tablename,$project->societe->id);
-		if ($selectList<0) {
+
+		if (!$selectList || ($selectList<0)) {
 			setEventMessage($formproject->error,'errors');
-		}
-		
-		if ($selectList)
-		{
+		} else {
 			print '<form action="'.$_SERVER["PHP_SELF"].'?id='.$projectid.'" method="post">';
 			print '<input type="hidden" name="tablename" value="'.$tablename.'">';
 			print '<input type="hidden" name="action" value="addelement">';