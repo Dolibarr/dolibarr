7e29889eab01c502c1945abf261c52d9b7ebb9ab
Marcos García de La Fuente <marcosgdf@gmail.com>
2014-07-18 03:13:15 +0200

    Fixed little bug

--- htdocs/core/class/interfaces.class.php
+++ htdocs/core/class/interfaces.class.php
@@ -23,7 +23,7 @@
  *   \brief			Fichier de la classe de gestion des triggers
  */
 
-require_once DOL_DOCUMENT_ROOT.'/htdocs/core/triggers/DolibarrTriggers.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/triggers/DolibarrTriggers.class.php';
 
 /**
  *   Class to manage triggers
