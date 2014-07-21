1c4048103e5600fcc4a6eb03dfad20f392fa4f59
Marcos García de La Fuente <marcosgdf@gmail.com>
2014-07-21 02:37:36 +0200

    Created constant DOL_VENDOR_ROOT

--- htdocs/filefunc.inc.php
+++ htdocs/filefunc.inc.php
@@ -145,6 +145,7 @@ if (empty($dolibarr_main_data_root))
 define('DOL_CLASS_PATH', 'class/');									// Filesystem path to class dir (defined only for some code that want to be compatible with old versions without this parameter)
 define('DOL_DATA_ROOT', $dolibarr_main_data_root);					// Filesystem data (documents)
 define('DOL_DOCUMENT_ROOT', $dolibarr_main_document_root);			// Filesystem core php (htdocs)
+define('DOL_VENDOR_ROOT', $dolibarr_main_document_root.'/../vendor');
 // Try to autodetect DOL_MAIN_URL_ROOT and DOL_URL_ROOT.
 // Note: autodetect works only in case 1, 2, 3 and 4 of phpunit test CoreTest.php. For case 5, 6, only setting value into conf.php will works.
 $tmp='';
