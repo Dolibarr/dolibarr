8ea52f52f5e7702f51cebb3778c37725d280176f
Laurent Destailleur <eldy@destailleur.fr>
2014-08-03 23:48:27 +0200

    Fix: Missing counter into document tab

--- htdocs/core/lib/agenda.lib.php
+++ htdocs/core/lib/agenda.lib.php
@@ -428,10 +428,15 @@ function actions_prepare_head($object)
 		$h++;
 	}
 
-	$head[$h][0] = DOL_URL_ROOT.'/comm/action/document.php?id='.$object->id;
-	$head[$h][1] = $langs->trans('Documents');
-	$head[$h][2] = 'documents';
-	$h++;
+    // Attached files
+    require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
+    $upload_dir = $conf->agenda->dir_output . "/" . $object->id;
+    $nbFiles = count(dol_dir_list($upload_dir,'files',0,'','(\.meta|_preview\.png)$'));
+    $head[$h][0] = DOL_URL_ROOT.'/comm/action/document.php?id='.$object->id;
+    $head[$h][1] = $langs->trans("Documents");
+	if($nbFiles > 0) $head[$h][1].= ' ('.$nbFiles.')';
+    $head[$h][2] = 'document';
+    $h++;
 
 	$head[$h][0] = DOL_URL_ROOT.'/comm/action/info.php?id='.$object->id;
 	$head[$h][1] = $langs->trans('Info');
