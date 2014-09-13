20bec865fde77d86d1db05045ac4ba0be05ff656
Laurent Destailleur <eldy@destailleur.fr>
2014-09-14 00:27:36 +0200

    Fix: checkstyle

--- htdocs/comm/action/peruser.php
+++ htdocs/comm/action/peruser.php
@@ -685,21 +685,22 @@ $db->close();
 
 
 
-
 /**
  * Show event of a particular day
  *
- * @param   int		$day             Day
- * @param   int		$month           Month
- * @param   int		$year            Year
- * @param   int		$monthshown      Current month shown in calendar view
- * @param   string	$style           Style to use for this day
- * @param   array	&$eventarray     Array of events
- * @param   int		$maxprint        Nb of actions to show each day on month view (0 means no limit)
- * @param   int		$maxnbofchar     Nb of characters to show for event line
- * @param   string	$newparam        Parameters on current URL
- * @param   int		$showinfo        Add extended information (used by day view)
- * @param   int		$minheight       Minimum height for each event. 60px by default.
+ * @param	string	$username		Login
+ * @param   int		$day            Day
+ * @param   int		$month          Month
+ * @param   int		$year           Year
+ * @param   int		$monthshown     Current month shown in calendar view
+ * @param   string	$style          Style to use for this day
+ * @param   array	&$eventarray    Array of events
+ * @param   int		$maxprint       Nb of actions to show each day on month view (0 means no limit)
+ * @param   int		$maxnbofchar    Nb of characters to show for event line
+ * @param   string	$newparam       Parameters on current URL
+ * @param   int		$showinfo       Add extended information (used by day view)
+ * @param   int		$minheight      Minimum height for each event. 60px by default.
+ * @param	boolean	$showheader		Show header
  * @return	void
  */
 function show_day_events2($username, $day, $month, $year, $monthshown, $style, &$eventarray, $maxprint=0, $maxnbofchar=16, $newparam='', $showinfo=0, $minheight=60, $showheader=false)
