f68cb3bfcbbf9e659066e53d532bd27ab345256b
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2014-09-18 14:52:59 +0200

    FIX: Variable declared boolean

--- htdocs/core/modules/modUser.class.php
+++ htdocs/core/modules/modUser.class.php
@@ -49,7 +49,7 @@ class modUser extends DolibarrModules
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		$this->description = "Gestion des utilisateurs (requis)";
-		$this->always_enabled = 1;	// Can't be disabled
+		$this->always_enabled = true;	// Can't be disabled
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 		$this->version = 'dolibarr';
