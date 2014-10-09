0390085f5401b7de1463986704287cc213aafad8
Laurent Destailleur <eldy@destailleur.fr>
2014-10-09 13:47:40 +0200

    Style for install match css of login.

--- htdocs/install/etape1.php
+++ htdocs/install/etape1.php
@@ -214,7 +214,9 @@ if (! $error)
             {
                 print '<div class="error">'.$db->error.'</div>';
                 if (! $db->connected) print $langs->trans("BecauseConnectionFailedParametersMayBeWrong").'<br><br>';
+                //print '<a href="#" onClick="javascript: history.back();">';
                 print $langs->trans("ErrorGoBackAndCorrectParameters");
+                //print '</a>';
                 $error++;
             }
         }
@@ -223,7 +225,9 @@ if (! $error)
     {
         print "<br>\nFailed to include_once(\"".$main_dir."/core/db/".$db_type.".class.php\")<br>\n";
         print '<div class="error">'.$langs->trans("ErrorWrongValueForParameter",$langs->transnoentities("WebPagesDirectory")).'</div>';
+        //print '<a href="#" onClick="javascript: history.back();">';
         print $langs->trans("ErrorGoBackAndCorrectParameters");
+        //print '</a>';
         $error++;
     }
 }
