38800139400f00ac574f8d43d31c8f1500e794ad
Juanjo Menent <jmenent@2byte.es>
2014-11-06 23:10:21 +0100

    Fix hooks from supplierorder to supplierinvoice

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -1083,7 +1083,7 @@ elseif ($action == 'update_extras')
 	{
 		// Actions on extra fields (by external module or standard code)
 		// FIXME le hook fait double emploi avec le trigger !!
-		$hookmanager->initHooks(array('supplierorderdao'));
+		$hookmanager->initHooks(array('supplierinvoicedao'));
 		$parameters=array('id'=>$object->id);
 
 		$reshook=$hookmanager->executeHooks('insertExtraFields',$parameters,$object,$action); // Note that $action and $object may have been modified by some hooks
