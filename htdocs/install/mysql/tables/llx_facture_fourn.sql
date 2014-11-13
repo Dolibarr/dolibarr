38c2dc8747de03e51aebed361a9e5b6252bea7d3
BENKE Charles <charles.fr@benke.fr>
2014-11-13 11:16:40 +0100

    Update llx_facture_fourn.sql
    increase ref fields size (like customers facture)

--- htdocs/install/mysql/tables/llx_facture_fourn.sql
+++ htdocs/install/mysql/tables/llx_facture_fourn.sql
@@ -22,11 +22,11 @@
 create table llx_facture_fourn
 (
   rowid					integer AUTO_INCREMENT PRIMARY KEY,
-  ref					varchar(30),
-  ref_supplier			varchar(50) NOT NULL,
+  ref					varchar(255),
+  ref_supplier			varchar(255) NOT NULL,
   entity				integer  DEFAULT 1 NOT NULL,	 -- multi company id
 
-  ref_ext				varchar(30),                  -- reference into an external system (not used by dolibarr)
+  ref_ext				varchar(255),                  -- reference into an external system (not used by dolibarr)
 
   type					smallint DEFAULT 0 NOT NULL,
   fk_soc				integer NOT NULL,
