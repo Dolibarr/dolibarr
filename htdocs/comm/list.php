46aea31e6c3401c4c90c26cacc6836f5bd547e64
Frédéric FRANCE <frederic.france@free.fr>
2014-11-23 09:31:20 +0100

    Update /comm/list.php

--- htdocs/comm/list.php
+++ htdocs/comm/list.php
@@ -75,7 +75,7 @@ $reshook=$hookmanager->executeHooks('doActions',$parameters);    // Note that $a
 if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
 
 // Do we click on purge search criteria ?
-if (GETPOST("button_removefilter_x"))
+if (GETPOST("button_removefilter_x") || GETPOST("button_removefilter")) // Both test are required to be compatible with all browsers
 {
 	$search_sale="";
 	$search_categ="";
