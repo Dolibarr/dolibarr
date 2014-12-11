a0112ba724b43dae8c9526a0fcc70192ed69c4f0
Laurent Destailleur <eldy@destailleur.fr>
2014-12-11 21:38:53 +0100

    Better position of elements

--- htdocs/core/tpl/login.tpl.php
+++ htdocs/core/tpl/login.tpl.php
@@ -89,17 +89,17 @@ $(document).ready(function () {
 <table class="left centpercent" summary="Login pass">
 <!-- Login -->
 <tr>
-<td valign="middle" class="nowrap">
+<td valign="middle" class="nowrap center">
 <span class="span-icon-user">
-<input type="text" id="username" placeholder="<?php echo $langs->trans("Login"); ?>" name="username" class="flat input-icon-user" size="15" value="<?php echo dol_escape_htmltag($login); ?>" tabindex="1" autofocus="autofocus" />
+<input type="text" id="username" placeholder="<?php echo $langs->trans("Login"); ?>" name="username" class="flat input-icon-user" size="20" value="<?php echo dol_escape_htmltag($login); ?>" tabindex="1" autofocus="autofocus" />
 </span>
 </td>
 </tr>
 <!-- Password -->
 <tr>
-<td valign="middle" class="nowrap">
+<td valign="middle" class="nowrap center">
 <span class="span-icon-password">
-<input id="password" placeholder="<?php echo $langs->trans("Password"); ?>" name="password" class="flat input-icon-password" type="password" size="15" value="<?php echo dol_escape_htmltag($password); ?>" tabindex="2" autocomplete="off" />
+<input id="password" placeholder="<?php echo $langs->trans("Password"); ?>" name="password" class="flat input-icon-password" type="password" size="20" value="<?php echo dol_escape_htmltag($password); ?>" tabindex="2" autocomplete="off" />
 </span>
 </td></tr>
 <?php
@@ -115,20 +115,24 @@ if (! empty($hookmanager->resArray['options'])) {
 ?>
 <?php
 	if ($captcha) {
-		// TODO: provide accessible captha variants
+		// Add a variable param to force not using cache (jmobile)
+		$php_self = preg_replace('/[&\?]time=(\d+)/','',$php_self);	// Remove param time
+		if (preg_match('/\?/',$php_self)) $php_self.='&time='.dol_print_date(dol_now(),'dayhourlog');
+		else $php_self.='?time='.dol_print_date(dol_now(),'dayhourlog');
+		// TODO: provide accessible captcha variants
 ?>
 	<!-- Captcha -->
 	<tr>
-	<td valign="top" class="nowrap none" align="left">
+	<td valign="top" class="nowrap none center">
 
-	<table class="login_table_securitycode" style="width: 100px;"><tr>
+	<table class="login_table_securitycode"><tr>
 	<td>
 	<span class="span-icon-security">
 	<input id="securitycode" placeholder="<?php echo $langs->trans("SecurityCode"); ?>" class="flat input-icon-security" type="text" size="12" maxlength="5" name="code" tabindex="3" />
 	</span>
 	</td>
 	<td><img src="<?php echo DOL_URL_ROOT ?>/core/antispamimage.php" border="0" width="80" height="32" id="img_securitycode" /></td>
-	<td><a href="<?php echo $php_self; ?>" tabindex="4"><?php echo $captcha_refresh; ?></a></td>
+	<td><a href="<?php echo $php_self; ?>" tabindex="4" data-role="button"><?php echo $captcha_refresh; ?></a></td>
 	</tr></table>
 
 	</td></tr>
@@ -158,7 +162,7 @@ if ($forgetpasslink || $helpcenterlink)
 	if ($dol_use_jmobile)    $moreparam.=(strpos($moreparam,'?')===false?'?':'&').'dol_use_jmobile='.$dol_use_jmobile;
 
 	echo '<br>';
-	echo '<div align="center" style="margin-top: 4px;">';
+	echo '<div align="center" style="margin-top: 8px;">';
 	if ($forgetpasslink) {
 		echo '<a class="alogin" href="'.DOL_URL_ROOT.'/user/passwordforgotten.php'.$moreparam.'">(';
 		echo $langs->trans('PasswordForgotten');
