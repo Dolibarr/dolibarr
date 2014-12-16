69c1de3fe12e6134e511febcd7617755cd9f00fb
Philippe GRAND <philippe.grand@atoo-net.com>
2014-12-16 15:07:23 +0100

    one close form too much, an other at the wrong place

--- htdocs/admin/compta.php
+++ htdocs/admin/compta.php
@@ -135,7 +135,6 @@ print '<td colspan="2">'.nl2br($langs->trans('OptionModeTrueDesc'));
 print "</td></tr>\n";
 print '<tr '.$bc[true].'><td width="200"><input type="radio" name="accounting_mode" value="CREANCES-DETTES"'.($accounting_mode == 'CREANCES-DETTES' ? ' checked' : '').'> '.$langs->trans('OptionModeVirtual').'</td>';
 print '<td colspan="2">'.nl2br($langs->trans('OptionModeVirtualDesc'))."</td></tr>\n";
-print '</form>';
 
 print "</table>\n";
 
@@ -163,11 +162,10 @@ foreach ($list as $key)
 	print '</td></tr>';
 }
 
-print '</form>';
 print "</table>\n";
 
 print '<br /><br /><div style="text-align:center"><input type="submit" class="button" value="'.$langs->trans('Modify').'" name="button"></div>';
-
+print '</form>';
 $db->close();
 
 llxFooter();
