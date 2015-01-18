1681e3b03d8cbe064d33115d2a1b128860790574
Laurent Destailleur <eldy@destailleur.fr>
2015-01-18 11:36:15 +0100

    Fix: Not allowed types (http://wiki.dolibarr.org/index.php/Langages_et_normes#Structure_des_tables_et_champs)

--- htdocs/install/mysql/migration/3.7.0-3.8.0.sql
+++ htdocs/install/mysql/migration/3.7.0-3.8.0.sql
@@ -32,8 +32,8 @@ ALTER TABLE llx_product_fournisseur_price ADD fk_price_expression integer DEFAUL
 insert into llx_c_tva(rowid,fk_pays,taux,recuperableonly,note,active) values ( 2131, 213, '5', '0', 'VAT 5%', 1);
 
 -- Add situation invoices
-ALTER TABLE llx_facture ADD situation_cycle_ref smallint UNSIGNED;
-ALTER TABLE llx_facture ADD situation_counter tinyint UNSIGNED;
-ALTER TABLE llx_facture ADD situation_final boolean;
+ALTER TABLE llx_facture ADD situation_cycle_ref integer;
+ALTER TABLE llx_facture ADD situation_counter integer;
+ALTER TABLE llx_facture ADD situation_final integer;
 ALTER TABLE llx_facturedet ADD situation_percent real;
 ALTER TABLE llx_facturedet ADD fk_prev_id integer;
