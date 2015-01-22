3a567e8287a017dc71ee383291bc31ff4f6e2572
cla <c.aschieri@19.coop>
2015-01-22 09:28:09 +0100

    Add extrafields support for supplier invoice showing extrafields after standard fields.

--- htdocs/fourn/facture/fiche.php
+++ htdocs/fourn/facture/fiche.php
@@ -1804,6 +1804,52 @@ else
         // Other options
         $parameters=array('colspan' => ' colspan="4"');
         $reshook=$hookmanager->executeHooks('formObjectOptions',$parameters,$object,$action); // Note that $action and $object may have been modified by hook
+        
+        // Extrafields
+        $extrafields = new ExtraFields($db);
+        $extralabels = $extrafields->fetch_name_optionals_label($object->table_element);
+        $ret = $extrafields->setOptionalsFromPost($extralabels, $object);
+        
+        if (empty($reshook) && ! empty($extrafields->attribute_label)) {
+        
+        	foreach ($extrafields->attribute_label as $key => $label) {
+        		if ($action == 'edit_extras') {
+        			$value = (isset($_POST["options_" . $key]) ? $_POST["options_" . $key] : $object->array_options ["options_" . $key]);
+        		} else {
+        			$value = $object->array_options ["options_" . $key];
+        		}
+        		if ($extrafields->attribute_type [$key] == 'separate') {
+        			print $extrafields->showSeparator($key);
+        		} else {
+        			print '<tr><td';
+        			if (! empty($extrafields->attribute_required [$key]))
+        				print ' class="fieldrequired"';
+        			print '>' . $label . '</td><td colspan="5">';
+        			// Convert date into timestamp format
+        			if (in_array($extrafields->attribute_type [$key], array('date','datetime'))) {
+        				$value = isset($_POST["options_" . $key]) ? dol_mktime($_POST["options_" . $key . "hour"], $_POST["options_" . $key . "min"], 0, $_POST["options_" . $key . "month"], $_POST["options_" . $key . "day"], $_POST["options_" . $key . "year"]) : $db->jdate($object->array_options ['options_' . $key]);
+        			}
+        
+        			if ($action == 'edit_extras' && $user->rights->fournisseur->facture->creer && GETPOST('attribute') == $key) {
+        				print '<form enctype="multipart/form-data" action="' . $_SERVER["PHP_SELF"] . '" method="post" name="formsoc">';
+        				print '<input type="hidden" name="action" value="update_extras">';
+        				print '<input type="hidden" name="attribute" value="' . $key . '">';
+        				print '<input type="hidden" name="token" value="' . $_SESSION ['newtoken'] . '">';
+        				print '<input type="hidden" name="id" value="' . $object->id . '">';
+        
+        				print $extrafields->showInputField($key, $value);
+        
+        				print '<input type="submit" class="button" value="' . $langs->trans('Modify') . '">';
+        				print '</form>';
+        			} else {
+        				print $extrafields->showOutputField($key, $value);
+        				if ($object->statut == 0 && $user->rights->fournisseur->facture->creer)
+        					print '<a href="' . $_SERVER['PHP_SELF'] . '?id=' . $object->id . '&action=edit_extras&attribute=' . $key . '">' . img_picto('', 'edit') . ' ' . $langs->trans('Modify') . '</a>';
+        			}
+        			print '</td></tr>' . "\n";
+        		}
+        	}
+        }
 
         print '</table>';
 
