d02bb2442bbd2e65e5b24128c8630768cb218c30
Drosis Nikos <drosis.nikos@gmail.com>
2015-02-03 16:00:31 +0200

    Update main.inc.php

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -237,7 +237,7 @@ if (! empty($conf->file->main_force_https))
             // $_SERVER["HTTPS"] is 'on' when link is https, otherwise $_SERVER["HTTPS"] is empty or 'off'
             if (empty($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != 'on')		// If link is http
             {
-                $newurl=preg_replace('/^http:/i','https:',DOL_MAIN_URL_ROOT).$_SERVER["REQUEST_URI"]["L,R=301"];
+                $newurl=preg_replace('/^http:/i','https:',DOL_MAIN_URL_ROOT).$_SERVER["REQUEST_URI"]["R,L"];
             }
         }
     }
@@ -247,7 +247,7 @@ if (! empty($conf->file->main_force_https))
         // $_SERVER["HTTPS"] is 'on' when link is https, otherwise $_SERVER["HTTPS"] is empty or 'off'
         if (empty($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != 'on')		// If link is http
         {
-            $newurl=$conf->file->main_force_https.$_SERVER["REQUEST_URI"]["L,R=301"];
+            $newurl=$conf->file->main_force_https.$_SERVER["REQUEST_URI"];
         }
     }
     // Start redirect
