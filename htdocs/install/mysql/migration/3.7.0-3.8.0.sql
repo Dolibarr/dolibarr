e76b6e740f442e865939303134488f821f709c46
Ion Agorria <ion@agorria.com>
2015-03-14 16:52:01 +0100

    SQL additions

--- htdocs/install/mysql/migration/3.7.0-3.8.0.sql
+++ htdocs/install/mysql/migration/3.7.0-3.8.0.sql
@@ -370,3 +370,23 @@ INSERT INTO llx_c_incoterms (code, libelle, active) VALUES ('DDP', 'Delivered Du
 -- Extrafields fk_object must be unique (1-1 relation)
 ALTER TABLE llx_societe_extrafields DROP INDEX idx_societe_extrafields;
 ALTER TABLE llx_societe_extrafields ADD UNIQUE INDEX uk_societe_extrafields (fk_object);
+
+create table llx_c_price_global_variable
+(
+	rowid					integer AUTO_INCREMENT PRIMARY KEY,
+	code					varchar(20) NOT NULL,
+	description		text DEFAULT NULL,
+	value					double(24,8) DEFAULT 0
+)ENGINE=innodb;
+
+create table llx_c_price_global_variable_updater
+(
+	rowid						integer AUTO_INCREMENT PRIMARY KEY,
+	type						integer NOT NULL,
+	description			text DEFAULT NULL,
+	parameters			text DEFAULT NULL,
+	fk_variable			integer NOT NULL,
+	update_interval	integer DEFAULT 0,
+	next_update			integer DEFAULT 0,
+	last_status			text DEFAULT NULL
+)ENGINE=innodb;
