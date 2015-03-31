08e7cb2e8488b323df50a6566c5e73ec20992218
Frédéric FRANCE <frederic.france@free.fr>
2015-03-31 21:57:48 +0200

    Correction for tooltip

--- htdocs/comm/action/class/actioncomm.class.php
+++ htdocs/comm/action/class/actioncomm.class.php
@@ -976,9 +976,9 @@ class ActionComm extends CommonObject
             $tooltip .= '<br><b>' . $langs->trans('Title') . ':</b> ' . $this->label;
         $label = $this->label;
         if (empty($label)) $label=$this->libelle;   // For backward compatibility
-        $linkclose = '" title="'.dol_escape_htmltag($tooltip, 1).'" class="classfortooltip">';
-        if ($option=='birthday') $link = '<a '.($classname?'class="'.$classname.'" ':'').'href="'.DOL_URL_ROOT.'/contact/perso.php?id='.$this->id.$linkclose;
-        else $link = '<a '.($classname?'class="'.$classname.'" ':'').'href="'.DOL_URL_ROOT.'/comm/action/card.php?id='.$this->id.$linkclose;
+        $linkclose = '" title="'.dol_escape_htmltag($tooltip, 1).'">';
+        if ($option=='birthday') $link = '<a class="'.$classname.' classfortooltip" href="'.DOL_URL_ROOT.'/contact/perso.php?id='.$this->id.$linkclose;
+        else $link = '<a class="'.$classname.' classfortooltip" href="'.DOL_URL_ROOT.'/comm/action/card.php?id='.$this->id.$linkclose;
         $linkend='</a>';
         //print 'rrr'.$this->libelle.'-'.$withpicto;
 
