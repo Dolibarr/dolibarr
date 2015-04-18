07fd6a3ae78b76989d04e4b403b7aeea75e3c09a
Maxime Kohlhaas <maxime@atm-consulting.fr>
2015-04-18 04:18:40 +0200

    Fix : delivery date was not a datetime

--- htdocs/install/mysql/migration/3.6.0-3.7.0.sql
+++ htdocs/install/mysql/migration/3.6.0-3.7.0.sql
@@ -1177,3 +1177,5 @@ ALTER TABLE llx_extrafields ADD COLUMN list integer DEFAULT 0 after perms;
 -- IVORY COST (id country=21)
 insert into llx_c_tva(rowid,fk_pays,taux,recuperableonly,localtax1,localtax1_type,localtax2,localtax2_type,note,active) values (211, 21,  '0','0',0,0,0,0,'IVA Rate 0',1);
 insert into llx_c_tva(rowid,fk_pays,taux,recuperableonly,localtax1,localtax1_type,localtax2,localtax2_type,note,active) values (212, 21,  '18','0',7.5,2,0,0,'IVA standard rate',1);
+
+ALTER TABLE llx_livraison MODIFY COLUMN date_delivery DATETIME NULL DEFAULT NULL;
