b203263e9188d051418e70fa281836e867295b24
Maxime Kohlhaas <maxime@atm-consulting.fr>
2015-05-06 12:16:36 +0200

    Fix : shipment PDF was not using generation params

--- htdocs/core/modules/expedition/modules_expedition.php
+++ htdocs/core/modules/expedition/modules_expedition.php
@@ -149,7 +149,7 @@ abstract class ModelNumRefExpedition
  * 	@param	Translate	$outputlangs	Objet lang a utiliser pour traduction
  *  @return int             			<=0 if KO, >0 if OK
  */
-function expedition_pdf_create($db, $object, $modele, $outputlangs)
+function expedition_pdf_create($db, $object, $modele, $outputlangs, $hidedetails=0, $hidedesc=0, $hideref=0)
 {
 	global $conf,$user,$langs;
 
@@ -214,7 +214,7 @@ function expedition_pdf_create($db, $object, $modele, $outputlangs)
 		// We save charset_output to restore it because write_file can change it if needed for
 		// output format that does not support UTF8.
 		$sav_charset_output=$outputlangs->charset_output;
-		if ($obj->write_file($object, $outputlangs, $srctemplatepath) > 0)
+		if ($obj->write_file($object, $outputlangs, $srctemplatepath, $hidedetails, $hidedesc, $hideref) > 0)
 		{
 			$outputlangs->charset_output=$sav_charset_output;
 
