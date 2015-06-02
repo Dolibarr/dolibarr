3f1613197e006e29163b4cdaa1e4c2519827e217
Florian HENRY <florian.henry@open-concept.pro>
2015-06-02 08:41:42 +0200

    FIX : If supplier invoice block linked element is display after other block total HT amount is not reset to 0 and sum other block (like customer orders values)

--- htdocs/fourn/facture/tpl/linkedobjectblock.tpl.php
+++ htdocs/fourn/facture/tpl/linkedobjectblock.tpl.php
@@ -40,6 +40,7 @@ else print_titre($langs->trans("RelatedBill"));
 	<td align="right"><?php echo $langs->trans("Status"); ?></td>
 </tr>
 <?php
+$total=0;
 $var=true;
 foreach($linkedObjectBlock as $object)
 {
