a7ce414d236a5cd3e1757d4abe1e50a3d1b72a22
Laurent Destailleur <eldy@destailleur.fr>
2015-06-20 14:15:32 +0200

    Fix translation

--- htdocs/admin/system/filecheck.php
+++ htdocs/admin/system/filecheck.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2005-2012  Laurent Destailleur     <eldy@users.sourceforge.net>
+/* Copyright (C) 2005-2015  Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2007       Rodolphe Quiedeville    <rodolphe@quiedeville.org>
  * Copyright (C) 2007-2012  Regis Houssin           <regis.houssin@capnetworks.com>
  * Copyright (C) 2015       Frederic France         <frederic.france@free.fr>
@@ -66,23 +66,33 @@ print '<br>';
 
 // Modified or missing files
 $file_list = array('missing' => array(), 'updated' => array());
-$xmlfile = DOL_DOCUMENT_ROOT.'/core/filelist-'.DOL_VERSION.'.xml';
-if (file_exists($xmlfile)) {
+
+// File to analyze
+//$xmlfile = DOL_DOCUMENT_ROOT.'/install/filelist-'.DOL_VERSION.'.xml';
+$xmlfile = DOL_DOCUMENT_ROOT.'/install/filelist.xml';
+
+if (file_exists($xmlfile))
+{
     $xml = simplexml_load_file($xmlfile);
-    if ($xml) {
+    if ($xml)
+    {
         $ret = getFilesUpdated($xml->dolibarr_root_dir[0]);
         print '<table class="noborder">';
         print '<tr class="liste_titre">';
         print '<td>' . $langs->trans("FilesMissing") . '</td>';
         print '</tr>'."\n";
         $var = true;
-        foreach ($file_list['missing'] as $file) {
+        foreach ($file_list['missing'] as $file)
+        {
             $var = !$var;
             print '<tr ' . $bc[$var] . '>';
             print '<td>'.$file.'</td>' . "\n";
             print "</tr>\n";
         }
         print '</table>';
+
+        print '<br>';
+
         print '<table class="noborder">';
         print '<tr class="liste_titre">';
         print '<td>' . $langs->trans("FilesUpdated") . '</td>';
@@ -97,7 +107,7 @@ if (file_exists($xmlfile)) {
         print '</table>';
     }
 } else {
-    print $langs->trans('XmlNotFound') . ': ' . DOL_DOCUMENT_ROOT . '/core/filelist-' . DOL_VERSION . '.xml';
+    print $langs->trans('XmlNotFound') . ': ' . $xmlfile;
 }
 
 llxFooter();
