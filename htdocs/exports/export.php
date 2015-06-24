c218843cdbf7941d93222d2cd1fe75b0e67a6f1a
Laurent Destailleur <eldy@destailleur.fr>
2015-06-24 18:29:29 +0200

    FIX Save of filters into export profiles failed.

--- htdocs/exports/export.php
+++ htdocs/exports/export.php
@@ -99,7 +99,7 @@ $entitytolang = array(
 );
 
 $array_selected=isset($_SESSION["export_selected_fields"])?$_SESSION["export_selected_fields"]:array();
-$array_filtervalue=isset($_SESSION["export_FilterValue_fields"])?$_SESSION["export_FilterValue_fields"]:array();
+$array_filtervalue=isset($_SESSION["export_filtered_fields"])?$_SESSION["export_filtered_fields"]:array();
 $datatoexport=GETPOST("datatoexport");
 $action=GETPOST('action', 'alpha');
 $confirm=GETPOST('confirm', 'alpha');
@@ -222,7 +222,6 @@ if ($action=='downfield' || $action=='upfield')
 if ($step == 1 || $action == 'cleanselect')
 {
     $_SESSION["export_selected_fields"]=array();
-    //$_SESSION["export_FilterValue_fields"]=array();
     $_SESSION["export_filtered_fields"]=array();
     $array_selected=array();
     $array_filtervalue=array();
@@ -280,12 +279,15 @@ if ($action == 'add_export_model')
 		}
 
 		$hexafiltervalue='';
-		foreach($array_filtervalue as $key=>$val)
+		if (! empty($array_filtervalue) && is_array($array_filtervalue))
 		{
-			if ($hexafilter) $hexafiltervalue.=',';
-			$hexafiltervalue.=$key.'='.$val;
+			foreach($array_filtervalue as $key=>$val)
+			{
+				if ($hexafiltervalue) $hexafiltervalue.=',';
+				$hexafiltervalue.=$key.'='.$val;
+			}
 		}
-
+		
 	    $objexport->model_name = $export_name;
 	    $objexport->datatoexport = $datatoexport;
 	    $objexport->hexa = $hexa;
@@ -311,10 +313,11 @@ if ($action == 'add_export_model')
 	}
 }
 
+// Reload an predefined export model
 if ($step == 2 && $action == 'select_model')
 {
     $_SESSION["export_selected_fields"]=array();
-    $_SESSION["export_FilterValue_fields"]=array();
+    $_SESSION["export_filtered_fields"]=array();
 
     $array_selected=array();
     $array_filtervalue=array();
@@ -331,16 +334,15 @@ if ($step == 2 && $action == 'select_model')
 		}
 		$_SESSION["export_selected_fields"]=$array_selected;
 
-		$fieldsarray=explode(',',$objexport->hexafilter);
 		$fieldsarrayvalue=explode(',',$objexport->hexafiltervalue);
 		$i=1;
-		foreach($fieldsarray as $val)
+		foreach($fieldsarrayvalue as $val)
 		{
 			$tmp=explode('=',$val);
 			$array_filtervalue[$tmp[0]]=$tmp[1];
 			$i++;
 		}
-		$_SESSION["export_FilterValue_fields"]=$array_filtervalue;
+		$_SESSION["export_filtered_fields"]=$array_filtervalue;
     }
 }
 
@@ -350,7 +352,7 @@ if ($step == 4 && $action == 'submitFormField')
 	// on boucle sur les champs selectionne pour recuperer la valeur
 	if (is_array($objexport->array_export_TypeFields[0]))
 	{
-		$_SESSION["export_FilterValue_fields"]=array();
+		$_SESSION["export_filtered_fields"]=array();
 		//var_dump($_POST);
 		foreach($objexport->array_export_TypeFields[0] as $code => $type)	// $code: s.fieldname $value: Text|Boolean|List:ccc
 		{
@@ -366,7 +368,7 @@ if ($step == 4 && $action == 'submitFormField')
 			}
 		}
 		$array_filtervalue=(! empty($objexport->array_export_FilterValue[0])?$objexport->array_export_FilterValue[0]:'');
-		$_SESSION["export_FilterValue_fields"]=$array_filtervalue;
+		$_SESSION["export_filtered_fields"]=$array_filtervalue;
 	}
 }
 
