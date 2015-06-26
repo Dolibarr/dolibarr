175cffbec8e84b4b4bdc950599b9a08c3e1a4ac9
Maxime Kohlhaas <maxime@atm-consulting.fr>
2015-06-26 16:59:19 +0200

    Debug : calendar must always be displayed on top of other layers

--- htdocs/core/js/lib_head.js
+++ htdocs/core/js/lib_head.js
@@ -57,7 +57,8 @@ function showDP(base,dateFieldID,format,codelang)
 
 	showDP.box=document.createElement("div");
 	showDP.box.className="bodyline";
-	showDP.box.style.siplay="block";
+	showDP.box.style.display="block";
+	showDP.box.style.zIndex="1000";
 	showDP.box.style.position="absolute";
 	showDP.box.style.top=thetop + "px";
 	showDP.box.style.left=theleft + "px";
