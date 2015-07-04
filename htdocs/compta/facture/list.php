e9b6a6a61e04416de973fad5bda40e89fd31f9e4
Laurent Destailleur <eldy@destailleur.fr>
2015-07-04 10:50:41 +0200

    NEW Add filter on status on invoice list

--- htdocs/compta/facture/list.php
+++ htdocs/compta/facture/list.php
@@ -330,7 +330,10 @@ if ($resql)
     print '<td class="liste_titre"></td>';
     print '<td class="liste_titre" align="right"><input class="flat" type="text" size="6" name="search_montant_ttc" value="'.$search_montant_ttc.'"></td>';
     print '<td class="liste_titre"></td>';
-    print '<td class="liste_titre"></td>';
+    print '<td class="liste_titre" align="right">';
+	$liststatus=array('0'=>$langs->trans("Draft"), '1'=>$langs->trans("Unpaid"), '2'=>$langs->trans("Paid"), '3'=>$langs->trans("Cancel"));
+	print $form->selectarray('search_status', $liststatus, $search_status, 1);
+    print '</td>';
     print '<td class="liste_titre" align="right"><input type="image" class="liste_titre" name="button_search" src="'.img_picto($langs->trans("Search"),'search.png','','',1).'" value="'.dol_escape_htmltag($langs->trans("Search")).'" title="'.dol_escape_htmltag($langs->trans("Search")).'">';
 	print '<input type="image" class="liste_titre" name="button_removefilter" src="'.img_picto($langs->trans("Search"),'searchclear.png','','',1).'" value="'.dol_escape_htmltag($langs->trans("RemoveFilter")).'" title="'.dol_escape_htmltag($langs->trans("RemoveFilter")).'">';
     print "</td></tr>\n";
