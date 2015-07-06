b88f209e52cae61c9937810b46f40f2b56d6f782
elarifr <github@accedinfo.com>
2015-07-06 20:24:35 +0200

    alexandre function getNomUtl added

--- htdocs/accountancy/class/accountingaccount.class.php
+++ htdocs/accountancy/class/accountingaccount.class.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2013-2014 Olivier Geffroy      <jeff@jeffinfo.com>
- * Copyright (C) 2013-2014 Alexandre Spangaro   <alexandre.spangaro@gmail.com>
+ * Copyright (C) 2013-2015 Alexandre Spangaro   <alexandre.spangaro@gmail.com>
  * Copyright (C) 2013-2014 Florian Henry        <florian.henry@open-concept.pro>
  * Copyright (C) 2014      Juanjo Menent        <jmenent@2byte.es>
  * Copyright (C) 2015      Ari Elbaz (elarifr)  <github@accedinfo.com>
@@ -67,8 +67,7 @@ class AccountingAccount extends CommonObject
 	 */
 	function fetch($rowid = null, $account_number = null)
 	{
-		if ($rowid || $account_number)
-		{
+		if ($rowid || $account_number) {
 			$sql = "SELECT rowid, datec, tms, fk_pcg_version, pcg_type, pcg_subtype, account_number, account_parent, label, fk_user_author, fk_user_modif, active";
 			$sql.= " FROM " . MAIN_DB_PREFIX . "accountingaccount WHERE";
 			if ($rowid) {
@@ -79,12 +78,10 @@ class AccountingAccount extends CommonObject
 
 			dol_syslog(get_class($this) . "::fetch sql=" . $sql, LOG_DEBUG);
 			$result = $this->db->query($sql);
-			if ($result)
-			{
+			if ($result) {
 				$obj = $this->db->fetch_object($result);
 
-				if ($obj)
-				{
+				if ($obj) {
 					$this->id = $obj->rowid;
 					$this->rowid = $obj->rowid;
 					$this->datec = $obj->datec;
@@ -100,18 +97,13 @@ class AccountingAccount extends CommonObject
 					$this->active = $obj->active;
 
 					return $this->id;
-				}
-				else
-				{
+				} else {
 					return 0;
 				}
-			}
-			else
-			{
+			} else {
 				dol_print_error($this->db);
 			}
 		}
-
 		return -1;
 	}
 
@@ -342,6 +334,31 @@ class AccountingAccount extends CommonObject
 		} else {
 			return - 1;
 		}
+	}
+
+	/**
+	 *	Return clicable name (with picto eventually)
+	 *
+	 *	@param		int		$withpicto		0=No picto, 1=Include picto into link, 2=Only picto
+	 *	@return		string					Chaine avec URL
+	 */
+	function getNomUrl($withpicto=0)
+	{
+		global $langs;
+
+		$result='';
+
+		$link = '<a href="'.DOL_URL_ROOT.'/accountancy/admin/card.php?id='.$this->id.'">';
+		$linkend='</a>';
+
+		$picto='billr';
+
+		$label=$langs->trans("Show").': '.$this->account_number.' - '.$this->label;
+
+		if ($withpicto) $result.=($link.img_object($label,$picto).$linkend);
+		if ($withpicto && $withpicto != 2) $result.=' ';
+		if ($withpicto != 2) $result.=$link.$this->account_number.$linkend;
+		return $result;
 	}
 
 	/**
