eff573e753ac1a061951ab0b59236ea3a762ebd7
Marcos García de La Fuente <marcosgdf@gmail.com>
2015-07-07 13:06:56 +0200

    Removed duplicated code from core modules

--- htdocs/core/modules/modApi.class.php
+++ htdocs/core/modules/modApi.class.php
@@ -232,21 +232,5 @@ class modApi extends DolibarrModules
 
 		return $this->_init($sql, $options);
 	}
-
-	/**
-	 *		Function called when module is disabled.
-	 *      Remove from database constants, boxes and permissions from Dolibarr database.
-	 *		Data directories are not deleted
-	 *
-     *      @param      string	$options    Options when enabling module ('', 'noboxes')
-	 *      @return     int             	1 if OK, 0 if KO
-	 */
-	function remove($options='')
-	{
-		$sql = array();
-
-		return $this->_remove($sql, $options);
-	}
-
 }
 
