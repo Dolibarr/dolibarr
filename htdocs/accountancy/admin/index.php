7160c5782f021c680329c76a6789016dd8b27c8a
elarifr <github@accedinfo.com>
2015-07-08 00:05:45 +0200

    add fill option in admin index.php experimental. no db update

--- htdocs/accountancy/admin/index.php
+++ htdocs/accountancy/admin/index.php
@@ -61,17 +61,17 @@ $list = array (
 		'ACCOUNTING_VAT_BUY_ACCOUNT',
 		'ACCOUNTING_VAT_SOLD_ACCOUNT',
 		'ACCOUNTING_ACCOUNT_SUSPENSE',
-		'ACCOUNTING_ACCOUNT_TRANSFER_CASH' 
+		'ACCOUNTING_ACCOUNT_TRANSFER_CASH'
 );
 
 /*
  * Actions
  */
- 
+
 $accounting_mode = defined('ACCOUNTING_MODE')?ACCOUNTING_MODE:'RECETTES-DEPENSES';
 
-if ($action == 'update')
-{
+if ($action == 'update') {
+  if ($_POST["button"] == $langs->trans('Modify')) {
     $error = 0;
 
     $accounting_modes = array(
@@ -80,7 +80,7 @@ if ($action == 'update')
     );
 
     $accounting_mode = GETPOST('accounting_mode','alpha');
-	
+
 	if (in_array($accounting_mode,$accounting_modes)) {
 
         if (!dolibarr_set_const($db, 'ACCOUNTING_MODE', $accounting_mode, 'chaine', 0, '', $conf->entity)) {
@@ -89,18 +89,18 @@ if ($action == 'update')
     } else {
         $error++;
     }
-	
+
 	$chartofaccounts = GETPOST('chartofaccounts', 'int');
-	
+
 	if (! empty($chartofaccounts)) {
-		
+
 		if (! dolibarr_set_const($db, 'CHARTOFACCOUNTS', $chartofaccounts, 'chaine', 0, '', $conf->entity)) {
 			$error ++;
 		}
 	} else {
 		$error ++;
 	}
-	
+
     foreach ($list as $constname) {
         $constvalue = GETPOST($constname, 'alpha');
 
@@ -124,7 +124,7 @@ if ($action == 'setlistsorttodo') {
 	$res = dolibarr_set_const($db, "ACCOUNTING_LIST_SORT_VENTILATION_TODO", $setlistsorttodo, 'yesno', 0, '', $conf->entity);
 	if (! $res > 0)
 		$error ++;
-	
+
 	if (! $error) {
 		setEventMessage($langs->trans("SetupSaved"), 'mesgs');
 	} else {
@@ -142,6 +142,51 @@ if ($action == 'setlistsortdone') {
 	} else {
 		setEventMessage($langs->trans("Error"), 'mesgs');
 	}
+}
+		foreach ($list as $key) {
+			if ($_POST['Fill_'.$key] == $langs->trans('FillEmptyValue')) {
+				switch($key) {
+					case 'ACCOUNTING_PRODUCT_BUY_ACCOUNT':
+						$sql1 = "UPDATE " . MAIN_DB_PREFIX . "product as p" ;
+						$sql1 .= " SET p.accountancy_code_buy ='" . $conf->global->ACCOUNTING_PRODUCT_BUY_ACCOUNT ."'";
+						$sql1 .= " WHERE p.fk_product_type = 0";
+						$sql1 .= " AND (p.accountancy_code_buy is NULL OR p.accountancy_code_buy ='')";
+						break;
+					case 'ACCOUNTING_PRODUCT_SOLD_ACCOUNT':
+						$sql1 = "UPDATE " . MAIN_DB_PREFIX . "product as p" ;
+						$sql1 .= " SET p.accountancy_code_sell = '" . GETPOST('ACCOUNTING_PRODUCT_SOLD_ACCOUNT', 'alpha')."'";
+						$sql1 .= " WHERE p.fk_product_type = 0";
+						$sql1 .= " AND (p.accountancy_code_sell is NULL OR p.accountancy_code_sell ='')";
+						break;
+					case 'ACCOUNTING_SERVICE_BUY_ACCOUNT':
+						$sql1 = "UPDATE " . MAIN_DB_PREFIX . "product as p" ;
+						$sql1 .= " SET p.accountancy_code_buy = '" . GETPOST('ACCOUNTING_SERVICE_BUY_ACCOUNT', 'alpha')."'" ;
+						$sql1 .= " WHERE p.fk_product_type = 1";
+						$sql1 .= " AND (p.accountancy_code_buy is NULL OR p.accountancy_code_buy ='')";
+						break;
+					case 'ACCOUNTING_SERVICE_SOLD_ACCOUNT':
+						$sql1 = "UPDATE " . MAIN_DB_PREFIX . "product as p" ;
+						$sql1 .= " SET p.accountancy_code_sell = '" . GETPOST('ACCOUNTING_SERVICE_SOLD_ACCOUNT', 'alpha')."'" ;
+						$sql1 .= " WHERE p.fk_product_type = 1";
+						$sql1 .= " AND (p.accountancy_code_sell is NULL OR p.accountancy_code_sell ='')";
+						break;
+					case 'ACCOUNTING_ACCOUNT_CUSTOMER':
+						$sql1 = "UPDATE " . MAIN_DB_PREFIX . "societe as s" ;
+						$sql1 .= " SET p.code_compta = '" . substr(GETPOST('ACCOUNTING_ACCOUNT_CUSTOMER', 'alpha'),0,-4)."' & lpad(rowid, 4, '0') ";
+						$sql1 .= " WHERE (p.code_compta is NULL OR p.code_compta = '')";
+						break;
+					case 'ACCOUNTING_ACCOUNT_SUPPLIER':
+						$sql1 = "UPDATE " . MAIN_DB_PREFIX . "societe as s" ;
+						$sql1 .= " SET p.code_compta_fournisseur  = '" . substr(GETPOST('ACCOUNTING_ACCOUNT_SUPPLIER', 'alpha'),0,-4)."' & lpad(rowid, 4, '0') ";
+						$sql1 .= " WHERE (p.code_compta_fournisseur is NULL OR p.code_compta_fournisseur  = '')";
+						break;
+				}
+				dol_syslog('accountancy/admin/index.php:: $sql=' . $sql1);
+				//$result = $db->query($sql1);
+				print_r($sql1);
+				setEventMessage($langs->trans("DefaultUpdateMade")." ".$key);
+			}
+		}
 }
 
 /*
@@ -258,6 +303,14 @@ foreach ($list as $key)
 	// Value
 	print '<td>';
 	print '<input type="text" size="20" id="'.$key.'" name="'.$key.'" value="'.$conf->global->$key.'">';
+	print '</td><td>';
+     if (in_array($key, array ( 	'ACCOUNTING_ACCOUNT_CUSTOMER',
+							'ACCOUNTING_ACCOUNT_SUPPLIER',
+							'ACCOUNTING_PRODUCT_BUY_ACCOUNT',
+							'ACCOUNTING_PRODUCT_SOLD_ACCOUNT',
+							'ACCOUNTING_SERVICE_BUY_ACCOUNT',
+							'ACCOUNTING_SERVICE_SOLD_ACCOUNT') ) ) {
+		print '<input type="submit" class=button id="Fill_'.$key.'" name="Fill_'.$key.'" value="'.$langs->trans('FillEmptyValue').'">'; }
 	print '</td></tr>';
 }
 
