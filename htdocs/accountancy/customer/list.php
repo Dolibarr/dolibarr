67652db7223a31aea9be33c3decc03ff8add5c2c
elarifr <github@accedinfo.com>
2015-07-11 16:09:40 +0200

    updated search in ventilation to do now you can search an invoice or ref or desc & also by vat to check some export if there is a % before search field it search field beginning by your request. type %request will search request in any part of the field

--- htdocs/accountancy/customer/list.php
+++ htdocs/accountancy/customer/list.php
@@ -42,10 +42,11 @@ $langs->load("accountancy");
 $action = GETPOST('action');
 $codeventil = GETPOST('codeventil', 'array');
 $mesCasesCochees = GETPOST('mesCasesCochees', 'array');
+$search_invoice = GETPOST('search_invoice','alpha');
 $search_ref     = GETPOST('search_ref','alpha');
 $search_label   = GETPOST('search_label','alpha');
 $search_desc    = GETPOST('search_desc','alpha');
-$search_vat    = GETPOST('search_vat','alpha');
+$search_vat     = GETPOST('search_vat','alpha');
 
 $sortfield = GETPOST('sortfield','alpha');
 $sortorder = GETPOST('sortorder','alpha');
@@ -132,7 +133,8 @@ print  '<script type="text/javascript">
  * Action
  */
 
-if ($action == 'ventil') {
+if ($action == $langs->trans("Ventilate"))
+{
 	print '<div><font color="red">' . $langs->trans("Processing") . '...</font></div>';
 	if (! empty($codeventil) && ! empty($mesCasesCochees)) {
 		print '<div><font color="red">' . count($mesCasesCochees) . ' ' . $langs->trans("SelectedLines") . '</font></div>';
@@ -188,20 +190,29 @@ $sql .= " WHERE f.fk_statut > 0 AND fk_code_ventilation <= 0";
 $sql .= " AND l.product_type IN (0,1)";
 $sql .= " AND (accsys.rowid='" . $conf->global->CHARTOFACCOUNTS . "' OR p.accountancy_code_sell IS NULL OR p.accountancy_code_sell ='')";
 //Add search filter like
-if (strlen(trim($search_ref))) {
+if (strlen(trim($search_invoice)))
+{
+	$sql .= " AND (f.facnumber like '%" . $search_invoice . "%')";
+}
+if (strlen(trim($search_ref))) 
+{
 	$sql .= " AND (p.ref like '" . $search_ref . "%')";
 }
-if (strlen(trim($search_label))) {
+if (strlen(trim($search_label))) 
+{
 	$sql .= " AND (p.label like '%" . $search_label . "%')";
 }
-if (strlen(trim($search_desc))) {
+if (strlen(trim($search_desc))) 
+{
 	$sql .= " AND (l.description like '%" . $search_desc . "%')";
 }
-if (strlen(trim($search_vat))) {
+if (strlen(trim($search_vat))) 
+{
 	$sql .= " AND (l.tva_tx like '" . $search_vat . "%')";
 }
 
-if (! empty($conf->multicompany->enabled)) {
+if (! empty($conf->multicompany->enabled)) 
+{
 	$sql .= " AND f.entity IN (" . getEntity("facture", 1) . ")";
 }
 //TODO: Remove comment
@@ -229,7 +240,7 @@ if ($result) {
 	print_liste_field_titre($langs->trans("RowId"), $_SERVER["PHP_SELF"],"l.rowid","",$param,'',$sortfield,$sortorder);	
 	
 	print '<form action="' . $_SERVER["PHP_SELF"] . '" method="post">' . "\n";
-	print '<input type="hidden" name="action" value="ventil">';
+//	print '<input type="hidden" name="action" value="ventil">';
 	
 	print '<table class="noborder" width="100%">';
 	print '<tr class="liste_titre">';
@@ -247,22 +258,23 @@ if ($result) {
 	print '<td align="center" colspan="2">' . $langs->trans("Ventilate") . '<br><label id="select-all">'.$langs->trans('All').'</label>/<label id="unselect-all">'.$langs->trans('None').'</label>'.'</td>';
 	print '</tr>';
 //	We add search filter
-/*	But Hit Enter will validate ventilation....
+//	But Hit Enter will validate ventilation....
 	print '<tr class="liste_titre">';
-	print '<td class="liste_titre" >&nbsp;</td>';
+	print '<td class="liste_titre"><input type="text" class="flat" size="10" name="search_invoice" value="' . $search_invoice . '"></td>';
 	print '<td class="liste_titre">%<input type="text" class="flat" size="10" name="search_ref" value="' . $search_ref . '"></td>';
 	print '<td class="liste_titre"><input type="text" class="flat" size="20" name="search_label" value="' . $search_label . '"></td>';
 	print '<td class="liste_titre"><input type="text" class="flat" size="30" name="search_desc" value="' . $search_desc . '"></td>';
-
-	print '<td class="liste_titre" colspan="2">&nbsp;</td>';
+	print '<td class="liste_titre">&nbsp;</td>';
 	print '<td class="liste_titre">%<input type="text" class="flat" size="5" name="search_vat" value="' . $search_vat . '"></td>';
+	print '<td class="liste_titre">&nbsp;</td>';
+	print '<td class="liste_titre">&nbsp;</td>';
 	print '<td align="right" colspan="2" class="liste_titre">';
 	print '<input type="image" class="liste_titre" src="'.img_picto($langs->trans("Search"),'search.png','','',1).'" name="button_search" value="'.dol_escape_htmltag($langs->trans("Search")).'" title="'.dol_escape_htmltag($langs->trans("Search")).'">';
 	print '&nbsp;';
 	print '<input type="image" class="liste_titre" src="'.img_picto($langs->trans("Search"),'searchclear.png','','',1).'" name="button_removefilter" value="'.dol_escape_htmltag($langs->trans("RemoveFilter")).'" title="'.dol_escape_htmltag($langs->trans("RemoveFilter")).'">';
 	print '</td>';
 	print '</tr>';
-*/
+
 	$facture_static = new Facture($db);
 	$product_static = new Product($db);
 	$form = new Form($db);
@@ -386,7 +398,7 @@ if ($result) {
 	}
 
 	print '</table>';
-	print '<br><div align="center"><input type="submit" class="butAction" value="' . $langs->trans("Ventilate") . '"></div>';
+	print '<br><div align="center"><input type="submit" class="butAction" name="action" value="' . $langs->trans("Ventilate") . '"></div>';
 	print '</form>';
 } else {
 	print $db->error();
