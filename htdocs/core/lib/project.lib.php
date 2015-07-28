a16d6b037cfab062c73f95caf53970b48cbf5185
Laurent Destailleur <eldy@destailleur.fr>
2015-07-28 12:06:57 +0200

    Fix: bad link to task

--- htdocs/core/lib/project.lib.php
+++ htdocs/core/lib/project.lib.php
@@ -378,7 +378,7 @@ function projectLinesa(&$inc, $parent, &$lines, &$level, $var, $showproject, &$t
 				// Title of task
 				print "<td>";
 				if ($showlineingray) print '<i>';
-				else print '<a href="'.DOL_URL_ROOT.'/projet/tasks/task.php?id='.$lines[$i]->id.($showproject?'':'&withproject=1').'">';
+				else print '<a href="'.DOL_URL_ROOT.'/projet/tasks/task.php?id='.$lines[$i]->id.'&withproject=1">';
 				for ($k = 0 ; $k < $level ; $k++)
 				{
 					print "&nbsp; &nbsp; &nbsp;";
