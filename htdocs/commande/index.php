676004e2e408e61a7576143e9eb68fee3a6d79ed
philippe grand <philippe.grand@atoo-net.com>
2015-08-15 15:42:54 +0200

    the form should be outside table to avoid errors

--- htdocs/commande/index.php
+++ htdocs/commande/index.php
@@ -62,15 +62,15 @@ print '<div class="fichecenter"><div class="fichethirdleft">';
 
 // Search customer orders
 $var=false;
-print '<table class="noborder nohover" width="100%">';
 print '<form method="post" action="'.DOL_URL_ROOT.'/commande/list.php">';
 print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+print '<table class="noborder nohover" width="100%">';
 print '<tr class="liste_titre"><td colspan="3">'.$langs->trans("SearchOrder").'</td></tr>';
 print '<tr '.$bc[$var].'><td>';
 print $langs->trans("Ref").':</td><td><input type="text" class="flat" name="sref" size=18></td><td rowspan="2"><input type="submit" value="'.$langs->trans("Search").'" class="button"></td></tr>';
 print '<tr '.$bc[$var].'><td class="nowrap">'.$langs->trans("Other").':</td><td><input type="text" class="flat" name="sall" size="18"></td>';
 print '</tr>';
-print "</form></table><br>\n";
+print "</table></form><br>\n";
 
 
 /*
