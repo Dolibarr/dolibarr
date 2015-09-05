8076ea85eab97f6c27859de0ed0b28f7ab8faf50
Laurent Destailleur <eldy@destailleur.fr>
2015-09-05 20:07:17 +0200

    3.6.4 was packaged

--- htdocs/filefunc.inc.php
+++ htdocs/filefunc.inc.php
@@ -29,7 +29,7 @@
  *  \brief      File that include conf.php file and commons lib like functions.lib.php
  */
 
-if (! defined('DOL_VERSION')) define('DOL_VERSION','3.6.4');
+if (! defined('DOL_VERSION')) define('DOL_VERSION','3.6.5');
 if (! defined('EURO')) define('EURO',chr(128));
 
 // Define syslog constants
