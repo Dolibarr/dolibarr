3c91b8c8900829ea1c0be6111c46be290d79d29c
Laurent Destailleur <eldy@destailleur.fr>
2015-09-09 12:11:57 +0200

    Force to select "use personalized value" to select a personalized screen option.

--- htdocs/user/param_ihm.php
+++ htdocs/user/param_ihm.php
@@ -93,7 +93,18 @@ if ($action == 'update' && ($caneditfield || ! empty($user->admin)))
         if ($_POST["check_MAIN_THEME"]=="on") $tabparam["MAIN_THEME"]=$_POST["main_theme"];
         else $tabparam["MAIN_THEME"]='';
 
-        $tabparam["MAIN_SEARCHFORM_CONTACT"]=$_POST["main_searchform_contact"];
+    	$val=(join(',',(colorStringToArray(GETPOST('THEME_ELDY_TOPMENU_BACK1'),array()))));
+    	if ($val == '') $tabparam['THEME_ELDY_TOPMENU_BACK1']='';
+        else $tabparam['THEME_ELDY_TOPMENU_BACK1']=join(',',colorStringToArray(GETPOST('THEME_ELDY_TOPMENU_BACK1'),array()));
+    
+    	$val=(join(',',(colorStringToArray(GETPOST('THEME_ELDY_BACKTITLE1'),array()))));
+    	if ($val == '') $tabparam['THEME_ELDY_BACKTITLE1']='';
+        else $tabparam['THEME_ELDY_BACKTITLE1']=join(',',colorStringToArray(GETPOST('THEME_ELDY_BACKTITLE1'),array()));
+        
+	    if (GETPOST('check_THEME_ELDY_USE_HOVER') == 'on') $tabparam["THEME_ELDY_USE_HOVER"]=1;
+    	else $tabparam["THEME_ELDY_USE_HOVER"]=0;
+        
+	    $tabparam["MAIN_SEARCHFORM_CONTACT"]=$_POST["main_searchform_contact"];
         $tabparam["MAIN_SEARCHFORM_SOCIETE"]=$_POST["main_searchform_societe"];
         $tabparam["MAIN_SEARCHFORM_PRODUITSERVICE"]=$_POST["main_searchform_produitservice"];
 
@@ -152,17 +163,48 @@ print '</table><br>';
 
 if ($action == 'edit')
 {
-	print '<script type="text/javascript" language="javascript">
-	jQuery(document).ready(function() {
-		$("#main_lang_default").change(function() {
-			$("#check_MAIN_LANG_DEFAULT").prop("checked", true);
-		});
-		$("#main_size_liste_limit").keyup(function() {
-			if ($(this).val().length) $("#check_SIZE_LISTE_LIMIT").prop("checked", true);
-			else $("#check_SIZE_LISTE_LIMIT").prop("checked", false);
-		});
-	});
-	</script>';
+    if (! empty($conf->use_javascript_ajax))
+    {/*
+        print '<script type="text/javascript" language="javascript">
+    	jQuery(document).ready(function() {
+    		$("#main_lang_default").change(function() {
+    			$("#check_MAIN_LANG_DEFAULT").prop("checked", true);
+    		});
+    		$("#main_size_liste_limit").keyup(function() {
+    			if ($(this).val().length) $("#check_SIZE_LISTE_LIMIT").prop("checked", true);
+    			else $("#check_SIZE_LISTE_LIMIT").prop("checked", false);
+    		});
+    	});
+    	</script>';*/
+    }	
+    if (! empty($conf->use_javascript_ajax))
+    {
+        print '<script type="text/javascript" language="javascript">
+        jQuery(document).ready(function() {
+        	function init_myfunc()
+        	{
+        		if (jQuery("#check_MAIN_LANG_DEFAULT").prop("checked")) { jQuery("#main_lang_default").removeAttr(\'disabled\'); }
+        		else { jQuery("#main_lang_default").attr(\'disabled\',\'disabled\'); }
+        		
+                if (jQuery("#check_SIZE_LISTE_LIMIT").prop("checked")) { jQuery("#main_size_liste_limit").removeAttr(\'disabled\'); }
+        		else { jQuery("#main_size_liste_limit").attr(\'disabled\',\'disabled\'); }
+        		
+                if (jQuery("#check_MAIN_THEME").prop("checked")) { jQuery(".themethumbs").removeAttr(\'disabled\'); }
+        		else { jQuery(".themethumbs").attr(\'disabled\',\'disabled\'); }
+            
+                if (jQuery("#check_THEME_ELDY_TOPMENU_BACK1").prop("checked")) { jQuery("#colorpickerTHEME_ELDY_TOPMENU_BACK1").removeAttr(\'disabled\'); }
+        		else { jQuery("#colorpickerTHEME_ELDY_TOPMENU_BACK1").attr(\'disabled\',\'disabled\'); }
+            }
+        	init_myfunc();
+        	jQuery("#check_SIZE_LISTE_LIMIT").click(function() { init_myfunc(); });
+            jQuery("#check_MAIN_LANG_DEFAULT").click(function() { init_myfunc(); });
+            jQuery("#check_MAIN_THEME").click(function() { init_myfunc(); });
+            jQuery("#check_THEME_ELDY_TOPMENU_BACK1").click(function() { init_myfunc(); });
+            jQuery("#check_THEME_ELDY_BACKTITLE1").click(function() { init_myfunc(); });
+        });
+        </script>';
+    }
+	
 
     clearstatcache();
     $var=true;
@@ -197,7 +239,7 @@ if ($action == 'edit')
     print '</table><br>';
 
     // Theme
-    show_theme($object,(($user->admin || empty($dolibarr_main_demo))?1:0),true);
+    show_theme($object, (($user->admin || empty($dolibarr_main_demo))?1:0), true);
 
     dol_fiche_end();
 
