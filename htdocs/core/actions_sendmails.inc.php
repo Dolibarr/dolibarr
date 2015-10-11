80bd563fdee8ed6ac2c3dbf3380cbf33465b76bf
Laurent Destailleur <eldy@destailleur.fr>
2015-10-11 20:41:06 +0200

    Use GETPOST

--- htdocs/core/actions_sendmails.inc.php
+++ htdocs/core/actions_sendmails.inc.php
@@ -56,7 +56,7 @@ if (! empty($_POST['removedfile']) && empty($_POST['removAll']))
 	$upload_dir_tmp = $vardir.'/temp';
 
 	// TODO Delete only files that was uploaded from email form
-	dol_remove_file_process($_POST['removedfile'],0);
+	dol_remove_file_process(GETPOST('removedfile','alpha'),0);
 	$action='presend';
 }
 
