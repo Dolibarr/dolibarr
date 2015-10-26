6f1d758eaecd2105fab08f2f676a5f8e657baa24
Laurent Destailleur <eldy@destailleur.fr>
2015-10-26 16:42:31 +0100

    Fix print feature for md theme

--- htdocs/theme/md/style.css.php
+++ htdocs/theme/md/style.css.php
@@ -560,11 +560,16 @@ td.showDragHandle {
 }
 #id-right {	/* This must stay id-right and not be replaced with echo $right */
 	width: 100%;
+<?php if (GETPOST("optioncss") != 'print') { ?>
 	padding-left: 194px;
 	padding-top: 12px;
+<?php } ?>	
 }
 
 .side-nav {
+<?php if (GETPOST("optioncss") == 'print') { ?>
+	display: none;
+<?php } else { ?>
 	background: #FFF;
 	border-right: 1px solid rgba(0,0,0,0.2);
 	bottom: 0;
@@ -600,6 +605,7 @@ td.showDragHandle {
 	-webkit-overflow-scrolling: touch;
 	overflow-x: hidden;
 	overflow-y: auto;
+<?php } ?>
 }
 .side-nav-vert {
 	margin-left: 194px;
