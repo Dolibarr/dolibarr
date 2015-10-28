8c84ce15768a10275f11ab886118e3678d4f1bf2
Laurent Destailleur <eldy@destailleur.fr>
2015-10-28 22:05:37 +0100

    Create table llx_stock_serial. This table may be used to store eatby and sellby date for a couple product-batch number in one place only.

--- htdocs/install/mysql/migration/3.8.0-3.9.0.sql
+++ htdocs/install/mysql/migration/3.8.0-3.9.0.sql
@@ -105,3 +105,17 @@ ALTER TABLE llx_product ADD COLUMN onportal tinyint DEFAULT 0 after tobuy;
 
 ALTER TABLE llx_user ADD COLUMN employee tinyint DEFAULT 1;
 
+
+create table llx_stock_lotserial
+(
+  rowid           integer AUTO_INCREMENT PRIMARY KEY,
+  datec           datetime,
+  tms             timestamp,
+  fk_user_creat   integer,
+  fk_user_modif   integer,
+  fk_product      integer NOT NULL,				-- Id of product
+  batch           varchar(30) DEFAULT NULL,		-- Lot or serial number
+  eatby           date DEFAULT NULL,			-- Eatby date
+  sellby          date DEFAULT NULL 			-- Sellby date
+) ENGINE=innodb;
+
