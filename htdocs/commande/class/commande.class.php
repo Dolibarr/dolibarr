ad821358790e17aa2619b3f2aad87be5ad74130a
Christophe Battarel <christophe.battarel@altairis.fr>
2015-11-05 15:36:13 +0100

    FIX: change order date on clone (as everywhere else)

--- htdocs/commande/class/commande.class.php
+++ htdocs/commande/class/commande.class.php
@@ -906,6 +906,7 @@ class Commande extends CommonOrder
         // Clear fields
         $this->user_author_id     = $user->id;
         $this->user_valid         = '';
+		$this->date				  = dol_now();
         $this->date_creation      = '';
         $this->date_validation    = '';
         $this->ref_client         = '';
