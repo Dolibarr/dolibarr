f602e1fba79fca9a1d707a27275af99bca1caf1b
aspangaro <alexandre.spangaro@gmail.com>
2015-11-12 05:48:56 +0100

    HRM : Add an email address in admin to prevent HRM external service (public accountant for exemple)

--- htdocs/hrm/admin/admin_establishment.php
+++ htdocs/hrm/admin/admin_establishment.php
@@ -22,7 +22,6 @@
  */
 require('../../main.inc.php');
 require_once DOL_DOCUMENT_ROOT.'/core/lib/hrm.lib.php';
-require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/hrm/class/establishment.class.php';
 
 $langs->load("admin");
