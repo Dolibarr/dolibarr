208bf2545fd1519c639097db6241f793a78a9adf
fmarcet <fmarcet@2byte.es>
2015-11-26 17:14:03 +0100

    Fix: Bug when idprof is mandatory on thrid creation and we use multicompany

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -2348,7 +2348,7 @@ class Societe extends CommonObject
         }
 
          //Verify duplicate entries
-        $sql  = "SELECT COUNT(*) as idprof FROM ".MAIN_DB_PREFIX."societe WHERE ".$field." = '".$value."'";
+        $sql  = "SELECT COUNT(*) as idprof FROM ".MAIN_DB_PREFIX."societe WHERE ".$field." = '".$value."' AND entity IN (".getEntity('societe',1).")";
         if($socid) $sql .= " AND rowid <> ".$socid;
         $resql = $this->db->query($sql);
         if ($resql)
