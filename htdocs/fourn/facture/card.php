39436490107c4df22d6676859b9b6f7c5639190c
phf <phf@atm-consulting.fr>
2015-12-07 16:32:57 +0100

    NEW hook function fetchOriginFourn

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -1282,6 +1282,9 @@ if ($action == 'create')
             $objectsrc = new $classname($db);
             $objectsrc->fetch(GETPOST('originid'));
             $objectsrc->fetch_thirdparty();
+			
+			$parameters=array();
+			$reshook=$hookmanager->executeHooks('fetchOriginFourn',$parameters,$objectsrc,$action); // Note that $action and $objectsrc may have been modified by some hooks
 
             $projectid			= (!empty($objectsrc->fk_project)?$objectsrc->fk_project:'');
             //$ref_client			= (!empty($objectsrc->ref_client)?$object->ref_client:'');
