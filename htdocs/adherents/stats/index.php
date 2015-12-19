1c1d176715a244c6f540d705e40b1c4a0473d27b
Laurent Destailleur <eldy@destailleur.fr>
2015-12-19 19:39:42 +0100

    Fix statistics was reporting bad value

--- htdocs/adherents/stats/index.php
+++ htdocs/adherents/stats/index.php
@@ -170,9 +170,9 @@ $data = $stats->getAllByYear();
 print '<table class="noborder">';
 print '<tr class="liste_titre" height="24">';
 print '<td align="center">'.$langs->trans("Year").'</td>';
-print '<td align="center">'.$langs->trans("NbOfSubscriptions").'</td>';
-print '<td align="center">'.$langs->trans("AmountTotal").'</td>';
-print '<td align="center">'.$langs->trans("AmountAverage").'</td>';
+print '<td align="right">'.$langs->trans("NbOfSubscriptions").'</td>';
+print '<td align="right">'.$langs->trans("AmountTotal").'</td>';
+print '<td align="right">'.$langs->trans("AmountAverage").'</td>';
 print '</tr>';
 
 $oldyear=0;
