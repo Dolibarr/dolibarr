fc92495ea6cb44bf48fe32a3d4ccf3560faed83c
phf <phf@atm-consulting.fr>
2015-12-28 17:21:56 +0100

    FIX display label only not empty

--- htdocs/societe/price.php
+++ htdocs/societe/price.php
@@ -518,7 +518,8 @@ if (! empty($conf->global->PRODUIT_CUSTOMER_PRICES)) {
                 $staticprod = new Product($db);
                 $staticprod->fetch($line->fk_product);
                 
-                print "<td>" . $staticprod->getNomUrl(1) . " - ". $staticprod->label ."</td>";
+                if (!empty($staticprod->label)) print "<td>" . $staticprod->getNomUrl(1) . " - ". $staticprod->label ."</td>";
+				else print "<td>" . $staticprod->getNomUrl(1) ."</td>";
                 print "<td>" . dol_print_date($line->datec, "dayhour") . "</td>";
                 
                 print '<td align="center">' . $langs->trans($line->price_base_type) . "</td>";
