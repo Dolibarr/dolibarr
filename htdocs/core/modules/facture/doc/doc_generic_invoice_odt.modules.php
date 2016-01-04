522f17e6f9df376209ce13fc8364b0e497e5b596
BENKE Charlie <charles.fr@benke.fr>
2016-01-04 15:07:17 +0100

    Update doc_generic_invoice_odt.modules.php

--- htdocs/core/modules/facture/doc/doc_generic_invoice_odt.modules.php
+++ htdocs/core/modules/facture/doc/doc_generic_invoice_odt.modules.php
@@ -1,7 +1,8 @@
 <?php
 /* Copyright (C) 2010-2012	Laurent Destailleur	<ely@users.sourceforge.net>
- * Copyright (C) 2012		Regis Houssin		<regis.houssin@capnetworks.com>
+* Copyright (C) 2012		Regis Houssin		<regis.houssin@capnetworks.com>
 * Copyright (C) 2014		Marcos García		<marcosgdf@gmail.com>
+* Copyright (C) 2016		Charlie Benke		<charlie@patas-monkey.com>
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
