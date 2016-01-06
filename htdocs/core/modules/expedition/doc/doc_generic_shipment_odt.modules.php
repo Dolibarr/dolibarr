743967a9b3c9f81067a1cc4abf05bb4f4869db16
BENKE Charlie <charles.fr@benke.fr>
2016-01-06 10:35:43 +0100

    Update doc_generic_shipment_odt.modules.php

--- htdocs/core/modules/expedition/doc/doc_generic_shipment_odt.modules.php
+++ htdocs/core/modules/expedition/doc/doc_generic_shipment_odt.modules.php
@@ -288,7 +288,7 @@ class doc_generic_shipment_odt extends ModelePdfExpedition
 				complete_substitutions_array($substitutionarray, $langs, $object);
 				// Call the ODTSubstitution hook
 				$parameters=array('file'=>$file,'object'=>$object,'outputlangs'=>$outputlangs,'substitutionarray'=>&$substitutionarray);
-				$reshook=$hookmanager->executeHooks('ODTSubstitution',$parameters,$this,$action);    // Note that $action and $object may have been modified by some hooks
+				$reshook=$hookmanager->executeHooks('ODTSubstitutionInit',$parameters,$this,$action);    // Note that $action and $object may have been modified by some hooks
 
 				// Line of free text
 				$newfreetext='';
