17ceca3758810e5b0bf6c907798acf80cb28991c
Laurent Destailleur <eldy@destailleur.fr>
2016-01-18 16:44:04 +0100

    Fix: no create button on list

--- htdocs/projet/tasks/list.php
+++ htdocs/projet/tasks/list.php
@@ -234,18 +234,6 @@ print "</table>";
 
 print '</form>';
 
-print '</div>';
-
-/*
- * Actions
- */
-if ($user->rights->projet->creer)
-{
-	print '<div class="tabsAction">';
-	print '<a class="butAction" href="'.DOL_URL_ROOT.'/projet/tasks.php?action=create">'.$langs->trans('AddTask').'</a>';
-	print '</div>';
-}
-
 
 llxFooter();
 
