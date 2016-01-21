7c4a9d140b7cb5c70cc5ff577175f55802898216
jfefe <jfefe@aternatik.fr>
2016-01-21 11:18:29 +0100

    Replace reserved keyword "Resource" by "DolResource"
    # Conflicts:
    #	htdocs/resource/list.php

--- htdocs/resource/add.php
+++ htdocs/resource/add.php
@@ -1,7 +1,7 @@
 <?php
-/* Copyright (C) 2013	Jean-François Ferry	 <jfefe@aternatik.fr>
- * Copyright (C) 2015	Alexandre Spangaro   <aspangaro.dolibarr@gmail.com>
- * Copyright (C) 2015	Laurent Destailleur  <eldy@users.sourceforge.net>
+/* Copyright (C) 2013   Jean-François Ferry      <jfefe@aternatik.fr>
+ * Copyright (C) 2015   Alexandre Spangaro   <aspangaro.dolibarr@gmail.com>
+ * Copyright (C) 2015   Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,15 +18,14 @@
  */
 
 /**
- *   	\file       resource/add.php
- *		\ingroup    resource
- *		\brief      Page to manage resource object
- *					Initialy built by build_class_from_table on 2013-07-24 16:03
+ *  \file       resource/add.php
+ *  \ingroup    resource
+ *  \brief      Page to manage resource object
  */
 
 require '../main.inc.php';
 
-require_once DOL_DOCUMENT_ROOT.'/resource/class/resource.class.php';
+require_once DOL_DOCUMENT_ROOT.'/resource/class/dolresource.class.php';
 require_once DOL_DOCUMENT_ROOT.'/resource/class/html.formresource.class.php';
 
 // Load traductions files required by page
@@ -36,15 +35,15 @@ $langs->load("other");
 $langs->load("resource");
 
 // Get parameters
-$id			= GETPOST('id','int');
-$action		= GETPOST('action','alpha');
-$cancel		= GETPOST('cancel','alpha');
+$id                     = GETPOST('id','int');
+$action         = GETPOST('action','alpha');
+$cancel         = GETPOST('cancel','alpha');
 if (empty($sortorder)) $sortorder="DESC";
 if (empty($sortfield)) $sortfield="t.rowid";
 if (empty($arch)) $arch = 0;
 
 if ($page == -1) {
-	$page = 0 ;
+        $page = 0 ;
 }
 
 $limit = $conf->global->limit;
@@ -56,60 +55,60 @@ $pagenext = $page + 1;
 // Protection if external user
 if ($user->societe_id > 0)
 {
-	accessforbidden();
+        accessforbidden();
 }
 
-$object = new Resource($db);
+$object = new DolResource($db);
 
 if ($action == 'confirm_add_resource')
 {
-	if (! $cancel)
-	{
-		$error='';
+        if (! $cancel)
+        {
+                $error='';
 
-		$ref=GETPOST('ref','alpha');
-		$description=GETPOST('description','alpha');
-		$fk_code_type_resource=GETPOST('fk_code_type_resource','alpha');
+                $ref=GETPOST('ref','alpha');
+                $description=GETPOST('description','alpha');
+                $fk_code_type_resource=GETPOST('fk_code_type_resource','alpha');
 
-		if (empty($ref))
-		{
-			$mesg=$langs->trans("ErrorFieldRequired",$langs->transnoentities("Ref"));
-			setEventMessages($mesg, null, 'errors');
-			$error++;
-		}
+                if (empty($ref))
+                {
+                        $mesg=$langs->trans("ErrorFieldRequired",$langs->transnoentities("Ref"));
+                        setEventMessages($mesg, null, 'errors');
+                        $error++;
+                }
 
-		if (! $error)
-		{
-			$object=new Resource($db);
-			$object->ref=$ref;
-			$object->description=$description;
-			$object->fk_code_type_resource=$fk_code_type_resource;
+                if (! $error)
+                {
+                        $object=new Dolresource($db);
+                        $object->ref=$ref;
+                        $object->description=$description;
+                        $object->fk_code_type_resource=$fk_code_type_resource;
 
-			$result=$object->create($user);
-			if ($result > 0)
-			{
-				// Creation OK
-				$db->commit();
-				setEventMessages($langs->trans('ResourceCreatedWithSuccess'), null, 'mesgs');
-				Header("Location: card.php?id=" . $object->id);
-				return;
-			}
-			else
-			{
-				// Creation KO
-				setEventMessages($object->error, $object->errors, 'errors');
-				$action = '';
-			}
-		}
-		else
-		{
-			$action = '';
-		}
-	}
-	else
-	{
-		Header("Location: list.php");
-	}
+                        $result=$object->create($user);
+                        if ($result > 0)
+                        {
+                                // Creation OK
+                                $db->commit();
+                                setEventMessages($langs->trans('ResourceCreatedWithSuccess'), null, 'mesgs');
+                                Header("Location: card.php?id=" . $object->id);
+                                return;
+                        }
+                        else
+                        {
+                                // Creation KO
+                                setEventMessages($object->error, $object->errors, 'errors');
+                                $action = '';
+                        }
+                }
+                else
+                {
+                        $action = '';
+                }
+        }
+        else
+        {
+                Header("Location: list.php");
+        }
 }
 
 
@@ -122,58 +121,58 @@ $formresource = new FormResource($db);
 
 if (! $action)
 {
-	$pagetitle=$langs->trans('AddResource');
-	llxHeader('',$pagetitle,'');
-	print load_fiche_titre($pagetitle,'','title_generic');
+        $pagetitle=$langs->trans('AddResource');
+        llxHeader('',$pagetitle,'');
+        print load_fiche_titre($pagetitle,'','title_generic');
 
-	print '<form method="post" action="'.$_SERVER['PHP_SELF'].'" name="add_resource">';
-	print '<input type="hidden" name="action" value="confirm_add_resource" />';
+        print '<form method="post" action="'.$_SERVER['PHP_SELF'].'" name="add_resource">';
+        print '<input type="hidden" name="action" value="confirm_add_resource" />';
 
-	dol_fiche_head('');
+        dol_fiche_head('');
 
-	print '<table class="border" width="100%">';
+        print '<table class="border" width="100%">';
 
-	// Ref / label
-	$field = 'ref';
-	print '<tr>';
-	print '<td class="fieldrequired">';
-	print $langs->trans('ResourceFormLabel_'.$field);
-	print '</td>';
-	print '<td>';
-	print '<input type="text" name="'.$field.'" value="'.$$field.'" />';
-	print '</td>';
-	print '</tr>';
+        // Ref / label
+        $field = 'ref';
+        print '<tr>';
+        print '<td class="fieldrequired">';
+        print $langs->trans('ResourceFormLabel_'.$field);
+        print '</td>';
+        print '<td>';
+        print '<input type="text" name="'.$field.'" value="'.$$field.'" />';
+        print '</td>';
+        print '</tr>';
 
-	// Type
-	print '<tr><td width="20%">'.$langs->trans("ResourceType").'</td>';
-	print '<td>';
-	$ret = $formresource->select_types_resource($object->fk_code_type_resource, 'fk_code_type_resource', '', 2, 1);
-	print '</td></tr>';
+        // Type
+        print '<tr><td width="20%">'.$langs->trans("ResourceType").'</td>';
+        print '<td>';
+        $ret = $formresource->select_types_resource($object->fk_code_type_resource, 'fk_code_type_resource', '', 2, 1);
+        print '</td></tr>';
 
-	// Description
-	$field = 'description';
-	print '<tr>';
-	print '<td class="tdtop">';
-	print $langs->trans('ResourceFormLabel_'.$field);
-	print '</td>';
-	print '<td>';
-	require_once DOL_DOCUMENT_ROOT.'/core/class/doleditor.class.php';
-	$doleditor = new DolEditor($field, $$field, 160, '', '', false);
-	$doleditor->Create();
-	print '</td>';
-	print '</tr>';
+        // Description
+        $field = 'description';
+        print '<tr>';
+        print '<td class="tdtop">';
+        print $langs->trans('ResourceFormLabel_'.$field);
+        print '</td>';
+        print '<td>';
+        require_once DOL_DOCUMENT_ROOT.'/core/class/doleditor.class.php';
+        $doleditor = new DolEditor($field, $$field, 160, '', '', false);
+        $doleditor->Create();
+        print '</td>';
+        print '</tr>';
 
-	print '</table>';
+        print '</table>';
 
-	dol_fiche_end('');
+        dol_fiche_end('');
 
-	echo '<div align="center">',
-	'<input type="submit" class="button" name="add" value="'.$langs->trans('Save').'" />',
-	' &nbsp; ',
-	'<input type="submit" class="button" name="cancel" value="'.$langs->trans("Cancel").'" />',
-	'</div>';
+        echo '<div align="center">',
+        '<input type="submit" class="button" name="add" value="'.$langs->trans('Save').'" />',
+        ' &nbsp; ',
+        '<input type="submit" class="button" name="cancel" value="'.$langs->trans("Cancel").'" />',
+        '</div>';
 
-	print '</form>';
+        print '</form>';
 }
 
 
