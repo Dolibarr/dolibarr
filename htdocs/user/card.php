dadc24462a417038e9a7d22aaa640fca524fdfb0
Laurent Destailleur <eldy@destailleur.fr>
2016-01-22 14:14:39 +0100

    Fix: We must ue the "small" size of imge to show on card pages.

--- htdocs/user/card.php
+++ htdocs/user/card.php
@@ -1783,7 +1783,7 @@ else
 
             // Photo
             print '<td align="center" valign="middle" width="25%" rowspan="'.$rowspan.'">';
-            print $form->showphoto('userphoto',$object,100,0,$caneditfield);
+            print $form->showphoto('userphoto',$object,100,0,$caneditfield,'photowithmargin','small');
             print '</td>';
 
             print '</tr>';
