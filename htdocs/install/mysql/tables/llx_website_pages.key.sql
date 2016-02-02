1fb1da5be4abdc914bab76657c6eacbbb05bb117
Laurent Destailleur <eldy@destailleur.fr>
2016-02-02 16:24:12 +0100

    Work on website module

--- htdocs/install/mysql/tables/llx_website_pages.key.sql
+++ htdocs/install/mysql/tables/llx_website_pages.key.sql
@@ -20,4 +20,3 @@ ALTER TABLE llx_website_page ADD UNIQUE INDEX uk_website_page_url (fk_website,pa
 
 ALTER TABLE llx_website_page ADD CONSTRAINT fk_website_page_website FOREIGN KEY (fk_website) REFERENCES llx_website (rowid);
 
-
