87c5854382640069f4895e4b3253b968b6f22c00
BENKE Charlie <charles.fr@benke.fr>
2016-02-04 16:58:19 +0100

    just change for rerun travis cheking

--- htdocs/core/tpl/contacts.tpl.php
+++ htdocs/core/tpl/contacts.tpl.php
@@ -1,6 +1,6 @@
 <?php
-/* Copyright (C) 2012      Regis Houssin       <regis.houssin@capnetworks.com>
- * Copyright (C) 2013-2015 Laurent Destailleur <eldy@users.sourceforge.net>
+/* Copyright (C) 2012      Regis Houssin        <regis.houssin@capnetworks.com>
+ * Copyright (C) 2013-2015 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2015-2016 Charlie BENKE 	<charlie@patas-monkey.com>
  *
  * This program is free software; you can redistribute it and/or modify
