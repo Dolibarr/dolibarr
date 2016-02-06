c6fe8e54793feb2f7c1c215295313cb23dac4fcf
Laurent Destailleur <eldy@destailleur.fr>
2016-02-06 17:13:24 +0100

    Fix first install on debian.

--- htdocs/install/inc.php
+++ htdocs/install/inc.php
@@ -77,7 +77,7 @@ $conffiletoshow = "htdocs/conf/conf.php";
 //$conffiletoshow = "/etc/dolibarr/conf.php";
 
 
-if (! defined('DONOTLOADCONF') && file_exists($conffile))
+if (! defined('DONOTLOADCONF') && file_exists($conffile) && filesize($conffile) > 8)    // Test on filesize is to ensure that conf file is more that an empty template with just <?php in first line
 {
     $result=include_once $conffile;	// Load conf file
     if ($result)
