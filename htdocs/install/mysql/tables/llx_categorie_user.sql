e3444e08a7fad56c992d1b0fea1ddf11402948c9
BENKE Charlie <charles.fr@benke.fr>
2016-02-13 10:50:46 +0100

    Create llx_categorie_user.sql

---
+++ htdocs/install/mysql/tables/llx_categorie_user.sql
@@ -0,0 +1,27 @@
+-- ============================================================================
+-- Copyright (C) 2016	Charlie Benke	<charlie@patas-monkey.com>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+--
+-- ===========================================================================
+--
+-- Table structure for table `llx_categorie_user`
+--
+
+CREATE TABLE IF NOT EXISTS `llx_categorie_user` (
+  `fk_categorie` int(11) NOT NULL,
+  `fk_user` int(11) NOT NULL,
+  `import_key` varchar(14) DEFAULT NULL,
+  PRIMARY KEY (`fk_categorie`,`fk_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
