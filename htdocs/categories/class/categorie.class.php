9f4909d8b1b02cb2297041de576cfc916de0ac4c
BENKE Charlie <charles.fr@benke.fr>
2016-02-13 17:27:53 +0100

    Update categorie.class.php

--- htdocs/categories/class/categorie.class.php
+++ htdocs/categories/class/categorie.class.php
@@ -9,7 +9,7 @@
  * Copyright (C) 2013       Philippe Grand          <philippe.grand@atoo-net.com>
  * Copyright (C) 2015       Marcos García           <marcosgdf@gmail.com>
  * Copyright (C) 2015       Raphaël Doursenaud      <rdoursenaud@gpcsolutions.fr>
- * Copyright (C) 2016       Charlie Benk	    <charlie@patas-monkey.com>
+ * Copyright (C) 2016       Charlie Benke	    <charlie@patas-monkey.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
