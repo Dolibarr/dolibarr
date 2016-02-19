cc524c818095d3e78fe85868f8bb6134c7a75cd6
Laurent Destailleur <eldy@destailleur.fr>
2016-02-19 04:41:29 +0100

    Fix bad picto

--- htdocs/fourn/facture/paiement.php
+++ htdocs/fourn/facture/paiement.php
@@ -534,7 +534,7 @@ if (empty($action))
         $paramlist.=(! empty($search_amount)?"&search_amount='".$search_amount:"");
         if ($optioncss != '') $paramlist.='&optioncss='.$optioncss;
 
-        print_barre_liste($langs->trans('SupplierPayments'), $page, $_SERVER["PHP_SELF"],$paramlist,$sortfield,$sortorder,'',$num);
+        print_barre_liste($langs->trans('SupplierPayments'), $page, $_SERVER["PHP_SELF"],$paramlist,$sortfield,$sortorder,'',$num, 0, 'title_accountancy.png');
 
         print '<form method="GET" action="'.$_SERVER["PHP_SELF"].'">';
         if ($optioncss != '') print '<input type="hidden" name="optioncss" value="'.$optioncss.'">';
