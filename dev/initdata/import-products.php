b46c23ab35e40969f6e86c7eba745ca3715b813d
Laurent Destailleur <eldy@destailleur.fr>
2016-02-25 03:19:11 +0100

    Fix bad file name

--- dev/initdata/import-products.php
+++ dev/initdata/import-products.php
@@ -34,7 +34,7 @@ if (substr($sapi_type, 0, 3) == 'cgi') {
 }
 
 // Recupere root dolibarr
-$path=preg_replace('/import-product.php/i','',$_SERVER["PHP_SELF"]);
+$path=preg_replace('/import-products.php/i','',$_SERVER["PHP_SELF"]);
 require ($path."../../htdocs/master.inc.php");
 include_once DOL_DOCUMENT_ROOT.'/societe/class/societe.class.php';
 include_once DOL_DOCUMENT_ROOT.'/product/class/product.class.php';
