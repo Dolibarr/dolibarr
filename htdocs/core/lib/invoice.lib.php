d0441fa05f362aab9d22d2dd3a0ff9d9e539d00e
Laurent Destailleur <eldy@destailleur.fr>
2016-03-10 11:16:12 +0100

    NEW: Show into badge on tab head the number of dedicated contacts for objects.

--- htdocs/core/lib/invoice.lib.php
+++ htdocs/core/lib/invoice.lib.php
@@ -45,8 +45,10 @@ function facture_prepare_head($object)
 
 	if (empty($conf->global->MAIN_DISABLE_CONTACTS_TAB))
 	{
-		$head[$h][0] = DOL_URL_ROOT.'/compta/facture/contact.php?facid='.$object->id;
+	    $nbContact = count($object->liste_contact(-1,'internal')) + count($object->liste_contact(-1,'external'));
+	    $head[$h][0] = DOL_URL_ROOT.'/compta/facture/contact.php?facid='.$object->id;
 		$head[$h][1] = $langs->trans('ContactsAddresses');
+		if ($nbContact > 0) $head[$h][1].= ' <span class="badge">'.$nbContact.'</span>';
 		$head[$h][2] = 'contact';
 		$h++;
 	}
