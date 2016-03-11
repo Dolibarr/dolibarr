444fce0bd9ccecef83a3dbbb8ed2704efcbd264d
Laurent Destailleur <eldy@destailleur.fr>
2016-03-11 18:34:05 +0100

    Prepare 3.7.4

--- htdocs/filefunc.inc.php
+++ htdocs/filefunc.inc.php
@@ -29,7 +29,7 @@
  *  \brief      File that include conf.php file and commons lib like functions.lib.php
  */
 
-if (! defined('DOL_VERSION')) define('DOL_VERSION','3.7.3');
+if (! defined('DOL_VERSION')) define('DOL_VERSION','3.7.4');
 if (! defined('EURO')) define('EURO',chr(128));
 
 // Define syslog constants
