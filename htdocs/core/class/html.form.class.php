68e06a46340954bd52ca32c939ef2971d2cdb762
Laurent Destailleur <eldy@destailleur.fr>
2016-03-25 15:34:23 +0100

    Revert "NEW Converted Form::textwithtooltip and Form::textwithpicto to static functions"
    This reverts commit 4d78932fe86cacbde680f3b4df04b2700622c438.

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -5084,7 +5084,7 @@ class Form
      *  @param	CommonObject	$object		Object we want to show links to
      *  @return	int							<0 if KO, >0 if OK
      */
-    function showLinkedObjectBlock(CommonObject $object)
+    function showLinkedObjectBlock($object)
     {
         global $conf,$langs,$hookmanager;
         global $bc;
