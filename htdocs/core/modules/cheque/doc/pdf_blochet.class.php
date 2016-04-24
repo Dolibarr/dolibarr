02aa3f5688be9f29bc1eade7c6eee2120965735a
Maxime Kohlhaas <maxime@atm-consulting.fr>
2016-04-24 12:14:03 +0200

    Debug double conf

--- htdocs/core/modules/cheque/doc/pdf_blochet.class.php
+++ htdocs/core/modules/cheque/doc/pdf_blochet.class.php
@@ -376,7 +376,6 @@ class BordereauChequeBlochet extends ModeleChequeReceipts
 		global $conf;
 		$default_font_size = pdf_getPDFFontSize($outputlangs);
 
-		global $conf;
 		$showdetails=$conf->global->MAIN_GENERATE_DOCUMENTS_SHOW_FOOT_DETAILS;
 		return pdf_pagefoot($pdf,$outputlangs,'BANK_CHEQUERECEIPT_FREE_TEXT',$this->emetteur,$this->marge_basse,$this->marge_gauche,$this->page_hauteur,$object,$showdetails,$hidefreetext);
 		$paramfreetext='BANK_CHEQUERECEIPT_FREE_TEXT';
