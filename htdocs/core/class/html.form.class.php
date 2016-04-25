2a0c508d475d7a06b9ee6526aa097d39e88dfd01
Christophe Battarel <christophe.battarel@altairis.fr>
2016-04-25 11:26:13 +0200

    FIX: end of select when no fournprice

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -2160,10 +2160,10 @@ class Form
                     $form.= $opt;
                     $i++;
                 }
-                $form.= '</select>';
-
-                $this->db->free($result);
             }
+
+            $form.= '</select>';
+            $this->db->free($result);
             return $form;
         }
         else
