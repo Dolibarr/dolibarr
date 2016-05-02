77494254687af3a5ba799d061c87034ae5307ed3
aspangaro <alexandre.spangaro@gmail.com>
2016-05-02 20:02:45 +0200

    Modify right to access on chart of accounts

--- htdocs/core/modules/modAccounting.class.php
+++ htdocs/core/modules/modAccounting.class.php
@@ -273,7 +273,7 @@ class modAccounting extends DolibarrModules
 		$this->rights[$r][0] = 50440;
 		$this->rights[$r][1] = 'Manage chart of accounts';
 		$this->rights[$r][2] = 'r';
-		$this->rights[$r][3] = 1;
+		$this->rights[$r][3] = 0;
 		$this->rights[$r][4] = 'chartofaccount';
 		$this->rights[$r][5] = '';
 		$r++;
