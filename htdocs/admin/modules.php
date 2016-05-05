5040fe46b0ce31adaf0dfb6507d480122f0c395d
Laurent Destailleur <eldy@destailleur.fr>
2016-05-05 11:15:53 +0200

    FIX Missing number total of modules

--- htdocs/admin/modules.php
+++ htdocs/admin/modules.php
@@ -223,7 +223,7 @@ asort($orders);
 //var_dump($modules);
 
 $nbofactivatedmodules=count($conf->modules);
-$moreinfo=$langs->trans("TotalNumberOfActivatedModules",($nbofactivatedmodules-1));
+$moreinfo=$langs->trans("TotalNumberOfActivatedModules",($nbofactivatedmodules-1), count($modules));
 if ($nbofactivatedmodules <= 1) $moreinfo .= ' '.img_warning($langs->trans("YouMustEnableOneModule"));
 print load_fiche_titre($langs->trans("ModulesSetup"),$moreinfo,'title_setup');
 
