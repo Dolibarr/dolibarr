4bde7ba5ae77d0e89e61f9f1bc11f8c713035b12
Bahfir Abbes <bafbes@gmail.com>
2016-05-08 10:26:37 +0100

    enhance test
    In thi case, I think there is no need to test also the non emptiness of the entire table

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -4250,7 +4250,7 @@ abstract class CommonObject
 
 		if (! empty($conf->global->MAIN_EXTRAFIELDS_DISABLED)) return 0;	// For avoid conflicts if trigger used
 
-        if (! empty($this->array_options) )
+        if (! empty($this->array_options["options_$key"]))
         {
             // Check parameters
             $langs->load('admin');
