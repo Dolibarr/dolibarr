105cebccc4cb6f259fbbabd8d6e998c2bf1ac7d3
nka11 <nicolas@karageuzian.com>
2016-05-12 09:29:17 +0200

    Composer vendor dir content should not be in versioning

--- htdocs/includes/jquery/plugins/jeditable/jquery.jeditable.ui-autocomplete.js
+++
@@ -1,9 +0,0 @@
-/* Create an inline datepicker which leverages the
-   jQuery UI autocomplete 
-*/
-$.editable.addInputType('autocomplete', {
-	element	: $.editable.types.text.element,
-	plugin	: function(settings, original) {
-		$('input', this).autocomplete(settings.autocomplete);
-	}
-});
