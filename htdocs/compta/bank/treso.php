e71833c0d98a22ef941278b7ae74518766b4e233
frederic34 <frederic.france@free.fr>
2016-06-19 20:24:20 +0200

    typo

--- htdocs/compta/bank/treso.php
+++ htdocs/compta/bank/treso.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2008-2009 Laurent Destailleur (Eldy)  <eldy@users.sourceforge.net>
  * Copyright (C) 2008      Raphael Bertrand (Resultic) <raphael.bertrand@resultic.fr>
  * Copyright (C) 2015	   Marcos García			   <marcosgdf@gmail.com
+ * Copyright (C) 2016      Frédéric France              <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
