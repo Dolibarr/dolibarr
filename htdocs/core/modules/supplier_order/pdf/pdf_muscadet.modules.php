9c5f1800c5a020e589dca23242538d88de547890
gauthier <gauthier.verdol@atm-consulting.fr>
2016-07-18 15:52:14 +0200

    FIX : $object->date_commande is always empty, so we can see date on pdf

--- htdocs/core/modules/supplier_order/pdf/pdf_muscadet.modules.php
+++ htdocs/core/modules/supplier_order/pdf/pdf_muscadet.modules.php
@@ -1071,12 +1071,12 @@ class pdf_muscadet extends ModelePDFSuppliersOrders
 			}
 		}
 
-		if (! empty($object->date_commande))
+		if (! empty($object->date))
 		{
 			$posy+=4;
 			$pdf->SetXY($posx,$posy);
 			$pdf->SetTextColor(0,0,60);
-			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("OrderDate")." : " . dol_print_date($object->date_commande,"day",false,$outputlangs,true), '', 'R');
+			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("OrderDate")." : " . dol_print_date($object->date,"day",false,$outputlangs,true), '', 'R');
 		}
 		else
 		{
