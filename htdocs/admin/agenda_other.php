6acff5dcb5ab514da4f5803b34a8b5e1206f5814
BENKE Charlie <charles.fr@benke.fr>
2016-07-25 11:31:10 +0200

    Update agenda_other.php

--- htdocs/admin/agenda_other.php
+++ htdocs/admin/agenda_other.php
@@ -1,8 +1,9 @@
 <?php
-/* Copyright (C) 2008-2015	Laurent Destailleur             <eldy@users.sourceforge.net>
- * Copyright (C) 2011		Regis Houssin                   <regis.houssin@capnetworks.com>
- * Copyright (C) 2011-2013  Juanjo Menent                   <jmenent@2byte.es>
- * Copyright (C) 2015		Jean-François Ferry		<jfefe@aternatik.fr>
+/* Copyright (C) 2008-2015	Laurent Destailleur     <eldy@users.sourceforge.net>
+ * Copyright (C) 2011		Regis Houssin           <regis.houssin@capnetworks.com>
+ * Copyright (C) 2011-2013  	Juanjo Menent           <jmenent@2byte.es>
+ * Copyright (C) 2015		Jean-François Ferry	<jfefe@aternatik.fr>
+ * Copyright (C) 2016		Charlie Benke		<charlie@patas-monkey.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
