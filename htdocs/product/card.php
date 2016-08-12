b7420372b7ea8d39fc043caeec25ad195921c7aa
aspangaro <alexandre.spangaro@gmail.com>
2016-08-12 22:53:30 +0200

    Uniformize presentation, add button cancel everywhere

--- htdocs/product/card.php
+++ htdocs/product/card.php
@@ -1145,7 +1145,11 @@ else
 
         dol_fiche_end();
 
-        print '<div class="center"><input type="submit" class="button" value="'.$langs->trans("Create").'"></div>';
+		print '<div class="center">';
+		print '<input type="submit" class="button" value="' . $langs->trans("Create") . '">';
+		print '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+		print '<input type="button" class="button" value="' . $langs->trans("Cancel") . '" onClick="javascript:history.go(-1)">';
+		print '</div>';
 
         print '</form>';
     }
