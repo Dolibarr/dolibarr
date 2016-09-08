308200f9bafc0c7959d03b481259099c8bb2dd73
Laurent Destailleur <eldy@destailleur.fr>
2016-09-08 18:14:31 +0200

    Fix tooltip on late button

--- htdocs/index.php
+++ htdocs/index.php
@@ -546,14 +546,15 @@ foreach($valid_dashboardlines as $board)
     $boxwork.= '<td align="right">';
     //if ($board->nbtodolate > 0)
     //{
-        $textlate = $langs->trans("Late").' = '.$langs->trans("DateReference").' > '.$langs->trans("DateToday").' '.(ceil($board->warning_delay) >= 0 ? '+' : '').ceil($board->warning_delay).' '.$langs->trans("days");
-        $boxwork.= '<a title="'.dol_escape_htmltag($textlate).'" class="dashboardlineindicatorlate'.($board->nbtodolate>0?' dashboardlineko':' dashboardlineok').'" href="'.$board->url.'"><span class="dashboardlineindicatorlate'.($board->nbtodolate>0?' dashboardlineko':' dashboardlineok').'">';
-        $boxwork.= $board->nbtodolate;
-        $boxwork.= '</span></a>';
+    $textlate = $langs->trans("NActionsLate",$board->nbtodolate);
+    $textlate .= ' ('.$langs->trans("Late").' = '.$langs->trans("DateReference").' > '.$langs->trans("DateToday").' '.(ceil($board->warning_delay) >= 0 ? '+' : '').ceil($board->warning_delay).' '.$langs->trans("days").')';
+    $boxwork.= '<a title="'.dol_escape_htmltag($textlate).'" class="dashboardlineindicatorlate'.($board->nbtodolate>0?' dashboardlineko':' dashboardlineok').'" href="'.$board->url.'"><span class="dashboardlineindicatorlate'.($board->nbtodolate>0?' dashboardlineko':' dashboardlineok').'">';
+    $boxwork.= $board->nbtodolate;
+    $boxwork.= '</span></a>';
     //}
     $boxwork.='</td>';
     $boxwork.='<td align="left">';
-    if ($board->nbtodolate > 0) $boxwork.=img_picto($langs->trans("NActionsLate",$board->nbtodolate).' (>'.ceil($board->warning_delay).' '.$langs->trans("days").')',"warning");
+    if ($board->nbtodolate > 0) $boxwork.=img_picto($textlate,"warning");
     else $boxwork.='&nbsp;';
     $boxwork.='</td>';
     /*print '<td class="nowrap" align="right">';
