ac509d6565f2cb059c1e99a3c99ad5f765fd203e
Laurent Destailleur <eldy@destailleur.fr>
2016-09-12 02:47:36 +0200

    FIX #2991

--- htdocs/opensurvey/card.php
+++ htdocs/opensurvey/card.php
@@ -46,11 +46,6 @@ if (GETPOST('id')) {
 $object=new Opensurveysondage($db);
 
 $result=$object->fetch(0, $numsondage);
-if ($result <= 0)
-{
-	dol_print_error($db,$object->error);
-	exit;
-}
 
 $expiredate=dol_mktime(0, 0, 0, GETPOST('expiremonth'), GETPOST('expireday'), GETPOST('expireyear'));
 
@@ -206,7 +201,17 @@ foreach ($toutsujet as $value)
 $toutsujet=str_replace("@","<br>",$toutsujet);
 $toutsujet=str_replace("°","'",$toutsujet);
 
-
+if (empty($object->ref))     // For survey, id is a hex string
+{
+    $langs->load("errors");
+    print $langs->trans("ErrorRecordNotFound");
+    
+    llxFooter();
+    
+    $db->close();
+    exit;
+}
+    
 print '<form name="updatesurvey" action="'.$_SERVER["PHP_SELF"].'?id='.$numsondage.'" method="POST">'."\n";
 print '<input type="hidden" name="action" value="update">';
 
@@ -442,6 +447,6 @@ print '</form>';
 
 print '<br>';
 
-llxFooterSurvey();
+llxFooter();
 
 $db->close();
