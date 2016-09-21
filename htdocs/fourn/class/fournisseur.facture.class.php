f826013cb382918f6e8984d65894bb0ed7c79761
BENKE Charlie <charles.fr@benke.fr>
2016-09-21 12:07:43 +0200

    Update fournisseur.facture.class.php

--- htdocs/fourn/class/fournisseur.facture.class.php
+++ htdocs/fourn/class/fournisseur.facture.class.php
@@ -2500,7 +2500,7 @@ class SupplierInvoiceLine extends CommonObjectLine
         $sql.= ", total_ttc='".price2num($this->total_ttc)."'";
         $sql.= " WHERE rowid = ".$this->rowid;
 
-        dol_syslog("CommandeFournisseurLigne.class.php::update_total", LOG_DEBUG);
+        dol_syslog("FactureFournisseurLigne.class.php::update_total", LOG_DEBUG);
 
         $resql=$this->db->query($sql);
         if ($resql)
