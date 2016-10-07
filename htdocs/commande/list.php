23255976ef995efe251177d252529c469ec79144
gauthier <gauthier.verdol@atm-consulting.fr>
2016-10-07 11:21:58 +0200

    FIX : require class discount

--- htdocs/commande/list.php
+++ htdocs/commande/list.php
@@ -35,6 +35,7 @@
 require '../main.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/discount.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.formother.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.formcompany.class.php';
