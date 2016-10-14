ca46ff8eb5db031f5053b7950222ecbccd156370
Laurent Destailleur <eldy@destailleur.fr>
2016-10-14 13:46:04 +0200

    Fix pagination/navigation on general ledger page

--- htdocs/accountancy/customer/list.php
+++ htdocs/accountancy/customer/list.php
@@ -4,6 +4,7 @@
  * Copyright (C) 2014-2015	Ari Elbaz (elarifr)	<github@accedinfo.com>
  * Copyright (C) 2013-2014	Florian Henry		<florian.henry@open-concept.pro>
  * Copyright (C) 2014	  	Juanjo Menent		<jmenent@2byte.es>
+ * Copyright (C) 2016	  	Laurent Destailleur <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
