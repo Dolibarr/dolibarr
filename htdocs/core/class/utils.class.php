1c5972129447a9714f22bbfde6804b9597c2a774
Laurent Destailleur <eldy@destailleur.fr>
2016-10-22 16:20:51 +0200

    FIX Solve backup when using mysqldump that return warning

--- htdocs/core/class/utils.class.php
+++ htdocs/core/class/utils.class.php
@@ -269,7 +269,7 @@ class Utils
 		            $i++;   // output line number
 		            $read = fgets($handlein);
 		            // Exclude warning line we don't want
-		            if ($i == 1 && preg_match('/'.preg_quote('Warning\*Using a password').'/i', $read)) continue;
+		            if ($i == 1 && preg_match('/Warning.*Using a password/i', $read)) continue;
 		            fwrite($handle,$read);
 		            if (preg_match('/'.preg_quote('-- Dump completed').'/i',$read)) $ok=1;
 		            elseif (preg_match('/'.preg_quote('SET SQL_NOTES=@OLD_SQL_NOTES').'/i',$read)) $ok=1;
