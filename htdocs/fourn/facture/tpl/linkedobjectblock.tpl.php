66ff13903059e02aa761cf02aff2dce9cb20ad1d
Laurent Destailleur <eldy@destailleur.fr>
2016-11-10 18:03:35 +0100

    Fix travis

--- htdocs/fourn/facture/tpl/linkedobjectblock.tpl.php
+++ htdocs/fourn/facture/tpl/linkedobjectblock.tpl.php
@@ -54,6 +54,7 @@ foreach($linkedObjectBlock as $key => $objectlink)
     	<td align="right"><a href="<?php echo $_SERVER["PHP_SELF"].'?id='.$object->id.'&action=dellink&dellinkid='.$key; ?>"><?php echo img_delete($langs->transnoentitiesnoconv("RemoveLink")); ?></a></td>
     </tr>
 <?php 
+}
 if (count($linkedObjectBlock) > 1)
 {
     ?>
