81e60f36e7dbabe2ac46f0054f7d28853e04d9d1
Laurent Destailleur <eldy@destailleur.fr>
2016-11-30 14:17:03 +0100

    Fix: message should not have color of warning

--- htdocs/theme/eldy/style.css.php
+++ htdocs/theme/eldy/style.css.php
@@ -3041,14 +3041,14 @@ div.error {
 
 /* Info admin */
 div.info {
-  color: #302010;
+  color: #303020;
   padding: 0.4em 0.4em 0.4em 0.4em;
   margin: 0.5em 0em 0.5em 0em;
-  border: 1px solid #DFBF9A;
+  border: 1px solid #AAAAAA;
   -moz-border-radius: 4px;
   -webkit-border-radius: 4px;
   border-radius: 4px;
-  background: #EFCFAA;
+  background: #D0DAD4;
   text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
 }
 
