17bc24e83e008b84b55e6ad93a59e6a724d18832
Laurent Destailleur <eldy@destailleur.fr>
2016-12-03 20:05:49 +0100

    Complete repair

--- htdocs/install/mysql/migration/repair.sql
+++ htdocs/install/mysql/migration/repair.sql
@@ -296,9 +296,9 @@ drop table tmp_c_shipment_mode;
 
 
 -- VMYSQL4.1 SET sql_mode = 'ALLOW_INVALID_DATES';
--- VMYSQL4.1 update llx_deplacementteclib set date_debut = date_create where DATE(STR_TO_DATE(date_debut, '%Y-%m-%d')) IS NULL;
+-- VMYSQL4.1 update llx_expensereport set date_debut = date_create where DATE(STR_TO_DATE(date_debut, '%Y-%m-%d')) IS NULL;
 -- VMYSQL4.1 SET sql_mode = 'NO_ZERO_DATE';
--- VMYSQL4.1 update llx_deplacementteclib set date_debut = date_create where DATE(STR_TO_DATE(date_debut, '%Y-%m-%d')) IS NULL;
+-- VMYSQL4.1 update llx_expensereport set date_debut = date_create where DATE(STR_TO_DATE(date_debut, '%Y-%m-%d')) IS NULL;
 
 -- VMYSQL4.1 SET sql_mode = 'ALLOW_INVALID_DATES';
 -- VMYSQL4.1 update llx_expensereport set date_fin = date_debut where DATE(STR_TO_DATE(date_fin, '%Y-%m-%d')) IS NULL;
@@ -306,7 +306,7 @@ drop table tmp_c_shipment_mode;
 -- VMYSQL4.1 update llx_expensereport set date_fin = date_debut where DATE(STR_TO_DATE(date_fin, '%Y-%m-%d')) IS NULL;
 
 -- VMYSQL4.1 SET sql_mode = 'ALLOW_INVALID_DATES';
--- VMYSQL4.1 update llx_expensereport set date_valid = date_create where DATE(STR_TO_DATE(date_valid, '%Y-%m-%d')) IS NULL;
+-- VMYSQL4.1 update llx_expensereport set date_valid = date_fin where DATE(STR_TO_DATE(date_valid, '%Y-%m-%d')) IS NULL;
 -- VMYSQL4.1 SET sql_mode = 'NO_ZERO_DATE';
--- VMYSQL4.1 update llx_expensereport set date_valid = date_create where DATE(STR_TO_DATE(date_valid, '%Y-%m-%d')) IS NULL;
+-- VMYSQL4.1 update llx_expensereport set date_valid = date_fin where DATE(STR_TO_DATE(date_valid, '%Y-%m-%d')) IS NULL;
 
