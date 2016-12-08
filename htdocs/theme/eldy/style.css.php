521eb3a520119c8b42e1364c3c8c121f12801dd6
Laurent Destailleur <eldy@destailleur.fr>
2016-12-08 12:44:58 +0100

    Prepare 5.0

--- htdocs/theme/eldy/style.css.php
+++ htdocs/theme/eldy/style.css.php
@@ -3041,10 +3041,10 @@ div.error {
 
 /* Info admin */
 div.info {
-  color: #303020;
+  color: #303035;
   padding: 0.4em 0.4em 0.4em 0.4em;
   margin: 0.5em 0em 0.5em 0em;
-  border: 1px solid #AAAAAA;
+  border: 1px solid #e0e0e0;
   -moz-border-radius: 4px;
   -webkit-border-radius: 4px;
   border-radius: 4px;
