9694a001792d8060b29cf79f35ebb3dd6531339d
Laurent Destailleur <eldy@destailleur.fr>
2017-01-02 12:39:12 +0100

    Fix sort is lost on agenda/event of projects

--- htdocs/projet/info.php
+++ htdocs/projet/info.php
@@ -35,6 +35,18 @@ $ref    = GETPOST('ref','alpha');
 $socid  = GETPOST('socid','int');
 $action = GETPOST('action','alpha');
 
+$limit = GETPOST("limit")?GETPOST("limit","int"):$conf->liste_limit;
+$sortfield = GETPOST("sortfield","alpha");
+$sortorder = GETPOST("sortorder");
+$page = GETPOST("page");
+$page = is_numeric($page) ? $page : 0;
+$page = $page == -1 ? 0 : $page;
+if (! $sortfield) $sortfield="a.datep,a.id";
+if (! $sortorder) $sortorder="ASC";
+$offset = $limit * $page ;
+$pageprev = $page - 1;
+$pagenext = $page + 1;
+
 if (GETPOST('actioncode','array'))
 {
     $actioncode=GETPOST('actioncode','array',3);
@@ -181,7 +193,7 @@ if (!empty($object->id))
     // List of all actions
     $filters=array();
     $filters['search_agenda_label']=$search_agenda_label;
-    show_actions_done($conf,$langs,$db,$object,null,0,$actioncode, '', $filters);
+    show_actions_done($conf,$langs,$db,$object,null,0,$actioncode, '', $filters, $sortfield, $sortorder);
 }
 
 
