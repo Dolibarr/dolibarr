2d77cb2da5ee371ae8ca00d992b1a4b9dc42fffa
Laurent Destailleur <eldy@destailleur.fr>
2017-02-07 00:54:54 +0100

    FIX #6379

--- htdocs/install/mysql/migration/4.0.0-5.0.0.sql
+++ htdocs/install/mysql/migration/4.0.0-5.0.0.sql
@@ -184,6 +184,7 @@ ALTER TABLE llx_resource_extrafields ADD INDEX idx_resource_extrafields (fk_obje
 
 INSERT INTO llx_const (name, value, type, note, visible) values (__ENCRYPT('MAIN_SIZE_SHORTLIST_LIMIT')__,__ENCRYPT('3')__,'chaine','Max length for small lists (tabs)',0);
 
+INSERT INTO llx_const (name, value, type, note, visible, entity) values (__ENCRYPT(''EXPEDITION_ADDON_NUMBER')__, __ENCRYPT('mod_expedition_safor')__,'chaine','Name for numbering manager for shipments',0,1);
 
 ALTER TABLE llx_bank_account ADD COLUMN note_public     		text;
 ALTER TABLE llx_bank_account ADD COLUMN model_pdf       		varchar(255);
