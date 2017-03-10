80702e4c0a63ad7a7e79127a575cd9f058e4dc16
Laurent Destailleur <eldy@destailleur.fr>
2017-03-10 11:22:27 +0100

    Fix Better HTML5 compliant

--- htdocs/core/boxes/box_graph_orders_permonth.php
+++ htdocs/core/boxes/box_graph_orders_permonth.php
@@ -253,7 +253,7 @@ class box_graph_orders_permonth extends ModeleBoxes
 
 		}
 		else {
-			$this->info_box_contents[0][0] = array('td' => 'align="left"',
+			$this->info_box_contents[0][0] = array('td' => '',
             'text' => $langs->trans("ReadPermissionNotAllowed"));
 		}
 	}
