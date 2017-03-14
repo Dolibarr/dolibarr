17db593b563ef33ec008e7036f4b2a37455063cc
Guido Schratzer <guido.schratzer@backbone.co.at>
2017-03-14 15:22:11 +0100

    FIX: sql error in function used_thirdparties_array
    if filter is false

--- htdocs/core/lib/agenda.lib.php
+++ htdocs/core/lib/agenda.lib.php
@@ -579,7 +579,7 @@ function used_thirdparties_array($socid=0, $filter=false)
     $sql.= ' FROM '.MAIN_DB_PREFIX."actioncomm";
     $sql.= ' where fk_soc > 0';
     if ($socid > 0) $sql.= ' AND fk_soc = '.$socid;
-    if (isset($filter)) $sql.= ' AND '.$filter;
+    if (! $filter == false) $sql.= ' AND '.$filter;
     $sql.= ' GROUP BY fk_soc;';
    	
     $resql=$db->query($sql);
