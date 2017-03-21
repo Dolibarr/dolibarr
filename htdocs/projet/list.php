e2e76b40dacaca429caeabc3f648de8b56282678
Laurent Destailleur <eldy@destailleur.fr>
2017-03-21 13:38:00 +0100

    FIX list of projects
    Conflicts:
    	htdocs/core/boxes/box_project.php

--- htdocs/projet/list.php
+++ htdocs/projet/list.php
@@ -196,6 +196,7 @@ $title=$langs->trans("Projects");
 if ($search_user == $user->id) $title=$langs->trans("MyProjects");
 
 // Get list of project id allowed to user (in a string list separated by coma)
+$projectsListId='';
 if (! $user->rights->projet->all->lire) $projectsListId = $projectstatic->getProjectsAuthorizedForUser($user,0,1,$socid);
 
 // Get id of types of contacts for projects (This list never contains a lot of elements)
@@ -245,7 +246,7 @@ if ($search_user > 0)
 $sql.= " WHERE p.entity IN (".getEntity('project',1).')';
 if (! $user->rights->projet->all->lire) $sql.= " AND p.rowid IN (".$projectsListId.")";     // public and assigned to, or restricted to company for external users
 // No need to check company, as filtering of projects must be done by getProjectsAuthorizedForUser
-if ($socid) $sql.= "  AND (p.fk_soc IS NULL OR p.fk_soc = 0 OR p.fk_soc = ".$socid.")";
+if ($socid) $sql.= " AND (p.fk_soc IS NULL OR p.fk_soc = 0 OR p.fk_soc = ".$socid.")";
 if ($search_categ > 0)    $sql.= " AND cs.fk_categorie = ".$db->escape($search_categ);
 if ($search_categ == -2)  $sql.= " AND cs.fk_categorie IS NULL";
 if ($search_ref) $sql .= natural_search('p.ref', $search_ref);
