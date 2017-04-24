86338d1781f812ebae2d584330fdd9e33cdef831
florian HENRY <florian.henry@atm-consulting.fr>
2017-04-24 09:03:25 +0200

    FIX : #6747

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -1435,8 +1435,8 @@ class Form
        {
         	if (! empty($conf->multicompany->transverse_mode))
         	{
-        		$sql.= ", ".MAIN_DB_PREFIX."usergroup_user as ug";
-        		$sql.= " WHERE ug.fk_user = u.rowid";
+        		$sql.= " LEFT JOIN ".MAIN_DB_PREFIX."usergroup_user as ug";
+        		$sql.= " ON ug.fk_user = u.rowid";
         		$sql.= " AND ug.entity = ".$conf->entity;
         	}
         	else
