2da0a129d103f09e50f91f3515299fcb84c22f89
Laurent Destailleur <eldy@destailleur.fr>
2017-05-10 23:41:43 +0200

    Multiple file upload on by default on desktop.

--- htdocs/core/class/html.formfile.class.php
+++ htdocs/core/class/html.formfile.class.php
@@ -128,7 +128,7 @@ class FormFile
             }
 
             $out .= '<input class="flat minwidth400" type="file"';
-            $out .= ((empty($conf->global->MAIN_USE_MULTIPLE_FILEUPLOAD) || $conf->browser->layout == 'phone')?' name="userfile"':' name="userfile[]" multiple');
+            $out .= ((! empty($conf->global->MAIN_DISABLE_MULTIPLE_FILEUPLOAD) || $conf->browser->layout != 'classic')?' name="userfile"':' name="userfile[]" multiple');
             $out .= (empty($conf->global->MAIN_UPLOAD_DOC) || empty($perm)?' disabled':'');
             $out .= '>';
             $out .= ' ';
