17f37cc9e1d9f879e667991c011aded6bdab45c4
phf <phf@atm-consulting.fr>
2017-05-11 11:41:00 +0200

    Fix wrong calculation

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -1084,6 +1084,7 @@ if (empty($reshook))
 										}
 
 										if ($totalamount != 0) {
+											$i = $i > 0 ? $i-1 : 0;
 										    $tva_tx = $lines[$i]->tva_tx;
 										    if (! empty($lines[$i]->vat_src_code) && ! preg_match('/\(/', $tva_tx)) $tva_tx .= ' ('.$lines[$i]->vat_src_code.')';
 											$amountdeposit[$tva_tx] = ($totalamount * $valuedeposit) / 100;
@@ -1134,7 +1135,7 @@ if (empty($reshook))
 							if (!empty($conf->global->MAIN_DEPOSIT_MULTI_TVA) && $diff != 0)
 							{
 								$object->fetch_lines();
-								$subprice_diff = $object->lines[0]->subprice - $diff / (1 + $object->lines[0]->tva_tx);
+								$subprice_diff = $object->lines[0]->subprice - $diff / (1 + $object->lines[0]->tva_tx / 100);
 								$object->updateline($object->lines[0]->id, $object->lines[0]->desc, $subprice_diff, $object->lines[0]->qty, $object->lines[0]->remise_percent, $object->lines[0]->date_start, $object->lines[0]->date_end, $object->lines[0]->tva_tx, 0, 0, 'HT', $object->lines[0]->info_bits, $object->lines[0]->product_type, 0, 0, 0, $object->lines[0]->pa_ht, $object->lines[0]->label, 0, array(), 100);
 							}
 							
