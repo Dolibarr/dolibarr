7c29e286f82f2ca3d3a77a6b67d0534067bad6b0
Laurent Destailleur <eldy@destailleur.fr>
2017-05-31 13:31:17 +0200

    Prepare 4.0.7

--- htdocs/filefunc.inc.php
+++ htdocs/filefunc.inc.php
@@ -31,7 +31,7 @@
  */
 
 if (! defined('DOL_APPLICATION_TITLE')) define('DOL_APPLICATION_TITLE','Dolibarr');
-if (! defined('DOL_VERSION')) define('DOL_VERSION','4.0.6');
+if (! defined('DOL_VERSION')) define('DOL_VERSION','4.0.7');
 
 if (! defined('EURO')) define('EURO',chr(128));
 
