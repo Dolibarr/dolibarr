a0b3dc415d6c4a23b7b729934650d2e2c59f50a3
METEO64 <benoit.lebret@meteoomnium.com>
2017-06-01 16:00:39 +0200

    Datestart dateend filtering trouble
    we need to use date start on begin of dateend filtering

--- htdocs/comm/action/listactions.php
+++ htdocs/comm/action/listactions.php
@@ -247,7 +247,7 @@ if ($filtert > 0 || $usergroup > 0)
 // The second or of next test is to take event with no end date (we suppose duration is 1 hour in such case)
 if ($dateselect > 0) $sql.= " AND ((a.datep2 >= '".$db->idate($dateselect)."' AND a.datep <= '".$db->idate($dateselect+3600*24-1)."') OR (a.datep2 IS NULL AND a.datep > '".$db->idate($dateselect-3600)."' AND a.datep <= '".$db->idate($dateselect+3600*24-1)."'))";
 if ($datestart > 0) $sql.= " AND a.datep BETWEEN '".$db->idate($datestart)."' AND '".$db->idate($datestart+3600*24-1)."'";
-if ($dateend > 0) $sql.= " AND a.datep2 BETWEEN '".$db->idate($dateend)."' AND '".$db->idate($dateend+3600*24-1)."'";
+if ($dateend > 0) $sql.= " AND a.datep2 BETWEEN '".$db->idate($datestart)."' AND '".$db->idate($dateend+3600*24-1)."'";
 $sql.= $db->order($sortfield,$sortorder);
 
 $nbtotalofrecords = '';
