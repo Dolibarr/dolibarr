02b5d9e6f7574be222e74be648d44e209d06113a
Laurent Destailleur <eldy@destailleur.fr>
2017-06-06 10:53:53 +0200

    FIX #6945

--- htdocs/contrat/document.php
+++ htdocs/contrat/document.php
@@ -55,7 +55,7 @@ $result = restrictedArea($user, 'contrat', $id);
 $sortfield = GETPOST("sortfield",'alpha');
 $sortorder = GETPOST("sortorder",'alpha');
 $page = GETPOST("page",'int');
-if ($page == -1) { $page = 0; }
+if (empty($page) || $page == -1) { $page = 0; }     // If $page is not defined, or '' or -1
 $offset = $conf->liste_limit * $page;
 $pageprev = $page - 1;
 $pagenext = $page + 1;
