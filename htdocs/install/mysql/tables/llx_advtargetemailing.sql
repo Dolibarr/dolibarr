095155a58b8e6e9a620955bcf57935304d0d89d3
Laurent Destailleur <eldy@destailleur.fr>
2017-06-20 10:06:17 +0200

    Enhance sql quality. Refused double quotes into sql content.

--- htdocs/install/mysql/tables/llx_advtargetemailing.sql
+++ htdocs/install/mysql/tables/llx_advtargetemailing.sql
@@ -14,7 +14,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program. If not, see <http://www.gnu.org/licenses/>.
 --
--- Table of "Plan de comptes" for accountancy expert module
+-- Table to setup advanced targeting for emailing
 -- ============================================================================
 
 CREATE TABLE llx_advtargetemailing
