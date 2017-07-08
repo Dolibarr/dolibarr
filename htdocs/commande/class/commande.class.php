8a9c38114f0cf47e2d5614c4dcf76c6fc8b9a691
Laurent Destailleur <eldy@destailleur.fr>
2017-07-08 19:05:51 +0200

    Fix bug reported by scrutinizer

--- htdocs/commande/class/commande.class.php
+++ htdocs/commande/class/commande.class.php
@@ -3372,14 +3372,14 @@ class Commande extends CommonOrder
      *	Return clicable link of object (with eventually picto)
      *
      *	@param      int			$withpicto                Add picto into link
-     *	@param      int			$option                   Where point the link (0=> main card, 1,2 => shipment)
+     *	@param      string	    $option                   Where point the link (0=> main card, 1,2 => shipment, 'nolink'=>No link)
      *	@param      int			$max          	          Max length to show
      *	@param      int			$short			          ???
      *  @param	    int   	    $notooltip		          1=Disable tooltip
      *  @param      int         $save_lastsearch_value    -1=Auto, 0=No save of lastsearch_values when clicking, 1=Save lastsearch_values whenclicking
      *	@return     string          			          String with URL
      */
-    function getNomUrl($withpicto=0, $option=0, $max=0, $short=0, $notooltip=0, $save_lastsearch_value=-1)
+    function getNomUrl($withpicto=0, $option='', $max=0, $short=0, $notooltip=0, $save_lastsearch_value=-1)
     {
         global $conf, $langs, $user;
 
@@ -3387,7 +3387,7 @@ class Commande extends CommonOrder
 
         $result='';
 
-        if (! empty($conf->expedition->enabled) && ($option == 1 || $option == 2)) $url = DOL_URL_ROOT.'/expedition/shipment.php?id='.$this->id;
+        if (! empty($conf->expedition->enabled) && ($option == '1' || $option == '2')) $url = DOL_URL_ROOT.'/expedition/shipment.php?id='.$this->id;
         else $url = DOL_URL_ROOT.'/commande/card.php?id='.$this->id;
 
         if ($option !== 'nolink')
