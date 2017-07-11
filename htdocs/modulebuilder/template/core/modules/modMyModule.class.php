e1d9dfc862c21ca279eb592db8472f0ae3ce31de
Laurent Destailleur <eldy@destailleur.fr>
2017-07-11 15:12:01 +0200

    Complete example of template

--- htdocs/modulebuilder/template/core/modules/modMyModule.class.php
+++ htdocs/modulebuilder/template/core/modules/modMyModule.class.php
@@ -288,6 +288,12 @@ class modMyModule extends DolibarrModules
 
 		$this->_load_tables('/mymodule/sql/');
 
+		// Create extrafields
+		include_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
+		$extrafields = new ExtraFields($this->db);
+		//$result1=$extrafields->addExtraField('myattr1', "New Attr 1 label", 'boolean', 1, 3, 'thirdparty');
+		//$result2=$extrafields->addExtraField('myattr2', "New Attr 2 label", 'string', 1, 10, 'project');
+
 		return $this->_init($sql, $options);
 	}
 
