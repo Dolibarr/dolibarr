2166b3d467f38e401690a3f5400431dc99962f2e
hguibourgdev <hguibourgdev@gmail.com>
2017-07-15 12:04:59 +0200

    Update card.php
    FIX "!" is missing on test : empty($object->date_livraison)

--- htdocs/commande/card.php
+++ htdocs/commande/card.php
@@ -2084,7 +2084,7 @@ if ($action == 'create' && $user->rights->commande->creer)
 			print '</form>';
 		} else {
 			print $object->date ? dol_print_date($object->date, 'day') : '&nbsp;';
-			if ($object->hasDelay() && empty($object->date_livraison)) {
+			if ($object->hasDelay() && ! empty($object->date_livraison)) {
 			    print ' '.img_picto($langs->trans("Late").' : '.$object->showDelay(), "warning");
 			}
 		}
