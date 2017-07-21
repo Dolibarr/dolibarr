5965bec5013d34a5e57993e2a99db35f9b13e8f0
Laurent Destailleur <eldy@destailleur.fr>
2017-07-21 15:06:53 +0200

    Update multicurrency.php

--- htdocs/admin/multicurrency.php
+++ htdocs/admin/multicurrency.php
@@ -100,7 +100,7 @@ elseif ($action == 'update_currency')
 	if ($submit == $langs->trans('Modify'))
 	{
 		$fk_multicurrency = GETPOST('fk_multicurrency', 'int');
-		$rate = GETPOST('rate', 'int');
+		$rate = price2num(GETPOST('rate', 'alpha'));
 		$currency = new MultiCurrency($db);
 
 		if ($currency->fetch($fk_multicurrency) > 0)
