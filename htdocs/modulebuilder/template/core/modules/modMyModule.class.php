cfe445ab143b54234a0359950e8f527202ce2b66
Laurent Destailleur <eldy@destailleur.fr>
2017-07-25 13:28:56 +0200

    Code comment

--- htdocs/modulebuilder/template/core/modules/modMyModule.class.php
+++ htdocs/modulebuilder/template/core/modules/modMyModule.class.php
@@ -129,6 +129,8 @@ class modMyModule extends DolibarrModules
 		// Example: $this->tabs = array('objecttype:+tabname1:Title1:mylangfile@mymodule:$user->rights->mymodule->read:/mymodule/mynewtab1.php?id=__ID__',  					// To add a new tab identified by code tabname1
         //                              'objecttype:+tabname2:SUBSTITUTION_Title2:mylangfile@mymodule:$user->rights->othermodule->read:/mymodule/mynewtab2.php?id=__ID__',  	// To add another new tab identified by code tabname2. Label will be result of calling all substitution functions on 'Title2' key.
         //                              'objecttype:-tabname:NU:conditiontoremove');                                                     										// To remove an existing tab identified by code tabname
+        // Can also be:	$this->tabs = array('data'=>'...', 'entity'=>0);
+        //
 		// where objecttype can be
 		// 'categories_x'	  to add a tab in category view (replace 'x' by type of category (0=product, 1=supplier, 2=customer, 3=member)
 		// 'contact'          to add a tab in contact view
