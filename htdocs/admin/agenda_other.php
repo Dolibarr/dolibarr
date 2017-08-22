4a310f36d877eb4f8735984529148d7766099d8c
Laurent Destailleur <eldy@destailleur.fr>
2017-08-22 08:12:30 +0200

    Fix help url

--- htdocs/admin/agenda_other.php
+++ htdocs/admin/agenda_other.php
@@ -174,12 +174,12 @@ else if ($action == 'setdoc')
 
 $formactions=new FormActions($db);
 $dirmodels=array_merge(array('/'),(array) $conf->modules_parts['models']);
-llxHeader();
+
+$wikihelp='EN:Module_Agenda_En|FR:Module_Agenda|ES:Módulo_Agenda';
+llxHeader('', $langs->trans("AgendaSetup"),$wikihelp);
 
 $linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToModuleList").'</a>';
 print load_fiche_titre($langs->trans("AgendaSetup"),$linkback,'title_setup');
-print "<br>\n";
-
 
 
 
