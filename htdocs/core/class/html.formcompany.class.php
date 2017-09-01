1ee0077e4ee9b91a47285e495f6fb383b0b2078b
Rui Strecht <rui.strecht@aliartalentos.com>
2017-09-01 16:10:19 +0100

    Fix: Added name to contributors

--- htdocs/core/class/html.formcompany.class.php
+++ htdocs/core/class/html.formcompany.class.php
@@ -2,6 +2,7 @@
 /* Copyright (C) 2008-2012	Laurent Destailleur	<eldy@users.sourceforge.net>
  * Copyright (C) 2008-2012	Regis Houssin		<regis.houssin@capnetworks.com>
  * Copyright (C) 2014		Juanjo Menent		<jmenent@2byte.es>
+ * Copyright (C) 2017		Rui Strecht			<rui.strecht@aliartalentos.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
