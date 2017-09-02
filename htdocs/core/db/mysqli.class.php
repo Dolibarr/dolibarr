3beee569a653aec0c3bb355630f4da71fe9fa4c7
Laurent Destailleur <eldy@destailleur.fr>
2017-09-02 15:12:21 +0200

    Enhance support of default value in extrafields

--- htdocs/core/db/mysqli.class.php
+++ htdocs/core/db/mysqli.class.php
@@ -779,7 +779,28 @@ class DoliDBMysqli extends DoliDB
         if ($field_desc['type'] == 'tinyint' || $field_desc['type'] == 'int' || $field_desc['type'] == 'varchar') {
         	$sql.="(".$field_desc['value'].")";
         }
-        if ($field_desc['null'] == 'not null' || $field_desc['null'] == 'NOT NULL') $sql.=" NOT NULL";
+        if ($field_desc['null'] == 'not null' || $field_desc['null'] == 'NOT NULL')
+        {
+        	// We will try to change format of column to NOT NULL. To be sure the ALTER works, we try to update fields that are NULL
+        	if ($field_desc['type'] == 'varchar' || $field_desc['type'] == 'text')
+        	{
+        		$sqlbis="UPDATE ".$table." SET ".$field_name." = '".$this->escape($field_desc['default'] ? $field_desc['default'] : '')."' WHERE ".$field_name." IS NULL";
+        		$this->query($sqlbis);
+        	}
+        	elseif ($field_desc['type'] == 'tinyint' || $field_desc['type'] == 'int')
+        	{
+        		$sqlbis="UPDATE ".$table." SET ".$field_name." = ".((int) $this->escape($field_desc['default'] ? $field_desc['default'] : 0))." WHERE ".$field_name." IS NULL";
+        		$this->query($sqlbis);
+        	}
+
+        	$sql.=" NOT NULL";
+        }
+
+        if ($field_desc['default'])
+        {
+        	if ($field_desc['type'] == 'tinyint' || $field_desc['type'] == 'int') $sql.=" DEFAULT ".$this->escape($field_desc['default']);
+        	elseif ($field_desc['type'] == 'text') $sql.=" DEFAULT '".$this->escape($field_desc['default'])."'";							// Default not supported on text fields
+        }
 
         dol_syslog(get_class($this)."::DDLUpdateField ".$sql,LOG_DEBUG);
         if (! $this->query($sql))
