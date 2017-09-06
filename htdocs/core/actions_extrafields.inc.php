9b357936088a39f9d378b0dce9a7d585b485be01
Regis Houssin <regis.houssin@capnetworks.com>
2017-09-06 11:08:23 +0200

    Fix: avoid "Uncaught Error: [] operator not supported for strings"

--- htdocs/core/actions_extrafields.inc.php
+++ htdocs/core/actions_extrafields.inc.php
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * or see http://www.gnu.org/
- * 
+ *
  * $elementype must be defined.
  */
 
@@ -25,6 +25,7 @@
 
 $maxsizestring=255;
 $maxsizeint=10;
+$mesg=array();
 
 $extrasize=GETPOST('size','int');
 $type=GETPOST('type','alpha');
