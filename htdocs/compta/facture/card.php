b87ce172c35f389c27bbde5ae15a9eb60d811414
Laurent Destailleur <eldy@destailleur.fr>
2017-09-11 13:39:03 +0200

    FIX hidden option MAIN_PROPAGATE_CONTACTS_FROM_ORIGIN

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -1249,6 +1249,7 @@ if (empty($reshook))
 						}
 
 						// Now we create same links to contact than the ones found on origin object
+						/* Useless, already into the create
 						if (! empty($conf->global->MAIN_PROPAGATE_CONTACTS_FROM_ORIGIN))
 						{
     						$originforcontact = $object->origin;
@@ -1271,7 +1272,7 @@ if (empty($reshook))
                                 }
     						}
     						else dol_print_error($resqlcontact);
-						}
+						}*/
 
 						// Hooks
 						$parameters = array('objFrom' => $srcobject);
