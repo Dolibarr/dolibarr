ffb3823655d9d84ca023057e6c3a88e003efae51
Laurent Destailleur <eldy@destailleur.fr>
2017-09-15 23:38:03 +0200

    Allow null in stock alert threshold to be compatible with #7162

--- htdocs/install/mysql/migration/6.0.0-7.0.0.sql
+++ htdocs/install/mysql/migration/6.0.0-7.0.0.sql
@@ -41,6 +41,9 @@ ALTER TABLE llx_website_page ADD COLUMN fk_user_modif integer;
 
 -- For 7.0
 
+ALTER TABLE llx_product MODIFY COLUMN seuil_stock_alerte integer DEFAULT NULL;
+-- VPGSQL8.2 ALTER TABLE llx_product ALTER COLUMN seuil_stock_alerte SET DEFAULT NULL;
+
 ALTER TABLE llx_facture_rec ADD COLUMN suspended integer DEFAULT 0;
 
 ALTER TABLE llx_facture_rec MODIFY COLUMN titre VARCHAR(100);
