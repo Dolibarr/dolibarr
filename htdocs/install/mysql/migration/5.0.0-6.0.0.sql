611f5f638dcb3ab93bce8a3536baa8bec71148a1
Laurent Destailleur <eldy@destailleur.fr>
2017-10-22 15:07:41 +0200

    FIX #7616

--- htdocs/install/mysql/migration/5.0.0-6.0.0.sql
+++ htdocs/install/mysql/migration/5.0.0-6.0.0.sql
@@ -157,6 +157,7 @@ ALTER TABLE llx_loan ADD COLUMN fk_projet integer DEFAULT NULL;
 
 ALTER TABLE llx_holiday ADD COLUMN fk_user_modif integer;
 ALTER TABLE llx_projet ADD COLUMN fk_user_modif integer;
+ALTER TABLE llx_projet_task ADD COLUMN fk_user_modif integer;
 
 ALTER TABLE llx_projet_task_time ADD COLUMN datec date;
 ALTER TABLE llx_projet_task_time ADD COLUMN tms timestamp;
