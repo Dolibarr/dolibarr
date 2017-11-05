e4ba6cfa1013ee0b8459afe0710785c3b199690a
Laurent Destailleur <eldy@destailleur.fr>
2017-11-05 03:57:26 +0100

    Debu website module

--- htdocs/core/lib/website.lib.php
+++ htdocs/core/lib/website.lib.php
@@ -34,6 +34,8 @@
 function dolWebsiteReplacementOfLinks($website, $content)
 {
 	// Replace php code. Note $content may come from database and does not contains body tags.
+
+	$content = preg_replace('/value="<\?php((?!\?>).)*\?>\n*/ims', 'value="...php..."', $content);
 	$content = preg_replace('/<\?php((?!\?>).)*\?>\n*/ims', '<span style="background: #ddd; border: 1px solid #ccc; border-radius: 4px;">...php...</span>', $content);
 
 	// Replace relative link / with dolibarr URL
