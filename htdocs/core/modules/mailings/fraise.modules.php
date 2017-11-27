a9e4fcfdca6fa094e0115e215a728d36196f3874
ptibogxiv <ptibogxiv@msn.com>
2017-11-27 15:49:28 +0100

    Update fraise.modules.php

--- htdocs/core/modules/mailings/fraise.modules.php
+++ htdocs/core/modules/mailings/fraise.modules.php
@@ -73,7 +73,7 @@ class mailing_fraise extends MailingTargets
         $statssql=array();
 
         $statssql[0] ="SELECT '".$this->db->escape($langs->trans("FundationMembers"))."' as label, count(*) as nb";
-        $statssql[0].=" FROM ".MAIN_DB_PREFIX."adherent where statut = 1 and entity = ".$conf->entity;
+        $statssql[0].=" FROM ".MAIN_DB_PREFIX."adherent where statut = 1 and entity IN (".getEntity('member').")";
 
         return $statssql;
     }
@@ -93,7 +93,7 @@ class mailing_fraise extends MailingTargets
     
         $sql  = "SELECT count(distinct(a.email)) as nb";
         $sql .= " FROM ".MAIN_DB_PREFIX."adherent as a";
-        $sql .= " WHERE (a.email IS NOT NULL AND a.email != '') AND a.entity = ".$conf->entity;
+        $sql .= " WHERE (a.email IS NOT NULL AND a.email != '') AND a.entity IN (".getEntity('member').")";
 
         // La requete doit retourner un champ "nb" pour etre comprise
         // par parent::getNbOfRecipients
