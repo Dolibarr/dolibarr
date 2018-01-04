dd8e7d657f32b82f0d1a231558ab46b98253f7fe
Laurent Destailleur <eldy@destailleur.fr>
2018-01-04 23:57:45 +0100

    Update card.php

--- htdocs/comm/propal/card.php
+++ htdocs/comm/propal/card.php
@@ -1411,9 +1411,9 @@ if ($action == 'create')
 	}
 	print '</tr>' . "\n";
 
-	// Contacts (ask contact only if thirdparty already defined). TODO do this also into order and invoice.
 	if ($socid > 0)
 	{
+         	// Contacts (ask contact only if thirdparty already defined). TODO do this also into order and invoice.
 		print "<tr><td>" . $langs->trans("DefaultContact") . '</td><td>';
 		$form->select_contacts($soc->id, $contactid, 'contactid', 1, $srccontactslist);
 		print '</td></tr>';
