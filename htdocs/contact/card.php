496ac644eb1cfc9977144b7bee49fed0e4924e83
Rui Strecht <rui.strecht@aliartalentos.com>
2018-01-25 16:10:04 +0000

    Updated label and added new translation for label

--- htdocs/contact/card.php
+++ htdocs/contact/card.php
@@ -607,7 +607,15 @@ else
             // State
             if (empty($conf->global->SOCIETE_DISABLE_STATE))
             {
-                print '<tr><td><label for="state_id">'.$langs->trans('State').'</label></td><td colspan="'.$colspan.'" class="maxwidthonsmartphone">';
+                if(!empty($conf->global->MAIN_SHOW_REGION_IN_STATE) && ($conf->global->MAIN_SHOW_REGION_IN_STATE == 1 || $conf->global->MAIN_SHOW_REGION_IN_STATE == 2))
+                {
+                    print '<tr><td><label for="state_id">'.$langs->trans('Region-State').'</label></td><td colspan="'.$colspan.'" class="maxwidthonsmartphone">';
+                }
+                else
+                {
+                    print '<tr><td><label for="state_id">'.$langs->trans('State').'</label></td><td colspan="'.$colspan.'" class="maxwidthonsmartphone">';
+                }
+
                 if ($object->country_id)
                 {
                     print $formcompany->select_state(GETPOST("state_id",'alpha')?GETPOST("state_id",'alpha'):$object->state_id,$object->country_code,'state_id');
@@ -852,7 +860,15 @@ else
             // State
             if (empty($conf->global->SOCIETE_DISABLE_STATE))
             {
-                print '<tr><td><label for="state_id">'.$langs->trans('State').'</label></td><td colspan="3" class="maxwidthonsmartphone">';
+                if(!empty($conf->global->MAIN_SHOW_REGION_IN_STATE) && ($conf->global->MAIN_SHOW_REGION_IN_STATE == 1 || $conf->global->MAIN_SHOW_REGION_IN_STATE == 2))
+                {
+                    print '<tr><td><label for="state_id">'.$langs->trans('Region-State').'</label></td><td colspan="3" class="maxwidthonsmartphone">';
+                }
+                else
+                {
+                    print '<tr><td><label for="state_id">'.$langs->trans('State').'</label></td><td colspan="3" class="maxwidthonsmartphone">';
+                }
+
                 print $formcompany->select_state($object->state_id,isset($_POST["country_id"])?GETPOST("country_id"):$object->country_id,'state_id');
                 print '</td></tr>';
             }
