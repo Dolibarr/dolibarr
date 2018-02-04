3cfb6aaa67f2daf97fe3c17f42ad5a228a94d7be
Laurent Destailleur <eldy@destailleur.fr>
2018-02-04 12:59:16 +0100

    Fix missing trigger CONTRACT_MODIFY

--- htdocs/contrat/class/contrat.class.php
+++ htdocs/contrat/class/contrat.class.php
@@ -1268,13 +1268,10 @@ class Contrat extends CommonObject
 		{
 			if (! $notrigger)
 			{
-				// Uncomment this and change MYOBJECT to your own tag if you
-				// want this action calls a trigger.
-
-				//// Call triggers
-				//$result=$this->call_trigger('MYOBJECT_MODIFY',$user);
-				//if ($result < 0) { $error++; //Do also what you must do to rollback action if trigger fail}
-				//// End call triggers
+				// Call triggers
+				$result=$this->call_trigger('CONTRACT_MODIFY',$user);
+				if ($result < 0) { $error++; }
+				// End call triggers
 				}
 			}
 
