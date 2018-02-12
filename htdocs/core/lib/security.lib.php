448fcae02fc36f196883460814f0df8416d2b0c8
Regis Houssin <regis.houssin@capnetworks.com>
2018-02-12 09:03:53 +0100

    Fix: move "fournisseur" in $checksoc

--- htdocs/core/lib/security.lib.php
+++ htdocs/core/lib/security.lib.php
@@ -378,11 +378,11 @@ function checkUserAccessToObject($user, $featuresarray, $objectid=0, $tableandsh
 		if ($feature == 'task') $feature='projet_task';
 
 		$check = array('adherent','banque','user','usergroup','produit','service','produit|service','categorie'); // Test on entity only (Objects with no link to company)
-		$checksoc = array('societe');	 // Test for societe object
+		$checksoc = array('societe','fournisseur');	 // Test for societe object
 		$checkother = array('contact','agenda');	 // Test on entity and link to third party. Allowed if link is empty (Ex: contacts...).
 		$checkproject = array('projet','project'); // Test for project object
 		$checktask = array('projet_task');
-		$nocheck = array('barcode','stock','fournisseur');	// No test
+		$nocheck = array('barcode','stock');	// No test
 		$checkdefault = 'all other not already defined'; // Test on entity and link to third party. Not allowed if link is empty (Ex: invoice, orders...).
 
 		// If dbtablename not defined, we use same name for table than module name
