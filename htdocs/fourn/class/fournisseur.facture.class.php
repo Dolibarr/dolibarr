239e83491333c71d705f6666818a98352e7a86b1
Marc de Lima Lucio <marc.delimalucio@atm-consulting.fr>
2018-02-27 10:03:51 +0100

    FIX: supplier credit notes: remove handled TODO

--- htdocs/fourn/class/fournisseur.facture.class.php
+++ htdocs/fourn/class/fournisseur.facture.class.php
@@ -2730,7 +2730,7 @@ class SupplierInvoiceLine extends CommonObjectLine
 				$this->error = $this->db->lasterror();
 			}
 		}
-		// TODO free discount linked to line as in customer invoice 
+
 		if (! $error)
 		{
 			$this->db->commit();
