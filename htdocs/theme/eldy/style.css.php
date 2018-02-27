1907b9c52dbfd4a4fe447768bd32fcda384b28b9
Laurent Destailleur <eldy@destailleur.fr>
2018-02-27 19:03:14 +0100

    Fix search on dates on template invoices

--- htdocs/theme/eldy/style.css.php
+++ htdocs/theme/eldy/style.css.php
@@ -277,7 +277,8 @@ input.select2-input {
 
 .liste_titre input[name=month_date_when], .liste_titre input[name=monthvalid], .liste_titre input[name=search_ordermonth], .liste_titre input[name=search_deliverymonth],
 .liste_titre input[name=search_smonth], .liste_titre input[name=search_month], .liste_titre input[name=search_emonth], .liste_titre input[name=smonth], .liste_titre input[name=month], .liste_titre select[name=month],
-.liste_titre input[name=month_lim], .liste_titre input[name=month_start], .liste_titre input[name=month_end], .liste_titre input[name=month_create] {
+.liste_titre input[name=month_lim], .liste_titre input[name=month_start], .liste_titre input[name=month_end], .liste_titre input[name=month_create],
+.liste_titre input[name=search_day_date_when], .liste_titre input[name=search_month_date_when], .liste_titre input[name=search_year_date_when] {
 	margin-right: 4px;
 }
 input[type=submit] {
