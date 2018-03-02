c478ab952fa8c139ab98fbd49dc4541694b7724e
Laurent Destailleur <eldy@destailleur.fr>
2018-03-02 11:57:39 +0100

    Fix block to generate donation document missing

--- htdocs/don/card.php
+++ htdocs/don/card.php
@@ -764,7 +764,7 @@ if (! empty($id) && $action != 'edit')
 	$filename	=	dol_sanitizeFileName($object->id);
 	$filedir	=	$conf->don->dir_output . "/" . dol_sanitizeFileName($object->id);
 	$urlsource	=	$_SERVER['PHP_SELF'].'?rowid='.$object->id;
-	$genallowed	=	($object->statut == 2 && ($object->paid == 0 || $user->admin) && $user->rights->don->lire);
+	$genallowed	=	(($object->paid == 0 || $user->admin) && $user->rights->don->lire);
 	$delallowed	=	$user->rights->don->creer;
 
 	print $formfile->showdocuments('donation',$filename,$filedir,$urlsource,$genallowed,$delallowed,$object->modelpdf);
