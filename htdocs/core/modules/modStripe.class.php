8c128a29854d48f3736c2d9f9cf32c3c8a929c3f
Laurent Destailleur <eldy@destailleur.fr>
2018-03-07 19:28:04 +0100

    Stripe has drop PHP 5.3

--- htdocs/core/modules/modStripe.class.php
+++ htdocs/core/modules/modStripe.class.php
@@ -71,7 +71,7 @@ class modStripe extends DolibarrModules
         // Dependencies
         $this->depends = array();		// List of modules id that must be enabled if this module is enabled
         $this->requiredby = array();	// List of modules id to disable if this one is disabled
-        $this->phpmin = array(5,3);					// Minimum version of PHP required by module
+        $this->phpmin = array(5,4);					// Minimum version of PHP required by module
         $this->need_dolibarr_version = array(5,0);	// Minimum version of Dolibarr required by module
         $this->langfiles = array("stripe");
 
