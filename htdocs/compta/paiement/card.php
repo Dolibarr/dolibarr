3287c03e67987e1479c03115fe46e4bafb839822
Laurent Destailleur <eldy@destailleur.fr>
2018-03-07 18:32:25 +0100

    Fix data on thirdparty tooltip

--- htdocs/compta/paiement/card.php
+++ htdocs/compta/paiement/card.php
@@ -338,15 +338,18 @@ if ($resql)
 		{
 			$objp = $db->fetch_object($resql);
 
-			print '<tr class="oddeven">';
+			$thirdpartystatic->fetch($objp->socid);
 
-            $invoice=new Facture($db);
-            $invoice->fetch($objp->facid);
-            $paiement = $invoice->getSommePaiement();
-            $creditnotes=$invoice->getSumCreditNotesUsed();
-            $deposits=$invoice->getSumDepositsUsed();
-            $alreadypayed=price2num($paiement + $creditnotes + $deposits,'MT');
-            $remaintopay=price2num($invoice->total_ttc - $paiement - $creditnotes - $deposits,'MT');
+			$invoice=new Facture($db);
+			$invoice->fetch($objp->facid);
+
+			$paiement = $invoice->getSommePaiement();
+			$creditnotes=$invoice->getSumCreditNotesUsed();
+			$deposits=$invoice->getSumDepositsUsed();
+			$alreadypayed=price2num($paiement + $creditnotes + $deposits,'MT');
+			$remaintopay=price2num($invoice->total_ttc - $paiement - $creditnotes - $deposits,'MT');
+
+			print '<tr class="oddeven">';
 
             // Invoice
 			print '<td>';
@@ -355,8 +358,6 @@ if ($resql)
 
 			// Third party
 			print '<td>';
-			$thirdpartystatic->id=$objp->socid;
-			$thirdpartystatic->name=$objp->name;
 			print $thirdpartystatic->getNomUrl(1);
 			print '</td>';
 
