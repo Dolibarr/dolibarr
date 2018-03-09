d82c62b638191f5ec960af456c8a841e306f8210
Maxime Kohlhaas <maxime@atm-consulting.fr>
2018-03-09 21:51:59 +0100

    Fix supplier order clone was keeping objectlinked

--- htdocs/fourn/class/fournisseur.commande.class.php
+++ htdocs/fourn/class/fournisseur.commande.class.php
@@ -321,8 +321,6 @@ class CommandeFournisseur extends CommonOrder
 
             if ($this->statut == 0) $this->brouillon = 1;
 
-			$this->fetchObjectLinked();
-
 			//$result=$this->fetch_lines();
             $this->lines=array();
 
