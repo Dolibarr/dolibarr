40fb803ffd3c43c4971a63f0252454f1e362ea88
florian HENRY <florian.henry@atm-consulting.fr>
2018-03-10 09:34:33 +0100

    scrunitizer

--- htdocs/compta/facture/class/paymentterm.class.php
+++ htdocs/compta/facture/class/paymentterm.class.php
@@ -57,7 +57,6 @@ class PaymentTerm // extends CommonObject
     function __construct($db)
     {
         $this->db = $db;
-        return 1;
     }
 
 
@@ -332,11 +331,11 @@ class PaymentTerm // extends CommonObject
 	}
 
 
- 	/**
+	/**
 	 *  Delete object in database
 	 *
-     *	@param      User	$user  		User that delete
-     *  @param      int		$notrigger	0=launch triggers after, 1=disable triggers
+	 *	@param      User	$user  		User that delete
+	 *  @param      int		$notrigger	0=launch triggers after, 1=disable triggers
 	 *	@return		int					<0 if KO, >0 if OK
 	 */
 	function delete($user, $notrigger=0)
