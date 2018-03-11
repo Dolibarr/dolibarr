5edaa9a98c240a1c9ed1865fb0cbed3e5fd3f86a
Laurent Destailleur <eldy@destailleur.fr>
2018-03-11 01:50:15 +0100

    Fix phpcs

--- htdocs/stripe/class/actions_stripe.class.php
+++ htdocs/stripe/class/actions_stripe.class.php
@@ -19,16 +19,18 @@
  */
 
 /**
- *	\file       htdocs/multicompany/actions_multicompany.class.php
- *	\ingroup    multicompany
- *	\brief      File Class multicompany
+ *	\file       htdocs/stripe/class/actions_stripe.class.php
+ *	\ingroup    stripe
+ *	\brief      File Class actionsstripeconnect
  */
 require_once DOL_DOCUMENT_ROOT.'/stripe/class/stripe.class.php';;
+
+
 $langs->load("stripe@stripe");
 
+
 /**
- *	\class      ActionsMulticompany
- *	\brief      Class Actions of the module multicompany
+ *	Class Actions Stripe Connect
  */
 class ActionsStripeconnect
 {
@@ -54,21 +56,25 @@ class ActionsStripeconnect
   
 
 	/**
-	 *
+	 * formObjectOptions
+	 * 
+	 * @param	array	$parameters		Parameters
+	 * @param	Object	$object			Object
+	 * @param	string	$action			Action
 	 */
 	function formObjectOptions($parameters=false, &$object, &$action='')
 	{
 		global $db,$conf,$user,$langs,$form;
 
-      if (! empty($conf->stripe->enabled) && (empty($conf->global->STRIPE_LIVE) || empty($conf->global->STRIPECONNECT_LIVE) || GETPOST('forcesandbox','alpha')))
-{
-	$service = 'StripeTest';
-	dol_htmloutput_mesg($langs->trans('YouAreCurrentlyInSandboxMode','Stripe'),'','warning');
-}
-else
-{
-	$service = 'StripeLive';
-}
+		if (! empty($conf->stripe->enabled) && (empty($conf->global->STRIPE_LIVE) || empty($conf->global->STRIPECONNECT_LIVE) || GETPOST('forcesandbox','alpha')))
+		{
+			$service = 'StripeTest';
+			dol_htmloutput_mesg($langs->trans('YouAreCurrentlyInSandboxMode','Stripe'),'','warning');
+		}
+		else
+		{
+			$service = 'StripeLive';
+		}
 
 		if (is_array($parameters) && ! empty($parameters))
 		{
@@ -80,88 +86,96 @@ else
 		
 
 		if (is_object($object) && $object->element == 'societe')
-		{ 
-				$this->resprints.= '<tr><td>';
-				$this->resprints.= '<table width="100%" class="nobordernopadding"><tr><td>';
-				$this->resprints.= $langs->trans('StripeCustomer');
-				$this->resprints.= '<td><td align="right">';
-//				$this->resprints.= '<a href="'.$dolibarr_main_url_root.dol_buildpath('/dolipress/card.php?socid='.$object->id, 1).'">'.img_edit().'</a>';
-				$this->resprints.= '</td></tr></table>';
-				$this->resprints.= '</td>';
-				$this->resprints.= '<td colspan="3">';
-        $stripe=new Stripe($db);
-if ($stripe->getStripeAccount($service)&&$object->client!=0) {
-$customer=$stripe->customerStripe($object->id,$stripe->getStripeAccount($service));
-$this->resprints.= $customer->id;
-}
-else {
-$this->resprints.= $langs->trans("NoStripe");
-}		 
-				$this->resprints.= '</td></tr>';
-			
-		} 
-    elseif (is_object($object) && $object->element == 'member'){
-				$this->resprints.= '<tr><td>';
-				$this->resprints.= '<table width="100%" class="nobordernopadding"><tr><td>';
-				$this->resprints.= $langs->trans('StripeCustomer');
-				$this->resprints.= '<td><td align="right">';
-				$this->resprints.= '</td></tr></table>';
-				$this->resprints.= '</td>';
-				$this->resprints.= '<td colspan="3">';
-        $stripe=new Stripe($db);
-if ($stripe->getStripeAccount($service)&&$object->fk_soc>'0') {
-$customer=$stripe->customerStripe($object->fk_soc,$stripe->getStripeAccount($service));
-$this->resprints.= $customer->id;
-}
-else {
-$this->resprints.= $langs->trans("NoStripe");
-}		 
-				$this->resprints.= '</td></tr>';    
-     
-				$this->resprints.= '<tr><td>';
-				$this->resprints.= '<table width="100%" class="nobordernopadding"><tr><td>';
-				$this->resprints.= $langs->trans('SubscriptionStripe');
-				$this->resprints.= '<td><td align="right">';
-				$this->resprints.= '</td></tr></table>';
-				$this->resprints.= '</td>';
-				$this->resprints.= '<td colspan="3">';
-        $stripe=new Stripe($db);
-if (7==4) {
-$customer=$stripe->customerStripe($object->id,$stripe->getStripeAccount($service));
-$this->resprints.= $customer->id;
-}
-else {
-$this->resprints.= $langs->trans("NoStripe");
-}		 
-				$this->resprints.= '</td></tr>';
-     }   
-   elseif (is_object($object) && $object->element == 'adherent_type'){ 
-				$this->resprints.= '<tr><td>';
-				$this->resprints.= '<table width="100%" class="nobordernopadding"><tr><td>';
-				$this->resprints.= $langs->trans('PlanStripe');
-				$this->resprints.= '<td><td align="right">';
-//				$this->resprints.= '<a href="'.$dolibarr_main_url_root.dol_buildpath('/dolipress/card.php?socid='.$object->id, 1).'">'.img_edit().'</a>';
-				$this->resprints.= '</td></tr></table>';
-				$this->resprints.= '</td>';
-				$this->resprints.= '<td colspan="3">';
-        $stripe=new Stripe($db);
-if (7==4) {
-$customer=$stripe->customerStripe($object->id,$stripe->getStripeAccount($service));
-$this->resprints.= $customer->id;
-}
-else {
-$this->resprints.= $langs->trans("NoStripe");
-}		 
-				$this->resprints.= '</td></tr>';
-     }
+		{
+			$this->resprints.= '<tr><td>';
+			$this->resprints.= '<table width="100%" class="nobordernopadding"><tr><td>';
+			$this->resprints.= $langs->trans('StripeCustomer');
+			$this->resprints.= '<td><td align="right">';
+			//				$this->resprints.= '<a href="'.$dolibarr_main_url_root.dol_buildpath('/dolipress/card.php?socid='.$object->id, 1).'">'.img_edit().'</a>';
+			$this->resprints.= '</td></tr></table>';
+			$this->resprints.= '</td>';
+			$this->resprints.= '<td colspan="3">';
+			$stripe=new Stripe($db);
+			if ($stripe->getStripeAccount($service)&&$object->client!=0) {
+				$customer=$stripe->customerStripe($object->id,$stripe->getStripeAccount($service));
+				$this->resprints.= $customer->id;
+			}
+			else {
+				$this->resprints.= $langs->trans("NoStripe");
+			}
+			$this->resprints.= '</td></tr>';
+				
+		}
+		elseif (is_object($object) && $object->element == 'member'){
+			$this->resprints.= '<tr><td>';
+			$this->resprints.= '<table width="100%" class="nobordernopadding"><tr><td>';
+			$this->resprints.= $langs->trans('StripeCustomer');
+			$this->resprints.= '<td><td align="right">';
+			$this->resprints.= '</td></tr></table>';
+			$this->resprints.= '</td>';
+			$this->resprints.= '<td colspan="3">';
+			$stripe=new Stripe($db);
+			if ($stripe->getStripeAccount($service)&&$object->fk_soc>'0') {
+				$customer=$stripe->customerStripe($object->fk_soc,$stripe->getStripeAccount($service));
+				$this->resprints.= $customer->id;
+			}
+			else {
+				$this->resprints.= $langs->trans("NoStripe");
+			}
+			$this->resprints.= '</td></tr>';
+			 
+			$this->resprints.= '<tr><td>';
+			$this->resprints.= '<table width="100%" class="nobordernopadding"><tr><td>';
+			$this->resprints.= $langs->trans('SubscriptionStripe');
+			$this->resprints.= '<td><td align="right">';
+			$this->resprints.= '</td></tr></table>';
+			$this->resprints.= '</td>';
+			$this->resprints.= '<td colspan="3">';
+			$stripe=new Stripe($db);
+			if (7==4) {
+				$customer=$stripe->customerStripe($object->id,$stripe->getStripeAccount($service));
+				$this->resprints.= $customer->id;
+			}
+			else {
+				$this->resprints.= $langs->trans("NoStripe");
+			}
+			$this->resprints.= '</td></tr>';
+		}
+		elseif (is_object($object) && $object->element == 'adherent_type'){
+			$this->resprints.= '<tr><td>';
+			$this->resprints.= '<table width="100%" class="nobordernopadding"><tr><td>';
+			$this->resprints.= $langs->trans('PlanStripe');
+			$this->resprints.= '<td><td align="right">';
+			//				$this->resprints.= '<a href="'.$dolibarr_main_url_root.dol_buildpath('/dolipress/card.php?socid='.$object->id, 1).'">'.img_edit().'</a>';
+			$this->resprints.= '</td></tr></table>';
+			$this->resprints.= '</td>';
+			$this->resprints.= '<td colspan="3">';
+			$stripe=new Stripe($db);
+			if (7==4) {
+				$customer=$stripe->customerStripe($object->id,$stripe->getStripeAccount($service));
+				$this->resprints.= $customer->id;
+			}
+			else {
+				$this->resprints.= $langs->trans("NoStripe");
+			}
+			$this->resprints.= '</td></tr>';
+		}
 		return 0;
 	}
-  
-  	function addMoreActionsButtons($parameters=false, &$object, &$action='')
+
+	/**
+	 * addMoreActionsButtons
+	 * 
+	 * @param arra	 	$parameters	Parameters
+	 * @param Object	$object		Object
+	 * @param string	$action		action
+	 * @return int					0
+	 */
+	function addMoreActionsButtons($parameters=false, &$object, &$action='')
 	{
 		global $db,$conf,$user,$langs,$form;
-    if (is_object($object) && $object->element == 'facture'){
-    			// On verifie si la facture a des paiements
+		if (is_object($object) && $object->element == 'facture'){
+			// On verifie si la facture a des paiements
 			$sql = 'SELECT pf.amount';
 			$sql .= ' FROM ' . MAIN_DB_PREFIX . 'paiement_facture as pf';
 			$sql .= ' WHERE pf.fk_facture = ' . $object->id;
@@ -183,34 +197,35 @@ $this->resprints.= $langs->trans("NoStripe");
 			$resteapayer = $object->total_ttc - $totalpaye;
 			// Request a direct debit order
 			if ($object->statut > Facture::STATUS_DRAFT && $object->statut < Facture::STATUS_ABANDONED && $object->paye == 0)
-			{   $stripe=new Stripe($db);
-			    if ($resteapayer > 0)
-			    {                    
-			        if ($stripe->getStripeAccount($conf->entity))  // a modifier avec droit stripe
-			        {
-			            $langs->load("withdrawals");
-			            print '<a class="butActionDelete" href="'.dol_buildpath('/stripeconnect/payment.php?facid='.$object->id.'&action=create', 1).'" title="'.dol_escape_htmltag($langs->trans("StripeConnectPay")).'">'.$langs->trans("StripeConnectPay").'</a>';
-			        }
-			        else
-			        {
-			            print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("NotEnoughPermissions")).'">'.$langs->trans("StripeConnectPay").'</a>';
-			        }
-			    }
-			   elseif ($resteapayer == 0)
-			    { 
-			print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("NotEnoughPermissions")).'">'.$langs->trans("StripeConnectPay").'</a>';
-         }    
+			{
+				$stripe=new Stripe($db);
+				if ($resteapayer > 0)
+				{
+					if ($stripe->getStripeAccount($conf->entity))  // a modifier avec droit stripe
+					{
+						$langs->load("withdrawals");
+						print '<a class="butActionDelete" href="'.dol_buildpath('/stripeconnect/payment.php?facid='.$object->id.'&action=create', 1).'" title="'.dol_escape_htmltag($langs->trans("StripeConnectPay")).'">'.$langs->trans("StripeConnectPay").'</a>';
+					}
+					else
+					{
+						print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("NotEnoughPermissions")).'">'.$langs->trans("StripeConnectPay").'</a>';
+					}
+				}
+				elseif ($resteapayer == 0)
+				{
+					print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("NotEnoughPermissions")).'">'.$langs->trans("StripeConnectPay").'</a>';
+				}
 			}
-      else {
-      print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("NotEnoughPermissions")).'">'.$langs->trans("StripeConnectPay").'</a>';
-      }
-    } 
-    elseif (is_object($object) && $object->element == 'invoice_supplier'){ 
-print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("StripeConnectPay")).'">'.$langs->trans("StripeConnectPay").'</a>';
-     } 
-    elseif (is_object($object) && $object->element == 'member'){ 
-print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("StripeAutoSubscription")).'">'.$langs->trans("StripeAutoSubscription").'</a>';
-     }    		           
+			else {
+				print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("NotEnoughPermissions")).'">'.$langs->trans("StripeConnectPay").'</a>';
+			}
+		}
+		elseif (is_object($object) && $object->element == 'invoice_supplier'){
+			print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("StripeConnectPay")).'">'.$langs->trans("StripeConnectPay").'</a>';
+		}
+		elseif (is_object($object) && $object->element == 'member'){
+			print '<a class="butActionRefused" href="#" title="'.dol_escape_htmltag($langs->trans("StripeAutoSubscription")).'">'.$langs->trans("StripeAutoSubscription").'</a>';
+		}
 		return 0;
 	}
 
