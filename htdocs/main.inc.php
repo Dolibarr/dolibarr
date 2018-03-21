a54316ef0582d8edbb27b8a6129f838718baafd2
Laurent Destailleur <eldy@destailleur.fr>
2018-03-21 20:43:26 +0100

    Update main.inc.php

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -1627,6 +1627,7 @@ function left_menu($menu_array_before, $helppagename='', $notused='', $menu_arra
 
 		if ($conf->use_javascript_ajax && empty($conf->global->MAIN_USE_OLD_SEARCH_FORM))
 		{
+			//$searchform.=$form->selectArrayAjax('searchselectcombo', DOL_URL_ROOT.'/core/ajax/selectsearchbox.php', $selected, '', '', 0, 1, 'vmenusearchselectcombo', 1, $langs->trans("Search"), 1);
 			$searchform.=$form->selectArrayFilter('searchselectcombo', $arrayresult, $selected, '', 1, 0, 1, 'vmenusearchselectcombo', 1, $langs->trans("Search"), 1);
 		}
 		else
