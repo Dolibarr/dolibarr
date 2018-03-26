26e2d3c4f0138b86d170eb1972687fa957e6d644
Laurent Destailleur <eldy@destailleur.fr>
2018-03-26 16:35:50 +0200

    Clean permissions tables

--- htdocs/install/mysql/migration/7.0.0-8.0.0.sql
+++ htdocs/install/mysql/migration/7.0.0-8.0.0.sql
@@ -42,6 +42,12 @@ DROP TABLE llx_c_accountingaccount;
 
 -- For 8.0
 
+delete from llx_rights_def where perms IS NULL;
+delete from llx_user_rights where fk_user not IN (select rowid from llx_user);
+delete from llx_usergroup_rights where fk_usergroup not in (select rowid from llx_usergroup);
+delete from llx_usergroup_rights where fk_id not in (select id from llx_rights_def);
+
+
 ALTER TABLE llx_c_type_fees ADD COLUMN llx_c_type_fees integer DEFAULT 0;
 
 ALTER TABLE llx_product_fournisseur_price DROP COLUMN unitcharges;
