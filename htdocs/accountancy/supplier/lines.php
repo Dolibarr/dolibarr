b0b88d18df07dd9840dae5bd87d6b4ab495470c6
Laurent Destailleur <eldy@destailleur.fr>
2018-04-20 02:08:09 +0200

    Fix class not found

--- htdocs/accountancy/supplier/lines.php
+++ htdocs/accountancy/supplier/lines.php
@@ -28,6 +28,7 @@ require '../../main.inc.php';
 
 require_once DOL_DOCUMENT_ROOT . '/core/class/html.formaccounting.class.php';
 require_once DOL_DOCUMENT_ROOT . '/fourn/class/fournisseur.facture.class.php';
+require_once DOL_DOCUMENT_ROOT . '/fourn/class/fournisseur.product.class.php';
 require_once DOL_DOCUMENT_ROOT . '/product/class/product.class.php';
 require_once DOL_DOCUMENT_ROOT . '/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT . '/core/lib/accounting.lib.php';
