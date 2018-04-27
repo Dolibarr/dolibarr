9afb4cc808df323a76df36df8d91bdcbd5fcc25b
Laurent Destailleur <eldy@destailleur.fr>
2018-04-27 11:44:17 +0200

    Update newpayment.php

--- htdocs/public/payment/newpayment.php
+++ htdocs/public/payment/newpayment.php
@@ -401,7 +401,7 @@ if ($action == 'charge' && ! empty($conf->stripe->enabled))
 	dol_syslog("POST keys  : ".join(',', array_keys($_POST)), LOG_DEBUG, 0, '_stripe');
 	dol_syslog("POST values: ".join(',', $_POST), LOG_DEBUG, 0, '_stripe');
   
-  if (GETPOST("paymenttoken",'alpha') or GETPOST("modepayment",'alpha')=='src_newcard'){
+  if (GETPOST("paymenttoken",'alpha') || GETPOST("modepayment",'alpha')=='src_newcard'){
   if (GETPOST("paymenttoken",'alpha') && NULL!==GETPOST("paymenttoken",'alpha')){
   $stripeSource = GETPOST("paymenttoken",'alpha');
   }else{
