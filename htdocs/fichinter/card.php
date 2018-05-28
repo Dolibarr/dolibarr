991c6d97c07a9626a34ee3a8f2e36d9f427b2a25
BENKE Charlene <charles.fr@benke.fr>
2018-05-28 16:44:49 +0200

    Update card.php

--- htdocs/fichinter/card.php
+++ htdocs/fichinter/card.php
@@ -5,7 +5,7 @@
  * Copyright (C) 2011-2017  Juanjo Menent			<jmenent@2byte.es>
  * Copyright (C) 2013       Florian Henry           <florian.henry@open-concept.pro>
  * Copyright (C) 2014-2018  Ferran Marcet           <fmarcet@2byte.es>
- * Copyright (C) 2014-2015 	Charlie Benke           <charlies@patas-monkey.com>
+ * Copyright (C) 2014-2018  Charlene Benke          <charlies@patas-monkey.com>
  * Copyright (C) 2015-2016  Abbes Bahfir            <bafbes@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -1643,6 +1643,14 @@ else if ($id > 0 || ! empty($ref))
 						print '<div class="inline-block divButAction"><a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&action=presend&mode=init#formmailbeforetitle">'.$langs->trans('SendMail').'</a></div>';
 					}
 					else print '<div class="inline-block divButAction"><a class="butActionRefused" href="#">'.$langs->trans('SendMail').'</a></div>';
+				}
+
+		    		// create interventional model
+				if ($object->statut == Fichinter::STATUS_DRAFT && $user->rights->ficheinter->creer && (count($object->lines) > 0)) {
+					print '<div class="inline-block divButAction">';
+					print '<a class="butAction" href="'.DOL_URL_ROOT.'/fichinter/', 1);
+					print 'card-rec.php?id='.$object->id.'&action=create"';
+					print '>'.$langs->trans("ChangeIntoRepeatableInterventional").'</a></div>';
 				}
 
 				// Proposal
