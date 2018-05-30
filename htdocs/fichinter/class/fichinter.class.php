0ce3b2b431e5c53bf29f2c4d2dc2f7a0ddd5b950
BENKE Charlene <charles.fr@benke.fr>
2018-05-30 14:19:30 +0200

    Add Hook on getNomUrl

--- htdocs/fichinter/class/fichinter.class.php
+++ htdocs/fichinter/class/fichinter.class.php
@@ -696,6 +696,15 @@ class Fichinter extends CommonObject
 			}
 			$linkclose.= ' title="'.dol_escape_htmltag($label, 1).'"';
 			$linkclose.=' class="classfortooltip"';
+
+			if (! is_object($hookmanager)) {
+				include_once DOL_DOCUMENT_ROOT.'/core/class/hookmanager.class.php';
+				$hookmanager=new HookManager($this->db);
+			}
+			$hookmanager->initHooks(array('fichinterdao'));
+			$parameters=array('id'=>$this->id);
+			$reshook=$hookmanager->executeHooks('getnomurltooltip',$parameters,$this,$action);    // Note that $action and $object may have been modified by some hooks
+			if ($reshook > 0) $linkclose = $hookmanager->resPrint;
 		}
 
 		$linkstart = '<a href="'.$url.'"';
