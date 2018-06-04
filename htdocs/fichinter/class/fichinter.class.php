61946446b2401d01bc2cbd9cbc0342a458d5f08b
Laurent Destailleur <eldy@destailleur.fr>
2018-06-04 13:36:38 +0200

    Update fichinter.class.php

--- htdocs/fichinter/class/fichinter.class.php
+++ htdocs/fichinter/class/fichinter.class.php
@@ -668,7 +668,7 @@ class Fichinter extends CommonObject
 	 */
 	function getNomUrl($withpicto=0, $option='', $notooltip=0, $save_lastsearch_value=-1)
 	{
-		global $conf, $langs;
+		global $conf, $langs, $hookmanager;
 
 		$result='';
 
@@ -697,10 +697,6 @@ class Fichinter extends CommonObject
 			$linkclose.= ' title="'.dol_escape_htmltag($label, 1).'"';
 			$linkclose.=' class="classfortooltip"';
 
-			if (! is_object($hookmanager)) {
-				include_once DOL_DOCUMENT_ROOT.'/core/class/hookmanager.class.php';
-				$hookmanager=new HookManager($this->db);
-			}
 			$hookmanager->initHooks(array('fichinterdao'));
 			$parameters=array('id'=>$this->id);
 			$reshook=$hookmanager->executeHooks('getnomurltooltip',$parameters,$this,$action);    // Note that $action and $object may have been modified by some hooks
