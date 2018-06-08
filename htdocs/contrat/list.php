758e5a51a563bfbb516085db3b4a837320dee4f9
Laurent Destailleur <eldy@destailleur.fr>
2018-06-08 19:35:53 +0200

    Fix contextpage was not saved in forms

--- htdocs/contrat/list.php
+++ htdocs/contrat/list.php
@@ -388,6 +388,7 @@ print '<input type="hidden" name="action" value="list">';
 print '<input type="hidden" name="sortfield" value="'.$sortfield.'">';
 print '<input type="hidden" name="sortorder" value="'.$sortorder.'">';
 print '<input type="hidden" name="page" value="'.$page.'">';
+print '<input type="hidden" name="contextpage" value="'.$contextpage.'">';
 
 print_barre_liste($langs->trans("ListOfContracts"), $page, $_SERVER["PHP_SELF"], $param, $sortfield, $sortorder, $massactionbutton, $num, $totalnboflines, 'title_commercial.png', 0, $newcardbutton, '', $limit);
 
