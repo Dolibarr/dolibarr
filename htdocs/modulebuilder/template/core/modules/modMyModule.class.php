98ae8e18df35257321025cc2365e33d652234e3d
Laurent Destailleur <eldy@destailleur.fr>
2018-06-13 21:27:57 +0200

    Update modMyModule.class.php

--- htdocs/modulebuilder/template/core/modules/modMyModule.class.php
+++ htdocs/modulebuilder/template/core/modules/modMyModule.class.php
@@ -315,7 +315,7 @@ class modMyModule extends DolibarrModules
 	public function init($options='')
 	{
 		$result=$this->_load_tables('/mymodule/sql/');
-        if(!$result) return -1;// Fix: Do not activate module if errors on module SQL queries execution
+		if ($result < 0) return -1; // Do not activate module if not allowed errors found on module SQL queries (the _load_table run sql with run_sql with error allowed parameter to 'default')
 
 		// Create extrafields
 		include_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
