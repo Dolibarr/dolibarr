0aa58537bb2bd31684b4cf77acd3fd43dcd18602
Philippe GRAND <philippe.grand@atoo-net.com>
2018-07-02 15:07:00 +0200

    update code comment

--- htdocs/core/modules/facture/doc/pdf_crabe.modules.php
+++ htdocs/core/modules/facture/doc/pdf_crabe.modules.php
@@ -69,24 +69,55 @@ class pdf_crabe extends ModelePDFFactures
 
 	/**
      * @var array() Minimum version of PHP required by module.
-	 * e.g.: PHP ≥ 5.3 = array(5, 3)
+	 * e.g.: PHP ≥ 5.4 = array(5, 4)
      */
-	public $phpmin = array(5, 2);
-
+	public $phpmin = array(5, 4); 
+	
 	/**
      * Dolibarr version of the loaded document
      * @public string
      */
 	public $version = 'dolibarr';
 
+	/**
+     * @var int page_largeur
+     */
     public $page_largeur;
+	
+	/**
+     * @var int page_hauteur
+     */
     public $page_hauteur;
+	
+	/**
+     * @var array format
+     */
     public $format;
+	
+	/**
+     * @var int marge_gauche
+     */
 	public $marge_gauche;
+	
+	/**
+     * @var int marge_droite
+     */
 	public $marge_droite;
+	
+	/**
+     * @var int marge_haute
+     */
 	public $marge_haute;
+	
+	/**
+     * @var int marge_basse
+     */
 	public $marge_basse;
-
+    
+	/**
+	 * Issuer
+	 * @var Societe
+	 */
 	public $emetteur;	// Objet societe qui emet
 
 	/**
