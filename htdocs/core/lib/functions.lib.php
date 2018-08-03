70adfb135dc332c30a225b1412fd1e50f71f2992
ATM-Nicolas <nicolas.prevost@atm-consulting.fr>
2018-08-03 09:51:29 +0200

    FIX : Missing behavior

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -5855,8 +5855,8 @@ function dolGetFirstLastname($firstname,$lastname,$nameorder=-1)
 
 	$ret='';
 	// If order not defined, we use the setup
-	if ($nameorder < 0) $nameorder=$conf->global->MAIN_FIRSTNAME_NAME_POSITION;
-	if (empty($nameorder) && ((string) $nameorder != '2'))
+	if ($nameorder < 0) $nameorder=empty($conf->global->MAIN_FIRSTNAME_NAME_POSITION);
+	if ($nameorder && ((string) $nameorder != '2'))
 	{
 		$ret.=$firstname;
 		if ($firstname && $lastname) $ret.=' ';
