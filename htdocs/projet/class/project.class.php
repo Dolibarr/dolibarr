4e16f64af0a95f0968cbdb37a8f6ec79fc18bdb7
atm-greg <gregory.blemand@atm-consulting.fr>
2018-08-09 12:13:06 +0200

    Fix project::delete to delete children tasks before parent tasks

--- htdocs/projet/class/project.class.php
+++ htdocs/projet/class/project.class.php
@@ -633,9 +633,7 @@ class Project extends CommonObject
 		$this->getLinesArray($user);
 
 		// Delete tasks
-		foreach($this->lines as &$task) {
-			$task->delete($user);
-		}
+		$this->deleteTasks($this->lines);
 
         // Delete project
         if (! $error)
@@ -710,6 +708,25 @@ class Project extends CommonObject
             $this->db->rollback();
             return -1;
         }
+    }
+    
+    /**
+     * 		Reoder tasks to delete children tasks first
+     *  
+     *  	@param     array   $arr       Array of tasks
+     */
+    function deleteTasks($arr)
+    {
+        global $user;
+        
+        $arrParents = array();
+        foreach($arr as $task)
+        {
+            if($task->hasChildren() < 0) $task->delete($user);
+            else $arrParents[] = $task;
+        }
+        
+        if (count($arrParents)) $this->deleteTasks($arrParents);
     }
 
     /**
