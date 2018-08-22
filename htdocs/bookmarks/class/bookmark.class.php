d03900c6f8e00dec8239bf8a1417a12c4ad6a51a
Philippe Grand <philippe.grand@atoo-net.com>
2018-08-22 22:36:07 +0200

    Update bookmark.class.php

--- htdocs/bookmarks/class/bookmark.class.php
+++ htdocs/bookmarks/class/bookmark.class.php
@@ -31,9 +31,9 @@ class Bookmark extends CommonObject
     public $element='bookmark';
     
     /**
-	 * @var string Name of table without prefix where object is stored
-	 */
-	public $table_element='bookmark';
+     * @var string Name of table without prefix where object is stored
+     */
+    public $table_element='bookmark';
 	
     public $ismultientitymanaged = 1;	// 0=No test on entity, 1=Test with field entity, 2=Test with link by societe
     public $picto = 'bookmark';
