8d7588fd07ffd51f4fdae56883140cfb90782d73
Philippe Grand <philippe.grand@atoo-net.com>
2018-08-24 12:32:59 +0200

    Update html.formcron.class.php

--- htdocs/core/class/html.formcron.class.php
+++ htdocs/core/class/html.formcron.class.php
@@ -29,9 +29,9 @@
 class FormCron extends Form
 {
 	/**
-     * @var DoliDb Database handler
-     */
-    public $db;
+     	 * @var DoliDb Database handler
+     	 */
+    	public $db;
     
 	var $error;
 
