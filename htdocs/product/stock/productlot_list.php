e00d6eda7ee4a96c03e342278dc2809552096df5
Laurent Destailleur <eldy@destailleur.fr>
2018-08-28 01:29:33 +0200

    Update productlot_list.php

--- htdocs/product/stock/productlot_list.php
+++ htdocs/product/stock/productlot_list.php
@@ -216,8 +216,9 @@ $reshook=$hookmanager->executeHooks('printFieldListSelect',$parameters);    // N
 $sql.=$hookmanager->resPrint;
 $sql.= " FROM ".MAIN_DB_PREFIX."product_lot as t";
 if (is_array($extrafields->attribute_label) && count($extrafields->attribute_label)) $sql.= " LEFT JOIN ".MAIN_DB_PREFIX."product_lot_extrafields as ef on (t.rowid = ef.fk_object)";
-$sql.= " LEFT JOIN ".MAIN_DB_PREFIX."product as p ON p.rowid = t.fk_product";
-$sql.= " WHERE p.entity IN (".getEntity('product').")";
+$sql.= ", ".MAIN_DB_PREFIX."product as p";
+$sql.= " WHERE p.rowid = t.fk_product";
+$sql.= " AND p.entity IN (".getEntity('product').")";
 
 if ($search_entity) $sql.= natural_search("entity",$search_entity);
 if ($search_product) $sql.= natural_search("p.ref",$search_product);
