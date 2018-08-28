7eef8247198b4a2fb6d4b5f73c0bcb38f609340b
Philippe Grand <philippe.grand@atoo-net.com>
2018-08-28 10:23:53 +0200

    Update ctyperesource.class.php

--- htdocs/core/class/ctyperesource.class.php
+++ htdocs/core/class/ctyperesource.class.php
@@ -50,9 +50,9 @@ class Ctyperesource
 	public $code;
 	
 	/**
-     * @var string proper name for given parameter
-     */
-    public $label;
+     	 * @var string proper name for given parameter
+     	 */
+    	public $label;
     
 	public $active;
 
