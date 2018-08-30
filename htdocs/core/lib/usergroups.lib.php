f8e97b3cb3021630d81a7fbcb06aabd58ea5ae56
gauthier <gauthier.verdol@atm-consulting.fr>
2018-08-30 14:48:06 +0200

    Revert "FIX : wrong number of files"
    This reverts commit df165cee8744e1da5e5906424da01b6b8f16a4be.

--- htdocs/core/lib/usergroups.lib.php
+++ htdocs/core/lib/usergroups.lib.php
@@ -167,7 +167,7 @@ function user_prepare_head($object)
 		// Attached files
 		require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
 		require_once DOL_DOCUMENT_ROOT.'/core/class/link.class.php';
-		$upload_dir = $conf->user->multidir_output[$conf->entity] . "/" . $object->id;
+		$upload_dir = $conf->user->dir_output . "/" . $object->id;
 		$nbFiles = count(dol_dir_list($upload_dir,'files',0,'','(\.meta|_preview.*\.png)$'));
 		$nbLinks=Link::count($db, $object->element, $object->id);
 		$head[$h][0] = DOL_URL_ROOT.'/user/document.php?userid='.$object->id;
