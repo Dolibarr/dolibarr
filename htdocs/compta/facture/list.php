e7d2bcc34c77dbfe5024aa672e067883a31bf6bb
Laurent Destailleur <eldy@destailleur.fr>
2018-09-04 11:17:08 +0200

    FIX #9316 Error when listing invoices

--- htdocs/compta/facture/list.php
+++ htdocs/compta/facture/list.php
@@ -509,7 +509,7 @@ if (! $sall)
 	$sql.= ' typent.code,';
 	$sql.= ' state.code_departement, state.nom,';
 	$sql.= ' country.code,';
-	$sql.= " p.rowid, p.ref";
+	$sql.= " p.rowid, p.ref, p.title";
 
 	foreach ($extrafields->attribute_label as $key => $val) //prevent error with sql_mode=only_full_group_by
 	{
