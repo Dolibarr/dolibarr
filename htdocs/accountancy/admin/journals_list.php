8c5a7857975bd5bd7382fadb835fc84fa69da59a
Frédéric FRANCE <frederic34@users.noreply.github.com>
2018-09-09 13:42:38 +0200

    completely comment unused code

--- htdocs/accountancy/admin/journals_list.php
+++ htdocs/accountancy/admin/journals_list.php
@@ -128,16 +128,16 @@ complete_dictionary_with_modules($taborder,$tabname,$tablib,$tabsql,$tabsqlsort,
 
 // Define elementList and sourceList (used for dictionary type of contacts "llx_c_type_contact")
 $elementList = array();
-	// Must match ids defined into eldy.lib.php
-	$sourceList = array(
-			'1' => $langs->trans('AccountingJournalType1'),
-			'2' => $langs->trans('AccountingJournalType2'),
-			'3' => $langs->trans('AccountingJournalType3'),
-			'4' => $langs->trans('AccountingJournalType4'),
-			'5' => $langs->trans('AccountingJournalType5'),
-			'8' => $langs->trans('AccountingJournalType8'),
-			'9' => $langs->trans('AccountingJournalType9')
-	);
+// Must match ids defined into eldy.lib.php
+$sourceList = array(
+	'1' => $langs->trans('AccountingJournalType1'),
+	'2' => $langs->trans('AccountingJournalType2'),
+	'3' => $langs->trans('AccountingJournalType3'),
+	'4' => $langs->trans('AccountingJournalType4'),
+	'5' => $langs->trans('AccountingJournalType5'),
+	'8' => $langs->trans('AccountingJournalType8'),
+	'9' => $langs->trans('AccountingJournalType9'),
+);
 
 /*
  * Actions
@@ -294,10 +294,10 @@ if (GETPOST('actionadd') || GETPOST('actionmodify'))
 	//$_GET["id"]=GETPOST('id', 'int');       // Force affichage dictionnaire en cours d'edition
 }
 
-if (GETPOST('actioncancel'))
-{
-	//$_GET["id"]=GETPOST('id', 'int');       // Force affichage dictionnaire en cours d'edition
-}
+//if (GETPOST('actioncancel'))
+//{
+//	$_GET["id"]=GETPOST('id', 'int');       // Force affichage dictionnaire en cours d'edition
+//}
 
 if ($action == 'confirm_delete' && $confirm == 'yes')       // delete
 {
