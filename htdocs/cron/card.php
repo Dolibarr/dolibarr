79b248fc67d7c33de207681da928b2586bdaf2f9
Laurent Destailleur <eldy@destailleur.fr>
2018-09-18 23:25:38 +0200

    FIX warning picto when next run date passed

--- htdocs/cron/card.php
+++ htdocs/cron/card.php
@@ -684,8 +684,8 @@ else
 	elseif (!empty($object->datenextrun)) { print img_picto('','object_calendarday').' '.dol_print_date($object->datenextrun,'dayhoursec');}
 	else {print $langs->trans('CronNone');}
 	//print '</strong>';
-	if ($object->maxnbrun && $object->nbrun >= $object->maxrun) print img_warning($langs->trans("Finished"));
-	if ($object->datenextrun && $object->datenextrun < $now) print img_warning($langs->trans("Late"));
+	if ($object->maxrun && $object->nbrun >= $object->maxrun) print img_warning($langs->trans("MaxRunReached"));
+	elseif ($object->datenextrun && $object->datenextrun < $now) print img_warning($langs->trans("Late"));
 	print "</td></tr>";
 
 	print '</table>';
