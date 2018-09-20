08325d40ce3cfa5db4d7f845c96d0e80f0a94496
Philippe GRAND <philippe.grand@atoo-net.com>
2018-09-20 11:18:02 +0200

    Standardize and update code

--- htdocs/core/modules/user/doc/doc_generic_user_odt.modules.php
+++ htdocs/core/modules/user/doc/doc_generic_user_odt.modules.php
@@ -41,7 +41,11 @@ class doc_generic_user_odt extends ModelePDFUser
 	 */
 	public $emetteur;
 
-	public $phpmin = array(5,4,0);	// Minimum version of PHP required by module
+	/**
+     * @var array() Minimum version of PHP required by module.
+	 * e.g.: PHP ≥ 5.4 = array(5, 4)
+     */
+	public $phpmin = array(5, 4);
 
 	/**
      * Dolibarr version of the loaded document
