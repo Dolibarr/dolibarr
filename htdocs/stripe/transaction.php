933f253c86bb9397109a6c3bfa4de28f560b6cb2
ptibogxiv <support@ptibogxiv.net>
2018-09-21 09:52:27 +0200

    Update transaction.php

--- htdocs/stripe/transaction.php
+++ htdocs/stripe/transaction.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2018       Thibault FOUCART        <support@ptibogxiv.net> 
+/* Copyright (C) 2018       Thibault FOUCART        <support@ptibogxiv.net>
  * Copyright (C) 2018       Frédéric France         <frederic.france@netlogic.fr>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -227,7 +227,7 @@ elseif ($txn->status=='failed')
 	print "</table>";
 	print '</div>';
 	print '</form>';
-} else {}
+}
 
 llxFooter();
 $db->close();
