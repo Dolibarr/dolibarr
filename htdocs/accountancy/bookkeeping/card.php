d6a9da57e11d64ecb68f21437c50fd2e0c0e7d44
gauthier <gauthier.verdol@atm-consulting.fr>
2018-10-01 14:11:19 +0200

    FIX : need to round with 2 decimals to avoid movements not correctly balanced

--- htdocs/accountancy/bookkeeping/card.php
+++ htdocs/accountancy/bookkeeping/card.php
@@ -651,8 +651,8 @@ if ($action == 'create')
 					print "</tr>\n";
 				}
 
-				$total_debit = price2num($total_debit);
-				$total_credit = price2num($total_credit);
+				$total_debit = price2num($total_debit, 2);
+				$total_credit = price2num($total_credit, 2);
 
 				if ($total_debit != $total_credit)
 				{
