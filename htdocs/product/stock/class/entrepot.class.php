d2ec703b5117b3913aa354e9db561c3053802935
Philippe GRAND <philippe.grand@atoo-net.com>
2018-10-04 09:24:57 +0200

    Standardize and update code

--- htdocs/product/stock/class/entrepot.class.php
+++ htdocs/product/stock/class/entrepot.class.php
@@ -60,23 +60,23 @@ class Entrepot extends CommonObject
 	 */
 	const STATUS_OPEN_INTERNAL = 2;
 
-	var $libelle;
+	public $libelle;
 
 	/**
 	 * @var string description
 	 */
 	public $description;
 
-	var $statut;
-	var $lieu;
-	var $address;
+	public $statut;
+	public $lieu;
+	public $address;
 	//! Code Postal
-	var $zip;
-	var $town;
-	var $fk_parent;
+	public $zip;
+	public $town;
+	public $fk_parent;
 
 	// List of short language codes for status
-	var $statuts = array();
+	public $statuts = array();
 
 	/**
 	 *  Constructor
