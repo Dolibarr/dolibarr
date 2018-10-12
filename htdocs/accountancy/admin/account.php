9d4f6dc44465b6ed0368dee271be2c97222affce
Philippe GRAND <philippe.grand@atoo-net.com>
2018-10-12 15:55:36 +0200

    Standardize and update code

--- htdocs/accountancy/admin/account.php
+++ htdocs/accountancy/admin/account.php
@@ -38,11 +38,11 @@ $id = GETPOST('id', 'int');
 $rowid = GETPOST('rowid', 'int');
 $contextpage=GETPOST('contextpage','aZ')?GETPOST('contextpage','aZ'):'accountingaccountlist';   // To manage different context of search
 
-$search_account = GETPOST("search_account");
-$search_label = GETPOST("search_label");
-$search_accountparent = GETPOST("search_accountparent");
-$search_pcgtype = GETPOST("search_pcgtype");
-$search_pcgsubtype = GETPOST("search_pcgsubtype");
+$search_account = GETPOST('search_account','alpha');
+$search_label = GETPOST('search_label','alpha');
+$search_accountparent = GETPOST('search_accountparent','alpha');
+$search_pcgtype = GETPOST('search_pcgtype','alpha');
+$search_pcgsubtype = GETPOST('search_pcgsubtype','alpha');
 
 // Security check
 if ($user->societe_id > 0) accessforbidden();
