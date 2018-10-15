6d4d8a0342492441ccd062b2546cad2a5802e84c
Laurent Destailleur <eldy@destailleur.fr>
2018-10-15 14:04:42 +0200

    Dolibarize data privacy policy module

--- htdocs/datapolicy/class/datapolicy.class.php
+++ htdocs/datapolicy/class/datapolicy.class.php
@@ -16,9 +16,9 @@
  */
 
 /**
- * \file    datapolicies/class/datapolicies.class.php
- * \ingroup datapolicies
- * \brief   Class to manage feature of Data Policies module.
+ * \file    datapolicy/class/datapolicy.class.php
+ * \ingroup datapolicy
+ * \brief   Class to manage feature of Data Policy module.
  */
 include_once DOL_DOCUMENT_ROOT . '/contact/class/contact.class.php';
 include_once DOL_DOCUMENT_ROOT . '/societe/class/societe.class.php';
@@ -26,9 +26,9 @@ include_once DOL_DOCUMENT_ROOT . '/adherents/class/adherent.class.php';
 
 
 /**
- * Class DataPolicies
+ * Class DataPolicy
  */
-Class DataPolicies extends Contact
+Class DataPolicy extends Contact
 {
 	/**
 	 * getAllContactNotInformed
@@ -45,8 +45,8 @@ Class DataPolicies extends Contact
         $sql .= " FROM " . MAIN_DB_PREFIX . "socpeople as c";
         $sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "societe as s ON c.fk_soc = s.rowid";
         $sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "socpeople_extrafields as spe ON spe.fk_object = c.rowid";
-        $sql .= " WHERE (c.statut=1 AND c.no_email=0 AND (spe.datapolicies_consentement=0 OR spe.datapolicies_consentement IS NULL) AND (spe.datapolicies_opposition_traitement=0 OR spe.datapolicies_opposition_traitement IS NULL) AND (spe.datapolicies_opposition_prospection=0 OR spe.datapolicies_opposition_prospection IS NULL))";
-        $sql .= " AND spe.datapolicies_send IS NULL";
+        $sql .= " WHERE (c.statut=1 AND c.no_email=0 AND (spe.datapolicy_consentement=0 OR spe.datapolicy_consentement IS NULL) AND (spe.datapolicy_opposition_traitement=0 OR spe.datapolicy_opposition_traitement IS NULL) AND (spe.datapolicy_opposition_prospection=0 OR spe.datapolicy_opposition_prospection IS NULL))";
+        $sql .= " AND spe.datapolicy_send IS NULL";
         $sql .= " AND c.entity=" . $conf->entity;
         $resql = $this->db->query($sql);
         if ($resql) {
@@ -57,7 +57,7 @@ Class DataPolicies extends Contact
                 $contact = new Contact($db);
                 $contact->fetch($obj->rowid);
 
-                DataPolicies::sendMailDataPoliciesContact($contact);
+                DataPolicy::sendMailDataPolicyContact($contact);
                 $i++;
             }
         } else {
@@ -80,8 +80,8 @@ Class DataPolicies extends Contact
         $sql = "SELECT s.rowid";
         $sql .= " FROM " . MAIN_DB_PREFIX . "societe as s";
         $sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "societe_extrafields as se ON se.fk_object = s.rowid";
-        $sql .= " WHERE s.statut=0 AND (se.datapolicies_consentement=0 OR se.datapolicies_consentement IS NULL) AND (se.datapolicies_opposition_traitement=0 OR se.datapolicies_opposition_traitement IS NULL) AND (se.datapolicies_opposition_prospection=0 OR se.datapolicies_opposition_prospection IS NULL)";
-        $sql .= " AND se.datapolicies_send IS NULL";
+        $sql .= " WHERE s.statut=0 AND (se.datapolicy_consentement=0 OR se.datapolicy_consentement IS NULL) AND (se.datapolicy_opposition_traitement=0 OR se.datapolicy_opposition_traitement IS NULL) AND (se.datapolicy_opposition_prospection=0 OR se.datapolicy_opposition_prospection IS NULL)";
+        $sql .= " AND se.datapolicy_send IS NULL";
         $sql .= " AND s.entity=" . $conf->entity;
         $resql = $this->db->query($sql);
         if ($resql) {
@@ -92,7 +92,7 @@ Class DataPolicies extends Contact
                 $societe = new Societe($db);
                 $societe->fetch($obj->rowid);
 
-                DataPolicies::sendMailDataPoliciesCompany($societe);
+                DataPolicy::sendMailDataPolicyCompany($societe);
                 $i++;
             }
         } else {
@@ -115,8 +115,8 @@ Class DataPolicies extends Contact
         $sql = "SELECT a.rowid";
         $sql .= " FROM " . MAIN_DB_PREFIX . "adherent as a";
         $sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "adherent_extrafields as ae ON ae.fk_object = a.rowid";
-        $sql .= " WHERE a.statut=0 AND (ae.datapolicies_consentement=0 OR ae.datapolicies_consentement IS NULL) AND (ae.datapolicies_opposition_traitement=0 OR ae.datapolicies_opposition_traitement IS NULL) AND (ae.datapolicies_opposition_prospection=0 OR ae.datapolicies_opposition_prospection IS NULL)";
-        $sql .= " AND ae.datapolicies_send IS NULL";
+        $sql .= " WHERE a.statut=0 AND (ae.datapolicy_consentement=0 OR ae.datapolicy_consentement IS NULL) AND (ae.datapolicy_opposition_traitement=0 OR ae.datapolicy_opposition_traitement IS NULL) AND (ae.datapolicy_opposition_prospection=0 OR ae.datapolicy_opposition_prospection IS NULL)";
+        $sql .= " AND ae.datapolicy_send IS NULL";
         $sql .= " AND a.entity=" . $conf->entity;
         $resql = $this->db->query($sql);
         if ($resql) {
@@ -127,7 +127,7 @@ Class DataPolicies extends Contact
                 $adherent = new Adherent($db);
                 $adherent->fetch($obj->rowid);
 
-                DataPolicies::sendMailDataPoliciesAdherent($adherent);
+                DataPolicy::sendMailDataPolicyAdherent($adherent);
                 $i++;
             }
         } else {
@@ -137,12 +137,12 @@ Class DataPolicies extends Contact
     }
 
     /**
-     * sendMailDataPoliciesContact
+     * sendMailDataPolicyContact
      *
      * @param 	mixed		$contact		Contact
      * @return	void
      */
-    function sendMailDataPoliciesContact($contact)
+    function sendMailDataPolicyContact($contact)
     {
      	global $langs, $conf, $db, $user;
 
@@ -171,8 +171,8 @@ Class DataPolicies extends Contact
      	$deliveryreceipt = 0;
 
      	$substitutionarray = array(
-     	'__LINKACCEPT__' => '<a href="'.dol_buildpath('/datapolicies/public/index.php?action=1&c='.$contact->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linka.'</a>',
-     	'__LINKREFUSED__' => '<a href="'.dol_buildpath('/datapolicies/public/index.php?action=2&c='.$contact->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linkr.'</a>',
+     	'__LINKACCEPT__' => '<a href="'.dol_buildpath('/datapolicy/public/index.php?action=1&c='.$contact->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linka.'</a>',
+     	'__LINKREFUSED__' => '<a href="'.dol_buildpath('/datapolicy/public/index.php?action=2&c='.$contact->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linkr.'</a>',
      	'__FIRSTNAME__' => $contact->firstname,
      	'__NAME__' => $contact->lastname,
      	'__CIVILITY__' => $contact->civility,
@@ -202,7 +202,7 @@ Class DataPolicies extends Contact
      		if (!$error) {
 
      			$resultmasssend .= $langs->trans("MailSent") . ': ' . $sendto . "<br>";
-     			$contact->array_options['options_datapolicies_send'] = date('Y-m-d', time());
+     			$contact->array_options['options_datapolicy_send'] = date('Y-m-d', time());
      			$contact->update($contact->id);
 
      		} else {
@@ -213,12 +213,12 @@ Class DataPolicies extends Contact
     }
 
     /**
-     * sendMailDataPoliciesCompany
+     * sendMailDataPolicyCompany
      *
      * @param Societe	$societe	Object societe
      * @return	void
      */
-    function sendMailDataPoliciesCompany($societe)
+    function sendMailDataPolicyCompany($societe)
     {
      	global $langs, $conf, $db, $user;
 
@@ -248,8 +248,8 @@ Class DataPolicies extends Contact
      	$deliveryreceipt = 0;
 
      	$substitutionarray = array(
-            '__LINKACCEPT__' => '<a href="'.dol_buildpath('/datapolicies/public/index.php?action=1&s='.$societe->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linka.'</a>',
-            '__LINKREFUSED__' => '<a href="'.dol_buildpath('/datapolicies/public/index.php?action=2&s='.$societe->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linkr.'</a>',
+            '__LINKACCEPT__' => '<a href="'.dol_buildpath('/datapolicy/public/index.php?action=1&s='.$societe->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linka.'</a>',
+            '__LINKREFUSED__' => '<a href="'.dol_buildpath('/datapolicy/public/index.php?action=2&s='.$societe->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linkr.'</a>',
      	);
      	$subject = make_substitutions($subject, $substitutionarray);
      	$message = make_substitutions($message, $substitutionarray);
@@ -275,7 +275,7 @@ Class DataPolicies extends Contact
 
      		if (!$error) {
      			$resultmasssend .= $langs->trans("MailSent") . ': ' . $sendto . "<br>";
-     			$societe->array_options['options_datapolicies_send'] = date('Y-m-d', time());
+     			$societe->array_options['options_datapolicy_send'] = date('Y-m-d', time());
      			$societe->update($societe->id);
      		} else {
      			dol_print_error($db);
@@ -285,12 +285,12 @@ Class DataPolicies extends Contact
     }
 
     /**
-     * sendMailDataPoliciesAdherent
+     * sendMailDataPolicyAdherent
      *
      * @param Adherent	$adherent		Member
      * @return void
      */
-    function sendMailDataPoliciesAdherent($adherent)
+    function sendMailDataPolicyAdherent($adherent)
     {
     	global $langs, $conf, $db, $user;
 
@@ -318,8 +318,8 @@ Class DataPolicies extends Contact
     	$deliveryreceipt = 0;
 
     	$substitutionarray = array(
-            '__LINKACCEPT__' => '<a href="'.dol_buildpath('/datapolicies/public/index.php?action=1&a='.$adherent->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linka.'</a>',
-            '__LINKREFUSED__' => '<a href="'.dol_buildpath('/datapolicies/public/index.php?action=2&a='.$adherent->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linkr.'</a>',
+            '__LINKACCEPT__' => '<a href="'.dol_buildpath('/datapolicy/public/index.php?action=1&a='.$adherent->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linka.'</a>',
+            '__LINKREFUSED__' => '<a href="'.dol_buildpath('/datapolicy/public/index.php?action=2&a='.$adherent->id.'&l='.$l.'&key='.$code,3).'" target="_blank">'.$linkr.'</a>',
     	);
     	$subject = make_substitutions($subject, $substitutionarray);
     	$message = make_substitutions($message, $substitutionarray);
@@ -346,7 +346,7 @@ Class DataPolicies extends Contact
 
     		if (!$error) {
     			$resultmasssend .= $langs->trans("MailSent") . ': ' . $sendto . "<br>";
-    			$adherent->array_options['options_datapolicies_send'] = date('Y-m-d', time());
+    			$adherent->array_options['options_datapolicy_send'] = date('Y-m-d', time());
     			$adherent->update($user);
 
     		} else {
