69c98d4b94f92220b077d3499d422186562df385
John BOTELLA <john.botella@atm-consulting.fr>
2018-10-29 17:29:32 +0100

    FIX fourn order clone

--- htdocs/fourn/class/fournisseur.commande.class.php
+++ htdocs/fourn/class/fournisseur.commande.class.php
@@ -1255,8 +1255,14 @@ class CommandeFournisseur extends CommonOrder
 	                    $this->lines[$i]->date_start,
                         $this->lines[$i]->date_end,
                         0,
-                        $this->lines[$i]->fk_unit
+	                    $this->lines[$i]->fk_unit,
+	                    0,  // pu_ht_devise
+	                    '',  // origin
+	                    0, //origin_id
+	                    $this->lines[$i]->special_code
 	                );
+	                
+	                
 	                if ($result < 0)
 	                {
 	                    dol_syslog(get_class($this)."::create ".$this->error, LOG_WARNING);	// do not use dol_print_error here as it may be a functionnal error
@@ -1439,13 +1445,13 @@ class CommandeFournisseur extends CommonOrder
 	 *  @param		int		$origin_id				Id of origin object
      *	@return     int             				<=0 if KO, >0 if OK
      */
-	public function addline($desc, $pu_ht, $qty, $txtva, $txlocaltax1=0.0, $txlocaltax2=0.0, $fk_product=0, $fk_prod_fourn_price=0, $ref_supplier='', $remise_percent=0.0, $price_base_type='HT', $pu_ttc=0.0, $type=0, $info_bits=0, $notrigger=false, $date_start=null, $date_end=null, $array_options=0, $fk_unit=null, $pu_ht_devise=0, $origin='', $origin_id=0)
+    public function addline($desc, $pu_ht, $qty, $txtva, $txlocaltax1=0.0, $txlocaltax2=0.0, $fk_product=0, $fk_prod_fourn_price=0, $ref_supplier='', $remise_percent=0.0, $price_base_type='HT', $pu_ttc=0.0, $type=0, $info_bits=0, $notrigger=false, $date_start=null, $date_end=null, $array_options=0, $fk_unit=null, $pu_ht_devise=0, $origin='', $origin_id=0, $special_code=0)
     {
         global $langs,$mysoc,$conf;
 
         $error = 0;
 
-        dol_syslog(get_class($this)."::addline $desc, $pu_ht, $qty, $txtva, $txlocaltax1, $txlocaltax2, $fk_product, $fk_prod_fourn_price, $ref_supplier, $remise_percent, $price_base_type, $pu_ttc, $type, $info_bits, $notrigger, $date_start, $date_end, $fk_unit, $pu_ht_devise, $origin, $origin_id");
+        dol_syslog(get_class($this)."::addline $desc, $pu_ht, $qty, $txtva, $txlocaltax1, $txlocaltax2, $fk_product, $fk_prod_fourn_price, $ref_supplier, $remise_percent, $price_base_type, $pu_ttc, $type, $info_bits, $notrigger, $date_start, $date_end, $fk_unit, $pu_ht_devise, $origin, $origin_id, $special_code");
         include_once DOL_DOCUMENT_ROOT.'/core/lib/price.lib.php';
 
         // Clean parameters
@@ -1621,7 +1627,7 @@ class CommandeFournisseur extends CommonOrder
             $this->line->total_localtax2=$total_localtax2;
             $this->line->total_ttc=$total_ttc;
             $this->line->product_type=$type;
-            $this->line->special_code=$this->special_code;
+            $this->line->special_code=$special_code;
             $this->line->origin=$origin;
             $this->line->origin_id=$origin_id;
             $this->line->fk_unit=$fk_unit;
