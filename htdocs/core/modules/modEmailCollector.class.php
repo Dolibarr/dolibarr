90729c03c4b21d0f9719a681355f9253ceea7e2a
Laurent Destailleur <eldy@destailleur.fr>
2018-11-05 10:28:44 +0100

    Fix dependencies of new modules

--- htdocs/core/modules/modEmailCollector.class.php
+++ htdocs/core/modules/modEmailCollector.class.php
@@ -87,7 +87,7 @@ class modEmailCollector extends DolibarrModules
 
 		// Dependencies
 		$this->hidden = false;			// A condition to hide module
-		$this->depends = array();		// List of module class names as string that must be enabled if this module is enabled
+		$this->depends = array('always'=>'modCron');		// List of module class names as string that must be enabled if this module is enabled
 		$this->requiredby = array();	// List of module ids to disable if this one is disabled
 		$this->conflictwith = array();	// List of module class names as string this module is in conflict with
 		$this->langfiles = array("admin");
