8893d8d59fcf08637e9465d5f3b0a4c3a0c6590d
Laurent Destailleur <eldy@destailleur.fr>
2018-11-10 11:56:23 +0100

    Fix compatiblity with new version of PHP

--- htdocs/exports/class/export.class.php
+++ htdocs/exports/class/export.class.php
@@ -131,11 +131,11 @@ class Export
     									//print_r("$perm[0]-$perm[1]-$perm[2]<br>");
     									if (! empty($perm[2]))
     									{
-    										$bool=$user->rights->$perm[0]->$perm[1]->$perm[2];
+    										$bool=$user->rights->{$perm[0]}->{$perm[1]}->{$perm[2]};
     									}
     									else
     									{
-    										$bool=$user->rights->$perm[0]->$perm[1];
+    										$bool=$user->rights->{$perm[0]}->{$perm[1]};
     									}
     									if ($perm[0]=='user' && $user->admin) $bool=true;
     									if (! $bool) break;
