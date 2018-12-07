c5e773fcfd5218eb19f1bc38a34cd454201d1139
aurelien goudeneche <44634756+AurelGDN@users.noreply.github.com>
2018-12-07 17:20:05 +0100

    space cast statement fix

--- htdocs/takepos/invoice.php
+++ htdocs/takepos/invoice.php
@@ -115,7 +115,7 @@ if (($action == "addline" || $action == "freezone") and $placeid == 0) {
         $invoice->date = dol_now();
         $invoice->ref = "(PROV-POS)";
         $invoice->module_source = 'takepos';
-        $invoice->pos_source = (string)(empty($place) ? '0' : $place);
+        $invoice->pos_source = (string) (empty($place)?'0':$place);
         $placeid = $invoice->create($user);
         $sql = "UPDATE " . MAIN_DB_PREFIX . "facture set facnumber='(PROV-POS-" . $place . ")' where rowid=" . $placeid;
         $db->query($sql);
