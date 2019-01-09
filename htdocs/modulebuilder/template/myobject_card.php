7262f90519b7b77c8578c802307f56ad96430790
Abbes Bahfir <bafbes@gmail.com>
2019-01-09 18:12:06 +0100

    linl back to list if cancelled on create

--- htdocs/modulebuilder/template/myobject_card.php
+++ htdocs/modulebuilder/template/myobject_card.php
@@ -118,8 +118,11 @@ if (empty($reshook))
 	$error=0;
 
 	$permissiontoadd = $user->rights->mymodule->write;
-	$permissiontodelete = $user->rights->mymodule->delete || ($permissiontoadd && $object->status == 0);
-	if (empty($backtopage)) $backtopage = dol_buildpath('/mymodule/myobject_card.php',1).'?id='.($id > 0 ? $id : '__ID__');
+	$permissiontodelete = $user->rights->mymodule->delete;
+	if (empty($backtopage)) {
+	    if (empty($id)) $backtopage = dol_buildpath('/mymodule/myobject_list.php',1);
+	    else $backtopage = dol_buildpath('/mymodule/myobject_card.php',1).($id > 0 ? $id : '__ID__');
+        }
 	$backurlforlist = dol_buildpath('/mymodule/myobject_list.php',1);
 	$triggermodname = 'MYMODULE_MYOBJECT_MODIFY';	// Name of trigger action code to execute when we modify record
 
