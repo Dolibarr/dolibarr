4d2231a6a2fb6f1c52f520181e0b3e2d9d3a1dfe
ptibogxiv <support@ptibogxiv.net>
2019-01-21 23:53:17 +0100

    Fix double <

--- htdocs/don/class/don.class.php
+++ htdocs/don/class/don.class.php
@@ -1,4 +1,4 @@
-<<?php
+<?php
 /* Copyright (C) 2002      Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (C) 2004-2008 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2009      Regis Houssin        <regis.houssin@inodbox.com>
