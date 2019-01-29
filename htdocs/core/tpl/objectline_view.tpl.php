508fb5c9cb82234a6523a35560d3c10cce473091
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2019-01-29 11:13:11 +0100

    Prise en charge des Travaux supplémentaires
    Corrige la filtration lors des situations client afin de prendre en compte la possibilité d'ajouter des travaux en cours de chantier.

--- htdocs/core/tpl/objectline_view.tpl.php
+++ htdocs/core/tpl/objectline_view.tpl.php
@@ -283,7 +283,7 @@ $domData .= ' data-product_type="'.$line->product_type.'"';
 
 	<td class="linecoldelete" align="center"><?php $coldisplay++; ?>
 		<?php
-		if (($this->situation_counter == 1 || !$this->situation_cycle_ref) && empty($disableremove)) {
+		if (($line->fk_prev_id == NULL ) && empty($disableremove)) { //La suppression n'est autorisée que si il n'y a pas de ligne dans une précédente situation
 			print '<a href="' . $_SERVER["PHP_SELF"] . '?id=' . $this->id . '&amp;action=ask_deleteline&amp;lineid=' . $line->id . '">';
 			print img_delete();
 			print '</a>';
