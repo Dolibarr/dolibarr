394659f84a30fa3ebc08387898a2eff4320f4584
atm-quentin <quentin.vial-gouteyron@atm-consulting.fr>
2019-02-20 17:00:07 +0100

    missing doc

--- htdocs/fourn/class/fournisseur.product.class.php
+++ htdocs/fourn/class/fournisseur.product.class.php
@@ -227,6 +227,8 @@ class ProductFournisseur extends Product
      *    @param  	float		$multicurrency_tx	            Rate currency
      *    @param  	string		$multicurrency_code	            Currency code
      *    @param  	string		$desc_fourn     	            Custom description for product_fourn_price
+     *    @param  	string		$barcode     	                Barcode
+     *    @param  	int		    $fk_barcode_type     	        Barcode type
      *    @return	int								<0 if KO, >=0 if OK
      */
     function update_buyprice($qty, $buyprice, $user, $price_base_type, $fourn, $availability, $ref_fourn, $tva_tx, $charges=0, $remise_percent=0, $remise=0, $newnpr=0, $delivery_time_days=0, $supplier_reputation='', $localtaxes_array=array(), $newdefaultvatcode='', $multicurrency_buyprice=0, $multicurrency_price_base_type='HT',$multicurrency_tx=1,$multicurrency_code='', $desc_fourn='', $barcode='', $fk_barcode_type='')
