e42487ae4ef6f4c82f115ae4f357eed68e818f1f
Laurent Destailleur <eldy@destailleur.fr>
2019-02-20 05:26:27 +0100

    CSS

--- htdocs/theme/eldy/style.css.php
+++ htdocs/theme/eldy/style.css.php
@@ -3371,7 +3371,7 @@ ul.noborder li:nth-child(even):not(.liste_titre) {
 	box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.20);
 }
 span.boxstatstext {
-	opacity: 0.8;
+	opacity: 0.7;
     line-height: 18px;
     color: #000;
 }
