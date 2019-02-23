0ca5e6e275eb420a480bbb182b98bae50d90f64c
Frédéric FRANCE <frederic.france@free.fr>
2019-02-23 21:47:04 +0100

    replace html code by html5 in reception

--- htdocs/reception/stats/index.php
+++ htdocs/reception/stats/index.php
@@ -255,7 +255,7 @@ print '<div class="fichecenter"><div class="fichethirdleft">';
 	arsort($arrayyears);
 	print $form->selectarray('year', $arrayyears, $year, 0);
 	print '</td></tr>';
-	print '<tr><td align="center" colspan="2"><input type="submit" name="submit" class="button" value="'.$langs->trans("Refresh").'"></td></tr>';
+	print '<tr><td class="center" colspan="2"><input type="submit" name="submit" class="button" value="'.$langs->trans("Refresh").'"></td></tr>';
 	print '</table>';
 	print '</form>';
 	print '<br><br>';
@@ -263,10 +263,10 @@ print '<div class="fichecenter"><div class="fichethirdleft">';
 
 print '<table class="noborder" width="100%">';
 print '<tr class="liste_titre" height="24">';
-print '<td align="center">'.$langs->trans("Year").'</td>';
-print '<td align="right">'.$langs->trans("NbOfReceptions").'</td>';
-/*print '<td align="center">'.$langs->trans("AmountTotal").'</td>';
-print '<td align="center">'.$langs->trans("AmountAverage").'</td>';*/
+print '<td class="center">'.$langs->trans("Year").'</td>';
+print '<td class="right">'.$langs->trans("NbOfReceptions").'</td>';
+/*print '<td class="center">'.$langs->trans("AmountTotal").'</td>';
+print '<td class="center">'.$langs->trans("AmountAverage").'</td>';*/
 print '</tr>';
 
 $oldyear=0;
@@ -279,22 +279,22 @@ foreach ($data as $val)
 
 
 		print '<tr class="oddeven" height="24">';
-		print '<td align="center"><a href="'.$_SERVER["PHP_SELF"].'?year='.$oldyear.'&amp;mode='.$mode.'">'.$oldyear.'</a></td>';
+		print '<td class="center"><a href="'.$_SERVER["PHP_SELF"].'?year='.$oldyear.'&amp;mode='.$mode.'">'.$oldyear.'</a></td>';
 
-		print '<td align="right">0</td>';
-		/*print '<td align="right">0</td>';
-		print '<td align="right">0</td>';*/
+		print '<td class="right">0</td>';
+		/*print '<td class="right">0</td>';
+		print '<td class="right">0</td>';*/
 		print '</tr>';
 	}
 
 	print '<tr class="oddeven" height="24">';
-	print '<td align="center">';
+	print '<td class="center">';
 	if ($year) print '<a href="'.$_SERVER["PHP_SELF"].'?year='.$year.'&amp;mode='.$mode.'">'.$year.'</a>';
 	else print $langs->trans("ValidationDateNotDefinedEvenIfReceptionValidated");
 	print '</td>';
-	print '<td align="right">'.$val['nb'].'</td>';
-	/*print '<td align="right">'.price(price2num($val['total'],'MT'),1).'</td>';
-	print '<td align="right">'.price(price2num($val['avg'],'MT'),1).'</td>';*/
+	print '<td class="right">'.$val['nb'].'</td>';
+	/*print '<td class="right">'.price(price2num($val['total'],'MT'),1).'</td>';
+	print '<td class="right">'.price(price2num($val['avg'],'MT'),1).'</td>';*/
 	print '</tr>';
 	$oldyear=$year;
 }
@@ -306,7 +306,7 @@ print '</div><div class="fichetwothirdright"><div class="ficheaddleft">';
 
 
 // Show graphs
-print '<table class="border" width="100%"><tr valign="top"><td align="center">';
+print '<table class="border" width="100%"><tr valign="top"><td class="center">';
 if ($mesg) { print $mesg; }
 else {
     print $px1->show();
@@ -328,8 +328,8 @@ dol_fiche_end();
 // TODO USe code similar to commande/stats/index.php instead of this one.
 /*
 print '<table class="border" width="100%">';
-print '<tr><td align="center">'.$langs->trans("Year").'</td>';
-print '<td width="40%" align="center">'.$langs->trans("NbOfReceptions").'</td></tr>';
+print '<tr><td class="center">'.$langs->trans("Year").'</td>';
+print '<td width="40%" class="center">'.$langs->trans("NbOfReceptions").'</td></tr>';
 
 $sql = "SELECT count(*) as nb, date_format(date_reception,'%Y') as dm";
 $sql.= " FROM ".MAIN_DB_PREFIX."reception";
@@ -348,7 +348,7 @@ if ($resql)
         $nbproduct = $row[0];
         $year = $row[1];
         print "<tr>";
-        print '<td align="center"><a href="month.php?year='.$year.'">'.$year.'</a></td><td align="center">'.$nbproduct.'</td></tr>';
+        print '<td class="center"><a href="month.php?year='.$year.'">'.$year.'</a></td><td class="center">'.$nbproduct.'</td></tr>';
         $i++;
     }
 }
