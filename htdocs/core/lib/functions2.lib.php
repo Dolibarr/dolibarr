efd27ec1d8e3efe476e10cd096edfcd2334bc928
ptibogxiv <support@ptibogxiv.net>
2019-02-24 16:11:52 +0100

    Fix declare functions2.lib.php for donations API

--- htdocs/core/lib/functions2.lib.php
+++ htdocs/core/lib/functions2.lib.php
@@ -2260,6 +2260,9 @@ function getModuleDirForApiClass($module)
     elseif ($module == 'adherent' || $module == 'members' || $module == 'memberstypes' || $module == 'subscriptions') {
         $moduledirforclass = 'adherents';
     }
+    elseif ($module == 'don' || $module == 'donations') {
+        $moduledirforclass = 'don';
+    }
     elseif ($module == 'banque' || $module == 'bankaccounts') {
         $moduledirforclass = 'compta/bank';
     }
