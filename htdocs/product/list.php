3b488089162ed25a780d0931c9b22320b5e15ce3
Laurent Destailleur <eldy@destailleur.fr>
2019-03-04 16:13:38 +0100

    Update list.php

--- htdocs/product/list.php
+++ htdocs/product/list.php
@@ -797,7 +797,7 @@ if ($resql)
 		// Label
 		if (! empty($arrayfields['p.label']['checked']))
 		{
-			print '<td class="tdoverflowmax220">'.dol_trunc($obj->label, 70).'</td>';
+			print '<td class="tdoverflowmax200">'.dol_trunc($obj->label, 70).'</td>';
 			if (! $i) $totalarray['nbfield']++;
 		}
 
