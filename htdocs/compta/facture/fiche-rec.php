1686b7b1dbc7b1ef06e2824d18b4efb82a7a5a50
Laurent Destailleur <eldy@destailleur.fr>
2019-03-04 14:41:20 +0100

    FIX label of bank account

--- htdocs/compta/facture/fiche-rec.php
+++ htdocs/compta/facture/fiche-rec.php
@@ -1390,7 +1390,7 @@ else
 
 		print '<tr><td class="nowrap">';
 		print '<table width="100%" class="nobordernopadding"><tr><td class="nowrap">';
-		print $langs->trans('RIB');
+		print $langs->trans('BankAccount');
 		print '<td>';
 		if (($action != 'editbankaccount') && $user->rights->facture->creer && ! empty($object->brouillon))
 			print '<td align="right"><a href="'.$_SERVER["PHP_SELF"].'?action=editbankaccount&amp;id='.$object->id.'">'.img_edit($langs->trans('SetBankAccount'),1).'</a></td>';
