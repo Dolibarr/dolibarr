cdf6e4b65a70d1eca19b2f7295b594ecf2a8efca
Laurent Destailleur <eldy@destailleur.fr>
2019-03-14 16:43:19 +0100

    Fix some PSR rules

--- htdocs/compta/resultat/result.php
+++ htdocs/compta/resultat/result.php
@@ -288,8 +288,8 @@ elseif ($modecompta=="BOOKKEEPING")
 	$j=1;
 	$sommes = array();
 	$totPerAccount = array();
-	if (!is_array($cats) && $cats<0) {
-		setEventMessages(null,$AccCat->errors,'errors');
+	if (!is_array($cats) && $cats < 0) {
+		setEventMessages(null, $AccCat->errors, 'errors');
 	} elseif (is_array($cats) && count($cats)>0) {
 		foreach ($cats as $cat)        // Loop on each group
 		{
@@ -314,7 +314,6 @@ elseif ($modecompta=="BOOKKEEPING")
 				foreach ($sommes as $code => $det) {
 					$vars[$code] = $det['NP'];
 				}
-
 
 				$result = strtr($formula, $vars);
 
