c6b3d81d618dc617134c4069e64d3470db4f6125
Laurent Destailleur <eldy@destailleur.fr>
2019-03-14 17:35:44 +0100

    Update html.form.class.php

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -2663,7 +2663,7 @@ class Form
 			if (count($scrit) > 1) $sql.=")";
 			if (! empty($conf->barcode->enabled)){
                 $sql.= " OR p.barcode LIKE '".$this->db->escape($prefix.$filterkey)."%'";
-                $sql.= " OR pfp.barcode LIKE '".$this->db->escape($prefix.$filterkey)."%'";
+                if (! empty($conf->global->BARCODE_USE_BARCODE_FOR_SUPPLIER_PRICES)) $sql.= " OR pfp.barcode LIKE '".$this->db->escape($prefix.$filterkey)."%'";
             }
 			$sql.=')';
 		}
