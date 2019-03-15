c9f89f3854a08d4f22ab86d90546595e915094cc
wdammak <26695620+wdammak@users.noreply.github.com>
2019-03-15 20:28:28 +0100

    Add fk_project to product table
    Prepare the generation of a product from a project.

--- htdocs/install/mysql/migration/9.0.0-10.0.0.sql
+++ htdocs/install/mysql/migration/9.0.0-10.0.0.sql
@@ -254,3 +254,5 @@ ALTER TABLE llx_expensereport_det ADD COLUMN subprice double(24,8) DEFAULT 0 NOT
 ALTER TABLE llx_product_attribute_combination ADD INDEX idx_product_att_com_product_parent (fk_product_parent);
 ALTER TABLE llx_product_attribute_combination ADD INDEX idx_product_att_com_product_child (fk_product_child);
 
+
+ALTER TABLE llx_actioncomm ADD COLUMN calling_duration integer;
