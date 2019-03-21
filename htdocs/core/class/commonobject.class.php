4c85068a85e533e333c45c814c647346cf33aa29
Laurent Destailleur <eldy@destailleur.fr>
2019-03-21 11:53:04 +0100

    FIX Can't insert if there is extrafields mandatory on another entity.

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -4979,8 +4979,9 @@ abstract class CommonObject
 			$table_element = $this->table_element;
 			if ($table_element == 'categorie') $table_element = 'categories'; // For compatibility
 
+			dol_syslog(get_class($this)."::insertExtraFields delete then insert", LOG_DEBUG);
+
 			$sql_del = "DELETE FROM ".MAIN_DB_PREFIX.$table_element."_extrafields WHERE fk_object = ".$this->id;
-			dol_syslog(get_class($this)."::insertExtraFields delete", LOG_DEBUG);
 			$this->db->query($sql_del);
 
 			$sql = "INSERT INTO ".MAIN_DB_PREFIX.$table_element."_extrafields (fk_object";
@@ -4990,6 +4991,11 @@ abstract class CommonObject
 				// Add field of attribut
 				if ($extrafields->attributes[$this->table_element]['type'][$attributeKey] != 'separate') // Only for other type than separator
 					$sql.=",".$attributeKey;
+			}
+			// We must insert a default value for fields for other entities that are mandatory to avoid not null error
+			foreach($extrafields->attributes[$this->table_element]['mandatoryfieldsofotherentities'] as  $tmpkey => $tmpval)
+			{
+			    $sql.=",".$tmpkey;
 			}
 			$sql .= ") VALUES (".$this->id;
 
@@ -5009,10 +5015,17 @@ abstract class CommonObject
 					}
 				}
 			}
+			// We must insert a default value for fields for other entities that are mandatory to avoid not null error
+			foreach($extrafields->attributes[$this->table_element]['mandatoryfieldsofotherentities'] as  $tmpkey => $tmpval)
+			{
+			    if (in_array($tmpval, array('int', 'double'))) $sql.=", 0";
+			    else $sql.=", ''";
+			}
+
 			$sql.=")";
 
-			dol_syslog(get_class($this)."::insertExtraFields insert", LOG_DEBUG);
 			$resql = $this->db->query($sql);
+
 			if (! $resql)
 			{
 				$this->error=$this->db->lasterror();
