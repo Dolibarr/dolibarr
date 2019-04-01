abd8dca3eac7f4e56cf845a8a78e4ce42fc2e6be
ptibogxiv <support@ptibogxiv.net>
2019-04-01 20:26:04 +0200

    Update api_memberstypes.class.php

--- htdocs/adherents/class/api_memberstypes.class.php
+++ htdocs/adherents/class/api_memberstypes.class.php
@@ -99,7 +99,7 @@ class MembersTypes extends DolibarrApi
         $sql = "SELECT t.rowid";
         $sql.= " FROM ".MAIN_DB_PREFIX."adherent_type as t";
         $sql.= ' WHERE t.entity IN ('.getEntity('member_type').')';
-        
+
         // Add sql filters
         if ($sqlfilters)
         {
