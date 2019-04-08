153e563c9f9ef38aa82b46c529a763aefac28f38
Laurent Destailleur <eldy@destailleur.fr>
2019-04-08 13:49:05 +0200

    Update perday.php

--- htdocs/projet/activity/perday.php
+++ htdocs/projet/activity/perday.php
@@ -36,7 +36,7 @@ require_once DOL_DOCUMENT_ROOT.'/holiday/class/holiday.class.php';
 
 $langs->loadLangs(array('projects','users','companies'));
 
-$hookmanager->initHooks(array('perdaycard'));
+$hookmanager->initHooks(array('timesheetperdaycard'));
 
 $action=GETPOST('action','aZ09');
 $mode=GETPOST("mode",'alpha');
