d611b704883fa842bebb0170da6decf1de78ebff
Philippe GRAND <philippe.grand@atoo-net.com>
2019-04-13 22:52:20 +0200

    translation

--- htdocs/core/modules/fichinter/doc/pdf_soleil.modules.php
+++ htdocs/core/modules/fichinter/doc/pdf_soleil.modules.php
@@ -25,7 +25,7 @@
 /**
  *	\file       htdocs/core/modules/fichinter/doc/pdf_soleil.modules.php
  *	\ingroup    ficheinter
- *	\brief      Fichier de la classe permettant de generer les fiches d'intervention au modele Soleil
+ *	\brief      file of Class used to generate the intervention forms for the Soleil model
  */
 require_once DOL_DOCUMENT_ROOT.'/core/modules/fichinter/modules_fichinter.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
@@ -135,13 +135,13 @@ class pdf_soleil extends ModelePDFFicheinter
 		$this->marge_haute =isset($conf->global->MAIN_PDF_MARGIN_TOP)?$conf->global->MAIN_PDF_MARGIN_TOP:10;
 		$this->marge_basse =isset($conf->global->MAIN_PDF_MARGIN_BOTTOM)?$conf->global->MAIN_PDF_MARGIN_BOTTOM:10;
 
-		$this->option_logo = 1;                    // Affiche logo
-		$this->option_tva = 0;                     // Gere option tva FACTURE_TVAOPTION
-		$this->option_modereg = 0;                 // Affiche mode reglement
-		$this->option_condreg = 0;                 // Affiche conditions reglement
-		$this->option_codeproduitservice = 0;      // Affiche code produit-service
-		$this->option_multilang = 1;               // Dispo en plusieurs langues
-		$this->option_draft_watermark = 1;		   //Support add of a watermark on drafts
+		$this->option_logo = 1;                    // Display logo
+		$this->option_tva = 1;                     // Manage the vat option FACTURE_TVAOPTION
+		$this->option_modereg = 1;                 // Display payment mode
+		$this->option_condreg = 1;                 // Display payment terms
+		$this->option_codeproduitservice = 1;      // Display product-service code
+		$this->option_multilang = 1;               // Available in several languages
+		$this->option_draft_watermark = 1;		   // Support add of a watermark on drafts
 
 		// Get source company
 		$this->emetteur=$mysoc;
