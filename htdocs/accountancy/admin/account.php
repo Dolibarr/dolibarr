2829bfc84f2677d2c2222ce3f17f0086a9994559
Abbes Bahfir <bafbes@gmail.com>
2019-04-14 17:11:38 +0100

    fix:Select chart plan from active countries only

--- htdocs/accountancy/admin/account.php
+++ htdocs/accountancy/admin/account.php
@@ -259,7 +259,7 @@ if ($resql)
     $sql = "SELECT a.rowid, a.pcg_version, a.label, a.active, c.code as country_code";
     $sql .= " FROM " . MAIN_DB_PREFIX . "accounting_system as a";
     $sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "c_country as c ON a.fk_country = c.rowid";
-    $sql .= " WHERE a.active = 1";
+    $sql .= " WHERE a.active = 1 AND c.active = 1";
     dol_syslog('accountancy/admin/account.php $sql='.$sql);
     print $sql;
     $resqlchart = $db->query($sql);
