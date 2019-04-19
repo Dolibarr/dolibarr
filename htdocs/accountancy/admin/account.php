6d34a380423d27f3c9b55d7dbd8ffff190505fb2
Laurent Destailleur <eldy@destailleur.fr>
2019-04-19 13:11:50 +0200

    Update account.php

--- htdocs/accountancy/admin/account.php
+++ htdocs/accountancy/admin/account.php
@@ -258,8 +258,8 @@ if ($resql)
     print '<select class="flat" name="chartofaccounts" id="chartofaccounts">';
     $sql = "SELECT a.rowid, a.pcg_version, a.label, a.active, c.code as country_code";
     $sql .= " FROM " . MAIN_DB_PREFIX . "accounting_system as a";
-    $sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "c_country as c ON a.fk_country = c.rowid";
-    $sql .= " WHERE a.active = 1 AND c.active = 1";
+    $sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "c_country as c ON a.fk_country = c.rowid AND c.active = 1";
+    $sql .= " WHERE a.active = 1";
     dol_syslog('accountancy/admin/account.php $sql='.$sql);
     print $sql;
     $resqlchart = $db->query($sql);
