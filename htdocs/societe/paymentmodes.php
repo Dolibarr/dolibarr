50fb8e034834b7ff9438f052c24e2c3600a85ff3
ptibogxiv <support@ptibogxiv.net>
2019-04-24 23:25:32 +0200

    Update paymentmodes.php

--- htdocs/societe/paymentmodes.php
+++ htdocs/societe/paymentmodes.php
@@ -804,7 +804,7 @@ if ($socid && $action != 'edit' && $action != 'create' && $action != 'editcard' 
 		if ($conf->facture->enabled && $user->rights->facture->lire) $elementTypeArray['invoice']=$langs->transnoentitiesnoconv('Invoices');
 		if ($conf->contrat->enabled && $user->rights->contrat->lire) $elementTypeArray['contract']=$langs->transnoentitiesnoconv('Contracts');
 
-	if (! empty($conf->stripe->enabled))
+	if (! empty($conf->stripe->enabled) && ! empty($conf->stripeconnect->enabled) && $conf->global->MAIN_FEATURES_LEVEL >= 2)
 	{
 		$permissiontowrite = $user->rights->societe->creer;
     $stripesupplieracc = $stripe->getStripeAccount($service, $object->id);								// Get Stripe OAuth connect account (no network access here)
