8c313b4b667755498eb4cb519ab8bdc472606a7b
Laurent Destailleur <eldy@destailleur.fr>
2019-04-24 12:13:40 +0200

    Update newpayment.php

--- htdocs/public/payment/newpayment.php
+++ htdocs/public/payment/newpayment.php
@@ -1527,7 +1527,7 @@ if ($source == 'donation')
 	$text='<b>'.$langs->trans("PaymentDonation").'</b>';
 	if (GETPOST('desc', 'alpha')) $text='<b>'.$langs->trans(GETPOST('desc', 'alpha')).'</b>';
 	print '<tr class="CTableRow'.($var?'1':'2').'"><td class="CTableRow'.($var?'1':'2').'">'.$langs->trans("Designation");
-	print '</td><td class="CTableRow'.($var?'1':'2').'"><b>'.$langs->trans("PaymentDonation").'</b>';
+	print '</td><td class="CTableRow'.($var?'1':'2').'">'.$text;
 	print '<input type="hidden" name="source" value="'.dol_escape_htmltag($source).'">';
 	print '<input type="hidden" name="ref" value="'.dol_escape_htmltag($don->ref).'">';
 	print '</td></tr>'."\n";
