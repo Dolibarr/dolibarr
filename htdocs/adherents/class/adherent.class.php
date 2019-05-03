bde6c523dabefdfa277b9e03914444255c17830b
ptibogxiv <support@ptibogxiv.net>
2019-05-03 13:01:13 +0200

    Update adherent.class.php

--- htdocs/adherents/class/adherent.class.php
+++ htdocs/adherents/class/adherent.class.php
@@ -35,7 +35,7 @@
 require_once DOL_DOCUMENT_ROOT.'/core/class/commonobject.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/categories/class/categorie.class.php';
-
+require_once DOL_DOCUMENT_ROOT.'/core/lib/payments.lib.php';
 
 /**
  *		Class to manage members of a foundation
@@ -169,8 +169,10 @@ class Adherent extends CommonObject
     public $poste;
 
 	public $morphy;
+  
 	public $public;
-
+  
+	public $public_payment_url;
     // -1:brouillon, 0:resilie, >=1:valide,paye
     // def in common object
     //public $statut;
@@ -1312,7 +1314,9 @@ class Adherent extends CommonObject
 				$this->photo			= $obj->photo;
 				$this->statut			= $obj->statut;
 				$this->public			= $obj->public;
-
+        
+        $this->public_payment_url	= getOnlinePaymentUrl(0, $element, $obj->ref);
+        
 				$this->datec			= $this->db->jdate($obj->datec);
                 $this->date_creation    = $this->db->jdate($obj->datec);
 				$this->datem			= $this->db->jdate($obj->datem);
