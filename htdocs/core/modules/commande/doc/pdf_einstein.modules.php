99a086f5bd0321c82c6c5c5bf8497a22b80be3df
Laurent Léonard <laurent@openthink.be>
2019-05-04 05:52:19 +0200

    Add customer code in customer orders documents

--- htdocs/core/modules/commande/doc/pdf_einstein.modules.php
+++ htdocs/core/modules/commande/doc/pdf_einstein.modules.php
@@ -1320,6 +1320,14 @@ class pdf_einstein extends ModelePDFCommandes
 		$pdf->SetTextColor(0, 0, 60);
 		$pdf->MultiCell(100, 3, $outputlangs->transnoentities("OrderDate")." : " . dol_print_date($object->date, "%d %b %Y", false, $outputlangs, true), '', 'R');
 
+		if ($object->thirdparty->code_client)
+		{
+			$posy+=4;
+			$pdf->SetXY($posx, $posy);
+			$pdf->SetTextColor(0, 0, 60);
+			$pdf->MultiCell(100, 3, $outputlangs->transnoentities("CustomerCode")." : " . $outputlangs->transnoentities($object->thirdparty->code_client), '', 'R');
+		}
+
 		// Get contact
 		if (!empty($conf->global->DOC_SHOW_FIRST_SALES_REP))
 		{
