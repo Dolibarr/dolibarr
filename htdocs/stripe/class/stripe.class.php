04558c2c88e1b03918ec83723d2f87f7111cac57
ptibogxiv <support@ptibogxiv.net>
2019-05-09 12:59:40 +0200

    Update stripe.class.php

--- htdocs/stripe/class/stripe.class.php
+++ htdocs/stripe/class/stripe.class.php
@@ -1,5 +1,6 @@
 <?php
-/* Copyright (C) 2018-2019 	Thibault FOUCART       <support@ptibogxiv.net>
+/*
+ * Copyright (C) 2018-2019 Thibault FOUCART     <support@ptibogxiv.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
