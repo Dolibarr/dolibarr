68118e55dc4e5a5154243f9bb74dc6d1e98a3ddd
Laurent Destailleur <eldy@destailleur.fr>
2019-05-18 00:58:43 +0200

    Update api_invoices.class.php

--- htdocs/compta/facture/class/api_invoices.class.php
+++ htdocs/compta/facture/class/api_invoices.class.php
@@ -75,7 +75,7 @@ class Invoices extends DolibarrApi
 		$this->invoice->totalpaid = $this->invoice->getSommePaiement();
 		$this->invoice->totalcreditnotes = $this->invoice->getSumCreditNotesUsed();
 		$this->invoice->totaldeposits = $this->invoice->getSumDepositsUsed();
-		$this->invoice->remaintopay = price2num($this->invoice->total_ttc - $this->invoice->totalpaye - $this->invoice->totalcreditnotes - $this->invoice->totaldeposits, 'MT');
+		$this->invoice->remaintopay = price2num($this->invoice->total_ttc - $this->invoice->totalpaid - $this->invoice->totalcreditnotes - $this->invoice->totaldeposits, 'MT');
 
 		if (! DolibarrApi::_checkAccessToResource('facture',$this->invoice->id)) {
 			throw new RestException(401, 'Access not allowed for login '.DolibarrApiAccess::$user->login);
