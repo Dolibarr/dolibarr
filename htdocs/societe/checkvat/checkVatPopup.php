e90bef1cd89bbaa1f08c1780a66a8509251c5c4a
Laurent Destailleur <eldy@destailleur.fr>
2019-05-22 16:41:19 +0200

    Fix Clean vat number for vies

--- htdocs/societe/checkvat/checkVatPopup.php
+++ htdocs/societe/checkvat/checkVatPopup.php
@@ -54,7 +54,7 @@ if (! $vatNumber)
 else
 {
 	$vatNumber = preg_replace('/\^\w/', '', $vatNumber);
-	$vatNumber = str_replace(' ', '', $vatNumber);
+	$vatNumber = str_replace(array(' ', '.'), '', $vatNumber);
 	$countryCode=substr($vatNumber, 0, 2);
 	$vatNumber=substr($vatNumber, 2);
 
