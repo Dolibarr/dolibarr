b9d437631a5a3a9b049af7afccf78fe2c44fb355
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2019-05-31 09:25:59 +0200

    update with html5 compliant code

--- htdocs/compta/facture/fiche-rec.php
+++ htdocs/compta/facture/fiche-rec.php
@@ -1180,7 +1180,7 @@ if ($action == 'create')
 		}
 		print "</table>\n";
 
-		print '<div align="center"><input type="submit" class="button" value="'.$langs->trans("Create").'">';
+		print '<div class="center"><input type="submit" class="button" value="'.$langs->trans("Create").'">';
 		print '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 		print '<input type="button" class="button" value="' . $langs->trans("Cancel") . '" onClick="javascript:history.go(-1)">';
 		print '</div>';
