9a974814fac002004afd5d9b8a90a501b6947653
Lionel VESSILLER <lvessiller>
2019-06-03 15:53:21 +0200

    NEW add extrafields in warehouses - fix load lang stocks

--- htdocs/product/admin/stock_extrafields.php
+++ htdocs/product/admin/stock_extrafields.php
@@ -29,7 +29,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/stock.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
 
 // Load translation files required by the page
-$langs->loadLangs(array('companies', 'admin', 'stock'));
+$langs->loadLangs(array('companies', 'admin', 'stocks'));
 
 $extrafields = new ExtraFields($db);
 $form = new Form($db);
