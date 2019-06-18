181375ff958955a0bd55684d8e4cd97592811672
Nicolas ZABOURI <info@inovea-conseil.com>
2019-06-18 17:09:46 +0200

    FIX travis space left

--- htdocs/opensurvey/index.php
+++ htdocs/opensurvey/index.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (C) 2013-2014 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2019      Nicolas ZABOURI      <info@inovea-conseil.com>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
