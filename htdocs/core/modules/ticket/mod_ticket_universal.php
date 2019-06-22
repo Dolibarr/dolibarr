9d698c257cae577a8dd853ffab06b836e2fad07b
Philippe GRAND <philippe.grand@atoo-net.com>
2019-06-22 12:10:32 +0200

    translation

--- htdocs/core/modules/ticket/mod_ticket_universal.php
+++ htdocs/core/modules/ticket/mod_ticket_universal.php
@@ -53,7 +53,7 @@ class mod_ticket_universal extends ModeleNumRefTicket
 	public $name='Universal';
 
     /**
-     *  Renvoi la description du modele de numerotation
+     *  Returns the description of the numbering model
      *
      *  @return string      Texte descripif
      */
