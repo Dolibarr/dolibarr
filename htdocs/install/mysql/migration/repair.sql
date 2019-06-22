64de57d781227abf818dff59a108ce8de4683aad
Laurent Destailleur <eldy@destailleur.fr>
2019-06-22 13:28:20 +0200

    FIX bad request

--- htdocs/install/mysql/migration/repair.sql
+++ htdocs/install/mysql/migration/repair.sql
@@ -205,8 +205,6 @@ CREATE TABLE tmp_user as (select * from llx_user);
 UPDATE llx_user SET fk_user = NULL where fk_user NOT IN (select rowid from tmp_user);
 
 
-update llx_user set fk_user = null where fk_user not in (select rowid from llx_user);
-
 
 UPDATE llx_product SET canvas = NULL where canvas = 'default@product';
 UPDATE llx_product SET canvas = NULL where canvas = 'service@product';
