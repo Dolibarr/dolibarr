31ffa6bc69aa8b16cce3bba4f1534e0cee8ff8df
Laurent Destailleur <eldy@destailleur.fr>
2019-06-24 18:43:16 +0200

    FIX #11218 #11150

--- htdocs/core/class/discount.class.php
+++ htdocs/core/class/discount.class.php
@@ -230,6 +230,7 @@ class DiscountAbsolute
         $userid = $user->id;
 		if (! ($userid > 0))		// For example when record is saved into an anonymous context with a not loaded object $user.
 		{
+			include_once DOL_DOCUMENT_ROOT.'/compta/facture/class/facture.class.php';
 			$tmpinvoice = new Facture($this->db);
 			$tmpinvoice->fetch($this->fk_facture_source);
 			$userid = $tmpinvoice->fk_user_author;	// We use the author of invoice
