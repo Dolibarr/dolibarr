33ce52364a9447bffc291ef9b7f24e257047efe8
Laurent Destailleur <eldy@destailleur.fr>
2019-06-25 16:08:22 +0200

    Drop no more used column

--- htdocs/install/mysql/migration/10.0.0-11.0.0.sql
+++ htdocs/install/mysql/migration/10.0.0-11.0.0.sql
@@ -43,3 +43,6 @@ ALTER TABLE llx_c_shipment_mode ADD COLUMN entity integer DEFAULT 1 NOT NULL;
 
 ALTER TABLE llx_c_shipment_mode DROP INDEX uk_c_shipment_mode;
 ALTER TABLE llx_c_shipment_mode ADD UNIQUE INDEX uk_c_shipment_mode (code, entity);
+
+ALTER TABLE llx_facture_fourn DROP COLUMN total;
+
