df0d540ffb171601570a3f8ea007f99f1125b202
ptibogxiv <support@ptibogxiv.net>
2019-06-25 15:47:16 +0200

    Update propal.class.php

--- htdocs/comm/propal/class/propal.class.php
+++ htdocs/comm/propal/class/propal.class.php
@@ -1618,8 +1618,9 @@ class Propal extends CommonObject
 	 * @param		int		$only_product	Return only physical products
 	 * @return		int						<0 if KO, >0 if OK
 	 */
-    public function fetch_lines($only_product = 0)
+	public function fetch_lines($only_product = 0, $loadalsotranslation = 0)
 	{
+		global $langs, $conf;
         // phpcs:enable
 		$this->lines=array();
 
@@ -1712,6 +1713,13 @@ class Propal extends CommonObject
 				$line->multicurrency_total_ttc 	= $objp->multicurrency_total_ttc;
 
 				$line->fetch_optionals();
+				
+				// multilangs
+        		if (! empty($conf->global->MAIN_MULTILANGS) && ! empty($objp->fk_product) && ! empty($loadalsotranslation)) {
+        		$line = new Product($this->db);
+        		$line->fetch($objp->fk_product);
+        		$line->getMultiLangs();
+        		}
 
 				$this->lines[$i]        = $line;
 				//dol_syslog("1 ".$line->fk_product);
