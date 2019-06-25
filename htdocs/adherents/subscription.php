e6b643e0734d5b6a4737e0b0d465f26416efcca4
Laurent Destailleur <eldy@destailleur.fr>
2019-06-25 11:07:14 +0200

    Fix syntax error

--- htdocs/adherents/subscription.php
+++ htdocs/adherents/subscription.php
@@ -667,7 +667,7 @@ if ($rowid > 0)
      */
     if ($action != 'addsubscription' && $action != 'create_thirdparty')
     {
-        $sql = "SELECT d.rowid, d.firstname, d.lastname, d.societe, d.type,";
+        $sql = "SELECT d.rowid, d.firstname, d.lastname, d.societe, d.fk_adherent_type as type,";
         $sql.= " c.rowid as crowid, c.subscription,";
         $sql.= " c.datec, c.fk_type as cfk_type,";
         $sql.= " c.dateadh as dateh,";
