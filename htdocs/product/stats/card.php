97827f5172d227981d936c7a6daeaf3a7e350837
Laurent Destailleur <eldy@destailleur.fr>
2019-06-29 01:44:49 +0200

    Fix print

--- htdocs/product/stats/card.php
+++ htdocs/product/stats/card.php
@@ -388,7 +388,7 @@ if ($result || empty($id))
 			}
 			else
 			{
-			    print $dategenerated=($mesg?'<font class="error">'.$mesg.'</font>':$langs->trans("ChartNotGenerated"));
+			    $dategenerated=($mesg?'<font class="error">'.$mesg.'</font>':$langs->trans("ChartNotGenerated"));
 			}
 			$linktoregenerate='<a href="'.$_SERVER["PHP_SELF"].'?id='.(GETPOST('id')?GETPOST('id'):$object->id).((string) $type != ''?'&type='.$type:'').'&action=recalcul&mode='.$mode.'&search_year='.$search_year.'&search_categ='.$search_categ.'">'.img_picto($langs->trans("ReCalculate").' ('.$dategenerated.')', 'refresh').'</a>';
 
