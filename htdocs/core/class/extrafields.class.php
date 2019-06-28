ff7ae5ef2060d42a86eb6274dcf9cccc8f62c715
Laurent Destailleur <eldy@destailleur.fr>
2019-06-28 12:56:31 +0200

    Update extrafields.class.php

--- htdocs/core/class/extrafields.class.php
+++ htdocs/core/class/extrafields.class.php
@@ -1645,8 +1645,8 @@ class ExtraFields
 		elseif ($type == 'double')
 		{
 			if (!empty($value)) {
-			//	$value=price($value);
-				$sizeparts = explode(",",$size);
+				//$value=price($value);
+				$sizeparts = explode(",", $size);
 				$number_decimals = $sizeparts[1];
 				$value=price($value, 0, $langs, 0, 0, $number_decimals, '');
 			}
