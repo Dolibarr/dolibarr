b37f55d13a775cda146aa252304464ed4267cfbb
Laurent Destailleur <eldy@destailleur.fr>
2019-06-29 16:06:40 +0200

    Add log

--- htdocs/core/modules/facture/doc/pdf_crabe.modules.php
+++ htdocs/core/modules/facture/doc/pdf_crabe.modules.php
@@ -237,6 +237,8 @@ class pdf_crabe extends ModelePDFFactures
         // phpcs:enable
 		global $user,$langs,$conf,$mysoc,$db,$hookmanager,$nblignes;
 
+		dol_syslog("write_file outputlangs->defaultlang=".(is_object($outputlangs) ? $outputlangs->defaultlang : 'null'));
+
 		if (! is_object($outputlangs)) $outputlangs=$langs;
 		// For backward compatibility with FPDF, force output charset to ISO, because FPDF expect text to be encoded in ISO
 		if (! empty($conf->global->MAIN_USE_FPDF)) $outputlangs->charset_output='ISO-8859-1';
