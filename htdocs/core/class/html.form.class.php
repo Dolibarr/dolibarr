9f9598ac9939469ad970c1deb9bcad0a6080c6c5
Laurent Destailleur <eldy@destailleur.fr>
2019-07-20 11:44:31 +0200

    FIX missing token

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -6607,7 +6607,7 @@ class Form
 
 			print '</table>';
 
-			if(!empty($compatibleImportElementsList))
+			if (!empty($compatibleImportElementsList))
 			{
 			    $res=@include dol_buildpath('core/tpl/ajax/objectlinked_lineimport.tpl.php');
 			}
