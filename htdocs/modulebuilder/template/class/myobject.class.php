83412e3c90692961c3f2ce5aaaad85147d2b5e12
Laurent Destailleur <eldy@destailleur.fr>
2019-08-03 16:38:57 +0200

    Fix warning

--- htdocs/modulebuilder/template/class/myobject.class.php
+++ htdocs/modulebuilder/template/class/myobject.class.php
@@ -388,7 +388,7 @@ class MyObject extends CommonObject
 		$sql = 'SELECT ';
 		$sql .= $this->getFieldList();
 		$sql .= ' FROM ' . MAIN_DB_PREFIX . $this->table_element. ' as t';
-		if ($this->ismultientitymanaged) $sql .= ' WHERE t.entity IN ('.getEntity($this->table_element).')';
+		if (isset($this->ismultientitymanaged) && $this->ismultientitymanaged == 1) $sql .= ' WHERE t.entity IN ('.getEntity($this->table_element).')';
 		else $sql .= ' WHERE 1 = 1';
 		// Manage filter
 		$sqlwhere = array();
