2dee289fc6eda76e05cc185583f02b1578b4ed35
Je2fb <46494485+Je2fb@users.noreply.github.com>
2019-08-07 17:16:34 +0200

    Update price_parser.class.php
    Calculation with supplier_min_price work
    
    Conflicts:
    	htdocs/product/dynamic_price/class/price_parser.class.php

--- htdocs/product/dynamic_price/class/price_parser.class.php
+++ htdocs/product/dynamic_price/class/price_parser.class.php
@@ -262,9 +262,11 @@ class PriceParser
             return -1;
         }
 
-        //Get the supplier min
-        $productFournisseur = new ProductFournisseur($this->db);
-        $supplier_min_price = $productFournisseur->find_min_price_product_fournisseur($product->id, 0, 0);
+		//Get the supplier min
+		$productFournisseur = new ProductFournisseur($this->db);
+		if ($productFournisseur->find_min_price_product_fournisseur($product->id, 0, 0) > 0) {
+		  	$supplier_min_price = $productFournisseur->fourn_unitprice;
+		}
 
         //Accessible values by expressions
         $extra_values = array_merge($extra_values, array(
