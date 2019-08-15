08f99b42724cc1e78364bcb352bfd15810102664
Jean Traullé <jtraulle@opencomp.fr>
2019-08-15 15:39:49 +0200

    Fix codestyle lessc.class.php should be excluded from ruleset as it is an external lib

--- htdocs/core/lib/website.lib.php
+++ htdocs/core/lib/website.lib.php
@@ -33,7 +33,7 @@
  * @return	boolean							True if OK
  * @see dolWebsiteOutput() for function used to replace content in a web server context
  */
-function dolWebsiteReplacementOfLinks($website, $content, $removephppart = 0, $contenttype='html')
+function dolWebsiteReplacementOfLinks($website, $content, $removephppart = 0, $contenttype = 'html')
 {
 	$nbrep = 0;
 
@@ -191,7 +191,7 @@ function dolKeepOnlyPhpCode($str)
  * @return  void
  * @see	dolWebsiteReplacementOfLinks()  for function used to replace content in the backoffice context.
  */
-function dolWebsiteOutput($content, $contenttype='html', $containerid='')
+function dolWebsiteOutput($content, $contenttype = 'html', $containerid = '')
 {
 	global $db, $langs, $conf, $user;
 	global $dolibarr_main_url_root, $dolibarr_main_data_root;
