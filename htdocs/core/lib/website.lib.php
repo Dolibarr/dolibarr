59625dceb5f205a281d303d86ad97c47624f0dd4
Laurent Destailleur <eldy@destailleur.fr>
2019-08-15 19:03:30 +0200

    Fix phpcs

--- htdocs/core/lib/website.lib.php
+++ htdocs/core/lib/website.lib.php
@@ -113,7 +113,7 @@ function dolKeepOnlyPhpCode($str)
  * @return	boolean							True if OK
  * @see dolWebsiteOutput() for function used to replace content in a web server context
  */
-function dolWebsiteReplacementOfLinks($website, $content, $removephppart = 0, $contenttype='html', $containerid='')
+function dolWebsiteReplacementOfLinks($website, $content, $removephppart = 0, $contenttype = 'html', $containerid = '')
 {
 	$nbrep = 0;
 
@@ -201,7 +201,7 @@ function dolWebsiteReplacementOfLinks($website, $content, $removephppart = 0, $c
  * @return  void
  * @see	dolWebsiteReplacementOfLinks()  for function used to replace content in the backoffice context.
  */
-function dolWebsiteOutput($content, $contenttype='html', $containerid='')
+function dolWebsiteOutput($content, $contenttype = 'html', $containerid = '')
 {
 	global $db, $langs, $conf, $user;
 	global $dolibarr_main_url_root, $dolibarr_main_data_root;
