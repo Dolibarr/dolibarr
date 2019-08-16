48125fd957be6bf8526340b829e297c8d30a35d7
Laurent Destailleur <eldy@destailleur.fr>
2019-08-16 17:32:58 +0200

    Fix phpcs

--- htdocs/product/stock/card.php
+++ htdocs/product/stock/card.php
@@ -74,7 +74,7 @@ if ($id > 0 || ! empty($ref)) {
 }
 
 // Initialize technical object to manage hooks of page. Note that conf->hooks_modules contains array of hook context
-$hookmanager->initHooks(array('warehousecard','globalcard'));
+$hookmanager->initHooks(array('warehousecard', 'globalcard'));
 
 /*
  * Actions
