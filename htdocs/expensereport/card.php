bc66be51a881499cadc9530cb245b4b5cd83d792
Laurent Destailleur <eldy@destailleur.fr>
2019-08-22 11:50:18 +0200

    Update card.php

--- htdocs/expensereport/card.php
+++ htdocs/expensereport/card.php
@@ -2058,9 +2058,11 @@ else
 				        $totalpaid += $objp->amount;
 				        $i++;
 				    }
-					if ( !is_null($totalpaid))
+					if (! is_null($totalpaid))
+					{
 				        $totalpaid = price2num($totalpaid);		// Round $totalpaid to fix floating problem after addition into loop
-
+					}
+					
 				    $remaintopay = price2num($object->total_ttc - $totalpaid);
 				    $resteapayeraffiche = $remaintopay;
 
