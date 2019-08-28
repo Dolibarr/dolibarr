27b61eca3254fe1732684bf279d52d1deffe4db5
Laurent Destailleur <eldy@destailleur.fr>
2019-08-28 18:12:02 +0200

    NEW Add column tms into the table of targets for emailing

--- htdocs/install/mysql/migration/10.0.0-11.0.0.sql
+++ htdocs/install/mysql/migration/10.0.0-11.0.0.sql
@@ -78,3 +78,6 @@ UPDATE llx_bank_url set url = REPLACE( url, 'compta/salaries/', 'salaries/');
 ALTER TABLE llx_stock_mouvement ADD COLUMN fk_projet INTEGER NOT NULL DEFAULT 0 AFTER model_pdf;
 
 ALTER TABLE llx_oauth_token ADD COLUMN fk_soc integer DEFAULT NULL after token;
+
+ALTER TABLE llx_mailing_cibles ADD COLUMN tms timestamp;
+
