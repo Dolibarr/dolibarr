fbafbc3e19f4dab00bda2b7a527021538fc0f3fb
Tim Otte <otte@meuser.it>
2019-09-04 15:16:29 +0200

    Added myself to the author list

--- htdocs/product/fournisseurs.php
+++ htdocs/product/fournisseurs.php
@@ -8,7 +8,8 @@
  * Copyright (C) 2014      Ion Agorria          <ion@agorria.com>
  * Copyright (C) 2015      Alexandre Spangaro   <aspangaro@open-dsi.fr>
  * Copyright (C) 2016      Ferran Marcet		<fmarcet@2byte.es>
- * Copyright (C) 2019       Frédéric France         <frederic.france@netlogic.fr>
+ * Copyright (C) 2019      Frédéric France      <frederic.france@netlogic.fr>
+ * Copyright (C) 2019      Tim Otte			    <otte@meuser.it>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
