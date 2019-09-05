0bf70dddc73972c566aa2456cf470d3c1db76799
Christophe Battarel <christophe@altairis.fr>
2019-09-05 15:33:16 +0200

    actualize from develop

--- htdocs/fourn/commande/dispatch.php
+++ htdocs/fourn/commande/dispatch.php
@@ -571,7 +571,7 @@ if ($id > 0 || ! empty($ref)) {
 					print '<td></td>';
 					print '<td></td>';
 				}
-				//print '<td class="right">' . $langs->trans("SupplierRef") . '</td>';
+				print '<td class="right">' . $langs->trans("SupplierRef") . '</td>';
 				print '<td class="right">' . $langs->trans("QtyOrdered") . '</td>';
 				print '<td class="right">' . $langs->trans("QtyDispatchedShort") . '</td>';
 				print '<td class="right">' . $langs->trans("QtyToDispatchShort") . '</td>';
@@ -655,7 +655,7 @@ if ($id > 0 || ! empty($ref)) {
 							$up_ht_disc = price2num($up_ht_disc * (100 - $objp->remise_percent) / 100, 'MU');
 
 						// Supplier ref
-						//print '<td class="right">'.$objp->sref.'</td>';
+						print '<td class="right">'.$objp->sref.'</td>';
 
 						// Qty ordered
 						print '<td class="right">' . $objp->qty . '</td>';
@@ -748,7 +748,7 @@ if ($id > 0 || ! empty($ref)) {
 							print '</tr>';
 
 							print '<tr class="oddeven" name="' . $type . $suffix . '">';
-							print '<td colspan="6">';
+							print '<td colspan="7">';
 							print '<input name="fk_commandefourndet' . $suffix . '" type="hidden" value="' . $objp->rowid . '">';
 							print '<input name="product' . $suffix . '" type="hidden" value="' . $objp->fk_product . '">';
 
