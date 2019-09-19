d40534e8fadf0a11034010bb27b53b8b70290294
gauthier <gauthier.verdol@atm-consulting.fr>
2019-09-19 15:21:52 +0200

    FIX : change product type must be allowed if we activate hidden conf

--- htdocs/product/card.php
+++ htdocs/product/card.php
@@ -1597,7 +1597,9 @@ else
 			{
 				// TODO change for compatibility with edit in place
 				$typeformat='select;0:'.$langs->trans("Product").',1:'.$langs->trans("Service");
-				print '<tr><td class="titlefield">'.$form->editfieldkey("Type", 'fk_product_type', $object->type, $object, $usercancreate, $typeformat).'</td><td colspan="2">';
+				print '<tr><td class="titlefield">';
+				print (!empty($conf->global->MAIN_ALLOW_CHANGE_PRODUCT_TYPE)) ? $form->editfieldkey("Type", 'fk_product_type', $object->type, $object, $usercancreate, $typeformat) : $langs->trans('Type');
+				print '</td><td colspan="2">';
 				print $form->editfieldval("Type", 'fk_product_type', $object->type, $object, $usercancreate, $typeformat);
 				print '</td></tr>';
 			}
