1b2558bb1f7002e837161d1f054042f1b05fe5cf
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2019-09-23 21:43:11 +0200

    typo

--- htdocs/core/boxes/box_accountancy_last_manual_entries.php
+++ htdocs/core/boxes/box_accountancy_last_manual_entries.php
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 /**
@@ -133,14 +133,6 @@ class box_accountancy_last_manual_entries extends ModeleBoxes
 						'text' => $label,
 						'asis' => 1,
 					);
-
-                    /*
-                    $this->info_box_contents[$line][] = array(
-                        'td' => 'class="tdoverflowmax150 maxwidth150onsmartphone"',
-                        'text' => $societestatic->getNomUrl(1),
-                        'asis' => 1,
-                    );
-                    */
 
                     $this->info_box_contents[$line][] = array(
                         'td' => 'class="nowrap right"',
