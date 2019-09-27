64a7bbe0fd43fc43a93554058143d40f67a76364
Laurent Destailleur <eldy@destailleur.fr>
2019-09-27 10:19:27 +0200

    Move ajax file

--- htdocs/takepos/ajax/ajax.php
+++ htdocs/takepos/ajax/ajax.php
@@ -16,7 +16,7 @@
  */
 
 /**
- *	\file       htdocs/takepos/ajax.php
+ *	\file       htdocs/takepos/ajax/ajax.php
  *	\brief      Ajax search component for TakePos. It search products of a category.
  */
 
@@ -30,7 +30,7 @@ if (! defined('NOREQUIREMENU'))		define('NOREQUIREMENU', '1');
 if (! defined('NOREQUIREHTML'))		define('NOREQUIREHTML', '1');
 if (! defined('NOREQUIREAJAX'))		define('NOREQUIREAJAX', '1');
 
-require '../main.inc.php';	// Load $user and permissions
+require '../../main.inc.php';	// Load $user and permissions
 require_once DOL_DOCUMENT_ROOT.'/categories/class/categorie.class.php';
 
 $category = GETPOST('category', 'alpha');
