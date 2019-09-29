f132f2f947c77b709d757629a3841e7cb4c0f16b
ptibogxiv <support@ptibogxiv.net>
2019-09-29 15:40:26 +0200

    Update 10.0.0-11.0.0.sql

--- htdocs/install/mysql/migration/10.0.0-11.0.0.sql
+++ htdocs/install/mysql/migration/10.0.0-11.0.0.sql
@@ -80,8 +80,8 @@ ALTER TABLE llx_extrafields ADD COLUMN printable boolean DEFAULT FALSE;
 ALTER TABLE llx_facture ADD COLUMN retained_warranty real DEFAULT NULL after situation_final;
 ALTER TABLE llx_facture ADD COLUMN retained_warranty_date_limit	date DEFAULT NULL after retained_warranty;
 ALTER TABLE llx_facture ADD COLUMN retained_warranty_fk_cond_reglement	integer  DEFAULT NULL after retained_warranty_date_limit;
-ALTER TABLE llx_facture ADD COLUMN date_cloture datetime DEFAULT NULL after date_valid;
-ALTER TABLE llx_facture ADD COLUMN fk_user_cloture integer DEFAULT NULL after fk_user_valid;
+ALTER TABLE llx_facture ADD COLUMN date_cosing datetime DEFAULT NULL after date_valid;
+ALTER TABLE llx_facture ADD COLUMN fk_user_closing integer DEFAULT NULL after fk_user_valid;
 
 ALTER TABLE llx_c_shipment_mode ADD COLUMN entity integer DEFAULT 1 NOT NULL;
 
