cf3c69ce0f90bbf03004e43d0bcf4a6fc0e6a79e
Laurent Destailleur <eldy@destailleur.fr>
2019-10-08 10:29:06 +0200

    Patch

--- htdocs/includes/tecnickcom/tcpdf/include/barcodes/qrcode.php
+++ htdocs/includes/tecnickcom/tcpdf/include/barcodes/qrcode.php
@@ -236,6 +236,7 @@ if (!defined('QRCODEDEFS')) {
 	/**
 	 * if false, checks all masks available, otherwise value tells count of masks need to be checked, mask id are got randomly
 	 */
+	// @CHANGE DOL LDR
 	define('QR_FIND_FROM_RANDOM', false);
 
 	/**
