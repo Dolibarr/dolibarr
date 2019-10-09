351f13bdcb5e5ebbb82ebacf22abf015db22eec9
Laurent De Coninck <lau.deconinck@gmail.com>
2019-10-09 08:14:59 +0200

    simplify isText

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -7173,12 +7173,7 @@ abstract class CommonObject
 	 */
 	public function isText($info)
 	{
-		if(is_array($info))
-		{
-			if(isset($info['type']) && $info['type']=='text') return true;
-			else return false;
-		}
-		return false;
+        return is_array($info) && isset($info['type']) && $info['type']=='text';
 	}
 
 	/**
