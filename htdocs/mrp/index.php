07488fec3d55297e620801ba079f55bdf3bd00ba
Frédéric FRANCE <frederic34@users.noreply.github.com>
2019-10-19 20:23:31 +0200

    Update index.php

--- htdocs/mrp/index.php
+++ htdocs/mrp/index.php
@@ -1,9 +1,10 @@
 <?php
 /* Copyright (C) 2001-2002	Rodolphe Quiedeville	<rodolphe@quiedeville.org>
- * Copyright (C) 2003		Jean-Louis Bergamo	<jlb@j1b.org>
- * Copyright (C) 2004-2017	Laurent Destailleur	<eldy@users.sourceforge.net>
- * Copyright (C) 2005-2012	Regis Houssin		<regis.houssin@inodbox.com>
- * Copyright (C) 2019           Nicolas ZABOURI         <info@inovea-conseil.com>
+ * Copyright (C) 2003		Jean-Louis Bergamo	    <jlb@j1b.org>
+ * Copyright (C) 2004-2017	Laurent Destailleur	    <eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2012	Regis Houssin		    <regis.houssin@inodbox.com>
+ * Copyright (C) 2019       Nicolas ZABOURI         <info@inovea-conseil.com>
+ * Copyright (C) 2019       Frédéric France         <frederic.france@netlogic.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -209,6 +210,10 @@ if ($resql)
 			print '</tr>';
 			$i++;
 		}
+	} else {
+		print '<tr class="oddeven">';
+		print '<td>' . $langs->trans("None") . '</td>';
+		print '</tr>';
 	}
 	print "</table></div>";
 	print "<br>";
