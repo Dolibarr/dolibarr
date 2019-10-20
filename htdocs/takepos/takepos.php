e224170dc2691a679ff3a63be65b8c9b7d904c16
Laurent Destailleur <eldy@destailleur.fr>
2019-10-20 15:24:52 +0200

    FIX Clean the + of categories on the product view only in POS module
    Conflicts:
    	htdocs/takepos/takepos.php

--- htdocs/takepos/takepos.php
+++ htdocs/takepos/takepos.php
@@ -408,9 +408,9 @@ function New() {
 function Search2() {
 	console.log("Search2");
 	pageproducts=0;
-	jQuery(".catwatermark").hide();
-	$.getJSON('./ajax.php?action=search&term='+$('#search').val(), function(data) {
-		for (i = 0; i < 30; i++) {
+	jQuery(".wrapper2 .catwatermark").hide();
+	$.getJSON('<?php echo DOL_URL_ROOT ?>/takepos/ajax/ajax.php?action=search&term='+$('#search').val(), function(data) {
+		for (i = 0; i < <?php echo $MAXPRODUCT ?>; i++) {
 			if (typeof (data[i]) == "undefined"){
 				$("#prodesc"+i).text("");
 				$("#proimg"+i).attr("src","genimg/empty.png");
