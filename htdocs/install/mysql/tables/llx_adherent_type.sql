d77215c40e80b96c58dbb34b5f214f5c03b874a3
ptibogxiv <support@ptibogxiv.net>
2019-10-22 12:52:14 +0200

    Update llx_adherent_type.sql

--- htdocs/install/mysql/tables/llx_adherent_type.sql
+++ htdocs/install/mysql/tables/llx_adherent_type.sql
@@ -30,7 +30,7 @@ create table llx_adherent_type
   statut           smallint NOT NULL DEFAULT 0,
   libelle          varchar(50) NOT NULL,
   morphy           varchar(3) NOT NULL,
-  duration         varchar(6) NOT NULL DEFAULT '1y',
+  duration         varchar(6) NOT NULL DEFAULT null,
   subscription     varchar(3) NOT NULL DEFAULT 'yes',
   vote             varchar(3) NOT NULL DEFAULT 'yes',
   note             text,
