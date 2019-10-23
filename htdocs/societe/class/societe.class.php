8ed6b41fc236d6d36144bc4d8b0a64fd96063982
altairis <contact@altairis.fr>
2019-10-23 17:30:32 +0200

    back

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -2367,7 +2367,6 @@ class Societe extends CommonObject
 		$sql = "SELECT rowid, email, statut, phone_mobile, lastname, poste, firstname";
 		$sql.= " FROM ".MAIN_DB_PREFIX."socpeople";
 		$sql.= " WHERE fk_soc = ".$this->id;
-		$sql.= " ORDER BY lastname, firstname";
 
 		$resql=$this->db->query($sql);
 		if ($resql)
