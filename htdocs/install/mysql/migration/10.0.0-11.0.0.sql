0052349a6a81f72c3c6ff27762f9f5c30d54b004
Frédéric FRANCE <frederic34@users.noreply.github.com>
2019-10-27 17:48:11 +0100

    Update 10.0.0-11.0.0.sql

--- htdocs/install/mysql/migration/10.0.0-11.0.0.sql
+++ htdocs/install/mysql/migration/10.0.0-11.0.0.sql
@@ -391,7 +391,7 @@ CREATE TABLE llx_mrp_mo(
 ALTER TABLE llx_mrp_mo ADD INDEX idx_mrp_mo_ref (ref);
 ALTER TABLE llx_mrp_mo ADD INDEX idx_mrp_mo_entity (entity);
 ALTER TABLE llx_mrp_mo ADD INDEX idx_mrp_mo_fk_soc (fk_soc);
-ALTER TABLE llx_mrp_mo ADD CONSTRAINT fk_mrp_mo_fk_user_creat FOREIGN KEY (fk_user_creat) REFERENCES user(rowid);
+ALTER TABLE llx_mrp_mo ADD CONSTRAINT fk_mrp_mo_fk_user_creat FOREIGN KEY (fk_user_creat) REFERENCES llx_user(rowid);
 ALTER TABLE llx_mrp_mo ADD INDEX idx_mrp_mo_status (status);
 ALTER TABLE llx_mrp_mo ADD INDEX idx_mrp_mo_fk_product (fk_product);
 ALTER TABLE llx_mrp_mo ADD INDEX idx_mrp_mo_date_start_planned (date_start_planned);
