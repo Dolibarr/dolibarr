af288e1fc61b7f360e928a44db075c595a633834
Philippe GRAND <philippe.grand@atoo-net.com>
2019-10-30 09:53:12 +0100

    add comment

--- htdocs/fichinter/class/fichinterrec.class.php
+++ htdocs/fichinter/class/fichinterrec.class.php
@@ -42,6 +42,10 @@ class FichinterRec extends Fichinter
     public $table_element = 'fichinter_rec';
     public $table_element_line = 'fichinter_rec';
     public $fk_element = 'fk_fichinter';
+    
+    /**
+     * @var string String with name of icon for myobject. Must be the part after the 'object_' into object_myobject.png
+     */
     public $picto = 'intervention';
 
     public $title;
