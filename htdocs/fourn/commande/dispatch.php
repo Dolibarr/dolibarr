c574f5990f63de104d294e0262b060e01426607e
altairis <contact@altairis.fr>
2019-11-07 14:31:30 +0100

    add constant DISPATCH_FORCE_QTY_INPUT

--- htdocs/fourn/commande/dispatch.php
+++ htdocs/fourn/commande/dispatch.php
@@ -780,7 +780,7 @@ if ($id > 0 || ! empty($ref)) {
 
 						// Qty to dispatch
 						print '<td class="right">';
-						print '<input id="qty' . $suffix . '" name="qty' . $suffix . '" type="text" class="width50 right" value="' . (GETPOST('qty' . $suffix) != '' ? GETPOST('qty' . $suffix) : $remaintodispatch) . '">';
+						print '<input id="qty' . $suffix . '" name="qty' . $suffix . '" type="text" class="width50 right" value="' . (GETPOST('qty' . $suffix) != '' ? GETPOST('qty' . $suffix) : ( ! empty($conf->global->DISPATCH_FORCE_QTY_INPUT) ? 0 : $remaintodispatch) ) .'">';
 						print '</td>';
 
 						print '<td>';
