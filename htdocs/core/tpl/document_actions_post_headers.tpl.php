Dolibarr/dolibarr

commit 991e1b2747c082522dad1f45e2528a4c4207dbab

NEW Add upload document on account statement

htdocs/core/tpl/document_actions_post_headers.tpl.php
@@ -2,6 +2,7 @@
 /* Copyright (C)    2013      Cédric Salvador     <csalvador@gpcsolutions.fr>
  * Copyright (C)    2013-2014 Laurent Destailleur <eldy@users.sourceforge.net>
  * Copyright (C)	2015	  Marcos García		  <marcosgdf@gmail.com>
+ * Copyright (C) 	2019	  Nicolas ZABOURI     <info@inovea-conseil.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -82,7 +83,7 @@ if (empty($conf->global->MAIN_DISABLE_SUGGEST_REF_AS_PREFIX))
 
 // Show upload form (document and links)
 $formfile->form_attach_new_file(
-    $_SERVER["PHP_SELF"].'?id='.$object->id.(empty($withproject)?'':'&withproject=1'),
+    $_SERVER["PHP_SELF"].'?id='.$object->id.(empty($withproject)?'':'&withproject=1').(empty($uri)?'':$uri),
     '',
     0,
     0,
@@ -117,6 +118,7 @@ $formfile->list_of_documents(
 );
 
 print "<br>";
+
 //List of links
-$formfile->listOfLinks($object, $permission, $action, GETPOST('linkid', 'int'), $param);
+$formfile->listOfLinks($object, $permission, $action, GETPOST('linkid', 'int'), $param,$relativepathwithnofile);
 print "<br>";

htdocs/compta/bank/account_statement_document.php
@@ -1,0 +1,156 @@
+<?php
+
+/* Copyright (C) 2003-2007 Rodolphe Quiedeville  <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2008 Laurent Destailleur   <eldy@users.sourceforge.net>
+ * Copyright (C) 2005      Marc Barilley / Ocebo <marc@ocebo.com>
+ * Copyright (C) 2005-2017 Regis Houssin         <regis.houssin@inodbox.com>
+ * Copyright (C) 2019	   Nicolas ZABOURI       <info@inovea-conseil.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 	\file       htdocs/compta/bank/document.php
+ * 	\ingroup    banque
+ * 	\brief      Page de gestion des documents attaches a un compte bancaire
+ */
+require '../../main.inc.php';
+require_once DOL_DOCUMENT_ROOT . "/core/lib/bank.lib.php";
+require_once DOL_DOCUMENT_ROOT . "/core/lib/files.lib.php";
+require_once DOL_DOCUMENT_ROOT . "/core/lib/images.lib.php";
+require_once DOL_DOCUMENT_ROOT . "/core/class/html.formfile.class.php";
+require_once DOL_DOCUMENT_ROOT . '/compta/bank/class/account.class.php';
+
+global $conf,$db,$langs;
+// Load translation files required by the page
+$langs->loadLangs(array('banks', 'companies', 'other'));
+
+$id = (GETPOST('id', 'int') ? GETPOST('id', 'int') : GETPOST('account', 'int'));
+$ref = GETPOST('ref', 'alpha');
+$action = GETPOST('action', 'alpha');
+$confirm = GETPOST('confirm', 'alpha');
+$num = (GETPOST('num', 'alpha') ? GETPOST('num', 'alpha') : GETPOST('sectionid', 'alpha'));
+
+$mesg = '';
+if (isset($_SESSION['DolMessage'])) {
+	$mesg = $_SESSION['DolMessage'];
+	unset($_SESSION['DolMessage']);
+}
+
+// Security check
+if ($user->socid) {
+	$action = '';
+	$socid = $user->socid;
+}
+if ($user->socid)
+	$socid = $user->socid;
+
+// Get parameters
+$sortfield = GETPOST("sortfield", 'alpha');
+$sortorder = GETPOST("sortorder", 'alpha');
+$page = GETPOST("page", 'int');
+if (empty($page) || $page == -1) { $page = 0; }
+$offset = $conf->liste_limit * $page;
+$pageprev = $page - 1;
+$pagenext = $page + 1;
+if (!$sortorder)
+	$sortorder = "ASC";
+if (!$sortfield)
+	$sortfield = "name";
+
+$object = new Account($db);
+if ($id > 0 || ! empty($ref)) $object->fetch($id, $ref);
+
+$result = restrictedArea($user, 'banque', $object->id, 'bank_account', '', '');
+
+
+/*
+ * Actions
+ */
+
+if (!empty($num))
+{
+	$object->fetch_thirdparty();
+	$upload_dir = $conf->bank->dir_output . "/".$id."/statement/" . dol_sanitizeFileName($num);
+}
+$backtopage = $_SERVER['PHP_SELF']."?account=".$id."&num=".$num;
+include_once DOL_DOCUMENT_ROOT . '/core/actions_linkedfiles.inc.php';
+
+
+/*
+ * View
+ */
+
+$title = $langs->trans("FinancialAccount").' - '.$langs->trans("Documents");
+$helpurl = "";
+llxHeader('', $title, $helpurl);
+
+$form = new Form($db);
+
+if ($id > 0 || !empty($ref)) {
+	if ($object->fetch($id, $ref)) {
+		$upload_dir = $conf->bank->dir_output . "/".$id."/statement/" . dol_sanitizeFileName($num);
+
+		// Onglets
+		$head = account_statement_prepare_head($object,$num);
+		dol_fiche_head($head, 'document', $langs->trans("FinancialAccount"), -1, 'account');
+
+
+		// Build file list
+		$filearray = dol_dir_list($upload_dir, "files", 0, '', '\.meta$', $sortfield, (strtolower($sortorder) == 'desc' ? SORT_DESC : SORT_ASC), 1);
+		$totalsize = 0;
+		foreach ($filearray as $key => $file) {
+			$totalsize+=$file['size'];
+		}
+
+		$morehtmlref = '';
+
+
+		$title=$langs->trans("AccountStatement").' '.$num.' - '.$langs->trans("BankAccount").' '.$object->getNomUrl(1, 'receipts');
+		print load_fiche_titre($title, '', 'title_bank.png');
+
+
+		print '<div class="fichecenter">';
+		print '<div class="underbanner clearboth"></div>';
+
+		print '<table class="border tableforfield centpercent">';
+		print '<tr><td class="titlefield">' . $langs->trans("NbOfAttachedFiles") . '</td><td colspan="3">' . count($filearray) . '</td></tr>';
+		print '<tr><td>' . $langs->trans("TotalSizeOfAttachedFiles") . '</td><td colspan="3">' .dol_print_size($totalsize, 1, 1).'</td></tr>';
+		print "</table>\n";
+
+		print '</div>';
+
+		dol_fiche_end();
+
+
+		$modulepart = 'bank';
+		$permission = $user->rights->banque->modifier;
+		$permtoedit = $user->rights->banque->modifier;
+		$param = '&id=' . $object->id.'&num='.$num;
+		$uri = '&num='.$num;
+		$relativepathwithnofile = $id."/statement/".$num."/";
+		include_once DOL_DOCUMENT_ROOT . '/core/tpl/document_actions_post_headers.tpl.php';
+	}
+	else {
+		dol_print_error($db);
+	}
+}
+else {
+	Header('Location: index.php');
+	exit;
+}
+
+// End of page
+llxFooter();
+$db->close();

htdocs/compta/bank/releve.php
@@ -4,6 +4,7 @@
  * Copyright (C) 2005-2013 Regis Houssin        <regis.houssin@inodbox.com>
  * Copyright (C) 2015      Jean-François Ferry	<jfefe@aternatik.fr>
  * Copyright (C) 2017      Patrick Delcroix	<pmpdelcroix@gmail.com>
+ * Copyright (C) 2019	  Nicolas ZABOURI     <info@inovea-conseil.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -360,7 +361,12 @@ else
 	 *   Show list of bank statements
 	 */
 
-    $mesprevnext='';
+	// Onglets
+	$head=account_statement_prepare_head($object,$numref);
+	dol_fiche_head($head, 'statement', $langs->trans("FinancialAccount"), 0, 'account');
+
+
+	$mesprevnext='';
 	$mesprevnext.='<div class="pagination"><ul>';
 	$mesprevnext.='<li class="pagination"><a class="paginationnext" href="'.$_SERVER["PHP_SELF"].'?rel=prev&amp;num='.$numref.'&amp;ve='.$ve.'&amp;account='.$object->id.'"><i class="fa fa-chevron-left" title="'.dol_escape_htmltag($langs->trans("Previous")).'"></i></a></li>';
 	//$mesprevnext.=' &nbsp; ';

htdocs/core/lib/bank.lib.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2012		Regis Houssin		<regis.houssin@inodbox.com>
  * Copyright (C) 2015		Alexandre Spangaro	<aspangaro@open-dsi.fr>
  * Copyright (C) 2016		Juanjo Menent   	<jmenent@2byte.es>
+ * Copyright (C) 2019	   Nicolas ZABOURI     <info@inovea-conseil.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -152,8 +153,49 @@ function bank_admin_prepare_head($object)
 
 	complete_head_from_modules($conf, $langs, $object, $head, $h, 'bank_admin', 'remove');
 
+
 	return $head;
 }
+
+
+/**
+ * Prepare array with list of tabs
+ *
+ * @param   Object	$object		Object related to tabs
+ * @param   Object	$num		val to account statement
+ * @return  array				Array of tabs to shoc
+ */
+function account_statement_prepare_head($object,$num)
+{
+	global $langs, $conf, $user,$db;
+	$h = 0;
+	$head = array();
+
+	$head[$h][0] = DOL_URL_ROOT . '/compta/bank/releve.php?account=' . $object->id.'&num='.$num;
+	$head[$h][1] = $langs->trans("AccountStatements");
+	$head[$h][2] = 'statement';
+	$h++;
+
+	// Attached files
+	require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
+	require_once DOL_DOCUMENT_ROOT.'/core/class/link.class.php';
+	$upload_dir = $conf->bank->dir_output . "/" . $object->id.'/'.dol_sanitizeFileName($num);
+	$nbFiles = count(dol_dir_list($upload_dir, 'files', 0, '', '(\.meta|_preview.*\.png)$'));
+	$nbLinks=Link::count($db, $object->element, $object->id);
+
+	$head[$h][0] = DOL_URL_ROOT . "/compta/bank/account_statement_document.php?account=" . $object->id."&num=".$num;
+	$head[$h][1] = $langs->trans("Documents");
+	if (($nbFiles+$nbLinks) > 0) $head[$h][1].= ' <span class="badge">'.($nbFiles+$nbLinks).'</span>';
+	$head[$h][2] = 'document';
+	$h++;
+
+	complete_head_from_modules($conf, $langs, $object, $head, $h, 'account_statement');
+
+	complete_head_from_modules($conf, $langs, $object, $head, $h, 'account_statement', 'remove');
+
+	return $head;
+}
+
 
 /**
  * Prepare array with list of tabs

commit 6a4ba74ee73d5a10e40b3bbffe3fd48b10afdabb

Fix regression introduced by 991e1b2747c082522dad1f45e2528a4c4207dbab

htdocs/core/tpl/document_actions_post_headers.tpl.php
@@ -83,7 +83,7 @@ if (empty($conf->global->MAIN_DISABLE_SUGGEST_REF_AS_PREFIX))
 
 // Show upload form (document and links)
 $formfile->form_attach_new_file(
-    $_SERVER["PHP_SELF"].'?id='.$object->id.(empty($withproject)?'':'&withproject=1').(empty($uri)?'':$uri),
+    $_SERVER["PHP_SELF"].'?id='.$object->id.(empty($withproject)?'':'&withproject=1'),
     '',
     0,
     0,