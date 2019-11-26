1fae37e08b91404976451dd4b94bf8871a27d0fb
Frédéric FRANCE <frederic.france@free.fr>
2019-11-26 20:51:10 +0100

    test psr12 operator spacing

--- htdocs/admin/ecm.php
+++ htdocs/admin/ecm.php
@@ -23,7 +23,7 @@
  */
 
 require '../main.inc.php';
-require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
+require_once DOL_DOCUMENT_ROOT . '/core/lib/admin.lib.php';
 
 // Load translation files required by the page
 $langs->load("admin");
@@ -36,10 +36,10 @@ if (! $user->admin) accessforbidden();
  */
 if (preg_match('/set_([a-z0-9_\-]+)/i', $action, $reg))
 {
-    $code=$reg[1];
+    $code = $reg[1];
     if (dolibarr_set_const($db, $code, 1, 'chaine', 0, '', $conf->entity) > 0)
     {
-        header("Location: ".$_SERVER["PHP_SELF"]);
+        header("Location: " . $_SERVER["PHP_SELF"]);
         exit;
     }
     else
@@ -50,10 +50,10 @@ if (preg_match('/set_([a-z0-9_\-]+)/i', $action, $reg))
 
 if (preg_match('/del_([a-z0-9_\-]+)/i', $action, $reg))
 {
-    $code=$reg[1];
+    $code = $reg[1];
     if (dolibarr_del_const($db, $code, $conf->entity) > 0)
     {
-        header("Location: ".$_SERVER["PHP_SELF"]);
+        header("Location: " . $_SERVER["PHP_SELF"]);
         exit;
     }
     else
@@ -67,18 +67,18 @@ if (preg_match('/del_([a-z0-9_\-]+)/i', $action, $reg))
  * View
  */
 
-$help_url='';
+$help_url = '';
 llxHeader('', $langs->trans("ECMSetup"), $help_url);
 
-$linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans("BackToModuleList").'</a>';
+$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php?restore_lastsearch_values=1">' . $langs->trans("BackToModuleList") . '</a>';
 print load_fiche_titre($langs->trans("ECMSetup"), $linkback, 'title_setup');
 print '<br>';
 
 print '<table class="noborder centpercent">';
 print '<tr class="liste_titre">';
-print '<td>'.$langs->trans("Description").'</td>';
+print '<td>' . $langs->trans("Description") . '</td>';
 print '<td class="center" width="20">&nbsp;</td>';
-print '<td class="center" width="100">'.$langs->trans("Value").'</td>'."\n";
+print '<td class="center" width="100">' . $langs->trans("Value") . '</td>' . "\n";
 print '</tr>';
 
 $form = new Form($db);
@@ -86,7 +86,7 @@ $form = new Form($db);
 // Mail required for members
 
 print '<tr class="oddeven">';
-print '<td>'.$langs->trans("ECMAutoTree").'</td>';
+print '<td>' . $langs->trans("ECMAutoTree") . '</td>';
 print '<td class="center" width="20">&nbsp;</td>';
 
 print '<td class="center" width="100">';
@@ -98,11 +98,11 @@ else
 {
 	if (empty($conf->global->ECM_AUTO_TREE_ENABLED))
 	{
-		print '<a href="'.$_SERVER['PHP_SELF'].'?action=set_ECM_AUTO_TREE_ENABLED">'.img_picto($langs->trans("Disabled"), 'off').'</a>';
+		print '<a href="' . $_SERVER['PHP_SELF'] . '?action=set_ECM_AUTO_TREE_ENABLED">' . img_picto($langs->trans("Disabled"), 'off') . '</a>';
 	}
 	elseif(! empty($conf->global->USER_MAIL_REQUIRED))
 	{
-		print '<a href="'.$_SERVER['PHP_SELF'].'?action=del_ECM_AUTO_TREE_ENABLED">'.img_picto($langs->trans("Enabled"), 'on').'</a>';
+		print '<a href="' . $_SERVER['PHP_SELF'] . '?action=del_ECM_AUTO_TREE_ENABLED">' . img_picto($langs->trans("Enabled"), 'on') . '</a>';
 	}
 }
 print '</td></tr>';
