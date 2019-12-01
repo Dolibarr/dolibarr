c780838e34ec7cdf9a48e47a4fba60ee43b9dbe4
Frédéric FRANCE <frederic34@users.noreply.github.com>
2019-12-01 20:50:18 +0100

    The property db does not exist on mod_propale_saphir

--- htdocs/core/modules/propale/mod_propale_saphir.php
+++ htdocs/core/modules/propale/mod_propale_saphir.php
@@ -64,11 +64,11 @@ class mod_propale_saphir extends ModeleNumRefPropales
      */
 	public function info()
     {
-    	global $conf, $langs;
+    	global $conf, $langs, $db;
 
 		$langs->load("bills");
 
-		$form = new Form($this->db);
+		$form = new Form($db);
 
 		$texte = $langs->trans('GenericNumRefModelDesc')."<br>\n";
 		$texte.= '<form action="'.$_SERVER["PHP_SELF"].'" method="POST">';
@@ -77,7 +77,7 @@ class mod_propale_saphir extends ModeleNumRefPropales
 		$texte.= '<input type="hidden" name="maskconstpropal" value="PROPALE_SAPHIR_MASK">';
 		$texte.= '<table class="nobordernopadding" width="100%">';
 
-		$tooltip=$langs->trans("GenericMaskCodes", $langs->transnoentities("Proposal"), $langs->transnoentities("Proposal"));
+		$tooltip = $langs->trans("GenericMaskCodes", $langs->transnoentities("Proposal"), $langs->transnoentities("Proposal"));
 		$tooltip.=$langs->trans("GenericMaskCodes2");
 		$tooltip.=$langs->trans("GenericMaskCodes3");
 		$tooltip.=$langs->trans("GenericMaskCodes4a", $langs->transnoentities("Proposal"), $langs->transnoentities("Proposal"));
