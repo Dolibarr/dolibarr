6e64a20f9e859d4b6f039ed1c1ad3d763bb156fb
Frédéric FRANCE <frederic34@users.noreply.github.com>
2019-12-13 11:16:46 +0100

    Update filecheck.php

--- htdocs/admin/system/filecheck.php
+++ htdocs/admin/system/filecheck.php
@@ -214,7 +214,7 @@ if (! $error && $xml)
         $includecustom=(empty($xml->dolibarr_htdocs_dir[0]['includecustom'])?0:$xml->dolibarr_htdocs_dir[0]['includecustom']);
 
         // Defined qualified files (must be same than into generate_filelist_xml.php)
-        $regextoinclude='\.(php|css|scss|html|xml|js|json|tpl|jpg|png|gif|ico|sql|lang|txt|yml|bak|md|mp3|mp4|wav|mkv|z|gz|zip|rar|tar|less|svg|eot|woff|woff2|ttf|manifest)$';
+        $regextoinclude='\.(php|php3|php4|php5|phtml|phps|phar|css|scss|html|xml|js|json|tpl|jpg|png|gif|ico|sql|lang|txt|yml|bak|md|mp3|mp4|wav|mkv|z|gz|zip|rar|tar|less|svg|eot|woff|woff2|ttf|manifest)$';
         $regextoexclude='('.($includecustom?'':'custom|').'documents|conf|install|public\/test|Shared\/PCLZip|nusoap\/lib\/Mail|php\/example|php\/test|geoip\/sample.*\.php|ckeditor\/samples|ckeditor\/adapters)$';  // Exclude dirs
         $scanfiles = dol_dir_list(DOL_DOCUMENT_ROOT, 'files', 1, $regextoinclude, $regextoexclude);
 
