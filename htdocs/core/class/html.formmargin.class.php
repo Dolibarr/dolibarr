720146c45f071364ef6d547a44880276fa6768b6
VESSILLER <lvessiller@open-dsi.fr>
2019-12-23 15:37:12 +0100

    FIX class Facture undefined in displaying margin information

--- htdocs/core/class/html.formmargin.class.php
+++ htdocs/core/class/html.formmargin.class.php
@@ -63,6 +63,8 @@ class FormMargin
 	{
 		global $conf, $db;
 
+        require_once DOL_DOCUMENT_ROOT . '/compta/facture/class/facture.class.php';
+
 		// Default returned array
 		$marginInfos = array(
 				'pa_products' => 0,
