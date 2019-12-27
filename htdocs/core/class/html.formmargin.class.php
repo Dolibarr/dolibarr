ef04842eb4618e6903aff6bd016310ad5f8b8bf6
ptibogxiv <support@ptibogxiv.net>
2019-12-27 09:17:33 +0100

    Fix important error in 10.0.5

--- htdocs/core/class/html.formmargin.class.php
+++ htdocs/core/class/html.formmargin.class.php
@@ -98,7 +98,7 @@ class FormMargin
 
 			$pv = $line->total_ht;
 			$pa_ht = ($pv < 0 ? - $line->pa_ht : $line->pa_ht);      // We choosed to have line->pa_ht always positive in database, so we guess the correct sign
-			if ($object->type == Facture::TYPE_SITUATION) {
+			if ($object->element == 'facture' && $object->type == Facture::TYPE_SITUATION) {
 				$pa = $line->qty * $pa_ht * ($line->situation_percent / 100);
 			} else {
 				$pa = $line->qty * $pa_ht;
