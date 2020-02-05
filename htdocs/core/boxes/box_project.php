81ed70ffc7ac61844088e1e664324e5fdacb8e78
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2020-02-05 09:18:39 +0100

    Update box_project.php

--- htdocs/core/boxes/box_project.php
+++ htdocs/core/boxes/box_project.php
@@ -99,8 +99,8 @@ class box_project extends ModeleBoxes
 
             $sql = "SELECT p.rowid, p.ref, p.title, p.fk_statut, p.public";
             $sql.= " FROM ".MAIN_DB_PREFIX."projet as p";
-            $sql.= " WHERE p.fk_statut = 1"; // Only open projects
-			$sql.= " WHERE entity IN (".getEntity('project').")"; // Only current entity or severals if permission ok
+            $sql.= " WHERE entity IN (".getEntity('project').")"; // Only current entity or severals if permission ok
+			$sql.= " AND p.fk_statut = 1"; // Only open projects
             if (! $user->rights->projet->all->lire) $sql.= " AND p.rowid IN (".$projectsListId.")"; // public and assigned to, or restricted to company for external users
 
             $sql.= " ORDER BY p.datec DESC";
