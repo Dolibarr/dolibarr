03ee488d5f2350e4ef9e2bc358159e13936540ce
Laurent Destailleur <eldy@destailleur.fr>
2020-02-11 10:40:14 +0100

    Fix look and feel v11

--- htdocs/exports/export.php
+++ htdocs/exports/export.php
@@ -938,7 +938,7 @@ if ($step == 4 && $datatoexport)
     // List of filtered fiels
     if (isset($objexport->array_export_TypeFields[0]) && is_array($objexport->array_export_TypeFields[0]))
     {
-    	print '<tr><td width="25%">'.$langs->trans("FilteredFields").'</td>';
+    	print '<tr><td>'.$langs->trans("FilteredFields").'</td>';
     	$list = '';
     	if (!empty($array_filtervalue))
     	{
@@ -952,7 +952,7 @@ if ($step == 4 && $datatoexport)
     			}
     		}
     	}
-    	print '<td>'.(!empty($list) ? $list : $langs->trans("None")).'</td>';
+    	print '<td>'.(!empty($list) ? $list : '<span class="opacitymedium">'.$langs->trans("None").'</span>').'</td>';
     	print '</tr>';
     }
 
