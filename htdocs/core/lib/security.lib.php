f19f706c386e8dc4f123bea2b6e9453eeccb29d6
Regis Houssin <regis.houssin@inodbox.com>
2020-02-13 07:55:52 +0100

    FIX better check

--- htdocs/core/lib/security.lib.php
+++ htdocs/core/lib/security.lib.php
@@ -122,7 +122,7 @@ function dol_hash($chain, $type = '0')
 	}
 
 	// Salt value
-	if (! empty($conf->global->MAIN_SECURITY_SALT) && ( ! $type == '4' || ! $type == 'md5openldap')) $chain=$conf->global->MAIN_SECURITY_SALT.$chain;
+	if (! empty($conf->global->MAIN_SECURITY_SALT) && ($type !== '4' || $type !== 'md5openldap')) $chain = $conf->global->MAIN_SECURITY_SALT.$chain;
 
 	if ($type == '1' || $type == 'sha1') return sha1($chain);
 	elseif ($type == '2' || $type == 'sha1md5') return sha1(md5($chain));
