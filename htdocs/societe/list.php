c6b05040a309366fa10e7944871a8d68d69f183a
stickler-ci <support@stickler-ci.com>
2020-02-18 17:36:00 +0000

    Fixing style errors.

--- htdocs/societe/list.php
+++ htdocs/societe/list.php
@@ -10,7 +10,7 @@
  * Copyright (C) 2017       Rui Strecht      	    <rui.strecht@aliartalentos.com>
  * Copyright (C) 2017       Juanjo Menent      	    <jmenent@2byte.es>
  * Copyright (C) 2018       Nicolas ZABOURI         <info@inovea-conseil.com>
- 
+
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
