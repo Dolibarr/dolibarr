bebc37abe39fd6371250b1e2954bc4f392be9a66
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2020-02-19 08:54:10 +0100

    Update list.php

--- htdocs/compta/sociales/list.php
+++ htdocs/compta/sociales/list.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2004-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2009 Regis Houssin        <regis.houssin@inodbox.com>
  * Copyright (C) 2016      Frédéric France      <frederic.france@free.fr>
+ * Copyright (C) 2020      Pierre Ardoin     	<mapiolca@me.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
