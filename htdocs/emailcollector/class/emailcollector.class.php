d255c9566181295d54fa93d8909ec5d3175b9da8
oscim <dom@oscim.fr>
2020-02-21 09:04:24 +0100

    Update emailcollector.class.php
    For support multiple () and select last () in result regex 
    Sample: object.email=EXTRACT:BODY:E-mail(:([a-zA-Z0-9_@.-]*)\n|\r);

--- htdocs/emailcollector/class/emailcollector.class.php
+++ htdocs/emailcollector/class/emailcollector.class.php
@@ -812,9 +812,9 @@ class EmailCollector extends CommonObject
                         //var_dump($tmpproperty.' - '.$regexstring.' - '.$regexoptions.' - '.$sourcestring);
                         if (preg_match('/'.$regexstring.'/'.$regexoptions, $sourcestring, $regforval))
                         {
-                            //var_dump($regforval[1]);exit;
+                            //var_dump($regforval[sizeof($regforval)-1]);exit;
                             // Overwrite param $tmpproperty
-                            $object->$tmpproperty = isset($regforval[1]) ?trim($regforval[1]) : null;
+                            $object->$tmpproperty = isset($regforval[sizeof($regforval)-1]) ?trim($regforval[sizeof($regforval)-1]) : null;
                         }
                         else
                         {
@@ -1423,9 +1423,9 @@ class EmailCollector extends CommonObject
                                         //var_dump($regexstring);var_dump($sourcestring);
                                         if (preg_match('/'.$regexstring.'/ms', $sourcestring, $regforval))
                                         {
-                                            //var_dump($regforval[1]);exit;
+                                            //var_dump($regforval[sizeof($regforval)-1]);exit;
                                             // Overwrite param $tmpproperty
-                                            $nametouseforthirdparty = isset($regforval[1]) ?trim($regforval[1]) : null;
+                                            $nametouseforthirdparty = isset($regforval[sizeof($regforval)-1]) ?trim($regforval[sizeof($regforval)-1]) : null;
                                         }
                                         else
                                         {
