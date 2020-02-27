df87c77cd82c07f1526eb377a638bd73c29d711f
Laurent Destailleur <eldy@destailleur.fr>
2020-02-27 01:02:55 +0100

    css

--- htdocs/theme/eldy/global.inc.php
+++ htdocs/theme/eldy/global.inc.php
@@ -1638,9 +1638,9 @@ div.statusrefbis {
    	vertical-align: text-bottom;
 }
 img.photoref, div.photoref {
-	border: 1px solid #DDD;
-    -webkit-box-shadow: 0px 0px 6px #DDD;
-    box-shadow: 0px 0px 6px #DDD;
+	/* border: 1px solid #DDD; */
+    -webkit-box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2), 0px 0px 2px rgba(0, 0, 0, 0.1);
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2), 0px 0px 2px rgba(0, 0, 0, 0.1);
     padding: 4px;
 	height: 80px;
 	width: 80px;
