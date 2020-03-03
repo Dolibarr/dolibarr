b6bad0c93658e5fa5859c474e2cf5d9c4f1e3d53
ptibogxiv <support@ptibogxiv.net>
2020-03-03 13:40:26 +0100

    Update api_proposals.class.php

--- htdocs/comm/propal/class/api_proposals.class.php
+++ htdocs/comm/propal/class/api_proposals.class.php
@@ -1,6 +1,7 @@
 <?php
 /* Copyright (C) 2015   Jean-François Ferry     <jfefe@aternatik.fr>
  * Copyright (C) 2016   Laurent Destailleur     <eldy@users.sourceforge.net>
+ * Copyright (C) 2020   Thibault FOUCART   		<support@ptibogxiv.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
