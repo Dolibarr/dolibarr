84606f29c01bc338fa7ab04d070564f725d5c490
Laurent Destailleur <eldy@destailleur.fr>
2020-03-09 09:14:25 +0100

    Trans

--- htdocs/core/triggers/interface_50_modAgenda_ActionsAuto.class.php
+++ htdocs/core/triggers/interface_50_modAgenda_ActionsAuto.class.php
@@ -829,7 +829,7 @@ class InterfaceActionsAuto extends DolibarrTriggers
 		    $object->sendtoid=0;
 		}
 		// TODO Merge all previous cases into this generic one
-		else	// $action = TICKET_CREATE, TICKET_MODIFY, TICKET_DELETE, ...
+		else	// $action = BILL_DELETE, TICKET_CREATE, TICKET_MODIFY, TICKET_DELETE, ...
 		{
 		    // Note: We are here only if $conf->global->MAIN_AGENDA_ACTIONAUTO_action is on (tested at begining of this function). Key can be set in agenda setup if defined into c_action_trigger
 		    // Load translation files required by the page
