7b170ff4e605fef500aed59ca711999719db0112
Laurent Destailleur <eldy@destailleur.fr>
2020-03-10 16:23:55 +0100

    Work on migrate script

---
+++ scripts/website/migrate_news_joomla2dolibarr.php
@@ -0,0 +1,46 @@
+#!/usr/bin/env php
+<?php
+/* Copyright (C) 2007-2016 Laurent Destailleur <eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file scripts/website/migrate_newsèjoomla2dolibarr.php
+ * \ingroup scripts
+ * \brief Migrate news from a Joomla databse into a Dolibarr website
+ */
+
+$sapi_type = php_sapi_name();
+$script_file = basename(__FILE__);
+$path = __DIR__ . '/';
+
+// Test if batch mode
+if (substr($sapi_type, 0, 3) == 'cgi') {
+	echo "Error: You are using PHP for CGI. To execute " . $script_file . " from command line, you must use PHP for CLI mode.\n";
+	exit(- 1);
+}
+
+@set_time_limit(0); // No timeout for this script
+define('EVEN_IF_ONLY_LOGIN_ALLOWED', 1); // Set this define to 0 if you want to lock your script when dolibarr setup is "locked to admin user only".
+
+$error = 0;
+
+
+
+
+
+
+
+exit($error);
