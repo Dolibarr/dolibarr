b6137696060cfa243acb76e0390c0db67ab15b05
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2020-03-22 06:09:47 +0100

    Work on split module fournisseur

--- htdocs/core/menus/standard/auguria.lib.php
+++ htdocs/core/menus/standard/auguria.lib.php
@@ -381,7 +381,7 @@ function print_left_auguria_menu($db, $menu_array_before, $menu_array_after, &$t
 
 					// Must match array $sourceList defined into journals_list.php
 					if ($objp->nature == 2 && ! empty($conf->facture->enabled)) $nature="sells";
-					if ($objp->nature == 3 && ! empty($conf->fournisseur->enabled)) $nature="purchases";
+					if ($objp->nature == 3 && (!empty($conf->fournisseur->enabled) && empty($conf->global->MAIN_USE_NEW_SUPPLIERMOD) || !empty($conf->supplier_invoice->enabled))) $nature="purchases";
 					if ($objp->nature == 4 && ! empty($conf->banque->enabled)) $nature="bank";
 					if ($objp->nature == 5 && ! empty($conf->expensereport->enabled)) $nature="expensereports";
 					if ($objp->nature == 1) $nature="various";
