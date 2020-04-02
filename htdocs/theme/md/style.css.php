a743110f03063164bad7da85cc5224c01bad45d7
altairis <contact@altairis.fr>
2020-04-02 10:44:24 +0200

    FIX md stylesheet to be included by external modules like eldy

--- htdocs/theme/md/style.css.php
+++ htdocs/theme/md/style.css.php
@@ -48,7 +48,7 @@ if (defined('THEME_ONLY_CONSTANT')) return;
 session_cache_limiter('public');
 
 
-require_once '../../main.inc.php';
+require_once __DIR__.'/../../main.inc.php'; // __DIR__ allow this script to be included in custom themes
 require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
 
 // Load user to have $user->conf loaded (not done into main because of NOLOGIN constant defined)
