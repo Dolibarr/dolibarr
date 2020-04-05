5ca9c71449e750bd5d1a72663581ed73f31a41b0
Laurent Destailleur <eldy@destailleur.fr>
2020-04-05 12:05:27 +0200

    css

--- htdocs/adherents/subscription/list.php
+++ htdocs/adherents/subscription/list.php
@@ -338,7 +338,7 @@ if (!empty($arrayfields['t.libelle']['checked']))
 if (!empty($arrayfields['d.bank']['checked']))
 {
 	print '<td class="liste_titre">';
-	$form->select_comptes($search_account, 'search_account', 0, '', 1);
+	$form->select_comptes($search_account, 'search_account', 0, '', 1, '', 0, 'maxwidth100');
 	print '</td>';
 }
 
@@ -489,7 +489,7 @@ while ($i < min($num, $limit))
 	// Banque
 	if (!empty($arrayfields['d.bank']['checked']))
 	{
-		print "<td>";
+		print '<td class="tdmaxoverflow150">';
 		if ($obj->fk_account > 0)
 		{
 			$accountstatic->id = $obj->fk_account;
