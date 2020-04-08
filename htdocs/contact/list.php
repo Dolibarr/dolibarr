da1b8f8c44224764d5c2ba680589ed5ee822962a
Anthony Berton <34568357+bb2a@users.noreply.github.com>
2020-04-08 08:56:10 +0200

    Update list.php

--- htdocs/contact/list.php
+++ htdocs/contact/list.php
@@ -153,7 +153,12 @@ $fieldstosearchall = array(
 	'p.firstname'=>'Firstname',
 	'p.email'=>'EMail',
 	's.nom'=>"ThirdParty",
+	'p.zip'=>"Zip",
+	'p.town'=>"Town",
 	'p.phone'=>"Phone",
+	'p.phone_perso'=>"PhonePerso",
+	'p.phone_mobile'=>"PhoneMobile",
+	'p.fax'=>"Fax",
     'p.note_public'=>"NotePublic",
     'p.note_private'=>"NotePrivate",
 );
