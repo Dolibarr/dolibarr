2887ae3f00ea65acae83f0ec8f234c2fecd7b046
oscim <dom@oscim.fr>
2020-04-10 08:54:39 +0200

    Update llx_societe_rib.key.sql
    fix key in table

--- htdocs/install/mysql/tables/llx_societe_rib.key.sql
+++ htdocs/install/mysql/tables/llx_societe_rib.key.sql
@@ -18,3 +18,5 @@
 
 
 ALTER TABLE llx_societe_rib ADD UNIQUE INDEX uk_societe_rib(label, fk_soc);
+
+ALTER TABLE llx_societe_rib ADD CONSTRAINT llx_societe_rib_fk_societe FOREIGN KEY (fk_soc) REFERENCES llx_societe(rowid);
