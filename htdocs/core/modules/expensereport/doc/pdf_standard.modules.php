3e7f0b364e97995dcf82e5e548da48c737a9f6f1
Laurent Destailleur <eldy@destailleur.fr>
2020-04-17 10:22:08 +0200

    FIX #13611

--- htdocs/core/modules/expensereport/doc/pdf_standard.modules.php
+++ htdocs/core/modules/expensereport/doc/pdf_standard.modules.php
@@ -745,7 +745,7 @@ class pdf_standard extends ModeleExpenseReport
    		$pdf->SetXY($posx, $posy);
    		$pdf->SetFont('', 'B', $default_font_size + 2);
    		$pdf->SetTextColor(111, 81, 124);
-		$pdf->MultiCell($this->page_largeur - $this->marge_droite - $posx, 3, $object->getLibStatut(0), '', 'R');
+   		$pdf->MultiCell($this->page_largeur - $this->marge_droite - $posx, 3, $outputlangs->transnoentities($object->statuts_short[$object->status]), '', 'R');
 
 		if ($showaddress) {
 			// Sender properties
