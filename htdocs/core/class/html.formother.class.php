43f4c5ac962d3a99b33f9cab2410c6d323a8a411
gauthier <gauthier.verdol@atm-consulting.fr>
2020-04-22 15:29:54 +0200

    FIX : model export list must be sorted by label

--- htdocs/core/class/html.formother.class.php
+++ htdocs/core/class/html.formother.class.php
@@ -75,7 +75,7 @@ class FormOther
         $sql.= " FROM ".MAIN_DB_PREFIX."export_model";
         $sql.= " WHERE type = '".$type."'";
 		if (!empty($fk_user)) $sql.=" AND fk_user IN (0, ".$fk_user.")";  // An export model
-        $sql.= " ORDER BY rowid";
+        $sql.= " ORDER BY label";
         $result = $this->db->query($sql);
         if ($result)
         {
