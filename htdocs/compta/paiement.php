3d78f2e8095fe78da526e5c410339679abf5444c
oscim <dom@oscim.fr>
2020-05-05 08:59:03 +0200

    Update paiement.php
    mass action payment

--- htdocs/compta/paiement.php
+++ htdocs/compta/paiement.php
@@ -63,6 +63,12 @@ if ($user->socid > 0)
 {
     $socid = $user->socid;
 }
+// list of limit id for invoice
+$facids		= GETPOST('facids','alpha');
+if(empty($facid) && !empty($facids)) {
+	$r =explode(',', $facids); 
+	$facid = $r[0]; 
+}
 
 $object = new Facture($db);
 
@@ -565,6 +571,11 @@ if ($action == 'create' || $action == 'confirm_paiement' || $action == 'add_paie
         {
             $sql .= ' AND type = 2'; // If paying back a credit note, we show all credit notes
         }
+		
+		// limits of invoice ids 
+		if(!empty($facids) )
+			$sql .= ' AND f.rowid IN('.$facids.')';
+		
         // Sort invoices by date and serial number: the older one comes first
         $sql .= ' ORDER BY f.datef ASC, f.ref ASC';
 
