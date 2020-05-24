6158e41822145b846baf1109adbc431046ed6698
Laurent Destailleur <eldy@destailleur.fr>
2020-05-24 23:44:04 +0200

    CSS

--- htdocs/takepos/admin/receipt.php
+++ htdocs/takepos/admin/receipt.php
@@ -82,7 +82,7 @@ llxHeader('', $langs->trans("CashDeskSetup"));
 $linkback = '<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToModuleList").'</a>';
 print load_fiche_titre($langs->trans("CashDeskSetup").' (TakePOS)', $linkback, 'title_setup');
 $head = takepos_prepare_head();
-dol_fiche_head($head, 'receipt', 'TakePOS', -1);
+dol_fiche_head($head, 'receipt', 'TakePOS', -1, 'cash-register');
 
 print '<form action="'.$_SERVER["PHP_SELF"].'?terminal='.(empty($terminal) ? 1 : $terminal).'" method="post">';
 print '<input type="hidden" name="token" value="'.newToken().'">';
