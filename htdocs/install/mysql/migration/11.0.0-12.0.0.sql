5a9d968a75a88b9fcfea9b0fc954da11004859d2
Laurent Destailleur <eldy@destailleur.fr>
2020-05-29 00:02:03 +0200

    Fix MAIN_BUGTRACK_ENABLELINK and MAIN_HELP_DISABLELINK setting

--- htdocs/install/mysql/migration/11.0.0-12.0.0.sql
+++ htdocs/install/mysql/migration/11.0.0-12.0.0.sql
@@ -67,8 +67,7 @@ ALTER TABLE llx_facturedet_rec_extrafields ADD INDEX idx_facturedet_rec_extrafie
 ALTER TABLE llx_facture_rec MODIFY COLUMN titre varchar(200) NOT NULL;
 
 -- This var is per entity now, so we remove const if global if exists
-delete from llx_const where name = 'PROJECT_HIDE_TASKS' and entity = 0;
-
+delete from llx_const where name in ('PROJECT_HIDE_TASKS', 'MAIN_BUGTRACK_ENABLELINK', 'MAIN_HELP_DISABLELINK') and entity = 0;
 
 -- For v12
 
