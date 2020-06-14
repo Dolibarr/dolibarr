36ec9703ba9f35d6158b04269722e769ab551fc3
DEMAREST Maxime (Indelog) <maxime@indelog.fr>
2020-06-14 15:28:04 +0200

    Forgot copyright in Socite class file

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -16,6 +16,7 @@
  * Copyright (C) 2018	    Philippe Grand	        <philippe.grand@atoo-net.com>
  * Copyright (C) 2019-2020  Josep Lluís Amador      <joseplluis@lliuretic.cat>
  * Copyright (C) 2019       Frédéric France         <frederic.france@netlogic.fr>
+ * Copyright (C) 2020       Maxime Demarest         <maxime@indlog.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
