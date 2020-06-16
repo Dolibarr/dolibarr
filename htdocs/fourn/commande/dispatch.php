f32f1778e5d7c029476f44391df4119a9b2acc3f
bahfir abbes <bafbes@gmail.com>
2020-06-16 01:34:58 +0100

    Fix:Erroneous use of used variable $name

--- htdocs/fourn/commande/dispatch.php
+++ htdocs/fourn/commande/dispatch.php
@@ -1131,8 +1131,29 @@ if ($id > 0 || !empty($ref)) {
 				print '<td>'.dol_print_date($db->jdate($objp->date_delivery), 'day').'</td>';
 
 				if (!empty($conf->productbatch->enabled)) {
-					print '<td class="dispatch_batch_number">'.$objp->batch.'</td>';
-					print '<td class="dispatch_dluo">'.dol_print_date($db->jdate($objp->eatby), 'day').'</td>';
+                    $sql2 = "select pl.rowid from ".MAIN_DB_PREFIX."product_lot pl";
+                    $sql2 .= " join ".MAIN_DB_PREFIX."product_batch pb on pl.batch=pb.batch";
+                    $sql2 .= " join ".MAIN_DB_PREFIX."product_stock ps on pb.fk_product_stock=ps.rowid";
+                    $sql2 .= " where ps.fk_product=$objp->pid and ps.fk_entrepot=$objp->warehouse_id";
+                    $resql2 = $db->query($sql2);
+                    if ($resql2) {
+                        $num2 = $db->num_rows($resql2);
+                        if ($num2 > 0) {
+                            $objp2 = $db->fetch_object($resql2);
+
+                        }
+                    }
+					print '<td class="dispatch_batch_number">';
+                    if(!empty($objp2)){
+                        include_once DOL_DOCUMENT_ROOT.'/product/stock/class/productlot.class.php';
+                        $lot=new Productlot($db);
+                        $lot->fetch($objp2->rowid);
+                        print $lot->getNomUrl(1);
+                    }
+                    else print $objp->batch;
+                    print '</td>';
+
+                    print '<td class="dispatch_dluo">'.dol_print_date($db->jdate($objp->eatby), 'day').'</td>';
 					print '<td class="dispatch_dlc">'.dol_print_date($db->jdate($objp->sellby), 'day').'</td>';
 				}
 
