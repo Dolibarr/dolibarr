781f571a155fda40886cbbee75e9a6e3d1c2306c
Laurent Destailleur <eldy@destailleur.fr>
2020-06-27 14:38:40 +0200

    Fix template

--- htdocs/modulebuilder/template/core/modules/modMyModule.class.php
+++ htdocs/modulebuilder/template/core/modules/modMyModule.class.php
@@ -276,7 +276,7 @@ class modMyModule extends DolibarrModules
 			'langs'=>'mymodule@mymodule', // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
 			'position'=>1000 + $r,
 			'enabled'=>'$conf->mymodule->enabled', // Define condition to show or hide menu entry. Use '$conf->mymodule->enabled' if entry must be visible if module is enabled.
-			'perms'=>'$user->rights->mymodule->myobject->read', // Use 'perms'=>'$user->rights->mymodule->level1->level2' if you want your menu with a permission rules
+			'perms'=>'1', // Use 'perms'=>'$user->rights->mymodule->myobject->read' if you want your menu with a permission rules
 			'target'=>'',
 			'user'=>2, // 0=Menu for internal users, 1=external users, 2=both
 		);
