6553c40bea8f467b58d79945e0b1b3bde74181d3
gauthier <gauthier.verdol@atm-consulting.fr>
2020-06-30 11:23:55 +0200

    FIX : create a deposit with amount using comma didn't work

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -1255,7 +1255,8 @@ if (empty($reshook))
 						if ($_POST['type'] == Facture::TYPE_DEPOSIT)
 						{
 							$typeamount = GETPOST('typedeposit', 'alpha');
-							$valuedeposit = GETPOST('valuedeposit', 'int');
+							$valuedeposit = GETPOST('valuedeposit');
+							$valuedeposit = price2num($valuedeposit, 'MU');
 
 							$amountdeposit = array();
 							if (!empty($conf->global->MAIN_DEPOSIT_MULTI_TVA))
