9e4d7627f3a4aef826191fb4b4f2aa821bad198c
andreubisquerra <jove@bisquerra.com>
2020-07-01 22:55:00 +0200

    Fix travis

--- htdocs/core/lib/takepos.lib.php
+++ htdocs/core/lib/takepos.lib.php
@@ -37,7 +37,7 @@ function takepos_prepare_head()
 	$head[$h][1] = $langs->trans("Parameters");
 	$head[$h][2] = 'setup';
 	$h++;
-	
+
 	$head[$h][0] = DOL_URL_ROOT.'/takepos/admin/appearance.php';
 	$head[$h][1] = $langs->trans("Appearance");
 	$head[$h][2] = 'appearance';
