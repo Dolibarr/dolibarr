135880131655385536c5ba2037b9b67df0d73e43
Philippe GRAND <philippe.grand@atoo-net.com>
2020-07-11 15:08:00 +0200

    Minimum version of PHP required by module

--- htdocs/core/modules/societe/doc/doc_generic_odt.modules.php
+++ htdocs/core/modules/societe/doc/doc_generic_odt.modules.php
@@ -44,9 +44,9 @@ class doc_generic_odt extends ModeleThirdPartyDoc
 
 	/**
      * @var array Minimum version of PHP required by module.
-     * e.g.: PHP ≥ 5.5 = array(5, 5)
+     * e.g.: PHP ≥ 5.6 = array(5, 6)
      */
-	public $phpmin = array(5, 5);
+	public $phpmin = array(5, 6);
 
 
 	/**
