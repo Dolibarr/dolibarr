83d1ff2a6cfaf6f1935a95eadfc32597cdfb5ced
Philippe GRAND <philippe.grand@atoo-net.com>
2020-08-03 09:25:17 +0200

    Change comment to fixe a lot of undefined method

--- htdocs/core/modules/expedition/doc/pdf_merou.modules.php
+++ htdocs/core/modules/expedition/doc/pdf_merou.modules.php
@@ -438,7 +438,7 @@ class pdf_merou extends ModelePdfExpedition
 	/**
 	 *   Show table for lines
 	 *
-	 *   @param		PDF			$pdf     		Object PDF
+	 *   @param		TCPDF		&$pdf     		Object PDF
 	 *   @param		string		$tab_top		Top position of table
 	 *   @param		string		$tab_height		Height of table (rectangle)
 	 *   @param		int			$nexY			Y
@@ -480,7 +480,7 @@ class pdf_merou extends ModelePdfExpedition
 	/**
 	 *   	Show footer of page. Need this->emetteur object
      *
-	 *   	@param	PDF			$pdf     			PDF
+	 *   	@param	TCPDF		&$pdf     			PDF
 	 * 		@param	Object		$object				Object to show
 	 *      @param	Translate	$outputlangs		Object lang for output
 	 *      @param	int			$hidefreetext		1=Hide free text
@@ -509,7 +509,7 @@ class pdf_merou extends ModelePdfExpedition
 	/**
 	 *  Show top header of page.
 	 *
-	 *  @param	PDF			$pdf     		Object PDF
+	 *  @param	TCPDF		&$pdf     		Object PDF
 	 *  @param  Object		$object     	Object to show
 	 *  @param  int	    	$showaddress    0=no, 1=yes
 	 *  @param  Translate	$outputlangs	Object lang for output
