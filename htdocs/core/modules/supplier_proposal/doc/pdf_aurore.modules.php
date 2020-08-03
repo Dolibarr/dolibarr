d6c84ef5ce5aca5acc41aef504720cb9c41fd4a9
Philippe GRAND <philippe.grand@atoo-net.com>
2020-08-03 10:02:25 +0200

    Change comment to fixe a lot of undefined method

--- htdocs/core/modules/supplier_proposal/doc/pdf_aurore.modules.php
+++ htdocs/core/modules/supplier_proposal/doc/pdf_aurore.modules.php
@@ -691,7 +691,7 @@ class pdf_aurore extends ModelePDFSupplierProposal
 	/**
 	 *  Show payments table
 	 *
-     *  @param	PDF			$pdf           Object PDF
+     *  @param	TCPDF		&$pdf           Object PDF
      *  @param  Object		$object         Object proposal
      *  @param  int			$posy           Position y in PDF
      *  @param  Translate	$outputlangs    Object langs for output
@@ -707,7 +707,7 @@ class pdf_aurore extends ModelePDFSupplierProposal
 	/**
 	 *   Show miscellaneous information (payment mode, payment term, ...)
 	 *
-	 *   @param		PDF			$pdf     		Object PDF
+	 *   @param		TCPDF		&$pdf     		Object PDF
 	 *   @param		Object		$object			Object to show
 	 *   @param		int			$posy			Y
 	 *   @param		Translate	$outputlangs	Langs object
@@ -874,7 +874,7 @@ class pdf_aurore extends ModelePDFSupplierProposal
 	/**
 	 *	Show total to pay
 	 *
-	 *	@param	PDF			$pdf            Object PDF
+	 *	@param	TCPDF		&$pdf            Object PDF
 	 *	@param  Facture		$object         Object invoice
 	 *	@param  int			$deja_regle     Montant deja regle
 	 *	@param	int			$posy			Position depart
@@ -1145,7 +1145,7 @@ class pdf_aurore extends ModelePDFSupplierProposal
 	/**
 	 *   Show table for lines
 	 *
-	 *   @param		PDF			$pdf     		Object PDF
+	 *   @param		TCPDF		&$pdf     		Object PDF
 	 *   @param		string		$tab_top		Top position of table
 	 *   @param		string		$tab_height		Height of table (rectangle)
 	 *   @param		int			$nexY			Y (not used)
@@ -1251,7 +1251,7 @@ class pdf_aurore extends ModelePDFSupplierProposal
 	/**
 	 *  Show top header of page.
 	 *
-	 *  @param	PDF			$pdf     		Object PDF
+	 *  @param	TCPDF		&$pdf     		Object PDF
 	 *  @param  Object		$object     	Object to show
 	 *  @param  int	    	$showaddress    0=no, 1=yes
 	 *  @param  Translate	$outputlangs	Object lang for output
@@ -1463,7 +1463,7 @@ class pdf_aurore extends ModelePDFSupplierProposal
     /**
      *  Show footer of page. Need this->emetteur object
      *
-     *  @param  PDF			$pdf     			PDF
+     *  @param  TCPDF		&$pdf     			PDF
      *  @param  Object		$object				Object to show
      *  @param  Translate	$outputlangs		Object lang for output
      *  @param  int			$hidefreetext		1=Hide free text
