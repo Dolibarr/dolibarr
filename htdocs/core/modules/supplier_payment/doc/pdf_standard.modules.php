6fa3e8df4e6bf6a0cf909b2af6a79c2765b8b049
Philippe GRAND <philippe.grand@atoo-net.com>
2020-08-03 10:00:20 +0200

    Change comment to fixe a lot of undefined method

--- htdocs/core/modules/supplier_payment/doc/pdf_standard.modules.php
+++ htdocs/core/modules/supplier_payment/doc/pdf_standard.modules.php
@@ -508,7 +508,7 @@ class pdf_standard extends ModelePDFSuppliersPayments
 	/**
 	 *	Show total to pay
 	 *
-	 *	@param	PDF				$pdf			Object PDF
+	 *	@param	TCPDF			&$pdf			Object PDF
 	 *	@param  PaiementFourn	$object         Object PaiementFourn
 	 *	@param	int				$posy			Position depart
 	 *	@param	Translate		$outputlangs	Objet langs
@@ -567,7 +567,7 @@ class pdf_standard extends ModelePDFSuppliersPayments
 	/**
 	 *   Show table for lines
 	 *
-	 *   @param		PDF			$pdf     		Object PDF
+	 *   @param		TCPDF		&$pdf     		Object PDF
 	 *   @param		integer		$tab_top		Top position of table
 	 *   @param		integer		$tab_height		Height of table (rectangle)
 	 *   @param		int			$nexY			Y (not used)
@@ -612,7 +612,7 @@ class pdf_standard extends ModelePDFSuppliersPayments
 	/**
 	 *  Show top header of page.
 	 *
-	 *  @param	PDF			$pdf     		Object PDF
+	 *  @param	TCPDF		&$pdf     		Object PDF
 	 *  @param  FactureFournisseur		$object     	Object to show
 	 *  @param  int	    	$showaddress    0=no, 1=yes
 	 *  @param  Translate	$outputlangs	Object lang for output
@@ -788,7 +788,7 @@ class pdf_standard extends ModelePDFSuppliersPayments
 	/**
 	 *   	Show footer of page. Need this->emetteur object
      *
-	 *   	@param	PDF			$pdf     			PDF
+	 *   	@param	TCPDF		&$pdf     			PDF
 	 * 		@param	FactureFournisseur		$object				Object to show
 	 *      @param	Translate	$outputlangs		Object lang for output
 	 *      @param	int			$hidefreetext		1=Hide free text
