9a5d0ab0a293aa859f5fe79232bcbc7d6289c68f
Laurent Destailleur <eldy@destailleur.fr>
2020-08-04 13:48:11 +0200

    FIX #14236

--- htdocs/adherents/admin/adherent_emails.php
+++ htdocs/adherents/admin/adherent_emails.php
@@ -93,6 +93,7 @@ if ($action == 'update' || $action == 'add')
 	$constnote = (GETPOSTISSET('constnote_'.$constname) ? GETPOST('constnote_'.$constname, 'none') : GETPOST('constnote'));
 
 	$typetouse = empty($oldtypetonewone[$consttype]) ? $consttype : $oldtypetonewone[$consttype];
+	$constvalue = preg_replace('/:member$/', '', $constvalue);
 
 	$res = dolibarr_set_const($db, $constname, $constvalue, $typetouse, 0, $constnote, $conf->entity);
 
@@ -106,26 +107,6 @@ if ($action == 'update' || $action == 'add')
 	{
 		setEventMessages($langs->trans("Error"), null, 'errors');
 	}
-}
-
-// Action to enable a submodule of the adherent module
-if ($action == 'set')
-{
-    $result = dolibarr_set_const($db, GETPOST('name', 'alpha'), GETPOST('value'), '', 0, '', $conf->entity);
-    if ($result < 0)
-    {
-        print $db->error();
-    }
-}
-
-// Action to disable a submodule of the adherent module
-if ($action == 'unset')
-{
-    $result = dolibarr_del_const($db, GETPOST('name', 'alpha'), $conf->entity);
-    if ($result < 0)
-    {
-        print $db->error();
-    }
 }
 
 
