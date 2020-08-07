9876e37309f0c9c4d1ca4d067e02a6533258e1a7
Laurent Destailleur <eldy@destailleur.fr>
2020-08-07 15:47:02 +0200

    A little perf and memory enhancement when building category tree

--- htdocs/categories/class/categorie.class.php
+++ htdocs/categories/class/categorie.class.php
@@ -1213,7 +1213,8 @@ class Categorie extends CommonObject
 		//print 'Result for id_categ='.$id_categ.' : '.$this->cats[$id_categ]['fullpath'].'<br>'."\n";
 
 		// We count number of _ to have level
-		$this->cats[$id_categ]['level'] = dol_strlen(preg_replace('/[^_]/i', '', $this->cats[$id_categ]['fullpath']));
+		$nbunderscore = substr_count($this->cats[$id_categ]['fullpath'], '_');
+		$this->cats[$id_categ]['level'] = ($nbunderscore ? $nbunderscore : null);
 
         return;
     }
