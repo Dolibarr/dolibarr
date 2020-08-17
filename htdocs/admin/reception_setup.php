75d368b7069cede179c7825ba12174a9a80e944c
Laurent Destailleur <eldy@destailleur.fr>
2020-08-17 14:08:52 +0200

    Better translation of option

--- htdocs/admin/reception_setup.php
+++ htdocs/admin/reception_setup.php
@@ -487,7 +487,7 @@ print "</td></tr>\n";
 */
 print '</table>';
 
-print '<div class="center"><input type="submit" class="button" value="'.$langs->trans("Modify").'"></div>';
+//print '<div class="center"><input type="submit" class="button" value="'.$langs->trans("Modify").'"></div>';
 
 print '</form>';
 
