8e70a731d37d08f648adcefd169b1f8ddedf5b49
Laurent Destailleur <eldy@destailleur.fr>
2020-08-29 04:42:14 +0200

    doc

--- htdocs/install/mysql/tables/llx_product_attribute_combination.sql
+++ htdocs/install/mysql/tables/llx_product_attribute_combination.sql
@@ -14,13 +14,14 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program. If not, see <https://www.gnu.org/licenses/>.
 --
+-- Table to store all product variants of a parent product
 -- ============================================================================
 
 CREATE TABLE llx_product_attribute_combination
 (
   rowid INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  fk_product_parent INTEGER NOT NULL,
-  fk_product_child INTEGER NOT NULL,
+  fk_product_parent INTEGER NOT NULL,					-- id of product id that is parent product
+  fk_product_child INTEGER NOT NULL,					-- id of product id that is variant (child) product
   variation_price DOUBLE(24,8) NOT NULL,
   variation_price_percentage INTEGER NULL,
   variation_weight REAL NOT NULL,
