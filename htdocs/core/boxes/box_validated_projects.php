043d46453531fb5e78fb53cad4276a485ffb6768
Adrien Raze <adrien.raze@atm-consulting.fr>
2020-09-02 09:15:15 +0200

    FIX : suppression lien nbTâche

--- htdocs/core/boxes/box_validated_projects.php
+++ htdocs/core/boxes/box_validated_projects.php
@@ -172,7 +172,6 @@ class box_validated_projects extends ModeleBoxes
                         'td' => 'class="center"',
                         'text' => $objp->taskNumber."&nbsp;".$langs->trans("Tasks"),
 						'asis' => 1,
-						'url' => DOL_URL_ROOT.'/custom/cligp/tasks/projettasks.php?id='.$objp->rowid.'&search_timespend==0&search_user_id='.$user->id,
                     );
                     $i++;
                 }
