efdfc2b68255813a070c59e82190f3e25a0f2146
stickler-ci <support@stickler-ci.com>
2020-09-07 08:23:12 +0000

    Fixing style errors.

--- htdocs/debugbar/class/autoloader.php
+++ htdocs/debugbar/class/autoloader.php
@@ -4,7 +4,7 @@
  * Simple autoloader, so we don't need Composer just for this.
  */
 
-spl_autoload_register(function($class) {
+spl_autoload_register(function ($class) {
     if (preg_match('/^DebugBar/', $class) || preg_match('/^'.preg_quote('Psr\Log', '/').'/', $class)) {
         $file = DOL_DOCUMENT_ROOT.'/includes/'.str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
         //var_dump($class.' - '.file_exists($file).' - '.$file);
