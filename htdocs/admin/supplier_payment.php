070f9a0c89ae52008790146c521eb988d35301f2
Frédéric FRANCE <frederic34@users.noreply.github.com>
2020-09-15 16:32:32 +0200

    uniformise scan_dir to scandir in admin supplier payments

--- htdocs/admin/supplier_payment.php
+++ htdocs/admin/supplier_payment.php
@@ -35,7 +35,7 @@ if (! $user->admin) accessforbidden();
 $action = GETPOST('action', 'alpha');
 $value = GETPOST('value', 'alpha');
 $label = GETPOST('label', 'alpha');
-$scandir = GETPOST('scan_dir', 'alpha');
+$scandir = GETPOST('scandir', 'alpha');
 $type='supplier_payment';
 
 
@@ -268,7 +268,7 @@ foreach ($dirmodels as $reldir)
                             }
                             else
                             {
-                                print '<a class="reposition" href="'.$_SERVER["PHP_SELF"].'?action=setmod&value='.preg_replace('/\.php$/', '', $file).'&scan_dir='.$module->scandir.'&label='.urlencode($module->name).'" alt="'.$langs->trans("Default").'">'.img_picto($langs->trans("Disabled"), 'switch_off').'</a>';
+                                print '<a class="reposition" href="'.$_SERVER["PHP_SELF"].'?action=setmod&value='.preg_replace('/\.php$/', '', $file).'&scandir='.$module->scandir.'&label='.urlencode($module->name).'" alt="'.$langs->trans("Default").'">'.img_picto($langs->trans("Disabled"), 'switch_off').'</a>';
                             }
                             print '</td>';
 
