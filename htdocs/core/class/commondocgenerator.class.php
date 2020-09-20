64a945652c289562f8737b498cdf284401c9649f
stickler-ci <support@stickler-ci.com>
2020-09-20 15:10:37 +0000

    Fixing style errors.

--- htdocs/core/class/commondocgenerator.class.php
+++ htdocs/core/class/commondocgenerator.class.php
@@ -1286,7 +1286,7 @@ abstract class CommonDocGenerator
         if (!empty($fields))
         {
             // Sort extrafields by rank
-            uasort($fields, function($a, $b) {
+            uasort($fields, function ($a, $b) {
                 return  ($a->rank > $b->rank) ? 1 : -1;
 			});
 
