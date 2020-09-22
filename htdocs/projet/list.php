72a9cf88206a0adb7a92bf84957a9f2929ab124a
Laurent Destailleur <eldy@destailleur.fr>
2020-09-22 16:03:36 +0200

    FIX #14751

--- htdocs/projet/list.php
+++ htdocs/projet/list.php
@@ -461,7 +461,6 @@ print '<input type="hidden" name="formfilteraction" id="formfilteraction" value=
 print '<input type="hidden" name="action" value="list">';
 print '<input type="hidden" name="sortfield" value="'.$sortfield.'">';
 print '<input type="hidden" name="sortorder" value="'.$sortorder.'">';
-print '<input type="hidden" name="type" value="'.$type.'">';
 print '<input type="hidden" name="contextpage" value="'.$contextpage.'">';
 
 // Show description of content
