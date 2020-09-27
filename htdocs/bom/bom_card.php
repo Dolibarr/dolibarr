57b7666bf163c064d21583c043b2e374f72a5c66
Laurent Destailleur <eldy@destailleur.fr>
2020-09-27 22:20:24 +0200

    FIX #14326

--- htdocs/bom/bom_card.php
+++ htdocs/bom/bom_card.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2017       Laurent Destailleur     <eldy@users.sourceforge.net>
+/* Copyright (C) 2017-2020  Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2019       Frédéric France         <frederic.france@netlogic.fr>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -183,6 +183,8 @@ if (empty($reshook))
     			unset($_POST['qty']);
     			unset($_POST['qty_frozen']);
     		    unset($_POST['disable_stock_change']);
+
+    		    $object->fetchLines();
     		}
 		}
 	}
