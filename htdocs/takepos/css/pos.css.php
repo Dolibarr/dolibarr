ac897b5b4d62750f94e8d2e8ff518789fb0b1092
Laurent Destailleur <eldy@destailleur.fr>
2020-09-29 17:06:08 +0200

    Fix css

--- htdocs/takepos/css/pos.css.php
+++ htdocs/takepos/css/pos.css.php
@@ -716,7 +716,8 @@ div#moreinfo, div#infowarehouse {
   width: 40%;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
   animation-name: animatetop;
-  animation-duration: 0.4s
+  animation-duration: 0.4s;
+  min-width: 200px;
 }
 
 @keyframes animatetop {
