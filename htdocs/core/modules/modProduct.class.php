03ede719890d03f2f1a8665056bef35330800abc
Laurent Destailleur <eldy@destailleur.fr>
2020-10-02 16:15:24 +0200

    Clean code

--- htdocs/core/modules/modProduct.class.php
+++ htdocs/core/modules/modProduct.class.php
@@ -51,7 +51,7 @@ class modProduct extends DolibarrModules
 		$this->numero = 50;
 
 		$this->family = "products";
-		$this->module_position = '25';
+		$this->module_position = '26';
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i', '', get_class($this));
 		$this->description = "Product management";
