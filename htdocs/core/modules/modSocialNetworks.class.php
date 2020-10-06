04f8de41884339f9686edb33bc10298b9c66a80d
Laurent Destailleur <eldy@destailleur.fr>
2020-10-06 17:59:31 +0200

    Remove dependency

--- htdocs/core/modules/modSocialNetworks.class.php
+++ htdocs/core/modules/modSocialNetworks.class.php
@@ -66,7 +66,7 @@ class modSocialNetworks extends DolibarrModules
 
         // Dependencies
         $this->hidden = !empty($conf->global->MODULE_SOCIALNETWORKS_DISABLED); // A condition to hide module
-		$this->depends = array('modSociete'); // List of module class names as string that must be enabled if this module is enabled
+		$this->depends = array(); // List of module class names as string that must be enabled if this module is enabled
 		$this->requiredby = array(); // List of module ids to disable if this one is disabled
 		$this->conflictwith = array(); // List of module class names as string this module is in conflict with
 		$this->phpmin = array(5, 4); // Minimum version of PHP required by module
