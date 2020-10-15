b9cbd84d4ebd380fb6e9850c4b08dbba70dfb195
ATM john <john.botella@atm-consulting.fr>
2020-10-15 09:30:48 +0200

    Fix page break

--- htdocs/includes/tecnickcom/tcpdf/include/tcpdf_fonts.php
+++ htdocs/includes/tecnickcom/tcpdf/include/tcpdf_fonts.php
@@ -1149,7 +1149,7 @@ class TCPDF_FONTS {
 								$subsetglyphs[$g] = true;
 							}
 						}
-					}	
+					}
 					break;
 				}
 				case 6: { // Format 6: Trimmed table mapping
@@ -1664,6 +1664,7 @@ class TCPDF_FONTS {
 	 * @public static
 	 */
 	public static function unichr($c, $unicode=true) {
+		$c = intval($c);
 		if (!$unicode) {
 			return chr($c);
 		} elseif ($c <= 0x7F) {
