693e345cab901e9b26e4d64802ed8e513a862bc4
Louis Carrese <louis@louiscarrese.fr>
2020-10-20 14:34:17 +0200

    CLOSE #15065 Fix formatting
    Had spaces instead of tabs...

--- htdocs/core/lib/pdf.lib.php
+++ htdocs/core/lib/pdf.lib.php
@@ -1278,12 +1278,11 @@ function pdf_getlinedesc($object, $i, $outputlangs, $hideref = 0, $hidedesc = 0,
 
 	// Description short of product line
 	$libelleproduitservice = $label;
-        if (!empty($libelleproduitservice) && !empty($conf->global->PDF_BOLD_PRODUCT_LABEL))
-        {
-            $libelleproduitservice = '<b>'.$libelleproduitservice.'</b>';
-        }
+	if (!empty($libelleproduitservice) && !empty($conf->global->PDF_BOLD_PRODUCT_LABEL)) {
+		$libelleproduitservice = '<b>' . $libelleproduitservice . '</b>';
+	}
 
-        // Description long of product line
+	// Description long of product line
 	if (!empty($desc) && ($desc != $label))
 	{
 		if ($libelleproduitservice && empty($hidedesc))
