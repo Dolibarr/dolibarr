7605cb18718086ea270000fa5724692f2e554ff9
Laurent Destailleur <eldy@destailleur.fr>
2020-10-21 19:54:28 +0200

    Update facture_situation.php

--- htdocs/admin/facture_situation.php
+++ htdocs/admin/facture_situation.php
@@ -104,7 +104,7 @@ $arrayAvailableType = array(
 	Facture::TYPE_SITUATION => $langs->trans("InvoiceSituation"),
 	Facture::TYPE_STANDARD.'+'.Facture::TYPE_SITUATION => $langs->trans("InvoiceSituation").' + '.$langs->trans("InvoiceStandard"),
 );
-$selected = $conf->global->{$confkey};
+$selected = $conf->global->$confkey;
 $curentInput = (empty($inputCount) ? 1 : ($inputCount + 1));
 $formSelectInvoiceType = $form->selectarray('value'.$curentInput, $arrayAvailableType, $selected, 1);
 _printInputFormPart($confkey, $langs->trans('AllowedInvoiceForRetainedWarranty'), '', array(), $formSelectInvoiceType);
