ec6c735d8964b6fa735098a33963d22e6f793676
Laurent Destailleur <eldy@destailleur.fr>
2020-10-27 19:44:15 +0100

    Clean code

--- htdocs/webservices/server_productorservice.php
+++ htdocs/webservices/server_productorservice.php
@@ -714,21 +714,6 @@ function updateProductOrService($authentication, $product)
         $newobject->customcode = $product['customcode'];
 
         $newobject->canvas = $product['canvas'];
-        /*foreach($product['lines'] as $line)
-        {
-            $newline=new FactureLigne($db);
-            $newline->type=$line['type'];
-            $newline->desc=$line['desc'];
-            $newline->fk_product=$line['fk_product'];
-            $newline->total_ht=$line['total_net'];
-            $newline->total_vat=$line['total_vat'];
-            $newline->total_ttc=$line['total'];
-            $newline->vat=$line['vat_rate'];
-            $newline->qty=$line['qty'];
-            $newline->fk_product=$line['product_id'];
-        }*/
-        //var_dump($product['ref_ext']);
-        //var_dump($product['lines'][0]['type']);
 
         $elementtype = 'product';
 
