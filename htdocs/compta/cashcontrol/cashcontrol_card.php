ffc961599c2f74d15c96cf66b20d3c1dc5883ce9
Laurent Destailleur <eldy@destailleur.fr>
2020-10-27 10:19:00 +0100

    FIX error 500 on cash fence

--- htdocs/compta/cashcontrol/cashcontrol_card.php
+++ htdocs/compta/cashcontrol/cashcontrol_card.php
@@ -28,6 +28,7 @@
  */
 
 require '../../main.inc.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/compta/bank/class/account.class.php';
 require_once DOL_DOCUMENT_ROOT.'/compta/cashcontrol/class/cashcontrol.class.php';
 
