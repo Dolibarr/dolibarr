6a589463883680b184e7a9e67eb041b5da774bf7
Birame SENE <bsene@pm.me>
2020-11-01 18:36:57 +0100

    use composer standard dir

--- htdocs/index.php
+++ htdocs/index.php
@@ -26,6 +26,7 @@
 
 define('NOCSRFCHECK', 1); // This is main home and login page. We must be able to go on it from another web site.
 
+require_once dirname(__DIR__).'/vendor/autoload.php';
 require 'main.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.formother.class.php';
 
