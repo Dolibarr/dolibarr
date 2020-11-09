279b4309b94e4af35b77a0f27269ca828b305e9e
lvessiller <lvessiller@open-dsi.fr>
2020-11-09 08:51:11 +0100

    NEW include "extrafields_list_array_fields" tpl in module builder

--- htdocs/core/tpl/extrafields_list_array_fields.tpl.php
+++ htdocs/core/tpl/extrafields_list_array_fields.tpl.php
@@ -25,6 +25,7 @@ if (! empty($extrafieldsobjectkey))	// $extrafieldsobject is the $object->table_
 					'position' => $extrafields->attributes[$extrafieldsobjectkey]['pos'][$key],
 					'enabled'  => (abs($extrafields->attributes[$extrafieldsobjectkey]['list'][$key]) != 3 && $extrafields->attributes[$extrafieldsobjectkey]['perms'][$key]),
 					'langfile' => $extrafields->attributes[$extrafieldsobjectkey]['langfile'][$key],
+					'help'     => $extrafields->attributes[$extrafieldsobjectkey]['help'][$key],
 				);
 		}
 	}
