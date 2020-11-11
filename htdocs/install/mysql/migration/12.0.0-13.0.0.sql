42ea51be497fb1dd507bd0ef5bc80d999a0293b1
Laurent Destailleur <eldy@destailleur.fr>
2020-11-11 07:43:45 +0100

    Fix migration

--- htdocs/install/mysql/migration/12.0.0-13.0.0.sql
+++ htdocs/install/mysql/migration/12.0.0-13.0.0.sql
@@ -499,3 +499,19 @@ UPDATE llx_const set value = 'mod_delivery_saphir' WHERE value = 'mod_livraison_
 UPDATE llx_rights_def set perms = 'delivery' WHERE perms = 'livraison' and module = 'expedition';
 UPDATE llx_rights_def set perms = 'delivery_advance' WHERE perms = 'livraison_advance' and module = 'expedition';
 
+
+
+CREATE TABLE llx_zapier_hook(
+    rowid integer AUTO_INCREMENT PRIMARY KEY,
+    entity integer DEFAULT 1 NOT NULL,
+    url varchar(255),
+    event varchar(255),
+    module varchar(128),
+    action varchar(128),
+    status integer,
+    date_creation datetime NOT NULL,
+    fk_user integer NOT NULL,
+    tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    import_key varchar(14)
+) ENGINE=innodb;
+
