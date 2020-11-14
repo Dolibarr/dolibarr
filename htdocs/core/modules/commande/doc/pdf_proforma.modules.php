1db8afb4a99706f37d0dd388e9b68abcf03a9400
Frédéric FRANCE <frederic34@users.noreply.github.com>
2020-11-14 20:40:17 +0100

    doxygen

--- htdocs/core/modules/commande/doc/pdf_proforma.modules.php
+++ htdocs/core/modules/commande/doc/pdf_proforma.modules.php
@@ -63,7 +63,7 @@ class pdf_proforma extends pdf_eratosthene
 	 *  Show top header of page.
 	 *
 	 *  @param	TCPDF		$pdf     		Object PDF
-	 *  @param  Object		$object     	Object to show
+	 *  @param  Commande	$object     	Object to show
 	 *  @param  int	    	$showaddress    0=no, 1=yes
 	 *  @param  Translate	$outputlangs	Object lang for output
 	 *  @param	string		$titlekey		Translation key to show as title of document
