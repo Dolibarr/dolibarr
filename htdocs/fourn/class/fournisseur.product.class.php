22985e7b611733afb6c3689495e71765eba0f21b
Francis Appels <francis.appels@yahoo.com>
2020-11-18 14:27:29 +0100

    Revert "use barcode iso supplier_barcode"
    This reverts commit 57705e225bce307f13e73063aea8b3cff8b4daee.

--- htdocs/fourn/class/fournisseur.product.class.php
+++ htdocs/fourn/class/fournisseur.product.class.php
@@ -105,9 +105,14 @@ class ProductFournisseur extends Product
 	public $fourn_multicurrency_unitprice;
 
 	/**
-	 * @var string $fk_barcode_type - Supplier barcode type
+	 * @var string $supplier_barcode - Supplier barcode
 	 */
-	public $fk_barcode_type;
+	public $supplier_barcode;
+
+	/**
+	 * @var string $supplier_fk_barcode_type - Supplier barcode type
+	 */
+	public $supplier_fk_barcode_type;
 
 	/**
 	 *	Constructor
@@ -542,8 +547,8 @@ class ProductFournisseur extends Product
 				$this->fourn_multicurrency_id          = $obj->fk_multicurrency;
 				$this->fourn_multicurrency_code        = $obj->multicurrency_code;
 				if ($conf->barcode->enabled) {
-					$this->barcode = $obj->barcode;
-					$this->fk_barcode_type = $obj->fk_barcode_type;
+					$this->supplier_barcode = $obj->barcode;
+					$this->supplier_fk_barcode_type = $obj->fk_barcode_type;
 				}
 
 				if (!empty($conf->global->PRODUCT_USE_SUPPLIER_PACKAGING)) {
@@ -656,8 +661,8 @@ class ProductFournisseur extends Product
 				}
 
 				if ($conf->barcode->enabled) {
-					$prodfourn->barcode = $record["barcode"];
-					$prodfourn->fk_barcode_type = $record["fk_barcode_type"];
+					$prodfourn->supplier_barcode = $record["barcode"];
+					$prodfourn->supplier_fk_barcode_type = $record["fk_barcode_type"];
 				}
 
 				if (!empty($conf->dynamicprices->enabled) && !empty($prodfourn->fk_supplier_price_expression)) {
