fabed34a993b8ee00aab8991555113debdcf1d4a
Laurent Destailleur <eldy@destailleur.fr>
2020-11-23 21:47:51 +0100

    FIX #15481

--- htdocs/user/passwordforgotten.php
+++ htdocs/user/passwordforgotten.php
@@ -71,7 +71,7 @@ if ($action == 'validatenewpassword' && $username && $passwordhash)
 	$result = $edituser->fetch('', $_GET["username"]);
 	if ($result < 0)
 	{
-		$message = '<div class="error">'.$langs->trans("ErrorLoginDoesNotExists", $username).'</div>';
+		$message = '<div class="error">'.dol_escape_htmltag($langs->trans("ErrorLoginDoesNotExists", $username)).'</div>';
 	} else {
 		if (dol_verifyHash($edituser->pass_temp, $passwordhash))
 		{
