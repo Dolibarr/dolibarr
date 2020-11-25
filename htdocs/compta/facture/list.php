4a316f06d57b284a35971c97a152886d8250bfec
Florian HENRY <florian.henry@open-concept.pro>
2020-11-25 10:24:44 +0100

    fix : finish bank transfert fourn from list

--- htdocs/compta/facture/list.php
+++ htdocs/compta/facture/list.php
@@ -307,7 +307,8 @@ if ($massaction == 'makepayment') {
 
 	header('Location: '.$loc);
 	exit;
-} elseif ($massaction == 'withdrawrequest')
+}
+elseif ($massaction == 'withdrawrequest')
 {
 	$langs->load("withdrawals");
 
@@ -667,7 +668,7 @@ if ($resql)
 		'presend'=>$langs->trans("SendByMail"),
 		//'makepayment'=>$langs->trans("InvoicePaymentsLimits"),   TODO Blank page when using this
 	);
-	if ($conf->prelevement->enabled) {
+	if ($conf->prelevement->enabled && !empty($user->rights->prelevement->bons->creer)) {
 			$langs->load("withdrawals");
 			$arrayofmassactions['withdrawrequest'] = $langs->trans("MakeWithdrawRequest");
 	}
