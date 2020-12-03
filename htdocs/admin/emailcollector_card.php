b324425db815d4a90a7f11c43d7f241a5b39c2e0
Laurent Destailleur <eldy@destailleur.fr>
2020-12-03 11:19:41 +0100

    code comment

--- htdocs/admin/emailcollector_card.php
+++ htdocs/admin/emailcollector_card.php
@@ -449,6 +449,7 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 	{
 		$morehtml .= 'Failed to open IMAP connection '.$connectstringsource;
 		$morehtml .= '<br>'.imap_last_error();
+		//var_dump(imap_errors())
 	}
 	else
 	{
