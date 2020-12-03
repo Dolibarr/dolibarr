210ccc4b43ca4435aca9706f81c8e4eae4470113
Laurent Destailleur <eldy@destailleur.fr>
2020-12-03 18:34:32 +0100

    FIX Missing very important performance indexes

--- htdocs/install/mysql/migration/12.0.0-13.0.0.sql
+++ htdocs/install/mysql/migration/12.0.0-13.0.0.sql
@@ -517,6 +517,9 @@ UPDATE llx_rights_def set perms = 'delivery' WHERE perms = 'livraison' and modul
 UPDATE llx_rights_def set perms = 'delivery_advance' WHERE perms = 'livraison_advance' and module = 'expedition';
 
 
+ALTER TABLE llx_commande_fournisseurdet ADD INDEX idx_commande_fournisseurdet_fk_commande (fk_commande);
+ALTER TABLE llx_commande_fournisseurdet ADD INDEX idx_commande_fournisseurdet_fk_product (fk_product);
+
 
 CREATE TABLE llx_zapier_hook(
     rowid integer AUTO_INCREMENT PRIMARY KEY,
