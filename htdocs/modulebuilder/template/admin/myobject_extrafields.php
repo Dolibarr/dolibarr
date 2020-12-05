025523c6c0e4162f824896a73fdfac99ffd73ccc
Frédéric FRANCE <frederic34@users.noreply.github.com>
2020-12-05 15:40:23 +0100

    Update myobject_extrafields.php

--- htdocs/modulebuilder/template/admin/myobject_extrafields.php
+++ htdocs/modulebuilder/template/admin/myobject_extrafields.php
@@ -31,7 +31,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/bom.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
 
 // Load translation files required by the page
-$langs->loadLangs(array('mrp@mrp', 'admin'));
+$langs->loadLangs(array('mymodule@mymodule', 'admin'));
 
 $extrafields = new ExtraFields($db);
 $form = new Form($db);
