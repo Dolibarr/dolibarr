f403f5c52e84e0392d21c7ac3ca6fb67a5276b10
zuiko <zuiko@free.fr>
2020-12-07 16:24:12 +0100

    Update html.form.class.php
    From PR #15565
    At first suggested change by frederic34 on French Forum
    In order to enhanced behaviour to select product on customer/supplier order to be able to use barcode reader efficiently,
    a part of the job is that the product must be automatically selected if it is unique and it's allways searching product with barcode reader if this product exists.
    I did not find any disadvantage in case of manual entry of the product reference.
    To complete this enhencement, another change is necessary on /core/lib/ajax.lib.php in function ajax_autocompleter.

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -2040,7 +2040,7 @@ class Form
 			if (!empty($conf->global->PRODUIT_CUSTOMER_PRICES) && !empty($socid)) {
 				$urloption .= '&socid='.$socid;
 			}
-			print ajax_autocompleter($selected, $htmlname, DOL_URL_ROOT.'/product/ajax/products.php', $urloption, $conf->global->PRODUIT_USE_SEARCH_TO_SELECT, 0, $ajaxoptions);
+			print ajax_autocompleter($selected, $htmlname, DOL_URL_ROOT.'/product/ajax/products.php', $urloption, $conf->global->PRODUIT_USE_SEARCH_TO_SELECT, 1, $ajaxoptions);
 
 			if (!empty($conf->variants->enabled)) {
 				?>
@@ -2792,7 +2792,7 @@ class Form
 
 			// mode=2 means suppliers products
 			$urloption = ($socid > 0 ? 'socid='.$socid.'&' : '').'htmlname='.$htmlname.'&outjson=1&price_level='.$price_level.'&type='.$filtertype.'&mode=2&status='.$status.'&finished='.$finished.'&alsoproductwithnosupplierprice='.$alsoproductwithnosupplierprice;
-			print ajax_autocompleter($selected, $htmlname, DOL_URL_ROOT.'/product/ajax/products.php', $urloption, $conf->global->PRODUIT_USE_SEARCH_TO_SELECT, 0, $ajaxoptions);
+			print ajax_autocompleter($selected, $htmlname, DOL_URL_ROOT.'/product/ajax/products.php', $urloption, $conf->global->PRODUIT_USE_SEARCH_TO_SELECT, 1, $ajaxoptions);
 			print ($hidelabel ? '' : $langs->trans("RefOrLabel").' : ').'<input type="text" size="20" name="search_'.$htmlname.'" id="search_'.$htmlname.'" value="'.$selected_input_value.'">';
 		}
 		else
