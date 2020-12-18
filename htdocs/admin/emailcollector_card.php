ffd1dc6726118d307ce4e6ef26e430dfba202237
mattsidnell <mattsidnell@gmail.com>
2020-12-18 14:38:33 +0000

    gg

--- htdocs/admin/emailcollector_card.php
+++ htdocs/admin/emailcollector_card.php
@@ -418,7 +418,7 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 	if (!$connection)
 	{
 		$morehtml .= 'Failed to open IMAP connection '.$connectstringsource;
-		$morehtml .= '<br>'.imap_last_error();
+		//$morehtml .= '<br>'.imap_last_error();
 		//var_dump(imap_errors())
 	} else {
 		$morehtml .= imap_num_msg($connection);
