d3bd709ad6fe1e559526633f92d8104e13443357
Laurent Destailleur <eldy@destailleur.fr>
2020-12-20 18:51:17 +0100

    Fix phpcs

--- htdocs/install/fileconf.php
+++ htdocs/install/fileconf.php
@@ -408,13 +408,13 @@ if (!empty($force_install_noedit)) {
                    id="db_pass" autocomplete="off"
 			       name="db_pass"
 			       value="<?php
-				// If $force_install_databasepass is on, we don't want to set password, we just show '***'. Real value will be extracted from the forced install file at step1.
-				$autofill = ((!empty($_SESSION['dol_save_pass'])) ? $_SESSION['dol_save_pass'] : str_pad('', strlen($force_install_databasepass), '*'));
-				if (!empty($dolibarr_main_prod)) {
-					$autofill = '';
-				}
-				print dol_escape_htmltag($autofill);
-				?>"
+					// If $force_install_databasepass is on, we don't want to set password, we just show '***'. Real value will be extracted from the forced install file at step1.
+					$autofill = ((!empty($_SESSION['dol_save_pass'])) ? $_SESSION['dol_save_pass'] : str_pad('', strlen($force_install_databasepass), '*'));
+					if (!empty($dolibarr_main_prod)) {
+						$autofill = '';
+					}
+					print dol_escape_htmltag($autofill);
+					?>"
 				<?php if ($force_install_noedit == 2 && $force_install_databasepass !== null) { print ' disabled'; } ?>
 			>
 		</td>
