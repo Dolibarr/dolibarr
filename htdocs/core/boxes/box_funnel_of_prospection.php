985e14a5799ee89d4d07c7b1dbe9f0c2e73ff0bf
lmarcouiller <lucas.marcouiller@gmail.com>
2020-12-21 15:54:19 +0100

    Revert "Ajout montant total opportunités sur  lead funnel"
    This reverts commit 6622464622b434f87af125106c8a2dd06dc42c0d.

--- htdocs/core/boxes/box_funnel_of_prospection.php
+++ htdocs/core/boxes/box_funnel_of_prospection.php
@@ -198,6 +198,7 @@ class box_funnel_of_prospection extends ModeleBoxes
 				$liststatus = array();
 				$data = array('');
 				$customlabels = array();
+				$total=0;
 				foreach ($listofstatus as $status) {
 					$customlabel = '';
 					$labelStatus = '';
@@ -220,13 +221,7 @@ class box_funnel_of_prospection extends ModeleBoxes
 							$stringtoprint .= "</tr>\n";
 						}
 					}
-					$customlabels[] = $customlabel;
-				}
-				for ($i = count($data) - 1; $i > 0; $i--) {
-					if ($i != count($data) - 1) {
-						$data[$i] += $data[$i + 1];
-					}
-					$customlabels[$i - 1] .= ', '.html_entity_decode($langs->trans("OpportunityTotalAmount")).': ' . $data[$i];
+					$customlabels[]=$customlabel;
 				}
 				$dataseries[] = $data;
 				if ($conf->use_javascript_ajax) {
