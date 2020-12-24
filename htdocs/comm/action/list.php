79fe5ee34ce6085cc70ffd94a199c25e3522465a
Florian HENRY <florian.henry@open-concept.pro>
2020-12-24 14:35:40 +0100

    actioncomm ok

--- htdocs/comm/action/list.php
+++ htdocs/comm/action/list.php
@@ -233,6 +233,7 @@ if (empty($reshook))
 	$uploaddir = true;
 	// Only users that can delete any event can remove records.
 	$permissiontodelete = $user->rights->agenda->allactions->delete;
+	$permissiontoadd = $user->rights->agenda->myactions->create;
 	include DOL_DOCUMENT_ROOT.'/core/actions_massactions.inc.php';
 }
 
@@ -297,6 +298,7 @@ if ($user->rights->agenda->allactions->delete)
 	$arrayofmassactions['predelete'] = '<span class="fa fa-trash paddingrightonly"></span>'.$langs->trans("Delete");
 }
 if (GETPOST('nomassaction', 'int') || in_array($massaction, array('presend', 'predelete'))) $arrayofmassactions = array();
+if ($user->rights->agenda->myactions->create) $arrayofmassactions['preaffecttag'] = '<span class="fa fa-tag paddingrightonly"></span>'.$langs->trans("AffectTag");
 $massactionbutton = $form->selectMassAction('', $arrayofmassactions);
 
 $sql = "SELECT";
@@ -532,6 +534,7 @@ if ($resql)
 
 	print $s;
 
+	$objecttmp = new ActionComm($db);
 	include DOL_DOCUMENT_ROOT.'/core/tpl/massactions_pre.tpl.php';
 
 	$moreforfilter = '';
