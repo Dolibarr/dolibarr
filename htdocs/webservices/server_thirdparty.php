37e9d139827b1167db475938b4de53dab57a7b86
Laurent Destailleur <eldy@destailleur.fr>
2021-01-01 21:25:37 +0100

    Fix warning

--- htdocs/webservices/server_thirdparty.php
+++ htdocs/webservices/server_thirdparty.php
@@ -510,7 +510,7 @@ function createThirdParty($authentication, $thirdparty)
 			$db->commit();
 
 			// Patch to add capability to associate (one) sale representative
-			if ($thirdparty['commid'] && $thirdparty['commid'] > 0)
+			if (!empty($thirdparty['commid']) && $thirdparty['commid'] > 0)
 				$newobject->add_commercial($fuser, $thirdparty["commid"]);
 
 			$objectresp = array('result'=>array('result_code'=>'OK', 'result_label'=>''), 'id'=>$newobject->id, 'ref'=>$newobject->ref);
