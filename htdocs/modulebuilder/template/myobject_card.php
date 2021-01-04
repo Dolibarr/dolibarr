30cd0412095c536ff70ca0af8ffe197e9318f40d
stickler-ci <support@stickler-ci.com>
2021-01-04 10:57:18 +0000

    Fixing style errors.

--- htdocs/modulebuilder/template/myobject_card.php
+++ htdocs/modulebuilder/template/myobject_card.php
@@ -478,13 +478,13 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 
 			// Validate
 			if ($object->status == $object::STATUS_DRAFT) {
-				if (empty($object->table_element_line) || (is_array($object->lines) && count($object->lines) > 0))	{	
+				if (empty($object->table_element_line) || (is_array($object->lines) && count($object->lines) > 0))	{
 					print dolGetButtonAction($langs->trans('Validate'), '', 'default', $_SERVER['PHP_SELF'].'?id='.$object->id.'&action=confirm_validate&confirm=yes', '', $permissiontoadd);
-				} else {	
-					$langs->load("errors");	
+				} else {
+					$langs->load("errors");
 					//print dolGetButtonAction($langs->trans('Validate'), '', 'default', $_SERVER['PHP_SELF'].'?id='.$object->id.'&action=confirm_validate&confirm=yes', '', 0);
-					print '<a class="butActionRefused" href="" title="'.$langs->trans("ErrorAddAtLeastOneLineFirst").'">'.$langs->trans("Validate").'</a>';	
-				}				
+					print '<a class="butActionRefused" href="" title="'.$langs->trans("ErrorAddAtLeastOneLineFirst").'">'.$langs->trans("Validate").'</a>';
+				}
 			}
 
 			// Clone
