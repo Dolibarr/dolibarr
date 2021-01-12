14e3be630c91cc3cff4da4783349719c12f48af9
stickler-ci <support@stickler-ci.com>
2021-01-12 20:16:34 +0000

    Fixing style errors.

--- htdocs/core/modules/barcode/mod_barcode_product_standard.php
+++ htdocs/core/modules/barcode/mod_barcode_product_standard.php
@@ -142,10 +142,10 @@ class mod_barcode_product_standard extends ModeleNumRefBarCode
 	}
     /**
      *  Return literal barcode type code from numerical rowid type of barcode
-     * 
-	 *	@param	Database 	$db			Database		
+     *
+	 *	@param	Database    $db         Database
      *  @param  int  		$type       Type of barcode (EAN, ISBN, ...) as rowid
-     *  @return string  
+     *  @return string
      */
 
     public function literalBarcodeType($db, $type = '')
@@ -208,7 +208,7 @@ class mod_barcode_product_standard extends ModeleNumRefBarCode
 		$numFinal = get_next_value($db, $mask, 'product', $field, $where, '', $now);
 		//if EAN13 calculate and substitute the last 13th character (* or ?) used in the mask by the EAN13 key
 		$literaltype = '';
-		$literaltype = $this->literalBarcodeType($db,$type);//get literal_Barcode_Type
+		$literaltype = $this->literalBarcodeType($db, $type);//get literal_Barcode_Type
 		if ($literaltype=='EAN13') //EAN13 rowid = 2
 		{
 			if (strlen($numFinal)==13)
