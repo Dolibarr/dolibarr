53b0207e80414c61221c402154a75ee776c89095
Francis Appels <francis.appels@yahoo.com>
2021-01-29 16:40:41 +0100

    mo line is line so no fetchLines

--- htdocs/mrp/class/mo.class.php
+++ htdocs/mrp/class/mo.class.php
@@ -1479,7 +1479,7 @@ class MoLine extends CommonObjectLine
 	public function fetch($id, $ref = null)
 	{
 		$result = $this->fetchCommon($id, $ref);
-		if ($result > 0 && !empty($this->table_element_line)) $this->fetchLines();
+
 		return $result;
 	}
 
