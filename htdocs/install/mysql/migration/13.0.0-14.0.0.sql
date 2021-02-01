f1352fba9725929d6241d02269355a4522af26a4
Gauthier PC portable 024 <gauthier.verdol@atm-consulting.fr>
2021-02-01 11:56:28 +0100

    FIX : migration script

--- htdocs/install/mysql/migration/13.0.0-14.0.0.sql
+++ htdocs/install/mysql/migration/13.0.0-14.0.0.sql
@@ -122,7 +122,7 @@ ALTER TABLE llx_societe ADD INDEX idx_societe_warehouse(fk_warehouse);
 ALTER TABLE llx_socpeople MODIFY poste varchar(255);
 
 
-create table llx_paiementtva
+create table llx_payment_vat
 (
   rowid           integer AUTO_INCREMENT PRIMARY KEY,
   fk_tva          integer,
@@ -139,18 +139,18 @@ create table llx_paiementtva
 
 )ENGINE=innodb;
 
-ALTER TABLE llx_tva ADD paye smallint default 0 NOT NULL;
+ALTER TABLE llx_tva ADD paye smallint default 1 NOT NULL;
 ALTER TABLE llx_tva ADD fk_account integer;
 
-INSERT INTO llx_paiementtva (fk_tva, datec, datep, amount, fk_typepaiement, num_paiement, note, fk_bank, fk_user_creat, fk_user_modif)
+INSERT INTO llx_payment_vat (fk_tva, datec, datep, amount, fk_typepaiement, num_paiement, note, fk_bank, fk_user_creat, fk_user_modif)
 SELECT rowid, NOW(), datep, amount, COALESCE(fk_typepayment, 0), num_payment, '', fk_bank, fk_user_creat, fk_user_modif FROM llx_tva;
 
 UPDATE llx_bank_url url INNER JOIN llx_tva tva ON tva.rowid = url.url_id SET type = 'vat', url.label = CONCAT('(', tva.label, ')') WHERE type = 'payment_vat';
 
 INSERT INTO llx_bank_url (fk_bank, url_id, url, label, type)
 SELECT b.fk_bank, ptva.rowid, REPLACE(b.url, 'tva/card.php', 'payment_vat/card.php'), '(paiement)', 'payment_vat'
-FROM llx_bank_url b INNER JOIN llx_tva tva ON (tva.fk_bank = b.fk_bank) INNER JOIN llx_paiementtva ptva on (ptva.fk_bank = b.fk_bank)
+FROM llx_bank_url b INNER JOIN llx_tva tva ON (tva.fk_bank = b.fk_bank) INNER JOIN llx_payment_vat ptva on (ptva.fk_bank = b.fk_bank)
 WHERE type = 'vat';
 
 ALTER TABLE llx_tva DROP COLUMN fk_bank;
-UPDATE llx_tva SET paye = 1;
+ALTER TABLE llx_tva ALTER paye SET DEFAULT 0;
