79a07928b9eb7b6bcc81b29bed5935f716400ad9
ptibogxiv <support@ptibogxiv.net>
2021-02-06 15:17:38 +0100

    NEW disabled duplicate API shipping methods
    now we have shipment methods API

--- htdocs/api/class/api_setup.class.php
+++ htdocs/api/class/api_setup.class.php
@@ -1220,64 +1220,6 @@ class Setup extends DolibarrApi
 	}
 
 	/**
-	 * Get the list of shipping methods.
-	 *
-	 * @param int       $limit      Number of items per page
-	 * @param int       $page       Page number {@min 0}
-	 * @param int       $active     Shipping methodsm is active or not {@min 0} {@max 1}
-	 * @param string    $sqlfilters SQL criteria to filter. Syntax example "(t.code:=:'CHQ')"
-	 *
-	 * @url     GET dictionary/shipping_methods
-	 *
-	 * @return array List of shipping methods
-	 *
-	 * @throws RestException 400
-	 */
-	public function getShippingModes($limit = 100, $page = 0, $active = 1, $sqlfilters = '')
-	{
-		$list = array();
-
-		$sql = "SELECT rowid as id, code, libelle as label, description, tracking, module";
-		$sql .= " FROM ".MAIN_DB_PREFIX."c_shipment_mode as t";
-		$sql .= " WHERE t.entity IN (".getEntity('c_shipment_mode').")";
-		$sql .= " AND t.active = ".$active;
-		// Add sql filters
-		if ($sqlfilters) {
-			if (!DolibarrApi::_checkFilters($sqlfilters)) {
-				throw new RestException(400, 'Error when validating parameter sqlfilters '.$sqlfilters);
-			}
-				$regexstring = '\(([^:\'\(\)]+:[^:\'\(\)]+:[^:\(\)]+)\)';
-			$sql .= " AND (".preg_replace_callback('/'.$regexstring.'/', 'DolibarrApi::_forge_criteria_callback', $sqlfilters).")";
-		}
-
-
-		//$sql.= $this->db->order($sortfield, $sortorder);
-
-		if ($limit) {
-			if ($page < 0) {
-				$page = 0;
-			}
-			$offset = $limit * $page;
-
-			$sql .= $this->db->plimit($limit, $offset);
-		}
-
-		$result = $this->db->query($sql);
-
-		if ($result) {
-			$num = $this->db->num_rows($result);
-			$min = min($num, ($limit <= 0 ? $num : $limit));
-			for ($i = 0; $i < $min; $i++) {
-				$list[] = $this->db->fetch_object($result);
-			}
-		} else {
-			throw new RestException(400, $this->db->lasterror());
-		}
-
-		return $list;
-	}
-
-	/**
 	 * Get the list of measuring units.
 	 *
 	 * @param string    $sortfield  Sort field
