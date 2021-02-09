9c535d6d7eabeb118b67e7c9dc526145651c7425
Laurent Destailleur <eldy@destailleur.fr>
2021-02-09 21:09:46 +0100

    Fix comment in js must be a js comment

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -1448,8 +1448,8 @@ function top_htmlhead($head, $title = '', $disablejs = 0, $disablehead = 0, $arr
                 {
                 	$pathckeditor = constant('JS_CKEDITOR');
                 }
-                print '<!-- enable ckeditor by main.inc.php -->';
                 print '<script>';
+                print '/* enable ckeditor by main.inc.php */';
                 print 'var CKEDITOR_BASEPATH = \''.$pathckeditor.'\';'."\n";
                 print 'var ckeditorConfig = \''.dol_buildpath($themesubdir.'/theme/'.$conf->theme.'/ckeditor/config.js'.($ext ? '?'.$ext : ''), 1).'\';'."\n"; // $themesubdir='' in standard usage
                 print 'var ckeditorFilebrowserBrowseUrl = \''.DOL_URL_ROOT.'/core/filemanagerdol/browser/default/browser.php?Connector='.DOL_URL_ROOT.'/core/filemanagerdol/connectors/php/connector.php\';'."\n";
