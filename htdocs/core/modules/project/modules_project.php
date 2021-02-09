2fc6d76029d94b905a89f8287ab1dcf5d8f1a3a0
Philippe GRAND <philippe.grand@atoo-net.com>
2021-02-09 14:09:02 +0100

    standardization, internationalisation

--- htdocs/core/modules/project/modules_project.php
+++ htdocs/core/modules/project/modules_project.php
@@ -106,12 +106,12 @@ abstract class ModelePDFProjects extends CommonDocGenerator
 		global $conf;
 
 		$type = 'project';
-		$liste = array();
+		$list = array();
 
 		include_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
-		$liste = getListOfModels($db, $type, $maxfilenamelength);
+		$list = getListOfModels($db, $type, $maxfilenamelength);
 
-		return $liste;
+		return $list;
 	}
 }
 
