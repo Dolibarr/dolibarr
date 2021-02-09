c1bbd076dfc68c5f5876135b243fe76982e722cd
Philippe GRAND <philippe.grand@atoo-net.com>
2021-02-09 14:13:49 +0100

    standardization, internationalisation

--- htdocs/core/modules/societe/modules_societe.class.php
+++ htdocs/core/modules/societe/modules_societe.class.php
@@ -51,12 +51,12 @@ abstract class ModeleThirdPartyDoc extends CommonDocGenerator
 		global $conf;
 
 		$type = 'company';
-		$liste = array();
+		$list = array();
 
 		include_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
-		$liste = getListOfModels($db, $type, $maxfilenamelength);
+		$list = getListOfModels($db, $type, $maxfilenamelength);
 
-		return $liste;
+		return $list;
 	}
 }
 
