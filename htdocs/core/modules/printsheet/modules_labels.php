fff2c1fbacfd57663770acbd62b58d4c8b776b69
Philippe GRAND <philippe.grand@atoo-net.com>
2021-02-09 14:06:22 +0100

    standardization, internationalisation

--- htdocs/core/modules/printsheet/modules_labels.php
+++ htdocs/core/modules/printsheet/modules_labels.php
@@ -54,12 +54,12 @@ class ModelePDFLabels
 		global $conf;
 
 		$type = 'members_labels';
-		$liste = array();
+		$list = array();
 
 		include_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
-		$liste = getListOfModels($db, $type, $maxfilenamelength);
+		$list = getListOfModels($db, $type, $maxfilenamelength);
 
-		return $liste;
+		return $list;
 	}
 }
 
