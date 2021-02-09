cd3ac694bbc825e023dea578494acff648e12350
Gauthier PC portable 024 <gauthier.verdol@atm-consulting.fr>
2021-02-09 10:38:35 +0100

    FIX : tables

--- htdocs/install/mysql/tables/llx_payment_salary.sql
+++ htdocs/install/mysql/tables/llx_payment_salary.sql
@@ -1,6 +1,5 @@
 -- ===================================================================
--- Copyright (C) 2004      Laurent Destailleur  <eldy@users.sourceforge.net>
--- Copyright (C) 2021      Gauthier VERDOL      <gauthier.verdol@atm-consulting.fr>
+-- Copyright (C) 2011-2018 Alexandre Spangaro <aspangaro@open-dsi.fr>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,16 +19,23 @@
 create table llx_payment_salary
 (
   rowid           integer AUTO_INCREMENT PRIMARY KEY,
-  fk_salary       integer,
-  datec           datetime,           -- date de creation
-  tms             timestamp,
-  datep           datetime,           -- payment date
-  amount          double(24,8) DEFAULT 0,
-  fk_typepaiement integer NOT NULL,
-  num_paiement    varchar(50),
+  ref             varchar(30) NULL,           -- payment reference number (currently NULL because there is no numbering manager yet)
+  tms             timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  datec           datetime,                   -- Create date
+  fk_user         integer NOT NULL,
+  datep           date,                       -- payment date
+  datev           date,                       -- value date (this field should not be here, only into bank tables)
+  salary          double(24,8),               -- salary of user when payment was done
+  amount          double(24,8) NOT NULL DEFAULT 0,
+  fk_projet       integer DEFAULT NULL,
+  fk_typepayment  integer NOT NULL,
+  num_payment     varchar(50),                -- num cheque or other
+  label           varchar(255),
+  datesp          date,                       -- date start period
+  dateep          date,                       -- date end period
+  entity          integer DEFAULT 1 NOT NULL, -- multi company id
   note            text,
-  fk_bank         integer NOT NULL,
-  fk_user_creat   integer,            -- creation user
-  fk_user_modif   integer             -- last modification user
-
+  fk_bank         integer,
+  fk_user_author  integer,                    -- user creating
+  fk_user_modif   integer                     -- user making last change
 )ENGINE=innodb;
