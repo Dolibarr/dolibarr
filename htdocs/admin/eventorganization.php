0bcfbb08d81168a26e3ed3742038fa55ef2d8d40
Florian HENRY <florian.henry@open-concept.pro>
2021-02-11 17:16:38 +0100

    on going

--- htdocs/admin/eventorganization.php
+++ htdocs/admin/eventorganization.php
@@ -16,34 +16,22 @@
  */
 
 /**
- * \file    eventorganization/admin/setup.php
+ * \file    htdocs/admin/setup.php
  * \ingroup eventorganization
  * \brief   EventOrganization setup page.
  */
 
 // Load Dolibarr environment
-$res = 0;
-// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
-if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
-// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
-$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
-while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) { $i--; $j--; }
-if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) $res = @include substr($tmp, 0, ($i + 1))."/main.inc.php";
-if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) $res = @include dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php";
-// Try main.inc.php using relative path
-if (!$res && file_exists("../../main.inc.php")) $res = @include "../../main.inc.php";
-if (!$res && file_exists("../../../main.inc.php")) $res = @include "../../../main.inc.php";
-if (!$res) die("Include of main fails");
+require '../main.inc.php';
 
 global $langs, $user;
 
 // Libraries
 require_once DOL_DOCUMENT_ROOT."/core/lib/admin.lib.php";
-require_once '../lib/eventorganization.lib.php';
-//require_once "../class/myclass.class.php";
+require_once DOL_DOCUMENT_ROOT.'/core/lib/eventorganization.lib.php';
 
 // Translations
-$langs->loadLangs(array("admin", "eventorganization@eventorganization"));
+$langs->loadLangs(array("admin", "eventorganization"));
 
 // Access control
 if (!$user->admin) {
