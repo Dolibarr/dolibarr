5d8aaefb261d363eb626d592f6a0e0068fe3697d
Florian HENRY <florian.henry@open-concept.pro>
2021-02-14 17:12:36 +0100

    fix indent

--- htdocs/modulebuilder/index.php
+++ htdocs/modulebuilder/index.php
@@ -1708,11 +1708,10 @@ if (is_array($listofmodules) && count($listofmodules) > 0) {
 				} else {
 					if (preg_match('/^([^@]+)@([^@]+)$/i', $urlpage, $regs))
 					{
-
 						$urltouse = dol_buildpath('/'.$regs[2].'/admin/'.$regs[1], 1);
 						$linktoenabledisable .= ' &nbsp; <a href="'.$urltouse.(preg_match('/\?/', $urltouse) ? '&' : '?').'save_lastsearch_values=1&backtopage='.urlencode($backtourl).'" title="'.$langs->trans("Setup").'">'.img_picto($langs->trans("Setup"), "setup", 'style="padding-right: 6px"').'</a>';
 					} else {
-						// Case standard admin page (not a page provided by the module but a page 
+						// Case standard admin page (not a page provided by the module but a page
 						$urltouse = DOL_URL_ROOT.'/admin/'.$urlpage;
 						$linktoenabledisable .= ' &nbsp; <a href="'.$urltouse.(preg_match('/\?/', $urltouse) ? '&' : '?').'save_lastsearch_values=1&backtopage='.urlencode($backtourl).'" title="'.$langs->trans("Setup").'">'.img_picto($langs->trans("Setup"), "setup", 'style="padding-right: 6px"').'</a>';
 					}
