19a55c45da117b52431869315a253ce9e20bcd4b
Laurent Destailleur <eldy@destailleur.fr>
2021-02-17 15:34:09 +0100

    Fix reposition

--- htdocs/societe/admin/societe.php
+++ htdocs/societe/admin/societe.php
@@ -726,6 +726,7 @@ $form = new Form($db);
 
 print '<form method="POST" action="'.$_SERVER['PHP_SELF'].'">';
 print '<input type="hidden" name="token" value="'.newToken().'">';
+print '<input type="hidden" name="page_y" value="">';
 print '<input type="hidden" name="action" value="updateoptions">';
 
 print '<div class="div-table-responsive-no-min">';
