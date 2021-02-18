f5b162a99260ee5202a9d1f8ab8a14733fcfa53a
daraelmin <daraelmin@gmail.com>
2021-02-18 08:10:14 +0100

    FIX 16165 Create customer discount without vat

--- htdocs/core/class/discount.class.php
+++ htdocs/core/class/discount.class.php
@@ -222,6 +222,7 @@ class DiscountAbsolute
 		if (empty($this->multicurrency_amount_ht)) $this->multicurrency_amount_ht = 0;
 		if (empty($this->multicurrency_amount_tva)) $this->multicurrency_amount_tva = 0;
 		if (empty($this->multicurrency_amount_ttc)) $this->multicurrency_amount_ttc = 0;
+		if (empty($this->tva_tx)) $this->tva_tx = 0;
 
 		// Check parameters
 		if (empty($this->description))
