5b771eb798ad246c37bbef82182d5375f59186ca
Laurent Destailleur <eldy@destailleur.fr>
2021-03-01 21:56:14 +0100

    Fix merge of 2 record.

--- htdocs/societe/card.php
+++ htdocs/societe/card.php
@@ -232,6 +232,7 @@ if (empty($reshook))
 					// TODO Mutualise the list into object societe.class.php
 					$objects = array(
 						'Adherent' => '/adherents/class/adherent.class.php',
+						'Don' => '/don/class/don.class.php',
 						'Societe' => '/societe/class/societe.class.php',
 						//'Categorie' => '/categories/class/categorie.class.php',
 						'ActionComm' => '/comm/action/class/actioncomm.class.php',
