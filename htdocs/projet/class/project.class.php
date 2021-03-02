4e5849e50b01c4efbbdaaf41a53254a34da8e43b
Florian HENRY <florian.henry@open-concept.pro>
2021-03-02 23:13:05 +0100

    remove debug

--- htdocs/projet/class/project.class.php
+++ htdocs/projet/class/project.class.php
@@ -708,7 +708,6 @@ class Project extends CommonObject
 			/* Return array even if empty*/
 			return $elements;
 		} else {
-			//$this->error = $this->db->error;
 			dol_print_error($this->db);
 		}
 	}
