7543b63a6ed9ff214c92b5768ad948284416f59b
Frédéric FRANCE <frederic34@users.noreply.github.com>
2021-03-05 08:49:56 +0100

    Fix #16544

--- htdocs/societe/consumption.php
+++ htdocs/societe/consumption.php
@@ -422,6 +422,7 @@ if ($sql_select)
 		$documentstatic->statut = $objp->status;
 		$documentstatic->status = $objp->status;
 		$documentstatic->paye = $objp->paid;
+		$documentstatic->alreadypaid = $objp->paid;
 
 		if (is_object($documentstaticline)) $documentstaticline->statut = $objp->status;
 
@@ -436,6 +437,8 @@ if ($sql_select)
 		if ($type_element == 'contract')
 		{
 			print $documentstaticline->getLibStatut(2);
+		} elseif ($type_element == 'invoice') {
+			print $documentstatic->getLibStatut(5, $objp->paid);
 		}
 		else
 		{
