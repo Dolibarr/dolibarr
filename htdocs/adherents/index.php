a3896bd2374f0783b1430c5f4a23030a6cf7268c
Laurent Destailleur <eldy@destailleur.fr>
2021-03-08 16:47:49 +0100

    Restore default tabs

--- htdocs/adherents/index.php
+++ htdocs/adherents/index.php
@@ -42,11 +42,11 @@ $langs->loadLangs(array("companies", "members"));
 
 // Security check
 $result = restrictedArea($user, 'adherent');
-if (!isset($form) || !is_object($form)) {
-	$form = new Form($db);
-}
-// Load $resultboxes (selectboxlist + boxactivated + boxlista + boxlistb)
-$resultboxes = FormOther::getBoxesArea($user, "2");
+
+
+/*
+ * Actions
+ */
 
 if (GETPOST('addbox')) {
 	// Add box (when submit is done from a form when ajax disabled)
@@ -61,9 +61,15 @@ if (GETPOST('addbox')) {
 	}
 }
 
+
 /*
  * View
  */
+
+$form = new Form($db);
+
+// Load $resultboxes (selectboxlist + boxactivated + boxlista + boxlistb)
+$resultboxes = FormOther::getBoxesArea($user, "2");
 
 llxHeader('', $langs->trans("Members"), 'EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros');
 
@@ -238,22 +244,259 @@ if ($conf->use_javascript_ajax) {
 print '<div class="clearboth"></div>';
 print '<div class="fichecenter fichecenterbis">';
 
-$boxlist = '<div class="twocolumns">';
+print '<div class="twocolumns">';
 
-$boxlist .= '<div class="firstcolumn fichehalfleft boxhalfleft" id="boxhalfleft">';
-$boxlist .= $searchbox;
-$boxlist .= $boxgraph;
-$boxlist .= $resultboxes['boxlista'];
-$boxlist .= '</div>'."\n";
+print '<div class="firstcolumn fichehalfleft boxhalfleft" id="boxhalfleft">';
+print $searchbox;
+print $boxgraph;
 
-$boxlist .= '<div class="secondcolumn fichehalfright boxhalfright" id="boxhalfright">';
-$boxlist .= $resultboxes['boxlistb'];
-$boxlist .= '</div>'."\n";
+// List of subscription by year
+$Total = array();
+$Number = array();
+$tot = 0;
+$numb = 0;
 
-$boxlist .= '</div>';
+$sql = "SELECT c.subscription, c.dateadh as dateh";
+$sql .= " FROM ".MAIN_DB_PREFIX."adherent as d, ".MAIN_DB_PREFIX."subscription as c";
+$sql .= " WHERE d.entity IN (".getEntity('adherent').")";
+$sql .= " AND d.rowid = c.fk_adherent";
 
-print $boxlist;
 
+$result = $db->query($sql);
+if ($result) {
+	$num = $db->num_rows($result);
+	$i = 0;
+	while ($i < $num) {
+		$objp = $db->fetch_object($result);
+		$year = dol_print_date($db->jdate($objp->dateh), "%Y");
+		$Total[$year] = (isset($Total[$year]) ? $Total[$year] : 0) + $objp->subscription;
+		$Number[$year] = (isset($Number[$year]) ? $Number[$year] : 0) + 1;
+		$tot += $objp->subscription;
+		$numb += 1;
+		$i++;
+	}
+}
+
+print '<div class="div-table-responsive-no-min">';
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre">';
+print '<th>'.$langs->trans("Year").'</th>';
+print '<th class="right">'.$langs->trans("Subscriptions").'</th>';
+print '<th class="right">'.$langs->trans("AmountTotal").'</th>';
+print '<th class="right">'.$langs->trans("AmountAverage").'</th>';
+print "</tr>\n";
+
+krsort($Total);
+$i = 0;
+foreach ($Total as $key=>$value) {
+	if ($i >= 8) {
+		print '<tr class="oddeven">';
+		print "<td>...</td>";
+		print "<td class=\"right\"></td>";
+		print "<td class=\"right\"></td>";
+		print "<td class=\"right\"></td>";
+		print "</tr>\n";
+		break;
+	}
+	print '<tr class="oddeven">';
+	print "<td><a href=\"./subscription/list.php?date_select=$key\">$key</a></td>";
+	print "<td class=\"right\">".$Number[$key]."</td>";
+	print "<td class=\"right\">".price($value)."</td>";
+	print "<td class=\"right\">".price(price2num($value / $Number[$key], 'MT'))."</td>";
+	print "</tr>\n";
+	$i++;
+}
+
+// Total
+print '<tr class="liste_total">';
+print '<td>'.$langs->trans("Total").'</td>';
+print "<td class=\"right\">".$numb."</td>";
+print '<td class="right">'.price($tot)."</td>";
+print "<td class=\"right\">".price(price2num($numb > 0 ? ($tot / $numb) : 0, 'MT'))."</td>";
+print "</tr>\n";
+print "</table></div>";
+
+print "<br>\n";
+
+
+print $resultboxes['boxlista'];
+print '</div>'."\n";
+
+print '<div class="secondcolumn fichehalfright boxhalfright" id="boxhalfright">';
+
+/*
+ * Latest modified members
+ */
+$max = $conf->global->MAIN_SIZE_SHORTLIST_LIMIT;
+
+$sql = "SELECT a.rowid, a.statut as status, a.lastname, a.firstname, a.societe as company, a.fk_soc,";
+$sql .= " a.gender, a.email, a.photo, a.morphy,";
+$sql .= " a.tms as datem, a.datefin as date_end_subscription,";
+$sql .= " ta.rowid as typeid, ta.libelle as label, ta.subscription as need_subscription";
+$sql .= " FROM ".MAIN_DB_PREFIX."adherent as a, ".MAIN_DB_PREFIX."adherent_type as ta";
+$sql .= " WHERE a.entity IN (".getEntity('adherent').")";
+$sql .= " AND a.fk_adherent_type = ta.rowid";
+$sql .= $db->order("a.tms", "DESC");
+$sql .= $db->plimit($max, 0);
+
+$resql = $db->query($sql);
+if ($resql) {
+	print '<div class="div-table-responsive-no-min">';
+	print '<table class="noborder centpercent">';
+	print '<tr class="liste_titre">';
+	print '<th colspan="4">'.$langs->trans("LastMembersModified", $max).'</th></tr>';
+
+	$num = $db->num_rows($resql);
+	if ($num) {
+		$i = 0;
+		while ($i < $num) {
+			$obj = $db->fetch_object($resql);
+
+			$staticmember->id = $obj->rowid;
+			$staticmember->ref = $obj->rowid;
+			$staticmember->lastname = $obj->lastname;
+			$staticmember->firstname = $obj->firstname;
+			$staticmember->gender = $obj->gender;
+			$staticmember->email = $obj->email;
+			$staticmember->photo = $obj->photo;
+			$staticmember->morphy = $obj->morphy;
+			$staticmember->statut = $obj->status;
+			$staticmember->need_subscription = $obj->need_subscription;
+			$staticmember->datefin = $db->jdate($obj->date_end_subscription);
+			if (!empty($obj->fk_soc)) {
+				$staticmember->fk_soc = $obj->fk_soc;
+				$staticmember->fetch_thirdparty();
+				$staticmember->name = $staticmember->thirdparty->name;
+			} else {
+				$staticmember->name = $obj->company;
+			}
+
+			$statictype->id = $obj->typeid;
+			$statictype->label = $obj->label;
+			$statictype->subscription = $obj->need_subscription;
+
+			print '<tr class="oddeven">';
+			print '<td class="nowraponall">'.$staticmember->getNomUrl(-1, 32).'</td>';
+			print '<td>'.$statictype->getNomUrl(1, 32).'</td>';
+			print '<td>'.dol_print_date($db->jdate($obj->datem), 'dayhour').'</td>';
+			print '<td class="right">'.$staticmember->getLibStatut(3).'</td>';
+			print '</tr>';
+			$i++;
+		}
+	}
+	print "</table></div>";
+	print "<br>";
+} else {
+	dol_print_error($db);
+}
+
+
+/*
+ * Last modified subscriptions
+ */
+$max = $conf->global->MAIN_SIZE_SHORTLIST_LIMIT;
+
+$sql = "SELECT a.rowid, a.statut as status, a.lastname, a.firstname, a.societe as company, a.fk_soc,";
+$sql .= " a.gender, a.email, a.photo, a.morphy,";
+$sql .= " a.datefin as date_end_subscription,";
+$sql .= " ta.rowid as typeid, ta.libelle as label, ta.subscription as need_subscription,";
+$sql .= " c.rowid as cid, c.tms as datem, c.datec as datec, c.dateadh as date_start, c.datef as date_end, c.subscription";
+$sql .= " FROM ".MAIN_DB_PREFIX."adherent as a, ".MAIN_DB_PREFIX."adherent_type as ta, ".MAIN_DB_PREFIX."subscription as c";
+$sql .= " WHERE a.entity IN (".getEntity('adherent').")";
+$sql .= " AND a.fk_adherent_type = ta.rowid";
+$sql .= " AND c.fk_adherent = a.rowid";
+$sql .= $db->order("c.tms", "DESC");
+$sql .= $db->plimit($max, 0);
+
+$resql = $db->query($sql);
+if ($resql) {
+	print '<div class="div-table-responsive-no-min">';
+	print '<table class="noborder centpercent">';
+	print '<tr class="liste_titre">';
+	print '<th colspan="5">'.$langs->trans("LastSubscriptionsModified", $max).'</th></tr>';
+
+	$num = $db->num_rows($resql);
+	if ($num) {
+		$i = 0;
+		while ($i < $num) {
+			$obj = $db->fetch_object($resql);
+
+			$staticmember->id = $obj->rowid;
+			$staticmember->ref = $obj->rowid;
+			$staticmember->lastname = $obj->lastname;
+			$staticmember->firstname = $obj->firstname;
+			$staticmember->gender = $obj->gender;
+			$staticmember->email = $obj->email;
+			$staticmember->photo = $obj->photo;
+			$staticmember->morphy = $obj->morphy;
+			$staticmember->statut = $obj->status;
+			$staticmember->need_subscription = $obj->need_subscription;
+			$staticmember->datefin = $db->jdate($obj->date_end_subscription);
+			if (!empty($obj->fk_soc)) {
+				$staticmember->fk_soc = $obj->fk_soc;
+				$staticmember->fetch_thirdparty();
+				$staticmember->name = $staticmember->thirdparty->name;
+			} else {
+				$staticmember->name = $obj->company;
+			}
+
+			$subscriptionstatic->id = $obj->cid;
+			$subscriptionstatic->ref = $obj->cid;
+
+			print '<tr class="oddeven">';
+			print '<td class="nowraponall">'.$subscriptionstatic->getNomUrl(1).'</td>';
+			print '<td class="nowraponall">'.$staticmember->getNomUrl(-1, 32, 'subscription').'</td>';
+			print '<td class="nowraponall">'.get_date_range($db->jdate($obj->date_start), $db->jdate($obj->date_end)).'</td>';
+			print '<td class="right">'.price($obj->subscription).'</td>';
+			//print '<td class="right">'.$staticmember->LibStatut($obj->statut,($obj->subscription=='yes'?1:0),$db->jdate($obj->date_end_subscription),5).'</td>';
+			print '<td class="right nowraponall">'.dol_print_date($db->jdate($obj->datem ? $obj->datem : $obj->datec), 'dayhour').'</td>';
+			print '</tr>';
+			$i++;
+		}
+	}
+	print "</table></div>";
+	print "<br>";
+} else {
+	dol_print_error($db);
+}
+
+
+// Summary of members by type
+print '<div class="div-table-responsive-no-min">';
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre">';
+print '<th>'.$langs->trans("MembersTypes").'</th>';
+print '<th class=right>'.$langs->trans("MembersStatusToValid").'</th>';
+print '<th class=right>'.$langs->trans("OutOfDate").'</th>';
+print '<th class=right>'.$langs->trans("UpToDate").'</th>';
+print '<th class=right>'.$langs->trans("MembersStatusResiliated").'</th>';
+print "</tr>\n";
+
+foreach ($AdherentType as $key => $adhtype) {
+	print '<tr class="oddeven">';
+	print '<td>'.$adhtype->getNomUrl(1, dol_size(32)).'</td>';
+	print '<td class="right">'.(isset($MemberToValidate[$key]) && $MemberToValidate[$key] > 0 ? $MemberToValidate[$key] : '').' '.$staticmember->LibStatut(-1, $adhtype->subscription, 0, 3).'</td>';
+	print '<td class="right">'.(isset($MembersValidated[$key]) && ($MembersValidated[$key] - (isset($MemberUpToDate[$key]) ? $MemberUpToDate[$key] : 0) > 0) ? $MembersValidated[$key] - (isset($MemberUpToDate[$key]) ? $MemberUpToDate[$key] : 0) : '').' '.$staticmember->LibStatut(1, $adhtype->subscription, 0, 3).'</td>';
+	print '<td class="right">'.(isset($MemberUpToDate[$key]) && $MemberUpToDate[$key] > 0 ? $MemberUpToDate[$key] : '').' '.$staticmember->LibStatut(1, $adhtype->subscription, $now, 3).'</td>';
+	print '<td class="right">'.(isset($MembersResiliated[$key]) && $MembersResiliated[$key] > 0 ? $MembersResiliated[$key] : '').' '.$staticmember->LibStatut(0, $adhtype->subscription, 0, 3).'</td>';
+	print "</tr>\n";
+}
+print '<tr class="liste_total">';
+print '<td class="liste_total">'.$langs->trans("Total").'</td>';
+print '<td class="liste_total right">'.$SommeA.' '.$staticmember->LibStatut(-1, $adhtype->subscription, 0, 3).'</td>';
+print '<td class="liste_total right">'.$SommeB.' '.$staticmember->LibStatut(1, $adhtype->subscription, 0, 3).'</td>';
+print '<td class="liste_total right">'.$SommeC.' '.$staticmember->LibStatut(1, $adhtype->subscription, $now, 3).'</td>';
+print '<td class="liste_total right">'.$SommeD.' '.$staticmember->LibStatut(0, $adhtype->subscription, 0, 3).'</td>';
+print '</tr>';
+
+print "</table>\n";
+print "</div>";
+
+print $resultboxes['boxlistb'];
+
+print '</div>'."\n";
+
+print '</div>';
 print '</div>';
 
 $parameters = array('user' => $user);
