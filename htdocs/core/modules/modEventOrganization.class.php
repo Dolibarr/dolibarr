7aec2fc02b333a7aede33e107e7048301c8fe67f
Florian HENRY <florian.henry@open-concept.pro>
2021-03-09 18:52:52 +0100

    create task

--- htdocs/core/modules/modEventOrganization.class.php
+++ htdocs/core/modules/modEventOrganization.class.php
@@ -66,7 +66,7 @@ class modEventOrganization extends DolibarrModules
 		// Define some features supported by module (triggers, login, substitutions, menus, css, etc...)
 		$this->module_parts = array(
 			// Set this to 1 if module has its own trigger directory (core/triggers)
-			'triggers' => 0,
+			'triggers' => 1,
 			// Set this to 1 if module has its own login method file (core/login)
 			'login' => 0,
 			// Set this to 1 if module has its own substitution function file (core/substitutions)
