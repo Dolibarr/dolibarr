2ff2640c8da4737beb399d2a39230ed4c7452d34
Laurent Destailleur <eldy@destailleur.fr>
2021-03-10 17:16:49 +0100

    Update card.php

--- htdocs/salaries/card.php
+++ htdocs/salaries/card.php
@@ -712,9 +712,9 @@ if ($id) {
 	print '</td></tr>';*/
 
 	if ($action == 'edit') {
-		print '<tr><td class="fieldrequired">' . $langs->trans("Amount") . '</td><td><input name="amount" size="10" value="' . price($object->amount, 0, $outputlangs, 1, -1, 2) . '"></td></tr>';
+		print '<tr><td class="fieldrequired">' . $langs->trans("Amount") . '</td><td><input name="amount" size="10" value="' . price($object->amount) . '"></td></tr>';
 	} else {
-		print '<tr><td>' . $langs->trans("Amount") . '</td><td>' . price($object->amount) . '</td></tr>';
+		print '<tr><td>' . $langs->trans("Amount") . '</td><td>' . price($object->amount, 0, $outputlangs, 1, -1, -1, $conf->currency) . '</td></tr>';
 	}
 
 	// Mode of payment
