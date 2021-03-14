02e21913ce76f444b27a849b21a2dee0de6b719e
Frédéric FRANCE <frederic34@users.noreply.github.com>
2021-03-14 17:33:13 +0100

    remove dol_buildpath

--- htdocs/bom/lib/bom.lib.php
+++ htdocs/bom/lib/bom.lib.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2019       Maxime Kohlhaas         <maxime@atm-consulting.fr>
- * Copyright (C) 2019       Frédéric France         <frederic.france@netlogic.fr>
+ * Copyright (C) 2019-2021  Frédéric France         <frederic.france@netlogic.fr>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -41,16 +41,10 @@ function bomAdminPrepareHead()
 	$head[$h][2] = 'settings';
 	$h++;
 
-	$head[$h][0] = dol_buildpath("/admin/bom_extrafields.php", 1);
+	$head[$h][0] = DOL_URL_ROOT."/admin/bom_extrafields.php";
 	$head[$h][1] = $langs->trans("ExtraFields");
 	$head[$h][2] = 'bom_extrafields';
 	$h++;
-
-	/*$head[$h][0] = DOL_URL_ROOT."/bom/admin/about.php";
-	$head[$h][1] = $langs->trans("About");
-	$head[$h][2] = 'about';
-	$h++;
-	*/
 
 	// Show more tabs from modules
 	// Entries must be declared in modules descriptor with line
