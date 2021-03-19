b0f26ca2ee5bf6728493357847c98ffd0d718bb5
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2021-03-19 09:14:47 +0100

    Update document.php
    $help_url = "EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros|DE:Modul_Mitglieder";
    	
    llxHeader("", $title, $help_url);

--- htdocs/adherents/document.php
+++ htdocs/adherents/document.php
@@ -89,8 +89,10 @@ include DOL_DOCUMENT_ROOT.'/core/actions_linkedfiles.inc.php';
 $form = new Form($db);
 
 $title = $langs->trans("Member")." - ".$langs->trans("Documents");
-$helpurl = "EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros";
-llxHeader("", $title, $helpurl);
+
+$help_url = "EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros|DE:Modul_Mitglieder";
+	
+llxHeader("", $title, $help_url);
 
 if ($id > 0) {
 	$result = $membert->fetch($object->typeid);
