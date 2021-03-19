b4e929e57f676cf6f064020bb45943d8baf9486c
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2021-03-19 09:36:49 +0100

    Update card.php
    $helpurl  ->  $help_url

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -2815,9 +2815,10 @@ if (!empty($conf->projet->enabled)) {
 $now = dol_now();
 
 $title = $langs->trans('InvoiceCustomer')." - ".$langs->trans('Card');
-$helpurl = "EN:Customers_Invoices|FR:Factures_Clients|ES:Facturas_a_clientes";
 
-llxHeader('', $title, $helpurl);
+$help_url = "EN:Customers_Invoices|FR:Factures_Clients|ES:Facturas_a_clientes";
+
+llxHeader('', $title, $help_url);
 
 // Mode creation
 
