e5eddff0a5bec929a6e9bbf9d128950a6159c869
stickler-ci <support@stickler-ci.com>
2021-03-19 08:15:14 +0000

    Fixing style errors.

--- htdocs/adherents/agenda.php
+++ htdocs/adherents/agenda.php
@@ -122,9 +122,9 @@ if ($object->id > 0) {
 	$langs->load("companies");
 
 	$title = $langs->trans("Member")." - ".$langs->trans("Agenda");
-	
+
 	$help_url = "EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros|DE:Modul_Mitglieder";
-	
+
 	llxHeader("", $title, $help_url);
 
 	if (!empty($conf->notification->enabled)) {
