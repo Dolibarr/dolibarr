d76bd583f5701f147cf0a77689fcc68078f8e31f
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2021-03-19 09:13:06 +0100

    Update agenda.php
    $help_url = "EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros|DE:Modul_Mitglieder";
    	
    llxHeader("", $title, $help_url);

--- htdocs/adherents/agenda.php
+++ htdocs/adherents/agenda.php
@@ -122,8 +122,10 @@ if ($object->id > 0) {
 	$langs->load("companies");
 
 	$title = $langs->trans("Member")." - ".$langs->trans("Agenda");
-	$helpurl = "EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros";
-	llxHeader("", $title, $helpurl);
+	
+	$help_url = "EN:Module_Foundations|FR:Module_Adh&eacute;rents|ES:M&oacute;dulo_Miembros|DE:Modul_Mitglieder";
+	
+	llxHeader("", $title, $help_url);
 
 	if (!empty($conf->notification->enabled)) {
 		$langs->load("mails");
