b7a46ae83806a52a752ee65e1e7576b52189c1cb
Frédéric FRANCE <frederic34@users.noreply.github.com>
2021-03-20 21:50:47 +0100

    fix php8 warnings

--- htdocs/adherents/admin/website.php
+++ htdocs/adherents/admin/website.php
@@ -178,7 +178,7 @@ if (!empty($conf->global->MEMBER_ENABLE_PUBLIC)) {
 	print '</td><td class="right">';
 	$listofval = array();
 	$listofval += $adht->liste_array();
-	$forcetype = $conf->global->MEMBER_NEWFORM_FORCETYPE ?: -1;
+	$forcetype = !empty($conf->global->MEMBER_NEWFORM_FORCETYPE) ? $conf->global->MEMBER_NEWFORM_FORCETYPE : -1;
 	print $form->selectarray("MEMBER_NEWFORM_FORCETYPE", $listofval, $forcetype, count($listofval) > 1 ? 1 : 0);
 	print "</td></tr>\n";
 
@@ -232,7 +232,7 @@ if (!empty($conf->global->MEMBER_ENABLE_PUBLIC)) {
 	print '<br>';
 	//print $langs->trans('FollowingLinksArePublic').'<br>';
 	print img_picto('', 'globe').' '.$langs->trans('BlankSubscriptionForm').':<br>';
-	if ($conf->multicompany->enabled) {
+	if (!empty($conf->multicompany->enabled)) {
 		$entity_qr = '?entity='.$conf->entity;
 	} else {
 		$entity_qr = '';
