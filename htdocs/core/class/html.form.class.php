5af02d96cd41709964f13acaa57d585f1dc6ee47
Laurent Destailleur <eldy@destailleur.fr>
2021-03-25 03:58:54 +0100

    Show entity for user only if different than current entity

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -2002,7 +2002,9 @@ class Form
 						if (!$obj->entity) {
 							$moreinfo .= ($moreinfo ? ' - ' : ' (').$langs->trans("AllEntities");
 						} else {
-							$moreinfo .= ($moreinfo ? ' - ' : ' (').($obj->label ? $obj->label : $langs->trans("EntityNameNotDefined"));
+							if ($obj->entity != $conf->entity) {
+								$moreinfo .= ($moreinfo ? ' - ' : ' (').($obj->label ? $obj->label : $langs->trans("EntityNameNotDefined"));
+							}
 						}
 					}
 					$moreinfo .= ($moreinfo ? ')' : '');
