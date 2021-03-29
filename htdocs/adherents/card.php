b6d03dcad08b373896cfe3f1c7741ad0fdfabc11
daraelmin <daraelmin@gmail.com>
2021-03-29 03:27:02 +0200

    Update copyright

--- htdocs/adherents/card.php
+++ htdocs/adherents/card.php
@@ -7,7 +7,7 @@
  * Copyright (C) 2012-2020  Philippe Grand          <philippe.grand@atoo-net.com>
  * Copyright (C) 2015-2018  Alexandre Spangaro      <aspangaro@open-dsi.fr>
  * Copyright (C) 2018-2020  Frédéric France         <frederic.france@netlogic.fr>
- * Copyright (C) 2021       Waël Almoman            <wael@almoman.com>
+ * Copyright (C) 2021       Waël Almoman            <info@almoman.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
