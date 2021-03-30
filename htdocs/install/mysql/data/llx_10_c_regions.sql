ab58ba80c9137610588bb2bb04f74e1ac986f6bb
Laurent Destailleur <eldy@destailleur.fr>
2021-03-30 19:17:12 +0200

    Update llx_10_c_regions.sql

--- htdocs/install/mysql/data/llx_10_c_regions.sql
+++ htdocs/install/mysql/data/llx_10_c_regions.sql
@@ -12,9 +12,6 @@
 -- Copyright (C) 2019~	   Lao Tian        <281388879@qq.com>
 -- Copyright (C) 2020-2021 Udo Tamm             <dev@dolibit.de>
 --
---
--- LICENSE ----------------------------------------------------------------------
---
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation; either version 3 of the License, or
