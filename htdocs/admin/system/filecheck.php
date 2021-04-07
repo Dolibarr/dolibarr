6c4f5b851d68c5ab726f6c5e3e9299d2b8d3af60
Laurent Destailleur <eldy@destailleur.fr>
2021-04-07 23:31:16 +0200

    Use all param of getURLContent

--- htdocs/admin/system/filecheck.php
+++ htdocs/admin/system/filecheck.php
@@ -158,7 +158,7 @@ if (GETPOST('target') == 'local') {
 	}
 }
 if (GETPOST('target') == 'remote') {
-	$xmlarray = getURLContent($xmlremote);
+	$xmlarray = getURLContent($xmlremote, 'GET', '', 1, array(), array('http', 'https'), 0);	// Accept http or https links on external remote server only
 
 	// Return array('content'=>response,'curl_error_no'=>errno,'curl_error_msg'=>errmsg...)
 	if (!$xmlarray['curl_error_no'] && $xmlarray['http_code'] != '400' && $xmlarray['http_code'] != '404') {
