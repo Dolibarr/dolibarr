fdb5b3b3da8796bad70a626ffaaf95627bfcd21e
lmarcouiller <lucas.marcouiller@gmail.com>
2021-04-09 11:28:40 +0200

    Fix error on module builder delete of a property

--- htdocs/modulebuilder/index.php
+++ htdocs/modulebuilder/index.php
@@ -1296,6 +1296,9 @@ if ($dirins && $action == 'addproperty' && !empty($module) && !empty($tabobj)) {
 if ($dirins && $action == 'confirm_deleteproperty' && $propertykey) {
 	$objectname = $tabobj;
 
+	$dirins = $dirread = $listofmodules[strtolower($module)]['moduledescriptorrootpath'];
+	$moduletype = $listofmodules[strtolower($module)]['moduletype'];
+
 	$srcdir = $dirread.'/'.strtolower($module);
 	$destdir = $dirins.'/'.strtolower($module);
 	dol_mkdir($destdir);
