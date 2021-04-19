8c428081ab94113a19ae70e0b1d8b8be4eeab84c
Gauthier PC portable 024 <gauthier.verdol@atm-consulting.fr>
2021-04-19 09:17:33 +0200

    FIX : test on link type

--- htdocs/comm/mailing/class/advtargetemailing.class.php
+++ htdocs/comm/mailing/class/advtargetemailing.class.php
@@ -638,11 +638,11 @@ class AdvanceTargetingMailing extends CommonObject
 						}
 					} elseif ($extrafields->attributes[$elementtype]['type'][$key] == 'boolean') {
 						if ($arrayquery['options_'.$key]!=''){
-							$sqlwhere[]= " (te.".$key." = ".((int)$arrayquery['options_'.$key]).")";
+							$sqlwhere[]= " (te.".$key." = ".$arrayquery['options_'.$key].")";
 						}
 					} elseif ($extrafields->attributes[$elementtype]['type'][$key] == 'link') {
 						if ($arrayquery['options_'.$key] > 0){
-                                                        $sqlwhere[]= " (te.".$key." = ".$arrayquery['options_'.$key].")";
+                                                        $sqlwhere[]= " (te.".$key." = ".((int)$arrayquery['options_'.$key]).")";
 						}
 					} else {
 						if (is_array($arrayquery['options_'.$key])) {
