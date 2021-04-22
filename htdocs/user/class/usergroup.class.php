c8f8fa079ae5abf5325f34dbfa33f94d18aefb18
Christian Foellmann <christian@foellmann.de>
2021-04-22 15:15:34 +0200

    load all user data when listing users of a group - allows for proper filtering

--- htdocs/user/class/usergroup.class.php
+++ htdocs/user/class/usergroup.class.php
@@ -237,7 +237,7 @@ class UserGroup extends CommonObject
 
 		$ret = array();
 
-		$sql = "SELECT u.rowid";
+		$sql = "SELECT u.*";
 		if (!empty($this->id)) {
 			$sql .= ", ug.entity as usergroup_entity";
 		}
