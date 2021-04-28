151e97a5b82a5689d7c7c9e2373880f11f072725
BENKE Charlene <1179011+defrance@users.noreply.github.com>
2021-04-28 15:15:43 +0200

    Update contact.php

--- htdocs/fourn/facture/contact.php
+++ htdocs/fourn/facture/contact.php
@@ -2,8 +2,8 @@
 /* Copyright (C) 2005		Patrick Rouillon	<patrick@rouillon.net>
  * Copyright (C) 2005-2015	Laurent Destailleur	<eldy@users.sourceforge.net>
  * Copyright (C) 2005-2012	Regis Houssin		<regis.houssin@inodbox.com>
- * Copyright (C) 2017      Ferran Marcet       	 <fmarcet@2byte.es>
-
+ * Copyright (C) 2017       Ferran Marcet       <fmarcet@2byte.es>
+ * Copyright (C) 2021       Charlene Benke      <fmarcet@2byte.es>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
