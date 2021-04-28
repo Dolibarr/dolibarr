4af2f8871a26448fddc2d038cb355aa0ace34d54
Gauthier PC portable 024 <gauthier.verdol@atm-consulting.fr>
2021-04-28 12:27:07 +0200

    FIX : on update current_timestamp

--- htdocs/install/mysql/migration/13.0.0-14.0.0.sql
+++ htdocs/install/mysql/migration/13.0.0-14.0.0.sql
@@ -259,7 +259,7 @@ create table llx_salary
   rowid           integer AUTO_INCREMENT PRIMARY KEY,
   ref             varchar(30) NULL,           -- payment reference number (currently NULL because there is no numbering manager yet)
   label           varchar(255),
-  tms             timestamp,
+  tms             timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   datec           datetime,                   -- Create date
   fk_user         integer NOT NULL,
   datep           date,                       -- payment date
