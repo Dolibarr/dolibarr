df33f6870b7c3b593125c6bbb76953a0d573210f
lmarcouiller <lucas.marcouiller@gmail.com>
2021-04-28 10:12:17 +0200

    fix errors in file

--- htdocs/knowledgemanagement/core/modules/modKnowledgeManagement.class.php
+++ htdocs/knowledgemanagement/core/modules/modKnowledgeManagement.class.php
@@ -350,65 +350,65 @@ class modKnowledgeManagement extends DolibarrModules
 		*/
 
 		$this->menu[$r++]=array(
-            // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-            'fk_menu'=>'fk_mainmenu=knowledgemanagement',
-            // This is a Left menu entry
-            'type'=>'left',
-            'titre'=>'MenuKnowledgeRecord',
-            'mainmenu'=>'knowledgemanagement',
-            'leftmenu'=>'knowledgemanagement_knowledgerecord',
-            'url'=>'/knowledgemanagement/knowledgerecord_list.php',
-            // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
-            'langs'=>'knowledgemanagement@knowledgemanagement',
-            'position'=>101,
-            // Define condition to show or hide menu entry. Use '$conf->knowledgemanagement->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
-            'enabled'=>'$conf->knowledgemanagement->enabled',
-            // Use 'perms'=>'$user->rights->knowledgemanagement->level1->level2' if you want your menu with a permission rules
-            'perms'=>'1',
-            'target'=>'',
-            // 0=Menu for internal users, 1=external users, 2=both
-            'user'=>2,
-        );
-        $this->menu[$r++]=array(
-            // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-            'fk_menu'=>'fk_mainmenu=knowledgemanagement,fk_leftmenu=knowledgemanagement_knowledgerecord',
-            // This is a Left menu entry
-            'type'=>'left',
-            'titre'=>'ListKnowledgeRecord',
-            'mainmenu'=>'knowledgemanagement',
-            'leftmenu'=>'knowledgemanagement_list',
-            'url'=>'/knowledgemanagement/knowledgerecord_list.php',
-            // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
-            'langs'=>'knowledgemanagement@knowledgemanagement',
-            'position'=>111,
-            // Define condition to show or hide menu entry. Use '$conf->knowledgemanagement->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
-            'enabled'=>'$conf->knowledgemanagement->enabled',
-            // Use 'perms'=>'$user->rights->knowledgemanagement->level1->level2' if you want your menu with a permission rules
-            'perms'=>'1',
-            'target'=>'',
-            // 0=Menu for internal users, 1=external users, 2=both
-            'user'=>2,
-        );
-        $this->menu[$r++]=array(
-            // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
-            'fk_menu'=>'fk_mainmenu=knowledgemanagement,fk_leftmenu=knowledgemanagement_knowledgerecord',
-            // This is a Left menu entry
-            'type'=>'left',
-            'titre'=>'NewKnowledgeRecord',
-            'mainmenu'=>'knowledgemanagement',
-            'leftmenu'=>'knowledgemanagement_new',
-            'url'=>'/knowledgemanagement/knowledgerecord_card.php?action=create',
-            // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
-            'langs'=>'knowledgemanagement@knowledgemanagement',
-            'position'=>110,
-            // Define condition to show or hide menu entry. Use '$conf->knowledgemanagement->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
-            'enabled'=>'$conf->knowledgemanagement->enabled',
-            // Use 'perms'=>'$user->rights->knowledgemanagement->level1->level2' if you want your menu with a permission rules
-            'perms'=>'1',
-            'target'=>'',
-            // 0=Menu for internal users, 1=external users, 2=both
-            'user'=>2
-        );
+			// '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'fk_menu'=>'fk_mainmenu=knowledgemanagement',
+			// This is a Left menu entry
+			'type'=>'left',
+			'titre'=>'MenuKnowledgeRecord',
+			'mainmenu'=>'knowledgemanagement',
+			'leftmenu'=>'knowledgemanagement_knowledgerecord',
+			'url'=>'/knowledgemanagement/knowledgerecord_list.php',
+			// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'langs'=>'knowledgemanagement@knowledgemanagement',
+			'position'=>101,
+			// Define condition to show or hide menu entry. Use '$conf->knowledgemanagement->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'enabled'=>'$conf->knowledgemanagement->enabled',
+			// Use 'perms'=>'$user->rights->knowledgemanagement->level1->level2' if you want your menu with a permission rules
+			'perms'=>'1',
+			'target'=>'',
+			// 0=Menu for internal users, 1=external users, 2=both
+			'user'=>2,
+		);
+		$this->menu[$r++]=array(
+			// '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'fk_menu'=>'fk_mainmenu=knowledgemanagement,fk_leftmenu=knowledgemanagement_knowledgerecord',
+			// This is a Left menu entry
+			'type'=>'left',
+			'titre'=>'ListKnowledgeRecord',
+			'mainmenu'=>'knowledgemanagement',
+			'leftmenu'=>'knowledgemanagement_list',
+			'url'=>'/knowledgemanagement/knowledgerecord_list.php',
+			// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'langs'=>'knowledgemanagement@knowledgemanagement',
+			'position'=>111,
+			// Define condition to show or hide menu entry. Use '$conf->knowledgemanagement->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'enabled'=>'$conf->knowledgemanagement->enabled',
+			// Use 'perms'=>'$user->rights->knowledgemanagement->level1->level2' if you want your menu with a permission rules
+			'perms'=>'1',
+			'target'=>'',
+			// 0=Menu for internal users, 1=external users, 2=both
+			'user'=>2,
+		);
+		$this->menu[$r++]=array(
+			// '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+			'fk_menu'=>'fk_mainmenu=knowledgemanagement,fk_leftmenu=knowledgemanagement_knowledgerecord',
+			// This is a Left menu entry
+			'type'=>'left',
+			'titre'=>'NewKnowledgeRecord',
+			'mainmenu'=>'knowledgemanagement',
+			'leftmenu'=>'knowledgemanagement_new',
+			'url'=>'/knowledgemanagement/knowledgerecord_card.php?action=create',
+			// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+			'langs'=>'knowledgemanagement@knowledgemanagement',
+			'position'=>110,
+			// Define condition to show or hide menu entry. Use '$conf->knowledgemanagement->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+			'enabled'=>'$conf->knowledgemanagement->enabled',
+			// Use 'perms'=>'$user->rights->knowledgemanagement->level1->level2' if you want your menu with a permission rules
+			'perms'=>'1',
+			'target'=>'',
+			// 0=Menu for internal users, 1=external users, 2=both
+			'user'=>2
+		);
 
 		/* END MODULEBUILDER LEFTMENU KNOWLEDGERECORD */
 		// Exports profiles provided by this module
