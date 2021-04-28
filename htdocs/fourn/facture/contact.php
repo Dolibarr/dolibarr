224084e1f25c12405dc0232ff49cb85eb2d86ace
stickler-ci <support@stickler-ci.com>
2021-04-28 13:10:07 +0000

    Fixing style errors.

--- htdocs/fourn/facture/contact.php
+++ htdocs/fourn/facture/contact.php
@@ -3,7 +3,7 @@
  * Copyright (C) 2005-2015	Laurent Destailleur	<eldy@users.sourceforge.net>
  * Copyright (C) 2005-2012	Regis Houssin		<regis.houssin@inodbox.com>
  * Copyright (C) 2017      Ferran Marcet       	 <fmarcet@2byte.es>
- 
+
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
