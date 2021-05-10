d86b393d6023868bedef93c4a76a9e05dce37426
Dorian Vabre <dorian.vabre@gmail.com>
2021-05-10 09:54:26 +0200

    useless line removed

--- htdocs/public/eventorganization/attendee_subscription.php
+++ htdocs/public/eventorganization/attendee_subscription.php
@@ -425,8 +425,7 @@ if (empty($reshook) && $action == 'add') {
 			} else {
 				dol_syslog("Failed to send EMail to ".$sendto, LOG_ERR, 0, '_payment');
 			}
-
-			$encodedid = dol_encode($id, $dolibarr_main_instance_unique_id);
+			
 			$securekeyurl = dol_hash($conf->global->EVENTORGANIZATION_SECUREKEY.'conferenceorbooth'.$id, 2);
 			$redirection = $dolibarr_main_url_root.'/public/eventorganization/subscriptionok.php?id='.$id.'&securekey='.$securekeyurl;
 			Header("Location: ".$redirection);
