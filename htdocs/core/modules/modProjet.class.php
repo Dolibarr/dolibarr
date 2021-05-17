c1b7747a88d53a3b14bf5c98102b040e31245219
stickler-ci <support@stickler-ci.com>
2021-05-17 08:21:29 +0000

    Fixing style errors.

--- htdocs/core/modules/modProjet.class.php
+++ htdocs/core/modules/modProjet.class.php
@@ -330,9 +330,9 @@ class modProjet extends DolibarrModules
 	{
 		global $conf, $langs;
 
-        if (dolibarr_del_const($this->db, 'EXPENSEREPORT_PROJECT_IS_REQUIRED', $conf->entity) < 0) {
-            return 0;
-        }
+		if (dolibarr_del_const($this->db, 'EXPENSEREPORT_PROJECT_IS_REQUIRED', $conf->entity) < 0) {
+			return 0;
+		}
 
 		// Permissions
 		$this->remove($options);
