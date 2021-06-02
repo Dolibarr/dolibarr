da7d821a3e18fa283cf79abe1d6a660c41b131dc
Laurent Destailleur <eldy@destailleur.fr>
2021-06-02 15:32:39 +0200

    css

--- htdocs/holiday/month_report.php
+++ htdocs/holiday/month_report.php
@@ -413,7 +413,7 @@ if ($num == 0) {
 			print '<td class="right">'.num_open_day($date_start_inmonth, $date_end_inmonth, 0, 1, $halfdayinmonth).'</td>';
 		}
 		if (!empty($arrayfields['cp.description']['checked'])) {
-			print '<td class="maxwidth300">'.dol_escape_htmltag(dolGetFirstLineOfText($obj->description)).'</td>';
+			print '<td class="maxwidth300 small">'.dol_escape_htmltag(dolGetFirstLineOfText($obj->description)).'</td>';
 		}
 
 		print '<td></td>';
