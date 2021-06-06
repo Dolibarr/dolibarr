ccc35326a52f85b7a0710345cc1593b588b667a4
Frédéric FRANCE <frederic34@users.noreply.github.com>
2021-06-06 17:25:44 +0200

    Update partnershiputils.class.php

--- htdocs/partnership/class/partnershiputils.class.php
+++ htdocs/partnership/class/partnershiputils.class.php
@@ -28,10 +28,10 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/geturl.lib.php';
 
-dol_include_once('partnership/lib/partnership.lib.php');
-dol_include_once('/partnership/class/partnership.class.php');
+require_once DOL_DOCUMENT_ROOT.'/partnership/lib/partnership.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/partnership/class/partnership.class.php';
 
-require_once DOL_DOCUMENT_ROOT."/societe/class/societe.class.php";
+require_once DOL_DOCUMENT_ROOT.'/societe/class/societe.class.php';
 require_once DOL_DOCUMENT_ROOT.'/adherents/class/adherent.class.php';
 /**
  *	Class with cron tasks of Partnership module
