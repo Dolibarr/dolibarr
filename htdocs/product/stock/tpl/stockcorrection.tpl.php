f68ca4bc61b769e8b53a07b8118338238ece0320
kkerleo <72852890+kkerleo@users.noreply.github.com>
2021-06-11 17:12:09 +0200

    Update stockcorrection.tpl.php

--- htdocs/product/stock/tpl/stockcorrection.tpl.php
+++ htdocs/product/stock/tpl/stockcorrection.tpl.php
@@ -64,15 +64,7 @@ print '<script type="text/javascript" language="javascript">
 			}
 			init_price();
 			jQuery("#mouvement").change(function() {
-				init_price();  print '<td'.($object->element == 'stock' ? '' : ' class="fieldrequired"').'>'.$langs->trans("batch_number").'</td><td colspan="3">';
-        if ($pdluoid > 0) {
-            // If form was opened for a specific pdluoid, field is disabled
-            print '<input type="text" name="batch_number_bis" size="40" disabled="disabled" value="'.(GETPOST('batch_number') ?GETPOST('batch_number') : $pdluo->batch).'">';
-            print '<input type="hidden" name="batch_number" value="'.(GETPOST('batch_number') ?GETPOST('batch_number') : $pdluo->batch).'">';
-        } else {
-            print '<input type="text" name="batch_number" size="40" value="'.(GETPOST('batch_number') ?GETPOST('batch_number') : $pdluo->batch).'">';
-        }
-        print '</td>';
+				init_price();  
 			});
 		});
 		</script>';
