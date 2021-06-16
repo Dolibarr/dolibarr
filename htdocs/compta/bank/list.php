cf9c595568ff2117fa963942bae4011e18d448af
Frédéric FRANCE <frederic34@users.noreply.github.com>
2021-06-16 12:01:52 +0200

    fix unknown right
    supprimer doesn't exists

--- htdocs/compta/bank/list.php
+++ htdocs/compta/bank/list.php
@@ -130,7 +130,7 @@ $object->fields = dol_sort_array($object->fields, 'position');
 $arrayfields = dol_sort_array($arrayfields, 'position');
 
 $permissiontoadd = $user->rights->banque->modifier;
-$permissiontodelete = $user->rights->banque->supprimer;
+$permissiontodelete = $user->rights->banque->configurer;
 
 /*
  * Actions
