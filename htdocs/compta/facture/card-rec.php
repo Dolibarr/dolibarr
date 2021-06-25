6e3647af2c4df126e7d4284b5afc4c2bba1543fb
Laurent Destailleur <eldy@destailleur.fr>
2021-06-26 00:08:19 +0200

    Debug v14

--- htdocs/compta/facture/card-rec.php
+++ htdocs/compta/facture/card-rec.php
@@ -193,11 +193,11 @@ if (empty($reshook)) {
 				$action = "create";
 				$error++;
 			}
-			if ($nb_gen_max === '') {
+			/*if ($nb_gen_max === '') {
 				setEventMessages($langs->transnoentities("ErrorFieldRequired", $langs->trans("MaxPeriodNumber")), null, 'errors');
 				$action = "create";
 				$error++;
-			}
+			}*/
 		}
 
 		if (!$error) {
@@ -964,7 +964,7 @@ if ($action == 'create') {
 
 		// Title
 		print '<tr><td class="titlefieldcreate fieldrequired">'.$langs->trans("Title").'</td><td>';
-		print '<input class="flat quatrevingtpercent" type="text" name="title" value="'.dol_escape_htmltag(GETPOST("titre", 'alphanohtml')).'">';
+		print '<input class="flat quatrevingtpercent" type="text" name="title" value="'.dol_escape_htmltag(GETPOST("title", 'alphanohtml')).'">';
 		print '</td></tr>';
 
 		// Third party
