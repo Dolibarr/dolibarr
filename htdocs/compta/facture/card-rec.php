ccffb36a3795bc0a0ba53cc3ef5ab2e84c20bf5a
Laurent Destailleur <eldy@destailleur.fr>
2021-06-26 00:14:53 +0200

    CSS

--- htdocs/compta/facture/card-rec.php
+++ htdocs/compta/facture/card-rec.php
@@ -44,7 +44,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/invoice.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
 
 // Load translation files required by the page
-$langs->loadLangs(array('bills', 'compta', 'admin', 'other', 'products', 'banks'));
+$langs->loadLangs(array('bills', 'companies', 'compta', 'admin', 'other', 'products', 'banks'));
 
 $action     = GETPOST('action', 'alpha');
 $massaction = GETPOST('massaction', 'alpha');
