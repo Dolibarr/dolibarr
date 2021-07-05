bcb06d5c909664e6195acc395bddb23357ba0824
stickler-ci <support@stickler-ci.com>
2021-07-05 07:40:44 +0000

    Fixing style errors.

--- htdocs/commande/list.php
+++ htdocs/commande/list.php
@@ -513,12 +513,9 @@ if ($search_status <> '') {
 		//$sql.= ' AND c.facture = 0'; // invoice not created
 		$sql .= ' AND ((c.fk_statut IN (1,2)) OR (c.fk_statut = 3 AND c.facture = 0))'; // validated, in process or closed but not billed
 	}
-	if ($search_status == -4)	// To bill
-	{
-
+	if ($search_status == -4) {	// To bill
 		$sql .= ' AND (c.fk_statut IN (1,2))'; // validated, in process
 	}
-
 }
 
 if ($search_datecloture_start) {
