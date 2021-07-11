d485d9d2fd1771f5fb8a2bd31576ae2467cdbddd
fr69400 <82267780+fr69400@users.noreply.github.com>
2021-07-11 09:45:59 +0200

    Update note.php

--- htdocs/contact/note.php
+++ htdocs/contact/note.php
@@ -57,11 +57,12 @@ $hookmanager->initHooks(array('contactnote'));
  * Actions
  */
 $reshook = $hookmanager->executeHooks('doActions', array(), $object, $action); // Note that $action and $object may have been modified by some hooks
-if ($reshook < 0)
+if ($reshook < 0) {
 	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
-
-if (empty($reshook))
-	include DOL_DOCUMENT_ROOT.'/core/actions_setnotes.inc.php'; // Must be include, not includ_once
+}
+if (empty($reshook)){
+	include DOL_DOCUMENT_ROOT.'/core/actions_setnotes.inc.php'; // Must be include, not include_once
+}
 
 
 /*
