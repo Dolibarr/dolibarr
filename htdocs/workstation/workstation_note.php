e43da11ac8d244f2b9e0119efd368cc4ae0f3d0b
fr69400 <82267780+fr69400@users.noreply.github.com>
2021-07-11 09:55:33 +0200

    Update workstation_note.php

--- htdocs/workstation/workstation_note.php
+++ htdocs/workstation/workstation_note.php
@@ -69,11 +69,12 @@ restrictedArea($user, $object->element, $object->id, $object->table_element, 'wo
  * Actions
  */
 $reshook = $hookmanager->executeHooks('doActions', array(), $object, $action); // Note that $action and $object may have been modified by some hooks
-if ($reshook < 0)
+if ($reshook < 0) {
 	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
-
-if (empty($reshook))
+}
+if (empty($reshook)){
 	include DOL_DOCUMENT_ROOT.'/core/actions_setnotes.inc.php'; // Must be include, not include_once
+}
 
 
 /*
