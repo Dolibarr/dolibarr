d5c8243a121677e1cd0a32f7ef464a9882b9e752
fr69400 <82267780+fr69400@users.noreply.github.com>
2021-07-11 09:54:35 +0200

    Update note.php

--- htdocs/societe/note.php
+++ htdocs/societe/note.php
@@ -57,13 +57,12 @@ $result = restrictedArea($user, 'societe', $object->id, '&societe');
  * Actions
  */
 $reshook = $hookmanager->executeHooks('doActions', array(), $object, $action); // Note that $action and $object may have been modified by some hooks
-if ($reshook < 0)
+if ($reshook < 0) {
 	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
-
-if (empty($reshook))
-	include DOL_DOCUMENT_ROOT.'/core/actions_setnotes.inc.php'; // Must be include, not includ_once
-
-include DOL_DOCUMENT_ROOT.'/core/actions_setnotes.inc.php'; // Must be include, not includ_once
+}
+if (empty($reshook)){
+	include DOL_DOCUMENT_ROOT.'/core/actions_setnotes.inc.php'; // Must be include, not include_once
+}
 
 
 /*
