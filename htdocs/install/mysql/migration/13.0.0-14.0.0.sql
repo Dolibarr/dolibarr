41016039b678fb887f0bc02089da7215bc35382f
Laurent Destailleur <eldy@destailleur.fr>
2021-07-11 19:39:18 +0200

    Fix missing very old table

--- htdocs/install/mysql/migration/13.0.0-14.0.0.sql
+++ htdocs/install/mysql/migration/13.0.0-14.0.0.sql
@@ -583,3 +583,16 @@ DROP TABLE llx_categorie_association;
 DROP TABLE llx_cond_reglement;
 DROP TABLE llx_zapier_hook_extrafields;
 
+create table llx_onlinesignature
+(
+  rowid                     integer AUTO_INCREMENT PRIMARY KEY,
+  entity                    integer DEFAULT 1 NOT NULL,
+  object_type               varchar(32) NOT NULL,
+  object_id					integer NOT NULL,
+  datec                     datetime NOT NULL,
+  tms                       timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  name						varchar(255) NOT NULL,
+  ip						varchar(128),
+  pathoffile				varchar(255)
+)ENGINE=innodb;
+
