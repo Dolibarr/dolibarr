dbed6bc0da64d23df249189b27d20e7a63d56ad4
Laurent Destailleur <eldy@users.sourceforge.net>
2021-07-21 01:18:48 +0200

    Fix #yogosha6678

--- htdocs/main.inc.php
+++ htdocs/main.inc.php
@@ -80,7 +80,7 @@ function realCharForNumericEntities($matches)
  * Warning: Such a protection can't be enough. It is not reliable as it will always be possible to bypass this. Good protection can
  * only be guaranted by escaping data during output.
  *
- * @param		string		$val		Value brut found int $_GET, $_POST or PHP_SELF
+ * @param		string		$val		Brut value found into $_GET, $_POST or PHP_SELF
  * @param		string		$type		0=POST, 1=GET, 2=PHP_SELF, 3=GET without sql reserved keywords (the less tolerant test)
  * @return		int						>0 if there is an injection, 0 if none
  */
