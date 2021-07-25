f7e76a30a216bc91f1517e0c7a6d92828992f37c
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2021-07-25 16:07:14 +0200

    Remove dol_syslog

--- htdocs/salaries/class/paymentsalary.class.php
+++ htdocs/salaries/class/paymentsalary.class.php
@@ -572,8 +572,6 @@ class PaymentSalary extends CommonObject
 				// Add link 'user' in bank_url between user and bank transaction
 				if (!$error) {
 					foreach ($this->amounts as $key => $value) {
-						dol_syslog(get_class($this) . '::Insertuser ' . $key);
-
 						if ($mode == 'payment_salary') {
 							$fuser = new User($this->db);
 							$fuser->fetch($this->fk_user);
