026e61f1eccc35b36e78a2ff94748c327f66c1f8
ptibogxiv <support@ptibogxiv.net>
2021-07-26 14:53:06 +0200

    Update extrafields_edit.tpl.php

--- htdocs/core/tpl/extrafields_edit.tpl.php
+++ htdocs/core/tpl/extrafields_edit.tpl.php
@@ -43,7 +43,7 @@ $reshook = $hookmanager->executeHooks('formObjectOptions', $parameters, $object,
 print $hookmanager->resPrint;
 if (empty($reshook)) {
 	$params = array();
-	$params['cols'] =isset($parameters[ 'colspanvalue' ]) ? $parameters[ 'colspanvalue' ] : null;
+	$params['cols'] =isset($parameters['colspanvalue']) ? $parameters['colspanvalue'] : null;
 	print $object->showOptionals($extrafields, 'edit', $params);
 }
 
