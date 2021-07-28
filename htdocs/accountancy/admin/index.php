42252abc07fb7a73050af8cc14205bbd96150e79
Frédéric FRANCE <frederic34@users.noreply.github.com>
2021-07-28 10:14:57 +0200

    remove debug

--- htdocs/accountancy/admin/index.php
+++ htdocs/accountancy/admin/index.php
@@ -93,7 +93,6 @@ if ($action == 'update') {
 		foreach ($list as $constname)
 		{
 			$constvalue = GETPOST($constname, 'alpha');
-			var_dump($constname);
 			if (!dolibarr_set_const($db, $constname, $constvalue, 'chaine', 0, '', $conf->entity)) {
 				$error++;
 			}
