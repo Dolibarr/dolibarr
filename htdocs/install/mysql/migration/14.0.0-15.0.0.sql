a8e7d379b4f78d8a6463b164ad8a2cf91517f95e
Florian HENRY <florian.henry@open-concept.pro>
2021-07-30 10:45:24 +0200

    NEW: add table product langs extrafields #18292

--- htdocs/install/mysql/migration/14.0.0-15.0.0.sql
+++ htdocs/install/mysql/migration/14.0.0-15.0.0.sql
@@ -39,3 +39,14 @@ ALTER TABLE llx_product_fournisseur_price MODIFY COLUMN ref_fourn varchar(128);
 ALTER TABLE llx_product_customer_price MODIFY COLUMN ref_customer varchar(128);
 
 
+create table llx_product_lang_extrafields
+(
+    rowid                     integer AUTO_INCREMENT PRIMARY KEY,
+    tms                       timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    fk_object                 integer NOT NULL,
+    import_key                varchar(14)                          		-- import key
+) ENGINE=innodb;
+
+ALTER TABLE llx_product_lang_extrafields ADD INDEX idx_product_lang_extrafields (fk_object);
+
+
