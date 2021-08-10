6ece5f6931c87ea30ac1778e58bb39158627f913
Laurent Destailleur <eldy@destailleur.fr>
2021-08-10 13:01:57 +0200

    FIX avoid to have link to create bookmar on page to create bookmark

--- htdocs/bookmarks/bookmarks.lib.php
+++ htdocs/bookmarks/bookmarks.lib.php
@@ -72,10 +72,12 @@ function printDropdownBookmarksList()
 	// Url to go on create new bookmark page
 	$newbtn = '';
 	if (!empty($user->rights->bookmark->creer)) {
-		//$urltoadd=DOL_URL_ROOT.'/bookmarks/card.php?action=create&amp;urlsource='.urlencode($url).'&amp;url='.urlencode($url);
-		$urltoadd = DOL_URL_ROOT.'/bookmarks/card.php?action=create&amp;url='.urlencode($url);
-		$newbtn .= '<a class="top-menu-dropdown-link" title="'.$langs->trans('AddThisPageToBookmarks').'" href="'.dol_escape_htmltag($urltoadd).'" >';
-		$newbtn .= img_picto('', 'add', '', false, 0, 0, '', 'paddingright').dol_escape_htmltag($langs->trans('AddThisPageToBookmarks')).'</a>';
+		if (!preg_match('/bookmarks\/card.php/', $_SERVER['PHP_SELF'])) {
+			//$urltoadd=DOL_URL_ROOT.'/bookmarks/card.php?action=create&amp;urlsource='.urlencode($url).'&amp;url='.urlencode($url);
+			$urltoadd = DOL_URL_ROOT.'/bookmarks/card.php?action=create&amp;url='.urlencode($url);
+			$newbtn .= '<a class="top-menu-dropdown-link" title="'.$langs->trans('AddThisPageToBookmarks').'" href="'.dol_escape_htmltag($urltoadd).'" >';
+			$newbtn .= img_picto('', 'add', '', false, 0, 0, '', 'paddingright').dol_escape_htmltag($langs->trans('AddThisPageToBookmarks')).'</a>';
+		}
 	}
 
 	// Menu with list of bookmarks
