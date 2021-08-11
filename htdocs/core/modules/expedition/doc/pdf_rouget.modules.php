816648f283a74a666112ce36e9565fbb2f772cd2
Anthony Berton <34568357+bb2a@users.noreply.github.com>
2021-08-11 12:28:44 +0200

    other_docs

--- htdocs/core/modules/expedition/doc/pdf_rouget.modules.php
+++ htdocs/core/modules/expedition/doc/pdf_rouget.modules.php
@@ -1047,10 +1047,12 @@ class pdf_rouget extends ModelePdfExpedition
 			$pdf->SetFillColor(255, 255, 255);
 
 			// Show sender name
-			$pdf->SetXY($posx + 2, $posy + 3);
-			$pdf->SetFont('', 'B', $default_font_size);
-			$pdf->MultiCell($widthrecbox - 2, 4, $outputlangs->convToOutputCharset($this->emetteur->name), 0, 'L');
-			$posy = $pdf->getY();
+			if (empty($conf->global->MAIN_PDF_NO_SENDER_NAME)) {
+				$pdf->SetXY($posx + 2, $posy + 3);
+				$pdf->SetFont('', 'B', $default_font_size);
+				$pdf->MultiCell($widthrecbox - 2, 4, $outputlangs->convToOutputCharset($this->emetteur->name), 0, 'L');
+				$posy = $pdf->getY();
+			}
 
 			// Show sender information
 			$pdf->SetXY($posx + 2, $posy);
