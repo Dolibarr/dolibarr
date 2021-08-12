4b1275bb3f45a455869009a5ba21fdf5a6bf07a1
Vincent Dieltiens <v.dieltiens@jdc-airports.com>
2021-08-12 14:12:33 +0200

    JDC supplier order template + ref

---
+++ htdocs/core/modules/supplier_order/mod_commande_fournisseur_jdc.php
@@ -0,0 +1,200 @@
+<?php
+/* Copyright (C) 2005-2008 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2009 Regis Houssin        <regis.houssin@inodbox.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * or see https://www.gnu.org/
+ */
+
+/**
+ *    	\file       htdocs/core/modules/supplier_order/mod_commande_fournisseur_muguet.php
+ *		\ingroup    commande
+ *		\brief      Fichier contenant la classe du modele de numerotation de reference de commande fournisseur Muguet
+ */
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/supplier_order/modules_commandefournisseur.php';
+
+
+/**
+ *	Classe du modele de numerotation de reference de commande fournisseur Muguet
+ */
+class mod_commande_fournisseur_jdc extends ModeleNumRefSuppliersOrders
+{
+	/**
+	 * Dolibarr version of the loaded document
+	 * @var string
+	 */
+	public $version = 'dolibarr'; // 'development', 'experimental', 'dolibarr'
+
+	/**
+	 * @var string Error code (or message)
+	 */
+	public $error = '';
+
+	/**
+	 * @var string Nom du modele
+	 * @deprecated
+	 * @see $name
+	 */
+	public $nom = 'jdc';
+
+	/**
+	 * @var string model name
+	 */
+	public $name = 'jdc';
+
+	public $prefix = '';
+
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		global $conf;
+
+		//if ((float) $conf->global->MAIN_VERSION_LAST_INSTALL >= 5.0) $this->prefix = 'PO'; // We use correct standard code "PO = Purchase Order"
+	}
+
+	/**
+	 * 	Return description of numbering module
+	 *
+	 *  @return     string      Text with description
+	 */
+	public function info()
+	{
+		global $langs;
+	  	return $langs->trans("JDCSupplierOrderRefModelDesc", $this->prefix);
+	}
+
+
+	/**
+	 * 	Return an example of numbering
+	 *
+	 *  @return     string      Example
+	 */
+	public function getExample()
+	{
+		return "P21156-PO001";
+	}
+
+
+	/**
+	 *  Checks if the numbers already in the database do not
+	 *  cause conflicts that would prevent this numbering working.
+	 *
+	 *  @return     boolean     false if conflict, true if ok
+	 */
+	public function canBeActivated()
+	{
+		return true;
+		global $conf, $langs, $db;
+
+		$coyymm = ''; $max = '';
+
+		$object->fetch_projet();
+
+                $projectRef = $object->project->ref;
+
+                $start = 9;
+                $sql = "SELECT MAX(CAST(SUBSTRING(ref FROM ".$start.") AS SIGNED)) as max";
+                $sql .= " FROM ".MAIN_DB_PREFIX."command_fournisseur";
+                $sql .= " WHERE ref LIKE '".$db->escape($projectRef)."-%'";
+
+		//$posindice = strlen($this->prefix) + 6;
+		//$sql = "SELECT MAX(CAST(SUBSTRING(ref FROM ".$posindice.") AS SIGNED)) as max";
+		//$sql .= " FROM ".MAIN_DB_PREFIX."commande_fournisseur";
+		//$sql .= " WHERE ref LIKE '".$db->escape($this->prefix)."____-%'";
+		//$sql .= " AND entity = ".$conf->entity;
+
+		$resql = $db->query($sql);
+		if ($resql)
+		{
+			$row = $db->fetch_row($resql);
+			if ($row) { $coyymm = substr($row[0], 0, 6); $max = $row[0]; }
+		}
+
+		if (!$coyymm || preg_match('/'.$this->prefix.'[0-9][0-9][0-9][0-9]/i', $coyymm))
+		{
+			return true;
+		} else {
+			$langs->load("errors");
+			$this->error = $langs->trans('ErrorNumRefModel', $max);
+			return false;
+		}
+	}
+
+	/**
+	 * 	Return next value
+	 *
+	 *  @param	Societe		$objsoc     Object third party
+	 *  @param  Object		$object		Object
+	 *  @return string      			Value if OK, 0 if KO
+	 */
+	public function getNextValue($objsoc = 0, $object = '')
+	{
+		global $db, $conf;
+
+		$object->fetch_projet();
+
+		$projectRef = $object->project->ref;
+
+		$start = 10;
+		$sql = "SELECT MAX(CAST(SUBSTRING(ref FROM ".$start.") AS SIGNED)) as max";
+		$sql .= " FROM ".MAIN_DB_PREFIX."commande_fournisseur";
+		$sql .= " WHERE ref LIKE '".$db->escape($projectRef)."-%'";
+
+		// First, we get the max value
+		//$posindice = strlen($this->prefix) + 6;
+		//$sql = "SELECT MAX(CAST(SUBSTRING(ref FROM ".$posindice.") AS SIGNED)) as max";
+		//$sql .= " FROM ".MAIN_DB_PREFIX."commande_fournisseur";
+		//$sql .= " WHERE ref LIKE '".$db->escape($this->prefix)."____-%'";
+		//$sql .= " AND entity = ".$conf->entity;
+
+		//$date = $object->date_commande; // Not always defined
+                //if (empty($date)) {
+                //      $date = $object->date; // Creation date is order date for suppliers orders
+                //}
+                //$yymm = strftime("%y%m", $date);
+
+
+		$resql = $db->query($sql);
+		if ($resql)
+		{
+			$obj = $db->fetch_object($resql);
+			if ($obj) $max = intval($obj->max);
+			else $max = 0;
+		}
+
+		if ($max >= (pow(10, 4) - 1)) $num = $max + 1; // If counter > 9999, we do not format on 4 chars, we take number as it is
+		else $num = sprintf("%04s", $max + 1);
+
+		return $projectRef."-PO".$num;
+	}
+
+
+	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
+	/**
+	 * 	Renvoie la reference de commande suivante non utilisee
+	 *
+	 *  @param	Societe		$objsoc     Object third party
+	 *  @param  Object	    $object		Object
+	 *  @return string      			Texte descripif
+	 */
+	public function commande_get_num($objsoc = 0, $object = '')
+	{
+		// phpcs:enable
+		return $this->getNextValue($objsoc, $object);
+	}
+}
