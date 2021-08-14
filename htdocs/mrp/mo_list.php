c67c9eb7179c68fb748369fbdcb1b600d4a9b004
Erik van Berkum <erikvanberkum@users.noreply.github.com>
2021-08-14 21:22:07 +0900

    Fix: MO status selector draft showing all values instead of showing only draft values

--- htdocs/mrp/mo_list.php
+++ htdocs/mrp/mo_list.php
@@ -235,7 +235,7 @@ foreach ($search as $key => $val) {
 		}
 		$mode_search = (($object->isInt($object->fields[$key]) || $object->isFloat($object->fields[$key])) ? 1 : 0);
 		if ((strpos($object->fields[$key]['type'], 'integer:') === 0) || (strpos($object->fields[$key]['type'], 'sellist:') === 0) || !empty($object->fields[$key]['arrayofkeyval'])) {
-			if ($search[$key] == '-1' || $search[$key] === '0') {
+			if ($search[$key] == '-1') {
 				$search[$key] = '';
 			}
 			$mode_search = 2;
