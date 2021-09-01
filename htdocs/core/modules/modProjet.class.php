bae73b9a52bf29749ef0a37fca53605672f80e38
Florian HENRY <florian.henry@open-concept.pro>
2021-09-01 15:16:11 +0200

    disabled module on inactive

--- htdocs/core/modules/modProjet.class.php
+++ htdocs/core/modules/modProjet.class.php
@@ -66,7 +66,7 @@ class modProjet extends DolibarrModules
 		// Dependencies
 		$this->hidden = false; // A condition to hide module
 		$this->depends = array(); // List of module class names as string that must be enabled if this module is enabled
-		$this->requiredby = array(); // List of module ids to disable if this one is disabled
+		$this->requiredby = array('modEventOrganization'); // List of module ids to disable if this one is disabled
 		$this->conflictwith = array(); // List of module class names as string this module is in conflict with
 		$this->phpmin = array(5, 6); // Minimum version of PHP required by module
 		$this->langfiles = array('projects');
