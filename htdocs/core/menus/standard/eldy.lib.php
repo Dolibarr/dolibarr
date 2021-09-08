eaf570243a04a1cdfc255f0cf255e3522d950571
MOREAU FRANCK <moreauf@theobald-groupe.com>
2021-09-08 16:22:23 +0200

    Update V14

--- htdocs/core/menus/standard/eldy.lib.php
+++ htdocs/core/menus/standard/eldy.lib.php
@@ -384,7 +384,7 @@ function print_eldy_menu($db, $atarget, $type_user, &$tabMenu, &$menu, $noout = 
 
 	// Tickets and knwoledge base
 	var_dump($conf->knowledgemanagement->enabled);
-	//exit();
+	exit();
 	$tmpentry = array(
 		'enabled'=>(!empty($conf->ticket->enabled) || !empty($conf->knowledgemanagement->enabled)),
 		'perms'=>(!empty($user->rights->ticket->read) || !empty($user->rights->knwoledgemanagement->read)),
