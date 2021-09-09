dd7f6b6a5cd6f08224ef93646632480c8890490b
stickler-ci <support@stickler-ci.com>
2021-09-09 08:17:52 +0000

    Fixing style errors.

--- htdocs/core/tpl/objectline_view.tpl.php
+++ htdocs/core/tpl/objectline_view.tpl.php
@@ -185,7 +185,7 @@ if (($line->info_bits & 2) == 2) {
 		if (!$line->date_start || !$line->date_end) {
 			// show warning under line
 			// we need to fetch product associated to line for some test
-			if ($object->element == 'propal'  || $object->element == 'order' || $object->element == 'propal_supplier' || $object->element == 'supplier_proposal' || $object->element == 'commande')  {
+			if ($object->element == 'propal'  || $object->element == 'order' || $object->element == 'propal_supplier' || $object->element == 'supplier_proposal' || $object->element == 'commande') {
 				$res = $line->fetch_product();
 				if ($res  > 0  ) {
 					if ($line->product->isService() && $line->product->isMandatoryPeriod()) {
