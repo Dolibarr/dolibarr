6583fdc993316822270d21cdf2864f94a9490047
stickler-ci <support@stickler-ci.com>
2021-09-09 06:19:42 +0000

    Fixing style errors.

--- htdocs/admin/pdf.php
+++ htdocs/admin/pdf.php
@@ -162,17 +162,17 @@ if ($action == 'update') {
 	}
 
 	if (GETPOSTISSET('PDF_USE_ALSO_LANGUAGE_CODE')) {
-    dolibarr_set_const($db, "PDF_USE_ALSO_LANGUAGE_CODE", GETPOST('PDF_USE_ALSO_LANGUAGE_CODE', 'alpha'), 'chaine', 0, '', $conf->entity);
-  }
-  
+		dolibarr_set_const($db, "PDF_USE_ALSO_LANGUAGE_CODE", GETPOST('PDF_USE_ALSO_LANGUAGE_CODE', 'alpha'), 'chaine', 0, '', $conf->entity);
+	}
+
 	if (GETPOSTISSET('MAIN_PDF_DISPLAY_DATETEXT')) {
-    dolibarr_set_const($db, "MAIN_PDF_DISPLAY_DATETEXT", GETPOST('MAIN_PDF_DISPLAY_DATETEXT', 'alpha'), 'chaine', 0, '', $conf->entity);
-  }
-  
+		dolibarr_set_const($db, "MAIN_PDF_DISPLAY_DATETEXT", GETPOST('MAIN_PDF_DISPLAY_DATETEXT', 'alpha'), 'chaine', 0, '', $conf->entity);
+	}
+
 	if (GETPOSTISSET('SHOW_SUBPRODUCT_REF_IN_PDF')) {
-    dolibarr_set_const($db, "SHOW_SUBPRODUCT_REF_IN_PDF", GETPOST('SHOW_SUBPRODUCT_REF_IN_PDF', 'alpha'), 'chaine', 0, '', $conf->entity);
-  }
-  
+		dolibarr_set_const($db, "SHOW_SUBPRODUCT_REF_IN_PDF", GETPOST('SHOW_SUBPRODUCT_REF_IN_PDF', 'alpha'), 'chaine', 0, '', $conf->entity);
+	}
+
 	if (GETPOSTISSET('PDF_SHOW_LINK_TO_ONLINE_PAYMENT')) {
 		dolibarr_set_const($db, "PDF_SHOW_LINK_TO_ONLINE_PAYMENT", GETPOST('PDF_SHOW_LINK_TO_ONLINE_PAYMENT', 'alpha'), 'chaine', 0, '', $conf->entity);
 	}
