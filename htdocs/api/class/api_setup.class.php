1cdba9fb807fcfa58e124509218b2b8987363e58
ptibogxiv <support@ptibogxiv.net>
2021-09-25 12:46:27 +0200

    NEW API get list of legal form of business

--- htdocs/api/class/api_setup.class.php
+++ htdocs/api/class/api_setup.class.php
@@ -1278,6 +1278,69 @@ class Setup extends DolibarrApi
 
 		return $list;
 	}
+	
+	/**
+	 * Get the list of legal form of business.
+	 *
+	 * @param string    $sortfield  Sort field
+	 * @param string    $sortorder  Sort order
+	 * @param int       $limit      Number of items per page
+	 * @param int       $page       Page number (starting from zero)
+	 * @param string    $country    To filter on country
+	 * @param int       $active     Lega form is active or not {@min 0} {@max 1}
+	 * @param string    $sqlfilters Other criteria to filter answers separated by a comma. Syntax example "(t.code:like:'A%') and (t.active:>=:0)"
+	 * @return array				List of legal form
+	 *
+	 * @url     GET dictionary/legal_form
+	 *
+	 * @throws RestException
+	 */
+	public function getListOfLegalForm($sortfield = "rowid", $sortorder = 'ASC', $limit = 100, $page = 0, $country = '', $active = 1, $sqlfilters = '')
+	{
+		$list = array();
+
+		//TODO link with multicurrency module
+		$sql = "SELECT t.rowid, t.code, t.fk_pays, t.libelle, t.isvatexempted, t.active, t.module, t.position";
+		$sql .= " FROM ".MAIN_DB_PREFIX."c_forme_juridique as t";
+		$sql .= " WHERE t.active = ".((int) $active);
+		if ($country) {
+			$sql .= " AND t.fk_pays = '".$this->db->escape($country)."'";
+		}
+		// Add sql filters
+		if ($sqlfilters) {
+			if (!DolibarrApi::_checkFilters($sqlfilters)) {
+				throw new RestException(503, 'Error when validating parameter sqlfilters '.$sqlfilters);
+			}
+			$regexstring = '\(([^:\'\(\)]+:[^:\'\(\)]+:[^\(\)]+)\)';
+			$sql .= " AND (".preg_replace_callback('/'.$regexstring.'/', 'DolibarrApi::_forge_criteria_callback', $sqlfilters).")";
+		}
+
+
+		$sql .= $this->db->order($sortfield, $sortorder);
+
+		if ($limit) {
+			if ($page < 0) {
+				$page = 0;
+			}
+			$offset = $limit * $page;
+
+			$sql .= $this->db->plimit($limit, $offset);
+		}
+
+		$result = $this->db->query($sql);
+
+		if ($result) {
+			$num = $this->db->num_rows($result);
+			$min = min($num, ($limit <= 0 ? $num : $limit));
+			for ($i = 0; $i < $min; $i++) {
+				$list[] = $this->db->fetch_object($result);
+			}
+		} else {
+			throw new RestException(503, 'Error when retrieving list of legal form: '.$this->db->lasterror());
+		}
+
+		return $list;
+	}
 
 	/**
 	 * Get the list of social networks.
