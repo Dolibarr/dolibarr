67cc5155b1ffaee23f0070ea6011892b2c555e4d
Philippe GRAND <philippe.grand@atoo-net.com>
2021-09-25 18:15:44 +0200

    fix :Warning: Undefined property: stdClass:: in C:\wamp64\www\dolibarr-dev\htdocs\core\lib\barcode.lib.php on line 62

--- htdocs/core/lib/barcode.lib.php
+++ htdocs/core/lib/barcode.lib.php
@@ -59,7 +59,9 @@ if (empty($font_loc)) {
 if (defined('PHP-BARCODE_PATH_COMMAND')) {
 	$genbarcode_loc = constant('PHP-BARCODE_PATH_COMMAND');
 } else {
-	$genbarcode_loc = $conf->global->GENBARCODE_LOCATION;
+	if (!empty($conf->global->GENBARCODE_LOCATION)) {
+		$genbarcode_loc = $conf->global->GENBARCODE_LOCATION;
+	}
 }
 
 
