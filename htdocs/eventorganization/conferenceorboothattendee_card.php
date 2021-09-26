c02b5b08eaebe788deb10c98762153928d4d71f1
Laurent Destailleur <eldy@destailleur.fr>
2021-09-26 12:58:34 +0200

    Fix trans

--- htdocs/eventorganization/conferenceorboothattendee_card.php
+++ htdocs/eventorganization/conferenceorboothattendee_card.php
@@ -33,7 +33,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/lib/project.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/projet/class/project.class.php';
 
 // Load translation files required by the page
-$langs->loadLangs(array("eventorganization", "other"));
+$langs->loadLangs(array("eventorganization", "other", "projects", "companies"));
 
 // Get parameters
 $id = GETPOST('id', 'int');
@@ -109,6 +109,7 @@ $permissiontodelete = $user->rights->eventorganization->delete || ($permissionto
 $permissionnote = $user->rights->eventorganization->write; // Used by the include of actions_setnotes.inc.php
 $permissiondellink = $user->rights->eventorganization->write; // Used by the include of actions_dellink.inc.php
 $upload_dir = $conf->eventorganization->multidir_output[isset($object->entity) ? $object->entity : 1];
+
 
 /*
  * Actions
