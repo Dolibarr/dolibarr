11723c2cd08fb15ffac8a51e8526fb9fec5c287a
javieralapps4up <javieral@apps4up.com>
2021-09-27 14:49:22 +0200

    Update date.lib.php
    Without words, sorry

--- htdocs/core/lib/date.lib.php
+++ htdocs/core/lib/date.lib.php
@@ -181,9 +181,9 @@ function dol_time_plus_duree($time, $duration_value, $duration_unit)
 		$newtimetotalmonths = (($newtimeyear * 12) + $newtimemonth);
 
 		if ($monthsexpected < $newtimetotalmonths) {
-			$newtimehours = dol_print_date($newtime, '%h');
-			$newtimemins = dol_print_date($newtime, '%i');
-			$newtimesecs = dol_print_date($newtime, '%s');
+			$newtimehours = dol_print_date($newtime, '%H');
+			$newtimemins = dol_print_date($newtime, '%M');
+			$newtimesecs = dol_print_date($newtime, '%S');
 
 			$datelim = dol_mktime($newtimehours, $newtimemins, $newtimesecs, $newtimemonth, 1, $newtimeyear);
 			$datelim -= (3600 * 24);
