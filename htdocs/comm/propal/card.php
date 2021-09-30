0c1ed1ec9f825d3c24a0944907c6e49dbcfdb747
stickler-ci <support@stickler-ci.com>
2021-09-30 10:59:29 +0000

    Fixing style errors.

--- htdocs/comm/propal/card.php
+++ htdocs/comm/propal/card.php
@@ -2025,7 +2025,7 @@ if ($action == 'create') {
 					'
 				);
 			}
-        }
+		}
 
 		if (!empty($conf->notification->enabled)) {
 			require_once DOL_DOCUMENT_ROOT.'/core/class/notify.class.php';
