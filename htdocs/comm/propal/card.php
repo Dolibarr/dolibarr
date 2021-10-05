4059b6428804f5c63c4e8d94beb778230f1d9dee
Marc de Lima Lucio <68746600+marc-dll@users.noreply.github.com>
2021-10-05 13:00:25 +0200

    FIX: deposit payment terms: langs

--- htdocs/comm/propal/card.php
+++ htdocs/comm/propal/card.php
@@ -2018,7 +2018,7 @@ if ($action == 'create') {
 					'type' => 'checkbox',
 					'tdclass' => 'showonlyifsigned',
 					'name' => 'generate_deposit',
-					'label' => $form->textwithpicto($langs->trans('GenerateDeposit', $object->deposit_percent), $langs->trans('PaymentConditionPermitsDepositGenerationSelected'))
+					'label' => $form->textwithpicto($langs->trans('GenerateDeposit', $object->deposit_percent), $langs->trans('DepositGenerationPermittedByThePaymentTermsSelected'))
 				);
 
 				$formquestion[] = array(
--- htdocs/commande/card.php
+++ htdocs/commande/card.php
@@ -2028,7 +2028,7 @@ if ($action == 'create' && $usercancreate) {
 						'type' => 'checkbox',
 						'tdclass' => '',
 						'name' => 'generate_deposit',
-						'label' => $form->textwithpicto($langs->trans('GenerateDeposit', $object->deposit_percent), $langs->trans('PaymentConditionPermitsDepositGenerationSelected'))
+						'label' => $form->textwithpicto($langs->trans('GenerateDeposit', $object->deposit_percent), $langs->trans('DepositGenerationPermittedByThePaymentTermsSelected'))
 					);
 
 					$formquestion[] = array(
