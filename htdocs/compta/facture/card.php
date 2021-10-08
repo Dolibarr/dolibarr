2da16ade6c722e1464a6c8f571beaba150f194d9
atm-florian <florian.mortgat@atm-consulting.fr>
2021-10-08 17:24:52 +0200

    FIX: consistent UX when calling a tab from the invoice card with empty ref / id

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -2756,6 +2756,17 @@ if (empty($reshook))
  * View
  */
 
+if (empty($object->id)) {
+	llxHeader();
+	$head = facture_prepare_head($object);
+	$langs->load('errors');
+	echo dol_get_fiche_head($head, 'compta', $langs->trans("InvoiceCustomer"), -1, 'bill'),
+		'<div class="error">' . $langs->trans("ErrorRecordNotFound") . '</div>';
+	llxFooter();
+	dol_shutdown();
+	exit;
+}
+
 $form = new Form($db);
 $formother = new FormOther($db);
 $formfile = new FormFile($db);
