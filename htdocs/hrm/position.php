0f6d29549b60b329ed0628212993ec43224b4b84
Gauthier PC portable 024 <gauthier.verdol@atm-consulting.fr>
2021-10-08 09:57:33 +0200

    FIX : travis

--- htdocs/hrm/position.php
+++ htdocs/hrm/position.php
@@ -458,7 +458,7 @@ function DisplayPositionList()
 	// Add fields from extrafields
 	if (!empty($extrafields->attributes[$object->table_element]['label'])) {
 		foreach ($extrafields->attributes[$object->table_element]['label'] as $key => $val) {
-			$sql .= ($extrafields->attributes[$object->table_element]['type'][$key] != 'separate' ? ", ef." . $key . ' as options_' . $key . ', ' : '');
+			$sql .= ($extrafields->attributes[$object->table_element]['type'][$key] != 'separate' ? ", ef." . $key . " as options_" . $key . ', ' : '');
 		}
 	}
 	// Add fields from hooks
