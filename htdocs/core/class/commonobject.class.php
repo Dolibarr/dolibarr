ff7727466ad129070c0b56965eb0457f0ddf1795
atm-lena <lena.papazian@atm-consulting.fr>
2021-10-14 17:13:44 +0200

    Function updateExtrafield : replace empty value to null

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -5492,7 +5492,6 @@ abstract class CommonObject
 			}
 
 			$sql .= ")";
-
 			$resql = $this->db->query($sql);
 			if (!$resql)
 			{
@@ -5792,7 +5791,7 @@ abstract class CommonObject
 			}
 
 			if ($linealreadyfound) {
-				if ($this->array_options["options_".$key] === null) {
+				if ($this->array_options["options_".$key] === '') {
 					$sql = "UPDATE ".MAIN_DB_PREFIX.$this->table_element."_extrafields SET ".$key." = null";
 				} else {
 					$sql = "UPDATE ".MAIN_DB_PREFIX.$this->table_element."_extrafields SET ".$key." = '".$this->db->escape($this->array_options["options_".$key])."'";
