ae1da432250d521728e575da33153670fb0afec7
Laurent Destailleur <eldy@destailleur.fr>
2021-10-20 11:30:38 +0200

    Update card.php

--- htdocs/societe/card.php
+++ htdocs/societe/card.php
@@ -1151,7 +1151,7 @@ if (is_object($objcanvas) && $objcanvas->displayCanvasExists($action)) {
                         	$("#typent_id").change();
                         	$("#effectif_id").val(id_ef15);
                         	$("#effectif_id").change();
-							/* Force recompute width of a select2 field when it was hidden and then shown */
+							/* Force to recompute the width of a select2 field when it was hidden and then shown programatically */
 							if ($("#civility_id").data("select2")) {
 								$("#civility_id").select2({width: "resolve"});
 							}
