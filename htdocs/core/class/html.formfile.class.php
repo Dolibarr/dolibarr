a0cd95d099229eb2c68fbcb3114136f778638a20
Philippe Grand <philippe.grand@atoo-net.com>
2021-10-22 09:59:08 +0200

    Update html.formfile.class.php

--- htdocs/core/class/html.formfile.class.php
+++ htdocs/core/class/html.formfile.class.php
@@ -1300,7 +1300,7 @@ class FormFile
 					print '<!-- Line list_of_documents '.$key.' relativepath = '.$relativepath.' -->'."\n";
 					// Do we have entry into database ?
 					print '<!-- In database: position='.$filearray[$key]['position'].' -->'."\n";
-					print '<tr class="oddeven" id="row-'.(!empty($filearray[$key]['rowid']) && $filearray[$key]['rowid'] > 0) ? $filearray[$key]['rowid'] : 'AFTER'.$lastrowid.'POS'.($i + 1)).'">';
+					print '<tr class="oddeven" id="row-'.(!empty($filearray[$key]['rowid']) && $filearray[$key]['rowid'] > 0) ? $filearray[$key]['rowid'] : 'AFTER'.$lastrowid.'POS'.($i + 1).'">';
 
 					// File name
 					print '<td class="minwith200">';
