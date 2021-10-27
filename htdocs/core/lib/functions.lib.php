10649e45dcabbec2109bb503df6769101ecada50
atm-florian <florian.mortgat@atm-consulting.fr>
2021-10-27 18:56:30 +0200

    PARTIAL FIX 14.0 - forbidden functions dol_eval() can be executed

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -8110,14 +8110,15 @@ function dol_eval($s, $returnvalue = 0, $hideerrors = 1)
 	}
 
 	// We block using of php exec or php file functions
-	$forbiddenphpstrings = array("exec(", "passthru(", "shell_exec(", "system(", "proc_open(", "popen(", "eval(", "dol_eval(", "executeCLI(");
-	$forbiddenphpstrings = array_merge($forbiddenphpstrings, array("fopen(", "file_put_contents(", "fputs(", "fputscsv(", "fwrite(", "fpassthru(", "unlink(", "mkdir(", "rmdir(", "symlink(", "touch(", "umask("));
-	$forbiddenphpstrings = array_merge($forbiddenphpstrings, array('function(', '$$', 'call_user_func('));
-	$forbiddenphpstrings = array_merge($forbiddenphpstrings, array('_ENV', '_SESSION', '_COOKIE', '_GET', '_POST', '_REQUEST'));
-	$forbiddenphpregex = 'global\s+\$';
+	$forbiddenphpstrings = array(
+		"exec", "passthru", "shell_exec", "system", "proc_open", "popen", "eval", "dol_eval", "executeCLI",
+		"fopen", "file_put_contents", "fputs", "fputscsv", "fwrite", "fpassthru", "unlink", "mkdir", "rmdir",
+		"symlink", "touch", "umask", 'function', 'call_user_func',
+		'_ENV', '_SESSION', '_COOKIE', '_GET', '_POST', '_REQUEST'
+	);
+	$forbiddenphpregex = 'global\s+\$|\b(' . implode('|', $forbiddenphpstrings) . ')\b|\$\$';
 	do {
 		$oldstringtoclean = $s;
-		$s = str_ireplace($forbiddenphpstrings, '__forbiddenstring__', $s);
 		$s = preg_replace('/'.$forbiddenphpregex.'/', '__forbiddenstring__', $s);
 		//$s = preg_replace('/\$[a-zA-Z0-9_\->\$]+\(/i', '', $s);	// Remove $function( call and $mycall->mymethod(
 	} while ($oldstringtoclean != $s);
