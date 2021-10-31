65d59405da76a97d906fa49e54432d946ea71af6
Laurent Destailleur <eldy@destailleur.fr>
2021-10-31 14:01:56 +0100

    Update document.php

--- htdocs/compta/facture/document.php
+++ htdocs/compta/facture/document.php
@@ -88,8 +88,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/actions_linkedfiles.inc.php';
 if (empty($object->id)) {
 	llxHeader();
 	$langs->load('errors');
-	echo '<div class="error">'.$langs->trans("ErrorRecordNotFound");
-	echo ' <a href="javascript:history.go(-1)">'.$langs->trans('GoBack').'</div>';
+	echo '<div class="error">'.$langs->trans("ErrorRecordNotFound").'</div>';
 	llxFooter();
 	exit;
 }
