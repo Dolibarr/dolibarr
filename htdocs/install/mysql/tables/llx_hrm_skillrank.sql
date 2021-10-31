bc972554c6a254c3aee643cd43cd9dda48c3d156
Frédéric FRANCE <frederic.france@free.fr>
2021-10-31 21:13:15 +0100

    replace reseved word rank

--- htdocs/install/mysql/tables/llx_hrm_skillrank.sql
+++ htdocs/install/mysql/tables/llx_hrm_skillrank.sql
@@ -15,16 +15,15 @@
 -- along with this program.  If not, see https://www.gnu.org/licenses/.
 
 
-CREATE TABLE llx_hrm_skillrank(
-	-- BEGIN MODULEBUILDER FIELDS
-	rowid integer AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	fk_skill integer NOT NULL, 
-	rank integer NOT NULL, 
-	fk_object integer NOT NULL, 
-	date_creation datetime NOT NULL, 
+CREATE TABLE llx_hrm_skillrank
+(
+	rowid integer AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	fk_skill integer NOT NULL,
+	rankorder integer NOT NULL,
+	fk_object integer NOT NULL,
+	date_creation datetime NOT NULL,
 	tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	fk_user_creat integer NOT NULL, 
-	fk_user_modif integer, 
+	fk_user_creat integer NOT NULL,
+	fk_user_modif integer,
 	objecttype varchar(128) NOT NULL
-	-- END MODULEBUILDER FIELDS
 ) ENGINE=innodb;
