06e6479a123c74b11cf732814077e485b8cb184a
Frédéric FRANCE <frederic34@users.noreply.github.com>
2021-11-10 12:24:53 +0100

    Update cashcontrol_card.php

--- htdocs/compta/cashcontrol/cashcontrol_card.php
+++ htdocs/compta/cashcontrol/cashcontrol_card.php
@@ -312,7 +312,7 @@ if ($action == "create" || $action == "start" || $action == 'close') {
 				} elseif ($syear && $smonth && $sday) {
 					$sql .= " AND dateo < '".$db->idate(dol_mktime(0, 0, 0, $smonth, $sday, $syear))."'";
 				} else {
-					dol_print_error('', 'Year not defined');
+					setEventMessages($langs->trans('YearNotDefined'), null, 'errors');
 				}
 
 				$resql = $db->query($sql);
@@ -356,7 +356,7 @@ if ($action == "create" || $action == "start" || $action == 'close') {
 			} elseif ($syear && $smonth && $sday) {
 				$sql .= " AND datef BETWEEN '".$db->idate(dol_mktime(0, 0, 0, $smonth, $sday, $syear))."' AND '".$db->idate(dol_mktime(23, 59, 59, $smonth, $sday, $syear))."'";
 			} else {
-				setEventMessages($langs->trans('Year not defined'), null, 'errors');
+				setEventMessages($langs->trans('YearNotDefined'), null, 'errors');
 			}
 
 			$resql = $db->query($sql);
