ba724204a430cc841a068b777ebf1d6e2ca727cc
Laurent Destailleur <eldy@destailleur.fr>
2021-11-11 15:50:32 +0100

    FIX Bad use of dol_concatdesc()

--- htdocs/datapolicy/class/datapolicy.class.php
+++ htdocs/datapolicy/class/datapolicy.class.php
@@ -259,11 +259,11 @@ class DataPolicy
         {
             if ($sendtocc)
             {
-                 $actionmsg .= dol_concatdesc($actionmsg, $langs->transnoentities('Bcc') . ": " . $sendtocc);
+                 $actionmsg = dol_concatdesc($actionmsg, $langs->transnoentities('Bcc') . ": " . $sendtocc);
             }
-            $actionmsg .= dol_concatdesc($actionmsg, $langs->transnoentities('MailTopic') . ": " . $subject);
-            $actionmsg .= dol_concatdesc($actionmsg, $langs->transnoentities('TextUsedInTheMessageBody') . ":");
-            $actionmsg .= dol_concatdesc($actionmsg, $message);
+            $actionmsg = dol_concatdesc($actionmsg, $langs->transnoentities('MailTopic') . ": " . $subject);
+            $actionmsg = dol_concatdesc($actionmsg, $langs->transnoentities('TextUsedInTheMessageBody') . ":");
+            $actionmsg = dol_concatdesc($actionmsg, $message);
         }
 
         // Send mail
@@ -329,11 +329,11 @@ class DataPolicy
         $actionmsg = $langs->transnoentities('MailSentBy') . ' ' . $from . ' ' . $langs->transnoentities('To') . ' ' . $sendto;
         if ($message) {
             if ($sendtocc) {
-                $actionmsg .= dol_concatdesc($actionmsg, $langs->transnoentities('Bcc') . ": " . $sendtocc);
+                $actionmsg = dol_concatdesc($actionmsg, $langs->transnoentities('Bcc') . ": " . $sendtocc);
             }
-            $actionmsg .= dol_concatdesc($actionmsg, $langs->transnoentities('MailTopic') . ": " . $subject);
-            $actionmsg .= dol_concatdesc($actionmsg, $langs->transnoentities('TextUsedInTheMessageBody') . ":");
-            $actionmsg .= dol_concatdesc($actionmsg, $message);
+            $actionmsg = dol_concatdesc($actionmsg, $langs->transnoentities('MailTopic') . ": " . $subject);
+            $actionmsg = dol_concatdesc($actionmsg, $langs->transnoentities('TextUsedInTheMessageBody') . ":");
+            $actionmsg = dol_concatdesc($actionmsg, $message);
         }
 
 
