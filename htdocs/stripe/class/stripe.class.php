daadcf2fc36662aeb8985771fbbe26c90aedaa2d
stickler-ci <support@stickler-ci.com>
2021-11-12 12:23:40 +0000

    Fixing style errors.

--- htdocs/stripe/class/stripe.class.php
+++ htdocs/stripe/class/stripe.class.php
@@ -411,7 +411,7 @@ class Stripe extends CommonObject
 			}
 			if (!empty($conf->global->STRIPE_KLARNA)) {
 				$paymentmethodtypes[] = "klarna";
-			}      
+			}
 			if (!empty($conf->global->STRIPE_BANCONTACT)) {
 				$paymentmethodtypes[] = "bancontact";
 			}
@@ -609,7 +609,7 @@ class Stripe extends CommonObject
 			}
 			if (!empty($conf->global->STRIPE_KLARNA)) {
 				$paymentmethodtypes[] = "klarna";
-			}      
+			}
 			if (!empty($conf->global->STRIPE_IDEAL)) {
 				$paymentmethodtypes[] = "ideal";
 			}
