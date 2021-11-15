336e5628b9beff3ea889184b79da4b91fd656ad4
ptibogxiv <support@ptibogxiv.net>
2021-11-15 20:39:39 +0100

    Update 14.0.0-15.0.0.sql

--- htdocs/install/mysql/migration/14.0.0-15.0.0.sql
+++ htdocs/install/mysql/migration/14.0.0-15.0.0.sql
@@ -449,7 +449,7 @@ ALTER TABLE llx_socpeople DROP COLUMN youtube;
 ALTER TABLE llx_socpeople DROP COLUMN whatsapp;
 
 INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (100, 'KLA', 'Klarna', 1, 1);
-INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (100, 'SOF', 'Sofort', 1, 1);
-INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (100, 'BAN', 'Bancontact' 1, 1);
-INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (100, 'IDE', 'iDeal' 1, 1);
-INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (100, 'GIR', 'Giropay' 1, 1)
+INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (101, 'SOF', 'Sofort', 1, 1);
+INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (102, 'BAN', 'Bancontact', 1, 1);
+INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (103, 'IDE', 'iDeal', 1, 1);
+INSERT INTO llx_c_paiement (id,code,libelle,type,active) values (104, 'GIR', 'Giropay', 1, 1)
