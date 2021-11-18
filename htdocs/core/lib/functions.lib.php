fe4bc68c7d58160f1a1abb8214994e51e00d744c
DrBarns <info@bolm24.de>
2021-11-18 11:09:37 +0100

    Fixed Tabs and Spaces

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -7294,20 +7294,20 @@ function getCommonSubstitutionArray($outputlangs, $onlykey = 0, $exclude = null,
 	if (empty($exclude) || !in_array('objectamount', $exclude)) {
 		include_once DOL_DOCUMENT_ROOT.'/core/lib/functionsnumtoword.lib.php';
 
-                if (is_object($object)){
-                    $sumpayed = $object->getSommePaiement();
-                    $sumdeposit = $object->getSumDepositsUsed();
-                    $sumcreditnote = $object->getSumCreditNotesUsed();
-                    $already_payed_all = $sumpayed + $sumdeposit + $sumcreditnote;
-                }
+		if (is_object($object)){
+			    $sumpayed = $object->getSommePaiement();
+			    $sumdeposit = $object->getSumDepositsUsed();
+			    $sumcreditnote = $object->getSumCreditNotesUsed();
+			    $already_payed_all = $sumpayed + $sumdeposit + $sumcreditnote;
+		} 
                 
 		$substitutionarray['__DATE_YMD__']        = is_object($object) ? (isset($object->date) ? dol_print_date($object->date, 'day', 0, $outputlangs) : null) : '';
 		$substitutionarray['__DATE_DUE_YMD__']    = is_object($object) ? (isset($object->date_lim_reglement) ? dol_print_date($object->date_lim_reglement, 'day', 0, $outputlangs) : null) : '';
 
 		$substitutionarray['__AMOUNT__']          = is_object($object) ? $object->total_ttc : '';
 		$substitutionarray['__AMOUNT_FORMATED__']          = is_object($object) ? ($object->total_ttc ? price($object->total_ttc, 0, $outputlangs, 0, 0, -1, $conf->currency) : null) : '';
-                $substitutionarray['__AMOUNT_REMAIN__']   = is_object($object) ? $object->total_ttc - $already_payed_all : '';	
-                $substitutionarray['__AMOUNT_REMAIN_FORMATED__'] = is_object($object) ? ($object->total_ttc ? price($object->total_ttc - $already_payed_all, 0, $outputlangs, 0, 0, -1, $conf->currency) : null) : '';
+		$substitutionarray['__AMOUNT_REMAIN__']   = is_object($object) ? $object->total_ttc - $already_payed_all : '';
+		$substitutionarray['__AMOUNT_REMAIN_FORMATED__'] = is_object($object) ? ($object->total_ttc ? price($object->total_ttc - $already_payed_all, 0, $outputlangs, 0, 0, -1, $conf->currency) : null) : '';
 		$substitutionarray['__AMOUNT_TEXT__']     = is_object($object) ? dol_convertToWord($object->total_ttc, $outputlangs, '', true) : '';
 		$substitutionarray['__AMOUNT_TEXTCURRENCY__'] = is_object($object) ? dol_convertToWord($object->total_ttc, $outputlangs, $conf->currency, true) : '';
 		$substitutionarray['__AMOUNT_EXCL_TAX__'] = is_object($object) ? $object->total_ht : '';
