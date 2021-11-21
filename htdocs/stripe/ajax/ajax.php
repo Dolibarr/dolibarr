903939e9222cee8c653a0cbe9bbec614ee99c5e3
ptibogxiv <support@ptibogxiv.net>
2021-11-21 18:31:15 +0100

    Update ajax.php

--- htdocs/stripe/ajax/ajax.php
+++ htdocs/stripe/ajax/ajax.php
@@ -1,5 +1,5 @@
 <?php
- * Copyright (C) 2021		Thibault FOUCART	<support@ptibogxiv.net>
+/* Copyright (C) 2021		Thibault FOUCART	<support@ptibogxiv.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
