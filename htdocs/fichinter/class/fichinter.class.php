d601f621943173642b393ba6e32979792a553bb7
Frédéric FRANCE <frederic.france@free.fr>
2021-11-23 21:38:29 +0100

    can merge products but experimental

--- htdocs/fichinter/class/fichinter.class.php
+++ htdocs/fichinter/class/fichinter.class.php
@@ -1374,6 +1374,23 @@ class Fichinter extends CommonObject
 
 		return CommonObject::commonReplaceThirdparty($db, $origin_id, $dest_id, $tables);
 	}
+
+	/**
+	 * Function used to replace a product id with another one.
+	 *
+	 * @param DoliDB $db Database handler
+	 * @param int $origin_id Old product id
+	 * @param int $dest_id New product id
+	 * @return bool
+	 */
+	public static function replaceProduct(DoliDB $db, $origin_id, $dest_id)
+	{
+		$tables = array(
+			'fichinterdet'
+		);
+
+		return CommonObject::commonReplaceProduct($db, $origin_id, $dest_id, $tables);
+	}
 }
 
 /**
