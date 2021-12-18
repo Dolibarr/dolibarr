fde9a6667f51a11989c824d8c2b86565fb067a7e
Laurent Destailleur <eldy@destailleur.fr>
2021-12-18 15:46:08 +0100

    Fix colspan

--- htdocs/accountancy/admin/account.php
+++ htdocs/accountancy/admin/account.php
@@ -404,6 +404,11 @@ if ($resql) {
 
 	$moreforfilter = '';
 
+	$accountstatic = new AccountingAccount($db);
+	$accountparent = new AccountingAccount($db);
+	$totalarray = array();
+	$totalarray['nbfield'] = 0;
+
 	print '<div class="div-table-responsive">';
 	print '<table class="tagtable liste'.($moreforfilter ? " listwithfilterbefore" : "").'">'."\n";
 
@@ -465,11 +470,6 @@ if ($resql) {
 	}
 	print_liste_field_titre($selectedfields, $_SERVER["PHP_SELF"], "", '', '', '', $sortfield, $sortorder, 'center maxwidthsearch ');
 	print "</tr>\n";
-
-	$accountstatic = new AccountingAccount($db);
-	$accountparent = new AccountingAccount($db);
-	$totalarray = array();
-	$totalarray['nbfield'] = 0;
 
 	$i = 0;
 	while ($i < min($num, $limit)) {
@@ -615,8 +615,13 @@ if ($resql) {
 	}
 
 	if ($num == 0) {
-		$totalarray['nbfield']++;
-		print '<tr><td colspan="'.$totalarray['nbfield'].'"><span class="opacitymedium">'.$langs->trans("None").'</span></td></tr>';
+		$colspan = 1;
+		foreach ($arrayfields as $key => $val) {
+			if (!empty($val['checked'])) {
+				$colspan++;
+			}
+		}
+		print '<tr><td colspan="'.$colspan.'"><span class="opacitymedium">'.$langs->trans("None").'</span></td></tr>';
 	}
 
 	print "</table>";
