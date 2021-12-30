9b14c684112564c82fc5b4887133c2800902f939
Laurent Destailleur <eldy@destailleur.fr>
2021-12-30 19:28:25 +0100

    Update facture.class.php

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -3147,7 +3147,7 @@ class Facture extends CommonInvoice
 		$fk_remise_except = '',
 		$price_base_type = 'HT',
 		$pu_ttc = 0,
-		$type = Product::TYPE_PRODUCT,
+		$type = 0,
 		$rang = -1,
 		$special_code = 0,
 		$origin = '',
