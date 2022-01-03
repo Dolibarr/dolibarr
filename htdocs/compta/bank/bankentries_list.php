64c74729651635f3fa1907d2ebc533b86d722ea9
Laurent Destailleur <eldy@destailleur.fr>
2022-01-03 22:01:08 +0100

    Fix link to account id

--- htdocs/compta/bank/bankentries_list.php
+++ htdocs/compta/bank/bankentries_list.php
@@ -1682,7 +1682,7 @@ if ($resql) {
 				}
 			}
 			if ($user->rights->banque->modifier) {
-				print '<a href="'.$_SERVER["PHP_SELF"].'?action=delete&amp;token='.newToken().'&amp;rowid='.$objp->rowid.'&amp;id='.$objp->bankid.'&amp;page='.$page.'">';
+				print '<a href="'.$_SERVER["PHP_SELF"].'?action=delete&token='.newToken().'&rowid='.$objp->rowid.'&page='.$page.'">';
 				print img_delete('', 'class="marginleftonly"');
 				print '</a>';
 			}
