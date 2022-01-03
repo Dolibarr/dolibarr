26c6908f667a00cd00737d26345e1917b3a1e82f
Ferran Marcet <fmarcet@2byte.es>
2022-01-03 13:22:07 +0100

    Fix: Correctly display product translations

--- htdocs/product/stock/movement_list.php
+++ htdocs/product/stock/movement_list.php
@@ -3,7 +3,7 @@
  * Copyright (C) 2004-2017	Laurent Destailleur		<eldy@users.sourceforge.net>
  * Copyright (C) 2005-2014	Regis Houssin			<regis.houssin@inodbox.com>
  * Copyright (C) 2015		Juanjo Menent			<jmenent@2byte.es>
- * Copyright (C) 2018		Ferran Marcet			<fmarcet@2byte.es>
+ * Copyright (C) 2018-2022	Ferran Marcet			<fmarcet@2byte.es>
  * Copyright (C) 2019       Frédéric France         <frederic.france@netlogic.fr>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -1000,6 +1000,23 @@ if ($resql)
     while ($i < min($num, $limit))
     {
         $objp = $db->fetch_object($resql);
+
+		// Multilangs
+		if (!empty($conf->global->MAIN_MULTILANGS))  // If multilang is enabled
+		{
+			$sql = "SELECT label";
+			$sql .= " FROM ".MAIN_DB_PREFIX."product_lang";
+			$sql .= " WHERE fk_product=".$objp->rowid;
+			$sql .= " AND lang='".$db->escape($langs->getDefaultLang())."'";
+			$sql .= " LIMIT 1";
+
+			$result = $db->query($sql);
+			if ($result)
+			{
+				$objtp = $db->fetch_object($result);
+				if (!empty($objtp->label)) $objp->produit = $objtp->label;
+			}
+		}
 
         $userstatic->id = $objp->fk_user_author;
         $userstatic->login = $objp->login;
