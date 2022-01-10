484ab8a4e96ab27ccc09188a62a848fd12231b82
Adrien Raze <adrien.raze@atm-consulting.fr>
2022-01-10 14:14:08 +0100

    FIX : Update llx_facture_fourn_rec_det.sql to modify date fields type

--- htdocs/install/mysql/tables/llx_facture_fourn_det_rec.sql
+++ htdocs/install/mysql/tables/llx_facture_fourn_det_rec.sql
@@ -41,8 +41,8 @@ create table llx_facture_fourn_det_rec
   total_localtax2	double(24,8) DEFAULT 0,		-- total LocalTax2 for total quantity of line
   total_ttc			double(24,8),					-- Total TTC de la ligne toute quantity et incluant remise ligne et globale
   product_type		integer DEFAULT 0,
-  date_start        datetime   DEFAULT NULL,       -- date debut si service
-  date_end          datetime   DEFAULT NULL,       -- date fin si service
+  date_start        integer DEFAULT NULL,       -- date debut si service
+  date_end          integer DEFAULT NULL,       -- date fin si service
   info_bits			integer DEFAULT 0,				-- TVA NPR ou non
   special_code		integer UNSIGNED DEFAULT 0,		-- code for special lines
   rang				integer DEFAULT 0,				-- ordre d'affichage
