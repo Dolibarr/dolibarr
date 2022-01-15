45666e7f0a7005751320abadcc63ef9d856fee01
stickler-ci <support@stickler-ci.com>
2022-01-15 21:45:20 +0000

    Fixing style errors.

--- htdocs/emailcollector/class/emailcollector.class.php
+++ htdocs/emailcollector/class/emailcollector.class.php
@@ -2365,7 +2365,7 @@ class EmailCollector extends CommonObject
 						// this code action is hook..... for support this call
 						global $hookmanager;
 
-						if ( ! in_array('emailcollectorcard',$hookmanager->hooks)) {
+						if ( ! in_array('emailcollectorcard', $hookmanager->hooks)) {
 							$hookmanager->initHooks(array('emailcollectorcard'));
 						}
 
