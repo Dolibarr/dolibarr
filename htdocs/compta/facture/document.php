85affdd82b400758858a1a11768a9123e650b36b
Atm-Gregr <greg.rastklan@atm-consulting.fr>
2022-01-21 14:28:14 +0100

    Intégration complète du commit 2bf63bc75e39b68af5ee508a695fdec0f64097c2

--- htdocs/compta/facture/document.php
+++ htdocs/compta/facture/document.php
@@ -112,10 +112,11 @@ if ($id > 0 || !empty($ref)) {
 	if ($object->fetch($id, $ref) > 0) {
 		$object->fetch_thirdparty();
 
-		$upload_dir = $conf->facture->multidir_output[$object->entity].'/'.dol_sanitizeFileName($object->ref);
+        #$upload_dir = $conf->facture->multidir_output[$object->entity].'/'.dol_sanitizeFileName($object->ref);
+        #if($object->entity == 1)$upload_dir= str_replace('invoice','facture',$upload_dir);
+        #else $upload_dir= str_replace('facture','invoice',$upload_dir);
 
-        if($object->entity == 1)$upload_dir= str_replace('invoice','facture',$upload_dir);
-        else $upload_dir= str_replace('facture','invoice',$upload_dir);
+        $upload_dir = $conf->facture->dir_output.'/'.dol_sanitizeFileName($object->ref);
 
 		$head = facture_prepare_head($object);
 		print dol_get_fiche_head($head, 'documents', $langs->trans('InvoiceCustomer'), -1, 'bill');
