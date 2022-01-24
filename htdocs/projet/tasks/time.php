ebda8d52f016c2d8db259d8e89dbcdd5d805d92b
stickler-ci <support@stickler-ci.com>
2022-01-24 22:30:25 +0000

    Fixing style errors.

--- htdocs/projet/tasks/time.php
+++ htdocs/projet/tasks/time.php
@@ -285,7 +285,6 @@ if (($action == 'updateline' || $action == 'updatesplitline') && !$cancel && $us
 			}
 
 			$result = $object->addTimeSpent($user);
-
 		} else {
 			$object->fetch($id, $ref);
 
@@ -1794,7 +1793,6 @@ if (($id > 0 || !empty($ref)) || $projectidforalltimes > 0 || $allprojectforuser
 					if ($resultFetch < 0) {
 						setEventMessages($product->error, $product->errors, 'errors');
 					} else {
-
 						print $product->getNomUrl(1);
 					}
 				}
