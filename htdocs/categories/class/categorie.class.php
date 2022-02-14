a3c1c707a438bdfac186f2317e514cdab9fe0e06
lmarcouiller <lucas.marcouiller@gmail.com>
2022-02-14 12:09:57 +0100

    Close #19930 : tag management in KM list

--- htdocs/categories/class/categorie.class.php
+++ htdocs/categories/class/categorie.class.php
@@ -185,7 +185,8 @@ class Categorie extends CommonObject
 		'contact'  => 'socpeople',
 		'account'  => 'bank_account', // old for bank account
 		'project'  => 'projet',
-		'warehouse'=> 'entrepot'
+		'warehouse'=> 'entrepot',
+		'knowledgemanagement' => 'knowledgemanagement_knowledgerecord'
 	);
 
 	/**
