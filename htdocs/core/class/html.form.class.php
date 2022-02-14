3305965c854cda61e9742e9b5ede6b7002c6d4d9
Adrien Raze <adrien.raze@atm-consulting.fr>
2022-02-14 15:04:12 +0100

    FIX : Error with selectForForms function

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -7379,10 +7379,7 @@ class Form
 
 		$objecttmp = null;
 
-		$InfoFieldList = explode(":", $objectdesc, 4);
-		$vartmp = $InfoFieldList[3];
-		$InfoFieldList[4] = preg_replace('/^.*:(\w*)$/', '\1', $vartmp);	// take the sort field
-		$InfoFieldList[3] = preg_replace('/:\w*$/', '', $vartmp);			// take the filter field
+		$InfoFieldList = explode(":", $objectdesc);
 
 		$classname = $InfoFieldList[0];
 		$classpath = $InfoFieldList[1];
