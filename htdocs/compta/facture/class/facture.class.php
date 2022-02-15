01ef9b64eebecc2660495a48a74bb7fd5b369a55
Christophe Battarel <christophe@altairis.fr>
2022-02-15 10:34:10 +0100

    fix parent line on create with no originid

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -790,7 +790,7 @@ class Facture extends CommonInvoice
 						$result = $newinvoiceline->insert();
 
 						// Defined the new fk_parent_line
-						if ($result > 0 && $newinvoiceline->product_type == 9) {
+						if ($result > 0) {
 							$fk_parent_line = $result;
 						}
 					}
