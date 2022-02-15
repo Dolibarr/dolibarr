ef04ce1e05b302379fbf09875ee7ac54f08c6cb2
Frédéric FRANCE <frederic34@users.noreply.github.com>
2022-02-15 17:10:32 +0100

    Update fichinterrec.class.php

--- htdocs/fichinter/class/fichinterrec.class.php
+++ htdocs/fichinter/class/fichinterrec.class.php
@@ -653,6 +653,15 @@ class FichinterRec extends Fichinter
 		if ($withpicto != 2) {
 			$result .= $link.$this->ref.$linkend;
 		}
+		global $action;
+		$hookmanager->initHooks(array($this->element . 'dao'));
+		$parameters = array('id'=>$this->id, 'getnomurl'=>$result);
+		$reshook = $hookmanager->executeHooks('getNomUrl', $parameters, $this, $action); // Note that $action and $object may have been modified by some hooks
+		if ($reshook > 0) {
+			$result = $hookmanager->resPrint;
+		} else {
+			$result .= $hookmanager->resPrint;
+		}
 		return $result;
 	}
 
