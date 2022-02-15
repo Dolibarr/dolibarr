7341e7ed0768f7fa97bcf310475304673a87982a
Frédéric FRANCE <frederic34@users.noreply.github.com>
2022-02-15 17:06:04 +0100

    Update don.class.php

--- htdocs/don/class/don.class.php
+++ htdocs/don/class/don.class.php
@@ -953,7 +953,15 @@ class Don extends CommonObject
 			$result .= $this->ref;
 		}
 		$result .= $linkend;
-
+		global $action;
+		$hookmanager->initHooks(array($this->element . 'dao'));
+		$parameters = array('id'=>$this->id, 'getnomurl'=>$result);
+		$reshook = $hookmanager->executeHooks('getNomUrl', $parameters, $this, $action); // Note that $action and $object may have been modified by some hooks
+		if ($reshook > 0) {
+			$result = $hookmanager->resPrint;
+		} else {
+			$result .= $hookmanager->resPrint;
+		}
 		return $result;
 	}
 
