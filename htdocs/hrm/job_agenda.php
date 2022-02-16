038298a638f88200d11688b1624cf987b5005de8
Atm-Gregr <greg.rastklan@atm-consulting.fr>
2022-02-16 16:10:48 +0100

    change button and add title

--- htdocs/hrm/job_agenda.php
+++ htdocs/hrm/job_agenda.php
@@ -189,17 +189,12 @@ if ($object->id > 0) {
 	}
 
 
-	print '<div class="tabsAction">';
-
 	if (!empty($conf->agenda->enabled)) {
 		if (!empty($user->rights->agenda->myactions->create) || !empty($user->rights->agenda->allactions->create)) {
-			print '<a class="butAction" href="'.DOL_URL_ROOT.'/comm/action/card.php?action=create'.$out.'">'.$langs->trans("AddAction").'</a>';
-		} else {
-			print '<a class="butActionRefused classfortooltip" href="#">'.$langs->trans("AddAction").'</a>';
-		}
+            $newcardbutton = '';
+            $newcardbutton .= dolGetButtonTitle($langs->trans('AddAction'), '', 'fa fa-plus-circle', DOL_URL_ROOT.'/comm/action/card.php?action=create'.$out);
+        }
 	}
-
-	print '</div>';
 
 	if (!empty($conf->agenda->enabled) && (!empty($user->rights->agenda->myactions->read) || !empty($user->rights->agenda->allactions->read))) {
 		$param = '&id='.$object->id.'&socid='.$socid;
@@ -211,7 +206,7 @@ if ($object->id > 0) {
 		}
 
 
-		//print load_fiche_titre($langs->trans("ActionsOnJob"), '', '');
+        print load_fiche_titre($langs->trans("ActionsOnJob"), $newcardbutton, '');
 
 		// List of all actions
 		$filters = array();
