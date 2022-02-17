c20d82499920c1c0028d681700e7b80770e884ac
Laurent Destailleur <eldy@destailleur.fr>
2022-02-17 19:30:18 +0100

    Tables of module ticket are create only when module is enabled.

--- htdocs/core/modules/modTicket.class.php
+++ htdocs/core/modules/modTicket.class.php
@@ -320,6 +320,11 @@ class modTicket extends DolibarrModules
 	{
 		global $conf, $langs;
 
+		$result = $this->_load_tables('/install/mysql/tables/', 'ticket');
+		if ($result < 0) {
+			return -1; // Do not activate module if error 'not allowed' returned when loading module SQL queries (the _load_table run sql with run_sql with the error allowed parameter set to 'default')
+		}
+
 		// Permissions
 		$this->remove($options);
 
