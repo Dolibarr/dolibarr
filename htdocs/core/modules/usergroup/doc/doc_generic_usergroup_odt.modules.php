dee5d8f9e865a051617c23162b75bd6174fad059
Laurent Destailleur <eldy@destailleur.fr>
2022-02-18 13:45:28 +0100

    Standardize name of button

--- htdocs/core/modules/usergroup/doc/doc_generic_usergroup_odt.modules.php
+++ htdocs/core/modules/usergroup/doc/doc_generic_usergroup_odt.modules.php
@@ -163,7 +163,7 @@ class doc_generic_usergroup_odt extends ModelePDFUserGroup
 		$texte .= $conf->global->USERGROUP_ADDON_PDF_ODT_PATH;
 		$texte .= '</textarea>';
 		$texte .= '</div><div style="display: inline-block; vertical-align: middle;">';
-		$texte .= '<input type="submit" class="button small reposition" name="Button" value="'.$langs->trans("Modify").'">';
+		$texte .= '<input type="submit" class="button small reposition" name="modify" value="'.$langs->trans("Modify").'">';
 		$texte .= '<br></div></div>';
 
 		// Scan directories
