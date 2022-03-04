4d38aedeb25dd2e4b0ec302b3e5a963e7cb658d3
eldk <webmaster@opalesurfcasting.net>
2022-03-04 18:13:36 +0100

    Update index.php

--- htdocs/api/index.php
+++ htdocs/api/index.php
@@ -174,18 +174,19 @@ if (!empty($conf->global->API_RESTRICT_ON_IP)) {
 	$allowedip = explode(' ', $conf->global->API_RESTRICT_ON_IP);
 
 	// allow the use of FQDN for dynamic dns access
-	for ($i = 0; $i < count($allowedip); $i++)
-		// if submitted host is not an IP but some FQDN
-	if (!preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $allowedip[$i])
-			&& preg_match('/^[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*\.$/', $allowedip[$i])) {
-		// set the IP of the host
-		$allowedip[$i] = gethostbyname($allowedip[$i]);
-		//  if submitted host is not an IP removes it
-	} elseif (!preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $allowedip[$i])) {
-		unset($allowedip[$i]);
-	}
-}
-
+	$count = count($allowedip);
+    for ($i = 0; $i < $count; $i++) {
+        // if submitted host is not an IP but some FQDN
+        if (!preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $allowedip[$i])
+            && preg_match('/^[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*\.$/', $allowedip[$i])) {
+            // set the IP of the host
+            $allowedip[$i] = gethostbyname($allowedip[$i]);
+            //  if submitted host is not an IP removes it
+        } else if (!preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $allowedip[$i])) {
+            unset($allowedip[$i]);
+        }
+    }
+	
 	$ipremote = getUserRemoteIP();
 if (!in_array($ipremote, $allowedip)) {
 	dol_syslog('Remote ip is '.$ipremote.', not into list '.$conf->global->API_RESTRICT_ON_IP);
