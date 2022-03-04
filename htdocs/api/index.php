41abb502cd57dc04bdd98670f799da381d321607
eldk <webmaster@opalesurfcasting.net>
2022-03-04 14:41:06 +0100

    Add use of FQDN in REST API allowed client list
    https://github.com/Dolibarr/dolibarr/issues/20254

--- htdocs/api/index.php
+++ htdocs/api/index.php
@@ -172,6 +172,23 @@ UploadFormat::$allowedMimeTypes = array('image/jpeg', 'image/png', 'text/plain',
 // Restrict API to some IPs
 if (!empty($conf->global->API_RESTRICT_ON_IP)) {
 	$allowedip = explode(' ', $conf->global->API_RESTRICT_ON_IP);
+	
+	// allow the use of FQDN for dynamic dns access
+	for ($i = 0; $i < count($allowedip); $i++) {
+        echo ' Submitted host : ' . $allowedip[$i];
+        // if submitted host is not an IP but some FQDN
+        if (!preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $allowedip[$i])
+            && preg_match('/^[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*\.[a-zA-Z0-9-]*\.$/', $allowedip[$i])) {
+            // set the IP of the host
+            $allowedip[$i] = gethostbyname($allowedip[$i]);
+            echo ' Host from name : ' . $allowedip[$i];
+            //  if submitted host is not an IP removes it
+        } else if (!preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', $allowedip[$i])) {
+            echo ' Host removed : ' . $allowedip[$i];
+            unset($allowedip[$i]);
+        }
+    }
+	
 	$ipremote = getUserRemoteIP();
 	if (!in_array($ipremote, $allowedip)) {
 		dol_syslog('Remote ip is '.$ipremote.', not into list '.$conf->global->API_RESTRICT_ON_IP);
