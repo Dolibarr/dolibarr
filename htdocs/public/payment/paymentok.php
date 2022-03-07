d610ef3e8f6c3fd5514ab4908bf02f1960eb5293
daraelmin <daraelmin@gmail.com>
2022-03-07 15:05:20 +0100

    Fix #17317 - Add default label for online subscription
    #Fix #17317 Subscription label is wrong when member pay online on renew and new form
    Add default label for subscription before payment info if specified in module setup

--- htdocs/public/payment/paymentok.php
+++ htdocs/public/payment/paymentok.php
@@ -445,6 +445,8 @@ if ($ispaymentok) {
 
 				$paymentdate = $now;
 				$amount = $FinalPaymentAmt;
+				// add default label for subscription before paymnetinfo if specified in module setup
+				$label = (empty($conf->global->MEMBER_NO_DEFAULT_LABEL)) ? $langs->trans("Subscription").' '.dol_print_date(($datefrom ? $datefrom : time()), "%Y") . ' - ': '';
 				$label = 'Online subscription '.dol_print_date($now, 'standard').' using '.$paymentmethod.' from '.$ipaddress.' - Transaction ID = '.$TRANSACTIONID;
 
 				// Payment informations
