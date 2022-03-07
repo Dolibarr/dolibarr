7847e049259c9520fd9766c7815a1a86678c6bf2
Laurent Destailleur <eldy@destailleur.fr>
2022-03-07 19:45:00 +0100

    CSS

--- htdocs/multicurrency/multicurrency_rate.php
+++ htdocs/multicurrency/multicurrency_rate.php
@@ -238,11 +238,11 @@ $help_url = '';
 llxHeader('', $title, $help_url, '');
 // Subheader
 $linkback = '<a href="'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans("BackToModuleList").'</a>';
-print load_fiche_titre($langs->trans($page_name), $linkback);
+print load_fiche_titre($langs->trans($page_name), $linkback, 'multicurrency');
 
 // Configuration header
 $head = multicurrencyAdminPrepareHead();
-print dol_get_fiche_head($head, 'ratelist', $langs->trans("ModuleSetup"), -1, "multicurrency");
+print dol_get_fiche_head($head, 'ratelist', '', -1, "");
 
 // ACTION
 
