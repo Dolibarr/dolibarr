feafbaded21f2f5575af4416d04a79a6d1464f37
Laurent Destailleur <eldy@destailleur.fr>
2022-03-07 14:31:19 +0100

    Update info.php

--- htdocs/projet/info.php
+++ htdocs/projet/info.php
@@ -62,12 +62,13 @@ if (GETPOST('actioncode', 'array')) {
 }
 $search_agenda_label = GETPOST('search_agenda_label');
 
+$hookmanager->initHooks(array('projectcardinfo'));
+
 // Security check
 $id = GETPOST("id", 'int');
 $socid = 0;
 //if ($user->socid > 0) $socid = $user->socid;    // For external user, no check is done on company because readability is managed by public status of project and assignement.
 $result = restrictedArea($user, 'projet', $id, 'projet&project');
-$hookmanager->initHooks(array('projectcardinfo'));
 
 if (!$user->rights->projet->lire) {
 	accessforbidden();
