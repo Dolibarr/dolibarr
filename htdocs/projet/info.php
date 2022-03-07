2ef42c178b96b7746e2d21c6b579a6c12b9568e0
Laurent Destailleur <eldy@destailleur.fr>
2022-03-07 14:30:38 +0100

    Update info.php

--- htdocs/projet/info.php
+++ htdocs/projet/info.php
@@ -67,7 +67,7 @@ $id = GETPOST("id", 'int');
 $socid = 0;
 //if ($user->socid > 0) $socid = $user->socid;    // For external user, no check is done on company because readability is managed by public status of project and assignement.
 $result = restrictedArea($user, 'projet', $id, 'projet&project');
-$hookmanager->initHooks(array('projetcardinfo'));
+$hookmanager->initHooks(array('projectcardinfo'));
 
 if (!$user->rights->projet->lire) {
 	accessforbidden();
