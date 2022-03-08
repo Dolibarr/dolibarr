eae3fe78f140e1b7e0f30c1112c6d75867686b36
BB2A Anthony Berton <bertonanthony@gmail.com>
2022-03-08 18:06:26 +0100

    init

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -8750,7 +8750,7 @@ function getLanguageCodeFromCountryCode($countrycode)
  */
 function complete_head_from_modules($conf, $langs, $object, &$head, &$h, $type, $mode = 'add')
 {
-	global $hookmanager;
+	global $hookmanager, $db;
 
 	if (isset($conf->modules_parts['tabs'][$type]) && is_array($conf->modules_parts['tabs'][$type])) {
 		foreach ($conf->modules_parts['tabs'][$type] as $value) {
@@ -8771,7 +8771,18 @@ function complete_head_from_modules($conf, $langs, $object, &$head, &$h, $type, 
 							complete_substitutions_array($substitutionarray, $langs, $object, array('needforkey'=>$values[2]));
 							$label = make_substitutions($reg[1], $substitutionarray);
 						} else {
-							$label = $langs->trans($values[2]);
+							$labeltemp = explode(',', $values[2]);
+							if (is_array($labeltemp)) {
+								$label = $langs->trans($labeltemp[0]);
+								if (!empty($labeltemp[1]) && is_object($object) && !empty($object->id)) {
+									dol_include_once($labeltemp[2]);
+									$obj = new $labeltemp[1]($db);
+									$function = $labeltemp[3];
+									$label .= $obj->$function($object->id, $obj);
+								}
+							} else {
+								$label = $langs->trans($values[2]);
+							}
 						}
 
 						$head[$h][0] = dol_buildpath(preg_replace('/__ID__/i', ((is_object($object) && !empty($object->id)) ? $object->id : ''), $values[5]), 1);
