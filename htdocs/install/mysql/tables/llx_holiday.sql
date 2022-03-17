8f7ea5d2a31861aa7bd20cd0121dfe5fce82d337
BENKE Charlene <1179011+defrance@users.noreply.github.com>
2022-03-17 18:13:50 +0100

    Update llx_holiday.sql

--- htdocs/install/mysql/tables/llx_holiday.sql
+++ htdocs/install/mysql/tables/llx_holiday.sql
@@ -31,7 +31,8 @@ description    VARCHAR( 255 ) NOT NULL,
 date_debut     DATE NOT NULL,
 date_fin       DATE NOT NULL,
 halfday        integer DEFAULT 0,				-- 0=start morning and end afternoon, -1=start afternoon end afternoon, 1=start morning and end morning, 2=start afternoon and end morning
-statut         integer NOT NULL DEFAULT '1',
+nb_open_day    double(24,8)     DEFAULT 0,
+statut         integer NOT NULL DEFAULT 1,
 fk_validator   integer NOT NULL,				-- who should approve
 date_valid     DATETIME DEFAULT NULL,			-- date approval (both date valid and date_approval)
 fk_user_valid  integer DEFAULT NULL,			-- user approval (both user valid and user that approved)
