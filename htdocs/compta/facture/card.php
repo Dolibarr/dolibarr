6dfad5d69fc97ae12f681e1ab974db66dc621b3d
Thomas Negre <tnegre@open-dsi.fr>
2022-03-24 11:09:39 +0100

    facture/card.php: replace redirection by  = ''

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -306,8 +306,7 @@ if (empty($reshook)) {
 			$last_of_type = $object->willBeLastOfSameType();
 			if (empty($object->date_validation) && !$last_of_type[0]) {
 				setEventMessages($langs->transnoentities("ErrorInvoiceIsNotLastOfSameType", $object->ref, dol_print_date($object->date, 'day'), dol_print_date($last_of_type[1], 'day')), null, 'errors');
-				header('Location: '.$_SERVER["PHP_SELF"].'?facid='.$id);
-				exit;
+				$action = '';
 			}
 		}
 
