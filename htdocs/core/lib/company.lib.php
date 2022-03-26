97a1774bf3a55fec7e92e563f934103f5027fc7a
Regis Houssin <regis.houssin@inodbox.com>
2022-03-26 12:57:30 +0100

    NEW add hooks contact tab badge and hooks parameter for avoid conflicts

--- htdocs/core/lib/company.lib.php
+++ htdocs/core/lib/company.lib.php
@@ -65,6 +65,15 @@ function societe_prepare_head(Societe $object)
 				$sql = "SELECT COUNT(p.rowid) as nb";
 				$sql .= " FROM ".MAIN_DB_PREFIX."socpeople as p";
 				$sql .= " WHERE p.fk_soc = ".((int) $object->id);
+				// Add table from hooks
+				$parameters = array('contacttab' => true);
+				$reshook = $hookmanager->executeHooks('printFieldListFrom', $parameters, $object); // Note that $action and $object may have been modified by hook
+				$sql .= $hookmanager->resPrint;
+				$sql .= " WHERE p.fk_soc = ".$object->id;
+				// Add where from hooks
+				$parameters = array('contacttab' => true);
+				$reshook = $hookmanager->executeHooks('printFieldListWhere', $parameters, $object); // Note that $action and $object may have been modified by hook
+				$sql .= $hookmanager->resPrint;
 				$resql = $db->query($sql);
 				if ($resql) {
 					$obj = $db->fetch_object($resql);
