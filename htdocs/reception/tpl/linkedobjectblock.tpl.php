5f6c5ccf926c38dce51a2a4fe3154530ba9ced9a
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2022-04-04 16:25:31 +0200

    NEW Supplier order - Show ref supplier of reception in linked object block

--- htdocs/reception/tpl/linkedobjectblock.tpl.php
+++ htdocs/reception/tpl/linkedobjectblock.tpl.php
@@ -56,7 +56,7 @@ foreach ($linkedObjectBlock as $key => $objectlink) {
 		} ?>
 		</td>
 		<td class="linkedcol-name nowraponall" ><?php echo $objectlink->getNomUrl(1); ?></td>
-		<td class="linkedcol-ref"></td>
+		<td class="linkedcol-ref"><?php echo $objectlink->ref_supplier; ?></td>
 		<td class="linkedcol-date"><?php echo dol_print_date($objectlink->date_delivery, 'day'); ?></td>
 		<td class="linkedcol-amount right"><?php
 		if ($user->rights->reception->lire) {
