db8497974eee92eefe998202c29ec5fd6557e859
John BOTELLA <john.botella@atm-consulting.fr>
2022-04-05 11:40:23 +0200

    FIX SPE ARCOOP : MENU LEFT MULTICOMPANY_BACKWARD_COMPATIBILITY

--- htdocs/core/class/menubase.class.php
+++ htdocs/core/class/menubase.class.php
@@ -636,7 +636,14 @@ class Menubase
 
 		$sql = "SELECT m.rowid, m.type, m.module, m.fk_menu, m.fk_mainmenu, m.fk_leftmenu, m.url, m.titre, m.prefix, m.langs, m.perms, m.enabled, m.target, m.mainmenu, m.leftmenu, m.position";
 		$sql .= " FROM ".MAIN_DB_PREFIX."menu as m";
-		$sql .= " WHERE m.entity IN (0,".$conf->entity.")";
+
+		$sql .= " WHERE ";
+		if (!empty($conf->global->MULTICOMPANY_BACKWARD_COMPATIBILITY)) {
+			$sql .= " m.entity IN (0,".(!empty($conf->multicompany->enabled) && !empty($conf->global->MULTICOMPANY_TRANSVERSE_MODE) ? "1," : "").$conf->entity.")";
+		} else {
+			$sql .= " m.entity IN (0,".$conf->entity.")";
+		}
+
 		$sql .= " AND m.menu_handler IN ('".$this->db->escape($menu_handler)."','all')";
 		if ($type_user == 0) {
 			$sql .= " AND m.usertype IN (0,2)";
