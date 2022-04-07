06f8af71effc40ba3b04a2c6ebacda1839bbb4d2
Laurent Destailleur <eldy@destailleur.fr>
2022-04-07 11:24:39 +0200

    Code comment

--- htdocs/install/mysql/data/llx_c_paiement.sql
+++ htdocs/install/mysql/data/llx_c_paiement.sql
@@ -37,6 +37,7 @@ insert into llx_c_paiement (id,code,libelle,type,active) values ( 3, 'PRE', 'Deb
 insert into llx_c_paiement (id,code,libelle,type,active) values ( 4, 'LIQ', 'Cash',              2, 1);
 insert into llx_c_paiement (id,code,libelle,type,active) values ( 6, 'CB',  'Credit card',       2, 1);
 insert into llx_c_paiement (id,code,libelle,type,active) values ( 7, 'CHQ', 'Cheque',            2, 1);
+-- Alternative not common payment modes
 insert into llx_c_paiement (id,code,libelle,type,active) values (50, 'VAD', 'Online payment',    2, 0);
 insert into llx_c_paiement (id,code,libelle,type,active) values (51, 'TRA', 'Traite',            2, 0);
 insert into llx_c_paiement (id,code,libelle,type,active) values (52, 'LCR', 'LCR',               2, 0);
