dabc898d80440574ac11b419393c2721c8586d2c
Inovea Conseil <inoveaconseil@users.noreply.github.com>
2022-04-27 16:45:15 +0200

    Revert "NEW Events on Proposal to Return to Draft"

--- htdocs/install/mysql/data/llx_const.sql
+++ htdocs/install/mysql/data/llx_const.sql
@@ -107,3 +107,4 @@ INSERT INTO llx_const (name, entity, value, type, visible) VALUES ('PRODUCT_PRIC
 -- Membership
 --
 INSERT INTO llx_const (name, entity, value, type, visible) VALUES ('ADHERENT_LOGIN_NOT_REQUIRED', 0, '1', 'string', 0);
+
