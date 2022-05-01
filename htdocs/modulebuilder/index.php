e2bf59658966716cf1f42eb4d48ce7c3d0739793
Laurent Destailleur <eldy@destailleur.fr>
2022-05-01 17:58:37 +0200

    Fix missing picto

--- htdocs/modulebuilder/index.php
+++ htdocs/modulebuilder/index.php
@@ -848,23 +848,29 @@ if ($dirins && $action == 'initobject' && $module && GETPOST('createtablearray',
 		setEventMessages($langs->trans("ErrorTableNotFound", $tablename), null, 'errors');
 	} else {
 		/**
-		 *  'type' if the field format ('integer', 'integer:Class:pathtoclass', 'varchar(x)', 'double(24,8)', 'text', 'html', 'datetime', 'timestamp', 'float')
+		 *  'type' field format ('integer', 'integer:ObjectClass:PathToClass[:AddCreateButtonOrNot[:Filter[:Sortfield]]]', 'sellist:TableName:LabelFieldName[:KeyFieldName[:KeyFieldParent[:Filter[:Sortfield]]]]', 'varchar(x)', 'double(24,8)', 'real', 'price', 'text', 'text:none', 'html', 'date', 'datetime', 'timestamp', 'duration', 'mail', 'phone', 'url', 'password')
+		 *         Note: Filter can be a string like "(t.ref:like:'SO-%') or (t.date_creation:<:'20160101') or (t.nature:is:NULL)"
 		 *  'label' the translation key.
-		 *  'enabled' is a condition when the field must be managed.
-		 *  'visible' says if field is visible in list (Examples: 0=Not visible, 1=Visible on list and create/update/view forms, 2=Visible on list only, 3=Visible on create/update/view form only (not list), 4=Visible on list and update/view form only (not create). Using a negative value means field is not shown by default on list but can be selected for viewing)
-		 *  'noteditable' says if field is not editable (1 or 0)
+		 *  'picto' is code of a picto to show before value in forms
+		 *  'enabled' is a condition when the field must be managed (Example: 1 or '$conf->global->MY_SETUP_PARAM' or '!empty($conf->multicurrency->enabled)' ...)
+		 *  'position' is the sort order of field.
 		 *  'notnull' is set to 1 if not null in database. Set to -1 if we must set data to null if empty ('' or 0).
-		 *  'default' is a default value for creation (can still be replaced by the global setup of default values)
+		 *  'visible' says if field is visible in list (Examples: 0=Not visible, 1=Visible on list and create/update/view forms, 2=Visible on list only, 3=Visible on create/update/view form only (not list), 4=Visible on list and update/view form only (not create). 5=Visible on list and view only (not create/not update). Using a negative value means field is not shown by default on list but can be selected for viewing)
+		 *  'noteditable' says if field is not editable (1 or 0)
+		 *  'default' is a default value for creation (can still be overwrote by the Setup of Default Values if field is editable in creation form). Note: If default is set to '(PROV)' and field is 'ref', the default value will be set to '(PROVid)' where id is rowid when a new record is created.
 		 *  'index' if we want an index in database.
 		 *  'foreignkey'=>'tablename.field' if the field is a foreign key (it is recommanded to name the field fk_...).
-		 *  'position' is the sort order of field.
 		 *  'searchall' is 1 if we want to search in this field when making a search from the quick search button.
-		 *  'isameasure' must be set to 1 if you want to have a total on list for this field. Field type must be summable like integer or double(24,8).
-		 *  'css' and 'cssview' and 'csslist' is the CSS style to use on field. 'css' is used in creation and update. 'cssview' is used in view mode. 'csslist' is used for columns in lists. For example: 'maxwidth200', 'wordbreak', 'tdoverflowmax200'
-		 *  'help' is a string visible as a tooltip on field
-		 *  'comment' is not used. You can store here any text of your choice. It is not used by application.
+		 *  'isameasure' must be set to 1 or 2 if field can be used for measure. Field type must be summable like integer or double(24,8). Use 1 in most cases, or 2 if you don't want to see the column total into list (for example for percentage)
+		 *  'css' and 'cssview' and 'csslist' is the CSS style to use on field. 'css' is used in creation and update. 'cssview' is used in view mode. 'csslist' is used for columns in lists. For example: 'css'=>'minwidth300 maxwidth500 widthcentpercentminusx', 'cssview'=>'wordbreak', 'csslist'=>'tdoverflowmax200'
+		 *  'help' is a 'TranslationString' to use to show a tooltip on field. You can also use 'TranslationString:keyfortooltiponlick' for a tooltip on click.
 		 *  'showoncombobox' if value of the field must be visible into the label of the combobox that list record
-		 *  'arrayofkeyval' to set list of value if type is a list of predefined values. For example: array("0"=>"Draft","1"=>"Active","-1"=>"Cancel")
+		 *  'disabled' is 1 if we want to have the field locked by a 'disabled' attribute. In most cases, this is never set into the definition of $fields into class, but is set dynamically by some part of code.
+		 *  'arrayofkeyval' to set a list of values if type is a list of predefined values. For example: array("0"=>"Draft","1"=>"Active","-1"=>"Cancel"). Note that type can be 'integer' or 'varchar'
+		 *  'autofocusoncreate' to have field having the focus on a create form. Only 1 field should have this property set to 1.
+		 *  'comment' is not used. You can store here any text of your choice. It is not used by application.
+		 *	'validate' is 1 if need to validate with $this->validateField()
+		 *  'copytoclipboard' is 1 or 2 to allow to add a picto to copy value into clipboard (1=picto after label, 2=picto after value)
 		 */
 
 		/*public $fields=array(
@@ -1000,7 +1006,7 @@ if ($dirins && $action == 'initobject' && $module && GETPOST('createtablearray',
 			if ($fieldname == 'entity') {
 				$index = 1;
 			}
-			// css
+			// css, cssview, csslist
 			$css = '';
 			$cssview = '';
 			$csslist = '';
@@ -1015,8 +1021,17 @@ if ($dirins && $action == 'initobject' && $module && GETPOST('createtablearray',
 				$cssview = 'wordbreak';
 			}
 
+			// type
+			$picto = $obj->Picto;
+			if ($obj->Field == 'fk_soc') {
+				$picto = 'company';
+			}
+			if (preg_match('/^fk_proj/', $obj->Field)) {
+				$picto = 'project';
+			}
 
-			$string .= "'".$obj->Field."' =>array('type'=>'".$type."', 'label'=>'".$label."',";
+			// Build the property string
+			$string .= "'".$obj->Field."'=>array('type'=>'".$type."', 'label'=>'".$label."',";
 			if ($default != '') {
 				$string .= " 'default'=>".$default.",";
 			}
@@ -1031,6 +1046,9 @@ if ($dirins && $action == 'initobject' && $module && GETPOST('createtablearray',
 			$string .= ", 'position'=>".$position;
 			if ($index) {
 				$string .= ", 'index'=>".$index;
+			}
+			if ($picto) {
+				$string .= ", 'picto'=>'".$picto."'";
 			}
 			if ($css) {
 				$string .= ", 'css'=>".$css;
