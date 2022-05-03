3c85577d91393efbf8bc1b028176d0e367077c1a
melina <melina.joum@altairis.net>
2022-05-03 09:52:20 +0200

    fix parameter oh hook

--- htdocs/takepos/invoice.php
+++ htdocs/takepos/invoice.php
@@ -182,7 +182,7 @@ if (!empty($conf->multicurrency->enabled) && !empty($_SESSION["takeposcustomercu
  */
 
 $parameters=array();
-$reshook=$hookmanager->executeHooks('doActions', $parameters, $object, $action);    // Note that $action and $object may have been modified by some hooks
+$reshook=$hookmanager->executeHooks('doActions', $parameters, $invoice, $action);    // Note that $action and $object may have been modified by some hooks
 if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
 
 if (empty($reshook)) {
