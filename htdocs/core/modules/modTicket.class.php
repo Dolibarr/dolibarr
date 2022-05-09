d0c95d87afe960e3074d611aa39d2f68433351b1
Laurent Destailleur <eldy@destailleur.fr>
2022-05-09 18:46:23 +0200

    Debug v16

--- htdocs/core/modules/modTicket.class.php
+++ htdocs/core/modules/modTicket.class.php
@@ -171,8 +171,8 @@ class modTicket extends DolibarrModules
 			0=>array('file'=>'box_last_ticket.php', 'enabledbydefaulton'=>'Home'),
 			1=>array('file'=>'box_last_modified_ticket.php', 'enabledbydefaulton'=>'Home'),
 			2=>array('file'=>'box_ticket_by_severity.php', 'enabledbydefaulton'=>'ticketindex'),
-			3=>array('file'=>'box_nb_ticket_last_x_days.php', 'enabledbydefaulton'=>'ticketindex'),
-			4=>array('file'=>'box_nb_tickets_type.php', 'enabledbydefaulton'=>'ticketindex'),
+			3=>array('file'=>'box_graph_nb_ticket_last_x_days.php', 'enabledbydefaulton'=>'ticketindex'),
+			4=>array('file'=>'box_graph_nb_tickets_type.php', 'enabledbydefaulton'=>'ticketindex'),
 			5=>array('file'=>'box_new_vs_close_ticket.php', 'enabledbydefaulton'=>'ticketindex')
 		); // Boxes list
 
