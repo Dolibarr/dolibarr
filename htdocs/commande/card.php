243dfacb1571703261ab10be0941781ab500685b
Frédéric FRANCE <frederic34@users.noreply.github.com>
2022-05-19 12:46:15 +0200

    Update card.php

--- htdocs/commande/card.php
+++ htdocs/commande/card.php
@@ -1633,7 +1633,7 @@ if ($action == 'create' && $usercancreate) {
 		// Contacts (ask contact only if thirdparty already defined).
 		print "<tr><td>".$langs->trans("DefaultContact").'</td><td>';
 		print img_picto('', 'contact', 'class="pictofixedwidth"');
-		print $form->selectcontacts($soc->id, $contactid, 'contactid', 1, $srccontactslist, '', 1);
+		print $form->selectcontacts($soc->id, $contactid, 'contactid', 1, $srccontactslist, '', 1, 'maxwidth200 widthcentpercentminusx');
 		print '</td></tr>';
 
 		// Ligne info remises tiers
