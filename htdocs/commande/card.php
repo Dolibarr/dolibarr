2114e51b0e0bfd8e31878fc210cfdf5f24c72af7
Frédéric FRANCE <frederic34@users.noreply.github.com>
2022-05-19 12:34:54 +0200

    Update card.php

--- htdocs/commande/card.php
+++ htdocs/commande/card.php
@@ -1690,7 +1690,7 @@ if ($action == 'create' && $usercancreate) {
 	// Shipping Method
 	if (!empty($conf->expedition->enabled)) {
 		print '<tr><td>'.$langs->trans('SendingMethod').'</td><td>';
-		print img_picto('', 'object_dollyrevert', 'class="pictofixedwidth"');
+		print img_picto('', 'object_dolly', 'class="pictofixedwidth"');
 		print $form->selectShippingMethod($shipping_method_id, 'shipping_method_id', '', 1, '', 0, 'maxwidth200 widthcentpercentminusx');
 		print '</td></tr>';
 	}
