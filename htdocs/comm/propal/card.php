8af848c5394b1c213b8764a443ab25a92fcd935c
Florian HENRY <florian.henry@open-concept.pro>
2022-05-20 10:40:59 +0200

    add Ref client in PDF and send to propal on create

--- htdocs/comm/propal/card.php
+++ htdocs/comm/propal/card.php
@@ -1644,7 +1644,7 @@ if ($action == 'create') {
 
 	// Ref customer
 	print '<tr class="field_ref_client"><td class="titlefieldcreate">'.$langs->trans('RefCustomer').'</td><td class="valuefieldcreate">';
-	print '<input type="text" name="ref_client" value="'.GETPOST('ref_client').'"></td>';
+	print '<input type="text" name="ref_client" value="'.(!empty($ref_client)?$ref_client:GETPOST('ref_client')).'"></td>';
 	print '</tr>';
 
 	// Third party
