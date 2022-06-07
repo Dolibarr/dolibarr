d4bbc72b1bee0f3291341d1481c4be306a2c25e1
Florian HENRY <florian.henry@open-concept.pro>
2022-06-07 14:31:22 +0200

    fix: 21139

--- htdocs/ticket/card.php
+++ htdocs/ticket/card.php
@@ -387,6 +387,7 @@ if (empty($reshook)) {
 			// Si déjà un user assigné on le supprime des contacts
 			if ($useroriginassign > 0) {
 				$internal_contacts = $object->listeContact(-1, 'internal', 0, 'SUPPORTTEC');
+
 				foreach ($internal_contacts as $key => $contact) {
 					if ($contact['id'] !== $usertoassign) {
 						$result = $object->delete_contact($contact['rowid']);
@@ -1055,7 +1056,7 @@ if ($action == 'create' || $action == 'presend') {
 			print '<input type="hidden" name="action" value="assign_user">';
 			print '<input type="hidden" name="track_id" value="'.$object->track_id.'">';
 			//print '<label for="fk_user_assign">'.$langs->trans("AssignUser").'</label> ';
-			print $form->select_dolusers($user->id, 'fk_user_assign', 1);
+			print $form->select_dolusers(empty($object->fk_user_assign)?$user->id:$object->fk_user_assign, 'fk_user_assign', 1);
 			print ' <input type="submit" class="button small" name="btn_assign_user" value="'.$langs->trans("Validate").'" />';
 			print '</form>';
 		}
