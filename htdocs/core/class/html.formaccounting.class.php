448027f8ed32879f2643d56eabbe743d2272004a
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2022-06-12 22:54:15 +0200

    Fix PHP 8.1.7

--- htdocs/core/class/html.formaccounting.class.php
+++ htdocs/core/class/html.formaccounting.class.php
@@ -505,6 +505,7 @@ class FormAccounting extends Form
 		}
 
 		// Build select
+		$out = '';
 		$out .= Form::selectarray($htmlname, $aux_account, $selectid, ($showempty ? (is_numeric($showempty) ? 1 : $showempty): 0), 0, 0, '', 0, 0, 0, '', $morecss, 1);
 		//automatic filling if we give the name of the subledger_label input
 		if (!empty($conf->use_javascript_ajax) && !empty($labelhtmlname)) {
