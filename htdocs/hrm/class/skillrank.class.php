74c4cb618d92f3e4f69664ef57ed55118d3e1cfa
Laurent Destailleur <eldy@destailleur.fr>
2022-06-12 18:49:14 +0200

    Update skillrank.class.php

--- htdocs/hrm/class/skillrank.class.php
+++ htdocs/hrm/class/skillrank.class.php
@@ -414,7 +414,7 @@ class SkillRank extends CommonObject
 		if (count($filter) > 0) {
 			foreach ($filter as $key => $value) {
 				if ($key == 't.rowid') {
-					$sqlwhere[] = $key." = ".$value;
+					$sqlwhere[] = $key." = ".((int) $value);
 				} elseif ($key != 'customsql' && in_array($this->fields[$key]['type'], array('date', 'datetime', 'timestamp'))) {
 					$sqlwhere[] = $key." = '".$this->db->idate($value)."'";
 				} elseif ($key == 'customsql') {
