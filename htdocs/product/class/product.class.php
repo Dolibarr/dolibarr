7a5d4cba5b877fd7c8cb432d53e5e1720f0cb024
Abdelaziz MISBAH <almisbah@almisbah.ma>
2022-06-15 11:28:39 +0100

    NEW Fetch product by ID with consideration of entities
    When we fetch a product by ID it return all related objects in all entities. We added a rule to take entities in consideration.

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -2338,7 +2338,8 @@ class Product extends CommonObject
 		}
 
 		if ($id) {
-			$sql .= " WHERE p.rowid = ".((int) $id)." AND p.entity IN (".getEntity($this->element).")";
+			$sql .= " WHERE p.rowid = ".((int) $id);
+			$sql .= " AND p.entity IN (".getEntity($this->element).")";
 		} else {
 			$sql .= " WHERE p.entity IN (".getEntity($this->element).")";
 			if ($ref) {
