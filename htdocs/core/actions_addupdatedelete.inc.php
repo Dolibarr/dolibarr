7917775989c191e1a8ba69947a9bdea51bc5b8c5
atm-lena <lena.papazian@atm-consulting.fr>
2022-06-16 10:05:05 +0200

    Add $noback for action update

--- htdocs/core/actions_addupdatedelete.inc.php
+++ htdocs/core/actions_addupdatedelete.inc.php
@@ -266,7 +266,7 @@ if ($action == 'update' && !empty($permissiontoadd)) {
 			$action = 'view';
 			$urltogo = $backtopage ? str_replace('__ID__', $result, $backtopage) : $backurlforlist;
 			$urltogo = preg_replace('/--IDFORBACKTOPAGE--/', $object->id, $urltogo); // New method to autoselect project after a New on another form object creation
-			if ($urltogo) {
+			if ($urltogo && !$noback) { {
 				header("Location: " . $urltogo);
 				exit;
 			}
