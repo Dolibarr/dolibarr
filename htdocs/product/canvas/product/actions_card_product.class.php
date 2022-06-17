5f36f79ab6336de9751fa4854d3e9fd2f11bdd15
stickler-ci <support@stickler-ci.com>
2022-06-17 03:50:59 +0000

    Fixing style errors.

--- htdocs/product/canvas/product/actions_card_product.class.php
+++ htdocs/product/canvas/product/actions_card_product.class.php
@@ -38,7 +38,7 @@ class ActionsCardProduct
 	// List of fiels for action=list
 	public $field_list = array();
 	public $list_datas = array();
-	
+
 	public $label;
 	public $description;
 	public $note;
@@ -48,8 +48,8 @@ class ActionsCardProduct
 	public $accountancy_code_sell;
 	public $accountancy_code_buy;
 	public $stock_reel;
-	
-	
+
+
 	/**
 	 *    Constructor
 	 *
@@ -66,14 +66,14 @@ class ActionsCardProduct
 		$this->targetmodule     = $targetmodule;
 		$this->canvas           = $canvas;
 		$this->card             = $card;
-		
+
 		$this->name = "product";
 		$this->definition = "Product canvas (défaut)";
 		$this->fieldListName    = "product_default";
 		$this->next_prev_filter = "canvas='product'";
 	}
-	
-	
+
+
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
 	/**
 	 *    Assign custom values for canvas (for example into this->tpl to be used by templates)
@@ -89,71 +89,71 @@ class ActionsCardProduct
 		global $limit, $offset, $sortfield, $sortorder;
 		global $conf, $langs, $user, $mysoc, $canvas;
 		global $form, $formproduct;
-		
+
 		$tmpobject = new Product($this->db);
 		if (!empty($id) || !empty($ref)) {
 			$tmpobject->fetch($id, $ref);
 		}
 		$this->object = $tmpobject;
-		
+
 		//parent::assign_values($action);
-		
+
 		foreach ($this->object as $key => $value) {
 			$this->tpl[$key] = $value;
 		}
-		
+
 		$this->tpl['error'] = get_htmloutput_errors($this->object->error, $this->object->errors);
-		
+
 		// canvas
 		$this->tpl['canvas'] = $this->canvas;
-		
+
 		// id
 		$this->tpl['id'] = $this->id;
-		
+
 		// Ref
 		$this->tpl['ref'] = $this->ref;
-		
+
 		// Label
 		$this->tpl['label'] = $this->label;
-		
+
 		// Description
 		$this->tpl['description'] = nl2br($this->description);
-		
+
 		// Statut
 		$this->tpl['status'] = $this->object->getLibStatut(2);
-		
+
 		// Note
 		$this->tpl['note'] = nl2br($this->note);
-		
+
 		if ($action == 'create') {
 			// Price
 			$this->tpl['price'] = $this->price;
 			$this->tpl['price_min'] = $this->price_min;
 			$this->tpl['price_base_type'] = $form->selectPriceBaseType($this->price_base_type, "price_base_type");
-			
+
 			// VAT
 			$this->tpl['tva_tx'] = $form->load_tva("tva_tx", -1, $mysoc, '');
 		}
-		
+
 		if ($action == 'view') {
 			$head = product_prepare_head($this->object);
-			
+
 			$this->tpl['showrefnav'] = $form->showrefnav($this->object, 'ref', '', 1, 'ref');
-			
+
 			$titre = $langs->trans("CardProduct".$this->object->type);
 			$picto = ($this->object->type == Product::TYPE_SERVICE ? 'service' : 'product');
 			$this->tpl['showhead'] = dol_get_fiche_head($head, 'card', $titre, 0, $picto);
 			$this->tpl['showend'] = dol_get_fiche_end();
-			
+
 			// Accountancy buy code
 			$this->tpl['accountancyBuyCodeKey'] = $form->editfieldkey("ProductAccountancyBuyCode", 'productaccountancycodesell', $this->accountancy_code_sell, $this, $user->rights->produit->creer);
 			$this->tpl['accountancyBuyCodeVal'] = $form->editfieldval("ProductAccountancyBuyCode", 'productaccountancycodesell', $this->accountancy_code_sell, $this, $user->rights->produit->creer);
-			
+
 			// Accountancy sell code
 			$this->tpl['accountancySellCodeKey'] = $form->editfieldkey("ProductAccountancySellCode", 'productaccountancycodebuy', $this->accountancy_code_buy, $this, $user->rights->produit->creer);
 			$this->tpl['accountancySellCodeVal'] = $form->editfieldval("ProductAccountancySellCode", 'productaccountancycodebuy', $this->accountancy_code_buy, $this, $user->rights->produit->creer);
 		}
-		
+
 		$this->tpl['finished'] = $this->object->finished;
 		$this->tpl['ref'] = $this->object->ref;
 		$this->tpl['label'] = $this->object->label;
@@ -161,58 +161,58 @@ class ActionsCardProduct
 		$this->tpl['type'] = $this->object->type;
 		$this->tpl['note'] = $this->object->note;
 		$this->tpl['seuil_stock_alerte'] = $this->object->seuil_stock_alerte;
-		
+
 		if ($action == 'create') {
 			// Title
 			$this->tpl['title'] = $langs->trans("NewProduct");
 		}
-		
+
 		if ($action == 'edit') {
 			$this->tpl['title'] = $langs->trans('Modify').' '.$langs->trans('Product').' : '.$this->object->ref;
 		}
-		
+
 		if ($action == 'create' || $action == 'edit') {
 			// Status
 			$statutarray = array('1' => $langs->trans("OnSell"), '0' => $langs->trans("NotOnSell"));
 			$this->tpl['status'] = $form->selectarray('statut', $statutarray, $this->object->status);
-			
+
 			$statutarray = array('1' => $langs->trans("ProductStatusOnBuy"), '0' => $langs->trans("ProductStatusNotOnBuy"));
 			$this->tpl['status_buy'] = $form->selectarray('statut_buy', $statutarray, $this->object->status_buy);
-			
+
 			$this->tpl['description'] = $this->description;
 			$this->tpl['note'] = $this->note;
-			
+
 			// Finished
 			$statutarray = array('1' => $langs->trans("Finished"), '0' => $langs->trans("RowMaterial"));
 			$this->tpl['finished'] = $form->selectarray('finished', $statutarray, $this->object->finished);
-			
+
 			// Weight
 			$this->tpl['weight'] = $this->object->weight;
 			$this->tpl['weight_units'] = $formproduct->selectMeasuringUnits("weight_units", "weight", $this->object->weight_units, 0, 2);
-			
+
 			// Length
 			$this->tpl['length'] = $this->object->length;
 			$this->tpl['length_units'] = $formproduct->selectMeasuringUnits("length_units", "size", $this->object->length_units, 0, 2);
-			
+
 			// Surface
 			$this->tpl['surface'] = $this->object->surface;
 			$this->tpl['surface_units'] = $formproduct->selectMeasuringUnits("surface_units", "surface", $this->object->surface_units, 0, 2);
-			
+
 			// Volume
 			$this->tpl['volume'] = $this->object->volume;
 			$this->tpl['volume_units'] = $formproduct->selectMeasuringUnits("volume_units", "volume", $this->object->volume_units, 0, 2);
 		}
-		
+
 		if ($action == 'view') {
 			// Photo
 			$this->tpl['nblines'] = 4;
 			if ($this->object->is_photo_available($conf->product->multidir_output[$this->object->entity])) {
 				$this->tpl['photos'] = $this->object->show_photos('product', $conf->product->multidir_output[$this->object->entity], 1, 1, 0, 0, 0, 80);
 			}
-			
+
 			// Nature
 			$this->tpl['finished'] = $this->object->getLibFinished();
-			
+
 			// Weight
 			if ($this->object->weight != '') {
 				$this->tpl['weight'] = $this->object->weight." ".measuringUnitString(0, "weight", $this->object->weight_units);
@@ -222,26 +222,26 @@ class ActionsCardProduct
 			if ($this->object->length != '') {
 				$this->tpl['length'] = $this->object->length." ".measuringUnitString(0, "size", $this->object->length_units);
 			}
-			
+
 			// Surface
 			if ($this->object->surface != '') {
 				$this->tpl['surface'] = $this->object->surface." ".measuringUnitString(0, "surface", $this->object->surface_units);
 			}
-			
+
 			// Volume
 			if ($this->object->volume != '') {
 				$this->tpl['volume'] = $this->object->volume." ".measuringUnitString(0, "volume", $this->object->volume_units);
 			}
-			
+
 			$this->tpl['fiche_end'] = dol_get_fiche_end();
 		}
-		
+
 		if ($action == 'list') {
 			$this->LoadListDatas($limit, $offset, $sortfield, $sortorder);
 		}
 	}
 
-	
+
 	/**
 	 * 	Fetch field list
 	 *
@@ -250,25 +250,25 @@ class ActionsCardProduct
 	private function getFieldListCanvas()
 	{
 		global $conf, $langs;
-		
+
 		$this->field_list = array();
-		
+
 		$sql = "SELECT rowid, name, alias, title, align, sort, search, visible, enabled, rang";
 		$sql .= " FROM ".MAIN_DB_PREFIX."c_field_list";
 		$sql .= " WHERE element = '".$this->db->escape($this->fieldListName)."'";
 		$sql .= " AND entity = ".$conf->entity;
 		$sql .= " ORDER BY rang ASC";
-		
+
 		$resql = $this->db->query($sql);
 		if ($resql) {
 			$num = $this->db->num_rows($resql);
-			
+
 			$i = 0;
 			while ($i < $num) {
 				$fieldlist = array();
-				
+
 				$obj = $this->db->fetch_object($resql);
-				
+
 				$fieldlist["id"] = $obj->rowid;
 				$fieldlist["name"] = $obj->name;
 				$fieldlist["alias"]		= $obj->alias;
@@ -281,7 +281,7 @@ class ActionsCardProduct
 				$fieldlist["order"]		= $obj->rang;
 
 				array_push($this->field_list, $fieldlist);
-				
+
 				$i++;
 			}
 			$this->db->free($resql);
@@ -289,8 +289,8 @@ class ActionsCardProduct
 			dol_print_error($this->db, $sql);
 		}
 	}
-	
-	
+
+
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
 	/**
 	 * 	Fetch datas list and save into ->list_datas
@@ -305,26 +305,26 @@ class ActionsCardProduct
 	{
 		// phpcs:enable
 		global $conf, $langs;
-		
+
 		$this->getFieldListCanvas();
-		
+
 		$this->list_datas = array();
-		
+
 		// Clean parameters
 		$sall = trim((GETPOST('search_all', 'alphanohtml') != '') ?GETPOST('search_all', 'alphanohtml') : GETPOST('sall', 'alphanohtml'));
-		
+
 		foreach ($this->field_list as $field) {
 			if ($field['enabled']) {
 				$fieldname = "s".$field['alias'];
 				$$fieldname = GETPOST($fieldname);
 			}
 		}
-		
+
 		$sql = 'SELECT DISTINCT ';
-		
+
 		// Fields requiered
 		$sql .= 'p.rowid, p.price_base_type, p.fk_product_type, p.seuil_stock_alerte, p.entity';
-		
+
 		// Fields not requiered
 		foreach ($this->field_list as $field) {
 			if ($field['enabled']) {
@@ -348,7 +348,7 @@ class ActionsCardProduct
 			}
 			$sql .= ")";
 		}
-		
+
 		// Search fields
 		foreach ($this->field_list as $field) {
 			if ($field['enabled']) {
@@ -372,19 +372,19 @@ class ActionsCardProduct
 		$resql = $this->db->query($sql);
 		if ($resql) {
 			$num = $this->db->num_rows($resql);
-			
+
 			$i = 0;
 			while ($i < min($num, $limit)) {
 				$datas = array();
-				
+
 				$obj = $this->db->fetch_object($resql);
-				
+
 				$datas["id"] = $obj->rowid;
-				
+
 				foreach ($this->field_list as $field) {
 					if ($field['enabled']) {
 						$alias = $field['alias'];
-						
+
 						if ($alias == 'ref') {
 							$this->id = $obj->rowid;
 							$this->ref 		= $obj->$alias;
@@ -421,5 +421,4 @@ class ActionsCardProduct
 			dol_print_error($this->db);
 		}
 	}
-
 }
