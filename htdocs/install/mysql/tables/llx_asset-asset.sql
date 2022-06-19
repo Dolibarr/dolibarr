3f43ba0bc0e1dc2ca8f34370d1d8a7565ed9a5b0
Laurent Destailleur <eldy@destailleur.fr>
2022-06-19 17:17:31 +0200

    Update llx_asset-asset.sql

--- htdocs/install/mysql/tables/llx_asset-asset.sql
+++ htdocs/install/mysql/tables/llx_asset-asset.sql
@@ -18,20 +18,20 @@
 -- Table for fixed asset
 --
 -- Data example:
--- INSERT INTO `llx_asset` (`rowid`, `ref`, `entity`, `label`, `fk_asset_model`, `reversal_amount_ht`, `acquisition_value_ht`, `recovered_vat`, `reversal_date`, `date_acquisition`, `date_start`, `qty`, `acquisition_type`, `asset_type`, `not_depreciated`, `disposal_date`, `disposal_amount_ht`, `fk_disposal_type`, `disposal_depreciated`, `disposal_subject_to_vat`, `note_public`, `note_private`, `date_creation`, `tms`, `fk_user_creat`, `fk_user_modif`, `last_main_doc`, `import_key`, `model_pdf`, `status`) VALUES
--- (1, 'LAPTOP', 1, 'LAPTOP xxx for accountancy department', 1, NULL, 1000.00000000, NULL, NULL, '2022-01-18', '2022-01-20', 0, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-01-18 14:31:21', '2022-03-09 14:09:46', 1, 1, NULL, NULL, NULL, 0);
+-- INSERT INTO llx_asset (ref, entity, label, fk_asset_model, reversal_amount_ht, acquisition_value_ht, recovered_vat, reversal_date, date_acquisition, date_start, qty, acquisition_type, asset_type, not_depreciated, disposal_date, disposal_amount_ht, fk_disposal_type, disposal_depreciated, disposal_subject_to_vat, note_public, note_private, date_creation, tms, fk_user_creat, fk_user_modif, last_main_doc, import_key, model_pdf, status) VALUES
+-- ('LAPTOP', 1, 'LAPTOP xxx for accountancy department', 1, NULL, 1000.00000000, NULL, NULL, '2022-01-18', '2022-01-20', 0, 0, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-01-18 14:31:21', '2022-03-09 14:09:46', 1, 1, NULL, NULL, NULL, 0);
 
 CREATE TABLE llx_asset(
-	rowid                   integer AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	ref                     varchar(128)    NOT NULL,
-	entity                  integer         DEFAULT 1 NOT NULL,
-	label                   varchar(255),
+    rowid                   integer AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    ref                     varchar(128)    NOT NULL,
+    entity                  integer         DEFAULT 1 NOT NULL,
+    label                   varchar(255),
 
     fk_asset_model          integer,
 
     reversal_amount_ht      double(24,8),
     acquisition_value_ht    double(24,8)    DEFAULT NULL,
-	recovered_vat           double(24,8),
+    recovered_vat           double(24,8),
 
     reversal_date           date,
 
@@ -51,15 +51,15 @@ CREATE TABLE llx_asset(
     disposal_depreciated    integer         DEFAULT 0,
     disposal_subject_to_vat integer         DEFAULT 0,
 
-	note_public             text,
-	note_private            text,
+    note_public             text,
+    note_private            text,
 
-	date_creation           datetime        NOT NULL,
-	tms                     timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	fk_user_creat           integer         NOT NULL,
-	fk_user_modif           integer,
-	last_main_doc           varchar(255),
-	import_key              varchar(14),
-	model_pdf               varchar(255),
-	status                  integer         NOT NULL
+    date_creation           datetime        NOT NULL,
+    tms                     timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    fk_user_creat           integer         NOT NULL,
+    fk_user_modif           integer,
+    last_main_doc           varchar(255),
+    import_key              varchar(14),
+    model_pdf               varchar(255),
+    status                  integer         NOT NULL
 ) ENGINE=innodb;
