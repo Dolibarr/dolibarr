56c4817d6decea4c69c16adfa438cf72e51adb16
Florian HENRY <florian.henry@open-concept.pro>
2022-07-08 17:30:15 +0200

    fix: Bad amount calc on Invoice index

--- htdocs/core/lib/invoice.lib.php
+++ htdocs/core/lib/invoice.lib.php
@@ -1165,6 +1165,7 @@ function getCustomerInvoiceUnpaidOpenTable($maxCount = 500, $socid = 0)
 						$i++;
 						$total += $obj->total_ht;
 						$total_ttc += $obj->total_ttc;
+						$totalam += $obj->am;
 						continue;
 					}
 
