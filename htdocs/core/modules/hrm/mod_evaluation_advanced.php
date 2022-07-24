394bfed965cfdd57152574da281e4f53e987d679
Laurent Destailleur <eldy@destailleur.fr>
2022-07-24 17:39:01 +0200

    Debug v16

--- htdocs/core/modules/hrm/mod_evaluation_advanced.php
+++ htdocs/core/modules/hrm/mod_evaluation_advanced.php
@@ -81,7 +81,7 @@ class mod_evaluation_advanced extends ModeleNumRefEvaluation
 		$texte .= '<tr><td>'.$langs->trans("Mask").':</td>';
 		$texte .= '<td class="right">'.$form->textwithpicto('<input type="text" class="flat minwidth175" name="maskEvaluation" value="'.$conf->global->HRM_EVALUATION_ADVANCED_MASK.'">', $tooltip, 1, 1).'</td>';
 
-		$texte .= '<td class="left" rowspan="2">&nbsp; <input type="submit" class="button" value="'.$langs->trans("Modify").'" name="Button"></td>';
+		$texte .= '<td class="left" rowspan="2">&nbsp; <input type="submit" class="button smallpaddingimp" value="'.$langs->trans("Modify").'" name="Button"></td>';
 
 		$texte .= '</tr>';
 
