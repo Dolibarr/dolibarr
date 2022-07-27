e1cd297d99f7a1b6768815a8e15b965b50c98328
Laurent Destailleur <eldy@destailleur.fr>
2022-07-27 13:49:28 +0200

    css

--- htdocs/user/card.php
+++ htdocs/user/card.php
@@ -833,7 +833,7 @@ if ($action == 'create' || $action == 'adduserldap') {
 	print '<table class="border centpercent">';
 
 	// Civility
-	print '<tr><td><label for="civility_code">'.$langs->trans("UserTitle").'</label></td><td colspan="3">';
+	print '<tr><td><label for="civility_code">'.$langs->trans("UserTitle").'</label></td><td>';
 	print $formcompany->select_civility(GETPOSTISSET("civility_code") ? GETPOST("civility_code", 'aZ09') : $object->civility_code, 'civility_code');
 	print '</td></tr>';
 
@@ -2077,7 +2077,7 @@ if ($action == 'create' || $action == 'adduserldap') {
 			}
 
 			// Civility
-			print '<tr><td class="titlefieldcreate"><label for="civility_code">'.$langs->trans("UserTitle").'</label></td><td colspan="3">';
+			print '<tr><td class="titlefieldcreate"><label for="civility_code">'.$langs->trans("UserTitle").'</label></td><td>';
 			if ($caneditfield && !$object->ldap_sid) {
 				print $formcompany->select_civility(GETPOSTISSET("civility_code") ? GETPOST("civility_code", 'aZ09') : $object->civility_code, 'civility_code');
 			} elseif ($object->civility_code) {
