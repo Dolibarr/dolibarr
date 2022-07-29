edb61faf25c98b1e6b355f2e2e530988ac75b082
Laurent Destailleur <eldy@destailleur.fr>
2022-07-29 18:16:29 +0200

    Fix lang

--- test/phpunit/LangTest.php
+++ test/phpunit/LangTest.php
@@ -230,6 +230,10 @@ class LangTest extends PHPUnit\Framework\TestCase
 				$result=preg_match('/%n/m', $filecontent);	// A sequence of char we don't want
 				//print __METHOD__." Result for checking we don't have bad percent char = ".$result."\n";
 				$this->assertTrue($result == 0, 'Found a sequence %n into the translation file '.$code.'/'.$file.'. We probably want %s');
+
+				$result=preg_match('/<<<<</m', $filecontent);	// A sequence of char we don't want
+				//print __METHOD__." Result for checking we don't have bad percent char = ".$result."\n";
+				$this->assertTrue($result == 0, 'Found a sequence <<<<< into the translation file '.$code.'/'.$file.'. Probably a bad merge of code were done.');
 			}
 		}
 
