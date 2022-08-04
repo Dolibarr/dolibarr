a064b25fdd6d1dfa052a7777137df0b147b557cd
Laurent Destailleur <eldy@destailleur.fr>
2022-08-04 11:14:21 +0200

    css

--- htdocs/user/ldap.php
+++ htdocs/user/ldap.php
@@ -94,7 +94,7 @@ llxHeader();
 $head = user_prepare_head($object);
 
 $title = $langs->trans("User");
-print dol_get_fiche_head($head, 'ldap', $title, 0, 'user');
+print dol_get_fiche_head($head, 'ldap', $title, -1, 'user');
 
 $linkback = '';
 
@@ -107,7 +107,7 @@ dol_banner_tab($object, 'id', $linkback, $user->rights->user->user->lire || $use
 print '<div class="fichecenter">';
 print '<div class="underbanner clearboth"></div>';
 
-print '<table class="border centpercent">';
+print '<table class="border centpercent tableforfield">';
 
 // Login
 print '<tr><td class="titlefield">'.$langs->trans("Login").'</td>';
