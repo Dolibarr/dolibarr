be825f19757f471d4f24d7f61da4aa8bdf83d956
Anthony Berton <bertonanthony@gmail.com>
2022-08-09 15:50:16 +0200

    @eldy update for you comment

--- htdocs/comm/propal/class/propal.class.php
+++ htdocs/comm/propal/class/propal.class.php
@@ -2060,7 +2060,7 @@ class Propal extends CommonObject
 
 			$this->db->begin();
 
-			if (!empty($this->duree_validite) {
+			if (isset($this->duree_validite) && !empty($this->fin_validite)) {
 				$fin_validite = $date + ($this->duree_validite * 24 * 3600);
 				$sql = "UPDATE ".MAIN_DB_PREFIX."propal SET datep = '".$this->db->idate($date)."',fin_validite = '".$this->db->idate($fin_validite)."'";
 				$sql .= " WHERE rowid = ".((int) $this->id)." AND fk_statut = ".self::STATUS_DRAFT;
