912a1b3a7eba9d3b966c764f784b3a79aaf4557a
Laurent Destailleur <eldy@destailleur.fr>
2022-08-14 15:13:15 +0200

    Update mod_propale_saphir.php

--- htdocs/core/modules/propale/mod_propale_saphir.php
+++ htdocs/core/modules/propale/mod_propale_saphir.php
@@ -85,7 +85,7 @@ class mod_propale_saphir extends ModeleNumRefPropales
 
 		// Parametrage du prefix
 		$texte .= '<tr><td>'.$langs->trans("Mask").':</td>';
-		$mask=!empty($conf->global->PROPALE_SAPHIR_MASK)?$conf->global->PROPALE_SAPHIR_MASK:"";
+		$mask = empty($conf->global->PROPALE_SAPHIR_MASK) ? '' : $conf->global->PROPALE_SAPHIR_MASK;
 		$texte .= '<td class="right">'.$form->textwithpicto('<input type="text" class="flat minwidth175" name="maskpropal" value="'.$mask.'">', $tooltip, 1, 1).'</td>';
 
 		$texte .= '<td class="left" rowspan="2">&nbsp; <input type="submit" class="button button-edit" name="Button"value="'.$langs->trans("Modify").'"></td>';
@@ -135,7 +135,7 @@ class mod_propale_saphir extends ModeleNumRefPropales
 		require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
 
 		// On defini critere recherche compteur
-		$mask=!empty($conf->global->PROPALE_SAPHIR_MASK)?$conf->global->PROPALE_SAPHIR_MASK:"";
+		$mask = empty($conf->global->PROPALE_SAPHIR_MASK) ? '' : $conf->global->PROPALE_SAPHIR_MASK;
 
 		if (!$mask) {
 			$this->error = 'NotConfigured';
