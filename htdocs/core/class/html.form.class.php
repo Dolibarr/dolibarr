68994752cdacaeb01dc0febcc60d574b4cc8e5c9
Regis Houssin <regis.houssin@inodbox.com>
2022-08-26 11:08:39 +0200

    FIX multiple php8 compatibility

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -6451,8 +6451,8 @@ class Form
 				} elseif ($usecalendar == 'jquery') {
 					if (!$disabled) {
 						// Output javascript for datepicker
-						$minYear = $conf->global->MIN_YEAR_SELECT_DATE ? $conf->global->MIN_YEAR_SELECT_DATE : (date('Y') - 100);
-						$maxYear = $conf->global->MAX_YEAR_SELECT_DATE ? $conf->global->MAX_YEAR_SELECT_DATE : (date('Y') + 100);
+						$minYear = getDolGlobalString('MIN_YEAR_SELECT_DATE', date('Y') - 100);
+						$maxYear = getDolGlobalString('MAX_YEAR_SELECT_DATE', date('Y') + 100);
 
 						$retstring .= "<script type='text/javascript'>";
 						$retstring .= "$(function(){ $('#".$prefix."').datepicker({
