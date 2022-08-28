492229b48c5047073c907bd7e5f2abc7f9b7f893
Philippe GRAND <philippe.grand@atoo-net.com>
2022-08-28 14:25:43 +0200

    FIX php8 compatibility

--- htdocs/public/onlinesign/newonlinesign.php
+++ htdocs/public/onlinesign/newonlinesign.php
@@ -139,7 +139,7 @@ if ($source == 'proposal') {
 	$securekeyseed = getDolGlobalString('PROPOSAL_ONLINE_SIGNATURE_SECURITY_TOKEN');
 }
 
-if (!dol_verifyHash($securekeyseed.$type.$ref.(empty($conf->multicompany->enabled) ? '' : $entity), $SECUREKEY, '0')) {
+if (!dol_verifyHash($securekeyseed.$type.$ref.(!isModEnabled('multicompany') ? '' : $entity), $SECUREKEY, '0')) {
 	http_response_code(403);
 	print 'Bad value for securitykey. Value provided '.dol_escape_htmltag($SECUREKEY).' does not match expected value for ref='.dol_escape_htmltag($ref);
 	exit(-1);
