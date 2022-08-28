24cd63486603c37c1eb22a7a77958b41e711b8dd
Philippe GRAND <philippe.grand@atoo-net.com>
2022-08-28 14:37:00 +0200

    FIX php8 compatibility

--- htdocs/user/hierarchy.php
+++ htdocs/user/hierarchy.php
@@ -121,7 +121,7 @@ if (!is_array($user_arbo) && $user_arbo < 0) {
 		$entitystring = '';
 
 		// TODO Set of entitystring should be done with a hook
-		if (!empty($conf->multicompany->enabled) && is_object($mc)) {
+		if (isModEnabled('multicompany') && is_object($mc)) {
 			if (empty($entity)) {
 				$entitystring = $langs->trans("AllEntities");
 			} else {
@@ -131,7 +131,7 @@ if (!is_array($user_arbo) && $user_arbo < 0) {
 		}
 
 		$li = $userstatic->getNomUrl(-1, '', 0, 1);
-		if (!empty($conf->multicompany->enabled) && $userstatic->admin && !$userstatic->entity) {
+		if (isModEnabled('multicompany') && $userstatic->admin && !$userstatic->entity) {
 			$li .= img_picto($langs->trans("SuperAdministrator"), 'redstar');
 		} elseif ($userstatic->admin) {
 			$li .= img_picto($langs->trans("Administrator"), 'star');
