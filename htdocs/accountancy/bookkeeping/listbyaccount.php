3ec7a2796021d8f5990fdf050bb073fa3749b946
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2022-08-29 05:43:33 +0200

    PHP8 Warnings

--- htdocs/accountancy/bookkeeping/listbyaccount.php
+++ htdocs/accountancy/bookkeeping/listbyaccount.php
@@ -824,17 +824,22 @@ print $hookmanager->resPrint;
 print_liste_field_titre($selectedfields, $_SERVER["PHP_SELF"], "", '', '', '', $sortfield, $sortorder, 'center maxwidthsearch ');
 print "</tr>\n";
 
-
-$total_debit = 0;
-$total_credit = 0;
-$sous_total_debit = 0;
-$sous_total_credit = 0;
 $displayed_account_number = null; // Start with undefined to be able to distinguish with empty
 
 // Loop on record
 // --------------------------------------------------------------------
 $i = 0;
+
 $totalarray = array();
+$totalarray['val'] = array ();
+$totalarray['nbfield'] = 0;
+$total_debit = 0;
+$total_credit = 0;
+$sous_total_debit = 0;
+$sous_total_credit = 0;
+$totalarray['val']['totaldebit'] = 0;
+$totalarray['val']['totalcredit'] = 0;
+
 while ($i < min($num, $limit)) {
 	$line = $object->lines[$i];
 
