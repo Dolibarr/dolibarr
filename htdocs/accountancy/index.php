2f9bb23a120c980d1b1bf7da866b868c62d24b9d
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-08-29 19:00:39 +0200

    Update index.php

--- htdocs/accountancy/index.php
+++ htdocs/accountancy/index.php
@@ -23,6 +23,8 @@
  * \brief   Home accounting module
  */
 
+
+// Load Dolibarr environment
 require '../main.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/accounting.lib.php';
