dd7dece5e8a1e12d57fa67a09ad5f70467209a6f
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2022-08-29 11:27:40 +0200

    Use isModEnabled

--- htdocs/compta/sociales/class/paymentsocialcontribution.class.php
+++ htdocs/compta/sociales/class/paymentsocialcontribution.class.php
@@ -552,7 +552,7 @@ class PaymentSocialContribution extends CommonObject
 
 		$error = 0;
 
-		if (!empty($conf->banque->enabled)) {
+		if (isModEnabled('banque')) {
 			include_once DOL_DOCUMENT_ROOT.'/compta/bank/class/account.class.php';
 
 			$acc = new Account($this->db);
