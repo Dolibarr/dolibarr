4d18ebf0da26966946c5e8d5f4233d9a051de522
Frédéric France <frederic.france@free.fr>
2022-09-01 08:14:29 +0200

    fix syntax

--- htdocs/webservices/admin/index.php
+++ htdocs/webservices/admin/index.php
@@ -103,12 +103,12 @@ $webservices = array(
 		'thirdparty'		=> 'isModEnabled("societe")',
 		'contact'			=> 'isModEnabled("societe")',
 		'productorservice'	=> '(isModEnabled("product") || isModEnabled("service"))',
-		'order'				=> 'isModEnabled('commande')',
+		'order'				=> 'isModEnabled("commande")',
 		'invoice'			=> 'isModEnabled("facture")',
 		'supplier_invoice'	=> 'isModEnabled("fournisseur")',
-		'actioncomm'		=> 'isModEnabled('agenda')',
-		'category'			=> 'isModEnabled('categorie')',
-		'project'			=> '!empty($conf->project->enabled)',
+		'actioncomm'		=> 'isModEnabled("agenda")',
+		'category'			=> 'isModEnabled("categorie")',
+		'project'			=> 'isModEnabled("project")',
 		'other'				=> ''
 );
 
