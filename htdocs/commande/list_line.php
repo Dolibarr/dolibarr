c0c12a40b088b57694981844bc2d130958b15e88
Anthony Berton <bertonanthony@gmail.com>
2022-09-01 16:37:13 +0200

    Fix inithook

--- htdocs/commande/list_line.php
+++ htdocs/commande/list_line.php
@@ -146,7 +146,7 @@ $show_shippable_command = GETPOST('show_shippable_command', 'aZ09');
 
 // Initialize technical object to manage hooks of page. Note that conf->hooks_modules contains array of hook context
 $object = new Commande($db);
-$hookmanager->initHooks(array('orderlist'));
+$hookmanager->initHooks(array('orderlistdetail'));
 $extrafields = new ExtraFields($db);
 
 // fetch optionals attributes and labels
