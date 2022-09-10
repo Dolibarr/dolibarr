4a693f7dcbb7f5939cd50be781ca5ddbedf8b66e
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 11:36:34 +0200

    Update modSociete.class.php

--- htdocs/core/modules/modSociete.class.php
+++ htdocs/core/modules/modSociete.class.php
@@ -28,7 +28,8 @@
  *	\ingroup    societe
  *	\brief      Description and activation file for the module societe (thirdparty)
  */
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 
 /**
