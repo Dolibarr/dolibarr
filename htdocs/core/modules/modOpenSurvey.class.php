7c9503ea43e4f98e4db1daddca801cf300afba9c
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 11:46:14 +0200

    Update modOpenSurvey.class.php

--- htdocs/core/modules/modOpenSurvey.class.php
+++ htdocs/core/modules/modOpenSurvey.class.php
@@ -23,7 +23,8 @@
  *      \ingroup    opensurvey
  *      \brief      Description and activation file for the module OpenSurvey
  */
-include_once DOL_DOCUMENT_ROOT."/core/modules/DolibarrModules.class.php";
+
+require_once DOL_DOCUMENT_ROOT."/core/modules/DolibarrModules.class.php";
 
 
 /**
