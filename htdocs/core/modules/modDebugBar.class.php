7baafeeb186096e74c10397b7681cfec3a569a58
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 12:00:38 +0200

    Update modDebugBar.class.php

--- htdocs/core/modules/modDebugBar.class.php
+++ htdocs/core/modules/modDebugBar.class.php
@@ -23,7 +23,8 @@
  *  \ingroup    debugbar
  *  \brief     Description and activation file for the module debugbar
  */
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 
 /**
