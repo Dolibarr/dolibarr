f8ab68e40e95ae6f55c8955084ec2bac4c065b3f
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 11:48:30 +0200

    Update modModuleBuilder.class.php

--- htdocs/core/modules/modModuleBuilder.class.php
+++ htdocs/core/modules/modModuleBuilder.class.php
@@ -23,7 +23,9 @@
  *  \ingroup    blockedlog
  *  \brief      Description and activation file for the module ModuleBuilder
  */
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
 
 /**
  *	Class to describe a ModuleBuilder module
