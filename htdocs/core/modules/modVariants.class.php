4503d4b095b8c5c9ea279d175a3efe54c1f06f6a
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 11:27:06 +0200

    Update modVariants.class.php

--- htdocs/core/modules/modVariants.class.php
+++ htdocs/core/modules/modVariants.class.php
@@ -25,7 +25,8 @@
  *  \ingroup    produit
  *  \brief      Description and activation file for the module product variants
  */
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 
 /**
