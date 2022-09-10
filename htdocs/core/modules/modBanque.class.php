7a5459219ce431787ad9da11aca0437cc763f721
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 12:05:53 +0200

    Update modBanque.class.php

--- htdocs/core/modules/modBanque.class.php
+++ htdocs/core/modules/modBanque.class.php
@@ -27,7 +27,7 @@
  *	\brief      Description and activation file for the module bank
  */
 
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 
 /**
