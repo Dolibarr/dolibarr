c3d6412e0f4bf1f41d65b60cc3b5549d4f135435
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 11:33:57 +0200

    Update modStripe.class.php

--- htdocs/core/modules/modStripe.class.php
+++ htdocs/core/modules/modStripe.class.php
@@ -23,7 +23,8 @@
  *  \ingroup    stripe
  *  \brief      Description and activation file for the module Stripe
  */
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 
 /**
