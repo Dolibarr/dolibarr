865873cac42cee56bd06e8d8993901cfa52a45fa
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 11:51:37 +0200

    Update modIntracommreport.class.php

--- htdocs/core/modules/modIntracommreport.class.php
+++ htdocs/core/modules/modIntracommreport.class.php
@@ -23,7 +23,8 @@
  * 	\ingroup    Intracomm report
  *	\brief      Description and activation file for the module intracomm report
  */
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 
 /**
