8424dee98872e2dfe72cdc4303e9412e9726dfbc
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 11:49:58 +0200

    Update modMailing.class.php

--- htdocs/core/modules/modMailing.class.php
+++ htdocs/core/modules/modMailing.class.php
@@ -25,7 +25,7 @@
  *	\brief      Description and activation file for the module Mailing
  */
 
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 
 /**
