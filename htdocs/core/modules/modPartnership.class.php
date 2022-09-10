9ea7c858fae51933ccf7682a8106056aaf2d065c
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 11:45:47 +0200

    Update modPartnership.class.php

--- htdocs/core/modules/modPartnership.class.php
+++ htdocs/core/modules/modPartnership.class.php
@@ -27,7 +27,8 @@
  *  \ingroup    partnership
  *  \brief      Description and activation file for module Partnership
  */
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 /**
  *  Description and activation class for module Partnership
