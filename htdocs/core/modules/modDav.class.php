335d30000aac73f08c462cac21fcc76dca947921
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-10 12:00:56 +0200

    Update modDav.class.php

--- htdocs/core/modules/modDav.class.php
+++ htdocs/core/modules/modDav.class.php
@@ -23,7 +23,8 @@
  *  \ingroup    dav
  *  \brief      Description and activation file for the module dav
  */
-include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+
+require_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
 
 
 /**
