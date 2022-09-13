c6144df3976be647ae8c2e95f462568ddcb2ce95
Laurent Destailleur <eldy@destailleur.fr>
2022-09-13 02:15:31 +0200

    Update create_ticket.php

--- htdocs/public/ticket/create_ticket.php
+++ htdocs/public/ticket/create_ticket.php
@@ -437,8 +437,8 @@ if (empty($reshook)) {
 							}
 							include_once DOL_DOCUMENT_ROOT.'/core/class/CMailFile.class.php';
 							$mailfile = new CMailFile($subject, $sendto, $from, $message_admin, $filepath, $mimetype, $filename, $sendtocc, '', $deliveryreceipt, -1, '', '', 'tic'.$object->id, '', 'ticket');
-							if ($mailfile->error || $mailfile->errors) {
-								setEventMessages($mailfile->error, !empty($mailfile->errors), 'errors');
+							if ($mailfile->error || !empty($mailfile->errors)) {
+								setEventMessages($mailfile->error, $mailfile->errors, 'errors');
 							} else {
 								$result = $mailfile->sendfile();
 							}
