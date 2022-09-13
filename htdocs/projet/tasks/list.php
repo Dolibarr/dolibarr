4f9c273c644f2ccc157674144ec9a87cd9102db4
Atm-Gregr <greg.rastklan@atm-consulting.fr>
2022-09-13 14:19:28 +0200

    retours

--- htdocs/projet/tasks/list.php
+++ htdocs/projet/tasks/list.php
@@ -988,7 +988,7 @@ while ($i < min($num, $limit)) {
 		// Description
 		if (!empty($arrayfields['t.description']['checked'])) {
 			print '<td>';
-			print nl2br($object->description);
+            print dolGetFirstLineOfText($object->description, 5);
 			print '</td>';
 			if (!$i) {
 				$totalarray['nbfield']++;
