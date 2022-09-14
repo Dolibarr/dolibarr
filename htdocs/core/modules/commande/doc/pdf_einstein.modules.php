85525f9ea935149310d9a32a8245e244629126dc
Philippe GRAND <philippe.grand@atoo-net.com>
2022-09-14 19:13:18 +0200

    update code toward php8 compliance

--- htdocs/core/modules/commande/doc/pdf_einstein.modules.php
+++ htdocs/core/modules/commande/doc/pdf_einstein.modules.php
@@ -149,10 +149,10 @@ class pdf_einstein extends ModelePDFCommandes
 		$this->page_largeur = $formatarray['width'];
 		$this->page_hauteur = $formatarray['height'];
 		$this->format = array($this->page_largeur, $this->page_hauteur);
-		$this->marge_gauche = isset($conf->global->MAIN_PDF_MARGIN_LEFT) ? $conf->global->MAIN_PDF_MARGIN_LEFT : 10;
-		$this->marge_droite = isset($conf->global->MAIN_PDF_MARGIN_RIGHT) ? $conf->global->MAIN_PDF_MARGIN_RIGHT : 10;
-		$this->marge_haute = isset($conf->global->MAIN_PDF_MARGIN_TOP) ? $conf->global->MAIN_PDF_MARGIN_TOP : 10;
-		$this->marge_basse = isset($conf->global->MAIN_PDF_MARGIN_BOTTOM) ? $conf->global->MAIN_PDF_MARGIN_BOTTOM : 10;
+		$this->marge_gauche = isset($conf->global->MAIN_PDF_MARGIN_LEFT) ? getDolGlobalInt('MAIN_PDF_MARGIN_LEFT') : 10;
+		$this->marge_droite = isset($conf->global->MAIN_PDF_MARGIN_RIGHT) ? getDolGlobalInt('MAIN_PDF_MARGIN_RIGHT') : 10;
+		$this->marge_haute = isset($conf->global->MAIN_PDF_MARGIN_TOP) ? getDolGlobalInt('MAIN_PDF_MARGIN_TOP') : 10;
+		$this->marge_basse = isset($conf->global->MAIN_PDF_MARGIN_BOTTOM) ? getDolGlobalInt('MAIN_PDF_MARGIN_BOTTOM') : 10;
 
 		$this->option_logo = 1; // Display logo
 		$this->option_tva = 1; // Manage the vat option FACTURE_TVAOPTION
