1a663b5105025d9bc256c4171292dfa7e4f7dd8c
Laurent Destailleur <eldy@destailleur.fr>
2022-09-17 13:41:07 +0200

    Fix page to setup OAUTH

--- htdocs/admin/oauth.php
+++ htdocs/admin/oauth.php
@@ -100,7 +100,7 @@ $form = new Form($db);
 $linkback = '<a href="'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans("BackToModuleList").'</a>';
 print load_fiche_titre($langs->trans('ConfigOAuth'), $linkback, 'title_setup');
 
-print '<form action="'.$_SERVER["PHP_SELF"].'" method="post">';
+print '<form action="'.$_SERVER["PHP_SELF"].'" method="POST">';
 print '<input type="hidden" name="token" value="'.newToken().'">';
 print '<input type="hidden" name="action" value="add">';
 
@@ -137,15 +137,8 @@ print '</form>';
 print '<br>';
 print '<br>';
 
+print dol_get_fiche_end();
 
-print '<form action="'.$_SERVER["PHP_SELF"].'" method="post">';
-print '<input type="hidden" name="token" value="'.newToken().'">';
-print '<input type="hidden" name="action" value="update">';
-
-print '<div class="div-table-responsive">';
-print '<table class="noborder centpercent">';
-
-$i = 0;
 
 //var_dump($list);
 foreach ($conf->global as $key => $val) {
@@ -155,82 +148,92 @@ foreach ($conf->global as $key => $val) {
 	}
 }
 
-// $list is defined into oauth.lib.php to the list of supporter OAuth providers.
-foreach ($listinsetup as $key) {
-	$supported = 0;
-	$keyforsupportedoauth2array = $key[0];						// May be OAUTH_GOOGLE_NAME or OAUTH_GOOGLE_xxx_NAME
-	$keyforsupportedoauth2array = preg_replace('/^OAUTH_/', '', $keyforsupportedoauth2array);
-	$keyforsupportedoauth2array = preg_replace('/_NAME$/', '', $keyforsupportedoauth2array);
-	if (preg_match('/^.*-/', $keyforsupportedoauth2array)) {
-		$keyforprovider = preg_replace('/^.*-/', '', $keyforsupportedoauth2array);
-	} else {
-		$keyforprovider = '';
-	}
-	$keyforsupportedoauth2array = preg_replace('/-.*$/', '', $keyforsupportedoauth2array);
-	$keyforsupportedoauth2array = 'OAUTH_'.$keyforsupportedoauth2array.'_NAME';
 
-	if (in_array($keyforsupportedoauth2array, array_keys($supportedoauth2array))) {
-		$supported = 1;
-	}
-	if (!$supported) {
-		continue; // show only supported
-	}
+if (count($listinsetup) > 0) {
+	print '<form action="'.$_SERVER["PHP_SELF"].'" method="POST">';
+	print '<input type="hidden" name="token" value="'.newToken().'">';
+	print '<input type="hidden" name="action" value="update">';
 
-	$i++;
+	print '<div class="div-table-responsive-no-min">';
+	print '<table class="noborder centpercent">';
 
-	print '<tr class="liste_titre'.($i > 1 ? ' liste_titre_add' : '').'">';
-	// Api Name
-	$label = $langs->trans($keyforsupportedoauth2array);
-	print '<td>';
-	print img_picto('', $supportedoauth2array[$keyforsupportedoauth2array]['picto'], 'class="pictofixedwidth"');
-	print $label;
-	if ($keyforprovider) {
-		print ' (<b>'.$keyforprovider.'</b>)';
-	} else {
-		print ' (<b>'.$langs->trans("NoName").'</b>)';
-	}
-	print '</td>';
-	print '<td>';
-	if (!empty($supportedoauth2array[$keyforsupportedoauth2array]['urlforcredentials'])) {
-		print $langs->trans("OAUTH_URL_FOR_CREDENTIAL", $supportedoauth2array[$keyforsupportedoauth2array]['urlforcredentials']);
-	}
-	print '</td>';
-	print '</tr>';
+	$i = 0;
 
-	if ($supported) {
-		$redirect_uri = $urlwithroot.'/core/modules/oauth/'.$supportedoauth2array[$keyforsupportedoauth2array]['callbackfile'].'_oauthcallback.php';
+	// $list is defined into oauth.lib.php to the list of supporter OAuth providers.
+	foreach ($listinsetup as $key) {
+		$supported = 0;
+		$keyforsupportedoauth2array = $key[0];						// May be OAUTH_GOOGLE_NAME or OAUTH_GOOGLE_xxx_NAME
+		$keyforsupportedoauth2array = preg_replace('/^OAUTH_/', '', $keyforsupportedoauth2array);
+		$keyforsupportedoauth2array = preg_replace('/_NAME$/', '', $keyforsupportedoauth2array);
+		if (preg_match('/^.*-/', $keyforsupportedoauth2array)) {
+			$keyforprovider = preg_replace('/^.*-/', '', $keyforsupportedoauth2array);
+		} else {
+			$keyforprovider = '';
+		}
+		$keyforsupportedoauth2array = preg_replace('/-.*$/', '', $keyforsupportedoauth2array);
+		$keyforsupportedoauth2array = 'OAUTH_'.$keyforsupportedoauth2array.'_NAME';
+
+		if (in_array($keyforsupportedoauth2array, array_keys($supportedoauth2array))) {
+			$supported = 1;
+		}
+		if (!$supported) {
+			continue; // show only supported
+		}
+
+		$i++;
+
+		print '<tr class="liste_titre'.($i > 1 ? ' liste_titre_add' : '').'">';
+		// Api Name
+		$label = $langs->trans($keyforsupportedoauth2array);
+		print '<td>';
+		print img_picto('', $supportedoauth2array[$keyforsupportedoauth2array]['picto'], 'class="pictofixedwidth"');
+		print $label;
+		if ($keyforprovider) {
+			print ' (<b>'.$keyforprovider.'</b>)';
+		} else {
+			print ' (<b>'.$langs->trans("NoName").'</b>)';
+		}
+		print '</td>';
+		print '<td>';
+		if (!empty($supportedoauth2array[$keyforsupportedoauth2array]['urlforcredentials'])) {
+			print $langs->trans("OAUTH_URL_FOR_CREDENTIAL", $supportedoauth2array[$keyforsupportedoauth2array]['urlforcredentials']);
+		}
+		print '</td>';
+		print '</tr>';
+
+		if ($supported) {
+			$redirect_uri = $urlwithroot.'/core/modules/oauth/'.$supportedoauth2array[$keyforsupportedoauth2array]['callbackfile'].'_oauthcallback.php';
+			print '<tr class="oddeven value">';
+			print '<td>'.$langs->trans("UseTheFollowingUrlAsRedirectURI").'</td>';
+			print '<td><input style="width: 80%" type"text" name="uri'.$keyforsupportedoauth2array.'" value="'.$redirect_uri.'">';
+			print '</td></tr>';
+		} else {
+			print '<tr class="oddeven value">';
+			print '<td>'.$langs->trans("UseTheFollowingUrlAsRedirectURI").'</td>';
+			print '<td>'.$langs->trans("FeatureNotYetSupported").'</td>';
+			print '</td></tr>';
+		}
+
+		// Api Id
 		print '<tr class="oddeven value">';
-		print '<td>'.$langs->trans("UseTheFollowingUrlAsRedirectURI").'</td>';
-		print '<td><input style="width: 80%" type"text" name="uri'.$keyforsupportedoauth2array.'" value="'.$redirect_uri.'">';
+		print '<td><label for="'.$key[1].'">'.$langs->trans("OAUTH_ID").'</label></td>';
+		print '<td><input type="text" size="100" id="'.$key[1].'" name="'.$key[1].'" value="'.$conf->global->{$key[1]}.'">';
 		print '</td></tr>';
-	} else {
+
+		// Api Secret
 		print '<tr class="oddeven value">';
-		print '<td>'.$langs->trans("UseTheFollowingUrlAsRedirectURI").'</td>';
-		print '<td>'.$langs->trans("FeatureNotYetSupported").'</td>';
+		print '<td><label for="'.$key[2].'">'.$langs->trans("OAUTH_SECRET").'</label></td>';
+		print '<td><input type="password" size="100" id="'.$key[2].'" name="'.$key[2].'" value="'.$conf->global->{$key[2]}.'">';
 		print '</td></tr>';
 	}
 
-	// Api Id
-	print '<tr class="oddeven value">';
-	print '<td><label for="'.$key[1].'">'.$langs->trans("OAUTH_ID").'</label></td>';
-	print '<td><input type="text" size="100" id="'.$key[1].'" name="'.$key[1].'" value="'.$conf->global->{$key[1]}.'">';
-	print '</td></tr>';
+	print '</table>'."\n";
+	print '</div>';
 
-	// Api Secret
-	print '<tr class="oddeven value">';
-	print '<td><label for="'.$key[2].'">'.$langs->trans("OAUTH_SECRET").'</label></td>';
-	print '<td><input type="password" size="100" id="'.$key[2].'" name="'.$key[2].'" value="'.$conf->global->{$key[2]}.'">';
-	print '</td></tr>';
+	print $form->buttonsSaveCancel("Modify", '');
+
+	print '</form>';
 }
-
-print '</table>'."\n";
-print '</div>';
-
-print dol_get_fiche_end();
-
-print $form->buttonsSaveCancel("Modify", '');
-
-print '</form>';
 
 // End of page
 llxFooter();
