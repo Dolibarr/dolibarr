0f9dd0b04a98cef3c1835c28852204af78a449be
Laurent Destailleur <eldy@destailleur.fr>
2022-09-22 14:53:34 +0200

    Fix missing test on multicurrency to show remain to pay vendor invoice

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -3507,7 +3507,7 @@ if ($action == 'create') {
 			print '<td class="right'.($resteapayeraffiche ? ' amountremaintopay' : (' '.$cssforamountpaymentcomplete)).'">'.price($resteapayeraffiche).'</td><td>&nbsp;</td></tr>';
 
 			// Remainder to pay Multicurrency
-			if ($object->multicurrency_code != $conf->currency || $object->multicurrency_tx != 1) {
+			if (isModEnabled('multicurreny') && $object->multicurrency_code != $conf->currency || $object->multicurrency_tx != 1) {
 				print '<tr><td colspan="'.$nbcols.'" class="right">';
 				print '<span class="opacitymedium">';
 				print $langs->trans('RemainderToPayMulticurrency');
