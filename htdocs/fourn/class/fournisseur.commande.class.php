fe501f7214935f16a1a6358b6febfd9cd29e583b
Laurent Destailleur <eldy@destailleur.fr>
2022-09-24 11:42:05 +0200

    Fix inventorycode

--- htdocs/fourn/class/fournisseur.commande.class.php
+++ htdocs/fourn/class/fournisseur.commande.class.php
@@ -2041,7 +2041,7 @@ class CommandeFournisseur extends CommonOrder
 					// $price should take into account discount (except if option STOCK_EXCLUDE_DISCOUNT_FOR_PMP is on)
 					$mouv->origin = &$this;
 					$mouv->setOrigin($this->element, $this->id);
-					$result = $mouv->reception($user, $product, $entrepot, $qty, $price, $comment, $eatby, $sellby, $batch, $inventorycode);
+					$result = $mouv->reception($user, $product, $entrepot, $qty, $price, $comment, $eatby, $sellby, $batch, '', 0, $inventorycode);
 					if ($result < 0) {
 						$this->error = $mouv->error;
 						$this->errors = $mouv->errors;
