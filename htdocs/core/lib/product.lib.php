49f1afccca09cf94d6714eec63c0a6e33d8ac838
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2022-09-25 06:09:29 +0200

    Use isModEnabled

--- htdocs/core/lib/product.lib.php
+++ htdocs/core/lib/product.lib.php
@@ -94,7 +94,7 @@ function product_prepare_head($object)
 		$h++;
 	}
 
-	if (!empty($conf->variants->enabled) && ($object->isProduct() || $object->isService())) {
+	if (isModEnabled('variants') && ($object->isProduct() || $object->isService())) {
 		global $db;
 
 		require_once DOL_DOCUMENT_ROOT.'/variants/class/ProductCombination.class.php';
