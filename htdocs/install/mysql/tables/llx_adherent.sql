702cfe356dc40c3c44b95bc30129be9ac0d6770b
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-09-26 23:16:03 +0200

    Update llx_adherent.sql
    add membersince date

--- htdocs/install/mysql/tables/llx_adherent.sql
+++ htdocs/install/mysql/tables/llx_adherent.sql
@@ -3,6 +3,7 @@
 -- Copyright (C) 2002-2003 Jean-Louis Bergamo   <jlb@j1b.org>
 -- Copyright (C) 2006-2009 Laurent Destailleur  <eldy@users.sourceforge.net>
 -- Copyright (C) 2009      Regis Houssin        <regis.houssin@inodbox.com>
+-- Copyright (C) 2021-2022 Udo Tammm            <dev@dolibit.de>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -37,21 +38,24 @@ create table llx_adherent
   civility         varchar(6),
   lastname         varchar(50),
   firstname        varchar(50),
-  login            varchar(50),                 -- login
+  login            varchar(50),                 -- login / username
   pass             varchar(50),                 -- password
   pass_crypted     varchar(128),
   fk_adherent_type integer NOT NULL,
   morphy           varchar(3) NOT NULL,         -- EN: legal entity / natural person  FR: personne morale / personne physique
-  societe          varchar(128),			          -- company name (should be same length than societe.name). No more used.
-  fk_soc           integer NULL,		            -- Link to third party linked to member
+  societe          varchar(128),                -- company name (should be same length than societe.name). No more used?
+  fk_soc           integer NULL,                -- Third-party linked to member
   address          text,
   zip              varchar(30),
   town             varchar(50),
   state_id         integer,
   country          integer,
+  
+  phone            varchar(30),
+  phone_perso      varchar(30),
+  phone_mobile     varchar(30),
   email            varchar(255),
   url              varchar(255) NULL,
-
   socialnetworks   text DEFAULT NULL,           -- json with socialnetworks
   --skype            varchar(255),                -- deprecated
   --twitter          varchar(255),                -- deprecated
@@ -63,25 +67,23 @@ create table llx_adherent
   --youtube          varchar(255),                -- deprecated
   --whatsapp         varchar(255),                -- deprecated
 
-  phone            varchar(30),
-  phone_perso      varchar(30),
-  phone_mobile     varchar(30),
   birth            date,                          -- birthday
+  membersince      date,                          -- member since date
   photo            varchar(255),                  -- filename or url of photo
-  statut           smallint NOT NULL DEFAULT 0,
-  public           smallint NOT NULL DEFAULT 0,   -- certain champ de la fiche sont ils public ou pas ?
-  datefin          datetime,                      -- end date of validity of the contribution / date de fin de validite de la cotisation
+  statut           smallint NOT NULL DEFAULT 0,   -- status / status / state of membership:  1 valid, 0 canceled, -1 draft, -2 excluded
+  public           smallint NOT NULL DEFAULT 0,   -- information are public
   default_lang     varchar(6) DEFAULT NULL,
   note_private     text DEFAULT NULL,
   note_public      text DEFAULT NULL,
   model_pdf		     varchar(255),
-  datevalid        datetime,                      -- date of validation
   datec            datetime,                      -- date of creation
+  datevalid        datetime,                      -- date of validation
+  datefin          datetime,                      -- end date of validity of the contribution / date de fin de validite de la cotisation
   tms              timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- last modification date
   fk_user_author   integer,                       -- can be null because member can be create by a guest
   fk_user_mod      integer,
   fk_user_valid    integer,
   canvas           varchar(32),                   -- type of canvas if used (null by default)
-  ip               varchar(250),                  -- ip used to create record (for public membership submission page)
+  ip               varchar(250),                  -- IP-address used to create the record (for public membership submission page)
   import_key       varchar(14)                    -- Import key
 )ENGINE=innodb;
