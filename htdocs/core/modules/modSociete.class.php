3e9e17c746ef6a5ce092d603e6e891a459285c6b
Laurent Destailleur <eldy@destailleur.fr>
2022-09-28 16:55:51 +0200

    Debug tooltip of import tool

--- htdocs/core/modules/modSociete.class.php
+++ htdocs/core/modules/modSociete.class.php
@@ -724,7 +724,7 @@ class modSociete extends DolibarrModules
 			$i++;
 		}
 
-		// Import list of contacts/additional addresses and attributes
+		// Import list of contacts/addresses of thirparties and attributes
 		$r++;
 		$this->import_code[$r] = $this->rights_class.'_'.$r;
 		$this->import_label[$r] = 'ImportDataset_company_2';
@@ -767,7 +767,7 @@ class modSociete extends DolibarrModules
 			}
 		}
 		// Add extra fields
-		$sql = "SELECT name, label, fieldrequired FROM ".MAIN_DB_PREFIX."extrafields WHERE type != 'separate' AND elementtype = 'socpeople' AND entity IN (0, ".$conf->entity.")";
+		$sql = "SELECT name, label, fieldrequired FROM ".MAIN_DB_PREFIX."extrafields WHERE type <> 'separate' AND elementtype = 'socpeople' AND entity IN (0, ".$conf->entity.")";
 		$resql = $this->db->query($sql);
 		if ($resql) {    // This can fail when class is used on an old database (during a migration for example)
 			while ($obj = $this->db->fetch_object($resql)) {
@@ -794,7 +794,7 @@ class modSociete extends DolibarrModules
 				'classfile' => '/core/class/cstate.class.php',
 				'class' => 'Cstate',
 				'method' => 'fetch',
-				'dict' => 'DictionaryStateCode'
+				'dict' => 'DictionaryCanton'
 			),
 			's.fk_pays' => array(
 				'rule' => 'fetchidfromcodeid',
