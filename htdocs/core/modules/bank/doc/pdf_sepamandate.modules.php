68dc522af5bce45b4411d1a06675bf80404ce3a1
Philippe GRAND <philippe.grand@atoo-net.com>
2022-09-29 12:01:07 +0200

    update code toward php8

--- htdocs/core/modules/bank/doc/pdf_sepamandate.modules.php
+++ htdocs/core/modules/bank/doc/pdf_sepamandate.modules.php
@@ -654,9 +654,7 @@ class pdf_sepamandate extends ModeleBankAccountDoc
 	protected function _pagefoot(&$pdf, $object, $outputlangs, $hidefreetext = 0)
 	{
 		// phpcs:enable
-		global $conf;
-
-		$showdetails = empty($conf->global->MAIN_GENERATE_DOCUMENTS_SHOW_FOOT_DETAILS) ? 0 : $conf->global->MAIN_GENERATE_DOCUMENTS_SHOW_FOOT_DETAILS;
+		$showdetails = getDolGlobalInt('MAIN_GENERATE_DOCUMENTS_SHOW_FOOT_DETAILS', 0);
 		return pdf_pagefoot($pdf, $outputlangs, 'PAYMENTORDER_FREE_TEXT', null, $this->marge_basse, $this->marge_gauche, $this->page_hauteur, $object, $showdetails, $hidefreetext);
 	}
 }
