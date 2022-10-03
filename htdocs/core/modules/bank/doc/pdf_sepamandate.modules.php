328d7962b3ad905d96d479d27d5810766cc89423
Philippe GRAND <philippe.grand@atoo-net.com>
2022-10-03 11:27:31 +0200

    update code towards php8 compliance

--- htdocs/core/modules/bank/doc/pdf_sepamandate.modules.php
+++ htdocs/core/modules/bank/doc/pdf_sepamandate.modules.php
@@ -182,7 +182,7 @@ class pdf_sepamandate extends ModeleBankAccountDoc
 				$pdf->SetCreator("Dolibarr ".DOL_VERSION);
 				$pdf->SetAuthor($outputlangs->convToOutputCharset($user->getFullName($outputlangs)));
 				$pdf->SetKeyWords($outputlangs->convToOutputCharset($object->ref)." ".$outputlangs->transnoentities("SepaMandate"));
-				if (!empty($conf->global->MAIN_DISABLE_PDF_COMPRESSION)) {
+				if (getDolGlobalString('MAIN_DISABLE_PDF_COMPRESSION')) {
 					$pdf->SetCompression(false);
 				}
 
