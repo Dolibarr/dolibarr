848b9f759355af0209ea49b4b043fa7eee436707
Philippe GRAND <philippe.grand@atoo-net.com>
2022-10-03 11:29:45 +0200

    update code towards php8 compliance

--- htdocs/core/modules/delivery/doc/pdf_typhon.modules.php
+++ htdocs/core/modules/delivery/doc/pdf_typhon.modules.php
@@ -284,7 +284,7 @@ class pdf_typhon extends ModelePDFDeliveryOrder
 				$pdf->SetCreator("Dolibarr ".DOL_VERSION);
 				$pdf->SetAuthor($outputlangs->convToOutputCharset($user->getFullName($outputlangs)));
 				$pdf->SetKeyWords($outputlangs->convToOutputCharset($object->ref)." ".$outputlangs->transnoentities("DeliveryOrder"));
-				if (!empty($conf->global->MAIN_DISABLE_PDF_COMPRESSION)) {
+				if (getDolGlobalString('MAIN_DISABLE_PDF_COMPRESSION')) {
 					$pdf->SetCompression(false);
 				}
 
