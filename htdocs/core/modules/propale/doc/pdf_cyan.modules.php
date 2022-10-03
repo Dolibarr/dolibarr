78cea181184aa87c57181546cba3d779db4cccd9
Philippe GRAND <philippe.grand@atoo-net.com>
2022-10-03 11:35:14 +0200

    update code towards php8 compliance

--- htdocs/core/modules/propale/doc/pdf_cyan.modules.php
+++ htdocs/core/modules/propale/doc/pdf_cyan.modules.php
@@ -360,7 +360,7 @@ class pdf_cyan extends ModelePDFPropales
 				$pdf->SetCreator("Dolibarr ".DOL_VERSION);
 				$pdf->SetAuthor($outputlangs->convToOutputCharset($user->getFullName($outputlangs)));
 				$pdf->SetKeyWords($outputlangs->convToOutputCharset($object->ref)." ".$outputlangs->transnoentities("PdfCommercialProposalTitle")." ".$outputlangs->convToOutputCharset($object->thirdparty->name));
-				if (!empty($conf->global->MAIN_DISABLE_PDF_COMPRESSION)) {
+				if (getDolGlobalString('MAIN_DISABLE_PDF_COMPRESSION')) {
 					$pdf->SetCompression(false);
 				}
 
