95cb38b52faefd14349861ae57d9708130e58b40
Philippe GRAND <philippe.grand@atoo-net.com>
2022-10-03 11:36:06 +0200

    update code towards php8 compliance

--- htdocs/core/modules/stock/doc/pdf_standard.modules.php
+++ htdocs/core/modules/stock/doc/pdf_standard.modules.php
@@ -251,7 +251,7 @@ class pdf_standard extends ModelePDFStock
 				$pdf->SetCreator("Dolibarr ".DOL_VERSION);
 				$pdf->SetAuthor($outputlangs->convToOutputCharset($user->getFullName($outputlangs)));
 				$pdf->SetKeyWords($outputlangs->convToOutputCharset($object->ref)." ".$outputlangs->transnoentities("Stock")." ".$outputlangs->convToOutputCharset($object->label));
-				if (!empty($conf->global->MAIN_DISABLE_PDF_COMPRESSION)) {
+				if (getDolGlobalString('MAIN_DISABLE_PDF_COMPRESSION')) {
 					$pdf->SetCompression(false);
 				}
 
