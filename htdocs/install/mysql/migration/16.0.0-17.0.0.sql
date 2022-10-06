bd4d6cb77e7b08a4ff97ee3bf3bbf4ede64cbd34
Laurent Destailleur <eldy@destailleur.fr>
2022-10-06 22:14:42 +0200

    Fix code of Bancontact

--- htdocs/install/mysql/migration/16.0.0-17.0.0.sql
+++ htdocs/install/mysql/migration/16.0.0-17.0.0.sql
@@ -55,6 +55,8 @@ ALTER TABLE llx_user DROP COLUMN idpers3;
 
 -- v17
 
+UPDATE llx_c_paiement SET code = 'BANCON' WHERE code = 'BAN' AND libelle = 'Bancontact';
+
 -- VMYSQL4.3 ALTER TABLE llx_partnership MODIFY COLUMN fk_user_creat integer NULL;
 -- VPGSQL8.2 ALTER TABLE llx_partnership ALTER COLUMN fk_user_creat DROP NOT NULL;
 
