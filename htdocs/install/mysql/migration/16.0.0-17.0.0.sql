10973e90aec004a651e5287534e28b816fe73f9f
Laurent Destailleur <eldy@destailleur.fr>
2022-10-06 20:01:00 +0200

    Add missing column to manage multicurrency

--- htdocs/install/mysql/migration/16.0.0-17.0.0.sql
+++ htdocs/install/mysql/migration/16.0.0-17.0.0.sql
@@ -186,3 +186,7 @@ ALTER TABLE llx_societe ADD INDEX idx_societe_nom(nom);
 ALTER TABLE llx_extrafields MODIFY COLUMN fielddefault text;
 
 ALTER TABLE llx_bank_url ADD INDEX idx_bank_url_url_id (url_id);
+
+ALTER TABLE llx_societe_remise_except ADD COLUMN multicurrency_code varchar(3) NULL;
+ALTER TABLE llx_societe_remise_except ADD COLUMN multicurrency_tx double(24,8) NULL;
+
