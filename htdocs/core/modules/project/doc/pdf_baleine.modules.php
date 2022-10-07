7fdf8e3fb123f5059bc41124f841b5dcf729b425
Philippe GRAND <philippe.grand@atoo-net.com>
2022-10-07 15:03:26 +0200

    update code towards php8 compliance

--- htdocs/core/modules/project/doc/pdf_baleine.modules.php
+++ htdocs/core/modules/project/doc/pdf_baleine.modules.php
@@ -229,7 +229,7 @@ class pdf_baleine extends ModelePDFProjects
 				$pdf->SetAutoPageBreak(1, 0);
 
 				$heightforinfotot = 40; // Height reserved to output the info and total part
-				$heightforfreetext = (isset($conf->global->MAIN_PDF_FREETEXT_HEIGHT) ? $conf->global->MAIN_PDF_FREETEXT_HEIGHT : 5); // Height reserved to output the free text on last page
+				$heightforfreetext = getDolGlobalInt('MAIN_PDF_FREETEXT_HEIGHT', 5); // Height reserved to output the free text on last page
 				$heightforfooter = $this->marge_basse + 8; // Height reserved to output the footer (value include bottom margin)
 				if (!empty($conf->global->MAIN_GENERATE_DOCUMENTS_SHOW_FOOT_DETAILS)) {
 					$heightforfooter += 6;
