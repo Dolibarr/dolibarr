53502ccd703e0a578d78c10be5862844055c7283
Regis Houssin <regis.houssin@inodbox.com>
2022-10-08 07:49:02 +0200

    FIX better fix for compatibility

--- htdocs/variants/ajax/get_attribute_values.php
+++ htdocs/variants/ajax/get_attribute_values.php
@@ -75,12 +75,7 @@ if ($prodattr->fetch($id) < 0) {
 
 $prodattrval = new ProductAttributeValue($db);
 
-$res = $prodattrval->fetchAllByProductAttribute($id);
-
-// Remove db for avoid error with postgresql
-foreach ($res as $key => $val) {
-	unset($res[$key]->db);
-}
+$res = $prodattrval->fetchAllByProductAttribute($id, false, 1);
 
 if ($res == -1) {
 	print json_encode(array(
