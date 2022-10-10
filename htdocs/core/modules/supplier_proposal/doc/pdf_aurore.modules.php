5a1716f62569b5fdedbb7d222def655d0bcf2099
Philippe GRAND <philippe.grand@atoo-net.com>
2022-10-10 11:05:09 +0200

    update code towards php8 compliance

--- htdocs/core/modules/supplier_proposal/doc/pdf_aurore.modules.php
+++ htdocs/core/modules/supplier_proposal/doc/pdf_aurore.modules.php
@@ -872,7 +872,7 @@ class pdf_aurore extends ModelePDFSupplierProposal
 
 			// If payment mode not forced or forced to VIR, show payment with BAN
 			if (empty($object->mode_reglement_code) || $object->mode_reglement_code == 'VIR') {
-				if (!empty($object->fk_bank) || !empty($conf->global->FACTURE_RIB_NUMBER)) {
+				if (!empty($object->fk_bank) || getDolGlobalInt('FACTURE_RIB_NUMBER')) {
 					$bankid = (empty($object->fk_bank) ? $conf->global->FACTURE_RIB_NUMBER : $object->fk_bank);
 					$account = new Account($this->db);
 					$account->fetch($bankid);
