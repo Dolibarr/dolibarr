fa4c0627bec404cb1be4ba9e44531fd02dbe4cba
Quentin VIAL-GOUTEYRON <quentin.vial-gouteyron@atm-consulting.fr>
2022-10-18 12:06:49 +0200

    NEW : Date d'entree en stock sur les exped au moment de la création

--- htdocs/product/class/productbatch.class.php
+++ htdocs/product/class/productbatch.class.php
@@ -447,6 +447,7 @@ class Productbatch extends CommonObject
 		$sql .= " t.eatby as oldeatby,"; // deprecated but may not be migrated into new table
 		$sql .= " t.batch,";
 		$sql .= " t.qty,";
+		$sql .= " MAX(sm.datem) as date_entree,";
 		$sql .= " t.import_key";
 		if ($fk_product > 0) {
 			$sql .= ", pl.rowid as lotid, pl.eatby as eatby, pl.sellby as sellby";
@@ -457,13 +458,17 @@ class Productbatch extends CommonObject
 			$sql .= " LEFT JOIN ".$dbs->prefix()."product_lot as pl ON pl.fk_product = ".((int) $fk_product)." AND pl.batch = t.batch";
 			// TODO May add extrafields to ?
 		}
+		$sql .= ' LEFT JOIN '.MAIN_DB_PREFIX.'product_stock AS ps ON (ps.rowid = fk_product_stock)';
+		$sql .= ' LEFT JOIN '.MAIN_DB_PREFIX.'stock_mouvement AS sm ON (sm.batch = t.batch AND ps.fk_entrepot=sm.fk_entrepot)';
 		$sql .= " WHERE fk_product_stock=".((int) $fk_product_stock);
 		if ($with_qty) {
 			$sql .= " AND t.qty <> 0";
 		}
-
+		$sql .= ' AND sm.type_mouvement IN (0,3)';
+		$sql .= ' GROUP BY t.rowid';
 		$sql .= " ORDER BY ";
 		// TODO : use product lifo and fifo when product will implement it
+		$sql .= 'date_entree ASC,t.batch ASC,';
 		if ($fk_product > 0) { $sql .= "pl.eatby ASC, pl.sellby ASC, "; }
 		$sql .= "t.eatby ASC, t.sellby ASC ";
 		$sql .= ", t.qty ".(!empty($conf->global->DO_NOT_TRY_TO_DEFRAGMENT_STOCKS_WAREHOUSE)?'DESC':'ASC'); // Note : qty ASC is important for expedition card, to avoid stock fragmentation
@@ -486,6 +491,7 @@ class Productbatch extends CommonObject
 				$tmp->batch = $obj->batch;
 				$tmp->qty = $obj->qty;
 				$tmp->import_key = $obj->import_key;
+				$tmp->stock_entry_date = $obj->date_entree;
 
 				$ret[$tmp->batch] = $tmp; // $ret is for a $fk_product_stock and unique key is on $fk_product_stock+batch
 				$i++;
