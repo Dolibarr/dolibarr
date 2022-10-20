b048d95cc82243b411a3478680a99d4d52e7312f
stickler-ci <support@stickler-ci.com>
2022-10-20 16:09:26 +0000

    Fixing style errors.

--- htdocs/product/card.php
+++ htdocs/product/card.php
@@ -1866,13 +1866,12 @@ if (is_object($objcanvas) && $objcanvas->displayCanvasExists($action)) {
 			print '<table class="border allwidth">';
 
 			// Ref
-			if (!empty($conf->global->MAIN_PRODUCT_REF_NOT_EDITABLE)) { 
+			if (!empty($conf->global->MAIN_PRODUCT_REF_NOT_EDITABLE)) {
 				print '<tr><td class="titlefieldcreate fieldrequired">'.$langs->trans("Ref").'</td><td colspan="3"><input name="ref" class="maxwidth200" maxlength="128" value="'.dol_escape_htmltag($object->ref).'"readonly="true"></td></tr>';
-			}
-			else {
+			} else {
 				print '<tr><td class="titlefieldcreate fieldrequired">'.$langs->trans("Ref").'</td><td colspan="3"><input name="ref" class="maxwidth200" maxlength="128" value="'.dol_escape_htmltag($object->ref).'"></td></tr>';
 			}
-			
+
 
 			// Label
 			print '<tr><td class="fieldrequired">'.$langs->trans("Label").'</td><td colspan="3"><input name="label" class="minwidth300 maxwidth400onsmartphone" maxlength="255" value="'.dol_escape_htmltag($object->label).'"></td></tr>';
