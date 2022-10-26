6c4b77a23c818c9b4225a2cd0659e7e93996835c
Anthony Berton <bertonanthony@gmail.com>
2022-10-26 18:35:58 +0200

    Hook

--- htdocs/commande/list_det.php
+++ htdocs/commande/list_det.php
@@ -1911,7 +1911,7 @@ if ($resql) {
 		include DOL_DOCUMENT_ROOT.'/core/tpl/extrafields_list_print_fields.tpl.php';
 		// Fields from hook
 		$parameters = array('arrayfields'=>$arrayfields, 'obj'=>$obj, 'i'=>$i, 'totalarray'=>&$totalarray);
-		$reshook = $hookmanager->executeHooks('printFieldListValue', $parameters); // Note that $action and $object may have been modified by hook
+		$reshook = $hookmanager->executeHooks('printFieldListValue', $parameters, $object, $action); // Note that $action and $object may have been modified by hook
 		print $hookmanager->resPrint;
 
 		// Date creation
@@ -2107,7 +2107,8 @@ if ($resql) {
 				$totalarray['nbfield']++;
 			}
 		}
-
+		// $totalarray['nbfield']--;
+		// $totalarray['nbfield']--;
 		print "</tr>\n";
 
 		$total += $obj->total_ht;
