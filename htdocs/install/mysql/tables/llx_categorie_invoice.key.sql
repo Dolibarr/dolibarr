26894a983fc5a862a7aec3376efd56da2f03cd0c
Florent Poinsaut <1256948+FlorentPoinsaut@users.noreply.github.com>
2022-11-04 14:09:27 +0100

    NEW #22693 Copyright update

--- htdocs/install/mysql/tables/llx_categorie_invoice.key.sql
+++ htdocs/install/mysql/tables/llx_categorie_invoice.key.sql
@@ -1,5 +1,6 @@
 -- ============================================================================
--- Copyright (C) 2007 Patrick Raguin       <patrick.raguin@gmail.com>
+-- Copyright (C) 2007 Patrick Raguin     <patrick.raguin@gmail.com>
+-- Copyright (C) 2022 Solution Libre SAS <contact@solution-libre.fr>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
