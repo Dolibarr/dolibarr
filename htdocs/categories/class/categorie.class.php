8a3fc5d10f661a0b2c5b85fab24130a9247a1cee
Florent Poinsaut <florent@solution-libre.fr>
2022-11-04 10:57:57 +0000

    NEW #22748 Add supplier order tags

--- htdocs/categories/class/categorie.class.php
+++ htdocs/categories/class/categorie.class.php
@@ -62,6 +62,7 @@ class Categorie extends CommonObject
 	const TYPE_KNOWLEDGEMANAGEMENT = 'knowledgemanagement';
 	const TYPE_INVOICE             = 'invoice';
 	const TYPE_SUPPLIER_INVOICE    = 'supplier_invoice';
+	const TYPE_SUPPLIER_ORDER      = 'supplier_order';
 
 	/**
 	 * @var string String with name of icon for myobject. Must be the part after the 'object_' into object_myobject.png
@@ -88,7 +89,8 @@ class Categorie extends CommonObject
 		'ticket'              => 12,
 		'knowledgemanagement' => 13,
 		'invoice'             => 14,
-		'supplier_invoice'    => 15
+		'supplier_invoice'    => 15,
+		'supplier_order'      => 16
 	);
 
 	/**
@@ -112,7 +114,8 @@ class Categorie extends CommonObject
 		12 => 'ticket',
 		13 => 'knowledgemanagement',
 		14 => 'invoice',
-		15 => 'supplier_invoice'
+		15 => 'supplier_invoice',
+		16 => 'supplier_order'
 	);
 
 	/**
@@ -159,7 +162,8 @@ class Categorie extends CommonObject
 		'ticket'              => 'Ticket',
 		'knowledgemanagement' => 'KnowledgeRecord',
 		'invoice'             => 'Facture',
-		'supplier_invoice'    => 'FactureFournisseur'
+		'supplier_invoice'    => 'FactureFournisseur',
+		'supplier_order'      => 'CommandeFournisseur'
 	);
 
 	/**
@@ -181,7 +185,8 @@ class Categorie extends CommonObject
 		'actioncomm'       => 'ActioncommCategoriesArea',
 		'website_page'     => 'WebsitePageCategoriesArea',
 		'invoice'          => 'InvoiceCategoriesArea',
-		'supplier_invoice' => 'SupplierInvoiceCategoriesArea'
+		'supplier_invoice' => 'SupplierInvoiceCategoriesArea',
+		'supplier_order'   => 'SupplierOrderCategoriesArea'
 	);
 
 	/**
@@ -198,7 +203,8 @@ class Categorie extends CommonObject
 		'warehouse'           => 'entrepot',
 		'knowledgemanagement' => 'knowledgemanagement_knowledgerecord',
 		'invoice'             => 'facture',
-		'supplier_invoice'    => 'facture_fourn'
+		'supplier_invoice'    => 'facture_fourn',
+		'supplier_order'      => 'commande_fournisseur'
 	);
 
 	/**
@@ -259,6 +265,7 @@ class Categorie extends CommonObject
 	 * @see Categorie::TYPE_TICKET
 	 * @see Categorie::TYPE_INVOICE
 	 * @see Categorie::TYPE_SUPPLIER_INVOICE
+	 * @see Categorie::TYPE_SUPPLIER_ORDER
 	 */
 	public $type;
 
--- htdocs/fourn/commande/card.php
+++ htdocs/fourn/commande/card.php
@@ -34,6 +34,7 @@
  */
 
 require '../../main.inc.php';
+require_once DOL_DOCUMENT_ROOT.'/categories/class/categorie.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/class/html.formorder.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/modules/supplier_order/modules_commandefournisseur.php';
@@ -263,6 +264,14 @@ if (empty($reshook)) {
 	// Set project
 	if ($action == 'classin' && $usercancreate && $caneditproject) {
 		$result = $object->setProject($projectid);
+		if ($result < 0) {
+			setEventMessages($object->error, $object->errors, 'errors');
+		}
+	}
+
+	// Set tags
+    if ($action == 'settags' && isModEnabled('categorie') && $usercancreate) {
+		$result = $object->setCategories(GETPOST('categories', 'array'));
 		if ($result < 0) {
 			setEventMessages($object->error, $object->errors, 'errors');
 		}
@@ -1393,6 +1402,13 @@ if (empty($reshook)) {
 				}
 			}
 
+			if (isModEnabled('categorie')) {
+				$categories = GETPOST('categories', 'array');
+				if (method_exists($object, 'setCategories')) {
+					$object->setCategories($categories);
+				}
+			}
+
 			if ($error) {
 				$langs->load("errors");
 				$db->rollback();
@@ -1774,6 +1790,15 @@ if ($action == 'create') {
 		print '<td class="maxwidthonsmartphone">';
 		print $form->selectMultiCurrency($currency_code, 'multicurrency_code');
 		print '</td></tr>';
+	}
+
+	// Categories
+	if (!empty($conf->categorie->enabled)) {
+		print '<tr><td>'.$langs->trans("Categories").'</td><td colspan="3">';
+		$cate_arbo = $form->select_all_categories(Categorie::TYPE_SUPPLIER_ORDER, '', 'parent', 64, 0, 1);
+		$arrayselected = GETPOST('categories', 'array');
+		print img_picto('', 'category').$form->multiselectarray('categories', $cate_arbo, $arrayselected, '', 0, 'quatrevingtpercent widthcentpercentminusx', 0, 0);
+		print "</td></tr>";
 	}
 
 	print '<tr><td>'.$langs->trans('NotePublic').'</td>';
@@ -2285,6 +2310,40 @@ if ($action == 'create') {
 			print $form->select_incoterms((!empty($object->fk_incoterms) ? $object->fk_incoterms : ''), (!empty($object->location_incoterms) ? $object->location_incoterms : ''), $_SERVER['PHP_SELF'].'?id='.$object->id);
 		}
 		print '</td></tr>';
+	}
+
+	// Tags-Categories
+	if (isModEnabled('categorie')) {
+		print '<tr><td>';
+		print '<table class="nobordernopadding centpercent"><tr><td>';
+		print $langs->trans("Categories");
+		print '<td><td class="right">';
+		if ($usercancreate) {
+			print '<a class="editfielda" href="'.DOL_URL_ROOT.'/fourn/commande/card.php?id='.$object->id.'&action=edittags&token='.newToken().'">'.img_edit().'</a>';
+		} else {
+			print '&nbsp;';
+		}
+		print '</td></tr></table>';
+		print '</td>';
+		print '<td>';
+		$cate_arbo = $form->select_all_categories(Categorie::TYPE_SUPPLIER_ORDER, '', 'parent', 64, 0, 1);
+		if ($action == 'edittags') {
+			print '<form method="POST" action="'.$_SERVER['PHP_SELF'].'?id='.$object->id.'">';
+			print '<input type="hidden" name="action" value="settags">';
+			print '<input type="hidden" name="token" value="'.newToken().'">';
+			$c = new Categorie($db);
+			$cats = $c->containing($object->id, Categorie::TYPE_SUPPLIER_ORDER);
+			$arrayselected=[];
+			foreach ($cats as $cat) {
+				$arrayselected[] = $cat->id;
+			}
+			print img_picto('', 'category').$form->multiselectarray('categories', $cate_arbo, $arrayselected, 0, 0, 'quatrevingtpercent widthcentpercentminusx', 0, '0');
+			print '<input type="submit" class="button valignmiddle" value="'.$langs->trans("Modify").'">';
+			print '</form>';
+		} else {
+			print $form->showCategories($object->id, Categorie::TYPE_SUPPLIER_ORDER, 1);
+		}
+		print "</td></tr>";
 	}
 
 	// Other attributes
