ec572823829b1bed5e0793cdb786f65cd4423737
VESSILLER <lvessiller@open-dsi.fr>
2022-11-16 14:39:47 +0100

    NEW hook printFieldListFrom in contact list

--- htdocs/contact/list.php
+++ htdocs/contact/list.php
@@ -403,6 +403,10 @@ if (!empty($search_categ_supplier) && $search_categ_supplier != '-1') {
 if (!$user->rights->societe->client->voir && !$socid) {
 	$sql .= " LEFT JOIN ".MAIN_DB_PREFIX."societe_commerciaux as sc ON s.rowid = sc.fk_soc";
 }
+// Add fields from hooks
+$parameters = array();
+$reshook = $hookmanager->executeHooks('printFieldListFrom', $parameters, $object, $action);    // Note that $action and $object may have been modified by hook
+$sql .= $hookmanager->resPrint;
 $sql .= ' WHERE p.entity IN ('.getEntity('socpeople').')';
 if (!$user->rights->societe->client->voir && !$socid) { //restriction
 	$sql .= " AND (sc.fk_user = ".((int) $user->id)." OR p.fk_soc IS NULL)";
