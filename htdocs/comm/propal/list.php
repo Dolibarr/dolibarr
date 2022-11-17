b8e5b10cace4d937bd27be277ad8c0db4b2b04d0
Laurent Destailleur <eldy@destailleur.fr>
2022-11-17 02:29:30 +0100

    Default field

--- htdocs/comm/propal/list.php
+++ htdocs/comm/propal/list.php
@@ -203,7 +203,7 @@ if (empty($user->socid)) {
 $checkedtypetiers = 0;
 $arrayfields = array(
 	'p.ref'=>array('label'=>"Ref", 'checked'=>1),
-	'p.ref_client'=>array('label'=>"RefCustomer", 'checked'=>1),
+	'p.ref_client'=>array('label'=>"RefCustomer", 'checked'=>-1),
 	'pr.ref'=>array('label'=>"ProjectRef", 'checked'=>1, 'enabled'=>(empty($conf->project->enabled) ? 0 : 1)),
 	'pr.title'=>array('label'=>"ProjectLabel", 'checked'=>0, 'enabled'=>(empty($conf->project->enabled) ? 0 : 1)),
 	's.nom'=>array('label'=>"ThirdParty", 'checked'=>1),
