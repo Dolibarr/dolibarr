c2d0f970637ab0ee1e22396f3406a8a239d14315
Laurent Destailleur <eldy@destailleur.fr>
2022-11-20 15:13:40 +0100

    Website

--- htdocs/install/doctemplates/websites/website_template-restaurant/containers/page250.tpl.php
+++ htdocs/install/doctemplates/websites/website_template-restaurant/containers/page250.tpl.php
@@ -113,28 +113,6 @@ ob_start();
                                                     >26.50<small>€</small></span
                                                 >
                                             </div>
-
-                                            <div
-                                                class="d-flex flex-wrap align-items-center"
-                                            >
-                                                <div class="reviews-stars">
-                                                    <i
-                                                        class="bi-star-fill reviews-icon"
-                                                    ></i>
-                                                    <i
-                                                        class="bi-star-fill reviews-icon"
-                                                    ></i>
-                                                    <i
-                                                        class="bi-star-fill reviews-icon"
-                                                    ></i>
-                                                    <i
-                                                        class="bi-star reviews-icon"
-                                                    ></i>
-                                                    <i
-                                                        class="bi-star reviews-icon"
-                                                    ></i>
-                                                </div>
-                                            </div>
                                         </div>
                                     </div>
 
@@ -158,28 +136,6 @@ ob_start();
                                                 <span class="price-tag ms-4"
                                                     >18.25<small>€</small></span
                                                 >
-                                            </div>
-
-                                            <div
-                                                class="d-flex flex-wrap align-items-center"
-                                            >
-                                                <div class="reviews-stars">
-                                                    <i
-                                                        class="bi-star-fill reviews-icon"
-                                                    ></i>
-                                                    <i
-                                                        class="bi-star-fill reviews-icon"
-                                                    ></i>
-                                                    <i
-                                                        class="bi-star-fill reviews-icon"
-                                                    ></i>
-                                                    <i
-                                                        class="bi-star-fill reviews-icon"
-                                                    ></i>
-                                                    <i
-                                                        class="bi-star reviews-icon"
-                                                    ></i>
-                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -215,22 +171,6 @@ ob_start();
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="video-wrap">
-                    <video
-                        autoplay=""
-                        loop=""
-                        muted=""
-                        class="custom-video"
-                        poster=""
-                    >
-                        <source
-                            src="image/aaab/video/production_ID_3769033.mp4"
-                            type="video/mp4"
-                        />
-                        Your browser does not support the video tag.
-                    </video>
                 </div>
 
                 <div class="overlay"></div>
