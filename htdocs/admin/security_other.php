bae0a0129473c3d145031070d3aaa1be2867c534
hystepik <lmarcouiller@nltechno.com>
2022-11-28 11:06:36 +0100

    modif public page

--- htdocs/admin/security_other.php
+++ htdocs/admin/security_other.php
@@ -196,7 +196,7 @@ print '<tr class="oddeven">';
 print '<td>'.$langs->trans("MaxNumberOfPostOnPublicPagesByIP").'</td><td class="right">';
 print '</td>';
 print '<td class="nowrap">';
-print '<input class="flat right width50" name="MAIN_SECURITY_MAX_POST_ON_PUBLIC_PAGES_BY_IP_ADDRESS" type="text" value="'.getDolGlobalInt("MAIN_SECURITY_MAX_POST_ON_PUBLIC_PAGES_BY_IP_ADDRESS", 1000).'">';
+print '<input class="flat right width50" name="MAIN_SECURITY_MAX_POST_ON_PUBLIC_PAGES_BY_IP_ADDRESS" type="text" value="'.getDolGlobalInt("MAIN_SECURITY_MAX_POST_ON_PUBLIC_PAGES_BY_IP_ADDRESS", 1000).'"> '.strtolower($langs->trans("Files"));
 print '</td>';
 print '</tr>';
 
@@ -204,7 +204,7 @@ print '<tr class="oddeven">';
 print '<td>'.$langs->trans("MaxNumberOfAttachementOnForms").'</td><td class="right">';
 print '</td>';
 print '<td class="nowrap">';
-print '<input class="flat right width50" name="MAIN_SECURITY_MAX_ATTACHMENT_ON_FORMS" type="text" value="'.getDolGlobalInt("MAIN_SECURITY_MAX_ATTACHMENT_ON_FORMS", 10).'">';
+print '<input class="flat right width50" name="MAIN_SECURITY_MAX_ATTACHMENT_ON_FORMS" type="text" value="'.getDolGlobalInt("MAIN_SECURITY_MAX_ATTACHMENT_ON_FORMS", 10).'"> '.strtolower($langs->trans("Files"));
 print '</td>';
 print '</tr>';
 
