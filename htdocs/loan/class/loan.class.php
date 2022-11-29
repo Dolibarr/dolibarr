00bf293b33041def533e1f19e59f23559f76fcb7
FLIO <oflifli@nltechno.com>
2022-11-29 22:50:07 +0100

    Fix(scrutinizer) https://scrutinizer-ci.com/g/Dolibarr/dolibarr/issues/develop/files/htdocs/loan/info.php?selectedLabels%5B0%5D=9&selectedSeverities%5B0%5D=10&orderField=lastFound&order=desc&honorSelectedPaths=0

--- htdocs/loan/class/loan.class.php
+++ htdocs/loan/class/loan.class.php
@@ -66,6 +66,7 @@ class Loan extends CommonObject
 	public $account_capital;
 	public $account_insurance;
 	public $account_interest;
+	public $total_paid;
 
 	/**
 	 * @var integer|string date_creation
