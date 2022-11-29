003fb75f697f66723ea7f80f5da253ab6fc0b37e
Lamrani Abdel <lamrani002@gmail.com>
2022-11-29 10:51:06 +0100

    fix by optimize condition about event Type

--- htdocs/comm/action/card.php
+++ htdocs/comm/action/card.php
@@ -713,7 +713,7 @@ if (empty($reshook) && $action == 'update') {
 			} else {
 				$object->type_id = dol_getIdFromCode($db, GETPOST("actioncode", 'aZ09'), 'c_actioncomm');
 			}
-		} 
+		}
 		$object->label       = GETPOST("label", "alphanohtml");
 		$object->datep       = $datep;
 		$object->datef       = $datef;
