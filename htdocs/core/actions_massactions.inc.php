2a16198981c30c45527e3c608a009defc6172aa3
Milen Karaganski <milen@blacktiehost.com>
2022-12-07 17:09:54 +0200

    Put back error counter $nbignored++;

--- htdocs/core/actions_massactions.inc.php
+++ htdocs/core/actions_massactions.inc.php
@@ -1041,6 +1041,7 @@ if (!$error && ($massaction == 'delete' || ($action == 'delete' && $confirm == '
 
 			if ($objectclass == 'Holiday' && ! in_array($objecttmp->statut, array(Holiday::STATUS_DRAFT, Holiday::STATUS_CANCELED, Holiday::STATUS_REFUSED))) {
 				$langs->load("errors");
+				$nbignored++;
 				$TMsg[] = '<div class="error">'.$langs->trans('ErrorLeaveRequestMustBeDraftCanceledOrRefusedToBeDeleted', $objecttmp->ref).'</div><br>';
 				continue;
 			}
