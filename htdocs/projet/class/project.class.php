f0f66327b938927b0d255f4ed28a1f79fa83adc5
Milen Karaganski <milen@blacktiehost.com>
2022-12-07 17:47:01 +0200

    Fix syntax error

--- htdocs/projet/class/project.class.php
+++ htdocs/projet/class/project.class.php
@@ -792,7 +792,7 @@ class Project extends CommonObject
 			}
 		}
 		if (count($sqlwhere) > 0) {
-			$sql .= ' AND ('.implode(' '.$filtermode.' ', $sqlwhere).')';
+			$sql .= ' AND ('.implode("'.$filtermode.'", $sqlwhere).')';
 		}
 
 		if (!empty($sortfield)) {
