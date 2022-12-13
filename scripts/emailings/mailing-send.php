8c6f3719c3e4b87ce78a1c09c529242413ebdd02
Laurent Destailleur <eldy@destailleur.fr>
2022-12-13 15:22:59 +0100

    Fix delay between emails

--- scripts/emailings/mailing-send.php
+++ scripts/emailings/mailing-send.php
@@ -80,7 +80,7 @@ if (empty($conf->global->MAILING_LIMIT_SENDBYCLI)) {
 print "***** ".$script_file." (".$version.") pid=".dol_getmypid()." *****\n";
 
 if (!empty($conf->global->MAILING_DELAY)) {
-	print 'A delay of '.((float) $conf->global->MAILING_DELAY * 1000000).' millisecond has been set between each email'."\n";
+	print 'A delay of '.((float) $conf->global->MAILING_DELAY * 1000000).' seconds has been set between each email'."\n";
 }
 
 if ($conf->global->MAILING_LIMIT_SENDBYCLI == '-1') {
