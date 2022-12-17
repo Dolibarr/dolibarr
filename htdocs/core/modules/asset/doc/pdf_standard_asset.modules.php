76126d119d8eb6dac0fb2f3f00af50fc56f08693
Philippe GRAND <philippe.grand@atoo-net.com>
2022-12-17 12:40:33 +0100

    update code toward php 8 compliance

--- htdocs/core/modules/asset/doc/pdf_standard_asset.modules.php
+++ htdocs/core/modules/asset/doc/pdf_standard_asset.modules.php
@@ -951,7 +951,7 @@ class pdf_standard_asset extends ModelePDFAsset
 				if (!empty($conf->mycompany->multidir_output[$object->entity])) {
 					$logodir = $conf->mycompany->multidir_output[$object->entity];
 				}
-				if (empty($conf->global->MAIN_PDF_USE_LARGE_LOGO)) {
+				if (!getDolGlobalInt('MAIN_PDF_USE_LARGE_LOGO')) {
 					$logo = $logodir.'/logos/thumbs/'.$this->emetteur->logo_small;
 				} else {
 					$logo = $logodir.'/logos/'.$this->emetteur->logo;
