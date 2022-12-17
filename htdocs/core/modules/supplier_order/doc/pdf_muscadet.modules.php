28679a9e403a6a868ec23268eacdf9646949c9c6
Philippe GRAND <philippe.grand@atoo-net.com>
2022-12-17 12:46:14 +0100

    update code toward php 8 compliance

--- htdocs/core/modules/supplier_order/doc/pdf_muscadet.modules.php
+++ htdocs/core/modules/supplier_order/doc/pdf_muscadet.modules.php
@@ -1171,7 +1171,7 @@ class pdf_muscadet extends ModelePDFSuppliersOrders
 				if (!empty($conf->mycompany->multidir_output[$object->entity])) {
 					$logodir = $conf->mycompany->multidir_output[$object->entity];
 				}
-				if (empty($conf->global->MAIN_PDF_USE_LARGE_LOGO)) {
+				if (!getDolGlobalInt('MAIN_PDF_USE_LARGE_LOGO')) {
 					$logo = $logodir.'/logos/thumbs/'.$this->emetteur->logo_small;
 				} else {
 					$logo = $logodir.'/logos/'.$this->emetteur->logo;
