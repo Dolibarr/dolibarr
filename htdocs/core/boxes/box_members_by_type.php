7c6543b52902d4f16f1a2b1c9f703dc7d12fbe4e
daraelmin <daraelmin@gmail.com>
2022-12-20 19:43:22 +0100

    Copyright

--- htdocs/core/boxes/box_members_by_type.php
+++ htdocs/core/boxes/box_members_by_type.php
@@ -3,7 +3,7 @@
  * Copyright (C) 2004-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@inodbox.com>
  * Copyright (C) 2015-2020 Frederic France      <frederic.france@netlogic.fr>
- * Copyright (C) 2021      Waël Almoman         <info@almoman.com>
+ * Copyright (C) 2021-2022 Waël Almoman         <info@almoman.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
