3004c3cd920b815651aaa36c491cd4444d7fb1e7
Josep Lluís <joseplluis@lliuretic.cat>
2022-12-21 13:22:56 +0100

    Update html.formpropal.class.php

--- htdocs/core/class/html.formpropal.class.php
+++ htdocs/core/class/html.formpropal.class.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2012 Laurent Destailleur   <eldy@users.sourceforge.net>
+ * Copyright (C) 2022 Josep Lluís Amador	<joseplluis@lliuretic.cat>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
