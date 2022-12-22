328b62eeb4dc4e35aa50209a2a3b4a6c0139c792
Frédéric France <frederic.france@free.fr>
2022-12-22 22:31:03 +0100

    clean code

--- htdocs/bookcal/availabilities_agenda.php
+++ htdocs/bookcal/availabilities_agenda.php
@@ -48,7 +48,7 @@ $res = 0;
 // Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
 if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
 	$res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
-}getDolGlobalString('AGENDA_DEFAULT_FILTER_TYPE_FOR_OBJECT')
+}
 // Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
 $tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
 while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
