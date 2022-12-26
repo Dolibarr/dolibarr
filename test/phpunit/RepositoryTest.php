ed0e96f2b69a91ac0132458f651de7e0b3636f8a
Laurent Destailleur <eldy@destailleur.fr>
2022-12-26 17:05:14 +0100

    Fix phpunit

--- test/phpunit/RepositoryTest.php
+++ test/phpunit/RepositoryTest.php
@@ -176,7 +176,7 @@ class RepositoryTest extends PHPUnit\Framework\TestCase
 		// Scan dir to guarante we don't have library jquery twice
 		$foundfiles=dol_dir_list(DOL_DOCUMENT_ROOT.'/includes/', 'files', 1, '^jquery\.min\.js', array('ckeditor'));
 		print __METHOD__." count(founddirs)=".count($foundfiles)."\n";
-		$this->assertEquals(1, count($foundfiles), 'We found jquery lib (jquery.min.js) twice '.$foundfiles[0]['fullname'].' + '.$foundfiles[1]['fullname']);
+		$this->assertEquals(1, count($foundfiles), 'We found jquery lib (jquery.min.js) twice '.(empty($foundfiles[0]) ? '' : $foundfiles[0]['fullname']).' '.(empty($foundfiles[1]) ? '' : $foundfiles[1]['fullname']));
 	}
 
 
