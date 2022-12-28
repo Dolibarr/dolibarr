085ee619baff7924bd996fcbb17b3254286e4d88
Laurent Destailleur <eldy@destailleur.fr>
2022-12-28 02:33:14 +0100

    Add more properties in hook getNomUrl

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -5127,7 +5127,7 @@ class Product extends CommonObject
 
 		global $action;
 		$hookmanager->initHooks(array('productdao'));
-		$parameters = array('id'=>$this->id, 'getnomurl' => &$result);
+		$parameters = array('id'=>$this->id, 'getnomurl' => &$result, 'label' => &$label);
 		$reshook = $hookmanager->executeHooks('getNomUrl', $parameters, $this, $action); // Note that $action and $object may have been modified by some hooks
 		if ($reshook > 0) {
 			$result = $hookmanager->resPrint;
