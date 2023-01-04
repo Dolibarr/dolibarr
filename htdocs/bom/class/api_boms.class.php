18550211c8179b29a4159a5f05abf1b1434a414c
stickler-ci <support@stickler-ci.com>
2023-01-04 17:32:30 +0000

    Fixing style errors.

--- htdocs/bom/class/api_boms.class.php
+++ htdocs/bom/class/api_boms.class.php
@@ -554,8 +554,8 @@ class Boms extends DolibarrApi
 			throw new RestException(400, "Wrong naming scheme '(PROV%)' is only allowed on 'DRAFT' status. For automatic increment use 'auto' on the 'ref' field.");
 		}
 
-		if ($this->bom->ref == 'auto'){
-			if (empty($this->bom->id) && $this->bom->status == 0){
+		if ($this->bom->ref == 'auto') {
+			if (empty($this->bom->id) && $this->bom->status == 0) {
 				$this->bom->ref = ''; // 'ref' will auto incremented with '(PROV' + newID + ')'
 			} else {
 				$this->bom->fetch_product();
