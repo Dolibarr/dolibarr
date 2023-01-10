9a249bcbb8b58f6576193604540ec2962543f895
Lamrani Abdel <lamrani002@gmail.com>
2023-01-10 12:10:43 +0100

    kanban mode for list of proposal supplier

--- htdocs/supplier_proposal/class/supplier_proposal.class.php
+++ htdocs/supplier_proposal/class/supplier_proposal.class.php
@@ -2723,6 +2723,42 @@ class SupplierProposal extends CommonObject
 
 		return CommonObject::commonReplaceProduct($db, $origin_id, $dest_id, $tables);
 	}
+
+
+	/**
+	 *	Return clicable link of object (with eventually picto)
+	 *
+	 *	@param      string	    $option                 Where point the link (0=> main card, 1,2 => shipment, 'nolink'=>No link)
+	 *  @return		string		HTML Code for Kanban thumb.
+	 */
+	public function getKanbanView($option = '')
+	{
+		global $langs;
+		$return = '<div class="box-flex-item box-flex-grow-zero">';
+		$return .= '<div class="info-box info-box-sm">';
+		$return .= '<span class="info-box-icon bg-infobox-action">';
+		$return .= img_picto('', $this->picto);
+		//$return .= '<i class="fa fa-dol-action"></i>'; // Can be image
+		$return .= '</span>';
+		$return .= '<div class="info-box-content">';
+		$return .= '<span class="info-box-ref">'.(method_exists($this, 'getNomUrl') ? $this->getNomUrl() : $this->ref).'</span>';
+		if (property_exists($this, 'socid')) {
+			$return .= '<span class="info-box-ref"> | '.$this->socid.'</span>';
+		}
+		if (property_exists($this, 'delivery_date')) {
+			$return .= '<br><span class="opacitymedium">'.$langs->trans("DateEnd").'</span> : <span class="info-box-label">'.dol_print_date($this->delivery_date).'</span>';
+		}
+		if (property_exists($this, 'total_ttc')) {
+			$return .='<br><span class="opacitymedium" >'.$langs->trans("AmountHT").' : </span><span class="info-box-label amount">'.price($this->total_ttc).'</span>';
+		}
+		if (method_exists($this, 'getLibStatut')) {
+			$return .= '<br><div class="info-box-status margintoponly">'.$this->getLibStatut(5).'</div>';
+		}
+		$return .= '</div>';
+		$return .= '</div>';
+		$return .= '</div>';
+		return $return;
+	}
 }
 
 
