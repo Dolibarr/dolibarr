7bcaae86f4b9656302a544805a316282e6591805
ptibogxiv <support@ptibogxiv.net>
2023-01-11 13:33:29 +0100

    Fix php 8 warning

--- htdocs/admin/dict.php
+++ htdocs/admin/dict.php
@@ -2514,7 +2514,7 @@ function fieldList($fieldlist, $obj = '', $tabname = '', $context = '')
 			print '</td>';
 		} elseif ($value == 'type_duration') {
 			print '<td>';
-			print $form->selectTypeDuration('', $obj->{$value}, array('i','h'));
+			print $form->selectTypeDuration('', (!empty($obj->{$value}) ? $obj->{$value}:''), array('i','h'));
 			print '</td>';
 		} else {
 			$fieldValue = isset($obj->{$value}) ? $obj->{$value}: '';
