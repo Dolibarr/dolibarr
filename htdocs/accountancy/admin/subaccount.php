64ecdd0632f4ef18518abcedf7ba117635e99362
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2023-01-14 18:35:09 +0100

    Update subaccount.php

--- htdocs/accountancy/admin/subaccount.php
+++ htdocs/accountancy/admin/subaccount.php
@@ -19,17 +19,18 @@
 
 /**
  * \file 		htdocs/accountancy/admin/subaccount.php
- * \ingroup     Accountancy (Double entries)
- * \brief		List of accounting sub-account (auxiliary accounts)
+ * \ingroup 	Accountancy (Double entries)
+ * \brief 		List of accounting sub-account (auxiliary accounts)
  */
 
 // Load Dolibarr environment
 require '../../main.inc.php';
-require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/accounting.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
+
 
 // Load translation files required by the page
-$langs->loadLangs(array("compta", "bills", "admin", "accountancy", "salaries", "hrm", "errors"));
+$langs->loadLangs(array("accountancy", "admin", "bills", "compta", "errors", "hrm", "salaries"));
 
 $mesg = '';
 $action = GETPOST('action', 'aZ09');
@@ -81,6 +82,7 @@ if ($conf->global->MAIN_FEATURES_LEVEL < 2) {
 	unset($arrayfields['reconcilable']);
 }
 
+
 /*
  * Actions
  */
@@ -120,10 +122,12 @@ if (empty($reshook)) {
 
 $form = new Form($db);
 
-$help_url = '';
-$title = $langs->trans('ChartOfIndividualAccountsOfSubsidiaryLedger');
 
+// Page Header
+$help_url = 'EN:Module_Double_Entry_Accounting#Setup';
+$title = $langs->trans('ChartOfIndividualAccountsOfSubsidiaryLedger');
 llxHeader('', $title, $help_url);
+
 
 // Customer
 $sql = "SELECT sa.rowid, sa.nom as label, sa.code_compta as subaccount, '1' as type, sa.entity";
@@ -219,7 +223,7 @@ if (!empty($search_type) && $search_type >= 0) {
 	$sql .= " HAVING type LIKE '".$db->escape($search_type)."'";
 }
 
-// User
+// User - Employee
 $sql .= " UNION ";
 $sql .= " SELECT u.rowid, u.lastname as label, u.accountancy_code as subaccount, '3' as type, u.entity FROM ".MAIN_DB_PREFIX."user u";
 $sql .= " WHERE u.entity IN (".getEntity('user').")";
@@ -400,14 +404,17 @@ if ($resql) {
 		if (!empty($arrayfields['type']['checked'])) {
 			print '<td class="center">';
 			$s = '';
+			
 			// Customer
 			if ($obj->type == 1) {
 				$s .= '<a class="customer-back" style="padding-left: 6px; padding-right: 6px" title="'.$langs->trans("Customer").'" href="'.DOL_URL_ROOT.'/comm/card.php?socid='.$obj->rowid.'">'.$langs->trans("Customer").'</a>';
 			} elseif ($obj->type == 2) {
-				// Supplier
+				
+			// Supplier
 				$s .= '<a class="vendor-back" style="padding-left: 6px; padding-right: 6px" title="'.$langs->trans("Supplier").'" href="'.DOL_URL_ROOT.'/fourn/card.php?socid='.$obj->rowid.'">'.$langs->trans("Supplier").'</a>';
 			} elseif ($obj->type == 3) {
-				// User
+				
+			// User - Employee
 				$s .= '<a class="user-back" style="padding-left: 6px; padding-right: 6px" title="'.$langs->trans("Employee").'" href="'.DOL_URL_ROOT.'/user/card.php?id='.$obj->rowid.'">'.$langs->trans("Employee").'</a>';
 			}
 			print $s;
@@ -440,14 +447,17 @@ if ($resql) {
 		// Action
 		print '<td class="center">';
 		$e = '';
+		
 		// Customer
 		if ($obj->type == 1) {
 			$e .= '<a class="editfielda" title="'.$langs->trans("Customer").'" href="'.DOL_URL_ROOT.'/societe/card.php?action=edit&token='.newToken().'&socid='.$obj->rowid.'&backtopage='.urlencode($_SERVER["PHP_SELF"]).'">'.img_edit().'</a>';
 		} elseif ($obj->type == 2) {
-			// Supplier
+			
+		// Supplier
 			$e .= '<a class="editfielda" title="'.$langs->trans("Supplier").'" href="'.DOL_URL_ROOT.'/societe/card.php?action=edit&token='.newToken().'&socid='.$obj->rowid.'&backtopage='.urlencode($_SERVER["PHP_SELF"]).'">'.img_edit().'</a>';
 		} elseif ($obj->type == 3) {
-			// User
+			
+		// User - Employee
 			$e .= '<a class="editfielda" title="'.$langs->trans("Employee").'" href="'.DOL_URL_ROOT.'/user/card.php?action=edit&token='.newToken().'&id='.$obj->rowid.'&backtopage='.urlencode($_SERVER["PHP_SELF"]).'">'.img_edit().'</a>';
 		}
 		print $e;
