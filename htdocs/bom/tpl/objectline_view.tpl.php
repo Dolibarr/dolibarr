777cfd5753a3d41fe75d3724a736a1e81e0426c8
Adrien Raze <adrien.raze@atm-consulting.fr>
2023-01-26 15:26:24 +0100

    FIX : Sub-Bom costs were not good

--- htdocs/bom/tpl/objectline_view.tpl.php
+++ htdocs/bom/tpl/objectline_view.tpl.php
@@ -171,7 +171,7 @@ $tmpbom->calculateCosts();
 print '<td id="costline_'.$line->id.'" class="linecolcost nowrap right">';
 $coldisplay++;
 if (!empty($line->fk_bom_child)) {
-	echo '<span class="amount">'.price($tmpbom->total_cost).'</span>';
+	echo '<span class="amount">'.price($tmpbom->total_cost * $line->qty).'</span>';
 } else {
 	echo '<span class="amount">'.price($line->total_cost).'</span>';
 }
