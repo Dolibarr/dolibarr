2c826a4fccec04a00748deb6fd2a0a4a46113342
kkhelifa <kkhelifa@open-dsi.fr>
2023-02-01 15:33:14 +0100

    FIX: Remove orphelan $this->db->rollback() in the function insertExtrafields()

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -6296,7 +6296,6 @@ abstract class CommonObject
 									$new_array_options[$key] = $object->id;
 								} else {
 									$this->error = "Id/Ref '".$value."' for object '".$object->element."' not found";
-									$this->db->rollback();
 									return -1;
 								}
 							}
