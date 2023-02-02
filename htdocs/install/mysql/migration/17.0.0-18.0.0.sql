1be5145148ec0037b545ac1fc834c97b4a6d7120
Laurent Destailleur <eldy@destailleur.fr>
2023-02-02 14:56:03 +0100

    NEW Add more zip, town, country for owner of a bank account

--- htdocs/install/mysql/migration/17.0.0-18.0.0.sql
+++ htdocs/install/mysql/migration/17.0.0-18.0.0.sql
@@ -64,3 +64,10 @@ ALTER TABLE llx_website ADD COLUMN pageviews_previous_month BIGINT UNSIGNED DEFA
 
 ALTER TABLE llx_product_stock ADD CONSTRAINT fk_product_product_rowid FOREIGN KEY (fk_product) REFERENCES llx_product (rowid);
 ALTER TABLE llx_product_stock ADD CONSTRAINT fk_entrepot_entrepot_rowid FOREIGN KEY (fk_entrepot) REFERENCES llx_entrepot (rowid);
+
+
+ALTER TABLE llx_bank_account ADD COLUMN owner_zip varchar(25);
+ALTER TABLE llx_bank_account ADD COLUMN owner_town varchar(50);
+ALTER TABLE llx_bank_account ADD COLUMN owner_country_id integer DEFAULT NULL;
+
+  
