016d1117a69f3818485b3b7ad8e8c0d4a321fa7c
Benjamin Falière <benjamin.faliere@altairis.fr>
2023-02-06 18:02:28 +0100

    added my name to the file

--- htdocs/bom/class/bom.class.php
+++ htdocs/bom/class/bom.class.php
@@ -1,5 +1,6 @@
 <?php
-/* Copyright (C) 2019  Laurent Destailleur <eldy@users.sourceforge.net>
+/* Copyright (C) 2019	Laurent Destailleur	<eldy@users.sourceforge.net>
+ * Copyright (C) 2023	Benjamin Falière	<benjamin.faliere@altairis.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
