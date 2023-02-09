6fb24bfcc78bcc125a31d0ea184fe98ae9e878aa
Laurent Destailleur <eldy@destailleur.fr>
2023-02-09 18:00:25 +0100

    Add missing index

--- htdocs/install/mysql/migration/17.0.0-18.0.0.sql
+++ htdocs/install/mysql/migration/17.0.0-18.0.0.sql
@@ -70,4 +70,13 @@ ALTER TABLE llx_bank_account ADD COLUMN owner_zip varchar(25);
 ALTER TABLE llx_bank_account ADD COLUMN owner_town varchar(50);
 ALTER TABLE llx_bank_account ADD COLUMN owner_country_id integer DEFAULT NULL;
 
-  
+
+ALTER TABLE llx_supplier_proposal ADD UNIQUE INDEX uk_supplier_proposal_ref (ref, entity);
+
+ALTER TABLE llx_supplier_proposal ADD INDEX idx_supplier_proposal_fk_soc (fk_soc);
+ALTER TABLE llx_supplier_proposal ADD INDEX idx_supplier_proposal_fk_user_author (fk_user_author);
+ALTER TABLE llx_supplier_proposal ADD INDEX idx_supplier_proposal_fk_user_valid (fk_user_valid);
+ALTER TABLE llx_supplier_proposal ADD INDEX idx_supplier_proposal_fk_projet (fk_projet);
+ALTER TABLE llx_supplier_proposal ADD INDEX idx_supplier_proposal_fk_account(fk_account);
+
+
