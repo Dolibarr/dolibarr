b52de45f73926958a7f7eecd978b7bed868c1b19
Philippe GRAND <philippe.grand@atoo-net.com>
2023-02-10 11:01:09 +0100

    fix : Warning: Undefined property: stdClass:: in /home/httpd/vhosts/aflac.fr/domains/dev.aflac.fr/httpdocs/core/modules/facture/mod_facture_mercure.php on line 88

--- htdocs/core/modules/facture/mod_facture_mercure.php
+++ htdocs/core/modules/facture/mod_facture_mercure.php
@@ -85,7 +85,7 @@ class mod_facture_mercure extends ModeleNumRefFactures
 
 		// Prefix setting of replacement invoices
 		$texte .= '<tr><td><span class="opacitymedium">'.$langs->trans("Mask").' ('.$langs->trans("InvoiceReplacement").'):</span></td>';
-		$texte .= '<td class="right">'.$form->textwithpicto('<input type="text" class="flat minwidth175" name="maskreplacement" value="'.$conf->global->FACTURE_MERCURE_MASK_REPLACEMENT.'">', $tooltip, 1, 1).'</td>';
+		$texte .= '<td class="right">'.$form->textwithpicto('<input type="text" class="flat minwidth175" name="maskreplacement" value="FACTURE_MERCURE_MASK_REPLACEMENT">', $tooltip, 1, 1).'</td>';
 		$texte .= '</tr>';
 
 		// Prefix setting of credit note
