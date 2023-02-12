6e3966abaeda0628503250c231c265fbb883ccc1
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2023-02-12 20:21:11 +0100

    Update hookmanager.class.php
    saw now  'dashboardPropals'  was twice

--- htdocs/core/class/hookmanager.class.php
+++ htdocs/core/class/hookmanager.class.php
@@ -183,7 +183,6 @@ class HookManager
 			'dashboardProductServices',
 			'dashboardProjects',
 			'dashboardPropals',
-			'dashboardPropals',
 			'dashboardSpecialBills',
 			'dashboardSupplierProposal',
 			'dashboardThirdparties',
