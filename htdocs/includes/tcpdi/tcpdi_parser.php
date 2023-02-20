8348c34fe1d20d5f29a3309bd2365e81963f7e85
Sylvain Legrand <sylvain.legrand@infras.fr>
2023-02-20 18:48:07 +0100

    Fix tcpdi_parser infinite loop (#23972)
    When mergin some old PDF we may have an infinite loop with this warning : "PHP Warning: Illegal string offset 'Type' in ./htdocs/includes/tcpdi/tcpdi_parser.php on line 706"

--- htdocs/includes/tcpdi/tcpdi_parser.php
+++ htdocs/includes/tcpdi/tcpdi_parser.php
@@ -715,8 +715,7 @@ class tcpdi_parser {
 				$next = strcspn($data, "\r\n", $offset);
 				if ($next > 0) {
 					$offset += $next;
-					list($obj, $unused) = $this->getRawObject($offset, $data);
-					return $obj;
+					return $this->getRawObject($offset, $data);
 				}
 				break;
 			}
