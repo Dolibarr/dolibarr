fd371cc52b517ea26f50d588cc2c8cf19bc520ce
Yoan Mollard <ymollard@users.noreply.github.com>
2023-02-24 23:31:06 +0100

    Fix member nature display in new.php

--- htdocs/public/members/new.php
+++ htdocs/public/members/new.php
@@ -539,6 +539,8 @@ if (empty($conf->global->MEMBER_NEWFORM_FORCETYPE)) {
 // Moral/Physic attribute
 $morphys["phy"] = $langs->trans("Physical");
 $morphys["mor"] = $langs->trans("Moral");
+print '<tr class="morphy"><td class="titlefield">'.$langs->trans('MemberNature').' <span style="color: red">*</span></td><td>'."\n";
+
 if (empty($conf->global->MEMBER_NEWFORM_FORCEMORPHY)) {
 	print '<tr class="morphy"><td class="titlefield">'.$langs->trans('MemberNature').' <span style="color: red">*</span></td><td>'."\n";
 	print $form->selectarray("morphy", $morphys, GETPOST('morphy'), 1);
