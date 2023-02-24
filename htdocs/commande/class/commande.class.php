652a215a07ffaace796ab0695fe941085764c483
Frédéric FRANCE <frederic.france@free.fr>
2023-02-24 16:57:39 +0100

    set errors from object

--- htdocs/commande/class/commande.class.php
+++ htdocs/commande/class/commande.class.php
@@ -1279,8 +1279,7 @@ class Commande extends CommonOrder
 				$action = '';
 				$reshook = $hookmanager->executeHooks('createFrom', $parameters, $this, $action); // Note that $action and $object may have been modified by some hooks
 				if ($reshook < 0) {
-					$this->errors += $hookmanager->errors;
-					$this->error = $hookmanager->error;
+					$this->setErrorsFromObject($hookmanager);
 					$error++;
 				}
 			}
@@ -1436,8 +1435,7 @@ class Commande extends CommonOrder
 			$action = '';
 			$reshook = $hookmanager->executeHooks('createFrom', $parameters, $this, $action); // Note that $action and $object may have been modified by some hooks
 			if ($reshook < 0) {
-				$this->errors += $hookmanager->errors;
-				$this->error = $hookmanager->error;
+				$this->setErrorsFromObject($hookmanager);
 				$error++;
 			}
 
