32ba0acb320f21d192f315ebcc700f05e7e9cdb6
Laurent Destailleur <eldy@destailleur.fr>
2023-02-26 12:03:55 +0100

    Fix navigation

--- htdocs/comm/mailing/cibles.php
+++ htdocs/comm/mailing/cibles.php
@@ -542,7 +542,7 @@ if ($object->fetch($id) >= 0) {
 
 		$massactionbutton = '';
 
-		print_barre_liste($langs->trans("MailSelectedRecipients"), $page, $_SERVER["PHP_SELF"], $param, $sortfield, $sortorder, $morehtmlcenter, $num, $nbtotalofrecords, 'generic', 0, '', '', $limit);
+		print_barre_liste($langs->trans("MailSelectedRecipients"), $page, $_SERVER["PHP_SELF"], $param, $sortfield, $sortorder, $morehtmlcenter, $num, $nbtotalofrecords, 'generic', 0, '', '', $limit, 0, 0, 1);
 
 		print '</form>';
 
