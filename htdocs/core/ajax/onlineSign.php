1ff1d5fd686624c9165d0614934ccc4ef91b21bd
Anthony Berton <anthony.berton@bb2a.fr>
2023-02-28 22:07:49 +0100

    size font

--- htdocs/core/ajax/onlineSign.php
+++ htdocs/core/ajax/onlineSign.php
@@ -173,7 +173,7 @@ if ($action == "importSignature") {
 						$wforimg = $s['w'] - 20 - $xforimgstart;
 
 						$pdf->SetXY($xforimgstart, $yforimgstart + round($wforimg / 4) - 4);
-						$pdf->SetFont($default_font, '', $default_font_size);
+						$pdf->SetFont($default_font, '', $default_font_size - 1);
 						$pdf->MultiCell($wforimg, 4, $langs->trans("DateSigning").': '.dol_print_date(dol_now(), "daytext", false, $langs, true), 0, 'L');
 						$pdf->SetXY($xforimgstart, $yforimgstart + round($wforimg / 4));
 						$pdf->MultiCell($wforimg, 4, $langs->trans("Lastname").': '.$online_sign_name, 0, 'L');
