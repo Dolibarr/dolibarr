80e73f2b9fee762b979b5fb62fc2d2d56844305d
Eric Seigne <eric.seigne@cap-rel.fr>
2023-03-02 08:47:18 +0100

    better situation invoice title

--- htdocs/core/modules/facture/doc/pdf_sponge.modules.php
+++ htdocs/core/modules/facture/doc/pdf_sponge.modules.php
@@ -1921,7 +1921,7 @@ class pdf_sponge extends ModelePDFFactures
 			$title = $outputlangs->transnoentities("InvoiceProForma");
 		}
 		if ($this->situationinvoice) {
-			$title = $outputlangs->transnoentities("PDFInvoiceSituation");
+			$title = $outputlangs->transnoentities("PDFSituationTitle", $object->situation_counter);
 		}
 		if (!empty($conf->global->PDF_USE_ALSO_LANGUAGE_CODE) && is_object($outputlangsbis)) {
 			$title .= ' - ';
@@ -1940,19 +1940,30 @@ class pdf_sponge extends ModelePDFFactures
 				$title .= $outputlangsbis->transnoentities("InvoiceProForma");
 			}
 		}
+        if (empty($this->situationinvoice)) {
+            $title .= ' '.$outputlangs->convToOutputCharset($object->ref);
+        }
+
+		if ($object->statut == $object::STATUS_DRAFT) {
+			$pdf->SetTextColor(128, 0, 0);
+			$title .= ' - '.$outputlangs->transnoentities("NotValidated");
+		}
+
 		$pdf->MultiCell($w, 3, $title, '', 'R');
 
 		$pdf->SetFont('', 'B', $default_font_size);
 
-		$posy += 5;
-		$pdf->SetXY($posx, $posy);
-		$pdf->SetTextColor(0, 0, 60);
-		$textref = $outputlangs->transnoentities("Ref")." : ".$outputlangs->convToOutputCharset($object->ref);
-		if ($object->statut == $object::STATUS_DRAFT) {
-			$pdf->SetTextColor(128, 0, 0);
-			$textref .= ' - '.$outputlangs->transnoentities("NotValidated");
-		}
-		$pdf->MultiCell($w, 4, $textref, '', 'R');
+        if ($this->situationinvoice) {
+            $posy += 5;
+            $pdf->SetXY($posx, $posy);
+            $pdf->SetTextColor(0, 0, 60);
+            $textref = $outputlangs->transnoentities("Ref")." : ".$outputlangs->convToOutputCharset($object->ref);
+            if ($object->statut == $object::STATUS_DRAFT) {
+                $pdf->SetTextColor(128, 0, 0);
+                $textref .= ' - '.$outputlangs->transnoentities("NotValidated");
+            }
+            $pdf->MultiCell($w, 4, $textref, '', 'R');
+        }
 
 		$posy += 1;
 		$pdf->SetFont('', '', $default_font_size - 2);
