3e0689879d267fb4c04c4535094de475d884783a
Adrien Raze <adrien.raze@atm-consulting.fr>
2023-03-03 10:25:23 +0100

    FIX : stickler-ci

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -8895,9 +8895,9 @@ class Form
 			);
 		}
 
-		if($object->table_element == 'commande_fournisseur') {
+		if ($object->table_element == 'commande_fournisseur') {
 			$possiblelinks['mo']['sql'] = "SELECT s.rowid as socid, s.nom as name, s.client, t.rowid, t.ref, t.rowid, '0' as total_ht FROM ".$this->db->prefix()."societe as s INNER JOIN ".$this->db->prefix().'mrp_mo as t ON t.fk_soc = s.rowid  WHERE t.entity IN ('.getEntity('mo').')';
-		} elseif($object->table_element == 'mrp_mo') {
+		} elseif ($object->table_element == 'mrp_mo') {
 			$possiblelinks['order_supplier']['sql'] = "SELECT s.rowid as socid, s.nom as name, s.client, t.rowid, t.ref, t.ref_supplier, t.total_ht FROM ".$this->db->prefix()."societe as s, ".$this->db->prefix().'commande_fournisseur as t WHERE t.fk_soc = s.rowid AND t.entity IN ('.getEntity('commande_fournisseur').')';
 		}
 
