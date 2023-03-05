d9c47b93df3fbd2e77608899e26f07a75d385d71
Yoan Mollard <ymollard@users.noreply.github.com>
2023-03-05 16:55:45 +0100

    Revert "Drop the double <tr>"
    This reverts commit a02e88dc281c0545150ecc7e9f2716a0787208a4.

--- htdocs/public/members/new.php
+++ htdocs/public/members/new.php
@@ -542,6 +542,7 @@ $morphys["mor"] = $langs->trans("Moral");
 print '<tr class="morphy"><td class="titlefield">'.$langs->trans('MemberNature').' <span style="color: red">*</span></td><td>'."\n";
 
 if (empty($conf->global->MEMBER_NEWFORM_FORCEMORPHY)) {
+	print '<tr class="morphy"><td class="titlefield">'.$langs->trans('MemberNature').' <span style="color: red">*</span></td><td>'."\n";
 	print $form->selectarray("morphy", $morphys, GETPOST('morphy'), 1);
 	print '</td></tr>'."\n";
 } else {
