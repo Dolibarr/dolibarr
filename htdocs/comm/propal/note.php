efcaa0780af31af52c12f43c4c1c764026d4bdda
bomuux <bruno@musmeaux.ovh>
2023-03-08 10:47:21 +0100

    Update note.php

--- htdocs/comm/propal/note.php
+++ htdocs/comm/propal/note.php
@@ -51,7 +51,6 @@ if ($id > 0 || !empty($ref)) {
 // Security check
 if ($user->socid > 0) {
 	$socid = $user->socid;
-	$object->id = $user->socid;
 }
 
 // Initialize technical object to manage hooks of page. Note that conf->hooks_modules contains array of hook context
