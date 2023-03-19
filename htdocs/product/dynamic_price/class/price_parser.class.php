4a79bf2667dc63fa018013a74d846dea12bce72a
priojk <priojk@users.noreply.github.com>
2023-03-19 11:10:01 +0100

    Sickler: indentation

--- htdocs/product/dynamic_price/class/price_parser.class.php
+++ htdocs/product/dynamic_price/class/price_parser.class.php
@@ -156,7 +156,7 @@ class PriceParser
 
 		//Retrieve all extrafield for product and add it to values
 		$extrafields = new ExtraFields($this->db);
-        $extralabels = $extrafields->fetch_name_optionals_label($product->table_element);
+		$extralabels = $extrafields->fetch_name_optionals_label($product->table_element);
 		$product->fetch_optionals();
 		foreach ($extralabels as $key => $label) {
 			$values["extrafield_".$key] = $product->array_options['options_'.$key];
