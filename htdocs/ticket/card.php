857111d47a24c126d5e4f31dcac515dd8452d849
Maximilien Rozniecki <mrozniecki@easya.solutions>
2023-03-23 11:16:32 +0100

    update to be as in the PR merged in develop

--- htdocs/ticket/card.php
+++ htdocs/ticket/card.php
@@ -893,9 +893,9 @@ if ($action == 'create' || $action == 'presend') {
 			} else {
 				$morehtmlref .= $form->form_thirdparty($url_page_current.'?track_id='.$object->track_id, $object->socid, 'none', '', 1, 0, 0, array(), 1);
 			}
-			if (empty($conf->global->MAIN_DISABLE_OTHER_LINK)) {
-				$object->fetch_thirdparty();
-				$morehtmlref .= ' (<a href="'.DOL_URL_ROOT.'/ticket/list.php?socid='.$object->thirdparty->id.'&search_societe='.urlencode($object->thirdparty->name).'">'.$langs->trans("OtherTickets").'</a>)';
+			if (empty($conf->global->MAIN_DISABLE_OTHER_LINK) && !empty($object->socid)) {
+				$morehtmlref .= ' (<a href="'.DOL_URL_ROOT.'/ticket/list.php?socid='.$object->socid.'&sortfield=t.datec&sortorder=desc">'.
+				img_picto($langs->trans("Tickets"), 'ticket', 'class="pictofixedwidth"').' '.$langs->trans("TicketHistory").'</a>';
 			}
 		}
 
