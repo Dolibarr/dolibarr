3d9aa470783c5bf0060004f4b89e2171d2310b3c
Anthony Berton <anthony.berton@bb2a.fr>
2023-04-05 12:47:14 +0200

    Copyright

--- htdocs/knowledgemanagement/knowledgerecord_list.php
+++ htdocs/knowledgemanagement/knowledgerecord_list.php
@@ -1,6 +1,7 @@
 <?php
-/* Copyright (C) 2007-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2021       Frédéric France         <frederic.france@netlogic.fr>
+/* Copyright (C) 2007-2017	Laurent Destailleur		<eldy@users.sourceforge.net>
+ * Copyright (C) 2021		Frédéric France			<frederic.france@netlogic.fr>
+ * Copyright (C) 2023		Anthony Berton			<anthony.berton@bb2a.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
