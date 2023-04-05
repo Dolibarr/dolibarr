9b141e9d1d2b43b4b9af8216be0d5a6e90871753
Laurent Destailleur <eldy@destailleur.fr>
2023-04-05 03:26:27 +0200

    var not used

--- htdocs/adherents/class/adherent_type.class.php
+++ htdocs/adherents/class/adherent_type.class.php
@@ -996,8 +996,6 @@ class AdherentType extends CommonObject
 	{
 		global $langs, $user;
 
-		$selected = (empty($arraydata['selected']) ? 0 : $arraydata['selected']);
-
 		$return = '<div class="box-flex-item box-flex-grow-zero">';
 		$return .= '<div class="info-box info-box-sm">';
 		$return .= '<span class="info-box-icon bg-infobox-action">';
@@ -1005,7 +1003,10 @@ class AdherentType extends CommonObject
 		$return .= '</span>';
 		$return .= '<div class="info-box-content">';
 		$return .= '<span class="info-box-ref">'.(method_exists($this, 'getNomUrl') ? $this->getNomUrl() : $this->ref).'</span>';
+
+		//$selected = (empty($arraydata['selected']) ? 0 : $arraydata['selected']);
 		//$return .= '<input id="cb'.$this->id.'" class="flat checkforselect fright" type="checkbox" name="toselect[]" value="'.$this->id.'"'.($selected ? ' checked="checked"' : '').'>';
+
 		if ($user->rights->adherent->configurer) {
 			$return .= '<span class="right paddingleft"><a class="editfielda" href="'.$_SERVER["PHP_SELF"].'?action=edit&rowid='.urlencode($this->ref).'">'.img_edit().'</a></span>';
 		} else {
