eae52f95d21ab6ff4694f417091d1adc1c866e2f
Laurent Destailleur <eldy@destailleur.fr>
2023-04-07 11:34:51 +0200

    Better log

--- htdocs/emailcollector/class/emailcollector.class.php
+++ htdocs/emailcollector/class/emailcollector.class.php
@@ -2169,6 +2169,8 @@ class EmailCollector extends CommonObject
 												}
 											}
 										}
+									} else {
+										dol_syslog("One and only one existing third party has been found");
 									}
 								}
 							}
@@ -2552,6 +2554,8 @@ class EmailCollector extends CommonObject
 										}
 									}
 								}
+							} else {
+								$operationslog .= '<br>Project already exists for msgid ='.dol_escape_htmltag($msgid);
 							}
 						} elseif ($operation['type'] == 'ticket') {
 							// Create ticket
