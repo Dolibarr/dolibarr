796a6778b9b5b996a0aec4f13a839793413425d6
Laurent Destailleur <eldy@destailleur.fr>
2023-04-27 14:45:06 +0200

    Fix warning

--- htdocs/compta/prelevement/factures.php
+++ htdocs/compta/prelevement/factures.php
@@ -324,12 +324,12 @@ if ($resql) {
 		print '<td>'.$langs->trans("Total").'</td>';
 		print '<td>&nbsp;</td>';
 		print '<td class="right">';
-		//if ($totalinvoices != $object->amount) print img_warning("AmountOfFileDiffersFromSumOfInvoices");		// It is normal to have total that differs. For an amount of invoice of 100, request to pay may be 50 only.
-		if ($totalamount_requested != $object->amount) {
-			print img_warning("AmountOfFileDiffersFromSumOfInvoices");
-		}
 		print "</td>\n";
 		print '<td class="right">';
+		// If the page show all record (no pagination) and total does not match total of file, we show a warning. Should not happen.
+		if (($nbtotalofrecords <= $num) && $totalamount_requested != $object->amount) {
+			print img_warning("AmountOfFileDiffersFromSumOfInvoices");
+		}
 		print price($totalamount_requested);
 		print "</td>\n";
 		print '<td>&nbsp;</td>';
