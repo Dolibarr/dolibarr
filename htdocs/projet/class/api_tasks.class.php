926aa6fec76e89a7ddac79da2051435914075242
VMR Global Solutions <contact@vmr-globalsolutions.eu>
2023-04-28 09:31:33 +0200

    API add / fix filter on extrafields in index()
    Add / change in api_xxx.class.php files the ability to filter on extrafields when missing in the public functions index()
    by changing the SQL query with the pattern 
    " FROM ".MAIN_DB_PREFIX"xxxx AS t LEFT JOIN ".MAIN_DB_PREFIX."xxxx_extrafields AS ef ON (ef.fk_object = t.rowid)

--- htdocs/projet/class/api_tasks.class.php
+++ htdocs/projet/class/api_tasks.class.php
@@ -127,7 +127,7 @@ class Tasks extends DolibarrApi
 		if ((!DolibarrApiAccess::$user->rights->societe->client->voir && !$socids) || $search_sale > 0) {
 			$sql .= ", sc.fk_soc, sc.fk_user"; // We need these fields in order to filter by sale (including the case where the user can only see his prospects)
 		}
-		$sql .= " FROM ".MAIN_DB_PREFIX."projet_task as t";
+		$sql .= " FROM ".MAIN_DB_PREFIX."projet_task AS t LEFT JOIN ".MAIN_DB_PREFIX."projet_task_extrafields AS ef ON (ef.fk_object = t.rowid)"; // Modification VMR Global Solutions to include extrafields as search parameters in the API GET call, so we will be able to filter on extrafields
 
 		if ((!DolibarrApiAccess::$user->rights->societe->client->voir && !$socids) || $search_sale > 0) {
 			$sql .= ", ".MAIN_DB_PREFIX."societe_commerciaux as sc"; // We need this table joined to the select in order to filter by sale
