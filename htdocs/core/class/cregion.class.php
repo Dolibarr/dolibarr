1d5cf1df7a82418fdedda960d02aa881d221d4d7
Richard Rondu <rondu.richard@lainwir3d.net>
2023-05-13 14:27:11 +0100

    Fix copyright of cregion.class.php
    The file is mostly a copy paste of eldy's work

--- htdocs/core/class/cregion.class.php
+++ htdocs/core/class/cregion.class.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) Richard Rondu  <rondu.richard@lainwir3d.net>
+ * Copyright (C) 2007-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
