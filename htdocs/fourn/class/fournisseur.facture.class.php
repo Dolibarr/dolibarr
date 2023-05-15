12471dcb0606fb0dd14a24b28eca523962b9d888
NextGestion <contact@nextgestion.com>
2023-05-15 09:48:45 +0100

    Processing a return from @eldy "So we should not have setEventMessage inside such method"

--- htdocs/fourn/class/fournisseur.facture.class.php
+++ htdocs/fourn/class/fournisseur.facture.class.php
@@ -1848,12 +1848,11 @@ class FactureFournisseur extends CommonInvoice
 							$result = $mouvP->reception($user, $this->lines[$i]->fk_product, $idwarehouse, $this->lines[$i]->qty, $up_ht_disc, $langs->trans("InvoiceValidatedInDolibarr", $num));
 						}
 						if ($result < 0) {
+							$this->error = $mouvP->error;
 							if (count($mouvP->errors)) {
-								setEventMessages(null, $mouvP->errors, 'errors');
-							} else {
-								setEventMessages($mouvP->error, $mouvP->errors, 'errors');
+								$this->errors = $mouvP->errors;
 							}
-							$error++;
+							return -2;
 						}
 					}
 				}
