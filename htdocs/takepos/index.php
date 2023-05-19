8e7d1a9f23a2691384161c0579e7186067ae7475
Christophe Battarel <christophe@altairis.fr>
2023-05-19 11:54:21 +0200

    NEW : TAkePOS - dont allow payment if no thirdparty selected

--- htdocs/takepos/index.php
+++ htdocs/takepos/index.php
@@ -532,6 +532,17 @@ function Reduction() {
 function CloseBill() {
 	invoiceid = $("#invoiceid").val();
 	console.log("Open popup to enter payment on invoiceid="+invoiceid);
+	<?php if ($conf->global->TAKEPOS_NO_GENERIC_THIRDPARTY) { ?>
+		if ($("#idcustomer").val() == "") {
+			alert("<?php echo $langs->trans('TakePosCustomerMandatory'); ?>");
+			<?php if ($conf->global->TAKEPOS_CHOOSE_CONTACT) { ?>
+				Contact();
+			<?php } else { ?>
+				Customer();
+			<?php } ?>
+			return;
+		}
+	<?php }
 	$.colorbox({href:"pay.php?place="+place+"&invoiceid="+invoiceid, width:"80%", height:"90%", transition:"none", iframe:"true", title:""});
 }
 
@@ -585,6 +596,7 @@ function New() {
 				//$('#poslines').scrollTop($('#poslines')[0].scrollHeight);
 			});
 			ClearSearch();
+			$("#idcustomer").val("");
 		}
 	});
 }
@@ -1046,6 +1058,7 @@ if (empty($conf->global->TAKEPOS_HIDE_HEAD_BAR)) {
 				</div>
 				<!-- section for customer -->
 				<div class="inline-block valignmiddle" id="customerandsales"></div>
+				<input type="hidden" id="idcustomer" value="">
 				<!-- section for shopping carts -->
 				<div class="inline-block valignmiddle" id="shoppingcart"></div>
 				<!-- More info about customer -->
