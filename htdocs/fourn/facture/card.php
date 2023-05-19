fe64992877dd1020f4cbe03e5fcd3a4f7c0c5149
Anthony Berton <anthony.berton@bb2a.fr>
2023-05-19 09:13:25 +0200

    Add for fac fourn

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -4041,7 +4041,9 @@ if ($action == 'create') {
 		$diroutput = $conf->fournisseur->facture->dir_output;
 		$autocopy = 'MAIN_MAIL_AUTOCOPY_SUPPLIER_INVOICE_TO';
 		$trackid = 'sinv'.$object->id;
-
+		if (getDolGlobalInt('MAIN_MAIL_WITH_TO_SELECTED_CONTACT_CUSTOMER')) {
+			$withtoselected = $object->getIdContact('external', 'BILLING');
+		}
 		include DOL_DOCUMENT_ROOT.'/core/tpl/card_presend.tpl.php';
 	}
 }
