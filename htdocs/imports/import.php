29d86ad94105a46318275d0025188842847ac162
Noé Courtier <noe.courtier@scopen.fr>
2023-05-26 11:35:26 +0200

    FIX: Set update keys set in module descriptor by default in multiselectarray

--- htdocs/imports/import.php
+++ htdocs/imports/import.php
@@ -146,6 +146,10 @@ $separator_used     = str_replace('\t', "\t", $separator);
 $objimport = new Import($db);
 $objimport->load_arrays($user, ($step == 1 ? '' : $datatoimport));
 
+if (empty($updatekeys && !empty($objimport->array_import_updatekeys[0]))) {
+    $updatekeys = array_keys($objimport->array_import_updatekeys[0]);
+}
+
 $objmodelimport = new ModeleImports();
 
 $form = new Form($db);
