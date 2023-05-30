4fa737914efa5bc1ee870a67fb9df962beafe4ca
Philippe GRAND <philippe.grand@atoo-net.com>
2023-05-30 10:18:49 +0200

    Fix : Minimal PHP version is now PHP 7.1 instead of PHP 7.0

--- htdocs/core/modules/expensereport/doc/pdf_standard.modules.php
+++ htdocs/core/modules/expensereport/doc/pdf_standard.modules.php
@@ -71,9 +71,9 @@ class pdf_standard extends ModeleExpenseReport
 
 	/**
 	 * @var array Minimum version of PHP required by module.
-	 * e.g.: PHP ≥ 7.0 = array(7, 0)
+	 * e.g.: PHP ≥ 7.1 = array(7, 1)
 	 */
-	public $phpmin = array(7, 0);
+	public $phpmin = array(7, 1);
 
 	/**
 	 * Dolibarr version of the loaded document
