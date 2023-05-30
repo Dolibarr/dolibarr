0f3c6b0ea3a42ce4f9a014cd4074a80ec8946658
Philippe GRAND <philippe.grand@atoo-net.com>
2023-05-30 10:26:12 +0200

    Fix : Minimal PHP version is now PHP 7.1 instead of PHP 7.0

--- htdocs/core/modules/supplier_payment/doc/pdf_standard.modules.php
+++ htdocs/core/modules/supplier_payment/doc/pdf_standard.modules.php
@@ -66,9 +66,9 @@ class pdf_standard extends ModelePDFSuppliersPayments
 
 	/**
 	 * @var array Minimum version of PHP required by module.
-	 * e.g.: PHP ≥ 7.0 = array(7, 0)
+	 * e.g.: PHP ≥ 7.1 = array(7, 1)
 	 */
-	public $phpmin = array(7, 0);
+	public $phpmin = array(7, 1);
 
 	/**
 	 * Dolibarr version of the loaded document
