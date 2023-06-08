d853a8f2207acacbc1de0c7c7690533dc7344969
Laurent Destailleur <eldy@destailleur.fr>
2023-06-08 10:16:58 +0200

    Fix phpcs

--- htdocs/core/js/lib_head.js.php
+++ htdocs/core/js/lib_head.js.php
@@ -1301,7 +1301,7 @@ $(document).ready(function() {
  */
 <?php
 if (empty($conf->global->MAIN_DISABLE_SELECT2_FOCUS_PROTECTION) && !defined('DISABLE_SELECT2_FOCUS_PROTECTION')) {
-?>
+	?>
 $(document).on('select2:open', () => {
 	console.log("Execute the focus (click on combo or use space when on component");
 	let allFound = document.querySelectorAll('.select2-container--open .select2-search__field');
@@ -1311,6 +1311,8 @@ $(document).on('select2:open', () => {
 		},0);
 	});
 });
-<?php } ?>
+	<?php
+}
+?>
 
 // End of lib_head.js.php
