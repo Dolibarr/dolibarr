69711d7c6d9ae2b72acf448acbd1cbaaf567dbbf
Rémi Champlon <remi.champlon@vold.lu>
2023-06-10 11:35:52 +0200

    remove css

--- htdocs/theme/md/style.css.php
+++ htdocs/theme/md/style.css.php
@@ -3409,10 +3409,6 @@ div.blockvmenulogo
 	max-width: 100px;
 	vertical-align: middle;
 }
-.backgroundforcompanylogo {
-	background-color: rgba(255,255,255,0.7);
-	border-radius: 5px;
-}
 .menulogocontainer img.mycompany {
 	object-fit: contain;
 	width: inherit;
