bd2f54c924a65936b8e548c19c0378c43bd309c4
Frédéric France <frederic.france@free.fr>
2023-06-15 20:28:43 +0200

    use user hasRight

--- htdocs/accountancy/bookkeeping/listbyaccount.php
+++ htdocs/accountancy/bookkeeping/listbyaccount.php
@@ -1297,7 +1297,7 @@ print "</table>";
 print '</div>';
 
 // TODO Replace this with mass delete action
-//if ($user->rights->accounting->mouvements->supprimer_tous) {
+//if ($user->hasRight('accounting', 'mouvements, 'supprimer_tous')) {
 //	print '<div class="tabsAction tabsActionNoBottom">'."\n";
 //	print '<a class="butActionDelete" name="button_delmvt" href="'.$_SERVER["PHP_SELF"].'?action=delbookkeepingyear&token='.newToken().($param ? '&'.$param : '').'">'.$langs->trans("DeleteMvt").'</a>';
 //	print '</div>';
