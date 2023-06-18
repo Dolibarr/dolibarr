f5f4f8de004cad6949daa33d3f3680565a1aac77
Rémi Champlon <remi.champlon@vold.lu>
2023-06-18 11:02:28 +0200

    fix warning when edit bank account

--- htdocs/compta/bank/card.php
+++ htdocs/compta/bank/card.php
@@ -1017,6 +1017,8 @@ if ($action == 'create') {
 		if (isModEnabled('categorie')) {
 			print '<tr><td>'.$langs->trans("Categories").'</td><td>';
 			$cate_arbo = $form->select_all_categories(Categorie::TYPE_ACCOUNT, '', 'parent', 64, 0, 1);
+
+			$arrayselected = array();
 			$c = new Categorie($db);
 			$cats = $c->containing($object->id, Categorie::TYPE_ACCOUNT);
 			if (is_array($cats)) {
