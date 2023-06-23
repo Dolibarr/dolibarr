c57fcdb7ece57104d9d8b189d900bc439433cb3a
vmaury <artec.vm@arnac.net>
2023-06-23 02:50:21 +0200

    Fix #25171 : add a function in Societe class to retrieve contacts of a societe

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -5276,4 +5276,98 @@ class Societe extends CommonObject
 		$return .= '</div>';
 		return $return;
 	}
+	
+	/**
+	 *    Get array of all contacts for a society (stored in societe_contacts instead of element_contacts for all other objects)
+	 *
+	 *    @param	int			socid		id of societe
+	 *    @param	int         $list       0:Return array contains all properties, 1:Return array contains just id
+	 *    @param    string      $code       Filter on this code of contact type ('SHIPPING', 'BILLING', ...)
+	 *	  @param    string      $element       Filter on this element of default contact type ('facture', 'propal', 'commande' ...)
+	 *    @return	array|int		        Array of contacts, -1 if error
+	 * 
+	 *
+	 * 
+	 */
+	function liste_contact_societe($list = 0, $code = '', $element = '')
+	{
+		// phpcs:enable
+		global $langs, $db;
+
+		$tab = array();
+
+		$sql = "SELECT sc.rowid, sc.fk_socpeople as id, sc.fk_c_type_contact"; // This field contains id of llx_socpeople or id of llx_user
+		
+		$sql .= ", t.fk_soc as socid, t.statut as statuscontact";
+		
+		$sql .= ", t.civility as civility, t.lastname as lastname, t.firstname, t.email";
+		$sql .= ", tc.source, tc.element, tc.code, tc.libelle";
+		$sql .= " FROM ".$db->prefix()."c_type_contact tc";
+		$sql .= ", ".$db->prefix()."societe_contacts sc";
+
+//			fk_soc ↓ =
+//	fk_c_type_contact	fk_socpeople	tms	import_key
+					
+		$sql .= " LEFT JOIN ".$db->prefix()."socpeople t on sc.fk_socpeople = t.rowid";
+		
+		$sql .= " WHERE sc.fk_soc = ".((int) $this->id);
+		$sql .= " AND sc.fk_c_type_contact = tc.rowid";
+		if (!empty($element)) {
+			$sql .= " AND tc.element = '".$db->escape($element)."'";
+		}
+		if ($code) {
+			$sql .= " AND tc.code = '".$db->escape($code)."'";
+		}
+		$sql .= " AND sc.entity = ".getEntity($this->element);
+		
+		$sql .= " AND tc.source = 'external'";
+		
+		$sql .= " AND tc.active=1";
+		
+		$sql .= " ORDER BY t.lastname ASC";
+
+		dol_syslog(get_class($this)."::liste_contact_societe", LOG_DEBUG);
+		$resql = $db->query($sql);
+		if ($resql) {
+			$num = $db->num_rows($resql);
+			$i = 0;
+			while ($i < $num) {
+				$obj = $db->fetch_object($resql);
+
+				if (!$list) {
+					$transkey = "TypeContact_".$obj->element."_".$obj->source."_".$obj->code;
+					$libelle_type = ($langs->trans($transkey) != $transkey ? $langs->trans($transkey) : $obj->libelle);
+					$tab[$i] = array(
+						'source' => $obj->source,
+						'socid' => $obj->socid,
+						'id' => $obj->id,
+						'nom' => $obj->lastname, // For backward compatibility
+						'civility' => $obj->civility,
+						'lastname' => $obj->lastname,
+						'firstname' => $obj->firstname,
+						'email'=>$obj->email,
+						'login'=> (empty($obj->login) ? '' : $obj->login),
+						'photo' => (empty($obj->photo) ? '' : $obj->photo),
+						'statuscontact' => $obj->statuscontact,
+						'rowid' => $obj->rowid,
+						'code' => $obj->code,
+						'element' => $obj->element,
+						'libelle' => $libelle_type,
+						'status' => $obj->statuslink,
+						'fk_c_type_contact' => $obj->fk_c_type_contact
+					);
+				} else {
+					$tab[$i] = $obj->id;
+				}
+
+				$i++;
+			}
+
+			return $tab;
+		} else {
+			$error = $db->lasterror();
+			dol_print_error($db);
+			return -1;
+		}
+	}
 }
