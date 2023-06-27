62b3cfd0c87a69f4720978c0312c36ade5d44f00
Rémi Champlon <remi.champlon@vold.lu>
2023-06-27 07:15:42 +0200

    remvoe checks

--- htdocs/expedition/card.php
+++ htdocs/expedition/card.php
@@ -648,7 +648,7 @@ if (empty($reshook)) {
 						$qty = "qtyl".$detail_batch->fk_expeditiondet.'_'.$detail_batch->id;
 						$batch_id = GETPOST($batch, 'int');
 						$batch_qty = GETPOST($qty, 'int');
-						if (!empty($batch_id) && ($batch_id != $detail_batch->fk_origin_stock || $batch_qty > 0)) {
+						if (!empty($batch_id)) {
 							if ($lotStock->fetch($batch_id) > 0 && $line->fetch($detail_batch->fk_expeditiondet) > 0) {	// $line is ExpeditionLine
 								if ($lines[$i]->entrepot_id != 0) {
 									// allow update line entrepot_id if not multi warehouse shipping
@@ -661,7 +661,7 @@ if (empty($reshook)) {
 								}
 
 								$line->detail_batch->fk_origin_stock = $batch_id;
-								$line->detail_batch->batch = $lotStock->batch;
+								$line->detail_batch->batch = $lotStock->batch;a
 								$line->detail_batch->id = $detail_batch->id;
 								$line->detail_batch->entrepot_id = $lotStock->warehouseid;
 								$line->detail_batch->qty = $batch_qty;
