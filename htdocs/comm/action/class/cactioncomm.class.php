9f807d9a20f575565f3cbdfabb54d73df718f24f
oussamanhairech <oussamazaks@gmail.com>
2023-07-03 16:08:32 +0200

    commit

--- htdocs/comm/action/class/cactioncomm.class.php
+++ htdocs/comm/action/class/cactioncomm.class.php
@@ -324,7 +324,7 @@ class CActionComm
 	 *  Return name of action type as a label translated
 	 *
 	 *	@param	int		$withpicto		0=No picto, 1=Include picto into link, 2=Picto only
-	 *  @return int|string			      	Label of action type
+	 *  @return string			      	Label of action type
 	 */
 	public function getNomUrl($withpicto = 0)
 	{
@@ -335,6 +335,6 @@ class CActionComm
 		if ($transcode != "Action".$this->code) {
 			return $transcode;
 		}
-		return -1;
+		return '';
 	}
 }
