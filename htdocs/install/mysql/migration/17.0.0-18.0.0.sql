2e0b2e28de5e5cb55ba629b3e190b2c8e8a55b79
Eric Seigne <eric.seigne@cap-rel.fr>
2023-07-05 18:00:57 +0200

    fix #25275: tms default timestamp

--- htdocs/install/mysql/migration/17.0.0-18.0.0.sql
+++ htdocs/install/mysql/migration/17.0.0-18.0.0.sql
@@ -34,6 +34,8 @@
 
 -- v17
 
+ALTER TABLE llx_emailcollector_emailcollector CHANGE COLUMN tms tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
 -- VMYSQL4.3 ALTER TABLE llx_hrm_skillrank CHANGE COLUMN `rank` rankorder integer;
 -- VPGSQL8.2 ALTER TABLE llx_hrm_skillrank CHANGE COLUMN rank rankorder integer;
 
