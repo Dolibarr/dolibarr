d39dd4b85123092a5394485de03a6909afca0055
Frédéric France <frederic.france@free.fr>
2023-07-06 01:07:50 +0200

    unify categories

--- htdocs/install/upgrade2.php
+++ htdocs/install/upgrade2.php
@@ -3514,6 +3514,23 @@ function migrate_element_category($db, $langs, $conf)
 	print '</td></tr>';
 
 	// knowledgemanagement
+	print '<tr><td colspan="4">';
+	// if module knowledgemanagement never enabled, table may not exist
+	if ($db->DDLDescTable(MAIN_DB_PREFIX."categorie_knowledgemanagement")) {
+		$sql = 'SELECT fk_categorie, fk_knowledgemanagement, import_key FROM '.MAIN_DB_PREFIX."categorie_knowledgemanagement";
+		$result = $db->query($sql);
+		if ($result) {
+			while ($obj = $db->fetch_object($result)) {
+				$db->query('INSERT INTO '.MAIN_DB_PREFIX.'element_category (fk_category, fk_element, import_key) VALUES('.(int) $obj->fk_categorie.','.(int)$obj->fk_knowledgemanagement.', '.(is_null($obj->import_key) ? 'null' : $db->escape($obj->import_key)).')');
+			}
+			print '<b>'.$langs->trans('MigrationCategories', 'Knowledgemanagement')."</b><br>\n";
+		} else {
+			$error++;
+		}
+	} else {
+		print '<b>'.$langs->trans('MigrationCategories', 'Knowledgemanagement')." Table not found</b><br>\n";
+	}
+	print '</td></tr>';
 
 	if ($error == 0) {
 		$db->commit();
