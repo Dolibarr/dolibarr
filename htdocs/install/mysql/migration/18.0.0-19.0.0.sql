ead0b37f3a783f7a6021df5e0c872a3361ac35e2
Hystepik <lucas.marcouiller@gmail.com>
2023-07-11 13:46:54 +0200

    remove constraint

--- htdocs/install/mysql/migration/18.0.0-19.0.0.sql
+++ htdocs/install/mysql/migration/18.0.0-19.0.0.sql
@@ -33,7 +33,6 @@
 -- -- VPGSQL8.2 SELECT dol_util_rebuild_sequences();
 
 
-- V19
+-- V19
 ALTER TABLE llx_ticket ADD COLUMN fk_contract integer DEFAULT 0 after fk_project;
 ALTER TABLE llx_bookcal_availabilities ADD COLUMN fk_bookcal_booking integer NOT NULL;
-ALTER TABLE llx_bookcal_availabilities ADD CONSTRAINT llx_bookcal_availabilities_fk_bookcal_booking FOREIGN KEY (fk_bookcal_booking) REFERENCES llx_bookcal_booking(rowid);
