9b1bae73f36c5f242746567b6276cba2571cc912
Charlène Benke <1179011+defrance@users.noreply.github.com>
2023-07-13 02:24:20 +0200

    Update card.php

--- htdocs/salaries/card.php
+++ htdocs/salaries/card.php
@@ -82,7 +82,7 @@ $childids = $user->getAllChildIds(1);
 $extrafields->fetch_name_optionals_label($object->table_element);
 
 // Initialize technical object to manage hooks of page. Note that conf->hooks_modules contains array of hook context
-$hookmanager->initHooks(array('salarycard', 'globalcard'));
+$hookmanager->initHooks(array('salariescard', 'globalcard'));
 
 if ($id > 0 || !empty($ref)) {
 	$object->fetch($id, $ref);
