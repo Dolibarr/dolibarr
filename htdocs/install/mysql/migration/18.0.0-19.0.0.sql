c31ee09b0ccbc1f4af46f5d0c50c69e9c72b00af
Noé Cendrier <noe.cendrier@altairis.fr>
2023-07-18 09:21:01 +0200

    we are now heading to 19.0 versions

--- htdocs/install/mysql/migration/18.0.0-19.0.0.sql
+++ htdocs/install/mysql/migration/18.0.0-19.0.0.sql
@@ -2,7 +2,7 @@
 --
 -- Be carefull to requests order.
 -- This file must be loaded by calling /install/index.php page
--- when current version is 18.0.0 or higher.
+-- when current version is 19.0.0 or higher.
 --
 -- To restrict request to Mysql version x.y minimum use -- VMYSQLx.y
 -- To restrict request to Pgsql version x.y minimum use -- VPGSQLx.y
