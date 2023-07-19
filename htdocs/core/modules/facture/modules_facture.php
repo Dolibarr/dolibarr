567fc875d37b37759d09915af3049778e270f3e8
Laurent Destailleur <eldy@users.sourceforge.net>
2023-07-20 00:45:57 +0200

    Debug v18

--- htdocs/core/modules/facture/modules_facture.php
+++ htdocs/core/modules/facture/modules_facture.php
@@ -112,7 +112,7 @@ abstract class ModelePDFFactures extends CommonDocGenerator
 		} elseif (file_exists(DOL_DOCUMENT_ROOT.'/includes/autoload.php')) {
 			require_once DOL_DOCUMENT_ROOT.'/includes/autoload.php';
 		} else {
-			$this->error = 'PHP library sprain/swiss-qr-bill was not found. Please install it with:<br>cd '.DOL_DOCUMENT_ROOT.'; cp composer.json.disabled composer.json; composer require sprain/swiss-qr-bill;';
+			$this->error = 'PHP library sprain/swiss-qr-bill was not found. Please install it with:<br>cd '.dirname(DOL_DOCUMENT_ROOT).'; cp composer.json.disabled composer.json; composer require sprain/swiss-qr-bill;';
 			return false;
 		}
 
