ef8682efa7f35ba0f02127eff4f0f04fd0b8104f
jyhere <jyhere@gmail.com>
2023-07-20 15:25:20 +0200

    FIX: sql error when passing origin and origin id, no need to pass theses values
    $object->origin is set to "commandeFournisseur', and it's definitively not working with the setStatus method.
    SQL error : Table 'dolibarr.llx_commandeFournisseur' doesn't exist
    
    I suppose origin should never be set to commandeFournisseur, but 'order_supplier' instead. Also, setStatus() $elementType is meant to be a table_element value, not an element value.

--- htdocs/core/triggers/interface_20_modWorkflow_WorkflowManager.class.php
+++ htdocs/core/triggers/interface_20_modWorkflow_WorkflowManager.class.php
@@ -439,7 +439,7 @@ class InterfaceWorkflowManager extends DolibarrTriggers
 				$diff_array = array_diff_assoc($qtyordred, $qtyshipped);
 				if (count($diff_array) == 0) {
 					//No diff => mean everythings is received
-					$ret = $order->setStatut(CommandeFournisseur::STATUS_RECEIVED_COMPLETELY, $object->origin_id, $object->origin, 'SUPPLIER_ORDER_CLOSE');
+					$ret = $order->setStatut(CommandeFournisseur::STATUS_RECEIVED_COMPLETELY, null, null, 'SUPPLIER_ORDER_CLOSE');
 					if ($ret < 0) {
 						$this->error = $order->error;
 						$this->errors = $order->errors;
