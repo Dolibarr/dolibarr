f4cec5283aa16dc2ced8183ae41334176ac68e21
JC Prieto <jcprieto@virtual20.com>
2023-07-24 19:41:00 +0200

    FIX: Update mouvementstock.class.php (#25396)
    * FIX: Update mouvementstock.class.php
    
    If first character change to upper case, some external modules don't show its origin in mouvements.
    
    * Update mouvementstock.class.php
    
    More elegant.
    We don't need to change to upper case (line 978) and then to lower case (line 980). It is only necessary when creating a new class (line 983)
    The module name and url are in lower case.

--- htdocs/product/stock/class/mouvementstock.class.php
+++ htdocs/product/stock/class/mouvementstock.class.php
@@ -975,9 +975,9 @@ class MouvementStock extends CommonObject
 				if ($origin_type) {
 					// Separate originetype with "@" : left part is class name, right part is module name
 					$origin_type_array = explode('@', $origin_type);
-					$classname = ucfirst($origin_type_array[0]);
+					$classname = $origin_type_array[0];
 					$modulename = empty($origin_type_array[1]) ? $classname : $origin_type_array[1];
-					$result = dol_include_once('/'.$modulename.'/class/'.strtolower($classname).'.class.php');
+					$result = dol_include_once('/'.$modulename.'/class/'.$classname.'.class.php');
 					if ($result) {
 						$classname = ucfirst($classname);
 						$origin = new $classname($this->db);
