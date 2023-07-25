82552e2cd6c12f77f9f097c259656d653e3c5563
aetiom <8208943+aetiom@users.noreply.github.com>
2023-07-25 15:23:34 +0200

    add product and service from invoice object line

--- htdocs/core/tpl/objectline_create.tpl.php
+++ htdocs/core/tpl/objectline_create.tpl.php
@@ -315,6 +315,30 @@ if ($nolinesbefore) {
 					<?php
 				}
 			}
+
+			$facid = GETPOST('facid', 'int');
+			echo '<div id="dropdownAddProductAndService" class="dropdown inline-block">';
+			echo '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" id="dropdownAddProductAndServiceLink" aria-haspopup="true" aria-expanded="false">';
+	        echo '<span class="fa fa-plus-circle valignmiddle paddingleft"> '.$langs->trans("AddProduct").'</span>';
+			echo '</a>';
+	        echo '<div class="dropdown-menu" aria-labelledby="dropdownAddProductAndServiceLink" style="top:auto; left:auto;">';
+			echo '<a class="dropdown-item" href="'.DOL_URL_ROOT.'/product/card.php?action=create&type=0&leftmenu=billing&backtopage='.urlencode($_SERVER["PHP_SELF"].'?facid='.$facid).'"> '.$langs->trans("NewProduct").'</a>';
+			echo '<a class="dropdown-item" href="'.DOL_URL_ROOT.'/product/card.php?action=create&type=1&leftmenu=billing&backtopage='.urlencode($_SERVER["PHP_SELF"].'?facid='.$facid).'"> '.$langs->trans("NewService").'</a>';
+			echo '</div>';
+			echo '</div>';
+
+			?>
+			<script>
+				$(document).ready(function(){
+					$("#dropdownAddProductAndService .dropdown-toggle").on("click", function(event) {
+						console.log("toggle addproduct dropdown");
+						event.preventDefault();
+						$("#dropdownAddProductAndService").toggleClass("open");
+					});
+				});
+			</script>
+			<?php
+
 			echo '<input type="hidden" name="pbq" id="pbq" value="">';
 			echo '</span>';
 		}
