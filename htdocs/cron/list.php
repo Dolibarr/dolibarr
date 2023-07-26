17631652543771982302500fdbe39fd32c84d6d9
Quentin VIAL-GOUTEYRON <quentin.vial-gouteyron@atm-consulting.fr>
2023-07-26 11:07:44 +0200

    delete line

--- htdocs/cron/list.php
+++ htdocs/cron/list.php
@@ -318,7 +318,6 @@ $sql .= $db->order($sortfield, $sortorder);
 $nbtotalofrecords = '';
 if (empty($conf->global->MAIN_DISABLE_FULL_SCANLIST)) {
 	$result = $db->query($sql);
-	print $sql;
 	$nbtotalofrecords = $db->num_rows($result);
 	if (($page * $limit) > $nbtotalofrecords) {	// if total resultset is smaller then paging size (filtering), goto and load page 0
 		$page = 0;
