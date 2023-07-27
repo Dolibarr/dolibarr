1da39b6ddce0836fa15ca3fe490d929391cb1517
Frédéric France <frederic.france@free.fr>
2023-07-27 09:41:43 +0200

    wip

--- htdocs/knowledgemanagement/knowledgerecord_list.php
+++ htdocs/knowledgemanagement/knowledgerecord_list.php
@@ -296,7 +296,7 @@ if (!empty($searchCategoryKnowledgemanagementList)) {
 	$listofcategoryid = '';
 	foreach ($searchCategoryKnowledgemanagementList as $searchCategoryKnowledgemanagement) {
 		if (intval($searchCategoryKnowledgemanagement) == -2) {
-			$searchCategoryKnowledgemanagementSqlList[] = "NOT EXISTS (SELECT ck.fk_knowledgemanagement FROM ".MAIN_DB_PREFIX."categorie_knowledgemanagement as ck WHERE t.rowid = ck.fk_knowledgemanagement)";
+			$searchCategoryKnowledgemanagementSqlList[] = "NOT EXISTS (SELECT ck.fk_element FROM ".MAIN_DB_PREFIX."element_category as ck WHERE ck.fk_category IN (SELECT rowid FROM ".MAIN_DB_PREFIX."categorie WHERE type=13) AND t.rowid = ck.fk_element)";
 		} elseif (intval($searchCategoryKnowledgemanagement) > 0) {
 			if (empty($searchCategoryKnowledgemanagementOperator)) {
 				$searchCategoryKnowledgemanagementSqlList[] = " EXISTS (SELECT ck.fk_element FROM ".MAIN_DB_PREFIX."element_category as ck WHERE t.rowid = ck.fk_element AND ck.fk_category = ".((int) $searchCategoryKnowledgemanagement).")";
