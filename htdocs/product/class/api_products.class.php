627e4141eaf981bdf24d73aa5fdb9d448260e6b5
ptibogxiv <support@ptibogxiv.net>
2023-07-28 14:24:16 +0200

    Fix php syntax (#25508)

--- htdocs/product/class/api_products.class.php
+++ htdocs/product/class/api_products.class.php
@@ -676,9 +676,9 @@ class Products extends DolibarrApi
 			require_once DOL_DOCUMENT_ROOT.'/product/class/productcustomerprice.class.php';
 			$prodcustprice = new Productcustomerprice($this->db);
 			$filter = array();
-			$filter['t.fk_product'] .= $id;
+			$filter['t.fk_product'] = $id;
 			if ($thirdparty_id) {
-				$filter['t.fk_soc'] .= $thirdparty_id;
+				$filter['t.fk_soc'] = $thirdparty_id;
 			}
 			$result = $prodcustprice->fetchAll('', '', 0, 0, $filter);
 		}
