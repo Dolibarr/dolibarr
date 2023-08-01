b52f4265186e16e49d6e200880aae58e1a166fa6
Laurent Destailleur <eldy@destailleur.fr>
2023-08-01 18:46:33 +0200

    Remove hardcoded value

--- htdocs/modulebuilder/template/myobject_agenda.php
+++ htdocs/modulebuilder/template/myobject_agenda.php
@@ -121,7 +121,7 @@ if (!$sortorder) {
 $object = new MyObject($db);
 $extrafields = new ExtraFields($db);
 $diroutputmassaction = $conf->mymodule->dir_output.'/temp/massgeneration/'.$user->id;
-$hookmanager->initHooks(array('myobjectagenda', 'globalcard')); // Note that conf->hooks_modules contains array
+$hookmanager->initHooks(array($object->element.'agenda', 'globalcard')); // Note that conf->hooks_modules contains array
 // Fetch optionals attributes and labels
 $extrafields->fetch_name_optionals_label($object->table_element);
 
