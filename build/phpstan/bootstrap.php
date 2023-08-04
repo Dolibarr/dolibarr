2777857f5be3bcce845265520db622dbf3ca511d
Laurent Destailleur <eldy@destailleur.fr>
2023-08-04 03:24:41 +0200

    Fix phpstan

--- build/phpstan/bootstrap.php
+++ build/phpstan/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 // Defined some constants and load Dolibarr env to reduce PHPStan bootstrap that fails to load a lot of things.
-define('DOL_DOCUMENT_ROOT', __DIR__ . '/../../htdocs');
-define('DOL_DATA_ROOT', __DIR__ . '/../../documents');
-define('DOL_URL_ROOT', '/');
+//define('DOL_DOCUMENT_ROOT', __DIR__ . '/../../htdocs');
+//define('DOL_DATA_ROOT', __DIR__ . '/../../documents');
+//define('DOL_URL_ROOT', '/');
 
-// Load the main.inc.php file to have functions llx_Header and llx_Footer defined
+// Load the main.inc.php file to have functions env defined
 if (! defined("NOLOGIN")) define("NOLOGIN", '1');
 global $conf, $langs, $user, $db;
 include_once __DIR__ . '/../../htdocs/main.inc.php';
