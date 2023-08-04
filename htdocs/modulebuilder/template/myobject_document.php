81b8d1e5328aabdd1ba1e58bd20fd91c78edd2c9
Laurent Destailleur <eldy@destailleur.fr>
2023-08-04 18:33:07 +0200

    Clean code

--- htdocs/modulebuilder/template/myobject_document.php
+++ htdocs/modulebuilder/template/myobject_document.php
@@ -163,7 +163,8 @@ include DOL_DOCUMENT_ROOT.'/core/actions_linkedfiles.inc.php';
 
 $form = new Form($db);
 
-$title = $langs->trans("MyObject").' - '.$langs->trans("Files");
+$title = $langs->trans("MyObject")." - ".$langs->trans("Files");
+//$title = $object->ref." - ".$langs->trans("Files");
 $help_url = '';
 //$help_url='EN:Module_Third_Parties|FR:Module_Tiers|ES:Empresas';
 llxHeader('', $title, $help_url);
