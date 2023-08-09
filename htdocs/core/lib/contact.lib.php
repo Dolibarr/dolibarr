954c54ad3faa6a0ede8351377b81869226d1d647
am97 <amaldona@tutanota.com>
2023-08-09 12:50:14 +0200

    Fix SQL syntax error (#25562)
    Single quotes are needed to indicate that 'project' and 'external' are strings, otherwise the requests fail

--- htdocs/core/lib/contact.lib.php
+++ htdocs/core/lib/contact.lib.php
@@ -71,7 +71,7 @@ function contact_prepare_head(Contact $object)
 			$sql .= ' FROM '.MAIN_DB_PREFIX.'projet as n';
 			$sql .= ' INNER JOIN '.MAIN_DB_PREFIX.'element_contact as cc ON (n.rowid = cc.element_id)';
 			$sql .= ' WHERE cc.fk_socpeople = '.((int) $object->id);
-			$sql .= ' AND cc.fk_c_type_contact IN (SELECT rowid FROM '.MAIN_DB_PREFIX.'c_type_contact WHERE element="project" AND source="external")';
+			$sql .= ' AND cc.fk_c_type_contact IN (SELECT rowid FROM '.MAIN_DB_PREFIX."c_type_contact WHERE element='project' AND source='external')";
 			$sql .= ' AND n.entity IN ('.getEntity('project').')';
 			$resql = $db->query($sql);
 			if ($resql) {
@@ -190,7 +190,7 @@ function show_contacts_projects($conf, $langs, $db, $object, $backtopage = '', $
 		$sql .= ' INNER JOIN '.MAIN_DB_PREFIX.'element_contact as cc ON (p.rowid = cc.element_id)';
 		$sql .= ' INNER JOIN '.MAIN_DB_PREFIX.'c_type_contact as ctc ON (ctc.rowid = cc.fk_c_type_contact)';
 		$sql .= ' WHERE cc.fk_socpeople = '.((int) $object->id);
-		$sql .= ' AND ctc.element="project" AND ctc.source="external"';
+		$sql .= " AND ctc.element='project' AND ctc.source='external'";
 		$sql .= ' AND p.entity IN ('.getEntity('project').')';
 		$sql .= ' ORDER BY p.dateo DESC';
 
