dcdd89a62d04f1f04199dc7b512231becbe2dd0f
notmarrco <marrco@wohecha.fr>
2023-08-11 20:21:23 +0300

    fix(bankaccount): Fix bad phpdoc return type and comments

--- htdocs/compta/bank/class/api_bankaccounts.class.php
+++ htdocs/compta/bank/class/api_bankaccounts.class.php
@@ -314,7 +314,7 @@ class BankAccounts extends DolibarrApi
 	 *
 	 * @param int    $id              ID of account
 	 * @param array  $request_data    data
-	 * @return Object
+	 * @return Object Object with cleaned properties
 	 */
 	public function put($id, $request_data = null)
 	{
