0bb3b6c73ace5b2d807f019f017974b711267c45
Laurent Destailleur <eldy@destailleur.fr>
2023-08-11 11:08:01 +0200

    Update facture.class.php

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -3126,10 +3126,8 @@ class Facture extends CommonInvoice
 		if ($close_note) {
 			$sql .= ", close_note='".$this->db->escape($close_note)."'";
 		}
-
-		$sql .= ', fk_user_closing = '.$user->id;
+		$sql .= ', fk_user_closing = '.((int) $user->id);
 		$sql .= ", date_closing = '".$this->db->idate($now)."'";
-		
 		$sql .= " WHERE rowid = ".((int) $this->id);
 
 		$resql = $this->db->query($sql);
