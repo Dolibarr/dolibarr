1d08e387fde10ab115cef9cd92de47c53808ad29
antonin_tdj <50403308+ibuiv@users.noreply.github.com>
2023-08-15 19:25:01 +0200

    Fix Computed Extrafields on list

--- htdocs/core/tpl/extrafields_list_print_fields.tpl.php
+++ htdocs/core/tpl/extrafields_list_print_fields.tpl.php
@@ -34,10 +34,7 @@ if (!empty($extrafieldsobjectkey) && !empty($extrafields->attributes[$extrafield
 				}
 				// If field is a computed field, we make computation to get value
 				if ($extrafields->attributes[$extrafieldsobjectkey]['computed'][$key]) {
-					//global $obj, $object;
-					//var_dump($extrafields->attributes[$extrafieldsobjectkey]['computed'][$key]);
-					//var_dump($obj);
-					//var_dump($extrafields->attributes[$extrafieldsobjectkey]['computed'][$key]);
+					$objectoffield = $object; //For compatibily with the computed formula
 					$value = dol_eval($extrafields->attributes[$extrafieldsobjectkey]['computed'][$key], 1, 1, '0');
 					if (is_numeric(price2num($value)) && $extrafields->attributes[$extrafieldsobjectkey]['totalizable'][$key]) {
 						$obj->$tmpkey = price2num($value);
