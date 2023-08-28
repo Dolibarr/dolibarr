a91b5bc8d865ab0a0db24d8d38fecf7e58a80bd3
Laurent Destailleur <eldy@destailleur.fr>
2023-08-28 19:21:06 +0200

    FIX hook formBuilddocOptions was broken when used by 2 modules

--- htdocs/core/class/hookmanager.class.php
+++ htdocs/core/class/hookmanager.class.php
@@ -200,7 +200,7 @@ class HookManager
 			'insertExtraFooter',
 			'printLeftBlock',
 			'formAddObjectLine',
-			'formBuilddocOption',
+			'formBuilddocOptions',
 			'showSocinfoOnPrint'
 		))) {
 			$hooktype = 'output';
