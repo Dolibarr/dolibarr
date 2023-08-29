a75bc75b1d49aa9c8747ad4b05e181466dc035fa
Frédéric France <frederic.france@free.fr>
2023-08-29 20:49:23 +0200

    doc

--- htdocs/core/class/commondocgenerator.class.php
+++ htdocs/core/class/commondocgenerator.class.php
@@ -109,6 +109,11 @@ abstract class CommonDocGenerator
 	public $option_escompte;
 	public $option_credit_note;
 
+	public $tva;
+	public $tva_array;
+	public $localtax1;
+	public $localtax2;
+
 	/**
 	 * @var int Tab Title Height
 	 */
