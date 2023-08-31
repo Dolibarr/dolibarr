b0ebc453381fdad23bf4d543e895d3f89a98807d
ATM-Sami <sami.filali@atm-consulting.fr>
2023-08-31 11:21:00 +0200

    Add subtotal for all types of linked objects and sort them by rowid (creation date)

--- htdocs/fourn/facture/tpl/linkedobjectblock.tpl.php
+++ htdocs/fourn/facture/tpl/linkedobjectblock.tpl.php
@@ -75,7 +75,7 @@ foreach ($linkedObjectBlock as $key => $objectlink) {
 	</tr>
 	<?php
 }
-if (count($linkedObjectBlock) > 1) {
+if (count($linkedObjectBlock)) {
 	?>
 	<tr class="liste_total <?php echo (empty($noMoreLinkedObjectBlockAfter) ? 'liste_sub_total' : ''); ?>">
 		<td><?php echo $langs->trans("Total"); ?></td>
