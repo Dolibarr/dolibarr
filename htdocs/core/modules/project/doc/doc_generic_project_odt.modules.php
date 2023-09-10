b546e7c624be02f72b96dffa1ba3f774d1f40c13
Laurent Destailleur <eldy@destailleur.fr>
2023-09-10 20:21:48 +0200

    css

--- htdocs/core/modules/project/doc/doc_generic_project_odt.modules.php
+++ htdocs/core/modules/project/doc/doc_generic_project_odt.modules.php
@@ -441,7 +441,7 @@ class doc_generic_project_odt extends ModelePDFProjects
 		$texte .= getDolGlobalInt('PROJECT_ADDON_PDF_ODT_PATH');
 		$texte .= '</textarea>';
 		$texte .= '</div><div style="display: inline-block; vertical-align: middle;">';
-		$texte .= '<input type="submit" class="button smallpaddingimp reposition" name="modify" value="'.dol_escape_htmltag($langs->trans("Modify")).'">';
+		$texte .= '<input type="submit" class="button button-edit reposition smallpaddingimp" name="modify" value="'.dol_escape_htmltag($langs->trans("Modify")).'">';
 		$texte .= '<br></div></div>';
 
 		// Scan directories
