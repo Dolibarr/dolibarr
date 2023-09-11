1dc928c728e776d51133b762324fa919ed88b92d
Laurent Destailleur <eldy@destailleur.fr>
2023-09-11 15:36:08 +0200

    Fix warning

--- htdocs/modulebuilder/index.php
+++ htdocs/modulebuilder/index.php
@@ -3310,7 +3310,7 @@ if ($module == 'initmodule') {
 		$h++;
 
 		$head2[$h][0] = $_SERVER["PHP_SELF"].'?tab=objects&module='.$module.($forceddirread ? '@'.$dirread : '');
-		$head2[$h][1] = ((!is_array($objects) || count($object) <= 0) ? $langs->trans("Objects") : $langs->trans("Objects").' <span class="badge">'.count($objects)."</span>");
+		$head2[$h][1] = ((!is_array($objects) || count($objects) <= 0) ? $langs->trans("Objects") : $langs->trans("Objects").' <span class="badge">'.count($objects)."</span>");
 		$head2[$h][2] = 'objects';
 		$h++;
 
