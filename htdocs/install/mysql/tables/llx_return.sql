5a6205490bdea24f794305dd4635bba06b9e9863
Christophe Battarel <christophe@altairis.fr>
2023-09-25 09:57:28 +0200

    add default for tms

--- htdocs/install/mysql/tables/llx_return.sql
+++ htdocs/install/mysql/tables/llx_return.sql
@@ -22,7 +22,7 @@
 create table llx_return
 (
   rowid					integer AUTO_INCREMENT PRIMARY KEY,
-  tms					timestamp,
+  tms					timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   ref					varchar(30)        NOT NULL,
   label					varchar(255),
   entity				integer  DEFAULT 1 NOT NULL,	-- multi company id
