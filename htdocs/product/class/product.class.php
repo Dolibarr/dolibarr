de5b97c7e7140a3b8475cebbfa6a08c1c4408a1b
tgsw <rilling@dausinger-giesen.de>
2023-10-12 15:39:42 +0200

    Show product description (#26193)
    * Show product description
    
    shows product-description on list of products when hover the product-ref
    
    * Update product.class.php
    
    limiting the display to 5 lines

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -5072,6 +5072,9 @@ class Product extends CommonObject
 		if (!empty($this->label)) {
 			$datas['label']= '<br><b>'.$langs->trans('ProductLabel').':</b> '.$this->label;
 		}
+		if (!empty($this->description)) {
+			$datas['description']= '<br><b>'.$langs->trans('ProductDescription').':</b> '.dolGetFirstLineofText($this->description, 5);
+		}
 		if ($this->type == Product::TYPE_PRODUCT || !empty($conf->global->STOCK_SUPPORTS_SERVICES)) {
 			if (isModEnabled('productbatch')) {
 				$langs->load("productbatch");
