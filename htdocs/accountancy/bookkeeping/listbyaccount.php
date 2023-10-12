473bc2e6fcd81427fd64a3ea09adc7a3ea8d27e0
Kamel Khelifa <kkhelifa@open-dsi.fr>
2023-10-12 14:58:00 +0200

    Add partial lettering

--- htdocs/accountancy/bookkeeping/listbyaccount.php
+++ htdocs/accountancy/bookkeeping/listbyaccount.php
@@ -479,7 +479,17 @@ if (empty($reshook)) {
 			if ($result < 0) {
 				setEventMessages('', $lettering->errors, 'errors');
 			} else {
-				setEventMessages($langs->trans('AccountancyOneLetteringModifiedSuccessfully'), array(), 'mesgs');
+				setEventMessages($langs->trans($result == 0 ? 'AccountancyNoLetteringModified' : 'AccountancyOneLetteringModifiedSuccessfully'), array(), 'mesgs');
+				header('Location: ' . $_SERVER['PHP_SELF'] . '?noreset=1' . $param);
+				exit();
+			}
+		} elseif ($type == 'sub' && $massaction == 'letteringpartial') {
+			$lettering = new Lettering($db);
+			$result = $lettering->updateLettering($toselect, false, true);
+			if ($result < 0) {
+				setEventMessages('', $lettering->errors, 'errors');
+			} else {
+				setEventMessages($langs->trans($result == 0 ? 'AccountancyNoLetteringModified' : 'AccountancyOneLetteringModifiedSuccessfully'), array(), 'mesgs');
 				header('Location: ' . $_SERVER['PHP_SELF'] . '?noreset=1' . $param);
 				exit();
 			}
@@ -510,7 +520,7 @@ if (empty($reshook)) {
 			if ($result < 0) {
 				setEventMessages('', $lettering->errors, 'errors');
 			} else {
-				setEventMessages($langs->trans('AccountancyOneUnletteringModifiedSuccessfully'), array(), 'mesgs');
+				setEventMessages($langs->trans($result == 0 ? 'AccountancyNoUnletteringModified' : 'AccountancyOneUnletteringModifiedSuccessfully'), array(), 'mesgs');
 				header('Location: ' . $_SERVER['PHP_SELF'] . '?noreset=1' . $param);
 				exit();
 			}
@@ -616,6 +626,7 @@ if (getDolGlobalInt('ACCOUNTING_ENABLE_LETTERING') && $user->rights->accounting-
 	$arrayofmassactions['letteringauto'] = img_picto('', 'check', 'class="pictofixedwidth"') . $langs->trans('LetteringAuto');
 	$arrayofmassactions['preunletteringauto'] = img_picto('', 'uncheck', 'class="pictofixedwidth"') . $langs->trans('UnletteringAuto');
 	$arrayofmassactions['letteringmanual'] = img_picto('', 'check', 'class="pictofixedwidth"') . $langs->trans('LetteringManual');
+	if ($type == 'sub') $arrayofmassactions['letteringpartial'] = img_picto('', 'check', 'class="pictofixedwidth"') . $langs->trans('LetteringPartial');
 	$arrayofmassactions['preunletteringmanual'] = img_picto('', 'uncheck', 'class="pictofixedwidth"') . $langs->trans('UnletteringManual');
 }
 if ($user->rights->accounting->mouvements->supprimer) {
@@ -717,6 +728,37 @@ if ($type == 'sub') {
 	$moreforfilter .= $formaccounting->select_auxaccount($search_accountancy_code_end, 'search_accountancy_code_end', $langs->trans('to'), 'maxwidth200');
 } else {
 	$moreforfilter .= $formaccounting->select_account($search_accountancy_code_end, 'search_accountancy_code_end', $langs->trans('to'), array(), 1, 1, 'maxwidth200');
+}
+if ($type == 'sub') {
+	$previousSubAccountKey = getDolGlobalString('ACCOUNTING_PREVIOUS_SUB_ACCOUNT_KEY', 'a');
+	$nextSubAccountKey = getDolGlobalString('ACCOUNTING_NEXT_SUB_ACCOUNT_KEY', 'z');
+	$moreforfilter .= '&nbsp;<a id="previous_account" accesskey="'. dol_escape_htmltag($previousSubAccountKey) . '" href="#"><i class="fa-solid fa-arrow-left"></i></a>';
+	$moreforfilter .= '&nbsp;<a id="next_account" accesskey="'. dol_escape_htmltag($nextSubAccountKey) . '" href="#"><i class="fa-solid fa-arrow-right"></i></a>';
+	$moreforfilter .= <<<SCRIPT
+ 	<script type="text/javascript">
+        jQuery(document).ready(function(){
+            var searchFormList = $('#searchFormList');
+            var searchAccountancyCodeStart = $('#search_accountancy_code_start');
+            var searchAccountancyCodeEnd = $('#search_accountancy_code_end');
+
+            jQuery('#previous_account').on('click', function() {
+                var previousOption = searchAccountancyCodeStart.find('option:selected').prev('option');
+                if (previousOption.length == 1) searchAccountancyCodeStart.val(previousOption.attr('value'));
+                searchAccountancyCodeEnd.val(searchAccountancyCodeStart.val());
+                searchFormList.submit();
+            });
+            jQuery('#next_account').on('click', function() {
+                var nextOption = searchAccountancyCodeStart.find('option:selected').next('option');
+                if (nextOption.length == 1) searchAccountancyCodeStart.val(nextOption.attr('value'));
+                searchAccountancyCodeEnd.val(searchAccountancyCodeStart.val());
+                searchFormList.submit();
+            });
+            jQuery('input[name="search_mvt_num"]').on("keypress", function(event) {
+                console.log(event);
+            });
+        });
+    </script>
+SCRIPT;
 }
 $moreforfilter .= '</div>';
 $moreforfilter .= '</div>';
