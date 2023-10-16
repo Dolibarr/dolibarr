d316566e1ec482e22af13da79d2d94bbed55c46a
William Mead <william.mead@manchenumerique.fr>
2023-10-16 13:35:31 +0200

    Reverted commit 165645ccd5ba4d0b3da372d329f4ef4c9bfa179e

--- htdocs/theme/md/style.css.php
+++ htdocs/theme/md/style.css.php
@@ -4,9 +4,9 @@
  * Copyright (C) 2007-2017	Regis Houssin			<regis.houssin@inodbox.com>
  * Copyright (C) 2011		Philippe Grand			<philippe.grand@atoo-net.com>
  * Copyright (C) 2012		Juanjo Menent			<jmenent@2byte.es>
- * Copyright (C) 2015		Alexandre Spangaro		<aspangaro@open-dsi.fr>
- * Copyright (C) 2018		Ferran Marcet			<fmarcet@2byte.es>
- * Copyright (C) 2021-2023	Anthony Berton			<anthony.berton@bb2a.fr>
+ * Copyright (C) 2015		Alexandre Spangaro      <aspangaro@open-dsi.fr>
+ * Copyright (C) 2018       Ferran Marcet           <fmarcet@2byte.es>
+ * Copyright (C) 2021-2023  Anthony Berton          <anthony.berton@bb2a.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
