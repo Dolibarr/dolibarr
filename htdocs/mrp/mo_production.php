e29e6415bb4fbced04fbbbeefbcd8e5d77ef3f11
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2023-10-17 14:45:36 +0200

    Add same action

--- htdocs/mrp/mo_production.php
+++ htdocs/mrp/mo_production.php
@@ -1139,6 +1139,16 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 						// Split All
 						print '<td></td>';
 
+						// Action Edit line
+						if ($object->status == Mo::STATUS_DRAFT) {
+							$href = $_SERVER["PHP_SELF"] . '?id=' . ((int) $object->id) . '&action=editline&token=' . newToken() . '&lineid=' . ((int) $line->id);
+							print '<td class="center">';
+							print '<a class="reposition" href="' . $href . '">';
+							print img_picto($langs->trans('TooltipEditAndRevertStockMovement'), 'edit');
+							print '</a>';
+							print '</td>';
+						}
+
 						// Action delete line
 						if ($permissiontodelete) {
 							$href = $_SERVER["PHP_SELF"].'?id='.((int) $object->id).'&action=deleteline&token='.newToken().'&lineid='.((int) $line->id).'&fk_movement='.((int) $line2['fk_stock_movement']);
