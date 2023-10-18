b1c1c6b32d6925152a6401f5e9185bd42485608c
Laurent Destailleur <eldy@destailleur.fr>
2023-10-18 13:50:04 +0200

    FIX Tooltip for search syntax must not appear on date fields

--- htdocs/core/js/lib_head.js.php
+++ htdocs/core/js/lib_head.js.php
@@ -1048,7 +1048,7 @@ function document_preview(file, type, title)
 
 		optionsbuttons = {}
 		if (mode == 'image' && showOriginalSizeButton)
-		{	
+		{
 			var curRot = 0;
 			optionsbuttons = {
 				"<?php echo dol_escape_js($langs->transnoentitiesnoconv("OriginalSize")); ?>": function() { console.log("Click on original size"); jQuery(".ui-dialog-content.ui-widget-content > object").css({ "max-height": "none" }); },
@@ -1312,7 +1312,7 @@ jQuery(document).ready(function() {
 	}
 
 	// Code to set tooltip on search field
-	jQuery('table.liste tr.liste_titre_filter td.liste_titre input[name^="search"][type=text]:not("maxwidthdate")').attr('title', '<?php echo dol_escape_js($langs->transnoentities("SearchSyntaxTooltipForStringOrNum")) ?>');
+	jQuery('table.liste tr.liste_titre_filter td.liste_titre input[name^="search"][type=text]:not(".maxwidthdate")').attr('title', '<?php echo dol_escape_js($langs->transnoentities("SearchSyntaxTooltipForStringOrNum")) ?>');
 });
 
 
