296b380bf246a46f68b03c4bf62dcb09b61cbc56
William Mead <william.mead@manchenumerique.fr>
2023-10-20 11:45:34 +0200

    Added contributor details

--- htdocs/install/mysql/tables/llx_resource.sql
+++ htdocs/install/mysql/tables/llx_resource.sql
@@ -1,6 +1,7 @@
 -- Module to manage resources into Dolibarr ERP/CRM
 -- Copyright (C) 2013       Jean-François Ferry   <jfefe@aternatik.fr>
 -- Copyright (C) 2016		    Gilles Poirier		    <glgpoirier@gmail.com>
+-- Copyright (C) 2023           William Mead            <william.mead@manchenumerique.fr>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
