5983306b382e7feb50a48f13ea50ad41e6aaceb6
MDW <mdeweerd@users.noreply.github.com>
2023-10-21 13:43:30 +0200

    Define DoliDB::VERSIONMIN as mandatory constant in subclass
    See https://stackoverflow.com/a/43134924/6378634
    
    This also helps static tool checks

--- htdocs/core/db/DoliDB.class.php
+++ htdocs/core/db/DoliDB.class.php
@@ -29,6 +29,8 @@ require_once DOL_DOCUMENT_ROOT.'/core/db/Database.interface.php';
  */
 abstract class DoliDB implements Database
 {
+	/** @var string Force subclass to implement VERSIONMIN */
+	const VERSIONMIN=self::VERSIONMIN;
 	/** @var bool|resource|mysqli|SQLite3|PgSql\connection Database handler */
 	public $db;
 	/** @var string Database type */
