402e99151bfd241f05f0f009371ed76f56adeded
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2023-10-25 12:11:18 +0200

    workflow: ticket: reindent after last changes
    No functional changes.

--- htdocs/core/triggers/interface_20_modWorkflow_WorkflowManager.class.php
+++ htdocs/core/triggers/interface_20_modWorkflow_WorkflowManager.class.php
@@ -503,21 +503,19 @@ class InterfaceWorkflowManager extends DolibarrTriggers
 					$list = $contrat->getListOfContracts($option = 'all', $status = [Contrat::STATUS_DRAFT, Contrat::STATUS_VALIDATED], $product_categories = [$conf->global->TICKET_PRODUCT_CATEGORY], $line_status = [ContratLigne::STATUS_INITIAL, ContratLigne::STATUS_OPEN]);
 					if (!is_array($list) || empty($list))
 						continue;
-					{
-						$number_contracts_found = count($list);
-						if ($number_contracts_found == 0)
-							continue;
+					$number_contracts_found = count($list);
+					if ($number_contracts_found == 0)
+						continue;
 
-						foreach ($list as $linked_contract) {
-							$object->setContract($linked_contract->id);
-							// don't set '$contractid' so it is not used when creating an intervention.
-						}
-
-						if ($number_contracts_found > 1 && empty(NOLOGIN))
-							setEventMessage($langs->trans('TicketManyContractsLinked'), 'warnings');
-
-						break;
+					foreach ($list as $linked_contract) {
+						$object->setContract($linked_contract->id);
+						// don't set '$contractid' so it is not used when creating an intervention.
 					}
+
+					if ($number_contracts_found > 1 && empty(NOLOGIN))
+						setEventMessage($langs->trans('TicketManyContractsLinked'), 'warnings');
+
+					break;
 				}
 				if ($number_contracts_found == 0) {
 					if (empty(NOLOGIN)) setEventMessage($langs->trans('TicketNoContractFoundToLink'), 'mesgs');
