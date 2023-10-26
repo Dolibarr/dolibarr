4761d70cd9e0f52ed3afd8b5e476d042143de352
andreubisquerra <jove@bisquerra.com>
2023-10-26 17:58:42 +0200

    NEW Default customer, category and product when enable TakePOS (#25031)
    * Default customer, category and product when enable TakePOS
    
    * Update cashdesk.lang
    
    * Default category and product when enable TakePOS
    
    Easy setup for TakePOS. Create a category and product when TakePOS is enabled. It checks if TakePOS has already been configured to avoid recreating the category and productif it has already been created.
    
    * Update modTakePos.class.php
    
    * Update modTakePos.class.php
    
    * Update cashdesk.lang
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/core/modules/modTakePos.class.php
+++ htdocs/core/modules/modTakePos.class.php
@@ -25,6 +25,7 @@
  *  \brief      Description and activation file for the module TakePos
  */
 include_once DOL_DOCUMENT_ROOT.'/core/modules/DolibarrModules.class.php';
+include_once DOL_DOCUMENT_ROOT.'/categories/class/categorie.class.php';
 
 
 /**
@@ -266,9 +267,54 @@ class modTakePos extends DolibarrModules
 	 */
 	public function init($options = '')
 	{
-		global $conf, $db;
+		global $conf, $db, $langs, $user;
+		$langs->load("cashdesk");
 
 		dolibarr_set_const($db, "TAKEPOS_PRINT_METHOD", "browser", 'chaine', 0, '', $conf->entity);
+		
+		//Default customer for Point of sale
+		if (empty(getDolGlobalInt('CASHDESK_ID_THIRDPARTY1'))) {
+			$societe = new Societe($db);
+
+			$societe->name = $langs->trans("DefaultPOSThirdLabel");
+			$societe->client = 1;
+			$societe->note_private = "Default customer automaticaly created by Point Of Sale module activation. Can be used as the default generic customer in the Point Of Sale setup. Can also be edited or removed if you don't need a generic customer."
+				
+			$result = $societe->create($user);
+			if ($result > 0) {
+				dolibarr_set_const($db, "CASHDESK_ID_THIRDPARTY1", $result, 'chaine', 0, '', $conf->entity);
+			} else {
+				setEventMessages($societe->error, $societe->errors, 'errors');
+			}
+		}
+		
+		//Create category if not exists
+		$categories = new Categorie($this->db);
+		$cate_arbo = $categories->get_full_arbo('product', 0, 1);
+		if (is_array($cate_arbo)) {
+			if (!count($cate_arbo)) {
+				$category = new Categorie($db);
+				
+				$category->label = $langs->trans("DefaultPOSCatLabel");
+				$category->type = Categorie::TYPE_PRODUCT;
+				
+				$result = $category->create($user);
+
+				if ($result > 0) {
+					/* TODO Create a generic product only if there is no product in category $result yet */
+					/*
+					$product = new Product($db);
+					$product->status = 1;
+					$product->ref = "takepos";
+					$product->label = $langs->trans("DefaultPOSProductLabel");
+					$product->create($user);
+					$product->setCategories($result);
+	 				*/
+				} else {
+					setEventMessages($category->error, $category->errors, 'errors');
+				}
+			}
+		}
 
 		$result = $this->_load_tables('/install/mysql/', 'takepos');
 		if ($result < 0) {
