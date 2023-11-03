d0ad397cbc1ebfecd783ebadc53c7f9b42369510
Frédéric FRANCE <frederic.france@free.fr>
2023-11-03 16:55:56 +0100

    add missing

--- htdocs/theme/md/style.css.php
+++ htdocs/theme/md/style.css.php
@@ -1319,6 +1319,7 @@ span.fa.fa-plus-circle.paddingleft {
 
 /* Themes for badges */
 <?php include dol_buildpath($path.'/theme/'.$theme.'/badges.inc.php', 0); ?>
+<?php include dol_buildpath($path.'/theme/'.$theme.'/flags-sprite.inc.php', 0); ?>
 
 .borderrightlight
 {
@@ -7948,4 +7949,3 @@ if (is_object($db)) {
 div.flot-text .flot-tick-label .tickLabel, .fa-color-unset {
 	color: unset;
 }
-
