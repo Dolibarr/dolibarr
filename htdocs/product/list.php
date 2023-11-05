e64d1f6e468d0727147ad3d296a90de255979f59
Laurent Destailleur <eldy@destailleur.fr>
2023-11-06 00:16:26 +0100

    Fix phpcs

--- htdocs/product/list.php
+++ htdocs/product/list.php
@@ -589,7 +589,7 @@ if (empty($conf->global->MAIN_DISABLE_FULL_SCANLIST)) {
 	$result = $db->query($sql);
 	$nbtotalofrecords = $db->num_rows($result);
 	*/
-	
+
 	if (($page * $limit) > $nbtotalofrecords) {	// if total resultset is smaller then paging size (filtering), goto and load page 0
 		$page = 0;
 		$offset = 0;
