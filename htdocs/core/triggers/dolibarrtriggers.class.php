96bec97b74f3b531e6af2d6cbcb0e3c0efdbd720
William Mead <william.mead@manchenumerique.fr>
2023-11-06 17:01:45 +0100

    Added contributor details

--- htdocs/core/triggers/dolibarrtriggers.class.php
+++ htdocs/core/triggers/dolibarrtriggers.class.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2014 Marcos García         <marcosgdf@gmail.com>
+ * Copyright (C) 2023 William Mead			<william.mead@manchenumerique.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
