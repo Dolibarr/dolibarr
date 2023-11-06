85beb3ea5de221cacfcd14d482fd877b017fe76a
atm-lena <52402938+atm-lena@users.noreply.github.com>
2023-11-06 11:15:43 +0100

    Unit Management for MO lines (#26120)
    * Unit Management for products in bom and mo
    
    * WIP
    
    * Unit in to produce tab
    
    * FIX wip
    
    * float val
    
    * Fix Travis
    
    * fk_unit type int
    
    * Clean code
    
    * Clean code
    
    * FIX code sniffer
    
    * Update llx_mrp_production.sql
    
    * Update 18.0.0-19.0.0.sql
    
    ---------
    
    Co-authored-by: atm-lena <lena.papazian@atm-consulting.fr>
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/install/mysql/migration/18.0.0-19.0.0.sql
+++ htdocs/install/mysql/migration/18.0.0-19.0.0.sql
@@ -173,3 +173,7 @@ ALTER TABLE llx_facturedet ADD COLUMN fk_warehouse integer NULL;	-- To store the
 
 ALTER TABLE llx_multicurrency_rate ADD COLUMN rate_indirect double DEFAULT 0;
 
+ALTER TABLE llx_mrp_production ADD COLUMN fk_unit integer DEFAULT NULL;
+-- VMYSQL4.1 UPDATE llx_mrp_production as mp INNER JOIN llx_bom_bomline as bbl ON mp.origin_id = bbl.rowid SET mp.fk_unit = bbl.fk_unit WHERE mp.origin_type = 'bomline' AND mk.fk_unit IS NULL;
+-- VMYSQL4.1 UPDATE llx_bom_bomline as bbl INNER JOIN llx_product as p ON p.rowid = bbl.fk_product SET bbl.fk_unit = p.fk_unit WHERE bbl.fk_unit IS NULL;
+
