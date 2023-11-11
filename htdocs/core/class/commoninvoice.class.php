b9f15e362ddbbd5c49f484813ac8076e339c19c1
sonikf <93765174+sonikf@users.noreply.github.com>
2023-11-11 13:57:18 +0200

    Add invoice subtype in template invoice

--- htdocs/core/class/commoninvoice.class.php
+++ htdocs/core/class/commoninvoice.class.php
@@ -708,6 +708,27 @@ abstract class CommonInvoice extends CommonObject
 				dol_print_error($this->db);
 				return -1;
 			}
+		} elseif ($table === 'facture_rec' || $table === 'facture_fourn_rec') {
+			$sql = "SELECT s.label FROM " . MAIN_DB_PREFIX . $table . " AS f";
+			$sql .= " INNER JOIN " . MAIN_DB_PREFIX . "c_invoice_subtype AS s ON f.subtype = s.rowid";
+			$sql .= " WHERE f.titre = '".$this->db->escape($this->titre)."'";
+
+			$resql = $this->db->query($sql);
+
+			if ($resql) {
+				$subtypeLabel = '';
+
+				while ($obj = $this->db->fetch_object($resql)) {
+					$subtypeLabel = $obj->label;
+				}
+
+				if (!empty($subtypeLabel)) {
+					print '  ' . $subtypeLabel;
+				}
+			} else {
+				dol_print_error($this->db);
+				return -1;
+			}
 		}
 	}
 
