e373c101363ee5974af7ed1da31b7b66861d85d4
Charlène Benke <1179011+defrance@users.noreply.github.com>
2023-11-13 21:09:02 +0100

    Update agenda.php

--- htdocs/fichinter/agenda.php
+++ htdocs/fichinter/agenda.php
@@ -71,7 +71,8 @@ if (!$sortorder) {
 // Initialize technical objects
 $object = new Fichinter($db);
 $extrafields = new ExtraFields($db);
-$diroutputmassaction = $conf->ficheinter->multidir_output[$conf->entity].'/temp/massgeneration/'.$user->id;
+$diroutputmassaction = getMultidirOutput($object).'/temp/massgeneration/'.$user->id;
+
 $hookmanager->initHooks(array('myobjectagenda', 'globalcard')); // Note that conf->hooks_modules contains array
 // Fetch optionals attributes and labels
 $extrafields->fetch_name_optionals_label($object->table_element);
@@ -79,7 +80,7 @@ $extrafields->fetch_name_optionals_label($object->table_element);
 // Load object
 include DOL_DOCUMENT_ROOT.'/core/actions_fetchobject.inc.php'; // Must be include, not include_once  // Must be include, not include_once. Include fetch and fetch_thirdparty but not fetch_optionals
 if ($id > 0 || !empty($ref)) {
-	$upload_dir = $conf->ficheinter->multidir_output[!empty($object->entity) ? $object->entity : $conf->entity]."/".$object->id;
+	$upload_dir = getMultidirOutput($object).'/'.$object->id;
 }
 
 $permissiontoread = $user->hasRight("fichinter", "lire");
