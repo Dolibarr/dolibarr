2a7cd31534020af123a7b20b327e3e58e590ba9a
Frédéric FRANCE <frederic34@users.noreply.github.com>
2023-11-14 09:35:26 +0100

    Update commoninvoice.class.php

--- htdocs/core/class/commoninvoice.class.php
+++ htdocs/core/class/commoninvoice.class.php
@@ -692,7 +692,6 @@ abstract class CommonInvoice extends CommonObject
 			$sql = "SELECT s.label FROM " . MAIN_DB_PREFIX . $table . " AS f";
 			$sql .= " INNER JOIN " . MAIN_DB_PREFIX . "c_invoice_subtype AS s ON f.subtype = s.rowid";
 			$sql .= " WHERE f.ref = '".$this->db->escape($this->ref)."'";
-
 		} elseif ($table === 'facture_rec' || $table === 'facture_fourn_rec') {
 			$sql = "SELECT s.label FROM " . MAIN_DB_PREFIX . $table . " AS f";
 			$sql .= " INNER JOIN " . MAIN_DB_PREFIX . "c_invoice_subtype AS s ON f.subtype = s.rowid";
