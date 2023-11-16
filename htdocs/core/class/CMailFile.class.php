c1f70d2710c65ee1eaf16c279bb50f981e83dd8e
Florian HENRY <florian.henry@open-concept.pro>
2023-11-16 17:33:33 +0100

    fix: When sendmail failed it return false, so >=0 do not trigger error

--- htdocs/core/class/CMailFile.class.php
+++ htdocs/core/class/CMailFile.class.php
@@ -963,7 +963,7 @@ class CMailFile
 					} catch (Exception $e) {
 						// Return an error if token not found
 						$this->error = $e->getMessage();
-						dol_syslog("CMailFile::sendfile: mail end error=1".$this->error, LOG_ERR);
+						dol_syslog("CMailFile::sendfile: mail end error=".$this->error, LOG_ERR);
 					}
 				}
 
@@ -1153,10 +1153,10 @@ class CMailFile
 
 				$res = true;
 				if (!empty($this->error) || !empty($this->errors) || !$result) {
-					var_dump('toto');
 					if (!empty($failedRecipients)) {
 						$this->errors[] = 'Transport failed for the following addresses: "' . join('", "', $failedRecipients) . '".';
 					}
+					$this->error .= join(' ',$this->errors);
 					dol_syslog("CMailFile::sendfile: mail end error=".$this->error, LOG_ERR);
 					$res = false;
 
@@ -1195,7 +1195,6 @@ class CMailFile
 		}
 
 		error_reporting($errorlevel); // Reactive niveau erreur origine
-		var_dump($res);
 		return $res;
 	}
 
