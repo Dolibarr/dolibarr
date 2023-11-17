67b0d81beeb8132e726f28588eeec758fd283a3e
Saami PERDRIX - ATM Consulting <saami.perdrix@atm-consulting.fr>
2023-11-17 12:31:30 +0100

    nom des triggers adapté selon objet

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -3405,9 +3405,10 @@ abstract class CommonObject
 	 *
 	 *  @param      string		$note		New value for note
 	 *  @param		string		$suffix		'', '_public' or '_private'
+     *  @param      int         $notrigger  1=Does not execute triggers, 0=execute triggers
 	 *  @return     int      		   		<0 if KO, >0 if OK
 	 */
-	public function update_note($note, $suffix = '', $call_trigger = 1)
+	public function update_note($note, $suffix = '', $notrigger = 0)
 	{
 		// phpcs:enable
 		global $user;
@@ -3451,9 +3452,31 @@ abstract class CommonObject
 				$this->note = $note; // deprecated
 				$this->note_private = $note;
 			}
-            if(!empty($call_trigger)) {
-                $ret = $this->call_trigger('UPDATE_NOTE', $user);
-                if($ret < 0 ) { return - 1; }
+            if(empty($notrigger)) {
+                switch($this->element) {
+                    case 'societe':
+                        $trigger_name = 'COMPANY_MODIFY';
+                        break;
+                    case 'commande':
+                        $trigger_name = 'ORDER_MODIFY';
+                        break;
+                    case 'facture':
+                        $trigger_name = 'BILL_MODIFY';
+                        break;
+                    case 'invoice_supplier':
+                        $trigger_name = 'BILL_SUPPLIER_MODIFY';
+                        break;
+                    case 'facturerec':
+                        $trigger_name = 'BILLREC_MODIFIY';
+                        break;
+                    case 'expensereport':
+                        $trigger_name = 'EXPENSE_REPORT_MODIFY';
+                        break;
+                    default:
+                        $trigger_name = strtoupper($this->element) . '_MODIFY';
+                }
+                $ret = $this->call_trigger($trigger_name, $user);
+                if($ret < 0 ) { return -1; }
             }
 			return 1;
 		} else {
