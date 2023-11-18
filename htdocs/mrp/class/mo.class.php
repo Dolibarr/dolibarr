c3939fc4d0796dc7003382b2696a2920e9feb780
Eric <1468823+rycks@users.noreply.github.com>
2023-11-19 00:54:39 +0100

    fix undef line and action (#26646)
    * fix undef line and action
    
    * fix update quantity
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/mrp/class/mo.class.php
+++ htdocs/mrp/class/mo.class.php
@@ -810,7 +810,7 @@ class Mo extends CommonObject
 					if ($moLine->role == 'toconsume' || $moLine->role == 'toproduce') {
 						if (empty($moLine->qty_frozen)) {
 							$qty = $newQty * $moLine->qty / $oldQty;
-							$moLine->qty = price2num($qty * (!empty($moLine->efficiency) ? $moLine->efficiency : 1 ), 'MS'); // Calculate with Qty to produce and efficiency
+							$moLine->qty = price2num($qty, 'MS');
 							$res = $moLine->update($user);
 							if (!$res) $error++;
 						}
@@ -1385,7 +1385,7 @@ class Mo extends CommonObject
 	 */
 	public function getNomUrl($withpicto = 0, $option = '', $notooltip = 0, $morecss = '', $save_lastsearch_value = -1)
 	{
-		global $conf, $langs, $hookmanager;
+		global $conf, $langs, $action, $hookmanager;
 
 		if (!empty($conf->dol_no_mouse_hover)) {
 			$notooltip = 1; // Force disable tooltips
@@ -1450,7 +1450,6 @@ class Mo extends CommonObject
 		$result .= $linkend;
 		//if ($withpicto != 2) $result.=(($addlabel && $this->label) ? $sep . dol_trunc($this->label, ($addlabel > 1 ? $addlabel : 0)) : '');
 
-		global $action, $hookmanager;
 		$hookmanager->initHooks(array('modao'));
 		$parameters = array('id'=>$this->id, 'getnomurl' => &$result);
 		$reshook = $hookmanager->executeHooks('getNomUrl', $parameters, $this, $action); // Note that $action and $object may have been modified by some hooks
