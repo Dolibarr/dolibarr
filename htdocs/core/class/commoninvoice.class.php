3935ae04182907d0ecf43649da061c1923f2cc77
Florian HENRY <florian.henry@open-concept.pro>
2023-11-18 15:09:04 +0100

    fix: phpstan

--- htdocs/core/class/commoninvoice.class.php
+++ htdocs/core/class/commoninvoice.class.php
@@ -696,12 +696,12 @@ abstract class CommonInvoice extends CommonObject
 	{
 		$subtypeLabel = '';
 		if ($table === 'facture' || $table === 'facture_fourn') {
-			$sql = "SELECT s.label FROM " . MAIN_DB_PREFIX . $table . " AS f";
-			$sql .= " INNER JOIN " . MAIN_DB_PREFIX . "c_invoice_subtype AS s ON f.subtype = s.rowid";
+			$sql = "SELECT s.label FROM " . $this->db->prefix() . $table . " AS f";
+			$sql .= " INNER JOIN " . $this->db->prefix() . "c_invoice_subtype AS s ON f.subtype = s.rowid";
 			$sql .= " WHERE f.ref = '".$this->db->escape($this->ref)."'";
 		} elseif ($table === 'facture_rec' || $table === 'facture_fourn_rec') {
-			$sql = "SELECT s.label FROM " . MAIN_DB_PREFIX . $table . " AS f";
-			$sql .= " INNER JOIN " . MAIN_DB_PREFIX . "c_invoice_subtype AS s ON f.subtype = s.rowid";
+			$sql = "SELECT s.label FROM " . $this->db->prefix() . $table . " AS f";
+			$sql .= " INNER JOIN " . $this->db->prefix() . "c_invoice_subtype AS s ON f.subtype = s.rowid";
 			$sql .= " WHERE f.titre = '".$this->db->escape($this->title)."'";
 		} else {
 			return -1;
