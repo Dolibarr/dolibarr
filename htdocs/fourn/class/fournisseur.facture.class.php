2d30422c45bb7bd9595ed82f97301940b97fc7db
moreauf <moreauf@theobald-groupe.com>
2023-11-18 17:29:28 +0100

    fix PHP STAN error on fournisseur facture class

--- htdocs/fourn/class/fournisseur.facture.class.php
+++ htdocs/fourn/class/fournisseur.facture.class.php
@@ -2752,7 +2752,7 @@ class FactureFournisseur extends CommonInvoice
 	 */
 	public function getTooltipContentArray($params)
 	{
-		global $conf, $langs;
+		global $conf, $langs, $mysoc;
 
 		$langs->load('bills');
 
