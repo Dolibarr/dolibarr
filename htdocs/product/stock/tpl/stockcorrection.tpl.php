9d67d79d3fe9bf619f5873169909b820cc43c304
VESSILLER <lvessiller@open-dsi.fr>
2023-11-18 09:44:40 +0100

    Use current token in ajax

--- htdocs/product/stock/tpl/stockcorrection.tpl.php
+++ htdocs/product/stock/tpl/stockcorrection.tpl.php
@@ -106,7 +106,7 @@ if ($disableSellBy == 0 || $disableEatBy == 0) {
 			var disableEatBy = '.dol_escape_js($disableSellBy).';
 			jQuery("#batch_number").change(function(event) {
 				var batch = jQuery(this).val();
-				jQuery.getJSON("'.DOL_URL_ROOT.'/product/ajax/product_lot.php?action=search&token='.newToken().'&product_id='.$id.'&batch="+batch, function(data) {
+				jQuery.getJSON("'.DOL_URL_ROOT.'/product/ajax/product_lot.php?action=search&token='.currentToken().'&product_id='.$id.'&batch="+batch, function(data) {
 					if (data.length > 0) {
 						var productLot = data[0];
 						if (disableSellBy == 0) {
