d697d1ae5246573b8ea9ecf4ef5f49ea28a99774
Frédéric France <frederic.france@free.fr>
2023-11-18 12:23:38 +0100

    remove duplicate

--- htdocs/accountancy/class/accountancyexport.class.php
+++ htdocs/accountancy/class/accountancyexport.class.php
@@ -347,10 +347,6 @@ class AccountancyExport
 		$downloadFilePath = '';
 		$archiveFullName = '';
 		$archivePath = '';
-		$exportFileFullName = '';
-		$downloadFileFullName = '';
-		$downloadFileMimeType = '';
-		$downloadFilePath = '';
 		$archiveFileList = array();
 		if ($withAttachment == 1) {
 			if ($downloadMode == 0) {
