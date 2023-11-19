196e0a7e4080d9f16e0e9e5a4a0ab0b16415e3ad
Frédéric France <frederic.france@free.fr>
2023-11-19 23:11:42 +0100

    fix

--- htdocs/datapolicy/class/actions_datapolicy.class.php
+++ htdocs/datapolicy/class/actions_datapolicy.class.php
@@ -188,34 +188,6 @@ class ActionsDatapolicy extends CommonHookActions
 		}
 	}
 
-	/**
-	 * Overloading the addMoreMassActions function : replacing the parent's function with the one below
-	 *
-	 * @param   array           $parameters		Hook metadatas (context, etc...)
-	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
-	 * @param   string          $action         Current action (if set). Generally create or edit or null
-	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
-	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
-	 */
-	public function addMoreMassActions($parameters, &$object, &$action, $hookmanager)
-	{
-		global $langs;
-
-		$error = 0; // Error counter
-
-		/* print_r($parameters); print_r($object); echo "action: " . $action; */
-		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2'))) {  // do something only for the context 'somecontext1' or 'somecontext2'
-			$this->resprints = '<option value="0"'.(!empty($disabled) ? ' disabled="disabled"' : '').'>'.$langs->trans("datapolicyMassAction").'</option>';
-		}
-
-		if (!$error) {
-			return 0; // or return 1 to replace standard code
-		} else {
-			$this->errors[] = 'Error message';
-			return -1;
-		}
-	}
-
 
 	/**
 	 * addMoreActionsButtons
