af8ca9a205fd533207784a78c1b4af9cc65391dd
evarisk-micka <111346550+evarisk-micka@users.noreply.github.com>
2023-11-19 10:53:50 +0100

    NEW #26676 [MassAction] add missing substitution variables (#26678)
    * NEW #26676 [MassAction] add missing substitution variables
    
    * Update massactions_pre.tpl.php
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/core/tpl/massactions_pre.tpl.php
+++ htdocs/core/tpl/massactions_pre.tpl.php
@@ -282,8 +282,11 @@ if ($massaction == 'presend') {
 
 	$substitutionarray['__EMAIL__'] = $sendto;
 	$substitutionarray['__CHECK_READ__'] = '<img src="'.DOL_MAIN_URL_ROOT.'/public/emailing/mailing-read.php?tag=undefined&securitykey='.dol_hash(getDolGlobalString('MAILING_EMAIL_UNSUBSCRIBE_KEY')."-undefined", 'md5').'" width="1" height="1" style="width:1px;height:1px" border="0"/>';
-	$substitutionarray['__PERSONALIZED__'] = ''; // deprecated
 	$substitutionarray['__CONTACTCIVNAME__'] = '';
+	$substitutionarray['__ONLINE_PAYMENT_URL__'] = 'UrlToPayOnlineIfApplicable';
+	$substitutionarray['__ONLINE_PAYMENT_TEXT_AND_URL__'] = 'TextAndUrlToPayOnlineIfApplicable';
+	$substitutionarray['__THIRDPARTY_NAME__'] = '__THIRDPARTY_NAME__';
+	$substitutionarray['__PROJECT_NAME__'] = '__PROJECT_NAME__';
 
 	$parameters = array(
 		'mode' => 'formemail'
