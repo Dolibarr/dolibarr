eba857b56fba79f9f92245e56746ae643070e858
Frédéric France <frederic.france@free.fr>
2023-11-21 00:15:44 +0100

    check phpstan bug with self constant reference

--- htdocs/core/db/DoliDB.class.php
+++ htdocs/core/db/DoliDB.class.php
@@ -30,7 +30,7 @@ require_once DOL_DOCUMENT_ROOT.'/core/db/Database.interface.php';
 abstract class DoliDB implements Database
 {
 	/** @var string Force subclass to implement VERSIONMIN */
-	// const VERSIONMIN=self::VERSIONMIN;
+	const VERSIONMIN=self::VERSIONMIN;
 	/** @var bool|resource|mysqli|SQLite3|PgSql\Connection Database handler */
 	public $db;
 	/** @var string Database type */
