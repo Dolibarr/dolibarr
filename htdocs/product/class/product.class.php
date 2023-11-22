0662a88b5842e4df53e40b4f3c6a2c5d299d99d2
Frédéric FRANCE <frederic34@users.noreply.github.com>
2023-11-22 14:00:00 +0100

    fix product service tooltip

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -5189,7 +5189,7 @@ class Product extends CommonObject
 		}
 		$params = [
 			'id' => $this->id,
-			'objecttype' => $this->element,
+			'objecttype' => (isset($this->type) ? ($this->type == 1 ? 'service' : 'product') : $this->element),
 			'option' => $option,
 			'nofetch' => 1,
 		];
