41e21d3c892d3c4130e5bf825bcf78e97bf019c8
Frédéric FRANCE <frederic.france@free.fr>
2023-11-23 22:58:26 +0100

    test can fail

--- htdocs/core/db/sqlite3.class.php
+++ htdocs/core/db/sqlite3.class.php
@@ -64,7 +64,6 @@ class DoliDBSqlite3 extends DoliDB
 	{
 		global $conf;
 
-		$type = $type; // for phpstan unused parameter
 		// Note that having "static" property for "$forcecharset" and "$forcecollate" will make error here in strict mode, so they are not static
 		if (!empty($conf->db->character_set)) {
 			$this->forcecharset = $conf->db->character_set;
