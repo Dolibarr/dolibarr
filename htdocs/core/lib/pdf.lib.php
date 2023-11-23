1c7239e6ff0670fadf31110aaf8db466222a98dd
Laurent Destailleur <eldy@destailleur.fr>
2023-11-23 17:19:10 +0100

    Fix type

--- htdocs/core/lib/pdf.lib.php
+++ htdocs/core/lib/pdf.lib.php
@@ -2556,7 +2556,7 @@ function pdf_getSizeForImage($realpath)
  *	@param	int			$i					Current line number
  *  @param  Translate	$outputlangs		Object langs for output
  *  @param	int			$hidedetails		Hide details (0=no, 1=yes, 2=just special lines)
- * 	@return	float							Return total of line excl tax
+ * 	@return	float|string					Return total of line excl tax
  */
 function pdfGetLineTotalDiscountAmount($object, $i, $outputlangs, $hidedetails = 0)
 {
