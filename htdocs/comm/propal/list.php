f86873684494de69ae4b928e1b2bc63929188009
Charlène Benke <1179011+defrance@users.noreply.github.com>
2023-11-27 15:25:57 +0100

    Update list.php

--- htdocs/comm/propal/list.php
+++ htdocs/comm/propal/list.php
@@ -11,7 +11,7 @@
  * Copyright (C) 2013      Cédric Salvador			<csalvador@gpcsolutions.fr>
  * Copyright (C) 2015      Jean-François Ferry		<jfefe@aternatik.fr>
  * Copyright (C) 2016-2021 Ferran Marcet			<fmarcet@2byte.es>
- * Copyright (C) 2017-2018 Charlene Benke			<charlie@patas-monkey.com>
+ * Copyright (C) 2017-20238 Charlene Benke			<charlene@patas-monkey.com>
  * Copyright (C) 2018	   Nicolas ZABOURI			<info@inovea-conseil.com>
  * Copyright (C) 2019-2021 Alexandre Spangaro		<aspangaro@open-dsi.fr>
  * Copyright (C) 2021	   Anthony Berton			<anthony.berton@bb2a.fr>
@@ -1941,7 +1941,8 @@ if ($search_date_signature_endyear) {
 				}
 
 				print '<td class="center">';
-				print $typenArray[$obj->typent_code];
+				if (!empty($obj->typent_code)
+					print $typenArray[$obj->typent_code];
 				print '</td>';
 				if (!$i) {
 					$totalarray['nbfield']++;
@@ -2014,10 +2015,11 @@ if ($search_date_signature_endyear) {
 			}
 			// Source - input reason
 			if (!empty($arrayfields['p.fk_input_reason']['checked'])) {
-				print '<td class="tdoverflowmax125" title="'.dol_escape_htmltag($form->cache_demand_reason[$obj->fk_input_reason]['label']).'">';
 				if ($obj->fk_input_reason > 0) {
+					print '<td class="tdoverflowmax125" title="'.dol_escape_htmltag($form->cache_demand_reason[$obj->fk_input_reason]['label']).'">';
 					print $form->cache_demand_reason[$obj->fk_input_reason]['label'];
-				}
+				} else
+					print '<td class="tdoverflowmax125">';
 				print '</td>';
 				if (!$i) {
 					$totalarray['nbfield']++;
