a241d85f84e2c95a96cb544df3dc6d3994653007
Laurent Destailleur <eldy@users.sourceforge.net>
2023-11-27 18:45:08 +0100

    FIX #26858

--- htdocs/emailcollector/class/emailcollector.class.php
+++ htdocs/emailcollector/class/emailcollector.class.php
@@ -1132,8 +1132,9 @@ class EmailCollector extends CommonObject
 		if (getDolGlobalString('MAIN_IMAP_USE_PHPIMAP')) {
 			if ($this->acces_type == 1) {
 				// Mode OAUth2 with PHP-IMAP
-				$supportedoauth2array = array();
-				require_once DOL_DOCUMENT_ROOT.'/core/lib/oauth.lib.php'; // define $supportedoauth2array
+				require_once DOL_DOCUMENT_ROOT.'/core/lib/oauth.lib.php';
+
+				$supportedoauth2array = getSupportedOauth2Array();
 
 				$keyforsupportedoauth2array = $this->oauth_service;
 				if (preg_match('/^.*-/', $keyforsupportedoauth2array)) {
