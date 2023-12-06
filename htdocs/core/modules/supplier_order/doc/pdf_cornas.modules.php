f74073f1a3baed7558038b48c43414a40917cca4
William Mead <william.mead@manchenumerique.fr>
2023-12-06 15:18:25 +0100

    Added contributor details

--- htdocs/core/modules/supplier_order/doc/pdf_cornas.modules.php
+++ htdocs/core/modules/supplier_order/doc/pdf_cornas.modules.php
@@ -6,6 +6,7 @@
  * Copyright (C) 2015      Marcos García         <marcosgdf@gmail.com>
  * Copyright (C) 2017      Ferran Marcet         <fmarcet@2byte.es>
  * Copyright (C) 2018-2022 Frédéric France       <frederic.france@netlogic.fr>
+ * Copyright (C) 2023		William Mead		<william.mead@manchenumerique.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
