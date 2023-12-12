765bad6d7b4c7f5a10564d9628e43c4abfefc2b2
vmaury <artec.vm@arnac.net>
2023-12-12 19:24:59 +0100

    restriction applys only to standard customer invoices

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -4076,10 +4076,13 @@ if ($action == 'create') {
 
 		print '<div class="div-table-responsive-no-min">';
 		print '<table class="noborder centpercent">';
-		$objectsrc2 = clone $objectsrc; // src object cloned in case it'll be used later
-		$object->srcObjDelBilledLines($objectsrc2);
-		$objectsrc2->printOriginLinesList('', $selectedLines);
-
+		if ($object->type == $object::TYPE_STANDARD) { // other types can drive to hazardous things
+			$objectsrc2 = clone $objectsrc; // src object cloned in case it'll be used later
+			$object->srcObjDelBilledLines($objectsrc2);
+			$objectsrc2->printOriginLinesList('', $selectedLines);
+		} else {
+			$objectsrc->printOriginLinesList('', $selectedLines);
+		}
 		print '</table>';
 		print '</div>';
 	}
