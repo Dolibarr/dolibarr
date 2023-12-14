91f1c2d4fb324c75a952db24147c7a844a8107d3
Dev2a <contact@dev2a.pro>
2023-12-14 14:00:53 +0100

    NEW Simplify the verification of context in hook

--- htdocs/core/class/commonhookactions.class.php
+++ htdocs/core/class/commonhookactions.class.php
@@ -36,4 +36,26 @@ abstract class CommonHookActions
 	 * @var array 	Array of results.
 	 */
 	public $results = array();
+
+	/**
+	 * Check context of hook
+	 * @param array $parameters Hook parameters.
+	 * @param array|string $allContexts Context to check
+	 * @return bool
+	 */
+	protected function isContext($parameters, $allContexts)
+	{
+		if (is_array($allContexts)) {
+			foreach ($allContexts as $context) {
+				if ($this->isContext($parameters, $context)) {
+					return true;
+				}
+			}
+		}
+		if ($parameters['currentcontext'] == $allContexts) {
+			return true;
+		}
+		$contexts = explode(':', $parameters['context']);
+		return in_array($allContexts, $contexts);
+	}
 }
