e4356ef635eda6fa0c865b259f74475effabf86e
Lamrani Abdel <lamrani002@gmail.com>
2023-12-19 15:53:17 +0100

    add column in table website

--- htdocs/install/mysql/tables/llx_website-website.sql
+++ htdocs/install/mysql/tables/llx_website-website.sql
@@ -42,5 +42,5 @@ CREATE TABLE llx_website
     pageviews_total BIGINT UNSIGNED DEFAULT 0,			-- increased by 1 at each page access, no reset
 	tms           timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     import_key    varchar(14),      -- import key	
-	name_template varchar(255) NULL
+	name_template varchar(255) NULL   --name of template imported
 ) ENGINE=innodb;
