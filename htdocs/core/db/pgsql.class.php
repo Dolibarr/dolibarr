8874ef84545dea92a1e60891723012c9531837fb
Laurent Destailleur <eldy@destailleur.fr>
2023-12-23 18:47:38 +0100

    Debug v19

--- htdocs/core/db/pgsql.class.php
+++ htdocs/core/db/pgsql.class.php
@@ -69,7 +69,7 @@ class DoliDBPgsql extends DoliDB
 	 *	Constructor.
 	 *	This create an opened connexion to a database server and eventually to a database
 	 *
-	 *	@param      string	$type		Type of database (mysql, pgsql...)
+	 *	@param      string	$type		Type of database (mysql, pgsql...). Not used.
 	 *	@param	    string	$host		Address of database server
 	 *	@param	    string	$user		Nom de l'utilisateur autorise
 	 *	@param	    string	$pass		Mot de passe
