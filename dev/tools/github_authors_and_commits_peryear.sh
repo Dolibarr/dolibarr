7b42e2d7c2c0ad264a1706cb80d33ced46025910
Laurent Destailleur <eldy@destailleur.fr>
2023-12-29 16:28:10 +0100

    Enhance statistics tool

--- dev/tools/github_authors_and_commits_peryear.sh
+++ dev/tools/github_authors_and_commits_peryear.sh
@@ -18,9 +18,10 @@ if [ "x$2" != "x" ]; then
 fi
 
 echo "--- Number of contributors for the year"
-echo "git log --since $FROM --before $TO | grep ^Author | sort -u -f -i -b | wc -l"
-git log --since $FROM --before $TO | grep ^Author | sort -u -f -i -b | wc -l
+echo "git log --since $FROM --before $TO | grep ^Author | awk -F'<' '{ print $1 }' | iconv -f UTF-8 -t ASCII//TRANSLIT | sort -u -f -i -b | wc -l"
+git log --since $FROM --before $TO | grep '^Author' | awk -F"<" '{ print $1 }' | iconv -f UTF-8 -t ASCII//TRANSLIT | sort -u -f -i -b | wc -l
 
 
 echo "--- Number of commit for the year"
-git log --pretty='format:%cd' --date=format:'%Y' | uniq -c | awk '{ if ($2 >= '$1') { print "Year: "$2", commits: "$1 } }'
+echo "git log --pretty='format:%cd' --date=format:'%Y' | sort | uniq -c | awk '{ if (\$2 >= '"$1"') { print \"Year: \"\$2\", commits: \"\$1 } }'"
+git log --pretty='format:%cd' --date=format:'%Y' | sort | uniq -c | awk '{ if ($2 >= '$1') { print "Year: "$2", commits: "$1 } }'
