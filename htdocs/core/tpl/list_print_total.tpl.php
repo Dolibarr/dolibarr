f7430662949b5f41083e32f3de7e4b557b93162f
vmaury <artec.vm@arnac.net>
2023-12-29 14:52:12 +0100

    Fix #24031 add - if option seleced - grand total to lists

--- htdocs/core/tpl/list_print_total.tpl.php
+++ htdocs/core/tpl/list_print_total.tpl.php
@@ -87,7 +87,7 @@ if (isset($totalarray['pos'])) {
  *
  * @param string $type of field (duration, string..)
  * @param string $val the value to display
- * 
+ *
  * @return void (direct print)
  */
 function printTotalValCell($type, $val)
