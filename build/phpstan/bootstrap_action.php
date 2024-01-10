f711144428020480ba742089b4f5a9d8e7e64a5f
Laurent Destailleur <eldy@destailleur.fr>
2024-01-10 14:52:12 +0100

    Try to move tmp dir

--- build/phpstan/bootstrap_action.php
+++ build/phpstan/bootstrap_action.php
@@ -1,4 +1,5 @@
 <?php
+
 // Defined some constants and load Dolibarr env to reduce PHPStan bootstrap that fails to load a lot of things.
 define('DOL_DOCUMENT_ROOT', __DIR__ . '/../../htdocs');
 define('DOL_DATA_ROOT', __DIR__ . '/../../documents');
@@ -9,5 +10,6 @@ define('MAIN_DB_PREFIX', 'llx_');
 // Load the main.inc.php file to have functions env defined
 define("NOLOGIN", '1');
 define("NOHTTPSREDIRECT", '1');
+
 global $conf, $db, $langs, $user;
 // include_once DOL_DOCUMENT_ROOT . '/../../htdocs/main.inc.php';
