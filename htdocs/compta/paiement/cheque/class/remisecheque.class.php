a4f94f84f19d4370bdc07411fca3e7127a7c8749
Frédéric FRANCE <frederic34@users.noreply.github.com>
2024-01-10 17:20:36 +0100

    fix default parameter (#27141)

--- htdocs/compta/paiement/cheque/class/remisecheque.class.php
+++ htdocs/compta/paiement/cheque/class/remisecheque.class.php
@@ -291,7 +291,7 @@ class RemiseCheque extends CommonObject
 	 *	@param  User	$user 		User that delete
 	 *	@return	int
 	 */
-	public function delete($user = null)
+	public function delete($user)
 	{
 		global $conf;
 
