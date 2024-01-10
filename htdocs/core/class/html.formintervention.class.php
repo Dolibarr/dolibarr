7228b91afc5bcbf2b89b79045b46c72fc9b76bdb
Charlène Benke <1179011+defrance@users.noreply.github.com>
2024-01-10 15:49:00 +0100

    Update html.formintervention.class.php

--- htdocs/core/class/html.formintervention.class.php
+++ htdocs/core/class/html.formintervention.class.php
@@ -74,12 +74,10 @@ class FormIntervention
 		$sql = "SELECT f.rowid, f.ref, f.fk_soc, f.fk_statut";
 		$sql .= " FROM ".$this->db->prefix()."fichinter as f";
 		$sql .= " WHERE f.entity = ".$conf->entity;
-		if ($socid  >0) {
-			if ($socid == '0') {
-				$sql .= " AND (f.fk_soc = 0 OR f.fk_soc IS NULL)";
-			} else {
+		if ($socid  > 0) {
 				$sql .= " AND f.fk_soc = ".((int) $socid);
-			}
+		} else {
+				$sql .= " AND (f.fk_soc = 0 OR f.fk_soc IS NULL)";
 		}
 		if ($draftonly) {
 			$sql .= " AND f.fk_statut = 0";
