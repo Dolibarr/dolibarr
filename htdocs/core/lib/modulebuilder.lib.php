aa426cf2acc82b82d6f5aef8a2983a4fedf739d2
Laurent Destailleur <eldy@destailleur.fr>
2024-01-14 12:21:45 +0100

    Update modulebuilder.lib.php

--- htdocs/core/lib/modulebuilder.lib.php
+++ htdocs/core/lib/modulebuilder.lib.php
@@ -1317,8 +1317,7 @@ function writeApiUrlsInDoc($file_api, $file_doc)
 
 	// build format asciidoc for urls in table
 	if (!$error) {
-		// Note: ...Obj"."t  to avoid codespell suggestion for Object
-		$asciiDocTable = "[options=\"header\"]\n|===\n|Obje"."t | URLs\n";  // phpcs:ignore
+		$asciiDocTable = "[options=\"header\"]\n|===\n|Object | URLs\n";  // phpcs:ignore
 		foreach ($groupedUrls as $objectName => $urls) {
 			$urlsList = implode(" +\n*", $urls);
 			$asciiDocTable .= "|$objectName | \n*$urlsList +\n";
