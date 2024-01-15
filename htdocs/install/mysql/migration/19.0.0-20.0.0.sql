3670ba75ba4f1172ea50c1f266af78e141402965
Christophe Battarel <christophe@altairis.fr>
2024-01-15 09:52:54 +0100

    rename tables

--- htdocs/install/mysql/migration/19.0.0-20.0.0.sql
+++ htdocs/install/mysql/migration/19.0.0-20.0.0.sql
@@ -203,3 +203,5 @@ ALTER TABLE llx_categorie ADD COLUMN position integer DEFAULT 0 AFTER color;
 
 ALTER TABLE llx_product DROP COLUMN onportal;
 
+RENAME TABLE llx_commande_fournisseur_dispatch_extrafields TO llx_receptiondet_batch_extrafields;
+RENAME TABLE llx_commande_fournisseur_dispatch TO llx_receptiondet_batch;
