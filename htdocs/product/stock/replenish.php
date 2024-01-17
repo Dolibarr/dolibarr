9861762b7f9cba722985b0a276855ff0f4b695d5
atm-lena <lena.papazian@atm-consulting.fr>
2024-01-17 15:17:20 +0100

    Define stock virtual by warehouse in replenish

--- htdocs/product/stock/replenish.php
+++ htdocs/product/stock/replenish.php
@@ -826,6 +826,7 @@ while ($i < ($limit ? min($num, $limit) : $num)) {
 		if ($usevirtualstock) {
 			// If option to increase/decrease is not on an object validation, virtual stock may differs from physical stock.
 			$stock = $prod->stock_theorique;
+			$stockwarehouse =  $prod->stock_warehouse[$fk_entrepot]->virtual;
 			//TODO $stockwarehouse = $prod->stock_warehouse[$fk_entrepot]->;
 		} else {
 			$stock = $prod->stock_reel;
