38ee4e0ef003bcdc519d0c9526f39868c353d4b3
Lamrani Abdel <lamrani002@gmail.com>
2024-01-17 12:33:33 +0100

    fix params in function

--- htdocs/core/class/html.formmail.class.php
+++ htdocs/core/class/html.formmail.class.php
@@ -890,7 +890,7 @@ class FormMail extends Form
 			//input prompt AI
 			require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 			if (isModEnabled('ai') && $apiKey = dolibarr_get_const($this->db, 'AI_CHATGPT_API_KEY', 0)) {
-				$out .= $this->getHtmlForInstruction($arraydefaultmessage, $helpforsubstitution);
+				$out .= $this->getHtmlForInstruction();
 			}
 			// Message
 			if (!empty($this->withbody)) {
