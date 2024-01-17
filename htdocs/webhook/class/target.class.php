6ba594efd8d03af3da4f7a19e9587b05cbc7aeda
Frédéric FRANCE <frederic34@users.noreply.github.com>
2024-01-17 18:38:06 +0100

    fix phpstan in target.class.php

--- htdocs/webhook/class/target.class.php
+++ htdocs/webhook/class/target.class.php
@@ -223,7 +223,7 @@ class Target extends CommonObject
 	public function create(User $user, $notrigger = 0)
 	{
 		$resultcreate = $this->createCommon($user, $notrigger);
-		$this->ref = $this->id;
+		$this->ref = (string) $this->id;
 
 		if ($resultcreate <= 0) {
 			return $resultcreate;
