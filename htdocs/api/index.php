b1b16677aea4823ff6952c27b3bfc78af90c142d
Mathieu "rid" Soula <msoula@gmx.com>
2024-01-18 10:36:10 +0100

    Fix indentation.

--- htdocs/api/index.php
+++ htdocs/api/index.php
@@ -447,7 +447,7 @@ require_once DOL_DOCUMENT_ROOT.'/includes/restler/framework/Luracast/Restler/Sco
 $apiMethodInfo = &$api->r->apiMethodInfo;
 $terminateCall = '_terminate_' . $apiMethodInfo->methodName . '_' . $api->r->responseFormat->getExtension();
 if (method_exists($apiMethodInfo->className, $terminateCall)) {
-  call_user_func(array(Scope::get($apiMethodInfo->className), $terminateCall), $responsedata);
+	call_user_func(array(Scope::get($apiMethodInfo->className), $terminateCall), $responsedata);
 }
 
 //session_destroy();
