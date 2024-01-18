3e907a00a9f4dac0826707d0191e2b2fe105643c
Mathieu "rid" Soula <msoula@gmx.com>
2024-01-18 10:18:03 +0100

    Revert changes
    Removes termination callback

--- htdocs/includes/restler/framework/Luracast/Restler/Restler.php
+++ htdocs/includes/restler/framework/Luracast/Restler/Restler.php
@@ -1686,27 +1686,4 @@ class Restler extends EventDispatcher
             ), $this->responseData);
         }
     }
-	
-    /**
-     * terminate call
-     *
-     * Call _terminate_{methodName}_{extension} if exists with the composed and
-     * serialized (applying the reponse format) response data
-     *
-     * @example _terminate_post_json
-     */
-    public function terminate()
-    {
-        $o = & $this->apiMethodInfo;
-        $terminateCall = '_terminate_' . $o->methodName . '_' .
-            $this->responseFormat->getExtension();
-        if (method_exists($o->className, $terminateCall)) {
-            $this->dispatch('terminateCall');
-            $this->responseData = call_user_func(array(
-                Scope::get($o->className),
-                $terminateCall
-            ), $this->responseData);
-        }
-    }
-
 }
