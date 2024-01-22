6f0a91856f70b672ffecb543071e00902908fcad
atm-lena <lena.papazian@atm-consulting.fr>
2024-01-22 12:42:34 +0100

    Missing Translations

--- htdocs/fourn/class/paiementfourn.class.php
+++ htdocs/fourn/class/paiementfourn.class.php
@@ -196,7 +196,7 @@ class PaiementFourn extends Paiement
 			// Add controls of input validity
 			if ($value_converted === false) {
 				// We failed to find the conversion for one invoice
-				$this->error = 'FailedToFoundTheConversionRateForInvoice';
+                $this->error = $langs->trans('FailedToFoundTheConversionRateForInvoice');
 				return -1;
 			}
 			if (empty($currencyofpayment)) {
