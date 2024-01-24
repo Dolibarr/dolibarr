25d19b1521331e8e88c5689ed7a3108bf7ae8aa3
thibdrev <thibault.drevet@gmail.com>
2024-01-24 21:13:22 +0100

    qual: phpstan for htdocs/compta/paiement_vat.php
    htdocs/compta/paiement_vat.php	101	Property PaymentVAT::$chid (int) does not accept array|string.

--- htdocs/compta/paiement_vat.php
+++ htdocs/compta/paiement_vat.php
@@ -32,7 +32,7 @@ require_once DOL_DOCUMENT_ROOT.'/compta/bank/class/account.class.php';
 // Load translation files required by the page
 $langs->loadLangs(array("banks", "bills"));
 
-$chid = GETPOST("id", 'int');
+$chid = GETPOSTINT("id");
 $action = GETPOST('action', 'alpha');
 $cancel = GETPOST('cancel');
 
@@ -190,7 +190,7 @@ if ($action == 'create') {
 
 	$sql = "SELECT sum(p.amount) as total";
 	$sql .= " FROM ".MAIN_DB_PREFIX."payment_vat as p";
-	$sql .= " WHERE p.fk_tva = ".((int) $chid);
+	$sql .= " WHERE p.fk_tva = ".$chid;
 	$resql = $db->query($sql);
 	if ($resql) {
 		$obj = $db->fetch_object($resql);
