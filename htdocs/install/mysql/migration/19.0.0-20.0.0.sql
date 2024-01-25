c2345a48c3a915badb7675ca363e1cab6b271328
Christophe Battarel <christophe@altairis.fr>
2024-01-25 10:29:18 +0100

    use alter table rename instead of rename table

--- htdocs/install/mysql/migration/19.0.0-20.0.0.sql
+++ htdocs/install/mysql/migration/19.0.0-20.0.0.sql
@@ -207,5 +207,5 @@ ALTER TABLE llx_product DROP COLUMN onportal;
 
 ALTER TABLE llx_knowledgemanagement_knowledgerecord MODIFY COLUMN answer longtext;
 
-RENAME TABLE llx_commande_fournisseur_dispatch_extrafields TO llx_receptiondet_batch_extrafields;
-RENAME TABLE llx_commande_fournisseur_dispatch TO llx_receptiondet_batch;
+ALTER TABLE llx_commande_fournisseur_dispatch_extrafields RENAME llx_receptiondet_batch_extrafields;
+ALTER TABLE llx_commande_fournisseur_dispatch RENAME llx_receptiondet_batch;
