deb9176e21148060829e4ebb7042c978b155b973
Hystepik <lmarcouiller@nltechno.com>
2024-01-29 15:33:37 +0100

    Close #27777 : new massaction subscription creation

--- htdocs/adherents/list.php
+++ htdocs/adherents/list.php
@@ -335,6 +335,40 @@ if (empty($reshook)) {
 		}
 	}
 
+	// Create external user
+	if ($action == 'createsubscription' && $confirm == "yes" && $user->hasRight('adherent', 'creer')) {
+		$tmpmember = new Adherent($db);
+		$adht = new AdherentType($db);
+		$error = 0;
+		$nbcreated = 0;
+		$db->begin();
+
+		foreach ($toselect as $id) {
+			$now = dol_now();
+			$tmpmember->fetch($id);
+			$res = $adht->fetch($tmpmember->typeid);
+			if ($res > 0) {
+				$amount = $adht->amount;
+				$result = $tmpmember->subscription($now, $amount);
+				if ($result < 0) {
+					$error++;
+				} else {
+					$nbcreated++;
+				}
+			} else {
+				$error++;
+			}
+		}
+
+		if (!$error) {
+			setEventMessages($langs->trans("XSubsriptionCreated", $nbcreated), null, 'mesgs');
+			$db->commit();
+		} else {
+			setEventMessages($langs->trans("XSubsriptionError", $error), null, 'mesgs');
+			$db->rollback();
+		}
+	}
+
 	// Mass actions
 	$objectclass = 'Adherent';
 	$objectlabel = 'Members';
@@ -706,6 +740,9 @@ if (isModEnabled('category') && $user->hasRight('adherent', 'creer')) {
 if ($user->hasRight('adherent', 'creer') && $user->hasRight('user', 'user', 'creer')) {
 	$arrayofmassactions['createexternaluser'] = img_picto('', 'user', 'class="pictofixedwidth"').$langs->trans("CreateExternalUser");
 }
+if ($user->hasRight('adherent', 'creer')) {
+	$arrayofmassactions['createsubscription'] = $langs->trans("CreateSubscription");
+}
 if (GETPOST('nomassaction', 'int') || in_array($massaction, array('presend', 'predelete', 'preaffecttag'))) {
 	$arrayofmassactions = array();
 }
@@ -740,6 +777,9 @@ $topicmail = "Information";
 $modelmail = "member";
 $objecttmp = new Adherent($db);
 $trackid = 'mem'.$object->id;
+if ($massaction == 'createsubscription') {
+	print $form->formconfirm($_SERVER["PHP_SELF"], $langs->trans("ConfirmMassSubsriptionCreation"), $langs->trans("ConfirmMassSubsriptionCreationQuestion", count($toselect)), "createsubscription", $formquestion, '', 0, 200, 500, 1);
+}
 include DOL_DOCUMENT_ROOT.'/core/tpl/massactions_pre.tpl.php';
 
 if ($search_all) {
