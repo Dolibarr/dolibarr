20a2e8e38a3dce1a7c4cd7f35383a9eb95f677f3
Laurent Destailleur <eldy@destailleur.fr>
2024-02-01 13:43:12 +0100

    Clean code

--- htdocs/adherents/class/adherent_type.class.php
+++ htdocs/adherents/class/adherent_type.class.php
@@ -199,7 +199,7 @@ class AdherentType extends CommonObject
 	 */
 	public function setMultiLangs($user)
 	{
-		global $conf, $langs;
+		global $langs;
 
 		$langs_available = $langs->get_available_languages(DOL_DOCUMENT_ROOT, 0, 2);
 		$current_lang = $langs->getDefaultLang();
@@ -316,7 +316,7 @@ class AdherentType extends CommonObject
 	 */
 	public function create($user, $notrigger = 0)
 	{
-		global $langs, $conf;
+		global $conf;
 
 		$error = 0;
 
@@ -379,7 +379,7 @@ class AdherentType extends CommonObject
 	 */
 	public function update($user, $notrigger = 0)
 	{
-		global $langs, $conf, $hookmanager;
+		global $langs;
 
 		$error = 0;
 
@@ -416,8 +416,6 @@ class AdherentType extends CommonObject
 					return -2;
 				}
 			}
-
-			$action = 'update';
 
 			// Actions on extra fields
 			if (!$error) {
@@ -492,8 +490,6 @@ class AdherentType extends CommonObject
 	 */
 	public function fetch($rowid)
 	{
-		global $langs, $conf;
-
 		$sql = "SELECT d.rowid, d.libelle as label, d.morphy, d.statut as status, d.duration, d.subscription, d.amount, d.caneditamount, d.mail_valid, d.note as note_public, d.vote";
 		$sql .= " FROM ".MAIN_DB_PREFIX."adherent_type as d";
 		$sql .= " WHERE d.rowid = ".(int) $rowid;
@@ -547,7 +543,7 @@ class AdherentType extends CommonObject
 	public function liste_array($status = -1)
 	{
 		// phpcs:enable
-		global $conf, $langs;
+		global $langs;
 
 		$adherenttypes = array();
 
@@ -585,8 +581,6 @@ class AdherentType extends CommonObject
 	 */
 	public function amountByType($status = null)
 	{
-		global $conf, $langs;
-
 		$amountbytype = array();
 
 		$sql = "SELECT rowid, amount";
@@ -627,8 +621,6 @@ class AdherentType extends CommonObject
 	 */
 	public function listMembersForMemberType($excludefilter = '', $mode = 0)
 	{
-		global $conf, $user;
-
 		$ret = array();
 
 		$sql = "SELECT a.rowid";
@@ -696,7 +688,7 @@ class AdherentType extends CommonObject
 	 */
 	public function getTooltipContentArray($params)
 	{
-		global $conf, $langs, $user;
+		global $langs;
 
 		$langs->load('members');
 
@@ -734,8 +726,6 @@ class AdherentType extends CommonObject
 	 */
 	public function getNomUrl($withpicto = 0, $maxlen = 0, $notooltip = 0, $morecss = '', $save_lastsearch_value = -1)
 	{
-		global $langs;
-
 		$result = '';
 		$option = '';
 
@@ -838,7 +828,6 @@ class AdherentType extends CommonObject
 	public function _load_ldap_dn($info, $mode = 0)
 	{
 		// phpcs:enable
-		global $conf;
 		$dn = '';
 		if ($mode == 0) {
 			$dn = getDolGlobalString('LDAP_KEY_MEMBERS_TYPES') . "=".$info[getDolGlobalString('LDAP_KEY_MEMBERS_TYPES')]."," . getDolGlobalString('LDAP_MEMBER_TYPE_DN');
@@ -863,8 +852,6 @@ class AdherentType extends CommonObject
 	public function _load_ldap_info()
 	{
 		// phpcs:enable
-		global $conf, $langs;
-
 		$info = array();
 
 		// Object classes
