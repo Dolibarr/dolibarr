79f13f3e79e2461a0ee295884cdef10751643ad2
jpb <jean-pascal.boudet@atm-consulting>
2024-02-06 15:58:44 +0100

    Spelling correction in a comment for the word "retrieve"

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -8037,7 +8037,7 @@ class Form
 		$objecttmp = null;
 		$InfoFieldList = array();
 
-		if ($objectfield && empty($objectdesc)) {	// We must retreive the objectdesc if empty from the field or extrafield
+		if ($objectfield && empty($objectdesc)) {	// We must retrieve the objectdesc, if empty, from the field or extrafield
 			// Example: $objectfield = 'product:options_package'
 			$tmparray = explode(':', $objectfield);
 			$objectdesc = '';
