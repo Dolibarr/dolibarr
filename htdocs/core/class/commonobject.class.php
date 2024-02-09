631779501627f9f8c59cef69bdb29eb4ebce8cd2
thibdrev <thibault.drevet@gmail.com>
2024-02-09 17:39:57 +0100

    Update commonobject.class.php

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -121,8 +121,8 @@ abstract class CommonObject
 	public $table_element_line = '';
 
 	/**
- 	 * Does this object supports the multicompany module ?
- 	 *
+	 * Does this object supports the multicompany module ?
+	 *
 	 * @var int|string 		0 if no test on entity, 1 if test with field entity, 2 if test with link by fk_soc, 'field@table' if test with link by field@table
 	 */
 	public $ismultientitymanaged;
