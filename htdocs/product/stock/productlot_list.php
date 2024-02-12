f4d138b9e1691efda43fa651857c6bac1f1ed659
MDW <mdeweerd@users.noreply.github.com>
2024-02-12 23:36:02 +0100

    Qual: Apply automatic phan fixes (deprecations, unneeded imports)
    # Qual: Apply automatic phan fixes (deprecations, unneeded imports)
    
    This applies automatic fixes by phan for deprecated functions, unneeded imports).

--- htdocs/product/stock/productlot_list.php
+++ htdocs/product/stock/productlot_list.php
@@ -448,7 +448,7 @@ if ($search_all) {
 		$setupstring .= $key."=".$val.";";
 	}
 	print '<!-- Search done like if PRODUCT_QUICKSEARCH_ON_FIELDS = '.$setupstring.' -->'."\n";
-	print '<div class="divsearchfieldfilter">'.$langs->trans("FilterOnInto", $search_all).join(', ', $fieldstosearchall).'</div>'."\n";
+	print '<div class="divsearchfieldfilter">'.$langs->trans("FilterOnInto", $search_all).implode(', ', $fieldstosearchall).'</div>'."\n";
 }
 
 // Filter on categories
