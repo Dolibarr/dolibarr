87c90e1bfb4e12277036369dad712d14ccbc046c
MDW <mdeweerd@users.noreply.github.com>
2024-02-12 23:08:27 +0100

    Qual: Apply automatic phan fixes (deprecations, unneeded imports)
    # Qual: Apply automatic phan fixes (deprecations, unneeded imports)
    
    This applies automatic fixes by phan for deprecated functions, unneeded imports).

--- htdocs/webhook/class/target.class.php
+++ htdocs/webhook/class/target.class.php
@@ -429,7 +429,7 @@ class Target extends CommonObject
 			return $records;
 		} else {
 			$this->errors[] = 'Error '.$this->db->lasterror();
-			dol_syslog(__METHOD__.' '.join(',', $this->errors), LOG_ERR);
+			dol_syslog(__METHOD__.' '.implode(',', $this->errors), LOG_ERR);
 
 			return -1;
 		}
