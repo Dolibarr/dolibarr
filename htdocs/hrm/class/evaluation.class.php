463c56958a5f4357f06a35b2eb523b7c503ba59b
MDW <mdeweerd@users.noreply.github.com>
2024-02-12 23:34:18 +0100

    Qual: Apply automatic phan fixes (deprecations, unneeded imports)
    # Qual: Apply automatic phan fixes (deprecations, unneeded imports)
    
    This applies automatic fixes by phan for deprecated functions, unneeded imports).

--- htdocs/hrm/class/evaluation.class.php
+++ htdocs/hrm/class/evaluation.class.php
@@ -457,7 +457,7 @@ class Evaluation extends CommonObject
 			return $records;
 		} else {
 			$this->errors[] = 'Error '.$this->db->lasterror();
-			dol_syslog(__METHOD__.' '.join(',', $this->errors), LOG_ERR);
+			dol_syslog(__METHOD__.' '.implode(',', $this->errors), LOG_ERR);
 
 			return -1;
 		}
