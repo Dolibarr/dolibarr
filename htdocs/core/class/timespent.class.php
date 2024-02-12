c1d2b3b70e9576726ee02d32e5f8b5845d64bdf0
MDW <mdeweerd@users.noreply.github.com>
2024-02-12 23:31:56 +0100

    Qual: Apply automatic phan fixes (deprecations, unneeded imports)
    # Qual: Apply automatic phan fixes (deprecations, unneeded imports)
    
    This applies automatic fixes by phan for deprecated functions, unneeded imports).

--- htdocs/core/class/timespent.class.php
+++ htdocs/core/class/timespent.class.php
@@ -393,7 +393,7 @@ class TimeSpent extends CommonObject
 			return $records;
 		} else {
 			$this->errors[] = 'Error '.$this->db->lasterror();
-			dol_syslog(__METHOD__.' '.join(',', $this->errors), LOG_ERR);
+			dol_syslog(__METHOD__.' '.implode(',', $this->errors), LOG_ERR);
 
 			return -1;
 		}
