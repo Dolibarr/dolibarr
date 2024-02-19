c696c46724a55c62cd0b8183688a1a8553362e4a
MDW <mdeweerd@users.noreply.github.com>
2024-02-19 12:06:16 +0100

    Qual: Improve test messages to help locate errors.
    # Qual: Improve test messages to help locate errors.
    
    Included a description of the test in the failing assertions to
    help locate the error.

--- test/phpunit/RestAPIUserTest.php
+++ test/phpunit/RestAPIUserTest.php
@@ -87,6 +87,7 @@ class RestAPIUserTest extends CommonClassTest
 
 		$this->api_url = DOL_MAIN_URL_ROOT.'/api/index.php';
 
+		$test = "API Test Setup - ";
 		$login = 'admin';
 		$password = 'admin';
 		$url = $this->api_url.'/login?login='.$login.'&password='.$password;
@@ -95,11 +96,11 @@ class RestAPIUserTest extends CommonClassTest
 		$result = getURLContent($url, 'GET', '', 1, array(), array('http', 'https'), 2);
 		print __METHOD__." result = ".var_export($result, true)."\n";
 		print __METHOD__." curl_error_no: ".$result['curl_error_no']."\n";
-		$this->assertEquals($result['curl_error_no'], '');
+		$this->assertEquals('', $result['curl_error_no'], "$test Should not have a curl error");
 		$object = json_decode($result['content'], true);	// If success content is just an id, if not an array
 
-		$this->assertNotNull($object, "Parsing of json result must not be null");
-		$this->assertNotEquals(500, (empty($object['error']['code']) ? 0 : $object['error']['code']), 'Error'.(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
+		$this->assertNotNull($object, "$test Parsing of JSON result must not be null");
+		$this->assertNotEquals(500, (empty($object['error']['code']) ? 0 : $object['error']['code']), "$test Error".(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
 		$this->assertEquals('200', $object['success']['code']);
 
 		$this->api_key = $object['success']['token'];
@@ -117,6 +118,7 @@ class RestAPIUserTest extends CommonClassTest
 	{
 		global $conf,$user,$langs,$db;
 
+		$test = "Invalid User -";
 		$url = $this->api_url.'/users/123456789?api_key='.$this->api_key;
 		//$addheaders=array('Content-Type: application/json');
 
@@ -124,20 +126,21 @@ class RestAPIUserTest extends CommonClassTest
 		$result = getURLContent($url, 'GET', '', 1, array(), array('http', 'https'), 2);
 		//print __METHOD__." result for get on unexisting user: ".var_export($result, true)."\n";
 		print __METHOD__." curl_error_no: ".$result['curl_error_no']."\n";
-		$this->assertEquals($result['curl_error_no'], '');
+		$this->assertEquals('', $result['curl_error_no'], "$test Should not have a curl error");
 		$object = json_decode($result['content'], true);
-		$this->assertNotNull($object, "Parsing of json result must not be null");
-		$this->assertEquals(404, (empty($object['error']['code']) ? 0 : $object['error']['code']), 'Error code is not 404');
+		$this->assertNotNull($object, "$test Parsing of JSON result must not be null");
+		$this->assertEquals(404, (empty($object['error']['code']) ? 0 : $object['error']['code']), "$test Error code is not 404");
 
+		$test = "Existing User -";
 		$url = $this->api_url.'/users/1?api_key='.$this->api_key;
 
 		print __METHOD__." Request GET url=".$url."\n";
 		$result = getURLContent($url, 'GET', '', 1, array(), array('http', 'https'), 2);
-		print __METHOD__." result for get on an existing user: ".var_export($result, true)."\n";
+		print __METHOD__." $test result for get: ".var_export($result, true)."\n";
 		print __METHOD__." curl_error_no: ".$result['curl_error_no']."\n";
-		$this->assertEquals($result['curl_error_no'], '');
+		$this->assertEquals('', $result['curl_error_no'], "$test should have no error");
 		$object = json_decode($result['content'], true);
-		$this->assertNotNull($object, "Parsing of json result must not be null");
+		$this->assertNotNull($object, "$test Parsing of JSON result must not be null");
 		$this->assertEquals(1, $object['statut']);
 
 		return $object['id'];
@@ -154,6 +157,7 @@ class RestAPIUserTest extends CommonClassTest
 	public function testRestCreateUser()
 	{
 		// attempt to create without mandatory fields :
+		$test = "Create User Missing Fields -";
 		$url = $this->api_url.'/users?api_key='.$this->api_key;
 		$addheaders = array('Content-Type: application/json');
 
@@ -168,12 +172,13 @@ class RestAPIUserTest extends CommonClassTest
 		$result = getURLContent($url, 'POST', $body, 1, $addheaders, array('http', 'https'), 2);
 		//print __METHOD__." Result for creating incomplete user".var_export($result, true)."\n";
 		print __METHOD__." curl_error_no: ".$result['curl_error_no']."\n";
-		$this->assertEquals($result['curl_error_no'], '');
+		$this->assertEquals('', $result['curl_error_no'], "$test Should not have a curl error");
 		$object = json_decode($result['content'], true);
-		$this->assertNotNull($object, "Parsing of json result must no be null");
-		$this->assertEquals(500, (empty($object['error']['code']) ? 0 : $object['error']['code']), 'Error'.(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
+		$this->assertNotNull($object, "$test Parsing of JSON result must not be null");
+		$this->assertEquals(500, (empty($object['error']['code']) ? 0 : $object['error']['code']), "$test Error".(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
 
 		// create regular user
+		$test = "Create Regular User -";
 		unset($result);
 		$bodyobj = array(
 			"login" => "testRestLogin".mt_rand(),
@@ -186,21 +191,22 @@ class RestAPIUserTest extends CommonClassTest
 		$result = getURLContent($url, 'POST', $body, 1, $addheaders, array('http', 'https'), 2);
 		print __METHOD__." result code for creating non existing user = ".var_export($result, true)."\n";
 		print __METHOD__." curl_error_no: ".$result['curl_error_no']."\n";
-		$this->assertEquals($result['curl_error_no'], '');
+		$this->assertEquals('', $result['curl_error_no'], "$test Should not have a curl error");
 		$object = json_decode($result['content'], true);	// If success content is just an id, if not an array
 
-		$this->assertNotNull($object, "Parsing of json result must no be null");
-		$this->assertNotEquals(500, ((is_scalar($object) || empty($object['error']) || empty($object['error']['code'])) ? 0 : $object['error']['code']), 'Error'.(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
+		$this->assertNotNull($object, "$test Parsing of JSON result must not be null");
+		$this->assertNotEquals(500, ((is_scalar($object) || empty($object['error']) || empty($object['error']['code'])) ? 0 : $object['error']['code']), "$test Error".(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
 		$this->assertGreaterThan(0, $object, 'ID returned is no > 0');
 
 		// attempt to create duplicated user
+		$test = "Create Duplicate User -";
 		print __METHOD__." Request POST url=".$url."\n";
 		$result = getURLContent($url, 'POST', $body, 1, $addheaders, array('http', 'https'), 2);
 		//print __METHOD__." Result for creating duplicate user".var_export($result, true)."\n";
 		print __METHOD__." curl_error_no: ".$result['curl_error_no']."\n";
-		$this->assertEquals($result['curl_error_no'], '');
+		$this->assertEquals('', $result['curl_error_no'], "$test Should not have a curl error");
 		$object = json_decode($result['content'], true);
-		$this->assertNotNull($object, "Parsing of json result must no be null");
-		$this->assertEquals(500, (empty($object['error']['code']) ? 0 : $object['error']['code']), 'Error'.(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
+		$this->assertNotNull($object, "$test Parsing of JSON result must not be null");
+		$this->assertEquals(500, (empty($object['error']['code']) ? 0 : $object['error']['code']), "$test Error".(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
 	}
 }
