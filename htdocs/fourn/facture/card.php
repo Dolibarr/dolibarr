d047ad4945b5d7cb47b656284680fef6a3d145c7
William Mead <william.mead@manchenumerique.fr>
2024-02-21 14:13:55 +0100

    Added contributor details

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -12,6 +12,7 @@
  * Copyright (C) 2018-2023  Frédéric France         <frederic.france@netlogic.fr>
  * Copyright (C) 2019       Ferran Marcet	        <fmarcet@2byte.es>
  * Copyright (C) 2022       Gauthier VERDOL         <gauthier.verdol@atm-consulting.fr>
+ * Copyright (C) 2024		William Mead			<william.mead@manchenumerique.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
