adefd41a5e39cb00e0bd453095377c5cc101713f
thibdrev <thibault.drevet@gmail.com>
2024-02-22 16:33:31 +0100

    Update product.class.php

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -501,10 +501,10 @@ class Product extends CommonObject
 	public $ref_supplier;
 
 	/**
-	 * Unit code ('km', 'm', 'l', 'p', ...)
-	 *
-	 * @var string
-	 */
+ 	 * @var int|null                ID of the unit of measurement (rowid in llx_c_units table)
+ 	 * @see measuringUnitString()
+ 	 * @see getLabelOfUnit()
+ 	 */
 	public $fk_unit;
 
 	/**
