42d1ffd89b0fe10bfdba435f6b72de06c26f9ebd
thibdrev <thibault.drevet@gmail.com>
2024-02-22 17:42:10 +0100

    phpcs

--- htdocs/core/class/commonobjectline.class.php
+++ htdocs/core/class/commonobjectline.class.php
@@ -51,9 +51,9 @@ abstract class CommonObjectLine extends CommonObject
 	public $picto = 'line';
 
 	/**
- 	 * @var int|null                ID of the unit of measurement (rowid in llx_c_units table)
- 	 * @see measuringUnitString()
- 	 * @see getLabelOfUnit()
+	 * @var int|null                ID of the unit of measurement (rowid in llx_c_units table)
+	 * @see measuringUnitString()
+	 * @see getLabelOfUnit()
  	 */
 	public $fk_unit;
 
