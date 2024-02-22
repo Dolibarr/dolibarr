5a915572a2c3a861cdf482f7f2a72ad921936895
thibdrev <thibault.drevet@gmail.com>
2024-02-22 16:32:33 +0100

    Update commonobjectline.class.php

--- htdocs/core/class/commonobjectline.class.php
+++ htdocs/core/class/commonobjectline.class.php
@@ -51,9 +51,10 @@ abstract class CommonObjectLine extends CommonObject
 	public $picto = 'line';
 
 	/**
-	 * Product/service unit code ('km', 'm', 'p', ...)
-	 * @var string
-	 */
+ 	 * @var int|null                ID of the unit of measurement (rowid in llx_c_units table)
+ 	 * @see measuringUnitString()
+ 	 * @see getLabelOfUnit()
+ 	 */
 	public $fk_unit;
 
 	public $date_debut_prevue;
