4897d1eb97800c7ab12cf9044177111ced1e14bf
Laurent Destailleur <eldy@destailleur.fr>
2024-02-25 17:20:28 +0100

    Fix warning

--- htdocs/workstation/class/workstation.class.php
+++ htdocs/workstation/class/workstation.class.php
@@ -397,7 +397,7 @@ class Workstation extends CommonObject
 			foreach ($filter as $key => $value) {
 				if ($key == 't.rowid') {
 					$sqlwhere[] = $key." = ".((int) $value);
-				} elseif (in_array($this->fields[$key]['type'], array('date', 'datetime', 'timestamp'))) {
+				} elseif (property_exists($this->fields, $key) && in_array($this->fields[$key]['type'], array('date', 'datetime', 'timestamp'))) {
 					$sqlwhere[] = $key." = '".$this->db->idate($value)."'";
 				} elseif ($key == 'customsql') {
 					$sqlwhere[] = $value;
