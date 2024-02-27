1f887977e1b6d7439b95bbad5bc3e0cd04c4cf0c
Frédéric France <frederic.france@free.fr>
2024-02-27 18:41:00 +0100

    new test

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -4473,11 +4473,6 @@ function getPictoForType($key)
 		'checkbox' => 'check-square',
 		'chkbxlst' => 'check-square',
 		'link' => 'link',
-		'icon' => "question",
-		'point' => "country",
-		'multipts' => 'country',
-		'linestrg' => "country",
-		'polygon' => "country",
 		'separate' => 'minus'
 	);
 
