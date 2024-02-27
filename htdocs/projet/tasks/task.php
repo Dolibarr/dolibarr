8bc918ac085a3c6373c8c86db2066a85b09550a5
MDW <mdeweerd@users.noreply.github.com>
2024-02-27 13:18:15 +0100

    Fix: Replace deprecated modulename with new in isModEnabled
    # Fix: Replace deprecated modulename with new in isModEnabled
    
    Replacement of old module names performed with dedicated detection
    and fixer in phan.

--- htdocs/projet/tasks/task.php
+++ htdocs/projet/tasks/task.php
@@ -367,7 +367,7 @@ if ($id > 0 || !empty($ref)) {
 		print '</td></tr>';
 
 		// Categories
-		if (isModEnabled('categorie')) {
+		if (isModEnabled('category')) {
 			print '<tr><td class="valignmiddle">'.$langs->trans("Categories").'</td><td>';
 			print $form->showCategories($projectstatic->id, 'project', 1);
 			print "</td></tr>";
