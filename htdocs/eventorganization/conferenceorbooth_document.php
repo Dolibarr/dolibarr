0c2d2eef1d1ea39b89c99637cdc5be061fffddfb
MDW <mdeweerd@users.noreply.github.com>
2024-02-27 13:11:46 +0100

    Fix: Replace deprecated modulename with new in isModEnabled
    # Fix: Replace deprecated modulename with new in isModEnabled
    
    Replacement of old module names performed with dedicated detection
    and fixer in phan.

--- htdocs/eventorganization/conferenceorbooth_document.php
+++ htdocs/eventorganization/conferenceorbooth_document.php
@@ -260,7 +260,7 @@ if (!empty($withproject)) {
 	print '</td></tr>';
 
 	// Categories
-	if (isModEnabled('categorie')) {
+	if (isModEnabled('category')) {
 		print '<tr><td class="valignmiddle">'.$langs->trans("Categories").'</td><td>';
 		print $form->showCategories($projectstatic->id, Categorie::TYPE_PROJECT, 1);
 		print "</td></tr>";
