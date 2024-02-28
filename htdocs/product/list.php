a1462d6f29d61e9ad937f95519040c263d1f007a
Mohamed DAOUD <daoud.mouhamed@gmail.com>
2024-02-28 18:40:53 +0100

    Fix GETPOST to GETPOSTINT issue (#28506)
    * Fix GETPOST to GETPOSTINT issue
    
    * Update list.php
    
    * Update list.php
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/product/list.php
+++ htdocs/product/list.php
@@ -90,19 +90,19 @@ $catid = GETPOSTINT('catid');
 if (!empty($catid) && empty($searchCategoryProductList)) {
 	$searchCategoryProductList = array($catid);
 }
-$search_tosell = GETPOSTINT("search_tosell");
-$search_tobuy = GETPOSTINT("search_tobuy");
+$search_tosell = GETPOST("search_tosell");
+$search_tobuy = GETPOST("search_tobuy");
 $search_country = GETPOSTINT("search_country");
 $search_state = GETPOSTINT("state_id");
 $fourn_id = GETPOSTINT("fourn_id");
-$search_tobatch = GETPOSTINT("search_tobatch");
+$search_tobatch = GETPOST("search_tobatch");
 $search_accountancy_code_sell = GETPOST("search_accountancy_code_sell", 'alpha');
 $search_accountancy_code_sell_intra = GETPOST("search_accountancy_code_sell_intra", 'alpha');
 $search_accountancy_code_sell_export = GETPOST("search_accountancy_code_sell_export", 'alpha');
 $search_accountancy_code_buy = GETPOST("search_accountancy_code_buy", 'alpha');
 $search_accountancy_code_buy_intra = GETPOST("search_accountancy_code_buy_intra", 'alpha');
 $search_accountancy_code_buy_export = GETPOST("search_accountancy_code_buy_export", 'alpha');
-$search_finished = GETPOSTINT("search_finished");
+$search_finished = GETPOST("search_finished");
 $search_units = GETPOSTINT('search_units');
 $optioncss = GETPOST('optioncss', 'alpha');
 $type = GETPOSTINT("type");
