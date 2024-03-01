c8b9de808561e770b455d91f750a3ab777d0d161
atm-adrien <adrien.raze@atm-consulting.fr>
2024-03-01 14:18:32 +0100

    FIX : Adding thez dependencies list feature for select fields

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -7329,6 +7329,7 @@ abstract class CommonObject
 				}
 				$out .= '<option value="'.$keyb.'"';
 				$out .= (((string) $value == (string) $keyb) ? ' selected' : '');
+				if(!empty($parent)) $isDependList =1;
 				$out .= (!empty($parent) ? ' parent="'.$parent.'"' : '');
 				$out .= '>'.$valb.'</option>';
 			}
