13011c74e04b7ea61922508327152fe844c2b49d
Frédéric France <frederic.france@free.fr>
2024-03-02 10:37:11 +0100

    fix phpstan property does not accept string

--- htdocs/stripe/class/stripe.class.php
+++ htdocs/stripe/class/stripe.class.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2018-2021 	Thibault FOUCART       <support@ptibogxiv.net>
+ * Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -55,6 +56,9 @@ class Stripe extends CommonObject
 	 */
 	public $entity;
 
+	/**
+	 * @var string
+	 */
 	public $statut;
 
 	public $type;
