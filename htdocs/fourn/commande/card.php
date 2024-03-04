55c0efae3fb5f8d387eec9674fa5c784a3caa9d2
noec764 <58433943+noec764@users.noreply.github.com>
2024-03-04 18:26:38 +0100

    FIX: Special code is now transmitted by args only in order supplier (#28619)
    Co-authored-by: Noé <noe@scopen.fr>

--- htdocs/fourn/commande/card.php
+++ htdocs/fourn/commande/card.php
@@ -1344,8 +1344,6 @@ if (empty($reshook)) {
 									$tva_tx = get_default_tva($soc, $mysoc, $lines[$i]->fk_product, $product_fourn_price_id);
 								}
 
-								$object->special_code = $lines[$i]->special_code;
-
 								$result = $object->addline(
 									$desc,
 									$lines[$i]->subprice,
@@ -1368,7 +1366,9 @@ if (empty($reshook)) {
 									$lines[$i]->fk_unit,
 									0,
 									$element,
-									!empty($lines[$i]->id) ? $lines[$i]->id : $lines[$i]->rowid
+									!empty($lines[$i]->id) ? $lines[$i]->id : $lines[$i]->rowid,
+									-1,
+									$lines[$i]->special_code
 								);
 
 								if ($result < 0) {
