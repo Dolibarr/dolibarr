b5f0b6ae17f26b40bc6269f4bd086ef83dc26f43
MDW <mdeweerd@users.noreply.github.com>
2024-03-12 16:16:15 +0100

    Qual: Fix PhanTypeExpectedObjectOrClassName by testing on classname
    # Qual: Fix PhanTypeExpectedObjectOrClassName by testing on classname
    
    PhanTypeExpectedObjectOrClassName should be fixed by testing that the classname is not
    empty.

--- htdocs/admin/supplier_order.php
+++ htdocs/admin/supplier_order.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2003-2007 Rodolphe Quiedeville    <rodolphe@quiedeville.org>
+ * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
 * Copyright (C) 2004-2011 Laurent Destailleur     <eldy@users.sourceforge.net>
 * Copyright (C) 2005-2011 Regis Houssin           <regis.houssin@inodbox.com>
 * Copyright (C) 2004      Sebastien Di Cintio     <sdicintio@ressource-toi.org>
@@ -92,18 +93,16 @@ if ($action == 'specimen') {  // For orders
 	// Search template files
 	$file = '';
 	$classname = '';
-	$filefound = 0;
 	$dirmodels = array_merge(array('/'), (array) $conf->modules_parts['models']);
 	foreach ($dirmodels as $reldir) {
 		$file = dol_buildpath($reldir."core/modules/supplier_order/doc/pdf_".$modele.".modules.php", 0);
 		if (file_exists($file)) {
-			$filefound = 1;
 			$classname = "pdf_".$modele;
 			break;
 		}
 	}
 
-	if ($filefound) {
+	if ($classname !== '') {
 		require_once $file;
 
 		$module = new $classname($db, $commande);
