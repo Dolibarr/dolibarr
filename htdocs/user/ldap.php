06a593a2c3aee24940e8f733ee15f17646a8a920
William Mead <william.mead@manchenumerique.fr>
2024-03-13 14:36:04 +0100

    Added contributor details

--- htdocs/user/ldap.php
+++ htdocs/user/ldap.php
@@ -1,6 +1,7 @@
 <?php
-/* Copyright (C) 2006-2012 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2006-2021 Regis Houssin        <regis.houssin@inodbox.com>
+/* Copyright (C) 2006-2012	Laurent Destailleur		<eldy@users.sourceforge.net>
+ * Copyright (C) 2006-2021	Regis Houssin			<regis.houssin@inodbox.com>
+ * Copyright (C) 2024		William Mead			<william.mead@manchenumerique.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
