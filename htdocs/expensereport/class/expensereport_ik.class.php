a12553944aa8220fd979623105ffa7285957cead
MDW <mdeweerd@users.noreply.github.com>
2024-03-15 00:21:45 +0100

    Fix PhanTypeMismatchPropertyDefault

--- htdocs/expensereport/class/expensereport_ik.class.php
+++ htdocs/expensereport/class/expensereport_ik.class.php
@@ -75,9 +75,9 @@ class ExpenseReportIk extends CommonObject
 	 * @var array<string,array{type:string,label:string,enabled:int<0,2>|string,position:int,notnull:int,visible:int,noteditable?:int,default?:string,index?:int,foreignkey?:string,searchall?:int,isameasure?:int,css?:string,help?:string,showoncombobox?:int,disabled?:int,arrayofkeyval?:array<int,string>,comment?:string}>  Array with all fields and their property. Do not use it as a static var. It may be modified by constructor.
 	 */
 	public $fields = array(
-		'rowid' => array('type' => 'integer', 'index' => true)
-		,'fk_c_exp_tax_cat' => array('type' => 'integer', 'index' => true)
-		,'fk_range' => array('type' => 'integer', 'index' => true)
+		'rowid' => array('type' => 'integer', 'index' => 1)
+		,'fk_c_exp_tax_cat' => array('type' => 'integer', 'index' => 1)
+		,'fk_range' => array('type' => 'integer', 'index' => 1)
 		,'coef' => array('type' => 'double')
 		,'ikoffset' => array('type' => 'double')
 	);
