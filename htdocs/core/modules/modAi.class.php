ca3141c255834b478dd89bc8e7f253d912d291d6
Laurent Destailleur <eldy@destailleur.fr>
2024-03-14 14:03:21 +0100

    Fix picto

--- htdocs/core/modules/modAi.class.php
+++ htdocs/core/modules/modAi.class.php
@@ -78,7 +78,7 @@ class modAi extends DolibarrModules
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
 		// To use a supported fa-xxx css style of font awesome, use this->picto='xxx'
-		$this->picto = 'fa-microchip';
+		$this->picto = 'fa-magic';
 
 		// Define some features supported by module (triggers, login, substitutions, menus, css, etc...)
 		$this->module_parts = array(
