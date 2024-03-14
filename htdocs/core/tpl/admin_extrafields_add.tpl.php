ba79d822fff2f796c65ed218515b1090a045e944
jyhere <jyhere@gmail.com>
2024-03-14 09:58:39 +0100

    UPDATE: add a conf param to disable extrafields code autofill
    I find this feature pretty annoying, so i added a misc conf global param to disable the feature.

--- htdocs/core/tpl/admin_extrafields_add.tpl.php
+++ htdocs/core/tpl/admin_extrafields_add.tpl.php
@@ -137,10 +137,12 @@ $listofexamplesforlink = 'Societe:societe/class/societe.class.php<br>Contact:con
 		});
 
 		/* Autofill the code with label */
+		<?php if (empty($conf->global->MAIN_EXTRAFIELDS_CODE_AUTOFILL_DISABLED)) : ?>
 		jQuery("#label").keyup(function() {
 			console.log("Update new field");
 			$("#attrname").val( $(this).val().replace(/[^a-zA-Z0-9_]/g, '').toLowerCase() );
 		});
+		<?php endif; ?>
 	});
 </script>
 
