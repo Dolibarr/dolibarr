23785393a3b6dac26ca69c5323e8c7ae4dfcaaad
MDW <mdeweerd@users.noreply.github.com>
2024-03-15 02:04:16 +0100

    Add csslist to json keys

--- htdocs/workstation/class/workstationusergroup.class.php
+++ htdocs/workstation/class/workstationusergroup.class.php
@@ -35,7 +35,7 @@ class WorkstationUserGroup extends CommonObject
 	public $element = 'workstationusergroup';
 
 	/**
-	 * @var array<string,array{type:string,label:string,enabled:int<0,2>|string,position:int,notnull:int,visible:int,noteditable?:int,default?:string,index?:int,foreignkey?:string,searchall?:int,isameasure?:int,css?:string,help?:string,showoncombobox?:int,disabled?:int,arrayofkeyval?:array<int,string>,comment?:string}>  Array with all fields and their property. Do not use it as a static var. It may be modified by constructor.
+	 * @var array<string,array{type:string,label:string,enabled:int<0,2>|string,position:int,notnull:int,visible:int,noteditable?:int,default?:string,index?:int,foreignkey?:string,searchall?:int,isameasure?:int,css?:string,csslist?:string,help?:string,showoncombobox?:int,disabled?:int,arrayofkeyval?:array<int,string>,comment?:string}>  Array with all fields and their property. Do not use it as a static var. It may be modified by constructor.
 	 */
 	public $fields = array(
 		'fk_workstation' => array('type' => 'integer'),
