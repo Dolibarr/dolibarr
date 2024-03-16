a132f5331159b3e290fe2e8e86748b86c02a0600
Laurent Destailleur <eldy@destailleur.fr>
2024-03-16 23:28:22 +0100

    Fix removed var_dump

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -5706,14 +5706,12 @@ abstract class CommonObject
 		// update model_pdf in object
 		$this->model_pdf = $saved_model;
 
-		var_dump($filefound);
 		if (in_array(get_class($this), array('Adherent'))) {
 			$resultwritefile = $obj->write_file($this, $outputlangs, $srctemplatepath, 'member', 1, 'tmp_cards', $moreparams);
 		} else {
 			$resultwritefile = $obj->write_file($this, $outputlangs, $srctemplatepath, $hidedetails, $hidedesc, $hideref, $moreparams);
 		}
 		// After call of write_file $obj->result['fullpath'] is set with generated file. It will be used to update the ECM database index.
-		var_dump($filefound);
 
 		if ($resultwritefile > 0) {
 			$outputlangs->charset_output = $sav_charset_output;
