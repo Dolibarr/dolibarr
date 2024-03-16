34a554e64d8bf61d8cb2d8b4ea2d8a35a9ac134c
Laurent Destailleur <eldy@destailleur.fr>
2024-03-16 15:59:43 +0100

    Update cticketcategory.class.php

--- htdocs/ticket/class/cticketcategory.class.php
+++ htdocs/ticket/class/cticketcategory.class.php
@@ -186,7 +186,7 @@ class CTicketCategory extends CommonObject
 
 		$this->db = $db;
 
-		if (!getDolGlobalString('MAIN_SHOW_TECHNICAL_ID')) && isset($this->fields['rowid']) {
+		if (!getDolGlobalString('MAIN_SHOW_TECHNICAL_ID') && isset($this->fields['rowid']) {
 			$this->fields['rowid']['visible'] = 0;
 		}
 		if (!isModEnabled('multicompany') && isset($this->fields['entity'])) {
