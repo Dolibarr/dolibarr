7bdbba3083e18fdecdd873c09b281d00968d7d5b
Zephyriony <142790847+zephyriony@users.noreply.github.com>
2024-03-20 08:59:08 +0100

    Dolibarr customer area
    I'm embarking on this out of ambition but also because I think Dolibarr lacks visibility for third parties and I'd like to build up this facet but I think you have to take it one small step at a time.

---
+++ htdocs/contact/area/card.php
@@ -0,0 +1,4 @@
+/*
+The first building block of a Dolibarr customer space for consulting and signing documents online, which as previously mentioned would be far more secure than the current links.
+On the other hand, many companies like mine have their own customer space that uses the Dolibarr database, which would make it possible to merge the two and exploit the Dolibarr functions and hooks to customize it.
+*/
