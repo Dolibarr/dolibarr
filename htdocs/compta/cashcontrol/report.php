629e445f307ed560bb265c74acda1898313ef751
MDW <mdeweerd@users.noreply.github.com>
2024-03-21 22:13:50 +0100

    Fix typing with casts

--- htdocs/compta/cashcontrol/report.php
+++ htdocs/compta/cashcontrol/report.php
@@ -387,7 +387,7 @@ if ($resql) {
 		print '<br>';
 	}
 
-	print $langs->trans("Total").' ('.$totalqty.' '.$langs->trans("Articles").') : <div class="inline-block amount width100">'.price($cash + $cheque + $bank + $other).'</div>';
+	print $langs->trans("Total").' ('.$totalqty.' '.$langs->trans("Articles").') : <div class="inline-block amount width100">'.price((float) $cash + (float) $cheque + (float) $bank + (float) $other).'</div>';
 
 	print '<br>'.$langs->trans("TotalVAT").' : <div class="inline-block amount width100">'.price($totalvat).'</div>';
 
