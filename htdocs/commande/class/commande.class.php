18c58a0cc8ed87c090c1f29eacd3089c3960a52d
VESSILLER <lvessiller@open-dsi.fr>
2024-03-22 12:21:51 +0100

    FIX order conflict

--- htdocs/commande/class/commande.class.php
+++ htdocs/commande/class/commande.class.php
@@ -4406,11 +4406,7 @@ class OrderLine extends CommonOrderLine
 		$sqlCheckShipmentLine = "SELECT";
 		$sqlCheckShipmentLine .= " ed.rowid";
 		$sqlCheckShipmentLine .= " FROM " . MAIN_DB_PREFIX . "expeditiondet ed";
-<<<<<<< HEAD
-		$sqlCheckShipmentLine .= " WHERE ed.fk_origin_line = " . ((int)$this->id);
-=======
 		$sqlCheckShipmentLine .= " WHERE ed.fk_origin_line = " . ((int) $this->id);
->>>>>>> 18.0
 
 		$resqlCheckShipmentLine = $this->db->query($sqlCheckShipmentLine);
 		if (!$resqlCheckShipmentLine) {
