04b3ed0c7e2b6d7bc5bfddf7175f6d0fae301784
MDW <mdeweerd@users.noreply.github.com>
2024-03-23 15:03:00 +0100

    Fix extrafeild typo

--- htdocs/comm/mailing/class/html.formadvtargetemailing.class.php
+++ htdocs/comm/mailing/class/html.formadvtargetemailing.class.php
@@ -1,6 +1,7 @@
 <?php
 /* Copyright (C) 2014       Florian Henry           <florian.henry@open-concept.pro>
  * Copyright (C) 2019       Frédéric France         <frederic.france@netlogic.fr>
+ * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -278,7 +279,7 @@ class FormAdvTargetEmailing extends Form
 	}
 
 	/**
-	 * Return multiselect list of entities for extrafeild type sellist
+	 * Return multiselect list of entities for extrafield type sellist
 	 *
 	 * @param string $htmlname control name
 	 * @param array $sqlqueryparam array
