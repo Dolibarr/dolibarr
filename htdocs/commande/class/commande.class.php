6f0b45477976e39333e415fc5913cec599d76754
MDW <mdeweerd@users.noreply.github.com>
2024-03-23 20:38:20 +0100

    Fix PhanNonClassMethodCall with extra test

--- htdocs/commande/class/commande.class.php
+++ htdocs/commande/class/commande.class.php
@@ -3764,7 +3764,7 @@ class Commande extends CommonOrder
 				$langs->load('project');
 				if (empty($this->project)) {
 					$res = $this->fetch_project();
-					if ($res > 0) {
+					if ($res > 0 && $this->project instanceof Project) {
 						$datas['project'] = '<br><b>'.$langs->trans('Project').':</b> '.$this->project->getNomUrl(1, '', 0, 1);
 					}
 				}
