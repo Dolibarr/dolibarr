2982465f566af2a5aae5a3908747aadcbb8d5d0b
MDW <mdeweerd@users.noreply.github.com>
2024-03-23 15:43:50 +0100

    Extra test to fix PhanTypeInvalidPropertyName

--- htdocs/reception/note.php
+++ htdocs/reception/note.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2004-2008 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2012  Regis Houssin        <regis.houssin@capnetworks.com>
  * Copyright (C) 2013 	   Florian Henry        <florian.henry@open-concept.pro>
+ * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
