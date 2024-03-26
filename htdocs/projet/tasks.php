47ef0645cfbda3138b7ceb51430226d1b1a67395
MDW <mdeweerd@users.noreply.github.com>
2024-03-26 13:23:08 +0100

    Fix arguments to dol_get_fiche_head

--- htdocs/projet/tasks.php
+++ htdocs/projet/tasks.php
@@ -724,7 +724,7 @@ if ($action == 'create' && $user->hasRight('projet', 'creer') && (empty($object-
 		print '<input type="hidden" name="id" value="'.$object->id.'">';
 	}
 
-	print dol_get_fiche_head('');
+	print dol_get_fiche_head();
 
 	print '<div class="div-table-responsive-no-min">';
 	print '<table class="border centpercent">';
