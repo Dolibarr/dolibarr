0a2481c702a8c69f8410d51212ac62112f4a8838
Yamil Esteban Garcia <120027058+developmentOYR@users.noreply.github.com>
2024-03-26 22:14:19 +0100

    Update fournisseur.facture.class.php
    IRPF reset when editing the label of a supplier invoice #29049

--- htdocs/fourn/class/fournisseur.facture.class.php
+++ htdocs/fourn/class/fournisseur.facture.class.php
@@ -1168,8 +1168,8 @@ class FactureFournisseur extends CommonInvoice
 		if (isset($this->localtax1)) {
 			$this->localtax1 = trim($this->localtax1);
 		}
-		if (isset($this->localtax2)) {
-			$this->localtax2 = trim($this->localtax2);
+		if (isset($this->total_localtax2)) {
+			$this->total_localtax2 = trim($this->total_localtax2);
 		}
 		if (empty($this->total_ht)) {
 			$this->total_ht = 0;
@@ -1243,7 +1243,7 @@ class FactureFournisseur extends CommonInvoice
 		$sql .= " close_code=".(isset($this->close_code) ? "'".$this->db->escape($this->close_code)."'" : "null").",";
 		$sql .= " close_note=".(isset($this->close_note) ? "'".$this->db->escape($this->close_note)."'" : "null").",";
 		$sql .= " localtax1=".(isset($this->localtax1) ? ((float) $this->localtax1) : "null").",";
-		$sql .= " localtax2=".(isset($this->localtax2) ? ((float) $this->localtax2) : "null").",";
+		$sql .= " localtax2=".(isset($this->total_localtax2) ? ((float) $this->total_localtax2) : "null").",";
 		$sql .= " total_ht=".(isset($this->total_ht) ? ((float) $this->total_ht) : "null").",";
 		$sql .= " total_tva=".(isset($this->total_tva) ? ((float) $this->total_tva) : "null").",";
 		$sql .= " total_ttc=".(isset($this->total_ttc) ? ((float) $this->total_ttc) : "null").",";
