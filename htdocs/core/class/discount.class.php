274a6d05286486f527cd35b1cc66cf158874344e
MDW <mdeweerd@users.noreply.github.com>
2024-03-27 18:56:24 +0100

    Improve phpdoc typing

--- htdocs/core/class/discount.class.php
+++ htdocs/core/class/discount.class.php
@@ -42,6 +42,9 @@ class DiscountAbsolute extends CommonObject
 	 */
 	public $socid;
 
+	/**
+	 * @var int<0,1>
+	 */
 	public $discount_type; // 0 => customer discount, 1 => supplier discount
 
 	public $total_ht;
@@ -132,7 +135,7 @@ class DiscountAbsolute extends CommonObject
 	 *  @param      int		$rowid       					id discount to load
 	 *  @param      int		$fk_facture_source				fk_facture_source
 	 *  @param		int		$fk_invoice_supplier_source		fk_invoice_supplier_source
-	 *	@return		int										Return integer <0 if KO, =0 if not found, >0 if OK
+	 *	@return		int<-1,1>								Return integer <0 if KO, =0 if not found, >0 if OK
 	 */
 	public function fetch($rowid, $fk_facture_source = 0, $fk_invoice_supplier_source = 0)
 	{
@@ -225,7 +228,7 @@ class DiscountAbsolute extends CommonObject
 	 *      Create a discount into database
 	 *
 	 *      @param      User	$user       User that create
-	 *      @return     int         		Return integer <0 if KO, >0 if OK
+	 *      @return     int<-1,1>      		Return integer <0 if KO, >0 if OK
 	 */
 	public function create($user)
 	{
@@ -299,8 +302,8 @@ class DiscountAbsolute extends CommonObject
 	/**
 	 *  Delete object in database. If fk_facture_source is defined, we delete all family with same fk_facture_source. If not, only with id is removed
 	 *
-	 *  @param      User    $user       Object of user asking to delete
-	 *  @return     int                 Return integer <0 if KO, >0 if OK
+	 *  @param	User	$user		Object of user asking to delete
+	 *  @return	int<-1,1>			Return integer <0 if KO, >0 if OK
 	 */
 	public function delete($user)
 	{
@@ -424,7 +427,7 @@ class DiscountAbsolute extends CommonObject
 	 *
 	 *	@param		int		$rowidline		Invoice line id (To use discount into invoice lines)
 	 *	@param		int		$rowidinvoice	Invoice id (To use discount as a credit note to reduce payment of invoice)
-	 *	@return		int						Return integer <0 if KO, >0 if OK
+	 *	@return		int<-1,1>				Return integer <0 if KO, >0 if OK
 	 */
 	public function link_to_invoice($rowidline, $rowidinvoice)
 	{
@@ -480,7 +483,7 @@ class DiscountAbsolute extends CommonObject
 	 *	Link the discount to a particular invoice line or a particular invoice.
 	 *	Do not call this if discount is linked to a reconcialiated invoice
 	 *
-	 *	@return		int							Return integer <0 if KO, >0 if OK
+	 *	@return		int<-1,1>					Return integer <0 if KO, >0 if OK
 	 */
 	public function unlink_invoice()
 	{
@@ -507,13 +510,13 @@ class DiscountAbsolute extends CommonObject
 	/**
 	 *  Return amount (with tax) of discounts currently available for a company, user or other criteria
 	 *
-	 *	@param		Societe		$company		Object third party for filter
-	 *	@param		User		$user			Filtre sur un user auteur des remises
+	 *	@param		?Societe	$company		Object third party for filter
+	 *	@param		?User		$user			Filtre sur un user auteur des remises
 	 * 	@param		string		$filter			Filter other. Warning: Do not use a user input value here.
-	 * 	@param		int			$maxvalue		Filter on max value for discount
-	 *  @param      int			$discount_type  0 => customer discount, 1 => supplier discount
-	 *  @param      int			$multicurrency  Return multicurrency_amount instead of amount
-	 * 	@return		int						Return integer <0 if KO, amount otherwise
+	 * 	@param		int|float	$maxvalue		Filter on max value for discount
+	 *  @param      int<0,1>	$discount_type  0 => customer discount, 1 => supplier discount
+	 *  @param      int<0,1>	$multicurrency  Return multicurrency_amount instead of amount
+	 * 	@return		int<-1,-1>|float			Return integer <0 if KO, amount otherwise
 	 */
 	public function getAvailableDiscounts($company = null, $user = null, $filter = '', $maxvalue = 0, $discount_type = 0, $multicurrency = 0)
 	{
@@ -566,8 +569,8 @@ class DiscountAbsolute extends CommonObject
 	 *  Should always be empty, except if option FACTURE_DEPOSITS_ARE_JUST_PAYMENTS or FACTURE_SUPPLIER_DEPOSITS_ARE_JUST_PAYMENTS is on (not recommended).
 	 *
 	 *	@param		CommonInvoice	$invoice		Object invoice (customer of supplier)
-	 *  @param 		int 		    $multicurrency 	1=Return multicurrency_amount instead of amount. TODO Add a mode multicurrency = -1 to return array with amount + multicurrency amount
-	 *	@return		int				     			Return integer <0 if KO, Sum of credit notes and deposits amount otherwise
+	 *  @param 		int<-1,1>	    $multicurrency 	1=Return multicurrency_amount instead of amount. TODO Add a mode multicurrency = -1 to return array with amount + multicurrency amount
+	 *	@return		int<-1,-1>|float     			Return integer <0 if KO, Sum of credit notes and deposits amount otherwise
 	 */
 	public function getSumDepositsUsed($invoice, $multicurrency = 0)
 	{
@@ -607,7 +610,7 @@ class DiscountAbsolute extends CommonObject
 	 *  Return amount (with tax) of all credit notes invoices + excess received used by invoice as a payment
 	 *
 	 *	@param      CommonInvoice	  $invoice	    	Object invoice
-	 *	@param      int			      $multicurrency	1=Return multicurrency_amount instead of amount. TODO Add a mode multicurrency = -1 to return array with amount + multicurrency amount
+	 *	@param      int<-1,1>	      $multicurrency	1=Return multicurrency_amount instead of amount. TODO Add a mode multicurrency = -1 to return array with amount + multicurrency amount
 	 *	@return     int					        		Return integer <0 if KO, Sum of credit notes and excess received amount otherwise
 	 */
 	public function getSumCreditNotesUsed($invoice, $multicurrency = 0)
@@ -647,7 +650,7 @@ class DiscountAbsolute extends CommonObject
 	 *    	Return amount (with tax) of all converted amount for this credit note
 	 *
 	 *	@param		CommonInvoice	  $invoice	    	Object invoice
-	 *	@param		int			      $multicurrency	Return multicurrency_amount instead of amount. TODO Add a mode multicurrency = -1 to return array with amount + multicurrency amount
+	 *	@param		int<-1,1>	      $multicurrency	Return multicurrency_amount instead of amount. TODO Add a mode multicurrency = -1 to return array with amount + multicurrency amount
 	 *	@return		int					        		Return integer <0 if KO, Sum of credit notes and deposits amount otherwise
 	 */
 	public function getSumFromThisCreditNotesNotUsed($invoice, $multicurrency = 0)
@@ -685,9 +688,9 @@ class DiscountAbsolute extends CommonObject
 	/**
 	 *  Return clickable ref of object (with picto or not)
 	 *
-	 *  @param		int		$withpicto		0=No picto, 1=Include picto into link, 2=Picto only
-	 *  @param		string	$option			Where to link to ('invoice' or 'discount')
-	 *  @return		string					String with URL
+	 *  @param	int<0,1>	$withpicto		0=No picto, 1=Include picto into link, 2=Picto only
+	 *  @param	string		$option			Where to link to ('invoice' or 'discount')
+	 *  @return	string						String with URL
 	 */
 	public function getNomUrl($withpicto, $option = 'invoice')
 	{
@@ -734,7 +737,7 @@ class DiscountAbsolute extends CommonObject
 	 *  Used to build previews or test instances.
 	 *	id must be 0 if object instance is a specimen.
 	 *
-	 *  @return	int
+	 *  @return	int<0,1>
 	 */
 	public function initAsSpecimen()
 	{
