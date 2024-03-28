d1cb440916068102d65a9ab71c8659f54a68a55b
Jonathan lescaut <jonathan.lescaut@atm-consulting.fr>
2024-03-28 17:56:39 +0100

    massAction unlocate commercial

--- htdocs/core/tpl/massactions_pre.tpl.php
+++ htdocs/core/tpl/massactions_pre.tpl.php
@@ -394,6 +394,16 @@ if ($massaction == 'presetcommercial') {
 			'value' => $form->multiselectarray('commercial', $userlist, null, 0, 0, 'quatrevingtpercent widthcentpercentminusx', 0, 0, '', '', '', 1));
 	print $form->formconfirm($_SERVER["PHP_SELF"], $langs->trans("ConfirmAllocateCommercial"), $langs->trans("ConfirmAllocateCommercialQuestion", count($toselect)), "affectcommercial", $formquestion, 1, 0, 200, 500, 1);
 }
+if ($massaction == 'unsetcommercial') {
+	$formquestion = array();
+	$userlist = $form->select_dolusers('', '', 0, null, 0, '', '', 0, 0, 0, 'AND u.statut = 1', 0, '', '', 0, 1);
+	$formquestion[] = array('type' => 'other',
+		'name' => 'Unassigncommercial',
+		'label' => $form->editfieldkey('UnallocateCommercial', 'commercial_id', '', $object, 0),
+		'value' => $form->multiselectarray('commercial', $userlist, null, 0, 0, 'quatrevingtpercent widthcentpercentminusx', 0, 0, '', '', '', 1));
+	print $form->formconfirm($_SERVER["PHP_SELF"], $langs->trans("ConfirmUnallocateCommercial"), $langs->trans("ConfirmUnallocateCommercialQuestion", count($toselect)), "Unassigncommercial", $formquestion, 1, 0, 200, 500, 1);
+}
+
 if ($massaction == 'preapproveleave') {
 	print $form->formconfirm($_SERVER["PHP_SELF"], $langs->trans("ConfirmMassLeaveApproval"), $langs->trans("ConfirmMassLeaveApprovalQuestion", count($toselect)), "approveleave", null, 'yes', 0, 200, 500, 1);
 }
