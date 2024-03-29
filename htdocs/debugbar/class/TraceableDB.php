67b12fc438117eca365ab1ae0d4d0bf166599c6c
Laurent Destailleur <eldy@users.sourceforge.net>
2024-03-29 17:08:31 +0100

    FIX bad search on _ char in like when Debugbar is on

--- htdocs/debugbar/class/TraceableDB.php
+++ htdocs/debugbar/class/TraceableDB.php
@@ -293,7 +293,7 @@ class TraceableDB extends DoliDB
 	 */
 	public function escapeforlike($stringtoencode)
 	{
-		return str_replace(array('_', '\\', '%'), array('\_', '\\\\', '\%'), (string) $stringtoencode);
+		return $this->db->escapeforlike($stringtoencode);
 	}
 
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
