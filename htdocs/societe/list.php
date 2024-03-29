2efe9475597152d28ddeb1d87045e9201f62e885
Jonathan lescaut <jonathan.lescaut@atm-consulting.fr>
2024-03-29 14:54:52 +0100

    GETPOSTISARRAY() $search_sale

--- htdocs/societe/list.php
+++ htdocs/societe/list.php
@@ -97,7 +97,11 @@ $search_idprof4 = trim(GETPOST('search_idprof4', 'alpha'));
 $search_idprof5 = trim(GETPOST('search_idprof5', 'alpha'));
 $search_idprof6 = trim(GETPOST('search_idprof6', 'alpha'));
 $search_vat = trim(GETPOST('search_vat', 'alpha'));
-$search_sale = GETPOST("search_sale", 'alphanohtml');
+if (GETPOSTISARRAY('search_sale')) {
+	$search_sale = GETPOST('search_sale', 'array:int');
+} else if (GETPOSTISSET('search_sale')) {
+	$search_sale = array(GETPOSTINT('search_sale'));
+}
 $search_categ_cus = GETPOSTINT("search_categ_cus");
 $search_categ_sup = GETPOSTINT("search_categ_sup");
 $searchCategoryCustomerOperator = 0;
@@ -594,6 +598,7 @@ if (!empty($search_sale) && $search_sale != '-1') {
 		$value = intval($value);
 		return $value >= 0;
 	});
+
 	$search_sale_req = implode(',', $search_sale_req);
 
 	if (count($search_sale) == 1 && in_array('-2', $search_sale)) {
