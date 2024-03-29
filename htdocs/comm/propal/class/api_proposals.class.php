0fbaf8b118744bfa41a3e20f7ae448f24a8a8912
Laurent Destailleur <eldy@destailleur.fr>
2024-03-29 11:25:33 +0100

    Fix example

--- htdocs/comm/propal/class/api_proposals.class.php
+++ htdocs/comm/propal/class/api_proposals.class.php
@@ -156,7 +156,7 @@ class Proposals extends DolibarrApi
 	 * @param int		$limit				Limit for list
 	 * @param int		$page				Page number
 	 * @param string	$thirdparty_ids		Thirdparty ids to filter commercial proposals (example '1' or '1,2,3') {@pattern /^[0-9,]*$/i}
-	 * @param string    $sqlfilters         Other criteria to filter answers separated by a comma. Syntax example "(t.ref:like:'SO-%') and (t.datec:<:'20160101')"
+	 * @param string    $sqlfilters         Other criteria to filter answers separated by a comma. Syntax example "(t.ref:like:'SO-%') and (t.datec:<:'2016-01-01')"
 	 * @param string    $properties	Restrict the data returned to these properties. Ignored if empty. Comma separated list of properties names
 	 * @return  array                       Array of order objects
 	 */
