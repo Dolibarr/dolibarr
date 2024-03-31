a783f2afcb709eb8e9bf83daddeffcf4b0609825
MDW <mdeweerd@users.noreply.github.com>
2024-03-31 05:02:34 +0200

    Do not use deprecated statut, fk_statut, paye

--- htdocs/user/class/user.class.php
+++ htdocs/user/class/user.class.php
@@ -77,13 +77,6 @@ class User extends CommonObject
 
 	/**
 	 * @var int
-	 * @deprecated Use $status
-	 * @see $status
-	 */
-	public $statut;
-
-	/**
-	 * @var int
 	 */
 	public $status;
 
@@ -691,7 +684,6 @@ class User extends CommonObject
 				$this->note_public = $obj->note_public;
 				$this->note_private = $obj->note_private;
 
-				$this->statut		= $obj->status;			// deprecated
 				$this->status		= $obj->status;
 
 				$this->photo		= $obj->photo;
@@ -1577,8 +1569,8 @@ class User extends CommonObject
 		$error = 0;
 
 		// Check parameters
-		if (isset($this->statut)) {
-			if ($this->statut == $status) {
+		if (isset($this->status)) {
+			if ($this->status == $status) {
 				return 0;
 			}
 		} elseif (isset($this->status) && $this->status == $status) {
@@ -1613,7 +1605,6 @@ class User extends CommonObject
 			return -$error;
 		} else {
 			$this->status = $status;
-			$this->statut = $status;
 			$this->db->commit();
 			return 1;
 		}
@@ -1822,7 +1813,7 @@ class User extends CommonObject
 				$entrepot->label = $langs->trans("PersonalStock", $this->getFullName($langs));
 				$entrepot->libelle = $entrepot->label; // For backward compatibility
 				$entrepot->description = $langs->trans("ThisWarehouseIsPersonalStock", $this->getFullName($langs));
-				$entrepot->statut = 1;
+				$entrepot->status = 1;
 				$entrepot->country_id = $mysoc->country_id;
 
 				$warehouseid = $entrepot->create($user);
@@ -3290,7 +3281,7 @@ class User extends CommonObject
 	 */
 	public function getLibStatut($mode = 0)
 	{
-		return $this->LibStatut(isset($this->statut) ? (int) $this->statut : (int) $this->status, $mode);
+		return $this->LibStatut((int) $this->status, $mode);
 	}
 
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
@@ -3631,7 +3622,6 @@ class User extends CommonObject
 		$this->iplastlogin = '127.0.0.1';
 		$this->datepreviouslogin = $now;
 		$this->ippreviouslogin = '127.0.0.1';
-		$this->statut = 1;		// deprecated
 		$this->status = 1;
 
 		$this->entity = 1;
