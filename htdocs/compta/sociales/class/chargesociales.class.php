4e350a78853c9e79c8ba0f57a52feef75e66e89d
MDW <mdeweerd@users.noreply.github.com>
2024-03-31 05:19:36 +0200

    Do not use deprecated statut, fk_statut, paye

--- htdocs/compta/sociales/class/chargesociales.class.php
+++ htdocs/compta/sociales/class/chargesociales.class.php
@@ -99,8 +99,7 @@ class ChargeSociales extends CommonObject
 	/**
 	 * @var int<0,1>
 	 */
-	public $paye;
-
+	public $paid;
 	/**
 	 * @deprecated Use $period
 	 * @var int|string
@@ -178,6 +177,18 @@ class ChargeSociales extends CommonObject
 	 */
 	const STATUS_PAID = 1;
 
+
+	/**
+	 * Provide list of deprecated properties and replacements
+	 *
+	 * @return array<string,string>  Old property to new property mapping
+	 */
+	protected function deprecatedProperties()
+	{
+		return array(
+			'paye' => 'paid',
+		) + parent::deprecatedProperties();
+	}
 
 	/**
 	 * Constructor
