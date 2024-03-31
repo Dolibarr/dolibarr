791b0cff4e4647ed284544963052eb874336056c
MDW <mdeweerd@users.noreply.github.com>
2024-03-31 05:02:28 +0200

    Do not use deprecated statut, fk_statut, paye

--- htdocs/projet/class/project.class.php
+++ htdocs/projet/class/project.class.php
@@ -206,12 +206,6 @@ class Project extends CommonObject
 	public $max_attendees;
 
 	/**
-	 * @var int status
-	 * @deprecated Use $status
-	 */
-	public $statut; // 0=draft, 1=opened, 2=closed
-
-	/**
 	 * @var int opportunity status
 	 */
 	public $opp_status; // opportunity status, into table llx_c_lead_status
@@ -784,7 +778,6 @@ class Project extends CommonObject
 				$this->user_modification_id = $obj->fk_user_modif;
 				$this->user_closing_id = $obj->fk_user_close;
 				$this->public = $obj->public;
-				$this->statut = $obj->status; // deprecated
 				$this->status = $obj->status;
 				$this->opp_status = $obj->fk_opp_status;
 				$this->opp_amount	= $obj->opp_amount;
@@ -1254,7 +1247,6 @@ class Project extends CommonObject
 			}
 
 			if (!$error) {
-				$this->statut = 1;
 				$this->status = 1;
 				$this->db->commit();
 				return 1;
@@ -1306,7 +1298,7 @@ class Project extends CommonObject
 				// End call triggers
 
 				if (!$error) {
-					$this->status = 2;
+					$this->status = self::STATUS_CLOSED;
 					$this->db->commit();
 					return 1;
 				} else {
@@ -1333,7 +1325,7 @@ class Project extends CommonObject
 	 */
 	public function getLibStatut($mode = 0)
 	{
-		return $this->LibStatut(isset($this->status) ? $this->status : $this->statut, $mode);
+		return $this->LibStatut($this->status, $mode);
 	}
 
 	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.ScopeNotCamelCaps
@@ -2327,7 +2319,6 @@ class Project extends CommonObject
 			while ($obj = $this->db->fetch_object($resql)) {
 				$response->nbtodo++;
 
-				$project_static->statut = $obj->status;
 				$project_static->status = $obj->status;
 				$project_static->opp_status = $obj->fk_opp_status;
 				$project_static->date_end = $this->db->jdate($obj->datee);
