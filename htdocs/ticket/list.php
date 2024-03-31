8067cc3475c22cf24e6679c728a86ed2fa634f92
MDW <mdeweerd@users.noreply.github.com>
2024-03-31 04:35:59 +0200

    Do not use deprecated statut, fk_statut

--- htdocs/ticket/list.php
+++ htdocs/ticket/list.php
@@ -1038,7 +1038,6 @@ while ($i < $imaxinloop) {
 	// Store properties in $object
 	$object->setVarsFromFetchObj($obj);
 	$object->type_code = $obj->type_code;
-	$object->status = $object->fk_statut; // because field name is fk_statut
 
 	if ($mode == 'kanban') {
 		if ($i == 0) {
