8e47ea05954c8367010fe086e5ad452ffea20e98
MDW <mdeweerd@users.noreply.github.com>
2024-03-31 04:34:43 +0200

    Do not use deprecated statut, fk_statut

--- htdocs/categories/class/api_categories.class.php
+++ htdocs/categories/class/api_categories.class.php
@@ -2,6 +2,7 @@
 /* Copyright (C) 2015       Jean-François Ferry     <jfefe@aternatik.fr>
  * Copyright (C) 2024       Jose MARTINEZ			<jose.martinez@pichinov.com>
  * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
+ * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -677,8 +678,9 @@ class Categories extends DolibarrApi
 	/**
 	 * Clean sensible object datas
 	 *
-	 * @param   Categorie  $object  Object to clean
-	 * @return  Object     			Object with cleaned properties
+	 * @template T of \Categorie
+	 * @param   T  $object		Object to clean
+	 * @return  T     			Object with cleaned properties
 	 */
 	protected function _cleanObjectDatas($object)
 	{
@@ -725,7 +727,7 @@ class Categories extends DolibarrApi
 		unset($object->fk_account);
 		unset($object->fk_project);
 		unset($object->note);
-		unset($object->statut);
+		unset($object->status);
 
 		return $object;
 	}
