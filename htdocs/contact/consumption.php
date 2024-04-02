bef4242b31869be101f06d18e0e6766ad66f5038
Laurent Destailleur <eldy@destailleur.fr>
2024-04-02 13:49:00 +0200

    Update consumption.php

--- htdocs/contact/consumption.php
+++ htdocs/contact/consumption.php
@@ -111,7 +111,7 @@ $formother = new FormOther($db);
 $productstatic = new Product($db);
 $objsoc = new Societe($db);
 
-$title = $langs->trans("ContactsRelatedItems");
+$title = $langs->trans("ContactRelatedItems");
 $help_url = 'EN:Module_Third_Parties|FR:Module_Tiers|ES:Empresas';
 llxHeader('', $title, $help_url);
 
