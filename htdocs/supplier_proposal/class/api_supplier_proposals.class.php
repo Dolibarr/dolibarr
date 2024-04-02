1bf02ca858e7fc0e57f1bc41e0a11d42f4e2bad5
Laurent Destailleur <eldy@destailleur.fr>
2024-04-02 14:46:34 +0200

    Update api_supplier_proposals.class.php

--- htdocs/supplier_proposal/class/api_supplier_proposals.class.php
+++ htdocs/supplier_proposal/class/api_supplier_proposals.class.php
@@ -105,7 +105,7 @@ class SupplierProposals extends DolibarrApi
 		}
 
 		if (!DolibarrApi::_checkAccessToResource('supplier_proposal', $this->supplier_proposal->id)) {
-			throw new RestException(401, 'Access not allowed for login '.DolibarrApiAccess::$user->login);
+			throw new RestException(403, 'Access not allowed for login '.DolibarrApiAccess::$user->login);
 		}
 
 		$this->supplier_proposal->fetchObjectLinked();
@@ -121,7 +121,7 @@ class SupplierProposals extends DolibarrApi
 	public function post($request_data = null)
 	{
 		if (!DolibarrApiAccess::$user->hasRight('supplier_proposal', 'creer')) {
-			throw new RestException(401, "Insuffisant rights");
+			throw new RestException(403, "Insuffisant rights");
 		}
 		// Check mandatory fields
 		$result = $this->_validate($request_data);
@@ -168,7 +168,7 @@ class SupplierProposals extends DolibarrApi
 		}
 
 		if (!DolibarrApi::_checkAccessToResource('supplier_proposal', $this->supplier_proposal->id)) {
-			throw new RestException(401, 'Access not allowed for login '.DolibarrApiAccess::$user->login);
+			throw new RestException(403, 'Access not allowed for login '.DolibarrApiAccess::$user->login);
 		}
 		foreach ($request_data as $field => $value) {
 			if ($field == 'id') {
