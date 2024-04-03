e96b245f3d85b320415313eaa2f8cf4b7811d699
Jonathan lescaut <jonathan.lescaut@atm-consulting.fr>
2024-04-03 12:05:16 +0200

    round capital societe

--- htdocs/core/class/commondocgenerator.class.php
+++ htdocs/core/class/commondocgenerator.class.php
@@ -323,7 +323,7 @@ abstract class CommonDocGenerator
 			'company_supplieraccountancycode'=>$object->code_compta_fournisseur,
 			'company_juridicalstatus'=>$object->forme_juridique,
 			'company_outstanding_limit'=>$object->outstanding_limit,
-			'company_capital'=>$object->capital,
+			'company_capital'=> price($object->capital, 0, '', 1, -1),
 			'company_idprof1'=>$object->idprof1,
 			'company_idprof2'=>$object->idprof2,
 			'company_idprof3'=>$object->idprof3,
