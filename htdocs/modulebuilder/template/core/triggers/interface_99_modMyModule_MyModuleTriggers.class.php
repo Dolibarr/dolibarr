c87dcc4e538cdfb4708d7a4017f7c6244cd709de
sonikf <93765174+sonikf@users.noreply.github.com>
2024-04-03 11:32:01 +0300

    Add recurring invoice triggers

--- htdocs/modulebuilder/template/core/triggers/interface_99_modMyModule_MyModuleTriggers.class.php
+++ htdocs/modulebuilder/template/core/triggers/interface_99_modMyModule_MyModuleTriggers.class.php
@@ -213,6 +213,14 @@ class InterfaceMyModuleTriggers extends DolibarrTriggers
 			//case 'LINEBILL_UPDATE':
 			//case 'LINEBILL_DELETE':
 
+			// Recurring Bills
+			//case 'BILLREC_CREATE':
+			//case 'BILLREC_MODIFY':
+			//case 'BILLREC_DELETE':
+			//case 'BILLREC_AUTOCREATEBILL':
+			//case 'LINEBILLREC_MODIFY':
+			//case 'LINEBILLREC_DELETE':
+
 			//Supplier Bill
 			//case 'BILL_SUPPLIER_CREATE':
 			//case 'BILL_SUPPLIER_UPDATE':
