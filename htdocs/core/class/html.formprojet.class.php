444e6193b9c74a970a7134f585f309ba26c03da2
Hystepik <lmarcouiller@nltechno.com>
2024-04-04 10:39:36 +0200

    fix precommit

--- htdocs/core/class/html.formprojet.class.php
+++ htdocs/core/class/html.formprojet.class.php
@@ -335,7 +335,7 @@ class FormProjets extends Form
 	 * @param string $showmore 'all' = Show project info, 'progress' = Show task progression, ''=Show nothing more
 	 * @param User $usertofilter User object to use for filtering
 	 * @param int 	$nooutput 		1=Return string, do not send to output
-	 * 
+	 *
 	 * @return int|string                   Nbr of tasks if OK, <0 if KO. If nooutput=1: Return a HTML select string.
 	 */
 	public function selectTasks($socid = -1, $selected = 0, $htmlname = 'taskid', $maxlength = 24, $option_only = 0, $show_empty = '1', $discard_closed = 0, $forcefocus = 0, $disabled = 0, $morecss = 'maxwidth500', $projectsListId = '', $showmore = 'all', $usertofilter = null, $nooutput = 0)
