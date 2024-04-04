389857fdf1aeda222c37f6a9416c689f6cee0639
Laurent Destailleur <eldy@destailleur.fr>
2024-04-04 13:16:32 +0200

    FIX missing trans on VolumeUnitcm3

--- htdocs/core/lib/signature.lib.php
+++ htdocs/core/lib/signature.lib.php
@@ -61,7 +61,7 @@ function showOnlineSignatureUrl($type, $ref, $obj = null)
  */
 function getOnlineSignatureUrl($mode, $type, $ref = '', $localorexternal = 1, $obj = null)
 {
-	global $conf, $dolibarr_main_url_root;
+	global $dolibarr_main_url_root;
 
 	if (empty($obj)) {
 		// For compatibility with 15.0 -> 19.0
--- htdocs/core/modules/expedition/doc/pdf_espadon.modules.php
+++ htdocs/core/modules/expedition/doc/pdf_espadon.modules.php
@@ -139,7 +139,7 @@ class pdf_espadon extends ModelePdfExpedition
 		}
 
 		// Load traductions files required by page
-		$outputlangs->loadLangs(array("main", "bills", "orders", "products", "dict", "companies", "propal", "deliveries", "sendings", "productbatch"));
+		$outputlangs->loadLangs(array("main", "bills", "orders", "products", "dict", "companies", "other", "propal", "deliveries", "sendings", "productbatch"));
 
 		// Show Draft Watermark
 		if ($object->statut == $object::STATUS_DRAFT && (getDolGlobalString('SHIPPING_DRAFT_WATERMARK'))) {
