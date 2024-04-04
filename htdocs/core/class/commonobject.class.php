2302708c98d8373434c0aa5a5a922262aebc0c58
thibdrev <thibault.drevet@gmail.com>
2024-04-04 20:51:46 +0200

    typo

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -127,7 +127,7 @@ abstract class CommonObject
 	public $table_element_line = '';
 
 	/**
-	 * @var int<0,2>|string 	Does this object supports the multicompany module ?
+	 * @var int<0,2>|string 	Does this object support the multicompany module ?
 	 * 0=No test on entity, 1=Test with field entity, 2=Test with link by fk_soc, 'field@table'=Test with link by field@table
 	 */
 	public $ismultientitymanaged;
