b17788ab5cf240cf49096c24750e107882890cc7
Laurent Destailleur <eldy@destailleur.fr>
2024-04-05 10:38:40 +0200

    Add missing ;

--- htdocs/install/mysql/migration/19.0.0-20.0.0.sql
+++ htdocs/install/mysql/migration/19.0.0-20.0.0.sql
@@ -294,4 +294,4 @@ ALTER TABLE llx_supplier_proposaldet MODIFY ref_fourn VARCHAR(128) NULL;
 ALTER TABLE llx_projet ADD COLUMN ref_ext varchar(50) after ref;
 
 -- bookcal
-ALTER TABLE llx_bookcal_calendar ADD COLUMN entity integer DEFAULT 1 NOT NULL AFTER rowid
+ALTER TABLE llx_bookcal_calendar ADD COLUMN entity integer DEFAULT 1 NOT NULL AFTER rowid;
