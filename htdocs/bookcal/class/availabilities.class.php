e4010a41a96a98fd3fda05683928b0fd316cc253
Laurent Destailleur <eldy@destailleur.fr>
2024-04-07 12:07:06 +0200

    Update availabilities.class.php

--- htdocs/bookcal/class/availabilities.class.php
+++ htdocs/bookcal/class/availabilities.class.php
@@ -53,7 +53,7 @@ class Availabilities extends CommonObject
 	 * @var int<0,2>|string  Does this object support multicompany module ?
 	 * 0=No test on entity, 1=Test with field entity, 'field@table'=Test with link by field@table
 	 */
-	public $ismultientitymanaged = 1; // done VIA calendar MC support
+	public $ismultientitymanaged = 'fk_bookcal_calendar@bookcal_calendar';
 
 	/**
 	 * @var int  Does object support extrafields ? 0=No, 1=Yes
