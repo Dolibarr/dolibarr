28a2a8f2d6488cb3a756c8add4bb7847f8914db6
Laurent Destailleur <eldy@destailleur.fr>
2024-04-07 12:56:05 +0200

    Update DolibarrModules.class.php

--- htdocs/core/modules/DolibarrModules.class.php
+++ htdocs/core/modules/DolibarrModules.class.php
@@ -173,11 +173,6 @@ class DolibarrModules // Can not be abstract, because we need to instantiate it 
 	public $module_parts = array();
 
 	/**
-	 * @var string[] enabled modules
-	 */
-	public $modules = [];
-
-	/**
 	 * @var        string Module documents ?
 	 * @deprecated Seems unused anywhere
 	 */
