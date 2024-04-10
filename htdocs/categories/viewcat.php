046ffbc024b5ac008eec8d0079cc251c2a3a53c2
Laurent Destailleur <eldy@destailleur.fr>
2024-04-10 16:31:50 +0200

    FIX getObjectsInCateg must not return corrupted orphelins

--- htdocs/categories/viewcat.php
+++ htdocs/categories/viewcat.php
@@ -678,6 +678,7 @@ if ($type == Categorie::TYPE_SUPPLIER) {
 		$permission = $user->hasRight('societe', 'creer');
 
 		$socs = $object->getObjectsInCateg($type, 0, $limit, $offset);
+
 		if ($socs < 0) {
 			dol_print_error($db, $object->error, $object->errors);
 		} else {
@@ -738,7 +739,7 @@ if ($type == Categorie::TYPE_SUPPLIER) {
 					// Link to delete from category
 					print '<td class="right">';
 					if ($permission) {
-						print "<a href= '".$_SERVER['PHP_SELF']."?".(empty($socid) ? 'id' : 'socid')."=".$object->id."&type=".$typeid."&action=unlink&token=".newToken()."&removeelem=".$soc->id."'>";
+						print '<a class="reposition" href="'.$_SERVER['PHP_SELF']."?".(empty($socid) ? 'id' : 'socid')."=".$object->id."&type=".$typeid."&action=unlink&token=".newToken()."&removeelem=".$soc->id.($limit?'&limit='.$limit:'').'">';
 						print $langs->trans("DeleteFromCat");
 						print img_picto($langs->trans("DeleteFromCat"), 'unlink', '', false, 0, 0, '', 'paddingleft');
 						print "</a>";
