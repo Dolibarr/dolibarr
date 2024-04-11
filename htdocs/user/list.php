b40d5213f68528f35c4bebe586cfad80cd131764
Benjamin Falière <benjamin.faliere@altairis.fr>
2024-04-11 15:28:27 +0200

    FIX syntax error

--- htdocs/user/list.php
+++ htdocs/user/list.php
@@ -238,7 +238,7 @@ if (!GETPOST('confirmmassaction', 'alpha') && $massaction != 'presend' && $massa
 	$massaction = '';
 }
 
-$parameters = array'arrayfields' => &$arrayfields);
+$parameters = array('arrayfields' => &$arrayfields);
 $reshook = $hookmanager->executeHooks('doActions', $parameters, $object, $action); // Note that $action and $object may have been modified by some hooks
 if ($reshook < 0) {
 	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
