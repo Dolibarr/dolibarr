00716ae41a3df0a3fbe90e0b00e860592490c1b0
Laurent Destailleur <eldy@destailleur.fr>
2024-04-14 01:30:23 +0200

    Doc

--- htdocs/install/mysql/tables/llx_categorie.sql
+++ htdocs/install/mysql/tables/llx_categorie.sql
@@ -1,8 +1,8 @@
 -- ============================================================================
 -- Copyright (C) 2005		Brice Davoleau		<e1davole@iu-vannes.fr>
 -- Copyright (C) 2005		Matthieu Valleton	<mv@seeschloss.org>
--- Copyright (C) 2005-2012	Regis Houssin		<regis.houssin@inodbox.com>		
--- Copyright (C) 2017       Laurent Destailleur  <eldy@users.sourceforge.net>
+-- Copyright (C) 2005-2012	Regis Houssin		<regis.houssin@inodbox.com>
+-- Copyright (C) 2017-2024  Laurent Destailleur <eldy@users.sourceforge.net>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -26,23 +26,15 @@ create table llx_categorie
 	fk_parent		integer DEFAULT 0 NOT NULL,
 	label 		    varchar(180) NOT NULL,				-- category ref/name
 	ref_ext			varchar(255),						-- reference into an external system (not used by dolibarr)
-	type	        integer DEFAULT 1 NOT NULL,			-- category type (product, supplier, customer, member)
+	type	        integer DEFAULT 1 NOT NULL,			-- category type (product, supplier, customer, member, ...). See array $MAP_ID into categorie.class.php for possible values.
 	description 	text,								-- description of the category
     color           varchar(8),                         -- color
 	position        integer DEFAULT 0,                  -- position
 	fk_soc          integer DEFAULT NULL,				-- not used by default. Used when option CATEGORY_ASSIGNED_TO_A_CUSTOMER is set.
 	visible         tinyint DEFAULT 1 NOT NULL,			-- determine if the products are visible or not
-    date_creation	datetime,							-- date creation 
+    date_creation	datetime,							-- date creation
     tms     		timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,							-- last modification date
     fk_user_creat	integer,							-- user making creation
     fk_user_modif	integer,							-- user making last change
     import_key      varchar(14)							-- Import key
 )ENGINE=innodb;
-
--- 
--- List of codes for the field type
---
--- 0 : product
--- 1 : supplier
--- 2 : customer
---
