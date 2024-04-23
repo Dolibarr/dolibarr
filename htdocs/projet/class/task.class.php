bf16701cd5c1d8a5260c478eb749ed35d2da5461
Eric Seigne <eric.seigne@cap-rel.fr>
2024-04-23 17:27:06 +0200

    sql error missing comma

--- htdocs/projet/class/task.class.php
+++ htdocs/projet/class/task.class.php
@@ -809,7 +809,7 @@ class Task extends CommonObject
 		$sql .= " p.rowid as projectid, p.ref, p.title as plabel, p.public, p.fk_statut as projectstatus, p.usage_bill_time,";
 		$sql .= " t.rowid as taskid, t.ref as taskref, t.label, t.description, t.fk_task_parent, t.duration_effective, t.progress, t.fk_statut as status,";
 		$sql .= " t.dateo as date_start, t.datee as date_end, t.planned_workload, t.rang, t.priority,";
-		$sql .= " t.note_public, t.note_private";
+		$sql .= " t.note_public, t.note_private,";
 		$sql .= " s.rowid as thirdparty_id, s.nom as thirdparty_name, s.email as thirdparty_email,";
 		$sql .= " p.fk_opp_status, p.opp_amount, p.opp_percent, p.budget_amount";
 		if (!empty($extrafields->attributes['projet']['label'])) {
