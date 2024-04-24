b46cebbaf5add71cc43723b6ac5487ec7ce681c3
Florian HENRY <florian.henry@open-concept.pro>
2024-04-24 22:24:56 +0200

    fix: when invoice is created by WORKFLOW_ORDER_AUTOCREATE_INVOICE on ORDER_CLOSE, the invoice must have the default bank account of the thirdparty is it's empty on order

--- htdocs/core/triggers/interface_20_modWorkflow_WorkflowManager.class.php
+++ htdocs/core/triggers/interface_20_modWorkflow_WorkflowManager.class.php
@@ -116,6 +116,14 @@ class InterfaceWorkflowManager extends DolibarrTriggers
 				if ($ret < 0) {
 					$this->error = $newobject->error;
 					$this->errors[] = $newobject->error;
+				} else {
+					if (empty($object->fk_account) && !empty($object->thirdparty->fk_account)) {
+						$res = $newobject->setBankAccount($object->thirdparty->fk_account, true, $user);
+						if ($ret < 0) {
+							$this->error = $newobject->error;
+							$this->errors[] = $newobject->error;
+						}
+					}
 				}
 
 				$object->clearObjectLinkedCache();
