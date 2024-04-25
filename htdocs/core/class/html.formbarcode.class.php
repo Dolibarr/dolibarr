8788e58f5702bf156fe74fbc22b335700c20293c
Philippe GRAND <philippe.grand@atoo-net.com>
2024-04-25 16:44:32 +0200

    fix : Warning: Undefined property: stdClass:: in /home/httpd/vhosts/aflac.fr/domains/dol190.aflac.fr/httpdocs/core/class/html.formbarcode.class.php on line 84

--- htdocs/core/class/html.formbarcode.class.php
+++ htdocs/core/class/html.formbarcode.class.php
@@ -81,7 +81,7 @@ class FormBarCode
 
 		// We check if barcode is already selected by default
 		if (((isModEnabled("product") || isModEnabled("service")) && getDolGlobalString('PRODUIT_DEFAULT_BARCODE_TYPE') == $code_id) ||
-		(isModEnabled("societe") && $conf->global->GENBARCODE_BARCODETYPE_THIRDPARTY == $code_id)) {
+		(isModEnabled("societe") && getDolGlobalString('GENBARCODE_BARCODETYPE_THIRDPARTY') == $code_id)) {
 			$disable = 'disabled';
 		}
 
