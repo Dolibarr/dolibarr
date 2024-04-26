fa24730647ed8b030d7408c0090764c681941d35
PsyCrow <93346975+PsyCrow-code@users.noreply.github.com>
2024-04-25 23:17:27 -0600

    Prevents PHP Warnings on multicurrency_rate.php
    Prevents PHP Warnings about undefined variables
    $type
    $texte
    $newcardbutton
    and Undefined array key "nbfield"

--- htdocs/multicurrency/multicurrency_rate.php
+++ htdocs/multicurrency/multicurrency_rate.php
@@ -64,6 +64,10 @@ $limit = GETPOST('limit', 'int') ? GETPOST('limit', 'int') : $conf->liste_limit;
 $sortfield 			= GETPOST('sortfield', 'aZ09comma');
 $sortorder 			= GETPOST('sortorder', 'aZ09comma');
 $page = (GETPOST("page", 'int') ? GETPOST("page", 'int') : 0);
+// Prevents PHP Warnings
+$type = ''; 			// PHP Warning:  Undefined variable $type on line 440
+$texte = ''; 			// PHP Warning:  Undefined variable $texte on line 442
+$newcardbutton = ''; 	// PHP Warning:  Undefined variable $newcardbutton on line 442
 
 if (empty($page) || $page == -1) {
 	$page = 0;
@@ -544,6 +548,7 @@ if ($resql) {
 
 	$i = 0;
 	$totalarray = array();
+	$totalarray['nbfield']=0;			// avoid PHP Warning:  Undefined array key "nbfield"
 	while ($i < min($num, $limit)) {
 		$obj = $db->fetch_object($resql);
 
