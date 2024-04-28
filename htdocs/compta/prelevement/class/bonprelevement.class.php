fb72227e9defeee69c46e17677b89ae707b74a9d
Laurent Destailleur <eldy@users.sourceforge.net>
2024-04-28 02:09:45 +0200

    Code comment

--- htdocs/compta/prelevement/class/bonprelevement.class.php
+++ htdocs/compta/prelevement/class/bonprelevement.class.php
@@ -3,11 +3,11 @@
  * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@inodbox.com>
  * Copyright (C) 2010-2015 Juanjo Menent        <jmenent@2byte.es>
  * Copyright (C) 2010-2014 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2014-2016 Ferran Marcet       <fmarcet@2byte.es>
- * Copyright (C) 2018      Nicolas ZABOURI     <info@inovea-conseil.com>
- * Copyright (C) 2019		JC Prieto			<jcprieto@virtual20.com><prietojc@gmail.com>
- * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
- * Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
+ * Copyright (C) 2014-2016 Ferran Marcet        <fmarcet@2byte.es>
+ * Copyright (C) 2018      Nicolas ZABOURI      <info@inovea-conseil.com>
+ * Copyright (C) 2019      JC Prieto			<jcprieto@virtual20.com><prietojc@gmail.com>
+ * Copyright (C) 2024      MDW					<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2024      Frédéric France      <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
