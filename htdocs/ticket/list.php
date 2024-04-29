a6a694cf5ebd8645938f982c431f646c7e97ddd9
ATM Irvine <irvine.fleith@atm-consulting.fr>
2024-04-29 14:33:24 +0200

    Added contracts lang

--- htdocs/ticket/list.php
+++ htdocs/ticket/list.php
@@ -38,7 +38,7 @@ include_once DOL_DOCUMENT_ROOT.'/core/class/html.formprojet.class.php';
 include_once DOL_DOCUMENT_ROOT.'/core/lib/project.lib.php';
 
 // Load translation files required by the page
-$langs->loadLangs(array("ticket", "companies", "other", "projects"));
+$langs->loadLangs(array("ticket", "companies", "other", "projects", "contracts"));
 
 // Get parameters
 $action     = GETPOST('action', 'aZ09') ? GETPOST('action', 'aZ09') : 'view'; // The action 'add', 'create', 'edit', 'update', 'view', ...
