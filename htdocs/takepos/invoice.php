3f414b3c75476cfe3ec626153f87bec0c79d1c20
Sami <sami.filali@atm-consulting.fr>
2024-04-30 16:51:09 +0200

    clean code

--- htdocs/takepos/invoice.php
+++ htdocs/takepos/invoice.php
@@ -1846,13 +1846,6 @@ if ($placeid > 0) {
 				if ($line->product_label) {
 					$htmlforlines .= $line->product_label;
 				}
-				if (!empty($line->batch)) {
-					$tooltiptext .= '<br><b>'.$langs->trans("LotSerial").'</b> : '.$line->batch.'<br>';
-				}
-				if (!empty($line->fk_warehouse)) {
-					$tooltiptext .= '<b>'.$langs->trans("Warehouse").'</b> : '.$line->fk_warehouse.'<br>';
-				}
-
 				if ($line->product_label != $line->desc) {
 					if ($line->product_label && $line->desc) {
 						$htmlforlines .= '<br>';
