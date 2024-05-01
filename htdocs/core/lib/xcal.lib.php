39b014c4ad7ebc21b3630d4196c396b13675fb1f
Laurent Destailleur <eldy@destailleur.fr>
2024-05-01 02:48:06 +0200

    Fix to add the logo image in some RSS files

--- htdocs/core/lib/xcal.lib.php
+++ htdocs/core/lib/xcal.lib.php
@@ -366,8 +366,9 @@ function build_rssfile($format, $title, $desc, $events_array, $outputfile, $filt
 		// Image
 		if (!empty($mysoc->logo_squarred_small)) {
 			$urlimage = $urlwithroot.'/viewimage.php?cache=1&amp;modulepart=mycompany&amp;file='.urlencode('logos/thumbs/'.$mysoc->logo_squarred_small);
-			if ($urlimage) {
-				fwrite($fichier, "<image><url><![CDATA[".$urlimage."]]></url><title>'.$title.</title></image>\n");
+			//$urlimage = $GLOBALS['website']->virtualhost
+			if ($urlimage && (empty($GLOBALS['website']) || preg_match('/'.preg_quote($GLOBALS['website']->virtualhost, '/').'/', $urlwithroot))) {
+				fwrite($fichier, "<image><url><![CDATA[".$urlimage."]]></url><title>'.$title.</title><link><![CDATA[".$url."]]></link></image>\n");
 			}
 		}
 
