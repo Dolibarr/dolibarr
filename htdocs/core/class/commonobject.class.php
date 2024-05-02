cc90397eb7d867a5e786795a21f36eda1dfc8622
Laurent Destailleur <eldy@destailleur.fr>
2024-05-02 13:14:30 +0200

    Try suggestion of mdeweerd

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -174,7 +174,7 @@ abstract class CommonObject
 	private $linkedObjectsFullLoaded = array();
 
 	/**
-	 * @var static	To store a cloned copy of the object before editing it (to keep track of its former properties)
+	 * @var ?static		To store a cloned copy of the object before editing it (to keep track of its former properties)
 	 */
 	public $oldcopy;
 
