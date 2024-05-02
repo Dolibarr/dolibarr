da4cb65d46e6076a753a6e02ffe34f5348beaf66
Frédéric FRANCE <frederic34@users.noreply.github.com>
2024-05-02 19:53:27 +0200

    fix phan

--- htdocs/takepos/invoice.php
+++ htdocs/takepos/invoice.php
@@ -629,7 +629,7 @@ if (empty($reshook)) {
 				echo "<center>".$langs->trans("SearchIntoBatch").": <b> $nbofsuggested </b></center><br><table>";
 				foreach ($prod->stock_warehouse as $tmpwarehouseid => $tmpval) {
 					if (getDolGlobalInt($constantforkey) && $tmpwarehouseid != getDolGlobalInt($constantforkey)) {
-						// Not on the forced warehous, so we ignore this warehous
+						// Not on the forced warehouse, so we ignore this warehouse
 						continue;
 					}
 					if (!empty($prod->stock_warehouse[$tmpwarehouseid]) && is_array($prod->stock_warehouse[$tmpwarehouseid]->detail_batch)) {
@@ -1832,8 +1832,8 @@ if ($placeid > 0) {
 					$htmlforlines .= img_object('', 'service').' ';
 				}
 			}
+			$tooltiptext = '';
 			if (!getDolGlobalString('TAKEPOS_SHOW_N_FIRST_LINES')) {
-				$tooltiptext = '';
 				if ($line->product_ref) {
 					$tooltiptext .= '<b>'.$langs->trans("Ref").'</b> : '.$line->product_ref.'<br>';
 					$tooltiptext .= '<b>'.$langs->trans("Label").'</b> : '.$line->product_label.'<br>';
