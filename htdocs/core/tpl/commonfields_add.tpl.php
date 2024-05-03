3bcd33b4d037ead84cfbec305ce26f9b34a1e4fd
Philippe Grand <philippe.grand@atoo-net.com>
2024-05-03 12:08:41 +0200

    Update commonfields_add.tpl.php
    fix tab/space

--- htdocs/core/tpl/commonfields_add.tpl.php
+++ htdocs/core/tpl/commonfields_add.tpl.php
@@ -37,10 +37,10 @@ $object->fields = dol_sort_array($object->fields, 'position');
 foreach ($object->fields as $key => $val) {
 	// Discard if field is a hidden field on form
 	// Ensure $val['visible'] is treated as an integer
-    $visible = (int)$val['visible'];
-    if (abs($visible) != 1 && abs($visible) != 3) {
-        continue;
-    }
+	$visible = (int)$val['visible'];
+	if (abs($visible) != 1 && abs($visible) != 3) {
+		continue;
+	}
 
 	if (array_key_exists('enabled', $val) && isset($val['enabled']) && !verifCond($val['enabled'])) {
 		continue; // We don't want this field
