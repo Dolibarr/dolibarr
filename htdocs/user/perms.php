e12fa90386bf8650ebb8675f7181cc926dc98e77
Laurent Destailleur <eldy@destailleur.fr>
2024-05-06 18:24:23 +0200

    FIX Code between perms.php differs from code into loadRights(). Bad test generates a list of permissions coming from a groupe wrong on a superadmin user.

--- htdocs/user/perms.php
+++ htdocs/user/perms.php
@@ -226,7 +226,10 @@ $sql = "SELECT DISTINCT gr.fk_id, gu.entity";	// fk_id are permission id and ent
 $sql .= " FROM ".MAIN_DB_PREFIX."usergroup_rights as gr,";
 $sql .= " ".MAIN_DB_PREFIX."usergroup_user as gu";	// all groups of a user
 $sql .= " WHERE gr.entity = ".((int) $entity);
-$sql .= " AND gu.entity =".((int) $entity);
+// The entity on the table gu=usergroup_user should be useless and should never be used because it is already into gr and r.
+// but when using MULTICOMPANY_TRANSVERSE_MODE, we may have inserted record that make rubbish result here due to the duplicate record of
+// other entities, so we are forced to add a filter on gu here
+$sql .= " AND gu.entity IN (0,".$conf->entity.")";
 $sql .= " AND gr.fk_usergroup = gu.fk_usergroup";
 $sql .= " AND gu.fk_user = ".((int) $object->id);
 
