c46d209e08c9dae2fa3ed2c037e6c0a97b317dfe
Laurent Destailleur <eldy@destailleur.fr>
2024-05-07 16:25:52 +0200

    Fix typo

--- htdocs/webportal/admin/setup.php
+++ htdocs/webportal/admin/setup.php
@@ -93,7 +93,7 @@ if (isModEnabled('invoice')) {
 }
 
 // Enable access for the partnership record
-if (isModEnabled('patnership')) {
+if (isModEnabled('partnership')) {
 	$access_list = array(
 		'hidden' => $langs->trans('WebPortalAccessHidden'),
 		'visible' => $langs->trans('WebPortalAccessVisible'),
