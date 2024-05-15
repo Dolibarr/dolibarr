83c04db0ef21d5bad0cec36c212c6c1b1cecc850
Frédéric FRANCE <frederic34@users.noreply.github.com>
2024-05-15 18:07:05 +0200

    fix phpunit (#29696)
    * fix phpunit
    
    * fix phpunit
    
    * fix phpunit

--- htdocs/webhook/ajax/webhook.php
+++ htdocs/webhook/ajax/webhook.php
@@ -1,5 +1,6 @@
 <?php
-/*
+/* Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
