00a38170faabb9b3284bdc1b60f21b36ae87352a
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2024-05-20 08:17:38 +0200

    FIX Accountancy - Avoid space on FEC name file

--- htdocs/accountancy/tpl/export_journal.tpl.php
+++ htdocs/accountancy/tpl/export_journal.tpl.php
@@ -63,7 +63,7 @@ if (($accountancyexport->getFormatCode($formatexportset) == 'fec' || $accountanc
 	}
 
 	$endaccountingperiod = dol_print_date(dol_get_last_day($tmparray['year'], $tmparray['mon']), 'dayxcard');
-
+	$siren = str_replace(" ", "", $siren);
 	$completefilename = $siren."FEC".$endaccountingperiod.".txt";
 } elseif ($accountancyexport->getFormatCode($formatexportset) == 'ciel' && $type_export == "general_ledger" && getDolGlobalString('ACCOUNTING_EXPORT_XIMPORT_FORCE_FILENAME')) {
 	$completefilename = "XIMPORT.TXT";
