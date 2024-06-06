0c522f379479ed7761ffc0db35858598ce458a7c
Florian HENRY <florian.henry@open-concept.pro>
2024-06-06 17:34:33 +0200

    fix: SQL fresh install script

--- htdocs/install/mysql/tables/llx_receptiondet_batch.key.sql
+++ htdocs/install/mysql/tables/llx_receptiondet_batch.key.sql
@@ -16,7 +16,6 @@
 --
 -- ===================================================================
 
-ALTER TABLE llx_receptiondet_batch ADD INDEX idx_receptiondet_batch_fk_commande (fk_commande);
 ALTER TABLE llx_receptiondet_batch ADD INDEX idx_receptiondet_batch_fk_reception (fk_reception);
 ALTER TABLE llx_receptiondet_batch ADD CONSTRAINT fk_receptiondet_batch_fk_reception FOREIGN KEY (fk_reception) REFERENCES llx_reception (rowid);
 ALTER TABLE llx_receptiondet_batch ADD INDEX idx_receptiondet_batch_fk_product (fk_product);
