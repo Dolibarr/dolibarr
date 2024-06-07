51ba0bbde787ed751ca919c773ef2c9e913f097f
Rémi Champlon <champlonremii@gmail.com>
2024-06-07 18:31:07 +0200

    Add pagination date

--- htdocs/expedition/class/api_shipments.class.php
+++ htdocs/expedition/class/api_shipments.class.php
@@ -95,11 +95,12 @@ class Shipments extends DolibarrApi
 	 * @param string		   $thirdparty_ids		Thirdparty ids to filter shipments of (example '1' or '1,2,3') {@pattern /^[0-9,]*$/i}
 	 * @param string           $sqlfilters          Other criteria to filter answers separated by a comma. Syntax example "(t.ref:like:'SO-%') and (t.date_creation:<:'20160101')"
 	 * @param string		   $properties			Restrict the data returned to these properties. Ignored if empty. Comma separated list of properties names
-	 * @return  array                               Array of shipment objects
+     * @param bool             $pagination_data     If this parameter is set to true the response will include pagination data. Default value is false. Page starts from 0*
+     * @return  array                               Array of shipment objects
 	 *
 	 * @throws RestException
 	 */
-	public function index($sortfield = "t.rowid", $sortorder = 'ASC', $limit = 100, $page = 0, $thirdparty_ids = '', $sqlfilters = '', $properties = '')
+	public function index($sortfield = "t.rowid", $sortorder = 'ASC', $limit = 100, $page = 0, $thirdparty_ids = '', $sqlfilters = '', $properties = '', $pagination_data = false)
 	{
 		if (!DolibarrApiAccess::$user->hasRight('expedition', 'lire')) {
 			throw new RestException(403);
@@ -140,7 +141,10 @@ class Shipments extends DolibarrApi
 			}
 		}
 
-		$sql .= $this->db->order($sortfield, $sortorder);
+        //this query will return total shipments with the filters given
+        $sqlTotals =  str_replace('SELECT t.rowid', 'SELECT count(t.rowid) as total', $sql);
+
+        $sql .= $this->db->order($sortfield, $sortorder);
 		if ($limit) {
 			if ($page < 0) {
 				$page = 0;
@@ -168,6 +172,23 @@ class Shipments extends DolibarrApi
 		} else {
 			throw new RestException(503, 'Error when retrieve commande list : '.$this->db->lasterror());
 		}
+
+        //if $pagination_data is true the response will contain element data with all values and element pagination with pagination data(total,page,limit)
+        if ($pagination_data) {
+            $totalsResult = $this->db->query($sqlTotals);
+            $total = $this->db->fetch_object($totalsResult)->total;
+
+            $tmp = $obj_ret;
+            $obj_ret = [];
+
+            $obj_ret['data'] = $tmp;
+            $obj_ret['pagination'] = [
+                'total' => (int) $total,
+                'page' => $page, //count starts from 0
+                'page_count' => ceil((int) $total/$limit),
+                'limit' => $limit
+            ];
+        }
 
 		return $obj_ret;
 	}
