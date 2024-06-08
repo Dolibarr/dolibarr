4052fd600b3c023f2c4bff11748db8a4ca4f0ad0
Vincent de Grandpré <vincent@de-grandpre.quebec>
2024-06-07 23:50:21 -0400

    oups pré-commit

--- htdocs/projet/tasks/time.php
+++ htdocs/projet/tasks/time.php
@@ -2439,7 +2439,7 @@ if (($id > 0 || !empty($ref)) || $projectidforalltimes > 0 || $allprojectforuser
 							}
 							$invoiced = true;
 						} else {
-							if($task_time->options_invoiceable == "1") {
+							if ($task_time->options_invoiceable == "1") {
 								print $langs->trans("No");
 							} else {
 								print $langs->trans("No")." ".$langs->trans("Invoiceable");
@@ -2498,7 +2498,7 @@ if (($id > 0 || !empty($ref)) || $projectidforalltimes > 0 || $allprojectforuser
 							// Disable select if task not invoiceable or already invoiced
 							$disabled = ($task_time->options_invoiceable!="1"||$invoiced);
 							$ctrl = '<input '.($disabled?'disabled':'').' id="cb' . $task_time->rowid . '" class="flat checkforselect marginleftonly" type="checkbox" name="toselect[]" value="' . $task_time->rowid . '"' . ($selected ? ' checked="checked"' : '') . '>';
-							if($disabled){
+							if ($disabled) {
 								// If disabled, a dbl-click very close outside the control
 								// will re-enable it, so that user is not blocked if needed.
 								print '<span id="cbsp'. $task_time->rowid . '">'.$ctrl.'</span>';
