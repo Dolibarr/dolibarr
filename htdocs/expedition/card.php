114ecfcee1691947e0ee93e2fd1171cbd189fe64
Zephyriony <142790847+zephyriony@users.noreply.github.com>
2024-06-08 12:04:48 +0200

    Update card.php

--- htdocs/expedition/card.php
+++ htdocs/expedition/card.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 /* Copyright (C) 2003-2008	Rodolphe Quiedeville	<rodolphe@quiedeville.org>
  * Copyright (C) 2005-2016	Laurent Destailleur		<eldy@users.sourceforge.net>
  * Copyright (C) 2005		Simon TOSSER			<simon@kornog-computing.com>
