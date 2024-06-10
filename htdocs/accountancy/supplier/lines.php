e23f2c23637b1259259c770ef8ee11fe3746be53
Laurent Destailleur <eldy@destailleur.fr>
2024-06-10 20:26:15 +0200

    css

--- htdocs/accountancy/supplier/lines.php
+++ htdocs/accountancy/supplier/lines.php
@@ -503,7 +503,8 @@ if ($result) {
 		print '<td>'.$objp->rowid.'</td>';
 
 		// Ref Invoice
-		print '<td class="nowraponall">'.$facturefournisseur_static->getNomUrl(1);
+		print '<td class="nowraponall tdoverflowmax125">';
+		print $facturefournisseur_static->getNomUrl(1);
 		if ($objp->ref_supplier) {
 			print '<br><span class="opacitymedium small">'.dol_escape_htmltag($objp->ref_supplier).'</span>';
 		}
@@ -551,7 +552,7 @@ if ($result) {
 		print '<td class="tdoverflowmax100">'.$thirdpartystatic->getNomUrl(1, 'supplier').'</td>';
 
 		// Country
-		print '<td>';
+		print '<td class="tdoverflowmax100">';
 		if ($objp->country_code) {
 			print $langs->trans("Country".$objp->country_code).' ('.$objp->country_code.')';
 		}
