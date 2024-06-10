f10ed2c12ec4802e183109cb264b2bb87da2861e
jpb <jean-pascal.boudet@atm-consulting>
2024-06-10 11:43:38 +0200

    fix extrafield input with the type int mus show 0 value if exist

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -8624,7 +8624,9 @@ abstract class CommonObject
 								} else {
 									$value = $getposttemp;
 								}
-							} else {
+							} elseif (in_array($extrafields->attributes[$this->table_element]['type'][$key], array('int')))   {
+								$value =  ( !empty($this->array_options["options_".$key]) || $this->array_options["options_".$key] === '0' ) ? $this->array_options["options_".$key] : '';
+							}else{
 								$value = (!empty($this->array_options["options_".$key]) ? $this->array_options["options_".$key] : ''); // No GET, no POST, no default value, so we take value of object.
 							}
 							//var_dump($keyprefix.' - '.$key.' - '.$keysuffix.' - '.$keyprefix.'options_'.$key.$keysuffix.' - '.$this->array_options["options_".$key.$keysuffix].' - '.$getposttemp.' - '.$value);
