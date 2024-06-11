27d92266fbdd743cc84b47bc159adaaa1f0a64b7
sonikf <93765174+sonikf@users.noreply.github.com>
2024-06-11 21:04:30 +0300

    Add missing names and fix translation for non latin languages (#29984)
    * Add missing name
    
    * Add missing name
    
    * fix translation for non latin languages
    
    * fix translation for non latin languages
    
    * Update product_lot_extrafields.php
    
    * Add missing name
    
    * Add missing name
    
    * Add missing name
    
    * Add missing name
    
    * Add missing name
    
    * Add missing name
    
    * Add missing name

--- htdocs/admin/expensereport_extrafields.php
+++ htdocs/admin/expensereport_extrafields.php
@@ -69,6 +69,8 @@ require DOL_DOCUMENT_ROOT.'/core/actions_extrafields.inc.php';
  * View
  */
 
+$textobject = $langs->transnoentitiesnoconv("ExpenseReports");
+
 llxHeader('', $langs->trans("ExpenseReportsSetup"));
 
 $linkback = '<a href="'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans("BackToModuleList").'</a>';
