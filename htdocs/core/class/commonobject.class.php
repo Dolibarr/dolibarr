03c76eb6b190994b38c4e2b59a5ab99e65cd7dca
Frédéric France <frederic.france@free.fr>
2024-06-12 17:53:53 +0200

    fix phpstan Property Product::$oldcopy in empty() is not falsy

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -935,6 +935,16 @@ abstract class CommonObject
 	}
 
 	/**
+	 * isEmpty We consider CommonObject isEmpty if id equal zéro
+	 *
+	 * @return bool
+	 */
+	public function isEmpty()
+	{
+		return (empty($this->id));
+	}
+
+	/**
 	 * setErrorsFromObject
 	 *
 	 * @param CommonObject $object commonobject
