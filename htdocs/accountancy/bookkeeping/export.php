ae5ded8a273c9d23b6e8ac958d67f49643ff3b65
Pichi1966 <57623859+josett225@users.noreply.github.com>
2024-06-12 17:54:53 +0200

    FIX : issue to get the right files exported in Quadratrus export.php
    The export of files does not work correctly when using Accounting option export Quadratus with file export.
    The files are not attached to the zip until we uncommment the 3 lines of this PR.

--- htdocs/accountancy/bookkeeping/export.php
+++ htdocs/accountancy/bookkeeping/export.php
@@ -1190,7 +1190,7 @@ while ($i < min($num, $limit)) {
 			require_once DOL_DOCUMENT_ROOT.'/compta/facture/class/facture.class.php';
 			$objectstatic = new Facture($db);
 			$objectstatic->fetch($line->fk_doc);
-			//$modulepart = 'facture';
+			$modulepart = 'facture'; // or use $objectstatic->element
 
 			$filename = dol_sanitizeFileName($line->doc_ref);
 			$filedir = $conf->facture->dir_output.'/'.dol_sanitizeFileName($line->doc_ref);
@@ -1202,7 +1202,7 @@ while ($i < min($num, $limit)) {
 			require_once DOL_DOCUMENT_ROOT.'/fourn/class/fournisseur.facture.class.php';
 			$objectstatic = new FactureFournisseur($db);
 			$objectstatic->fetch($line->fk_doc);
-			//$modulepart = 'invoice_supplier';
+			$modulepart = 'invoice_supplier'; // or use $objectstatic->element
 
 			$filename = dol_sanitizeFileName($line->doc_ref);
 			$filedir = $conf->fournisseur->facture->dir_output.'/'.get_exdir($line->fk_doc, 2, 0, 0, $objectstatic, $modulepart).dol_sanitizeFileName($line->doc_ref);
@@ -1214,7 +1214,7 @@ while ($i < min($num, $limit)) {
 			require_once DOL_DOCUMENT_ROOT.'/expensereport/class/expensereport.class.php';
 			$objectstatic = new ExpenseReport($db);
 			$objectstatic->fetch($line->fk_doc);
-			//$modulepart = 'expensereport';
+			$modulepart = 'expensereport'; // or use $objectstatic->element
 
 			$filename = dol_sanitizeFileName($line->doc_ref);
 			$filedir = $conf->expensereport->dir_output.'/'.dol_sanitizeFileName($line->doc_ref);
