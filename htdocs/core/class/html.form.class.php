607186ad6119605284d21ddf150af5a00321710d
Eric Seigne <eric.seigne@cap-rel.fr>
2024-06-12 08:18:04 +0200

    add check if tmparray is an array

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -8067,7 +8067,7 @@ class Form
 					}
 				} else {
 					// For a property in ->fields
-					if (array_key_exists($tmparray[1], $objectforfieldstmp->fields)) {
+					if (is_array($tmparray) && array_key_exists($tmparray[1], $objectforfieldstmp->fields)) {
 						$objectdesc = $objectforfieldstmp->fields[$tmparray[1]]['type'];
 						$objectdesc = preg_replace('/^integer[^:]*:/', '', $objectdesc);
 					}
