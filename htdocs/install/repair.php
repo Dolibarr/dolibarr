14ca7e80a69f119c8f5e02f40ab76163922751d2
Laurent Destailleur <eldy@destailleur.fr>
2024-06-12 14:34:24 +0200

    Fix test

--- htdocs/install/repair.php
+++ htdocs/install/repair.php
@@ -108,7 +108,7 @@ print 'Option set_empty_time_spent_amount is '.(GETPOST('set_empty_time_spent_am
 print 'Option force_utf8_on_tables (force utf8 + row=dynamic), for mysql/mariadb only is '.(GETPOST('force_utf8_on_tables', 'alpha') ? GETPOST('force_utf8_on_tables', 'alpha') : 'undefined').'<br>'."\n";
 print '<span class="valignmiddle">'."Option force_utf8mb4_on_tables (force utf8mb4 + row=dynamic, EXPERIMENTAL!), for mysql/mariadb only ('test' or 'confirmed') is ".(GETPOST('force_utf8mb4_on_tables', 'alpha') ? GETPOST('force_utf8mb4_on_tables', 'alpha') : 'undefined');
 print '</span>';
-if ($dolibarr_main_db_character_set == 'utf8mb4') {
+if ($dolibarr_main_db_character_set != 'utf8mb4') {
 	print '<img src="../theme/eldy/img/warning.png" class="pictofortooltip valignmiddle" title="If you switch to utf8mb4, you must also check the value for $dolibarr_main_db_character_set and $dolibarr_main_db_collation into conf/conf.php file.">';
 }
 print "<br>\n";
