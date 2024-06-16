b16303f0ed6ff4077f051b545344fa694b07a12b
Frédéric France <frederic.france@free.fr>
2024-06-16 09:45:07 +0200

    fix phpstan

--- htdocs/core/modules/import/modules_import.php
+++ htdocs/core/modules/import/modules_import.php
@@ -2,6 +2,7 @@
 /* Copyright (C) 2005-2012	Laurent Destailleur	<eldy@users.sourceforge.net>
  * Copyright (C) 2005-2012	Regis Houssin		<regis.houssin@inodbox.com>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -75,7 +76,7 @@ class ModeleImports
 	 * PHP minimal version required by driver
 	 * @var array{0:int,1:int}
 	 */
-	public $phpmin = array();
+	public $phpmin = array(7, 0);
 
 	/**
 	 * Label of external lib used by driver
