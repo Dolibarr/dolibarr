d502d8a6a3c454068539b3ce949d4c34f508d9b6
Zephyriony <142790847+zephyriony@users.noreply.github.com>
2024-06-18 18:06:13 +0200

    Update list.php

--- htdocs/commande/list.php
+++ htdocs/commande/list.php
@@ -557,7 +557,7 @@ if (empty($reshook)) {
 				}
 			}
 
-			$objecttmp->note_public = dol_concatdesc($objecttmp->note_public, $langs->transnoentities($cmd->ref).(empty($cmd->ref_client) ? '' : ' ('.$cmd->ref_client.')').(empty($cmd->ref_supplier) ? '' : ' ('.$cmd->ref_supplier.')').' ');
+			$objecttmp->note_public = dol_concatdesc($objecttmp->note_public, $langs->transnoentities($cmd->ref).(empty($cmd->ref_client) ? '' : ' ('.$cmd->ref_client.')'));
 			$objecttmp->update($user);
 
 			//$cmd->classifyBilled($user);        // Disabled. This behavior must be set or not using the workflow module.
