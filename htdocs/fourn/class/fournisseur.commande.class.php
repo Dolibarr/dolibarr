fceb29973a5691763eaf72ee98bc16d662c7900c
Benjamin Falière <benjamin.faliere@altairis.fr>
2024-06-18 15:02:18 +0200

    NEW(CommandeFournisseur): add proporty CommandeFournisseur:class_element_line

--- htdocs/fourn/class/fournisseur.commande.class.php
+++ htdocs/fourn/class/fournisseur.commande.class.php
@@ -65,6 +65,11 @@ class CommandeFournisseur extends CommonOrder
 	public $table_element_line = 'commande_fournisseurdet';
 
 	/**
+	 * @var string Name of classe line
+	 */
+	public $class_element_line = 'CommandeFournisseurLigne';
+
+	/**
 	 * @var string Field with ID of parent key if this field has a parent
 	 */
 	public $fk_element = 'fk_commande';
