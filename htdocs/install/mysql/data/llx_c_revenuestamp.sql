6b410ffe6a9d2779b818f8ada8185479fa0c8e02
sonikf <93765174+sonikf@users.noreply.github.com>
2024-06-19 14:43:32 +0300

    Add revenuestamp rates for Greece

--- htdocs/install/mysql/data/llx_c_revenuestamp.sql
+++ htdocs/install/mysql/data/llx_c_revenuestamp.sql
@@ -1,4 +1,4 @@
--- Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
+-- Copyright (C) 2004-2024 Laurent Destailleur  <eldy@users.sourceforge.net>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
