3c86acf744c52389a9d0f9598373ae29fc3b4f92
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2024-06-22 08:18:45 +0200

    Situation invoice #29897 -  Add admin message to prevent to  move to progressive mode with an help of a preferred partner

--- htdocs/accountancy/index.php
+++ htdocs/accountancy/index.php
@@ -1,7 +1,7 @@
 <?php
-/* Copyright (C) 2016-2020  Laurent Destailleur		<eldy@users.sourceforge.net>
- * Copyright (C) 2016-2023  Alexandre Spangaro		<aspangaro@easya.solutions>
- * Copyright (C) 2019-2021  Frédéric France			<frederic.france@netlogic.fr>
+/* Copyright (C) 2016-2020  Laurent Destailleur     <eldy@users.sourceforge.net>
+ * Copyright (C) 2016-2024  Alexandre Spangaro      <alexandre@inovea-conseil.com>
+ * Copyright (C) 2019-2021  Frédéric France         <frederic.france@netlogic.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -124,6 +124,7 @@ if (isModEnabled('accounting')) {
 
 	if (getDolGlobalInt('INVOICE_USE_SITUATION') == 1) {
 		print info_admin($langs->trans("SorryThisModuleIsNotCompatibleWithTheExperimentalFeatureOfSituationInvoices"));
+		print info_admin($langs->trans("WarningExperimentalFeatureInvoiceSituationNeedToUpgradeToProgressiveMode"));
 		print "<br>";
 	}
 
