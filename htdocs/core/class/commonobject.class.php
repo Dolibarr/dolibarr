a98ff8b076cf081f3d45692a5a8ff9bcba670e20
ICMathieu <mathieu@inovea-conseil.com>
2024-06-26 15:03:53 +0200

    NEW_CARE_GRANDCHILD

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -3325,7 +3325,8 @@ abstract class CommonObject
 
 			dol_syslog(get_class($this)."::line_order search all parent lines", LOG_DEBUG);
 			$resql = $this->db->query($sql);
-			$grandchild = empty(getDolGlobalInt('CARE_GRANDCHILD')) ? 0 : getDolGlobalInt('CARE_GRANDCHILD');
+			$grandchild = getDolGlobalInt('CARE_GRANDCHILD');
+			$grandchild = empty($grandchild) ? 0 : getDolGlobalInt('CARE_GRANDCHILD');
 			if ($resql) {
 				$i = 0;
 				$num = $this->db->num_rows($resql);
