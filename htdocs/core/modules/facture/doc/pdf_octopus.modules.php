4b7cf4cbfce69f802188fb408a7f52ffa34da593
Frédéric France <frederic.france@free.fr>
2024-06-29 20:46:34 +0200

    fix deprecated

--- htdocs/core/modules/facture/doc/pdf_octopus.modules.php
+++ htdocs/core/modules/facture/doc/pdf_octopus.modules.php
@@ -2148,7 +2148,7 @@ class pdf_octopus extends ModelePDFFactures
 		}
 
 		if (getDolGlobalString('PDF_SHOW_PROJECT_TITLE')) {
-			$object->fetch_projet();
+			$object->fetchProject();
 			if (!empty($object->project->ref)) {
 				$posy += 3;
 				$pdf->SetXY($posx, $posy);
@@ -2158,7 +2158,7 @@ class pdf_octopus extends ModelePDFFactures
 		}
 
 		if (getDolGlobalString('PDF_SHOW_PROJECT')) {
-			$object->fetch_projet();
+			$object->fetchProject();
 			if (!empty($object->project->ref)) {
 				$outputlangs->load("projects");
 				$posy += 3;
