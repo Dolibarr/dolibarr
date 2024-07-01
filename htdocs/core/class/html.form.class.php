cb3a581b90c37c53b626ded0f6fa31d96dc0c083
Charlène Benke <1179011+defrance@users.noreply.github.com>
2024-07-02 00:33:02 +0200

    Update html.form.class.php

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -9403,6 +9403,8 @@ class Form
 					if (!isModEnabled('mrp')) {
 						continue; // Do not show if module disabled
 					}
+				} elseif ($objecttype == 'project_task') {
+					$tplpath = 'projet/tasks';
 				}
 
 				global $linkedObjectBlock;
