aafd23d501820b781b48aafc33c0eb67bc04f222
Frédéric France <frederic.france@free.fr>
2024-07-02 20:21:57 +0200

    fix checkFunctionArgumentTypes

--- htdocs/core/modules/facture/doc/pdf_crabe.modules.php
+++ htdocs/core/modules/facture/doc/pdf_crabe.modules.php
@@ -16,8 +16,21 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.		- '#expects float#'
-e.modules.php
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * or see https://www.gnu.org/
+ *
+ */
+
+ /**
+ *	\file       htdocs/core/modules/facture/doc/pdf_crabe.modules.php
  *	\ingroup    invoice
  *	\brief      File of class to generate customers invoices from crabe model
  */
