384ce063e134be4e6bf0e1a905fa7426d2120cf9
mrozniecki <mrozniecki@easya.solutions>
2024-07-10 17:06:10 +0200

    Fix precommit

--- htdocs/admin/openid_connect.php
+++ htdocs/admin/openid_connect.php
@@ -121,7 +121,7 @@ $form = new Form($db);
 llxHeader();
 
 $linkback='<a href="'.DOL_URL_ROOT.'/admin/modules.php">'.$langs->trans("BackToModuleList").'</a>';
-print load_fiche_titre($langs->trans("OpenIDconnectSetup"),$linkback,'title_setup');
+print load_fiche_titre($langs->trans("OpenIDconnectSetup"), $linkback, 'title_setup');
 print "<br>\n";
 
 $head = openid_connect_prepare_head();
