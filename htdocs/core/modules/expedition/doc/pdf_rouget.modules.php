a3aa5b493016060507938cfbb668f2e7d4ed123d
VESSILLER <lvessiller@open-dsi.fr>
2024-07-12 14:28:47 +0200

    FIX expedition PDF models using units labels

--- htdocs/core/modules/expedition/doc/pdf_rouget.modules.php
+++ htdocs/core/modules/expedition/doc/pdf_rouget.modules.php
@@ -734,11 +734,11 @@ class pdf_rouget extends ModelePdfExpedition
 			$object->volume_units = $object->size_units * 3;
 		}
 
-		if ($totalWeight != '') {
-			$totalWeighttoshow = showDimensionInBestUnit($totalWeight, 0, "weight", $outputlangs);
+		if (!empty($totalWeight)) {
+			$totalWeighttoshow = showDimensionInBestUnit($totalWeight, 0, "weight", $outputlangs, -1, 'no', 1);
 		}
-		if ($totalVolume != '') {
-			$totalVolumetoshow = showDimensionInBestUnit($totalVolume, 0, "volume", $outputlangs);
+		if (!empty($totalVolume)) {
+			$totalVolumetoshow = showDimensionInBestUnit($totalVolume, 0, "volume", $outputlangs, -1, 'no', 1);
 		}
 		if (!empty($object->trueWeight)) {
 			$totalWeighttoshow = showDimensionInBestUnit($object->trueWeight, $object->weight_units, "weight", $outputlangs);
