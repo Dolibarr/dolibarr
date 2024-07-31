bf023ab8d47fea72933df5e0e3d0eccfd9681d56
MDW <mdeweerd@users.noreply.github.com>
2024-07-31 19:16:43 +0200

    Fix phpdoc for phpstan

--- htdocs/variants/class/ProductCombination2ValuePair.class.php
+++ htdocs/variants/class/ProductCombination2ValuePair.class.php
@@ -179,8 +179,8 @@ class ProductCombination2ValuePair
 	/**
 	 * Delete all ProductCombination2ValuePair linked to a given ProductCombination ID.
 	 *
-	 * @param       int     	$fk_combination         ID of the ProductCombination
-	 * @return      int<-1|1>                            -1 if KO, 1 if OK
+	 * @param       int					$fk_combination	ID of the ProductCombination
+	 * @return      int<-1,-1>|int<1,1>					-1 if KO, 1 if OK
 	 */
 	public function deleteByFkCombination($fk_combination)
 	{
