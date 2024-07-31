31a560422d53adbe51518ea05feda3ba122887c9
lamrani abdelwadoud <lamrani002@gmail.com>
2024-07-31 19:12:40 +0200

    New Preview mai topic value on Ckeditor  (#30195)
    * New update for templateMail by creating files
    
    * New updating for get php content
    
    * Update html.formmail.class.php
    
    * Fix updating content news
    
    * Fix CTI Errors
    
    * edit html file for news
    
    * Add automaticaly subject value to editor
    
    * Update html.formmail.class.php
    
    * FIX name and path of file
    
    * Fix path for files
    
    * Fix errors variables
    
    * replace file in ajax folder
    
    * rename path
    
    * update the news file
    
    * fix phan errors
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

---
+++ htdocs/core/ajax/mailtemplate.php
@@ -0,0 +1,58 @@
+<?php
+
+// Just for display errors in editor
+ini_set('display_errors', 1);
+
+if (!defined('NOTOKENRENEWAL')) {
+	define('NOTOKENRENEWAL', '1'); // Disables token renewal
+}
+if (!defined('NOREQUIREMENU')) {
+	define('NOREQUIREMENU', '1');
+}
+if (!defined('NOREQUIREHTML')) {
+	define('NOREQUIREHTML', '1');
+}
+if (!defined('NOREQUIREAJAX')) {
+	define('NOREQUIREAJAX', '1');
+}
+if (!defined('NOREQUIRESOC')) {
+	define('NOREQUIRESOC', '1');
+}
+require_once '../../main.inc.php';
+require_once '../lib/files.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
+
+top_httphead();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && GETPOSTISSET('content')) {
+	$content = GETPOST('content');
+
+	if (!empty($selectedPosts)) {
+		$newsList = '';
+
+		foreach ($selectedPosts as $post) {
+			$newsList .= '<div style="display: flex; align-items: flex-start; justify-content: flex-start; width: 100%; max-width: 800px; margin-top: 20px;margin-bottom: 50px; padding: 20px;">
+                            <div style="flex-grow: 1; margin-right: 30px; max-width: 600px; margin-left: 100px;">
+                                <h2 style="margin: 0; font-size: 1.5em;">' . htmlentities($post['title']) . '</h2>
+                                <p style="margin: 10px 0; color: #555;">' . htmlentities($post['description']) . '</p>
+                                <span style="display: block; margin-bottom: 5px; color: #888;">Created By: <strong>' . htmlentities($post['user_fullname']) . '</strong></span>
+                                <br>
+                                <span style="display: block; color: #888;">' . dol_print_date($post['date_creation'], 'daytext', 'tzserver', $langs) . '</span>
+                            </div>
+                            <div style="flex-shrink: 0; margin-left: 100px; float: right;">
+                                ' . ($post['image'] ? '<img alt="Image" width="130px" height="130px" style="border-radius: 10px;" src="' . DOL_URL_ROOT . '/viewimage.php?modulepart=medias&file=' . htmlentities($post['image']) . '">' : '<img alt="Gray rectangle" width="130px" height="130px" style="border-radius: 10px;" src="__GRAY_RECTANGLE__">') . '
+                            </div>
+                        </div>';
+		}
+
+		$content = str_replace('__NEWS_LIST__', $newsList, $content);
+	} else {
+		$content = str_replace('__NEWS_LIST__', 'No articles selected', $content);
+	}
+
+
+	print $content;
+} else {
+	print 'No content provided or invalid token';
+}
