ff8310ac5322f338a10479d81a1d2cdfd13d51f2
Florent Poinsaut <florent@poinsaut.fr>
2024-08-01 14:16:59 +0200

    FIX #30520 Creator user data displayed in the form after an error in the user creation form

--- htdocs/user/card.php
+++ htdocs/user/card.php
@@ -75,7 +75,7 @@ $group = GETPOSTINT("group", 3);
 $cancel		= GETPOST('cancel', 'alpha');
 $contextpage = GETPOST('contextpage', 'aZ') ? GETPOST('contextpage', 'aZ') : 'useracard'; // To manage different context of search
 
-if (empty($id) && $action != 'create') {
+if (empty($id) && $action != 'add' && $action != 'create') {
 	$id = $user->id;
 }
 
