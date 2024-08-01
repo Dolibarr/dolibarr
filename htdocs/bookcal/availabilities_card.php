f63b1e32bf282974554fdfde64d22fabc22438b6
Frédéric FRANCE <frederic.france@free.fr>
2024-08-01 16:15:48 +0200

    fix loadxxx expects int

--- htdocs/bookcal/availabilities_card.php
+++ htdocs/bookcal/availabilities_card.php
@@ -1,6 +1,7 @@
 <?php
-/* Copyright (C) 2017 Laurent Destailleur  <eldy@users.sourceforge.net>
- * Copyright (C) 2022 Alice Adminson <aadminson@example.com>
+/* Copyright (C) 2017 		Laurent Destailleur  	<eldy@users.sourceforge.net>
+ * Copyright (C) 2022 		Alice Adminson 			<aadminson@example.com>
+ * Copyright (C) 2024		Frédéric France			<frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -248,7 +249,7 @@ if ($action == 'create') {
 		exit;
 	}
 
-	print load_fiche_titre($langs->trans("NewAvailabilities"), '', '', 'object_'.$object->picto);
+	print load_fiche_titre($langs->trans("NewAvailabilities"), '', 'object_'.$object->picto);
 	print '<form method="POST" action="'.$_SERVER["PHP_SELF"].'">';
 	print '<input type="hidden" name="token" value="'.newToken().'">';
 	if ($error != 0) {
