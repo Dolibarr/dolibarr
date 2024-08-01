7c0e56dddb024439b2cfc082dec43485d87264c0
MDW <mdeweerd@users.noreply.github.com>
2024-08-01 04:47:10 +0200

    Add abstract write_file to ModelePDFFactures

--- htdocs/admin/invoice.php
+++ htdocs/admin/invoice.php
@@ -114,7 +114,7 @@ if ($action == 'updateMask') {
 		require_once $file;
 
 		$module = new $classname($db);
-		'@phan-var-force CommonDocGenerator $module';
+		'@phan-var-force ModelePDFFactures $module';
 
 		if ($module->write_file($facture, $langs) > 0) {
 			header("Location: ".DOL_URL_ROOT."/document.php?modulepart=facture&file=SPECIMEN.pdf");
