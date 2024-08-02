98aef837acb56066b17927a589e2e09cfa26a018
MDW <mdeweerd@users.noreply.github.com>
2024-08-02 19:52:05 +0200

    Qual: Simplify expression (phan notice)

--- htdocs/bookcal/class/availabilities.class.php
+++ htdocs/bookcal/class/availabilities.class.php
@@ -969,7 +969,7 @@ class Availabilities extends CommonObject
 				$mybool = ((bool) @include_once $dir.$file) || $mybool;
 			}
 
-			if ($mybool === false) {
+			if (!$mybool) {
 				dol_print_error(null, "Failed to include file ".$file);
 				return '';
 			}
