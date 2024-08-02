64ec999e7a498ae6a083c9714ef1eac5f0751ae9
MDW <mdeweerd@users.noreply.github.com>
2024-08-02 20:48:23 +0200

    Qual: Simplify expression (phan notice)

--- htdocs/knowledgemanagement/class/knowledgerecord.class.php
+++ htdocs/knowledgemanagement/class/knowledgerecord.class.php
@@ -1015,7 +1015,7 @@ class KnowledgeRecord extends CommonObject
 				$mybool = ((bool) @include_once $dir.$file) || $mybool;
 			}
 
-			if ($mybool === false) {
+			if (!$mybool) {
 				dol_print_error(null, "Failed to include file ".$file);
 				return '';
 			}
