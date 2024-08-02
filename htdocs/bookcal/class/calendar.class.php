f958e6c72feb37c9b679c0473a86b6c1b0cbc9df
MDW <mdeweerd@users.noreply.github.com>
2024-08-02 19:52:07 +0200

    Qual: Simplify expression (phan notice)

--- htdocs/bookcal/class/calendar.class.php
+++ htdocs/bookcal/class/calendar.class.php
@@ -960,7 +960,7 @@ class Calendar extends CommonObject
 				$mybool = ((bool) @include_once $dir.$file) || $mybool;
 			}
 
-			if ($mybool === false) {
+			if (!$mybool) {
 				dol_print_error(null, "Failed to include file ".$file);
 				return '';
 			}
