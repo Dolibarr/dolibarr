d911aaac4ce0799f1f1f5db007a5b73c07b32a21
MDW <mdeweerd@users.noreply.github.com>
2024-08-02 20:48:15 +0200

    Qual: Simplify expression (phan notice)

--- htdocs/hrm/class/position.class.php
+++ htdocs/hrm/class/position.class.php
@@ -995,7 +995,7 @@ class Position extends CommonObject
 				$mybool = ((bool) @include_once $dir.$file) || $mybool;
 			}
 
-			if ($mybool === false) {
+			if (!$mybool) {
 				dol_print_error(null, "Failed to include file " . $file);
 				return '';
 			}
