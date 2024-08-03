aa2cd0a1fe70bba85d9fdc1686083a41a0ba2d23
MDW <mdeweerd@users.noreply.github.com>
2024-08-03 18:27:10 +0200

    fixup! Qual: Apply PhanPluginDuplicateExpressionAssignmentOperation as phan suggests

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -6865,7 +6865,7 @@ class FactureLigne extends CommonInvoiceLine
 						$res_credit_note = $this->db->query($sql_credit_note);
 						if ($res_credit_note) {
 							while ($cn = $this->db->fetch_object($res_credit_note)) {
-								$cumulated_percent = $cumulated_percent + floatval($cn->situation_percent);
+								$cumulated_percent += floatval($cn->situation_percent);
 							}
 						} else {
 							dol_print_error($this->db);
