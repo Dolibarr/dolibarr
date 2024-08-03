ca30901a71fcf85a8743060251250b890a0abf18
MDW <mdeweerd@users.noreply.github.com>
2024-08-03 02:31:50 +0200

    Fix phan typing issue with ?? operator

--- htdocs/asset/class/assetmodel.class.php
+++ htdocs/asset/class/assetmodel.class.php
@@ -506,7 +506,7 @@ class AssetModel extends CommonObject
 		if (!empty($this->fields['date_validation'])) {
 			$sql .= ", date_validation = '" . $this->db->idate($now) . "'";
 		}
-		if (!empty($this->fields['fk_user_valid'])) {
+		if (!empty($this->fields['fk_user_valid'] ?? null)) {
 			$sql .= ", fk_user_valid = " . ((int) $user->id);
 		}
 		$sql .= " WHERE rowid = " . ((int) $this->id);
