bb5c66add4685f180c72e981226c4444860a7966
MDW <mdeweerd@users.noreply.github.com>
2024-08-06 21:54:21 +0200

    Qual: phan/Correct module type and ignore false positive

--- htdocs/admin/propal.php
+++ htdocs/admin/propal.php
@@ -285,7 +285,7 @@ foreach ($dirmodels as $reldir) {
 					}
 
 					if ($module->isEnabled()) {
-						print '<tr class="oddeven"><td>'.$module->nom."</td><td>\n";
+						print '<tr class="oddeven"><td>'.$module->getName()."</td><td>\n";
 						print $module->info($langs);
 						print '</td>';
 
@@ -426,7 +426,7 @@ foreach ($dirmodels as $reldir) {
 								print(empty($module->name) ? $name : $module->name);
 								print "</td><td>\n";
 								if (method_exists($module, 'info')) {
-									print $module->info($langs);
+									print $module->info($langs);  // @phan-suppress-current-line PhanUndeclaredMethod
 								} else {
 									print $module->description;
 								}
