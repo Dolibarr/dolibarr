21eb9565b473e4ab9de722ba014a14a5c68a3dc0
MDW <mdeweerd@users.noreply.github.com>
2024-08-06 16:10:22 +0200

    Qual: Fix PhanPluginUnknownObjectMethodCall (add typing hints)
    # Qual: Fix PhanPluginUnknownObjectMethodCall (add typing hints)
    
    Add/improve typing hint for PhanPluginUnknownObjectMethodCall

--- htdocs/admin/fichinter.php
+++ htdocs/admin/fichinter.php
@@ -295,6 +295,8 @@ foreach ($dirmodels as $reldir) {
 
 					$module = new $file();
 
+					'@phan-var-force ModeleNumRefFicheinter $module';
+
 					if ($module->isEnabled()) {
 						// Show modules according to features level
 						if ($module->version == 'development' && getDolGlobalInt('MAIN_FEATURES_LEVEL') < 2) {
@@ -431,6 +433,8 @@ foreach ($dirmodels as $reldir) {
 
 						require_once $dir.'/'.$file;
 						$module = new $classname($db);
+
+						'@phan-var-force ModelePDFFicheinter $module';
 
 						$modulequalified = 1;
 						if ($module->version == 'development' && getDolGlobalInt('MAIN_FEATURES_LEVEL') < 2) {
