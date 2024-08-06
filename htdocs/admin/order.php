fad07e5a37f2927dbb9a3ca17bc20c62a955fbfa
MDW <mdeweerd@users.noreply.github.com>
2024-08-06 21:54:13 +0200

    Qual: phan/Correct module type and ignore false positive

--- htdocs/admin/order.php
+++ htdocs/admin/order.php
@@ -436,7 +436,7 @@ foreach ($dirmodels as $reldir) {
 							require_once $dir.'/'.$file;
 							$module = new $classname($db);
 
-							'@phan-var-force ModeleRefCommandes $module';
+							'@phan-var-force ModelePDFCommandes $module';
 
 							$modulequalified = 1;
 							if ($module->version == 'development' && getDolGlobalInt('MAIN_FEATURES_LEVEL') < 2) {
@@ -451,7 +451,7 @@ foreach ($dirmodels as $reldir) {
 								print(empty($module->name) ? $name : $module->name);
 								print "</td><td>\n";
 								if (method_exists($module, 'info')) {
-									print $module->info($langs);
+									print $module->info($langs);  // @phan-suppress-current-line PhanUndeclaredMethod
 								} else {
 									print $module->description;
 								}
