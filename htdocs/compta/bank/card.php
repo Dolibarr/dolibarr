163e6239649dc6e03d6409f5971ac0f3017448c6
HENRY Florian <florian.henry@open-concept.pro>
2024-08-07 03:08:04 +0200

    FIX: on change ref for bank account attachement are lost (#30529)
    * FIX: on change ref for bank account attachement are lost
    
    * FIX: on change ref for bank account attachement are lost
    
    * FIX: on change ref for bank account attachement are lost
    
    * Update card.php
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/compta/bank/card.php
+++ htdocs/compta/bank/card.php
@@ -80,7 +80,6 @@ if (GETPOST("id", 'int') || GETPOST("ref")) {
 
 $result = restrictedArea($user, 'banque', $id, 'bank_account&bank_account', '', '', $fieldid);
 
-
 /*
  * Actions
  */
@@ -222,6 +221,7 @@ if (empty($reshook)) {
 		$object = new Account($db);
 		$object->fetch(GETPOST("id", 'int'));
 
+		$object->oldref = $object->ref;
 		$object->ref = dol_string_nospecial(trim(GETPOST('ref', 'alpha')));
 		$object->label = trim(GETPOST("label", 'alphanohtml'));
 		$object->courant = GETPOST("type");
