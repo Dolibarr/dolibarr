4e1fa1906bb8be4f2d36bb9eb455d8fc4222bcd3
Jon Bendtsen <xcodeauthor@jonb.dk>
2024-08-08 21:19:34 +0200

    trying to use a different method to read the variable

--- htdocs/api/class/api_setup.class.php
+++ htdocs/api/class/api_setup.class.php
@@ -1197,7 +1197,7 @@ class Setup extends DolibarrApi
 		$list = array();
 
 		if (!DolibarrApiAccess::$user->admin
-			&& (!getDolGlobalString('API_LOGINS_ALLOWED_FOR_GET_EXTRAFIELDS') || DolibarrApiAccess::$user->login != $conf->global->API_LOGINS_ALLOWED_FOR_GET_EXTRAFIELDS)) {
+			&& (!getDolGlobalString('API_LOGINS_ALLOWED_FOR_GET_EXTRAFIELDS') || DolibarrApiAccess::$user->login != getDolGlobalString('API_LOGINS_ALLOWED_FOR_GET_EXTRAFIELDS'))) {
 			throw new RestException(403, 'Error API open to admin users only or to the users with logins defined into constant API_LOGINS_ALLOWED_FOR_GET_EXTRAFIELDS');
 		}
 
