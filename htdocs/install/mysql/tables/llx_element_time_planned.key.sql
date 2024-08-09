375d0b881c2fff81b532f6f749ac9256344d43cf
Charlène Benke <1179011+defrance@users.noreply.github.com>
2024-08-09 16:16:29 +0200

    Introduce planned time on élement
    The goal of this table is to store planned time on element (same logic as element_time, but it's for previsionnal time on element).
    
     My goal is to add prévisional feature on interventional with a specific user like i allready on my myschedule module (not with the same table) and standardise it for the other dolibarr element (like mpr, ticket, ...)

---
+++ htdocs/install/mysql/tables/llx_element_time_planned.key.sql
@@ -0,0 +1,23 @@
+-- ===========================================================================
+-- Copyright (C) 2014 Laurent Destailleur  <eldy@users.sourceforge.net>
+-- Copyright (C) 2024 Charlene Benke	   <charlene@patas-monkey.com>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <https://www.gnu.org/licenses/>.
+--
+-- ===========================================================================
+
+ALTER TABLE llx_element_time_planned ADD INDEX idx_element_time_planned_element (fk_element);
+ALTER TABLE llx_element_time_planned ADD INDEX idx_element_time_planned_date (element_date);
+ALTER TABLE llx_element_time_planned ADD INDEX idx_element_time_planned_datehour (element_datehour);
+
