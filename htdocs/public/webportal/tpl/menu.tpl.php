13bca77fd202559161930b8c1aa0fcca8967e1c5
Frédéric FRANCE <frederic.france@free.fr>
2024-08-13 15:33:52 +0200

    fix phpstan

--- htdocs/public/webportal/tpl/menu.tpl.php
+++ htdocs/public/webportal/tpl/menu.tpl.php
@@ -1,8 +1,23 @@
 <!-- file menu.tpl.php -->
 <?php
 /* Copyright (C) 2024		Frédéric France			<frederic.france@free.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * or see https://www.gnu.org/
  */
-// Protection to avoid direct call of template
+
+ // Protection to avoid direct call of template
 if (empty($context) || !is_object($context)) {
 	print "Error, template page can't be called as URL";
 	exit(1);
