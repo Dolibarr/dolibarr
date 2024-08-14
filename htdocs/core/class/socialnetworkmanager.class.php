168a96cb44bf12dc2c37315fe378f23c00a1c5ec
Lamrani Abdel <lamrani002@gmail.com>
2024-08-14 15:23:52 +0200

    Fix other conflits

--- htdocs/core/class/socialnetworkmanager.class.php
+++ htdocs/core/class/socialnetworkmanager.class.php
@@ -1,9 +1,6 @@
 <?php
 /* Copyright (C) 2024 Laurent Destailleur <eldy@users.sourceforge.net>
-<<<<<<< HEAD
-=======
  * Copyright (C) 2024		Frédéric France			<frederic.france@free.fr>
->>>>>>> d784bceeef02e94a0f2a5f337fe4bd4e38d91c1c
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
