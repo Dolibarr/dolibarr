4c470c2254d201e1cf3111b5be8ec2ea291f36ec
Frédéric FRANCE <frederic.france@free.fr>
2024-08-17 17:05:06 +0200

    fix ignored phpstan

--- htdocs/hrm/skill_card.php
+++ htdocs/hrm/skill_card.php
@@ -566,7 +566,7 @@ if ($action != "create" && $action != "edit") {
 	foreach ($objectline->fields as $key => $val) {
 		// If $val['visible']==0, then we never show the field
 		if (!empty($val['visible'])) {
-			$visible = (int) dol_eval($val['visible'], 1, 1, '1');
+			$visible = (int) dol_eval((string) $val['visible'], 1, 1, '1');
 			$arrayfields['t.' . $key] = array(
 				'label' => $val['label'],
 				'checked' => (($visible < 0) ? 0 : 1),
