607ef94ada95ec47e8036d2be82336bfd5177912
thibdrev <thibault.drevet@gmail.com>
2024-08-20 15:09:48 +0200

    Update productMargins.php

--- htdocs/margin/tabs/productMargins.php
+++ htdocs/margin/tabs/productMargins.php
@@ -96,7 +96,7 @@ if (empty($search_invoice_date_start) && empty($search_invoice_date_end) && !GET
 		$search_invoice_date_start = strtotime($fiscalYear->date_start);
 		$search_invoice_date_end = strtotime($fiscalYear->date_end);
 	} else {
-		$month_start = ($conf->global->SOCIETE_FISCAL_MONTH_START ? $conf->global->SOCIETE_FISCAL_MONTH_START : 1);
+		$month_start = getDolGlobalInt('SOCIETE_FISCAL_MONTH_START', 1);
 		$year_start = (int) dol_print_date(dol_now(), '%Y');
 		if (dol_print_date(dol_now(), '%m') < $month_start) {
 			$year_start--; // If current month is lower that starting fiscal month, we start last year
