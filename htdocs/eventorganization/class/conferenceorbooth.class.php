6cc94890b932ea6f45dac60923d7e1e2e4a4029c
thibdrev <thibault.drevet@gmail.com>
2024-08-21 15:49:37 +0200

    Update conferenceorbooth.class.php

--- htdocs/eventorganization/class/conferenceorbooth.class.php
+++ htdocs/eventorganization/class/conferenceorbooth.class.php
@@ -686,9 +686,9 @@ class ConferenceOrBooth extends ActionComm
 	/**
 	 *	Return a thumb for kanban views
 	 *
-	 *	@param      string	    $option                 Where point the link (0=> main card, 1,2 => shipment, 'nolink'=>No link)
-	 *  @param		array		$arraydata				Array of data
-	 *  @return		string								HTML Code for Kanban thumb.
+	 *	@param      string	    			$option                 Where point the link (0=> main card, 1,2 => shipment, 'nolink'=>No link)
+	 *  @param		array{string,mixed}		$arraydata				Array of data
+	 *  @return		string											HTML Code for Kanban thumb.
 	 */
 	public function getKanbanView($option = '', $arraydata = null)
 	{
