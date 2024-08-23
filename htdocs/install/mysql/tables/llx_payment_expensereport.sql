4571808a1c9ff08f4b39f717bff798c74a69b57e
ems-co <info@ems-erp.com>
2024-08-24 00:09:10 +0300

    Update llx_payment_expensereport.sql

--- htdocs/install/mysql/tables/llx_payment_expensereport.sql
+++ htdocs/install/mysql/tables/llx_payment_expensereport.sql
@@ -30,4 +30,4 @@ create table llx_payment_expensereport
   fk_bank                 integer NOT NULL,
   fk_user_creat           integer,            -- creation user
   fk_user_modif           integer             -- last modification user
-)ENGINE=innodb;
+)ENGINE=innodb DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
