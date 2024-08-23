31ad9a78ece20325c1a955172faef6a50055c25a
ems-co <info@ems-erp.com>
2024-08-24 00:08:54 +0300

    Update llx_payment_donation.sql

--- htdocs/install/mysql/tables/llx_payment_donation.sql
+++ htdocs/install/mysql/tables/llx_payment_donation.sql
@@ -32,4 +32,4 @@ create table llx_payment_donation
   fk_bank         integer NOT NULL,
   fk_user_creat   integer,                              -- creation user
   fk_user_modif   integer                               -- last modification user
-)ENGINE=innodb;
+)ENGINE=innodb DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
