bcfaaa414fdb1446d5fc41c29671690b291e99bd
atm-adrien <67913809+atm-adrien@users.noreply.github.com>
2024-08-23 15:05:53 +0200

    FIX : Propal stats page did not work (#30736)

--- htdocs/product/stats/propal.php
+++ htdocs/product/stats/propal.php
@@ -100,7 +100,7 @@ if ($id > 0 || !empty($ref)) {
 		setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
 	}
 
-	llxHeader("", "", $langs->trans("CardProduct".$product->type), '', '', 0, 0, '', '', '', 'mod-product page-stats_propal');
+	llxHeader("", "", $langs->trans("CardProduct".$product->type), '', '', 0, 0, '', '', 'mod-product page-stats_propal');
 
 	if ($result > 0) {
 		$head = product_prepare_head($product);
