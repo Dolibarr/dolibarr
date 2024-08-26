4a3b534446b83774a5c62a9fe9f71c2c54a7d99b
William Mead <william.mead@manchenumerique.fr>
2024-08-26 17:32:34 +0200

    Added contributor details

--- htdocs/user/list.php
+++ htdocs/user/list.php
@@ -6,6 +6,7 @@
  * Copyright (C) 2016		Marcos García			<marcosgdf@gmail.com>
  * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024		Benjamin Falière		<benjamin.faliere@altairis.fr>
+ * Copyright (C) 2024		William Mead			<william.mead@manchenumerique.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
