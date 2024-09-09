c24f69045d67781813a1a62841a2c988f71829f0
MDW <mdeweerd@users.noreply.github.com>
2024-09-09 23:25:59 +0200

    getToolTip() can take null for $coc

--- htdocs/core/modules/member/modules_member.class.php
+++ htdocs/core/modules/member/modules_member.class.php
@@ -85,7 +85,7 @@ abstract class ModeleNumRefMembers extends CommonNumRefGenerator
 	 *  Return description of module parameters
 	 *
 	 *  @param	Translate	$langs      Output language
-	 *  @param	Societe		$soc		Third party object
+	 *  @param	?Societe	$soc		Third party object
 	 *  @return	string					HTML translated description
 	 */
 	public function getToolTip($langs, $soc)
