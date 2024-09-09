910daa24aa0c19d085ccd87888c17c033c71f720
Regis Houssin <regis.houssin@inodbox.com>
2024-09-09 04:26:22 +0200

    FIX wrong subprice if price base type is TTC (#30887)

--- htdocs/commande/class/commande.class.php
+++ htdocs/commande/class/commande.class.php
@@ -3241,7 +3241,7 @@ class Commande extends CommonOrder
 			$this->line->localtax1_type = empty($localtaxes_type[0]) ? '' : $localtaxes_type[0];
 			$this->line->localtax2_type = empty($localtaxes_type[2]) ? '' : $localtaxes_type[2];
 			$this->line->remise_percent = $remise_percent;
-			$this->line->subprice       = $subprice;
+			$this->line->subprice       = $pu_ht;
 			$this->line->info_bits      = $info_bits;
 			$this->line->special_code   = $special_code;
 			$this->line->total_ht       = $total_ht;
