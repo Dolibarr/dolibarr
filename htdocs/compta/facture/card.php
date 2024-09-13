6115817a77d4633bf1b007d140d665acd5bcbb07
MDW <mdeweerd@users.noreply.github.com>
2024-09-13 13:39:16 +0200

    Fix uninitialized $price_base_type

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -2161,6 +2161,7 @@ if (empty($reshook)) {
 		$price_ttc_devise = '';
 		$price_min = '';
 		$price_min_ttc = '';
+		$price_base_type = '';
 
 		if (GETPOST('price_ht') !== '') {
 			$price_ht = price2num(GETPOST('price_ht'), 'MU', 2);
