cba3938a6556653d707aeca4f316da3c5a042779
Kamel Khelifa <kkhelifa@open-dsi.fr>
2024-09-17 17:08:17 +0200

    Correction

--- htdocs/ticket/list.php
+++ htdocs/ticket/list.php
@@ -1120,9 +1120,7 @@ while ($i < $imaxinloop) {
 					print $s;
 					print '</span>';
 				} elseif ($key == 'duration') {
-					print '<span title="'.dol_escape_htmltag($s).'">';
 					print (isset($object->duration) ? convertSecondToTime($object->duration, 'all', getDolGlobalInt('MAIN_DURATION_OF_WORKDAY')) : '');
-					print '</span>';
 				} elseif ($key == 'tms') {
 					print dol_print_date($db->jdate($obj->$key), 'dayhour', 'tzuser');
 				} elseif ($key == 'fk_user_create') {
