6e04c0a81c2cec1011c87a513da0989170f6713c
Mohamed DAOUD <daoud.mouhamed@gmail.com>
2024-09-20 10:36:40 +0200

    add town country partnership list

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -12895,7 +12895,7 @@ function getElementProperties($elementType)
 		$classfile = 'mailing';
 		$classname = 'Mailing';
 		$subelement = '';
-	} elseif ($elementType == 'member') {
+	} elseif ($elementType == 'member' || $elementType == 'adherent') {
 		$classpath = 'adherents/class';
 		$module = 'adherent';
 		$subelement = 'adherent';
