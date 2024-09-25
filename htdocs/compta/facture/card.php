a270ad802716a34462ac21fe7c8db87af4267377
Ulysse Valdenaire <uvaldenaire@easya.solutions>
2024-09-25 11:36:29 +0200

    Fix wrong invoice status when invoice has discount
    1) If we add a discount to a customer invoice, the status stays 'unpaid' instead of 'started'.
    This fix sets the status to 'started' when we add a discount to an invoice.
    
    2) If we want to close this invoice as 'partially paid', the status is 'close (unpaid)' instead of 'partially paid'.
    This fix sets the status to 'partially paid' when we want to close the invoice as 'partially paid'.

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -4657,8 +4657,8 @@ if ($action == 'create') {
 	}
 	$morehtmlref .= '</div>';
 
-	$object->totalpaid = $totalpaid; // To give a chance to dol_banner_tab to use already paid amount to show correct status
-
+	$sumofpaymentall = $totalpaid + $totalcreditnotes + $totaldeposits;
+	$object->totalpaid = $sumofpaymentall; // To give a chance to dol_banner_tab to use already paid amount to show correct status
 	dol_banner_tab($object, 'ref', $linkback, 1, 'ref', 'ref', $morehtmlref, '', 0, '', '');
 
 	// Call Hook tabContentViewInvoice
