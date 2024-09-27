7db9a5afb7bd274eac5c20cfd8b69e3c9454a46d
Laurent Destailleur <eldy@destailleur.fr>
2024-09-27 14:02:21 +0200

    Fix label in export MO

--- htdocs/exports/export.php
+++ htdocs/exports/export.php
@@ -79,7 +79,7 @@ $entitytoicon = array(
 	'bomm'         => 'bom',
 	'bomline'      => 'bom',
 	'conferenceorboothattendee' => 'contact',
-	'inventory_line' => 'inventory'
+	'inventory_line' => 'inventory',
 );
 
 // Translation code, array duplicated in import.php, was not synchronized, TODO put it somewhere only once
@@ -131,6 +131,7 @@ $entitytolang = array(
 	'translation'  => 'Translation',
 	'bom'          => 'BOM',
 	'bomline'      => 'BOMLine',
+	'mrp'          => 'ManufacturingOrder',
 	'conferenceorboothattendee' => 'Attendee',
 	'inventory'   => 'Inventory',
 	'inventory_line' => 'InventoryLine'
