f54efb885caa667f286bdf4ad2e6ece1f49c7a5b
Frédéric FRANCE <frederic.france@free.fr>
2024-10-01 20:04:10 +0200

    fix

--- htdocs/core/actions_printing.inc.php
+++ htdocs/core/actions_printing.inc.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2014-2016  Laurent Destailleur  	<eldy@users.sourceforge.net>
- * Copyright (C) 2018-2024	Frédéric France      	<frederic.france@free.fr>
+ * Copyright (C) 2014-2024	Frédéric France      	<frederic.france@free.fr>
  * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
