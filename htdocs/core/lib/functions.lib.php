b5730db759609e5187be808f88d790c01ed349e1
Laurent Destailleur <eldy@destailleur.fr>
2024-10-02 12:18:55 +0200

    Fix error

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -13336,7 +13336,7 @@ function forgeSQLFromUniversalSearchCriteria($filter, &$errorstr = '', $noand = 
 {
 	global $db, $user;
 
-	if (is_null($filter) || $filter === '') {
+	if (is_null($filter) || is_array($filter) || $filter === '') {
 		return '';
 	}
 	if (!preg_match('/^\(.*\)$/', $filter)) {    // If $filter does not start and end with ()
