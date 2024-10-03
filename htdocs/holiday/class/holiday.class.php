b545e5178066be1f5fc773406590c2039c2a90e3
tnegre <tnegre@open-dsi.fr>
2024-10-03 16:41:24 +0200

    fix : missing update for extrafields on holidays

--- htdocs/holiday/class/holiday.class.php
+++ htdocs/holiday/class/holiday.class.php
@@ -1068,6 +1068,13 @@ class Holiday extends CommonObject
 		}
 
 		if (!$error) {
+			$result = $this->insertExtraFields();
+			if ($result < 0) {
+				$error++;
+			}
+		}
+
+		if (!$error) {
 			if (!$notrigger) {
 				// Call trigger
 				$result = $this->call_trigger('HOLIDAY_MODIFY', $user);
