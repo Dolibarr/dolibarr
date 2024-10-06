2b4570703f7ad9d8fdd07ff536f198b76c936cfa
sonikf <93765174+sonikf@users.noreply.github.com>
2024-10-06 21:34:33 +0300

    fix phan php-stan (8.2)

--- htdocs/core/modules/commande/doc/pdf_einstein.modules.php
+++ htdocs/core/modules/commande/doc/pdf_einstein.modules.php
@@ -1275,7 +1275,7 @@ class pdf_einstein extends ModelePDFCommandes
 	 *  @param  Translate	$outputlangs	Object lang for output
 	 *  @param  Translate	$outputlangsbis	Object lang for output bis
 	 *  @param	string		$titlekey		Translation key to show as title of document
-	 *  @return	array						top shift of linked object lines
+	 *  @return	array<string, int|float>	top shift of linked object lines
 	 */
 	protected function _pagehead(&$pdf, $object, $showaddress, $outputlangs, $outputlangsbis = null, $titlekey = "PdfOrderTitle")
 	{
