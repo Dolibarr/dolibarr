5ef619eae3be60cec64723f732975608473953f6
Laurent Destailleur <eldy@destailleur.fr>
2024-10-07 12:02:37 +0200

    FIX look and feel v20

--- htdocs/core/boxes/box_comptes.php
+++ htdocs/core/boxes/box_comptes.php
@@ -74,7 +74,9 @@ class box_comptes extends ModeleBoxes
 
 		$this->max = $max;
 
-		$this->info_box_head = array('text' => $langs->trans("BoxTitleCurrentAccounts"));
+		$this->info_box_head = array(
+			'text' => $langs->trans("BoxTitleCurrentAccounts").'<a class="paddingleft" href="'.DOL_URL_ROOT.'/compta/bank/list.php?search_status=opened"><span class="badge">...</span></a>'
+		);
 
 		if ($user->hasRight('banque', 'lire')) {
 			$sql = "SELECT b.rowid, b.ref, b.label, b.bank, b.number, b.courant, b.clos, b.rappro, b.url";
