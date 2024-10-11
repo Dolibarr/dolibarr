ca377375e6216266eea3c8d6dc3c0b12ee753a06
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2024-10-11 13:21:07 +0200

    accountancysystem: improve error reporting
    Forward the database error to the caller when an error happens.

--- htdocs/accountancy/class/accountancysystem.class.php
+++ htdocs/accountancy/class/accountancysystem.class.php
@@ -190,12 +190,12 @@ class AccountancySystem extends CommonObject
 				$result = $this->rowid;
 			} else {
 				$result = - 2;
-				$this->error = "AccountancySystem::Create Error $result";
+				$this->error = "AccountancySystem::Create Error $result: " . $this->db->lasterror();
 				dol_syslog($this->error, LOG_ERR);
 			}
 		} else {
 			$result = - 1;
-			$this->error = "AccountancySystem::Create Error $result";
+			$this->error = "AccountancySystem::Create Error $result: " . $this->db->lasterror();
 			dol_syslog($this->error, LOG_ERR);
 		}
 
