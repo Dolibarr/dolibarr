875fb7729f12fe2e167e28649e944387c1c0c78d
MDW <mdeweerd@users.noreply.github.com>
2024-10-14 04:03:21 +0200

    fixup! Do not use deprecated statut, fk_statut

--- htdocs/webportal/class/webportalpropal.class.php
+++ htdocs/webportal/class/webportalpropal.class.php
@@ -120,10 +120,6 @@ class WebPortalPropal extends Propal
 	//public $multicurrency_total_ht;
 	//public $multicurrency_total_tva;
 	//public $multicurrency_total_ttc;
-
-	/*
-	 * @var int status
-	 */
 	//public $fk_statut; // Managed in parent
 	// END MODULEBUILDER PROPERTIES
 
