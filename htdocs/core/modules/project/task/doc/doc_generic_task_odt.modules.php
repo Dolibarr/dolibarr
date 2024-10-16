9ad71b980a13523c39d0192d0217a9bcbac5c542
Laurent Destailleur <eldy@users.sourceforge.net>
2024-10-16 20:46:52 +0200

    NEW Can choose the zip handler to build ODT

--- htdocs/core/modules/project/task/doc/doc_generic_task_odt.modules.php
+++ htdocs/core/modules/project/task/doc/doc_generic_task_odt.modules.php
@@ -548,7 +548,7 @@ class doc_generic_task_odt extends ModelePDFTask
 						$srctemplatepath,
 						array(
 							'PATH_TO_TMP'	  => $conf->project->dir_temp,
-							'ZIP_PROXY'		  => 'PclZipProxy', // PhpZipProxy or PclZipProxy. Got "bad compression method" error when using PhpZipProxy.
+							'ZIP_PROXY'		  => getDolGlobalString('MAIN_ODF_ZIP_PROXY', 'PclZipProxy'), // PhpZipProxy or PclZipProxy. Got "bad compression method" error when using PhpZipProxy.
 							'DELIMITER_LEFT'  => '{',
 							'DELIMITER_RIGHT' => '}'
 						)
