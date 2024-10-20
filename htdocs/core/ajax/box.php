169d6aa805dcb62433c3b1f3e95c8e6661c1c5ef
Frédéric FRANCE <frederic.france@free.fr>
2024-10-20 20:23:53 +0200

    fix phpstan

--- htdocs/core/ajax/box.php
+++ htdocs/core/ajax/box.php
@@ -1,6 +1,6 @@
 <?php
-/* Copyright (C) 2005-2012	Regis Houssin		<regis.houssin@inodbox.com>
- * Copyright (C) 2007-2012	Laurent Destailleur	<eldy@users.sourceforge.net>
+/* Copyright (C) 2005-2012  Regis Houssin           <regis.houssin@inodbox.com>
+ * Copyright (C) 2007-2012  Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
