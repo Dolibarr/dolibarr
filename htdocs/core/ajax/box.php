0d2f1cf666283aa71b3deaf9ec8b8400c2d65747
MDW <mdeweerd@users.noreply.github.com>
2024-10-24 14:01:30 +0200

    Qual: Fix phpstan notices (int expected) (#31485)
    # Qual: Fix phpstan notices (int expected)
    
    Fix several notices where int was expected (casting/phpdoc/GETPOSTINT).
    Separate phpstan exceptions for methods containing 'pdf' from
    other methods/functions.

--- htdocs/core/ajax/box.php
+++ htdocs/core/ajax/box.php
@@ -2,6 +2,7 @@
 /* Copyright (C) 2005-2012  Regis Houssin           <regis.houssin@inodbox.com>
  * Copyright (C) 2007-2012  Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
+ * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -44,7 +45,10 @@ require_once DOL_DOCUMENT_ROOT.'/core/class/infobox.class.php';
 
 $boxid = GETPOSTINT('boxid');
 $boxorder = GETPOST('boxorder');
-$zone = GETPOST('zone');		// Can be '0' or '1' or 'pagename'...
+$zone = GETPOST('zone');		// Can be key for zone
+if ($zone !== '') {
+	$zone = (int) $zone;
+}
 $userid = GETPOSTINT('userid');
 
 // Security check
