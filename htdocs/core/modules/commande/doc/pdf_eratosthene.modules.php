387a83f47162ec5557d6f8ad664fb12c6ad1b549
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2024-10-24 12:16:29 +0200

    fix php stan

--- htdocs/core/modules/commande/doc/pdf_eratosthene.modules.php
+++ htdocs/core/modules/commande/doc/pdf_eratosthene.modules.php
@@ -817,7 +817,7 @@ class pdf_eratosthene extends ModelePDFCommandes
 					$drawTabHeight = $drawTabBottom - $drawTabTop;
 					$this->_tableau($pdf, $drawTabTop, $drawTabHeight, 0, $outputlangs, $drawTabHideTop, $hideBottom, $object->multicurrency_code, $outputlangsbis);
 
-					$hideFreeText = $i != $pdf->getNumPages(); // Display free text only in last page
+					$hideFreeText = $i != $pdf->getNumPages() ? 1 : 0; // Display free text only in last page
 					$this->_pagefoot($pdf, $object, $outputlangs, $hideFreeText);
 
 					$pdf->setPage($i); // in case of _pagefoot or _tableau change it
