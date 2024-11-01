8361913db996d96b13cbb56ca8ba731744494d0e
Frédéric France <frederic.france@free.fr>
2024-11-01 10:38:00 +0100

    update phpstan baseline

--- htdocs/exports/export.php
+++ htdocs/exports/export.php
@@ -34,6 +34,14 @@ require_once DOL_DOCUMENT_ROOT.'/exports/class/export.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/modules/export/modules_export.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
 
+/**
+ * @var Conf $conf
+ * @var DoliDB $db
+ * @var HookManager $hookmanager
+ * @var Translate $langs
+ * @var User $user
+ */
+
 // Load translation files required by the page
 $langs->loadlangs(array('admin', 'exports', 'other', 'users', 'companies', 'projects', 'suppliers', 'products', 'bank', 'bills'));
 
