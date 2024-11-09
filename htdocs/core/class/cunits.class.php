7c0375cde5ef94f0efffc56012a435e071fa6916
Frédéric France <frederic.france@free.fr>
2024-11-09 22:52:28 +0100

    fix calculate costs bom

--- htdocs/core/class/cunits.class.php
+++ htdocs/core/class/cunits.class.php
@@ -37,8 +37,8 @@ class CUnits extends CommonDict
 	 */
 	public $records = array();
 
-	//var $element='ctypent';			//!< Id that identify managed objects
-	//var $table_element='ctypent';	//!< Name of table without prefix where object is stored
+	// public $element = 'cunits';			//!< Id that identify managed objects
+	// public $table_element = 'c_units';	//!< Name of table without prefix where object is stored
 
 	/**
 	 * @var string label
