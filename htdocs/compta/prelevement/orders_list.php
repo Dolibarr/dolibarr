0709a7d762e59cb7e01b1b48005414f3295ca59c
Frédéric France <frederic.france@free.fr>
2024-11-10 17:21:52 +0100

    fix phpstan

--- htdocs/compta/prelevement/orders_list.php
+++ htdocs/compta/prelevement/orders_list.php
@@ -34,6 +34,7 @@ require_once DOL_DOCUMENT_ROOT.'/compta/bank/class/account.class.php';
 /**
  * @var Conf $conf
  * @var DoliDB $db
+ * @var Form $form
  * @var HookManager $hookmanager
  * @var Translate $langs
  * @var User $user
