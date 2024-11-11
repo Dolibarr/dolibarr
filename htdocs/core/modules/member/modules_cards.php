1d7b021dda44a5b1533602284b941e018b655a7b
Frédéric France <frederic.france@free.fr>
2024-11-11 15:10:28 +0100

    version

--- htdocs/core/modules/member/modules_cards.php
+++ htdocs/core/modules/member/modules_cards.php
@@ -4,6 +4,7 @@
  * Copyright (C) 2004	   Eric Seigne			<eric.seigne@ryxeo.com>
  * Copyright (C) 2005-2009 Regis Houssin		<regis.houssin@inodbox.com>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
