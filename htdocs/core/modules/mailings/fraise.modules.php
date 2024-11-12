f31ab567552b4c251d101c4f8d7f1a8c747e01bc
Frédéric France <frederic.france@free.fr>
2024-11-13 00:13:12 +0100

    add phpdoc

--- htdocs/core/modules/mailings/fraise.modules.php
+++ htdocs/core/modules/mailings/fraise.modules.php
@@ -2,7 +2,7 @@
 /* Copyright (C) 2005       Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2009  Regis Houssin           <regis.houssin@inodbox.com>
  * Copyright (C) 2018-2024  Frédéric France         <frederic.france@free.fr>
- * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
