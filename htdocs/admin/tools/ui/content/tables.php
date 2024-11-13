4ff3bcb64541063fbb6fff53975e5bbbc7a92743
Laurent Destailleur <eldy@destailleur.fr>
2024-11-13 22:32:50 +0100

    Update tables.php

--- htdocs/admin/tools/ui/content/tables.php
+++ htdocs/admin/tools/ui/content/tables.php
@@ -88,7 +88,7 @@ $documentation->showSidebar(); ?>
 								<td class="left">Jack</td>
 								<td class="left">Sparrow</td>
 								<td class="center">29</td>
-								<td class="right">Carribean</td>
+								<td class="right">Caribean</td>
 							</tr>
 							<tr>
 								<td class="left">Sacha</td>
