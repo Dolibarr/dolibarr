8273df251ad230a970c857c0a5ade5e70ec95e47
Mohamed DAOUD <daoud.mouhamed@gmail.com>
2024-11-15 17:22:05 +0100

    Update extrafields.class.php

--- htdocs/core/class/extrafields.class.php
+++ htdocs/core/class/extrafields.class.php
@@ -1938,7 +1938,7 @@ class ExtraFields
 
 		// If alwayseditable is false, and object is not in draft, then showOutputField
 		$currentObject = fetchObjectByElement($currentObjectId, $extrafieldsobjectkey);
-		// @phan-suppress PhanUndeclaredConstantOfClass
+		// @phan-suppress-next-line PhanUndeclaredConstantOfClass
 		if ($alwayseditable == 0 && $currentObject->status != $currentObject::STATUS_DRAFT) {
 			$out = $this->showOutputField($key, $value, $moreparam, $extrafieldsobjectkey, null, $currentObject);
 		}
