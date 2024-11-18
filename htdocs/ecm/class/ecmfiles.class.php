4937a83640381a6c9bfce51aadc1b862ad9339c5
Mohamed DAOUD <daoud.mouhamed@gmail.com>
2024-11-18 23:17:32 +0100

    fix ecmfile

--- htdocs/ecm/class/ecmfiles.class.php
+++ htdocs/ecm/class/ecmfiles.class.php
@@ -29,6 +29,7 @@
 
 // Put here all includes required by your class file
 require_once DOL_DOCUMENT_ROOT.'/core/class/commonobject.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/commonobjectline.class.php';
 
 /**
  * Class to manage ECM files
