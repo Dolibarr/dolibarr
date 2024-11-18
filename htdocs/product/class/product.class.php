8598d1603be7cfc578ae19c95bda2bf07bd23341
Frédéric FRANCE <frederic.france@free.fr>
2024-11-18 16:37:19 +0100

    restore change removed in merge develop in develop

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -2675,7 +2675,7 @@ class Product extends CommonObject
 				$price = (float) price2num($newprice) / (1 + ((float) $newvat / 100));
 				$price = (float) price2num($price, 'MU');
 
-				if ($newminprice != '' || $newminprice == 0) {
+				if ($newminprice == 0) {
 					$price_min_ttc = (float) price2num($newminprice, 'MU');
 					$price_min = (float) price2num($newminprice) / (1 + ($newvat / 100));
 					$price_min = (float) price2num($price_min, 'MU');
@@ -2688,7 +2688,7 @@ class Product extends CommonObject
 				$price_ttc = ($newnpr != 1) ? (float) price2num($newprice) * (1 + ($newvat / 100)) : $price;
 				$price_ttc = (float) price2num($price_ttc, 'MU');
 
-				if ($newminprice !== '' || $newminprice == 0) {
+				if ($newminprice == 0) {
 					$price_min = (float) price2num($newminprice, 'MU');
 					$price_min_ttc = (float) price2num($newminprice) * (1 + ($newvat / 100));
 					$price_min_ttc = (float) price2num($price_min_ttc, 'MU');
