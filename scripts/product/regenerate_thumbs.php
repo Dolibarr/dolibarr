1bf7ef698e23042e3c240bde1ccd6213c4298c03
Frédéric France <frederic.france@free.fr>
2024-11-19 20:24:00 +0100

    fix phpstan

--- scripts/product/regenerate_thumbs.php
+++ scripts/product/regenerate_thumbs.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
-/* Copyright (C) 2007-2016 Laurent Destailleur <eldy@users.sourceforge.net>
- * Copyright (C) 2015 Jean Heimburger <http://tiaris.eu>
+/* Copyright (C) 2007-2016  Laurent Destailleur     <eldy@users.sourceforge.net>
+ * Copyright (C) 2015       Jean Heimburger         <http://tiaris.eu>
  * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
