a23baf0bbaeb136bd2321495b9c90249f61884e4
Frédéric FRANCE <frederic34@users.noreply.github.com>
2024-11-19 13:14:00 +0100

    Update project.class.php

--- htdocs/projet/class/project.class.php
+++ htdocs/projet/class/project.class.php
@@ -95,7 +95,7 @@ class Project extends CommonObject
 	public $dateo;
 
 	/**
-	 * @var ?int|string 	Field with Date start
+	 * @var null|int|string 	Field with Date start
 	 */
 	public $date_start;
 
@@ -106,7 +106,7 @@ class Project extends CommonObject
 	public $datee;
 
 	/**
-	 * @var ?int|string 	Field with Date end
+	 * @var null|int|string 	Field with Date end
 	 */
 	public $date_end;
 
