fb8467b4a7f22e67c814d1a54910733726a957d8
Frédéric France <frederic.france@free.fr>
2024-11-24 21:47:29 +0100

    fix CI

--- scripts/company/sync_contacts_dolibarr2ldap.php
+++ scripts/company/sync_contacts_dolibarr2ldap.php
@@ -1,8 +1,8 @@
 #!/usr/bin/env php
 <?php
 /*
- * Copyright (C) 2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
- * Copyright (C) 2006-2009 Laurent Destailleur <eldy@users.sourceforge.net>
+ * Copyright (C) 2005       Rodolphe Quiedeville    <rodolphe@quiedeville.org>
+ * Copyright (C) 2006-2009  Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
