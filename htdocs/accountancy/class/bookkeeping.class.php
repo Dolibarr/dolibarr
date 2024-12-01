424e834e0b935d4bbfd4196a18f90744212ea49d
Pichi1966 <57623859+josett225@users.noreply.github.com>
2024-12-01 18:03:07 +0100

    Update bookkeeping.class.php Find duplicates with subledger_account optional

--- htdocs/accountancy/class/bookkeeping.class.php
+++ htdocs/accountancy/class/bookkeeping.class.php
@@ -328,7 +328,7 @@ class BookKeeping extends CommonObject
 		$this->piece_num = 0;
 
 		// First check if line not yet already in bookkeeping.
-		// Note that we must include 'doc_type - fk_doc - numero_compte - label - montant - compte tiers - label compte tiers' to be sure to have unicity of line (because we may have several lines
+		// Note that we must include 'doc_type - fk_doc - numero_compte - label - subledger_account (if not empty)' to be sure to have unicity of line (because we may have several lines
 		// with same doc_type, fk_doc, numero_compte for 1 invoice line when using localtaxes with same account)
 		// WARNING: This is not reliable, label may have been modified. This is just a small protection.
 		// The page that make transfer make the test on couple (doc_type - fk_doc) only.
@@ -342,9 +342,9 @@ class BookKeeping extends CommonObject
 		}
 		$sql .= " AND numero_compte = '".$this->db->escape($this->numero_compte)."'";
 		$sql .= " AND label_operation = '".$this->db->escape($this->label_operation)."'";
-		$sql .= " AND montant = '".((float) $this->montant)."'";
-		$sql .= " AND subledger_account = '".$this->db->escape($this->subledger_account)."'";
-		$sql .= " AND subledger_label = '".$this->db->escape($this->subledger_label)."'";
+		if (!empty($this->subledger_account)) { 
+			$sql .= " AND subledger_account = '".$this->db->escape($this->subledger_account)."'";
+		}
 		$sql .= " AND entity = ".$conf->entity; // Do not use getEntity for accounting features
 
 		$resql = $this->db->query($sql);
