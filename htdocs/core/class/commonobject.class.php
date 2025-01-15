6337b1913db7e79a97b4daebf1596b0d8e18398f
Amael-PE <amael@amael.io>
2025-01-15 23:32:20 +0100

    FIX Bug that prevented FactureFournisseur creation
    Bug was due to updatePrice in CommonObject which do not have prorata_discount

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -3905,7 +3905,9 @@ abstract class CommonObject
 				$sql .= ", multicurrency_total_ht = ".((float) price2num($this->multicurrency_total_ht, 'MT', 1));
 				$sql .= ", multicurrency_total_tva = ".((float) price2num($this->multicurrency_total_tva, 'MT', 1));
 				$sql .= ", multicurrency_total_ttc = ".((float) price2num($this->multicurrency_total_ttc, 'MT', 1));
-				$sql .= ", prorata_discount = ". $this->prorata_discount;
+				if (isset($this->prorata_discount)) {
+					$sql .= ", prorata_discount = ". ((float) $this->prorata_discount);
+				}
 				$sql .= " WHERE rowid = ".((int) $this->id);
 
 				dol_syslog(get_class($this)."::update_price", LOG_DEBUG);
