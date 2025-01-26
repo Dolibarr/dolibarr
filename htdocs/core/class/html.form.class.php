d8230afa304d3b99e9ad3f813c792fc20086e443
Regis Houssin <regis.houssin@inodbox.com>
2025-01-26 11:12:10 +0100

    FIX pre-commit error

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -4567,7 +4567,7 @@ class Form
 			// The current entity takes priority and is completed by the main entity
 			if (isModEnabled('multicompany') && !empty($this->cache_types_paiements)) {
 				$entity = $conf->entity;
-				$codes = array_column(array_filter($this->cache_types_paiements, function($e) use ($entity) {
+				$codes = array_column(array_filter($this->cache_types_paiements, function ($e) use ($entity) {
 					return $e['entity'] === $entity;
 				}), 'code', 'code');
 				foreach ($this->cache_types_paiements as $key => $value) {
