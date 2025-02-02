68cc7931337a4cb6f08a425ac3e61bd4fcbf2670
Regis Houssin <regis.houssin@inodbox.com>
2025-02-02 06:34:41 +0100

    FIX restore default code

--- htdocs/categories/class/categorie.class.php
+++ htdocs/categories/class/categorie.class.php
@@ -1458,7 +1458,7 @@ class Categorie extends CommonObject
 					if ($i == count($way)) {	// Last category in hierarchy
 						// Check contrast with background and correct text color
 						$forced_color = 'categtextwhite';
-						if (empty($cat->color)) {
+						if (!empty($cat->color)) {
 							if (colorIsLight($cat->color)) {	// If color is light, we force color to dark
 								$forced_color = 'categtextblack';
 							}
