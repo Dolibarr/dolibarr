bb768dc362776eb30b9346e2c56bb1b40c3dd10a
VESSILLER <lvessiller@open-dsi.fr>
2025-02-06 15:13:09 +0100

    FIX max file size to download not needed for WebPortal

--- htdocs/webportal/controllers/document.controller.class.php
+++ htdocs/webportal/controllers/document.controller.class.php
@@ -212,15 +212,6 @@ class DocumentController extends Controller
 			exit;
 		}
 
-		$fileSize = dol_filesize($fullpath_original_file); // the returned value is in byte
-		$fileSizeMaxDefault = 20 * 1024 * 1024; // 20 Mo by default
-		$fileSizeMax = getDolGlobalInt('MAIN_SECURITY_MAXFILESIZE_DOWNLOADED', $fileSizeMaxDefault);
-		if ($fileSize > $fileSizeMax) {
-			dol_syslog('ErrorFileSizeTooLarge: ' . $fileSize);
-			print 'ErrorFileSizeTooLarge: ' . $fileSize . ' (max ' . $fileSizeMax . ')';
-			exit;
-		}
-
 		// Hooks
 		$hookmanager->initHooks(array('document'));
 		$parameters = array('ecmfile' => $ecmfile, 'modulepart' => $modulepart, 'original_file' => $original_file,
