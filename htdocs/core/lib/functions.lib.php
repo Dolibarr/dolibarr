bdd6a3e7b6ff6eb8d78db29c939ce7949b0ce858
Kévin Begot <kevinbegot@MacBook-Pro-de-Kevin.local>
2025-02-13 12:27:10 +0100

    [ADD] clean code

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -6193,19 +6193,6 @@ function dol_print_error($db = null, $error = '', $errors = null)
 			$syslog .= ", msg=".$msg;
 		}
 	}
-	if (empty($dolibarr_main_prod)
-		&& !empty($_SERVER['DOCUMENT_ROOT'])
-		&& function_exists('xdebug_call_file')
-		&& function_exists('xdebug_call_line')
-		&& function_exists('xdebug_call_function')) {
-
-		$out .= '<b>XDebug information:</b><br>' . "\n";
-		$out .= 'File: ' . xdebug_call_file() . '<br>' . "\n";
-		$out .= 'Line: ' . xdebug_call_line() . '<br>' . "\n";
-		$out .= 'Function: ' . xdebug_call_function() . '<br>' . "\n";
-		$out .= '<br>' . "\n";
-	}
-
 
 	// Return a http header with error code if possible
 	if (!headers_sent()) {
