23c7fd7dcbd57d6ca5ac235ecc8e3c0c0f04fb70
francoisPE <60675606+francoisPE@users.noreply.github.com>
2025-02-16 18:46:54 +0100

    Update card.php
    <div class="div-table-responsive-no-min" style="overflow: initial !important;" >
    
    Signed-off-by: francoisPE <60675606+francoisPE@users.noreply.github.com>

--- htdocs/fourn/commande/card.php
+++ htdocs/fourn/commande/card.php
@@ -1926,7 +1926,7 @@ if ($action == 'create') {
 		$title = $langs->trans('ProductsAndServices');
 		print load_fiche_titre($title);
 
-		print '<div class="div-table-responsive-no-min">';
+		print '<div class="div-table-responsive-no-min" style="overflow: initial !important;" >';
 		print '<table class="noborder centpercent">';
 
 		$selectedLines = array();
@@ -2482,7 +2482,7 @@ if ($action == 'create') {
 			include DOL_DOCUMENT_ROOT.'/core/tpl/ajaxrow.tpl.php';
 		}
 
-		print '<div class="div-table-responsive-no-min">';
+		print '<div class="div-table-responsive-no-min" style="overflow: initial !important;" >';
 		print '<table id="tablelines" class="noborder noshadow centpercent">';
 
 		// Add free products/services form
