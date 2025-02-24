eb0a20bb55b7d7892a5b53c3501b603e73c2f195
Frédéric FRANCE <frederic.france@free.fr>
2025-02-24 11:02:37 +0100

    fix CI branch 21

--- htdocs/core/customreports.php
+++ htdocs/core/customreports.php
@@ -1,7 +1,7 @@
 <?php
 /* Copyright (C) 2020-2024 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
- * Copyright (C) 2024		Frédéric France			<frederic.france@free.fr>
+ * Copyright (C) 2024-2025  Frédéric France			<frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,7 +39,7 @@
  * @var Translate $langs
  * @var User $user
  *
- * @var array	$toselect
+ * @var int[]	$toselect
  */
 
 if (!defined('USE_CUSTOM_REPORT_AS_INCLUDE')) {
