191f70113554f72005d8dbb3f2db805fd4e63f62
francoisPE <60675606+francoisPE@users.noreply.github.com>
2025-02-25 11:36:09 +0100

    FEAT: give validator permission note.php
    Signed-off-by: francoisPE <60675606+francoisPE@users.noreply.github.com>

--- htdocs/fourn/commande/note.php
+++ htdocs/fourn/commande/note.php
@@ -55,8 +55,8 @@ $object = new CommandeFournisseur($db);
 $object->fetch($id, $ref);
 
 // Permissions
-$permissionnote = ($user->hasRight("fournisseur", "commande", "creer") || $user->hasRight("supplier_order", "creer")); // Used by the include of actions_setnotes.inc.php
 $usercancreate	= ($user->hasRight("fournisseur", "commande", "creer") || $user->hasRight("supplier_order", "creer"));
+$permissionnote = ((!empty($usercancreate)) || (getDolGlobalString('MAIN_USE_ADVANCED_PERMS') && $user->hasRight("fournisseur", "supplier_invoice_advance", "validate")));
 $permissiontoadd	= $usercancreate; // Used by the include of actions_addupdatedelete.inc.php
 
 
