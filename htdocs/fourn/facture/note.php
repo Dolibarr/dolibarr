f97ae7a4b7cebbc04016f9bccfd5d1a79d25b95a
francoisPE <60675606+francoisPE@users.noreply.github.com>
2025-02-25 19:43:08 +0100

    remove dead code in note.php
    Signed-off-by: francoisPE <60675606+francoisPE@users.noreply.github.com>

--- htdocs/fourn/facture/note.php
+++ htdocs/fourn/facture/note.php
@@ -54,7 +54,6 @@ $object = new FactureFournisseur($db);
 $object->fetch($id, $ref);
 
 $usercancreate = ($user->hasRight("fournisseur", "facture", "creer") || $user->hasRight("supplier_invoice", "creer"));
-$permissionnote = ((!empty($usercancreate)) || (getDolGlobalString('MAIN_USE_ADVANCED_PERMS') && $user->hasRight("fournisseur", "supplier_invoice_advance", "validate")));
 $permissiontoadd = $usercancreate;
 
 /*
