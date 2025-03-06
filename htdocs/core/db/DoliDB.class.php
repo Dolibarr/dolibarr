80c762c1b1d3e3e9ad5cf5cf8f4a82b8d1ccf662
Eric Seigne <eric.seigne@cap-rel.fr>
2025-03-06 15:51:51 +0100

    sanitize backport from 19.0

--- htdocs/core/db/DoliDB.class.php
+++ htdocs/core/db/DoliDB.class.php
@@ -121,24 +121,17 @@ abstract class DoliDB implements Database
 	}
 
 	/**
-	 * Sanitize a string for SQL forging
-	 *
-	 * @param   string 	$stringtosanitize 	String to escape
-	 * @param   int		$allowsimplequote 	1=Allow simple quotes in string. When string is used as a list of SQL string ('aa', 'bb', ...)
-	 * @param   int		$allowspaces 		1=Allow spaces in string. When string is a sentense or a composed word
-	 * @return  string                      String escaped
-	 */
-	public function sanitize($stringtosanitize, $allowsimplequote = 0, $allowspaces = 0)
+		 * Sanitize a string for SQL forging
+		 *
+		 * @param   string 	$stringtosanitize 	String to escape
+		 * @param   int		$allowsimplequote 	1=Allow simple quotes in string. When string is used as a list of SQL string ('aa', 'bb', ...)
+		 * @param	int		$allowsequals		1=Allow equals sign
+		 * @param	int		$allowsspace		1=Allow space char
+		 * @return  string                      String escaped
+		 */
+	public function sanitize($stringtosanitize, $allowsimplequote = 0, $allowsequals = 0, $allowsspace = 0)
 	{
-		$regex = "^a-z0-9_\-\.,";
-		if ($allowspaces) {
-			$regex .= " ";
-		}
-		if ($allowsimplequote) {
-			$regex .= "\'";
-		}
-
-		return preg_replace('/[' . $regex . ']/i', '', $stringtosanitize);
+		return preg_replace('/[^a-z0-9_\-\.,'.($allowsequals ? '=' : '').($allowsimplequote ? "\'" : '').($allowsspace ? ' ' : '').']/i', '', $stringtosanitize);
 	}
 
 	/**
