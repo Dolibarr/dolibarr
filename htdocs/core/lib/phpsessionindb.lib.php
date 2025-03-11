9c123b78ae132f6507e01e11a4427c3ee8e3facb
Frédéric FRANCE <frederic.france@free.fr>
2025-03-11 13:33:38 +0100

    list sessions in db

--- htdocs/core/lib/phpsessionindb.lib.php
+++ htdocs/core/lib/phpsessionindb.lib.php
@@ -251,7 +251,7 @@ session_set_save_handler("dolSessionOpen", "dolSessionClose", "dolSessionRead", 
 /**
  * List sessions in db
  *
- * @return array<mixed, array{login: ?string, age: string, remote_ip: string, user_agent: string}>
+ * @return array<mixed,array{login:?string,age:string,creation:?int,modification:?int,remote_ip:string,user_agent:string}>
  */
 function dolListSessions()
 {
@@ -268,6 +268,8 @@ function dolListSessions()
 		$arrayofsessions[$obj->session_id] = [
 			"login" => $obj->login,
 			"age" => $obj->last_accessed,
+			"creation" => null,
+			"modification" => null,
 			"remote_ip" => $obj->remote_ip,
 			"user_agent" => $obj->user_agent,
 		];
