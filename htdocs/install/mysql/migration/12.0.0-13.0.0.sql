22b0473845a4aea6ebb15e6019b4ac46980080a2
Eric Seigne <eric.seigne@cap-rel.fr>
2025-03-13 15:13:03 +0100

    tms field in llx_product is not updated to default timestamp

--- htdocs/install/mysql/migration/12.0.0-13.0.0.sql
+++ htdocs/install/mysql/migration/12.0.0-13.0.0.sql
@@ -333,6 +333,8 @@ ALTER TABLE llx_product_warehouse_properties MODIFY COLUMN desiredstock float;
 
 ALTER TABLE llx_product ADD COLUMN fk_state integer DEFAULT NULL AFTER fk_country;
 
+ALTER TABLE llx_product CHANGE COLUMN tms tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
 ALTER TABLE llx_projet ADD COLUMN email_msgid varchar(255);
 ALTER TABLE llx_ticket ADD COLUMN email_msgid varchar(255);
 ALTER TABLE llx_actioncomm ADD COLUMN reply_to varchar(255);
