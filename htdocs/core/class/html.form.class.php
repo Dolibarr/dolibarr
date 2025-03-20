fb13d5c26ee7d9a121fce1152bec6b38150f86f2
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2025-03-20 12:07:34 +0100

    Fix : MAIN_SEARCH_PRODUCT_BY_FOURN_REF search critera

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -3089,7 +3089,7 @@ class Form
 
 				// include search in supplier ref
 				if (getDolGlobalString('MAIN_SEARCH_PRODUCT_BY_FOURN_REF')) {
-					$sqlSupplierSearch .= !empty($sqlSupplierSearch) ? ' OR ':'';
+					$sqlSupplierSearch .= !empty($sqlSupplierSearch) ? ' AND ':'';
 					$sqlSupplierSearch .= " pfp.ref_fourn LIKE '" . $this->db->escape($prefix . $crit) . "%'";
 				}
 				$sql .= ")";
