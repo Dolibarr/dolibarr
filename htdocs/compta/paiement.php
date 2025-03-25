4c951bbf9ce4cce897439f084f03ce7c3c41dbbd
Laurent Destailleur <eldy@destailleur.fr>
2025-03-25 16:34:19 +0100

    Update paiement.php

--- htdocs/compta/paiement.php
+++ htdocs/compta/paiement.php
@@ -503,7 +503,7 @@ if ($result >= 0) {
 	$datepayment = dol_mktime(12, 0, 0, GETPOSTINT('remonth'), GETPOSTINT('reday'), GETPOSTINT('reyear'));
 	$datepayment = ($datepayment == '' ? (!getDolGlobalString('MAIN_AUTOFILL_DATE') ? -1 : '') : $datepayment);
 	$adddateof = array(array('adddateof'=>$facture->date));
-	$adddateof[] = array('adddateof'=>$facture->date_lim_reglement, 'labeladddateof'=>$langs->transnoentities('DateMaxPayment'));
+	$adddateof[] = array('adddateof'=>$facture->date_lim_reglement, 'labeladddateof'=>$langs->transnoentities('DateDue'));
 	print $form->selectDate($datepayment, '', 0, 0, 0, "add_paiement", 1, 1, 0, '', '', $adddateof);
 	print '</td></tr>';
 
