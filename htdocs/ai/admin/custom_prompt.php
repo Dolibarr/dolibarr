956f8a7d718aba89f9c4439c69a01ce1e602d4ad
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2025-03-26 15:18:44 +0100

    Update custom_prompt.php
    changed:  $arrayofia -> $arrayofai

--- htdocs/ai/admin/custom_prompt.php
+++ htdocs/ai/admin/custom_prompt.php
@@ -41,7 +41,7 @@ require_once DOL_DOCUMENT_ROOT."/core/class/html.formai.class.php";
 $langs->loadLangs(array("admin", "website", "other"));
 
 $arrayofaifeatures = getListOfAIFeatures();
-$arrayofia = getListOfAIServices();
+$arrayofai = getListOfAIServices();
 
 // Parameters
 $action = GETPOST('action', 'aZ09');
