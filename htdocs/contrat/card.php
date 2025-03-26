8727f90e1e62586e72dc698578be1e2392ed13bd
atm-florian <florian.mortgat@atm-consulting.fr>
2025-03-26 15:19:26 +0100

    FIX v22 warnings and remove seemingly useless code

--- htdocs/contrat/card.php
+++ htdocs/contrat/card.php
@@ -1689,7 +1689,7 @@ if ($action == 'create') {
 					// Label
 					print '<td class="linecoldescription minwidth300imp">';
 					$coldisplay++;
-					print '<div id="line_'.$line->id.'"></div>';
+					print '<div id="line_'.$line->rowid.'"></div>';
 					if ($objp->fk_product > 0) {
 						$productstatic->id = $objp->fk_product;
 						$productstatic->type = $objp->ptype;
@@ -1718,13 +1718,7 @@ if ($action == 'create') {
 
 						// Add description in form
 						if ($line->fk_product > 0 && getDolGlobalInt('PRODUIT_DESC_IN_FORM_ACCORDING_TO_DEVICE')) {
-							if ($line->element == 'facturedetrec') {
-								print (!empty($line->description) && $line->description != $line->product_label) ? (($line->date_start_fill || $line->date_end_fill) ? '' : '<br>').'<br>'.dol_htmlentitiesbr($line->description) : '';
-							} elseif ($line->element == 'invoice_supplier_det_rec') {
-								print (!empty($line->description) && $line->description != $line->label) ? (($line->date_start || $line->date_end) ? '' : '<br>').'<br>'.dol_htmlentitiesbr($line->description) : '';
-							} else {
-								print (!empty($line->description) && $line->description != $line->product_label) ? (($line->date_start || $line->date_end) ? '' : '<br>').'<br>'.dol_htmlentitiesbr($line->description) : '';
-							}
+							print (!empty($line->description) && $line->description != $line->product_label) ? (($line->date_start || $line->date_end) ? '' : '<br>').'<br>'.dol_htmlentitiesbr($line->description) : '';
 						}
 					} else {
 						print img_object($langs->trans("ShowProductOrService"), ($objp->product_type ? 'service' : 'product')).' '.dol_htmlentitiesbr($objp->description)."\n";
