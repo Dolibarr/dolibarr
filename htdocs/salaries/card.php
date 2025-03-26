4ec2535dede023f227cf40234d6525b67d795314
Frédéric FRANCE <frederic34@users.noreply.github.com>
2025-03-26 16:21:41 +0100

    Update card.php

--- htdocs/salaries/card.php
+++ htdocs/salaries/card.php
@@ -1,14 +1,14 @@
 <?php
 /* Copyright (C) 2011-2024	Alexandre Spangaro			<alexandre@inovea-conseil.com>
  * Copyright (C) 2014-2020	Laurent Destailleur			<eldy@users.sourceforge.net>
- * Copyright (C) 2015		    Jean-François Ferry			<jfefe@aternatik.fr>
- * Copyright (C) 2015		    Charlie BENKE				    <charlie@patas-monkey.com>
- * Copyright (C) 2018-2024	Frédéric France				  <frederic.france@free.fr>
- * Copyright (C) 2021		    Gauthier VERDOL				  <gauthier.verdol@atm-consulting.fr>
- * Copyright (C) 2023		    Maxime Nicolas				  <maxime@oarces.com>
- * Copyright (C) 2023		    Benjamin GREMBI				  <benjamin@oarces.com>
- * Copyright (C) 2024-2025	MDW							        <mdeweerd@users.noreply.github.com>
- * Copyright (C) 2024-2025		Nick Fragoulis
+ * Copyright (C) 2015		Jean-François Ferry			<jfefe@aternatik.fr>
+ * Copyright (C) 2015		Charlie BENKE				<charlie@patas-monkey.com>
+ * Copyright (C) 2018-2024	Frédéric France				<frederic.france@free.fr>
+ * Copyright (C) 2021		Gauthier VERDOL				<gauthier.verdol@atm-consulting.fr>
+ * Copyright (C) 2023		Maxime Nicolas				<maxime@oarces.com>
+ * Copyright (C) 2023		Benjamin GREMBI				<benjamin@oarces.com>
+ * Copyright (C) 2024-2025	MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2024-2025	Nick Fragoulis
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
