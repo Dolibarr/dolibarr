a45c9dd5e1be571bbdaa65314501428d7608862e
Laurent Destailleur <eldy@destailleur.fr>
2025-03-31 23:52:39 +0200

    Update myobject.class.php

--- htdocs/modulebuilder/template/class/myobject.class.php
+++ htdocs/modulebuilder/template/class/myobject.class.php
@@ -1099,7 +1099,7 @@ class MyObject extends CommonObject
 			$this->setErrorsFromObject($objectline);
 			return $result;
 		} else {
-			/** @phpstan-ignore-next-line */	
+			/** @phpstan-ignore-next-line */
 			$this->lines = $result;
 			return $this->lines;
 		}
