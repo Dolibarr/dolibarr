a98602bc57e6a5519abd00f0136626124523cf90
Laurent Destailleur <eldy@destailleur.fr>
2025-04-01 20:54:27 +0200

    Update price.lib.php

--- htdocs/core/lib/price.lib.php
+++ htdocs/core/lib/price.lib.php
@@ -198,9 +198,9 @@ function calcul_price_total($qty, $pu, $remise_percent_ligne, $txtva, $uselocalt
 	}
 
 	// initialize total (may be HT or TTC depending on price_base_type)
-	if ($remise_percent_ligne && getDolGlobalInt('MAIN_ROUND_UNIT_PRICE_BEFORE_QTY', 0)) {
+	if ($remise_percent_ligne && getDolGlobalString('MAIN_APPLY_DISCOUNT_ON_UNIT_PRICE_THEN_ROUND_BEFORE_MULTIPLICATION_BY_QTY')) {	// MAIN_APPLY_DISCOUNT_ON_UNIT_PRICE_THEN_ROUND_BEFORE_MULTIPLICATION_BY_QTY can be 'MU', 2, ...
 		$tot_sans_remise = $pu * $qty * ($progress / 100);
-		$tot_avec_remise_ligne = round($pu * (1 - ((float) $remise_percent_ligne / 100)), 2) * $qty * ($progress / 100);
+		$tot_avec_remise_ligne = price2num($pu * (1 - ((float) $remise_percent_ligne / 100)), getDolGlobalString('MAIN_APPLY_DISCOUNT_ON_UNIT_PRICE_THEN_ROUND_BEFORE_MULTIPLICATION_BY_QTY')) * $qty * ($progress / 100);
 	} else {
 		$tot_sans_remise = $pu * $qty * ($progress / 100);
 		$tot_avec_remise_ligne = $tot_sans_remise * (1 - ((float) $remise_percent_ligne / 100));
