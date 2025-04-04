5b092a47c04d4bcac3bb4f1067b05ae818dfdc1a
Frédéric FRANCE <frederic.france@free.fr>
2025-04-04 12:15:26 +0200

    fix

--- htdocs/core/class/extrafields.class.php
+++ htdocs/core/class/extrafields.class.php
@@ -9,7 +9,7 @@
  * Copyright (C) 2015-2023  Charlene BENKE          <charlene@patas-monkey.com>
  * Copyright (C) 2016       Raphaël Doursenaud      <rdoursenaud@gpcsolutions.fr>
  * Copyright (C) 2017       Nicolas ZABOURI         <info@inovea-conseil.com>
- * Copyright (C) 2018-2024  Frédéric France         <frederic.france@free.fr>
+ * Copyright (C) 2018-2025  Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2022 		Antonin MARCHAL         <antonin@letempledujeu.fr>
  * Copyright (C) 2024-2025	MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024		Benoît PASCAL			<contact@p-ben.com>
@@ -999,7 +999,7 @@ class ExtraFields
 			$elementtype = 'commande_fournisseur';
 		}
 
-		if ($elementtype != 'all' && $this->attributes[$elementtype]['loaded'] == 1 && !$forceload) {
+		if ($elementtype != 'all' && isset($this->attributes[$elementtype]) && $this->attributes[$elementtype]['loaded'] == 1 && !$forceload) {
 			return $this->attributes[$elementtype]['label'];
 		}
 
