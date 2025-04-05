a4edb7115c2fcf976205c9588240d71e5fdd3d68
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-04-05 22:49:26 +0200

    adding api and object for showing objectlinks. I've created it as an object in case others needed it

---
+++ htdocs/api/class/api_objectlinks.class.php
@@ -0,0 +1,216 @@
+<?php
+/*
+/* Copyright (C) 2025  Jon Bendtsen         <jon.bendtsen.github@jonb.dk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+require_once DOL_DOCUMENT_ROOT.'/api/class/api.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/functions.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/objectlink.class.php';
+
+
+/**
+ * API that gives shows links between objects in an Dolibarr instance.
+ *
+ * @access protected
+ * @class  DolibarrApiAccess {@requires user,external}
+ */
+class ObjectLinks extends DolibarrApi
+{
+	/**
+	 * @var ObjectLink $objectlink {@type ObjectLink}
+	 */
+	public $objectlink;
+
+	/**
+	 * Constructor of the class
+	 */
+	public function __construct()
+	{
+		global $db;
+		$this->db = $db;
+		$this->objectlink = new ObjectLink($this->db);
+	}
+
+	/**
+	 * Get properties of a ObjectLink object
+	 *
+	 * Return an array with object link information
+	 *
+	 * @param   int         $id				ID of objectlink
+	 * @return  Object						Object with cleaned properties
+	 *
+	 * @throws	RestException
+	 */
+	public function get($id)
+	{
+		return $this->_fetch($id);
+	}
+
+	/**
+	 * Delete an object link
+	 *
+	 * @param   int     $id         object link ID
+	 * @return  array
+	 */
+	public function delete($id)
+	{
+		// Reverse permission check. First we find out which kind of objects are linked, and if the user has rights to that then we present it.
+		$result = $this->objectlink->fetch($id);
+
+		$result = $this->db->query($sql);
+		if ($result) {
+			if (!DolibarrApiAccess::$user->hasRight(((string) $result->sourcetype), 'lire')) {
+				throw new RestException(403);
+			}
+			if (!DolibarrApiAccess::$user->hasRight(((string) $result->targettype), 'lire')) {
+				throw new RestException(403);
+			}
+		} else {
+			throw new RestException(404, 'Object Link not found');
+		}
+
+		if (!$this->objectlink->delete(DolibarrApiAccess::$user)) {
+			throw new RestException(500, 'Error when delete objectlink : '.$this->objectlink->error);
+		}
+
+		return array(
+			'success' => array(
+				'code' => 200,
+				'message' => 'object link deleted'
+			)
+		);
+	}
+
+	/**
+	 * Get properties of an object link
+	 *
+	 * Return an array with object links
+	 *
+	 * @param   int         $id             ID of objectlink
+	 * @return  Object						Object with cleaned properties
+	 *
+	 * @throws	RestException
+	 */
+	private function _fetch($id)
+	{
+		$result = $this->objectlink->fetch($id);
+		if ($result) {
+			if (!DolibarrApiAccess::$user->hasRight(((string) $this->objectlink->sourcetype), 'lire')) {
+				throw new RestException(403);
+			}
+			if (!DolibarrApiAccess::$user->hasRight(((string) $this->objectlink->targettype), 'lire')) {
+				throw new RestException(403);
+			}
+		} else {
+			throw new RestException(404, 'Object Link not found');
+		}
+
+		return $this->_cleanObjectDatas($this->objectlink);
+	}
+
+	// phpcs:disable PEAR.NamingConventions.ValidFunctionName.PublicUnderscore
+	/**
+	 * Clean sensible object datas
+	 *
+	 * @param   Object  $object     Object to clean
+	 * @return  Object              Object with cleaned properties
+	 */
+	protected function _cleanObjectDatas($object)
+	{
+		// phpcs:enable
+		$object = parent::_cleanObjectDatas($object);
+
+		unset($object->module);
+		unset($object->entity);
+		unset($object->import_key);
+		unset($object->array_languages);
+		unset($object->contacts_ids);
+		unset($object->linkedObjectsIds);
+		unset($object->canvas);
+		unset($object->fk_project);
+		unset($object->contact_id);
+		unset($object->user);
+		unset($object->origin_type);
+		unset($object->origin_id);
+		unset($object->ref);
+		unset($object->ref_ext);
+		unset($object->statut);
+		unset($object->status);
+		unset($object->country_id);
+		unset($object->country_code);
+		unset($object->state_id);
+		unset($object->region_id);
+		unset($object->barcode_type);
+		unset($object->barcode_type_coder);
+		unset($object->mode_reglement_id);
+		unset($object->cond_reglement_id);
+		unset($object->demand_reason_id);
+		unset($object->transport_mode_id);
+		unset($object->shipping_method_id);
+		unset($object->shipping_method);
+		unset($object->fk_multicurrency);
+		unset($object->multicurrency_code);
+		unset($object->multicurrency_tx);
+		unset($object->multicurrency_total_ht);
+		unset($object->multicurrency_total_tva);
+		unset($object->multicurrency_total_ttc);
+		unset($object->multicurrency_total_localtax1);
+		unset($object->multicurrency_total_localtax2);
+		unset($object->last_main_doc);
+		unset($object->fk_account);
+		unset($object->note_public);
+		unset($object->note_private);
+		unset($object->total_ht);
+		unset($object->total_tva);
+		unset($object->total_localtax1);
+		unset($object->total_localtax2);
+		unset($object->total_ttc);
+		unset($object->lines);
+		unset($object->actiontypecode);
+		unset($object->name);
+		unset($object->lastname);
+		unset($object->firstname);
+		unset($object->civility_id);
+		unset($object->date_creation);
+		unset($object->date_validation);
+		unset($object->date_modification);
+		unset($object->tms);
+		unset($object->date_cloture);
+		unset($object->user_author);
+		unset($object->user_creation);
+		unset($object->user_creation_id);
+		unset($object->user_valid);
+		unset($object->user_validation);
+		unset($object->user_validation_id);
+		unset($object->user_closing_id);
+		unset($object->user_modification);
+		unset($object->user_modification_id);
+		unset($object->fk_user_creat);
+		unset($object->fk_user_modif);
+		unset($object->totalpaid);
+		unset($object->product);
+		unset($object->cond_reglement_supplier_id);
+		unset($object->deposit_percent);
+		unset($object->retained_warranty_fk_cond_reglement);
+		unset($object->warehouse_id);
+		unset($object->target);
+		unset($object->array_options);
+		unset($object->extraparams);
+		unset($object->specimen);
+
+		return $object;
+	}
+}
---
+++ htdocs/core/class/objectlink.class.php
@@ -0,0 +1,211 @@
+<?php
+/* Copyright (C) 2025  Jon Bendtsen         <jon.bendtsen.github@jonb.dk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ *	\file       htdocs/core/class/commonobject.class.php
+ *	\ingroup    core
+ *	\brief      File of parent class of all other business classes (invoices, contracts, proposals, orders, ...)
+ */
+
+require_once DOL_DOCUMENT_ROOT.'/core/class/doldeprecationhandler.class.php';
+
+/**
+ *	Parent class of all other business classes (invoices, contracts, proposals, orders, ...)
+ *
+ * @phan-forbid-undeclared-magic-properties
+ */
+class ObjectLink extends CommonObject
+{
+	/**
+	 * @var string ID to identify managed object
+	 */
+	public $element = 'objectlink';
+
+	/**
+	 * @var string Name of table without prefix where object is stored
+	 */
+	public $table_element = 'element_element';
+
+	const TRIGGER_PREFIX = 'OBJECTLINK'; // to be overridden in child class implementations, i.e. 'BILL', 'TASK', 'PROPAL', etc.
+
+	/**
+	 * @var int source id is a foreign key
+	 */
+	public $fk_source;
+	/**
+	 * @var string source type
+	 */
+	public $sourcetype;
+
+	/**
+	 * @var int source id is a foreign key
+	 */
+	public $target;
+	/**
+	 * @var string source type
+	 */
+	public $targettype;
+
+	/**
+	 * @var string relation type, not sure if ever used, but it is in the database
+	 */
+	public $relationtype;
+
+	/**
+	 * Constructor of the class
+	 */
+	public function __construct()
+	{
+		global $db;
+		$this->db = $db;
+	}
+
+	/**
+	 *	Get object link from database.
+	 *
+	 *	@param      int			$rowid       	row Id of object link
+	 *	@return     int         				>0 if OK, <0 if KO, 0 if not found
+	 */
+	public function fetch($rowid) {
+		$sql = "SELECT rowid, fk_source, sourcetype, fk_target,";
+		$sql .= " targettype, relationtype FROM";
+		$sql .= " ".MAIN_DB_PREFIX.$this->table_element;
+		$sql .= " WHERE rowid = ".((int) $rowid);
+
+		dol_syslog(get_class($this)."::fetch", LOG_DEBUG);
+		$result = $this->db->query($sql);
+		if ($result) {
+			$obj = $this->db->fetch_object($result);
+			if ($obj) {
+				$this->id = $obj->rowid;
+				$this->entity = $obj->entity;
+
+				$this->fk_source = $obj->fk_source;
+				$this->sourcetype = $obj->sourcetype;
+				$this->fk_target = $obj->fk_target;
+				$this->targettype = $obj->targettype;
+				$this->relationtype = $obj->relationtype;
+
+				return 1;
+			} else {
+				$this->error = 'Object link with id '.$rowid.' not found sql='.$sql;
+				return 0;
+			}
+		} else {
+			$this->error = $this->db->error();
+			return -1;
+		}
+	}
+
+	/**
+	 *	Delete the object link
+	 *
+	 *	@param	User	$user		User object
+	 *	@param	int		$rowid		rowid of object link
+	 *	@param	int		$notrigger	1=Does not execute triggers, 0= execute triggers
+	 * 	@return	int					Return integer <=0 if KO, >0 if OK
+	 */
+	public function delete($user, $rowid, $notrigger = 0) {
+
+		global $conf, $langs;
+		require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
+
+		$error = 0;
+
+		dol_syslog(get_class($this)."::delete ".$this->id, LOG_DEBUG);
+
+		$this->db->begin();
+
+		if (!$notrigger) {
+			// Call trigger
+			$result = $this->call_trigger('OBJECTLINK_DELETE', $user);
+			if ($result < 0) {
+				$error++;
+			}
+			// End call triggers
+		}
+
+		// Delete object link 	
+		if (!$error) {
+			$sql = "DELETE FROM ".MAIN_DB_PREFIX.$this->table_element." WHERE rowid = ".((int) $this->id);
+			$res = $this->db->query($sql);
+			if (!$res) {
+				$error++;
+				$this->error = $this->db->lasterror();
+				$this->errors[] = $this->error;
+				dol_syslog(get_class($this)."::delete error ".$this->error, LOG_ERR);
+			}
+		}
+
+		if (!$error) {
+			dol_syslog(get_class($this)."::delete ".$this->id." by ".$user->id, LOG_DEBUG);
+			$this->db->commit();
+			return 1;
+		} else {
+			$this->db->rollback();
+			return -1;
+		}
+	}
+	 
+	/**
+	 *	Create object link
+	 *
+	 *	@param		User	$user			Object user that make creation
+	 *  @param		int		$fk_source		source id of object we link from
+	 *  @param		string	$sourcetype		type of the source object
+	 *  @param		int		$fk_target		target id of object we link to
+	 *  @param		string	$targettype 	type of the target object
+	 *  @param		string	$relationtype 	type of the relation, usually null
+	 *	@param		int	    $notrigger		Disable all triggers
+	 *	@return 	int			        	Return integer <0 if KO, >0 if OK
+	 */
+	public function create($user, $fk_source, $sourcetype, $fk_target, $targettype, $relationtype = Null, $notrigger = 0)
+	{
+		global $conf, $langs;
+		$error = 0;
+
+		// Clean parameters
+		dol_syslog(get_class($this)."::create user=".$user->id);
+
+		$this->db->begin();
+
+		$sql = "INSERT INTO ".MAIN_DB_PREFIX."$this->table_element";
+		if ($relationtype) {
+			$sql .= " (fk_source, sourcetype, fk_target, targettype, relationtype )";
+		} else {
+			$sql .= " (fk_source, sourcetype, fk_target, targettype )";
+		}
+		$sql .= " VALUES (".((int) $this->fk_source).", ".((string) $this->sourcetype).",";
+		$sql .= ((int) $this->fk_target).", ".((string) $this->targettype);
+		if ($relationtype) {
+			$sql .= ", ".((string) $this->targettype);
+		}
+		$sql .= ")";
+
+		dol_syslog(get_class($this)."::create", LOG_DEBUG);
+		dol_syslog("sql=".$sql, LOG_DEBUG);
+		$resql = $this->db->query($sql);
+		if ($resql) {
+			return 0;
+		} else {
+			$this->error = $this->db->lasterror();
+			$this->db->rollback();
+			return -1;
+		}
+	}
+
+}
