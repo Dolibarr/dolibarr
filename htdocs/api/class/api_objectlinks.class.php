9cce312ab261e670b9260857cc619509e8384641
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-04-06 23:02:09 +0200

    result is now not a static number

--- htdocs/api/class/api_objectlinks.class.php
+++ htdocs/api/class/api_objectlinks.class.php
@@ -107,8 +107,7 @@ class ObjectLinks extends DolibarrApi
 			throw new RestException(403, 'denied access to create the objectlinks targettype='.$this->objectlink->targettype);
 		}
 
-		//$result = $this->objectlink->create(DolibarrApiAccess::$user, $fk_source, $sourcetype, $fk_target, $targettype, $relationtype, $notrigger);
-		$result = -1;
+		$result = $this->objectlink->create(DolibarrApiAccess::$user, $fk_source, $sourcetype, $fk_target, $targettype, $relationtype, $notrigger);
 
 		if ($result < 0 ) {
 			throw new RestException(500, 'Error when create objectlink : '.$this->objectlink->error);
