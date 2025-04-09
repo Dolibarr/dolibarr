0cfea138ff2f1a6563bed04af8bd65e716c78ab9
Hystepik <hystepik@gmail.com>
2025-04-09 14:24:06 +0200

    New AI style rephraser

--- htdocs/core/class/html.formai.class.php
+++ htdocs/core/class/html.formai.class.php
@@ -133,6 +133,13 @@ class FormAI extends Form
 		$out .= img_picto('', 'edit', 'class="pictofixedwidth paddingrightonly"');
 		$out .= $form->selectarray("ai_summarize".$htmlContent."_select", $summarizearray, 0, $langs->trans("SummarizeByAI").'...', 0, 0, 'minwidth250 ai_summarize'.$htmlContent.'_select', 1);
 		$out .= '</div>';
+		$out .= '<br>';
+
+		$stylearray = getListForAIRephraseStyle();
+		$out .= '<div id="ai_rephraser'.$htmlContent.'" class="ai_rephraser'.$htmlContent.' paddingtop paddingbottom ai_feature">';
+		$out .= img_picto('', 'edit', 'class="pictofixedwidth paddingrightonly"');
+		$out .= $form->selectarray("ai_rephraser".$htmlContent."_select", $stylearray, 0, $langs->trans("RephraserByAI").'...', 0, 0, 'minwidth250 ai_rephraser'.$htmlContent.'_select', 1);
+		$out .= '</div>';
 
 		$out .= '<div id="ai_status_message'.$htmlContent.'" class="fieldrequired hideobject marginrightonly margintoponly">';
 		$out .= '<i class="fa fa-spinner fa-spin fa-2x fa-fw valignmiddle marginrightonly"></i>'.$langs->trans("AIProcessingPleaseWait", getDolGlobalString('AI_API_SERVICE', 'chatgpt'));
@@ -147,6 +154,7 @@ class FormAI extends Form
 			$(document).ready(function() {
 				$('#ai_translation".$htmlContent."_select').data('functionai', 'texttranslation')
 				$('#ai_summarize".$htmlContent."_select').data('functionai', 'textsummarize')
+				$('#ai_rephraser".$htmlContent."_select').data('functionai', 'textrephraser')
 
 				$('#ai_instructions".$htmlContent."').keyup(function(){
 					console.log('We type a key up on #ai_instructions".$htmlContent."');
@@ -179,6 +187,13 @@ class FormAI extends Form
 				});
 
 				$('#ai_summarize".$htmlContent."_select').on('change', function() {
+					console.log('We change #ai_summarize".$htmlContent."_select with lang '+$(this).val());
+					if ($(this).val() != null && $(this).val() != '' && $(this).val() != '-1') {
+						prepareCallAIGenerator($(this));
+					}
+				});
+
+				$('#ai_rephraser".$htmlContent."_select').on('change', function() {
 					console.log('We change #ai_summarize".$htmlContent."_select with lang '+$(this).val());
 					if ($(this).val() != null && $(this).val() != '' && $(this).val() != '-1') {
 						prepareCallAIGenerator($(this));
@@ -240,7 +255,10 @@ class FormAI extends Form
 								break;
 						}
 						instructions = 'Summarize the following text '+ (unit == 'percent' ? 'by ' : 'in') + width + ' ' + unit + ': ' + texttomodify;
-					}else {
+					} else if (functionai == 'textrephraser') {
+						style = $('#ai_rephraser'+htmlname+'_select').val();
+						instructions = 'Rephrase the following text in the '+style+' style: ' + texttomodify;
+					} else {
 						instructions = userprompt;
 					}
 
@@ -364,12 +382,14 @@ class FormAI extends Form
 							//}
 						}
 
-						// remove readonly
+						// Remove all value from Ai Section select
 						$('#ai_instructions'+htmlname).val('');
 						$('#ai_translation'+htmlname+'_select').val('-1');
 						$('#ai_translation'+htmlname+'_select').trigger('change');
 						$('#ai_summarize'+htmlname+'_select').val('-1');
 						$('#ai_summarize'+htmlname+'_select').trigger('change');
+						$('#ai_rephraser'+htmlname+'_select').val('-1');
+						$('#ai_rephraser'+htmlname+'_select').trigger('change');
 						$('#ai_status_message'+htmlname).hide();
 						$('#ai_dropdown'+htmlname).hide();
 					},
