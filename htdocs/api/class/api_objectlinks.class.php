21ce730bfe02ae03fd6255fc3eea5738c3fd66b7
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-04-09 22:07:04 +0200

    making sure deleteByValues always has a return statement

--- htdocs/api/class/api_objectlinks.class.php
+++ htdocs/api/class/api_objectlinks.class.php
@@ -312,13 +312,7 @@ class ObjectLinks extends DolibarrApi
 
 		if ($findresult < 0 ) {
 			throw new RestException(500, 'Error when finding objectlink : '.$this->objectlink->error);
-		}
-
-		if ($findresult == 0 ) {
-			throw new RestException(404, 'Object Link not found');
-		}
-
-		if ($findresult > 0 ) {
+		} elseif ($findresult > 0 ) {
 			$result = $this->objectlink->delete(DolibarrApiAccess::$user, $this->objectlink->notrigger);
 
 			if ($result < 0 ) {
@@ -331,6 +325,9 @@ class ObjectLinks extends DolibarrApi
 					'message' => 'object link deleted'
 				)
 			);
+
+		} else {
+			throw new RestException(404, 'Object Link not found');
 		}
 	}
 
