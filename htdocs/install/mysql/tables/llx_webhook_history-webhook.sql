a5e60620f51be449101fbb5c1af2e236262a8a12
Laurent Destailleur <eldy@destailleur.fr>
2025-04-16 17:54:23 +0200

    Update llx_webhook_history-webhook.sql

--- htdocs/install/mysql/tables/llx_webhook_history-webhook.sql
+++ htdocs/install/mysql/tables/llx_webhook_history-webhook.sql
@@ -17,17 +17,17 @@
 CREATE TABLE llx_webhook_history(
 	-- BEGIN MODULEBUILDER FIELDS
 	rowid integer AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	ref varchar(128) NOT NULL,
+	--ref varchar(128) NOT NULL,
 	trigger_data text NOT NULL,
 	fk_target integer NOT NULL,
 	url integer NOT NULL,
-	note_public text,
+	--note_public text,
 	note_private text,
 	date_creation datetime NOT NULL,
 	tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	fk_user_creat integer NOT NULL,
-	fk_user_modif integer,
+	--fk_user_modif integer,
 	import_key varchar(14),
-	status integer NOT NULL
+	status integer DEFAULT 1 NOT NULL
 	-- END MODULEBUILDER FIELDS
 ) ENGINE=innodb;
