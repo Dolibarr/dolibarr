dac23ab35a48ed42f36b884188954725cb0a2a1e
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-04-18 09:27:07 +0200

    adding more statuses and explaining boarding pass better

--- htdocs/eventorganization/class/conferenceorboothattendee.class.php
+++ htdocs/eventorganization/class/conferenceorboothattendee.class.php
@@ -61,13 +61,15 @@ class ConferenceOrBoothAttendee extends CommonObject
 
 	const STATUS_DRAFT = 0;
 	const STATUS_VALIDATED = 1;
+	const STATUS_ORDERED = 4;				// order has been sent
+	const STATUS_INVOICED = 6;				// invoice has been sent
 	const STATUS_CANCELED = 9;
 	const STATUS_PAIDPARTIALLY = 20;		// if at the conference entrance ask for more payment
 	const STATUS_PAIDFULL = 25;
 	const STATUS_BOARDINGPASSAUTO = 30;		// pre conference a boarding pass was auto generated
 	const STATUS_BOARDINGPASSUSER = 32;		// pre conference a boarding pass was user generated
 	const STATUS_USED = 35;					// no more entrances can be done using this ticket
-	// idea with STATUS_BOARDINGPASS is to at that generation ask them when they arrive and use that to door staffing
+	// idea with STATUS_BOARDINGPASS is just like a flight, before flight have them get their boarding pass, and at that generation ask them when they arrive and use that to adjust door staffing
 	// idea with STATUS_USED is to make sure only one person can get in, hopefully later linked with some badge id so badge id can be invalidated is lost
 	// idea with STATUS_PAID is to track if it was fully paid or some payment needs to happen at the door
 
