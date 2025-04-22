8feaa4e7ff9698cd9795018667a92bc66772dbf1
yannis <yannishoareau26@gmail.com>
2025-04-22 16:24:39 +0200

    Fix phan

--- htdocs/subtotals/core/substitutions/functions_subtotals.lib.php
+++ htdocs/subtotals/core/substitutions/functions_subtotals.lib.php
@@ -3,11 +3,11 @@
 * 		functions xxx_completesubstitutionarray are called by make_substitutions() if file
 * 		is inside directory htdocs/core/substitutions
 *
-*		@param	array		$substitutionarray	Array with substitution key=>val
-*		@param	Translate	$langs			Output langs
-*		@param	Object		$object			Object to use to get values
-*		@param 	Object 		$line 			Line to use to get values
-* 		@return	void					The entry parameter $substitutionarray is modified
+*		@param	array<string,string|float|null>		$substitutionarray	Array with substitution key=>val
+*		@param	Translate							$langs				Output langs
+*		@param	Object								$object				Object to use to get values
+*		@param 	object 								$line 				Line to use to get values
+* 		@return	void													The entry parameter $substitutionarray is modified
 */
 function subtotals_completesubstitutionarray_lines(&$substitutionarray, $langs, $object, $line)
 {
