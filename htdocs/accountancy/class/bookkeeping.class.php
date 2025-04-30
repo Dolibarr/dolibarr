73bcc0c38d04f7c118962378e7c0a81f0143d3cb
Marc de Lima Lucio <68746600+marc-dll@users.noreply.github.com>
2025-04-30 12:16:59 +0200

    FIX: accountancy: warning when account not found in plan

--- htdocs/accountancy/class/bookkeeping.class.php
+++ htdocs/accountancy/class/bookkeeping.class.php
@@ -2147,7 +2147,7 @@ class BookKeeping extends CommonObject
 		dol_syslog(get_class($this)."::select_account", LOG_DEBUG);
 		$resql = $this->db->query($sql);
 		if ($resql) {
-			$obj = '';
+			$obj = (object) array('label' => '');
 			if ($this->db->num_rows($resql)) {
 				$obj = $this->db->fetch_object($resql);
 			}
