f5cc9643dfc18a9b095e4723c2fbc90023613ed1
William Mead <william@m34d.com>
2025-04-30 09:47:28 +0200

    Added contributor info

--- htdocs/fichinter/class/api_interventions.class.php
+++ htdocs/fichinter/class/api_interventions.class.php
@@ -2,6 +2,7 @@
 /* Copyright (C) 2015   Jean-François Ferry     <jfefe@aternatik.fr>
  * Copyright (C) 2016	Laurent Destailleur		<eldy@users.sourceforge.net>
  * Copyright (C) 2025		MDW					<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2025	William Mead			<william@m34d.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
