4f003765118bf8683364b4c3b8781422580fb749
sonikf <93765174+sonikf@users.noreply.github.com>
2025-05-06 14:57:45 +0300

    NEW Add description in llx_expeditiondet

--- htdocs/install/mysql/migration/21.0.0-22.0.0.sql
+++ htdocs/install/mysql/migration/21.0.0-22.0.0.sql
@@ -287,4 +287,6 @@ ALTER TABLE llx_facture_fourn_rec ADD COLUMN usenewcurrencyrate integer DEFAULT 
 
 ALTER TABLE llx_don ADD COLUMN ip varchar(250);
 
+ALTER TABLE llx_expeditiondet ADD COLUMN description text AFTER fk_entrepot;
+
 INSERT INTO llx_c_type_container (code, label, active, module, position, typecontainer, entity) VALUES ('setup', 'Setup screen', 1, 'system', 500, 'library', __ENTITY__);
