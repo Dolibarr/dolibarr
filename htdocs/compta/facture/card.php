70194316b0a12bc37a09c608672c82ea68e237f0
Lenin Rivas <53640168+leninrivas@users.noreply.github.com>
2025-05-12 19:29:00 -0500

    FIX price_min_ttc

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -19,7 +19,7 @@
  * Copyright (C) 2023		Nick Fragoulis
  * Copyright (C) 2024-2025	MDW							<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024-2025	Alexandre Spangaro			<alexandre@inovea-conseil.com>
- * Copyright (C) 2025		Lenin Rivas					<alexandre@inovea-conseil.com>
+ * Copyright (C) 2025		Lenin Rivas					<lenin.rivas777@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
