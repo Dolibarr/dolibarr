eb4f2624549a74a35c4b565b9bd28891ce80374f
Clément <142790847+clementgde@users.noreply.github.com>
2025-05-22 10:15:51 +0200

    Update functions.lib.php

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -7356,7 +7356,8 @@ function price2num($amount, $rounding = '', $option = 0)
  * @param	string|float			$amount			Amount value to format
  * @return  string                      			String to show amount with style of total
  */
-function totalAmount($amount) {
+function totalAmount($amount)
+{
 	return '<span class="amount">'.$amount.'</span>';
 }
 
