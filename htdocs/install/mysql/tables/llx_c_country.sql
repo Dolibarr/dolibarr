eb5980335c5e951d4bb1b3dbe8c7ae9b43c15804
ptibogxiv <support@ptibogxiv.net>
2025-05-25 15:41:38 +0200

    NEW llx_c_country.sql add phone code

--- htdocs/install/mysql/tables/llx_c_country.sql
+++ htdocs/install/mysql/tables/llx_c_country.sql
@@ -25,6 +25,7 @@ create table llx_c_country
   code     		varchar(2)         NOT NULL,	-- the iso 2 alpha code
   code_iso 		varchar(3),						-- the iso 3 alpha code
   numeric_code 	varchar(3),						-- the iso numeric number
+  phone code	varchar(8),						-- the phone code
   label    		varchar(128)       NOT NULL,
   eec      		tinyint DEFAULT 0  NOT NULL,
   sepa     		tinyint DEFAULT 0  NOT NULL,
