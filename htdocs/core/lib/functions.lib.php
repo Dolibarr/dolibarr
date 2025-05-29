45d582f4d24fb6617c63b6acdba9b07bb7b0e896
Clément <142790847+clementgde@users.noreply.github.com>
2025-05-29 11:49:55 +0200

    Update functions.lib.php

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -1031,14 +1031,14 @@ function GETPOST($paramname, $check = 'alphanohtml', $method = 0, $filter = null
 										if (!getDolGlobalString('MAIN_DISABLE_DEFAULT_FILTER_FOR_QUICK_SEARCH')) {
 											$forbidden_chars_to_replace = array(" ", "'", "/", "\\", ":", "*", "?", "\"", "<", ">", "|", ";", "="); // we accept _, -, . and ,
 											if ($check != 'array') {
-												array_push($forbidden_chars_to_replace,"[", "]");
+												array_push($forbidden_chars_to_replace, "[", "]");
 											}
 											$out = dol_string_nospecial($user->default_values[$relativepathstring]['filters'][$defkey][$paramname], '', $forbidden_chars_to_replace);
 										}
 									} else {
 										$forbidden_chars_to_replace = array(" ", "'", "/", "\\", ":", "*", "?", "\"", "<", ">", "|", ";", "="); // we accept _, -, . and ,
 										if ($check != 'array') {
-											array_push($forbidden_chars_to_replace,"[", "]");
+											array_push($forbidden_chars_to_replace, "[", "]");
 										}
 										$out = dol_string_nospecial($user->default_values[$relativepathstring]['filters'][$defkey][$paramname], '', $forbidden_chars_to_replace);
 									}
