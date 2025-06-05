218e097b2d8621ed992b3a719628bd6694c82a90
ATM-nicolasV <nicolas.vidal@atm-consutling.fr>
2025-06-05 14:37:44 +0200

    add param to filter product on formsetup item

--- htdocs/core/class/html.formsetup.class.php
+++ htdocs/core/class/html.formsetup.class.php
@@ -926,7 +926,7 @@ class FormSetupItem
 		} elseif ($this->type == 'product') {
 			if (isModEnabled("product") || isModEnabled("service")) {
 				$selected = (empty($this->fieldValue) ? '' : $this->fieldValue);
-				$out .= $this->form->select_produits($selected, $this->confKey, '', 0, 0, 1, 2, '', 0, array(), 0, '1', 0, $this->cssClass, 0, '', null, 1);
+				$out .= $this->form->select_produits($selected, $this->confKey, $this->fieldAttr["type_filter"], 0, 0, 1, 2, '', 0, array(), 0, '1', 0, $this->cssClass, 0, '', null, 1);
 			}
 		} elseif ($this->type == 'selectBankAccount') {
 			if (isModEnabled("bank")) {
