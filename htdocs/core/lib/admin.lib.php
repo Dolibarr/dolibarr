46e9acb703bd6f7fdec428c7c4aa9dcacd0254a7
Theobald <f.moreau@theobald-groupe.com>
2025-06-12 23:54:35 +0200

    FIX: Warning on external modules Pages: Unknown array key. WIP

--- htdocs/core/lib/admin.lib.php
+++ htdocs/core/lib/admin.lib.php
@@ -901,6 +901,11 @@ function security_prepare_head()
 	$head[$h][2] = 'audit';
 	$h++;
 
+	$head[$h][0] = DOL_URL_ROOT."/admin/openid.php";
+	$head[$h][1] = $langs->trans("Audit");
+	$head[$h][2] = 'audit';
+	$h++;
+
 
 	// Show permissions lines
 	$nbPerms = 0;
