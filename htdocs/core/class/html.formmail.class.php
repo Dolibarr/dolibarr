2bb49372984dcc628a36a9e8df3e6e7411bf89ab
Florian HENRY <florian.henry@open-concept.pro>
2025-06-13 09:26:18 +0200

    fix: warning Form IA and settings

--- htdocs/core/class/html.formmail.class.php
+++ htdocs/core/class/html.formmail.class.php
@@ -1102,10 +1102,14 @@ class FormMail extends Form
 				$showlinktoailabel = $langs->trans("AIEnhancements");
 				$formatforouput = '';
 				$htmlname = 'message';
+
 				$formai->substit = $this->substit;
 				$formai->substit_lines = $this->substit_lines;
 
 				// Fill $out
+				if (!isset($db)) {
+					$db=$db = $this->db;
+				}
 				include DOL_DOCUMENT_ROOT.'/core/tpl/formlayoutai.tpl.php';
 
 				$out .= '</td>';
