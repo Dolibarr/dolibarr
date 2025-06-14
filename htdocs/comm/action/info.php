e4392d4f8b05420ae6d069f351add7345561ad06
Laurent Destailleur <eldy@destailleur.fr>
2025-06-14 02:31:58 +0200

    Update info.php

--- htdocs/comm/action/info.php
+++ htdocs/comm/action/info.php
@@ -53,6 +53,11 @@ if ($user->socid && $socid) {
 	$result = restrictedArea($user, 'societe', $socid);
 }
 
+$object = new ActionComm($db);
+$object->fetch($id);
+
+$usercancreate = $user->hasRight('agenda', 'allactions', 'create') || (($object->authorid == $user->id || $object->userownerid == $user->id) && $user->hasRight('agenda', 'myactions', 'create'));
+
 
 /*
  * View
@@ -63,11 +68,7 @@ $form = new Form($db);
 $help_url = 'EN:Module_Agenda_En|FR:Module_Agenda|ES:M&omodulodulo_Agenda|DE:Modul_Terminplanung';
 llxHeader('', $langs->trans("Agenda"), $help_url);
 
-$object = new ActionComm($db);
-$object->fetch($id);
 $object->info($object->id);
-
-$usercancreate = $user->hasRight('agenda', 'allactions', 'create') || (($object->authorid == $user->id || $object->userownerid == $user->id) && $user->hasRight('agenda', 'myactions', 'create'));
 
 $head = actions_prepare_head($object);
 print dol_get_fiche_head($head, 'info', $langs->trans("Action"), -1, 'action');
