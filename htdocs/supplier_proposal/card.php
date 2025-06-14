36236c84cda1af6c8290e0848d2fa547231774ee
Vincent Maury <artec.vm@arnac.net>
2025-06-14 16:24:59 +0200

    Fix issue #34301

--- htdocs/supplier_proposal/card.php
+++ htdocs/supplier_proposal/card.php
@@ -1404,11 +1404,11 @@ if ($action == 'create') {
 
 			print '<tr>';
 			print '<td>'.$langs->trans("Project").'</td><td colspan="2">';
-            if ($socid > 0) { // external user
-                $projSocFilter = $socid;
-            } elseif ((int)$soc->id == 0 || !empty(getDolGlobalString('PROJECT_CAN_ALWAYS_LINK_TO_ALL_SUPPLIERS'))) {
-                $projSocFilter = -1;
-            } else $projSocFilter = $soc->id;
+			if ($socid > 0) { // external user
+				$projSocFilter = $socid;
+			} elseif ((int) $soc->id == 0 || !empty(getDolGlobalString('PROJECT_CAN_ALWAYS_LINK_TO_ALL_SUPPLIERS'))) {
+				$projSocFilter = -1;
+			} else $projSocFilter = $soc->id;
 			print img_picto('', 'project', 'class="pictofixedwidth"').$formproject->select_projects($projSocFilter, $projectid, 'projectid', 0, 0, 1, 1, 0, 0, 0, '', 1, 0, 'maxwidth500');
 			print ' &nbsp; <a href="'.DOL_URL_ROOT.'/projet/card.php?socid='.$soc->id.'&action=create&status=1&backtopage='.urlencode($_SERVER["PHP_SELF"].'?action=create&socid='.$soc->id).'"><span class="fa fa-plus-circle valignmiddle" title="'.$langs->trans("AddProject").'"></span></a>';
 
