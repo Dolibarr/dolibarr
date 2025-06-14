6a647c4e01c186c9ff1ff13028b750c247a4a99a
William Mead <william@m34d.com>
2025-06-14 11:11:11 +0200

    Added expense report API validate

--- htdocs/expensereport/class/api_expensereports.class.php
+++ htdocs/expensereport/class/api_expensereports.class.php
@@ -589,6 +589,52 @@ class ExpenseReports extends DolibarrApi
 		);
 	}*/
 
+	/**
+	 * Validate an expense report
+	 *
+	 * If you get a bad value for param notrigger check, provide this in body
+	 * {
+	 *   "notrigger": 0
+	 * }
+	 *
+	 * @since	23.0.0	Initial implementation
+	 *
+	 * @param	int		$id				Expense report ID
+	 * @param	int		$notrigger		1=Does not execute triggers, 0= execute triggers
+	 *
+	 * @url		POST	{id}/validate
+	 *
+	 * @return	Object
+	 *
+	 * @throws RestException
+	 */
+	public function validate($id, $notrigger = 0)
+	{
+		if (!DolibarrApiAccess::$user->hasRight('expensereport', 'creer')) {
+			throw new RestException(403, "Insuffisant rights");
+		}
+		$result = $this->expensereport->fetch($id);
+		if (!$result) {
+			throw new RestException(404, 'Expense report not found');
+		}
+
+		if (!DolibarrApi::_checkAccessToResource('expensereport', $this->expensereport->id)) {
+			throw new RestException(403, 'Access not allowed for login '.DolibarrApiAccess::$user->login);
+		}
+
+		$result = $this->expensereport->setValidate(DolibarrApiAccess::$user, $notrigger);
+		if ($result == 0) {
+			throw new RestException(304, 'Error nothing done. May be object is already validated');
+		}
+		if ($result < 0) {
+			throw new RestException(500, 'Error when validating expense report: '.$this->expensereport->error);
+		}
+
+		$this->expensereport->fetchObjectLinked();
+
+		return $this->_cleanObjectDatas($this->expensereport);
+	}
+
 
 
 	/**
