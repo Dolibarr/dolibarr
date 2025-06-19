9acf0bd5a19779e7c2bc446bd9d8a787861f4c81
Marc de Lima Lucio <68746600+marc-dll@users.noreply.github.com>
2025-06-19 11:09:50 +0200

    FIX: ci

--- dev/tools/github_pr_reviewers_webhook.php
+++ dev/tools/github_pr_reviewers_webhook.php
@@ -86,39 +86,39 @@ define('GITHUB_API_VERSION', '2022-11-28');
 $config = @require_once __DIR__ . '/github_pr_reviewers_webhook.config.php';
 
 if (false === $config) {
-    _error('Could not load config');
+	_error('Could not load config');
 }
 
 $secret = $config['secret'] ?? '';
 
 if (empty($secret)) {
-    _error('Empty secret configuration');
+	_error('Empty secret configuration');
 }
 
 $reviewers = $config['reviewers'] ?? [ ];
 
 if (empty($reviewers)) {
-    _error('Empty reviewers configuration');
+	_error('Empty reviewers configuration');
 }
 
 $token = $config['token'] ?? '';
 
 if (empty($token)) {
-    _error('Empty token configuration');
+	_error('Empty token configuration');
 }
 
 
 $event = $_SERVER['HTTP_X_GITHUB_EVENT'] ?? '';
 
 if (empty($event)) {
-    _error('GitHub event name not found', 400);
+	_error('GitHub event name not found', 400);
 }
 
 
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
 
 if (empty($signature)) {
-    _error('Message signature not found', 400);
+	_error('Message signature not found', 400);
 }
 
 $expectedSignature = 'sha256=' . hash_hmac('sha256', file_get_contents('php://input'), $secret);
@@ -128,20 +128,20 @@ _debug('signature = ' . var_export($signature, true));
 
 // Use `hash_equals()` instead of direct comparison to avoir timing attacks (@see https://www.php.net/manual/en/function.hash-equals.php)
 if (! hash_equals($signature, $expectedSignature)) {
-    _error('Invalid webhook signature', 401);
+	_error('Invalid webhook signature', 401);
 }
 
 _debug('event = ' . var_export($event, true));
 
 if ('pull_request' !== $event) {
-    _out('Event ' . var_export($event, true) . ' not qualified');
-    exit;
+	_out('Event ' . var_export($event, true) . ' not qualified');
+	exit;
 }
 
 $rawPayload = $_REQUEST['payload'] ?? '';
 
 if (empty($rawPayload)) {
-    _error('Empty payload', 400);
+	_error('Empty payload', 400);
 }
 
 // _debug('rawPayload = ' . var_export($payload, true));
@@ -149,7 +149,7 @@ if (empty($rawPayload)) {
 $payload = json_decode($rawPayload, /* associative: */ true);
 
 if (null === $payload) {
-    _error('Could not decode payload, got ' . var_export($rawPayload, true) . ' in input');
+	_error('Could not decode payload, got ' . var_export($rawPayload, true) . ' in input');
 }
 
 // _debug('payload = ' . var_export($payload, true));
@@ -158,75 +158,75 @@ if (null === $payload) {
 $targetBranch = $payload['pull_request']['base']['ref'] ?? null;
 
 if (null === $targetBranch) {
-    _error('Target branch not in payload');
+	_error('Target branch not in payload');
 }
 
 _debug('targetBranch = ' . var_export($targetBranch, true));
 
 
 if (! array_key_exists($targetBranch, $reviewers)) {
-    _out('Target branch ' . var_export($targetBranch, true) . ' not qualified');
-    exit;
+	_out('Target branch ' . var_export($targetBranch, true) . ' not qualified');
+	exit;
 }
 
 $wantedReviewers = $reviewers[$targetBranch];
 
 if (! is_array($wantedReviewers) && ! is_string($wantedReviewers)) {
-    _error('Wanted reviewers incorrectly set in config for branch ' . var_export($targetBranch, true));
+	_error('Wanted reviewers incorrectly set in config for branch ' . var_export($targetBranch, true));
 }
 
 if (! is_array($wantedReviewers) && ! empty($wantedReviewers)) {
-    $wantedReviewers = [ $wantedReviewers ];
+	$wantedReviewers = [ $wantedReviewers ];
 }
 
 if (empty($wantedReviewers)) {
-    _out('Branch ' . var_export($targetBranch, true) . ' configured with no reviewers, not qualified');
-    exit;
+	_out('Branch ' . var_export($targetBranch, true) . ' configured with no reviewers, not qualified');
+	exit;
 }
 
 $action = $payload['action'] ?? null;
 
 if (null === $action) {
-    _error('Pull request action not in payload');
+	_error('Pull request action not in payload');
 }
 
 
 if ('opened' !== $action && 'edited' !== $action && 'ready_for_review' !== $action) {
-    _out('Action ' . var_export($action, true) . ' not qualified');
-    exit;
+	_out('Action ' . var_export($action, true) . ' not qualified');
+	exit;
 }
 
 
 $isDraft = $payload['pull_request']['draft'] ?? null;
 
 if (null === $isDraft) {
-    _error('Pull request draft boolean not in payload');
+	_error('Pull request draft boolean not in payload');
 }
 
 if ('ready_for_review' !== $action && $isDraft) {
-    _out('Pull request opened as draft : not qualified');
-    exit;
+	_out('Pull request opened as draft : not qualified');
+	exit;
 }
 
 
 $author = $payload['pull_request']['user']['login'] ?? null;
 
 if (null === $author) {
-    _error('Pull request author not in payload');
+	_error('Pull request author not in payload');
 }
 
 $currentReviewers = $payload['pull_request']['requested_reviewers'] ?? null;
 
 if (null === $currentReviewers) {
-    _error('Current reviewers not in payload');
+	_error('Current reviewers not in payload');
 }
 
 // GitHub API returns an error 422 if we try to add the author as a reviewer, we have to filter them out
 $reviewersToBeAdded = array_diff($wantedReviewers, $currentReviewers, empty($author) ? [ ] : [ $author ]);
 
 if (empty($reviewersToBeAdded)) {
-    _out('Reviewers already requested or author of the pull request : not qualified');
-    exit;
+	_out('Reviewers already requested or author of the pull request : not qualified');
+	exit;
 }
 
 
@@ -237,37 +237,37 @@ _debug('Adding reviewers: ' . implode(', ', $reviewersToBeAdded));
 $pullRequestUrl = $payload['pull_request']['url'] ?? null;
 
 if (null === $pullRequestUrl) {
-    _error('Pull request API URL not in payload');
+	_error('Pull request API URL not in payload');
 }
 
 $c = curl_init($pullRequestUrl . '/requested_reviewers');
 
 if (false === $c) {
-    _error('Could not init cURL');
+	_error('Could not init cURL');
 }
 
 
 $setMethodReturn = curl_setopt($c, CURLOPT_CUSTOMREQUEST, 'POST');
 
 if (false === $setMethodReturn) {
-    _error('Could not set request method: ' . curl_error($c));
+	_error('Could not set request method: ' . curl_error($c));
 }
 
 $setHeadersReturn = curl_setopt($c, CURLOPT_HTTPHEADER, [
-    'Accept: application/vnd.github+json',
-    'Authorization: Bearer ' . $token,
-    'X-GitHub-Api-Version: ' . GITHUB_API_VERSION,
-    'User-Agent: dolibarr-github-webhook-handler/1.0 dolibarr/20250616', // PHP cURL implementation has no default User-Agent yet, and GitHub REST API requires one
-    'Content-Type: application/json',
+	'Accept: application/vnd.github+json',
+	'Authorization: Bearer ' . $token,
+	'X-GitHub-Api-Version: ' . GITHUB_API_VERSION,
+	'User-Agent: dolibarr-github-webhook-handler/1.0 dolibarr/20250616', // PHP cURL implementation has no default User-Agent yet, and GitHub REST API requires one
+	'Content-Type: application/json',
 ]);
 
 if (false === $setHeadersReturn) {
-    _error('Could not set request headers: ' . curl_error($c));
+	_error('Could not set request headers: ' . curl_error($c));
 }
 
 $setBodyReturn = curl_setopt($c, CURLOPT_POSTFIELDS, json_encode([
-    'reviewers' => $reviewersToBeAdded,
-    'team_reviewers' => [ ], // TODO
+	'reviewers' => $reviewersToBeAdded,
+	'team_reviewers' => [ ], // TODO
 ]));
 
 
@@ -275,32 +275,32 @@ $setBodyReturn = curl_setopt($c, CURLOPT_POSTFIELDS, json_encode([
 $setFailOnErrorReturn = curl_setopt($c, CURLOPT_FAILONERROR, true);
 
 if (false === $setFailOnErrorReturn) {
-    _error('Could not set fail on error: ' . curl_error($c));
+	_error('Could not set fail on error: ' . curl_error($c));
 }
  */
 
 $setReturnTransferReturn = curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 
 if (false === $setReturnTransferReturn) {
-    _error('Could not set return transfer: ' . curl_error($c));
+	_error('Could not set return transfer: ' . curl_error($c));
 }
 
 $response = curl_exec($c);
 
 if (false === $response) {
-    _error('Error handling cURL request: ' . curl_error($c));
+	_error('Error handling cURL request: ' . curl_error($c));
 }
 
 $responseCode = curl_getinfo($c, CURLINFO_RESPONSE_CODE);
 
 if (false === $responseCode) {
-    _error('Error getting response code: ' . curl_error($c));
+	_error('Error getting response code: ' . curl_error($c));
 }
 
 _debug('responseCode = ' . $responseCode);
 
 if ($responseCode < 200 || $responseCode > 399) {
-    _error('Error from GitHub API, code ' . $responseCode . ': ' . $response);
+	_error('Error from GitHub API, code ' . $responseCode . ': ' . $response);
 }
 
 _out('Added the following reviewers: ' . implode(', ', $reviewersToBeAdded));
@@ -308,22 +308,34 @@ _out('Added the following reviewers: ' . implode(', ', $reviewersToBeAdded));
 curl_close($c);
 
 
+/**
+ * @param string $message
+ * @return void
+ */
 function _out(string $message): void
 {
-    echo $message . PHP_EOL;
+	echo $message . PHP_EOL;
 }
 
+/**
+ * @param string $message
+ * @return void
+ */
 function _debug(string $message): void
 {
-    if (isset($_REQUEST['debug'])) {
-        _out($message);
-    }
+	if (isset($_REQUEST['debug'])) {
+		_out($message);
+	}
 }
 
+/**
+ * @param string $message
+ * @return void
+ */
 function _error(string $message): void
 {
-    http_response_code(500);
-    _out('Error: ' . $message);
-    exit;
+	http_response_code(500);
+	_out('Error: ' . $message);
+	exit;
 }
 
