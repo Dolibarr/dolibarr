192d5dd0f6426e554c0595f2a486d9e308adeb50
Laurent Destailleur <eldy@destailleur.fr>
2025-06-22 12:24:35 +0200

    Update project.lib.php

--- htdocs/core/lib/project.lib.php
+++ htdocs/core/lib/project.lib.php
@@ -924,21 +924,21 @@ function projectLinesa(&$inc, $parent, &$lines, &$level, $var, $showproject, &$t
 								if (!empty($c->photo)) {
 									if (get_class($c) == 'User') {
 										/** @var User $c */
-										/** @phan-var-force User $c */
+										'@phan-var-force User $c'
 										print $c->getNomUrl(-2, '', 0, 0, 24, 1, '', ($ifisrt ? '' : 'notfirst'));
 									} else {
 										/** @var Contact $c */
-										/** @phan-var-force Contact $c */
+										'@phan-var-force Contact $c'
 										print $c->getNomUrl(-2, '', 0, '', -1, 0, ($ifisrt ? '' : 'notfirst'));
 									}
 								} else {
 									if (get_class($c) == 'User') {
 										/** @var User $c */
-										/** @phan-var-force User $c */
+										'@phan-var-force User $c'
 										print $c->getNomUrl(2, '', 0, 0, 24, 1, '', ($ifisrt ? '' : 'notfirst'));
 									} else {
 										/** @var Contact $c */
-										/** @phan-var-force Contact $c */
+										'@phan-var-force Contact $c'
 										print $c->getNomUrl(2, '', 0, '', -1, 0, ($ifisrt ? '' : 'notfirst'));
 									}
 								}
