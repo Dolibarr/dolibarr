76e12cd261066fcd20162f1b59842a5c4d60778e
Florian HENRY <florian.henry@open-concept.pro>
2025-06-24 11:16:36 +0200

    fix php warning

--- htdocs/reception/class/reception.class.php
+++ htdocs/reception/class/reception.class.php
@@ -1981,7 +1981,7 @@ class Reception extends CommonObject
 						if ($qty <= 0) {
 							continue;
 						}
-						dol_syslog(get_class($this)."::reopen reception movement index ".$i." ed.rowid=".$obj->rowid." edb.rowid=".$obj->edbrowid);
+						dol_syslog(get_class($this)."::reopen reception movement index ".$i." ed.rowid=".$obj->rowid);
 
 						//var_dump($this->lines[$i]);
 						$mouvS = new MouvementStock($this->db);
