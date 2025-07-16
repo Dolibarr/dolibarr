30677d0d02ea247df2cc5c20e8767aa509d9a8ac
Hystepik <hystepik@gmail.com>
2025-07-16 14:18:33 +0200

    fix CI

--- htdocs/webhook/triggerhistory_card.php
+++ htdocs/webhook/triggerhistory_card.php
@@ -588,15 +588,6 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 
 		$morehtmlcenter = dolGetButtonTitle($langs->trans('SeeAll'), '', 'fa fa-bars imgforviewmode', dol_buildpath('/webhook/history_agenda.php', 1).'?id='.$object->id);
 
-		$includeeventlist = 0;
-
-		// List of actions on element
-		if ($includeeventlist) {
-			include_once DOL_DOCUMENT_ROOT.'/core/class/html.formactions.class.php';
-			$formactions = new FormActions($db);
-			$somethingshown = $formactions->showactions($object, $object->element.'@'.$object->module, (is_object($object->thirdparty) ? $object->thirdparty->id : 0), 1, '', $MAXEVENT, '', $morehtmlcenter);
-		}
-
 		print '</div></div>';
 	}
 
