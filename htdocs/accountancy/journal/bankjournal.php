80e67e665ad904e631fbe8892671eeb7d614eb2f
kollibar <thomas.kolli@gmail.com>
2025-07-17 22:29:22 +0200

    Correction for the generation of the bankjournal for a paiement for different third parties party (in case of paiement multi third partie is active : option "Allow payments on different third parties bills but same parent company" = true)
    I've made a bug issu about it : https://github.com/Dolibarr/dolibarr/issues/34755

--- htdocs/accountancy/journal/bankjournal.php
+++ htdocs/accountancy/journal/bankjournal.php
@@ -298,6 +298,7 @@ if ($result) {
 		// Set accountancy code for thirdparty (example: '411CU...' or '411' if no subledger account defined on customer)
 		$compta_soc = 'NotDefined';
 		$accountancy_code_general = 'NotDefined';
+		$socid = $obj->socid;
 		if ($lineisapurchase > 0) {
 			$accountancy_code_general = (!empty($obj->accountancy_code_supplier_general) && $obj->accountancy_code_supplier_general != '-1') ? $obj->accountancy_code_supplier_general : $account_supplier;
 			$compta_soc = (($obj->code_compta_fournisseur != "") ? $obj->code_compta_fournisseur : $account_supplier);
@@ -360,6 +361,31 @@ if ($result) {
 		if (!empty($obj->amount_main_currency)) {
 			// If $obj->amount_main_currency is set, it means that $obj->amount is not in same currency, we must use $obj->amount_main_currency
 			$amounttouse = $obj->amount_main_currency;
+		}
+
+		// in case of paiement multi-third parties ( one payments for different third parties bills with same parent company)
+		// 		because in this case $obj->amount = the total of the paiement and not the paiement for each company part
+		// 		and we need only the part for each company
+		if( $lineisapurchase == 1) {
+			$sqlamount = "SELECT SUM(pf.amount) as amount";
+			$sqlamount .= " FROM ".MAIN_DB_PREFIX."paiementfounr_facturefourn AS pf";
+			$sqlamount .= " INNER JOIN ".MAIN_DB_PREFIX."paiementfourn AS p ON pf.fk_paiementfourn = p.rowid";
+			$sqlamount .= " RIGHT JOIN ".MAIN_DB_PREFIX."facture AS f ON pf.fk_facturefourn = f.rowid";
+			$sqlamount .= " WHERE p.fk_bank = '" . $obj->rowid . "'";
+			$sqlamount .= " AND f.fk_soc ='" . $obj->socid . "'";
+		}
+		if( $lineisasale == 1 ) {
+			$sqlamount = "SELECT SUM(pf.amount) as amount";
+			$sqlamount .= " FROM ".MAIN_DB_PREFIX."paiement_facture AS pf";
+			$sqlamount .= " INNER JOIN ".MAIN_DB_PREFIX."paiement AS p ON pf.fk_paiement = p.rowid";
+			$sqlamount .= " RIGHT JOIN ".MAIN_DB_PREFIX."facture AS f ON pf.fk_facture = f.rowid";
+			$sqlamount .= " WHERE p.fk_bank = '" . $obj->rowid . "'";
+			$sqlamount .= " AND f.fk_soc ='" . $obj->socid . "'";
+		}
+		if( $lineisapurchase == 1 || $lineisasale == 1) {
+			$resultamount = $db->query($sqlamount);
+			$objamount = $db->fetch_object($resultamount);
+			if( ! empty($objamount->amount)) $amounttouse = $objamount->amount;
 		}
 
 		// get_url may return -1 which is not traversable
@@ -426,7 +452,9 @@ if ($result) {
 					$societestatic->name = $links[$key]['label'];
 					$societestatic->email = $tabcompany[$obj->rowid]['email'];
 					$tabpay[$obj->rowid]["soclib"] = $societestatic->getNomUrl(1, '', 30);
-					if ($compta_soc) {
+					if ($compta_soc  && $socid == $links[$key]['url_id'] ) {	// we have to add only if $compta_soc is defined AND if $links[$key]['url_id'] is equal to socid 
+																				// (if not we will add $amounttouse as many times as there are third parties involved in the payment and this for each line. 
+																				// That is to say that if there are 2 third parties, we will double the amount compared to what we should find, triple it if there are 3 third parties...)
 						if (empty($tabtp[$obj->rowid][$compta_soc])) {
 							$tabtp[$obj->rowid][$compta_soc] = $amounttouse;
 						} else {
