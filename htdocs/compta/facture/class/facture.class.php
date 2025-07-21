da6fbac3873c8b36fecbc1f3a7077726536c6c7d
atm-florian <florian.mortgat@atm-consulting.fr>
2025-07-21 10:51:12 +0200

    FIX 17.0: when you clone an invoice that was created from a template invoice, the clone should not be linked to the template invoice

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -1255,6 +1255,7 @@ class Facture extends CommonInvoice
 		$object->user_valid         = null; // deprecated
 		$object->fk_user_author     = $user->id;
 		$object->fk_user_valid      = null;
+		$object->fk_fac_rec_source  = null;
 		$object->fk_facture_source  = 0;
 		$object->date_creation      = '';
 		$object->date_modification = '';
