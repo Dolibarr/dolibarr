4ff77985623b349616fcfeae60954eab27fda982
sonikf <93765174+sonikf@users.noreply.github.com>
2025-07-25 11:50:55 +0300

    fix PhanPluginDuplicateExpressionAssignmentOperation

--- htdocs/core/modules/facture/doc/pdf_octopus.modules.php
+++ htdocs/core/modules/facture/doc/pdf_octopus.modules.php
@@ -1470,7 +1470,7 @@ class pdf_octopus extends ModelePDFFactures
 			}
 
 			// Previous balance
-			$old_balance = $old_balance - $total_payments;
+			$old_balance -= $total_payments;
 
 			// New balance
 			$new_balance = $old_balance + $object->total_ttc - $current_date_payments;
