a1c6e54295c1afc223624133af3fd029fff5da7b
sonikf <93765174+sonikf@users.noreply.github.com>
2025-07-31 20:47:20 +0300

    Update 21.0.0-22.0.0.sql

--- htdocs/install/mysql/migration/21.0.0-22.0.0.sql
+++ htdocs/install/mysql/migration/21.0.0-22.0.0.sql
@@ -45,7 +45,6 @@ ALTER TABLE llx_societe_account ADD UNIQUE INDEX uk_societe_account_login_websit
 
 ALTER TABLE llx_c_country ADD COLUMN sepa tinyint DEFAULT 0 NOT NULL;
 UPDATE llx_c_country SET sepa = 1 WHERE code IN ('AL','AD','AT','BE','BG','CH','CY','CZ','DE','DK','EE','ES','FI','FR','GB','GG','GR','HR','HU','IE','IM','IS','IT','JE','LI','LT','LU','LV','MC','MD','ME','MK','MT','NL','NO','PL','PM','PT','RO','RS','SE','SI','SK','SM','VA');
-UPDATE llx_c_country SET eec = 0 WHERE code='MC';
 
 -- fix element
 UPDATE llx_c_type_contact set element='shipping' WHERE element='expedition';
