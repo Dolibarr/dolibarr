b5288fe0ff79f35db2c30113c82be6bfbb51bca4
Kamel Khelifa <kkhelifa@open-dsi.fr>
2025-08-05 11:53:36 +0200

    NEW : Change the path for the mailing files because if you have more than 100 mailing you have the files of the previous mailing show in the newest. Only activate if the global MAILING_USE_NEW_PATH_FOR_FILES is set to 1. Add a script for move files to new path.

---
+++ scripts/emailings/migrate_files_path.php
@@ -0,0 +1,144 @@
+#!/usr/bin/env php
+<?php
+/*
+ * Copyright (C) 2025 Open-Dsi <support@open-dsi.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file scripts/emailings/migrate_files_path.php
+ * \ingroup scripts
+ * \brief Migrate files from old system prior to 23 to new path for 23+
+ */
+
+if (!defined('NOSESSION')) {
+	define('NOSESSION', '1');
+}
+
+$sapi_type = php_sapi_name();
+$script_file = basename(__FILE__);
+$path = __DIR__.'/';
+
+// Test if batch mode
+if (substr($sapi_type, 0, 3) == 'cgi') {
+	echo "Error: You are using PHP for CGI. To execute ".$script_file." from command line, you must use PHP for CLI mode.\n";
+	exit(1);
+}
+
+@set_time_limit(0); // No timeout for this script
+define('EVEN_IF_ONLY_LOGIN_ALLOWED', 1); // Set this define to 0 if you want to lock your script when dolibarr setup is "locked to admin user only".
+
+// Include and load Dolibarr environment variables
+require_once $path."../../htdocs/master.inc.php";
+require_once DOL_DOCUMENT_ROOT.'/core/lib/functionscli.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/comm/mailing/class/mailing.class.php';
+require_once DOL_DOCUMENT_ROOT."/core/lib/files.lib.php";
+// After this $db, $mysoc, $langs, $conf and $hookmanager are defined (Opened $db handler to database will be closed at end of file).
+// $user is created but empty.
+/**
+ * @var DoliDB $db
+ * @var HookManager $hookmanager
+ * @var Translate $langs
+ * @var User $user
+ */
+// $langs->setDefaultLang('en_US'); // To change default language of $langs
+$langs->load("main"); // To load language file for default language
+
+// Global variables
+$version = DOL_VERSION;
+$error = 0;
+$forcecommit = 0;
+
+$hookmanager->initHooks(array('cli'));
+
+
+/*
+ * Main
+ */
+
+print "***** ".$script_file." (".$version.") pid=".dol_getmypid()." *****\n";
+dol_syslog($script_file." launched with arg ".implode(',', $argv));
+
+if (!isset($argv[1]) || $argv[1] != 'mailing') {
+	print "Usage:  $script_file mailing\n";
+	exit(1);
+}
+
+print '--- start'."\n";
+
+// Case to migrate mailing path
+if ($argv[1] == 'mailing') {
+	$mailing = new Mailing($db);
+
+	$sql = "SELECT rowid as mid from ".MAIN_DB_PREFIX."mailing"; // Get list of all mailing
+	$resql = $db->query($sql);
+	if ($resql) {
+		while ($obj = $db->fetch_object($resql)) {
+			$mailing->fetch($obj->mid);
+			print " migrating mailing id=".$mailing->id." ref=".$mailing->ref."\n";
+			migrate_mailing_filespath($mailing);
+		}
+	} else {
+		print "\n sql error ".$sql;
+		exit();
+	}
+}
+
+$db->close(); // Close $db database opened handler
+
+exit($error);
+
+/**
+ * Migrate file from old path to new one for mailing $mailing
+ *
+ * @param 	Mailing $mailing		Object mailing
+ * @return 	void
+ */
+function migrate_mailing_filespath($mailing)
+{
+	global $conf;
+
+	$dir = $conf->mailing->dir_output;
+	$origin = $dir.'/'.get_exdir($mailing->id, 2, 0, 1, $mailing, 'mailing');
+	$destin = $dir.'/'.get_exdir($mailing->id, 0, 0, 1, $mailing, 'mailing');
+
+	$error = 0;
+
+	$origin_osencoded = dol_osencode($origin);
+	$destin_osencoded = dol_osencode($destin);
+	dol_mkdir($destin);
+
+	if (dol_is_dir($origin)) {
+		$handle = opendir($origin_osencoded);
+		if (is_resource($handle)) {
+			while (($file = readdir($handle)) !== false) {
+				if ($file != '.' && $file != '..' && is_dir($origin_osencoded.'/'.$file)) {
+					$thumbs = opendir($origin_osencoded.'/'.$file);
+					if (is_resource($thumbs)) {
+						dol_mkdir($destin.'/'.$file);
+						while (($thumb = readdir($thumbs)) !== false) {
+							dol_move($origin.'/'.$file.'/'.$thumb, $destin.'/'.$file.'/'.$thumb);
+						}
+						// dol_delete_dir($origin.'/'.$file);
+					}
+				} else {
+					if (dol_is_file($origin.'/'.$file)) {
+						dol_move($origin.'/'.$file, $destin.'/'.$file);
+					}
+				}
+			}
+		}
+	}
+}
