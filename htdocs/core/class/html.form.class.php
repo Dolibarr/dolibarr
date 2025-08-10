9a69da3bb7b3a51e34e54d70949e617068c97a8c
Laurent Destailleur <eldy@destailleur.fr>
2025-08-10 02:26:56 +0200

    Update html.form.class.php

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -3220,11 +3220,11 @@ class Form
 		$sql .= ' WHERE p.entity IN (' . getEntity('product') . ')';
 		if (getDolGlobalString('PRODUIT_ATTRIBUTES_HIDECHILD')) {
 			if (getDolGlobalString('PRODUIT_ATTRIBUTES_HIDECHILD_BUT_ALLOW_SEARCH_IN_EAN13')) {
-				if (strlen($filterkey)!=13) {
-    				$sql .= " AND NOT EXISTS (SELECT pac.rowid FROM ".$this->db->prefix()."product_attribute_combination as pac WHERE pac.fk_product_child = p.rowid)";
+				if (strlen($filterkey) != 13) {
+					$sql .= " AND NOT EXISTS (SELECT pac.rowid FROM ".$this->db->prefix()."product_attribute_combination as pac WHERE pac.fk_product_child = p.rowid)";
         		}
-      		} else {
-        		$sql .= " AND NOT EXISTS (SELECT pac.rowid FROM ".$this->db->prefix()."product_attribute_combination as pac WHERE pac.fk_product_child = p.rowid)";
+			} else {
+				$sql .= " AND NOT EXISTS (SELECT pac.rowid FROM ".$this->db->prefix()."product_attribute_combination as pac WHERE pac.fk_product_child = p.rowid)";
 			}
 		}
 		if ($finished == 0) {
