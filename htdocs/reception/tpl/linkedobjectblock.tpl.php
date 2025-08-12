95fb8976c89c7b05a5c0a65b4e1168d9b99200aa
Frédéric FRANCE <frederic.france@free.fr>
2025-08-12 12:15:35 +0200

    fix

--- htdocs/reception/tpl/linkedobjectblock.tpl.php
+++ htdocs/reception/tpl/linkedobjectblock.tpl.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2014 Marcos García       <marcosgdf@gmail.com>
  * Copyright (C) 2019 Laurent Destailleur <eldy@users.sourceforge.net>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2025       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -36,13 +37,15 @@ global $noMoreLinkedObjectBlockAfter;
 $langs = $GLOBALS['langs'];
 '@phan-var-force Translate $langs';
 $linkedObjectBlock = $GLOBALS['linkedObjectBlock'];
-'@phan-var-force CommonObject[] $linkedObjectBlock';
+'@phan-var-force Reception[] $linkedObjectBlock';
+/** @var Reception[] $linkedObjectBlock */
 
 // Load translation files required by the page
 $langs->load("receptions");
 
 $linkedObjectBlock = dol_sort_array($linkedObjectBlock, 'date', 'desc', 0, 0, 1);
-'@phan-var-force CommonObject[] $linkedObjectBlock';  // Repeat because type lost after dol_sort_array)
+'@phan-var-force Reception[] $linkedObjectBlock';  // Repeat because type lost after dol_sort_array)
+/** @var Reception[] $linkedObjectBlock */
 
 $total = 0;
 $ilink = 0;
