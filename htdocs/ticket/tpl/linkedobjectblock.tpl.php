e8b82bae0e3eef715a0f46f8366340e87655345d
Frédéric FRANCE <frederic.france@free.fr>
2025-08-14 14:37:47 +0200

    fix

--- htdocs/ticket/tpl/linkedobjectblock.tpl.php
+++ htdocs/ticket/tpl/linkedobjectblock.tpl.php
@@ -1,7 +1,8 @@
 <?php
-/* Copyright (C) 2010-2012 Regis Houssin <regis.houssin@inodbox.com>
- * Copyright (C) 2013      Jean-François FERRY <hello@librethic.io>
- * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+/* Copyright (C) 2010-2012  Regis Houssin 			<regis.houssin@inodbox.com>
+ * Copyright (C) 2013       Jean-François FERRY 	<hello@librethic.io>
+ * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2025       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,14 +31,17 @@ global $noMoreLinkedObjectBlockAfter;
 
 $langs = $GLOBALS['langs'];
 '@phan-var-force Translate $langs';
+/** @var Translate $langs */
 $linkedObjectBlock = $GLOBALS['linkedObjectBlock'];
-'@phan-var-force CommonObject[] $linkedObjectBlock';
+'@phan-var-force Ticket[] $linkedObjectBlock';
+/** @var Ticket[] $linkedObjectBlock */
 
 // Load translation files required by the page
 $langs->load('ticket');
 
 $linkedObjectBlock = dol_sort_array($linkedObjectBlock, 'datec', 'desc', 0, 0, 1);
-'@phan-var-force CommonObject[] $linkedObjectBlock';  // Repeat because type lost after dol_sort_array)
+'@phan-var-force Ticket[] $linkedObjectBlock';  // Repeat because type lost after dol_sort_array)
+/** @var Ticket[] $linkedObjectBlock */
 
 $total = 0;
 $ilink = 0;
