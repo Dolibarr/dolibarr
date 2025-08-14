30cac51db62e6d32e10195a26e2de37d400b89b8
Florian HENRY <florian.henry@open-concept.pro>
2025-08-14 10:20:53 +0200

    fix: with MAIN_MAIL_ADD_INLINE_IMAGES_IF_IN_MEDIAS and multicompany image are not find on send mail

--- htdocs/core/class/CMailFile.class.php
+++ htdocs/core/class/CMailFile.class.php
@@ -270,7 +270,7 @@ class CMailFile
 				// This convert an embedd file with src="/viewimage.php?modulepart... into a cid link
 				// TODO Exclude viewimage used for the read tracker ?
 				$dolibarr_main_data_root_images=$dolibarr_main_data_root;
-				if ($conf->entity!==1) {
+				if ((int)$conf->entity!==1) {
 					$dolibarr_main_data_root_images.='/'.$conf->entity.'/';
 				}
 				$findimg = $this->findHtmlImages($dolibarr_main_data_root_images.'/medias');
