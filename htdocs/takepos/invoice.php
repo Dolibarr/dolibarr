333c876caa38df5023aec088d6763495ca347d5d
Frédéric FRANCE <frederic.france@free.fr>
2025-08-15 08:48:24 +0200

    fix doc

--- htdocs/takepos/invoice.php
+++ htdocs/takepos/invoice.php
@@ -1873,6 +1873,7 @@ if (!empty($_SESSION["basiclayout"]) && $_SESSION["basiclayout"] == 1) {
 		$result = $object->fetch($catid);
 		$prods = $object->getObjectsInCateg("product");
 		/** @var Product[] $prods */
+		'@phan-var-force  Product[] $prod';
 		$htmlforlines = '';
 		foreach ($prods as $row) {
 			if (defined('INCLUDE_PHONEPAGE_FROM_PUBLIC_PAGE')) {
