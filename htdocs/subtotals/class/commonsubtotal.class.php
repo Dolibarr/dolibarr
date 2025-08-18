a556c543aeb0cad0bf0a775129b1fb3091641c20
Frédéric FRANCE <frederic.france@free.fr>
2025-08-18 10:54:20 +0200

    clean code

--- htdocs/subtotals/class/commonsubtotal.class.php
+++ htdocs/subtotals/class/commonsubtotal.class.php
@@ -64,6 +64,7 @@ trait CommonSubtotal
 	 */
 	public function addSubtotalLine($langs, $desc, $depth, $options = array(), $parent_line = 0)
 	{
+		/** @var Propal|Commande|Facture|FactureRec|Expedition|SupplierProposal|CommandeFournisseur|FactureFournisseur */
 		if (empty($desc)) {
 			if (isset($this->errors)) {
 				$this->errors[] = $langs->trans("TitleNeedDesc");
