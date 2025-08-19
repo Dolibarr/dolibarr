abc9abd5af0306271495fc701137c92899d2a540
Frédéric FRANCE <frederic34@users.noreply.github.com>
2025-08-19 21:48:02 +0200

    clean code

--- htdocs/fourn/facture/tpl/linkedobjectblock.tpl.php
+++ htdocs/fourn/facture/tpl/linkedobjectblock.tpl.php
@@ -1,8 +1,8 @@
 <?php
-/* Copyright (C) 2010-2011  Regis Houssin <regis.houssin@inodbox.com>
- * Copyright (C) 2014       Marcos García <marcosgdf@gmail.com>
- * Copyright (C) 2015       Charlie Benke <charlie@patas-monkey.com>
- * Copyright (C) 2016       Laurent Destailleur <eldy@users.sourceforge.net>
+/* Copyright (C) 2010-2011  Regis Houssin 			<regis.houssin@inodbox.com>
+ * Copyright (C) 2014       Marcos García 			<marcosgdf@gmail.com>
+ * Copyright (C) 2015       Charlie Benke 			<charlie@patas-monkey.com>
+ * Copyright (C) 2016       Laurent Destailleur 	<eldy@users.sourceforge.net>
  * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2025       Frédéric France         <frederic.france@free.fr>
  *
@@ -53,7 +53,7 @@ foreach ($linkedObjectBlock as $key => $objectlink) {
 	} ?>
 	<tr class="<?php echo $trclass; ?>">
 		<td><?php echo $langs->trans("SupplierInvoice"); ?></td>
-		<td><a href="<?php echo DOL_URL_ROOT.'/fourn/facture/card.php?facid='.$objectlink->id ?>"><?php echo img_object($langs->trans("ShowBill"), "bill").' '.$objectlink->ref; ?></a></td>
+		<td><?php echo $objectlink->getNomUrl(1) ?></td>
 		<td class="left"><?php echo $objectlink->ref_supplier; ?></td>
 		<td class="center"><?php echo dol_print_date($objectlink->date, 'day'); ?></td>
 		<td class="right"><?php
@@ -62,7 +62,7 @@ foreach ($linkedObjectBlock as $key => $objectlink) {
 			if ($object->type == FactureFournisseur::TYPE_CREDIT_NOTE) {
 				$sign = -1;
 			}
-			if ($objectlink->statut != 3) {
+			if ($objectlink->status != 3) {
 				// If not abandoned
 				$total += $sign * $objectlink->total_ht;
 				echo price($objectlink->total_ht);
