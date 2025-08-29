ab3fa39c55c9d6ef1669dff17ea4e1f0b1594cab
Frédéric FRANCE <frederic.france@free.fr>
2025-08-29 09:41:50 +0200

    setup

--- htdocs/workstation/workstation_card.php
+++ htdocs/workstation/workstation_card.php
@@ -1,6 +1,6 @@
 <?php
 
-/* Copyright (C) 2017 Laurent Destailleur  <eldy@users.sourceforge.net>
+/* Copyright (C) 2017       Laurent Destailleur         <eldy@users.sourceforge.net>
  * Copyright (C) 2024-2025  Frédéric France             <frederic.france@free.fr>
  * Copyright (C) 2025		MDW							<mdeweerd@users.noreply.github.com>
  *
