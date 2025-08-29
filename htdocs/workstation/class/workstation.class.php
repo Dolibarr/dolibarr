a33dc2f6d63e2a296055fb5cc1d693d251137d0e
Frédéric FRANCE <frederic.france@free.fr>
2025-08-29 06:36:37 +0200

    wip workstation tms

--- htdocs/workstation/class/workstation.class.php
+++ htdocs/workstation/class/workstation.class.php
@@ -1,7 +1,7 @@
 <?php
-/* Copyright (C) 2017       Laurent Destailleur <eldy@users.sourceforge.net>
- * Copyright (C) 2020       Gauthier VERDOL     <gauthier.verdol@atm-consulting.fr>
- * Copyright (C) 2023-2024  Frédéric France         <frederic.france@free.fr>
+/* Copyright (C) 2017       Laurent Destailleur 	<eldy@users.sourceforge.net>
+ * Copyright (C) 2020       Gauthier VERDOL     	<gauthier.verdol@atm-consulting.fr>
+ * Copyright (C) 2023-2025  Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2024-2025	MDW						<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -800,8 +800,8 @@ class Workstation extends CommonObject
 	 */
 	public function info($id)
 	{
-		$sql = 'SELECT rowid, date_creation as datec, tms as datem,';
-		$sql .= ' fk_user_creat, fk_user_modif';
+		$sql = 'SELECT t.rowid, t.date_creation as datec, t.tms as datem,';
+		$sql .= ' t.fk_user_creat, t.fk_user_modif';
 		$sql .= ' FROM '.MAIN_DB_PREFIX.$this->table_element.' as t';
 		$sql .= ' WHERE t.rowid = '.((int) $id);
 		$result = $this->db->query($sql);
--- htdocs/workstation/lib/workstation.lib.php
+++ htdocs/workstation/lib/workstation.lib.php
@@ -1,6 +1,6 @@
 <?php
-/* Copyright (C) 2020 Gauthier VERDOL <gauthier.verdol@atm-consulting.fr>
- * Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
+/* Copyright (C) 2020       Gauthier VERDOL             <gauthier.verdol@atm-consulting.fr>
+ * Copyright (C) 2024-2025  Frédéric France             <frederic.france@free.fr>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -42,16 +42,11 @@ function workstationAdminPrepareHead()
 	$h++;
 
 	/*
-	$head[$h][0] = dol_buildpath("/workstation/admin/myobject_extrafields.php", 1);
+	$head[$h][0] = DOL_URL_ROOT."/admin/workstation_extrafields.php";
 	$head[$h][1] = $langs->trans("ExtraFields");
 	$head[$h][2] = 'myobject_extrafields';
 	$h++;
 	*/
-
-	/*$head[$h][0] = require_once "/admin/about.php";
-	$head[$h][1] = $langs->trans("About");
-	$head[$h][2] = 'about';
-	$h++;*/
 
 	// Show more tabs from modules
 	// Entries must be declared in modules descriptor with line
