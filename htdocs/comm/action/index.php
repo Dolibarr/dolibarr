9b845a8bb40a95ca3f9e19f6bbdc3f18357965a2
Frédéric FRANCE <frederic.france@free.fr>
2025-09-01 21:38:59 +0200

    add doc

--- htdocs/comm/action/index.php
+++ htdocs/comm/action/index.php
@@ -1603,7 +1603,7 @@ $color_file = DOL_DOCUMENT_ROOT."/theme/".$conf->theme."/theme_vars.inc.php";
 if (is_readable($color_file)) {
 	global $theme_datacolor;
 	include $color_file;
-	/** @var array{0:array{0:int,1:int,2:int},1:array{0:int,1:int,2:int},2:array{0:int,1:int,2:int},3:array{0:int,1:int,2:int}} $theme_datacolor */
+	/** @var array<int,mixed> $theme_datacolor */
 }
 
 $massactionbutton = '';
