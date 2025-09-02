f272860a31cfe0048f047135446982f433d7e513
Frédéric FRANCE <frederic.france@free.fr>
2025-09-02 15:44:04 +0200

    clean code

--- htdocs/fichinter/card.php
+++ htdocs/fichinter/card.php
@@ -956,7 +956,7 @@ if ($action == 'create') {
 			}
 			$objectsrc->fetch_thirdparty();
 
-			$projectid = (!empty($objectsrc->fk_project) ? $objectsrc->fk_project : 0);
+			$projectid = (int) $objectsrc->fk_project;
 
 			$soc = $objectsrc->thirdparty;
 
