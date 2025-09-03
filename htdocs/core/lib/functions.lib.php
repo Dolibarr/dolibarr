cacc6e28bf338ebd658146fd9598865ef9317148
Lucas Marcouiller <lmarcouiller@dolicloud.com>
2025-09-03 15:52:57 +0200

    fix pre commit

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -1569,7 +1569,7 @@ function dol_buildpath($path, $type = 0, $returnemptyifnotfound = 0)
 				}
 				// if (@file_exists($dirroot.'/'.$path)) {
 				if (@file_exists($dirroot . '/' . $path)) {	// avoid [php:warn]
-					if ($key != 'main' && preg_match('/^core\//', $path)) {	// When searching into an alternative custom path, we don't wan't path like 'core/...' because path should be 'modulename/core/...'
+					if ($key != 'main' && preg_match('/^core\//', $path)) {	// When searching into an alternative custom path, we don't want path like 'core/...' because path should be 'modulename/core/...'
 						continue;
 					}
 					$res = $dirroot . '/' . $path;
