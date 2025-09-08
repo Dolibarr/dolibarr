5d0bba0b5fd3a66eaa878bc08f92f545b7bd1596
Frédéric FRANCE <frederic.france@free.fr>
2025-09-08 10:39:23 +0200

    clean code

--- htdocs/commande/class/commande.class.php
+++ htdocs/commande/class/commande.class.php
@@ -535,7 +535,7 @@ class Commande extends CommonOrder
 		if (!$error && (preg_match('/^[\(]?PROV/i', $this->ref) || empty($this->ref))) { // empty should not happened, but when it occurs, the test save life
 			$num = $this->getNextNumRef($soc);
 		} else {
-			$num = $this->ref;
+			$num = (string) $this->ref;
 		}
 		$this->newref = dol_sanitizeFileName($num);
 
