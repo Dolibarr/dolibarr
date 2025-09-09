294260ad6fba4f179888f3f4c42b86e75dc941c7
Frédéric FRANCE <frederic.france@free.fr>
2025-09-09 09:25:18 +0200

    clean code

--- htdocs/adherents/type.php
+++ htdocs/adherents/type.php
@@ -233,7 +233,7 @@ if ($action == 'update' && $user->hasRight('adherent', 'configurer')) {
 	$object->label = trim($label);
 	$object->morphy	= trim($morphy);
 	$object->status	= (int) $status;
-	$object->subscription = (int) $subscription;
+	$object->subscription = (string) (int) $subscription;
 	$object->amount = ($amount == '' ? '' : price2num($amount, 'MT'));
 	$object->caneditamount = $caneditamount;
 	$object->duration_value = $duration_value;
