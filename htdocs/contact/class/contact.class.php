295b0877071113e8ce506e0a691a66e5568e3144
Frédéric FRANCE <frederic.france@free.fr>
2025-09-09 20:25:41 +0200

    fix

--- htdocs/contact/class/contact.class.php
+++ htdocs/contact/class/contact.class.php
@@ -1587,13 +1587,13 @@ class Contact extends CommonObject
 			$linkclose = ($morecss ? ' class="'.$morecss.'"' : '');
 		}
 
-		if ($option == 'nolink' || empty($url)) {
+		if ($option == 'nolink') {
 			$linkstart = '<span';
 		} else {
 			$linkstart = '<a href="'.$url.'"';
 		}
 		$linkstart .= $linkclose.'>';
-		if ($option == 'nolink' || empty($url)) {
+		if ($option == 'nolink') {
 			$linkend = '</span>';
 		} else {
 			$linkend = '</a>';
