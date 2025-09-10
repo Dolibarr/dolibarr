97c5074ab19c2ee33e59b88d8cef8fec15d401b0
Florian HENRY <florian.henry@open-concept.pro>
2025-09-10 17:24:45 +0200

    feat: Subtotal module

--- htdocs/core/modules/modSubtotals.class.php
+++ htdocs/core/modules/modSubtotals.class.php
@@ -62,9 +62,9 @@ class modSubtotals extends DolibarrModules
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i', '', get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
-		$this->description = "Subtotal and title lines for certain documents";
+		$this->description = "SubTotalModuleDesc";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = 'experimental';
+		$this->version = 'dolibarr';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Name of image file used for this module.
