1d211edd6a23bf79a2d86c20556e845e21e09f49
Frédéric FRANCE <frederic34@users.noreply.github.com>
2025-09-11 18:07:53 +0200

    Update product.class.php

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -1493,7 +1493,7 @@ class Product extends CommonObject
 
 		if ($result >= 0) {
 			// $this->oldcopy should have been set by the caller of update (here properties were already modified)
-			if (is_null($this->oldcopy) || (is_object($this->oldcopy) && $this->oldcopy->isEmpty()) || !method_exists($this->oldcopy, 'hasbatch')) {
+			if (is_null($this->oldcopy) || (is_object($this->oldcopy) && $this->oldcopy->isEmpty())) {
 				$this->oldcopy = dol_clone($this, 1); // 1 to clone with methods to avoid fatal error with $this->oldcopy->hasbatch()
 			}
 			// Test if batch management is activated on existing product
