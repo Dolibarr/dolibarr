52faa519cdf98d7f19b94839d861cb9bae2cd3c2
atm-florian <florian.mortgat@atm-consulting.fr>
2025-09-12 08:52:17 +0200

    FIX 17.0: ContratLigne::fetch() returns 1 even if the line doesn't exist

--- htdocs/contrat/class/contrat.class.php
+++ htdocs/contrat/class/contrat.class.php
@@ -3079,7 +3079,7 @@ class ContratLigne extends CommonObjectLine
 	 *
 	 *  @param	int		$id         Id object
 	 *  @param	string	$ref		Ref of contract line
-	 *  @return int         		<0 if KO, >0 if OK
+	 *  @return int         		<0 if KO, >0 if OK, 0 if not found
 	 */
 	public function fetch($id, $ref = '')
 	{
@@ -3200,6 +3200,9 @@ class ContratLigne extends CommonObjectLine
 				$this->rang = $obj->rang;
 
 				$this->fetch_optionals();
+			} else {
+				$this->db->free($resql);
+				return 0;
 			}
 
 			$this->db->free($resql);
