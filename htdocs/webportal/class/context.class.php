8ba5c0e525af71ed305d60271362630b4883c54e
webmaster67 <46778677+bigbears67@users.noreply.github.com>
2025-09-15 20:39:52 +0200

    Update context.class.php
    ajout d'accès à fichiers joints du tiers.

--- htdocs/webportal/class/context.class.php
+++ htdocs/webportal/class/context.class.php
@@ -2,7 +2,7 @@
 /* Copyright (C) 2023-2024 	Laurent Destailleur         <eldy@users.sourceforge.net>
  * Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
- *
+ * Copyright (C) 2025		Schaffhauser sébastien		<sebastien@webmaster67.fr>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
