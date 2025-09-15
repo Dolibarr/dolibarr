494e887a311525292958543b6b7d09f93be3d6b6
Frédéric FRANCE <frederic.france@free.fr>
2025-09-15 22:32:37 +0200

    clean code

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -2203,6 +2203,7 @@ if ($action == 'create') {
 		}
 		$objectsrc = new $classname($db);
 		'@phan-var-force Project|Commande|Propal|Facture|Contrat|CommandeFournisseur|CommonObject $objectsrc';
+		/** @var CommandeFournisseur|CommonObject $objectsrc */
 		$objectsrc->fetch($originid);
 		$objectsrc->fetch_thirdparty();
 
@@ -2419,7 +2420,7 @@ if ($action == 'create') {
 			}
 			$cond_reglement_id = $invoice_predefined->cond_reglement_id;
 			$mode_reglement_id = $invoice_predefined->mode_reglement_id;
-			$fk_account = (int) $invoice_predefined->fk_account;
+			$fk_account = $invoice_predefined->fk_account;
 			$note_public = $invoice_predefined->note_public;
 			$note_private = $invoice_predefined->note_private;
 
