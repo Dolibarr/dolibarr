af25b8dd420960fc2b4cfef159bfbc05a316d7b1
Frédéric FRANCE <frederic.france@free.fr>
2025-09-15 22:42:15 +0200

    clean code

--- htdocs/salaries/class/paymentsalary.class.php
+++ htdocs/salaries/class/paymentsalary.class.php
@@ -996,11 +996,11 @@ class PaymentSalary extends CommonObject
 		$return .= img_picto('', $this->picto);
 		$return .= '</span>';
 		$return .= '<div class="info-box-content">';
-		$return .= '<span class="info-box-ref inline-block tdoverflowmax150 valignmiddle">'.(method_exists($this, 'getNomUrl') ? $this->getNomUrl(1) : $this->ref).'</span>';
+		$return .= '<span class="info-box-ref inline-block tdoverflowmax150 valignmiddle">' . $this->getNomUrl(1) . '</span>';
 		if ($selected >= 0) {
 			$return .= '<input id="cb'.$this->id.'" class="flat checkforselect fright" type="checkbox" name="toselect[]" value="'.$this->id.'"'.($selected ? ' checked="checked"' : '').'>';
 		}
-		if (property_exists($this, 'fk_bank') && is_numeric($this->fk_bank)) {
+		if (!empty($this->fk_bank) && is_numeric($this->fk_bank)) {
 			require_once DOL_DOCUMENT_ROOT.'/compta/bank/class/account.class.php';
 			$account = new AccountLine($this->db);
 			$account->fetch($this->fk_bank);
