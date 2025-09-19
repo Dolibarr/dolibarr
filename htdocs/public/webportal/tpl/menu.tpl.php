c61ba02a3d65f43985833f4a814204a00bffa99b
webmaster67 <46778677+bigbears67@users.noreply.github.com>
2025-09-19 12:49:39 +0200

    Update menu.tpl.php

--- htdocs/public/webportal/tpl/menu.tpl.php
+++ htdocs/public/webportal/tpl/menu.tpl.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2024		Frédéric France			<frederic.france@free.fr>
+ * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024		sebastien schaffhauser	<sebastien@webmaster67.fr>
  *
  * This program is free software; you can redistribute it and/or modify
