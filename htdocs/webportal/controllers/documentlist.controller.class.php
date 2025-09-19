0f34a799f0d37c3c37f49cbb78e69f904e2dc19f
webmaster67 <46778677+bigbears67@users.noreply.github.com>
2025-09-19 12:30:46 +0200

    Update documentlist.controller.class.php

--- htdocs/webportal/controllers/documentlist.controller.class.php
+++ htdocs/webportal/controllers/documentlist.controller.class.php
@@ -1,11 +1,22 @@
 <?php
 /*
  * Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
- * Copyright (c) 2025       Schaffhauser sébastien      <sebastien@webmaster67.fr>
+ * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2025		Schaffhauser sébastien		<sebastien@webmaster67.fr>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation.
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 
 require_once __DIR__ . '/abstractdocument.controller.class.php';
 
