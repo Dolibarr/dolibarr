13662a2021520ea5eee9ef80e858d7a8ce99c859
Dylan <d.bourdere@gmail.com>
2025-09-23 16:41:14 +0200

    phpcs

--- htdocs/core/lib/pdf.lib.php
+++ htdocs/core/lib/pdf.lib.php
@@ -2856,58 +2856,58 @@ function pdfExtractMetadata($file, $field = 'Keywords')
  * @return void
  */
 function pdf_render_subtotals(
-    TCPDF $pdf,
-    CommonDocGenerator $generator,
-    float $curY,
-    CommonObject $object,
-    int $i,
-    Translate $outputlangs,
-    int $hideref,
-    int $hidedesc,
-    array $bgColor,
-    bool $isSubtotal = false,
-    bool $applySubtotalLogic = true
-): void {
-    $savePage = $pdf->getPage();
-    $saveX = $pdf->GetX();
-    $originalDesc = $object->lines[$i]->desc;
-    $prevAlign = $generator->cols['desc']['content']['align'];
+	TCPDF              $pdf,
+	CommonDocGenerator $generator,
+	float              $curY,
+	CommonObject       $object,
+	int                $i,
+	Translate          $outputlangs,
+	int                $hideref,
+	int                $hidedesc,
+	array              $bgColor,
+	bool               $isSubtotal = false,
+	bool               $applySubtotalLogic = true
+): void
+{
+	$savePage = $pdf->getPage();
+	$saveX = $pdf->GetX();
+	$prevAlign = $generator->cols['desc']['content']['align'];
 
-    if ($isSubtotal && $applySubtotalLogic) {
-        if ($object->lines[$i]->qty < 0) {
-            $outputlangs->load("subtotals");
-            $object->lines[$i]->desc = $outputlangs->trans("SubtotalOf", $object->lines[$i]->desc);
-            $generator->cols['desc']['content']['align'] = ($prevAlign === 'L') ? 'R' : 'L';
-        }
-    }
+	if ($isSubtotal && $applySubtotalLogic) {
+		if ($object->lines[$i]->qty < 0) {
+			$outputlangs->load("subtotals");
+			$object->lines[$i]->desc = $outputlangs->trans("SubtotalOf", $object->lines[$i]->desc);
+			$generator->cols['desc']['content']['align'] = ($prevAlign === 'L') ? 'R' : 'L';
+		}
+	}
 
-    $pdf->startTransaction();
-    $pdf->SetXY($saveX, $curY);
-    $generator->printColDescContent($pdf, $curY, 'desc', $object, $i, $outputlangs, $hideref, $hidedesc);
-    $pageAfter = $pdf->getPage();
-    $yAfter = $pdf->GetY();
-    $pdf->rollbackTransaction(true);
+	$pdf->startTransaction();
+	$pdf->SetXY($saveX, $curY);
+	$generator->printColDescContent($pdf, $curY, 'desc', $object, $i, $outputlangs, $hideref, $hidedesc);
+	$pageAfter = $pdf->getPage();
+	$yAfter = $pdf->GetY();
+	$pdf->rollbackTransaction(true);
 
-    $pdf->SetFillColor($bgColor[0], $bgColor[1], $bgColor[2]);
-    $width = $generator->page_largeur - $generator->marge_droite - $generator->marge_gauche;
+	$pdf->SetFillColor($bgColor[0], $bgColor[1], $bgColor[2]);
+	$width = $generator->page_largeur - $generator->marge_droite - $generator->marge_gauche;
 
 	$pdf->SetXY($generator->marge_gauche, $curY);
 	if ($pageAfter === $savePage) {
 		$pdf->MultiCell($width, max(0, $yAfter - $curY), '', 0, '', true);
-    } else {
+	} else {
 		$pdf->MultiCell($width, $pdf->getPageHeight() - $pdf->getBreakMargin() - $curY, '', 0, '', true);
 
-        $pdf->setPage($pageAfter);
-        $pdf->SetXY($generator->marge_gauche, $pdf->getMargins()['top']);
-        $pdf->MultiCell($width, max(0, $yAfter - $pdf->getMargins()['top']), '', 0, '', true);
+		$pdf->setPage($pageAfter);
+		$pdf->SetXY($generator->marge_gauche, $pdf->getMargins()['top']);
+		$pdf->MultiCell($width, max(0, $yAfter - $pdf->getMargins()['top']), '', 0, '', true);
 
-        $pdf->setPage($savePage);
-    }
+		$pdf->setPage($savePage);
+	}
 
-    $pdf->SetTextColor(colorIsLight(implode(',', $bgColor)));
-    $pdf->SetXY($saveX, $curY);
-    $generator->printColDescContent($pdf, $curY, 'desc', $object, $i, $outputlangs, $hideref, $hidedesc);
-    $generator->setAfterColsLinePositionsData('desc', $pdf->GetY(), $pdf->getPage());
+	$pdf->SetTextColor(colorIsLight(implode(',', $bgColor)));
+	$pdf->SetXY($saveX, $curY);
+	$generator->printColDescContent($pdf, $curY, 'desc', $object, $i, $outputlangs, $hideref, $hidedesc);
+	$generator->setAfterColsLinePositionsData('desc', $pdf->GetY(), $pdf->getPage());
 
-    $generator->cols['desc']['content']['align'] = $prevAlign;
+	$generator->cols['desc']['content']['align'] = $prevAlign;
 }
