d1c614244a524887e0ec89a557b63dca9851459b
sonikf <93765174+sonikf@users.noreply.github.com>
2025-09-24 17:26:45 +0300

    fix phpstan / php-stan (8.2)

--- htdocs/expedition/tpl/objectline_edit.tpl.php
+++ htdocs/expedition/tpl/objectline_edit.tpl.php
@@ -40,6 +40,7 @@ require_once DOL_DOCUMENT_ROOT."/product/class/html.formproduct.class.php";
  * @var CommonObject $this
  * @var CommonObject $object
  * @var HookManager $hookmanager
+ * @var expeditionligne $line
  * @var Societe $buyer
  * @var Societe $seller
  * @var Translate $langs
