2e7084d7fb3ba0118f8ea7152b5698393a6a4373
Frédéric FRANCE <frederic34@users.noreply.github.com>
2025-09-25 13:26:29 +0200

    fix CI

--- htdocs/salaries/class/paymentsalary.class.php
+++ htdocs/salaries/class/paymentsalary.class.php
@@ -902,13 +902,13 @@ class PaymentSalary extends CommonObject
 			$linkclose = ($morecss ? ' class="'.$morecss.'"' : '');
 		}
 
-		if ($option == 'nolink' || empty($url)) {
+		if ($option == 'nolink') {
 			$linkstart = '<span';
 		} else {
 			$linkstart = '<a href="'.$url.'"';
 		}
 		$linkstart .= $linkclose.'>';
-		if ($option == 'nolink' || empty($url)) {
+		if ($option == 'nolink') {
 			$linkend = '</span>';
 		} else {
 			$linkend = '</a>';
