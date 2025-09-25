54d2665bf6b3fdc930a83894c204bed7af2e6bbf
atm-GregM <gregory.maza@atm-consulting.fr>
2025-09-25 11:28:12 +0200

    FIX check commande liste validate

--- htdocs/commande/list.php
+++ htdocs/commande/list.php
@@ -781,7 +781,7 @@ if ($action == 'validate' && $permissiontoadd && $objectclass !== null) {
 					} else {
 						$idwarehouse = 0;
 					}
-					if ($objecttmp->valid($user, $idwarehouse)) {
+					if ($objecttmp->valid($user, $idwarehouse) > 0) {
 						setEventMessages($langs->trans('hasBeenValidated', $objecttmp->ref), null, 'mesgs');
 					} else {
 						setEventMessages($objecttmp->error, $objecttmp->errors, 'errors');
