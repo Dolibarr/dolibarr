7d1fdb700506be8866f6bb77c91f12bf7235d7b1
priojk <git-jk@bloxera.com>
2025-09-26 17:31:55 +0200

    Use float

--- htdocs/fourn/facture/paiement.php
+++ htdocs/fourn/facture/paiement.php
@@ -218,7 +218,7 @@ if (empty($reshook)) {
 					}
 				}
 
-				$formquestion[$i++] = array('type' => 'hidden', 'name' => $key, 'value' => GETPOST($key));
+				$formquestion[$i++] = array('type' => 'hidden', 'name' => $key, 'value' => GETPOSTFLOAT($key));
 			}
 		}
 
