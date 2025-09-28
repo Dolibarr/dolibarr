049b88c38e08fc4a745d77b244fd6bdbef821912
Frédéric France <frederic.france@free.fr>
2025-09-28 08:55:06 +0200

    clean code

--- htdocs/don/class/paymentdonation.class.php
+++ htdocs/don/class/paymentdonation.class.php
@@ -1,6 +1,6 @@
 <?php
 /* Copyright (C) 2015       Alexandre Spangaro	  		<aspangaro@open-dsi.fr>
- * Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
+ * Copyright (C) 2024-2025  Frédéric France             <frederic.france@free.fr>
  * Copyright (C) 2024-2025	MDW							<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -639,7 +639,7 @@ class PaymentDonation extends CommonObject
 				$this->datep,
 				(string) $this->paymenttype, // Payment mode id or code ("CHQ or VIR for example")
 				$label,
-				$amount,
+				(float) $amount,
 				$this->num_payment,
 				0,
 				$user,
