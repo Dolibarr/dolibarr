8a2fef6b827f7a7b781387b1e7b6a10e9f1faf44
Frédéric France <frederic.france@free.fr>
2025-09-28 16:53:46 +0200

    clean code

--- htdocs/eventorganization/class/conferenceorboothattendee.class.php
+++ htdocs/eventorganization/class/conferenceorboothattendee.class.php
@@ -134,11 +134,11 @@ class ConferenceOrBoothAttendee extends CommonObject
 	 */
 	public $email;
 	/**
-	 * @var string
+	 * @var ?string
 	 */
 	public $firstname;
 	/**
-	 * @var string
+	 * @var ?string
 	 */
 	public $lastname;
 	/**
@@ -161,14 +161,7 @@ class ConferenceOrBoothAttendee extends CommonObject
 	 * @var string|float (Price)
 	 */
 	public $amount;
-	/**
-	 * @var string
-	 */
-	public $note_public;
-	/**
-	 * @var string
-	 */
-	public $note_private;
+
 	/**
 	 * @var int
 	 */
@@ -182,15 +175,16 @@ class ConferenceOrBoothAttendee extends CommonObject
 	 */
 	public $last_main_doc;
 	/**
-	 * @var string
+	 * @var ?string
 	 */
 	public $import_key;
 	/**
-	 * @var string
+	 * @var ?string
 	 */
 	public $model_pdf;
+
 	/**
-	 * @var int
+	 * @var ?int
 	 */
 	public $status;
 	// END MODULEBUILDER PROPERTIES
@@ -350,20 +344,11 @@ class ConferenceOrBoothAttendee extends CommonObject
 		unset($object->import_key);
 
 		// Clear fields
-		if (property_exists($object, 'ref')) {
-			// @phan-suppress-next-line PhanTypeInvalidDimOffset
-			$object->ref = empty($this->fields['ref']['default']) ? "(PROV)" : $this->fields['ref']['default'];
-		}
-		if (property_exists($object, 'status')) {
-			$object->status = self::STATUS_DRAFT;
-		}
-		if (property_exists($object, 'date_creation')) {
-			$object->date_creation = dol_now();
-		}
-		if (property_exists($object, 'date_modification')) {
-			$object->date_modification = null;
-		}
-		// ...
+		$object->ref = empty($this->fields['ref']['default']) ? "(PROV)" : $this->fields['ref']['default'];
+		$object->status = self::STATUS_DRAFT;
+		$object->date_creation = dol_now();
+		$object->date_modification = null;
+
 		// Clear extrafields that are unique
 		if (is_array($object->array_options) && count($object->array_options) > 0) {
 			$extrafields->fetch_name_optionals_label($this->table_element);
@@ -397,7 +382,7 @@ class ConferenceOrBoothAttendee extends CommonObject
 
 		if (!$error) {
 			// copy external contacts if same company
-			if (property_exists($this, 'fk_soc') && $this->fk_soc == $object->socid) {
+			if (!empty($this->fk_soc) && $this->fk_soc == $object->socid) {
 				if ($this->copy_linked_contact($object, 'external') < 0) {
 					$error++;
 				}
@@ -612,7 +597,7 @@ class ConferenceOrBoothAttendee extends CommonObject
 
 		// Define new ref
 		$num = (string) $this->ref;
-		if (!$error && (preg_match('/^[\(]?PROV/i', $num) || empty($num))) { // empty should not happened, but when it occurs, the test saves life
+		if (preg_match('/^[\(]?PROV/i', $num) || empty($num)) { // empty should not happened, but when it occurs, the test saves life
 			$num = $this->getNextNumRef();
 		}
 
@@ -1096,7 +1081,6 @@ class ConferenceOrBoothAttendee extends CommonObject
 		global $conf, $langs;
 
 		$result = 0;
-		$includedocgeneration = 0;
 
 		$langs->load("eventorganization@eventorganization");
 
@@ -1112,41 +1096,11 @@ class ConferenceOrBoothAttendee extends CommonObject
 
 		$modelpath = "core/modules/eventorganization/doc/";
 
-		if ($includedocgeneration && !empty($modele)) {
+		if (!empty($modele)) {
 			$result = $this->commonGenerateDocument($modelpath, $modele, $outputlangs, $hidedetails, $hidedesc, $hideref, $moreparams);
 		}
 
 		return $result;
-	}
-
-	/**
-	 * Action executed by scheduler
-	 * CAN BE A CRON TASK. In such a case, parameters come from the schedule job setup field 'Parameters'
-	 * Use public function doScheduledJob($param1, $param2, ...) to get parameters
-	 *
-	 * @return	int			0 if OK, <>0 if KO (this function is used also by cron so only 0 is OK)
-	 */
-	public function doScheduledJob()
-	{
-		global $conf, $langs;
-
-		//$conf->global->SYSLOG_FILE = 'DOL_DATA_ROOT/dolibarr_mydedicatedlofile.log';
-
-		$error = 0;
-		$this->output = '';
-		$this->error = '';
-
-		dol_syslog(__METHOD__, LOG_DEBUG);
-
-		$now = dol_now();
-
-		$this->db->begin();
-
-		// ...
-
-		$this->db->commit();
-
-		return $error;
 	}
 
 	/**
