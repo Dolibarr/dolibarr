ca8e09a44faa11d799278aec0177f2fe40720621
webmaster67 <46778677+bigbears67@users.noreply.github.com>
2025-09-30 09:28:47 +0200

    Update documentlist.controller.class.php

--- htdocs/webportal/controllers/documentlist.controller.class.php
+++ htdocs/webportal/controllers/documentlist.controller.class.php
@@ -105,17 +105,19 @@ class DocumentListController extends AbstractDocumentController
 			$linkBuilder = static function (array $file) use ($client_dir_name) {
 				return DOL_URL_ROOT . '/document.php?modulepart=societe&attachment=1&file=' . urlencode($client_dir_name . '/' . $file['name']);
 			};
+
+			// 3. Encapsulate the link builder in an array, as required by displayFileBrowser
 			$linkBuilderArray = [
 				'file' => $linkBuilder
 			];
 
-			// 3. Call the parent method to display the table
+			// 4. Call the correct parent method (displayFileBrowser) with the correct parameter type
 			$this->displayFileBrowser(
 				$langs->trans('MyDocuments'),
 				$fileList,
 				$langs->trans('NoDocumentAvailable'),
-				$linkBuilder
-				);
+				$linkBuilderArray
+			);
 		}
 
 		print '</main>';
