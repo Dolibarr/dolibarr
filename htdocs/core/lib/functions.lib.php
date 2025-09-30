935045ce01247079d48be7ec008e2d965fcc99ba
Frédéric FRANCE <frederic.france@free.fr>
2025-09-30 10:18:55 +0200

    add example

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -1658,7 +1658,7 @@ function dol_buildpath($path, $type = 0, $returnemptyifnotfound = 0)
  * @param	bool 	 	 				$addurlroot Add DOL_URL_ROOT
  * @return string									path
  */
-function dolBuildUrl($path, $params = [], $addtoken = false, $addurlroot = true)
+function dolBuildUrl($path, $params = [], $addtoken = false, $addurlroot = false)
 {
 	global $db, $hookmanager;
 
