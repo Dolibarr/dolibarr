e11c524953372356ce5d721188350757a5246f59
Frédéric France <frederic.france@free.fr>
2025-10-04 20:42:32 +0200

    clean code

--- htdocs/product/dynamic_price/class/price_parser.class.php
+++ htdocs/product/dynamic_price/class/price_parser.class.php
@@ -118,13 +118,13 @@ class PriceParser
 		if (in_array($code, array(9, 14, 19, 20))) { //Errors which have 0 arg
 			return $langs->trans("ErrorPriceExpression".$code);
 		} elseif (in_array($code, array(1, 2, 3, 4, 5, 8, 10, 11, 17, 21, 22))) { //Errors which have 1 arg
-			return $langs->trans("ErrorPriceExpression".$code, $info);
+			return $langs->trans("ErrorPriceExpression".$code, (string) $info);
 		} elseif (in_array($code, array(6, 23))) { //Errors which have 2 args
 			return $langs->trans("ErrorPriceExpression".$code, $info[0], $info[1]);
 		} elseif (in_array($code, array(7, 12, 13, 15, 16, 18))) { //Internal errors
 			return $langs->trans("ErrorPriceExpressionInternal", $code);
 		} else { //Unknown errors
-			return $langs->trans("ErrorPriceExpressionUnknown", $code);
+			return $langs->trans("ErrorPriceExpressionUnknown", (string) $code);
 		}
 	}
 
