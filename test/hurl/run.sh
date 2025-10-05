77a3774c9cd09d65baae6f2d34498fd0cf25a09e
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-10-05 13:50:50 +0200

    change the APIHEADER to be read from the environment

--- test/hurl/run.sh
+++ test/hurl/run.sh
@@ -4,19 +4,16 @@ echo "First we run tests that do not require authentication"
 find api/ gui/ public/ -type f -iname '00*.hurl' -exec hurl --test "{}" \;
 
 # Now we get ready to run tests that do require authentication
-if [[ -f .settings/default.properties ]]; then
-	source .settings/default.properties
-else
-	echo "Warning, there are no .settings/default.properties to source, so tests which require an authentication header will fail!"
-fi
 if [[ -z ${APIHEADER+x} ]]; then
-	echo "APIHEADER bash variable is unset even after 'source .settings/default.properties'"
-	exit 127
+	echo "APIHEADER bash variable is unset, no API tests that require authentication"
+else
+	echo "Now we are ready to run API tests that do require authentication"
+	find api/ -type f -iname '10*.hurl' -not -iname '00*.hurl' -exec hurl --header "${APIHEADER}" --test "{}" \;
 fi
 
-echo "Now we are ready to run API tests that do require authentication"
-find api/ -type f -iname '10*.hurl' -not -iname '00*.hurl' -exec hurl --header "${APIHEADER}" --test "{}" \;
-
-
-# echo "Now we are ready to run GUI tests that do require authentication"
-# find gui/ -type f -iname '10*.hurl' -not -iname '00*.hurl' -exec hurl --header "${GUIHEADER}" --test "{}" \;
+#if [[ -z ${GUIHEADER+x} ]]; then
+#	echo "GUIHEADER bash variable is unset, no GUI tests that require authentication"
+#else
+#	echo "Now we are ready to run GUI tests that do require authentication"
+#	find gui/ -type f -iname '10*.hurl' -not -iname '00*.hurl' -exec hurl --header "${GUIHEADER}" --test "{}" \;
+#fi
