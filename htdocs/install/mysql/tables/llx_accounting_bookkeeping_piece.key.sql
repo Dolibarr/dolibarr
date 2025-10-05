8972c37bd09944a009d1f436b0ebf16978ba9343
Alexandre SPANGARO <alexandre.spangaro@gmail.com>
2025-10-05 07:20:44 +0200

    SQL Accountancy - Add a table to manage transaction

---
+++ htdocs/install/mysql/tables/llx_accounting_bookkeeping_piece.key.sql
@@ -0,0 +1,27 @@
+-- ============================================================================
+-- Copyright (C) 2025		Alexandre Spangaro		<alexandre@inovea-conseil.com>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <https://www.gnu.org/licenses/>.
+--
+-- ============================================================================
+
+ALTER TABLE llx_accounting_bookkeeping_piece ADD UNIQUE INDEX uk_accounting_bookkeeping_piece_ref (ref, entity);
+
+ALTER TABLE llx_accounting_bookkeeping_piece ADD INDEX idx_accounting_bookkeeping_piece_fk_user_author (fk_user_author);
+ALTER TABLE llx_accounting_bookkeeping_piece ADD INDEX idx_accounting_bookkeeping_piece_fk_user_modif (fk_user_modif);
+ALTER TABLE llx_accounting_bookkeeping_piece ADD INDEX idx_accounting_bookkeeping_piece_fk_user_valid (fk_user_valid);
+ALTER TABLE llx_accounting_bookkeeping_piece ADD INDEX idx_accounting_bookkeeping_piece_fk_user_closing (fk_user_closing);
+
+ALTER TABLE llx_accounting_bookkeeping_piece ADD CONSTRAINT fk_accounting_bookkeeping_piece_fk_user_author    FOREIGN KEY (fk_user_author) REFERENCES llx_user (rowid);
+ALTER TABLE llx_accounting_bookkeeping_piece ADD CONSTRAINT fk_accounting_bookkeeping_piece_fk_user_valid     FOREIGN KEY (fk_user_valid)  REFERENCES llx_user (rowid);
