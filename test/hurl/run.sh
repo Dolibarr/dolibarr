d48fc61a022798c127e4008b1c343708daf4c516
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-10-05 13:43:18 +0200

    modifying run.sh to get it past commit checks

--- test/hurl/run.sh
+++ test/hurl/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "First we run tests that do not require authentication"
-hurl --test $( find api/ gui/ public/ -type f -iname '00*.hurl' )
+find api/ gui/ public/ -type f -iname '00*.hurl' -exec hurl --test "{}" \;
 
 # Now we get ready to run tests that do require authentication
 if [[ -f .settings/default.properties ]]; then
@@ -15,7 +15,8 @@ if [[ -z ${APIHEADER+x} ]]; then
 fi
 
 echo "Now we are ready to run API tests that do require authentication"
-hurl --header "${APIHEADER}" --test $( find api/ -type f -iname '*.hurl' -not -iname '00*.hurl' )
+find api/ -type f -iname '10*.hurl' -not -iname '00*.hurl' -exec hurl --header "${APIHEADER}" --test "{}" \;
+
 
 # echo "Now we are ready to run GUI tests that do require authentication"
-# hurl --header "${APIHEADER}" --test $( find gui/ -type f -iname '*.hurl' -not -iname '00*.hurl' )
+# find gui/ -type f -iname '10*.hurl' -not -iname '00*.hurl' -exec hurl --header "${GUIHEADER}" --test "{}" \;
