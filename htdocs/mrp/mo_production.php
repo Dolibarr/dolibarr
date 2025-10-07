bcaa0adba489ce236e0ee0ac4b260ce037ba98ac
Frédéric France <frederic.france@free.fr>
2025-10-07 18:51:04 +0200

    fix mo production with dlc dluo

--- htdocs/mrp/mo_production.php
+++ htdocs/mrp/mo_production.php
@@ -1556,9 +1556,6 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 				$nblinetoproduce++;
 			}
 		}
-		var_dump($nblinetoproduce);
-		var_dump($atleastoneatby);
-		var_dump($atleastonsellby);
 
 		$newcardbutton = '';
 		$url = $_SERVER["PHP_SELF"].'?id='.$object->id.'&action=addproduceline&token='.newToken();
