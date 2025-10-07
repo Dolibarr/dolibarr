9d38cabd60bddefefa1e42c89f7d9297ee207be9
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-10-07 18:20:30 +0200

    Trying to find out which line evaluates to noting

--- htdocs/comm/mailing/class/mailing_targets.class.php
+++ htdocs/comm/mailing/class/mailing_targets.class.php
@@ -185,7 +185,10 @@ class MailingTarget extends CommonObject
 
 		$sql = "INSERT INTO ".MAIN_DB_PREFIX."mailing_target";
 		$sql .= " (fk_mailing, fk_contact, email, statut)";
-		$sql .= " VALUES ('".((int) $this->fk_mailing)."', "((int) $this->fk_contact).", '".$this->db->escape($this->email)."', ".((int) $conf->statut).")";
+		$sql .= " VALUES ('".((int) $this->fk_mailing)."', ";
+		$sql .= " .((int) $this->fk_contact)., ";
+		$sql .= "'".$this->db->escape($this->email)."', ";
+		$sql .= " .((int) $conf->statut)).";
 
 		dol_syslog(__METHOD__, LOG_DEBUG);
 
