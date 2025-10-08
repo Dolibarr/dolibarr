6b128621f7bbcff318739d021fe64e70939d12a0
Frédéric FRANCE <frederic.france@free.fr>
2025-10-08 13:46:55 +0200

    clean code

--- htdocs/api/class/api_documents.class.php
+++ htdocs/api/class/api_documents.class.php
@@ -882,19 +882,18 @@ class Documents extends DolibarrApi
 				throw new RestException(500, 'Modulepart '.$modulepart.' not implemented yet.');
 			}
 
-			if (is_object($object)) {
-				if ($fetchbyid) {
-					// @phan-suppress-next-line PhanPluginSuspiciousParamPosition
-					$result = $object->fetch((int) $ref);
-				} else {
-					$result = $object->fetch(0, $ref);
-				}
+			// at this step $object is always an object
+			if ($fetchbyid) {
+				// @phan-suppress-next-line PhanPluginSuspiciousParamPosition
+				$result = $object->fetch((int) $ref);
+			} else {
+				$result = $object->fetch(0, $ref);
+			}
 
-				if ($result == 0) {
-					throw new RestException(404, "Object with ref '".$ref."' was not found.");
-				} elseif ($result < 0) {
-					throw new RestException(500, 'Error while fetching object: '.$object->error);
-				}
+			if ($result == 0) {
+				throw new RestException(404, "Object with ref '".$ref."' was not found.");
+			} elseif ($result < 0) {
+				throw new RestException(500, 'Error while fetching object: '.$object->error);
 			}
 
 			if (!($object->id > 0)) {
