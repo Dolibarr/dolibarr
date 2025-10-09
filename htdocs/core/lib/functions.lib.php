1249a29c5b971858818439a2cb3a432ca2452de0
Frédéric France <frederic.france@free.fr>
2025-10-09 21:09:48 +0200

    add dolBuildUrl

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -12673,7 +12673,9 @@ function complete_head_from_modules($conf, $langs, $object, &$head, &$h, $type, 
 				$url = preg_replace('/__ID__/i', ((is_object($object) && !empty($object->id)) ? $object->id : ''), $values[5]);
 				$link = parse_url($url);
 				$query = [];
-				parse_str($link['query'], $query);
+				if (isset($link['query'])) {
+					parse_str($link['query'], $query);
+				}
 				$newtab[0] = dolBuildUrl(dol_buildpath($link['path'], 1), $query);
 				$newtab[1] = $label;
 				$newtab[2] = str_replace('+', '', $values[1]);
