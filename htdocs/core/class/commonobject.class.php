7b79720941e28f310bfec1e42685d282d2f71fb1
Laurent Destailleur <eldy@destailleur.fr>
2025-10-15 01:03:05 +0200

    Update commonobject.class.php

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -8000,7 +8000,7 @@ abstract class CommonObject
 				}
 
 				if (!$filter_categorie) {
-					isset($InfoFieldList[1]) ? $fields_label = explode('|', $InfoFieldList[1]) : $fields_label=array();
+					$fields_label = isset($InfoFieldList[1]) ? explode('|', $InfoFieldList[1]) : array();
 					if (!empty($fields_label)) {
 						$keyList .= ', ';
 						$keyList .= implode(', ', $fields_label);
