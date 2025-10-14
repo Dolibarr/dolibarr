8edf707edd3d9839aa71f7bc4f4f33cfa9ced552
Laurent Destailleur <eldy@destailleur.fr>
2025-10-14 22:17:44 +0200

    Update api_setup.class.php

--- htdocs/api/class/api_setup.class.php
+++ htdocs/api/class/api_setup.class.php
@@ -1045,7 +1045,7 @@ class Setup extends DolibarrApi
 		if ($type == 'expedition' && !getDolGlobalInt('SHIPPING_USE_ITS_OWN_CONTACTS')) {
 			$type = 'commande';
 		}
-		
+
 		$sql = "SELECT rowid, code, element as type, libelle as label, source, module, position";
 		$sql .= " FROM ".MAIN_DB_PREFIX."c_type_contact as t";
 		$sql .= " WHERE t.active = ".((int) $active);
