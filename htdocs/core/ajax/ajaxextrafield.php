545b7b4147be39f9a00395d4a06f4cce7d4493fb
Laurent Destailleur <eldy@destailleur.fr>
2025-10-15 01:03:44 +0200

    Update ajaxextrafield.php

--- htdocs/core/ajax/ajaxextrafield.php
+++ htdocs/core/ajax/ajaxextrafield.php
@@ -180,7 +180,7 @@ if ($object instanceof CommonObject) {
 		}
 
 		if (!$filter_categorie) {
-			isset($InfoFieldList[1]) ? $fields_label = explode('|', $InfoFieldList[1]) : $fields_label=array();
+			$fields_label = isset($InfoFieldList[1]) ? explode('|', $InfoFieldList[1]) : array();
 			if (!empty($fields_label)) {
 				$keyList .= ', ';
 				$keyList .= implode(', ', $fields_label);
