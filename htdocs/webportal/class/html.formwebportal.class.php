e44f7f00e7e8e1a3f792a0f4029c86de81665056
Laurent Destailleur <eldy@destailleur.fr>
2025-10-15 01:01:25 +0200

    Update html.formwebportal.class.php

--- htdocs/webportal/class/html.formwebportal.class.php
+++ htdocs/webportal/class/html.formwebportal.class.php
@@ -842,7 +842,7 @@ class FormWebPortal extends Form
 				}
 
 				if (!$filter_categorie) {
-					isset($InfoFieldList[1]) ? $fields_label = explode('|', $InfoFieldList[1]) : $fields_label=array();
+					$fields_label = isset($InfoFieldList[1]) ? explode('|', $InfoFieldList[1]) : array();
 					if (!empty($fields_label)) {
 						$keyList .= ', ';
 						$keyList .= implode(', ', $fields_label);
