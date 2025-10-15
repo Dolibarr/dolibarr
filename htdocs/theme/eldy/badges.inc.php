9673b8991d20b48abfd8aa231745ddbdcf2e5703
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2025-10-15 08:56:01 +0200

    fix css badge pill definition

--- htdocs/theme/eldy/badges.inc.php
+++ htdocs/theme/eldy/badges.inc.php
@@ -69,10 +69,16 @@ if (!defined('ISLOADEDBYSTEELSHEET')) {
 	}
 }
 
-.badge-pill, .tabs .badge {
+.tabs .badge {
 	padding-right: .5em;
 	padding-left: .5em;
 	border-radius: 0.25rem;
+}
+
+.badge-pill{
+	padding-right: .8em;
+	padding-left: 0.8em;
+	border-radius: 0.5rem;
 }
 
 .badge-dot {
