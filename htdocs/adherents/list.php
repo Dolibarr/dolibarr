a3ab516d17d7ed44dab66f10eb4869f8d529a1df
Frédéric France <frederic.france@free.fr>
2025-10-19 12:12:34 +0200

    clean code

--- htdocs/adherents/list.php
+++ htdocs/adherents/list.php
@@ -744,7 +744,12 @@ if ($search_datem_end) {
 
 // Add $query from extra fields
 include DOL_DOCUMENT_ROOT.'/core/tpl/extrafields_list_search_param.tpl.php';
+// build $param
 $param = http_build_query($query);
+// Add $param from hooks
+$parameters = array('param' => &$param);
+$reshook = $hookmanager->executeHooks('printFieldListSearchParam', $parameters, $object, $action); // Note that $action and $object may have been modified by hook
+$param .= $hookmanager->resPrint;
 
 // List of mass actions available
 $arrayofmassactions = array(
