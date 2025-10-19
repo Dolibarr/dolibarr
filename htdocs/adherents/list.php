fb76fbd2c5b69f1db19a9bef0905aa3c7c8bd5f3
Frédéric France <frederic.france@free.fr>
2025-10-19 10:22:21 +0200

    clean code

--- htdocs/adherents/list.php
+++ htdocs/adherents/list.php
@@ -645,96 +645,105 @@ if ($search_type > 0) {
 }
 
 // $parameters
-$param = '';
+$query = [];
 if (!empty($mode)) {
-	$param .= '&mode='.urlencode($mode);
+	$query += ['mode' => $mode];
 }
 if (!empty($contextpage) && $contextpage != $_SERVER["PHP_SELF"]) {
-	$param .= '&contextpage='.urlencode($contextpage);
+	$query += ['contextpage' => $contextpage];
 }
 if ($limit > 0 && $limit != $conf->liste_limit) {
-	$param .= '&limit='.((int) $limit);
+	$query += ['limit' => $limit];
 }
 if ($optioncss != '') {
-	$param .= '&optioncss='.urlencode($optioncss);
+	$query += ['optioncss' => $optioncss];
 }
 if ($groupby != '') {
-	$param .= '&groupby='.urlencode($groupby);
+	$query += ['groupby' => $groupby];
 }
 if ($search_all != "") {
-	$param .= "&search_all=".urlencode($search_all);
+	$query += ['search_all' => $serach_all];
 }
 if ($search_ref) {
-	$param .= "&search_ref=".urlencode($search_ref);
+	$query += ['search-ref' => $search_ref];
 }
 if ($search_civility) {
-	$param .= "&search_civility=".urlencode($search_civility);
+	$query += ['search_civility' => $search_civility];
 }
 if ($search_firstname) {
-	$param .= "&search_firstname=".urlencode($search_firstname);
+	$query += ['search_firstname' => $search_firstname];
 }
 if ($search_lastname) {
-	$param .= "&search_lastname=".urlencode($search_lastname);
+	$query += ['search_lastname' => $serach_lastname];
 }
 if ($search_gender) {
-	$param .= "&search_gender=".urlencode($search_gender);
+	$query += ['search_gender' => $search_gender];
 }
 if ($search_login) {
-	$param .= "&search_login=".urlencode($search_login);
+	$query += ['search_login' => $search_login];
 }
 if ($search_email) {
-	$param .= "&search_email=".urlencode($search_email);
+	$query += ['search_email' => $search_email];
 }
 if ($search_categ > 0 || $search_categ == -2) {
-	$param .= "&search_categ=".urlencode((string) ($search_categ));
+	$query += ['search_categ' => $search_categ];
 }
 if ($search_company) {
-	$param .= "&search_company=".urlencode($search_company);
+	$query += ['search_company' => $search_company];
 }
 if ($search_address != '') {
-	$param .= "&search_address=".urlencode($search_address);
+	$query += ['search_address' => $search_address];
 }
 if ($search_town != '') {
-	$param .= "&search_town=".urlencode($search_town);
+	$query += ['search_town' => $search_town];
 }
 if ($search_zip != '') {
-	$param .= "&search_zip=".urlencode($search_zip);
+	$query += ['search_zip' => $search_zip];
 }
 if ($search_state != '') {
-	$param .= "&search_state=".urlencode($search_state);
+	$query += ['search_state' => $search_state];
 }
 if ($search_country != '') {
-	$param .= "&search_country=".urlencode($search_country);
+	$query += ['search_country' => $search_country];
 }
 if ($search_phone != '') {
-	$param .= "&search_phone=".urlencode($search_phone);
+	$query += ['search_phone' => $search_phone];
 }
 if ($search_phone_perso != '') {
-	$param .= "&search_phone_perso=".urlencode($search_phone_perso);
+	$query += ['search_phone_perso' => $search_phone_perso];
 }
 if ($search_phone_mobile != '') {
-	$param .= "&search_phone_mobile=".urlencode($search_phone_mobile);
+	$query += ['search_phone_mobile' => $search_phone_mobile];
 }
 if ($search_filter && $search_filter != '-1') {
-	$param .= "&search_filter=".urlencode($search_filter);
+	$query += ['search_filter' => $search_filter];
 }
 if ($search_status != "" && $search_status != -3) {
-	$param .= "&search_status=".urlencode($search_status);
+	$query += ['search_status' => $search_status];
 }
 if ($search_import_key != '') {
-	$param .= '&search_import_key='.urlencode($search_import_key);
+	$query += ['search_import_key' => $search_import_key];
 }
 if ($search_type > 0) {
-	$param .= "&search_type=".urlencode($search_type);
+	$query += ['search_type' => $search_type];
 }
 if ($search_datec_start) {
-	$param .= '&search_datec_start_day='.dol_print_date($search_datec_start, '%d').'&search_datec_start_month='.dol_print_date($search_datec_start, '%m').'&search_datec_start_year='.dol_print_date($search_datec_start, '%Y');
+	$query += [
+		'search_datec_start_day' => dol_print_date($search_datec_start, '%d'),
+		'search_datec_start_month' => dol_print_date($search_datec_start, '%m'),
+		'search_datec_start_year' => dol_print_date($search_datec_start, '%Y'),
+	];
 }
 if ($search_datem_end) {
-	$param .= '&search_datem_end_day='.dol_print_date($search_datem_end, '%d').'&search_datem_end_month='.dol_print_date($search_datem_end, '%m').'&search_datem_end_year='.dol_print_date($search_datem_end, '%Y');
+	$query += [
+		'search_datem_end_day' => dol_print_date($search_datem_end, '%d'),
+		'search_datem_end_month' => dol_print_date($search_datem_end, '%m'),
+		'search_datem_end_year' => dol_print_date($search_datem_end, '%Y'),
+	];
 }
 
-// Add $param from extra fields
+$param = http_build_query($query);
+// Add $query from extra fields
 include DOL_DOCUMENT_ROOT.'/core/tpl/extrafields_list_search_param.tpl.php';
 
 // List of mass actions available
