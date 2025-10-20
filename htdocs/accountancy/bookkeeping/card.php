23e93c29d7c2bf5bf07b24764098521571d65939
Alexandre SPANGARO <alexandre.spangaro@gmail.com>
2025-10-20 03:55:08 +0200

    Fix phan

--- htdocs/accountancy/bookkeeping/card.php
+++ htdocs/accountancy/bookkeeping/card.php
@@ -199,7 +199,7 @@ if (empty($reshook)) {
 			$action = '';
 		}
 		$subledger_account_str = is_array($subledger_account) ? reset($subledger_account) : (string) $subledger_account;
-		if (!checkGeneralAccountAllowsAuxiliary($db, $accountingaccount_number, $subledger_account)) {
+		if (!checkGeneralAccountAllowsAuxiliary($db, $accountingaccount_number, $subledger_account_str)) {
 			$error++;
 			setEventMessages($langs->trans("ErrorAccountNotCentralized"). ". " . $langs->trans("RemoveSubsidiaryAccountOrAdjustTheGeneralAccount"), null, 'errors');
 		}
