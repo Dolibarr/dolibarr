a205b6f610f22c017501fab09c54b724aa72fdbd
Laurent Destailleur <eldy@destailleur.fr>
2025-10-23 13:36:32 +0200

    FIX Enhance SQL query with conditional entity filtering
    Added conditional filtering for usergroup entity in SQL query based on MULTICOMPANY_TRANSVERSE_MODE setting.

--- htdocs/user/perms.php
+++ htdocs/user/perms.php
@@ -237,7 +237,14 @@ $sql = "SELECT DISTINCT gr.fk_id, gu.entity";	// fk_id are permission id and ent
 $sql .= " FROM ".MAIN_DB_PREFIX."usergroup_rights as gr,";
 $sql .= " ".MAIN_DB_PREFIX."usergroup_user as gu";	// all groups of a user
 $sql .= " WHERE gr.entity = ".((int) $entity); // it's very important, don't change please !
-$sql .= " AND gu.entity = ".((int) $entity); // it's very important, don't change please !
+// The entity on the table gu=usergroup_user should be useless and should never be used because it is already into gr and r.
+// but when using MULTICOMPANY_TRANSVERSE_MODE, we may have inserted record that make rubbish result here due to the duplicate record of
+// other entities, so we are forced to add a filter on gu here
+if (getDolGlobalString("MULTICOMPANY_TRANSVERSE_MODE_FIX_WHEN_GU_CONTAINS_0")) {
+	$sql .= " AND gu.entity IN (0,".$conf->entity.")";
+} else {
+	$sql .= " AND gu.entity = ".((int) $entity);
+}
 $sql .= " AND gr.fk_usergroup = gu.fk_usergroup";
 $sql .= " AND gu.fk_user = ".((int) $object->id);
 
