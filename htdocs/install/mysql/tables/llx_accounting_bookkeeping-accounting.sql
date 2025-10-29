38eb7545247c71f5c04b95067102ba26e2ff794e
Alexandre SPANGARO <alexandre.spangaro@gmail.com>
2025-10-29 14:09:47 +0100

    Copyright

--- htdocs/install/mysql/tables/llx_accounting_bookkeeping-accounting.sql
+++ htdocs/install/mysql/tables/llx_accounting_bookkeeping-accounting.sql
@@ -1,6 +1,7 @@
 -- ============================================================================
 -- Copyright (C) 2013-2014	Olivier Geffroy      	<jeff@jeffinfo.com>
 -- Copyright (C) 2013-2025	Alexandre Spangaro   	<alexandre@inovea-conseil.com>
+-- Copyright (C) 2025		Jean-Rémi TAPONIER		<jean-remi@netlogic.fr>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
