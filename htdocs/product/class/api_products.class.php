4e0f0090b6dbb3e955ab0711e6d4536b48acc1da
Laurent Destailleur <eldy@destailleur.fr>
2025-11-02 18:29:40 +0100

    Fix return type annotation in API documentation

--- htdocs/product/class/api_products.class.php
+++ htdocs/product/class/api_products.class.php
@@ -2217,7 +2217,7 @@ class Products extends DolibarrApi
 	 * @param string $type           Type of the contact (BILLING, SHIPPING, CUSTOMER)
 	 * @param string $source         external=Contact extern (llx_socpeople), internal=Contact intern (llx_user)
 	 * @param int    $notrigger      Disable all triggers
-	 * @return array<string,{code:int,message:string}>
+	 * @return array<string,array{code:int,message:string}>
 	 *
 	 * @url	POST {id}/contact/{contactid}/{type}
 	 *
@@ -2266,7 +2266,7 @@ class Products extends DolibarrApi
 	 *
 	 * @url	DELETE {id}/contact/{contactid}/{type}
 	 *
-	 * @return array<string,{code:int,message:string}>
+	 * @return array<string,array{code:int,message:string}>
 	 *
 	 * @throws RestException 401
 	 * @throws RestException 404
