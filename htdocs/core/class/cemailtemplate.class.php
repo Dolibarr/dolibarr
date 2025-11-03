527b563a8c73d22ac0e2d4e162b215cef40a0a0b
MDW <mdeweerd@users.noreply.github.com>
2025-11-03 23:59:41 +0100

    Qual: Make $fields type covariant (phpstan)
    # Qual: Make $fields type covariant (phpstan)

--- htdocs/core/class/cemailtemplate.class.php
+++ htdocs/core/class/cemailtemplate.class.php
@@ -45,7 +45,7 @@ class CEmailTemplate extends CommonObject
 
 	// BEGIN MODULEBUILDER PROPERTIES
 	/**
-	 * @var array<string,array{type:string,label:string,enabled:int<0,2>|string,position:int,notnull?:int,visible:int<-6,6>|string,alwayseditable?:int<0,1>,noteditable?:int<0,1>,default?:string,index?:int,foreignkey?:string,searchall?:int<0,1>,isameasure?:int<0,1>,css?:string,csslist?:string,help?:string,showoncombobox?:int<0,4>,disabled?:int<0,1>,arrayofkeyval?:array<int|string,string>,autofocusoncreate?:int<0,1>,comment?:string,copytoclipboard?:int<1,2>,validate?:int<0,1>,showonheader?:int<0,1>}>	Array with all fields and their property. Do not use it as a static var. It may be modified by constructor.
+	 * @var array<string,array{type:string,label:string,langfile?:string,enabled:int<0,2>|string,position:int,notnull?:int,visible:int<-6,6>|string,alwayseditable?:int<0,1>|string,noteditable?:int<0,1>,default?:string,index?:int,foreignkey?:string,searchall?:int<0,1>,isameasure?:int<0,1>,css?:string,cssview?:string,csslist?:string,help?:string,showoncombobox?:int<0,4>|string,disabled?:int<0,1>,arrayofkeyval?:array<int|string,string>,autofocusoncreate?:int<0,1>,comment?:string,copytoclipboard?:int<1,2>,validate?:int<0,1>,showonheader?:int<0,1>}>	Array with all fields and their property. Do not use it as a static var. It may be modified by constructor.
 	 */
 	public $fields = array(
 		"rowid" => array("type" => "integer", "label" => "TechnicalID", 'enabled' => 1, 'position' => 10, 'notnull' => 1, 'visible' => -1,),
@@ -280,7 +280,7 @@ class CEmailTemplate extends CommonObject
 			$sql .= " ".((int) $this->datec).",";
 		}
 		$sql .= " '".$this->db->escape($this->label)."',";
-		$sql .= " ".((int) $this->position).", ".((int) $this->defaultfortype ).",";
+		$sql .= " ".((int) $this->position).", ".((int) $this->defaultfortype).",";
 		if (is_null($this->enabled)) {
 			$sql .= " 1,";
 		} else {
@@ -497,7 +497,7 @@ class CEmailTemplate extends CommonObject
 	public function apifetch($id, $label = '')
 	{
 		// Check parameters
-		if (($id ==0 || empty($id)) && empty($label)) {
+		if (($id == 0 || empty($id)) && empty($label)) {
 			dol_syslog(get_class($this)."::apifetch id and label are empty", LOG_DEBUG);
 			$this->error = 'id='.$id.' and label are empty';
 			return -1;
