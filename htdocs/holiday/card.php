5dad095f0223189cf7b686f80d35ca538fec9875
Laurent Destailleur <eldy@destailleur.fr>
2025-11-06 02:08:40 +0100

    Clarify comments regarding default approver logic

--- htdocs/holiday/card.php
+++ htdocs/holiday/card.php
@@ -1161,13 +1161,13 @@ if ((empty($id) && empty($ref)) || $action == 'create' || $action == 'add') {
 		if (empty($include_users)) {
 			print img_warning().' '.$langs->trans("NobodyHasPermissionToValidateHolidays");
 		} else {
-			// Defined default approver (the forced approved of user or the supervisor if no forced value defined)
-			// Note: This use will be set only if the deinfed approvr has permission to approve so is inside include_users
+			// Defined default approver (the forced approver of edited user or the supervisor of user if no forced value defined)
+			// Note: This user will be set only if the defined approver has permission to approve so is inside include_users
 			$defaultselectuser = (empty($user->fk_user_holiday_validator) ? $user->fk_user : $user->fk_user_holiday_validator);
 			if ($fuserid != $user->id) {
-				$target = new User($db);
-				$target->fetch($fuserid);
-				$defaultselectuser = (empty($target->fk_user_holiday_validator) ? $target->fk_user : $target->fk_user_holiday_validator);
+				$fuser = new User($db);
+				$fuser->fetch($fuserid);
+				$defaultselectuser = (empty($fuser->fk_user_holiday_validator) ? $fuser->fk_user : $fuser->fk_user_holiday_validator);
 			}
 
 			if (getDolGlobalString('HOLIDAY_DEFAULT_VALIDATOR')) {
