29a4908f55a118e5a3b4c49d522a940bece50f8a
Kamel Khelifa <kkhelifa@open-dsi.fr>
2025-11-06 14:57:57 +0100

    Correction phpstan

--- htdocs/public/webportal/tpl/card-edit-footer.tpl.php
+++ htdocs/public/webportal/tpl/card-edit-footer.tpl.php
@@ -8,15 +8,15 @@ if (empty($context) || !is_object($context)) {
 	exit(1);
 }
 '@phan-var-force Context $context';
-'@phan-var-force Controller $this';
+'@phan-var-force AbstractCardController $this';
 
 /**
- * @var Conf				$conf
- * @var HookManager			$hookmanager
- * @var Translate			$langs
- * @var Context				$context
- * @var Controller 			$this
- * @var FormCardWebPortal 	$formCard
+ * @var Conf					$conf
+ * @var HookManager				$hookmanager
+ * @var Translate				$langs
+ * @var Context					$context
+ * @var AbstractCardController 	$this
+ * @var FormCardWebPortal 		$formCard
  */
 $formCard = $this->formCard;
 
