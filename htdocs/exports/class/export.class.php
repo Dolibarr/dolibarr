dfb6d2b4e4e0fa66e778ece93f6ac5fd90a54b6f
Frédéric France <frederic.france@free.fr>
2025-11-08 18:27:02 +0100

    clean code

--- htdocs/exports/class/export.class.php
+++ htdocs/exports/class/export.class.php
@@ -4,6 +4,7 @@
  * Copyright (C) 2012       Charles-Fr BENKE    <charles.fr@benke.fr>
  * Copyright (C) 2016       Raphaël Doursenaud  <rdoursenaud@gpcsolutions.fr>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2025       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -754,7 +755,7 @@ class Export
 				// Generate title line
 				$objmodel->write_title($this->array_export_fields[$indice], $array_selected, $outputlangs, isset($this->array_export_TypeFields[$indice]) ? $this->array_export_TypeFields[$indice] : null);
 
-				$MAXFORTEST = 0;	// For test on large database, we can set it to a non zero value to limit the export size
+				$MAXFORTEST = getDolGlobalInt('MAX_FOR_TEXT_EXPORT');	// For test on large database, we can set it to a non zero value to limit the export size
 				$counterlineexported = 0;
 				while ($obj = $this->db->fetch_object($resql)) {
 					$counterlineexported++;
