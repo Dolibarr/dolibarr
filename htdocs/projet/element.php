fa68e05dd33ccc40e65e63640a3a899b5b5a0791
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-11-09 23:54:17 +0100

    Fix PHP Warning:  Undefined array key "nblinesnull" in /var/www/html/projet/element.php

--- htdocs/projet/element.php
+++ htdocs/projet/element.php
@@ -1642,7 +1642,7 @@ foreach ($listofreferent as $key => $value) {
 						if (isModEnabled('salaries')) {
 							// TODO Permission to read daily rate to show value
 							$total_ht_by_line = price2num($tmpprojtime['amount'], 'MT');
-							if ($tmpprojtime['nblinesnull'] > 0) {
+							if (isset($tmpprojtime['nblinesnull']) && ($tmpprojtime['nblinesnull'] > 0)) {
 								$langs->load("errors");
 								$warning = $langs->trans("WarningSomeLinesWithNullHourlyRate", $conf->currency);
 							}
@@ -1679,7 +1679,7 @@ foreach ($listofreferent as $key => $value) {
 					if ($warning) {
 						print ' '.img_warning($warning);
 					}
-					if ($tmpprojtime['nblinesnull'] > 0) {
+					if (isset($tmpprojtime['nblinesnull']) && ($tmpprojtime['nblinesnull'] > 0)) {
 						if ($tmpprojtime['nbuserthmnull'] > 0) {
 							$title = $langs->trans("EnterUsersHourlyRateFirst");
 							print ' '.img_picto($title, "sync", '', 0, 0, 0, '', 'opacitymedium');
@@ -1747,7 +1747,7 @@ foreach ($listofreferent as $key => $value) {
 					if ($warning) {
 						print ' '.img_warning($warning);
 					}
-					if ($tmpprojtime['nblinesnull'] > 0) {
+					if (isset($tmpprojtime['nblinesnull']) && ($tmpprojtime['nblinesnull'] > 0)) {
 						if ($tmpprojtime['nbuserthmnull'] > 0) {
 							$title = $langs->trans("EnterUsersHourlyRateFirst");
 							print ' '.img_picto($title, "sync", '', 0, 0, 0, '', 'opacitymedium');
