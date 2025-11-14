47f4f0998eff99b12c7bb41f8907373f4862d4e8
Frédéric FRANCE <frederic34@users.noreply.github.com>
2025-11-14 20:37:01 +0100

    Update interface_50_modNotification_Notification.class.php

--- htdocs/core/triggers/interface_50_modNotification_Notification.class.php
+++ htdocs/core/triggers/interface_50_modNotification_Notification.class.php
@@ -4,7 +4,6 @@
  * Copyright (C) 2013-2014 Marcos García        <marcosgdf@gmail.com>
  * Copyright (C) 2022      Anthony Berton     	<anthony.berton@bb2a.fr>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
- * Copyright (C) 2025       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
