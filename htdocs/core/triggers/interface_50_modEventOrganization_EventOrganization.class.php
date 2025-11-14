752be01cd4378d5a2803b2cacc58d7797bc27eca
Frédéric FRANCE <frederic34@users.noreply.github.com>
2025-11-14 20:39:06 +0100

    Update interface_50_modEventOrganization_EventOrganization.class.php

--- htdocs/core/triggers/interface_50_modEventOrganization_EventOrganization.class.php
+++ htdocs/core/triggers/interface_50_modEventOrganization_EventOrganization.class.php
@@ -1,7 +1,7 @@
 <?php
-/* Copyright (C) 2005-2017	Laurent Destailleur 	  <eldy@users.sourceforge.net>
- * Copyright (C) 2021		    Florian Henry		        <florian.henry@scopen.fr>
- * Copyright (C) 2024		    MDW							        <mdeweerd@users.noreply.github.com>
+/* Copyright (C) 2005-2017	Laurent Destailleur 	<eldy@users.sourceforge.net>
+ * Copyright (C) 2021		Florian Henry		 	<florian.henry@scopen.fr>
+ * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2025       Frédéric France         <frederic.france@free.fr>
  *
  * This program is free software; you can redistribute it and/or modify
