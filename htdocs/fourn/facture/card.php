3d1029f40f1bcf4fce38a929e0c83038a3c06b44
MDW <mdeweerd@users.noreply.github.com>
2025-11-19 12:53:50 +0100

    FIX: Correct assignment to linkedObjectsIds  in card.php
    # FIX: Correct assignment to linkedObjectsIds  in card.php
    
    Modified the structure of linkedObjectsIds to include the rowid as a key to match the property type.

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -1156,7 +1156,7 @@ if (empty($reshook)) {
 					$objectsrc->fetch_thirdparty();
 
 					if (!empty($object->origin_type) && !empty($object->origin_id)) {
-						$object->linkedObjectsIds[$object->origin_type] = $object->origin_id;
+						$object->linkedObjectsIds[$object->origin_type][$object->rowid] = $object->origin_id;
 					}
 
 					// Add also link with order if object is reception
