d11a25d60b028e00f37d021bfeec32146549183a
tcesar <tcesar@albalogic.fr>
2025-11-19 19:45:31 +0100

    Fix Concat of PDF to invoices

--- htdocs/core/modules/facture/doc/pdf_octopus.modules.php
+++ htdocs/core/modules/facture/doc/pdf_octopus.modules.php
@@ -1180,8 +1180,8 @@ class pdf_octopus extends ModelePDFFactures
 				if (getDolGlobalInt('MAIN_PDF_ADD_TERMSOFSALE_INVOICE')) {
 					$termsofsalefilename = getDolGlobalString('MAIN_INFO_INVOICE_TERMSOFSALE');
 					$termsofsale = $conf->invoice->dir_output.'/'.$termsofsalefilename;
-					if (!empty($conf->mycompany->multidir_output[$object->entity ?? $conf->entity]) && !empty($mysoc->termsofsale)) {
-						$termsofsale = $conf->mycompany->multidir_output[$object->entity ?? $conf->entity].'/'.$mysoc->termsofsale;
+					if (!empty($conf->invoice->multidir_output[$object->entity ?? $conf->entity])) {
+						$termsofsale = $conf->invoice->multidir_output[$object->entity ?? $conf->entity].'/'.$termsofsalefilename;
 					}
 					if (file_exists($termsofsale) && is_readable($termsofsale)) {
 						$pagecount = $pdf->setSourceFile($termsofsale);
