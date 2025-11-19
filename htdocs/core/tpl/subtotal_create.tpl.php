a374f4e7b9544ed6ab2561fe8821a9688fa1522c
Charlène Benke <1179011+defrance@users.noreply.github.com>
2025-11-19 16:25:53 +0100

    Add 'fichinter' to element check in subtotal_create

--- htdocs/core/tpl/subtotal_create.tpl.php
+++ htdocs/core/tpl/subtotal_create.tpl.php
@@ -65,7 +65,7 @@ $page = $_SERVER["PHP_SELF"];
 
 if ($object->element == 'facture') {
 	$page .= '?facid=' . $object->id;
-} elseif (in_array($object->element, array('propal', 'commande', 'facturerec', 'shipping'))) {
+} elseif (in_array($object->element, array('propal', 'commande', 'facturerec', 'shipping', 'fichinter'))) {
 	$page .= '?id=' . $object->id;
 }
 
