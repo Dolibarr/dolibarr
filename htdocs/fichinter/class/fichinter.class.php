67392c95ed92a551ee84577d84b934fb926f25cb
Charlène Benke <1179011+defrance@users.noreply.github.com>
2025-11-23 12:45:15 +0100

    Add fk_element property to fichinter class
    necessary for up and down line

--- htdocs/fichinter/class/fichinter.class.php
+++ htdocs/fichinter/class/fichinter.class.php
@@ -102,6 +102,11 @@ class Fichinter extends CommonObject
 	public $class_element_line = 'FichinterLigne';
 
 	/**
+	 * @var string Field name with ID of parent key if this field has a parent
+	 */
+	public $fk_element = "fk_fichinter";
+	
+	/**
 	 * @var string String with name of icon for myobject. Must be the part after the 'object_' into object_myobject.png
 	 */
 	public $picto = 'intervention';
