7bea8b8fa318f984409b2a34d67749a660c3e1b3
Francis <162715304+le-reparateur@users.noreply.github.com>
2025-11-28 10:49:50 +0100

    Refactor subtotal line insertion logic

--- htdocs/expedition/list.php
+++ htdocs/expedition/list.php
@@ -429,15 +429,14 @@ if (empty($reshook)) {
 							}
 							// if we insert first line of sending we must insert comment before first line
 							if ($i == 0) {
-							$titleexped = dol_concatdesc("", $langs->trans("Order").': '.$expdCmdSrc->ref. ' - '. $langs->trans("Shipment").': '.$expd->ref.($expd->date_shipping ? ' - '.dol_print_date($expd->date_shipping, 'day') : '')."id".$id_sending);
-							$depth = 1;
-							$subtotal_options = array();
-							$restitle = $objecttmp->addSubtotalLine($langs, $titleexped, (int) $depth, $subtotal_options);
-							$rang++;
-							$TFactThirdNbLines[$expd->socid]++;
+								$titleexped = dol_concatdesc("", $langs->trans("Order").': '.$expdCmdSrc->ref. ' - '. $langs->trans("Shipment").': '.$expd->ref.($expd->date_shipping ? ' - '.dol_print_date($expd->date_shipping, 'day') : '')."id".$id_sending);
+								$depth = 1;
+								$subtotal_options = array();
+								$restitle = $objecttmp->addSubtotalLine($langs, $titleexped, (int) $depth, $subtotal_options);
+								$rang++;
+								$TFactThirdNbLines[$expd->socid]++;
 							}
 							
-
 							$result = $objecttmp->addline(
 								$desc,
 								$lines[$i]->subprice,
