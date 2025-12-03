5ee8f912c56d607a8b4cfe9a459ae2a738c270d8
Bahfir Abbes <bafbes@gmail.com>
2025-12-03 14:28:45 +0100

    Update invoice.php
    Fix location of hook print result

--- htdocs/takepos/invoice.php
+++ htdocs/takepos/invoice.php
@@ -1350,10 +1350,11 @@ if (empty($reshook)) {
 		if ($remaintopay <= 0 && getDolGlobalString('TAKEPOS_AUTO_PRINT_TICKETS') && $action != "history") {
 			$sectionwithinvoicelink .= '<script type="text/javascript">console.log("Emulate click on #buttonprint"); $("#buttonprint").click();</script>';
 		}
-	} else {
-		$sectionwithinvoicelink = $hookmanager->resPrint;
-	}
+	} 
+} else {
+	$sectionwithinvoicelink = $hookmanager->resPrint;
 }
+
 
 
 /*
