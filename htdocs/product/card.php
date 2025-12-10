1a8e1a38aec64615b4effea5e089b2d6a00599d7
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2025-12-10 11:41:45 +0100

    False Def Bom on product clone

--- htdocs/product/card.php
+++ htdocs/product/card.php
@@ -993,7 +993,8 @@ if (empty($reshook)) {
 
 						if (!$error && isModEnabled('bom') && $user->hasRight('bom', 'write')) {
 							$defbomidac = 0; // to avoid cloning same BOM twice
-							if (GETPOST('clone_defbom') && $object->fk_default_bom > 0) {
+							$clone_defbom = GETPOSTISSET('clone_defbom') ? 1 : 0;
+							if($clone_defbom && $object->fk_default_bom > 0) {
 								$bomstatic = new BOM($db);
 								$bomclone = $bomstatic->createFromClone($user, $object->fk_default_bom);
 								if ((int) $bomclone < 0) {
