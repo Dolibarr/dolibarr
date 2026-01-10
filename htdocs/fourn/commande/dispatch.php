0bf4f359504c6157c0d102a9a093fa078dcae321
Laurent Destailleur <eldy@destailleur.fr>
2026-01-10 17:10:37 +0100

    Update dispatch.php

--- htdocs/fourn/commande/dispatch.php
+++ htdocs/fourn/commande/dispatch.php
@@ -328,7 +328,7 @@ if ($action == 'dispatch' && $permissiontoreceive) {
 
 			if (getDolGlobalString('SUPPLIER_ORDER_CAN_UPDATE_BUYINGPRICE_DURING_RECEIPT')) {
 				if (!isModEnabled("multicurrency") && empty($conf->dynamicprices->enabled)) {
-					$dto = GETPOST("dto_".$reg[1].'_'.$reg[2]);
+					$dto = GETPOSTFLOAT("dto_".$reg[1].'_'.$reg[2]);
 					if (!empty($dto)) {
 						$unit_price = price2num((float) GETPOST("pu_".$reg[1]) * (100 - $dto) / 100, 'MU');
 					}
