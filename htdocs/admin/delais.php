392997af5c3cd51264ec7a287235989d4ae8363a
Lucas Marcouiller <lmarcouiller@dolicloud.com>
2026-01-15 09:56:34 +0100

    revert some changes

--- htdocs/admin/delais.php
+++ htdocs/admin/delais.php
@@ -161,25 +161,25 @@ $modules = array(
 
 $labelmeteo = array(0 => $langs->trans("No"), 1 => $langs->trans("Yes"), 2 => $langs->trans("OnMobileOnly"));
 
-if (!getDolGlobalInt("MAIN_DELAY_MEMBERS")) {
+if (!isset($conf->global->MAIN_DELAY_MEMBERS)) {
 	$conf->global->MAIN_DELAY_MEMBERS = 0; // Must be same value than into conf.class.php
 }
-if (!getDolGlobalInt("MAIN_DELAY_ACTIONS_TODO")) {
+if (!isset($conf->global->MAIN_DELAY_ACTIONS_TODO)) {
 	$conf->global->MAIN_DELAY_ACTIONS_TODO = 7; // Must be same value than into conf.class.php
 }
-if (!getDolGlobalInt("MAIN_DELAY_ORDERS_TO_PROCESS")) {
+if (!isset($conf->global->MAIN_DELAY_ORDERS_TO_PROCESS)) {
 	$conf->global->MAIN_DELAY_ORDERS_TO_PROCESS = 2;
 }
-if (!getDolGlobalInt("MAIN_DELAY_SUPPLIER_ORDERS_TO_PROCESS")) {
+if (!isset($conf->global->MAIN_DELAY_SUPPLIER_ORDERS_TO_PROCESS)) {
 	$conf->global->MAIN_DELAY_SUPPLIER_ORDERS_TO_PROCESS = 7;
 }
-if (!getDolGlobalInt("MAIN_DELAY_ORDERS_TO_PROCESS")) {
+if (!isset($conf->global->MAIN_DELAY_ORDERS_TO_PROCESS)) {
 	$conf->global->MAIN_DELAY_ORDERS_TO_PROCESS = 2;
 }
-if (!getDolGlobalInt("MAIN_DELAY_ORDERS_TO_PROCESS")) {
+if (!isset($conf->global->MAIN_DELAY_ORDERS_TO_PROCESS)) {
 	$conf->global->MAIN_DELAY_ORDERS_TO_PROCESS = 2;
 }
-if (!getDolGlobalInt("MAIN_DELAY_ORDERS_TO_PROCESS")) {
+if (!isset($conf->global->MAIN_DELAY_ORDERS_TO_PROCESS)) {
 	$conf->global->MAIN_DELAY_ORDERS_TO_PROCESS = 2;
 }
 
