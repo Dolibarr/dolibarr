382bad7135e95b18fdb06ef581335dd908530622
Charlène Benke <1179011+defrance@users.noreply.github.com>
2026-01-16 15:37:23 +0100

    FIX : Implicitly nullable parameters are deprecated
    generate warning on first install

--- htdocs/includes/mobiledetect/mobiledetectlib/Mobile_Detect.php
+++ htdocs/includes/mobiledetect/mobiledetectlib/Mobile_Detect.php
@@ -886,7 +886,7 @@ class Mobile_Detect
      *                          from the $headers array instead.
      */
     public function __construct(
-        array $headers = null,
+        ?array $headers = null,
         $userAgent = null
     ) {
         $this->setHttpHeaders($headers);
