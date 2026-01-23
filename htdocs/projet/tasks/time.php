45f94311ce221cfe78f4505155d522216da77fcf
Joachim Kueter <priojk@users.noreply.github.com>
2026-01-23 14:21:43 +0100

    Minor change
    to trigger checks again

--- htdocs/projet/tasks/time.php
+++ htdocs/projet/tasks/time.php
@@ -10,6 +10,7 @@
  * Copyright (C) 2024-2025	MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024		Vincent de Grandpré		<vincent@de-grandpre.quebec>
  * Copyright (C) 2024		Solution Libre SAS		<contact@solution-libre.fr>
+ * Copyright (C) 2026		Joachim Kueter			<git-jk@bloxera.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
