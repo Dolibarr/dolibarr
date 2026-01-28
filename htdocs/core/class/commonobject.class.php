e27cbaa073bd2bd80af543ed5cd451c7ff983317
Laurent Destailleur <eldy@destailleur.fr>
2026-01-28 14:46:20 +0100

    Update commonobject.class.php

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -6618,6 +6618,7 @@ abstract class CommonObject
 				}
 				// use geo sql fonction to read as text
 				if (!empty($extrafields->attributes[$this->table_element]['type'][$name]) && in_array($extrafields->attributes[$this->table_element]['type'][$name], array('point', 'multipts', 'linestrg', 'polygon'))) {
+					// TODO Add an abstraction method in the database driver
 					$sql .= ", ST_AsWKT(".$name.") as ".$name;
 				}
 			}
