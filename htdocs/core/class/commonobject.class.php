75664dd9ccca2ad26c95e9a2889e890c57a97238
atm-jonathan <jonathan.lescaut@atm-consulting.fr>
2026-01-28 11:00:58 +0100

    prevent ST_AsWKT()

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -6617,8 +6617,7 @@ abstract class CommonObject
 					$sql .= ", ".$name;
 				}
 				// use geo sql fonction to read as text
-				if (empty($extrafields->attributes[$this->table_element]['type'][$name]) || in_array($extrafields->attributes[$this->table_element]['type'][$name], array('point', 'multipts', 'linestrg', 'polygon'))) {
-					// TODO Add an abstraction method in the database driver
+				if (!empty($extrafields->attributes[$this->table_element]['type'][$name]) && in_array($extrafields->attributes[$this->table_element]['type'][$name], array('point', 'multipts', 'linestrg', 'polygon'))) {
 					$sql .= ", ST_AsWKT(".$name.") as ".$name;
 				}
 			}
