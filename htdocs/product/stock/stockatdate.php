12dc68f0a9fba120cbbba064c4129ecc30fc9aed
Noé Cendrier <noe.cendrier@altairis.fr>
2026-02-06 11:35:20 +0100

    FIX: bad argument for print_liste_field_titre

--- htdocs/product/stock/stockatdate.php
+++ htdocs/product/stock/stockatdate.php
@@ -599,7 +599,7 @@ if ($ext == 'csv') {
 		$tooltiptext = $langs->trans("QtyAtDate").' x '.$langs->trans("SellingPrice").' ('.$langs->trans("Currently").')';
 		print_liste_field_titre("EstimatedStockValueSell", $_SERVER["PHP_SELF"], "", '', $param, '', $sortfield, $sortorder, 'right ', $tooltiptext, 1);
 		$tooltiptext = $langs->trans("MovementsSinceDate");
-		print_liste_field_titre($langs->trans("since"), $_SERVER["PHP_SELF"], '', '', $param, '', '', '', 'right ', $tooltiptext, 1);
+		print_liste_field_titre('since', $_SERVER["PHP_SELF"], '', '', $param, '', '', '', 'right ', $tooltiptext, 1);
 		print_liste_field_titre('CurrentStock', $_SERVER["PHP_SELF"], $fieldtosortcurrentstock, '', $param, '', $sortfield, $sortorder, 'right ');
 	}
 
