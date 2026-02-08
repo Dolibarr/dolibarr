d6fe39ba6098a64dc682284dcb235d11f2aae677
Yves LE GUENNEC <yveslg76@gmail.com>
2026-02-08 17:52:25 +0100

    FIX #37147 Limit account fetch to active chart of accounts in PDF exports
    When multiple chart of accounts have the same account number, the PDF
    exports (balance and ledger) could retrieve the wrong account label.

--- htdocs/core/modules/accountancy/doc/pdf_balance.modules.php
+++ htdocs/core/modules/accountancy/doc/pdf_balance.modules.php
@@ -303,7 +303,7 @@ class pdf_balance extends ModelePdfAccountancy
 		$groupDebit = $groupCredit = $totalDebit = $totalCredit = 0;
 		for ($i = 0; $i < $nblines; $i++) {
 			$accountingAccount = new AccountingAccount($this->db);
-			$accountingAccount->fetch(0, $object->lines[$i]->numero_compte);
+			$accountingAccount->fetch(0, $object->lines[$i]->numero_compte, true);
 
 			// Init the first account group
 			if (empty($accountGroup)) {
