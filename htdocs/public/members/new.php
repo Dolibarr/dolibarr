95a3db4eca62539a936736c49f3e2412da54076f
MDW <mdeweerd@users.noreply.github.com>
2026-02-10 23:50:44 +0100

    Fix: Missing initialisations members/new.php
    Following a suppression of assignments, the variables disabledphy and disabledmor were undefined.

--- htdocs/public/members/new.php
+++ htdocs/public/members/new.php
@@ -8,7 +8,7 @@
  * Copyright (C) 2018       Alexandre Spangaro      <aspangaro@open-dsi.fr>
  * Copyright (C) 2021       Waël Almoman            <info@almoman.com>
  * Copyright (C) 2022       Udo Tamm                <dev@dolibit.de>
- * Copyright (C) 2024-2025	MDW						<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2024-2026	MDW						<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -248,7 +248,7 @@ if (empty($reshook) && getDolGlobalInt("MEMBER_SEARCH_MEMBER_PUBLIC_FORM_CREATE"
 		}
 	}
 
-	if (!$memberfound && GETPOST("morphy") == 'mor' && GETPOSTISSET("societe") ) {
+	if (!$memberfound && GETPOST("morphy") == 'mor' && GETPOSTISSET("societe")) {
 		$sql = "SELECT a.rowid as id";
 		$sql .= " FROM ".MAIN_DB_PREFIX."adherent as a";
 		$sql .= " JOIN ".MAIN_DB_PREFIX."societe as s";
@@ -430,7 +430,7 @@ if (empty($reshook) && $action == 'add') {	// Test on permission not required he
 		$adh->birth       = $birthday;
 		$adh->phone   = GETPOST('phone');
 		$adh->phone_perso = GETPOST('phone_perso');
-		$adh->phone_mobile= GETPOST('phone_mobile');
+		$adh->phone_mobile = GETPOST('phone_mobile');
 
 		$adh->ip = getUserRemoteIP();
 
@@ -758,6 +758,8 @@ if (getDolGlobalString('MEMBER_SKIP_TABLE') || getDolGlobalString('MEMBER_NEWFOR
 		}
 
 		print '<tr><td class="fieldrequired">'.$langs->trans("MemberNature")."</td><td>\n";
+		$disabledphy = '';
+		$disabledmor = '';
 		//$disabledphy = ($checkednature == "mor" ? ' disabled="disabled"' : '');
 		//$disabledmor = ($checkednature == "phy" ? ' disabled="disabled"' : '');
 		print '<span id="spannature1" class="nonature-back spannature paddinglarge marginrightonly"><label for="phisicalinput" class="valignmiddle">'.$morphys["phy"].'<input id="phisicalinput" class="flat checkforselect marginleftonly valignmiddle" type="radio" name="morphy" value="phy"'.($checkednature == "phy" ? ' checked="checked"' : '').$disabledphy.'></label></span>';
@@ -842,7 +844,7 @@ if (getDolGlobalString('MEMBER_SKIP_TABLE') || getDolGlobalString('MEMBER_NEWFOR
 				});
 			</script>';
 		}
-			print '</td></tr>'."\n";
+		print '</td></tr>'."\n";
 	} else {
 		//print $morphys[$conf->global->MEMBER_NEWFORM_FORCEMORPHY];
 		print '<input type="hidden" id="morphy" name="morphy" value="' . getDolGlobalString('MEMBER_NEWFORM_FORCEMORPHY').'">';
@@ -1209,7 +1211,7 @@ if (getDolGlobalString('MEMBER_SKIP_TABLE') || getDolGlobalString('MEMBER_NEWFOR
 		$minimumamountbytype_json = json_encode($minimumamountbytype);
 		$amountbytype_json = json_encode($amountbytype);
 		$amountformuladescriptionbytype_json = json_encode($amountformuladescriptionbytype);
-		$currencysymbol =$langs->getCurrencySymbol($conf->currency);
+		$currencysymbol = $langs->getCurrencySymbol($conf->currency);
 
 		print '<script>
 		jQuery(function($) {
