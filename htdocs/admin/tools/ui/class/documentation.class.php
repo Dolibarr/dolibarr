04f0f047f87be49ab579371419e32b27e3fc8dc9
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2026-02-13 13:34:23 +0100

    Fix phan

--- htdocs/admin/tools/ui/class/documentation.class.php
+++ htdocs/admin/tools/ui/class/documentation.class.php
@@ -368,7 +368,8 @@ class Documentation
 	/**
 	 *    Recursive function to set Menu
 	 *
-	 * @param array{url?: string, icon?: string, summary?: array<string,string>, submenu?: array<string,array>} $menu Menu entry or submenu
+	 * @param array $menu Menu entry or submenu
+	 * @phan-var-force array<string, array{url?: string, icon?: string, summary?: array<string,string>, submenu?: array<string,array>}> $menu
 	 * @param int   $level level of menu
 	 * @return void
 	 */
