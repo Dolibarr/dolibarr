15f01cadeca2883df39493258b94d720951186f7
Alexandre SPANGARO <alexandre.spangaro@gmail.com>
2026-02-13 06:30:25 +0100

    FIX Societe - Wrong control on update of VAT Intra

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -8,7 +8,7 @@
  * Copyright (C) 2008       Patrick Raguin          <patrick.raguin@auguria.net>
  * Copyright (C) 2010-2018  Juanjo Menent           <jmenent@2byte.es>
  * Copyright (C) 2013       Florian Henry           <florian.henry@open-concept.pro>
- * Copyright (C) 2013-2023  Alexandre Spangaro      <aspangaro@open-dsi.fr>
+ * Copyright (C) 2013-2026  Alexandre Spangaro      <alexandre@inovea-conseil.com>
  * Copyright (C) 2013       Peter Fontaine          <contact@peterfontaine.fr>
  * Copyright (C) 2014-2015  Marcos García           <marcosgdf@gmail.com>
  * Copyright (C) 2015       Raphaël Doursenaud      <rdoursenaud@gpcsolutions.fr>
@@ -1368,7 +1368,7 @@ class Societe extends CommonObject
 		$this->supplier_order_min_amount = price2num($this->supplier_order_min_amount);
 
 		$this->tva_assuj			= trim($this->tva_assuj);
-		$this->tva_intra			= dol_sanitizeFileName($this->tva_intra, '');
+		$this->tva_intra			= trim($this->tva_intra);
 		$this->vat_reverse_charge	= empty($this->vat_reverse_charge) ? '0' : '1';
 		if (empty($this->status)) {
 			$this->status = 0;
