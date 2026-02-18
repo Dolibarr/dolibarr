78c17eda31da2c9f870d58f002853e74cdc5c9e2
splohmer <113967431+splohmer@users.noreply.github.com>
2026-02-18 06:37:14 +0100

    typo in comment

--- htdocs/core/class/fileupload.class.php
+++ htdocs/core/class/fileupload.class.php
@@ -396,7 +396,7 @@ class FileUpload
 		// Remove path information and dots around the filename, to prevent uploading
 		// into different directories or replacing hidden system files.
 		$file_name = basename(dol_sanitizeFileName($name));
-		$file_name = preg_replace('/ {2,}/', ' ', $file_name); // replaces multiple spaces into one space linke the upload flow via input field
+		$file_name = preg_replace('/ {2,}/', ' ', $file_name); // replaces multiple spaces into one space like the upload flow via input field
 		// Add missing file extension for known image types:
 		$matches = array();
 		if (strpos($file_name, '.') === false && preg_match('/^image\/(gif|jpe?g|png)/', $type, $matches)) {
