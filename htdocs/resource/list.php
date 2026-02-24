883ddefa754c8eda0dc45229341e57f420c6914a
atm-nicolasV <nicolas.vidal@atm-consulting.fr>
2026-02-24 10:30:35 +0100

    add microsoft exchange oauth

--- htdocs/resource/list.php
+++ htdocs/resource/list.php
@@ -168,7 +168,7 @@ include DOL_DOCUMENT_ROOT.'/core/tpl/extrafields_list_array_fields.tpl.php';
 
 $object->fields = dol_sort_array($object->fields, 'position');
 $arrayfields = dol_sort_array($arrayfields, 'position');
-'@phan-var-force array<string,array{label:string,checked?:int<0,1>,position?:int,help?:string}> $arrayfields';// dol_sort_array looses type for Phan
+'@phan-var-force array<string,array{label:string,checked?:int<0,1>,position?:int,help?:string}> $arrayfields'; // dol_sort_array looses type for Phan
 
 include DOL_DOCUMENT_ROOT.'/core/actions_changeselectedfields.inc.php';
 
@@ -337,7 +337,7 @@ if (!getDolGlobalInt('MAIN_DISABLE_FULL_SCANLIST')) {
 		dol_print_error($db);
 	}
 
-	if (($page * $limit) > $nbtotalofrecords) {// if total resultset is smaller than the paging size (filtering), goto and load page 0
+	if (($page * $limit) > $nbtotalofrecords) { // if total resultset is smaller than the paging size (filtering), goto and load page 0
 		$page = 0;
 		$offset = 0;
 	}
@@ -611,6 +611,7 @@ while ($i < $imaxinloop) {
 	$objectstatic->url = $obj->url;
 
 	print '<tr data-rowid="'.$obj->rowid.'" class="oddeven">';
+
 	// Action column
 	if (getDolGlobalString('MAIN_CHECKBOX_LEFT_COLUMN')) {
 		print '<td class="nowrap center">';
