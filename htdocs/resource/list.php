6fdd044d3e1067abab7580fee2f116f8676f7f6f
Anthony Berton <anthony.berton@bb2a.fr>
2026-02-25 14:14:22 +0100

    FIX - Twice the check in resource list column

--- htdocs/resource/list.php
+++ htdocs/resource/list.php
@@ -626,22 +626,6 @@ while ($i < $imaxinloop) {
 		}
 	}
 
-	// Action column
-	if (getDolGlobalString('MAIN_CHECKBOX_LEFT_COLUMN')) {
-		print '<td class="nowrap center">';
-		if ($massactionbutton || $massaction) {   // If we are in select mode (massactionbutton defined) or if we have already selected and sent an action ($massaction) defined
-			$selected = 0;
-			if (in_array($obj->rowid, $arrayofselected)) {
-				$selected = 1;
-			}
-			print '<input id="cb'.$obj->rowid.'" class="flat checkforselect" type="checkbox" name="toselect[]" value="'.$obj->rowid.'"'.($selected ? ' checked="checked"' : '').'>';
-		}
-		print '</td>';
-		if (!$i) {
-			$totalarray['nbfield']++;
-		}
-	}
-
 	if (!empty($arrayfields['t.ref']['checked'])) {
 		print '<td>'.$objectstatic->getNomUrl(5).'</td>';
 		if (!$i) {
