807f71683af5a4b443f10efc4b39f3ecd322d664
Vincent de Grandpré <vincent@de-grandpre.quebec>
2026-02-26 19:10:43 -0500

    pass over loaded third party in thirdpartystatic

--- htdocs/emailcollector/class/emailcollector.class.php
+++ htdocs/emailcollector/class/emailcollector.class.php
@@ -3809,7 +3809,7 @@ class EmailCollector extends CommonObject
 
 								'actionparam' =>  $operation['actionparam'],
 
-								'thirdpartyid' => $thirdpartyid,
+								'thirdpartyid' => !$thirdpartyid?$thirdpartystatic->id:0,
 								'objectid' => $objectid,
 								'objectemail' => $objectemail,
 
