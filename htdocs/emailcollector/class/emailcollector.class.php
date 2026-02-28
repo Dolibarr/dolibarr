c1570916fa42ade4b26e1347f6c7dfc315c16834
Laurent Destailleur <eldy@destailleur.fr>
2026-02-28 12:36:29 +0100

    Refactor thirdpartyid assignment for clarity

--- htdocs/emailcollector/class/emailcollector.class.php
+++ htdocs/emailcollector/class/emailcollector.class.php
@@ -3809,7 +3809,7 @@ class EmailCollector extends CommonObject
 
 								'actionparam' =>  $operation['actionparam'],
 
-								'thirdpartyid' => !$thirdpartyid?$thirdpartystatic->id:$thirdpartyid,
+								'thirdpartyid' => ($thirdpartyid ? $thirdpartyid : $thirdpartystatic->id),
 								'objectid' => $objectid,
 								'objectemail' => $objectemail,
 
