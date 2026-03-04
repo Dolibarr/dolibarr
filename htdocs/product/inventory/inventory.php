7337e7e25076a2b35e3d0a4e92419b10bec8bb77
vmaury <vmaury@vmaury-Lafite-Pro-16-AMD>
2026-03-04 12:31:18 +0100

    Fix Inventory bugs #33207 and #33292 : when click on "validate and generate movements" quantities not still recorded are not recorded

--- htdocs/product/inventory/inventory.php
+++ htdocs/product/inventory/inventory.php
@@ -1204,7 +1204,7 @@ print '</form>';
 
 print '<script type="text/javascript">
 					$(document).ready(function() {
-						
+
                         $(".paginationnext:last").click(function(e){
                             var form = $("#formrecord");
    							var actionURL = "'.$_SERVER['PHP_SELF'].'?id='.$object->id.'&page='.($page).$paramwithsearch.'";
@@ -1219,7 +1219,7 @@ print '<script type="text/javascript">
     					});
 
 
-                         $(".paginationprevious:last").click(function(e){
+                        $(".paginationprevious:last").click(function(e){
                             var form = $("#formrecord");
    							var actionURL = "'.$_SERVER['PHP_SELF'].'?id='.$object->id.'&page='.($page).$paramwithsearch.'";
    							$.ajax({
@@ -1230,9 +1230,9 @@ print '<script type="text/javascript">
            				 	window.location.href = "'.$_SERVER['PHP_SELF'].'?id='.$object->id.'&page='.($page - 1).$paramwithsearch.'";
        					 	}});
 							return false;
-						 });
+						});
 
-                          $("#idbuttonmakemovementandclose").click(function(e){
+                        $("#idbuttonmakemovementandclose").click(function(e){
                             var form = $("#formrecord");
    							var actionURL = "'.$_SERVER['PHP_SELF'].'?id='.$object->id.'&page='.($page).$paramwithsearch.'";
    							$.ajax({
@@ -1243,7 +1243,7 @@ print '<script type="text/javascript">
            				 	window.location.href = "'.$_SERVER['PHP_SELF'].'?id='.$object->id.'&page='.($page - 1).$paramwithsearch.'&action=record";
        					 	}});
 							return false;
-						 });
+						});
 					});
 </script>';
 
