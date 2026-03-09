a500feb8d64033e6a425f69cfa3f8ba8afd4481b
Charlène Benke <1179011+defrance@users.noreply.github.com>
2026-03-09 13:04:23 +0100

    Fix SQL query formatting in card-rec.php

--- htdocs/fichinter/card-rec.php
+++ htdocs/fichinter/card-rec.php
@@ -794,7 +794,7 @@ if ($action == 'create') {
 	} else {
 		// List mode
 		$param = '';
-	
+
 		$sql = "SELECT f.rowid as id, s.nom as name, s.rowid as socid, f.title,";
 		$sql .= " f.duree, f.fk_contrat, f.fk_projet as fk_project, f.frequency, f.nb_gen_done, f.nb_gen_max,";
 		$sql .= " f.date_last_gen, f.date_when, f.datec, f.status";
