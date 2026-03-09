c005fe46edd2d8d346517f15877d03806871bd0f
jpb <jean-pascal.boudet@atm-consulting>
2026-03-09 10:32:59 +0100

    retour

--- htdocs/core/tpl/objectline_edit.tpl.php
+++ htdocs/core/tpl/objectline_edit.tpl.php
@@ -306,7 +306,7 @@ $coldisplay++;
 	<?php
 	// Shippable Status (Empty cell for edit mode to keep column alignment)
 	if ($object->element == 'commande' && isModEnabled('stock') && isModEnabled('shipping') && getDolGlobalString('ORDER_ENABLE_SHIPPABLE_ICON_ON_CARD') && ($object->status > 0 && $object->status < 3)) {
-	print '<td class="linecolstock center">';
+		print '<td class="linecolstock center">';
 		print '&nbsp;';
 		print '</td>';
 	}
