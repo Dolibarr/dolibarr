9fd8413ba9fa2294871853741aa1c228d30f0478
jpb <jean-pascal.boudet@atm-consulting>
2026-03-10 11:13:21 +0100

    missed element

--- htdocs/commande/list.php
+++ htdocs/commande/list.php
@@ -3014,6 +3014,7 @@ while ($i < $imaxinloop) {
 						// On ne remonte plus le détail textwarning, mais on garde l’icône d’avertissement
 						print $form->textwithtooltip('', $langs->trans("NotEnoughForAllOrders"), 2, 1, img_picto('', 'error', '', 0, 0, 0, '', 2), '', 2);
 					}
+				}
 			}
 			print '</td>';
 			if (!$i) {
