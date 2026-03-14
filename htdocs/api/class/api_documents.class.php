1a308814aebda953db2e22f086e9a0f46a31457e
Laurent Magnin <laurent1magnin@gmail.com>
2026-03-14 19:56:55 +0100

    Update api_documents.class.php

--- htdocs/api/class/api_documents.class.php
+++ htdocs/api/class/api_documents.class.php
@@ -8,6 +8,7 @@
  * Copyright (C) 2025		MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2025		William Mead			<william@m34d.com>
  * Copyright (C) 2025-2026	Charlene Benke			<charlene@patas-monkey.com>
+ * Copyright (C) 2026		Laurent Magnin			<laurent.magnin@evarisk.com>
  *
  * This program is free software you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
