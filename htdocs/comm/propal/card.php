24ae86a7a118ff08f2a5d438c867d8c8c4a1dc4e
Laurent Destailleur <eldy@destailleur.fr>
2026-03-15 12:46:58 +0100

    Refactor proposal closing and document generation logic
    Reload the proposal object after closing and before generating the document. Removed the redundant fetch call and the header redirection.

--- htdocs/comm/propal/card.php
+++ htdocs/comm/propal/card.php
@@ -372,6 +372,9 @@ if (empty($reshook)) {
 		if ($result > 0 && getDolGlobalString('PROPAL_SKIP_ACCEPT_REFUSE')) {
 			$result = $object->closeProposal($user, $object::STATUS_SIGNED);
 		}
+
+		$ret = $object->fetch($id); // Reload to get new records
+
 		if ($result >= 0) {
 			if (!getDolGlobalString('MAIN_DISABLE_PDF_AUTOUPDATE')) {
 				$outputlangs = $langs;
@@ -387,15 +390,12 @@ if (empty($reshook)) {
 					$outputlangs->setDefaultLang($newlang);
 				}
 				$model = $object->model_pdf;
-				$ret = $object->fetch($id); // Reload to get new records
 				if ($ret > 0) {
 					$object->fetch_thirdparty();
 				}
 
 				$object->generateDocument($model, $outputlangs, $hidedetails, $hidedesc, $hideref);
 			}
-			header('Location: ' . $_SERVER["PHP_SELF"] . '?id=' . $object->id);
-			exit();
 		} else {
 			$langs->load("errors");
 			if (count($object->errors) > 0) {
