36feaf406b6aea0bee5bdb30d426f885b93325a6
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2026-03-15 10:25:50 +0100

    Fix doc php stan

--- dev/tools/phan/config.php
+++ dev/tools/phan/config.php
@@ -346,6 +346,7 @@ return [
 		'htdocs/includes/',
 		'htdocs/install/doctemplates/websites/',
 		'htdocs/core/class/lessc.class.php', // External library
+		'htdocs/admin/tools/ui/',
 		PHAN_DIR . '/stubs/',
 	],
 	//'exclude_file_regex' => '@^vendor/.*/(tests?|Tests?)/@',
@@ -354,7 +355,7 @@ return [
 		// mymodule seen in cti, but not in git.
 		.'|htdocs/custom/.*'  // Ignore all custom modules @phpstan-ignore-line
 		.'|htdocs/.*/canvas/.*/tpl/.*.tpl.php'  // @phpstan-ignore-line
-		.'|htdocs/admin/tools/ui/(?!class/documentation\.class\.php).*'  // @phpstan-ignore-line
+		.'|htdocs/admin/tools/ui/.*'  // @phpstan-ignore-line
 		//.'|htdocs/modulebuilder/template/.*'  // @phpstan-ignore-line
 		// Included as stub (better analysis)
 		.'|htdocs/includes/nusoap/.*'  // @phpstan-ignore-line
