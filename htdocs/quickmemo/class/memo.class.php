3a2b5ce4b5dbaf0a162969092795ac218572a2cf
Laurent Destailleur <eldy@destailleur.fr>
2026-03-16 09:03:52 +0100

    Update memo.class.php

--- htdocs/quickmemo/class/memo.class.php
+++ htdocs/quickmemo/class/memo.class.php
@@ -381,22 +381,6 @@ class Memo extends CommonObject
 			$this->setErrorsFromObject($object);
 		}
 
-		if (!$error) {
-			// copy internal contacts
-			if ($this->copy_linked_contact($object, 'internal') < 0) {
-				$error++;
-			}
-		}
-
-		if (!$error) {
-			// copy external contacts if same company
-			if (!empty($object->socid) && ((property_exists($this, 'fk_soc') && ($this->fk_soc == $object->socid)) || (property_exists($this, 'socid') && ($this->socid == $object->socid)))) {	// @phpstan-ignore-line
-				if ($this->copy_linked_contact($object, 'external') < 0) {
-					$error++;
-				}
-			}
-		}
-
 		unset($object->context['createfromclone']);
 
 		// End
