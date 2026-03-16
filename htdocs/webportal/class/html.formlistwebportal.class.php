c0c4b6049429c48ae5bae031b2bbb4a346939a15
Charlène Benke <1179011+defrance@users.noreply.github.com>
2026-03-16 10:15:34 +0100

    Update html.formlistwebportal.class.php

--- htdocs/webportal/class/html.formlistwebportal.class.php
+++ htdocs/webportal/class/html.formlistwebportal.class.php
@@ -662,7 +662,9 @@ class FormListWebPortal
 	 */
 	public function printValue($field_key, $field_spec, &$record, $i, &$totalarray)
 	{
-		global $conf;
+		global $conf, $hookmanager;
+
+		$context = Context::getInstance();
 
 		$out = '';
 		if (is_object($this->object)) {
@@ -705,7 +707,15 @@ class FormListWebPortal
 						}
 					}
 				} else {
-					$out = $this->form->showOutputFieldForObject($this->object, $field_spec, $field_key, $this->object->$field_key, '');
+					// Add fields from hooks
+					$parameters = array();
+					$reshook = $hookmanager->executeHooks('printvaluelist', $parameters, $context);
+					if (empty($hookmanager->resPrint)) {
+						$out = $this->form->showOutputFieldForObject($this->object, $field_spec, $field_key, $this->object->$field_key, '');
+					} else {
+						$out = $hookmanager->resPrint;
+						$hookmanager->resPrint = '';
+					}
 				}
 			}
 
