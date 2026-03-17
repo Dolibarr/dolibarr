0fe42ccbe06abd1fb7c9617af6f4cf2e04b8fab8
Pascal <sarl@hubrecht.info>
2026-03-17 12:48:58 +0100

    Fix syslog configuration

--- htdocs/core/modules/syslog/mod_syslog_syslog.php
+++ htdocs/core/modules/syslog/mod_syslog_syslog.php
@@ -86,7 +86,7 @@ class mod_syslog_syslog extends LogHandler
 	{
 		global $langs;
 
-		$facility = constant(getDolGlobalString('SYSLOG_FACILITY'));
+		$facility = defined(getDolGlobalString('SYSLOG_FACILITY')) ? constant(getDolGlobalString('SYSLOG_FACILITY')) : null;
 
 		if ($facility) {
 			// Only LOG_USER supported on Windows
@@ -117,7 +117,7 @@ class mod_syslog_syslog extends LogHandler
 			return; // Global option to disable output of this handler
 		}
 
-		if (getDolGlobalString('SYSLOG_FACILITY')) {  // Example LOG_USER
+		if (getDolGlobalString('SYSLOG_FACILITY') && defined(getDolGlobalString('SYSLOG_FACILITY'))) {  // Example LOG_USER
 			$facility = constant($conf->global->SYSLOG_FACILITY);
 		} else {
 			$facility = constant('LOG_USER');
