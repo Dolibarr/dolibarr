d508288caedb274ec291161e3360c8316794f95d
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-03-17 14:22:14 +0100

    refactor(notification): remove contract-specific template branch

--- htdocs/core/class/notify.class.php
+++ htdocs/core/class/notify.class.php
@@ -1289,11 +1289,7 @@ class Notify
 				if (!empty($mailTemplateLabel)) {
 					include_once DOL_DOCUMENT_ROOT.'/core/class/html.formmail.class.php';
 					$formmail = new FormMail($this->db);
-					if ($object_type == 'contract') {
-						$emailTemplate = $formmail->getEMailTemplate($this->db, $object_type, $user, $outputlangs, 0, 1, $mailTemplateLabel);
-					} else {
-						$emailTemplate = $formmail->getEMailTemplate($this->db, $object_type.'_send', $user, $outputlangs, 0, 1, $mailTemplateLabel);
-					}
+					$emailTemplate = $formmail->getEMailTemplate($this->db, $object_type.'_send', $user, $outputlangs, 0, 1, $mailTemplateLabel);
 				}
 				if (!empty($mailTemplateLabel) && is_object($emailTemplate) && $emailTemplate->id > 0) {
 					if (property_exists($object, 'thirdparty')) {
