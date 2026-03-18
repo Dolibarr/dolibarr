9e29e576566766d4c49d6ec29937554deb03e664
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-03-18 14:28:26 +0100

    Update stocktransfer_card.php

--- htdocs/product/stock/stocktransfer/stocktransfer_card.php
+++ htdocs/product/stock/stocktransfer/stocktransfer_card.php
@@ -2,7 +2,7 @@
 /* Copyright (C) 2017 		Laurent Destailleur  	<eldy@users.sourceforge.net>
  * Copyright (C) 2024-2025	MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024-2025  Frédéric France         <frederic.france@free.fr>
- * Copyright (C) 2025       Pierre Ardoin           <developpeur@lesmetiersdubatiment.fr>
+ * Copyright (C) 2025-2026       Pierre Ardoin           <developpeur@lesmetiersdubatiment.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
