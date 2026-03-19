9415f7aa51a54d575827f3bf4ec1c43316d04eef
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-03-19 15:20:36 +0100

    feat(stocktransfer): add quick add entry for stock transfers

--- htdocs/core/modules/modStockTransfer.class.php
+++ htdocs/core/modules/modStockTransfer.class.php
@@ -102,11 +102,9 @@ class modStockTransfer extends DolibarrModules
 			),
 			// Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context to 'all'
 			'hooks' => array(
-				//   'data' => array(
-				//       'hookcontext1',
-				//       'hookcontext2',
-				//   ),
-				//   'entity' => '0',
+				'data' => array(
+					'stocktransfercard',
+				),
 			),
 			// Set this to 1 if features of module are opened to external users
 			'moduleforexternal' => 0,
