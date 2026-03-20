e9284db1df33749f704a05d2a9a7fabceb12611a
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-03-20 12:13:30 +0100

    admin: show stock transfer mail template type when module enabled

--- htdocs/admin/mails_templates.php
+++ htdocs/admin/mails_templates.php
@@ -242,7 +242,7 @@ if (isModEnabled("shipping")) {
 	$elementList['shipping_send'] = img_picto('', 'dolly', 'class="pictofixedwidth"').dol_escape_htmltag($langs->trans('MailToSendShipment'));
 	$elementList['delivery_send'] = img_picto('', 'dolly', 'class="pictofixedwidth"').dol_escape_htmltag($langs->trans('MailToSendDelivery'));
 }
-if (isModEnabled('stocktransfer') && $user->hasRight('stocktransfer', 'stocktransfer', 'read')) {
+if (isModEnabled('stocktransfer')) {
 	$elementList['stocktransfer_send'] = img_picto('', 'dolly', 'class="pictofixedwidth"').dol_escape_htmltag($langs->trans('MailToSendStockTransfer'));
 }
 if (isModEnabled("reception")) {
