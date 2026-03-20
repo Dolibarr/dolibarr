a36b1b843195495b0d16d45bc3fe0fdb7da38d3a
Laurent Destailleur <eldy@destailleur.fr>
2026-03-20 17:22:33 +0100

    Fix typo in loadRights method call

--- htdocs/core/ajax/onlineSign.php
+++ htdocs/core/ajax/onlineSign.php
@@ -303,7 +303,8 @@ if ($action == "importSignature") {
 						//customer is not a user !?! so could we use same user as validation ?
 						$user = new User($db);
 						$user->fetch($object->user_validation_id);
-						$user-loadRights();
+						$user->loadRights();
+						
 						$object->context = array('closedfromonlinesignature' => 'closedfromonlinesignature');
 						$result = $object->call_trigger('PROPAL_CLOSE_SIGNED', $user);
 						if ($result < 0) {
