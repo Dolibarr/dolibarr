fc20f9d18eba5db2a9cb5d4a1488ae6d5191b448
Rikard Bosnjakovic <bos@ribit.se>
2026-03-23 09:19:11 +0100

    Fix #37575
    Update product.lib.php to support local option
    MAIN_DISABLE_PRODUCTS_CONTACTS_TAB which disables the contacts tab.

--- htdocs/core/lib/product.lib.php
+++ htdocs/core/lib/product.lib.php
@@ -172,7 +172,7 @@ function product_prepare_head($object)
 			$h++;
 		}
 	}
-	if (!getDolGlobalString('MAIN_DISABLE_CONTACTS_TAB')) {
+	if (!getDolGlobalString('MAIN_DISABLE_PRODUCTS_CONTACTS_TAB')) {
 		$objectsrc = $object;
 		if ($object->origin == 'product' && $object->origin_id > 0) {
 			$objectsrc = new Product($db);
