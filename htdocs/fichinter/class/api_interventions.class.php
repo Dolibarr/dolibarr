830022996693dc526a03fca0c60af2a1f9fe5a68
William Mead <william@m34d.com>
2026-03-25 06:49:12 +0100

    Fixed return

--- htdocs/fichinter/class/api_interventions.class.php
+++ htdocs/fichinter/class/api_interventions.class.php
@@ -823,10 +823,16 @@ class Interventions extends DolibarrApi
 
 		$updateRes = $objectline->update(DolibarrApiAccess::$user);
 
-		if ($updateRes > 0) {
-			return $this->_cleanObjectDatas($this->fichinter);
+		if ($updateRes >= 0) {
+			$result = $this->fichinter->fetch($id);
+			if ($result > 0) {
+				return $this->_cleanObjectDatas($this->fichinter);
+			} else {
+				throw new RestException(500, $this->fichinter->error);
+			}
+		} else {
+			throw new RestException(500, $objectline->error);
 		}
-		return false;
 	}
 
 
