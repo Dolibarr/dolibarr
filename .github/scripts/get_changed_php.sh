615be3167e06b1581114edb9adb270313451aee4
MDW <mdeweerd@users.noreply.github.com>
2026-03-30 18:51:21 +0200

    Fix: 'forbidden_files: unbound variable'

--- .github/scripts/get_changed_php.sh
+++ .github/scripts/get_changed_php.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+b!/bin/bash
 # Copyright (C) 2025		MDW	<mdeweerd@users.noreply.github.com>
 
 set -euo pipefail
@@ -73,6 +73,7 @@ all_changed_files=$(IFS=" " ; echo "${changed_php_files[*]}")
 all_changed_lang=$(IFS=" " ; echo "${changed_lang_files[*]}")
 
 
+forbidden_files=""
 #forbidden_files=$(echo "$all_changed_lang" | grep -E 'htdocs/langs/([^/]+)/.*\.lang$' | grep -v 'htdocs/langs/en_US/')
 #if [ -n "$forbidden_files" ]; then
 #  echo "You tried to modify one or more language files that are not allowed to be modified in Pull requests."
