fefb3c7b9634b05ff94ebcc97d0703e1a271df34
Jon Bendtsen <jonbendtsen@jonb.dk>
2026-04-04 11:29:41 +0200

    Added thirdparty head ontop of conferences or booth page

--- htdocs/eventorganization/conferenceorbooth_list.php
+++ htdocs/eventorganization/conferenceorbooth_list.php
@@ -65,6 +65,7 @@ $mode       = GETPOST('mode', 'aZ'); // The output mode ('list', 'kanban', 'hier
 $id = GETPOSTINT('id');
 $projectid = GETPOSTINT('projectid');
 $projectref = GETPOST('ref', 'alpha');
+$thirdpartyid  = GETPOSTINT('thirdpartyid');
 
 // Load variable for pagination
 $limit = GETPOSTINT('limit') ? GETPOSTINT('limit') : $conf->liste_limit;
@@ -84,7 +85,11 @@ $object = new ConferenceOrBooth($db);
 $project = new Project($db);
 $extrafields = new ExtraFields($db);
 $diroutputmassaction = $conf->eventorganization->dir_output.'/temp/massgeneration/'.$user->id;
-$hookmanager->initHooks(array($contextpage)); // Note that conf->hooks_modules contains array of activated contexes
+if ($thirdpartyid > 0) {
+	$hookmanager->initHooks(array('thirdpartybooth', 'globalcard'));
+} else {
+	$hookmanager->initHooks(array($contextpage)); // Note that conf->hooks_modules contains array of activated contexes
+}
 
 // Fetch optionals attributes and labels
 $extrafields->fetch_name_optionals_label($object->table_element);
@@ -309,6 +314,62 @@ if ($projectid > 0 || $projectref) {
 
 llxHeader('', $title, $help_url, '', 0, 0, '', '', '', 'mod-eventorganization page-list bodyforlist');
 
+// Copied almost verbertum from htdocs/ticket/list.php
+if ($thirdpartyid && !$projectid && $user->hasRight('societe', 'lire')) {
+	$socstat = new Societe($db);
+	$res = $socstat->fetch($thirdpartyid);
+	if ($res > 0) {
+		$tmpobject = $object;
+		$object = $socstat; // $object must be of type Societe when calling societe_prepare_head
+		$head = societe_prepare_head($socstat);
+		$object = $tmpobject;
+
+		print dol_get_fiche_head($head, 'eventorganization', $langs->trans("ThirdParty"), -1, 'company');
+
+		dol_banner_tab($socstat, 'socid', '', ($user->socid ? 0 : 1), 'rowid', 'nom');
+
+		print '<div class="fichecenter">';
+
+		print '<div class="underbanner clearboth"></div>';
+		print '<table class="border centpercent tableforfield">';
+
+		// Type Prospect/Customer/Supplier
+		print '<tr><td class="titlefield">'.$langs->trans('NatureOfThirdParty').'</td><td>';
+		print $socstat->getTypeUrl(1);
+		print '</td></tr>';
+
+		// Customer code
+		if ($socstat->client && !empty($socstat->code_client)) {
+			print '<tr><td class="titlefield">';
+			print $langs->trans('CustomerCode').'</td><td>';
+			print showValueWithClipboardCPButton(dol_escape_htmltag($socstat->code_client));
+			$tmpcheck = $socstat->check_codeclient();
+			if ($tmpcheck != 0 && $tmpcheck != -5) {
+				print ' <span class="error">('.$langs->trans("WrongCustomerCode").')</span>';
+			}
+			print '</td>';
+			print '</tr>';
+		}
+		// Supplier code
+		if ($socstat->fournisseur && !empty($socstat->code_fournisseur)) {
+			print '<tr><td class="titlefield">';
+			print $langs->trans('SupplierCode').'</td><td>';
+			print showValueWithClipboardCPButton(dol_escape_htmltag($socstat->code_fournisseur));
+			$tmpcheck = $socstat->check_codefournisseur();
+			if ($tmpcheck != 0 && $tmpcheck != -5) {
+				print ' <span class="error">('.$langs->trans("WrongSupplierCode").')</span>';
+			}
+			print '</td>';
+			print '</tr>';
+		}
+
+		print '</table>';
+		print '</div>';
+		print dol_get_fiche_end();
+
+		print '<br>';
+	}
+}
 
 if ($projectid > 0) {
 	// To verify role of users
@@ -613,6 +674,9 @@ if ($object->ismultientitymanaged == 1) {
 }
 if ($projectid > 0) {
 	$sql .= " AND t.fk_project = ".((int) $project->id);
+}
+if (!empty($thirdpartyid)) {
+	$sql .= " AND t.fk_soc = ".((int) $thirdpartyid);
 }
 foreach ($search as $key => $val) {
 	if (array_key_exists($key, $object->fields)) {
