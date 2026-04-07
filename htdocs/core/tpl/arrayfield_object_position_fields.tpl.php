03fb0ef5c556d836bbc91740a1692d217a47d857
Lucas Marcouiller <lmarcouiller@dolicloud.com>
2026-04-07 11:01:58 +0200

    try fix CI

--- htdocs/core/tpl/arrayfield_object_position_fields.tpl.php
+++ htdocs/core/tpl/arrayfield_object_position_fields.tpl.php
@@ -16,7 +16,6 @@
 
 '
 @phan-var-force array<string,array{label:string,checked?:string,position?:int,help?:string,enabled?:string}> $arrayfields
-@phan-var-force string|array<string|int, mixed>			$contextpage
 ';
 
 // Protection to avoid direct call of template
@@ -25,7 +24,7 @@ if (empty($user) || !is_object($user)) {
 	exit(1);
 }
 
-$tmpvar = "MAIN_POSITIONFIELDS_" . $contextpage; // To get list of saved position fields to show
+$tmpvar = "MAIN_POSITIONFIELDS_" . !empty($contextpage) ? $contextpage : ""; // To get list of saved position fields to show
 if (!empty($user->conf->$tmpvar)) {        // A list of fields was already customized for user
 	$tmparray = dolExplodeIntoArray($user->conf->$tmpvar, ',', ':');
 	foreach ($arrayfields as $key => $val) {
