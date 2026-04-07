493b4bdaa55e5d8fe09b427fec65253b8d381c38
Alexandre SPANGARO <alexandre.spangaro@gmail.com>
2026-04-07 09:40:48 +0200

    CI

--- htdocs/core/modules/modSupplierOrder.class.php
+++ htdocs/core/modules/modSupplierOrder.class.php
@@ -276,7 +276,7 @@ if (getDolGlobalString('MAIN_USE_NEW_SUPPLIERMOD')) {
 			$this->export_sql_end[$r] .= ' WHERE f.fk_soc = s.rowid AND f.rowid = fd.fk_commande';
 			$this->export_sql_end[$r] .= ' AND f.entity IN (' . getEntity('supplier_order') . ')';
 			if (is_object($user) && !$user->hasRight('societe', 'client', 'voir')) {
-				$this->export_sql_end[$r] .= ' AND sc.fk_user = ' . ((int) $user->id);
+				$this->export_sql_end[$r] .= ' AND sc.fk_user = ' . ( (int) $user->id);
 			}
 
 			// Imports
@@ -470,8 +470,8 @@ if (getDolGlobalString('MAIN_USE_NEW_SUPPLIERMOD')) {
 			}
 
 			$sql_order = array(
-				"DELETE FROM " . MAIN_DB_PREFIX . "document_model WHERE nom = '" . $this->db->escape($this->const[0][2]) . "' AND type = 'order_supplier' AND entity = " . ((int) $conf->entity),
-				"INSERT INTO " . MAIN_DB_PREFIX . "document_model (nom, type, entity) VALUES('" . $this->db->escape($this->const[0][2]) . "', 'order_supplier', " . ((int) $conf->entity) . ")",
+				"DELETE FROM " . MAIN_DB_PREFIX . "document_model WHERE nom = '" . $this->db->escape($this->const[0][2]) . "' AND type = 'order_supplier' AND entity = " . ( (int) $conf->entity),
+				"INSERT INTO " . MAIN_DB_PREFIX . "document_model (nom, type, entity) VALUES('" . $this->db->escape($this->const[0][2]) . "', 'order_supplier', " . ( (int) $conf->entity) . ")",
 			);
 
 			$sql = $sql_order;
