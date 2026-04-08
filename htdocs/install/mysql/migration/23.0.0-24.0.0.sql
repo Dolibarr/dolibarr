ec69acfa09ecd61d623915a3f6898f59b7a0c118
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-04-08 21:01:48 +0200

    Update 23.0.0-24.0.0.sql to enhance invoice module

--- htdocs/install/mysql/migration/23.0.0-24.0.0.sql
+++ htdocs/install/mysql/migration/23.0.0-24.0.0.sql
@@ -97,8 +97,12 @@ ALTER TABLE llx_categorie_mo ADD CONSTRAINT fk_categorie_mo_fk_mo_rowid FOREIGN 
 
 
 ALTER TABLE llx_facture ADD COLUMN fk_thirdparty_rib_id integer NULL;
-ALTER TABLE llx_facture_fourn ADD COLUMN fk_thirdparty_rib_id integer NULL;
+ALTER TABLE llx_facture ADD COLUMN amount_retained_warranty double(24,8) DEFAULT 0 NOT NULL AFTER retained_warranty;
+ALTER TABLE llx_facture ADD COLUMN prorata_account double(24,8) DEFAULT 0 NOT NULL AFTER amount_retained_warranty;
+ALTER TABLE llx_facture ADD COLUMN amount_prorata_account double(24,8) DEFAULT 0 NOT NULL AFTER prorata_account;
+ALTER TABLE llx_facture ADD COLUMN amount_price_review double(24,8) DEFAULT 0 NOT NULL AFTER amount_prorata_account;
 
+ALTER TABLE llx_facture_fourn ADD COLUMN fk_thirdparty_rib_id integer NULL;
 ALTER TABLE llx_facture_fourn ADD COLUMN payment_reference varchar(25);
 ALTER TABLE llx_facture_fourn ADD COLUMN dispute_status	integer DEFAULT 0;
 
