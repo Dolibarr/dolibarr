9590a1b94618b6d0cdd517435a944e41e27feebb
Florian HENRY <florian.henry@open-concept.pro>
2026-04-09 08:09:21 +0200

    fix: On Supplier proposal external contact type is SERVICE not CUSTOMER

--- htdocs/core/modules/supplier_proposal/doc/pdf_zenith.modules.php
+++ htdocs/core/modules/supplier_proposal/doc/pdf_zenith.modules.php
@@ -1382,7 +1382,7 @@ class pdf_zenith extends ModelePDFSupplierProposal
 
 
 
-			// If CUSTOMER contact defined on proposal, we use it. Note: Even if this is a supplier object, the code for external contact that follow order is 'CUSTOMER'
+			// If SUPPLIER/SERVICE contact defined on proposal, we use it. Note: Even if this is a supplier object, the code for external contact that follow order is 'SERVICE'
 			$usecontact = false;
 			if (!getDolGlobalInt('SUPPLIER_PROPOSAL_ADD_BILLING_CONTACT')) {
 				$arrayidcontact = $object->getIdContact('external', 'SERVICE');
