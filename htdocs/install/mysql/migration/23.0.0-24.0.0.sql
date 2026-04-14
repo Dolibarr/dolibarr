3cd12ac60100ef927ff5fb88df962b8b821bd5e9
Regis Houssin <regis.houssin@inodbox.com>
2026-04-14 21:04:43 +0200

    FIX backport from sql upgrade of v23 #37874

--- htdocs/install/mysql/migration/23.0.0-24.0.0.sql
+++ htdocs/install/mysql/migration/23.0.0-24.0.0.sql
@@ -35,6 +35,11 @@
 
 -- V23 forgotten
 
+-- VMYSQL4.1 DROP INDEX idx_categorie_project_fk_task ON llx_categorie_project_task;
+-- VPGSQL8.2 DROP INDEX idx_categorie_project_fk_task;
+ALTER TABLE llx_categorie_project_task DROP FOREIGN KEY fk_categorie_project_task_rowid;
+ALTER TABLE llx_categorie_project_task ADD INDEX idx_categorie_project_fk_task (fk_project_task);
+ALTER TABLE llx_categorie_project_task ADD CONSTRAINT fk_categorie_project_task_rowid FOREIGN KEY (fk_project_task) REFERENCES llx_projet_task (rowid);
 
 -- V24 migration
 
