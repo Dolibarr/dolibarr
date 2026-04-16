b69340aa1c73e918292469a46f8b73d98b1034df
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2026-04-16 17:08:16 +0200

    php stan

--- htdocs/quickmemo/class/memo.class.php
+++ htdocs/quickmemo/class/memo.class.php
@@ -1429,8 +1429,11 @@ class Memo extends CommonObject
 		}
 
 		// Security check
+		/** @var array<string, mixed> $list */
 		return array_filter($list, function ($k) {
-			return is_string($k) && strlen($k) <= 64 && preg_match('/^[a-zA-Z0-9_]+$/', $k) === 1;
+			return is_string($k)
+				&& strlen($k) <= 64
+				&& preg_match('/^[a-zA-Z0-9_]+$/', $k) === 1;
 		}, ARRAY_FILTER_USE_KEY);
 	}
 
