2bbfd0c58bce63b387e755c54682f8d1b11b1009
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2026-04-16 14:41:24 +0200

    fix string type

--- htdocs/core/modules/modQuickMemo.class.php
+++ htdocs/core/modules/modQuickMemo.class.php
@@ -99,7 +99,6 @@ class modQuickMemo extends DolibarrModules
 		$compatibleHooks = array_unique($compatibleHooks);
 		// Security check
 		$compatibleHooks = array_filter($compatibleHooks, function (string $k) {
-			if (!is_string($k)) { return false; }
 			return preg_match('/^[a-zA-Z0-9_]+$/', $k);
 		});
 
--- htdocs/quickmemo/class/memo.class.php
+++ htdocs/quickmemo/class/memo.class.php
@@ -1430,8 +1430,7 @@ class Memo extends CommonObject
 
 		// Security check
 		return array_filter($list, function (string $k) {
-			if (!is_string($k)) { return false; }
-			return strlen((string) $k) <= 64 && preg_match('/^[a-zA-Z0-9_]+$/', (string) $k);
+			return strlen($k) <= 64 && preg_match('/^[a-zA-Z0-9_]+$/', $k);
 		}, ARRAY_FILTER_USE_KEY);
 	}
 
