68972a5b96da3510172031cb5b6294366ab036cb
sonikf <93765174+sonikf@users.noreply.github.com>
2026-04-21 09:56:05 +0300

    fix pre-commit

--- htdocs/ai/tools/categories.class.php
+++ htdocs/ai/tools/categories.class.php
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
- 
+
 /**
  * \file htdocs/ai/tools/categories.php
  * \ingroup ai
