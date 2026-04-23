00e3231213feb9280c8b65a0ce752918e74f3daf
Jon Bendtsen <jonbendtsen@jonb.dk>
2026-04-23 15:27:52 +0200

    PHPStan complains that this will always evaluate to true, but it is waiting for PR #37920

--- htdocs/core/class/html.formmailing.class.php
+++ htdocs/core/class/html.formmailing.class.php
@@ -137,12 +137,8 @@ class FormMailing extends Form
 		$out .= '<!-- 3 buttons Add, Delete and Cancel -->';
 		require_once DOL_DOCUMENT_ROOT.'/core/modules/mailings/modules_mailings.php';
 		$staticmailingtarget = new MailingTargets($this->db);
-		if (method_exists($staticmailingtarget, 'addTargetsToDatabase')) {
-			$out .= '<input type="submit" class="butAction button-add small reposition" id="button_add_'.$actionname.'" name="button_add_'.$actionname.'" value="'.$langs->trans("Add").'">';
-		}
-		if (method_exists($staticmailingtarget, 'deleteTargetsFromDatabase')) {
-			$out .= '<input type="submit" class="butActionDelete button-delete small reposition" id="button_delete_'.$actionname.'" name="button_delete_'.$actionname.'" value="'.$langs->trans("Delete").'">';
-		}
+		$out .= '<input type="submit" class="butAction button-add small reposition" id="button_add_'.$actionname.'" name="button_add_'.$actionname.'" value="'.$langs->trans("Add").'">';
+		$out .= '<input type="submit" class="butActionDelete button-delete small reposition" id="button_delete_'.$actionname.'" name="button_delete_'.$actionname.'" value="'.$langs->trans("Delete").'">';
 		$out .= '<input type="submit" class="button button-cancel reposition" id="cancel" name="cancel" value="'.$langs->trans("Cancel").'" />';
 		$out .= '</div><br>';
 
