d2a790594a6457fd90612a3a9f7f4ca4f3c56a8c
Jon Bendtsen <jonbendtsen@jonb.dk>
2026-04-24 10:42:51 +0200

    Disable delete button until github PR #37920 brings in the function to delete mass mailing targets

--- htdocs/core/class/html.formmailing.class.php
+++ htdocs/core/class/html.formmailing.class.php
@@ -138,7 +138,8 @@ class FormMailing extends Form
 		require_once DOL_DOCUMENT_ROOT.'/core/modules/mailings/modules_mailings.php';
 		$staticmailingtarget = new MailingTargets($this->db);
 		$out .= '<input type="submit" class="butAction button-add small reposition" id="button_add_'.$actionname.'" name="button_add_'.$actionname.'" value="'.$langs->trans("Add").'">';
-		$out .= '<input type="submit" class="butActionDelete button-delete small reposition" id="button_delete_'.$actionname.'" name="button_delete_'.$actionname.'" value="'.$langs->trans("Delete").'">';
+		// $out .= '<input type="submit" class="butActionDelete button-delete small reposition" id="button_delete_'.$actionname.'" name="button_delete_'.$actionname.'" value="'.$langs->trans("Delete").'">';
+		// Waiting for github PR #37920
 		$out .= '<input type="submit" class="button button-cancel reposition" id="cancel" name="cancel" value="'.$langs->trans("Cancel").'" />';
 		$out .= '</div><br>';
 
