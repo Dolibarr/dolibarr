35289fab95131cee9dfff7f54beeb609feeea788
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-04-27 21:15:52 +0200

    Update 23.0.0-24.0.0.sql

--- htdocs/install/mysql/migration/23.0.0-24.0.0.sql
+++ htdocs/install/mysql/migration/23.0.0-24.0.0.sql
@@ -100,10 +100,10 @@ ALTER TABLE llx_categorie_mo ADD INDEX idx_categorie_mo_fk_mo (fk_mo);
 ALTER TABLE llx_categorie_mo ADD CONSTRAINT fk_categorie_mo_categorie_rowid FOREIGN KEY (fk_categorie) REFERENCES llx_categorie (rowid);
 ALTER TABLE llx_categorie_mo ADD CONSTRAINT fk_categorie_mo_fk_mo_rowid FOREIGN KEY (fk_mo) REFERENCES llx_mrp_mo (rowid);
 
-
 ALTER TABLE llx_facture ADD COLUMN fk_thirdparty_rib_id integer NULL;
-ALTER TABLE llx_facture_fourn ADD COLUMN fk_thirdparty_rib_id integer NULL;
+ALTER TABLE llx_facture ADD COLUMN amount_prorata_account double(24,8) DEFAULT 0 NOT NULL AFTER prorata_account;
 
+ALTER TABLE llx_facture_fourn ADD COLUMN fk_thirdparty_rib_id integer NULL;
 ALTER TABLE llx_facture_fourn ADD COLUMN payment_reference varchar(25);
 ALTER TABLE llx_facture_fourn ADD COLUMN dispute_status	integer DEFAULT 0;
 
