0a5cda031277fc47c001c2e0d6800ece304c54ca
Jon Bendtsen <jonbendtsen@jonb.dk>
2026-04-28 14:14:55 +0200

    Fix #37999 Updating product price

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -1501,7 +1501,7 @@ class Product extends CommonObject
 
 		if ($result >= 0) {
 			// $this->oldcopy should have been set by the caller of update (here properties were already modified)
-			if (is_null($this->oldcopy) || (is_object($this->oldcopy) && $this->oldcopy->isEmpty())) {
+			if (is_null($this->oldcopy) || !($this->oldcopy instanceof Product) || $this->oldcopy->isEmpty()) {
 				$this->oldcopy = dol_clone($this, 1); // 1 to clone with methods to avoid fatal error with $this->oldcopy->hasbatch()
 			}
 			// Test if batch management is activated on existing product
