d396985d13063ea7cd1654ae92f911d9033caaf4
Zakaria Boushaba <z.boushaba@vold.africa>
2026-04-29 15:54:21 +0100

    fix: clear resPrint after reading selectForFormsListUrl hook value to prevent showOptionals from appending stale URL params as visible text

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -9500,6 +9500,7 @@ class Form
 			$reshook = $hookmanager->executeHooks('selectForFormsListUrl', $parameters, $objecttmp, $action);
 			if (!empty($reshook)) {
 				$urloption = $hookmanager->resPrint;
+                $hookmanager->resPrint = '';
 			}
 
 			// Activate the auto complete using ajax call.
