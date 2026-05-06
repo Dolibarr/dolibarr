bb8f5f5041ecbbaa276201de79ce7521753009db
Daniel Bauer <d.bauer@elaax.net>
2026-05-06 14:09:44 +0200

    Fix BookCal: prevent double booking, mail templates, confirm/cancel

--- htdocs/bookcal/class/availabilities.class.php
+++ htdocs/bookcal/class/availabilities.class.php
@@ -3,6 +3,8 @@
  * Copyright (C) 2022       Alice Adminson              <aadminson@example.com>
  * Copyright (C) 2024-2025  Frédéric France             <frederic.france@free.fr>
  * Copyright (C) 2024-2025	MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2026		Daniel Bauer				<d.bauer@elaax.net>
+ * Copyright (C) 2026		ELAAX IT-Service			(https://www.elaax.de)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -967,6 +969,11 @@ class Availabilities extends CommonObject
 		}
 
 		$modelpath = "core/modules/bookcal/doc/";
+		if (!is_dir(DOL_DOCUMENT_ROOT.'/'.$modelpath)) {
+			$this->error = '';
+			$this->errors = array();
+			return 0;
+		}
 
 		if (!empty($modele)) {
 			$result = $this->commonGenerateDocument($modelpath, $modele, $outputlangs, $hidedetails, $hidedesc, $hideref, $moreparams);
