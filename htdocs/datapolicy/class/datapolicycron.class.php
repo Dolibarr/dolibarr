242de0fdaa7e702cdf6240aefa35fa95156e4e6c
daraelmin <8067223+daraelmin@users.noreply.github.com>
2026-05-12 18:08:22 +0200

    Copyright

--- htdocs/datapolicy/class/datapolicycron.class.php
+++ htdocs/datapolicy/class/datapolicycron.class.php
@@ -4,6 +4,7 @@
  * Copyright (C) 2024      William Mead      <william.mead@manchenumerique.fr>
  * Copyright (C) 2024-2025	MDW                      <mdeweerd@users.noreply.github.com>
  * Copyright (C) 2025      Quentin VIAL--GOUTEYRON   <quentin.vial-gouteyron@atm-consulting.fr>
+ * Copyright (C) 2025      Waël Almoman              <info@almoman.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
