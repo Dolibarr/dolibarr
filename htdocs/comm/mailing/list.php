7cc1d3e665f6c8e726f265e2687400f421bdde2b
Frédéric FRANCE <frederic.france@free.fr>
2026-05-15 09:34:57 +0200

    clean code

--- htdocs/comm/mailing/list.php
+++ htdocs/comm/mailing/list.php
@@ -3,8 +3,8 @@
  * Copyright (C) 2005-2010	Regis Houssin				<regis.houssin@inodbox.com>
  * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024		Alexandre Spangaro			<alexandre@inovea-conseil.com>
- * Copyright (C) 2024-2026  Frédéric France         <frederic.france@free.fr>
- * Copyright (C) 2025		Jon Bendtsen            <jon.bendtsen.github@jonb.dk>
+ * Copyright (C) 2024-2026  Frédéric France             <frederic.france@free.fr>
+ * Copyright (C) 2025		Jon Bendtsen                <jon.bendtsen.github@jonb.dk>
  * Copyright (C) 2025       Josep Lluís Amador Teruel   <joseplluis@lliuretic.cat>
  *
  * This program is free software; you can redistribute it and/or modify
