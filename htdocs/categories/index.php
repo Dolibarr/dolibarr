5905024aaa00ffdfad6187bc1afe53df47b0848e
Jon Bendtsen <jonbendtsen@jonb.dk>
2026-05-18 11:28:53 +0200

    Will now show product and service categories when 1 or both modules for products or services are enabled #38264

--- htdocs/categories/index.php
+++ htdocs/categories/index.php
@@ -80,15 +80,19 @@ llxHeader('', $title, '', '', 0, 0, '', '');
 // Get list of category type
 $arrayofcateg = array();
 foreach ($categstatic->MAP_ID as $key => $idtype) {
-	if ($key == 'service') {
+	if ($key == 'product' && !isModEnabled("product")) {
 		continue;
 	}
+	if ($key == 'service' && !isModEnabled("service")) {
+		continue;
+	}
+
 	$arrayofcateg[$idtype] = array();
 	$arrayofcateg[$idtype]['key'] = $key;
 	$arrayofcateg[$idtype]['nb'] = 0;
 	$arrayofcateg[$idtype]['label'] = $langs->transnoentitiesnoconv($categstatic::$MAP_TYPE_TITLE_AREA[$key]);
 	$arrayofcateg[$idtype]['labelwithoutaccent'] = dol_string_unaccent($langs->transnoentitiesnoconv($categstatic::$MAP_TYPE_TITLE_AREA[$key]));
-	if ($key == 'product') {
+	if ($key == 'product' || $key == 'service') {
 		$arrayofcateg[$idtype]['label'] = $langs->transnoentitiesnoconv("ProductsAndServices");
 		$arrayofcateg[$idtype]['labelwithoutaccent'] = dol_string_unaccent($langs->transnoentitiesnoconv("ProductsAndServices"));
 	}
