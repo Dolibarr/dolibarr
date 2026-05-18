0c2f04d65809ce6762fe1e0e35f2aa9f0b0549de
Frédéric FRANCE <frederic.france@free.fr>
2026-05-18 11:33:28 +0200

    clean code

--- htdocs/holiday/card.php
+++ htdocs/holiday/card.php
@@ -51,7 +51,6 @@ require_once DOL_DOCUMENT_ROOT.'/core/class/doleditor.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/holiday.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/holiday/class/holiday.class.php';
-require_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
 
 // Get parameters
 $action = GETPOST('action', 'aZ09');
