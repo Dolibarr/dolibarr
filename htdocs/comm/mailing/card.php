7b298ccfe74dbbe609ac51276e8dbda87d0f83a3
sonikf <93765174+sonikf@users.noreply.github.com>
2026-05-19 13:07:43 +0300

    fix phan

--- htdocs/comm/mailing/card.php
+++ htdocs/comm/mailing/card.php
@@ -1000,6 +1000,9 @@ if ($action == 'create') {	// aaa
 						$text .= '<br><textarea class="quatrevingtpercent" rows="'.ROWS_2.'" wrap="soft" disabled>php ./scripts/emailings/mailing-send.php '.$object->id.' '.$user->login.'</textarea>';
 					}
 
+					$labelbuttonyes = $langs->transnoentities('Confirm');
+					$labelbuttonno  = $langs->transnoentities('Cancel');
+
 					print $form->formconfirm($_SERVER['PHP_SELF'].'?id='.$object->id, $langs->trans('SendMailing'), $text, 'sendallconfirmed', '', '', 1, 380, 660, 0, $langs->transnoentities("Confirm"), $langs->transnoentities("Cancel"));
 				}
 			}
