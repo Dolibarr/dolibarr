6cbf0ba10945e7987dd68bf3a34296f306774031
Braito <braito4@hotmail.com>
2026-05-19 12:15:24 +0200

    Add AI email cleaner MCP tools

---
+++ htdocs/install/mysql/tables/llx_emailcollector_ai_cleaning-ai.key.sql
@@ -0,0 +1,21 @@
+-- ===================================================================
+-- Copyright (C) 2026
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <https://www.gnu.org/licenses/>.
+-- ===================================================================
+
+ALTER TABLE llx_emailcollector_ai_cleaning ADD INDEX idx_emailcollector_ai_cleaning_entity_msgid (entity, msgid);
+ALTER TABLE llx_emailcollector_ai_cleaning ADD INDEX idx_emailcollector_ai_cleaning_entity_collector (entity, collector_id, rowid);
+ALTER TABLE llx_emailcollector_ai_cleaning ADD INDEX idx_emailcollector_ai_cleaning_date (date_creation);
+ALTER TABLE llx_emailcollector_ai_cleaning ADD INDEX idx_emailcollector_ai_cleaning_context (entity, context_profile_code);
