af4389f3fe4b3a177b71863f9c103b454efff820
sonikf <93765174+sonikf@users.noreply.github.com>
2026-05-19 12:09:31 +0300

    fix translation

--- htdocs/comm/mailing/card.php
+++ htdocs/comm/mailing/card.php
@@ -1000,7 +1000,7 @@ if ($action == 'create') {	// aaa
 						$text .= '<br><textarea class="quatrevingtpercent" rows="'.ROWS_2.'" wrap="soft" disabled>php ./scripts/emailings/mailing-send.php '.$object->id.' '.$user->login.'</textarea>';
 					}
 
-					print $form->formconfirm($_SERVER['PHP_SELF'].'?id='.$object->id, $langs->trans('SendMailing'), $text, 'sendallconfirmed', '', '', 1, 380, 660, 0, $langs->trans("Confirm"), $langs->trans("Cancel"));
+					print $form->formconfirm($_SERVER['PHP_SELF'].'?id='.$object->id, $langs->trans('SendMailing'), $text, 'sendallconfirmed', '', '', 1, 380, 660, 0, $langs->transnoentities("Confirm"), $langs->transnoentities("Cancel"));
 				}
 			}
 
