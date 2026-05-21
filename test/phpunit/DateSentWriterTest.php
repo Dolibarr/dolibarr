cf00bb47af0c44fa0ec0f709f93cc3624739b5e6
Quentin VIAL--GOUTEYRON <quentin.vial-gouteyron@atm-consulting.fr>
2026-05-21 09:22:16 +0200

    fix: merge phpstan-ignore into class docblock to satisfy PHPCS
    PHPCS requires /** style comment for class declarations.
    Moving @phpstan-ignore class.notFound into the single class docblock
    avoids the two-docblock situation that prevented suppression.

--- test/phpunit/DateSentWriterTest.php
+++ test/phpunit/DateSentWriterTest.php
@@ -40,8 +40,8 @@ $conf->global->MAIN_DISABLE_ALL_MAILS = 1;
  * @property mixed $savuser
  * @property mixed $savlangs
  * @property mixed $savdb
+ * @phpstan-ignore class.notFound
  */
-// @phpstan-ignore-next-line class.notFound
 class DateSentWriterTest extends CommonClassTest
 {
 	/**
