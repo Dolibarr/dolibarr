17bce1a8fb35fb4fb2224ea96bc7764fd79b0e89
Frédéric FRANCE <frederic34@users.noreply.github.com>
2026-05-22 11:53:17 +0200

    fix test

--- htdocs/core/filemanagerdol/connectors/php/config.inc.php
+++ htdocs/core/filemanagerdol/connectors/php/config.inc.php
@@ -50,7 +50,7 @@ if ($pos == '/') {
 $entity = ((!empty($_SESSION['dol_entity']) && $_SESSION['dol_entity'] > 1) ? $_SESSION['dol_entity'] : null);
 
 
-if (!empty($user->admin) && !$user->hasRight('website', 'write')) {
+if (!(empty($user->admin) || !$user->hasRight('website', 'write'))) {
 	accessforbidden('Need to be admin or having write permission on website module');
 	exit;
 }
