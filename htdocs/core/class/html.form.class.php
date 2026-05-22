19b9076befc58270d078a090c935a083b84a6c5f
Jon Bendtsen <jonbendtsen@jonb.dk>
2026-05-22 21:11:21 +0200

    move date last

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -1536,7 +1536,6 @@ class Form
 			} else {
 				$htmltoenteralink .= '<td>' . $objp->name . '</td>';
 			}
-			$htmltoenteralink .= '<td>' . $objp->date_subscription . '</td>';
 			$fetchcproject = $projectstatic->fetch($objp->fk_project);
 			if ($fetchcproject) {
 				$htmltoenteralink .= '<td>' . $projectstatic->getNomUrl(0). '</td>';
