6afeb414028e0f0456cf4f5d35cd1dfb197899ab
Braito <braito4@hotmail.com>
2026-05-22 11:54:34 +0200

    chore: align copyright headers with Dolibarr rules

--- htdocs/core/modules/modAi.class.php
+++ htdocs/core/modules/modAi.class.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2018-2019  Nicolas ZABOURI         <info@inovea-conseil.com>
  * Copyright (C) 2019-2024  Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2026       Braito                  <braito4@hotmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- htdocs/install/mysql/tables/llx_ai_unassigned_pdf_queue-ai.key.sql
+++ htdocs/install/mysql/tables/llx_ai_unassigned_pdf_queue-ai.key.sql
@@ -1,5 +1,5 @@
 -- ===================================================================
--- Copyright (C) 2026
+-- Copyright (C) 2026  Braito                  <braito4@hotmail.com>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
