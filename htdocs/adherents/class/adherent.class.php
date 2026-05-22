cedb9ef47b0019869de3ea6744b05a7d4368bd94
Jon Bendtsen <jonbendtsen@jonb.dk>
2026-05-22 19:02:21 +0200

    Add error message when subscription is not allowed #38287

--- htdocs/adherents/class/adherent.class.php
+++ htdocs/adherents/class/adherent.class.php
@@ -1846,8 +1846,9 @@ class Adherent extends CommonObject
 		$allowedStatuses = array_map('intval', explode(',', $allowedStatusesStr));
 		$currentMemberStatus = $this->statut;
 		if (!in_array($currentMemberStatus, $allowedStatuses)) {
+			$currentMemberStatusStr = $this->getLibStatut();
 			dol_syslog('User='.$user->id.' tried to create subscription for member='.$this->id.' with status='.$currentMemberStatus.' only allowed are status='.$allowedStatusesStr, LOG_WARNING);
-			$error = $langs->trans("ErrorSubscriptionNotAllowed", $currentMemberStatus);
+			$this->error = $langs->trans("ErrorSubscriptionNotAllowed", $currentMemberStatusStr);
 			return -1;
 		}
 
