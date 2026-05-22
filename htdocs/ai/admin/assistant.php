5b0f4141db696670e89a7cad7d899b653c315f35
Laurent Destailleur <eldy@destailleur.fr>
2026-05-22 10:57:47 +0200

    Update assistant.php

--- htdocs/ai/admin/assistant.php
+++ htdocs/ai/admin/assistant.php
@@ -330,7 +330,7 @@ if (getDolGlobalString('AI_ASSISTANT_ENABLED')) {
 		print '<div class="center">';
 
 		if ($currentService && $currentService !== '-1') {
-			print '<a class="reposition button smallpaddingimp" href="'.dol_escape_htmltag($_SERVER['PHP_SELF']).'?action=test_provider&token='.dol_escape_htmltag(newToken()).'&service_key='.dol_escape_htmltag($currentService).'">'.$langs->trans('TestConnection').'</a>';
+			print '<a class="reposition button smallpaddingimp" href="'.$_SERVER['PHP_SELF'].'?action=test_provider&token='.newToken().'&service_key='.urlencode($currentService).'">'.$langs->trans('TestConnection').'</a>';
 		}
 
 		print '</div>';
