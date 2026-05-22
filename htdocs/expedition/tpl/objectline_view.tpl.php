0bb964a6dd58e137aa809aee1c7329192425ba7b
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-05-22 10:47:19 +0200

    Update objectline_view.tpl.php

--- htdocs/expedition/tpl/objectline_view.tpl.php
+++ htdocs/expedition/tpl/objectline_view.tpl.php
@@ -4,11 +4,12 @@
  * Copyright (C) 2012-2013	Christophe Battarel	<christophe.battarel@altairis.fr>
  * Copyright (C) 2012       Cédric Salvador     <csalvador@gpcsolutions.fr>
  * Copyright (C) 2012-2014  Raphaël Doursenaud  <rdoursenaud@gpcsolutions.fr>
- * Copyright (C) 2013		    Florian Henry		<florian.henry@open-concept.pro>
- * Copyright (C) 2017		    Juanjo Menent		<jmenent@2byte.es>
+ * Copyright (C) 2013		Florian Henry		<florian.henry@open-concept.pro>
+ * Copyright (C) 2017		Juanjo Menent		<jmenent@2byte.es>
  * Copyright (C) 2024-2025	MDW					<mdeweerd@users.noreply.github.com>
- * Copyright (C) 2024       Frédéric France         <frederic.france@free.fr>
- * Copyright (C) 2025		    Nick Fragoulis
+ * Copyright (C) 2024       Frédéric France     <frederic.france@free.fr>
+ * Copyright (C) 2025		Nick Fragoulis
+ * Copyright (C) 2026		Pierre Ardoin		<developpeur@lesmetiersdubatiment.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
