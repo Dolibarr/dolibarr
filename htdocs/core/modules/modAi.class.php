9f3a799e17d06b592af3e95a50a04cedda8e0c89
Braito <braito4@hotmail.com>
2026-05-22 11:54:34 +0200

    chore: align copyright headers with Dolibarr rules

--- htdocs/core/modules/modAi.class.php
+++ htdocs/core/modules/modAi.class.php
@@ -4,6 +4,7 @@
  * Copyright (C) 2019-2024  Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2024		MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2026		Jose Martinez			<jose.martinez@pichinov.com>
+ * Copyright (C) 2026		Braito					<braito4@hotmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
