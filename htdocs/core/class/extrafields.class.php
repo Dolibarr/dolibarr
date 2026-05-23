015c124f87ce13d0d0880ef4a629444f7f632233
MDW <mdeweerd@users.noreply.github.com>
2026-05-23 14:43:46 +0200

    Fix CI: Fix PHPStan notices

--- htdocs/core/class/extrafields.class.php
+++ htdocs/core/class/extrafields.class.php
@@ -1931,7 +1931,7 @@ class ExtraFields
 						}
 					}
 
-					$InfoFieldList[5] = (string) $InfoFieldList[5] ?? '';
+					$InfoFieldList[5] = (string) $InfoFieldList[5];
 
 					$filter_categorie = false;
 					if (count($InfoFieldList) > 5 && ($InfoFieldList[5] != '')) {
