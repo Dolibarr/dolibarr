16f6c5e50ea00472921f8b50bd0bea9f87ba2f43
Vincent Maury <artec.vm@arnac.net>
2026-05-28 11:44:12 +0200

    Update fournisseur.product.class.php

--- htdocs/fourn/class/fournisseur.product.class.php
+++ htdocs/fourn/class/fournisseur.product.class.php
@@ -1022,8 +1022,8 @@ class ProductFournisseur extends Product
 	 */
 	public static function replaceThirdparty(DoliDB $dbs, $origin_id, $dest_id)
 	{
-		// Issue #38456 To avoid sql errors like "Error in query (1062): Duplicate entry '610185-2551-1-1' for key 'uk_product_fournisseur_price_ref'" when calling CommonObject::commonReplaceThirdparty(..
-		// we delete origin product prices existing in destination before update
+		/* Issue #38456 To avoid sql errors like "Error in query (1062): Duplicate entry '610185-2551-1-1' for key 'uk_product_fournisseur_price_ref'" when calling CommonObject::commonReplaceThirdparty(..
+		we delete origin product prices existing in destination before update */
 		$sql = ' DELETE p_old FROM '.$dbs->prefix().'product_fournisseur_price p_old ';
 		$sql .= '	 WHERE p_old.fk_soc = '.$origin_id.' AND ';
 		$sql .= '		 EXISTS (SELECT 1 FROM '.$dbs->prefix().'product_fournisseur_price p_new	WHERE p_new.fk_product = p_old.fk_product AND p_new.fk_soc = '.$dest_id.')';
