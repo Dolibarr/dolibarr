d11c0a5fd6c4ac68e120cb5700b84262fc1cbfab
David Beniamine <david.beniamine@tetras-libre.fr>
2026-06-05 16:24:26 +0200

    Add setting to allow tls bad cert

--- htdocs/core/class/ldap.class.php
+++ htdocs/core/class/ldap.class.php
@@ -397,6 +397,10 @@ class Ldap
 						//ldap_set_option($this->connection, LDAP_OPT_PROTOCOL_VERSION, 3);
 						//ldap_set_option($this->connection, LDAP_OPT_REFERRALS, 0);
 
+                        if (getDolGlobalString('LDAP_SERVER_TLS_IGNORE_CERT')) {
+							dol_syslog(get_class($this)."::connectBind ignoring tls cert", LOG_WARNING);
+                            ldap_set_option($this->connection, LDAP_OPT_X_TLS_REQUIRE_CERT, LDAP_OPT_X_TLS_ALLOW);
+                        }
 						$resulttls = ldap_start_tls($this->connection);
 						if (!$resulttls) {
 							dol_syslog(get_class($this)."::connectBind failed to start tls", LOG_WARNING);
