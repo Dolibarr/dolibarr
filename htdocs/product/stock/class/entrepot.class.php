9230b1b08beaae88ee7d51f591fdc35d3844795e
Charlène Benke <1179011+defrance@users.noreply.github.com>
2026-06-08 15:58:35 +0200

    Update entrepot.class.php

--- htdocs/product/stock/class/entrepot.class.php
+++ htdocs/product/stock/class/entrepot.class.php
@@ -44,7 +44,7 @@ class Entrepot extends CommonObject
 	 * @var string Field with ID of parent key if this field has a parent
 	 */
 	public $fk_element = 'fk_stock';
-	
+
 	/**
 	 * @var string Name of table without prefix where object is stored
 	 */
