0d96979561b987eea6069697223c94cb50525fd9
Frederic <fefed22@gmail.com>
2026-06-08 17:10:02 +0200

    Implement customer-specific discount handling
    Add logic to handle customer-specific discount retrieval.

--- htdocs/core/tpl/objectline_create.tpl.php
+++ htdocs/core/tpl/objectline_create.tpl.php
@@ -1059,6 +1059,22 @@ if (!empty($object->thirdparty)) {
 								jQuery("#price_ht").val(data.price_ht);
 							}
 
+							// usefull to retrieve percent from customer specific price
+							if (typeof data.discount !== 'undefined' && data.discount !== null && data.discount !== '') {
+							    var remisePercentInput = jQuery('#remise_percent');
+							
+							    if (remisePercentInput.length > 0) {
+							        console.log("Remise spécifique client trouvée : " + data.discount + "%");
+							
+							        remisePercentInput
+							            .val(data.discount)
+							            .trigger('input')
+							            .trigger('change');
+							    } else {
+							        console.warn("Champ #remise_percent introuvable, remise non appliquée");
+							    }
+							}
+
 							// Set values for any fields in the form options_SOMETHING
 							for (var key in data.array_options) {
 								if (data.array_options.hasOwnProperty(key)) {
