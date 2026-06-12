4709202b0378b09282b486cbd0882ce6284d9f54
Eric Seigne <eric.seigne@cap-rel.fr>
2026-06-12 12:23:49 +0200

    update datamodel (review from pairs)

--- htdocs/install/mysql/tables/llx_schemas.sql
+++ htdocs/install/mysql/tables/llx_schemas.sql
@@ -1,62 +1,76 @@
 --
 CREATE TABLE llx_schemas (
-    rowid INT AUTO_INCREMENT PRIMARY KEY,
+    rowid 			integer AUTO_INCREMENT PRIMARY KEY,
 	-- uuid from json file
-    uuid VARCHAR(64) NOT NULL,
+    uuid 			varchar(64) NOT NULL,
 	-- name of object like keyboard, mouse, screen ...
-    name VARCHAR(64) NOT NULL,
+    name 			varchar(64) NOT NULL,
 	-- name displayed to user
-    label VARCHAR(255) NOT NULL,
-	-- type of object (dolibarr element)
-    elementtype VARCHAR(64) NOT NULL,
+    label 			varchar(255) NOT NULL,
 	-- kind of schema (family like "cerfa")
-    schema_kind VARCHAR(32) NULL,
+    schema_kind 	varchar(32) NULL,
 	-- long description
-    description TEXT COMMENT,
+    description 	text,
 	-- json for meta object composed of other parts
 	-- note even if that is a json store is TEXT for "old" version of mariadb
-    composed_of TEXT NULL,
+    composed_of 	JSON NULL,
 	-- orignal json in case of remote deleted file (for example)
-    json_schema TEXT COMMENT,
+    json_schema 	JSON,
 	-- 0 disabled / 1 enabled
-    active TINYINT DEFAULT 1 NOT NULL,
-    date_creation DATETIME NOT NULL,
-    tms TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    fk_user_creat INT,
-    fk_user_modif INT,
-    entity INT DEFAULT 1 NOT NULL
-) ENGINE=InnoDB;
+    active 			integer DEFAULT 1 NOT NULL,
+	-- note : version is in json and parent uuid is in json too
+    date_creation 	datetime NOT NULL,
+    tms 			timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    fk_user_creat 	integer,
+    fk_user_modif 	integer,
+    entity 			integer DEFAULT 1 NOT NULL
+) ENGINE=innodb;
 
 -- Table with all fields description to be able to request it like dolibarr native extrafields
 --
 CREATE TABLE llx_schemas_field (
-    rowid INT AUTO_INCREMENT PRIMARY KEY,
+    rowid 			integer AUTO_INCREMENT PRIMARY KEY,
+    name 			varchar(64) NOT NULL,
+    entity 			integer DEFAULT 1 NOT NULL,
+    label 			varchar(255) NOT NULL,
+	-- there is no element type because there is only one def of schema
+	-- and link will be on element_element
+	-- like elementtype note some new type will become like note_audio ...
+    type 			varchar(64),
+    size 			varchar(8) DEFAULT NULL,
+    fieldcomputed 	text,
+    fielddefault 	text,
+    fieldunique 	integer DEFAULT 0,
+    fieldrequired 	integer DEFAULT,
+    perms 			varchar(255),
+    enabled 		varchar(255),
+    pos 			integer DEFAULT 0,
+    alwayseditable 	integer DEFAULT 0,
+    emptyonclone 	integer DEFAULT 0,
+    param 			text,
+    list 			varchar(255) DEFAULT '1',
+	printegerable 	integer DEFAULT 0,
+	-- is the extrafield output on tooltip
+	showintooltip	integer DEFAULT 0,
+    totalizable 	boolean default false,
+    langs 			varchar(64),
+    help 			text,
+	-- a prompt to autofill the value with AI
+	aiprompt		text,
+    css 			varchar(255),
+    cssview 		varchar(255),
+    csslist 		varchar(255),
+	-- 1 if field contains personal data (GDPR/nLPD/LGPD)
+    personal_data	integer DEFAULT 0,
+	-- user making creation
+	fk_user_author	integer,
+	-- user making last change
+	fk_user_modif	integer,
 	-- link to schema
-    fk_schema INT NOT NULL,
-    entity INT DEFAULT 1 NOT NULL,
-    name VARCHAR(64) NOT NULL,
-    label VARCHAR(255) NOT NULL,
-    type VARCHAR(64) NOT NULL,
-    size VARCHAR(8),
-    pos INT DEFAULT 0 NOT NULL,
-    visible TINYINT DEFAULT 0 NOT NULL,
-    fieldunique TINYINT DEFAULT 0 NOT NULL,
-    fieldrequired TINYINT DEFAULT 0 NOT NULL,
-    fielddefault VARCHAR(255),
-    fieldcomputed TEXT,
-    perms VARCHAR(255),
-    enabled VARCHAR(255) DEFAULT '1',
-    list VARCHAR(24) DEFAULT '1',
-    printable TINYINT DEFAULT 0,
-    totalizable TINYINT DEFAULT 0 NOT NULL,
-    alwayseditable TINYINT DEFAULT 0 NOT NULL,
-    emptyonclone TINYINT DEFAULT 0 NOT NULL,
-    param TEXT,
-    help TEXT,
-    langs VARCHAR(64),
-    css VARCHAR(255),
-    cssview VARCHAR(255),
-    csslist VARCHAR(255),
-    hidden TINYINT DEFAULT 0 COMMENT 'cache'
-) ENGINE=InnoDB;
+    fk_schema 		integer NOT NULL,
+	-- date of creation
+	datec			datetime,
+	-- last modification date
+	tms             timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=innodb;
 
