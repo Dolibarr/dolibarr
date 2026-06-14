ludon89/dolidev

commit 97c86d66ad67add51825c76c475f365a3762cb18

Workstation_extrafields table missing (#38696)
llx_workstation_workstation_extrafields is missing in v23 if this was migrated from precedent dolibarr version

htdocs/install/mysql/migration/22.0.0-23.0.0.sql
@@ -440,4 +440,15 @@ INSERT INTO llx_accounting_system (fk_country, pcg_version, label, active) VALUE
 -- Rename OIDC enable constant: openidconnect was converted from a module to a config-level feature (#36051)
 UPDATE llx_const SET name = 'MAIN_AUTHENTICATION_OIDC_ON' WHERE name = 'MAIN_MODULE_OPENIDCONNECT';
 
+-- this table was created only during fresh install
+CREATE llx_workstation_workstation_extrafields
+(
+    rowid           integer     AUTO_INCREMENT PRIMARY KEY,
+    tms             timestamp   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    fk_object       integer     NOT NULL,
+    import_key      varchar(14)                          -- import key
+) ENGINE=innodb;
+
+ALTER TABLE llx_workstation_workstation_extrafields ADD INDEX idx_workstation_workstation_extrafields (fk_object);
+
 -- end of migration

Dolibarr/dolibarr

commit 790b4076f5d10d863cb65c9737df2bb52e51fe12

Fix SQL migration error introduced in #38696

htdocs/install/mysql/migration/22.0.0-23.0.0.sql
@@ -441,7 +441,7 @@ INSERT INTO llx_accounting_system (fk_country, pcg_version, label, active) VALUE
 UPDATE llx_const SET name = 'MAIN_AUTHENTICATION_OIDC_ON' WHERE name = 'MAIN_MODULE_OPENIDCONNECT';
 
 -- this table was created only during fresh install
-CREATE llx_workstation_workstation_extrafields
+CREATE TABLE llx_workstation_workstation_extrafields
 (
     rowid           integer     AUTO_INCREMENT PRIMARY KEY,
     tms             timestamp   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,