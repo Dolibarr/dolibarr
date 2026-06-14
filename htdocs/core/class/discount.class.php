3ffe35de096378bdf18596b79dd0e23ce719bb60
Vincent de Grandpré <vincent@de-grandpre.quebec>
2026-06-13 22:37:33 -0400

    remove attributes in favor of parent

--- htdocs/core/class/discount.class.php
+++ htdocs/core/class/discount.class.php
@@ -62,14 +62,6 @@ class DiscountAbsolute extends CommonObject
 	 */
 	public $total_ttc;
 	/**
-	 * @var float
-	 */
-	public $total_localtax1;
-	/**
-	 * @var float
-	 */
-	public $total_total_localtax2;
-	/**
 	 * @var string|float
 	 * @deprecated
 	 */
