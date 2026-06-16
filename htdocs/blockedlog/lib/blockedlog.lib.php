25bdb74e6296eaf1a9646a58565c3d17046b110a
Laurent Destailleur <eldy@users.sourceforge.net>
2026-06-16 03:12:07 +0200

    ci

--- htdocs/blockedlog/lib/blockedlog.lib.php
+++ htdocs/blockedlog/lib/blockedlog.lib.php
@@ -398,7 +398,7 @@ function sumAmountsForUnalterableEvent($block, &$refinvoicefound, &$totalhtamoun
  *
  * @param 	string	$idprof1				Counter ID/value of ne record
  * @param 	string	$registrationnumber		Registration number
- * @param	string	$force					False. Use true for tests.
+ * @param	boolean	$force					False. Use true for tests.
  * @return	string							Obfuscationkey or 'ERROR ...' if error.
  */
 function callApiToGetObfuscationKey($idprof1, $registrationnumber, $force = false)
