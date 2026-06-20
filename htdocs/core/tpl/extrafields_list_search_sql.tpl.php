c83382b53ad2ffb001ca72106e44fd17f2b60021
Laurent Destailleur <eldy@users.sourceforge.net>
2026-06-20 17:37:51 +0200

    CI

--- htdocs/core/tpl/extrafields_list_search_sql.tpl.php
+++ htdocs/core/tpl/extrafields_list_search_sql.tpl.php
@@ -103,8 +103,8 @@ if (!empty($extrafieldsobjectkey) && !empty($search_array_options) && is_array($
 			if (is_array($crit)) {
 				$crit = implode(' ', $crit); // natural_search() expects a string
 			} elseif ($typ === 'select' and is_string($crit) and strpos($crit, ',') === false) {
-				$critSelect = "'".implode("','", array_map(array($db, 'escape'), explode(',', $crit)))."'";
-				$morewhere .= " AND (".$extrafieldsobjectprefix.$tmpkey." IN (".$db->sanitize($critSelect, 1).") )";
+				$sanitizedCritSelect = "'".implode("','", array_map(array($db, 'escape'), explode(',', $crit)))."'";
+				$morewhere .= " AND (".$extrafieldsobjectprefix.$tmpkey." IN (".$sanitizedCritSelect.") )";
 				continue;
 			}
 			$morewhere .= natural_search($extrafieldsobjectprefix.$tmpkey, $crit, $mode_search);
