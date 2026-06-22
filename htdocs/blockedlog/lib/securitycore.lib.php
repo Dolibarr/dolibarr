9fde9fae8eac0d5e7360115d7b4d8c2e9d98d896
MDW <mdeweerd@users.noreply.github.com>
2026-06-22 23:37:54 +0200

    Fix: CI - PHPStan notices, functions.lib.php formatting (#38965)
    * Qual: Remove redundant tests
    
    # Qual: Remove redundant tests
    
    Remove redundant tests as reported by PHPStan
    
    * Qual: Fix missing initialisation
    
    # Qual: Fix missing initialisation
    
    In case of catched exception, $hmac_encoded_secret_key was not defined
    
    * Fix: search parameter re-encoding
    
    # Fix: search parameter re-encoding
    
    * Fix: Add new chain initialization in securitycore.lib.php
    
    # Fix: Add new chain initialization in securitycore.lib.php
    
    - Initialize `$newchain` variable before the loop to avoid potential undefined variable issues
    
    * Qual: Fix formatting in functions.lib.php
    
    - Fixed formatting and spacing issues
    
    * Qual: Cast nullable variable to string
    
    # Qual: Cast nullable variable to string
    
    preg_replace requires a string, ensured by casting nullable to string.
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/blockedlog/lib/securitycore.lib.php
+++ htdocs/blockedlog/lib/securitycore.lib.php
@@ -1,5 +1,6 @@
 <?php
 /* Copyright (C) 2024  Laurent Destailleur     <eldy@users.sourceforge.net>
+ * Copyright (C) 2026		MDW						<mdeweerd@users.noreply.github.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -177,6 +178,8 @@ function dolDecrypt($chain, $key = '', $patterntotest = '')
 			}
 
 			$keys = explode(',', $key);
+
+			$newchain = '';
 
 			// Loop on each possible keys (usually one, but can be more in future if we have a list of keys)
 			foreach ($keys as $tmpkey) {
