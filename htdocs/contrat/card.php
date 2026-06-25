9570b49024021909b1baabd34c36409815db872d
kkhelifa-opendsi <kkhelifa@open-dsi.fr>
2026-06-25 21:28:08 +0200

    FIX: Fix rang of added lines when create a contract from another element (#38856)

--- htdocs/contrat/card.php
+++ htdocs/contrat/card.php
@@ -360,6 +360,7 @@ if (empty($reshook)) {
 
 						$fk_parent_line = 0;
 						$num = count($lines);
+						$rang = 0;
 
 						for ($i = 0; $i < $num; $i++) {
 							$product_type = ($lines[$i]->product_type ? $lines[$i]->product_type : 0);
@@ -429,7 +430,7 @@ if (empty($reshook)) {
 									$lines[$i]->pa_ht,
 									$array_options,
 									(int) $lines[$i]->fk_unit,
-									$num + 1
+									$rang++
 								);
 
 								if ($result < 0) {
