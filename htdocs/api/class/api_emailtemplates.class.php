ed8131db7d546ecd0435534bff1eaebf2a629092
f-hoedl <hoefla14@htl-kaindorf.ac.at>
2026-07-12 12:06:07 +0200

    FIX: Require delete permission for email templates API

--- htdocs/api/class/api_emailtemplates.class.php
+++ htdocs/api/class/api_emailtemplates.class.php
@@ -87,9 +87,9 @@ class EmailTemplates extends DolibarrApi
 	 */
 	public function deleteById($id)
 	{
-		$allowaccess = $this->_checkAccessRights('lire');
+		$allowaccess = $this->_checkAccessRights('supprimer');
 		if (!$allowaccess) {
-			throw new RestException(403, 'denied read access to email templates');
+			throw new RestException(403, 'denied delete access to email templates');
 		}
 
 		$result = $this->email_template->apifetch($id, '');
@@ -125,9 +125,9 @@ class EmailTemplates extends DolibarrApi
 	 */
 	public function deleteByLAbel($label)
 	{
-		$allowaccess = $this->_checkAccessRights('lire');
+		$allowaccess = $this->_checkAccessRights('supprimer');
 		if (!$allowaccess) {
-			throw new RestException(403, 'denied read access to email templates');
+			throw new RestException(403, 'denied delete access to email templates');
 		}
 
 		$result = $this->email_template->apifetch(0, $label);
