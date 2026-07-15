34c78e21b6945abf896a0b6d8f5e7204a4fc8015
HENRY Florian <florian.henry@open-concept.pro>
2026-07-16 01:29:56 +0200

    fix: avoid Access refused to xxx.xxx.xxx.xxx by SQL or Script injection protection in main.inc.php:analyseVarsForSqlAndScriptsInjection type=1 (#39188)
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/contrat/messaging.php
+++ htdocs/contrat/messaging.php
@@ -182,7 +182,7 @@ $morehtmlref .= $form->editfieldval("RefSupplier", 'ref_supplier', $object->ref_
 // Thirdparty
 $morehtmlref .= '<br>'.$object->thirdparty->getNomUrl(1);
 if (!getDolGlobalString('MAIN_DISABLE_OTHER_LINK') && $object->thirdparty->id > 0) {
-	$morehtmlref .= ' (<a href="'.DOL_URL_ROOT.'/contrat/list.php?socid='.$object->thirdparty->id.'&search_name='.urlencode($object->thirdparty->name).'">'.$langs->trans("OtherContracts").'</a>)';
+	$morehtmlref .= ' (<a href="'.DOL_URL_ROOT.'/contrat/list.php?socid='.$object->thirdparty->id.'">'.$langs->trans("OtherContracts").'</a>)';
 }
 // Project
 if (isModEnabled('project')) {
