8b5b510bcece40058c35d323a682867bc5e763d2
Frédéric FRANCE <frederic34@users.noreply.github.com>
2026-07-31 16:22:27 +0200

    Update 24.0.0-25.0.0.sql

--- htdocs/install/mysql/migration/24.0.0-25.0.0.sql
+++ htdocs/install/mysql/migration/24.0.0-25.0.0.sql
@@ -50,8 +50,15 @@ ALTER TABLE llx_societe_perentity ADD COLUMN cond_reglement tinyint DEFAULT NULL
 ALTER TABLE llx_societe_perentity ADD COLUMN mode_reglement_supplier tinyint DEFAULT NULL;
 ALTER TABLE llx_societe_perentity ADD COLUMN cond_reglement_supplier tinyint DEFAULT NULL;
 
-
-
+-- extrafields for links
+CREATE TABLE llx_links_extrafields
+(
+  rowid                     integer AUTO_INCREMENT PRIMARY KEY,
+  tms                       timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  fk_object                 integer NOT NULL,
+  import_key                varchar(14)                             -- import key
+) ENGINE=innodb;
+ALTER TABLE llx_links_extrafields ADD UNIQUE INDEX uk_links_extrafields (fk_object);
 
 
 -- end of migration
