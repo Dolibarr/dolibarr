d60b7f473f3c56a464fb5e3f25d2f293a5f799cc
Anthony Berton <34568357+BB2A-Anthony@users.noreply.github.com>
2026-08-10 18:19:00 +0200

    Update eldy.lib.php

--- htdocs/core/menus/standard/eldy.lib.php
+++ htdocs/core/menus/standard/eldy.lib.php
@@ -11,6 +11,7 @@
  * Copyright (C) 2024-2025	MDW							<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024-2026	Alexandre Spangaro			<alexandre@inovea-conseil.com>
  * Copyright (C) 2025       Josep Lluís Amador          <joseplluis@lliuretic.cat>
+ * Copyright (C) 2026		Anthony Berton				<anthony.berton@bb2a.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
