c81e771d7c84ae8668b03ab462c24c75a58545e7
Anthony Berton <34568357+BB2A-Anthony@users.noreply.github.com>
2026-08-10 18:19:58 +0200

    Update copyright information in modKnowledgeManagement

--- htdocs/core/modules/modKnowledgeManagement.class.php
+++ htdocs/core/modules/modKnowledgeManagement.class.php
@@ -3,7 +3,8 @@
  * Copyright (C) 2018-2019  Nicolas ZABOURI         <info@inovea-conseil.com>
  * Copyright (C) 2019-2024	Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2021 SuperAdmin
- * Copyright (C) 2024-2026	MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2024-2026	MDW						<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2026		Anthony Berton			<anthony.berton@bb2a.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
