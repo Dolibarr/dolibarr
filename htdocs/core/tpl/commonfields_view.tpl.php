9bd16ba111d09d474273058de40a350cfe01ffc4
Anthony Berton <34568357+BB2A-Anthony@users.noreply.github.com>
2026-08-10 18:17:16 +0200

    Update commonfields_view.tpl.php

--- htdocs/core/tpl/commonfields_view.tpl.php
+++ htdocs/core/tpl/commonfields_view.tpl.php
@@ -2,6 +2,7 @@
 /* Copyright (C) 2017       Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2024-2025  Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2025		MDW						<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2026		Anthony Berton			<anthony.berton@bb2a.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
