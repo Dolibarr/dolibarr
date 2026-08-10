e68129f4fdd4e132ea44a949f94550f7d17ac94f
Anthony Berton <34568357+BB2A-Anthony@users.noreply.github.com>
2026-08-10 18:19:32 +0200

    Add copyright for Anthony Berton
    Added copyright notice for Anthony Berton.

--- htdocs/core/modules/modFicheinter.class.php
+++ htdocs/core/modules/modFicheinter.class.php
@@ -6,6 +6,7 @@
  * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@inodbox.com>
  * Copyright (C) 2026      Frédéric France      <frederic.france@free.fr>
  * Copyright (C) 2026	   Charlene Benke		<charlene@patas-monkey.com>
+ * Copyright (C) 2026		Anthony Berton		<anthony.berton@bb2a.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
