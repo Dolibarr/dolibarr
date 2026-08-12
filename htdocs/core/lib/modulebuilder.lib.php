f7bb5c1d08408da87323e2826ddfdde43c7843ba
Florian HENRY <florian.henry@open-concept.pro>
2026-08-12 14:51:46 +0200

    fix: module builder rebuild class mistype foreignkey so delete them

--- htdocs/core/lib/modulebuilder.lib.php
+++ htdocs/core/lib/modulebuilder.lib.php
@@ -156,7 +156,7 @@ function rebuildObjectClass($destdir, $module, $objectname, $newmask, $readdir =
 					$texttoinsert .= ' "index" => "'.(int) $val['index'].'",';
 				}
 				if (!empty($val['foreignkey'])) {
-					$texttoinsert .= ' "foreignkey" => "'.$val['foreignkey'].'",';
+					$texttoinsert .= ' "foreignkey" => "'.dol_escape_php($val['foreignkey']).'",';
 				}
 				if (!empty($val['searchall'])) {
 					$texttoinsert .= ' "searchall" => "'.(int) $val['searchall'].'",';
