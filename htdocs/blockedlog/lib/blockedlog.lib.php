6b3b1e05894d501ff9858f9c951cbe92b88f698b
Laurent Destailleur <eldy@destailleur.fr>
2026-08-13 20:13:30 +0200

    Doc

--- htdocs/blockedlog/lib/blockedlog.lib.php
+++ htdocs/blockedlog/lib/blockedlog.lib.php
@@ -39,7 +39,10 @@ function getBlockedLogVersionToShow()
 	// Protection if we used a past old version not yet certified, we change version shown.
 	if (!constant('CERTIF_LNE')) {	// Hard coded in version
 		// $versionbadge = preg_replace('/^(\d)\./', '\1b.', $versionbadge);	// Not yet required.
-	};
+		if (constant('DOLCERT_NAME') != 'BlockedLog') {
+			$versionbadge = constant('DOLCERT_NAME').'-'.$versionbadge;
+		}
+	}
 
 	return $versionbadge;
 }
