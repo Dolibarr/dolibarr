18db31c0fd2d387297df54f659d93504792a9ae6
Pichinov-Jose <jose.martinez@pichinov.com>
2026-08-23 20:08:07 +0200

    Add copyright

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -19,6 +19,7 @@
  * Copyright (C) 2024-2026	MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2025		Lenin Rivas				<lenin.rivas777@gmail.com>
  * Copyright (C) 2026		Anthony Berton			<anthony.berton@bb2a.fr>
+ * Copyright (C) 2026		Jose Martinez			<jose.martinez@pichinov.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
