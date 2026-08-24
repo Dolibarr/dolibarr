e1e9805d430ba5a3e114a1206407cf27462008d5
atm-jonathan <jonathan.lescaut@atm-consulting.fr>
2026-08-24 15:19:39 +0200

    FIX : task ref numbering uses current year instead of project one
    The task numbering modules (mod_task_simple, mod_task_universal) build the
    mask from $object->date_c, expecting a Task. projet/tasks.php was passing
    the Project, so creating a task on an old project produced a ref with the
    project creation year (TK23...) instead of the current one.
    
    Also fix Task::createFromClone(), which computed the ref before setting the
    new creation date, making a clone inherit the source task year.

--- htdocs/projet/class/task.class.php
+++ htdocs/projet/class/task.class.php
@@ -2273,6 +2273,8 @@ class Task extends CommonObjectLine
 
 		$origin_task->fetch($fromid);
 
+		$clone_task->date_c = $datec;	// Set the new creation date before computing the ref, else the numbering mask uses the source task year
+
 		$defaultref = '';
 		$obj = !getDolGlobalString('PROJECT_TASK_ADDON') ? 'mod_task_simple' : $conf->global->PROJECT_TASK_ADDON;
 		if (getDolGlobalString('PROJECT_TASK_ADDON') && is_readable(DOL_DOCUMENT_ROOT."/core/modules/project/task/" . getDolGlobalString('PROJECT_TASK_ADDON').".php")) {
@@ -2288,7 +2290,6 @@ class Task extends CommonObjectLine
 		$clone_task->ref				= $defaultref;
 		$clone_task->fk_project = $project_id;
 		$clone_task->fk_task_parent = $parent_task_id;
-		$clone_task->date_c = $datec;
 		$clone_task->planned_workload = $origin_task->planned_workload;
 		$clone_task->rang = $origin_task->rang;
 		$clone_task->priority = $origin_task->priority;
