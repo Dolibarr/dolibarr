b8fd8d7108fab1306c4e92f05aed3acdd98d7927
Pichinov-Jose <jose.martinez@pichinov.com>
2026-08-25 20:42:37 +0200

    Show the foreign-currency credits info on its own line
    Appended inline after the available-credits sentence, the currency info
    could make the line wider than the invoice card left column and push it
    into the right one. A line break keeps the native sentence width and
    shows the currency detail under it.

--- htdocs/core/class/discount.class.php
+++ htdocs/core/class/discount.class.php
@@ -6,7 +6,7 @@
  * Copyright (C) 2024-2026	MDW							<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024		Noé Cendrier		<noe.cendrier@altairis.fr>
  * Copyright (C) 2026		Vincent de Grandpré	<vincent@de-grandpre.quebec>
- * Copyright (C) 2026		José MARTINEZ		<jose.martinez@pichinov.com>
+ * Copyright (C) 2026		Jose Martinez			<jose.martinez@pichinov.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
