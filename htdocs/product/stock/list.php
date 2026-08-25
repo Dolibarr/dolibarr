568e8bcbb7ffc850cc9554f7a702681ae94ae95a
Régis Houssin <regis.houssin@inodbox.com>
2026-08-25 14:13:06 +0200

    NEW: add completeArrayFields hook call on list pages missing it (#39722)
    Several list pages (proposal, order, intervention, shipment,
    reception, member, stock, project, knowledge record, contact) never
    called the completeArrayFields hook, unlike thirdparty, invoice and
    product/service lists which already do. This left third-party modules
    with no way to register additional columns into the native
    show/hide-columns selector on these pages.

--- htdocs/product/stock/list.php
+++ htdocs/product/stock/list.php
@@ -148,6 +148,10 @@ foreach ($object->fields as $key => $val) {
 // Extra fields
 include DOL_DOCUMENT_ROOT.'/core/tpl/extrafields_list_array_fields.tpl.php';
 
+// Add hook to complete $arrayfields
+$parameters = array('arrayfields' => &$arrayfields);
+$reshook = $hookmanager->executeHooks('completeArrayFields', $parameters, $object, $action); // Note that $action and $object may have been modified by hook
+
 $object->fields = dol_sort_array($object->fields, 'position');
 $arrayfields = dol_sort_array($arrayfields, 'position');
 
