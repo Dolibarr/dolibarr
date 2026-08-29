9f6470f0b90acd3ee0b3f32de6ec6f89b315e1e2
Pichinov-Jose <jose.martinez@pichinov.com>
2026-08-29 09:03:26 +0200

    Add Jose Martinez copyright line on modified files

--- htdocs/product/admin/product_tools.php
+++ htdocs/product/admin/product_tools.php
@@ -3,6 +3,7 @@
  * Copyright (C) 2013-2015 Laurent Destailleur <eldy@users.sourceforge.net>
  * Copyright (C) 2024       Frédéric France             <frederic.france@free.fr>
  * Copyright (C) 2025		MDW							<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2026		Jose Martinez			<jose.martinez@pichinov.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
