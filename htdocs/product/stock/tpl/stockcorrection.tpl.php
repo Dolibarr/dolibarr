bd39950f230148d02b7eb70faa3d14d08b75fb49
Pichinov-Jose <jose.martinez@pichinov.com>
2026-08-29 09:02:57 +0200

    Add Jose Martinez copyright line on modified files

--- htdocs/product/stock/tpl/stockcorrection.tpl.php
+++ htdocs/product/stock/tpl/stockcorrection.tpl.php
@@ -2,6 +2,7 @@
 /* Copyright (C) 2010-2017  Laurent Destailleur     <eldy@users.sourceforge.net>
  * Copyright (C) 2018-2025  Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2024-2025	MDW						<mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2026		Jose Martinez			<jose.martinez@pichinov.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
