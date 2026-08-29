8a44516316531ec96631b38bd0ee247621e5c49c
Pichinov-Jose <jose.martinez@pichinov.com>
2026-08-29 09:37:52 +0200

    Add copyright header

--- htdocs/fourn/facture/card.php
+++ htdocs/fourn/facture/card.php
@@ -16,6 +16,7 @@
  * Copyright (C) 2024-2026	MDW						<mdeweerd@users.noreply.github.com>
  * Copyright (C) 2026		Vincent de Grandpré		<vincent@de-grandpre.quebec>
  * Copyright (C) 2026		Lionel Vessiller		<lvessiller@open-dsi.fr>
+ * Copyright (C) 2026		José MARTINEZ			<jose.martinez@pichinov.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
