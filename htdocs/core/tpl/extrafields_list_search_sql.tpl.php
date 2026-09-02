7b77243d0bdd74ad58ac4af433dc46fce7e3ea92
Frédéric FRANCE <frederic34@users.noreply.github.com>
2026-09-02 21:53:37 +0200

    fix extrafields_list_search_sql.tpl.php

--- htdocs/core/tpl/extrafields_list_search_sql.tpl.php
+++ htdocs/core/tpl/extrafields_list_search_sql.tpl.php
@@ -29,10 +29,10 @@
  * @var ExtraFields $extrafields
  *
  * @var string $sql
- * @var string $extrafieldsobjectkey
- * @var string $search_options_pattern
- * @var string $extrafieldsobjectprefix
- * @var array $search_array_options
+ * @var ?string $extrafieldsobjectkey
+ * @var ?string $search_options_pattern
+ * @var ?string $extrafieldsobjectprefix
+ * @var array<string,mixed> $search_array_options
  */
 '
 @phan-var-force CommonObject $object
