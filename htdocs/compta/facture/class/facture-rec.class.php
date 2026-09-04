f1aea231aff68e6b6d9a198d3ebcb6fbbb05e7e4
Adrien Raze <adrien.raze@atm-consulting.fr>
2026-09-04 09:37:19 +0200

    Precommit

--- htdocs/compta/facture/class/facture-rec.class.php
+++ htdocs/compta/facture/class/facture-rec.class.php
@@ -1467,7 +1467,7 @@ class FactureRec extends CommonInvoice
 			$sql .= ' AND rowid = '.((int) $restrictioninvoiceid);
 		}
 		$sql .= $this->db->order('entity', 'ASC');
-		if(getDolGlobalInt('NB_REC_FACT_CUSTOMER_GEN_BY_CALL')) {
+		if (getDolGlobalInt('NB_REC_FACT_CUSTOMER_GEN_BY_CALL')) {
 			$sql .= $this->db->plimit(getDolGlobalInt('NB_REC_FACT_CUSTOMER_GEN_BY_CALL'));
 		}
 		//print $sql;exit;
