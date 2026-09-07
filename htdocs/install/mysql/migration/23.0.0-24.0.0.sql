f7d677e43e4571d61a0e06caaa1f6b7316e19b0e
Laurent Destailleur <eldy@destailleur.fr>
2026-09-07 17:21:36 +0200

    SEC: A user must also be admin to use the permission create/run scheduled job - reported by Nguyen Viet Tin

--- htdocs/install/mysql/migration/23.0.0-24.0.0.sql
+++ htdocs/install/mysql/migration/23.0.0-24.0.0.sql
@@ -573,4 +573,6 @@ UPDATE llx_const SET name = __ENCRYPT('ACCOUNTANCY_AUXACCOUNT_USE_SEARCH_TO_SELE
 
 ALTER TABLE llx_adherent MODIFY COLUMN societe VARCHAR(128);
 
+DELETE FROM llx_rights_def WHERE module= 'cron' AND perms = 'execute';
+
 -- end of migration
