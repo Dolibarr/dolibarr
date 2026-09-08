398fea8cddee7465d887b12cfa0e457581133952
Laurent Destailleur <eldy@destailleur.fr>
2026-09-08 16:35:42 +0200

    Fix colspan

--- htdocs/core/class/html.formfile.class.php
+++ htdocs/core/class/html.formfile.class.php
@@ -1076,7 +1076,6 @@ class FormFile
 					if (is_object($hookmanager)) {
 						$addcolumforpicto = ($delallowed || $printer || $morepicto);
 						$colspan = (4 + ($addcolumforpicto ? 1 : 0));
-						$colspanmore = 0;
 						$parameters = array('tmpout' => &$tmpout, 'colspan' => ($colspan + $colspanmore), 'socid' => (isset($GLOBALS['socid']) ? $GLOBALS['socid'] : ''), 'id' => (isset($GLOBALS['id']) ? $GLOBALS['id'] : ''), 'modulepart' => $modulepart, 'relativepath' => $relativepath);
 						$res = $hookmanager->executeHooks('formBuilddocLineOptions', $parameters, $file);
 						if (empty($res)) {
@@ -1113,6 +1112,9 @@ class FormFile
 					if ($delallowed || $printer || $morepicto) {
 						$out .= '<td></td>';
 					}
+					if ($colspanmore) {
+						$out .= '<td colspan="'.$colspanmore.'"></td>';
+					}
 					$out .= '</tr>'."\n";
 				}
 				$this->numoffiles++;
@@ -1121,7 +1123,7 @@ class FormFile
 			if (count($file_list) == 0 && count($link_list) == 0 && $headershown) {
 				$addcolumforpicto = ($delallowed || $printer || $morepicto);
 				$colspan = (4 + ($addcolumforpicto ? 1 : 0));
-				$out .= '<tr><td colspan="'.$colspan.'"><span class="opacitymedium">'.$langs->trans("None").'</span></td></tr>'."\n";
+				$out .= '<tr><td colspan="'.($colspan + $colspanmore).'"><span class="opacitymedium">'.$langs->trans("None").'</span></td></tr>'."\n";
 			}
 		}
 
