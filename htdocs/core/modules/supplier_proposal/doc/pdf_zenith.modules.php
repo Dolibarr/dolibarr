4f2b73105a0b60ce40f08335f88779a527eb0b61
MDW <mdeweerd@users.noreply.github.com>
2026-09-12 03:33:16 +0200

    FIX #40005: Correct text truncation for PDFs (now correct when MAIN_DISABLE_TRUNC is on) (#40126)
    * FIX #40005: Correct text truncation for PDFs (incorrect when MAIN_DISABLE_TRUNC is on)
    
    # FIX #40005: Correct text truncation for PDFs (incorrect when MAIN_DISABLE_TRUNC is on)
    
    This commit replaces all instances of dol_trunc with the new pdfTruncateText method in various PDF generation modules.
    
    This is required to cope with `MAIN_DISABLE_TRUNC=1` which converts dol_trunc to a no-op because of CSS based truncation.
    PDF does not rely on CSS.
    
    * Fix: Remove UTF-8 encoding parameter in pdfTruncateText calls
    
    # Fix: Remove UTF-8 encoding parameter in pdfTruncateText calls
    
    The changes remove the UTF-8 encoding parameter from pdfTruncateText calls in the following files:
    - htdocs/core/modules/action/doc/pdf_standard_actions.class.php
    - htdocs/core/modules/facture/doc/pdf_octopus.modules.php
    - htdocs/core/modules/facture/doc/pdf_sponge.modules.php
    
    This parameter is not needed after changing dol_substr to pdfTruncateText.
    
    Highlighted by @pixodeo.
    
    * refactor: Move pdfTruncateText method to pdf.lib.php
    
    The pdfTruncateText method has been moved from CommonDocGenerator class to pdf.lib.php and renamed to pdf_truncate_text.
    
    * No ellipsis where none requested in original code
    
    ---------
    
    Co-authored-by: Laurent Destailleur <eldy@destailleur.fr>

--- htdocs/core/modules/supplier_proposal/doc/pdf_zenith.modules.php
+++ htdocs/core/modules/supplier_proposal/doc/pdf_zenith.modules.php
@@ -6,7 +6,7 @@
  * Copyright (C) 2015      Marcos García         <marcosgdf@gmail.com>
  * Copyright (C) 2017      Ferran Marcet         <fmarcet@2byte.es>
  * Copyright (C) 2018-2026  Frédéric France       <frederic.france@free.fr>
- * Copyright (C) 2024-2025	MDW					 <mdeweerd@users.noreply.github.com>
+ * Copyright (C) 2024-2026	MDW					 <mdeweerd@users.noreply.github.com>
  * Copyright (C) 2024	   Nick Fragoulis
  *
  * This program is free software; you can redistribute it and/or modify
@@ -1274,7 +1274,7 @@ class pdf_zenith extends ModelePDFSupplierProposal
 				$posy += 3;
 				$pdf->SetXY($posx, $posy);
 				$pdf->SetTextColor(0, 0, 60);
-				$pdf->MultiCell(100, 3, $outputlangs->transnoentities("Project")." : ".(empty($object->project->title) ? '' : dol_trunc($object->project->title, 50)), '', 'R');
+				$pdf->MultiCell(100, 3, $outputlangs->transnoentities("Project")." : ".(empty($object->project->title) ? '' : pdf_truncate_text($pdf, $object->project->title, 50)), '', 'R');
 			}
 		}
 
