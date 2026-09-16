369d0c2a956cc36f509da1f3489a5f0b383251c7
Laurent Destailleur <eldy@destailleur.fr>
2026-09-16 04:01:26 +0200

    Add docker for ia agent

---
+++ dev/build/docker-vibe/vibes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Build image
+#sudo docker build dev/build/docker-vibe -t docker-vibe --no-cache
+sudo docker build dev/build/docker-vibe -t docker-vibe
+
+# Run image
+export GIT_DIR=$(basename "$PWD")
+sudo docker run --rm -it -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -e HOST_USER="$(id -un)" -e HOST_GROUP="$(id -un)" --network=host -v "$HOME/git/test:/test" -v "$HOME/.vibe:/home/$(id -un)/.vibe" --mount type=bind,src="$HOME/git/$GIT_DIR",dst="/$GIT_DIR" -w "/$GIT_DIR" docker-vibe bash
