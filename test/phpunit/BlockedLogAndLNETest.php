406dbed4b42a96947690f9c9828ef00b80c9ab8f
Laurent Destailleur <eldy@destailleur.fr>
2026-09-17 20:48:42 +0200

    Merge branch 'develop' of git@github.com:Dolibarr/dolibarr.git into develop

--- test/phpunit/BlockedLogAndLNETest.php
+++ test/phpunit/BlockedLogAndLNETest.php
@@ -73,7 +73,7 @@ class BlockedLogAndLNETest extends CommonClassTest
 
 		$moduleiniterror = 0;
 
-		print 'BlockedLogAndLNETest var_dump(mysoc) = '.var_dump($mysoc)."\n";
+		print 'BlockedLogAndLNETest var_dump(mysoc) = '.var_export($mysoc, true)."\n";
 		print 'BlockedLogAndLNETest mysoc country_code = '.$mysoc->country_code."\n";
 
 		//$result = $blockedlogmodule->remove();
