ba0e5d969a46447c733992e6f3b98b7927a322b9
Laurent Destailleur <eldy@destailleur.fr>
2026-09-18 16:50:26 +0200

    AI setup

--- dev/build/docker-vibe/vibes.sh
+++ dev/build/docker-vibe/vibes.sh
@@ -7,4 +7,15 @@ sudo docker build dev/build/docker-vibe -t docker-vibe
 # Run image
 GIT_DIR=$(basename "$PWD")
 export GIT_DIR
-sudo docker run --rm -it -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -e HOST_USER="$(id -un)" -e HOST_GROUP="$(id -un)" --network=host -v "$HOME/.vibe:/home/$(id -un)/.vibe" --mount "type=bind,src=$HOME/git/$GIT_DIR,dst=/$GIT_DIR" -w "/$GIT_DIR" docker-vibe bash
+
+sudo docker run --rm -it \
+  -e HOST_UID="$(id -u)" \
+  -e HOST_GID="$(id -g)" \
+  -e HOST_USER="$(id -un)" \
+  -e HOST_GROUP="$(id -un)" \
+  --network=host \
+  --mount "type=bind,src=$HOME/git/$GIT_DIR,dst=/$GIT_DIR" \
+  --mount "type=bind,src=$HOME/.vibe,dst=/home/$(id -un)/.vibe" \
+  --mount "type=bind,src=$HOME/.ssh/github-token,dst=/home/$(id -un)/.ssh/github-token,readonly" \
+  -w "/$GIT_DIR" \
+  docker-vibe bash
