58f8e297bb84cf7989f05c10e361d06224be550c
PsyCrow <93346975+PsyCrow-code@users.noreply.github.com>
2026-09-21 16:38:09 -0600

    Fix HTML output for import button link

--- htdocs/reception/tpl/linkedobjectblock.tpl.php
+++ htdocs/reception/tpl/linkedobjectblock.tpl.php
@@ -52,7 +52,7 @@ foreach ($linkedObjectBlock as $key => $objectlink) {
 	<tr class="<?php echo $trclass; ?>">
 		<td class="linkedcol-element tdoverflowmax100">
 		<?php if (!empty($showImportButton) && $conf->global->MAIN_ENABLE_IMPORT_LINKED_OBJECT_LINES) {
-			print '<a class="objectlinked_importbtn" href="'.$objectlink->getNomUrl(0, '', 0, 1).'&amp;action=selectlines"  data-element="'.$objectlink->element.'"  data-id="'.$objectlink->id.'"  > <i class="fa fa-indent"></i> </a';
+			print '<a class="objectlinked_importbtn" href="'.$objectlink->getNomUrl(0, '', 0, 1).'&amp;action=selectlines"  data-element="'.$objectlink->element.'"  data-id="'.$objectlink->id.'"  > <i class="fa fa-indent"></i> </a>';
 		} ?>
 		<?php echo $langs->trans("Reception"); ?>
 		</td>
