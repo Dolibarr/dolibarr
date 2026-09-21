6f791ed3288d93fdcb12efb264b5992670cb4eb6
PsyCrow <93346975+PsyCrow-code@users.noreply.github.com>
2026-09-21 16:37:00 -0600

    FIX Keep import icon visible in linked object block

--- htdocs/ticket/tpl/linkedobjectblock.tpl.php
+++ htdocs/ticket/tpl/linkedobjectblock.tpl.php
@@ -46,10 +46,11 @@ foreach ($linkedObjectBlock as $key => $objectlink) {
 	}
 	?>
 	<tr class="<?php echo $trclass; ?>" >
-		<td class="linkedcol-element tdoverflowmax100"><?php echo $langs->trans("Ticket"); ?>
+		<td class="linkedcol-element tdoverflowmax100">
 		<?php if (!empty($showImportButton) && $conf->global->MAIN_ENABLE_IMPORT_LINKED_OBJECT_LINES) {
-			print '<a class="objectlinked_importbtn" href="'.$objectlink->getNomUrl(0, '', 0, 1).'&amp;action=selectlines"  data-element="'.$objectlink->element.'"  data-id="'.$objectlink->id.'"  > <i class="fa fa-indent"></i> </a';
+			print '<a class="objectlinked_importbtn" href="'.$objectlink->getNomUrl(0, '', 0, 1).'&amp;action=selectlines"  data-element="'.$objectlink->element.'"  data-id="'.$objectlink->id.'"  > <i class="fa fa-indent"></i> </a>';
 		} ?>
+		<?php echo $langs->trans("Ticket"); ?>
 		</td>
 		<td class="linkedcol-name tdoverflowmax150"><?php echo $objectlink->getNomUrl(1); ?></td>
 		<td class="linkedcol-ref center"><?php echo $objectlink->ref_client; ?></td>
