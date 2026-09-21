d0b5983feb445504ce72eedbaaa919de62ddfc7a
Eric Seigne <eric.seigne@cap-rel.fr>
2026-09-21 23:35:49 +0200

    migration 24 to 25

--- htdocs/install/mysql/migration/24.0.0-25.0.0.sql
+++ htdocs/install/mysql/migration/24.0.0-25.0.0.sql
@@ -268,3 +268,78 @@ ALTER TABLE llx_ai_request_log ADD COLUMN tokens_output integer;
 ALTER TABLE llx_ai_request_log ADD COLUMN model varchar(255);
 
 ALTER TABLE llx_actioncomm ADD COLUMN registration_enabled smallint NOT NULL DEFAULT 0 AFTER max_participants;
+
+-- NEW schemas table and schemas extrafields
+CREATE TABLE llx_schemas (
+    rowid 			integer AUTO_INCREMENT PRIMARY KEY,
+	-- uuid from json file
+    uuid 			varchar(64) NOT NULL,
+	-- name of object like keyboard, mouse, screen ...
+    name 			varchar(64) NOT NULL,
+	-- name displayed to user
+    label 			varchar(255) NOT NULL,
+	-- kind of schema (family like "cerfa")
+    schema_kind 	varchar(32) NULL,
+	-- long description
+    description 	text,
+	-- json for meta object composed of other parts
+	-- note even if that is a json store is TEXT for "old" version of mariadb
+    composed_of 	JSON NULL,
+	-- orignal json in case of remote deleted file (for example)
+    json_schema 	JSON,
+	-- 0 disabled / 1 enabled
+    active 			integer DEFAULT 1 NOT NULL,
+	-- note : version is in json and parent uuid is in json too
+    date_creation 	datetime NOT NULL,
+    tms 			timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    fk_user_creat 	integer,
+    fk_user_modif 	integer,
+    entity 			integer DEFAULT 1 NOT NULL
+) ENGINE=innodb;
+
+CREATE TABLE llx_extrafields_schemas (
+	rowid 			integer AUTO_INCREMENT PRIMARY KEY,
+    name 			varchar(64) NOT NULL,
+    entity 			integer DEFAULT 1 NOT NULL,
+    label 			varchar(255) NOT NULL,
+	-- there is no element type because there is only one def of schema
+	-- and link will be on element_element
+	-- like elementtype note some new type will become like note_audio ...
+    type 			varchar(64),
+    size 			varchar(8) DEFAULT NULL,
+    fieldcomputed 	text,
+    fielddefault 	text,
+    fieldunique 	integer DEFAULT 0,
+    fieldrequired 	integer DEFAULT 0,
+    perms 			varchar(255),
+    enabled 		varchar(255),
+    pos 			integer DEFAULT 0,
+    alwayseditable 	integer DEFAULT 0,
+    emptyonclone 	integer DEFAULT 0,
+    param 			text,
+    list 			varchar(255) DEFAULT '1',
+	printable 		integer DEFAULT 0,
+	-- is the extrafield output on tooltip
+	showintooltip	integer DEFAULT 0,
+    totalizable 	boolean default false,
+    langs 			varchar(64),
+    help 			text,
+	-- a prompt to autofill the value with AI
+	aiprompt		text,
+    css 			varchar(255),
+    cssview 		varchar(255),
+    csslist 		varchar(255),
+	-- 1 if field contains personal data (GDPR/nLPD/LGPD)
+    personal_data	integer DEFAULT 0,
+	-- user making creation
+	fk_user_author	integer,
+	-- user making last change
+	fk_user_modif	integer,
+	-- link to schema
+    fk_schema 		integer NOT NULL,
+	-- date of creation
+	datec			datetime,
+	-- last modification date
+	tms             timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=innodb;
+
