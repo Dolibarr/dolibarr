7795e20df4ec6a13297852a202873b42695f539d
Braito <braito4@hotmail.com>
2026-09-21 11:02:42 +0200

    NEW Add independent conference session registration

--- test/phpunit/ActionCommTest.php
+++ test/phpunit/ActionCommTest.php
@@ -93,6 +93,7 @@ class ActionCommTest extends CommonClassTest
 		$localobject->datep       = $now;
 		$localobject->datef       = $now;
 		$localobject->max_participants = 25;
+		$localobject->registration_enabled = 1;
 		$localobject->percentage  = -1;   // Not applicable
 		$localobject->socid       = 0;
 		$localobject->contactid   = 0;
@@ -140,6 +141,7 @@ class ActionCommTest extends CommonClassTest
 
 		$this->assertLessThan($result, 0);
 		$this->assertSame(25, (int) $localobject->max_participants);
+		$this->assertSame(1, (int) $localobject->registration_enabled);
 		print __METHOD__." id=".$id." result=".$result."\n";
 		return $localobject;
 	}
@@ -163,6 +165,7 @@ class ActionCommTest extends CommonClassTest
 
 		$localobject->label = 'New label';
 		$localobject->max_participants = 30;
+		$localobject->registration_enabled = 0;
 		$result = $localobject->update($user);
 
 		$this->assertLessThan($result, 0);
@@ -170,6 +173,7 @@ class ActionCommTest extends CommonClassTest
 		$resultfetch = $updatedobject->fetch($localobject->id);
 		$this->assertLessThan($resultfetch, 0);
 		$this->assertSame(30, (int) $updatedobject->max_participants);
+		$this->assertSame(0, (int) $updatedobject->registration_enabled);
 		print __METHOD__." id=".$localobject->id." result=".$result."\n";
 		return $localobject->id;
 	}
