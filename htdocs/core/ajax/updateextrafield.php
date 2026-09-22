9812a51ccfc15bc82a40589d4b7a5c013cefc6d9
Laurent Destailleur <eldy@destailleur.fr>
2026-09-22 20:01:54 +0200

    Add a protection on modified fields

--- htdocs/core/ajax/updateextrafield.php
+++ htdocs/core/ajax/updateextrafield.php
@@ -79,9 +79,18 @@ if ($usesublevelpermission && !$user->hasRight($module, $element, 'write') && !$
 
 restrictedArea($user, $object->module, $object, $object->table_element, $usesublevelpermission);
 
-// TODO
 // Add blacklist of some forbidden field name.
-
+$blacklistedfields = array('pass', 'pass_crypted', 'pass_temp', 'api_key', 'openid', 'admin', 'status', 'statut');
+$canreadsalary = ((isModEnabled('salaries') && $user->hasRight('salaries', 'read')) || !isModEnabled('salaries'));
+if (!$canreadsalary) {
+	$blacklistedfields[] = 'salary';
+	$blacklistedfields[] = 'salaryextra';
+	$blacklistedfields[] = 'thm';
+	$blacklistedfields[] = 'tjm';
+}
+if (in_array($field, $blacklistedfields)) {
+	access_forbidden("Can't edit a field blacklisted with name ".$field);
+}
 
 
 /*
